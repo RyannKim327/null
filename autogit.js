ae0b14973453c999c516a08491cae5b2fd753c8c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:49:03 +0800

    1-7-2024 7:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,65 @@
-let str = "Hello, world!";
-let reversedStr = str.split('').reverse().join('');
-console.log(reversedStr);
-let str = "Hello, world!";
-let reversedStr = '';
-for (let i = str.length-1; i >= 0; i--) {
-    reversedStr += str[i];
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  // ...
 }
-console.log(reversedStr);
-let str = "Hello, world!";
-let reversedStr = Array.prototype.reduce.call(str, (reversed, char) => char + reversed, '');
-console.log(reversedStr);
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+  // ...
+}
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  // ...
+}
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  }
+
+  // ...
+}
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return binarySearch(arr, target, start, mid - 1);
+  }
+
+  // ...
+}
+function binarySearch(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
+  }
+
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return binarySearch(arr, target, start, mid - 1);
+  } else {
+    return binarySearch(arr, target, mid + 1, end);
+  }
+}
+const arr = [2, 4, 6, 8, 10, 12, 14, 16];
+const target = 10;
+const index = binarySearch(arr, target);
+console.log(index); // Output: 4
