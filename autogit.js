f5b71e1f9bea66ff79c300c909ecc523b624c258
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:44:06 +0800

    1-7-2024 7:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,41 @@
-Area = (base * height) / 2
-// Function to calculate the area of a triangle
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
+function fibonacci(limit) {
+  var fib = [0, 1];
+  for (var i = 2; i < limit; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
 }
+function fibonacciSearch(arr, target) {
+  var fibArray = fibonacci(arr.length);
+  var offset = -1;
+  
+  var i = fibArray.length - 1;
+  
+  while (i > 1) {
+    var index = Math.min(offset + fibArray[i - 2], arr.length - 1);
+  
+    if (arr[index] < target) {
+      i--;
+      offset = index;
+    } else if (arr[index] > target) {
+      i -= 2;
+    } else {
+      return index;
+    }
+  }
+  
+  if (fibArray[1] === target && arr[offset + 1] === target) {
+    return offset + 1;
+  }
+  
+  return -1;
+}
+var arr = [1, 3, 5, 7, 9, 11, 13];
+var target = 7;
 
-// Example usage
-var triangleBase = 5;
-var triangleHeight = 8;
-var triangleArea = calculateTriangleArea(triangleBase, triangleHeight);
-console.log("The area of the triangle is: " + triangleArea);
+var index = fibonacciSearch(arr, target);
+if (index !== -1) {
+  console.log("Element", target, "found at index", index);
+} else {
+  console.log("Element", target, "not found in the array");
+}
