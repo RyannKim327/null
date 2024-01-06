c54504de12d18daab3bcbdc8167938f8f4933077
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:56:40 +0800

    1-7-2024 7:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,7 @@
-function isAnagram(str1, str2) {
-  // Convert strings to lowercase and remove non-alphanumeric characters
-  str1 = str1.toLowerCase().replace(/[\W_]/g, '');
-  str2 = str2.toLowerCase().replace(/[\W_]/g, '');
+const pattern = "abc";
+const text = "ababcabcabcabc";
 
-  // Sort the characters of both strings
-  const sortedStr1 = str1.split('').sort().join('');
-  const sortedStr2 = str2.split('').sort().join('');
+const matchIndexes = boyerMooreSearch(pattern, text);
 
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
-}
-
-// Example usage
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-  console.log("The strings are anagrams.");
-} else {
-  console.log("The strings are not anagrams.");
-}
+console.log("Match indexes:", matchIndexes);
+Match indexes: [2, 5, 8, 11, 14]
