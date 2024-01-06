c0eb318d451e445810bb022e0e4b77302e9414fb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:45:40 +0800

    1-7-2024 7:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,6 @@
-class Graph {
-  constructor() {
-    this.vertices = [];
-    this.adjList = new Map();
-  }
+let str = 'Hello   World!   ';
 
-  addVertex(vertex) {
-    this.vertices.push(vertex);
-    this.adjList.set(vertex, []);
-  }
+// Remove all spaces
+let strWithoutSpaces = str.replace(/ /g, '');
 
-  addEdge(v, w) {
-    this.adjList.get(v).push(w);
-    this.adjList.get(w).push(v);
-  }
-}
-function breadthFirstSearch(graph, startVertex) {
-  const visited = new Map();
-  const queue = [];
-
-  visited.set(startVertex, true);
-  queue.push(startVertex);
-
-  while (queue.length > 0) {
-    const vertex = queue.shift();
-    const neighbors = graph.adjList.get(vertex);
-
-    console.log(vertex); // Do something with the visited vertex, e.g., print it
-
-    for (const neighbor of neighbors) {
-      if (!visited.get(neighbor)) {
-        visited.set(neighbor, true);
-        queue.push(neighbor);
-      }
-    }
-  }
-}
-const graph = new Graph();
-
-// Add vertices
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
-
-// Add edges
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('B', 'E');
-graph.addEdge('C', 'F');
-breadthFirstSearch(graph, 'A');
+console.log(strWithoutSpaces); // Output: "HelloWorld!"
