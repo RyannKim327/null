ae135b07ec71958c78ebc1ad7c9b7a14edab0ad3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:57:20 +0800

    1-7-2024 7:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,5 @@
-function isPrime(number) {
-  // Check if the number is less than 2
-  if (number < 2) {
-    return false;
-  }
+var str = "123";
+var num = parseInt(str);
 
-  // Iterate from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    // If the number is divisible by i, it is not prime
-    if (number % i === 0) {
-      return false;
-    }
-  }
-
-  // If the number is not divisible by any number from 2 to its square root, it is prime
-  return true;
-}
-const number = 17;
-if (isPrime(number)) {
-  console.log(`${number} is prime`);
-} else {
-  console.log(`${number} is not prime`);
-}
+console.log(num);  // Output: 123
+console.log(typeof num);  // Output: number
