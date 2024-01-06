e2e28f846597ad769acab36247d14fcfbdba6e36
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:51:40 +0800

    1-7-2024 7:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,74 +1,63 @@
-function initializeGraph(vertices, edges) {
-   const graph = {};
-   for (let i = 0; i < vertices.length; i++) {
-      const vertex = vertices[i];
-      graph[vertex] = [];
-   }
+class Graph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
 
-   for (let i = 0; i < edges.length; i++) {
-      const edge = edges[i];
-      const src = edge[0];
-      const dest = edge[1];
-      const weight = edge[2];
-      graph[src].push({ dest, weight });
-   }
+  // Add a vertex/node to the graph
+  addVertex(vertex) {
+    if (!this.adjacencyList.has(vertex)) {
+      this.adjacencyList.set(vertex, []);
+    }
+  }
 
-   return graph;
-}
-function bellmanFord(graph, start) {
-   const vertices = Object.keys(graph);
-   const distances = {};
-   const parents = {};
-   for (let i = 0; i < vertices.length; i++) {
-      const vertex = vertices[i];
-      distances[vertex] = Infinity;
-      parents[vertex] = null;
-   }
-   distances[start] = 0;
+  // Add an edge/connection between two vertices/nodes
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList.has(vertex1) && this.adjacencyList.has(vertex2)) {
+      const connections1 = this.adjacencyList.get(vertex1);
+      const connections2 = this.adjacencyList.get(vertex2);
+      connections1.push(vertex2);
+      connections2.push(vertex1);
+    }
+  }
 
-   for (let i = 0; i < vertices.length - 1; i++) {
-      for (let j = 0; j < vertices.length; j++) {
-         const vertex = vertices[j];
-         const edges = graph[vertex];
-         for (let k = 0; k < edges.length; k++) {
-            const edge = edges[k];
-            const dest = edge.dest;
-            const weight = edge.weight;
-            if (distances[vertex] + weight < distances[dest]) {
-               distances[dest] = distances[vertex] + weight;
-               parents[dest] = vertex;
-            }
-         }
+  // Depth-First Search algorithm
+  dfs(startVertex) {
+    const visited = new Set();
+    this._dfsRecursive(startVertex, visited);
+  }
+
+  _dfsRecursive(vertex, visited) {
+    visited.add(vertex);
+    console.log(vertex);
+
+    const connections = this.adjacencyList.get(vertex);
+    for (const connection of connections) {
+      if (!visited.has(connection)) {
+        this._dfsRecursive(connection, visited);
       }
-   }
-
-   for (let i = 0; i < vertices.length; i++) {
-      const vertex = vertices[i];
-      const edges = graph[vertex];
-      for (let j = 0; j < edges.length; j++) {
-         const edge = edges[j];
-         const dest = edge.dest;
-         const weight = edge.weight;
-         if (distances[vertex] + weight < distances[dest]) {
-            throw new Error("Negative-weight cycle detected!");
-         }
-      }
-   }
-
-   return { distances, parents };
+    }
+  }
 }
-const vertices = ['A', 'B', 'C', 'D', 'E'];
-const edges = [
-   ['A', 'B', 3],
-   ['B', 'C', 4],
-   ['C', 'D', 2],
-   ['D', 'E', 5],
-   ['E', 'A', -10]
-];
 
-const graph = initializeGraph(vertices, edges);
-const startVertex = 'A';
-const result = bellmanFord(graph, startVertex);
+// Example usage:
+const graph = new Graph();
 
-console.log(result.distances); // Output: { A: 0, B: 3, C: 7, D: 9, E: -5 }
-console.log(result.parents); // Output: { A: null, B: 'A', C: 'B', D: 'C', E: 'D' }
+// Create the nodes/vertices
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+// Create the connections/edges
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+// Perform depth-first search starting from vertex 'A'
+graph.dfs('A');
