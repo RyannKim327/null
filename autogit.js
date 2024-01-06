de45521c30058562365dce62e87831ea8e4253b7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:53:30 +0800

    1-7-2024 7:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,10 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
-}
-
-const originalString = 'Hello World!';
-const reversedString = reverseString(originalString);
-
-console.log(reversedString);  // Outputs: "!dlroW olleH"
-function reverseString(str) {
-  let reversed = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
+function factorial(n) {
+  // Base case
+  if (n === 0) {
+    return 1;
   }
-  return reversed;
+
+  // Recursive case
+  return n * factorial(n - 1);
 }
-
-const originalString = 'Hello World!';
-const reversedString = reverseString(originalString);
-
-console.log(reversedString);  // Outputs: "!dlroW olleH"
-function reverseString(str) {
-  return str.split('').reduce((reversed, char) => char + reversed, '');
-}
-
-const originalString = 'Hello World!';
-const reversedString = reverseString(originalString);
-
-console.log(reversedString);  // Outputs: "!dlroW olleH"
+console.log(factorial(5));  // Output: 120
