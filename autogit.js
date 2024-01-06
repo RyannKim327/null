93da2ead35647ebbeec14c8b5f1f72c07cf93543
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 07:50:11 +0800

    1-7-2024 7:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,8 @@
-function merge(leftArr, rightArr) {
-  let resultArr = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
+let stringNumber = "123";
+let convertedNumber = parseInt(stringNumber);
 
-  // Merge the arrays until one of them is exhausted
-  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-    if (leftArr[leftIndex] < rightArr[rightIndex]) {
-      resultArr.push(leftArr[leftIndex]);
-      leftIndex++;
-    } else {
-      resultArr.push(rightArr[rightIndex]);
-      rightIndex++;
-    }
-  }
+console.log(convertedNumber); // Output: 123
+let stringNumber = "123";
+let convertedNumber = Number(stringNumber);
 
-  // Append the remaining elements (if any) of leftArr
-  while (leftIndex < leftArr.length) {
-    resultArr.push(leftArr[leftIndex]);
-    leftIndex++;
-  }
-
-  // Append the remaining elements (if any) of rightArr
-  while (rightIndex < rightArr.length) {
-    resultArr.push(rightArr[rightIndex]);
-    rightIndex++;
-  }
-
-  return resultArr;
-}
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  // Split the array into two halves
-  const mid = Math.floor(arr.length / 2);
-  const leftArr = arr.slice(0, mid);
-  const rightArr = arr.slice(mid);
-
-  // Recursively sort the two halves
-  const sortedLeftArr = mergeSort(leftArr);
-  const sortedRightArr = mergeSort(rightArr);
-
-  // Merge the sorted halves using the merge function
-  return merge(sortedLeftArr, sortedRightArr);
-}
-const arr = [5, 3, 8, 4, 2, 1, 9, 7, 6];
-console.log(mergeSort(arr));
+console.log(convertedNumber); // Output: 123
