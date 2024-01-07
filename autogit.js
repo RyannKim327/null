41203d7207777ff2e775e5a49cf23b6366680709
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 09:03:11 +0800

    1-7-2024 9:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,23 @@
-// Graph represented as an adjacency list
-const graph = {
-  A: ['B', 'C'],
-  B: ['D', 'E'],
-  C: ['F'],
-  D: [],
-  E: ['F'],
-  F: []
-};
+function isPrime(number) {
+  // Check if the number is less than 2
+  if (number < 2) {
+    return false;
+  }
 
-function depthFirstSearch(graph, start) {
-  const visited = new Set(); // To store visited nodes
-  const stack = [start]; // Start with the initial node
-
-  while (stack.length > 0) {
-    const current = stack.pop(); // Get the last inserted node
-
-    if (!visited.has(current)) {
-      visited.add(current); // Mark the node as visited
-      console.log(current); // Process the current node however you want
-
-      const neighbors = graph[current]; // Get the neighbors of the current node
-      for (let i = neighbors.length - 1; i >= 0; i--) {
-        const neighbor = neighbors[i];
-        if (!visited.has(neighbor)) {
-          stack.push(neighbor); // Push neighbors onto the stack if they haven't been visited
-        }
-      }
+  // Check for divisibility from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
+
+  return true;
 }
 
-// Perform DFS starting from node 'A'
-depthFirstSearch(graph, 'A');
+// Usage example
+const number = 23;
+if (isPrime(number)) {
+  console.log(number + " is a prime number");
+} else {
+  console.log(number + " is not a prime number");
+}
