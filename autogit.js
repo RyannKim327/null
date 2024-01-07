6f8ad866ea6c9f48ada1c1f5695ffad241574691
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:22:12 +0800

    1-7-2024 8:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,9 @@
-// Taking user input using prompt
-let userInput1 = prompt("Enter a number: ");
-let userInput2 = prompt("Enter another number: ");
-
-// Converting user input to numbers
-let number1 = parseFloat(userInput1);
-let number2 = parseFloat(userInput2);
-
-// Performing a basic operation (addition in this case)
-let result = number1 + number2;
-
-// Displaying the result
-console.log("The sum of the two numbers is: " + result);
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the fetched data
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
