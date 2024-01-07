33febb5ac9e9e650c343560337fe9ef1b9507709
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 09:00:36 +0800

    1-7-2024 9:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,12 @@
-class Graph {
-  constructor() {
-    this.adjList = new Map();
-  }
+const axios = require('axios');
 
-  addVertex(vertex) {
-    this.adjList.set(vertex, []);
-  }
-
-  addEdge(v1, v2) {
-    this.adjList.get(v1).push(v2);
-    this.adjList.get(v2).push(v1);
-  }
-
-  getAdjacencyList() {
-    return this.adjList;
-  }
-}
-function depthFirstSearch(graph, start) {
-  const visited = new Set();
-
-  function dfs(vertex) {
-    visited.add(vertex);
-
-    console.log(vertex); // Or perform any other desired operation
-
-    const neighbors = graph.get(vertex);
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        dfs(neighbor);
-      }
-    }
-  }
-
-  dfs(start);
-}
-const graph = new Graph();
-
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-depthFirstSearch(graph.getAdjacencyList(), 'A');
+// Make a GET request to a specific API endpoint
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    // Handle the successful response
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    // Handle any errors that occurred during the request
+    console.error('Error:', error);
+  });
