8576b481bd8b4dbf884e545559d952afcc8570e9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:05:11 +0800

    1-8-2024 0:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,56 @@
-function findMajorityElement(arr) {
-  let count = 0;
-  let candidate;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (count === 0) {
-      candidate = arr[i];
-      count = 1;
-    } else if (arr[i] === candidate) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Verification step to ensure the candidate is the majority element 
-  count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === candidate) {
-      count++;
-    }
-  }
-
-  if (count > arr.length / 2) {
-    return candidate;
-  } else {
-    return "No majority element found";
+class HashTable {
+  constructor(size = 10) {
+    this.size = size;
+    this.buckets = new Array(size);
   }
 }
+HashTable.prototype.hash = function (key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
+  }
+  return hash % this.size;
+};
+HashTable.prototype.set = function (key, value) {
+  const index = this.hash(key);
+  if (!this.buckets[index]) {
+    this.buckets[index] = [];
+  }
+  this.buckets[index].push([key, value]);
+};
+HashTable.prototype.get = function (key) {
+  const index = this.hash(key);
+  if (!this.buckets[index]) {
+    return undefined;
+  }
+  for (let i = 0; i < this.buckets[index].length; i++) {
+    if (this.buckets[index][i][0] === key) {
+      return this.buckets[index][i][1];
+    }
+  }
+  return undefined;
+};
+HashTable.prototype.remove = function (key) {
+  const index = this.hash(key);
+  if (!this.buckets[index]) {
+    return undefined;
+  }
+  for (let i = 0; i < this.buckets[index].length; i++) {
+    if (this.buckets[index][i][0] === key) {
+      const value = this.buckets[index][i][1];
+      this.buckets[index].splice(i, 1);
+      return value;
+    }
+  }
+  return undefined;
+};
+const hashTable = new HashTable();
 
-// Example usage
-const arr = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(arr);
-console.log(majorityElement);  // Output: 2
+hashTable.set('key1', 'value1');
+hashTable.set('key2', 'value2');
+
+console.log(hashTable.get('key1')); // Output: value1
+console.log(hashTable.get('key2')); // Output: value2
+
+hashTable.remove('key1');
+console.log(hashTable.get('key1')); // Output: undefined
