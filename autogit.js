81a84f3e2729a4be179dd53635eaf55cc1ab79d9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:34:03 +0800

    1-7-2024 8:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,8 @@
-function fibonacciSearch(arr, x, n) {
-  if (x === arr[0]) {
-    return 0;
-  }
-
-  let fib2 = 1;
-  let fib1 = 0;
-  let fib = fib1 + fib2;
-
-  while (fib < n) {
-    fib2 = fib1;
-    fib1 = fib;
-    fib = fib1 + fib2;
-  }
-
-  let offset = -1;
-
-  while (fib > 1) {
-    const i = Math.min(offset + fib2, n - 1);
-
-    if (arr[i] < x) {
-      fib = fib1;
-      fib1 = fib2;
-      fib2 = fib - fib1;
-      offset = i;
-    } else if (arr[i] > x) {
-      fib = fib2;
-      fib1 = fib1 - fib2;
-      fib2 = fib - fib1;
-    } else {
-      return i;
-    }
-  }
-
-  if (fib1 === 1 && arr[offset + 1] === x) {
-    return offset + 1;
-  }
-
-  return -1;
-}
-const arr = [1, 4, 7, 9, 14, 21, 30];
-const x = 9;
-const n = arr.length;
-
-const index = fibonacciSearch(arr, x, n);
-
-if (index === -1) {
-  console.log("Element not found in the array.");
-} else {
-  console.log(`Element found at index ${index}.`);
-}
+var string1 = "Hello";
+var string2 = "World";
+var result = string1 + " " + string2;
+console.log(result); // Output: "Hello World"
+var string1 = "Hello";
+var string2 = "World";
+var result = string1.concat(" ", string2);
+console.log(result); // Output: "Hello World"
