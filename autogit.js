164d4238fe4cf572633db147cad63bd1646953d8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:06:36 +0800

    1-7-2024 8:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,43 @@
-let arr = [1, 2, 3, 4, 5];
-let index = arr.indexOf(3); // Find the index of the element you want to remove
-if (index !== -1) {
-  arr.splice(index, 1); // Remove one element starting from the index
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
 }
-console.log(arr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-arr = arr.filter(item => item !== 3); // Create a new array without the element
-console.log(arr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let index = arr.indexOf(3); // Find the index of the element you want to remove
-if (index !== -1) {
-  arr = [...arr.slice(0, index), ...arr.slice(index + 1)]; // Create a new array without the element
+
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
 }
-console.log(arr); // Output: [1, 2, 4, 5]
+
+// Method to add a node to the end of the linked list
+LinkedList.prototype.addNode = function(value) {
+  var newNode = new LinkedListNode(value);
+
+  if (this.head === null) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+};
+
+// Method to find the length of the linked list
+LinkedList.prototype.getLength = function() {
+  var current = this.head;
+  var count = 0;
+
+  while (current !== null) {
+    count++;
+    current = current.next;
+  }
+
+  return count;
+};
+
+// Example usage
+var list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+
+console.log(list.getLength()); // Output: 3
