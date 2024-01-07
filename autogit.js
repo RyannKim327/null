02c05fa76ac56a3aad5b3c91564e3e408acec624
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 09:16:05 +0800

    1-7-2024 9:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,18 @@
-const arr = [1, 5, 3, 9, 2, 7];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-const max = Math.max(...arr);
-console.log(max); // Output: 9
+function getIntersection(head1, head2) {
+  let p1 = head1;
+  let p2 = head2;
+
+  while (p1 !== p2) {
+    p1 = p1 === null ? head2 : p1.next;
+    p2 = p2 === null ? head1 : p2.next;
+  }
+
+  return p1; // or p2, both will point to the intersection (if exists)
+}
