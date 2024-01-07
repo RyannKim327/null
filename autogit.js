452e294d1dc67a1ca7756d6653c4ab3e4a7b6626
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:19:09 +0800

    1-7-2024 8:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,5 @@
-function findMajorityElement(nums) {
-  let majorityElement = nums[0];
-  let count = 1;
+let str = '123';
+let num = parseInt(str);
 
-  for (let i = 1; i < nums.length; i++) {
-    if (count === 0) {
-      majorityElement = nums[i];
-      count = 1;
-    } else if (majorityElement === nums[i]) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Verify if majorityElement is the actual majority element
-  count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === majorityElement) {
-      count++;
-    }
-  }
-
-  if (count > nums.length / 2) {
-    return majorityElement;
-  }
-
-  return null; // No majority element
-}
-
-// Example usage:
-const array = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(array);
-console.log(majorityElement); // Output: 2
+console.log(num); // Output: 123
+console.log(typeof num); // Output: number
