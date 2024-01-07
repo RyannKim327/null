c1c67dada3455a726b218d8e780669107fc56c95
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:10:50 +0800

    1-8-2024 0:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,39 @@
-class Node {
-    constructor(order, leaf) {
-        this.order = order; // Maximum number of keys the node can hold
-        this.keys = []; // Array of keys
-        this.children = []; // Array of child nodes
-        this.leaf = leaf; // Whether the node is a leaf or not
-    }
+function bwt(str) {
+  // Implement the BWT algorithm here
 }
-class BTree {
-    constructor(order) {
-        this.root = null; // Initialize the root node
-        this.order = order; // Maximum number of keys each node can hold
-    }
+function bwt(str) {
+  const rotations = [str];
+  
+  // Add all the rotations of the input string to the `rotations` array
+  for (let i = 1; i < str.length; i++) {
+    rotations.push(str.slice(i) + str.slice(0, i));
+  }
 }
-BTree.prototype.insert = function(key) {
-    if (this.root === null) { // If the tree is empty
-        this.root = new Node(this.order, true); // Create the root node as a leaf
-        this.root.keys.push(key); // Insert the key
-    } else {
-        if (this.root.keys.length === this.order - 1) { // If the root is full
-            const newNode = new Node(this.order, false); // Create a new root node
-            newNode.children.push(this.root); // Make the old root a child of the new root
-            this.splitChild(newNode, 0, this.root); // Split the old root and move 1 key to the new root
-            this.root = newNode; // Set the new root
-        }
-        this.insertNonFull(this.root, key); // Insert the key into the non-full root node
-    }
-};
-BTree.prototype.insertNonFull = function(node, key) {
-    let i = node.keys.length - 1;
-    if (node.leaf) { // If the node is a leaf
-        while (i >= 0 && key < node.keys[i]) {
-            i--;
-        }
-        node.keys.splice(i + 1, 0, key); // Insert the key at the correct position
-    } else { // If the node is not a leaf
-        while (i >= 0 && key < node.keys[i]) {
-            i--;
-        }
-        i++;
-        if (node.children[i].keys.length === this.order - 1) { // If the child is full
-            this.splitChild(node, i, node.children[i]); // Split the child node
-            if (key > node.keys[i]) {
-                i++;
-            }
-        }
-        this.insertNonFull(node.children[i], key); // Recursively insert the key
-    }
-};
-BTree.prototype.splitChild = function(parent, index, child) {
-    const newNode = new Node(this.order, child.leaf); // Create a new node
-    parent.keys.splice(index, 0, child.keys[this.order - 1]); // Move the median key of the child to the parent
-    parent.children.splice(index + 1, 0, newNode); // Add the new node as a child of the parent
-
-    newNode.keys = child.keys.splice(this.order, this.order - 1); // Move the right half of the keys to the new node
-    if (!child.leaf) {
-        newNode.children = child.children.splice(this.order, this.order); // Move the right half of the children to the new node
-    }
-};
+function bwt(str) {
+  const rotations = [str];
+  
+  for (let i = 1; i < str.length; i++) {
+    rotations.push(str.slice(i) + str.slice(0, i));
+  }
+  
+  rotations.sort();
+}
+function bwt(str) {
+  const rotations = [str];
+  
+  for (let i = 1; i < str.length; i++) {
+    rotations.push(str.slice(i) + str.slice(0, i));
+  }
+  
+  rotations.sort();
+  
+  let transformedStr = '';
+  for (const rotation of rotations) {
+    transformedStr += rotation.slice(-1);
+  }
+  
+  return transformedStr;
+}
+const input = 'banana';
+const transformed = bwt(input);
+console.log(transformed);
