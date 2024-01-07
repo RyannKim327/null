585676f579ef66b23e0f5e294f70d50675e67f9f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:03:16 +0800

    1-8-2024 1:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,97 +1,47 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+class HashTable {
+  constructor(size = 100) {
+    this.size = size;
+    this.buckets = new Array(size);
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  isEmpty() {
-    return this.head === null;
-  }
-
-  append(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+    for (let i = 0; i < this.size; i++) {
+      this.buckets[i] = [];
     }
   }
 
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
+  hash(key) {
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
     }
+    return hashValue % this.size;
   }
 
-  delete(value) {
-    if (this.isEmpty()) {
-      return;
-    }
+  insert(key, value) {
+    const index = this.hash(key);
+    this.buckets[index].push({ key, value });
+  }
 
-    if (this.head.value === value) {
-      this.head = this.head.next;
-
-      if (this.head === null) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next !== null && currentNode.next.value !== value) {
-      currentNode = currentNode.next;
-    }
-
-    if (currentNode.next !== null) {
-      currentNode.next = currentNode.next.next;
-
-      if (currentNode.next === null) {
-        this.tail = currentNode;
+  search(key) {
+    const index = this.hash(key);
+    for (let bucket of this.buckets[index]) {
+      if (bucket.key === key) {
+        return bucket.value;
       }
     }
+    return null;
   }
 
-  toArray() {
-    const array = [];
-    let currentNode = this.head;
-
-    while (currentNode !== null) {
-      array.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-
-    return array;
-  }
-
-  toString() {
-    return this.toArray().join(" -> ");
+  remove(key) {
+    const index = this.hash(key);
+    this.buckets[index] = this.buckets[index].filter((bucket) => bucket.key !== key);
   }
 }
-const linkedList = new LinkedList();
-linkedList.append(3);
-linkedList.prepend(2);
-linkedList.append(4);
-linkedList.prepend(1);
+const hashTable = new HashTable();
+hashTable.insert("name", "John");
+hashTable.insert("age", 30);
 
-console.log(linkedList.toArray()); // Output: [1, 2, 3, 4]
-console.log(linkedList.toString()); // Output: 1 -> 2 -> 3 -> 4
+console.log(hashTable.search("name"));  // Output: John
+console.log(hashTable.search("age"));   // Output: 30
 
-linkedList.delete(3);
-console.log(linkedList.toArray()); // Output: [1, 2, 4]
-console.log(linkedList.toString()); // Output: 1 -> 2 -> 4
+hashTable.remove("age");
+console.log(hashTable.search("age"));   // Output: null
