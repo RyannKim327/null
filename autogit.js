0af53d82a52d7960367af69e8655743ea6b22f36
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:48:13 +0800

    1-8-2024 0:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,12 @@
-function countOccurrences(string, word) {
-  // Split the string by spaces to get an array of words
-  var words = string.split(" ");
-  
-  // Use the filter method to create a new array that only contains the target word
-  var filteredWords = words.filter(function (w) {
-    return w === word;
-  });
-  
-  // Return the length of the filtered array, which represents the number of occurrences
-  return filteredWords.length;
+function randomSort(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
-// Example usage
-var sentence = "The quick brown fox jumps over the lazy dog.";
-var targetWord = "the";
-console.log(countOccurrences(sentence, targetWord));  // Output: 2
+// Usage example:
+const array = [1, 2, 3, 4, 5];
+const sortedArray = randomSort(array);
+console.log(sortedArray);
