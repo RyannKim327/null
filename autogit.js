954c5909a304c1b7f5792a1205a8e32309c58193
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:09:01 +0800

    1-7-2024 8:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,16 @@
-let str = "Hello, World! How are you?";
-let withoutSpaces = str.split(" ").join("");
-console.log(withoutSpaces);
-let str = "Hello, World! How are you?";
-let withoutSpaces = str.replace(/\s/g, "");
-console.log(withoutSpaces);
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function getIntersectionNode(headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+
+  while (p1 !== p2) {
+    p1 = p1 === null ? headB : p1.next;
+    p2 = p2 === null ? headA : p2.next;
+  }
+  
+  return p1; // or p2, as they will be either the common node or null (no intersection)
+}
