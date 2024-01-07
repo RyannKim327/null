f54e17a794b2d7deab58d6615fe79dee3ca2cb92
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:02:00 +0800

    1-8-2024 1:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,23 @@
-function findMiddleElement(head) {
-   let slow = head;
-   let fast = head;
-
-   while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
-   }
-
-   return slow;
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  
+  const pivot = arr[arr.length - 1];
+  const leftArr = [];
+  const rightArr = [];
+  
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+  
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 }
+
+// Example usage:
+const array = [8, 4, 2, 9, 1, 5];
+console.log(quickSort(array)); // Output: [1, 2, 4, 5, 8, 9]
