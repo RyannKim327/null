1d19352585bbf3f5efd7f8ce74bd04d908dd06ea
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:08:38 +0800

    1-7-2024 8:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,6 @@
-function getLinkedListLength(head) {
-  let count = 0;
-  let currentNode = head;
-  
-  while (currentNode !== null) {
-    count++;
-    currentNode = currentNode.next;
-  }
-  
-  return count;
-}
+let str = "Hello, World! How are you?";
+let withoutSpaces = str.split(" ").join("");
+console.log(withoutSpaces);
+let str = "Hello, World! How are you?";
+let withoutSpaces = str.replace(/\s/g, "");
+console.log(withoutSpaces);
