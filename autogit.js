12baf85134da42f6dd4fcad2cf1488c83f43a952
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:42:39 +0800

    1-8-2024 0:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,4 @@
-class Node {
-  constructor() {
-    this.children = {};
-    this.indexes = [];
-  }
-}
-class SuffixTree {
-  constructor(input) {
-    this.root = new Node();
-    this.text = input;
-    
-    // Build the suffix tree
-    this.buildSuffixTree();
-  }
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-  // Function to build the suffix tree
-  buildSuffixTree() {
-    const n = this.text.length;
-    for (let i = 0; i < n; i++) {
-      let suffix = this.text.substring(i);
-      this.insertSuffix(suffix, i);
-    }
-  }
-
-  // Function to insert a suffix into the suffix tree
-  insertSuffix(suffix, position) {
-    let currentNode = this.root;
-    for (let i = 0; i < suffix.length; i++) {
-      const char = suffix[i];
-      if (!currentNode.children[char]) {
-        currentNode.children[char] = new Node();
-      }
-      currentNode = currentNode.children[char];
-    }
-    // Store the position of the suffix
-    currentNode.indexes.push(position);
-  }
-}
-// Example usage
-const input = "banana";
-const suffixTree = new SuffixTree(input);
-
-console.log(suffixTree.root);
+console.log(lowercaseStr); // Output: hello world
