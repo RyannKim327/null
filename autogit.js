3fc7aeede2cc07e43383d4d6dda2ddc201b52765
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:51:19 +0800

    1-8-2024 0:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,29 @@
-class Node {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
-  }
+function calculateMean(numbers) {
+   // Code goes here
 }
-
-function isPalindrome(head) {
-  let values = [];
-
-  while (head) {
-    values.push(head.value);
-    head = head.next;
-  }
-
-  let start = 0;
-  let end = values.length - 1;
-
-  while (start < end) {
-    if (values[start] !== values[end]) {
-      return false;
-    }
-    start++;
-    end--;
-  }
-  
-  return true;
+function calculateMean(numbers) {
+   let sum = 0;
+   // Code goes here
 }
+function calculateMean(numbers) {
+   let sum = 0;
+   
+   for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+   }
 
-// Example usage:
-const node1 = new Node('a');
-const node2 = new Node('b');
-const node3 = new Node('c');
-const node4 = new Node('b');
-const node5 = new Node('a');
+   // Code goes here
+}
+function calculateMean(numbers) {
+   let sum = 0;
+   
+   for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+   }
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-
-console.log(isPalindrome(node1)); // Output: true
+   const mean = sum / numbers.length;
+   return mean;
+}
+const numbers = [1, 2, 3, 4, 5];
+const mean = calculateMean(numbers);
+console.log(mean);
