27ebabcd887a15f53c8dc094d73b08ee5cc40107
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:21:41 +0800

    1-7-2024 8:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,13 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  var swapped;
-  
-  do {
-    swapped = false;
-    
-    for (var i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  
-  return arr;
-}
+// Taking user input using prompt
+let userInput1 = prompt("Enter a number: ");
+let userInput2 = prompt("Enter another number: ");
 
-// Example usage
-var array = [5, 3, 8, 4, 2];
-console.log("Original array: ", array);
-console.log("Sorted array: ", bubbleSort(array));
+// Converting user input to numbers
+let number1 = parseFloat(userInput1);
+let number2 = parseFloat(userInput2);
+
+// Performing a basic operation (addition in this case)
+let result = number1 + number2;
+
+// Displaying the result
+console.log("The sum of the two numbers is: " + result);
