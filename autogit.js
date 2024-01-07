64a05b6b48a0aa3734328c36d1d7c8029aa5b55c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:59:09 +0800

    1-7-2024 8:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,22 @@
-let arr = [1, 2, 3, 4, 5];
-let index = arr.indexOf(3); // Find the index of the element to remove
-if (index !== -1) {
-   arr.splice(index, 1);  // Remove one element at the found index
-}
-console.log(arr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-arr = arr.filter(item => item !== elementToRemove);
-console.log(arr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === elementToRemove) {
-        arr.splice(i, 1);
-        break; // Stop the loop once the element is removed
+function findFirstRepeatedCharacter(str) {
+    const charMap = {};
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = true;
+        }
     }
+
+    return null; // No repeated character found
 }
-console.log(arr); // Output: [1, 2, 4, 5]
+const str = "Hello, World!";
+const repeatedChar = findFirstRepeatedCharacter(str);
+
+if (repeatedChar) {
+    console.log("First repeated character:", repeatedChar);
+} else {
+    console.log("No repeated character found.");
+}
