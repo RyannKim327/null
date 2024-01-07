58a66d4840043eb49fdcf7cd8b78ad71c4b794c4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 09:01:44 +0800

    1-7-2024 9:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,17 @@
-function isAnagram(str1, str2) {
-  // Remove non-alphabetic characters and convert to lowercase
-  str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-  str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-  // Create frequency tables for both strings
-  const freqTable1 = createFrequencyTable(str1);
-  const freqTable2 = createFrequencyTable(str2);
-
-  // Compare the character frequency tables
-  return compareFrequencyTables(freqTable1, freqTable2);
-}
-
-function createFrequencyTable(str) {
-  const freqTable = {};
-  for (let char of str) {
-    freqTable[char] = (freqTable[char] || 0) + 1;
-  }
-  return freqTable;
-}
-
-function compareFrequencyTables(freqTable1, freqTable2) {
-  if (Object.keys(freqTable1).length !== Object.keys(freqTable2).length) {
-    return false;
-  }
-
-  for (let key in freqTable1) {
-    if (freqTable1[key] !== freqTable2[key]) {
-      return false;
+function selectionSort(arr) {
+  const len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]];
     }
   }
-
-  return true;
+  return arr;
 }
-
-// Example usage:
-console.log(isAnagram('listen', 'silent')); // Output: true
-console.log(isAnagram('hello', 'world')); // Output: false
+const array = [64, 25, 12, 22, 11];
+console.log(selectionSort(array)); // Output: [11, 12, 22, 25, 64]
