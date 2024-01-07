a6bd67bdc7e02af546acfe92470ced7d413492f5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:00:27 +0800

    1-8-2024 1:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,18 @@
-let str = " Hello World! ";
-let trimmedStr = str.replace(/\s/g, "");
-console.log(trimmedStr); // Output: "HelloWorld!"
-let str = " Hello World! ";
-let trimmedStr = str.trim();
-console.log(trimmedStr); // Output: "Hello World!"
-let str = " Hello World! ";
-let trimmedStr = str.split(" ").join("");
-console.log(trimmedStr); // Output: "HelloWorld!"
+// Fetching random user data from an API
+fetch('https://randomuser.me/api/')
+  .then(response => response.json())
+  .then(data => {
+    // Extracting user information from the API response
+    const user = data.results[0];
+    const name = `${user.name.first} ${user.name.last}`;
+    const email = user.email;
+    const location = `${user.location.city}, ${user.location.country}`;
+    
+    // Logging the user information
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Location:', location);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
