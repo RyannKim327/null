e10922a3fd4f0ac3d9691095c640d060956f13fb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:13:55 +0800

    1-8-2024 1:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,34 @@
-function ListNode(val) {
-  this.val = val;
-  this.next = null;
-}
+function longestCommonSubstring(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  let maxLength = 0; // Length of the longest common substring
+  let endIndex = 0; // End index of the longest common substring
 
-function isLinkedListPalindrome(head) {
-  let slow = head;
-  let fast = head;
-  const stack = [];
+  // Create a 2D array to store the lengths of common substrings
+  const lengths = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
 
-  // Find the middle of the linked list
-  while (fast && fast.next) {
-    stack.push(slow.val);
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  if (fast) {
-    // The linked list is odd in length, skip the middle node
-    slow = slow.next;
-  }
-
-  // Traverse the remaining nodes and compare with stack values
-  while (slow) {
-    if (slow.val !== stack.pop()) {
-      return false;
+  // Iterate over the strings and fill in the lengths array
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        lengths[i][j] = lengths[i - 1][j - 1] + 1;
+        if (lengths[i][j] > maxLength) {
+          maxLength = lengths[i][j];
+          endIndex = i - 1;
+        }
+      } else {
+        lengths[i][j] = 0;
+      }
     }
-    slow = slow.next;
   }
 
-  return true;
+  // Extract the longest common substring using the endIndex
+  const longestSubstring = str1.substr(endIndex - maxLength + 1, maxLength);
+  return longestSubstring;
 }
+
+// Example usage
+const str1 = "abcdefg";
+const str2 = "xyzabcd";
+const result = longestCommonSubstring(str1, str2);
+console.log(result); // Output: "abcd"
