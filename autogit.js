642000a429159ed3d61443bac1d9354bb94193f1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 09:16:58 +0800

    1-7-2024 9:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,45 @@
-function topologicalSort(graph) {
-  const visited = new Set();
-  const stack = [];
-
-  function dfs(node) {
-    visited.add(node);
-    
-    for (const neighbor of graph[node]) {
-      if (!visited.has(neighbor)) {
-        dfs(neighbor);
-      }
-    }
-
-    stack.unshift(node);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  for (const node in graph) {
-    if (!visited.has(node)) {
-      dfs(node);
-    }
-  }
-
-  return stack;
 }
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['D'],
-  'C': ['D', 'E'],
-  'D': ['F'],
-  'E': [],
-  'F': ['G'],
-  'G': []
-};
-const sortedNodes = topologicalSort(graph);
-console.log(sortedNodes);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  getLength() {
+    let current = this.head;
+    let length = 0;
+
+    while (current) {
+      length++;
+      current = current.next;
+    }
+
+    return length;
+  }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+
+console.log(linkedList.getLength());  // Output: 3
