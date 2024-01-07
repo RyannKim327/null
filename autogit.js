da9210b004ef92c4b1eed7159aba6285c0ceb724
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:04:32 +0800

    1-8-2024 1:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,5 @@
-// Importing Axios library
-const axios = require('axios');
+// Prompt the user for input
+const userInput = prompt("Enter some text:");
 
-// Function to make GET request using Axios
-const fetchData = async (url) => {
-  try {
-    const response = await axios.get(url);
-    console.log('Response:', response.data);
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
-};
-
-// Usage example
-fetchData('https://jsonplaceholder.typicode.com/posts/1');
+// Display the input on the console
+console.log("User input:", userInput);
