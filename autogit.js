8b64a261732fc00976e01b0efb0457b2b0f74507
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:30:18 +0800

    1-7-2024 8:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,18 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Output: hello world
+function reverseWords(str) {
+   // Step 1: Split the string into an array of words
+   const wordsArray = str.split(' ');
+
+   // Step 2: Reverse the order of the array
+   const reversedArray = wordsArray.reverse();
+
+   // Step 3: Join the reversed array back into a string
+   const reversedString = reversedArray.join(' ');
+
+   return reversedString;
+}
+
+// Example usage
+const inputString = "Hello world, how are you?";
+const reversedWordsString = reverseWords(inputString);
+console.log(reversedWordsString);
+you? are how world, Hello
