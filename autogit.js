d95bc3ec83175b73a66b465136208a80c18bbf04
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:31:14 +0800

    1-8-2024 0:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,5 @@
-function longestCommonPrefix(strings) {
-   if (strings.length === 0) return '';
-   if (strings.length === 1) return strings[0];
-
-   let prefix = strings[0];
-
-   for (let i = 0; i < prefix.length; i++) {
-      for (let j = 1; j < strings.length; j++) {
-         if (i >= strings[j].length || strings[j][i] !== prefix[i]) {
-            return prefix;
-         }
-      }
-      prefix += strings[0][i];
-   }
-
-   return prefix;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-const strings = ['apple', 'ape', 'apricot'];
-const commonPrefix = longestCommonPrefix(strings);
-console.log(commonPrefix); // Output: 'ap'
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(43)); // Output: 101011
