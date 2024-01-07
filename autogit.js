6a253336ba3747ac5814928b0653743b96e5e010
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:12:26 +0800

    1-7-2024 8:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,8 @@
-const arr = [1, 3, 5, 7, 9];
-const target = 5;
-const index = binarySearch(arr, target);
-console.log(index); // Outputs: 2
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
