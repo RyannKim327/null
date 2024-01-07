f131dedfbb7d5d6317414bd759c006dfd31d9bd1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:00:55 +0800

    1-7-2024 8:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,20 @@
-let str = "Hello, world! This is a string with spaces.";
-let stringWithoutSpaces = str.replace(/\s/g, "");
+var array = [1, 2, 3, 4, 4, 5, 6, 6];
+var uniqueArray = [...new Set(array)];
 
-console.log(stringWithoutSpaces);
-Hello,world!Thisisastringwithspaces.
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
+var array = [1, 2, 3, 4, 4, 5, 6, 6];
+var uniqueArray = array.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+});
+
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
+var array = [1, 2, 3, 4, 4, 5, 6, 6];
+var uniqueArray = [];
+
+for (var i = 0; i < array.length; i++) {
+    if (uniqueArray.indexOf(array[i]) === -1) {
+        uniqueArray.push(array[i]);
+    }
+}
+
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
