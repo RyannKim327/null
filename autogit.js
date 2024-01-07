d62845bd7f83831afb6b2671126f143f9b2bb92d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:38:27 +0800

    1-8-2024 0:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,39 @@
-const graph = [[1, 2], [3], [3], []];
-function dfs(vertex, visited, stack, graph) {
-  visited[vertex] = true;
+function findLCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
 
-  for (const adj of graph[vertex]) {
-    if (!visited[adj]) {
-      dfs(adj, visited, stack, graph);
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
     }
   }
 
-  stack.push(vertex);
-}
-function topologicalSort(graph) {
-  const numVertices = graph.length;
-  const visited = new Array(numVertices).fill(false);
-  const stack = [];
-
-  for (let vertex = 0; vertex < numVertices; vertex++) {
-    if (!visited[vertex]) {
-      dfs(vertex, visited, stack, graph);
+  // Reconstruct the LCS
+  let lcs = "";
+  let i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
     }
   }
 
-  return stack.reverse();
+  return lcs;
 }
-const graph = [[1, 2], [3], [3], []];
-const sortedOrder = topologicalSort(graph);
-console.log(sortedOrder); // [0, 1, 2, 3]
+
+// Example usage:
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+const lcs = findLCS(str1, str2);
+console.log(lcs); // Output: "ADH"
