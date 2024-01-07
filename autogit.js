7c7bbc1b2dc8353ff1f02de2e2b8439d252339f5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:07:58 +0800

    1-7-2024 8:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,20 @@
-const str = "Hello, World!";
-const substring = "Hello";
+function insertionSort(array) {
+  const length = array.length;
 
-if (str.includes(substring)) {
-  console.log("Substring found!");
-} else {
-  console.log("Substring not found!");
+  for (let i = 1; i < length; i++) {
+    let current = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = current;
+  }
+
+  return array;
 }
+const unsortedArray = [7, 3, 9, 2, 6];
+const sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray);
