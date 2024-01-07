cecd6a2d97334cfa0f1f067be54e0e070a5d2fcb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:44:28 +0800

    1-8-2024 0:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,3 @@
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-function findMiddleElement(head) {
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  return slow;
-}
-const list = new ListNode(1);
-list.next = new ListNode(2);
-list.next.next = new ListNode(3);
-list.next.next.next = new ListNode(4);
-list.next.next.next.next = new ListNode(5);
-
-console.log(findMiddleElement(list).val); // Output: 3
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: "hello world"
