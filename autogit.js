2b9f65167506ecc4405b9bb99cc9ba17b8d8c9ab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:01:05 +0800

    1-8-2024 1:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,112 @@
-// Fetching random user data from an API
-fetch('https://randomuser.me/api/')
-  .then(response => response.json())
-  .then(data => {
-    // Extracting user information from the API response
-    const user = data.results[0];
-    const name = `${user.name.first} ${user.name.last}`;
-    const email = user.email;
-    const location = `${user.location.city}, ${user.location.country}`;
-    
-    // Logging the user information
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Location:', location);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null || node.value === value) {
+      return node;
+    } else if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.deleteNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.deleteNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.deleteNode(node.right, minNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log(bst.search(6)); // Output: Node { value: 6, left: Node { value: 4, ... }, ... }
+
+bst.delete(6);
+
+console.log(bst.search(6)); // Output: null
