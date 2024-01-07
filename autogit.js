095c8ee28f82bc5528526d7371800e9271746b20
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:07:11 +0800

    1-8-2024 0:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,33 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function countLeafNodes(root) {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null && root.right === null) {
+    return 1;
+  }
+  
+  const leftCount = countLeafNodes(root.left);
+  const rightCount = countLeafNodes(root.right);
+  
+  return leftCount + rightCount;
+}
+// Create a binary tree
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+// Count the leaf nodes
+const leafNodeCount = countLeafNodes(root);
+console.log("Number of leaf nodes:", leafNodeCount); // Output: Number of leaf nodes: 4
