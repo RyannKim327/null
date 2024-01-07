e42e0a3cfa2b86f83a70e19a3338c821263c5393
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:11:25 +0800

    1-8-2024 0:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,44 @@
-function bwt(str) {
-  // Implement the BWT algorithm here
-}
-function bwt(str) {
-  const rotations = [str];
-  
-  // Add all the rotations of the input string to the `rotations` array
-  for (let i = 1; i < str.length; i++) {
-    rotations.push(str.slice(i) + str.slice(0, i));
+// Node definition
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
-function bwt(str) {
-  const rotations = [str];
-  
-  for (let i = 1; i < str.length; i++) {
-    rotations.push(str.slice(i) + str.slice(0, i));
+
+// Binary Tree definition
+class BinaryTree {
+  constructor() {
+    this.root = null;
   }
-  
-  rotations.sort();
+
+  // Recursive function to count leaf nodes
+  countLeafNodes(node) {
+    if (node === null) {
+      return 0;
+    }
+    if (node.left === null && node.right === null) {
+      return 1;
+    }
+    return this.countLeafNodes(node.left) + this.countLeafNodes(node.right);
+  }
+
+  // Public method to count leaf nodes in the tree
+  getLeafNodeCount() {
+    return this.countLeafNodes(this.root);
+  }
 }
-function bwt(str) {
-  const rotations = [str];
-  
-  for (let i = 1; i < str.length; i++) {
-    rotations.push(str.slice(i) + str.slice(0, i));
-  }
-  
-  rotations.sort();
-  
-  let transformedStr = '';
-  for (const rotation of rotations) {
-    transformedStr += rotation.slice(-1);
-  }
-  
-  return transformedStr;
-}
-const input = 'banana';
-const transformed = bwt(input);
-console.log(transformed);
+
+// Creating a binary tree
+const tree = new BinaryTree();
+tree.root = new Node(1);
+tree.root.left = new Node(2);
+tree.root.right = new Node(3);
+tree.root.left.left = new Node(4);
+tree.root.left.right = new Node(5);
+tree.root.right.left = new Node(6);
+
+// Getting the number of leaf nodes
+const leafNodeCount = tree.getLeafNodeCount();
+console.log("Number of leaf nodes:", leafNodeCount);
