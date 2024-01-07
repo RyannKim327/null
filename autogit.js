dd64fda3a91d6cc3ed9cd28c948f35e78c4981bd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:46:50 +0800

    1-7-2024 8:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,124 +1,49 @@
-class TreeNode {
-  constructor(key) {
-    this.key = key;
-    this.left = null;
-    this.right = null;
-    this.height = 1;
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
   }
 }
-class AVLTree {
-  constructor() {
-    this.root = null;
+HashTable.prototype.hash = function(key) {
+  let hash = 0;
+
+  // Custom hash function example:
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
   }
 
-  // Calculate the height of a node
-  getHeight(node) {
-    if (node === null) return 0;
-    return node.height;
+  // Modulo operation to fit within table size
+  return hash % this.size;
+};
+HashTable.prototype.insert = function(key, value) {
+  const index = this.hash(key);
+
+  if (!this.table[index]) {
+    this.table[index] = [];
   }
 
-  // Update the height of a node
-  updateHeight(node) {
-    node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+  this.table[index].push({ key, value });
+};
+HashTable.prototype.get = function(key) {
+  const index = this.hash(key);
+
+  if (!this.table[index]) {
+    return undefined;
   }
 
-  // Get the balance factor of a node
-  getBalanceFactor(node) {
-    if (node === null) return 0;
-    return this.getHeight(node.left) - this.getHeight(node.right);
-  }
-
-  // Perform a right rotation at the given node
-  rotateRight(z) {
-    const y = z.left;
-    const T3 = y.right;
-
-    y.right = z;
-    z.left = T3;
-
-    this.updateHeight(z);
-    this.updateHeight(y);
-
-    return y;
-  }
-
-  // Perform a left rotation at the given node
-  rotateLeft(z) {
-    const y = z.right;
-    const T2 = y.left;
-
-    y.left = z;
-    z.right = T2;
-
-    this.updateHeight(z);
-    this.updateHeight(y);
-
-    return y;
-  }
-
-  // Insert a key into the tree
-  insert(key) {
-    this.root = this.insertNode(this.root, key);
-  }
-
-  // Insert a key recursively
-  insertNode(root, key) {
-    // Perform the normal BST insertion
-    if (root === null) return new TreeNode(key);
-    if (key < root.key) root.left = this.insertNode(root.left, key);
-    else if (key > root.key) root.right = this.insertNode(root.right, key);
-    else return root; // Duplicate keys not allowed
-
-    // Update height of ancestor node
-    this.updateHeight(root);
-
-    // Check the balance factor and perform rotations if needed
-    const balanceFactor = this.getBalanceFactor(root);
-
-    // Left Left Case
-    if (balanceFactor > 1 && key < root.left.key) {
-      return this.rotateRight(root);
+  // Linear search within the appropriate index
+  for (let i = 0; i < this.table[index].length; i++) {
+    if (this.table[index][i].key === key) {
+      return this.table[index][i].value;
     }
-
-    // Right Right Case
-    if (balanceFactor < -1 && key > root.right.key) {
-      return this.rotateLeft(root);
-    }
-
-    // Left Right Case
-    if (balanceFactor > 1 && key > root.left.key) {
-      root.left = this.rotateLeft(root.left);
-      return this.rotateRight(root);
-    }
-
-    // Right Left Case
-    if (balanceFactor < -1 && key < root.right.key) {
-      root.right = this.rotateRight(root.right);
-      return this.rotateLeft(root);
-    }
-
-    return root;
   }
 
-  // Traverse the tree in-order
-  inOrderTraversal(node = this.root) {
-    if (node === null) return [];
+  return undefined;
+};
+const myTable = new HashTable(10);
+myTable.insert('name', 'John');
+myTable.insert('age', 25);
 
-    const result = [];
-    result.push(...this.inOrderTraversal(node.left));
-    result.push(node.key);
-    result.push(...this.inOrderTraversal(node.right));
-
-    return result;
-  }
-}
-const avlTree = new AVLTree();
-
-avlTree.insert(10);
-avlTree.insert(20);
-avlTree.insert(30);
-avlTree.insert(40);
-avlTree.insert(50);
-avlTree.insert(25);
-
-console.log(avlTree.inOrderTraversal()); // [10, 20, 25, 30, 40, 50]
+console.log(myTable.get('name'));  // Output: 'John'
+console.log(myTable.get('age'));   // Output: 25
+console.log(myTable.get('city'));  // Output: undefined
