091a6ca4fd8bfdc9acaf6438d9d992aef62fa0e9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:12:32 +0800

    1-8-2024 1:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,28 @@
-const axios = require('axios');
-
-async function fetchData() {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    console.log(response.data);
-  } catch (error) {
-    console.log(error.message);
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-fetchData();
+function diameterOfBinaryTree(root) {
+  let diameter = 0;
+
+  function getHeight(node) {
+    if (node === null) {
+      return 0;
+    }
+
+    const leftHeight = getHeight(node.left);
+    const rightHeight = getHeight(node.right);
+
+    diameter = Math.max(diameter, leftHeight + rightHeight);
+
+    return 1 + Math.max(leftHeight, rightHeight);
+  }
+
+  getHeight(root);
+
+  return diameter;
+}
