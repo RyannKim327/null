974317b735a5660f881ed6b5580cf914c35522cd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:08:20 +0800

    1-7-2024 8:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,11 @@
-function insertionSort(array) {
-  const length = array.length;
-
-  for (let i = 1; i < length; i++) {
-    let current = array[i];
-    let j = i - 1;
-
-    while (j >= 0 && array[j] > current) {
-      array[j + 1] = array[j];
-      j--;
-    }
-
-    array[j + 1] = current;
+function getLinkedListLength(head) {
+  let count = 0;
+  let currentNode = head;
+  
+  while (currentNode !== null) {
+    count++;
+    currentNode = currentNode.next;
   }
-
-  return array;
+  
+  return count;
 }
-const unsortedArray = [7, 3, 9, 2, 6];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray);
