1e6ce6c5c3519c5668220263af549b68b13c6430
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:25:23 +0800

    1-8-2024 0:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,11 @@
-function findFirstRepeatedChar(str) {
-  const chars = str.split('');
-  const frequency = {};
-  
-  for (let char of chars) {
-    if (frequency[char]) {
-      return char;
-    } else {
-      frequency[char] = 1;
-    }
-  }
-  
-  return null;
-}
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-// Example usage:
-console.log(findFirstRepeatedChar("hello"));  // Output: "l"
-console.log(findFirstRepeatedChar("JavaScript"));  // Output: "a"
-console.log(findFirstRepeatedChar("Stack Overflow"));  // Output: " " (whitespace)
-console.log(findFirstRepeatedChar("abcdefg"));  // Output: null (no repeated characters)
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.value;
+}
