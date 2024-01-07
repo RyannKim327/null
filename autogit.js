f7cb203f6628052e8ac2f374e8406d2c0df5c7bc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:55:44 +0800

    1-8-2024 0:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,116 +1,103 @@
-class SkipNode {
-  constructor(value, level) {
-    this.value = value;
-    this.next = Array(level + 1).fill(null);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
-class SkipList {
+
+class LinkedList {
   constructor() {
-    this.head = new SkipNode(-Infinity, 0);
-    this.maxLevel = 0;
-    this.length = 0;
+    this.head = null;
+    this.tail = null;
   }
 
-  randomLevel() {
-    let level = 0;
-    while (Math.random() < 0.5 && level < this.maxLevel + 1) {
-      level++;
-    }
-    return level;
-  }
+  append(data) {
+    const newNode = new Node(data);
 
-  insert(value) {
-    const newNode = new SkipNode(value, this.randomLevel());
-    
-    if (newNode.level > this.maxLevel) {
-      this.maxLevel = newNode.level;
-    }
-
-    let current = this.head;
-    const update = Array(this.maxLevel + 1).fill(null);
-
-    for (let level = this.maxLevel; level >= 0; level--) {
-      while (current.next[level] && current.next[level].value < value) {
-        current = current.next[level];
-      }
-      update[level] = current;
-    }
-
-    for (let level = 0; level <= newNode.level; level++) {
-      newNode.next[level] = update[level].next[level];
-      update[level].next[level] = newNode;
-    }
-
-    this.length++;
-  }
-
-  remove(value) {
-    let current = this.head;
-    const update = Array(this.maxLevel + 1).fill(null);
-
-    for (let level = this.maxLevel; level >= 0; level--) {
-      while (current.next[level] && current.next[level].value < value) {
-        current = current.next[level];
-      }
-      update[level] = current;
-    }
-
-    current = current.next[0];
-
-    if (current && current.value === value) {
-      for (let level = 0; level <= this.maxLevel; level++) {
-        if (update[level].next[level] !== current) {
-          break;
-        }
-        update[level].next[level] = current.next[level];
-      }
-      this.length--;
+    if (this.tail) {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else { // if the list is empty
+      this.head = newNode;
+      this.tail = newNode;
     }
   }
 
-  search(value) {
-    let current = this.head;
+  prepend(data) {
+    const newNode = new Node(data);
 
-    for (let level = this.maxLevel; level >= 0; level--) {
-      while (current.next[level] && current.next[level].value < value) {
-        current = current.next[level];
-      }
+    if (this.head) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else { // if the list is empty
+      this.head = newNode;
+      this.tail = newNode;
     }
+  }
 
-    current = current.next[0];
+  find(data) {
+    let currentNode = this.head;
 
-    if (current && current.value === value) {
-      return current;
+    while (currentNode) {
+      if (currentNode.data === data) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
     }
 
     return null;
   }
 
-  print() {
-    let current = this.head.next[0];
-    const result = [];
-
-    while (current) {
-      result.push(current.value);
-      current = current.next[0];
+  remove(data) {
+    if (!this.head) {
+      return;
     }
 
-    console.log(result);
+    if (this.head.data === data) {
+      if (this.head === this.tail) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+      }
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (currentNode.next.data === data) {
+        if (currentNode.next === this.tail) {
+          this.tail = currentNode;
+        }
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  print() {
+    let currentNode = this.head;
+    const listArr = [];
+
+    while (currentNode) {
+      listArr.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+
+    console.log(listArr.join(' -> '));
   }
 }
-const skipList = new SkipList();
+const list = new LinkedList();
+list.append(10);
+list.append(20);
+list.append(30);
+list.prepend(5);
+list.print(); // Output: 5 -> 10 -> 20 -> 30
 
-skipList.insert(3);
-skipList.insert(6);
-skipList.insert(2);
-skipList.insert(8);
-skipList.insert(1);
+const foundNode = list.find(20);
+console.log(foundNode.data); // Output: 20
 
-skipList.print(); // Output: [1, 2, 3, 6, 8]
-
-skipList.remove(6);
-
-skipList.print(); // Output: [1, 2, 3, 8]
-
-const foundNode = skipList.search(2);
-console.log(foundNode.value); // Output: 2
+list.remove(10);
+list.print(); // Output: 5 -> 20 -> 30
