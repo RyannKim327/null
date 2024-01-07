e6e8fe99209dcd50799a2f26cebe9e8818113998
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:47:14 +0800

    1-7-2024 8:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,28 @@
-class HashTable {
-  constructor(size) {
-    this.table = new Array(size);
-    this.size = size;
-  }
-}
-HashTable.prototype.hash = function(key) {
-  let hash = 0;
+function binarySearchRecursive(arr, target, start = 0, end = arr.length - 1) {
+  if (start <= end) {
+    const mid = Math.floor((start + end) / 2);
 
-  // Custom hash function example:
-  for (let i = 0; i < key.length; i++) {
-    hash += key.charCodeAt(i);
-  }
-
-  // Modulo operation to fit within table size
-  return hash % this.size;
-};
-HashTable.prototype.insert = function(key, value) {
-  const index = this.hash(key);
-
-  if (!this.table[index]) {
-    this.table[index] = [];
-  }
-
-  this.table[index].push({ key, value });
-};
-HashTable.prototype.get = function(key) {
-  const index = this.hash(key);
-
-  if (!this.table[index]) {
-    return undefined;
-  }
-
-  // Linear search within the appropriate index
-  for (let i = 0; i < this.table[index].length; i++) {
-    if (this.table[index][i].key === key) {
-      return this.table[index][i].value;
+    if (arr[mid] === target) {
+      return mid;
     }
+
+    if (arr[mid] > target) {
+      return binarySearchRecursive(arr, target, start, mid - 1);
+    }
+
+    return binarySearchRecursive(arr, target, mid + 1, end);
   }
 
-  return undefined;
-};
-const myTable = new HashTable(10);
-myTable.insert('name', 'John');
-myTable.insert('age', 25);
+  return -1;
+}
 
-console.log(myTable.get('name'));  // Output: 'John'
-console.log(myTable.get('age'));   // Output: 25
-console.log(myTable.get('city'));  // Output: undefined
+// Usage example:
+const array = [1, 3, 5, 7, 9, 11, 13, 15];
+const target = 11;
+const resultIndex = binarySearchRecursive(array, target);
+
+if (resultIndex !== -1) {
+  console.log(`Target ${target} found at index ${resultIndex}.`);
+} else {
+  console.log(`Target ${target} not found in the array.`);
+}
