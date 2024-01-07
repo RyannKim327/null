dbead2f74cb04081ce192cf1d84ce975212cef35
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 01:02:25 +0800

    1-8-2024 1:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,20 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  
-  const pivot = arr[arr.length - 1];
-  const leftArr = [];
-  const rightArr = [];
-  
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      leftArr.push(arr[i]);
-    } else {
-      rightArr.push(arr[i]);
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    
+    // Move elements of `arr[0..i-1]`, that are greater than `key`, to one position ahead of their current position
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    
+    arr[j + 1] = key;
   }
   
-  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  return arr;
 }
 
 // Example usage:
-const array = [8, 4, 2, 9, 1, 5];
-console.log(quickSort(array)); // Output: [1, 2, 4, 5, 8, 9]
+let array = [5, 2, 6, 1, 3, 9];
+console.log(insertionSort(array)); // Output: [1, 2, 3, 5, 6, 9]
