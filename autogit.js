c3d7643d7aaa3aadee5169f3bbf34a11d493cf86
RyannKim327 <rksesgundo123@gmail.com>
2024-01-08 00:13:31 +0800

    1-8-2024 0:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,137 @@
-function findMajorityElement(arr) {
-  let candidate;
-  let count = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (count === 0) {
-      candidate = arr[i];
-      count = 1;
-    } else if (candidate === arr[i]) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Verify if the candidate is actually the majority element
-  count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === candidate) {
-      count++;
-    }
-  }
-
-  if (count > arr.length / 2) {
-    return candidate;
-  } else {
-    return -1; // No majority element
+class RedBlackTreeNode {
+  constructor(value, color) {
+    this.value = value;
+    this.color = color;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
   }
 }
+class RedBlackTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  // Insert a node into the tree
+  insert(value) {
+    const newNode = new RedBlackTreeNode(value, "red");
+    
+    if (this.root === null) {
+      this.root = newNode;
+      this.root.color = "black";
+    } else {
+      this.insertNode(this.root, newNode);
+      this.fixViolation(newNode);
+    }
+  }
 
-// Example usage
-const array = [2, 2, 3, 4, 2, 2, 5];
-const majorityElement = findMajorityElement(array);
-console.log(majorityElement);
+  insertNode(root, newNode) {
+    if (newNode.value < root.value) {
+      if (root.left === null) {
+        root.left = newNode;
+        newNode.parent = root;
+      } else {
+        this.insertNode(root.left, newNode);
+      }
+    } else {
+      if (root.right === null) {
+        root.right = newNode;
+        newNode.parent = root;
+      } else {
+        this.insertNode(root.right, newNode);
+      }
+    }
+  }
+  
+  fixViolation(node) {
+    while (node.parent !== null && node.parent.color === "red") {
+      if (node.parent === node.parent.parent.left) {
+        const uncle = node.parent.parent.right;
+        
+        if (uncle !== null && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.right) {
+            node = node.parent;
+            this.rotateLeft(node);
+          }
+          
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateRight(node.parent.parent);
+        }
+      } else {
+        const uncle = node.parent.parent.left;
+        
+        if (uncle !== null && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.left) {
+            node = node.parent;
+            this.rotateRight(node);
+          }
+          
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateLeft(node.parent.parent);
+        }
+      }
+    }
+    
+    this.root.color = "black";
+  }
+  
+  rotateLeft(node) {
+    const newRoot = node.right;
+    node.right = newRoot.left;
+    
+    if (newRoot.left !== null) {
+      newRoot.left.parent = node;
+    }
+    
+    newRoot.parent = node.parent;
+    
+    if (node.parent === null) {
+      this.root = newRoot;
+    } else if (node === node.parent.left) {
+      node.parent.left = newRoot;
+    } else {
+      node.parent.right = newRoot;
+    }
+    
+    newRoot.left = node;
+    node.parent = newRoot;
+  }
+  
+  rotateRight(node) {
+    const newRoot = node.left;
+    node.left = newRoot.right;
+    
+    if (newRoot.right !== null) {
+      newRoot.right.parent = node;
+    }
+    
+    newRoot.parent = node.parent;
+    
+    if (node.parent === null) {
+      this.root = newRoot;
+    } else if (node === node.parent.right) {
+      node.parent.right = newRoot;
+    } else {
+      node.parent.left = newRoot;
+    }
+    
+    newRoot.right = node;
+    node.parent = newRoot;
+  }
+}
+const tree = new RedBlackTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(30);
