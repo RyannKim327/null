e25563919bbf654df37c4d55ef1a74da995f8b05
RyannKim327 <rksesgundo123@gmail.com>
2024-01-07 08:15:52 +0800

    1-7-2024 8:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,27 @@
-// Prompt the user for input
-var userInput = prompt('Enter something:');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+let head = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
 
-// Display the user input
-console.log('You entered: ' + userInput);
+head.next = node2;
+node2.next = node3;
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+
+  while (current) {
+    next = current.next; // Store the next node
+    current.next = prev; // Reverse the current node's pointer
+    prev = current; // Move prev one step forward
+    current = next; // Move current one step forward
+  }
+
+  return prev; // The new head of the reversed linked list
+}
+let reversedHead = reverseLinkedList(head);
