4fbfd9135c68957951afaaec0e2ad07c435db103
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 06:49:20 +0800

    1-9-2024 6:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,12 @@
-function mergeSortIterative(array) {
-  const arrayLength = array.length;
-  let subarraySize = 1;
+let str = "  Remove   whitespace  ";
 
-  while (subarraySize < arrayLength) {
-    let left = 0;
+// Using regular expression to remove whitespace
+let newStr = str.replace(/\s/g, "");
 
-    while (left + subarraySize < arrayLength) {
-      const middle = left + subarraySize - 1;
-      const right = Math.min(left + subarraySize * 2 - 1, arrayLength - 1);
+console.log(newStr); // Output: "Removewhitespace"
+let str = "  Remove   whitespace  ";
 
-      merge(array, left, middle, right);
+// Using trim() method to remove leading and trailing whitespace
+let newStr = str.trim();
 
-      left += subarraySize * 2;
-    }
-
-    subarraySize *= 2;
-  }
-
-  return array;
-}
-
-function merge(array, left, middle, right) {
-  const leftArray = array.slice(left, middle + 1);
-  const rightArray = array.slice(middle + 1, right + 1);
-  let i = 0;
-  let j = 0;
-  let k = left;
-
-  while (i < leftArray.length && j < rightArray.length) {
-    if (leftArray[i] <= rightArray[j]) {
-      array[k] = leftArray[i];
-      i++;
-    } else {
-      array[k] = rightArray[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < leftArray.length) {
-    array[k] = leftArray[i];
-    i++;
-    k++;
-  }
-
-  while (j < rightArray.length) {
-    array[k] = rightArray[j];
-    j++;
-    k++;
-  }
-}
-const arr = [9, 5, 1, 3, 6, 2, 8, 4, 7];
-console.log(mergeSortIterative(arr)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(newStr); // Output: "Remove   whitespace"
