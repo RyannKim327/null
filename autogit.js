1b993c77360d27e43847ff341c993010a9b4537c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 15:25:08 +0800

    1-9-2024 15:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,23 @@
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
+// Function to perform insertion sort
+function insertionSort(array) {
+  const length = array.length;
+  
+  for (let i = 1; i < length; i++) {
+    let key = array[i];
+    let j = i - 1;
+
+    // Move elements of array[0..i-1], that are greater than key, to one position ahead
+    // of their current position
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = key;
+  }
+  
+  return array;
 }
 
-// Example usage:
-var triangleArea = calculateTriangleArea(5, 8);
-console.log("The area of the triangle is: " + triangleArea);
+// Test the insertion sort function
+const array = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(array)); // Output: [1, 2, 3, 4, 5, 6]
