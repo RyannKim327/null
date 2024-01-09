586bf79e5dec4e851d3a37302b6101826742c16a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:38:27 +0800

    1-10-2024 2:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,91 +1,11 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+function getIntersectionNode(headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
+
+  while (p1 !== p2) {
+    p1 = p1 === null ? headB : p1.next;
+    p2 = p2 === null ? headA : p2.next;
   }
+
+  return p1; // Return either p1 or p2, since they will be the same when an intersection is found.
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  append(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  prepend(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  delete(data) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-      if (!this.head) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let current = this.head;
-    while (current.next && current.next.data !== data) {
-      current = current.next;
-    }
-
-    if (current.next) {
-      current.next = current.next.next;
-      if (!current.next) {
-        this.tail = current;
-      }
-    }
-  }
-
-  search(data) {
-    let current = this.head;
-    while (current) {
-      if (current.data === data) {
-        return true;
-      }
-      current = current.next;
-    }
-    return false;
-  }
-
-  print() {
-    let current = this.head;
-    let result = '';
-    while (current) {
-      result += current.data + ' ';
-      current = current.next;
-    }
-    console.log(result.trim());
-  }
-}
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.prepend(0);
-linkedList.delete(2);
-linkedList.print(); // Output: 0 1 3
-console.log(linkedList.search(1)); // Output: true
-console.log(linkedList.search(2)); // Output: false
