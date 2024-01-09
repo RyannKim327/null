ede1343b1442880d9ddf3a17dd2d96f73161b4fd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:33:13 +0800

    1-9-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,110 +1,17 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
+function isAnagram(str1, str2) {
+  // Remove non-alphabetic characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
+
+  // Sort the characters of both strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
 }
 
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(data) {
-    const newNode = new Node(data);
-
-    if (!this.root) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.data < node.data) {
-      if (!node.left) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (!node.right) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  search(node, data) {
-    if (!node || node.data === data) {
-      return node;
-    }
-
-    if (data < node.data) {
-      return this.search(node.left, data);
-    } 
-
-    return this.search(node.right, data);
-  }
-
-  remove(data) {
-    this.root = this.removeNode(this.root, data);
-  }
-
-  removeNode(node, key) {
-    if (!node) {
-      return null;
-    }
-
-    if (key < node.data) {
-      node.left = this.removeNode(node.left, key);
-      return node;
-    } else if (key > node.data) {
-      node.right = this.removeNode(node.right, key);
-      return node;
-    } else {
-      if (!node.left && !node.right) {
-        return null;
-      }
-
-      if (!node.left) {
-        return node.right;
-      }
-
-      if (!node.right) {
-        return node.left;
-      }
-
-      const minValue = this.findMinValue(node.right);
-      node.data = minValue;
-      node.right = this.removeNode(node.right, minValue);
-
-      return node;
-    }
-  }
-  
-  findMinValue(node) {
-    if (node.left === null) {
-      return node.data;
-    }
-
-    return this.findMinValue(node.left);
-  }
-}
-const tree = new BinaryTree();
-tree.insert(8);
-tree.insert(3);
-tree.insert(10);
-tree.insert(1);
-tree.insert(6);
-tree.insert(14);
-tree.insert(4);
-tree.insert(7);
-tree.insert(13);
-
-console.log(tree.search(tree.root, 6)); // Node { data: 6, left: ..., right: ... }
-
-tree.remove(6);
-console.log(tree.search(tree.root, 6)); // null
+// Example usage
+console.log(isAnagram('listen', 'silent'));  // Output: true
+console.log(isAnagram('hello', 'goodbye'));  // Output: false
+console.log(isAnagram('rail Safety', 'fairy Tales'));  // Output: true
