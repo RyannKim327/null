5166b24de410d566b7be21ec1129ebb0b3425928
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:40:14 +0800

    1-9-2024 23:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,8 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Example usages:
-console.log(isSortedAscending([1, 2, 3, 4, 5]));  // true
-console.log(isSortedAscending([1, 2, 4, 3, 5]));  // false
-console.log(isSortedAscending([5, 4, 3, 2, 1]));  // false
-console.log(isSortedAscending([]));               // true (an empty array is considered sorted)
+var string1 = "Hello";
+var string2 = " World";
+var result = string1 + string2;
+console.log(result); // Output: "Hello World"
+var string1 = "Hello";
+var string2 = " World";
+var result = string1.concat(string2);
+console.log(result); // Output: "Hello World"
