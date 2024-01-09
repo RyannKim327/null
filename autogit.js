1f3bff7e8bc6432738716f9a3747db723aa6a77c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:03:19 +0800

    1-10-2024 0:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,20 @@
-function getIntersectionNode(headA, headB) {
-  let p1 = headA;
-  let p2 = headB;
-
-  while (p1 !== p2) {
-    p1 = p1 === null ? headB : p1.next;
-    p2 = p2 === null ? headA : p2.next;
-  }
-
-  return p1;
+function countOccurrences(string, word) {
+  const regex = new RegExp('\\b' + word + '\\b', 'gi');
+  const matches = string.match(regex);
+  return matches ? matches.length : 0;
 }
+
+const inputString = 'Hello world, hello planet, hello universe';
+const searchWord = 'hello';
+
+console.log(countOccurrences(inputString, searchWord)); // Output: 3
+function countOccurrences(string, word) {
+  const wordsArray = string.split(' ');
+  const filteredArray = wordsArray.filter((w) => w.toLowerCase() === word.toLowerCase());
+  return filteredArray.length;
+}
+
+const inputString = 'Hello world, hello planet, hello universe';
+const searchWord = 'hello';
+
+console.log(countOccurrences(inputString, searchWord)); // Output: 3
