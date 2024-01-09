af4ebbb30719e07494c852759199e5cbdf57ac67
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:11:07 +0800

    1-9-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,59 @@
-let str = "  Hello,   World!  ";
-let trimmedStr = str.replace(/\s/g, "");
+function merge(arr, temp, leftStart, leftEnd, rightStart, rightEnd) {
+  let i = leftStart;
+  let j = rightStart;
+  let k = leftStart;
 
-console.log(trimmedStr); // Output: "Hello,World!"
+  while (i <= leftEnd && j <= rightEnd) {
+    if (arr[i] <= arr[j]) {
+      temp[k++] = arr[i++];
+    } else {
+      temp[k++] = arr[j++];
+    }
+  }
+
+  while (i <= leftEnd) {
+    temp[k++] = arr[i++];
+  }
+
+  while (j <= rightEnd) {
+    temp[k++] = arr[j++];
+  }
+
+  for (i = leftStart; i <= rightEnd; i++) {
+    arr[i] = temp[i];
+  }
+}
+function mergeSortIterative(arr) {
+  const size = arr.length;
+  const temp = new Array(size);
+
+  let blockSize = 1;
+
+  while (blockSize < size) {
+    let i = 0;
+
+    while (i < size - blockSize) {
+      const leftStart = i;
+      const mid = i + blockSize;
+      const leftEnd = mid - 1;
+      const rightStart = mid;
+      const rightEnd =
+        i + 2 * blockSize - 1 < size - 1 ? i + 2 * blockSize - 1 : size - 1;
+
+      merge(arr, temp, leftStart, leftEnd, rightStart, rightEnd);
+
+      i += 2 * blockSize;
+    }
+
+    temp.forEach((value, index) => {
+      arr[index] = value;
+    });
+
+    blockSize *= 2;
+  }
+
+  return arr;
+}
+const arr = [9, 3, 7, 5, 1, 6, 2, 8, 4];
+const sortedArr = mergeSortIterative(arr);
+console.log(sortedArr);
