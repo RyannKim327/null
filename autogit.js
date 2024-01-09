ff868c301cc6bfd947d62947a1db0cdd1298884f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:08:33 +0800

    1-10-2024 2:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,73 @@
-const array = [1, 2, 3, 1, 2, 3];
-const uniqueArray = Array.from(new Set(array));
-console.log(uniqueArray);
-const array = [1, 2, 3, 1, 2, 3];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray);
-const array = [1, 2, 3, 1, 2, 3];
-const uniqueArray = array.reduce((accumulator, currentValue) => {
-  if (!accumulator.includes(currentValue)) {
-    accumulator.push(currentValue);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.adjacentNodes = [];
   }
-  return accumulator;
-}, []);
-console.log(uniqueArray);
+}
+
+function biDirectionalSearch(start, target) {
+  const forwardQueue = [];
+  const backwardQueue = [];
+  const forwardVisited = new Set();
+  const backwardVisited = new Set();
+  const parentsForward = new Map();
+  const parentsBackward = new Map();
+
+  forwardQueue.push(start);
+  backwardQueue.push(target);
+  forwardVisited.add(start);
+  backwardVisited.add(target);
+
+  while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+    const forwardNode = forwardQueue.shift();
+    if (backwardVisited.has(forwardNode)) {
+      // Path found!
+      const path = getPath(forwardNode, parentsForward, parentsBackward);
+      return path;
+    }
+
+    for (const adjacentNode of forwardNode.adjacentNodes) {
+      if (!forwardVisited.has(adjacentNode)) {
+        forwardQueue.push(adjacentNode);
+        forwardVisited.add(adjacentNode);
+        parentsForward.set(adjacentNode, forwardNode);
+      }
+    }
+
+    const backwardNode = backwardQueue.shift();
+    if (forwardVisited.has(backwardNode)) {
+      // Path found!
+      const path = getPath(backwardNode, parentsForward, parentsBackward);
+      return path;
+    }
+
+    for (const adjacentNode of backwardNode.adjacentNodes) {
+      if (!backwardVisited.has(adjacentNode)) {
+        backwardQueue.push(adjacentNode);
+        backwardVisited.add(adjacentNode);
+        parentsBackward.set(adjacentNode, backwardNode);
+      }
+    }
+  }
+
+  // No path found
+  return null;
+}
+
+function getPath(node, parentsForward, parentsBackward) {
+  const path = [];
+  
+  let current = node;
+  while (current) {
+    path.unshift(current);
+    current = parentsForward.get(current);
+  }
+
+  current = parentsBackward.get(node);
+  while (current) {
+    path.push(current);
+    current = parentsBackward.get(current);
+  }
+
+  return path;
+}
