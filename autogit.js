4aab79e66a2b5d68a3fa0de9324e99afc2997bb1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:08:45 +0800

    1-10-2024 0:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,41 @@
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = array.reduce((accumulator, value) => {
-  if (!accumulator.includes(value)) {
-    accumulator.push(value);
+function findLCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+
+  // Create a 2D matrix with (m+1) rows and (n+1) columns
+  const matrix = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+  // Build the matrix using bottom-up dynamic programming approach
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1] + 1;
+      } else {
+        matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+      }
+    }
   }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = [];
-for (let i = 0; i < array.length; i++) {
-  if (uniqueArray.indexOf(array[i]) === -1) {
-    uniqueArray.push(array[i]);
+
+  // Reconstruct the longest common subsequence
+  let lcs = "";
+  let i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (matrix[i - 1][j] > matrix[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
   }
+
+  return lcs;
 }
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
+
+// Example usage:
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+const lcs = findLCS(str1, str2);
+console.log(lcs);  // Output: "ADH"
