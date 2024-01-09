76ed7dc9f25fa5932b20ab141e61b40bdb62855f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 15:24:26 +0800

    1-9-2024 15:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,22 @@
-const string1 = "Hello";
-const string2 = "World";
-const combinedString = string1 + " " + string2;
-console.log(combinedString);
-Hello World
-const string1 = "Hello";
-const string2 = "World";
-const combinedString = string1.concat(" ", string2);
-console.log(combinedString);
-Hello World
+const cron = require('node-cron');
+
+// Function to execute at scheduled intervals
+function doSomething() {
+  console.log('Task executed');
+  // Add your code here to perform the required task
+}
+
+// Cron schedule
+const cronSchedule = '*/5 * * * *'; // Executes every 5 minutes
+
+// Define the cron job
+const cronJob = cron.schedule(cronSchedule, doSomething);
+
+// Start the cron job
+cronJob.start();
+
+// Stop the cron job after 1 hour
+setTimeout(() => {
+  cronJob.stop();
+  console.log('Cron job stopped');
+}, 3600000); // 1 hour in milliseconds
