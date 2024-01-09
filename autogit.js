9dab64e22d144e4f3dcade1784ad2a051724d7b4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 21:59:01 +0800

    1-9-2024 21:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,12 @@
-function breadthLimitedSearch(startNode, limit) {
-  let queue = [startNode];
-  let visited = new Set([startNode]);
-  let depth = 0;
-
-  while (queue.length > 0 && depth <= limit) {
-    let currentNode = queue.shift();
-
-    if (currentNode === goalNode) {
-      // Goal node found, return solution or perform action
-      return currentNode;
-    }
-
-    for (let neighbor of currentNode.neighbors) {
-      if (!visited.has(neighbor)) {
-        visited.add(neighbor);
-        queue.push(neighbor);
-      }
-    }
-
-    depth++;
+function findStringLength(str) {
+  var count = 0;
+  for (var i = 0; str[i] !== undefined; i++) {
+    count++;
   }
-
-  // No solution found within the specified limit
-  return null;
+  return count;
 }
+
+// Example usage:
+var myString = "Hello, world!";
+var length = findStringLength(myString);
+console.log("Length:", length);
