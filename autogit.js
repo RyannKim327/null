19394deeaadeec0fa96679088247c3ce123db55a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:10:28 +0800

    1-9-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,5 @@
-function bubbleSort(array) {
-  const length = array.length;
-  for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        // Swap elements
-        const temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-      }
-    }
-  }
-  return array;
-}
+let str = "10";
+let num = parseInt(str);
 
-// Usage example
-const unsortedArray = [5, 3, 8, 4, 2];
-const sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [2, 3, 4, 5, 8]
+console.log(num); // Output: 10
+console.log(typeof num); // Output: "number"
