077552e030ad257fbed46146fac1827e4b0273bc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:10:47 +0800

    1-9-2024 23:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,24 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result);  // Output: "Hello World"
-let str1 = "Hello";
-let str2 = "World";
-let result = str1.concat(" ", str2);
-console.log(result);  // Output: "Hello World"
+function findFirstNonRepeatingCharacter(inputString) {
+  const charCount = {};
+
+  for (let char of inputString.split('')) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  for (let char of inputString.split('')) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+// Example usage
+const string = 'aabbcdeff';
+const firstNonRepeatingChar = findFirstNonRepeatingCharacter(string);
+console.log(firstNonRepeatingChar); // Output: 'c'
