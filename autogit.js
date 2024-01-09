33060c9305fe44602552a263dcc62d596db57885
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:39:48 +0800

    1-10-2024 1:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,52 @@
-function largestPrimeFactor(number) {
-    let factor = 2;
-    let largestFactor = 1;
-    
-    while (factor * factor <= number) {
-        if (number % factor === 0) { // If factor divides the number
-            largestFactor = factor; // Update largestFactor
-            number = number / factor; // Divide the number by factor
-        } else {
-            factor++; // Increment factor
-        }
-    }
-    
-    if (number > largestFactor) {
-        largestFactor = number; // Remaining number is the largest prime factor
-    }
-    
-    return largestFactor;
-}
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
 
-// Example usage:
-console.log(largestPrimeFactor(13195)); // Output: 29
-console.log(largestPrimeFactor(600851475143)); // Output: 6857
+  addNode(node) {
+    this.adjList.set(node, []);
+  }
+
+  addEdge(node1, node2) {
+    this.adjList.get(node1).push(node2);
+    this.adjList.get(node2).push(node1);
+  }
+
+  getNeighbors(node) {
+    return this.adjList.get(node);
+  }
+}
+function dfs(graph, startNode) {
+  let visited = new Set();
+
+  function dfsVisit(node) {
+    visited.add(node);
+    console.log(node); // Perform any required operations
+
+    let neighbors = graph.getNeighbors(node);
+    for (let neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        dfsVisit(neighbor);
+      }
+    }
+  }
+
+  dfsVisit(startNode);
+}
+let graph = new Graph();
+
+// Add nodes
+graph.addNode(1);
+graph.addNode(2);
+graph.addNode(3);
+graph.addNode(4);
+graph.addNode(5);
+
+// Add edges
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+graph.addEdge(3, 4);
+graph.addEdge(4, 5);
+
+dfs(graph, 1); // Start the depth-first search from node 1
