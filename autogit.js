372abb6f44efbba0f839136c5cc51a3b2c2798d6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:22:37 +0800

    1-10-2024 0:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,9 @@
-function generateFibonacci(n) {
-  let fib = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib;
+var str = 'Hello, World!';
+var reversed = str.split('').reverse().join('');
+console.log(reversed);  // Output: "!dlroW ,olleH"
+var str = 'Hello, World!';
+var reversed = '';
+for (var i = str.length - 1; i >= 0; i--) {
+  reversed += str.charAt(i);
 }
-function fibonacciSearch(arr, searchElement) {
-  let fib = generateFibonacci(arr.length);
-  let offset = -1;
-  let currentFib = fib[arr.length - 1]; // Largest Fibonacci number less than or equal to array length
-  let prevFib = fib[arr.length - 2]; // Second largest Fibonacci number less than or equal to array length
-
-  while (currentFib > 1) {
-    let i = Math.min(offset + prevFib, arr.length - 1);
-
-    if (arr[i] < searchElement) {
-      currentFib = fib[fib.findIndex((val) => val === prevFib)];
-      prevFib = fib[fib.findIndex((val) => val === prevFib) - 1];
-      offset = i;
-    } else if (arr[i] > searchElement) {
-      currentFib = prevFib;
-      prevFib = fib[fib.findIndex((val) => val === prevFib) - 1];
-    } else {
-      return i;
-    }
-  }
-  if (prevFib === 1 && arr[offset + 1] === searchElement) {
-    return offset + 1;
-  }
-  return -1;
-}
-let arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
-let searchElement = 16;
-let index = fibonacciSearch(arr, searchElement);
-
-console.log("Array:", arr);
-console.log(`"${searchElement}" is found at index`, index);
-
-// Output:
-// Array: [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-// "16" is found at index 4
+console.log(reversed);  // Output: "!dlroW ,olleH"
