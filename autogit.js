8218a90a81882f717a8b96d2e3e183c9d513b2f8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 21:56:38 +0800

    1-9-2024 21:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,13 @@
-let str = "Hello World!";
-let lowercaseStr = str.toLowerCase();
+function areAnagrams(str1, str2) {
+  const sortedStr1 = str1.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+  const sortedStr2 = str2.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+  return sortedStr1 === sortedStr2;
+}
+const string1 = 'listen';
+const string2 = 'silent';
 
-console.log(lowercaseStr); // Output: hello world!
+if (areAnagrams(string1, string2)) {
+  console.log('The strings are anagrams');
+} else {
+  console.log('The strings are not anagrams');
+}
