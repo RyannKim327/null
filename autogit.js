15bb6c122413262d5dca1cce179a62f8f60a9e31
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:29:34 +0800

    1-10-2024 0:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,53 @@
-let array = [5, 12, 8, 9, 3, 15];
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
 
-let maxValue = Math.max(...array);
+  addVertex(vertex) {
+    this.adjList.set(vertex, []);
+  }
 
-console.log(maxValue); // Output: 15
+  addEdge(vertex1, vertex2) {
+    this.adjList.get(vertex1).push(vertex2);
+    this.adjList.get(vertex2).push(vertex1);
+  }
+
+  bfs(startingNode) {
+    const visited = new Set();
+    const queue = [];
+  
+    visited.add(startingNode);
+    queue.push(startingNode);
+
+    while (queue.length !== 0) {
+      const currentNode = queue.shift();
+      const neighbors = this.adjList.get(currentNode);
+
+      console.log(currentNode);
+
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      }
+    }
+  }
+}
+
+// Usage example:
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("D", "E");
+
+graph.bfs("A");
