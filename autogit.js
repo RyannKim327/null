726a31ee03f306e73a7dfbf0c8679f16eb4d9d8b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:47:01 +0800

    1-9-2024 23:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,166 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-  while (fast !== null && fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next.next;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-    if (slow === fast) {
-      // Linked list contains a cycle
-      return true;
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
     }
   }
 
-  // No cycle found
-  return false;
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return node;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      }
+
+      if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minRightNode = this.findMinNode(node.right);
+      node.value = minRightNode.value;
+      node.right = this.removeNode(node.right, minRightNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  preOrderTraversal(callback) {
+    this.preOrderTraversalNode(this.root, callback);
+  }
+
+  preOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      callback(node.value);
+      this.preOrderTraversalNode(node.left, callback);
+      this.preOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  postOrderTraversal(callback) {
+    this.postOrderTraversalNode(this.root, callback);
+  }
+
+  postOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.postOrderTraversalNode(node.left, callback);
+      this.postOrderTraversalNode(node.right, callback);
+      callback(node.value);
+    }
+  }
 }
+const bst = new BinarySearchTree();
+
+bst.insert(4);
+bst.insert(2);
+bst.insert(1);
+bst.insert(3);
+bst.insert(6);
+bst.insert(5);
+bst.insert(7);
+
+console.log("In-order traversal:");
+bst.inOrderTraversal((value) => console.log(value));
+
+console.log("Pre-order traversal:");
+bst.preOrderTraversal((value) => console.log(value));
+
+console.log("Post-order traversal:");
+bst.postOrderTraversal((value) => console.log(value));
+
+console.log("Searching for value 5:");
+const foundNode = bst.search(5);
+console.log(foundNode);
+
+bst.remove(4);
+
+console.log("In-order traversal after removing 4:");
+bst.inOrderTraversal((value) => console.log(value));
