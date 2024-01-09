17301a1a25f71d87820b522c5014be63f03667b1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:08:36 +0800

    1-9-2024 23:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,15 @@
 function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
+  // Remove all non-alphanumeric characters and convert to lowercase
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-  while (left < right) {
-    if (str[left] !== str[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-  
-  return true;
+  // Reverse the string
+  var reverseStr = str.split('').reverse().join('');
+
+  // Compare the original string with the reversed string
+  return str === reverseStr;
 }
+
+// Test the function
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("Hello")); // false
+console.log(isPalindrome("A man, a plan, a canal, Panama!")); // true
