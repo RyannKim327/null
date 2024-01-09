391ea097f186717e3eda076d772f85794d601208
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:40:30 +0800

    1-10-2024 0:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,190 +1,21 @@
-// Red-Black Tree Node
-class RBTreeNode {
-  constructor(value, color) {
-    this.value = value;
-    this.color = color;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
+function isPrime(num) {
+  // Check if the number is less than 2
+  if (num < 2) {
+    return false;
   }
+
+  // Check if the number is divisible by any number from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  // The number is only divisible by 1 and itself, so it is prime
+  return true;
 }
 
-// Red-Black Tree implementation
-class RedBlackTree {
-  constructor() {
-    this.root = null;
-  }
-
-  // Insert a value into the tree
-  insert(value) {
-    const newNode = new RBTreeNode(value, "red"); // Start with a red node
-    if (this.root === null) {
-      // Empty tree
-      newNode.color = "black"; // Make root black
-      this.root = newNode;
-    } else {
-      let current = this.root;
-      while (current !== null) {
-        if (value < current.value) {
-          if (current.left === null) {
-            // Insert as left child
-            current.left = newNode;
-            newNode.parent = current;
-            break;
-          } else {
-            current = current.left;
-          }
-        } else if (value > current.value) {
-          if (current.right === null) {
-            // Insert as right child
-            current.right = newNode;
-            newNode.parent = current;
-            break;
-          } else {
-            current = current.right;
-          }
-        } else {
-          // Duplicate value, do not insert
-          return;
-        }
-      }
-
-      this.fixTreeAfterInsert(newNode);
-    }
-  }
-
-  // Restore red-black properties after insert
-  fixTreeAfterInsert(node) {
-    while (
-      node.parent !== null &&
-      node.parent.color === "red" &&
-      node !== this.root
-    ) {
-      const parentNode = node.parent;
-      const grandparentNode = node.parent.parent;
-
-      if (parentNode === grandparentNode.left) {
-        // Left subtree case
-        const uncleNode = grandparentNode.right;
-
-        if (uncleNode !== null && uncleNode.color === "red") {
-          // Case 1: Uncle is red
-          parentNode.color = "black";
-          uncleNode.color = "black";
-          grandparentNode.color = "red";
-          node = grandparentNode;
-        } else {
-          // Case 2: Uncle is black
-          if (node === parentNode.right) {
-            node = parentNode;
-            this.rotateLeft(node);
-          }
-          // Case 3: Uncle is black (after Case 2)
-          parentNode.color = "black";
-          grandparentNode.color = "red";
-          this.rotateRight(grandparentNode);
-        }
-
-      } else {
-        // Right subtree case
-        const uncleNode = grandparentNode.left;
-
-        if (uncleNode !== null && uncleNode.color === "red") {
-          // Case 1: Uncle is red
-          parentNode.color = "black";
-          uncleNode.color = "black";
-          grandparentNode.color = "red";
-          node = grandparentNode;
-        } else {
-          // Case 2: Uncle is black
-          if (node === parentNode.left) {
-            node = parentNode;
-            this.rotateRight(node);
-          }
-          // Case 3: Uncle is black (after Case 2)
-          parentNode.color = "black";
-          grandparentNode.color = "red";
-          this.rotateLeft(grandparentNode);
-        }
-      }
-    }
-
-    this.root.color = "black"; // Ensure root is always black
-  }
-
-  // Rotate node to the left
-  rotateLeft(node) {
-    const rightChild = node.right;
-    node.right = rightChild.left;
-
-    if (rightChild.left !== null) {
-      rightChild.left.parent = node;
-    }
-
-    rightChild.parent = node.parent;
-
-    if (node.parent === null) {
-      this.root = rightChild;
-    } else if (node === node.parent.left) {
-      node.parent.left = rightChild;
-    } else {
-      node.parent.right = rightChild;
-    }
-
-    rightChild.left = node;
-    node.parent = rightChild;
-  }
-
-  // Rotate node to the right
-  rotateRight(node) {
-    const leftChild = node.left;
-    node.left = leftChild.right;
-
-    if (leftChild.right !== null) {
-      leftChild.right.parent = node;
-    }
-
-    leftChild.parent = node.parent;
-
-    if (node.parent === null) {
-      this.root = leftChild;
-    } else if (node === node.parent.left) {
-      node.parent.left = leftChild;
-    } else {
-      node.parent.right = leftChild;
-    }
-
-    leftChild.right = node;
-    node.parent = leftChild;
-  }
-
-  // Search for a value in the tree
-  search(value) {
-    let current = this.root;
-    while (current !== null) {
-      if (value === current.value) {
-        return true;
-      } else if (value < current.value) {
-        current = current.left;
-      } else {
-        current = current.right;
-      }
-    }
-    return false; // Value not found
-  }
-}
-// Create a Red-Black Tree instance
-const rbTree = new RedBlackTree();
-
-// Insert values
-rbTree.insert(5);
-rbTree.insert(3);
-rbTree.insert(8);
-rbTree.insert(2);
-rbTree.insert(4);
-rbTree.insert(7);
-rbTree.insert(9);
-
-// Search for a value
-console.log(rbTree.search(4)); // Output: true
-console.log(rbTree.search(6)); // Output: false
+// Example usage
+console.log(isPrime(11)); // true
+console.log(isPrime(15)); // false
+console.log(isPrime(23)); // true
