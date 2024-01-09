9b2e3dd2230334e053e2373755e382a6f5194f34
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:08:14 +0800

    1-9-2024 23:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,14 @@
-class ListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
   }
+  
+  return true;
 }
-
-function findMiddleElement(head) {
-  // Initialize slow and fast pointers
-  let slow = head;
-  let fast = head;
-
-  // Move fast pointer two steps ahead and slow pointer one step ahead
-  while (fast !== null && fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  // The slow pointer will be at the middle element
-  return slow.value;
-}
-
-// Example usage
-const list = new ListNode(1);
-list.next = new ListNode(2);
-list.next.next = new ListNode(3);
-list.next.next.next = new ListNode(4);
-list.next.next.next.next = new ListNode(5);
-
-console.log(findMiddleElement(list)); // Output: 3
