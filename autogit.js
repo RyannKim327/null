622a393ef86bcc3576d7d9bbae70f4d3f3778749
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 15:27:01 +0800

    1-9-2024 15:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,8 @@
-function rotate(str) {
-  return str.slice(-1) + str.slice(0, -1);
+function removeVowels(str) {
+  // Use regular expression to match all vowels (case insensitive)
+  return str.replace(/[aeiou]/gi, '');
 }
-let rotations = [text];
 
-for (let i = 1; i < text.length; i++) {
-  rotations.push(rotate(rotations[i - 1]));
-}
-rotations.sort();
-let bwt = rotations.map(str => str.slice(-1)).join('');
-let originalIndex = rotations.indexOf(text);
-return { bwt, originalIndex };
-function burrowsWheelerTransform(text) {
-  function rotate(str) {
-    return str.slice(-1) + str.slice(0, -1);  
-  }
-
-  let rotations = [text];
-
-  for (let i = 1; i < text.length; i++) {
-    rotations.push(rotate(rotations[i - 1]));
-  }
-
-  rotations.sort();
-
-  let bwt = rotations.map(str => str.slice(-1)).join('');
-
-  let originalIndex = rotations.indexOf(text);
-
-  return { bwt, originalIndex };
-}
-let result = burrowsWheelerTransform("banana");
-console.log(result.bwt);   // Outputs: "annb$aa"
-console.log(result.originalIndex);  // Outputs: 4
+var inputString = "Hello World";
+var result = removeVowels(inputString);
+console.log(result); // Output: "Hll Wrld"
