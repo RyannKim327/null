47057dfc78c3d40a190a16e2edfbb595fea0af71
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:11:50 +0800

    1-10-2024 1:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,11 @@
-function longestCommonSubsequence(string1, string2) {
-  const dp = Array(string1.length + 1)
-    .fill(0)
-    .map(() => Array(string2.length + 1).fill(0));
+function isPalindrome(str) {
+  // remove non-alphanumeric characters and convert to lowercase
+  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-  for (let i = 1; i <= string1.length; i++) {
-    for (let j = 1; j <= string2.length; j++) {
-      if (string1[i - 1] === string2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-
-  let lcs = '';
-  let i = string1.length;
-  let j = string2.length;
-
-  while (i > 0 && j > 0) {
-    if (string1[i - 1] === string2[j - 1]) {
-      lcs = string1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
+  // check if the string is the same when reversed
+  return str === str.split('').reverse().join('');
 }
 
-// Example usage:
-const string1 = 'AGGTAB';
-const string2 = 'GXTXAYB';
-const result = longestCommonSubsequence(string1, string2);
-console.log(result); // Output: GTAB
+// example usage
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('hello')); // false
