c0369d99284c37ee0e4ffad24112a80319d5ed53
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:09:31 +0800

    1-9-2024 22:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,5 @@
-function getRandomNumber(min, max) {
-  // Generate a random decimal between 0 and 1
-  const randomDecimal = Math.random();
+const numbers = [5, 3, 10, 1, 2, 8];
+numbers.sort((a, b) => a - b);
 
-  // Map the decimal to the range between min and max
-  const randomNumber = randomDecimal * (max - min + 1) + min;
-
-  // Return the integer value of the random number
-  return Math.floor(randomNumber);
-}
-
-// Example usage: Generate a random number between 1 and 100
-const randomNum = getRandomNumber(1, 100);
-console.log(randomNum);
+console.log(numbers);
+[1, 2, 3, 5, 8, 10]
