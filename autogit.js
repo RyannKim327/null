298aef8ed4dee88877c1dcc908b8ee1c6bdf02a4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:37:43 +0800

    1-10-2024 2:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,91 @@
-function countOccurrences(str, word) {
-  const regex = new RegExp(`\\b${word}\\b`, 'gi');
-  const matches = str.match(regex);
-  return matches ? matches.length : 0;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-const text = "The quick brown fox jumps over the lazy dog.";
-const searchTerm = "the";
-const occurrences = countOccurrences(text, searchTerm);
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-console.log(`Number of occurrences of "${searchTerm}" in the given text: ${occurrences}`);
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  prepend(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  delete(data) {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.head.data === data) {
+      this.head = this.head.next;
+      if (!this.head) {
+        this.tail = null;
+      }
+      return;
+    }
+
+    let current = this.head;
+    while (current.next && current.next.data !== data) {
+      current = current.next;
+    }
+
+    if (current.next) {
+      current.next = current.next.next;
+      if (!current.next) {
+        this.tail = current;
+      }
+    }
+  }
+
+  search(data) {
+    let current = this.head;
+    while (current) {
+      if (current.data === data) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+
+  print() {
+    let current = this.head;
+    let result = '';
+    while (current) {
+      result += current.data + ' ';
+      current = current.next;
+    }
+    console.log(result.trim());
+  }
+}
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.prepend(0);
+linkedList.delete(2);
+linkedList.print(); // Output: 0 1 3
+console.log(linkedList.search(1)); // Output: true
+console.log(linkedList.search(2)); // Output: false
