dcfe42eb92f733bc980133c5a72d634e36ebdbb1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:34:44 +0800

    1-9-2024 23:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,87 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function isPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+  
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
   }
+  
+  return true;
 }
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.size === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
-
-  dequeue() {
-    if (this.size === 0) {
-      return null;
-    }
-    const removedNode = this.head;
-    if (this.size === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.next;
-      removedNode.next = null;
-    }
-    this.size--;
-    return removedNode.value;
-  }
-
-  peek() {
-    if (this.size === 0) {
-      return null;
-    }
-    return this.head.value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  print() {
-    let current = this.head;
-    const values = [];
-    while (current !== null) {
-      values.push(current.value);
-      current = current.next;
-    }
-    console.log(values.join(' -> '));
-  }
-}
-
-// Usage:
-
-const myQueue = new Queue();
-
-myQueue.enqueue(1);
-myQueue.enqueue(2);
-myQueue.enqueue(3);
-
-console.log(myQueue.getSize()); // Output: 3
-
-console.log(myQueue.peek()); // Output: 1
-
-myQueue.print(); // Output: 1 -> 2 -> 3
-
-console.log(myQueue.dequeue()); // Output: 1
-
-myQueue.print(); // Output: 2 -> 3
-
-console.log(myQueue.isEmpty()); // Output: false
+console.log(isPalindrome("racecar"));    // true
+console.log(isPalindrome("hello"));      // false
+console.log(isPalindrome("level"));      // true
+console.log(isPalindrome("A man a plan a canal Panama"));    // false
