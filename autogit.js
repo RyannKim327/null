3d6a47bd71fad975142498f2c522f021c58a0292
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:31:47 +0800

    1-10-2024 1:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,59 @@
-function beamSearch(beamSize, initialState, getNextStates, scoreFunction, terminationCondition) {
-  let beam = [initialState];
+class SkipListNode {
+  constructor(value, level) {
+    this.value = value;
+    this.next = new Array(level + 1);
+    this.level = level;
+  }
+}
+class SkipList {
+  constructor() {
+    this.head = new SkipListNode(null, 0);
+    this.level = 0;
+  }
+  // implement the operations like insert, delete, search, etc.
+}
+class SkipList {
+  // ...
 
-  while (!terminationCondition()) {
-    let nextBeam = [];
-
-    for (let state of beam) {
-      let nextStates = getNextStates(state);
-
-      for (let nextState of nextStates) {
-        nextState.score = scoreFunction(nextState);
-        nextBeam.push(nextState);
+  insert(value) {
+    const update = new Array(this.level + 1);
+    let currentNode = this.head;
+  
+    for (let i = this.level; i >= 0; i--) {
+      while (
+        currentNode.next[i] !== undefined &&
+        currentNode.next[i].value < value
+      ) {
+        currentNode = currentNode.next[i];
+      }
+      update[i] = currentNode;
+    }
+  
+    currentNode = currentNode.next[0];
+  
+    if (currentNode === undefined || currentNode.value !== value) {
+      const newNode = new SkipListNode(value, this.randomLevel());
+  
+      if (newNode.level > this.level) {
+        for (let i = this.level + 1; i <= newNode.level; i++) {
+          update[i] = this.head;
+        }
+  
+        this.level = newNode.level;
+      }
+  
+      for (let i = 0; i <= newNode.level; i++) {
+        newNode.next[i] = update[i].next[i];
+        update[i].next[i] = newNode;
       }
     }
-
-    nextBeam.sort((a, b) => b.score - a.score);
-    beam = nextBeam.slice(0, beamSize);
   }
 
-  return beam[0];
+  randomLevel() {
+    let level = 0;
+    while (Math.random() < 0.5 && level < 32) {
+      level++;
+    }
+    return level;
+  }
 }
