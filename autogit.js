9272e281cad2e1a4fb904dfc9fe922985720fcc2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:23:46 +0800

    1-10-2024 1:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,55 @@
-function isPalindrome(str) {
-  let start = 0;
-  let end = str.length - 1;
-
-  while (start < end) {
-    if (str[start] !== str[end]) {
-      return false;
-    }
-    start++;
-    end--;
+class Node {
+  constructor(label) {
+    this.label = label;
+    this.neighbors = [];
   }
-  
-  return true;
 }
 
-// Example usage:
-console.log(isPalindrome("racecar"));  // Output: true
-console.log(isPalindrome("hello"));    // Output: false
+class Graph {
+  constructor() {
+    this.nodes = {};
+  }
+
+  addNode(label) {
+    this.nodes[label] = new Node(label);
+  }
+
+  addEdge(source, destination) {
+    const sourceNode = this.nodes[source];
+    const destinationNode = this.nodes[destination];
+    sourceNode.neighbors.push(destinationNode);
+    // If your graph is undirected, you may also add the following line
+    // destinationNode.neighbors.push(sourceNode);
+  }
+}
+function depthFirstSearch(graph, startNode) {
+  const visited = {};
+
+  function dfs(node) {
+    if (!node) return;
+
+    visited[node.label] = true;
+    console.log(node.label); // Process the visited node
+
+    for (const neighbor of node.neighbors) {
+      if (!visited[neighbor.label]) {
+        dfs(neighbor); // Recursively visit neighbors
+      }
+    }
+  }
+
+  dfs(graph.nodes[startNode]);
+}
+const graph = new Graph();
+
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addNode('D');
+graph.addNode('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'E');
+depthFirstSearch(graph, 'A');
