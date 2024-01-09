633ea608562c9ec361ebf95f117c7da2c9919674
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:41:22 +0800

    1-9-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,38 @@
-const cron = require('node-cron');
+function generateBadCharTable(pattern) {
+  const table = {};
 
-// Function to be executed
-const task = () => {
-  console.log('Task executed!');
-};
+  for (let i = 0; i < pattern.length - 1; i++) {
+    table[pattern[i]] = pattern.length - 1 - i;
+  }
 
-// Schedule the task to run every minute
-cron.schedule('* * * * *', task);
+  return table;
+}
+function boyerMooreHorspoolSearch(text, pattern) {
+  const badCharTable = generateBadCharTable(pattern);
+  const n = text.length;
+  const m = pattern.length;
+  let i = m - 1;
 
-// Output a message when the script starts
-console.log('Script started.');
+  while (i < n) {
+    let k = 0;
 
-// Keep the script running
-setInterval(() => {
-  console.log('Script running...');
-}, 1000);
+    while (k < m && pattern[m - 1 - k] === text[i - k]) {
+      k++;
+    }
+
+    if (k === m) {
+      return i - m + 1; // Match found, return the starting index
+    } else {
+      const badCharShift = badCharTable[text[i]];
+      i += badCharShift || m; // Shift based on bad character or pattern size
+    }
+  }
+
+  return -1; // No match found
+}
+const text = "Lorem ipsum dolor sit amet";
+const pattern = "sit";
+
+const index = boyerMooreHorspoolSearch(text, pattern);
+
+console.log("Pattern found at index:", index); // Output: Pattern found at index: 17
