fd33f6b51418a904df269b1074beb94d77c87725
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:42:37 +0800

    1-9-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,25 @@
-function findCommonElements(array1, array2) {
-  let commonElements = [];
-  for (let i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      commonElements.push(array1[i]);
-    }
+function burrowsWheelerTransform(input) {
+  function cyclicShift(str) {
+    return str.slice(-1) + str.slice(0, -1);
   }
-  return commonElements;
+  
+  const rotations = [];
+  for (let i = 0; i < input.length; i++) {
+    rotations.push(input);
+    input = cyclicShift(input);
+  }
+  
+  rotations.sort();
+  
+  let bwtString = '';
+  for (let i = 0; i < rotations.length; i++) {
+    bwtString += rotations[i].slice(-1);
+  }
+  
+  return bwtString;
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  return array1.filter((element) => array2.includes(element));
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  const set1 = new Set(array1);
-  const commonElements = [...new Set(array2.filter((element) => set1.has(element)))];
-  return commonElements;
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+// Usage example
+const inputString = 'banana';
+const bwtResult = burrowsWheelerTransform(inputString);
+console.log(bwtResult);
