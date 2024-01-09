f27a7b80259c6667312c375793503ab56ec19f1f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:48:29 +0800

    1-10-2024 2:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,24 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
+function merge(leftArr, rightArr) {
+  let sortedArr = [];
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] <= rightArr[0]) {
+      sortedArr.push(leftArr.shift());
+    } else {
+      sortedArr.push(rightArr.shift());
+    }
+  }
+  // Concatenate any remaining elements
+  return sortedArr.concat(leftArr).concat(rightArr);
+}
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, mid);
+  const rightArr = arr.slice(mid);
 
-console.log(lowercaseStr);  // Output: "hello world"
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
+}
+const array = [8, 4, 2, 9, 3, 1, 5, 7, 6];
+console.log(mergeSort(array));
