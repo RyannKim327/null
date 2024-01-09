ded318367dcedf3ca8def380e6cf466a8e38ab46
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:23:25 +0800

    1-10-2024 1:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,18 @@
-class TrieNode {
-  constructor() {
-    this.value = null;
-    this.children = new Map();
+function isPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false;
+    }
+    start++;
+    end--;
   }
+  
+  return true;
 }
-class Trie {
-  constructor() {
-    this.root = new TrieNode();
-  }
-}
-Trie.prototype.insert = function (word) {
-  let node = this.root;
-  
-  for (let i = 0; i < word.length; i++) {
-    const char = word[i];
-    
-    if (!node.children.has(char)) {
-      node.children.set(char, new TrieNode());
-    }
-    
-    node = node.children.get(char);
-  }
-  
-  node.value = word; // Mark the last node with the word value
-};
-Trie.prototype.search = function (word) {
-  let node = this.root;
-  
-  for (let i = 0; i < word.length; i++) {
-    const char = word[i];
-    
-    if (!node.children.has(char)) {
-      return false; // Word doesn't exist
-    }
-    
-    node = node.children.get(char);
-  }
-  
-  return node.value === word; // Return true if the node represents the complete word
-};
-Trie.prototype.startsWith = function (prefix) {
-  let node = this.root;
-  
-  for (let i = 0; i < prefix.length; i++) {
-    const char = prefix[i];
-    
-    if (!node.children.has(char)) {
-      return false; // Prefix doesn't exist
-    }
-    
-    node = node.children.get(char);
-  }
-  
-  return true; // Prefix exists
-};
+
+// Example usage:
+console.log(isPalindrome("racecar"));  // Output: true
+console.log(isPalindrome("hello"));    // Output: false
