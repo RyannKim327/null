50e4a11b16a7a5987d41335599c23f3554208dc0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:30:25 +0800

    1-10-2024 2:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,23 @@
-let str = "This is a string with spaces";
+function selectionSort(array) {
+  const length = array.length;
+  
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+    
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+    
+    if (minIndex !== i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]];
+    }
+  }
+  
+  return array;
+}
 
-// Using regular expression to remove spaces
-let stringWithoutSpaces = str.replace(/\s/g, "");
-
-console.log(stringWithoutSpaces);  // Output: "Thisisastringwithspaces"
+// Example usage
+const numbers = [64, 25, 12, 22, 11];
+console.log(selectionSort(numbers));
