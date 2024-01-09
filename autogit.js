f9d8dc0c9e1137eb3e31b3bbdb4b137b2087f784
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:45:43 +0800

    1-10-2024 0:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,18 @@
-class Node {
-  constructor(value, left, right) {
-    this.value = value;
-    this.left = left;
-    this.right = right;
+function findSecondLargest(arr) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
   }
+
+  return secondLargest;
 }
+const arr = [1, 5, 2, 9, 10, 6];
 
-function sumBinaryTree(root) {
-  if (!root) {
-    return 0; // Return 0 if the tree is empty
-  }
-
-  // Recursively calculate the sum of nodes in the left and right subtrees
-  const leftSum = sumBinaryTree(root.left);
-  const rightSum = sumBinaryTree(root.right);
-
-  // Return the sum of the current node's value and the sums from the subtrees
-  return root.value + leftSum + rightSum;
-}
-
-// Create a binary tree
-const root = new Node(1);
-
-root.left = new Node(2);
-root.right = new Node(3);
-
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-
-// Calculate the sum of all nodes
-const sum = sumBinaryTree(root);
-console.log(sum); // Output: 28
+console.log(findSecondLargest(arr)); // Output: 9
