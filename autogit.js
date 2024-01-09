498ab9f406b8caf6db6d2a033d9304b66adf5cd4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:29:14 +0800

    1-10-2024 0:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,5 @@
-function getLength(head) {
-  let length = 0;
-  let currentNode = head;
+let array = [5, 12, 8, 9, 3, 15];
 
-  while (currentNode !== null) {
-    length++;
-    currentNode = currentNode.next;
-  }
+let maxValue = Math.max(...array);
 
-  return length;
-}
-// Define a sample node structure
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-// Create a linked list with 3 nodes
-const head = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-
-head.next = node2;
-node2.next = node3;
-
-// Get the length of the linked list
-const length = getLength(head);
-console.log(length); // Output: 3
+console.log(maxValue); // Output: 15
