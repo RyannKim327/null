0765cf04c4caae08727dceb8403232015f792689
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:30:28 +0800

    1-10-2024 0:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,59 @@
-function isAnagram(str1, str2) {
-  // Convert strings to lowercase and remove non-alphabetic characters
-  const cleanStr1 = str1.toLowerCase().replace(/[^a-z]/g, '');
-  const cleanStr2 = str2.toLowerCase().replace(/[^a-z]/g, '');
-
-  // Sort the characters of both strings
-  const sortedStr1 = cleanStr1.split('').sort().join('');
-  const sortedStr2 = cleanStr2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-// Example usage
-const string1 = "listen";
-const string2 = "silent";
+  append(value) {
+    const newNode = new Node(value);
 
-if (isAnagram(string1, string2)) {
-  console.log("The strings are anagrams.");
-} else {
-  console.log("The strings are not anagrams.");
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  isPalindrome() {
+    let slow = this.head;
+    let fast = this.head;
+    let stack = [];
+
+    while (fast && fast.next) {
+      stack.push(slow.value);
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // If the linked list has an odd number of elements,
+    // skip the middle node
+    if (fast) {
+      slow = slow.next;
+    }
+
+    while (slow) {
+      if (stack.pop() !== slow.value) {
+        return false;
+      }
+      slow = slow.next;
+    }
+
+    return true;
+  }
 }
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(2);
+linkedList.append(1);
+console.log(linkedList.isPalindrome()); // Output: true
