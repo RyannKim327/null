e793fda5cb27b21ec2d5384ef6d0739fea85824a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:19:38 +0800

    1-9-2024 23:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,17 @@
-function isAnagram(str1, str2) {
-  const sortedStr1 = str1.replace(/\s/g, '').toLowerCase().split('').sort().join('');
-  const sortedStr2 = str2.replace(/\s/g, '').toLowerCase().split('').sort().join('');
-  
-  return sortedStr1 === sortedStr2;
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let key = array[i];
+        let j = i - 1;
+
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j--;
+        }
+
+        array[j + 1] = key;
+    }
+
+    return array;
 }
-console.log(isAnagram('listen', 'silent')); // Output: true
-console.log(isAnagram('hello', 'world')); // Output: false
+let arr = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(arr));  // Output: [1, 2, 3, 4, 5, 6]
