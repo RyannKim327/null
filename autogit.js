fa6f77e0d0366e752d795c7fe743f8bc48401363
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:02:21 +0800

    1-10-2024 0:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,4 @@
-class TreeNode {
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
@@ -6,69 +6,125 @@ class TreeNode {
   }
 }
 
-function countLeafNodes(root) {
-  if (root === null) {
-    return 0;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
   }
 
-  if (root.left === null && root.right === null) {
-    return 1;
-  }
+  insert(value) {
+    const newNode = new Node(value);
 
-  return countLeafNodes(root.left) + countLeafNodes(root.right);
-}
-
-// Example usage:
-const root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(6);
-
-console.log('Number of leaf nodes:', countLeafNodes(root)); // Output: 3
-class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-function countLeafNodes(root) {
-  if (root === null) {
-    return 0;
-  }
-
-  const stack = [];
-  let count = 0;
-  stack.push(root);
-
-  while (stack.length > 0) {
-    const node = stack.pop();
-
-    if (node.left === null && node.right === null) {
-      count++;
-    }
-
-    if (node.left !== null) {
-      stack.push(node.left);
-    }
-
-    if (node.right !== null) {
-      stack.push(node.right);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
     }
   }
 
-  return count;
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  find(value) {
+    return this.findNode(this.root, value);
+  }
+
+  findNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      return this.findNode(node.left, value);
+    } else if (value > node.value) {
+      return this.findNode(node.right, value);
+    } else {
+      return node;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // Case 1: No child nodes
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      // Case 2: One child node
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // Case 3: Two child nodes
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.removeNode(node.right, minNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  inorder() {
+    this.inorderTraversal(this.root);
+  }
+
+  inorderTraversal(node) {
+    if (node !== null) {
+      this.inorderTraversal(node.left);
+      console.log(node.value);
+      this.inorderTraversal(node.right);
+    }
+  }
 }
+const bst = new BinarySearchTree();
 
-// Example usage:
-const root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(6);
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
 
-console.log('Number of leaf nodes:', countLeafNodes(root)); // Output: 3
+console.log(bst.find(60)); // Output: Node { value: 60, left: null, right: null }
+
+bst.inorder(); // Output: 20 30 40 50 60 70 80
+
+bst.remove(30);
+console.log(bst.find(30)); // Output: null
+
+bst.inorder(); // Output: 20 40 50 60 70 80
