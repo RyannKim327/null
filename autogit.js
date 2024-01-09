8c760fae9cfe734cdfe07bfb7db8b8525bea0363
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 15:25:34 +0800

    1-9-2024 15:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,70 @@
-// Function to perform insertion sort
-function insertionSort(array) {
-  const length = array.length;
-  
-  for (let i = 1; i < length; i++) {
-    let key = array[i];
-    let j = i - 1;
-
-    // Move elements of array[0..i-1], that are greater than key, to one position ahead
-    // of their current position
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
-      j = j - 1;
-    }
-    array[j + 1] = key;
+class Node {
+  constructor(state, parent, action, g, h) {
+    this.state = state;
+    this.parent = parent;
+    this.action = action;
+    this.g = g;
+    this.h = h;
+    this.f = g + h;
   }
-  
-  return array;
 }
 
-// Test the insertion sort function
-const array = [5, 2, 4, 6, 1, 3];
-console.log(insertionSort(array)); // Output: [1, 2, 3, 4, 5, 6]
+function heuristic(node, goal) {
+  // Calculate heuristic value (e.g., using Euclidean Distance)
+  // Return a number representing the estimated cost from `node` to `goal`
+}
+
+function getNeighbors(node) {
+  // Get neighboring nodes from `node`
+  // Return a list of nodes
+}
+
+function AStarSearch(initial, goal) {
+  const openSet = [initial];
+  const closedSet = new Set();
+
+  while (openSet.length > 0) {
+    // Remove node with lowest f value from openSet
+    const current = openSet.shift();
+
+    if (current.state === goal.state) {
+      // Goal reached, backtrack to get the path
+      let path = [];
+      let currentPathNode = current;
+      while (currentPathNode !== null) {
+        path.unshift(currentPathNode);
+        currentPathNode = currentPathNode.parent;
+      }
+      return path;
+    }
+
+    closedSet.add(current);
+
+    const neighbors = getNeighbors(current);
+    for (const neighbor of neighbors) {
+      const isInClosedSet = closedSet.has(neighbor);
+      if (isInClosedSet) {
+        continue;
+      }
+
+      const g = current.g + neighbor.cost;
+      const h = heuristic(neighbor, goal);
+      const f = g + h;
+
+      const isOpenSet = openSet.includes(neighbor);
+      if (!isOpenSet || g < neighbor.g) {
+        neighbor.g = g;
+        neighbor.h = h;
+        neighbor.f = f;
+        neighbor.parent = current;
+
+        if (!isOpenSet) {
+          openSet.push(neighbor);
+        }
+      }
+    }
+  }
+
+  // No solution found
+  return null;
+}
