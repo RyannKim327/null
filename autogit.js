f738e3a46c47c89ebf9a4b4a00310bd58365ee76
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 15:28:33 +0800

    1-9-2024 15:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,26 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
+const head = new Node(1);
+const second = new Node(2);
+const third = new Node(3);
 
-class LinkedList {
-  constructor() {
-    this.head = null;
+head.next = second;
+second.next = third;
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
 
-  addNode(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let currentNode = this.head;
-      while (currentNode.next) {
-        currentNode = currentNode.next;
-      }
-      currentNode.next = newNode;
-    }
-  }
-
-  findNthNodeFromEnd(n) {
-    if (!this.head || n <= 0) {
-      return null;
-    }
-
-    let slow = this.head;
-    let fast = this.head;
-
-    for (let i = 0; i < n; i++) {
-      if (fast === null) {
-        return null; // if n is greater than the length of the list
-      }
-      fast = fast.next;
-    }
-
-    while (fast !== null) {
-      slow = slow.next;
-      fast = fast.next;
-    }
-
-    return slow.data;
-  }
+  return prev;
 }
-
-// Example usage:
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
-
-console.log(linkedList.findNthNodeFromEnd(2)); // Output: 4
+head = reverseLinkedList(head);
