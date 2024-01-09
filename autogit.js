11c6935ab708d2a6dd5e4cf4a5b79441ca5a5be4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 21:39:34 +0800

    1-9-2024 21:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,19 @@
-let stringNumber = "42";
-let integerNumber = parseInt(stringNumber);
+function factorialLoop(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
 
-console.log(integerNumber); // Output: 42
+// Example usage
+console.log(factorialLoop(5)); // Output: 120
+function factorialRecursion(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorialRecursion(n - 1);
+}
+
+// Example usage
+console.log(factorialRecursion(5)); // Output: 120
