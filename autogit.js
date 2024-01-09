976af3b704199c75c6ca4b3be4140e2c41625332
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:25:27 +0800

    1-10-2024 0:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,5 @@
-function getMaxDigits(arr) {
-  let maxDigits = 0;
-  for (let num of arr) {
-    maxDigits = Math.max(maxDigits, Math.floor(Math.log10(Math.abs(num))) + 1);
-  }
-  return maxDigits;
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
 }
-function radixSort(arr) {
-  const maxDigits = getMaxDigits(arr);
-  let buckets = Array.from({ length: 10 }, () => []);
-}
-function radixSort(arr) {
-  const maxDigits = getMaxDigits(arr);
-  let buckets = Array.from({ length: 10 }, () => []);
-
-  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
-    // Code for each digit position
-  }
-}
-function radixSort(arr) {
-  const maxDigits = getMaxDigits(arr);
-  let buckets = Array.from({ length: 10 }, () => []);
-
-  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
-    for (let num of arr) {
-      let digit = Math.floor(Math.abs(num) / Math.pow(10, digitPosition)) % 10;
-      buckets[digit].push(num);
-    }
-  }
-}
-function radixSort(arr) {
-  const maxDigits = getMaxDigits(arr);
-  let buckets = Array.from({ length: 10 }, () => []);
-
-  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
-    for (let num of arr) {
-      let digit = Math.floor(Math.abs(num) / Math.pow(10, digitPosition)) % 10;
-      buckets[digit].push(num);
-    }
-
-    arr = [].concat(...buckets);
-    buckets = Array.from({ length: 10 }, () => []);
-  }
-
-  return arr;
-}
-function radixSort(arr) {
-  // Step 1-6 code goes here...
-
-  return arr;
-}
-const array = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(array)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+var randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber);
