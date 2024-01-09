052692b757aebfa91ca653b4eb5962105df756eb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:18:13 +0800

    1-10-2024 2:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,22 @@
-function mergeSort(array) {
-  const length = array.length;
-  const tempArray = Array.from(array);
-  
-  for (let width = 1; width < length; width *= 2) {
-    for (let i = 0; i < length; i += 2 * width) {
-      merge(array, tempArray, i, Math.min(i + width, length), Math.min(i + 2 * width, length));
-    }
-    
-    // Swap the arrays
-    [array, tempArray] = [tempArray, array];
+function isPrime(num) {
+  // Check if the number is less than 2
+  if (num < 2) {
+    return false;
   }
-  
-  return array;
+
+  // Loop from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    // If the number is divisible by any i, it is not prime
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  // The number is prime if it passes the previous checks
+  return true;
 }
 
-function merge(array, tempArray, left, mid, right) {
-  let i = left;
-  let j = mid;
-  
-  for (let k = left; k < right; k++) {
-    if (i < mid && (j >= right || array[i] <= array[j])) {
-      tempArray[k] = array[i];
-      i++;
-    } else {
-      tempArray[k] = array[j];
-      j++;
-    }
-  }
-}
-const array = [4, 3, 2, 1, 5];
-const sortedArray = mergeSort(array);
-console.log(sortedArray);
+// Usage
+console.log(isPrime(5));  // output: true
+console.log(isPrime(10)); // output: false
+console.log(isPrime(17)); // output: true
