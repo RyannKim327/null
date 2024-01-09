9da5da8c0d9e8ee9d0782ea9a2e297f9a9796d77
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:49:22 +0800

    1-10-2024 1:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,77 @@
-function generateFibonacciArray(n) {
-  let fibSeq = [0, 1];
-  let i = 2;
-  
-  while (fibSeq[i - 1] < n) {
-    fibSeq[i] = fibSeq[i - 1] + fibSeq[i - 2];
-    i++;
+class BTreeNode {
+  constructor(degree, leaf) {
+    this.degree = degree;
+    this.leaf = leaf;
+    this.keys = [];
+    this.children = [];
   }
-  
-  return fibSeq;
 }
-function fibonacciSearch(arr, target) {
-  let fibSeq = generateFibonacciArray(arr.length);
-  let offset = -1;
 
-  while (fibSeq.length > 2) {
-    const fibIndex = fibSeq.length - 3;
-    const mid = offset + fibSeq[fibIndex];
+class BTree {
+  constructor(degree) {
+    this.degree = degree;
+    this.root = new BTreeNode(degree, true);
+  }
 
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] > target) {
-      fibSeq.splice(fibIndex + 1);
+  insert(key) {
+    const root = this.root;
+    if (root.keys.length === (2 * this.degree) - 1) {
+      const newNode = new BTreeNode(this.degree, false);
+      this.root = newNode;
+      newNode.children.push(root);
+      this.splitChild(newNode, 0);
+      this.insertNonFull(newNode, key);
     } else {
-      fibSeq.splice(fibIndex, 1);
-      offset = mid;
+      this.insertNonFull(root, key);
     }
   }
 
-  if (fibSeq[0] === target && arr[offset + 1] === target) {
-    return offset + 1;
+  insertNonFull(node, key) {
+    let index = node.keys.length - 1;
+    if (node.leaf) {
+      while (index >= 0 && key < node.keys[index]) {
+        node.keys[index + 1] = node.keys[index];
+        index--;
+      }
+      node.keys[index + 1] = key;
+    } else {
+      while (index >= 0 && key < node.keys[index]) {
+        index--;
+      }
+      index++;
+      if (node.children[index].keys.length === (2 * this.degree) - 1) {
+        this.splitChild(node, index);
+        if (key > node.keys[index]) {
+          index++;
+        }
+      }
+      this.insertNonFull(node.children[index], key);
+    }
   }
 
-  return -1;
-}
-const numbers = [1, 5, 8, 12, 20, 28, 30, 42, 50];
-const target = 30;
+  splitChild(parent, index) {
+    const degree = this.degree;
+    const child = parent.children[index];
+    const newNode = new BTreeNode(degree, child.leaf);
 
-const index = fibonacciSearch(numbers, target);
-if (index !== -1) {
-  console.log(`Element ${target} found at index ${index}.`);
-} else {
-  console.log(`Element ${target} not found in the array.`);
+    parent.keys.splice(index, 0, child.keys[degree - 1]);
+    parent.children.splice(index + 1, 0, newNode);
+
+    newNode.keys = child.keys.splice(degree, degree - 1);
+    newNode.children = child.children.splice(degree, degree);
+  }
+
+  search(key, node = this.root) {
+    let i = 0;
+    while (i < node.keys.length && key > node.keys[i]) {
+      i++;
+    }
+    if (node.keys[i] === key) {
+      return true;
+    }
+    if (node.leaf) {
+      return false;
+    }
+    return this.search(key, node.children[i]);
+  }
 }
