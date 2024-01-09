18b980785269a8965844e50750a2e38d9df96ae3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:18:49 +0800

    1-10-2024 1:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,56 @@
-const numbers = [5, 10, 2, 8, 3];
-const max = Math.max(...numbers);
-console.log(max); // Output: 10
+function prefixTable(pattern) {
+  const table = [0];
+  let prefixLen = 0;
+  let i = 1;
+  
+  while (i < pattern.length) {
+    if (pattern[i] === pattern[prefixLen]) {
+      prefixLen++;
+      table[i] = prefixLen;
+      i++;
+    } else {
+      if (prefixLen !== 0) {
+        prefixLen = table[prefixLen - 1];
+      } else {
+        table[i] = 0;
+        i++;
+      }
+    }
+  }
+  
+  return table;
+}
+function KMPSearch(pattern, text) {
+  const result = [];
+  const M = pattern.length;
+  const N = text.length;
+  const table = prefixTable(pattern);
+  
+  let i = 0;
+  let j = 0;
+  
+  while (i < N) {
+    if (pattern[j] === text[i]) {
+      i++;
+      j++;
+    }
+    
+    if (j === M) {
+      result.push(i - j);
+      j = table[j - 1];
+    } else if (i < N && pattern[j] !== text[i]) {
+      if (j !== 0) {
+        j = table[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+  
+  return result;
+}
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+
+const indices = KMPSearch(pattern, text);
+console.log(indices); // Output: [10]
