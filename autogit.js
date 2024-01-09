8a01077d45d7529ffc92d573700b204e8ad7c14d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:23:23 +0800

    1-9-2024 23:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,66 @@
-function boyerMooreHorspool(text, pattern) {
-  let skipTable = {};
-
-  for (let i = 0; i < pattern.length - 1; i++) {
-    skipTable[pattern[i]] = pattern.length - 1 - i;
+class Graph {
+  constructor(numVertices) {
+    this.vertices = [];
+    this.edges = [];
+    this.numVertices = numVertices;
   }
 
-  let n = text.length;
-  let m = pattern.length;
-  let i = 0;
+  addVertex(vertex) {
+    this.vertices.push(vertex);
+  }
 
-  while (i <= n - m) {
-    if (pattern === text.slice(i, i + m)) {
-      return i;
-    } else {
-      i += skipTable[text[i + m - 1]] || m;
+  addEdge(source, destination, weight) {
+    const edge = new Edge(source, destination, weight);
+    this.edges.push(edge);
+  }
+}
+
+class Edge {
+    constructor(source, destination, weight) {
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
+function bellmanFord(graph, source) {
+  const distances = new Array(graph.numVertices).fill(Infinity);
+  distances[source] = 0;
+
+  for (let i = 0; i < graph.numVertices - 1; i++) {
+    for (const edge of graph.edges) {
+      const { source, destination, weight } = edge;
+      if (distances[source] + weight < distances[destination]) {
+        distances[destination] = distances[source] + weight;
+      }
     }
   }
 
-  return -1;
+  for (const edge of graph.edges) {
+    const { source, destination, weight } = edge;
+    if (distances[source] + weight < distances[destination]) {
+      console.log("Negative weight cycle found!");
+      return [];
+    }
+  }
+
+  return distances;
 }
+// Create a graph
+const graph = new Graph(5);
+graph.addVertex(0);
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
 
-// Example usage:
-let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-let pattern = "ipsum";
+graph.addEdge(0, 1, 2);
+graph.addEdge(0, 2, 4);
+graph.addEdge(1, 2, -1);
+graph.addEdge(1, 3, 1);
+graph.addEdge(2, 3, 3);
+graph.addEdge(3, 4, 2);
+graph.addEdge(4, 2, -3);
 
-console.log(boyerMooreHorspool(text, pattern)); // Output: 6
+const source = 0;
+const distances = bellmanFord(graph, source);
+console.log(distances);
