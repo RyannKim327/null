a73550f619cd17f63bc15699555c23bc9836767e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:53:30 +0800

    1-10-2024 0:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,11 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  if (start > end) {
-    return -1;
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  const mid = Math.floor((start + end) / 2);
-
-  if (arr[mid] === target) {
-    return mid;
-  } else if (target < arr[mid]) {
-    return binarySearch(arr, target, start, mid - 1);
-  } else {
-    return binarySearch(arr, target, mid + 1, end);
-  }
+  return slow.value;
 }
-const array = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
-const target = 23;
-
-const index = binarySearch(array, target);
-console.log(`Element ${target} found at index ${index}`);
