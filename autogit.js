be816140a0b6e559953714932212142ba4befad2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:08:48 +0800

    1-9-2024 22:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,15 @@
-function selectionSort(arr) {
-  var n = arr.length;
-
-  for (var i = 0; i < n - 1; i++) {
-    var minIndex = i;
-
-    for (var j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    if (minIndex !== i) {
-      var temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
-    }
+function maxSubarraySum(arr) {
+  let maxSum = arr[0];
+  let currentSum = arr[0];
+  
+  for (let i = 1; i < arr.length; i++) {
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    maxSum = Math.max(maxSum, currentSum);
   }
 
-  return arr;
+  return maxSum;
 }
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const maxSum = maxSubarraySum(array);
 
-// Example usage:
-var arr = [64, 25, 12, 22, 11];
-console.log(selectionSort(arr)); // Output: [11, 12, 22, 25, 64]
+console.log(maxSum); // Output: 6 (subarray: [4, -1, 2, 1])
