63341d1c58b0c2113586e052ff5b0ccae4651e99
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:36:50 +0800

    1-10-2024 0:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,33 @@
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
+function bwt(input) {
+  // Code to implement BWT algorithm
 }
-
-// Example usage:
-var base = 10;
-var height = 5;
-var area = calculateTriangleArea(base, height);
-console.log('The area of the triangle is: ' + area);
+let rotations = [];
+for (let i = 0; i < input.length; i++) {
+  let rotation = input.slice(i) + input.slice(0, i);
+  rotations.push(rotation);
+}
+rotations.sort();
+let transformed = "";
+for (let rotation of rotations) {
+  transformed += rotation.charAt(rotation.length - 1);
+}
+return transformed;
+function bwt(input) {
+  let rotations = [];
+  for (let i = 0; i < input.length; i++) {
+    let rotation = input.slice(i) + input.slice(0, i);
+    rotations.push(rotation);
+  }
+  
+  rotations.sort();
+  
+  let transformed = "";
+  for (let rotation of rotations) {
+    transformed += rotation.charAt(rotation.length - 1);
+  }
+  
+  return transformed;
+}
+let inputStr = "banana";
+let transformedStr = bwt(inputStr);
+console.log(transformedStr);  // Output: "annb$aa"
