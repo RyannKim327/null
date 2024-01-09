e09870d54f568f89152803d20c81e16961ece967
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:23:01 +0800

    1-10-2024 0:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,53 @@
-var str = 'Hello, World!';
-var reversed = str.split('').reverse().join('');
-console.log(reversed);  // Output: "!dlroW ,olleH"
-var str = 'Hello, World!';
-var reversed = '';
-for (var i = str.length - 1; i >= 0; i--) {
-  reversed += str.charAt(i);
+function getMaxDigits(arr) {
+  let maxDigits = 0;
+  for (let num of arr) {
+    maxDigits = Math.max(maxDigits, Math.floor(Math.log10(Math.abs(num))) + 1);
+  }
+  return maxDigits;
 }
-console.log(reversed);  // Output: "!dlroW ,olleH"
+function radixSort(arr) {
+  const maxDigits = getMaxDigits(arr);
+  let buckets = Array.from({ length: 10 }, () => []);
+}
+function radixSort(arr) {
+  const maxDigits = getMaxDigits(arr);
+  let buckets = Array.from({ length: 10 }, () => []);
+
+  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
+    // Code for each digit position
+  }
+}
+function radixSort(arr) {
+  const maxDigits = getMaxDigits(arr);
+  let buckets = Array.from({ length: 10 }, () => []);
+
+  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
+    for (let num of arr) {
+      let digit = Math.floor(Math.abs(num) / Math.pow(10, digitPosition)) % 10;
+      buckets[digit].push(num);
+    }
+  }
+}
+function radixSort(arr) {
+  const maxDigits = getMaxDigits(arr);
+  let buckets = Array.from({ length: 10 }, () => []);
+
+  for (let digitPosition = 0; digitPosition < maxDigits; digitPosition++) {
+    for (let num of arr) {
+      let digit = Math.floor(Math.abs(num) / Math.pow(10, digitPosition)) % 10;
+      buckets[digit].push(num);
+    }
+
+    arr = [].concat(...buckets);
+    buckets = Array.from({ length: 10 }, () => []);
+  }
+
+  return arr;
+}
+function radixSort(arr) {
+  // Step 1-6 code goes here...
+
+  return arr;
+}
+const array = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(array)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
