3750f972f716ad3c9177fc0a9dc119e2f05271bc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:45:12 +0800

    1-10-2024 2:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,14 @@
-let str = "Hello World!";
-let lowercaseStr = str.toLowerCase();
+function randomSort(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
 
-console.log(lowercaseStr);
-hello world!
+// Example usage:
+const array = [1, 2, 3, 4, 5];
+console.log("Before sorting:", array);
+
+const sortedArray = randomSort(array);
+console.log("After sorting:", sortedArray);
