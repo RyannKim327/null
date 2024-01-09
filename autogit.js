b1b3c8876e8ac8e66d5dbe4cb58f2d88e27ac513
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 23:31:31 +0800

    1-9-2024 23:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,14 @@
-function boyerMooreHorspool(text, pattern) {
-  const matches = [];
-  const shiftTable = {};
-
-  // Initialize shift table
-  for (let i = 0; i < pattern.length - 1; i++) {
-    shiftTable[pattern[i]] = pattern.length - 1 - i;
-  }
-
-  let i = pattern.length - 1;
-  while (i < text.length) {
-    let j = 0;
-
-    // Check for a match
-    while (j < pattern.length && text[i - j] === pattern[pattern.length - 1 - j]) {
-      j++;
-    }
-
-    if (j === pattern.length) {
-      // Match found, push the index to the matches array
-      matches.push(i - pattern.length + 1);
-    }
-
-    // Update the index with the shift value
-    if (shiftTable[text[i]]) {
-      i += shiftTable[text[i]];
-    } else {
-      i += pattern.length;
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
     }
   }
-
-  return matches;
+  return count;
 }
-
-// Example usage
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-const pattern = "ipsum";
-const matches = boyerMooreHorspool(text, pattern);
-console.log(matches);  // Output: [6]
+let str = "Hello, World!";
+let char = "l";
+let occurrences = countOccurrences(str, char);
+console.log(`Number of occurrences of "${char}" in "${str}": ${occurrences}`);
+// Output: Number of occurrences of "l" in "Hello, World!": 3
