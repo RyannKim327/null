190e26e3c7f8d5c28d0e7565025995c84ba8b4c2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 00:35:25 +0800

    1-10-2024 0:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,152 @@
+// Define a class for red-black tree nodes
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.color = "red"; // "red" or "black"
   }
 }
 
-class LinkedList {
+// Define the red-black tree class
+class RedBlackTree {
   constructor() {
-    this.head = null;
+    this.root = null;
   }
 
-  addNode(data) {
-    const newNode = new Node(data);
+  // Insert a value into the tree
+  insert(value) {
+    const newNode = new Node(value);
 
-    if (!this.head) {
-      this.head = newNode;
+    // Add the new node as a regular binary search tree would
+    if (!this.root) {
+      this.root = newNode;
     } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
+      let currentNode = this.root;
+      while (currentNode) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.right;
+        }
       }
-      current.next = newNode;
     }
+
+    this.fixViolations(newNode);
   }
 
-  getLength() {
-    let current = this.head;
-    let count = 0;
+  // Perform necessary rotations and recoloring to fix tree violations
+  fixViolations(node) {
+    while (node && node.parent && node.parent.color === "red") {
+      let uncle = null;
 
-    while (current) {
-      count++;
-      current = current.next;
+      if (node.parent === node.parent.parent.left) {
+        uncle = node.parent.parent.right; // Get the uncle node
+        if (uncle && uncle.color === "red") {
+          // Case 1: Uncle is red
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.right) {
+            // Case 2: Uncle is black and node is a right child
+            node = node.parent;
+            this.rotateLeft(node);
+          }
+
+          // Case 3: Uncle is black and node is a left child
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateRight(node.parent.parent);
+        }
+      } else {
+        uncle = node.parent.parent.left; // Get the uncle node
+        if (uncle && uncle.color === "red") {
+          // Case 1: Uncle is red
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.left) {
+            // Case 2: Uncle is black and node is a left child
+            node = node.parent;
+            this.rotateRight(node);
+          }
+
+          // Case 3: Uncle is black and node is a right child
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateLeft(node.parent.parent);
+        }
+      }
+    }
+    this.root.color = "black"; // Ensure the root is always black
+  }
+
+  // Rotate the tree left at the given node
+  rotateLeft(node) {
+    const rightChild = node.right;
+    node.right = rightChild.left;
+
+    if (rightChild.left) {
+      rightChild.left.parent = node;
     }
 
-    return count;
+    rightChild.parent = node.parent;
+
+    if (!node.parent) {
+      this.root = rightChild;
+    } else if (node === node.parent.left) {
+      node.parent.left = rightChild;
+    } else {
+      node.parent.right = rightChild;
+    }
+
+    rightChild.left = node;
+    node.parent = rightChild;
+  }
+
+  // Rotate the tree right at the given node
+  rotateRight(node) {
+    const leftChild = node.left;
+    node.left = leftChild.right;
+
+    if (leftChild.right) {
+      leftChild.right.parent = node;
+    }
+
+    leftChild.parent = node.parent;
+
+    if (!node.parent) {
+      this.root = leftChild;
+    } else if (node === node.parent.right) {
+      node.parent.right = leftChild;
+    } else {
+      node.parent.left = leftChild;
+    }
+
+    leftChild.right = node;
+    node.parent = leftChild;
   }
 }
 
-// Usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-
-console.log(linkedList.getLength()); // Output: 3
+// Example usage
+const tree = new RedBlackTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(30);
+tree.insert(40);
+tree.insert(50);
