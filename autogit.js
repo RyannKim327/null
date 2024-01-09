c2ba6303a24852e2261f3fdc4028b66af7d1c98d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:04:18 +0800

    1-10-2024 2:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,22 @@
-class Node {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
-  }
+let str = "Hello World";
+let reversedStr = str.split('').reverse().join('');
+console.log(reversedStr); // Output: "dlroW olleH"
+let str = "Hello World";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
+}
+console.log(reversedStr); // Output: "dlroW olleH"
+let str = "Hello World";
+let reversedStr = [...str].reverse().join('');
+console.log(reversedStr); // Output: "dlroW olleH"
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
 }
 
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-
-  while (current !== null) {
-    const next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  return prev; // new head of the reversed list
-}
-// Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-console.log('Original Linked List:');
-let temp = head;
-while (temp !== null) {
-  console.log(temp.value);
-  temp = temp.next;
-}
-
-const reversedHead = reverseLinkedList(head);
-console.log('Reversed Linked List:');
-temp = reversedHead;
-while (temp !== null) {
-  console.log(temp.value);
-  temp = temp.next;
-}
-Original Linked List:
-1
-2
-3
-4
-5
-Reversed Linked List:
-5
-4
-3
-2
-1
+let str = "Hello World";
+let reversedStr = reverseString(str);
+console.log(reversedStr); // Output: "dlroW olleH"
