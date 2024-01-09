56daef805a8fffa025d3cdfe364bb365ed80f243
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:28:38 +0800

    1-10-2024 1:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,6 @@
-function findMaxSubarray(arr) {
-  let maxSum = arr[0];
-  let currentSum = arr[0];
-  let startIndex = 0;
-  let endIndex = 0;
-  let tempStartIndex = 0;
+let str = "   Hello,   World!   ";
 
-  for (let i = 1; i < arr.length; i++) {
-    if (currentSum + arr[i] < arr[i]) {
-      currentSum = arr[i];
-      tempStartIndex = i;
-    } else {
-      currentSum += arr[i];
-    }
+// Using Regular Expression to remove whitespace
+let trimmedStr = str.replace(/\s/g, "");
 
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-      startIndex = tempStartIndex;
-      endIndex = i;
-    }
-  }
-
-  return arr.slice(startIndex, endIndex + 1);
-}
-
-// Example usage:
-const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const maxSubarray = findMaxSubarray(array);
-console.log(maxSubarray); // Output: [4, -1, 2, 1]
+console.log(trimmedStr); // Output: "Hello,World!"
