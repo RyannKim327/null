62c7c73b35e10303402990f44ea9be041e300d48
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 01:06:32 +0800

    1-10-2024 1:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,14 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
+let str1 = "Hello";
+let str2 = "world";
 
-  enqueue(value) {
-    const newNode = new Node(value);
+let concatenated = str1 + " " + str2;
 
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
+console.log(concatenated);
+// Output: Hello world
+let str1 = "Hello";
+let str2 = "world";
 
-    this.size++;
-  }
+let concatenated = str1.concat(" ", str2);
 
-  dequeue() {
-    if (!this.head) {
-      return null; // Empty queue
-    }
-
-    const removedNode = this.head;
-    this.head = this.head.next;
-    removedNode.next = null;
-    this.size--;
-
-    if (!this.head) {
-      this.tail = null; // Last element removed
-    }
-
-    return removedNode.value;
-  }
-}
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-console.log(queue.dequeue()); // Output: 3
-console.log(queue.dequeue()); // Output: null (empty queue)
+console.log(concatenated);
+// Output: Hello world
