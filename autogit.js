d480b3e5998c0335bfe76ea91a0ba4328a76c4e1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 02:03:10 +0800

    1-10-2024 2:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,34 @@
-// Node class represents a particular node in the graph
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.adjacentNodes = [];
-    this.visited = false;
-  }
-  
-  addAdjacentNode(node) {
-    this.adjacentNodes.push(node);
-  }
-}
+function longestCommonSubstring(str1, str2) {
+  // Create a matrix to store lengths of longest common suffixes of substrings
+  const matrix = Array(str1.length + 1)
+    .fill(0)
+    .map(() => Array(str2.length + 1).fill(0));
 
-// DFS algorithm implementation
-function depthFirstSearch(node) {
-  if (!node) {
-    return;
-  }
-  
-  node.visited = true;
-  console.log(node.value);
-  
-  for (let adjacentNode of node.adjacentNodes) {
-    if (!adjacentNode.visited) {
-      depthFirstSearch(adjacentNode);
+  let maxLength = 0; // Length of longest common substring
+  let endIndex = 0; // Index of the last character of longest common substring
+
+  // Fill the matrix and update max length and ending index
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1] + 1;
+        if (matrix[i][j] > maxLength) {
+          maxLength = matrix[i][j];
+          endIndex = i - 1;
+        }
+      } else {
+        matrix[i][j] = 0;
+      }
     }
   }
+
+  // Extract the longest common substring using the ending index
+  const longestSubstring = str1.substring(endIndex - maxLength + 1, endIndex + 1);
+  return longestSubstring;
 }
 
 // Example usage
-// Create nodes
-const nodeA = new Node("A");
-const nodeB = new Node("B");
-const nodeC = new Node("C");
-const nodeD = new Node("D");
-const nodeE = new Node("E");
-
-// Build the graph
-nodeA.addAdjacentNode(nodeB);
-nodeA.addAdjacentNode(nodeC);
-nodeB.addAdjacentNode(nodeD);
-nodeD.addAdjacentNode(nodeE);
-nodeE.addAdjacentNode(nodeB);
-
-// Call DFS starting from nodeA
-depthFirstSearch(nodeA);
+const str1 = "javascript";
+const str2 = "substring";
+const longestSubStr = longestCommonSubstring(str1, str2);
+console.log(longestSubStr); // Output: "string"
