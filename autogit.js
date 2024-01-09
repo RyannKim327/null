f9009cfa3aa7b6c903ebecb2199e56ec11f6bab9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-09 22:20:32 +0800

    1-9-2024 22:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,25 @@
-function reverseLinkedList(head) {
-  if (!head || !head.next) {
-    return head;
+function findLongestCommonPrefix(strings) {
+  if (strings.length === 0) {
+    return "";
   }
 
-  let previousNode = null;
-  let currentNode = head;
-  let nextNode = null;
+  let prefix = "";
+  const minString = strings.reduce((min, str) => (str.length < min.length ? str : min));
 
-  while (currentNode) {
-    nextNode = currentNode.next;
-    currentNode.next = previousNode;
-    previousNode = currentNode;
-    currentNode = nextNode;
+  for (let i = 0; i < minString.length; i++) {
+    const char = minString[i];
+    for (const str of strings) {
+      if (str[i] !== char) {
+        return prefix;
+      }
+    }
+    prefix += char;
   }
 
-  head = previousNode;
-  return head;
+  return prefix;
 }
+
+// Example usage:
+const strings = ["flower", "flow", "flight"];
+const longestCommonPrefix = findLongestCommonPrefix(strings);
+console.log(longestCommonPrefix);  // Output: "fl"
