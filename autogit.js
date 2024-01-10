2838075562b248e25858974b5a70c995afb960b5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 22:13:30 +0800

    1-10-2024 22:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,28 @@
-function largestPrimeFactor(number) {
-  let divisor = 2;
-  while (divisor <= number) {
-    if (number % divisor === 0) {
-      number /= divisor;
-    } else {
-      divisor++;
+function interpolationSearch(arr, x, low, high) {
+  if (low <= high && x >= arr[low] && x <= arr[high]) {
+    const position =
+      low +
+      Math.floor(((high - low) * (x - arr[low])) / (arr[high] - arr[low]));
+
+    if (arr[position] === x) {
+      return position;
+    }
+
+    if (arr[position] < x) {
+      return interpolationSearch(arr, x, position + 1, high);
+    }
+
+    if (arr[position] > x) {
+      return interpolationSearch(arr, x, low, position - 1);
     }
   }
-  return divisor;
+
+  return -1;
 }
 
-// Usage example
-const number = 123456789;
-const largestFactor = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is ${largestFactor}`);
+// Usage example:
+const array = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+const element = 14;
+
+const index = interpolationSearch(array, element, 0, array.length - 1);
+console.log(`Element found at index ${index}`);
