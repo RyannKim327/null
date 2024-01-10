beb732c37d9b177266ed0e1ceeefe45d83059c12
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 22:23:05 +0800

    1-10-2024 22:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,31 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let current = arr[i];
-    let j = i - 1;
-
-    while (j >= 0 && arr[j] > current) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-
-    arr[j + 1] = current;
+class ListNode {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
   }
-
-  return arr;
 }
 
-// Example usage:
-let numbers = [5, 3, 1, 4, 2];
-console.log(insertionSort(numbers)); // Output: [1, 2, 3, 4, 5]
+function isPalindrome(head) {
+  const values = [];
+
+  // Traverse the linked list and store values in an array
+  let current = head;
+  while (current !== null) {
+    values.push(current.val);
+    current = current.next;
+  }
+
+  let start = 0;
+  let end = values.length - 1;
+
+  // Compare values at both pointers
+  while (start < end) {
+    if (values[start] !== values[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
+}
