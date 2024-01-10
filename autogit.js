d5b9b4e3265bd581d85708670253bb04783d9f71
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 21:52:18 +0800

    1-10-2024 21:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,16 @@
-function isAnagram(str1, str2) {
-  // Remove any non-alphabetic characters and convert to lowercase
-  str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-  str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-  // Sort the characters of both strings
-  const sortedStr1 = str1.split('').sort().join('');
-  const sortedStr2 = str2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
+function largestPrimeFactor(number) {
+  let divisor = 2;
+  while (divisor <= number) {
+    if (number % divisor === 0) {
+      number /= divisor;
+    } else {
+      divisor++;
+    }
+  }
+  return divisor;
 }
 
-// Example usage
-const string1 = 'listen';
-const string2 = 'silent';
-console.log(isAnagram(string1, string2)); // Output: true
+// Usage example
+const number = 123456789;
+const largestFactor = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestFactor}`);
