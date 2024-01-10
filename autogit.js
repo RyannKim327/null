70b9550322c9c78a5556ba5396e9525d7029001a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-10 21:49:45 +0800

    1-10-2024 21:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,17 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+function isAnagram(str1, str2) {
+  // Remove any non-alphabetic characters and convert to lowercase
+  str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+
+  // Sort the characters of both strings
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
+}
+
+// Example usage
+const string1 = 'listen';
+const string2 = 'silent';
+console.log(isAnagram(string1, string2)); // Output: true
