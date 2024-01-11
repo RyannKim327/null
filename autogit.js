8af72129dedcae8ca792448d8887ceaaee6b8c1e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:24:46 +0800

    1-12-2024 0:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,43 @@
-let str = "  Hello   World  ";
+function buildMaxHeap(arr) {
+    let length = arr.length;
+    let start = Math.floor(length / 2) - 1;
 
-// Remove all whitespace using regular expression and replace()
-let result = str.replace(/\s/g, "");
+    for (let i = start; i >= 0; i--) {
+        heapify(arr, length, i);
+    }
+}
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
 
-console.log(result);
-HelloWorld
-let str = "  Hello   World  ";
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
 
-// Remove leading and trailing whitespace using trim()
-let result = str.trim();
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
 
-console.log(result);
-Hello   World
+    if (largest !== i) {
+        let temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+function heapSort(arr) {
+    let length = arr.length;
+
+    buildMaxHeap(arr);
+
+    for (let i = length - 1; i > 0; i--) {
+        let temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+    return arr;
+}
+let array = [12, 11, 13, 5, 6, 7];
+console.log(heapSort(array)); // Output: [5, 6, 7, 11, 12, 13]
