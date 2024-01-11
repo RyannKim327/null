0b9bd4e70be860c9058cb579c4a8dba54e79a92e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:56:15 +0800

    1-11-2024 22:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,71 +1,17 @@
-class Graph {
-  constructor(vertices) {
-    this.vertices = vertices;
-    this.edges = [];
+function isPrime(number) {
+  // Handle special cases
+  if (number <= 1) {
+    return false;
   }
-
-  addEdge(source, destination, weight) {
-    this.edges.push(new Edge(source, destination, weight));
-  }
-}
-
-class Edge {
-  constructor(source, destination, weight) {
-    this.source = source;
-    this.destination = destination;
-    this.weight = weight;
-  }
-}
-
-function BellmanFord(graph, source) {
-  const distance = Array(graph.vertices).fill(Number.POSITIVE_INFINITY);
-  const predecessor = Array(graph.vertices).fill(null);
-
-  distance[source] = 0;
-
-  for (let i = 1; i < graph.vertices; ++i) {
-    for (let j = 0; j < graph.edges.length; ++j) {
-      const edge = graph.edges[j];
-      const u = edge.source;
-      const v = edge.destination;
-      const weight = edge.weight;
-
-      if (distance[u] !== Number.POSITIVE_INFINITY && distance[u] + weight < distance[v]) {
-        distance[v] = distance[u] + weight;
-        predecessor[v] = u;
-      }
+  
+  // Check if number is divisible by any number from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
-
-  for (let i = 0; i < graph.edges.length; ++i) {
-    const u = graph.edges[i].source;
-    const v = graph.edges[i].destination;
-    const weight = graph.edges[i].weight;
-
-    if (distance[u] !== Number.POSITIVE_INFINITY && distance[u] + weight < distance[v]) {
-      throw new Error('Graph contains a negative-weight cycle');
-    }
-  }
-
-  // Return the distance and predecessor arrays
-  return { distance, predecessor };
+  
+  return true;
 }
-const graph = new Graph(5);
-graph.addEdge(0, 1, 6);
-graph.addEdge(0, 3, 7);
-graph.addEdge(1, 2, 5);
-graph.addEdge(1, 3, 8);
-graph.addEdge(1, 4, -4);
-graph.addEdge(2, 1, -2);
-graph.addEdge(3, 2, -3);
-graph.addEdge(3, 4, 9);
-graph.addEdge(4, 0, 2);
-graph.addEdge(4, 2, 7);
-
-const source = 0;
-const { distance, predecessor } = BellmanFord(graph, source);
-
-console.log("Vertex\t Distance\tPredecessor");
-for (let i = 0; i < graph.vertices; i++) {
-    console.log(i + "\t" + distance[i] + "\t\t" + predecessor[i]);
-}
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(12)); // Output: false
