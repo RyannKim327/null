90042ed6c5e2e86471c5380c38ef05afd24aa12b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:42:28 +0800

    1-11-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,53 @@
-function factorial(n) {
-  // Base case: when n is 0 or 1, the factorial is 1
-  if (n === 0 || n === 1) {
-    return 1;
+class Graph {
+  constructor() {
+    this.adjList = {};
   }
-  
-  // Recursive case: factorial(n) = n * factorial(n-1)
-  return n * factorial(n - 1);
+
+  addVertex(vertex) {
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjList[vertex1] || !this.adjList[vertex2]) {
+      throw new Error('Vertex does not exist in the graph.');
+    }
+
+    this.adjList[vertex1].push(vertex2);
+    this.adjList[vertex2].push(vertex1);
+  }
+
+  dfs(startVertex) {
+    const visited = new Set();
+
+    const dfsHelper = (vertex) => {
+      console.log(vertex);
+      visited.add(vertex);
+
+      const neighbors = this.adjList[vertex];
+      for (let neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          dfsHelper(neighbor);
+        }
+      }
+    };
+
+    dfsHelper(startVertex);
+  }
 }
 
-// Example usage
-console.log(factorial(5)); // Output: 120
+// Example usage:
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('D', 'E');
+
+graph.dfs('A');
