433fa48adb2b05cce255e5d0bd489ca134ee8af0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:14:33 +0800

    1-11-2024 23:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,15 @@
-function calculateMean(numbers) {
-  let sum = 0;
-  const totalNumbers = numbers.length;
-
-  for (let i = 0; i < totalNumbers; i++) {
-    sum += numbers[i];
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    }
+    array[j + 1] = key;
   }
-
-  const mean = sum / totalNumbers;
-  return mean;
+  return array;
 }
+const unsortedArray = [7, 2, 4, 1, 5, 3];
+const sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray);
