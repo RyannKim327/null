ed288391a0310fdcd53f7565091f377efab26d5c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:33:45 +0800

    1-12-2024 0:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,85 +1,15 @@
-class ListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Reverse the string
+  const reversed = str.split('').reverse().join('');
+
+  // Compare the original string with the reversed string
+  return str === reversed;
 }
 
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-
-  while (current) {
-    const temp = current.next;
-    current.next = prev;
-    prev = current;
-    current = temp;
-  }
-
-  return prev;
-}
-
-function isLinkedListPalindrome(head) {
-  if (!head || !head.next) {
-    // An empty list or a list with a single node is considered a palindrome
-    return true;
-  }
-
-  let slow = head;
-  let fast = head;
-  let prevSlow = head;
-  let middleNode = null;
-  let isPalindrome = true;
-
-  while (fast && fast.next) {
-    fast = fast.next.next;
-    prevSlow = slow;
-    slow = slow.next;
-  }
-
-  // If the linked list has an odd number of nodes, fast will not be null
-  if (fast) {
-    middleNode = slow;
-    slow = slow.next;
-  }
-
-  prevSlow.next = null; // Break the list into two halves
-
-  // Reverse the second half of the linked list
-  let reversedHalf = reverseLinkedList(slow);
-
-  let temp1 = head;
-  let temp2 = reversedHalf;
-
-  // Compare the first half with the reversed second half
-  while (temp2) {
-    if (temp1.value !== temp2.value) {
-      isPalindrome = false;
-      break;
-    }
-
-    temp1 = temp1.next;
-    temp2 = temp2.next;
-  }
-
-  // Restore the original linked list by reversing the reversed half back
-  reversedHalf = reverseLinkedList(reversedHalf);
-
-  // Restore the original second half by re-attaching the middle node
-  if (middleNode) {
-    prevSlow.next = middleNode;
-    middleNode.next = reversedHalf;
-  } else {
-    prevSlow.next = reversedHalf;
-  }
-
-  return isPalindrome;
-}
-// Create a sample linked list: 1->2->3->2->1
-const head = new ListNode(1);
-head.next = new ListNode(2);
-head.next.next = new ListNode(3);
-head.next.next.next = new ListNode(2);
-head.next.next.next.next = new ListNode(1);
-
-console.log(isLinkedListPalindrome(head)); // Output: true
+// Example usage:
+console.log(isPalindrome('A man, a plan, a canal: Panama')); // true
+console.log(isPalindrome('race car')); // true
+console.log(isPalindrome('hello world')); // false
