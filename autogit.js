d1c8827e31553d29129334cb3a37d7b65c1ebdfb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:45:29 +0800

    1-11-2024 22:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,7 @@
-function getRandomNumber(min, max) {
-  // Generate a random decimal between 0 and 1
-  const randomDecimal = Math.random();
-  
-  // Scale the decimal to the desired range and round it
-  const randomNumber = Math.floor(randomDecimal * (max - min + 1)) + min;
-  
-  return randomNumber;
-}
+let str = "  Hello  World!  ";
 
-// Example usage
-const minNumber = 1;
-const maxNumber = 10;
-const randomNum = getRandomNumber(minNumber, maxNumber);
-console.log(randomNum);
+// Using regex to remove whitespace
+let newStr = str.replace(/\s/g, "");
+
+console.log(newStr);
+HelloWorld!
