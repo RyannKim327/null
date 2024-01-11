e0e8904eee154dce39b36ef1f3c62cdd75b7f87a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:12:04 +0800

    1-12-2024 0:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,36 @@
-function calculateMean(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  return sum / numbers.length;
-}
+function heapify(arr, length, i) {
+  let largest = i;
+  const left = 2 * i + 1;
+  const right = 2 * i + 2;
 
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log(mean);
+  if (left < length && arr[left] > arr[largest])
+    largest = left;
+
+  if (right < length && arr[right] > arr[largest])
+    largest = right;
+
+  if (largest !== i) {
+    [arr[i], arr[largest]] = [arr[largest], arr[i]];
+    heapify(arr, length, largest);
+  }
+}
+function heapSort(arr) {
+  const length = arr.length;
+
+  for (let i = Math.floor(length / 2) - 1; i >= 0; i--)
+    heapify(arr, length, i);
+
+  for (let i = length - 1; i >= 1; i--) {
+    [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
+  }
+
+  return arr;
+}
+const arr = [4, 10, 3, 5, 1];
+console.log("Original Array:", arr);
+
+const sortedArr = heapSort(arr);
+console.log("Sorted Array:", sortedArr);
+Original Array: [4, 10, 3, 5, 1]
+Sorted Array: [1, 3, 4, 5, 10]
