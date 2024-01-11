80162561c27907f066d8518179955e04ebd9a142
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:25:44 +0800

    1-11-2024 23:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,18 @@
-let str = "Hello world! This is a string with spaces.";
-let stringWithoutSpaces = str.split(" ").join("");
-console.log(stringWithoutSpaces);
-let str = "Hello world! This is a string with spaces.";
-let stringWithoutSpaces = str.replace(/\s/g, "");
-console.log(stringWithoutSpaces);
-let str = "Hello world! This is a string with spaces.";
-let stringWithoutSpaces = str.replaceAll(" ", "");
-console.log(stringWithoutSpaces);
+function reverseWords(str) {
+  // Split the string into an array of words
+  var wordsArray = str.split(" ");
+
+  // Reverse the order of words in the array
+  var reversedArray = wordsArray.reverse();
+
+  // Join the words back into a single string
+  var reversedString = reversedArray.join(" ");
+
+  return reversedString;
+}
+
+// Example usage
+var originalString = "Hello world!";
+
+var reversedString = reverseWords(originalString);
+console.log(reversedString); // Output: "world! Hello"
