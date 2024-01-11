2dea40ce8a81f6de1fc08aea8fae30906dab16da
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:25:22 +0800

    1-11-2024 23:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,9 @@
-function findLongestCommonPrefix(strings) {
-  if (strings.length === 0) return "";
-  if (strings.length === 1) return strings[0];
-
-  let commonPrefix = strings[0];
-
-  for (let i = 1; i < strings.length; i++) {
-    const currentString = strings[i];
-    let j = 0;
-
-    while (j < commonPrefix.length && j < currentString.length && commonPrefix[j] === currentString[j]) {
-      j++;
-    }
-
-    commonPrefix = commonPrefix.substring(0, j);
-
-    if (commonPrefix === "")
-      break;
-  }
-
-  return commonPrefix;
-}
-
-// Example usage:
-const strings = ["apple", "app", "appreciate"];
-const longestCommonPrefix = findLongestCommonPrefix(strings);
-console.log(longestCommonPrefix);  // Output: "app"
+let str = "Hello world! This is a string with spaces.";
+let stringWithoutSpaces = str.split(" ").join("");
+console.log(stringWithoutSpaces);
+let str = "Hello world! This is a string with spaces.";
+let stringWithoutSpaces = str.replace(/\s/g, "");
+console.log(stringWithoutSpaces);
+let str = "Hello world! This is a string with spaces.";
+let stringWithoutSpaces = str.replaceAll(" ", "");
+console.log(stringWithoutSpaces);
