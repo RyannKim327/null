2e1548977944522b46d31f37bc92686759e8662e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:44:38 +0800

    1-11-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,33 @@
-const readline = require('readline');
+function depthLimitedSearch(node, goal, depthLimit) {
+  if (node === goal) {
+    // Goal found
+    return true;
+  }
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+  if (depthLimit === 0) {
+    // Reached depth limit without finding the goal
+    return false;
+  }
 
-rl.question('Enter a value: ', (answer) => {
-  console.log('You entered:', answer);
+  // Recursively explore child nodes
+  for (let child of node.getChildren()) {
+    if (depthLimitedSearch(child, goal, depthLimit - 1)) {
+      // Goal found in child node
+      return true;
+    }
+  }
 
-  rl.close();
-});
+  // Goal not found
+  return false;
+}
+const rootNode = /* your starting node */;
+const goalNode = /* your goal node */;
+const depthLimit = /* your desired depth limit */;
+
+const found = depthLimitedSearch(rootNode, goalNode, depthLimit);
+
+if (found) {
+  console.log('Goal found!');
+} else {
+  console.log('Goal not found within the depth limit.');
+}
