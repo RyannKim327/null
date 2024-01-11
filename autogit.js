46e5a18a9239fb5034184d3c3c4dcb836932ae1b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:12:47 +0800

    1-11-2024 22:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,9 @@
-function longestCommonSubsequence(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  // Create a 2D array to store the lengths of LCS
-  const dp = Array(m + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0));
-
-  // Building the LCS matrix
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1.charAt(i - 1) === str2.charAt(j - 1)) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-      }
-    }
-  }
-
-  // Reconstructing the LCS from the matrix
-  let lcs = "";
-  let i = m;
-  let j = n;
-  while (i > 0 && j > 0) {
-    if (str1.charAt(i - 1) === str2.charAt(j - 1)) {
-      lcs = str1.charAt(i - 1) + lcs;
-      i--;
-      j--;
-    } else if (dp[i][j - 1] > dp[i - 1][j]) {
-      j--;
-    } else {
-      i--;
-    }
-  }
-
-  return lcs;
-}
-
-// Example usage
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-const result = longestCommonSubsequence(str1, str2);
-console.log(result); // Output: ADH
+let str = '  hello  world  ';
+let result = str.replace(/\s/g, ''); 
+console.log(result); // Output: 'helloworld'
+let str = '  hello  world  ';
+let result = str.trim(); 
+console.log(result); // Output: 'hello  world'
+let str = '  hello  world  ';
+let result = str.replace(/\s+/g, ' '); 
+console.log(result); // Output: ' hello world '
