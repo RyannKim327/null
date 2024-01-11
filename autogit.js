522daf6799fe8b14c11e5a76e09c184bbaada759
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:55:55 +0800

    1-11-2024 22:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,71 @@
-// Node class
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.edges = [];
+  }
+
+  addEdge(source, destination, weight) {
+    this.edges.push(new Edge(source, destination, weight));
   }
 }
 
-// LinkedList class
-class LinkedList {
-  constructor() {
-    this.head = null;
+class Edge {
+  constructor(source, destination, weight) {
+    this.source = source;
+    this.destination = destination;
+    this.weight = weight;
   }
+}
 
-  // Function to insert elements at the end of the list
-  insert(data) {
-    const newNode = new Node(data);
+function BellmanFord(graph, source) {
+  const distance = Array(graph.vertices).fill(Number.POSITIVE_INFINITY);
+  const predecessor = Array(graph.vertices).fill(null);
 
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let curr = this.head;
-      while (curr.next !== null) {
-        curr = curr.next;
+  distance[source] = 0;
+
+  for (let i = 1; i < graph.vertices; ++i) {
+    for (let j = 0; j < graph.edges.length; ++j) {
+      const edge = graph.edges[j];
+      const u = edge.source;
+      const v = edge.destination;
+      const weight = edge.weight;
+
+      if (distance[u] !== Number.POSITIVE_INFINITY && distance[u] + weight < distance[v]) {
+        distance[v] = distance[u] + weight;
+        predecessor[v] = u;
       }
-      curr.next = newNode;
     }
   }
 
-  // Function to find the middle element of the list
-  findMiddleElement() {
-    let slowPointer = this.head;
-    let fastPointer = this.head;
+  for (let i = 0; i < graph.edges.length; ++i) {
+    const u = graph.edges[i].source;
+    const v = graph.edges[i].destination;
+    const weight = graph.edges[i].weight;
 
-    while (fastPointer !== null && fastPointer.next !== null) {
-      slowPointer = slowPointer.next;
-      fastPointer = fastPointer.next.next;
+    if (distance[u] !== Number.POSITIVE_INFINITY && distance[u] + weight < distance[v]) {
+      throw new Error('Graph contains a negative-weight cycle');
     }
-
-    return slowPointer.data;
   }
+
+  // Return the distance and predecessor arrays
+  return { distance, predecessor };
 }
+const graph = new Graph(5);
+graph.addEdge(0, 1, 6);
+graph.addEdge(0, 3, 7);
+graph.addEdge(1, 2, 5);
+graph.addEdge(1, 3, 8);
+graph.addEdge(1, 4, -4);
+graph.addEdge(2, 1, -2);
+graph.addEdge(3, 2, -3);
+graph.addEdge(3, 4, 9);
+graph.addEdge(4, 0, 2);
+graph.addEdge(4, 2, 7);
 
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.insert(1);
-linkedList.insert(2);
-linkedList.insert(3);
-linkedList.insert(4);
-linkedList.insert(5);
+const source = 0;
+const { distance, predecessor } = BellmanFord(graph, source);
 
-// Find the middle element
-const middleElement = linkedList.findMiddleElement();
-console.log("Middle element:", middleElement);
+console.log("Vertex\t Distance\tPredecessor");
+for (let i = 0; i < graph.vertices; i++) {
+    console.log(i + "\t" + distance[i] + "\t\t" + predecessor[i]);
+}
