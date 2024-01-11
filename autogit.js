3b95d3ba59a8185ca6e41c82dd4bc61e093b2e6c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:54:47 +0800

    1-11-2024 23:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,46 @@
-let numbers = [10, 5, 20, 15, 30];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.neighbors = [];
+  }
+  
+  addNeighbor(neighbor) {
+    this.neighbors.push(neighbor);
+  }
+}
 
-let max = Math.max(...numbers);
+function breadthFirstSearch(startNode) {
+  const queue = [];
+  const visited = new Set();
+  
+  queue.push(startNode);
+  visited.add(startNode);
+  
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+    console.log(currentNode.value); // Process the node here
+    
+    for (const neighbor of currentNode.neighbors) {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+}
+// Create the graph
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
 
-console.log(max); // Output: 30
+nodeA.addNeighbor(nodeB);
+nodeA.addNeighbor(nodeC);
+nodeB.addNeighbor(nodeC);
+nodeC.addNeighbor(nodeA);
+nodeC.addNeighbor(nodeD);
+nodeD.addNeighbor(nodeD);
+
+// Perform breadth-first search
+breadthFirstSearch(nodeA);
