fa64c6c6a2c27e2c406095f5bdcac7fd4bb33fd5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:08:23 +0800

    1-11-2024 22:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,89 +1,28 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+function topologicalSort(adjacencyList) {
+  const result = [];
+  const visited = new Set();
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
+  function dfs(vertex) {
+    visited.add(vertex);
 
-  append(data) {
-    const newNode = new Node(data);
-    
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  prepend(data) {
-    const newNode = new Node(data);
-
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  delete(data) {
-    if (this.head === null) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-
-      if (this.head === null) {
-        this.tail = null;
-      }
-      
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next !== null) {
-      if (currentNode.next.data === data) {
-        currentNode.next = currentNode.next.next;
-
-        if (currentNode.next === null) {
-          this.tail = currentNode;
+    const neighbors = adjacencyList[vertex];
+    if (neighbors) {
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          dfs(neighbor);
         }
-
-        return;
       }
+    }
 
-      currentNode = currentNode.next;
+    result.push(vertex);
+  }
+
+  for (const vertex in adjacencyList) {
+    if (!visited.has(vertex)) {
+      dfs(vertex);
     }
   }
 
-  print() {
-    let currentNode = this.head;
-    const result = [];
-
-    while (currentNode !== null) {
-      result.push(currentNode.data);
-      currentNode = currentNode.next;
-    }
-
-    console.log(result);
-  }
+  result.reverse();
+  return result;
 }
-const list = new LinkedList();
-
-list.append(10);
-list.append(20);
-list.prepend(5);
-list.print(); // Output: [5, 10, 20]
-
-list.delete(10);
-list.print(); // Output: [5, 20]
