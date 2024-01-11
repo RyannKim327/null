6c2d5e1267171311820db69eb4691785ddb9e074
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:46:27 +0800

    1-11-2024 23:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,35 @@
-const str = 'Hello, world!';
-const substring = 'world';
+function LCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  const dp = [...Array(m + 1)].map(() => Array(n + 1).fill(0));
 
-if (str.includes(substring)) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
-}
-const str = 'Hello, world!';
-const substring = 'world';
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      else
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+    }
+  }
 
-if (str.indexOf(substring) !== -1) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
+  let lcs = '';
+  let i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i][j - 1] > dp[i - 1][j]) {
+      j--;
+    } else {
+      i--;
+    }
+  }
+
+  return lcs;
 }
+
+// Example usage:
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+console.log(LCS(str1, str2)); // Output: 'ADH'
