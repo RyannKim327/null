34d81f67c5c2fac60c1cc8744e6877d2fdbcd07a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:24:36 +0800

    1-11-2024 22:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,9 @@
-const array = [1, 5, 2, 9, 3];
-const max = Math.max(...array);
-console.log(max); // Output: 9
-const array = [1, 5, 2, 9, 3];
-let max = array[0]; // Assume first element as max
-
-for (let i = 1; i < array.length; i++) {
-  if (array[i] > max) {
-    max = array[i];
-  }
-}
-
-console.log(max); // Output: 9
+let str = "Hello World";
+let result = str.replace(/\s/g, "");
+console.log(result); // Output: "HelloWorld"
+let str = "Hello World";
+let result = str.split(" ").join("");
+console.log(result); // Output: "HelloWorld"
+let str = "Hello World";
+let result = str.replaceAll(" ", "");
+console.log(result); // Output: "HelloWorld"
