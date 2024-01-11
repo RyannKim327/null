ced8f0f94b9c572caece59b3072d12f96f50b04d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:11:49 +0800

    1-11-2024 23:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,111 +1,49 @@
-class BTreeNode {
-  constructor(order) {
-    this.order = order; // the maximum number of keys the node can hold
-    this.keys = [];     // sorted array of keys
-    this.values = [];   // array of corresponding values
-    this.children = []; // array of child nodes
-    this.isLeaf = true; // flag to indicate if the node is a leaf
+class Node {
+  constructor() {
+    this.edges = {}; // Incoming edges
+    this.children = {}; // Child nodes
   }
 }
-class BTree {
-  constructor(order) {
-    this.root = new BTreeNode(order);
-    this.order = order;
+class SuffixTree {
+  constructor() {
+    this.root = new Node(); // The root node of the tree
   }
 
-  insert(key, value) {
-    const { root, order } = this;
+  // Implement other methods here
+}
+insert(string) {
+  for (let i = 0; i < string.length; i++) {
+    let currentNode = this.root;
+    const suffix = string.substring(i);
+    for (let j = 0; j < suffix.length; j++) {
+      const char = suffix.charAt(j);
 
-    if (root.keys.length === 2 * order - 1) {
-      const newRoot = new BTreeNode(order);
-      newRoot.children.push(root);
-      this.root = newRoot;
-      this.splitChild(newRoot, 0);
-      this.insertNonFull(newRoot, key, value);
-    } else {
-      this.insertNonFull(root, key, value);
-    }
-  }
-
-  insertNonFull(node, key, value) {
-    let i = node.keys.length - 1;
-
-    if (node.isLeaf) {
-      while (i >= 0 && key < node.keys[i]) {
-        node.keys[i + 1] = node.keys[i];
-        node.values[i + 1] = node.values[i];
-        i--;
+      if (!currentNode.edges[char]) {
+        // If edge doesn't exist, create a new node and add the edge
+        const newNode = new Node();
+        currentNode.edges[char] = newNode;
+        currentNode.children[char] = newNode;
       }
 
-      node.keys[i + 1] = key;
-      node.values[i + 1] = value;
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-
-      i++;
-
-      if (node.children[i].keys.length === 2 * this.order - 1) {
-        this.splitChild(node, i);
-
-        if (key > node.keys[i]) {
-          i++;
-        }
-      }
-
-      this.insertNonFull(node.children[i], key, value);
-    }
-  }
-
-  splitChild(parent, index) {
-    const { order } = this;
-    const child = parent.children[index];
-    const newChild = new BTreeNode(order);
-    parent.keys.splice(index, 0, child.keys[order - 1]);
-    parent.values.splice(index, 0, child.values[order - 1]);
-    parent.children.splice(index + 1, 0, newChild);
-    newChild.isLeaf = child.isLeaf;
-
-    for (let j = 0; j < order - 1; j++) {
-      newChild.keys[j] = child.keys[j + order];
-      newChild.values[j] = child.values[j + order];
-    }
-
-    if (!child.isLeaf) {
-      for (let j = 0; j < order; j++) {
-        newChild.children[j] = child.children[j + order];
-      }
-    }
-
-    child.keys.length = order - 1;
-    child.values.length = order - 1;
-    child.children.length = child.isLeaf ? 0 : order;
-  }
-
-  search(key) {
-    return this.searchNode(this.root, key);
-  }
-
-  searchNode(node, key) {
-    let i = 0;
-
-    while (i < node.keys.length && key > node.keys[i]) {
-      i++;
-    }
-
-    if (node.keys[i] === key) {
-      return node.values[i];
-    } else if (node.isLeaf) {
-      return null;
-    } else {
-      return this.searchNode(node.children[i], key);
+      currentNode = currentNode.edges[char];
     }
   }
 }
-const bTree = new BTree(3); // Order 3 B-tree
-bTree.insert(5, "Five");
-bTree.insert(3, "Three");
-bTree.insert(7, "Seven");
-console.log(bTree.search(5)); // Output: "Five"
-console.log(bTree.search(10)); // Output: null
+search(pattern) {
+  let currentNode = this.root;
+  for (let i = 0; i < pattern.length; i++) {
+    const char = pattern.charAt(i);
+    if (currentNode.edges[char]) {
+      currentNode = currentNode.edges[char];
+    } else {
+      return false; // Pattern not found
+    }
+  }
+  return true; // Pattern found
+}
+const suffixTree = new SuffixTree();
+suffixTree.insert("banana");
+suffixTree.insert("apple");
+console.log(suffixTree.search("banana")); // Output: true
+console.log(suffixTree.search("apple")); // Output: true
+console.log(suffixTree.search("orange")); // Output: false
