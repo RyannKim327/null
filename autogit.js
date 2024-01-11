edb5583e5324c03b894aac465dccb9fb23da8c6e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:01:48 +0800

    1-12-2024 0:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,18 @@
-const getMax = (arr) => Math.max(...arr);
-const maxNumber = getMax(arr);
-const maxDigits = Math.floor(Math.log10(maxNumber) + 1);
-const getDigit = (num, position) => Math.floor(Math.abs(num) / Math.pow(10, position)) % 10;
-const buckets = Array.from({ length: 10 }, () => []);
-for (let i = 0; i < maxDigits; i++) {
-  for (let j = 0; j < arr.length; j++) {
-    const digit = getDigit(arr[j], i);
-    buckets[digit].push(arr[j]);
-  }
-  arr = [].concat(...buckets);
-  buckets.forEach((bucket) => (bucket.length = 0));
+function reverseWords(str) {
+  // Split the string into an array of words
+  var wordsArray = str.split(" ");
+
+  // Reverse the order of words in the array
+  var reversedArray = wordsArray.reverse();
+
+  // Join the reversed array elements into a string
+  var reversedString = reversedArray.join(" ");
+
+  return reversedString;
 }
-return arr;
-function radixSort(arr) {
-  const getMax = (arr) => Math.max(...arr);
-  const maxNumber = getMax(arr);
-  const maxDigits = Math.floor(Math.log10(maxNumber) + 1);
-  
-  const getDigit = (num, position) => Math.floor(Math.abs(num) / Math.pow(10, position)) % 10;
 
-  const buckets = Array.from({ length: 10 }, () => []);
+// Example usage
+var originalString = "Hello World, how are you?";
+var reversedString = reverseWords(originalString);
 
-  for (let i = 0; i < maxDigits; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      const digit = getDigit(arr[j], i);
-      buckets[digit].push(arr[j]);
-    }
-    arr = [].concat(...buckets);
-    buckets.forEach((bucket) => (bucket.length = 0));
-  }
-
-  return arr;
-}
-const myArray = [23, 345, 5467, 12, 2345, 9852];
-console.log(radixSort(myArray)); // Output: [12, 23, 345, 2345, 5467, 9852]
+console.log(reversedString);  // Outputs: "you? are how World, Hello"
