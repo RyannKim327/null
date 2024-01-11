697c5274dbf7495e3fab2509acd050d208eb4277
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:48:08 +0800

    1-11-2024 22:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,68 @@
-// Sample array of integers
-let numbers = [5, 2, 9, 1, 7];
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  // ...
+}
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  let maxDigits = Math.ceil(Math.log10(maxNum + 1));
+  // ...
+}
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  let maxDigits = Math.ceil(Math.log10(maxNum + 1));
 
-// Sorting the array in ascending order
-numbers.sort(function(a, b) {
-  return a - b;
-});
+  let digitBuckets = Array.from({ length: 10 }, () => []);
+  // ...
+}
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  let maxDigits = Math.ceil(Math.log10(maxNum + 1));
 
-console.log(numbers); // Output: [1, 2, 5, 7, 9]
+  let digitBuckets = Array.from({ length: 10 }, () => []);
+
+  for (let i = 0; i < maxDigits; i++) {
+    // ...
+  }
+}
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  let maxDigits = Math.ceil(Math.log10(maxNum + 1));
+
+  let digitBuckets = Array.from({ length: 10 }, () => []);
+
+  for (let i = 0; i < maxDigits; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let digit = Math.floor((arr[j] / Math.pow(10, i)) % 10);
+      digitBuckets[digit].push(arr[j]);
+    }
+
+    // ...
+  }
+}
+function radixSort(arr) {
+  let maxNum = Math.max(...arr);
+  let maxDigits = Math.ceil(Math.log10(maxNum + 1));
+
+  let digitBuckets = Array.from({ length: 10 }, () => []);
+
+  for (let i = 0; i < maxDigits; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      let digit = Math.floor((arr[j] / Math.pow(10, i)) % 10);
+      digitBuckets[digit].push(arr[j]);
+    }
+
+    arr = [].concat(...digitBuckets);
+    digitBuckets = Array.from({ length: 10 }, () => []);
+  }
+
+  return arr;
+}
+function radixSort(arr) {
+  // Implementation here...
+
+  return arr;
+}
+let unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
+let sortedArray = radixSort(unsortedArray);
+
+console.log(sortedArray); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
