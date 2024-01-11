ba6a3bdf806b1a45a8bf6b74c3b1289dbfd20501
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:45:23 +0800

    1-11-2024 23:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,88 @@
-function isPrime(num) {
-  // 1 and non-integer numbers are not prime
-  if (num <= 1 || !Number.isInteger(num)) {
-    return false;
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
   }
 
-  // Check if it is divisible by any number from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
+  // Add a new node at the end of the linked list
+  append(value) {
+    const newNode = new ListNode(value);
+    
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
 
-  return true;
-}
+  // Insert a new node at the beginning of the linked list
+  prepend(value) {
+    const newNode = new ListNode(value);
+    
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
 
-// Usage
-console.log(isPrime(17)); // true
-console.log(isPrime(15)); // false
+  // Delete the first occurrence of a node with the given value
+  delete(value) {
+    if (this.head === null) {
+      return;
+    }
+    
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      if (this.head === null) {
+        this.tail = null;
+      }
+      return;
+    }
+    
+    let currentNode = this.head;
+    
+    while (currentNode.next !== null) {
+      if (currentNode.next.value === value) {
+        if (this.tail === currentNode.next) {
+          this.tail = currentNode;
+        }
+        currentNode.next = currentNode.next.next;
+        return;
+      }
+      
+      currentNode = currentNode.next;
+    }
+  }
+
+  // Print the values of the linked list
+  print() {
+    let currentNode = this.head;
+    const values = [];
+    
+    while (currentNode !== null) {
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    
+    console.log(values.join(' -> '));
+  }
+}
+const linkedList = new LinkedList();
+linkedList.append(10); // Adds 10 to the end of the list
+linkedList.append(20); // Adds 20 to the end of the list
+linkedList.prepend(5);  // Adds 5 to the beginning of the list
+linkedList.print();    // Outputs: 5 -> 10 -> 20
+linkedList.delete(10); // Deletes the node with value 10
+linkedList.print();    // Outputs: 5 -> 20
