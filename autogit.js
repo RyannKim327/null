4e475e76f471a189d8459425f5076d42735563a1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:21:56 +0800

    1-12-2024 0:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,22 @@
-let str = "Hello World!";
-let lowerCaseStr = str.toLowerCase();
+function findNthNodeFromEnd(head, n) {
+  let p1 = head;
+  let p2 = head;
 
-console.log(lowerCaseStr); // Output: "hello world!"
+  // Move p2 n nodes ahead
+  for (let i = 0; i < n; i++) {
+    if (p2 === null) {
+      // Length of the linked list is less than n
+      return null;
+    }
+    p2 = p2.next;
+  }
+
+  // Move both pointers together until p2 reaches the end
+  while (p2 !== null) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  // p1 will be pointing to the nth node from the end of the linked list
+  return p1.value;
+}
