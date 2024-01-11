3379d330e1da3bd8107dc8698bbab191e26e175c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:17:04 +0800

    1-12-2024 0:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,12 @@
-class TarjanSCC {
-  constructor(graph) {
-    this.graph = graph;
-    this.component = [];
-    this.low = [];
-    this.ids = [];
-    this.onStack = [];
-    this.id = 0;
+let arr = [5, 2, 8, 1, 6];
 
-    this.stronglyConnectedComponents();
-  }
+// Sorting the array in ascending order
+arr.sort((a, b) => a - b);
 
-  stronglyConnectedComponents() {
-    for (let node = 0; node < this.graph.length; node++) {
-      if (!this.ids[node]) {
-        this.dfs(node);
-      }
-    }
-  }
+console.log(arr); // Output: [1, 2, 5, 6, 8]
+let arr = [5, 2, 8, 1, 6];
 
-  dfs(node) {
-    this.ids[node] = this.low[node] = ++this.id;
-    this.onStack.push(node);
+// Sorting the array in descending order
+arr.sort((a, b) => b - a);
 
-    for (let neighbor of this.graph[node]) {
-      if (!this.ids[neighbor]) {
-        this.dfs(neighbor);
-        this.low[node] = Math.min(this.low[node], this.low[neighbor]);
-      } else if (this.onStack.includes(neighbor)) {
-        this.low[node] = Math.min(this.low[node], this.ids[neighbor]);
-      }
-    }
-
-    if (this.low[node] === this.ids[node]) {
-      const stronglyConnectedComponent = [];
-      let curNode;
-      do {
-        curNode = this.onStack.pop();
-        stronglyConnectedComponent.push(curNode);
-      } while (curNode !== node);
-      this.component.push(stronglyConnectedComponent);
-    }
-  }
-}
-const graph = [[1], [2, 4], [3], [0, 5], [2], [0, 6], [0, 2]];
-const tarjan = new TarjanSCC(graph);
-console.log(tarjan.component);
+console.log(arr); // Output: [8, 6, 5, 2, 1]
