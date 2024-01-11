021de6d6634f069ba47131a5b1f719b7ac1baa8b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:57:38 +0800

    1-11-2024 23:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,35 @@
-let str = "  Hello,    World!  ";
-let trimmedStr = str.replace(/\s/g, "");
+function topologicalSort(graph) {
+  const visited = new Set();
+  const stack = [];
 
-console.log(trimmedStr); // Output: "Hello,World!"
-let str = "  Hello,    World!  ";
-let trimmedStr = str.trim();
+  function dfs(vertex) {
+    visited.add(vertex);
 
-console.log(trimmedStr); // Output: "Hello,    World!"
+    for (const adjacentVertex of graph[vertex]) {
+      if (!visited.has(adjacentVertex)) {
+        dfs(adjacentVertex);
+      }
+    }
+
+    stack.push(vertex);
+  }
+
+  for (const vertex in graph) {
+    if (!visited.has(vertex)) {
+      dfs(vertex);
+    }
+  }
+
+  return stack.reverse();
+}
+const graph = {
+  A: ['B', 'C'],
+  B: ['D'],
+  C: ['D', 'E'],
+  D: [],
+  E: ['F'],
+  F: [],
+};
+
+const sortedVertices = topologicalSort(graph);
+console.log(sortedVertices); // Output: ['A', 'C', 'E', 'F', 'B', 'D']
