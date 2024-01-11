229317ee2a84d802c6d38f19234a8fb9f5faa16b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:46:53 +0800

    1-11-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,5 @@
-function radixSort(arr) {
-  const max = Math.max(...arr);
-  const maxDigits = Math.floor(Math.log10(max) + 1);
-
-  for (let i = 0; i < maxDigits; i++) {
-    const buckets = Array.from({ length: 10 }, () => []);
-
-    for (let j = 0; j < arr.length; j++) {
-      const digit = Math.floor((arr[j] / Math.pow(10, i)) % 10);
-      buckets[digit].push(arr[j]);
-    }
-
-    arr = buckets.flat();
-  }
-
-  return arr;
-}
-const unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
-const sortedArray = radixSort(unsortedArray);
-
-console.log(sortedArray); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+// Make a GET request to an API and log the response
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log('Error:', error));
