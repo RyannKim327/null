aaac349a2fc11e7ec07e99416e5fdd0515d4689a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:15:34 +0800

    1-11-2024 23:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,27 @@
-function longestCommonSubstring(str1, str2) {
-  // Create a matrix to store the lengths of common substrings
-  const matrix = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+function selectionSort(array) {
+  const length = array.length;
   
-  let longestLength = 0; // length of longest common substring
-  let longestEndIndex = 0; // ending index of longest common substring
-  
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-        if (matrix[i][j] > longestLength) {
-          longestLength = matrix[i][j];
-          longestEndIndex = i - 1;
-        }
-      } else {
-        matrix[i][j] = 0;
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
       }
     }
+
+    if (minIndex !== i) {
+      // Swap values
+      const temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
   }
-  
-  // Extract the longest common substring
-  const longestSubstring = str1.substr(longestEndIndex - longestLength + 1, longestLength);
-  
-  return longestSubstring;
+
+  return array;
 }
 
 // Example usage:
-const str1 = "abcdxyz";
-const str2 = "xyzabcd";
-console.log(longestCommonSubstring(str1, str2)); // Output: "abcd"
+const unsortedArray = [5, 2, 8, 12, 1, 6];
+const sortedArray = selectionSort(unsortedArray);
+console.log(sortedArray);
