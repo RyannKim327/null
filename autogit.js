89fbacd37acf159d9e6cbfb6468f15fdebbe5348
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:18:05 +0800

    1-11-2024 22:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,37 @@
-// Prompt the user for input
-let userInput = prompt("Enter your name:");
+class Node {
+  constructor() {
+    this.children = {};
+    // Add any other required properties
+  }
+}
+class SuffixTree {
+  constructor(text) {
+    this.root = new Node();
+    this.text = text;
+    this.buildSuffixTree();
+  }
+  
+  buildSuffixTree() {
+    // Implement the logic to build the suffix tree here
+  }
+  
+  // Implement other required methods here
+}
+buildSuffixTree() {
+  const len = this.text.length;
+  for (let i = 0; i < len; i++) {
+    this.addSuffix(this.text.substring(i));
+  }
+}
 
-// Display the user input on the console
-console.log("Hello, " + userInput + "!");
+addSuffix(suffix) {
+  let currentNode = this.root;
+  for (let i = 0; i < suffix.length; i++) {
+    const currentChar = suffix[i];
+    if (!currentNode.children[currentChar]) {
+      currentNode.children[currentChar] = new Node();
+    }
+    currentNode = currentNode.children[currentChar];
+  }
+  // Store any metadata or flags at the last character node of the suffix
+}
