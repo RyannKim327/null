1f91358624a1876465804cf4473de6f58f19b9ed
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:29:40 +0800

    1-11-2024 22:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,9 @@
-class HashTable {
-  constructor() {
-    this.table = new Map();
-  }
+const CronJob = require('cron').CronJob;
 
-  // Generate a hash code for a given key
-  hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
-    }
-    return hash;
-  }
+// Create a job that runs every minute
+const job = new CronJob('* * * * *', function() {
+  console.log('This job runs every minute!');
+}, null, true, 'America/New_York'); // Change timezone as per your preference
 
-  // Insert a key-value pair into the hash table
-  insert(key, value) {
-    const hashedKey = this.hash(key);
-    this.table.set(hashedKey, value);
-  }
-
-  // Retrieve a value associated with a given key from the hash table
-  get(key) {
-    const hashedKey = this.hash(key);
-    return this.table.get(hashedKey);
-  }
-
-  // Remove a key-value pair from the hash table
-  remove(key) {
-    const hashedKey = this.hash(key);
-    this.table.delete(hashedKey);
-  }
-
-  // Check if the hash table contains a given key
-  contains(key) {
-    const hashedKey = this.hash(key);
-    return this.table.has(hashedKey);
-  }
-}
-
-// Usage example
-const myHashTable = new HashTable();
-myHashTable.insert('name', 'John');
-myHashTable.insert('age', 30);
-
-console.log(myHashTable.get('name'));  // Output: "John"
-console.log(myHashTable.contains('age'));  // Output: true
-
-myHashTable.remove('age');
-console.log(myHashTable.contains('age'));  // Output: false
+// Start the job
+job.start();
