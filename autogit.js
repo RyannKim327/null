7bed282dde7b87cd2a2cf7596018cc70fa3e44eb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:25:05 +0800

    1-12-2024 0:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,15 @@
-function buildMaxHeap(arr) {
-    let length = arr.length;
-    let start = Math.floor(length / 2) - 1;
-
-    for (let i = start; i >= 0; i--) {
-        heapify(arr, length, i);
-    }
+let str = "Hello World";
+let reversedStr = str.split('').reverse().join('');
+console.log(reversedStr); // Output: dlroW olleH
+let str = "Hello World";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-function heapify(arr, n, i) {
-    let largest = i;
-    let left = 2 * i + 1;
-    let right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest !== i) {
-        let temp = arr[i];
-        arr[i] = arr[largest];
-        arr[largest] = temp;
-
-        heapify(arr, n, largest);
-    }
-}
-function heapSort(arr) {
-    let length = arr.length;
-
-    buildMaxHeap(arr);
-
-    for (let i = length - 1; i > 0; i--) {
-        let temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-
-        heapify(arr, i, 0);
-    }
-    return arr;
-}
-let array = [12, 11, 13, 5, 6, 7];
-console.log(heapSort(array)); // Output: [5, 6, 7, 11, 12, 13]
+console.log(reversedStr); // Output: dlroW olleH
+let str = "Hello World";
+let reversedStr = [...str].reverse().join('');
+console.log(reversedStr); // Output: dlroW olleH
+let str = "Hello World";
+str = str.split('').reverse().join('');
+console.log(str); // Output: dlroW olleH
