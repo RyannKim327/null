5b68b892132584073d6935014ba50f6fb6d440e8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 00:25:23 +0800

    1-12-2024 0:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,17 @@
-let str = "Hello World";
-let reversedStr = str.split('').reverse().join('');
-console.log(reversedStr); // Output: dlroW olleH
-let str = "Hello World";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
-}
-console.log(reversedStr); // Output: dlroW olleH
-let str = "Hello World";
-let reversedStr = [...str].reverse().join('');
-console.log(reversedStr); // Output: dlroW olleH
-let str = "Hello World";
-str = str.split('').reverse().join('');
-console.log(str); // Output: dlroW olleH
+const cron = require('node-cron');
+
+// Define the cron schedule
+const schedule = '0 * * * *'; // Runs every hour at the 0th minute
+
+// Define the cron job function
+const cronJob = cron.schedule(schedule, () => {
+  // Code to be executed on schedule
+  console.log('Running cron job...');
+  // Add your desired code here
+}, {
+  scheduled: true, // Start the cron job immediately
+  timezone: 'America/New_York' // Set the timezone as per your requirements
+});
+
+// Start the cron job
+cronJob.start();
