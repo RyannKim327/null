d62d76c1a2e55eeac8d3cca4a9c457f0976612b5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:10:11 +0800

    1-11-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,11 @@
-function dijkstra(graph, source) {
-  const dist = {};
-  const visited = {};
+function getLinkedListLength(head) {
+  let length = 0;
+  let currentNode = head;
 
-  // Initialize distances to all nodes as infinity except source node
-  for (let vertex in graph) {
-    dist[vertex] = Infinity;
-  }
-  dist[source] = 0;
-
-  while (true) {
-
-    let current = null;
-
-    // Select the node with the smallest distance
-    for (let vertex in graph) {
-      if (!visited[vertex] && (current === null || dist[vertex] < dist[current])) {
-        current = vertex;
-      }
-    }
-
-    if (current === null) {
-      break;
-    }
-
-    visited[current] = true;
-
-    // Update distances to neighboring nodes
-    for (let neighbor in graph[current]) {
-      let distance = graph[current][neighbor];
-      let totalDistance = dist[current] + distance;
-      if (totalDistance < dist[neighbor]) {
-        dist[neighbor] = totalDistance;
-      }
-    }
+  while (currentNode !== null) {
+    length++;
+    currentNode = currentNode.next;
   }
 
-  return dist;
+  return length;
 }
