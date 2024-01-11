2e53d4f4a5e60ffee1012438dbbc1a6b7b62d774
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 22:46:19 +0800

    1-11-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,21 @@
-let str = "  Hello  World!  ";
+function radixSort(arr) {
+  const max = Math.max(...arr);
+  const maxDigits = Math.floor(Math.log10(max) + 1);
 
-// Using regex to remove whitespace
-let newStr = str.replace(/\s/g, "");
+  for (let i = 0; i < maxDigits; i++) {
+    const buckets = Array.from({ length: 10 }, () => []);
 
-console.log(newStr);
-HelloWorld!
+    for (let j = 0; j < arr.length; j++) {
+      const digit = Math.floor((arr[j] / Math.pow(10, i)) % 10);
+      buckets[digit].push(arr[j]);
+    }
+
+    arr = buckets.flat();
+  }
+
+  return arr;
+}
+const unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
+const sortedArray = radixSort(unsortedArray);
+
+console.log(sortedArray); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
