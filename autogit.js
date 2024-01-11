bfa220587b9c0e2cbe85a09361a7e00bcd5e58f4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-11 23:00:32 +0800

    1-11-2024 23:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,7 @@
-function countingSort(arr) {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-
-  arr.forEach((num) => {
-    count[num - min]++;
-  });
-
-  for (let i = 1; i < count.length; i++) {
-    count[i] += count[i - 1];
-  }
-
-  const output = Array(arr.length);
-
-  arr.forEach((num) => {
-    output[count[num - min] - 1] = num;
-    count[num - min]--;
-  });
-
-  return output;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
 // Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-const sortedArr = countingSort(arr);
-console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
+console.log(decimalToBinary(10));  // Output: 1010
+console.log(decimalToBinary(25));  // Output: 11001
