806eeb9603ba63ed38773be89c92b21116024aaf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:20:29 +0800

    1-12-2024 9:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,50 @@
-function getStringLength(str) {
-  let count = 0;
-  for (let i = 0; str[i] !== undefined; i++) {
-    count++;
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.adjList = new Map();
   }
-  return count;
+
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+  }
+
+  bfs(startingNode) {
+    const visited = new Set();
+    const queue = [];
+
+    visited.add(startingNode);
+    queue.push(startingNode);
+
+    while (queue.length > 0) {
+      const currentNode = queue.shift();
+      console.log(currentNode);
+
+      const neighbors = this.adjList.get(currentNode);
+
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
+      }
+    }
+  }
 }
 
-// Example usage:
-const myString = "Hello, World!";
-const length = getStringLength(myString);
-console.log(length); // Output: 13
+// example usage
+const graph = new Graph(6);
+const vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+for (const vertex of vertices) {
+  graph.adjList.set(vertex, []);
+}
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+graph.bfs('A');
