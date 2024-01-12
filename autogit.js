efafe25c4180c53732dc984a73a2cec2dadf836a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:21:54 +0800

    1-12-2024 10:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,17 @@
-function getRandomNumber(min, max) {
-  // Adding 1 to the range to include the maximum value
-  const range = max - min + 1;
-
-  // Generate a random number within the range
-  const randomNumber = Math.floor(Math.random() * range) + min;
-
-  return randomNumber;
+function bubbleSort(array) {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        swapped = true;
+      }
+    }
+  }
+  return array;
 }
 
-// Usage example:
-const minRange = 1;
-const maxRange = 10;
-
-const randomNum = getRandomNumber(minRange, maxRange);
-console.log(randomNum);
+// Example usage:
+const arr = [5, 3, 8, 1, 2];
+console.log(bubbleSort(arr)); // Output: [1, 2, 3, 5, 8]
