44bc15d674235ee50b1ec2daf24ad509d96d46b7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:04:53 +0800

    1-12-2024 9:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,69 +1,122 @@
-function bidirectionalSearch(start, end, getNeighbors) {
-  const forwardFrontier = new Set([start]);
-  const backwardFrontier = new Set([end]);
-  
-  const forwardParents = {};
-  const backwardParents = {};
-
-  forwardParents[start] = null;
-  backwardParents[end] = null;
-
-  while (forwardFrontier.size > 0 && backwardFrontier.size > 0) {
-    // Check if any node in the forward frontier exists in the backward frontier
-    for (const node of forwardFrontier) {
-      if (backwardFrontier.has(node)) {
-        // Path found, reconstruct and return the complete path
-        return reconstructPath(node, forwardParents, backwardParents);
-      }
-    }
-    
-    // Expand nodes in the forward frontier
-    const newForwardFrontier = new Set();
-    for (const node of forwardFrontier) {
-      const neighbors = getNeighbors(node);
-      for (const neighbor of neighbors) {
-        if (!forwardParents.hasOwnProperty(neighbor)) {
-          forwardParents[neighbor] = node;
-          newForwardFrontier.add(neighbor);
-        }
-      }
-    }
-    forwardFrontier.clear();
-    forwardFrontier.addAll(newForwardFrontier);
-    
-    // Expand nodes in the backward frontier
-    const newBackwardFrontier = new Set();
-    for (const node of backwardFrontier) {
-      const neighbors = getNeighbors(node);
-      for (const neighbor of neighbors) {
-        if (!backwardParents.hasOwnProperty(neighbor)) {
-          backwardParents[neighbor] = node;
-          newBackwardFrontier.add(neighbor);
-        }
-      }
-    }
-    backwardFrontier.clear();
-    backwardFrontier.addAll(newBackwardFrontier);
+class Node {
+  constructor(value, color) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.color = color;
   }
-
-  // No path found
-  return null;
 }
+class RedBlackTree {
+  constructor() {
+    this.root = null;
+  }
+  // Implement the operations here...
+}
+RedBlackTree.prototype.leftRotate = function(node) {
+  const rightChild = node.right;
+  node.right = rightChild.left;
 
-function reconstructPath(node, forwardParents, backwardParents) {
-  const forwardPath = [];
-  let current = node;
-  while (current) {
-    forwardPath.unshift(current);
-    current = forwardParents[current];
+  if (rightChild.left !== null)
+    rightChild.left.parent = node;
+
+  rightChild.parent = node.parent;
+
+  if (node.parent === null)
+    this.root = rightChild;
+  else if (node === node.parent.left)
+    node.parent.left = rightChild;
+  else
+    node.parent.right = rightChild;
+
+  rightChild.left = node;
+  node.parent = rightChild;
+}
+RedBlackTree.prototype.rightRotate = function(node) {
+  const leftChild = node.left;
+  node.left = leftChild.right;
+
+  if (leftChild.right !== null)
+    leftChild.right.parent = node;
+
+  leftChild.parent = node.parent;
+
+  if (node.parent === null)
+    this.root = leftChild;
+  else if (node === node.parent.left)
+    node.parent.left = leftChild;
+  else
+    node.parent.right = leftChild;
+
+  leftChild.right = node;
+  node.parent = leftChild;
+}
+RedBlackTree.prototype.insert = function(value) {
+  const newNode = new Node(value, "red");
+
+  let current = this.root;
+  let parent = null;
+
+  while (current !== null) {
+    parent = current;
+
+    if (value < current.value)
+      current = current.left;
+    else
+      current = current.right;
   }
-  
-  const backwardPath = [];
-  current = backwardParents[node];
-  while (current) {
-    backwardPath.push(current);
-    current = backwardParents[current];
+
+  newNode.parent = parent;
+
+  if (parent === null)
+    this.root = newNode;
+  else if (value < parent.value)
+    parent.left = newNode;
+  else
+    parent.right = newNode;
+
+  this.fixInsert(newNode);
+}
+RedBlackTree.prototype.fixInsert = function(node) {
+  while (node !== this.root && node.parent.color === "red") {
+    if (node.parent === node.parent.parent.left) {
+      const uncle = node.parent.parent.right;
+
+      if (uncle !== null && uncle.color === "red") {
+        node.parent.color = "black";
+        uncle.color = "black";
+        node.parent.parent.color = "red";
+        node = node.parent.parent;
+      } else {
+        if (node === node.parent.right) {
+          node = node.parent;
+          this.leftRotate(node);
+        }
+
+        node.parent.color = "black";
+        node.parent.parent.color = "red";
+        this.rightRotate(node.parent.parent);
+      }
+    } else {
+      const uncle = node.parent.parent.left;
+
+      if (uncle !== null && uncle.color === "red") {
+        node.parent.color = "black";
+        uncle.color = "black";
+        node.parent.parent.color = "red";
+        node = node.parent.parent;
+      } else {
+        if (node === node.parent.left) {
+          node = node.parent;
+          this.rightRotate(node);
+        }
+
+        node.parent.color = "black";
+        node.parent.parent.color = "red";
+        this.leftRotate(node.parent.parent);
+      }
+    }
   }
-  
-  return forwardPath.concat(backwardPath);
+
+  this.root.color = "black";
 }
