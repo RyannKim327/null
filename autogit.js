405ff5bf9bbb34956f05774f67910a6d5121ba0a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:27:37 +0800

    1-12-2024 9:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,23 @@
-function getStringLength(str) {
-  let length = 0;
-  while (str[length] !== undefined) {
-    length++;
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return length;
-}
 
-const myString = "Hello, World!";
-const length = getStringLength(myString);
-console.log(length); // Output: 13
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quicksort(left), pivot, ...quicksort(right)];
+}
+const arr = [4, 2, 7, 1, 3];
+const sortedArr = quicksort(arr);
+
+console.log(sortedArr);  // Output: [1, 2, 3, 4, 7]
