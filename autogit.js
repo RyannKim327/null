e45f0dbc2a1953a83be8cb470a31d64139211551
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:38:50 +0800

    1-12-2024 9:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,23 @@
-var array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
+function binarySearch(arr, target, start = 0) {
+  let low = start;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (target === arr[mid]) {
+      return mid;
+    } else if (target < arr[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  
+  return -1;
+}
+const myArray = [2, 4, 7, 10, 14, 16, 19, 20, 26, 29];
+const targetElement = 16;
+
+const resultIndex = binarySearch(myArray, targetElement);
+console.log(resultIndex); // Output: 5
