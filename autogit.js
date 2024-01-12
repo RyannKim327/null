c97493ffe2459f26246eeec398f451ce2c76380d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:30:45 +0800

    1-12-2024 10:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,5 @@
-function getMax(arr) {
-  let max = 0;
-  for (let num of arr) {
-    if (num > max) {
-      max = num;
-    }
-  }
-  return max;
-}
-function radixSort(arr) {
-  const max = getMax(arr);
-  let exp = 1;
-  
-  while (max / exp > 0) {
-    countingSort(arr, exp);
-    exp *= 10;
-  }
-  
-  return arr;
-}
-function countingSort(arr, exp) {
-  const n = arr.length;
-  const output = new Array(n).fill(0);
-  const count = new Array(10).fill(0);
-  
-  for (let i = 0; i < n; i++) {
-    const digit = Math.floor(arr[i] / exp) % 10;
-    count[digit]++;
-  }
-  
-  for (let i = 1; i < 10; i++) {
-    count[i] += count[i - 1];
-  }
-  
-  for (let i = n - 1; i >= 0; i--) {
-    const digit = Math.floor(arr[i] / exp) % 10;
-    output[count[digit] - 1] = arr[i];
-    count[digit]--;
-  }
-  
-  for (let i = 0; i < n; i++) {
-    arr[i] = output[i];
-  }
-}
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr));
+const array = [3, 8, 1, 6, 2, 5];
+
+const max = Math.max(...array);
+
+console.log(max); // Output: 8
