c77199877a0ffae6eca47c68567990fedb91d325
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:15:36 +0800

    1-12-2024 9:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,72 @@
-let str = "Hello, World!";
-let reversedString = str.split('').reverse().join('');
-console.log(reversedString);
-let str = "Hello, World!";
-let reversedString = '';
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedString += str[i];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
-console.log(reversedString);
-function reverseString(str) {
-  if (str === '')
-    return '';
-  else
-    return reverseString(str.substr(1)) + str.charAt(0);
-}
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-let str = "Hello, World!";
-let reversedString = reverseString(str);
-console.log(reversedString);
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  dequeue() {
+    if (!this.head) {
+      return null;
+    }
+
+    const dequeuedValue = this.head.value;
+
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+    }
+
+    return dequeuedValue;
+  }
+
+  isEmpty() {
+    return this.head === null;
+  }
+
+  peek() {
+    if (!this.head) {
+      return null;
+    }
+    return this.head.value;
+  }
+}
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.peek()); // Output: 1
+
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 2
+
+console.log(queue.isEmpty()); // Output: false
+
+console.log(queue.peek()); // Output: 3
+
+console.log(queue.dequeue()); // Output: 3
+
+console.log(queue.isEmpty()); // Output: true
+
+console.log(queue.dequeue()); // Output: null
