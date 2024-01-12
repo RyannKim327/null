aaca58b02d9309199755f0621c017b70c2970d08
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:32:47 +0800

    1-12-2024 10:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,31 @@
-class Graph {
-  constructor(vertices) {
-    this.vertices = vertices;
-    this.edges = [];
-  }
-
-  addEdge(source, destination, weight) {
-    this.edges.push({ source, destination, weight });
-  }
+function Node(value) {
+  this.value = value;
+  this.next = null;
 }
-function initializeDistance(graph, source) {
-  const distance = [];
 
-  for (let i = 0; i < graph.vertices; i++) {
-    distance[i] = Infinity;
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  // Move the fast pointer by 2 steps and the slow pointer by 1 step
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  distance[source] = 0;
-  return distance;
+  // The value of the slow pointer will be the middle element
+  return slow.value;
 }
-function bellmanFord(graph, source) {
-  const distance = initializeDistance(graph, source);
 
-  // Relax edges
-  for (let i = 0; i < graph.vertices - 1; i++) {
-    for (let j = 0; j < graph.edges.length; j++) {
-      const { source, destination, weight } = graph.edges[j];
-      if (distance[source] + weight < distance[destination]) {
-        distance[destination] = distance[source] + weight;
-      }
-    }
-  }
+// Example usage:
 
-  // Check for negative weight cycles
-  for (let i = 0; i < graph.edges.length; i++) {
-    const { source, destination, weight } = graph.edges[i];
-    if (distance[source] + weight < distance[destination]) {
-      throw new Error('Graph contains negative weight cycle');
-    }
-  }
+// Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
 
-  return distance;
-}
-const graph = new Graph(5);
-graph.addEdge(0, 1, -1);
-graph.addEdge(0, 2, 4);
-graph.addEdge(1, 2, 3);
-graph.addEdge(1, 3, 2);
-graph.addEdge(1, 4, 2);
-graph.addEdge(3, 2, 5);
-graph.addEdge(3, 1, 1);
-graph.addEdge(4, 3, -3);
-const source = 0;
-const shortestPaths = bellmanFord(graph, source);
-console.log(shortestPaths);
+// Find the middle element
+const middleElement = findMiddleElement(head);
+console.log(middleElement); // Output: 3
