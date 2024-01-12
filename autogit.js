4a4feeee6a2102d756f9a3bfbbec13a70e027eac
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:11:38 +0800

    1-12-2024 10:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,21 @@
-const axios = require('axios');
+function isAnagram(str1, str2) {
+  // Remove non-letter characters
+  str1 = str1.replace(/[^A-Za-z]/g, '').toLowerCase();
+  str2 = str2.replace(/[^A-Za-z]/g, '').toLowerCase();
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function (response) {
-    console.log('Response:', response.data);
-  })
-  .catch(function (error) {
-    console.error('Error:', error);
-  });
+  // Sort the strings
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
+}
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (isAnagram(string1, string2)) {
+  console.log('The strings are anagrams.');
+} else {
+  console.log('The strings are not anagrams.');
+}
+The strings are anagrams.
