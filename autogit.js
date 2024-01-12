3f61670fabd1a7852d836d0cdf9c4aa85aa05c88
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:42:05 +0800

    1-12-2024 10:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,5 @@
-function findMajorityElement(nums) {
-  let candidate = null;
-  let count = 0;
+// Prompt the user for input
+var userInput = prompt("Enter something:");
 
-  for (let num of nums) {
-    if (count === 0) {
-      candidate = num;
-      count = 1;
-    } else if (candidate === num) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // At this point, the candidate might be the majority element
-  // We need to validate it by counting its occurrences
-
-  count = 0;
-  for (let num of nums) {
-    if (num === candidate) {
-      count++;
-    }
-  }
-
-  if (count > nums.length / 2) {
-    return candidate;
-  } else {
-    return "No majority element found";
-  }
-}
-
-// Example usage
-const array = [2, 2, 1, 1, 1, 2, 2];
-console.log(findMajorityElement(array)); // Output: 2
+// Display the user input on the console
+console.log("You entered: " + userInput);
