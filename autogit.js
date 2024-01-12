b1ed7d0a1147c93f1d55136cd15c434d22708d7d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:35:07 +0800

    1-12-2024 10:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,80 @@
-function calculateMean(numbers) {
-  const sum = numbers.reduce((total, num) => total + num, 0);
-  const mean = sum / numbers.length;
-  return mean;
+function tarjanSCC(graph) {
+  let index = 0;
+  const stack = [];
+  const SCC = [];
 }
+function strongConnect(node, index, stack, SCC) {
+  // Set the depth index for the current node
+  node.index = index;
+  node.lowlink = index;
+  index++;
+  stack.push(node);
 
-// Example usage:
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log(mean);
+  // Consider successors of the current node
+  for (let neighbor of node.neighbors) {
+    if (neighbor.index === undefined) {
+      // Successor node has not yet been visited; recurse on it
+      strongConnect(neighbor, index, stack, SCC);
+      node.lowlink = Math.min(node.lowlink, neighbor.lowlink);
+    } else if (stack.includes(neighbor)) {
+      // Successor node is in the stack, meaning it is part of the current SCC
+      node.lowlink = Math.min(node.lowlink, neighbor.index);
+    }
+  }
+
+  // If node is a root node, pop the stack and generate an SCC
+  if (node.lowlink === node.index) {
+    let component = [];
+    let w;
+    do {
+      w = stack.pop();
+      component.push(w);
+    } while (w !== node);
+    SCC.push(component);
+  }
+}
+for (let node of graph) {
+  if (node.index === undefined) {
+    strongConnect(node, index, stack, SCC);
+  }
+}
+return SCC;
+function tarjanSCC(graph) {
+  let index = 0;
+  const stack = [];
+  const SCC = [];
+
+  function strongConnect(node, index, stack, SCC) {
+    node.index = index;
+    node.lowlink = index;
+    index++;
+    stack.push(node);
+
+    for (let neighbor of node.neighbors) {
+      if (neighbor.index === undefined) {
+        strongConnect(neighbor, index, stack, SCC);
+        node.lowlink = Math.min(node.lowlink, neighbor.lowlink);
+      } else if (stack.includes(neighbor)) {
+        node.lowlink = Math.min(node.lowlink, neighbor.index);
+      }
+    }
+
+    if (node.lowlink === node.index) {
+      let component = [];
+      let w;
+      do {
+        w = stack.pop();
+        component.push(w);
+      } while (w !== node);
+      SCC.push(component);
+    }
+  }
+
+  for (let node of graph) {
+    if (node.index === undefined) {
+      strongConnect(node, index, stack, SCC);
+    }
+  }
+
+  return SCC;
+}
