9d7a163fbc8a312d1b0b459a54a50b8b80500dfb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:38:48 +0800

    1-12-2024 10:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,11 @@
-const str = "Hello, World!";
-const reversedStr = str.split("").reverse().join("");
-console.log(reversedStr);
-const str = "Hello, World!";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-}
-console.log(reversedStr);
-function reverseString(str) {
-    if (str === "") {
-        return "";
-    } else {
-        return reverseString(str.substr(1)) + str.charAt(0);
-    }
+// Function to calculate the area of a triangle
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
 
-const str = "Hello, World!";
-const reversedStr = reverseString(str);
-console.log(reversedStr);
+// Example usage
+var base = 5;
+var height = 7;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
