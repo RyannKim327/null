7e373a9fbc9be25e8874582a42315df3be0ca12b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:30:11 +0800

    1-12-2024 9:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,97 @@
-const mergeArrays = (arr1, arr2) => {
-  let mergedArray = [];
-  let i = 0, j = 0;
+class TreeNode {
+  constructor(order) {
+    this.order = order; // Order of the B-tree
+    this.keys = []; // Array to hold keys
+    this.children = []; // Array to hold child pointers
+  }
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      mergedArray.push(arr1[i]);
-      i++;
+  isLeaf() {
+    return this.children.length === 0;
+  }
+}
+
+class BTree {
+  constructor(order) {
+    this.root = new TreeNode(order);
+    this.order = order;
+  }
+
+  insert(key) {
+    const node = this.root;
+    if (node.keys.length === (2 * this.order) - 1) {
+      const newRoot = new TreeNode(this.order);
+      this.root = newRoot;
+      newRoot.children[0] = node;
+      this.splitChild(newRoot, 0);
+      this.insertNonFull(newRoot, key);
     } else {
-      mergedArray.push(arr2[j]);
-      j++;
+      this.insertNonFull(node, key);
     }
   }
 
-  while (i < arr1.length) {
-    mergedArray.push(arr1[i]);
-    i++;
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+    if (node.isLeaf()) {
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i + 1] = node.keys[i];
+        i--;
+      }
+      node.keys[i + 1] = key;
+    } else {
+      while (i >= 0 && key < node.keys[i]) {
+        i--;
+      }
+      i = i + 1;
+      if (node.children[i].keys.length === (2 * this.order) - 1) {
+        this.splitChild(node, i);
+        if (key > node.keys[i]) {
+          i++;
+        }
+      }
+      this.insertNonFull(node.children[i], key);
+    }
   }
 
-  while (j < arr2.length) {
-    mergedArray.push(arr2[j]);
-    j++;
+  splitChild(parent, index) {
+    const order = this.order;
+    const child = parent.children[index];
+    const newChild = new TreeNode(order);
+
+    parent.keys.splice(index, 0, child.keys[order - 1]);
+
+    parent.children.splice(index + 1, 0, newChild);
+
+    newChild.keys = child.keys.splice(order, order - 1);
+    if (!child.isLeaf()) {
+      newChild.children = child.children.splice(order, order);
+    }
   }
 
-  return mergedArray;
-};
-const findMedianSortedArrays = (nums1, nums2) => {
-  const mergedArray = mergeArrays(nums1, nums2);
-  const length = mergedArray.length;
-  const middleIndex = Math.floor(length / 2);
-
-  // Check if the length is odd
-  if (length % 2 !== 0) {
-    return mergedArray[middleIndex];
+  search(key) {
+    return this.searchRecursive(this.root, key);
   }
 
-  // If the length is even, calculate the average of the middle two elements
-  return (mergedArray[middleIndex - 1] + mergedArray[middleIndex]) / 2;
-};
-const nums1 = [1, 3, 5];
-const nums2 = [2, 4, 6];
-const median = findMedianSortedArrays(nums1, nums2);
-console.log(median); // Output: 3.5
+  searchRecursive(node, key) {
+    let i = 0;
+    while (i < node.keys.length && key > node.keys[i]) {
+      i++;
+    }
+    if (node.keys[i] === key) {
+      return true;
+    }
+    if (node.isLeaf()) {
+      return false;
+    }
+    return this.searchRecursive(node.children[i], key);
+  }
+}
+const bTree = new BTree(3); // Initialize a B-tree with order 3
+
+bTree.insert(7);
+bTree.insert(3);
+bTree.insert(12);
+bTree.insert(1);
+bTree.insert(5);
+
+console.log(bTree.search(12)); // Output: true
+console.log(bTree.search(9)); // Output: false
