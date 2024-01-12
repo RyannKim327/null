40b4b7db350cb020795b3aed4a80e86e4b5e980a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:01:01 +0800

    1-12-2024 10:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function factorialLoop(num) {
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorialLoop(5)); // Output: 120
+function factorialRecursive(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorialRecursive(num - 1);
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
-
-  isEmpty() {
-    return this.length === 0;
-  }
-
-  addToTail(value) {
-    const node = new Node(value);
-    if (this.isEmpty()) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-    this.length++;
-  }
-
-  removeFromHead() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    const value = this.head.value;
-    if (this.length === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.next;
-    }
-    this.length--;
-    return value;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.list = new LinkedList();
-  }
-
-  isEmpty() {
-    return this.list.isEmpty();
-  }
-
-  enqueue(value) {
-    this.list.addToTail(value);
-  }
-
-  dequeue() {
-    return this.list.removeFromHead();
-  }
-}
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-console.log(queue.isEmpty()); // Output: false
-console.log(queue.dequeue()); // Output: 3
-console.log(queue.isEmpty()); // Output: true
+console.log(factorialRecursive(5)); // Output: 120
