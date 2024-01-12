7efb82dfdc1921b0dc04aa02630ccb5f460c1aab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:57:58 +0800

    1-12-2024 9:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,76 +1,9 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
+const cron = require('cron');
 
-  // Helper method to insert a value into the tree
-  insert(value) {
-    const newNode = new Node(value);
+// Create a new cron job that runs every minute
+const job = new cron.CronJob('* * * * *', () => {
+  console.log('This function will be executed every minute');
+}, null, true, 'UTC');
 
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  // Recursive helper method to insert a value into the tree
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  // Helper method to perform an in-order traversal of the tree
-  inOrderTraversal(callback) {
-    this.inOrderTraversalNode(this.root, callback);
-  }
-
-  // Recursive helper method to perform an in-order traversal of the tree
-  inOrderTraversalNode(node, callback) {
-    if (node !== null) {
-      this.inOrderTraversalNode(node.left, callback);
-      callback(node.value);
-      this.inOrderTraversalNode(node.right, callback);
-    }
-  }
-}
-const tree = new BinarySearchTree();
-tree.insert(8);
-tree.insert(3);
-tree.insert(10);
-tree.insert(1);
-tree.insert(6);
-tree.insert(14);
-tree.insert(4);
-tree.insert(7);
-tree.insert(13);
-
-// Example usage of in-order traversal
-tree.inOrderTraversal((value) => console.log(value));
-1
-3
-4
-6
-7
-8
-10
-13
-14
+// Start the cron job
+job.start();
