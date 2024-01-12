a3121ccedfae2fa5b31bd01060cc4e0926e06258
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 08:44:09 +0800

    1-12-2024 8:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,58 @@
-function findKthSmallest(arr, k) {
-  // Perform a quickselect to find the kth smallest element
-  return quickselect(arr, 0, arr.length - 1, k - 1);
+function burrowsWheelerTransform(input) {
+  // Implement the BWT algorithm here
+}
+function burrowsWheelerTransform(input) {
+  input += '$';
+  // Rest of the code
+}
+function generateCyclicRotations(input) {
+  const rotations = [];
+  const len = input.length;
+  for (let i = 0; i < len; i++) {
+    rotations.push(input);
+    input = input.charAt(len - 1) + input.slice(0, len - 1);
+  }
+  return rotations;
 }
 
-function quickselect(arr, low, high, k) {
-  if (low === high) {
-    // If there's only one element in the range, return it
-    return arr[low];
-  }
-
-  // Partition the array and get the pivot index
-  const pivotIndex = partition(arr, low, high);
-
-  if (k === pivotIndex) {
-    // If pivot index is the kth smallest element, return it
-    return arr[pivotIndex];
-  } else if (k < pivotIndex) {
-    // If kth smallest element is in the left partition, recursively quickselect on the left half
-    return quickselect(arr, low, pivotIndex - 1, k);
-  } else {
-    // If kth smallest element is in the right partition, recursively quickselect on the right half
-    return quickselect(arr, pivotIndex + 1, high, k);
-  }
+function burrowsWheelerTransform(input) {
+  input += '$';
+  const rotations = generateCyclicRotations(input);
+  // Rest of the code
 }
+function burrowsWheelerTransform(input) {
+  input += '$';
+  const rotations = generateCyclicRotations(input);
+  rotations.sort();
 
-function partition(arr, low, high) {
-  // Choose the rightmost element as the pivot
-  const pivot = arr[high];
-  let i = low;
+  // Rest of the code
+}
+function burrowsWheelerTransform(input) {
+  input += '$';
+  const rotations = generateCyclicRotations(input);
+  rotations.sort();
 
-  for (let j = low; j < high; j++) {
-    // If current element is smaller than or equal to pivot, swap it with the element at index i
-    if (arr[j] <= pivot) {
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-      i++;
+  let transformed = '';
+  for (const rotation of rotations) {
+    transformed += rotation.charAt(len - 1);
+  }
+  
+  // Rest of the code
+}
+function burrowsWheelerTransform(input) {
+  input += '$';
+  const rotations = generateCyclicRotations(input);
+  rotations.sort();
+
+  let transformed = '';
+  let index = null;
+  for (let i = 0; i < rotations.length; i++) {
+    const rotation = rotations[i];
+    transformed += rotation.charAt(len - 1);
+    if (rotation === input) {
+      index = i;
     }
   }
 
-  // Swap the pivot with the element at index i
-  [arr[i], arr[high]] = [arr[high], arr[i]];
-  return i;
+  // Rest of the code
 }
-
-// Example usage
-const array = [5, 9, 1, 3, 7];
-const k = 2;
-const kthSmallest = findKthSmallest(array, k);
-console.log(kthSmallest); // Output: 3
