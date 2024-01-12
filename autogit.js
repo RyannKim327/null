46d4f6a1621f22010b4790ea6add0064dad50037
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 08:59:13 +0800

    1-12-2024 8:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,57 @@
-function binarySearch(array, target, start = 0, end = array.length - 1) {
-  if (start > end) {
-    return -1; // target value not found
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
   }
 
-  const middle = Math.floor((start + end) / 2);
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 
-  if (array[middle] === target) {
-    return middle; // target value found
-  } else if (array[middle] > target) {
-    return binarySearch(array, target, start, middle - 1); // search in the left half
-  } else {
-    return binarySearch(array, target, middle + 1, end); // search in the right half
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1] || !this.adjacencyList[vertex2]) {
+      return;
+    }
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
   }
 }
+function depthFirstSearch(graph, startVertex) {
+  const visited = {};
+  const result = [];
+
+  function dfs(vertex) {
+    if (!vertex) {
+      return null;
+    }
+
+    visited[vertex] = true;
+    result.push(vertex);
+
+    graph.adjacencyList[vertex].forEach(neighbor => {
+      if (!visited[neighbor]) {
+        dfs(neighbor);
+      }
+    });
+  }
+
+  dfs(startVertex);
+
+  return result;
+}
+// Create a graph
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('D', 'E');
+graph.addEdge('C', 'E');
+
+// Test DFS
+console.log(depthFirstSearch(graph, 'A')); // Output: [ 'A', 'B', 'D', 'E', 'C' ]
