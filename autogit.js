2aaf6d2403db4cf3efc36d55cdeb4af41ca04534
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:08:48 +0800

    1-12-2024 10:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,9 @@
-function createBCST(pattern) {
-  const bcst = {};
+let str = "  Hello,   World!   ";
 
-  for (let i = 0; i < pattern.length - 1; i++) {
-    bcst[pattern[i]] = pattern.length - 1 - i;
-  }
+// Remove leading and trailing whitespace
+str = str.trim();
 
-  return bcst;
-}
-function search(text, pattern) {
-  const bcst = createBCST(pattern);
-  const patternLength = pattern.length;
-  const textLength = text.length;
-  let i = patternLength - 1;
+// Remove all whitespace characters
+str = str.replace(/\s+/g, '');
 
-  while (i < textLength) {
-    let j = patternLength - 1;
-    let k = i;
-
-    while (j >= 0 && text[k] === pattern[j]) {
-      j--;
-      k--;
-    }
-
-    if (j === -1) {
-      return k + 1; // found the pattern
-    } else {
-      const shift = bcst[text[i]];
-      i += shift || patternLength;
-    }
-  }
-
-  return -1; // pattern not found
-}
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-const pattern = "sit";
-
-const index = search(text, pattern);
-
-if (index !== -1) {
-  console.log(`Pattern found at index ${index}.`);
-} else {
-  console.log("Pattern not found.");
-}
+console.log(str); // Output: "Hello,World!"
