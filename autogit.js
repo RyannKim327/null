11c44896982458f83140a24d0483e8de1fb45ed3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:10:41 +0800

    1-12-2024 10:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,119 @@
-function bwt(string) {
-  // Step 1: Create an array to store all rotations
-  let rotations = [];
-  
-  // Step 2: Generate all cyclic rotations of the input string
-  for (let i = 0; i < string.length; i++) {
-    rotations.push(string.slice(i) + string.slice(0, i));
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  // Step 3: Sort the rotations array lexicographically
-  rotations.sort();
-  
-  // Step 4: Extract the last characters from each rotation
-  let bwtResult = '';
-  for (let i = 0; i < rotations.length; i++) {
-    bwtResult += rotations[i][string.length - 1];
-  }
-  
-  // Step 5: Return the BWT result
-  return bwtResult;
 }
 
-const input = 'banana';
-const bwtResult = bwt(input);
-console.log(bwtResult);  // Outputs: 'annb$aa'
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value === node.value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.removeNode(node.right, minNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  // Other helper methods, like traversal methods, can be added here
+}
+const bst = new BinarySearchTree();
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+console.log(bst.search(40)); // Output: true
+console.log(bst.search(90)); // Output: false
+
+bst.remove(30);
+console.log(bst.search(30)); // Output: false
