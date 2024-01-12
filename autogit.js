7731a7ddb7bd819caabc7680994a492c7501f0ab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:06:33 +0800

    1-12-2024 9:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,66 +1,8 @@
-class TrieNode {
-  constructor(value) {
-    this.value = value;
-    this.isEndOfWord = false;
-    this.children = {};
-  }
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-class Trie {
-  constructor() {
-    this.root = new TrieNode(null);
-  }
 
-  insert(word) {
-    let currentNode = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-
-      if (!currentNode.children[char]) {
-        currentNode.children[char] = new TrieNode(char);
-      }
-
-      currentNode = currentNode.children[char];
-    }
-
-    currentNode.isEndOfWord = true;
-  }
-
-  search(word) {
-    let currentNode = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-
-      if (!currentNode.children[char]) {
-        return false; // Word does not exist
-      }
-
-      currentNode = currentNode.children[char];
-    }
-
-    return currentNode.isEndOfWord;
-  }
-
-  startsWith(prefix) {
-    let currentNode = this.root;
-    for (let i = 0; i < prefix.length; i++) {
-      const char = prefix[i];
-
-      if (!currentNode.children[char]) {
-        return false; // No word starts with the given prefix
-      }
-
-      currentNode = currentNode.children[char];
-    }
-
-    return true;
-  }
-}
-const trie = new Trie();
-
-trie.insert("apple");
-console.log(trie.search("apple")); // true
-console.log(trie.search("app")); // false
-console.log(trie.startsWith("app")); // true
-
-trie.insert("app");
-console.log(trie.search("app")); // true
+// Example usage
+const decimal = 10;
+const binary = decimalToBinary(decimal);
+console.log(binary); // Output: 1010
