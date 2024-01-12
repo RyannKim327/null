f9afd13ddb66b401d769dd1c6f5897c31049951d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:38:34 +0800

    1-12-2024 10:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,101 +1,20 @@
-class BinaryHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2);
-  }
-
-  getLeftChildIndex(index) {
-    return 2 * index + 1;
-  }
-
-  getRightChildIndex(index) {
-    return 2 * index + 2;
-  }
-  
-  swap(index1, index2) {
-    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-  }
-
-  insert(element) {
-    this.heap.push(element);
-    this.heapifyUp();
-  }
-
-  extractMin() {
-    if (this.isEmpty()) {
-      return null;
-    }
-
-    const min = this.heap[0];
-    const lastElement = this.heap.pop();
-
-    if (!this.isEmpty()) {
-      this.heap[0] = lastElement;
-      this.heapifyDown();
-    }
-
-    return min;
-  }
-
-  heapifyUp() {
-    let currentIndex = this.heap.length - 1;
-
-    while (currentIndex > 0) {
-      const parentIndex = this.getParentIndex(currentIndex);
-
-      if (this.heap[currentIndex] < this.heap[parentIndex]) {
-        this.swap(currentIndex, parentIndex);
-        currentIndex = parentIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  heapifyDown() {
-    let currentIndex = 0;
-
-    while (this.getLeftChildIndex(currentIndex) < this.heap.length) {
-      const leftChildIndex = this.getLeftChildIndex(currentIndex);
-      const rightChildIndex = this.getRightChildIndex(currentIndex);
-      let smallerChildIndex = leftChildIndex;
-
-      if (
-        rightChildIndex < this.heap.length &&
-        this.heap[rightChildIndex] < this.heap[leftChildIndex]
-      ) {
-        smallerChildIndex = rightChildIndex;
-      }
-
-      if (this.heap[currentIndex] > this.heap[smallerChildIndex]) {
-        this.swap(currentIndex, smallerChildIndex);
-        currentIndex = smallerChildIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
+const str = "Hello, World!";
+const reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
+const str = "Hello, World!";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
 }
-const priorityQueue = new BinaryHeap();
+console.log(reversedStr);
+function reverseString(str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverseString(str.substr(1)) + str.charAt(0);
+    }
+}
 
-priorityQueue.insert(5);
-priorityQueue.insert(2);
-priorityQueue.insert(8);
-priorityQueue.insert(1);
-
-console.log(priorityQueue.extractMin()); // Output: 1
-console.log(priorityQueue.extractMin()); // Output: 2
-console.log(priorityQueue.extractMin()); // Output: 5
-console.log(priorityQueue.extractMin()); // Output: 8
-console.log(priorityQueue.extractMin()); // Output: null
+const str = "Hello, World!";
+const reversedStr = reverseString(str);
+console.log(reversedStr);
