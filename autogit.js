bda1ef4be4e2d33f452fdae89888acd7f25d0166
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:14:01 +0800

    1-12-2024 10:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,10 @@
-function binarySearch(searchValue, sortedArray, startIndex = 0, endIndex = sortedArray.length - 1) {
-  // ...
-}
-if (startIndex > endIndex || searchValue < sortedArray[startIndex] || searchValue > sortedArray[endIndex]) {
-  return -1;
-}
-const middleIndex = Math.floor((startIndex + endIndex) / 2);
-if (sortedArray[middleIndex] === searchValue) {
-  return middleIndex;
-}
-if (sortedArray[middleIndex] > searchValue) {
-  return binarySearch(searchValue, sortedArray, startIndex, middleIndex - 1);
-}
-if (sortedArray[middleIndex] < searchValue) {
-  return binarySearch(searchValue, sortedArray, middleIndex + 1, endIndex);
-}
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var str1 = "Hello";
+var str2 = "World";
 
-console.log(binarySearch(6, arr)); // Output: 5
+var result = str1 + " " + str2;
+console.log(result); // Output: "Hello World"
+var str1 = "Hello";
+var str2 = "World";
+
+var result = str1.concat(" ", str2);
+console.log(result); // Output: "Hello World"
