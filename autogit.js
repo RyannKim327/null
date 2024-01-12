e6e1006376a3fbc42766b8b3199009bf77967aa9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:56:11 +0800

    1-12-2024 9:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,69 @@
-let array = [1, 2, 3, 3, 4, 4, 5];
-let uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-let array = [1, 2, 3, 3, 4, 4, 5];
-let uniqueArray = array.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-let array = [1, 2, 3, 3, 4, 4, 5];
-let uniqueArray = [];
-
-for (let i = 0; i < array.length; i++) {
-    if (uniqueArray.indexOf(array[i]) === -1) {
-        uniqueArray.push(array[i]);
-    }
+function fibonacciSearch(arr, x, n) {
+  // ...
 }
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+function generateFib(n) {
+  const fib = [0, 1];
+  while (fib[fib.length - 1] < n) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return fib;
+}
+const fib = generateFib(n);
+
+let offset = -1;
+let prev = fib[fib.length - 2];
+let curr = fib[fib.length - 3];
+while (curr >= 0 && curr < n) {
+  if (arr[offset + prev] < x) {
+    offset = offset + prev;
+    prev = curr;
+    curr = fib[fib.indexOf(prev) - 1];
+  } else if (arr[offset + prev] > x) {
+    prev = curr;
+    curr = fib[fib.indexOf(curr) - 2];
+  } else {
+    return offset + prev;
+  }
+}
+return -1;
+function generateFib(n) {
+  const fib = [0, 1];
+  while (fib[fib.length - 1] < n) {
+    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+  }
+  return fib;
+}
+
+function fibonacciSearch(arr, x, n) {
+  const fib = generateFib(n);
+
+  let offset = -1;
+  let prev = fib[fib.length - 2];
+  let curr = fib[fib.length - 3];
+
+  while (curr >= 0 && curr < n) {
+    if (arr[offset + prev] < x) {
+      offset = offset + prev;
+      prev = curr;
+      curr = fib[fib.indexOf(prev) - 1];
+    } else if (arr[offset + prev] > x) {
+      prev = curr;
+      curr = fib[fib.indexOf(curr) - 2];
+    } else {
+      return offset + prev;
+    }
+  }
+
+  return -1;
+}
+const arr = [4, 8, 15, 16, 23, 42];
+const x = 16;
+const n = arr.length;
+
+const index = fibonacciSearch(arr, x, n);
+
+if (index === -1) {
+  console.log("Element not found");
+} else {
+  console.log(`Element found at index ${index}`);
+}
