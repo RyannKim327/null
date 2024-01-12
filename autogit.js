54f06a88089ab6ad1fd4191975298a9dddcd2782
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:31:20 +0800

    1-12-2024 10:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,9 @@
-const array = [3, 8, 1, 6, 2, 5];
-
-const max = Math.max(...array);
-
-console.log(max); // Output: 8
+const array = [4, 2, 9, 1, 5];
+array.sort((a, b) => a - b);
+console.log(array); // Output: [1, 2, 4, 5, 9]
+const array = [4, 2, 9, 1, 5];
+array.sort((a, b) => a - b).reverse();
+console.log(array); // Output: [9, 5, 4, 2, 1]
+const array = [4, 2, 9, 1, 5];
+const sortedArray = array.slice().sort((a, b) => a - b);
+console.log(sortedArray); // Output: [1, 2, 4, 5, 9]
