faa7084709b09bf0fa3b3e12115303cecab5baef
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:18:44 +0800

    1-12-2024 10:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-function factorialIterative(num) {
-  let result = 1;
-  for (let i = 2; i <= num; i++) {
-    result *= i;
-  }
-  return result;
+function validateEmail(email) {
+  const pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+  return pattern.test(email);
 }
-function factorialRecursive(num) {
-  if (num === 0 || num === 1) {
-    return 1;
-  }
-  return num * factorialRecursive(num - 1);
-}
-console.log(factorialIterative(5));    // Output: 120
-console.log(factorialRecursive(5));    // Output: 120
