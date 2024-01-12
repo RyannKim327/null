dfcdcbf9b837777316456b726128007192f11aab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:13:30 +0800

    1-12-2024 10:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,19 @@
-function buildPrefixTable(pattern) {
-  const prefixTable = [0];
-  let len = 0;
-  let i = 1;
-
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      prefixTable[i] = len;
-      i++;
-    } else {
-      if (len > 0) {
-        len = prefixTable[len - 1];
-      } else {
-        prefixTable[i] = 0;
-        i++;
-      }
-    }
-  }
-
-  return prefixTable;
+function binarySearch(searchValue, sortedArray, startIndex = 0, endIndex = sortedArray.length - 1) {
+  // ...
 }
-function kmpSearch(text, pattern) {
-  const prefixTable = buildPrefixTable(pattern);
-  const matches = [];
-
-  let i = 0; // index in text
-  let j = 0; // index in pattern
-
-  while (i < text.length) {
-    if (text[i] === pattern[j]) {
-      i++;
-      j++;
-    }
-
-    if (j === pattern.length) {
-      matches.push(i - j); // pattern found at index i-j in the text
-      j = prefixTable[j - 1];
-    } else if (i < text.length && text[i] !== pattern[j]) {
-      if (j !== 0) {
-        j = prefixTable[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-
-  return matches;
+if (startIndex > endIndex || searchValue < sortedArray[startIndex] || searchValue > sortedArray[endIndex]) {
+  return -1;
 }
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
+const middleIndex = Math.floor((startIndex + endIndex) / 2);
+if (sortedArray[middleIndex] === searchValue) {
+  return middleIndex;
+}
+if (sortedArray[middleIndex] > searchValue) {
+  return binarySearch(searchValue, sortedArray, startIndex, middleIndex - 1);
+}
+if (sortedArray[middleIndex] < searchValue) {
+  return binarySearch(searchValue, sortedArray, middleIndex + 1, endIndex);
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const matches = kmpSearch(text, pattern);
-console.log(matches); // Output: [10]
+console.log(binarySearch(6, arr)); // Output: 5
