8c6a01fef5131d2f85132dd6ea2fb99263633fc9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:18:20 +0800

    1-12-2024 9:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,8 @@
-function areAnagrams(str1, str2) {
-  // Remove non-alphabetic characters and spaces
-  str1 = str1.replace(/[^A-Za-z]/g, '').toLowerCase();
-  str2 = str2.replace(/[^A-Za-z]/g, '').toLowerCase();
-
-  // Sort the strings
-  str1 = str1.split('').sort().join('');
-  str2 = str2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return str1 === str2;
-}
-
-// Example usage
-const string1 = 'listen';
-const string2 = 'silent';
-
-if (areAnagrams(string1, string2)) {
-  console.log('The strings are anagrams!');
-} else {
-  console.log('The strings are not anagrams.');
-}
+const array = [1, 2, 3, 4, 3, 2, 1];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray); // Output: [1, 2, 3, 4]
+const array = [1, 2, 3, 4, 3, 2, 1];
+const uniqueArray = array.filter((value, index, self) => {
+  return self.indexOf(value) === index;
+});
+console.log(uniqueArray); // Output: [1, 2, 3, 4]
