086417494391a06757d9a40207b8245c7e080fd1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:43:54 +0800

    1-12-2024 9:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,49 @@
-function findMiddleElement(head) {
-  if (head === null) {
-    return null; // Empty list
+class Graph {
+  constructor() {
+    this.vertices = {};
   }
 
-  let slowPointer = head;
-  let fastPointer = head;
-
-  while (fastPointer !== null && fastPointer.next !== null) {
-    slowPointer = slowPointer.next; // Move slow pointer by 1
-    fastPointer = fastPointer.next.next; // Move fast pointer by 2
+  addVertex(vertex) {
+    this.vertices[vertex] = [];
   }
 
-  return slowPointer;
+  addEdge(vertex1, vertex2) {
+    this.vertices[vertex1].push(vertex2);
+    this.vertices[vertex2].push(vertex1);
+  }
+
+  getNeighbors(vertex) {
+    return this.vertices[vertex];
+  }
 }
-let middleElement = findMiddleElement(head);
+function breadthFirstSearch(graph, startVertex) {
+  const queue = [startVertex]; // Initialize a queue with the starting vertex
+  const visited = new Set(); // Track the visited vertices
+  visited.add(startVertex);
+
+  while (queue.length > 0) {
+    const currentVertex = queue.shift();
+    console.log(currentVertex); // Or perform any other operation on the current vertex
+
+    const neighbors = graph.getNeighbors(currentVertex);
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        queue.push(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+}
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+breadthFirstSearch(graph, 'A');
