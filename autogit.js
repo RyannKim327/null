3de1b8119eb630ef8d34bb133d0f7213c2493ec1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 08:43:28 +0800

    1-12-2024 8:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,44 @@
-function isAnagram(str1, str2) {
-  // Convert strings to lowercase and remove non-alphabetic characters
-  str1 = str1.toLowerCase().replace(/[^a-z]/g, '');
-  str2 = str2.toLowerCase().replace(/[^a-z]/g, '');
+class HashTable {
+  constructor() {
+    this.table = {};
+  }
 
-  // Convert strings into arrays of characters
-  let arr1 = str1.split('');
-  let arr2 = str2.split('');
-
-  // Sort the arrays
-  arr1.sort();
-  arr2.sort();
-
-  // Compare the sorted arrays
-  return arr1.join('') === arr2.join('');
+  // Other methods will be added below
 }
+hash(key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
+  }
+  return hash;
+}
+insert(key, value) {
+  const index = this.hash(key);
+  if (!this.table[index]) {
+    this.table[index] = {};
+  }
+  this.table[index][key] = value;
+}
+remove(key) {
+  const index = this.hash(key);
+  if (this.table[index] && this.table[index][key]) {
+    delete this.table[index][key];
+  }
+}
+get(key) {
+  const index = this.hash(key);
+  if (this.table[index] && this.table[index][key]) {
+    return this.table[index][key];
+  }
+  return undefined;
+}
+const myTable = new HashTable();
+myTable.insert("apple", 10);
+myTable.insert("banana", 5);
+myTable.insert("orange", 15);
 
-// Example usage
-console.log(isAnagram('listen', 'silent')); // true
-console.log(isAnagram('hello', 'world')); // false
+console.log(myTable.get("apple")); // Output: 10
+console.log(myTable.get("banana")); // Output: 5
+
+myTable.remove("banana");
+console.log(myTable.get("banana")); // Output: undefined
