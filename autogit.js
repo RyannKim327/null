781ef672c84aec17fc918a70df7595ff54ff1c28
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 09:30:57 +0800

    1-12-2024 9:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,7 @@
-function factorialIterative(num) {
-  if (num < 0) {
-    return "Factorial not defined for negative numbers";
-  }
-  let result = 1;
-  for (let i = 2; i <= num; i++) {
-    result *= i;
-  }
-  return result;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
-// Example usage
-console.log(factorialIterative(5));  // Output: 120
-function factorialRecursive(num) {
-  if (num < 0) {
-    return "Factorial not defined for negative numbers";
-  }
-  if (num === 0 || num === 1) {
-    return 1;
-  }
-  return num * factorialRecursive(num - 1);
-}
-
-// Example usage
-console.log(factorialRecursive(5));  // Output: 120
+console.log(decimalToBinary(10));    // Output: "1010"
+console.log(decimalToBinary(42));    // Output: "101010"
+console.log(decimalToBinary(255));   // Output: "11111111"
