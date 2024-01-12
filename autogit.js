912c8a61f1c97b3a66c01877f01629cbba3e00c3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:17:33 +0800

    1-12-2024 10:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,10 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-}
-reverse() {
-  let prev = null;
-  let current = this.head;
-  let next = null;
+let str = "42";
+let num = parseInt(str);
 
-  while (current !== null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
+console.log(num); // Output: 42
+console.log(typeof num); // Output: number
+let str = "Hello";
+let num = parseInt(str);
 
-  this.head = prev;
-}
-const linkedList = new LinkedList();
-
-// Add nodes to the list (for example)
-linkedList.head = new Node(1);
-linkedList.head.next = new Node(2);
-linkedList.head.next.next = new Node(3);
-linkedList.head.next.next.next = new Node(4);
-
-// Reverse the list
-linkedList.reverse();
+console.log(num); // Output: NaN
+console.log(isNaN(num)); // Output: true
