42dd1bd967a0d155f87636d1571fc8d3f80ff518
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 08:43:47 +0800

    1-12-2024 8:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,49 @@
-class HashTable {
-  constructor() {
-    this.table = {};
+function findKthSmallest(arr, k) {
+  // Perform a quickselect to find the kth smallest element
+  return quickselect(arr, 0, arr.length - 1, k - 1);
+}
+
+function quickselect(arr, low, high, k) {
+  if (low === high) {
+    // If there's only one element in the range, return it
+    return arr[low];
   }
 
-  // Other methods will be added below
-}
-hash(key) {
-  let hash = 0;
-  for (let i = 0; i < key.length; i++) {
-    hash += key.charCodeAt(i);
-  }
-  return hash;
-}
-insert(key, value) {
-  const index = this.hash(key);
-  if (!this.table[index]) {
-    this.table[index] = {};
-  }
-  this.table[index][key] = value;
-}
-remove(key) {
-  const index = this.hash(key);
-  if (this.table[index] && this.table[index][key]) {
-    delete this.table[index][key];
-  }
-}
-get(key) {
-  const index = this.hash(key);
-  if (this.table[index] && this.table[index][key]) {
-    return this.table[index][key];
-  }
-  return undefined;
-}
-const myTable = new HashTable();
-myTable.insert("apple", 10);
-myTable.insert("banana", 5);
-myTable.insert("orange", 15);
+  // Partition the array and get the pivot index
+  const pivotIndex = partition(arr, low, high);
 
-console.log(myTable.get("apple")); // Output: 10
-console.log(myTable.get("banana")); // Output: 5
+  if (k === pivotIndex) {
+    // If pivot index is the kth smallest element, return it
+    return arr[pivotIndex];
+  } else if (k < pivotIndex) {
+    // If kth smallest element is in the left partition, recursively quickselect on the left half
+    return quickselect(arr, low, pivotIndex - 1, k);
+  } else {
+    // If kth smallest element is in the right partition, recursively quickselect on the right half
+    return quickselect(arr, pivotIndex + 1, high, k);
+  }
+}
 
-myTable.remove("banana");
-console.log(myTable.get("banana")); // Output: undefined
+function partition(arr, low, high) {
+  // Choose the rightmost element as the pivot
+  const pivot = arr[high];
+  let i = low;
+
+  for (let j = low; j < high; j++) {
+    // If current element is smaller than or equal to pivot, swap it with the element at index i
+    if (arr[j] <= pivot) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+    }
+  }
+
+  // Swap the pivot with the element at index i
+  [arr[i], arr[high]] = [arr[high], arr[i]];
+  return i;
+}
+
+// Example usage
+const array = [5, 9, 1, 3, 7];
+const k = 2;
+const kthSmallest = findKthSmallest(array, k);
+console.log(kthSmallest); // Output: 3
