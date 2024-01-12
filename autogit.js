56293e2d3e1fb813d3a00cd296fca9b0a079c428
RyannKim327 <rksesgundo123@gmail.com>
2024-01-12 10:14:50 +0800

    1-12-2024 10:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,50 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Add a new node at the end of the list
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Find the length of the linked list
+  findLength() {
+    let current = this.head;
+    let length = 0;
+
+    while (current) {
+      length++;
+      current = current.next;
+    }
+
+    return length;
+  }
 }
 
 // Example usage
-var randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+linkedList.add(5);
+
+console.log(linkedList.findLength()); // Output: 5
