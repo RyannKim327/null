31ba1db653710d2a40dd69fc01df7a3186aa3a15
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:38:19 +0800

    1-13-2024 14:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,47 @@
-class ListNode {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
-function reverseLinkedList(head) {
-  let current = head;
-  let prev = null;
 
-  while (current !== null) {
-    const nextNode = current.next;
-    current.next = prev;
-    prev = current;
-    current = nextNode;
+class LinkedList {
+  constructor() {
+    this.head = null;
   }
+  
+  append(data) {
+    const newNode = new Node(data);
 
-  return prev;
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+  
+  getLength() {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      count++;
+      current = current.next;
+    }
+
+    return count;
+  }
 }
-// Create a sample linked list: 1 -> 2 -> 3 -> 4 -> null
-const node4 = new ListNode(4);
-const node3 = new ListNode(3, node4);
-const node2 = new ListNode(2, node3);
-const node1 = new ListNode(1, node2);
 
-console.log("Original linked list:");
-let current = node1;
-while (current !== null) {
-  console.log(current.value);
-  current = current.next;
-}
-
-const reversed = reverseLinkedList(node1);
-
-console.log("Reversed linked list:");
-let currentReversed = reversed;
-while (currentReversed !== null) {
-  console.log(currentReversed.value);
-  currentReversed = currentReversed.next;
-}
-Original linked list:
-1
-2
-3
-4
-Reversed linked list:
-4
-3
-2
-1
+// Example usage:
+const myList = new LinkedList();
+myList.append(1);
+myList.append(2);
+myList.append(3);
+console.log(myList.getLength()); // Output: 3
