3cdad374ebb985e98231279a6375aa6c0027757c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:17:47 +0800

    1-13-2024 14:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,29 @@
-// Define a Graph class representing the graph structure
-class Graph {
-  constructor() {
-    this.nodes = [];
-    this.adjList = {};
-  }
-
-  addNode(node) {
-    this.nodes.push(node);
-    this.adjList[node] = [];
-  }
-
-  addEdge(node1, node2) {
-    this.adjList[node1].push(node2);
-    this.adjList[node2].push(node1);
-  }
-
-  getNeighbors(node) {
-    return this.adjList[node];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-// Perform Breadth-First Search on a graph
-function breadthFirstSearch(graph, startNode) {
-  const visited = new Set(); // Track visited nodes
-  const queue = []; // Queue for BFS
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-  visited.add(startNode);
-  queue.push(startNode);
-
-  while (queue.length > 0) {
-    const currentNode = queue.shift();
-    console.log(currentNode);
-
-    const neighbors = graph.getNeighbors(currentNode);
-
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        visited.add(neighbor);
-        queue.push(neighbor);
-      }
-    }
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
+
+  return slow.value;
 }
 
-// Example usage:
-const graph = new Graph();
+// Create a linked list
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
 
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addNode('E');
-
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'D');
-graph.addEdge('B', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'E');
-
-breadthFirstSearch(graph, 'A');
+// Find the middle element
+const middleElement = findMiddleElement(head);
+console.log(middleElement); // Output: 3
