c51cd025886dffb9a596ae1cdfc0b38f4f4f4a03
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 15:02:23 +0800

    1-13-2024 15:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,49 @@
-const numbers = [10, 5, 8, 20, 3];
-const maximumValue = Math.max(...numbers);
+class HashTable {
+  constructor() {
+    this.table = {};
+  }
 
-console.log(maximumValue); // Output: 20
+  // Hash function to generate an index
+  hash(key) {
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
+    }
+    return hashValue % 100; // Choose a prime number for the table size
+  }
+
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = {};
+    }
+    this.table[index][key] = value;
+  }
+
+  // Retrieve a value from the hash table using a key
+  get(key) {
+    const index = this.hash(key);
+    if (this.table[index] && this.table[index][key]) {
+      return this.table[index][key];
+    }
+    return null;
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table[index] && this.table[index][key]) {
+      delete this.table[index][key];
+    }
+  }
+}
+
+// Usage:
+const hashTable = new HashTable();
+hashTable.insert("name", "John");
+hashTable.insert("age", 25);
+console.log(hashTable.get("name")); // Output: John
+console.log(hashTable.get("age")); // Output: 25
+hashTable.remove("age");
+console.log(hashTable.get("age")); // Output: null
