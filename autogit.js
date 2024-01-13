a07d9fd4417b98710833dcdd983c8dfe8a9b412a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:59:00 +0800

    1-13-2024 14:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,4 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const middle = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, middle));
-  const right = mergeSort(arr.slice(middle));
-
-  return merge(left, right);
-}
-
-function merge(left, right) {
-  let result = [];
-  let i = 0;
-  let j = 0;
-
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i]);
-      i++;
-    } else {
-      result.push(right[j]);
-      j++;
-    }
-  }
-
-  while (i < left.length) {
-    result.push(left[i]);
-    i++;
-  }
-
-  while (j < right.length) {
-    result.push(right[j]);
-    j++;
-  }
-
-  return result;
-}
-
-// Testing the mergeSort function
-const arr = [8, 4, 9, 2, 5, 1, 6, 3, 7];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr);
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
