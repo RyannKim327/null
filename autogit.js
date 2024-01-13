33c12295cd39f73c9e124a3957b5b6c046fb556a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:20:52 +0800

    1-13-2024 14:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,118 +1,50 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+// Create a graph class
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.adjList = new Map();
+    for (let v of vertices) {
+      this.adjList.set(v, []);
+    }
+  }
+
+  // Add an edge to the graph
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+    this.adjList.get(w).push(v);
+  }
+
+  // Depth-First Search traversal
+  dfs(startingNode) {
+    let visited = {};
+    this._dfsUtil(startingNode, visited);
+  }
+
+  // Recursive utility function for DFS
+  _dfsUtil(vertex, visited) {
+    visited[vertex] = true;
+    console.log(vertex);
+
+    let neighbors = this.adjList.get(vertex);
+    for (let neighbor of neighbors) {
+      if (!visited[neighbor]) {
+        this._dfsUtil(neighbor, visited);
+      }
+    }
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+// Test the depth-first search algorithm
+let vertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+let graph = new Graph(vertices);
 
-  // Add a new node to the end of the list
-  append(data) {
-    const newNode = new Node(data);
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('B', 'E');
+graph.addEdge('C', 'F');
+graph.addEdge('C', 'G');
+graph.addEdge('E', 'H');
 
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  // Insert a new node at a specified position
-  insertAt(data, position) {
-    const newNode = new Node(data);
-
-    if (position === 0) {
-      newNode.next = this.head;
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      let previous = null;
-      let count = 0;
-
-      while (count < position && current) {
-        previous = current;
-        current = current.next;
-        count++;
-      }
-
-      newNode.next = current;
-      previous.next = newNode;
-    }
-  }
-
-  // Remove a node at a specified position
-  removeAt(position) {
-    if (!this.head) {
-      return;
-    }
-
-    let current = this.head;
-    let previous = null;
-    let count = 0;
-
-    if (position === 0) {
-      this.head = current.next;
-    } else {
-      while (count < position && current) {
-        previous = current;
-        current = current.next;
-        count++;
-      }
-  
-      if (!current) {
-        return;
-      }
-  
-      previous.next = current.next;
-    }
-  }
-
-  // Get the size of the linked list
-  size() {
-    let count = 0;
-    let current = this.head;
-
-    while (current) {
-      count++;
-      current = current.next;
-    }
-
-    return count;
-  }
-
-  // Convert the linked list to an array
-  toArray() {
-    const array = [];
-    let current = this.head;
-
-    while (current) {
-      array.push(current.data);
-      current = current.next;
-    }
-
-    return array;
-  }
-}
-const list = new LinkedList();
-
-list.append(10);
-list.append(20);
-list.append(30);
-list.insertAt(15, 1);
-list.insertAt(25, 4);
-
-console.log(list.toArray()); // [10, 15, 20, 25, 30]
-console.log(list.size()); // 5
-
-list.removeAt(2);
-
-console.log(list.toArray()); // [10, 15, 25, 30]
-console.log(list.size()); // 4
+console.log('Depth-First Traversal:');
+graph.dfs('A');
