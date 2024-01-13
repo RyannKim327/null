d80fb2809b747976831ee8aa837d2dfec4b8c639
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:22:38 +0800

    1-13-2024 14:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,28 @@
-function findMiddleElement(head) {
-  let slow = head;
-  let fast = head;
-
-  while (fast !== null && fast.next !== null) {
-    slow = slow.next;
-    fast = fast.next.next;
+function interpolationSearch(arr, target, start = 0) {
+  if (start > arr.length - 1) {
+    return -1; // Target value not found
   }
 
-  return slow.value;
+  let pos = start + Math.floor(
+    (target - arr[start]) * (arr.length - 1 - start) / (arr[arr.length - 1] - arr[start])
+  );
+
+  if (arr[pos] === target) {
+    return pos; // Target value found
+  }
+
+  if (arr[pos] > target) {
+    return interpolationSearch(arr, target, pos + 1);
+  }
+
+  if (arr[pos] < target) {
+    return interpolationSearch(arr, target, pos - 1);
+  }
+
+  return -1; // Target value not found
 }
+const arr = [1, 4, 8, 12, 16, 20, 24, 28, 32, 36];
+const target = 16;
+
+const result = interpolationSearch(arr, target);
+console.log(`Target value found at index ${result}`);
