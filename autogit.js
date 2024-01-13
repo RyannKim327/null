ada56143273dd690d0ef43724afbf41cde93cbe4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:26:18 +0800

    1-13-2024 14:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,62 @@
-let string1 = "Hello";
-let string2 = "World";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-let concatenatedString = string1 + " " + string2;
-console.log(concatenatedString);  // Output: "Hello World"
-let string1 = "Hello";
-let string2 = "World";
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-let concatenatedString = string1.concat(" ", string2);
-console.log(concatenatedString);  // Output: "Hello World"
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+    
+    let current = this.root;
+    while (true) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  search(value) {
+    let current = this.root;
+    
+    while (current && current.value !== value) {
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    
+    return current;
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(5);
+bst.insert(3);
+bst.insert(7);
+bst.insert(1);
+bst.insert(4);
+
+console.log(bst.search(4)); // Output: Node { value: 4, left: null, right: null }
+console.log(bst.search(6)); // Output: null
