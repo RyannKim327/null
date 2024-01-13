8b65927cedeb65ab7023132bab9f360ce316fca5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:41:40 +0800

    1-13-2024 14:41 [User mode]

--- autogit.js
+++
@@ -1,6 +0,0 @@
-let str = "Hello, world! This is a string with spaces.";
-
-// Remove all spaces using regular expression
-let stringWithoutSpaces = str.replace(/\s/g, "");
-
-console.log(stringWithoutSpaces); // Output: "Hello,world!Thisisastringwithspaces."
