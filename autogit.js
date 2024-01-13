62fbed231dffe22bd31252d82de6bdaa3a0a08f8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 15:08:23 +0800

    1-13-2024 15:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,14 @@
-let str = 'Hello, World!';
+// API endpoint URL
+const apiUrl = 'https://api.example.com/data';
 
-let reversedString = str.split('').reverse().join('');
-console.log(reversedString); // Output: "!dlroW ,olleH"
-let str = 'Hello, World!';
-let reversedString = '';
-
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedString += str[i];
-}
-
-console.log(reversedString); // Output: "!dlroW ,olleH"
-function reverseString(str) {
-  if (str === '') {
-    return '';
-  } else {
-    return reverseString(str.substr(1)) + str.charAt(0);
-  }
-}
-
-let reversedString = reverseString('Hello, World!');
-console.log(reversedString); // Output: "!dlroW ,olleH"
+// Fetch data from the API
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    // Process the received data
+    console.log(data);
+  })
+  .catch((error) => {
+    // Handle any errors from the API request
+    console.error('Error:', error);
+  });
