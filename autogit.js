9632972f4c435aea13dc7fac793838162a11bbba
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:52:43 +0800

    1-13-2024 14:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,24 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-}
-reverse() {
-  let prev = null;
-  let current = this.head;
-  let next = null;
+function findLinkedListLength(head) {
+  let currentNode = head;
+  let length = 0;
 
-  while (current !== null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
+  while (currentNode !== null) {
+    length++;
+    currentNode = currentNode.next;
   }
 
-  this.head = prev;
+  return length;
 }
-const linkedList = new LinkedList();
-linkedList.insert(1);
-linkedList.insert(2);
-linkedList.insert(3);
-linkedList.insert(4);
-
-console.log("Before reverse:");
-linkedList.print(); // Output: 1 -> 2 -> 3 -> 4
-
-linkedList.reverse();
-
-console.log("After reverse:");
-linkedList.print(); // Output: 4 -> 3 -> 2 -> 1
-print() {
-  let current = this.head;
-  let values = [];
-  while (current !== null) {
-    values.push(current.data);
-    current = current.next;
+// Example linked list
+let head = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: null
+    }
   }
-  console.log(values.join(' -> '));
-}
+};
+
+console.log(findLinkedListLength(head)); // Output: 3
