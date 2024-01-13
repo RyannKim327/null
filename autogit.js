06f303c46d06060fee61e945d8462f7a1d443978
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:53:01 +0800

    1-13-2024 14:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,10 @@
-function findLinkedListLength(head) {
-  let currentNode = head;
-  let length = 0;
-
-  while (currentNode !== null) {
-    length++;
-    currentNode = currentNode.next;
+function getStringLength(str) {
+  var count = 0;
+  while (str[count] !== undefined) {
+    count++;
   }
-
-  return length;
+  return count;
 }
-// Example linked list
-let head = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: null
-    }
-  }
-};
-
-console.log(findLinkedListLength(head)); // Output: 3
+var myString = "Hello, world!";
+var length = getStringLength(myString);
+console.log(length); // Outputs: 13
