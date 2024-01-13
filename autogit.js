b83c2c9bce07cdaa6b878ef68cdf3979231c61b2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:26:37 +0800

    1-13-2024 14:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,8 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(value) {
-    const newNode = new Node(value);
-    
-    if (!this.root) {
-      this.root = newNode;
-      return;
-    }
-    
-    let current = this.root;
-    while (true) {
-      if (value < current.value) {
-        if (!current.left) {
-          current.left = newNode;
-          return;
-        }
-        current = current.left;
-      } else {
-        if (!current.right) {
-          current.right = newNode;
-          return;
-        }
-        current = current.right;
-      }
-    }
-  }
-
-  search(value) {
-    let current = this.root;
-    
-    while (current && current.value !== value) {
-      if (value < current.value) {
-        current = current.left;
-      } else {
-        current = current.right;
-      }
-    }
-    
-    return current;
-  }
-}
-const bst = new BinarySearchTree();
-bst.insert(5);
-bst.insert(3);
-bst.insert(7);
-bst.insert(1);
-bst.insert(4);
-
-console.log(bst.search(4)); // Output: Node { value: 4, left: null, right: null }
-console.log(bst.search(6)); // Output: null
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
