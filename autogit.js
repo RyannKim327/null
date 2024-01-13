7726517c966443d5f20146ab50f4c6fbd65846c7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:17:26 +0800

    1-13-2024 14:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,62 @@
-function findMaxSubarraySum(array) {
-  let maxSoFar = array[0];
-  let maxEndingHere = array[0];
-
-  for (let i = 1; i < array.length; i++) {
-    maxEndingHere = Math.max(maxEndingHere + array[i], array[i]);
-    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+// Define a Graph class representing the graph structure
+class Graph {
+  constructor() {
+    this.nodes = [];
+    this.adjList = {};
   }
 
-  return maxSoFar;
+  addNode(node) {
+    this.nodes.push(node);
+    this.adjList[node] = [];
+  }
+
+  addEdge(node1, node2) {
+    this.adjList[node1].push(node2);
+    this.adjList[node2].push(node1);
+  }
+
+  getNeighbors(node) {
+    return this.adjList[node];
+  }
+}
+
+// Perform Breadth-First Search on a graph
+function breadthFirstSearch(graph, startNode) {
+  const visited = new Set(); // Track visited nodes
+  const queue = []; // Queue for BFS
+
+  visited.add(startNode);
+  queue.push(startNode);
+
+  while (queue.length > 0) {
+    const currentNode = queue.shift();
+    console.log(currentNode);
+
+    const neighbors = graph.getNeighbors(currentNode);
+
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push(neighbor);
+      }
+    }
+  }
 }
 
 // Example usage:
-const array = [-2, -3, 4, -1, -2, 1, 5, -3];
-const maxSum = findMaxSubarraySum(array);
-console.log(maxSum);  // Output: 7
+const graph = new Graph();
+
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addNode('D');
+graph.addNode('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+
+breadthFirstSearch(graph, 'A');
