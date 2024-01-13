d192a7ccca35d5df75a30710be914a2ea33620a7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:25:43 +0800

    1-13-2024 14:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,10 @@
-function longestCommonSubstring(str1, str2) {
-  const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
-  let maxLen = 0;
-  let endIndex = 0;
+let string1 = "Hello";
+let string2 = "World";
 
-  for (let ch1 = 1; ch1 <= str1.length; ch1++) {
-    for (let ch2 = 1; ch2 <= str2.length; ch2++) {
-      if (str1[ch1 - 1] === str2[ch2 - 1]) {
-        dp[ch1][ch2] = dp[ch1 - 1][ch2 - 1] + 1;
-        if (dp[ch1][ch2] > maxLen) {
-          maxLen = dp[ch1][ch2];
-          endIndex = ch1;
-        }
-      }
-    }
-  }
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString);  // Output: "Hello World"
+let string1 = "Hello";
+let string2 = "World";
 
-  return str1.substring(endIndex - maxLen, endIndex);
-}
-
-// Example usage:
-const str1 = "abcde";
-const str2 = "bcdf";
-const longestSubstring = longestCommonSubstring(str1, str2);
-console.log(longestSubstring); // Output: "bcd"
+let concatenatedString = string1.concat(" ", string2);
+console.log(concatenatedString);  // Output: "Hello World"
