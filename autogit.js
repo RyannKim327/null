ef11ffebfa2e632c6c5f288df197141d1b8ac97b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:34:12 +0800

    1-13-2024 14:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,17 @@
-function longestCommonSubstring(str1, str2) {
-  // Create the matrix to store lengths of the common substrings
-  const matrix = Array(str1.length + 1)
-    .fill(0)
-    .map(() => Array(str2.length + 1).fill(0));
+function reverseWords(str) {
+  // Step 1: Split the string into an array of words
+  var wordsArr = str.split(" ");
 
-  // Variable to store the length of the longest common substring
-  let longestLength = 0;
+  // Step 2: Reverse the order of the array
+  var reversedWordsArr = wordsArr.reverse();
 
-  // Variable to store the ending index of the longest common substring
-  let endIndex = -1;
+  // Step 3: Join the array elements into a string
+  var reversedStr = reversedWordsArr.join(" ");
 
-  // Loop to fill in the matrix
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-
-        // Check if the current length is the longest
-        if (matrix[i][j] > longestLength) {
-          longestLength = matrix[i][j];
-          endIndex = i - 1; // Update the ending index
-        }
-      } else {
-        matrix[i][j] = 0;
-      }
-    }
-  }
-
-  // Extract the longest common substring
-  const longestSubstring = str1.substring(endIndex - longestLength + 1, endIndex + 1);
-  return longestSubstring;
+  return reversedStr;
 }
 
 // Example usage
-const str1 = "abcdefg";
-const str2 = "cdefhij";
-const result = longestCommonSubstring(str1, str2);
-console.log(result); // Output: "cdef"
+var inputString = "Hello World!";
+var reversedString = reverseWords(inputString);
+console.log(reversedString);  // Output: "World! Hello"
