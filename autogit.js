9aeffd72c2dfc7af55cfcfccb2f0b0c40d003c25
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:51:31 +0800

    1-13-2024 14:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,16 @@
-function areAnagrams(str1, str2) {
-  // Convert the strings to lowercase and remove whitespace
-  str1 = str1.toLowerCase().replace(/\s/g, '');
-  str2 = str2.toLowerCase().replace(/\s/g, '');
-  
-  // Sort the strings
-  const sortedStr1 = str1.split('').sort().join('');
-  const sortedStr2 = str2.split('').sort().join('');
-  
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
-}
+const str = "Hello, World!";
+const substring = "Hello";
 
-// Test the function
-const string1 = 'listen';
-const string2 = 'silent';
-console.log(areAnagrams(string1, string2)); // Output: true
+if (str.includes(substring)) {
+  console.log("Substring found");
+} else {
+  console.log("Substring not found");
+}
+const str = "Hello, World!";
+const substring = "Hello";
+
+if (str.indexOf(substring) !== -1) {
+  console.log("Substring found");
+} else {
+  console.log("Substring not found");
+}
