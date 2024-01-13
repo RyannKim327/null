539e76a8d3d36c56ffff8f6a9e1663ba3da29ea6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 15:08:42 +0800

    1-13-2024 15:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,17 @@
-// API endpoint URL
-const apiUrl = 'https://api.example.com/data';
+function factorial(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
 
-// Fetch data from the API
-fetch(apiUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    // Process the received data
-    console.log(data);
-  })
-  .catch((error) => {
-    // Handle any errors from the API request
-    console.error('Error:', error);
-  });
+console.log(factorial(5)); // Output: 120
+function factorial(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5)); // Output: 120
