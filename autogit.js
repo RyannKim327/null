a463ff0e94da2913fc2482dfdbe8eb3827491c31
RyannKim327 <rksesgundo123@gmail.com>
2024-01-13 14:36:06 +0800

    1-13-2024 14:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,4 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters (optional)
-  str = str.replace(/[^A-Za-z0-9]/g, '');
+const array = [5, 2, 9, 1, 7, 4];
 
-  // Convert to lowercase (optional)
-  str = str.toLowerCase();
-
-  // Reverse the string
-  var reversedStr = str.split('').reverse().join('');
-  
-  // Compare the reversed string with the original string
-  if (str === reversedStr) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// Example usage
-console.log(isPalindrome('Madam')); // Output: true
-console.log(isPalindrome('Racecar')); // Output: true
-console.log(isPalindrome('Hello')); // Output: false
+const max = Math.max(...array);
+console.log(max); // Output: 9
