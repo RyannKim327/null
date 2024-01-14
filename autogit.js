48c4cb6af3c76ab15879030c8159322dd2e95605
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:02:59 +0800

    1-14-2024 11:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,41 @@
-function findMajorityElement(nums) {
-  let majority = null;
-  let count = 0;
-
-  for (let num of nums) {
-    if (count === 0) {
-      majority = num;
-      count++;
-    } else if (num === majority) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Verify if the majority element is indeed the majority
-  count = 0;
-  for (let num of nums) {
-    if (num === majority) {
-      count++;
-    }
-  }
-
-  if (count > nums.length / 2) {
-    return majority;
-  } else {
-    return null; // No majority element found
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
 
-// Example Usage
-const nums = [1, 3, 3, 3, 2, 2, 3, 3, 4, 4, 4, 4, 4];
-const majorityElement = findMajorityElement(nums);
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
 
-console.log(`The majority element is: ${majorityElement}`);
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    // If the fast pointer catches up with the slow pointer, a cycle is present
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false; // If we reach null, there is no cycle
+}
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = null;
+
+console.log(hasCycle(node1)); // Output: false
+
+// Create a cycle in the linked list
+node5.next = node2;
+
+console.log(hasCycle(node1)); // Output: true
