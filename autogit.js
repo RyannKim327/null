2ad0ed876571fe96fe49a3b9d58b6a49ea221b92
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:04:05 +0800

    1-14-2024 11:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,87 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
-  const alphanumericStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
-  
-  // Compare the original string with its reversed version
-  return alphanumericStr === alphanumericStr.split('').reverse().join('');
+class SkipListNode {
+  constructor(value, level) {
+    this.value = value;
+    this.next = new Array(level + 1).fill(null);
+  }
 }
+class SkipList {
+  constructor() {
+    this.head = new SkipListNode(-Infinity, 0);
+    this.level = 0;
+  }
+}
+class SkipList {
+  // ... existing code
 
-// Test the function
-const inputString = 'A man, a plan, a canal, Panama!';
-console.log(isPalindrome(inputString)); // Output: true
+  search(target) {
+    let currentNode = this.head;
+    for (let i = this.level; i >= 0; i--) {
+      while (currentNode.next[i] && currentNode.next[i].value < target) {
+        currentNode = currentNode.next[i];
+      }
+    }
+    currentNode = currentNode.next[0];
+    if (currentNode && currentNode.value === target) {
+      return currentNode;
+    }
+    return null;
+  }
+
+  insert(value) {
+    const update = new Array(this.level + 1);
+    let currentNode = this.head;
+    for (let i = this.level; i >= 0; i--) {
+      while (currentNode.next[i] && currentNode.next[i].value < value) {
+        currentNode = currentNode.next[i];
+      }
+      update[i] = currentNode;
+    }
+    currentNode = currentNode.next[0];
+    if (!currentNode || currentNode.value !== value) {
+      const newNode = new SkipListNode(value, this.randomLevel());
+      if (newNode.level > this.level) {
+        for (let i = this.level + 1; i <= newNode.level; i++) {
+          update[i] = this.head;
+        }
+        this.level = newNode.level;
+      }
+      for (let i = 0; i <= newNode.level; i++) {
+        newNode.next[i] = update[i].next[i];
+        update[i].next[i] = newNode;
+      }
+    }
+  }
+
+  delete(value) {
+    const update = new Array(this.level + 1);
+    let currentNode = this.head;
+    for (let i = this.level; i >= 0; i--) {
+      while (currentNode.next[i] && currentNode.next[i].value < value) {
+        currentNode = currentNode.next[i];
+      }
+      update[i] = currentNode;
+    }
+    currentNode = currentNode.next[0];
+    if (currentNode && currentNode.value === value) {
+      for (let i = 0; i <= this.level; i++) {
+        if (update[i].next[i] !== currentNode) break;
+        update[i].next[i] = currentNode.next[i];
+      }
+    }
+  }
+
+  // ... existing code
+}
+class SkipList {
+  // ... existing code
+
+  randomLevel() {
+    let level = 0;
+    while (Math.random() < 0.5 && level < this.level + 1) {
+      level++;
+    }
+    return level;
+  }
+
+  // ... existing code
+}
