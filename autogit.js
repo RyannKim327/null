b9b4baab2c2d2b805e6bf52c2dc0f66249a47a2d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:09:03 +0800

    1-14-2024 11:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,109 +1,44 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false; // If the list is empty or has only one node, there can't be a cycle
+  }
+
+  let tortoise = head;
+  let hare = head;
+
+  while (hare && hare.next) {
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+
+    if (tortoise === hare) {
+      return true; // The pointers meet, indicating a cycle
+    }
+  }
+
+  return false; // The hare reaches the end of the list without meeting the tortoise, indicating no cycle
+}
+// Define the linked list with the nodes
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
 
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
+// Create the linked list
+const head = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
 
-  insert(value) {
-    const newNode = new Node(value);
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
-    if (!this.root) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
+// Create a cycle in the linked list
+node5.next = node2;
 
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (!node.left) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (!node.right) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  contains(value) {
-    return this.containsNode(this.root, value);
-  }
-
-  containsNode(node, value) {
-    if (!node) {
-      return false;
-    }
-
-    if (value === node.value) {
-      return true;
-    } else if (value < node.value) {
-      return this.containsNode(node.left, value);
-    } else {
-      return this.containsNode(node.right, value);
-    }
-  }
-
-  remove(value) {
-    this.root = this.removeNode(this.root, value);
-  }
-
-  removeNode(node, value) {
-    if (!node) {
-      return null;
-    }
-
-    if (value === node.value) {
-      if (!node.left && !node.right) {
-        return null;
-      } else if (!node.left) {
-        return node.right;
-      } else if (!node.right) {
-        return node.left;
-      } else {
-        const temp = this.findMinNode(node.right);
-        node.value = temp.value;
-        node.right = this.removeNode(node.right, temp.value);
-        return node;
-      }
-    } else if (value < node.value) {
-      node.left = this.removeNode(node.left, value);
-      return node;
-    } else {
-      node.right = this.removeNode(node.right, value);
-      return node;
-    }
-  }
-
-  findMinNode(node) {
-    if (!node.left) {
-      return node;
-    } else {
-      return this.findMinNode(node.left);
-    }
-  }
-}
-const tree = new BinaryTree();
-
-tree.insert(10);
-tree.insert(5);
-tree.insert(15);
-tree.insert(2);
-tree.insert(7);
-
-console.log(tree.contains(7)); // Output: true
-console.log(tree.contains(20)); // Output: false
-
-tree.remove(5);
-console.log(tree.contains(5)); // Output: false
+// Check if the linked list contains a cycle
+console.log(hasCycle(head)); // Output: true
