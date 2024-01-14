b5025d921391c7a813239deb6d28ea757a81408c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:20:41 +0800

    1-14-2024 10:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,8 @@
-let str = "This is a sample string with spaces";
-
-// Using the replace() method with a regular expression
-let stringWithoutSpaces = str.replace(/\s/g, "");
-
-console.log(stringWithoutSpaces);
-Thisisasamplestringwithspaces
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
