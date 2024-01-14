a9ae6409381047067b85d362d6b51db882e598bf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:30:31 +0800

    1-14-2024 10:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,91 @@
-function findCommonElements(array1, array2) {
-  const commonElements = [];
-
-  array1.forEach(element => {
-    if (array2.includes(element)) {
-      commonElements.push(element);
-    }
-  });
-
-  return commonElements;
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-// Example usage:
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+  insert(value) {
+    const newNode = new TreeNode(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
 
-const result = findCommonElements(array1, array2);
-console.log(result); // Output: [4, 5]
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+    if (value === node.value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
+  inorderTraversal(callback) {
+    this.inorderTraversalNode(this.root, callback);
+  }
+
+  inorderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inorderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inorderTraversalNode(node.right, callback);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(4);
+bst.insert(2);
+bst.insert(1);
+bst.insert(3);
+bst.insert(6);
+bst.insert(5);
+bst.insert(7);
+
+console.log(bst.search(5)); // Output: true
+console.log(bst.search(8)); // Output: false
+
+bst.inorderTraversal((value) => {
+  console.log(value);
+});
+// Output:
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
