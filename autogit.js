af5b4e2ee3d60ffbf99b23d8d64f3b99d7bc346f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:20:09 +0800

    1-14-2024 10:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,7 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+let str = "This is a sample string with spaces";
 
-function findMiddleElement(head) {
-    let slowPtr = head;
-    let fastPtr = head;
+// Using the replace() method with a regular expression
+let stringWithoutSpaces = str.replace(/\s/g, "");
 
-    while (fastPtr !== null && fastPtr.next !== null) {
-        slowPtr = slowPtr.next;
-        fastPtr = fastPtr.next.next;
-    }
-
-    return slowPtr.value;
-}
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-const node5 = new Node(5);
-
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-
-const middleElement = findMiddleElement(node1);
-console.log(middleElement); // Output: 3
+console.log(stringWithoutSpaces);
+Thisisasamplestringwithspaces
