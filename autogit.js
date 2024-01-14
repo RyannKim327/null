909086e96eb83c57f84cb733fd46d3089053c12a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:51:01 +0800

    1-14-2024 10:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,74 @@
-function findSecondLargest(arr) {
-  let largest = arr[0];
-  let secondLargest = arr[1];
+class Node {
+  constructor(value, level) {
+    this.value = value;
+    this.next = new Array(level);
+  }
+}
+class SkipList {
+  constructor() {
+    this.head = new Node(null, 1);
+    this.level = 1;
+    this.maxLevel = 16; // maximum number of levels in the skip list
+    this.min = Number.MIN_VALUE; // minimum possible value
+    this.max = Number.MAX_VALUE; // maximum possible value
+  }
+}
+class SkipList {
+  ...
 
-  for (let i = 2; i < arr.length; i++) {
-    if (arr[i] > largest) {
-      secondLargest = largest;
-      largest = arr[i];
-    } else if (arr[i] < largest && arr[i] > secondLargest) {
-      secondLargest = arr[i];
+  insert(value) {
+    const update = new Array(this.maxLevel);
+    let current = this.head;
+
+    for (let i = this.level - 1; i >= 0; i--) {
+      while (current.next[i] && current.next[i].value < value) {
+        current = current.next[i];
+      }
+      update[i] = current;
+    }
+
+    const newNode = new Node(value, this.randomLevel());
+    if (newNode.level > this.level) {
+      for (let i = this.level; i < newNode.level; i++) {
+        update[i] = this.head;
+      }
+      this.level = newNode.level;
+    }
+
+    for (let i = 0; i < newNode.level; i++) {
+      newNode.next[i] = update[i].next[i];
+      update[i].next[i] = newNode;
     }
   }
 
-  return secondLargest;
+  randomLevel() {
+    let level = 1;
+    while (Math.random() < 0.5 && level < this.maxLevel) {
+      level++;
+    }
+    return level;
+  }
 }
+class SkipList {
+  ...
 
-// Test the function
-const arr = [5, 10, 2, 8, 15];
-console.log(findSecondLargest(arr)); // Output: 10
+  search(value) {
+    let current = this.head;
+    for (let i = this.level - 1; i >= 0; i--) {
+      while (current.next[i] && current.next[i].value < value) {
+        current = current.next[i];
+      }
+    }
+    current = current.next[0];
+    if (current && current.value === value) {
+      return true;
+    }
+    return false;
+  }
+}
+const skipList = new SkipList();
+skipList.insert(3);
+skipList.insert(7);
+skipList.insert(4);
+console.log(skipList.search(7)); // Output: true
+console.log(skipList.search(5)); // Output: false
