e35f7a7c008f6198893c15854a080cdc2de0754b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:51:22 +0800

    1-14-2024 10:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,74 +1,16 @@
-class Node {
-  constructor(value, level) {
-    this.value = value;
-    this.next = new Array(level);
-  }
+let array = [1, 2, 3, 4, 5];
+let index = array.indexOf(3); // Find the index of the element you want to remove
+if (index !== -1) {
+  array.splice(index, 1); // Remove the element at the specified index
 }
-class SkipList {
-  constructor() {
-    this.head = new Node(null, 1);
-    this.level = 1;
-    this.maxLevel = 16; // maximum number of levels in the skip list
-    this.min = Number.MIN_VALUE; // minimum possible value
-    this.max = Number.MAX_VALUE; // maximum possible value
-  }
+console.log(array); // Output: [1, 2, 4, 5]
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+array = array.filter(item => item !== elementToRemove);
+console.log(array); // Output: [1, 2, 4, 5]
+let array = [1, 2, 3, 4, 5];
+let index = array.indexOf(3); // Find the index of the element you want to remove
+if (index !== -1) {
+  array = [...array.slice(0, index), ...array.slice(index + 1)];
 }
-class SkipList {
-  ...
-
-  insert(value) {
-    const update = new Array(this.maxLevel);
-    let current = this.head;
-
-    for (let i = this.level - 1; i >= 0; i--) {
-      while (current.next[i] && current.next[i].value < value) {
-        current = current.next[i];
-      }
-      update[i] = current;
-    }
-
-    const newNode = new Node(value, this.randomLevel());
-    if (newNode.level > this.level) {
-      for (let i = this.level; i < newNode.level; i++) {
-        update[i] = this.head;
-      }
-      this.level = newNode.level;
-    }
-
-    for (let i = 0; i < newNode.level; i++) {
-      newNode.next[i] = update[i].next[i];
-      update[i].next[i] = newNode;
-    }
-  }
-
-  randomLevel() {
-    let level = 1;
-    while (Math.random() < 0.5 && level < this.maxLevel) {
-      level++;
-    }
-    return level;
-  }
-}
-class SkipList {
-  ...
-
-  search(value) {
-    let current = this.head;
-    for (let i = this.level - 1; i >= 0; i--) {
-      while (current.next[i] && current.next[i].value < value) {
-        current = current.next[i];
-      }
-    }
-    current = current.next[0];
-    if (current && current.value === value) {
-      return true;
-    }
-    return false;
-  }
-}
-const skipList = new SkipList();
-skipList.insert(3);
-skipList.insert(7);
-skipList.insert(4);
-console.log(skipList.search(7)); // Output: true
-console.log(skipList.search(5)); // Output: false
+console.log(array); // Output: [1, 2, 4, 5]
