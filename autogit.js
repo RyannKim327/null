b45df5d705ddde79ed26f3207325e66348abf284
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 12:00:49 +0800

    1-14-2024 12:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,13 @@
-const cron = require('node-cron');
+function isArraySortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false; // If any element is greater than the next element, the array is not sorted in ascending order
+    }
+  }
+  return true; // If no such elements are found, the array is sorted in ascending order
+}
+const myArray = [1, 2, 3, 4, 5];
+console.log(isArraySortedAscending(myArray)); // Output: true
 
-// Schedule a task to run every minute
-cron.schedule('* * * * *', () => {
-  console.log('Running a task every minute');
-  // Add your desired code here
-});
-
-// Schedule a task to run at 9:30 AM every day
-cron.schedule('30 9 * * *', () => {
-  console.log('Running a task at 9:30 AM every day');
-  // Add your desired code here
-});
-
-// Schedule a task to run on Sundays at 5 PM
-cron.schedule('0 17 * * 0', () => {
-  console.log('Running a task on Sundays at 5 PM');
-  // Add your desired code here
-});
+const anotherArray = [5, 2, 8, 1, 3];
+console.log(isArraySortedAscending(anotherArray)); // Output: false
