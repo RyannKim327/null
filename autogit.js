0c9328b90fb013e360627dab0351c299e9c4d019
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:30:59 +0800

    1-14-2024 10:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,91 +1,156 @@
-class TreeNode {
-  constructor(value) {
+class Node {
+  constructor(value, color) {
     this.value = value;
     this.left = null;
     this.right = null;
+    this.parent = null;
+    this.color = color;
   }
 }
-class BinarySearchTree {
+class RedBlackTree {
   constructor() {
     this.root = null;
   }
-
-  insert(value) {
-    const newNode = new TreeNode(value);
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    }
-    if (value === node.value) {
-      return true;
-    }
-
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    } else {
-      return this.searchNode(node.right, value);
-    }
-  }
-
-  inorderTraversal(callback) {
-    this.inorderTraversalNode(this.root, callback);
-  }
-
-  inorderTraversalNode(node, callback) {
-    if (node !== null) {
-      this.inorderTraversalNode(node.left, callback);
-      callback(node.value);
-      this.inorderTraversalNode(node.right, callback);
-    }
-  }
 }
-const bst = new BinarySearchTree();
-bst.insert(4);
-bst.insert(2);
-bst.insert(1);
-bst.insert(3);
-bst.insert(6);
-bst.insert(5);
-bst.insert(7);
+RedBlackTree.prototype.insert = function (value) {
+  const newNode = new Node(value, "red");
 
-console.log(bst.search(5)); // Output: true
-console.log(bst.search(8)); // Output: false
+  if (!this.root) {
+    newNode.color = "black";
+    this.root = newNode;
+  } else {
+    this.insertNode(this.root, newNode);
+    this.fixViolation(newNode);
+  }
+};
 
-bst.inorderTraversal((value) => {
-  console.log(value);
-});
-// Output:
-// 1
-// 2
-// 3
-// 4
-// 5
-// 6
-// 7
+RedBlackTree.prototype.insertNode = function (node, newNode) {
+  if (newNode.value < node.value) {
+    if (node.left === null) {
+      node.left = newNode;
+      newNode.parent = node;
+    } else {
+      this.insertNode(node.left, newNode);
+    }
+  } else {
+    if (node.right === null) {
+      node.right = newNode;
+      newNode.parent = node;
+    } else {
+      this.insertNode(node.right, newNode);
+    }
+  }
+};
+
+RedBlackTree.prototype.fixViolation = function (node) {
+  while (
+    node !== this.root &&
+    node.parent.color === "red" &&
+    node.color !== "black"
+  ) {
+    const parent = node.parent;
+    const grandparent = node.parent.parent;
+
+    if (parent === grandparent.left) {
+      const uncle = grandparent.right;
+
+      if (uncle !== null && uncle.color === "red") {
+        // Case 1: Uncle is red
+        grandparent.color = "red";
+        parent.color = "black";
+        uncle.color = "black";
+        node = grandparent;
+      } else {
+        if (node === parent.right) {
+          // Case 2: Uncle is black and node is a right child
+          this.rotateLeft(parent);
+          node = parent;
+          parent = node.parent;
+        }
+
+        // Case 3: Uncle is black and node is a left child
+        this.rotateRight(grandparent);
+        const temp = parent.color;
+        parent.color = grandparent.color;
+        grandparent.color = temp;
+        node = parent;
+      }
+    } else {
+      const uncle = grandparent.left;
+
+      if (uncle !== null && uncle.color === "red") {
+        // Case 1: Uncle is red
+        grandparent.color = "red";
+        parent.color = "black";
+        uncle.color = "black";
+        node = grandparent;
+      } else {
+        if (node === parent.left) {
+          // Case 2: Uncle is black and node is a left child
+          this.rotateRight(parent);
+          node = parent;
+          parent = node.parent;
+        }
+
+        // Case 3: Uncle is black and node is a right child
+        this.rotateLeft(grandparent);
+        const temp = parent.color;
+        parent.color = grandparent.color;
+        grandparent.color = temp;
+        node = parent;
+      }
+    }
+  }
+
+  this.root.color = "black";
+};
+
+RedBlackTree.prototype.rotateLeft = function (node) {
+  const rightChild = node.right;
+  node.right = rightChild.left;
+  if (rightChild.left !== null) {
+    rightChild.left.parent = node;
+  }
+
+  rightChild.parent = node.parent;
+  if (node.parent === null) {
+    this.root = rightChild;
+  } else if (node === node.parent.left) {
+    node.parent.left = rightChild;
+  } else {
+    node.parent.right = rightChild;
+  }
+
+  rightChild.left = node;
+  node.parent = rightChild;
+};
+
+RedBlackTree.prototype.rotateRight = function (node) {
+  const leftChild = node.left;
+  node.left = leftChild.right;
+  if (leftChild.right !== null) {
+    leftChild.right.parent = node;
+  }
+
+  leftChild.parent = node.parent;
+  if (node.parent === null) {
+    this.root = leftChild;
+  } else if (node === node.parent.right) {
+    node.parent.right = leftChild;
+  } else {
+    node.parent.left = leftChild;
+  }
+
+  leftChild.right = node;
+  node.parent = leftChild;
+};
+const tree = new RedBlackTree();
+tree.insert(7);
+tree.insert(3);
+tree.insert(18);
+tree.insert(10);
+tree.insert(22);
+tree.insert(8);
+tree.insert(11);
+
+console.log(tree.root);
