f6cc8b27a1d7cb0b1d26fa5e92150a3a09ca2013
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:43:23 +0800

    1-14-2024 11:43 [User mode]

---
+++ autogit.js
@@ -0,0 +1,8 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
