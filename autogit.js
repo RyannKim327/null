a20c78e3ee6e582fc75de32f23ba677da6a5e764
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:49:26 +0800

    1-14-2024 10:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,94 +1,12 @@
-function Graph() {
-  this.vertices = {};
-}
+let str = "  Hello,   World!   ";
 
-Graph.prototype.addVertex = function(name, edges) {
-  this.vertices[name] = edges;
-};
+// Remove all whitespace (spaces, tabs, newlines) from the string
+let strippedStr = str.replace(/\s/g, "");
 
-Graph.prototype.addEdge = function(vertex, edge, weight) {
-  this.vertices[vertex][edge] = weight;
-};
-function dijkstra(graph, startVertex) {
-  var distances = {};
-  var visited = {};
-  var queue = new PriorityQueue();
+console.log(strippedStr);  // Output: "Hello,World!"
+let str = "  Hello,   World!   ";
 
-  // Initialize distances and visited status for all vertices
-  for (var vertex in graph.vertices) {
-    distances[vertex] = Infinity;
-    visited[vertex] = false;
-  }
+// Remove leading and trailing whitespace from the string
+let trimmedStr = str.trim();
 
-  // Set the distance to the start vertex as 0
-  distances[startVertex] = 0;
-
-  // Enqueue the start vertex with priority 0
-  queue.enqueue(startVertex, 0);
-
-  while (!queue.isEmpty()) {
-    var currentVertex = queue.dequeue().element;
-    visited[currentVertex] = true;
-
-    var neighbors = graph.vertices[currentVertex];
-
-    for (var neighbor in neighbors) {
-      var distance = distances[currentVertex] + neighbors[neighbor];
-
-      if (distance < distances[neighbor]) {
-        distances[neighbor] = distance;
-      }
-
-      if (!visited[neighbor]) {
-        queue.enqueue(neighbor, distances[neighbor]);
-        visited[neighbor] = true;
-      }
-    }
-  }
-
-  return distances;
-}
-function PriorityQueue() {
-  this.elements = [];
-}
-
-PriorityQueue.prototype.enqueue = function(element, priority) {
-  var queueElement = { element: element, priority: priority };
-
-  if (this.isEmpty()) {
-    this.elements.push(queueElement);
-  } else {
-    var added = false;
-    for (var i = 0; i < this.elements.length; i++) {
-      if (queueElement.priority < this.elements[i].priority) {
-        this.elements.splice(i, 0, queueElement);
-        added = true;
-        break;
-      }
-    }
-
-    if (!added) {
-      this.elements.push(queueElement);
-    }
-  }
-};
-
-PriorityQueue.prototype.dequeue = function() {
-  return this.elements.shift();
-};
-
-PriorityQueue.prototype.isEmpty = function() {
-  return this.elements.length === 0;
-};
-var graph = new Graph();
-
-graph.addVertex('A', { B: 4, C: 2 });
-graph.addVertex('B', { A: 4, C: 1, D: 5 });
-graph.addVertex('C', { A: 2, B: 1, D: 8 });
-graph.addVertex('D', { B: 5, C: 8, E: 2 });
-graph.addVertex('E', { D: 2 });
-
-var startVertex = 'A';
-var shortestPaths = dijkstra(graph, startVertex);
-
-console.log(shortestPaths);
+console.log(trimmedStr);  // Output: "Hello,   World!"
