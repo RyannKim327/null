b9cf432f6a5967da08eb7356a7b33e9d08b5b385
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:41:49 +0800

    1-14-2024 10:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,16 @@
-function Graph() {
-  this.nodes = {};
-}
-Graph.prototype.addNode = function(node, neighbors) {
-  this.nodes[node] = neighbors;
-};
-function dijkstra(graph, startNode) {
-  var distances = {};
-  distances[startNode] = 0;
-  
-  // Set all other distances to infinity
-  for (var node in graph.nodes) {
-    if (node !== startNode) {
-      distances[node] = Infinity;
-    }
-  }
-}
-function findMinNode(distances, visited) {
-  var minNode = null;
-  
-  for (var node in distances) {
-    if (!visited[node] && (minNode === null || distances[node] < distances[minNode])) {
-      minNode = node;
-    }
-  }
-  
-  return minNode;
-}
-var visited = {};
-var currentNode = startNode;
+const str = 'Hello, World!';
+const substring = 'World';
 
-while (currentNode) {
-  var neighbors = graph.nodes[currentNode];
-  
-  for (var neighbor in neighbors) {
-    var distance = distances[currentNode] + neighbors[neighbor];
-    if (distance < distances[neighbor]) {
-      distances[neighbor] = distance;
-    }
-  }
-  
-  visited[currentNode] = true;
-  currentNode = findMinNode(distances, visited);
+if (str.includes(substring)) {
+  console.log('Substring found!');
+} else {
+  console.log('Substring not found!');
 }
-var graph = new Graph();
-graph.addNode('A', { B: 4, C: 2 });
-graph.addNode('B', { C: 1, D: 5 });
-graph.addNode('C', { B: 2, D: 8 });
-graph.addNode('D', { E: 3 });
-graph.addNode('E', {});
+const str = 'Hello, World!';
+const substring = 'World';
 
-var startNode = 'A';
-var distances = dijkstra(graph, startNode);
-
-console.log(distances); // Output: { A: 0, B: 3, C: 2, D: 6, E: Infinity }
+if (str.indexOf(substring) !== -1) {
+  console.log('Substring found!');
+} else {
+  console.log('Substring not found!');
+}
