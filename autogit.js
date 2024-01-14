370cb6006a31c2776686a687e5f2106941f5ee25
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:45:45 +0800

    1-14-2024 10:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,53 @@
-function isArraySorted(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+function computePrefixTable(pattern) {
+  const prefixTable = [0];
+  let length = 0;
+  let i = 1;
+  
+  while (i < pattern.length) {
+    if (pattern[i] === pattern[length]) {
+      length++;
+      prefixTable[i] = length;
+      i++;
+    } else {
+      if (length !== 0) {
+        length = prefixTable[length - 1];
+      } else {
+        prefixTable[i] = 0;
+        i++;
+      }
     }
   }
-  return true;
+  
+  return prefixTable;
 }
-const array1 = [1, 2, 3, 4, 5];
-console.log(isArraySorted(array1)); // Output: true
+function KMP(text, pattern) {
+  const prefixTable = computePrefixTable(pattern);
+  const occurrences = [];
+  let i = 0;
+  let j = 0;
+  
+  while (i < text.length) {
+    if (pattern[j] === text[i]) {
+      i++;
+      j++;
+    }
+    
+    if (j === pattern.length) {
+      occurrences.push(i - j);
+      j = prefixTable[j - 1];
+    } else if (i < text.length && pattern[j] !== text[i]) {
+      if (j !== 0) {
+        j = prefixTable[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+  
+  return occurrences;
+}
+const text = "ABCABCDABABCDABCDABDE";
+const pattern = "ABCDABD";
 
-const array2 = [4, 2, 6, 1, 8];
-console.log(isArraySorted(array2)); // Output: false
+const occurrences = KMP(text, pattern);
+console.log(occurrences); // [11]
