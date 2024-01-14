408d68a0b557360f93c3ea0df1637309f38ff02d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:08:35 +0800

    1-14-2024 10:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,104 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  if (start <= end) {
-    const mid = Math.floor((start + end) / 2);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+enqueue(data) {
+  const newNode = new Node(data);
 
-    if (arr[mid] === target) {
-      return mid;
-    }
+  if (!this.head) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+}
+dequeue() {
+  if (!this.head) {
+    return null;
+  }
+  
+  const data = this.head.data;
+  this.head = this.head.next;
 
-    if (arr[mid] > target) {
-      return binarySearch(arr, target, start, mid - 1);
-    }
+  if (!this.head) {
+    this.tail = null;
+  }
 
-    if (arr[mid] < target) {
-      return binarySearch(arr, target, mid + 1, end);
+  return data;
+}
+peek() {
+  if (!this.head) {
+    return null;
+  }
+
+  return this.head.data;
+}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
 
-  return -1;
-}
-const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-const target = 12;
+  dequeue() {
+    if (!this.head) {
+      return null;
+    }
 
-const position = binarySearch(arr, target);
-console.log(`The target element is at index ${position}.`); // Output: The target element is at index 5.
+    const data = this.head.data;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null;
+    }
+
+    return data;
+  }
+
+  peek() {
+    if (!this.head) {
+      return null;
+    }
+
+    return this.head.data;
+  }
+}
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.peek()); // Output: 1
+
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 2
+
+console.log(queue.peek()); // Output: 3
