2453af6084d50394fc05c343c571cf77d4a05f57
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 12:06:11 +0800

    1-14-2024 12:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,145 +1,44 @@
-function generateLPS(pattern) {
-  const lps = [0];
-  let len = 0;
-  let i = 1;
+class Graph {
+  constructor() {
+    this.graph = {};
+  }
 
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
+  addEdge(src, dest) {
+    if (!this.graph[src]) {
+      this.graph[src] = [];
+    }
+    this.graph[src].push(dest);
+  }
+
+  breadthFirstSearch(startingNode) {
+    let visited = new Set();
+    let queue = [];
+
+    visited.add(startingNode);
+    queue.push(startingNode);
+
+    while (queue.length > 0) {
+      let currentNode = queue.shift();
+      console.log(currentNode);
+
+      let neighbors = this.graph[currentNode];
+      for (let neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          visited.add(neighbor);
+          queue.push(neighbor);
+        }
       }
     }
   }
-
-  return lps;
 }
 
-function KMPSearch(text, pattern) {
-  const lps = generateLPS(pattern);
-  const matches = [];
-  let i = 0;
-  let j = 0;
+// Example Usage:
 
-  while (i < text.length) {
-    if (text[i] === pattern[j]) {
-      i++;
-      j++;
-    }
-
-    if (j === pattern.length) {
-      matches.push(i - j);
-      j = lps[j - 1];
-    }
-    else if (i < text.length && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-
-  return matches;
-}
-
-// Example usage:
-const text = "ABCABABABA";
-const pattern = "ABA";
-const matches = KMPSearch(text, pattern);
-console.log(matches); // Output: [3, 5, 7]
-function generateBadCharShift(pattern) {
-  const badCharShift = {};
-  const patternLength = pattern.length;
-
-  for (let i = 0; i < patternLength; i++) {
-    badCharShift[pattern[i]] = patternLength - i - 1;
-  }
-
-  return badCharShift;
-}
-
-function generateSuffixes(pattern) {
-  const suff = [];
-  const patternLength = pattern.length;
-  let lastPrefixPosition = patternLength;
-
-  for (let i = patternLength - 1; i >= 0; i--) {
-    if (isPrefix(pattern, i + 1)) {
-      lastPrefixPosition = i + 1;
-    }
-    suff[patternLength - 1 - i] = lastPrefixPosition - i + patternLength - 1;
-  }
-
-  for (let i = 0; i < patternLength - 1; i++) {
-    const suffixLength = suffixLengthValue(pattern, i);
-    suff[suffixLength] = patternLength - 1 - i + suffixLength;
-  }
-
-  return suff;
-}
-
-function isPrefix(pattern, p) {
-  for (let i = p, j = 0; i < pattern.length; i++, j++) {
-    if (pattern[i] !== pattern[j]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function suffixLengthValue(pattern, p) {
-  let length = 0;
-  let i = p;
-  let j = pattern.length - 1;
-
-  while (i >= 0 && pattern[i] === pattern[j]) {
-    length++;
-    i--;
-    j--;
-  }
-
-  return length;
-}
-
-function BMISearch(text, pattern) {
-  const matches = [];
-  const badCharShift = generateBadCharShift(pattern);
-  const suffixes = generateSuffixes(pattern);
-  const textLength = text.length;
-  const patternLength = pattern.length;
-  let i = 0;
-
-  while (i <= textLength - patternLength) {
-    let j = patternLength - 1;
-
-    while (j >= 0 && pattern[j] === text[i + j]) {
-      j--;
-    }
-
-    if (j < 0) {
-      matches.push(i);
-      i += suffixes[0];
-    } else {
-      const shiftBadChar = badCharShift[text[i + j]] || patternLength;
-      const shiftGoodSuffix = suffixes[j];
-
-      i += Math.max(shiftBadChar, shiftGoodSuffix);
-    }
-  }
-
-  return matches;
-}
-
-// Example usage:
-const text = "ABCABABABA";
-const pattern = "ABA";
-const matches = BMISearch(text, pattern);
-console.log(matches); // Output: [3, 5, 7]
+let graph = new Graph();
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+graph.addEdge(2, 5);
+graph.addEdge(3, 6);
+graph.addEdge(3, 7);
+graph.breadthFirstSearch(1);
