918b1b10e8718f32b00c0d1945c0fcf78a3a5514
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:29:54 +0800

    1-14-2024 10:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,18 @@
-const array = [1, 2, 3, 4, 5];
-const elementToRemove = 3; // The element you want to remove
+function findCommonElements(array1, array2) {
+  const commonElements = [];
 
-// Find the index of the element to remove
-const index = array.indexOf(elementToRemove);
+  array1.forEach(element => {
+    if (array2.includes(element)) {
+      commonElements.push(element);
+    }
+  });
 
-// If the element is found, remove it from the array
-if (index > -1) {
-  array.splice(index, 1);
+  return commonElements;
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
+// Example usage:
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+
+const result = findCommonElements(array1, array2);
+console.log(result); // Output: [4, 5]
