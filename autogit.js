f8d556811c393a705fc2a3ea2bb6da0ec477b7fd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:40:15 +0800

    1-14-2024 11:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,52 @@
-const axios = require('axios');
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-// Make a GET request
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => {
-    console.log('GET Response:', response.data);
-  })
-  .catch((error) => {
-    console.error('GET Error:', error);
-  });
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  let next = null;
 
-// Make a POST request
-const postData = {
-  title: 'foo',
-  body: 'bar',
-  userId: 1
-};
+  while (current != null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
 
-axios.post('https://jsonplaceholder.typicode.com/posts', postData)
-  .then((response) => {
-    console.log('POST Response:', response.data);
-  })
-  .catch((error) => {
-    console.error('POST Error:', error);
-  });
+  return prev;
+}
+// Create a sample linked list: 1 -> 2 -> 3 -> 4 -> null
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+
+console.log('Original list:');
+let current = head;
+while (current != null) {
+  console.log(current.data);
+  current = current.next;
+}
+
+const reversedHead = reverseLinkedList(head);
+
+console.log('Reversed list:');
+current = reversedHead;
+while (current != null) {
+  console.log(current.data);
+  current = current.next;
+}
+Original list:
+1
+2
+3
+4
+Reversed list:
+4
+3
+2
+1
