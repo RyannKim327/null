144e08324253dd4f42715aea5f3974e3f176cb1b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:59:40 +0800

    1-14-2024 11:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,67 @@
-function quicksort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  
-  const pivot = arr[arr.length - 1];
-  const left = [];
-  const right = [];
-  
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
-    }
-  }
-  
-  return [...quicksort(left), pivot, ...quicksort(right)];
-}
+let fib2 = 0;
+let fib1 = 1;
+let fib = fib2 + fib1;
 
-// Example usage:
-const array = [5, 3, 8, 4, 2, 1, 9, 6, 7];
-console.log(quicksort(array)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+while (fib < n) {
+    fib2 = fib1;
+    fib1 = fib;
+    fib = fib2 + fib1;
+}
+let index = Math.min(fib1, n - 1);
+while (arr[index] !== x && fib > 1) {
+    if (arr[index] < x) {
+        fib = fib1;
+        fib1 = fib2;
+        fib2 = fib - fib1;
+        index += fib2;
+    } else {
+        fib = fib2;
+        fib1 = fib1 - fib2;
+        fib2 = fib - fib1;
+        index -= fib2;
+    }
+}
+if (arr[index] === x) {
+    return index;
+} else {
+    return -1;
+}
+function fibonacciSearch(arr, x, n) {
+    let fib2 = 0;
+    let fib1 = 1;
+    let fib = fib2 + fib1;
+
+    while (fib < n) {
+        fib2 = fib1;
+        fib1 = fib;
+        fib = fib2 + fib1;
+    }
+
+    let index = Math.min(fib1, n - 1);
+
+    while (arr[index] !== x && fib > 1) {
+        if (arr[index] < x) {
+            fib = fib1;
+            fib1 = fib2;
+            fib2 = fib - fib1;
+            index += fib2;
+        } else {
+            fib = fib2;
+            fib1 = fib1 - fib2;
+            fib2 = fib - fib1;
+            index -= fib2;
+        }
+    }
+
+    if (arr[index] === x) {
+        return index;
+    } else {
+        return -1;
+    }
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const x = 6;
+const n = arr.length;
+
+const result = fibonacciSearch(arr, x, n);
+console.log(result);  // Output: 5
