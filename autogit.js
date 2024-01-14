79fda3b9fe6c0eabb42107b3cea5ae6ac68339b3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:29:37 +0800

    1-14-2024 11:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,4 @@
-function bwt(input) {
-  // ...
-}
-function rotate(str) {
-  const rotations = [];
-  for (let i = 0; i < str.length; i++) {
-    rotations.push(str.slice(i) + str.slice(0, i));
-  }
-  return rotations;
-}
-function bwt(input) {
-  const rotations = rotate(input).sort();
-  // ...
-}
-function bwt(input) {
-  const rotations = rotate(input).sort();
-  const transformed = rotations.map((rotation) => rotation.slice(-1)).join('');
-  const originalIndex = rotations.indexOf(input);
-  // ...
-}
-function bwt(input) {
-  const rotations = rotate(input).sort();
-  const transformed = rotations.map((rotation) => rotation.slice(-1)).join('');
-  const originalIndex = rotations.indexOf(input);
-  return { transformed, originalIndex };
-}
-const input = 'hello world';
-const { transformed, originalIndex } = bwt(input);
-console.log('Transformed:', transformed);
-console.log('Original Index:', originalIndex);
+let stringNum = "42";
+let integerNum = parseInt(stringNum);
+
+console.log(integerNum); // Output: 42
