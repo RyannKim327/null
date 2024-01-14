d3a295e47a185f81f44eff90297097ff0fd339b8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 09:57:08 +0800

    1-14-2024 9:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,30 @@
-const array = [1, 2, 2, 3, 3, 4, 5, 5];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 2, 3, 3, 4, 5, 5];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 2, 3, 3, 4, 5, 5];
-const uniqueArray = array.reduce((accumulator, current) => {
-  if (!accumulator.includes(current)) {
-    accumulator.push(current);
+function findLongestCommonSubstring(string1, string2) {
+  let maxLength = 0;
+  let endIndex = 0;
+  const dp = [];
+
+  for (let i = 0; i <= string1.length; i++) {
+    dp[i] = new Array(string2.length + 1).fill(0);
+    for (let j = 0; j <= string2.length; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 0;
+      } else if (string1[i - 1] === string2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        if (dp[i][j] > maxLength) {
+          maxLength = dp[i][j];
+          endIndex = i;
+        }
+      } else {
+        dp[i][j] = 0;
+      }
+    }
   }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
+
+  return string1.slice(endIndex - maxLength, endIndex);
+}
+
+// Example usage
+const string1 = "abcdef";
+const string2 = "abdf";
+
+console.log(findLongestCommonSubstring(string1, string2)); // Output: "ab"
