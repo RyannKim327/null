96d0b3b7c85ab8f2eb0f159af6d6636a93fd267f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:37:50 +0800

    1-14-2024 10:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,16 @@
-function generateFibonacci(n) {
-  let fib = [0, 1];
-  for (let i = 2; i < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib;
-}
-let fib = generateFibonacci(n);
-let offset = -1;
-while (fib[n] > 1) {
-  let i = Math.min(offset + fib[n - 2], n - 1);
-
-  if (arr[i] < target) {
-    fib[n] = fib[n - 1];
-    fib[n - 1] = fib[n - 2];
-    fib[n - 2] = fib[n] - fib[n - 1];
-    offset = i;
-  } else if (arr[i] > target) {
-    fib[n] = fib[n - 2];
-    fib[n - 1] = fib[n - 1] - fib[n - 2];
-    fib[n - 2] = fib[n] - fib[n - 1];
-  } else {
-    return i;
-  }
-}
-if (fib[0] === 1 && arr[offset + 1] === target) {
-  return offset + 1;
-}
-return -1;
-function fibonacciSearch(arr, target, n) {
-  function generateFibonacci(n) {
-    let fib = [0, 1];
-    for (let i = 2; i < n; i++) {
-      fib[i] = fib[i - 1] + fib[i - 2];
-    }
-    return fib;
-  }
-
-  let fib = generateFibonacci(n);
-  let offset = -1;
-  while (fib[n] > 1) {
-    let i = Math.min(offset + fib[n - 2], n - 1);
-
-    if (arr[i] < target) {
-      fib[n] = fib[n - 1];
-      fib[n - 1] = fib[n - 2];
-      fib[n - 2] = fib[n] - fib[n - 1];
-      offset = i;
-    } else if (arr[i] > target) {
-      fib[n] = fib[n - 2];
-      fib[n - 1] = fib[n - 1] - fib[n - 2];
-      fib[n - 2] = fib[n] - fib[n - 1];
-    } else {
-      return i;
+function countCharacterOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      count++;
     }
   }
-
-  if (fib[0] === 1 && arr[offset + 1] === target) {
-    return offset + 1;
-  }
-  return -1;
+  return count;
 }
+
+// Example usage:
+const myString = "Hello, world!";
+const myCharacter = "o";
+
+const occurrenceCount = countCharacterOccurrences(myString, myCharacter);
+console.log(`The character "${myCharacter}" occurs ${occurrenceCount} times in the string.`);
