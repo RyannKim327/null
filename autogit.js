899fe08955d17b5b590fa116888e490c3eda4f45
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 12:00:27 +0800

    1-14-2024 12:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,19 @@
-class TreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
+const cron = require('node-cron');
 
-function sumBinaryTree(root) {
-  if (!root) {
-    return 0;
-  }
+// Schedule a task to run every minute
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute');
+  // Add your desired code here
+});
 
-  let sum = 0;
+// Schedule a task to run at 9:30 AM every day
+cron.schedule('30 9 * * *', () => {
+  console.log('Running a task at 9:30 AM every day');
+  // Add your desired code here
+});
 
-  function dfs(node) {
-    sum += node.val;
-    if (node.left) {
-      dfs(node.left);
-    }
-    if (node.right) {
-      dfs(node.right);
-    }
-  }
-
-  dfs(root);
-
-  return sum;
-}
-
-// Example usage:
-
-//     1
-//    / \
-//   2   3
-//  / \   \
-// 4   5   6
-
-const tree = new TreeNode(
-  1,
-  new TreeNode(2, new TreeNode(4), new TreeNode(5)),
-  new TreeNode(3, null, new TreeNode(6))
-);
-
-console.log(sumBinaryTree(tree)); // Output: 21 (1 + 2 + 4 + 5 + 3 + 6)
+// Schedule a task to run on Sundays at 5 PM
+cron.schedule('0 17 * * 0', () => {
+  console.log('Running a task on Sundays at 5 PM');
+  // Add your desired code here
+});
