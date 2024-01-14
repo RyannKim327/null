db654135d2a4dfa445204983d09e00a8dc853fa6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:42:07 +0800

    1-14-2024 11:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,8 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  if (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] > target) {
-      return binarySearch(arr, target, start, mid - 1);
-    } else {
-      return binarySearch(arr, target, mid + 1, end);
-    }
-  }
-
-  return -1;
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
-const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-const target = 12;
 
-const index = binarySearch(arr, target);
-
-if (index !== -1) {
-  console.log(`The target value ${target} was found at index ${index}.`);
-} else {
-  console.log(`The target value ${target} was not found in the array.`);
-}
+// Example usage
+const inputString = 'Hello, World!';
+const stringWithoutVowels = removeVowels(inputString);
+console.log(stringWithoutVowels);  // Output: "Hll, Wrld!"
