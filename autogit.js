0ef9221cd72e7e9d68af8581af29220ddc9fb885
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:38:08 +0800

    1-14-2024 11:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,26 @@
-let str = "This is a sample string with spaces";
-let newStr = str.replace(/\s/g, "");
-console.log(newStr);
-let str = "This is a sample string with spaces";
-let newStr = str.split(" ").join("");
-console.log(newStr);
-let str = "This is a sample string with spaces";
-let newStr = str.replace(/ /g, "");
-console.log(newStr);
+function bubbleSort(arr) {
+  let len = arr.length;
+  let swapped;
+  
+  do {
+    swapped = false;
+    
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap elements
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  
+  return arr;
+}
+
+// Usage example:
+let unsortedArray = [5, 3, 8, 4, 2];
+let sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // Output: [2, 3, 4, 5, 8]
