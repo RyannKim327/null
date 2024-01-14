d279b191ff4b36f4fdedbe509a730b150c1fc135
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:28:11 +0800

    1-14-2024 11:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,8 @@
-function countCharOccurrences(str, char) {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === char) {
-      count++;
-    }
-  }
-  return count;
-}
-
-const str = "Hello, world!";
-const charToCount = "o";
-const occurrenceCount = countCharOccurrences(str, charToCount);
-console.log(`The character '${charToCount}' occurs ${occurrenceCount} times.`);
-function countCharOccurrences(str, char) {
-  const count = str.split("").filter((c) => c === char).length;
-  return count;
-}
-
-const str = "Hello, world!";
-const charToCount = "o";
-const occurrenceCount = countCharOccurrences(str, charToCount);
-console.log(`The character '${charToCount}' occurs ${occurrenceCount} times.`);
-function countCharOccurrences(str, char) {
-  const regex = new RegExp(char, "g");
-  const matches = str.match(regex);
-  return matches ? matches.length : 0;
-}
-
-const str = "Hello, world!";
-const charToCount = "o";
-const occurrenceCount = countCharOccurrences(str, charToCount);
-console.log(`The character '${charToCount}' occurs ${occurrenceCount} times.`);
+fetch('https://api.example.com/data')
+   .then(response => response.json())
+   .then(data => {
+      console.log(data);
+   })
+   .catch(error => {
+      console.log('Error:', error);
+   });
