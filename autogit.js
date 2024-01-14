007339ef0d07a0d5e9f86876f22dbb006cc019e9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:10:11 +0800

    1-14-2024 11:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,5 @@
-// Prompt the user for a number
-const num1 = parseFloat(prompt("Enter a number:"));
+let str = "123";
+let num = parseInt(str);
 
-// Prompt the user for another number
-const num2 = parseFloat(prompt("Enter another number:"));
-
-// Perform addition, subtraction, multiplication, and division on the two numbers
-const sum = num1 + num2;
-const difference = num1 - num2;
-const product = num1 * num2;
-const quotient = num1 / num2;
-
-// Output the results
-console.log("Sum:", sum);
-console.log("Difference:", difference);
-console.log("Product:", product);
-console.log("Quotient:", quotient);
+console.log(num);  // Output: 123
+console.log(typeof num);  // Output: number
