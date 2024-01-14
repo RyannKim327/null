f11fac88cb2ab86581572fdfa20f934723304401
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:04:59 +0800

    1-14-2024 10:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,15 @@
-const axios = require('axios');
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log('Response received:');
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error occurred:');
-    console.log(error);
-  });
+// Example usage
+const sortedArray = [1, 2, 3, 4, 5];
+const unsortedArray = [3, 2, 1, 4, 5];
+
+console.log(isSortedAscending(sortedArray));      // Output: true
+console.log(isSortedAscending(unsortedArray));    // Output: false
