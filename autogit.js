8e75656950626a357ea9ada400df548d08c362b6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:43:20 +0800

    1-14-2024 10:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,39 @@
-function selectionSort(arr) {
-  const n = arr.length;
+function longestCommonSubsequence(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
 
-  for (let i = 0; i < n - 1; i++) {
-    let minIndex = i;
+  // Create a 2D matrix and initialize it with zeros
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
 
-    for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    if (minIndex !== i) {
-      let temp = arr[minIndex];
-      arr[minIndex] = arr[i];
-      arr[i] = temp;
+  // Populate the matrix by comparing characters of the two strings
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1])
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      else
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
     }
   }
 
-  return arr;
+  // Retrieve the longest common subsequence
+  let lcs = '';
+  let i = m;
+  let j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i][j - 1] > dp[i - 1][j]) {
+      j--;
+    } else {
+      i--;
+    }
+  }
+
+  return lcs;
 }
-const array = [64, 25, 12, 22, 11];
-console.log(selectionSort(array)); // Output: [11, 12, 22, 25, 64]
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const result = longestCommonSubsequence(str1, str2);
+console.log(result); // Output: ADH
