e065404a91cc84d4ac337ca61b850901373d2337
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:25:06 +0800

    1-14-2024 11:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,3 @@
-function isPrime(number) {
-  if (number <= 1) {
-    return false;
-  }
-  
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-  
-  return true;
-}
-
-// Example usage
-console.log(isPrime(7));  // true
-console.log(isPrime(12)); // false
+let str = "Hello,    world!   ";
+let withoutSpaces = str.replace(/\s/g, "");
+console.log(withoutSpaces); // Output: "Hello,world!"
