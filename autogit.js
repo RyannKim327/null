98509b6852eda947bc91bf056c50452ab7dd90f8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:10:09 +0800

    1-14-2024 10:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,8 @@
-function getRandomNumber(min, max) {
-  // Generate a random decimal between 0 and 1
-  const randomNumber = Math.random();
-  
-  // Scale the random decimal to fit the given range
-  const scaledNumber = randomNumber * (max - min + 1);
-  
-  // Shift the number to the starting point of the range
-  const finalNumber = Math.floor(scaledNumber) + min;
-  
-  return finalNumber;
-}
-const minNumber = 1;
-const maxNumber = 100;
+const string = "Hello, World!";
+const substring = "Hello";
 
-const randomNumber = getRandomNumber(minNumber, maxNumber);
-console.log(randomNumber);
+if (string.includes(substring)) {
+  console.log("The string contains the substring.");
+} else {
+  console.log("The string does not contain the substring.");
+}
