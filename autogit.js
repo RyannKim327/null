3e9546d33b29efdf48ec8b68b1de518f6ebc76a8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:59:47 +0800

    1-14-2024 10:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,22 @@
-let arr = [1, 2, 3, 4, 5];
-let indexToRemove = 2;
-
-arr.splice(indexToRemove, 1);
-
-console.log(arr);  // [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-arr = arr.filter(item => item !== elementToRemove);
-
-console.log(arr);  // [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-let indexToRemove = arr.indexOf(elementToRemove);
-if (indexToRemove !== -1) {
-  arr.splice(indexToRemove, 1);
+function bubbleSort(arr) {
+  var len = arr.length;
+  
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap arr[j] and arr[j + 1]
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  
+  return arr;
 }
 
-console.log(arr);  // [1, 2, 4, 5]
+// Example usage:
+var unsortedArray = [5, 3, 8, 1, 2, 9];
+var sortedArray = bubbleSort(unsortedArray);
+
+console.log(sortedArray);
