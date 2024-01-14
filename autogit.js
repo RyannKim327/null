dc0989cf6b0d301f337e23d3ecba4a905c775359
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 11:18:41 +0800

    1-14-2024 11:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,42 @@
-function longestCommonPrefix(strs) {
-  let lcp = '';
-  
-  if (strs.length === 0) {
-    return lcp;
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
   }
   
-  for (let i = 0; i < strs[0].length; i++) {
-    const char = strs[0][i];
-    
-    for (let j = 1; j < strs.length; j++) {
-      if (strs[j][i] !== char) {
-        return lcp;
-      }
-    }
-    
-    lcp += char;
-  }
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
   
-  return lcp;
+  return merge(mergeSort(left), mergeSort(right));
 }
-const strings = ['flower', 'flow', 'flight'];
-console.log(longestCommonPrefix(strings));  // Output: "fl"
+
+function merge(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let merged = [];
+  
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
+    } else {
+      merged.push(arr2[j]);
+      j++;
+    }
+  }
+  
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+  
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+  
+  return merged;
+}
+const array = [5, 8, 2, 1, 6, 3, 7, 4];
+const sortedArray = mergeSort(array);
+console.log(sortedArray);
