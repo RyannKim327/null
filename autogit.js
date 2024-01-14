c78e8270fde79391648d31ebd71f71a6adfe401c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 10:24:25 +0800

    1-14-2024 10:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,26 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+let arr = [5, 3, 9, 1, 7];
+let max = -Infinity;
+let secondMax = -Infinity;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    secondMax = max;
+    max = arr[i];
+  } else if (arr[i] < max && arr[i] > secondMax) {
+    secondMax = arr[i];
   }
-  
-  const middle = Math.floor(arr.length / 2);
-  const left = arr.slice(0, middle);
-  const right = arr.slice(middle);
-  
-  return merge(mergeSort(left), mergeSort(right));
+}
+console.log(secondMax);
+let arr = [5, 3, 9, 1, 7];
+let max = -Infinity;
+let secondMax = -Infinity;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > max) {
+    secondMax = max;
+    max = arr[i];
+  } else if (arr[i] < max && arr[i] > secondMax) {
+    secondMax = arr[i];
+  }
 }
 
-function merge(left, right) {
-  let result = [];
-  let i = 0;
-  let j = 0;
-  
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i++]);
-    } else {
-      result.push(right[j++]);
-    }
-  }
-  
-  while (i < left.length) {
-    result.push(left[i++]);
-  }
-  
-  while (j < right.length) {
-    result.push(right[j++]);
-  }
-  
-  return result;
-}
-
-// Example usage
-const arr = [8, 4, 2, 9, 3, 1, 5, 7, 6];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(secondMax);
