43e930f71a47870e16cd952006c831532daa9c91
RyannKim327 <rksesgundo123@gmail.com>
2024-01-14 12:01:30 +0800

    1-14-2024 12:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,111 @@
-function isArraySortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false; // If any element is greater than the next element, the array is not sorted in ascending order
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
-  return true; // If no such elements are found, the array is sorted in ascending order
-}
-const myArray = [1, 2, 3, 4, 5];
-console.log(isArraySortedAscending(myArray)); // Output: true
 
-const anotherArray = [5, 2, 8, 1, 3];
-console.log(isArraySortedAscending(anotherArray)); // Output: false
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  find(value) {
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let deletedNode = null;
+
+    if (this.head.value === value) {
+      deletedNode = this.head;
+      this.head = this.head.next;
+
+      if (this.head === null) {
+        this.tail = null;
+      }
+    } else {
+      let currentNode = this.head;
+
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next;
+          currentNode.next = currentNode.next.next;
+
+          if (currentNode.next === null) {
+            this.tail = currentNode;
+          }
+          break;
+        }
+        currentNode = currentNode.next;
+      }
+    }
+
+    return deletedNode;
+  }
+
+  toArray() {
+    const values = [];
+    let currentNode = this.head;
+
+    while (currentNode) {
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return values;
+  }
+
+  print() {
+    console.log(this.toArray().join(" -> "));
+  }
+}
+const linkedList = new LinkedList();
+linkedList.append(5);
+linkedList.append(10);
+linkedList.prepend(1);
+linkedList.append(15);
+linkedList.print();  // Output: 1 -> 5 -> 10 -> 15
+
+console.log(linkedList.find(10));  // Output: Node { value: 10, next: Node { value: 15, next: null } }
+
+console.log(linkedList.delete(5));  // Output: Node { value: 5, next: Node { value: 10, next: Node { value: 15, next: null } } }
+linkedList.print();  // Output: 1 -> 10 -> 15
