60e0bbdb1bc7e57dd678ebb0e5176fcbb4e94900
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 19:59:48 +0800

    1-15-2024 19:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,18 @@
-function findMedianSortedArrays(nums1, nums2) {
-  const combined = nums1.concat(nums2);
-  combined.sort((a, b) => a - b);
-
-  const length = combined.length;
-  const middle = Math.floor(length / 2);
-
-  if (length % 2 === 0) {
-    return (combined[middle - 1] + combined[middle]) / 2;
-  } else {
-    return combined[middle];
+function bubbleSort(array) {
+  var len = array.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
   }
+  return array;
 }
 
-// Example usage:
-const nums1 = [1, 3];
-const nums2 = [2, 4];
-const median = findMedianSortedArrays(nums1, nums2);
-console.log(median); // Output: 2.5
+// Example usage
+var myArray = [5, 3, 8, 4, 2];
+console.log(bubbleSort(myArray));
+// Output: [2, 3, 4, 5, 8]
