d598311680c9ad28ede32770bb834264f3d3afff
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:22:54 +0800

    1-15-2024 20:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,20 @@
-area = (base * height) / 2;
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
-}
+function findNthNodeFromEnd(head, n) {
+  let pointer1 = head;
+  let pointer2 = head;
 
-// Example usage
-var base = 5;
-var height = 7;
-var triangleArea = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + triangleArea);
+  // Move pointer1 n nodes ahead
+  for (let i = 0; i < n; i++) {
+    if (pointer1 === null) {
+      return null; // Invalid input, list smaller than n
+    }
+    pointer1 = pointer1.next;
+  }
+
+  // Move both pointers until pointer1 reaches the end of the list
+  while (pointer1 !== null) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+
+  return pointer2; // Return the nth node from the end
+}
