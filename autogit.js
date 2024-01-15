ad7b7c3cd91b46daf1fc6e1b0fa4110e6f8b6f5d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:37:32 +0800

    1-15-2024 21:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,8 @@
-class Node {
-  constructor() {
-    this.children = {};
-  }
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
-class SuffixTree {
-  constructor() {
-    this.root = new Node();
-  }
 
-  // Implementation methods go here
-}
-insert(word) {
-  for (let i = 0; i < word.length; i++) {
-    let currentNode = this.root;
-    for (let j = i; j < word.length; j++) {
-      const currentChar = word[j];
-      if (!currentNode.children[currentChar]) {
-        currentNode.children[currentChar] = new Node();
-      }
-      currentNode = currentNode.children[currentChar];
-      // You can perform additional operations on currentNode if needed
-    }
-  }
-}
-search(substring) {
-  let currentNode = this.root;
-  for (let i = 0; i < substring.length; i++) {
-    const currentChar = substring[i];
-    if (!currentNode.children[currentChar]) {
-      return false; // Substring not found
-    }
-    currentNode = currentNode.children[currentChar];
-  }
-  return true; // Substring found
-}
+// Example usage:
+var input = 'Hello, World!';
+var output = removeVowels(input);
+console.log(output); // Output: "Hll, Wrld!"
