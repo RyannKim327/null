6ee7014615d52c9d90b47385a17bef00acef866b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 19:58:58 +0800

    1-15-2024 19:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,132 +1,143 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.height = 1;
-  }
+    constructor(value, color) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.color = color;
+    }
 }
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
 
-class AVLTree {
-  constructor() {
-    this.root = null;
-  }
+    // Implement the necessary tree operations here
+}
+// Left Rotation
+leftRotate(node) {
+    const rightChild = node.right;
+    node.right = rightChild.left;
 
-  // Helper function to get the height of a node
-  getNodeHeight(node) {
-    if (node == null) return 0;
-    return node.height;
-  }
+    if (rightChild.left !== null) {
+        rightChild.left.parent = node;
+    }
 
-  // Helper function to get the balance factor of a node
-  getBalanceFactor(node) {
-    if (node == null) return 0;
-    return this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
-  }
+    rightChild.parent = node.parent;
 
-  // Helper function to update the height of a node
-  updateNodeHeight(node) {
-    if (node == null) return;
-    node.height = Math.max(
-      this.getNodeHeight(node.left),
-      this.getNodeHeight(node.right)
-    ) + 1;
-  }
-
-  // Rotate the given node to the left
-  rotateLeft(node) {
-    const newRoot = node.right;
-    const tmp = newRoot.left;
-
-    newRoot.left = node;
-    node.right = tmp;
-
-    this.updateNodeHeight(node);
-    this.updateNodeHeight(newRoot);
-
-    return newRoot;
-  }
-
-  // Rotate the given node to the right
-  rotateRight(node) {
-    const newRoot = node.left;
-    const tmp = newRoot.right;
-
-    newRoot.right = node;
-    node.left = tmp;
-
-    this.updateNodeHeight(node);
-    this.updateNodeHeight(newRoot);
-
-    return newRoot;
-  }
-
-  // Insert a new value into the AVL tree
-  insert(value) {
-    this.root = this.insertNode(this.root, value);
-  }
-
-  insertNode(node, value) {
-    if (node == null) return new Node(value);
-
-    if (value < node.value) {
-      node.left = this.insertNode(node.left, value);
-    } else if (value > node.value) {
-      node.right = this.insertNode(node.right, value);
+    if (node.parent === null) {
+        this.root = rightChild;
+    } else if (node === node.parent.left) {
+        node.parent.left = rightChild;
     } else {
-      // Duplicate values are not allowed in AVL trees
-      return node;
+        node.parent.right = rightChild;
     }
 
-    this.updateNodeHeight(node); // Update the height of the current node
-
-    const balanceFactor = this.getBalanceFactor(node);
-
-    // Left Left Case
-    if (balanceFactor > 1 && value < node.left.value) {
-      return this.rotateRight(node);
-    }
-
-    // Right Right Case
-    if (balanceFactor < -1 && value > node.right.value) {
-      return this.rotateLeft(node);
-    }
-
-    // Left Right Case
-    if (balanceFactor > 1 && value > node.left.value) {
-      node.left = this.rotateLeft(node.left);
-      return this.rotateRight(node);
-    }
-
-    // Right Left Case
-    if (balanceFactor < -1 && value < node.right.value) {
-      node.right = this.rotateRight(node.right);
-      return this.rotateLeft(node);
-    }
-
-    return node;
-  }
-
-  // Print the AVL tree in-order
-  printInOrder() {
-    this.printInOrderNode(this.root);
-  }
-
-  printInOrderNode(node) {
-    if (node != null) {
-      this.printInOrderNode(node.left);
-      console.log(node.value);
-      this.printInOrderNode(node.right);
-    }
-  }
+    rightChild.left = node;
+    node.parent = rightChild;
 }
 
-// Usage
-const avlTree = new AVLTree();
-avlTree.insert(5);
-avlTree.insert(3);
-avlTree.insert(8);
-avlTree.insert(2);
-avlTree.insert(4);
+// Right Rotation
+rightRotate(node) {
+    const leftChild = node.left;
+    node.left = leftChild.right;
 
-avlTree.printInOrder(); // Output: 2 3 4 5 8
+    if (leftChild.right !== null) {
+        leftChild.right.parent = node;
+    }
+
+    leftChild.parent = node.parent;
+
+    if (node.parent === null) {
+        this.root = leftChild;
+    } else if (node === node.parent.right) {
+        node.parent.right = leftChild;
+    } else {
+        node.parent.left = leftChild;
+    }
+
+    leftChild.right = node;
+    node.parent = leftChild;
+}
+
+// Color flip
+flipColors(node) {
+    node.color = !node.color;
+    node.left.color = !node.left.color;
+    node.right.color = !node.right.color;
+}
+insert(value) {
+    const newNode = new Node(value, true); // Red color for new nodes
+
+    // Perform regular BST insert
+    if (this.root === null) {
+        this.root = newNode;
+    } else {
+        this.insertNode(this.root, newNode);
+    }
+
+    // Fix any violations that may have occurred during insert
+    this.fixInsertViolation(newNode);
+}
+
+insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+        if (node.left === null) {
+            node.left = newNode;
+            newNode.parent = node;
+        } else {
+            this.insertNode(node.left, newNode);
+        }
+    } else if (newNode.value > node.value) {
+        if (node.right === null) {
+            node.right = newNode;
+            newNode.parent = node;
+        } else {
+            this.insertNode(node.right, newNode);
+        }
+    }
+}
+
+fixInsertViolation(node) {
+    while (node !== this.root && node.color && node.parent.color) {
+        if (node.parent === node.parent.parent.left) {
+            const uncle = node.parent.parent.right;
+
+            if (uncle !== null && uncle.color) {
+                node.parent.color = false;
+                uncle.color = false;
+                node.parent.parent.color = true;
+                node = node.parent.parent;
+            } else {
+                if (node === node.parent.right) {
+                    node = node.parent;
+                    this.leftRotate(node);
+                }
+
+                node.parent.color = false;
+                node.parent.parent.color = true;
+                this.rightRotate(node.parent.parent);
+            }
+        } else {
+            const uncle = node.parent.parent.left;
+
+            if (uncle !== null && uncle.color) {
+                node.parent.color = false;
+                uncle.color = false;
+                node.parent.parent.color = true;
+                node = node.parent.parent;
+            } else {
+                if (node === node.parent.left) {
+                    node = node.parent;
+                    this.rightRotate(node);
+                }
+
+                node.parent.color = false;
+                node.parent.parent.color = true;
+                this.leftRotate(node.parent.parent);
+            }
+        }
+    }
+
+    this.root.color = false;
+}
