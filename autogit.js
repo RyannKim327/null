3d0cc232249a6ee8135d1f3d759bc4acb41fc4e9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:57:01 +0800

    1-15-2024 20:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,12 @@
-function findSecondLargest(arr) {
-  let max = -Infinity;
-  let secondMax = -Infinity;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      secondMax = max;
-      max = arr[i];
-    } else if (arr[i] > secondMax && arr[i] !== max) {
-      secondMax = arr[i];
-    }
+function findStringLength(str) {
+  let count = 0;
+  while (str[count] !== undefined) {
+    count++;
   }
-
-  return secondMax;
+  return count;
 }
-const array = [1, 5, 2, 8, 3, 9, 4, 7, 6];
-const secondLargest = findSecondLargest(array);
-console.log(secondLargest); // Output: 8
+
+// Example usage:
+const myString = "Hello, World!";
+const stringLength = findStringLength(myString);
+console.log(stringLength); // Output: 13
