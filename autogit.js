b2cb9d5483cdb5839fbdf7ee213346c3adbea16d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:31:04 +0800

    1-15-2024 21:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,5 @@
-function findMaxSubarray(arr) {
-  let maxSoFar = arr[0];
-  let maxEndingHere = arr[0];
-  
-  for (let i = 1; i < arr.length; i++) {
-    maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-    maxSoFar = Math.max(maxSoFar, maxEndingHere);
-  }
-  
-  return maxSoFar;
-}
+let stringNumber = "42";
+let integerNumber = parseInt(stringNumber);
 
-// Example usage:
-const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const maxSubarraySum = findMaxSubarray(array);
-console.log(maxSubarraySum);  // Output: 6 (corresponding to [4, -1, 2, 1])
+console.log(integerNumber); // Output: 42
+console.log(typeof integerNumber); // Output: "number"
