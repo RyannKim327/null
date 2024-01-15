7a86d2fa659a42ada67dbfd316a7a6e2480d9370
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:05:11 +0800

    1-15-2024 20:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,9 @@
-function reverseWords(str) {
-  // Step 1: Split the string into an array of words
-  let words = str.split(" ");
+const cron = require('node-cron');
 
-  // Step 2: Reverse the array
-  words.reverse();
-
-  // Step 3: Join the reversed array into a string
-  return words.join(" ");
-}
-
-// Example usage
-let originalString = "Hello, World!";
-let reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: "World! Hello,"
+// Schedule a cron job to run every minute
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute');
+  
+  // Code logic to be executed here
+});
+npm install node-cron
