f616bd0b2482fdaa5429bee8700469bbaa4044bf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:21:02 +0800

    1-15-2024 21:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,27 @@
-function longestCommonPrefix(strings) {
-  if (strings.length === 0) {
-    return '';
-  }
-
-  let prefix = '';
-
-  for (let i = 0; i < strings[0].length; i++) {
-    const char = strings[0][i];
-
-    for (let j = 1; j < strings.length; j++) {
-      if (strings[j][i] !== char) {
-        return prefix;
-      }
-    }
-
-    prefix += char;
-  }
-
-  return prefix;
+function findCommonElements(array1, array2) {
+  return array1.filter(element => array2.includes(element));
 }
-const strings = ['apple', 'ape', 'april'];
-const commonPrefix = longestCommonPrefix(strings);
 
-console.log(commonPrefix); // Output: 'ap'
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+function findCommonElements(array1, array2) {
+  const set = new Set(array2);
+  return array1.filter(element => set.has(element));
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+function findCommonElements(array1, array2) {
+  return array1.reduce((commonElements, element) => {
+    if (array2.includes(element)) {
+      commonElements.push(element);
+    }
+    return commonElements;
+  }, []);
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
