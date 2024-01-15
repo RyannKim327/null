3b8f094dd19f3a61d982574daebbe5c89367a8a4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:37:54 +0800

    1-15-2024 21:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,37 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
-}
+function topologicalSort(graph) {
+  const visited = new Set();
+  const stack = [];
 
-// Example usage:
-var input = 'Hello, World!';
-var output = removeVowels(input);
-console.log(output); // Output: "Hll, Wrld!"
+  function dfs(node) {
+    visited.add(node);
+    if (graph[node]) {
+      for (const neighbor of graph[node]) {
+        if (!visited.has(neighbor)) {
+          dfs(neighbor);
+        }
+      }
+    }
+    stack.unshift(node);
+  }
+
+  for (const node in graph) {
+    if (!visited.has(node)) {
+      dfs(node);
+    }
+  }
+
+  return stack;
+}
+// Example graph
+const graph = {
+  'A': ['B', 'C'],
+  'B': ['D'],
+  'C': ['D', 'E'],
+  'D': ['F'],
+  'E': ['F'],
+  'F': []
+};
+
+// Perform topological sort
+const sortedNodes = topologicalSort(graph);
+console.log(sortedNodes); // Output: ['A', 'C', 'E', 'B', 'D', 'F']
