14c279f6f5dcd3774896f5df02bee7f603aaf160
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:26:42 +0800

    1-15-2024 20:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,56 @@
-function mergeSortIterative(arr) {
-  var len = arr.length;
-  var size, leftStart;
+let index = 0; // Global index counter
+let stack = []; // Stack to keep track of visited nodes
+let scc = []; // Array to store the strongly connected components
+let lowLink = []; // Array to store the low link value of each node
+let onStack = []; // Array to track if a node is currently on the stack
+function tarjan(node) {
+  // Set the low link value and index of the current node
+  lowLink[node] = index;
+  index++;
+  stack.push(node);
+  onStack[node] = true;
 
-  for (size = 1; size < len; size *= 2) {
-    for (leftStart = 0; leftStart < len; leftStart += 2 * size) {
-      var mid = leftStart + size - 1;
-      var rightEnd = Math.min(leftStart + 2 * size - 1, len - 1);
-
-      merge(arr, leftStart, mid, rightEnd);
+  // Explore each neighbor of the current node
+  for (let neighbor of graph[node]) {
+    if (lowLink[neighbor] === undefined) {
+      // Neighbor has not been visited yet, recurse on it
+      tarjan(neighbor);
+      lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
+    } else if (onStack[neighbor]) {
+      // Neighbor is on the stack and therefore in the current SCC
+      lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
     }
   }
 
-  return arr;
-}
-
-function merge(arr, leftStart, mid, rightEnd) {
-  var leftEnd = mid;
-  var rightStart = mid + 1;
-
-  var left = arr.slice(leftStart, leftEnd + 1);
-  var right = arr.slice(rightStart, rightEnd + 1);
-
-  var i = 0; // Pointer for the left subarray
-  var j = 0; // Pointer for the right subarray
-  var k = leftStart; // Pointer for the merged array
-
-  while (i < left.length && j < right.length) {
-    if (left[i] <= right[j]) {
-      arr[k] = left[i];
-      i++;
-    } else {
-      arr[k] = right[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < left.length) {
-    arr[k] = left[i];
-    i++;
-    k++;
-  }
-
-  while (j < right.length) {
-    arr[k] = right[j];
-    j++;
-    k++;
+  // If node is a root node, pop the stack and generate a new SCC
+  if (lowLink[node] === index - 1) {
+    let component = [];
+    let top = null;
+    do {
+      top = stack.pop();
+      onStack[top] = false;
+      component.push(top);
+    } while (top != node);
+    scc.push(component);
   }
 }
+for (let node = 0; node < graph.length; node++) {
+  if (lowLink[node] === undefined) {
+    tarjan(node);
+  }
+}
+return scc;
+let graph = [
+  [1, 3],
+  [2],
+  [0],
+  [4],
+  [3],
+  [5, 7],
+  [6],
+  [5]
+];
 
-// Usage example:
-var arr = [56, 3, 41, 8, 1, 67, 32, 15];
-console.log(mergeSortIterative(arr));
+let result = tarjanSCC(graph);
+console.log(result);
+[[4, 3], [2, 1, 0], [7, 6, 5]]
