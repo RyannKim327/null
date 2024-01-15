8235e29562e0e66a5afd6910e61137b87ef1ed58
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:40:01 +0800

    1-15-2024 21:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,43 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('An error occurred:', error);
-  });
+class HashTable {
+  constructor() {
+    this.table = {};
+  }
+
+  // Hash function to convert keys into unique hash codes
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
+
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const hashCode = this.hash(key);
+    this.table[hashCode] = value;
+  }
+
+  // Retrieve the value associated with a given key
+  get(key) {
+    const hashCode = this.hash(key);
+    return this.table[hashCode];
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const hashCode = this.hash(key);
+    if (this.table.hasOwnProperty(hashCode)) {
+      delete this.table[hashCode];
+    }
+  }
+}
+
+// Usage:
+const myHashTable = new HashTable();
+myHashTable.insert("name", "John");
+myHashTable.insert("age", 25);
+console.log(myHashTable.get("name")); // Output: John
+console.log(myHashTable.get("age")); // Output: 25
+myHashTable.remove("age");
+console.log(myHashTable.get("age")); // Output: undefined
