35f9adc76c9df8c199aef19e92ddec4cc23e1879
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:16:22 +0800

    1-15-2024 20:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,107 +1,78 @@
-// Node class to represent each node in the trie
-class TrieNode {
+class LinkedList {
   constructor() {
-    this.children = {};
-    this.isEndOfWord = false;
+    this.head = null;
+    this.tail = null;
+  }
+
+  addToHead(value) {
+    const newNode = new Node(value, this.head);
+    if (!this.head) {
+      this.tail = newNode;
+    }
+    this.head = newNode;
+  }
+
+  addToTail(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+  }
+
+  removeHead() {
+    if (!this.head) {
+      return null;
+    }
+    const removedValue = this.head.value;
+    this.head = this.head.next;
+    if (!this.head) {
+      this.tail = null;
+    }
+    return removedValue;
+  }
+
+  search(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  remove(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    if (this.head.value === value) {
+      return this.removeHead();
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        const removedValue = currentNode.next.value;
+        currentNode.next = currentNode.next.next;
+        if (!currentNode.next) {
+          this.tail = currentNode;
+        }
+        return removedValue;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
   }
 }
 
-// Trie class
-class Trie {
-  constructor() {
-    this.root = new TrieNode();
-  }
-
-  // Function to insert a word into the trie
-  insert(word) {
-    let node = this.root;
-
-    // Traverse through each character of the word
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-
-      // If the current character is not present as a child in the Trie
-      // Create a new node and set it as a child
-      if (!node.children[char]) {
-        node.children[char] = new TrieNode();
-      }
-
-      // Move to the next node
-      node = node.children[char];
-    }
-
-    // Mark the end of word
-    node.isEndOfWord = true;
-  }
-
-  // Function to search for a word in the trie
-  search(word) {
-    let node = this.root;
-
-    // Traverse through each character of the word
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-
-      // If the current character is not present as a child in the Trie
-      // Word doesn't exist, return false
-      if (!node.children[char]) {
-        return false;
-      }
-
-      // Move to the next node
-      node = node.children[char];
-    }
-
-    // Return true only if the node at the end of word is marked
-    return node.isEndOfWord;
-  }
-
-  // Function to delete a word from the trie
-  delete(word) {
-    this.deleteRecursive(this.root, word, 0);
-  }
-
-  deleteRecursive(node, word, index) {
-    // Base case: If we have reached the end of word
-    if (index === word.length) {
-      // Unmark the node as the end of word
-      if (!node.isEndOfWord) {
-        return false; // Word doesn't exist
-      }
-      node.isEndOfWord = false;
-
-      // Check if the current node has no other children
-      return Object.keys(node.children).length === 0;
-    }
-
-    const char = word[index];
-    const childNode = node.children[char];
-
-    // If the current character is not present as a child in the Trie
-    // Word doesn't exist, return false
-    if (!childNode) {
-      return false;
-    }
-
-    const shouldDeleteChild = this.deleteRecursive(childNode, word, index + 1);
-
-    // If the child node has no other children and it's not the end of another word,
-    // Delete the child node
-    if (shouldDeleteChild) {
-      delete node.children[char];
-      return Object.keys(node.children).length === 0;
-    }
-
-    return false;
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
   }
 }
-
-// Usage:
-const trie = new Trie();
-trie.insert("apple");
-trie.insert("application");
-console.log(trie.search("apple")); // Output: true
-console.log(trie.search("app")); // Output: false
-console.log(trie.search("application")); // Output: true
-trie.delete("application");
-console.log(trie.search("application")); // Output: false
