b337f900f9c7a99d0047b930d40a5ee48b2fcf9b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 19:59:16 +0800

    1-15-2024 19:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,143 +1,68 @@
 class Node {
-    constructor(value, color) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-        this.color = color;
-    }
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
-class RedBlackTree {
-    constructor() {
-        this.root = null;
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Method to reverse the linked list
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current) {
+      next = current.next; // Store the next node
+      current.next = prev; // Reverse the current node's pointer
+      prev = current; // Move prev to current node
+      current = next; // Move current to next node
     }
 
-    // Implement the necessary tree operations here
-}
-// Left Rotation
-leftRotate(node) {
-    const rightChild = node.right;
-    node.right = rightChild.left;
+    this.head = prev; // Reset the head to the last node
+  }
 
-    if (rightChild.left !== null) {
-        rightChild.left.parent = node;
-    }
+  // Method to add a new node to the linked list
+  add(value) {
+    const newNode = new Node(value);
 
-    rightChild.parent = node.parent;
-
-    if (node.parent === null) {
-        this.root = rightChild;
-    } else if (node === node.parent.left) {
-        node.parent.left = rightChild;
+    if (!this.head) {
+      this.head = newNode;
     } else {
-        node.parent.right = rightChild;
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
-
-    rightChild.left = node;
-    node.parent = rightChild;
+  }
+  
+  // Method to display the linked list
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
 }
 
-// Right Rotation
-rightRotate(node) {
-    const leftChild = node.left;
-    node.left = leftChild.right;
+// Example usage
+const linkedList = new LinkedList();
 
-    if (leftChild.right !== null) {
-        leftChild.right.parent = node;
-    }
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
 
-    leftChild.parent = node.parent;
+console.log("Original Linked List:");
+linkedList.display();
 
-    if (node.parent === null) {
-        this.root = leftChild;
-    } else if (node === node.parent.right) {
-        node.parent.right = leftChild;
-    } else {
-        node.parent.left = leftChild;
-    }
+linkedList.reverse();
 
-    leftChild.right = node;
-    node.parent = leftChild;
-}
-
-// Color flip
-flipColors(node) {
-    node.color = !node.color;
-    node.left.color = !node.left.color;
-    node.right.color = !node.right.color;
-}
-insert(value) {
-    const newNode = new Node(value, true); // Red color for new nodes
-
-    // Perform regular BST insert
-    if (this.root === null) {
-        this.root = newNode;
-    } else {
-        this.insertNode(this.root, newNode);
-    }
-
-    // Fix any violations that may have occurred during insert
-    this.fixInsertViolation(newNode);
-}
-
-insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-        if (node.left === null) {
-            node.left = newNode;
-            newNode.parent = node;
-        } else {
-            this.insertNode(node.left, newNode);
-        }
-    } else if (newNode.value > node.value) {
-        if (node.right === null) {
-            node.right = newNode;
-            newNode.parent = node;
-        } else {
-            this.insertNode(node.right, newNode);
-        }
-    }
-}
-
-fixInsertViolation(node) {
-    while (node !== this.root && node.color && node.parent.color) {
-        if (node.parent === node.parent.parent.left) {
-            const uncle = node.parent.parent.right;
-
-            if (uncle !== null && uncle.color) {
-                node.parent.color = false;
-                uncle.color = false;
-                node.parent.parent.color = true;
-                node = node.parent.parent;
-            } else {
-                if (node === node.parent.right) {
-                    node = node.parent;
-                    this.leftRotate(node);
-                }
-
-                node.parent.color = false;
-                node.parent.parent.color = true;
-                this.rightRotate(node.parent.parent);
-            }
-        } else {
-            const uncle = node.parent.parent.left;
-
-            if (uncle !== null && uncle.color) {
-                node.parent.color = false;
-                uncle.color = false;
-                node.parent.parent.color = true;
-                node = node.parent.parent;
-            } else {
-                if (node === node.parent.left) {
-                    node = node.parent;
-                    this.rightRotate(node);
-                }
-
-                node.parent.color = false;
-                node.parent.parent.color = true;
-                this.leftRotate(node.parent.parent);
-            }
-        }
-    }
-
-    this.root.color = false;
-}
+console.log("Reversed Linked List:");
+linkedList.display();
