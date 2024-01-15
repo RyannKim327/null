ac56cd2aebfac778182c9a54a934f9926b925c9d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:23:22 +0800

    1-15-2024 21:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,17 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
+function isAnagram(str1, str2) {
+  // Remove non-alphabetic characters
+  str1 = str1.replace(/[^A-Za-z]/g, '').toLowerCase();
+  str2 = str2.replace(/[^A-Za-z]/g, '').toLowerCase();
+
+  // Sort the characters
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
 }
-var myString = "Hello, World!";
-var stringWithoutVowels = removeVowels(myString);
-console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
+
+// Usage example
+const string1 = 'listen';
+const string2 = 'silent';
+console.log(isAnagram(string1, string2)); // true
