2f0f535c4c8ca59d0877b9aaea86864b56039cc7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:12:48 +0800

    1-15-2024 20:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,44 @@
-let stringNumber = "123";
-let integerNumber = parseInt(stringNumber);
+function longestCommonSubsequence(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  
+  // Create a 2D array to store the LCS lengths
+  const dp = new Array(m + 1);
+  for (let i = 0; i <= m; i++) {
+    dp[i] = new Array(n + 1).fill(0);
+  }
+  
+  // Compute LCS lengths
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  
+  // Construct the LCS string
+  let lcs = '';
+  let i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+  
+  return lcs;
+}
 
-console.log(typeof integerNumber);  // Output: "number"
-console.log(integerNumber);         // Output: 123
-let stringNumber = "123";
-let integerNumber = Number(stringNumber);
-
-console.log(typeof integerNumber);  // Output: "number"
-console.log(integerNumber);         // Output: 123
+// Usage example
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: ADH
