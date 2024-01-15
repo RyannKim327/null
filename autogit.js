c2dd11352070e78259641a6fe91c9179734716c6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:12:43 +0800

    1-15-2024 21:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,26 @@
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString = string1 + " " + string2;
-console.log(concatenatedString); // Output: "Hello World"
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString = string1.concat(" ", string2);
-console.log(concatenatedString); // Output: "Hello World"
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const str = "Hello, world!";
+const char = "o";
+
+const occurrences = countOccurrences(str, char);
+console.log(`The character '${char}' occurs ${occurrences} times in the string.`);
+function countOccurrences(str, char) {
+  const occurrences = str.split(char).length - 1;
+  return occurrences;
+}
+
+const str = "Hello, world!";
+const char = "o";
+
+const occurrences = countOccurrences(str, char);
+console.log(`The character '${char}' occurs ${occurrences} times in the string.`);
+The character 'o' occurs 2 times in the string.
