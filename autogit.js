c85331f133581571a034f05af279f6d82bd7b0e0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:22:21 +0800

    1-15-2024 21:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,4 @@
-function findCommonElements(array1, array2) {
-  return array1.filter(element => array2.includes(element));
-}
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  const set = new Set(array2);
-  return array1.filter(element => set.has(element));
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  return array1.reduce((commonElements, element) => {
-    if (array2.includes(element)) {
-      commonElements.push(element);
-    }
-    return commonElements;
-  }, []);
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+console.log(lowercaseStr); // Output: hello world
