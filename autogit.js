8db32d39437c0d77c3095a681c685c8439195ab8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:10:40 +0800

    1-15-2024 21:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,37 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-  return arr;
 }
+function findNthNodeFromEnd(head, n) {
+  let slow = head;
+  let fast = head;
+  
+  // Move the fast pointer n nodes ahead
+  for (let i = 0; i < n; i++) {
+    if (fast === null) {
+      return null; // n is greater than the length of the linked list
+    }
+    fast = fast.next;
+  }
+  
+  // Move both pointers until the fast pointer reaches the end
+  while (fast !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  
+  return slow;
+}
+// Create a linked list: 1 -> 2 -> 3 -> 4 -> 5 -> null
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
 
-// Example usage:
-var unsortedArray = [5, 3, 8, 1, 2, 7];
-var sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 5, 7, 8]
+let n = 2; // Find the 2nd node from the end
+
+let result = findNthNodeFromEnd(head, n);
+console.log(result.data); // Output: 4
