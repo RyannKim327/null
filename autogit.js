ef79eadcf11e3b81dd02ee047b7f49533fd01ee5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:12:16 +0800

    1-15-2024 21:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,8 @@
-let str = "Hello, World!";
-let reversedStr = str.split("").reverse().join("");
-console.log(reversedStr);
-let str = "Hello, World!";
-let reversedStr = "";
-for(let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-}
-console.log(reversedStr);
-function reverseString(str) {
-    if (str === "") {
-        return "";
-    } else {
-        return reverseString(str.substr(1)) + str.charAt(0);
-    }
-}
-
-let str = "Hello, World!";
-let reversedStr = reverseString(str);
-console.log(reversedStr);
-!dlroW ,olleH
+let string1 = "Hello";
+let string2 = "World";
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString); // Output: "Hello World"
+let string1 = "Hello";
+let string2 = "World";
+let concatenatedString = string1.concat(" ", string2);
+console.log(concatenatedString); // Output: "Hello World"
