ea90a54f78729b3d5b038e9398d1d894ecdd4136
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 21:10:25 +0800

    1-15-2024 21:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,18 @@
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
 }
 
-// Example usage
-var base = 5;
-var height = 3;
-var triangleArea = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + triangleArea);
+// Example usage:
+var unsortedArray = [5, 3, 8, 1, 2, 7];
+var sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 3, 5, 7, 8]
