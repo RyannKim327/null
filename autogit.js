74127a192ffcdf88fd7cea9fb018c11fd8d9a03c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 20:10:21 +0800

    1-15-2024 20:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,15 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  var swapped;
-  do {
-    swapped = false;
-    for (var i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
-  } while (swapped);
-}
-var arr = [5, 3, 8, 2, 1, 4];
-bubbleSort(arr);
-console.log(arr);
-function bubbleSort(arr) {
-  var len = arr.length;
-  var swapped;
-  do {
-    swapped = false;
-    for (var i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
-    }
-  } while (swapped);
+  }
+  return true;
 }
 
 // Example usage
-var arr = [5, 3, 8, 2, 1, 4];
-bubbleSort(arr);
-console.log(arr); // [1, 2, 3, 4, 5, 8]
+const array1 = [1, 2, 3, 4, 5];
+console.log(isSortedAscending(array1));  // Output: true
+
+const array2 = [1, 3, 2, 4, 5];
+console.log(isSortedAscending(array2));  // Output: false
