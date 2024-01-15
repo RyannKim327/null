d3f012f15c29a70610b0fbd84142d7cf6b8fb451
RyannKim327 <rksesgundo123@gmail.com>
2024-01-15 19:56:56 +0800

    1-15-2024 19:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,7 @@
-function breadthFirstSearch(startNode) {
-  const queue = [startNode]; // Initialize the queue with the starting node
-  const visited = new Set(); // Create a set to store the visited nodes
-
-  while (queue.length !== 0) {
-    const currentNode = queue.shift(); // Dequeue a node from the queue
-
-    if (visited.has(currentNode)) {
-      continue; // Skip to the next iteration if the node has been visited already
-    }
-
-    visited.add(currentNode); // Mark the current node as visited
-    console.log(currentNode); // Process the current node (print it in this case)
-
-    // Enqueue all adjacent nodes that have not been visited
-    currentNode.adjacentNodes.forEach((adjacentNode) => {
-      if (!visited.has(adjacentNode)) {
-        queue.push(adjacentNode);
-      }
-    });
-  }
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
+
+var input = 'Hello World';
+var output = removeVowels(input);
+console.log(output);  // Output: "Hll Wrld"
