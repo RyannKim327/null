ad3c66125c98f118acd9ae677ccfb1338b12dc11
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:37:29 +0800

    1-17-2024 0:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,8 @@
-function bubbleSort(array) {
-  let len = array.length;
-  let swapped;
-
-  do {
-    swapped = false;
-
-    for (let i = 0; i < len - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        let temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
-        swapped = true;
-      }
-    }
-
-    len--;
-  } while (swapped);
-
-  return array;
-}
-
-// Example usage:
-let unsortedArray = [5, 3, 8, 2, 1, 4];
-let sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
