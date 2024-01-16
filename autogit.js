d63aa099e6af80f9742630c34189337b36f91c20
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:48:09 +0800

    1-16-2024 23:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,11 @@
-function getIntersectionNode(headA, headB) {
-    let p1 = headA;
-    let p2 = headB;
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
 
-    while (p1 !== p2) {
-        p1 = p1 ? p1.next : headB;
-        p2 = p2 ? p2.next : headA;
-    }
+const commonElements = array1.filter(element => array2.includes(element));
+console.log(commonElements); // Output: [4, 5]
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
 
-    return p1;
-}
+const set1 = new Set(array1);
+const commonElements = [...new Set(array2.filter(element => set1.has(element)))];
+console.log(commonElements); // Output: [4, 5]
