b04c4fc99dce76869f85b2a0033211176e3e29bf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:51:52 +0800

    1-17-2024 0:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,3 @@
-function depthLimitedSearch(node, target, depthLimit) {
-  if (node.value === target) {
-    return node;
-  }
-
-  if (depthLimit === 0) {
-    return null;
-  }
-
-  for (const childNode of node.children) {
-    const result = depthLimitedSearch(childNode, target, depthLimit - 1);
-    if (result) {
-      return result;
-    }
-  }
-
-  return null;
-}
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: "hello world"
