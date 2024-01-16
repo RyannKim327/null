c65cacd8a0aafc9e7222689d3f4d6fd3b0a50f37
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:48:39 +0800

    1-16-2024 23:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,40 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+function fibonacciSearch(arr, x) {
+  let fibM2 = 0;
+  let fibM1 = 1;
+  let fibM = fibM2 + fibM1;
 
-const commonElements = array1.filter(element => array2.includes(element));
-console.log(commonElements); // Output: [4, 5]
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+  while (fibM < arr.length) {
+    fibM2 = fibM1;
+    fibM1 = fibM;
+    fibM = fibM2 + fibM1;
+  }
 
-const set1 = new Set(array1);
-const commonElements = [...new Set(array2.filter(element => set1.has(element)))];
-console.log(commonElements); // Output: [4, 5]
+  let offset = -1;
+
+  while (fibM > 1) {
+    let i = Math.min(offset + fibM2, arr.length - 1);
+
+    if (arr[i] < x) {
+      fibM = fibM1;
+      fibM1 = fibM2;
+      fibM2 = fibM - fibM1;
+      offset = i;
+    } else if (arr[i] > x) {
+      fibM = fibM2;
+      fibM1 = fibM1 - fibM2;
+      fibM2 = fibM - fibM1;
+    } else {
+      return i;
+    }
+  }
+
+  if (fibM1 && arr[offset + 1] === x) {
+    return offset + 1;
+  }
+
+  return -1;
+}
+const array = [1, 3, 5, 9, 13, 21, 34, 55, 89];
+const value = 13;
+
+console.log(fibonacciSearch(array, value)); // Output: 4
