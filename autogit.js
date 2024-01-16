69b8d58ff9151c443e629dc1b90620cd69f1cfab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:13:24 +0800

    1-17-2024 1:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,22 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
+function findMajorityElement(arr) {
+  let map = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]] === undefined) {
+      map[arr[i]] = 1;
+    } else {
+      map[arr[i]]++;
+    }
+
+    if (map[arr[i]] > arr.length / 2) {
+      return arr[i];
+    }
+  }
+
+  return null;
 }
 
-var input = "Hello, World!";
-var output = removeVowels(input);
-
-console.log(output); // Output: Hll, Wrld!
+// Example usage:
+const arr = [1, 2, 3, 2, 2, 2, 5, 4, 2];
+const majorityElement = findMajorityElement(arr);
+console.log(majorityElement); // Output: 2
