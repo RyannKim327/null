909147e1269a4bed210abe696505121c2f70dd20
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:07:32 +0800

    1-17-2024 0:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,24 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
+function burrowsWheelerTransform(input) {
+  // Add end of text marker
+  input += '\0';
 
-console.log(array); // Output: [5, 4, 3, 2, 1]
+  // Generate all rotations
+  var rotations = [];
+  for (var i = 0; i < input.length; i++) {
+    rotations.push(input.slice(i) + input.slice(0, i));
+  }
+
+  // Sort rotations
+  rotations.sort();
+
+  // Extract last characters of each rotation
+  var result = '';
+  for (var i = 0; i < rotations.length; i++) {
+    result += rotations[i][rotations[i].length - 1];
+  }
+
+  return result;
+}
+var input = 'banana';
+var transformed = burrowsWheelerTransform(input);
+console.log(transformed); // Outputs: 'annb\0aa'
