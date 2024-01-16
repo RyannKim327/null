b822a8a84760adc37dfd090e9696c25b0b502e92
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:29:01 +0800

    1-16-2024 23:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,5 @@
-function isPrime(number) {
-  // Check if the number is less than 2, as prime numbers start from 2
-  if (number < 2) {
-    return false;
-  }
+// Taking input from user
+var userInput = prompt("Enter a value:");
 
-  // Check for divisibility from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-// Example usage:
-console.log(isPrime(17));  // Returns: true
-console.log(isPrime(6));   // Returns: false
+// Displaying the input as output
+console.log("You entered: " + userInput);
