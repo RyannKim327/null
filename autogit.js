3fecb5b75710ba1575efe2b6c1252823b74dbfb5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:02:29 +0800

    1-17-2024 0:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,14 @@
-class Node {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
-  }
-}
+function reverseLinkedList(head) {
+    let previous = null;
+    let current = head;
+    let next = null;
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  add(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
+    while (current !== null) {
+        next = current.next;
+        current.next = previous;
+        previous = current;
+        current = next;
     }
-  }
 
-  getLength() {
-    let count = 0;
-    let current = this.head;
-    while (current) {
-      count++;
-      current = current.next;
-    }
-    return count;
-  }
+    return previous;
 }
-
-// Example usage
-const list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(3);
-console.log(list.getLength()); // Output: 3
