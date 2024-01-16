261fc14d83ddcc6e0e786ffd8746a0c33efa45ab
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:05:06 +0800

    1-16-2024 23:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,64 @@
-function selectionSort(array) {
-  const length = array.length;
-  
-  for (let i = 0; i < length - 1; i++) {
-    let minIndex = i;
-    
-    for (let j = i + 1; j < length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
+function bidirectionalSearch(start, goal) {
+  const forwardQueue = [start];
+  const backwardQueue = [goal];
+  const forwardVisited = new Set();
+  const backwardVisited = new Set();
+  const forwardParent = new Map();
+  const backwardParent = new Map();
+
+  forwardVisited.add(start);
+  backwardVisited.add(goal);
+
+  while (forwardQueue.length && backwardQueue.length) {
+    // Expand forward search
+    const currentForward = forwardQueue.shift();
+    for (const neighbor of getNeighbors(currentForward)) {
+      if (!forwardVisited.has(neighbor)) {
+        forwardQueue.push(neighbor);
+        forwardVisited.add(neighbor);
+        forwardParent.set(neighbor, currentForward);
+      }
+      if (backwardVisited.has(neighbor)) {
+        return getPath(neighbor, forwardParent, backwardParent);
       }
     }
-    
-    if (minIndex !== i) {
-      [array[i], array[minIndex]] = [array[minIndex], array[i]];
+
+    // Expand backward search
+    const currentBackward = backwardQueue.shift();
+    for (const neighbor of getNeighbors(currentBackward)) {
+      if (!backwardVisited.has(neighbor)) {
+        backwardQueue.push(neighbor);
+        backwardVisited.add(neighbor);
+        backwardParent.set(neighbor, currentBackward);
+      }
+      if (forwardVisited.has(neighbor)) {
+        return getPath(neighbor, forwardParent, backwardParent);
+      }
     }
   }
-  
-  return array;
+
+  return null; // No path found
 }
 
-// Example usage:
-const unsortedArray = [4, 2, 1, 5, 3];
-const sortedArray = selectionSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5]
+function getNeighbors(node) {
+  // Implement this function based on your graph structure
+  // It should return an array of neighboring nodes for the given node
+}
+
+function getPath(intersectingNode, forwardParent, backwardParent) {
+  const path = [];
+  let currentNode = intersectingNode;
+
+  while (currentNode) {
+    path.unshift(currentNode);
+    currentNode = forwardParent.get(currentNode);
+  }
+
+  currentNode = backwardParent.get(intersectingNode);
+  while (currentNode) {
+    path.push(currentNode);
+    currentNode = backwardParent.get(currentNode);
+  }
+
+  return path;
+}
