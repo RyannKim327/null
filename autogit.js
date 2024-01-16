01276423b42f0690aa13728b155419f02a8561ed
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:25:33 +0800

    1-17-2024 2:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,29 @@
-let myArray = [1, 2, 3, 4, 5];
-myArray.reverse();
+function findCommonElements(arr1, arr2) {
+  let commonElements = [];
 
-console.log(myArray); // Output: [5, 4, 3, 2, 1]
-let myArray = [1, 2, 3, 4, 5];
-let reversedArray = myArray.slice().reverse();
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      commonElements.push(arr1[i]);
+    }
+  }
 
-console.log(myArray); // Output: [1, 2, 3, 4, 5]
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+  return commonElements;
+}
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((element) => arr2.includes(element));
+}
+function findCommonElements(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const commonElements = new Set();
+
+  arr2.forEach((element) => {
+    if (set1.has(element)) {
+      commonElements.add(element);
+    }
+  });
+
+  return Array.from(commonElements);
+}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7];
+console.log(findCommonElements(array1, array2));  // Output: [4, 5]
