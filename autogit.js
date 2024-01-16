3a8e26df7c9b0828974ef963f43c5d320f564646
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:35:17 +0800

    1-17-2024 0:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,41 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function connectToServer() {
+    return new Promise((resolve, reject) => {
+        // Create a new XMLHttpRequest object
+        const xhr = new XMLHttpRequest();
+
+        // Define the request URL and method
+        const url = 'http://example.com/api';
+        const method = 'GET';
+
+        // Configure the request
+        xhr.open(method, url, true);
+
+        // Set the callback for when the request is complete
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject(new Error(`Request failed with status ${xhr.status}`));
+            }
+        };
+
+        // Set the callback for network errors
+        xhr.onerror = function () {
+            reject(new Error('Network error'));
+        };
+
+        // Send the request
+        xhr.send();
+    });
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  append(value) {
-    const newNode = new Node(value);
-
-    if (this.tail) {
-      this.tail.next = newNode;
-    } else {
-      this.head = newNode;
-    }
-
-    this.tail = newNode;
-  }
-
-  prepend(value) {
-    const newNode = new Node(value);
-    newNode.next = this.head;
-    this.head = newNode;
-
-    if (!this.tail) {
-      this.tail = newNode;
-    }
-  }
-
-  delete(value) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.value === value) {
-      this.head = this.head.next;
-      if (!this.head) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next) {
-      if (currentNode.next.value === value) {
-        currentNode.next = currentNode.next.next;
-        if (!currentNode.next) {
-          this.tail = currentNode;
-        }
-        return;
-      }
-      currentNode = currentNode.next;
-    }
-  }
-
-  search(value) {
-    let currentNode = this.head;
-    while (currentNode) {
-      if (currentNode.value === value) {
-        return currentNode;
-      }
-      currentNode = currentNode.next;
-    }
-    return null;
-  }
-}
-const linkedList = new LinkedList();
-linkedList.append(10);
-linkedList.append(20);
-linkedList.append(30);
-linkedList.prepend(5);
-linkedList.delete(20);
-console.log(linkedList.search(10)); // Output: Node { value: 10, next: Node { ... } }
-console.log(linkedList.search(20)); // Output: null
+// Usage example
+connectToServer()
+    .then(response => {
+        console.log('Response:', response);
+        // Do something with the response
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        // Handle the error
+    });
