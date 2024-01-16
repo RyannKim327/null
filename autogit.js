a324d86377a4b370af0247b37044b4a46318d8d2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:29:59 +0800

    1-17-2024 1:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,13 @@
-function shellSort(array) {
-  const gaps = [701, 301, 132, 57, 23, 10, 4, 1];
-
-  for (let gap of gaps) {
-    for (let i = gap; i < array.length; i++) {
-      let temp = array[i];
-      let j;
-
-      for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
-        array[j] = array[j - gap];
-      }
-
-      array[j] = temp;
-    }
+function findMedianSortedArrays(array1, array2) {
+  let combinedArray = array1.concat(array2).sort((a, b) => a - b);
+  let length = combinedArray.length;
+  let median;
+  
+  if (length % 2 === 0) {
+    median = (combinedArray[length / 2 - 1] + combinedArray[length / 2]) / 2;
+  } else {
+    median = combinedArray[Math.floor(length / 2)];
   }
-
-  return array;
+  
+  return median;
 }
-
-// Example usage:
-const arr = [6, 5, 3, 1, 8, 7, 2, 4];
-console.log(shellSort(arr));
