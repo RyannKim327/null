93c5a347b33da1b72360973eccb5a1e5b0c51d22
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:22:42 +0800

    1-17-2024 0:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,110 +1,43 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+function bubbleSort(arr) {
+  // Bubble sort logic goes here
+}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    // Bubble sort logic goes here
   }
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      // Compare adjacent elements and swap if necessary
+    }
   }
-
-  isEmpty() {
-    return this.head === null;
+}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        // Swap elements
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+  }
+}
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        // Swap elements
+        let temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
   }
   
-  // Add a node to the end of the list
-  append(data) {
-    const newNode = new Node(data);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  // Add a node to the beginning of the list
-  prepend(data) {
-    const newNode = new Node(data);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  // Remove the first occurrence of a node with the given data
-  remove(data) {
-    if (this.isEmpty()) {
-      return;
-    }
-
-    // If the head node needs to be removed
-    if (this.head.data === data) {
-      this.head = this.head.next;
-
-      // Check if the list becomes empty after removing the head
-      if (this.head === null) {
-        this.tail = null;
-      }
-
-      return;
-    }
-
-    let current = this.head;
-    let previous = null;
-
-    while (current !== null) {
-      if (current.data === data) {
-        previous.next = current.next;
-
-        // If the last node needs to be removed, update the tail
-        if (current === this.tail) {
-          this.tail = previous;
-        }
-
-        return;
-      }
-
-      previous = current;
-      current = current.next;
-    }
-  }
-
-  // Print the linked list
-  print() {
-    if (this.isEmpty()) {
-      console.log("Linked list is empty.");
-      return;
-    }
-
-    let current = this.head;
-    let listStr = "";
-
-    while (current !== null) {
-      listStr += current.data + " -> ";
-      current = current.next;
-    }
-
-    listStr += "null";
-    console.log(listStr);
-  }
+  return arr;
 }
-
-// Usage example:
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.prepend(0);
-linkedList.print(); // Output: 0 -> 1 -> 2 -> 3 -> null
-linkedList.remove(2);
-linkedList.print(); // Output: 0 -> 1 -> 3 -> null
+let array = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(array)); // Output: [ 11, 12, 22, 25, 34, 64, 90 ]
