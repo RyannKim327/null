e7171a00080a7b6cde8d5acfd8371b6b8efe9b12
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:30:07 +0800

    1-17-2024 2:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,47 @@
-class HashTable {
-  constructor(size) {
-    this.size = size;
-    this.buckets = Array(size);
+function findMedianSortedArrays(nums1, nums2) {
+  const merged = mergeSortedArrays(nums1, nums2);
+  const length = merged.length;
+  
+  if (length % 2 === 0) {
+    const middleIndex = length / 2;
+    const previousIndex = middleIndex - 1;
+    return (merged[middleIndex] + merged[previousIndex]) / 2;
+  } else {
+    const middleIndex = Math.floor(length / 2);
+    return merged[middleIndex];
   }
 }
-HashTable.prototype.hash = function (key) {
-  let hash = 0;
-  for (let i = 0; i < key.length; i++) {
-    hash += key.charCodeAt(i);
-  }
-  return hash % this.size;
-};
-HashTable.prototype.put = function (key, value) {
-  const index = this.hash(key);
-  if (!this.buckets[index]) {
-    this.buckets[index] = [];
-  }
-  this.buckets[index].push({ key, value });
-};
-HashTable.prototype.get = function (key) {
-  const index = this.hash(key);
-  if (!this.buckets[index]) {
-    return null;
-  }
-  for (let i = 0; i < this.buckets[index].length; i++) {
-    if (this.buckets[index][i].key === key) {
-      return this.buckets[index][i].value;
+
+function mergeSortedArrays(nums1, nums2) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
+  
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) {
+      merged.push(nums1[i]);
+      i++;
+    } else {
+      merged.push(nums2[j]);
+      j++;
     }
   }
-  return null;
-};
-const hashTable = new HashTable(10);
-hashTable.put("apple", 5);
-hashTable.put("banana", 2);
-console.log(hashTable.get("apple")); // Output: 5
-console.log(hashTable.get("banana")); // Output: 2
-console.log(hashTable.get("orange")); // Output: null
+
+  while (i < nums1.length) {
+    merged.push(nums1[i]);
+    i++;
+  }
+  
+  while (j < nums2.length) {
+    merged.push(nums2[j]);
+    j++;
+  }
+  
+  return merged;
+}
+
+// Example usage:
+const nums1 = [1, 3];
+const nums2 = [2, 4, 5];
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median);
