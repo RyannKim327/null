39a9a61eb02b302a46e075ec043e73fa0895452b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:21:57 +0800

    1-16-2024 23:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,58 @@
-const graph = {
-  A: { B: 5, C: 2 },
-  B: { A: 5, C: 1, D: 3 },
-  C: { A: 2, B: 1, D: 2 },
-  D: { B: 3, C: 2 },
-};
-function dijkstra(graph, startNode) {
-  // Step 1: Initialize data structures
-  const distances = {};
-  const visited = {};
-
-  for (let vertex in graph) {
-    distances[vertex] = Infinity;
-    visited[vertex] = false;
+class HashTable {
+  constructor() {
+    this.table = new Array(100);
   }
 
-  distances[startNode] = 0;
+  // Compute the hash code for a given key
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.table.length;
+  }
 
-  // Step 2: Traverse the graph
-  for (let vertex in graph) {
-    let currentNode = minDistanceNode(distances, visited);
-    visited[currentNode] = true;
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const index = this.hash(key);
+    if (this.table[index] === undefined) {
+      this.table[index] = [];
+    }
+    this.table[index].push({ key, value });
+  }
 
-    for (let neighbor in graph[currentNode]) {
-      let distance = graph[currentNode][neighbor];
-      let totalDistance = distances[currentNode] + distance;
+  // Get the value associated with a given key
+  get(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i].key === key) {
+          return this.table[index][i].value;
+        }
+      }
+    }
+    return undefined;
+  }
 
-      if (totalDistance < distances[neighbor]) {
-        distances[neighbor] = totalDistance;
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i].key === key) {
+          this.table[index].splice(i, 1);
+          break;
+        }
       }
     }
   }
-
-  // Step 3: Output the shortest distances
-  return distances;
 }
-function minDistanceNode(distances, visited) {
-  let minDistance = Infinity;
-  let minNode = null;
 
-  for (let node in distances) {
-    let distance = distances[node];
-
-    if (distance < minDistance && !visited[node]) {
-      minDistance = distance;
-      minNode = node;
-    }
-  }
-
-  return minNode;
-}
-const startNode = 'A';
-const shortestDistances = dijkstra(graph, startNode);
-
-console.log(shortestDistances);
+// Example usage:
+const myHashTable = new HashTable();
+myHashTable.insert("name", "John");
+myHashTable.insert("age", 30);
+console.log(myHashTable.get("name")); // Output: John
+console.log(myHashTable.get("age"));  // Output: 30
+myHashTable.remove("age");
+console.log(myHashTable.get("age"));  // Output: undefined
