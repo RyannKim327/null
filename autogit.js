a7edb9530cb6caab608544cf7ee37a4236c67efc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:24:01 +0800

    1-16-2024 23:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,8 @@
-function isPalindrome(str) {
-  var i = 0; // Start from the beginning of the string
-  var j = str.length - 1; // Start from the end of the string
+const str = 'Hello, World!';
+const substring = 'World';
 
-  // Compare characters from both ends until i < j
-  while (i < j) {
-    // If characters don't match, return false
-    if (str[i] !== str[j]) {
-      return false;
-    }
-    i++; // Move i to the next character
-    j--; // Move j to the previous character
-  }
-
-  // If the loop completes without returning false, the string is a palindrome
-  return true;
+if (str.includes(substring)) {
+  console.log('Substring found!');
+} else {
+  console.log('Substring not found!');
 }
-console.log(isPalindrome("level")); // true
-console.log(isPalindrome("hello")); // false
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("")); // true
