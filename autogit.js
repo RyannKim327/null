44269fbdec914344f38f2c960274c3d8c81781f0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:10:03 +0800

    1-17-2024 0:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,74 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  
-}
-if (start > end) {
-  return -1;
-}
-const mid = Math.floor((start + end) / 2);
-if (arr[mid] === target) {
-  return mid;
-}
-if (arr[mid] > target) {
-  return binarySearch(arr, target, start, mid - 1);
-}
-if (arr[mid] < target) {
-  return binarySearch(arr, target, mid + 1, end);
-}
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  if (start > end) {
-    return -1;
+class BTreeNode {
+  constructor(t, leaf = true) {
+    this.keys = [];
+    this.children = [];
+    this.leaf = leaf;
+    this.t = t;
   }
-  
-  const mid = Math.floor((start + end) / 2);
-  
-  if (arr[mid] === target) {
-    return mid;
-  }
-  
-  if (arr[mid] > target) {
-    return binarySearch(arr, target, start, mid - 1);
-  }
-  
-  return binarySearch(arr, target, mid + 1, end);
-}
-const arr = [1, 5, 10, 12, 15];
-const target = 10;
 
-const index = binarySearch(arr, target);
+  insertNonFull(key) {
+    let i = this.keys.length - 1;
 
-if (index !== -1) {
-  console.log(`Found ${target} at index ${index}`);
-} else {
-  console.log(`${target} not found in the array`);
+    if (this.leaf) {
+      while (i >= 0 && key < this.keys[i]) {
+        this.keys[i + 1] = this.keys[i];
+        i--;
+      }
+
+      this.keys[i + 1] = key;
+    } else {
+      while (i >= 0 && key < this.keys[i]) {
+        i--;
+      }
+
+      if (this.children[i + 1].keys.length === 2 * this.t - 1) {
+        this.splitChild(i + 1, this.children[i + 1]);
+
+        if (key > this.keys[i + 1]) {
+          i++;
+        }
+      }
+
+      this.children[i + 1].insertNonFull(key);
+    }
+  }
+
+  splitChild(i, y) {
+    const newNode = new BTreeNode(y.t, y.leaf);
+
+    this.children.splice(i + 1, 0, newNode);
+    this.keys.splice(i, 0, y.keys[y.keys.length - this.t]);
+
+    newNode.keys = y.keys.splice(this.t, y.keys.length);
+    if (!y.leaf) {
+      newNode.children = y.children.splice(this.t, y.children.length);
+    }
+  }
 }
+
+class BTree {
+  constructor(t) {
+    this.root = new BTreeNode(t, true);
+    this.t = t;
+  }
+
+  insert(key) {
+    const root = this.root;
+
+    if (root.keys.length === (2 * this.t) - 1) {
+      const newRoot = new BTreeNode(this.t, false);
+      this.root = newRoot;
+      newRoot.children[0] = root;
+      newRoot.splitChild(0, root);
+      newRoot.insertNonFull(key);
+    } else {
+      root.insertNonFull(key);
+    }
+  }
+}
+const bTree = new BTree(3);
+bTree.insert(10);
+bTree.insert(20);
+bTree.insert(30);
+
+console.log(bTree.root); // Check the root node
