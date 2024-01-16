9e494a2fb7a694cc91462f275c8080529056cd26
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:27:42 +0800

    1-17-2024 1:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,7 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  // ... implementation goes here
-}
-if (start > end) {
-  return -1;
-}
-var mid = Math.floor((start + end) / 2);
-if (arr[mid] === target) {
-  return mid;
-}
-if (arr[mid] > target) {
-  return binarySearch(arr, target, start, mid - 1);
-}
-if (arr[mid] < target) {
-  return binarySearch(arr, target, mid + 1, end);
-}
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-  if (start > end) {
-    return -1;
-  }
+let str = "  Hello,   World!   ";
 
-  var mid = Math.floor((start + end) / 2);
+// Using regular expression to remove whitespace
+let trimmedStr = str.replace(/\s/g, "");
 
-  if (arr[mid] === target) {
-    return mid;
-  }
-
-  if (arr[mid] > target) {
-    return binarySearch(arr, target, start, mid - 1);
-  }
-
-  if (arr[mid] < target) {
-    return binarySearch(arr, target, mid + 1, end);
-  }
-}
+console.log(trimmedStr);
+Hello,World!
