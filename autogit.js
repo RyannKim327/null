7dbb1c6c8890310ace7080e9526e171477dab7a6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:16:19 +0800

    1-17-2024 1:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,22 @@
-function findMedianSortedArrays(nums1, nums2) {
-  const merged = mergeSortedArrays(nums1, nums2);
-  const length = merged.length;
-  
-  if (length % 2 === 1) {
-    return merged[Math.floor(length / 2)];
-  } else {
-    const mid = length / 2;
-    return (merged[mid - 1] + merged[mid]) / 2;
-  }
-}
+function findFirstRepeatedChar(str) {
+  const charFrequency = {};
 
-function mergeSortedArrays(nums1, nums2) {
-  const merged = [];
-  let i = 0;
-  let j = 0;
-  
-  while (i < nums1.length && j < nums2.length) {
-    if (nums1[i] < nums2[j]) {
-      merged.push(nums1[i]);
-      i++;
-    } else {
-      merged.push(nums2[j]);
-      j++;
+  for (let char of str) {
+    if (charFrequency[char]) {
+      return char; // First repeated character found
     }
-  }
-  
-  while (i < nums1.length) {
-    merged.push(nums1[i]);
-    i++;
+    charFrequency[char] = true;
   }
 
-  while (j < nums2.length) {
-    merged.push(nums2[j]);
-    j++;
-  }
-  
-  return merged;
+  return null; // No repeated character found
 }
 
 // Example usage
-const nums1 = [1, 3, 5];
-const nums2 = [2, 4, 6];
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 3.5
+const inputString = "Hello World!";
+const firstRepeatedChar = findFirstRepeatedChar(inputString);
+
+if (firstRepeatedChar) {
+  console.log("The first repeated character is:", firstRepeatedChar);
+} else {
+  console.log("No repeated character found in the string.");
+}
