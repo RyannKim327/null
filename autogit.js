83b46bb34dd3813e9178ed7b8db85acde2ace589
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:09:21 +0800

    1-16-2024 23:9 [User mode]

---
+++ autogit.js
@@ -0,0 +1,23 @@
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) {
+    return '';
+  }
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, prefix.length - 1);
+      if (prefix === '') {
+        return '';
+      }
+    }
+  }
+
+  return prefix;
+}
+
+// Example usage:
+const strings = ['flower', 'flow', 'flight'];
+const result = longestCommonPrefix(strings);
+console.log(result); // Output: 'fl'
