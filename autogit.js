2f7a77d8589979f47263ec9bf58f18516a4f7a46
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:54:28 +0800

    1-17-2024 2:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,17 @@
-function merge(subarray1, subarray2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
+function bubbleSort(arr) {
+  var len = arr.length;
 
-  while (i < subarray1.length && j < subarray2.length) {
-    if (subarray1[i] <= subarray2[j]) {
-      mergedArray.push(subarray1[i]);
-      i++;
-    } else {
-      mergedArray.push(subarray2[j]);
-      j++;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      // Swap adjacent elements if they are in the wrong order
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
   }
-
-  while (i < subarray1.length) {
-    mergedArray.push(subarray1[i]);
-    i++;
-  }
-
-  while (j < subarray2.length) {
-    mergedArray.push(subarray2[j]);
-    j++;
-  }
-
-  return mergedArray;
+  return arr;
 }
-function mergeSort(array) {
-  if (array.length <= 1) {
-    return array;
-  }
-
-  const mid = Math.floor(array.length / 2);
-  const leftHalf = array.slice(0, mid);
-  const rightHalf = array.slice(mid);
-
-  const sortedLeftHalf = mergeSort(leftHalf);
-  const sortedRightHalf = mergeSort(rightHalf);
-
-  return merge(sortedLeftHalf, sortedRightHalf);
-}
-const array = [5, 2, 9, 1, 7, 6, 4, 3, 8];
-const sortedArray = mergeSort(array);
-console.log(sortedArray);  // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numbers = [5, 3, 8, 4, 2];
+console.log(bubbleSort(numbers)); // Output: [2, 3, 4, 5, 8]
