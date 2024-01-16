19e41565fcd99e6e3c1461a3f78e1f782988ecbe
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:43:48 +0800

    1-17-2024 2:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,15 @@
-function dijkstra(graph, source) {
-  const distance = {};
-  const visited = new Set();
-  const pq = new PriorityQueue();
-
-  // Initialize distances
-  for (const node in graph) {
-    distance[node] = Infinity;
-  }
-  distance[source] = 0;
-
-  pq.enqueue(source, 0);
-
-  while (!pq.isEmpty()) {
-    const { element: current } = pq.dequeue();
-
-    if (visited.has(current)) continue;
-
-    visited.add(current);
-
-    for (const neighbor in graph[current]) {
-      const weight = graph[current][neighbor];
-      const totalDistance = distance[current] + weight;
-
-      if (totalDistance < distance[neighbor]) {
-        distance[neighbor] = totalDistance;
-        pq.enqueue(neighbor, totalDistance);
-      }
-    }
-  }
-
-  return distance;
+function countOccurrences(string, character) {
+  var charArray = string.split('');
+  var filteredArray = charArray.filter(function (char) {
+    return char === character;
+  });
+  return filteredArray.length;
 }
+
+// Example usage:
+var myString = "Hello, World!";
+var characterToCount = "o";
+var occurrenceCount = countOccurrences(myString, characterToCount);
+
+console.log("Number of occurrences of '" + characterToCount + "': " + occurrenceCount);
+Number of occurrences of 'o': 2
