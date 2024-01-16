81781b64ab816cc5c8544ca30a6d81e079ddabbc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:58:23 +0800

    1-16-2024 23:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,25 @@
-let str = "Hello, World!";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // output: "hello, world!"
+function bwt(input) {
+
+}
+function rotate(input) {
+  return input.slice(-1) + input.slice(0, -1);
+}
+function rotations(input) {
+  let rotations = [];
+  for (let i = 0; i < input.length; i++) {
+    rotations.push(rotate(input));
+    input = rotate(input);
+  }
+  return rotations;
+}
+function bwt(input) {
+  let rotationsArray = rotations(input);
+  let sortedArray = rotationsArray.sort();
+  let bwtOutput = "";
+  for (let i = 0; i < sortedArray.length; i++) {
+    bwtOutput += sortedArray[i].slice(-1);
+  }
+  return bwtOutput;
+}
+let inputString = "banana";
+console.log(bwt(inputString)); // Output: "annb$aa"
