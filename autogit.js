aa99bf326628828815483fb620ee741d6a5aa3f6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:26:46 +0800

    1-17-2024 1:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,92 +1,20 @@
-class GraphNode {
-  constructor(id) {
-    this.id = id;
-    this.adjacentNodes = [];
-  }
-
-  addAdjacentNode(node) {
-    this.adjacentNodes.push(node);
+let str = "Hello, World!";
+let reversedStr = str.split("").reverse().join("");
+console.log(reversedStr); // Output: "!dlroW ,olleH"
+let str = "Hello, World!";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str.charAt(i);
+}
+console.log(reversedStr); // Output: "!dlroW ,olleH"
+function reverseString(str) {
+  if (str === "") {
+    return "";
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0);
   }
 }
 
-function biDirectionalSearch(startNode, endNode) {
-  let startSet = new Set();
-  let endSet = new Set();
-  let visitedStart = {};
-  let visitedEnd = {};
-
-  startSet.add(startNode);
-  visitedStart[startNode.id] = null;
-
-  endSet.add(endNode);
-  visitedEnd[endNode.id] = null;
-
-  while (startSet.size > 0 && endSet.size > 0) {
-    let commonNode = expandSearch(startSet, visitedStart, visitedEnd);
-    if (commonNode != null) {
-      return getPath(commonNode, visitedStart, visitedEnd);
-    }
-
-    commonNode = expandSearch(endSet, visitedEnd, visitedStart);
-    if (commonNode != null) {
-      return getPath(commonNode, visitedStart, visitedEnd);
-    }
-  }
-
-  return null;
-}
-
-function expandSearch(set, visited, otherVisited) {
-  let currentNode = set.values().next().value;
-  set.delete(currentNode);
-
-  for (let adjacentNode of currentNode.adjacentNodes) {
-    if (!visited.hasOwnProperty(adjacentNode.id)) {
-      set.add(adjacentNode);
-      visited[adjacentNode.id] = currentNode;
-
-      if (otherVisited.hasOwnProperty(adjacentNode.id)) {
-        return adjacentNode;
-      }
-    }
-  }
-
-  return null;
-}
-
-function getPath(commonNode, visitedStart, visitedEnd) {
-  let path = [];
-  let currentNode = commonNode;
-
-  while (currentNode != null) {
-    path.push(currentNode.id);
-    currentNode = visitedStart[currentNode.id];
-  }
-  path.reverse();
-
-  currentNode = visitedEnd[commonNode.id];
-  while (currentNode != null) {
-    path.push(currentNode.id);
-    currentNode = visitedEnd[currentNode.id];
-  }
-
-  return path;
-}
-
-// Example usage
-let node1 = new GraphNode(1);
-let node2 = new GraphNode(2);
-let node3 = new GraphNode(3);
-let node4 = new GraphNode(4);
-let node5 = new GraphNode(5);
-
-node1.addAdjacentNode(node2);
-node2.addAdjacentNode(node1);
-node2.addAdjacentNode(node3);
-node3.addAdjacentNode(node2);
-node3.addAdjacentNode(node4);
-node4.addAdjacentNode(node3);
-node4.addAdjacentNode(node5);
-node5.addAdjacentNode(node4);
-
-console.log(biDirectionalSearch(node1, node5));
+let str = "Hello, World!";
+let reversedStr = reverseString(str);
+console.log(reversedStr); // Output: "!dlroW ,olleH"
