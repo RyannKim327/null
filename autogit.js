ee4426ce35bc5475e6e80ba010fa02398ef6fa1b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:30:56 +0800

    1-16-2024 23:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,18 @@
-function isPrime(number) {
-  // Check if the number is less than 2
-  if (number < 2) {
-    return false;
-  }
-
-  // Iterate from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    // If the number is divisible by any value in this range, it's not prime
-    if (number % i === 0) {
-      return false;
+class ListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
     }
-  }
-
-  // If the number is not divisible by any value in the range, it's prime
-  return true;
 }
-console.log(isPrime(7));   // Output: true
-console.log(isPrime(12));  // Output: false
-console.log(isPrime(97));  // Output: true
+
+function findLinkedListLength(head) {
+    let length = 0;
+    let currentNode = head;
+
+    while (currentNode !== null) {
+        length++;
+        currentNode = currentNode.next;
+    }
+
+    return length;
+}
