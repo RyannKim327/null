efc47d95b5fb8f957ccdb0431408c131e00c0701
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:53:13 +0800

    1-17-2024 1:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,51 @@
-function LinkedListNode(value) {
-  this.value = value;
-  this.next = null;
-}
+function mergeSortIterative(arr) {
+  // Create an array of subarrays with each element in arr
+  let subarrays = arr.map((el) => [el]);
 
-function findLinkedListLength(head) {
-  let length = 0;
-  let current = head;
+  // Merge and sort each adjacent pair of subarrays until only one subarray remains
+  while (subarrays.length > 1) {
+    let mergedArray = [];
 
-  while (current !== null) {
-    length++;
-    current = current.next;
+    for (let i = 0; i < subarrays.length; i += 2) {
+      let mergedSubarray = merge(subarrays[i], subarrays[i + 1]);
+      mergedArray.push(mergedSubarray);
+    }
+
+    subarrays = mergedArray;
   }
 
-  return length;
+  return subarrays[0];
 }
 
-// Example usage:
-const node1 = new LinkedListNode(1);
-const node2 = new LinkedListNode(2);
-const node3 = new LinkedListNode(3);
+// Helper function to merge two sorted arrays
+function merge(arr1, arr2) {
+  let mergedArray = [];
+  let i = 0;
+  let j = 0;
 
-node1.next = node2;
-node2.next = node3;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
 
-const head = node1;
+  // Copy the remaining elements from arr1 or arr2 (if any)
+  while (i < arr1.length) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
 
-const length = findLinkedListLength(head);
-console.log(length); // Output: 3
+  return mergedArray;
+}
+
+// Usage
+const arr = [5, 2, 8, 3, 1, 6, 9, 4, 7];
+console.log(mergeSortIterative(arr)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
