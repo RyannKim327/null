f7622349b5f94f8e481d1372b8fefc01afd39991
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:25:01 +0800

    1-17-2024 0:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,81 +1,9 @@
-class SkipListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = [];
-  }
-}
+const axios = require('axios');
 
-class SkipList {
-  constructor() {
-    this.head = new SkipListNode(-Infinity);
-    this.tail = new SkipListNode(Infinity);
-    this.head.next.push(this.tail);
-  }
-}
-SkipList.prototype.search = function (value) {
-  let currentNode = this.head;
-
-  for (let i = this.maxLevel(); i >= 0; i--) {
-    while (
-      currentNode.next[i] !== this.tail &&
-      currentNode.next[i].value < value
-    ) {
-      currentNode = currentNode.next[i];
-    }
-  }
-
-  return currentNode;
-};
-SkipList.prototype.insert = function (value) {
-  const node = new SkipListNode(value);
-  const update = new Array(this.maxLevel() + 1);
-
-  let currentNode = this.head;
-  for (let i = this.maxLevel(); i >= 0; i--) {
-    while (
-      currentNode.next[i] !== this.tail &&
-      currentNode.next[i].value < value
-    ) {
-      currentNode = currentNode.next[i];
-    }
-    update[i] = currentNode;
-  }
-
-  for (let i = 0; i < node.next.length; i++) {
-    node.next[i] = update[i].next[i];
-    update[i].next[i] = node;
-  }
-};
-SkipList.prototype.remove = function (value) {
-  const update = new Array(this.maxLevel() + 1);
-
-  let currentNode = this.head;
-  for (let i = this.maxLevel(); i >= 0; i--) {
-    while (
-      currentNode.next[i] !== this.tail &&
-      currentNode.next[i].value < value
-    ) {
-      currentNode = currentNode.next[i];
-    }
-    update[i] = currentNode;
-  }
-
-  const nodeToRemove = currentNode.next[0];
-  if (nodeToRemove.value === value) {
-    for (let i = 0; i < nodeToRemove.next.length; i++) {
-      update[i].next[i] = nodeToRemove.next[i];
-    }
-  }
-};
-SkipList.prototype.contains = function (value) {
-  const node = this.search(value);
-  return node.next[0].value === value;
-};
-
-SkipList.prototype.display = function () {
-  let currentNode = this.head.next[0];
-  while (currentNode !== this.tail) {
-    console.log(currentNode.value);
-    currentNode = currentNode.next[0];
-  }
-};
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
