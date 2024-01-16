e5df99316ee26973cde85b6f67f68f7ad06cedb3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:36:33 +0800

    1-17-2024 0:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,12 @@
-let array = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
 
-// Find the index of the element you want to remove
-let index = array.indexOf(3);
+// Find the index of the element
+let index = arr.indexOf(elementToRemove);
 
-if (index !== -1) {
-  // Use the splice() method to remove the element
-  array.splice(index, 1);
+// If the element exists in the array, remove it
+if (index > -1) {
+  arr.splice(index, 1);
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
+console.log(arr); // Output: [1, 2, 4, 5]
