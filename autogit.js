05ccaf8f6b8239779e67eedafeae5b5a21a3b640
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:32:38 +0800

    1-17-2024 0:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,28 @@
-function isPalindrome(str) {
-  // Convert the string to lowercase and remove non-alphanumeric characters
-  str = str.toLowerCase().replace(/[\W_]/g, '');
+function makeRequest(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
 
-  // Reverse the string and compare with the original string
-  return str === str.split('').reverse().join('');
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      callback(null, xhr.responseText);
+    } else {
+      callback(new Error('Request failed with status ' + xhr.status));
+    }
+  };
+
+  xhr.onerror = function() {
+    callback(new Error('Request failed'));
+  };
+
+  xhr.send();
 }
 
-// Test the function
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // Output: true
-console.log(isPalindrome("racecar")); // Output: true
-console.log(isPalindrome("hello")); // Output: false
+// Example usage
+makeRequest('http://example.com/api/data', function(error, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(response);
+    // Do something with the response
+  }
+});
