3c7943798328f82ee78ec651bc2d0b474a22763d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:55:47 +0800

    1-17-2024 0:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -5,94 +5,59 @@ class Node {
   }
 }
 
-class LinkedList {
+class Queue {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0;
   }
 
-  append(value) {
+  enqueue(value) {
     const newNode = new Node(value);
 
-    if (!this.head) {
-      // If the list is empty, set the new node as the head and tail
+    if (this.isEmpty()) {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      // If the list is not empty, append the new node to the tail
       this.tail.next = newNode;
       this.tail = newNode;
     }
+
+    this.size++;
   }
 
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      // If the list is empty, set the new node as the head and tail
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      // If the list is not empty, prepend the new node to the head
-      newNode.next = this.head;
-      this.head = newNode;
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
     }
+
+    const dequeuedNode = this.head;
+
+    if (this.head === this.tail) {
+      this.tail = null;
+    }
+
+    this.head = this.head.next;
+    this.size--;
+
+    return dequeuedNode.value;
   }
 
-  delete(value) {
-    if (!this.head) {
-      // If the list is empty, return
-      return;
-    }
-
-    if (this.head.value === value) {
-      // If the head node should be deleted, update the head
-      this.head = this.head.next;
-
-      if (!this.head) {
-        // If the list becomes empty, update the tail as well
-        this.tail = null;
-      }
-
-      return;
-    }
-
-    let currentNode = this.head;
-
-    while (currentNode.next !== null) {
-      if (currentNode.next.value === value) {
-        // If the next node's value matches the value to be deleted, update the next reference
-        currentNode.next = currentNode.next.next;
-
-        if (currentNode.next === null) {
-          // If the next node is the tail, update the tail
-          this.tail = currentNode;
-        }
-
-        return;
-      }
-
-      currentNode = currentNode.next;
-    }
-  }
-
-  toArray() {
-    const array = [];
-    let currentNode = this.head;
-
-    while (currentNode) {
-      array.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-
-    return array;
+  isEmpty() {
+    return this.size === 0;
   }
 }
-const linkedList = new LinkedList();
+const queue = new Queue();
 
-linkedList.append(5);
-linkedList.append(10);
-linkedList.prepend(2);
-linkedList.delete(5);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
 
-console.log(linkedList.toArray());  // Output: [2, 10]
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 2
+
+queue.enqueue(4);
+console.log(queue.isEmpty()); // Output: false
+console.log(queue.dequeue()); // Output: 3
+console.log(queue.dequeue()); // Output: 4
+console.log(queue.isEmpty()); // Output: true
