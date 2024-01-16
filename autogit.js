462a2589a45e9abf9bd04c26804766d709d52549
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:15:17 +0800

    1-17-2024 1:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,5 @@
-let str = "Hello, World!";
-let reversedStr = str.split('').reverse().join('');
-console.log(reversedStr);
-let str = "Hello, World!";
-let reversedStr = '';
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
-}
-console.log(reversedStr);
-!dlroW ,olleH
+// Get user input
+var userInput = prompt("Enter something:");
+
+// Display the user input
+console.log("You entered: " + userInput);
