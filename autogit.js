d932a61581be60e388b6f38d34e0411562a93bba
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:53:48 +0800

    1-17-2024 1:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,45 @@
-function mergeSortIterative(arr) {
-  // Create an array of subarrays with each element in arr
-  let subarrays = arr.map((el) => [el]);
-
-  // Merge and sort each adjacent pair of subarrays until only one subarray remains
-  while (subarrays.length > 1) {
-    let mergedArray = [];
-
-    for (let i = 0; i < subarrays.length; i += 2) {
-      let mergedSubarray = merge(subarrays[i], subarrays[i + 1]);
-      mergedArray.push(mergedSubarray);
-    }
-
-    subarrays = mergedArray;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  return subarrays[0];
 }
 
-// Helper function to merge two sorted arrays
-function merge(arr1, arr2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      mergedArray.push(arr1[i]);
-      i++;
+  addNode(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
     } else {
-      mergedArray.push(arr2[j]);
-      j++;
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
   }
 
-  // Copy the remaining elements from arr1 or arr2 (if any)
-  while (i < arr1.length) {
-    mergedArray.push(arr1[i]);
-    i++;
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
   }
-  while (j < arr2.length) {
-    mergedArray.push(arr2[j]);
-    j++;
-  }
-
-  return mergedArray;
 }
 
-// Usage
-const arr = [5, 2, 8, 3, 1, 6, 9, 4, 7];
-console.log(mergeSortIterative(arr)); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Example usage
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+
+console.log(list.getLength()); // Output: 4
