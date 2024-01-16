34554b63abd97e58bb324d45ba906e94cf6b09c1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:37:31 +0800

    1-16-2024 23:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,44 @@
-let str = "  remove white space  ";
-let newStr = str.replace(/\s/g, "");
-console.log(newStr);
-"removewhitespace"
-let str = "  remove white space  ";
-let trimmedStr = str.trim();
-let newStr = trimmedStr.replace(/\s/g, "");
-console.log(newStr);
-"removewhitespace"
+function findCommonElements(arr1, arr2) {
+  const commonElements = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        commonElements.push(arr1[i]);
+        break;
+      }
+    }
+  }
+  
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7];
+
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+function findCommonElements(arr1, arr2) {
+  return arr1.filter(value => arr2.includes(value));
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7];
+
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+function findCommonElements(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const commonElements = [];
+
+  for (const value of arr2) {
+    if (set1.has(value)) {
+      commonElements.push(value);
+    }
+  }
+  
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7];
+
+console.log(findCommonElements(array1, array2)); // Output: [4, 5]
