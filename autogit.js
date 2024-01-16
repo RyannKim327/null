457dd9b48f4c3ed7dbf97d67286b5be0b3233083
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:12:22 +0800

    1-17-2024 1:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,24 @@
-function buildLPS(pattern) {
-  const lps = new Array(pattern.length).fill(0);
-  let i = 0, j = 1;
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  while (j < pattern.length) {
-    if (pattern[i] === pattern[j]) {
-      lps[j] = i + 1;
-      i++;
-      j++;
-    } else {
-      if (i !== 0) {
-        i = lps[i - 1];
-      } else {
-        lps[j] = 0;
-        j++;
-      }
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const less = [];
+  const greater = [];
+
+  for (const element of arr) {
+    if (element < pivot) {
+      less.push(element);
+    } else if (element > pivot) {
+      greater.push(element);
     }
   }
 
-  return lps;
+  return [...quicksort(less), pivot, ...quicksort(greater)];
 }
 
-function stringMatch(text, pattern) {
-  const lps = buildLPS(pattern);
-  let i = 0, j = 0;
-
-  while (i < text.length) {
-    if (pattern[j] === text[i]) {
-      i++;
-      j++;
-    }
-
-    if (j === pattern.length) {
-      console.log('Match found at index', i - j);
-      j = lps[j - 1];
-    } else if (i < text.length && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-}
-
-// Example usage
-const text = 'ABABDABACDABABCABAB';
-const pattern = 'ABABCABAB';
-stringMatch(text, pattern);
+// Example usage:
+const array = [9, 4, 2, 7, 3, 1, 6, 8, 5];
+const sortedArray = quicksort(array);
+console.log(sortedArray);
