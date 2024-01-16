dd0cdbb0627672493702e1bca5ff7feac351e8bb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:00:15 +0800

    1-16-2024 22:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,74 @@
-function insertionSort(array) {
-  const arr = [...array];
-
-  for (let i = 1; i < arr.length; i++) {
-    const currentValue = arr[i];
-    let j = i - 1;
-
-    while (j >= 0 && arr[j] > currentValue) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-
-    arr[j + 1] = currentValue;
+class Node {
+  constructor(key, value, forward = []) {
+    this.key = key;
+    this.value = value;
+    this.forward = forward;
   }
-
-  return arr;
 }
-const unsortedArray = [5, 2, 9, 1, 7];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray);  // Output: [1, 2, 5, 7, 9]
+class SkipList {
+  constructor(maxLevel = 16) {
+    this.head = new Node(-Infinity, null, new Array(maxLevel));
+    this.maxLevel = maxLevel;
+  }
+}
+randomLevel() {
+  let level = 1;
+  while (Math.random() < 0.5 && level < this.maxLevel) {
+    level++;
+  }
+  return level;
+}
+insert(key, value) {
+  const update = new Array(this.maxLevel);
+  let current = this.head;
+  
+  for (let i = this.maxLevel - 1; i >= 0; i--) {
+    while (current.forward[i] && current.forward[i].key < key) {
+      current = current.forward[i];
+    }
+    update[i] = current;
+  }
+  
+  const level = this.randomLevel();
+  const newNode = new Node(key, value, new Array(level));
+  
+  for (let i = 0; i < level; i++) {
+    newNode.forward[i] = update[i].forward[i];
+    update[i].forward[i] = newNode;
+  }
+}
+search(key) {
+  let current = this.head;
+  
+  for (let i = this.maxLevel - 1; i >= 0; i--) {
+    while (current.forward[i] && current.forward[i].key < key) {
+      current = current.forward[i];
+    }
+  }
+  
+  current = current.forward[0];
+  if (current && current.key === key) {
+    return current.value;
+  } else {
+    return null;
+  }
+}
+remove(key) {
+  const update = new Array(this.maxLevel);
+  let current = this.head;
+  
+  for (let i = this.maxLevel - 1; i >= 0; i--) {
+    while (current.forward[i] && current.forward[i].key < key) {
+      current = current.forward[i];
+    }
+    update[i] = current;
+  }
+  
+  current = current.forward[0];
+  if (current && current.key === key) {
+    for (let i = 0; i < this.maxLevel; i++) {
+      if (update[i].forward[i] !== current) break;
+      update[i].forward[i] = current.forward[i];
+    }
+  }
+}
