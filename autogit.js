b4205cdc43191ceb5dbaa5c879d33926cf298e45
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:19:55 +0800

    1-17-2024 0:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,15 @@
-function findCommonElements(array1, array2) {
-  const common = [];
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      common.push(array1[i]);
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
   }
-  
-  return common;
+  return true;
 }
 
+// Example usage:
 const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+console.log(isSortedAscending(array1)); // Output: true
 
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  return array1.filter(element => array2.includes(element));
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  const set1 = new Set(array1);
-  const common = [];
-
-  for (const element of array2) {
-    if (set1.has(element)) {
-      common.push(element);
-    }
-  }
-
-  return common;
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+const array2 = [1, 3, 2, 4, 5];
+console.log(isSortedAscending(array2)); // Output: false
