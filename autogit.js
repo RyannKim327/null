f413bdc0e37225ae0f243a91f2e67c30ec52fa6e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:23:54 +0800

    1-17-2024 2:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,8 @@
-let str = "Hello,   World!";
-let withoutSpaces = str.replace(/\s/g, "");
-console.log(withoutSpaces); // Output: "Hello,World!"
-let str = "Hello,   World!";
-let withoutSpaces = str.split(" ").join("");
-console.log(withoutSpaces); // Output: "Hello,World!"
-let str = "Hello,   World!";
-let withoutSpaces = "";
-for (let i = 0; i < str.length; i++) {
-  if (str[i] !== " ") {
-    withoutSpaces += str[i];
-  }
-}
-console.log(withoutSpaces); // Output: "Hello,World!"
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
