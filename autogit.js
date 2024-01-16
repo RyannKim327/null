a0851550a6da8987ad96ad266dad9df8c0dea905
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:15:35 +0800

    1-17-2024 1:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,8 @@
-// Get user input
-var userInput = prompt("Enter something:");
-
-// Display the user input
-console.log("You entered: " + userInput);
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
