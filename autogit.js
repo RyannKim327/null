cad4a9d84d0ad1232111af2798c1fd46eaf70d9d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:27:19 +0800

    1-16-2024 23:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,8 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  
-  for (var i = 0; i < len - 1; i++) {
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap elements
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  
-  return arr;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-var numbers = [5, 3, 8, 4, 2];
-console.log(bubbleSort(numbers)); // Output: [2, 3, 4, 5, 8]
+
+// Example usage
+const decimalNumber = 10;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber); // Output: 1010
