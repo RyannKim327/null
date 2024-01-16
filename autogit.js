74146ce5e6443b2ef2ffb719669501763705001c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:24:17 +0800

    1-17-2024 2:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,48 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+// AsyncTask class
+class ConnectTask extends AsyncTask<String, Void, String> {
+
+  // Pre-execute tasks
+  protected void onPreExecute() {
+    // Display a progress dialog or perform setup tasks
+  }
+
+  // Background task
+  protected String doInBackground(String... params) {
+    try {
+      // Connect to the server
+      URL url = new URL(params[0]);
+      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+
+      // Read the response
+      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+      StringBuilder response = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        response.append(line);
+      }
+      reader.close();
+
+      // Return the response
+      return response.toString();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  // Post-execute tasks
+  protected void onPostExecute(String result) {
+    // Process the response data
+    if (result != null) {
+      // Display the result or perform further actions
+    } else {
+      // Show an error message or handle the failure
+    }
+  }
+}
+
+// Usage
+String serverUrl = "https://myserver.com/api/data";
+ConnectTask connectTask = new ConnectTask();
+connectTask.execute(serverUrl);
