128570de74460083622b03b5db1808ceb5422051
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 02:23:34 +0800

    1-17-2024 2:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,14 @@
-let userInput = prompt('Enter your name:');
-if (userInput) {
-  console.log('Hello, ' + userInput + '!'); 
-} else {
-  console.log('No name entered.');
+let str = "Hello,   World!";
+let withoutSpaces = str.replace(/\s/g, "");
+console.log(withoutSpaces); // Output: "Hello,World!"
+let str = "Hello,   World!";
+let withoutSpaces = str.split(" ").join("");
+console.log(withoutSpaces); // Output: "Hello,World!"
+let str = "Hello,   World!";
+let withoutSpaces = "";
+for (let i = 0; i < str.length; i++) {
+  if (str[i] !== " ") {
+    withoutSpaces += str[i];
+  }
 }
+console.log(withoutSpaces); // Output: "Hello,World!"
