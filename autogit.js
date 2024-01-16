c9a7fc5dfbea346c4d69436ffbc9e357ea56ffff
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:45:16 +0800

    1-17-2024 1:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,12 @@
-function countCharOccurrences(string, char) {
-  // Use the split() method to convert the string into an array of characters
-  var charArray = string.split("");
-
-  // Use the filter() method to create a new array containing only the occurrences of the specified character
-  var filteredArray = charArray.filter(function (c) {
-    return c === char;
-  });
-
-  // Return the length of the filtered array, which indicates the count of occurrences
-  return filteredArray.length;
+const str = "Hello, World!";
+const reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
+const str = "Hello, World!";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-
-// Example usage
-var myString = "Hello, world!";
-var myChar = "o";
-var occurrenceCount = countCharOccurrences(myString, myChar);
-
-console.log("The character '" + myChar + "' occurs " + occurrenceCount + " times in the string.");
+console.log(reversedStr);
+const str = "Hello, World!";
+const reversedStr = [...str].reverse().join("");
+console.log(reversedStr);
