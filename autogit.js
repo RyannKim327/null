08bf869ab91d76fa802d580fb06c5012f09f765a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:34:54 +0800

    1-17-2024 0:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,80 @@
-const CronJob = require('cron').CronJob;
-
-// Function to be executed
-function greetUser() {
-  console.log('Hello, world!');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-// Cron job scheduling
-const cronJob = new CronJob('0 0 * * *', greetUser); // Runs every day at midnight
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-// Start the cron job
-cronJob.start();
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.tail) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+
+    this.tail = newNode;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      if (!this.head) {
+        this.tail = null;
+      }
+      return;
+    }
+
+    let currentNode = this.head;
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+        if (!currentNode.next) {
+          this.tail = currentNode;
+        }
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+  }
+
+  search(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+}
+const linkedList = new LinkedList();
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.prepend(5);
+linkedList.delete(20);
+console.log(linkedList.search(10)); // Output: Node { value: 10, next: Node { ... } }
+console.log(linkedList.search(20)); // Output: null
