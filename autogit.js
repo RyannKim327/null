abbdfa75164a73e40a70bd8e121a1db6e72cfcec
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 00:02:08 +0800

    1-17-2024 0:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,42 @@
-let str = "HELLO WORLD";
-let lowercaseStr = str.toLowerCase();
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-console.log(lowercaseStr); // Output: "hello world"
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+}
+
+// Example usage
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+console.log(list.getLength()); // Output: 3
