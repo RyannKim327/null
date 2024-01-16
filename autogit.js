8ea3215fa9c084c81a631b0db56550652bb60e95
RyannKim327 <rksesgundo123@gmail.com>
2024-01-16 23:01:10 +0800

    1-16-2024 23:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,32 @@
-let str = "  Hello,  World!  ";
-let trimmedStr = str.replace(/\s/g, "");
-console.log(trimmedStr);
-// Output: "Hello,World!"
-let str = "  Hello,  World!  ";
-let trimmedStr = str.trim();
-console.log(trimmedStr);
-// Output: "Hello,  World!"
-let str = "  Hello,  World!  ";
-let trimmedStr = str.split(" ").join("");
-console.log(trimmedStr);
-// Output: "Hello,World!"
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true; // Cycle detected
+    }
+  }
+
+  return false; // No cycle found
+}
+const head = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+// Uncomment the line below to introduce a cycle
+// node4.next = node2;
+
+console.log(hasCycle(head)); // false
