1b6052c1445a4ff2c3ef64522d16d224b3345172
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:44:39 +0800

    1-17-2024 1:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,24 @@
-function longestCommonSubstring(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  // Create a matrix to store the lengths of the longest common suffixes
-  const matrix = Array(m + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0));
-
-  let longestLength = 0; // Length of the longest common substring
-  let endIndex = 0; // Ending index of the longest common substring in str1
-
-  // Iterate over each character of str1 and str2
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        // Characters match
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-
-        if (matrix[i][j] > longestLength) {
-          longestLength = matrix[i][j];
-          endIndex = i - 1;
-        }
-      } else {
-        // Characters don't match
-        matrix[i][j] = 0;
+function longestCommonPrefix(strings) {
+  let prefix = '';
+  
+  if (strings.length === 0) {
+    return prefix;
+  }
+  
+  for (let i = 0; i < strings[0].length; i++) {
+    const char = strings[0][i];
+    
+    for (let j = 1; j < strings.length; j++) {
+      if (strings[j][i] !== char || i >= strings[j].length) {
+        return prefix;
       }
     }
+    
+    prefix += char;
   }
-
-  // Extract the longest common substring from str1
-  const longestSubstring = str1.slice(endIndex - longestLength + 1, endIndex + 1);
-  return longestSubstring;
+  
+  return prefix;
 }
-
-// Example usage:
-const str1 = "JavaScript is awesome";
-const str2 = "Awesome is JavaScript";
-const longestSubstring = longestCommonSubstring(str1, str2);
-console.log(longestSubstring); // Output: "JavaScript"
+const strings = ['apple', 'app', 'application', 'aptitude'];
+const commonPrefix = longestCommonPrefix(strings);
+console.log(commonPrefix); // Output: 'app'
