7cd68b779d506c94dcc3a3adc199198f1cc96245
RyannKim327 <rksesgundo123@gmail.com>
2024-01-17 01:19:34 +0800

    1-17-2024 1:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,58 @@
-// Prompt the user for input
-var userInput = prompt('Enter something:');
+function findStronglyConnectedComponents(graph) {
+  let index = 0;
+  const stack = [];
+  const result = [];
 
-// Display the input on the screen
-console.log('You entered: ' + userInput);
+  function tarjanDFS(vertex) {
+    vertex.index = index;
+    vertex.lowLink = index;
+    index += 1;
+    stack.push(vertex);
+    vertex.onStack = true;
+
+    for (const adjacentVertex of graph[vertex]) {
+      if (adjacentVertex.index === undefined) {
+        tarjanDFS(adjacentVertex);
+        vertex.lowLink = Math.min(vertex.lowLink, adjacentVertex.lowLink);
+      } else if (adjacentVertex.onStack) {
+        vertex.lowLink = Math.min(vertex.lowLink, adjacentVertex.index);
+      }
+    }
+
+    if (vertex.lowLink === vertex.index) {
+      const component = [];
+      let v;
+      do {
+        v = stack.pop();
+        v.onStack = false;
+        component.push(v);
+      } while (v !== vertex);
+      result.push(component);
+    }
+  }
+
+  for (const vertex of Object.keys(graph)) {
+    if (graph[vertex].index === undefined) {
+      tarjanDFS(graph[vertex]);
+    }
+  }
+
+  return result;
+}
+
+// Example usage:
+const graph = {
+  A: ['B', 'C'],
+  B: ['D'],
+  C: ['D', 'E'],
+  D: ['A'],
+  E: ['F'],
+  F: ['G'],
+  G: ['E', 'H'],
+  H: ['I'],
+  I: ['J'],
+  J: ['H'],
+};
+
+const components = findStronglyConnectedComponents(graph);
+console.log(components);
