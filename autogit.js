1b8d73d3e18d00403137738e7ced992ae1ff9c8f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 06:00:18 +0800

    1-18-2024 6:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,37 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) {
-    return '';
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  let prefix = '';
-  const firstStr = strs[0];
-
-  for (let i = 0; i < firstStr.length; i++) {
-    const char = firstStr[i];
-    
-    for (let j = 1; j < strs.length; j++) {
-      if (strs[j][i] !== char) {
-        return prefix;
-      }
-    }
-    
-    prefix += char;
-  }
-  
-  return prefix;
 }
 
-const strings = ['flower', 'flow', 'flight'];
-console.log(longestCommonPrefix(strings)); // Output: 'fl'
+function findNthNodeFromEnd(head, n) {
+  let slow = head;
+  let fast = head;
+
+  // Move the fast pointer n nodes ahead
+  for (let i = 0; i < n; i++) {
+    if (fast === null) {
+      return null; // Indicates n is greater than the length of the list
+    }
+    fast = fast.next;
+  }
+
+  // Move both pointers until the fast pointer reaches the end
+  while (fast !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  // 'slow' will be pointing to the nth node from the end
+  return slow;
+}
+// Create the linked list: 1 -> 2 -> 3 -> 4 -> 5 -> null
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+const n = 2;
+const result = findNthNodeFromEnd(head, n);
+console.log(result.value); // Output: 4
