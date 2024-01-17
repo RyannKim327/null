e84707908884c3fad328cb1deac5c47472934a6f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:49:18 +0800

    1-18-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,57 @@
-function bubbleSort(array) {
-  let swapped;
-  do {
-    swapped = false;
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        const temp = array[i];
-        array[i] = array[i + 1];
-        array[i + 1] = temp;
-        swapped = true;
+class HashTable {
+  constructor() {
+    this.size = 16; // Size of the hash table (can be adjusted)
+    this.buckets = Array(this.size); // Array to hold the hash table data
+  }
+
+  hash(key) {
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
+    }
+    return hashValue % this.size; // Hash value mapped to a bucket index
+  }
+
+  insert(key, value) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = []; // Create an empty bucket if it doesn't exist
+    }
+    this.buckets[index].push({ key, value }); // Add key-value pair to the bucket
+  }
+
+  search(key) {
+    const index = this.hash(key);
+    if (this.buckets[index]) {
+      for (let i = 0; i < this.buckets[index].length; i++) {
+        if (this.buckets[index][i].key === key) {
+          return this.buckets[index][i].value; // Return the value if key is found
+        }
       }
     }
-  } while (swapped);
+    return null; // Return null if key is not found
+  }
 
-  return array;
+  remove(key) {
+    const index = this.hash(key);
+    if (this.buckets[index]) {
+      for (let i = 0; i < this.buckets[index].length; i++) {
+        if (this.buckets[index][i].key === key) {
+          this.buckets[index].splice(i, 1); // Remove key-value pair from the bucket
+          break;
+        }
+      }
+    }
+  }
 }
-const unsortedArray = [5, 8, 2, 1, 9, 3];
-const sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 5, 8, 9]
+
+// Example usage:
+const hashTable = new HashTable();
+hashTable.insert("name", "John");
+hashTable.insert("age", 25);
+
+console.log(hashTable.search("name")); // Output: John
+console.log(hashTable.search("age")); // Output: 25
+
+hashTable.remove("age");
+console.log(hashTable.search("age")); // Output: null
