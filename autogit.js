e95b47709a215ed1e6a4af2c1c302bc52844c04a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:22:00 +0800

    1-18-2024 4:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,3 @@
-function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
-
-  while (left < right) {
-    if (str[left] !== str[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-
-  return true;
-}
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr);
