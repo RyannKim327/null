1822064339f5b987cc021fbc372f2b1ba867fba7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:54:42 +0800

    1-18-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,28 @@
-function reverseString(string) {
-  // Split the string into an array of characters
-  var array = string.split('');
-  // Reverse the order of the array
-  var reverseArray = array.reverse();
-  // Join the reversed array back into a string
-  var reversedString = reverseArray.join('');
-  // Return the reversed string
-  return reversedString;
-}
-
-var originalString = 'Hello, World!';
-var reversedString = reverseString(originalString);
-console.log(reversedString);
-function reverseString(string) {
-  var reversedString = '';
-  // Iterate through the string in reverse order
-  for (var i = string.length - 1; i >= 0; i--) {
-    // Append each character to the reversed string
-    reversedString += string[i];
+function longestCommonPrefix(strings) {
+  if (strings.length === 0) {
+    return '';
   }
-  // Return the reversed string
-  return reversedString;
-}
 
-var originalString = 'Hello, World!';
-var reversedString = reverseString(originalString);
-console.log(reversedString);
-var originalString = 'Hello, World!';
-var reversedString = originalString.split('').reverse().join('');
-console.log(reversedString);
+  if (strings.length === 1) {
+    return strings[0];
+  }
+
+  const firstString = strings[0];
+  let prefix = '';
+
+  for (let i = 0; i < firstString.length; i++) {
+    const currentChar = firstString[i];
+
+    for (let j = 1; j < strings.length; j++) {
+      if (i >= strings[j].length || strings[j][i] !== currentChar) {
+        return prefix;
+      }
+    }
+
+    prefix += currentChar;
+  }
+
+  return prefix;
+}
+const strings = ['apple', 'append', 'appetite'];
+console.log(longestCommonPrefix(strings));  // Output: 'app'
