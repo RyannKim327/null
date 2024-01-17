099098bb9c4cc310d637ee61d09d001bbb538030
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:38:08 +0800

    1-18-2024 5:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,31 @@
-const array = [5, 10, 3, 8, 15];
-const max = Math.max(...array);
+function bubbleSort(arr) {
+  var i, j;
+  var len = arr.length;
+  var swapped;
+  
+  for (i = 0; i < len - 1; i++) {
+    swapped = false;
+    
+    for (j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        
+        swapped = true;
+      }
+    }
+    
+    // If no elements were swapped in the inner loop, then the array is already sorted
+    if (!swapped) {
+      break;
+    }
+  }
+  
+  return arr;
+}
 
-console.log(max);
-15
+// Usage example:
+var array = [9, 4, 10, 3, 1];
+console.log(bubbleSort(array)); // Output: [1, 3, 4, 9, 10]
