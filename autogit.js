b473ec628639f027efbd7e36c484ff91a1268a94
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:07:54 +0800

    1-18-2024 5:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,8 @@
-function isPalindrome(str) {
-  // Step 1
-  const cleanedStr = str.replace(/[\W_]/g, '');
-
-  // Step 2
-  const lowercaseStr = cleanedStr.toLowerCase();
-
-  // Step 3
-  const reversedStr = lowercaseStr.split('').reverse().join('');
-
-  // Step 4
-  return lowercaseStr === reversedStr;
-}
-
-// Usage
-const str1 = "A man, a plan, a canal: Panama";
-console.log(isPalindrome(str1)); // Output: true
-
-const str2 = "race car";
-console.log(isPalindrome(str2)); // Output: true
-
-const str3 = "hello world";
-console.log(isPalindrome(str3)); // Output: false
+let string1 = "Hello";
+let string2 = "World";
+let result = string1 + " " + string2;
+console.log(result); // Output: "Hello World"
+let string1 = "Hello";
+let string2 = "World";
+let result = string1.concat(" ", string2);
+console.log(result); // Output: "Hello World"
