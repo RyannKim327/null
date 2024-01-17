6da8bd62835146ff5904486d4466d24221df552e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:54:32 +0800

    1-18-2024 4:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,10 @@
-function buildBadMatchTable(pattern) {
-  const table = {};
-  const patternLength = pattern.length;
-  
-  for (let i = 0; i < patternLength - 1; i++) {
-    const char = pattern[i];
-    const shift = patternLength - 1 - i;
-    table[char] = shift;
-  }
-  
-  return table;
-}
-function boyerMooreHorspool(text, pattern) {
-  const result = [];
-  const textLength = text.length;
-  const patternLength = pattern.length;
-  
-  if (patternLength > textLength) {
-    return result;
-  }
-  
-  const badMatchTable = buildBadMatchTable(pattern);
-  let i = 0;
-  
-  while (i <= textLength - patternLength) {
-    let j = patternLength - 1;
-    
-    while (j >= 0 && pattern[j] === text[i + j]) {
-      j--;
-    }
-    
-    if (j < 0) {
-      result.push(i);
-      i += 1; // Slide the window by 1
-    } else {
-      const mismatchedChar = text[i + j];
-      const shift = badMatchTable[mismatchedChar] || patternLength;
-      i += shift;
-    }
-  }
-  
-  return result;
-}
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
