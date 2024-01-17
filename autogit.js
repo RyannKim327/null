de8310dde9527209d385d53f1c0edd89d0865e32
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:53:21 +0800

    1-18-2024 5:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,19 @@
-let string1 = "Hello";
-let string2 = "World";
-let result = string1 + " " + string2;
-console.log(result); // Output: "Hello World"
-let string1 = "Hello";
-let string2 = "World";
-let result = string1.concat(" ", string2);
-console.log(result); // Output: "Hello World"
+function insertionSort(arr) {
+  const len = arr.length;
+
+  for (let i = 1; i < len; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentValue;
+  }
+
+  return arr;
+}
+const arr = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(arr)); // Outputs: [1, 2, 3, 4, 5, 6]
