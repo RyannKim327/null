3a9572aef8e4387f35638dd74b4e89acad0cd5e7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:49:15 +0800

    1-18-2024 4:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,32 @@
-class Graph {
-  constructor(numOfVertices) {
-    this.vertices = numOfVertices;
-    this.edges = [];
-  }
-
-  addEdge(source, destination, weight) {
-    this.edges.push({ source, destination, weight });
-  }
-}
-function bellmanFord(graph, source) {
-  let distances = {};
-
-  // Step 1: Initialize distances from source to all other vertices as Infinity
-  for (let i = 0; i < graph.vertices; i++) {
-    distances[i] = Infinity;
-  }
-
-  distances[source] = 0;
-
-  // Step 2: Relax all edges |V| - 1 times
-  for (let i = 0; i < graph.vertices - 1; i++) {
-    for (let j = 0; j < graph.edges.length; j++) {
-      let { source, destination, weight } = graph.edges[j];
-      if (distances[source] + weight < distances[destination]) {
-        distances[destination] = distances[source] + weight;
-      }
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
+  return max;
+}
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+function radixSort(arr) {
+  let max = findMax(arr);
+  let maxDigitCount = Math.floor(Math.log10(max) + 1);
 
-  // Step 3: Check for negative-weight cycles
-  for (let i = 0; i < graph.edges.length; i++) {
-    let { source, destination, weight } = graph.edges[i];
-    if (distances[source] + weight < distances[destination]) {
-      throw new Error("Graph contains negative-weight cycle");
+  for (let i = 0; i < maxDigitCount; i++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+
+    for (let j = 0; j < arr.length; j++) {
+      let digit = getDigit(arr[j], i);
+      digitBuckets[digit].push(arr[j]);
     }
+
+    arr = [].concat(...digitBuckets);
   }
 
-  return distances;
+  return arr;
 }
-let graph = new Graph(5);
-graph.addEdge(0, 1, 6);
-graph.addEdge(0, 4, 7);
-graph.addEdge(1, 2, 5);
-graph.addEdge(1, 3, -4);
-graph.addEdge(1, 4, 8);
-graph.addEdge(2, 1, -2);
-graph.addEdge(3, 0, 2);
-graph.addEdge(3, 2, 7);
-graph.addEdge(4, 2, -3);
-graph.addEdge(4, 3, 9);
-let sourceNode = 0;
-let shortestPaths = bellmanFord(graph, sourceNode);
-console.log(shortestPaths);
+let array = [170, 45, 75, 90, 802, 24, 2, 66];
+let sortedArray = radixSort(array);
+console.log(sortedArray);
