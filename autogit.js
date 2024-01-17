199a2adbfba25b3296adbb6b53d536697afb8c7b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:24:22 +0800

    1-18-2024 5:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,40 @@
-function longestCommonSubstring(str1, str2) {
-  // Create a 2D array to store the lengths of longest common suffixes
-  const matrix = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+function findLongestIncreasingSubsequence(arr) {
+  const n = arr.length;
+  const dp = new Array(n).fill(1);
 
-  let maxLength = 0; // Length of longest common substring
-  let endIndex = 0; // End index of longest common substring in str1
-
-  // Traverse each character of the strings
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-        if (matrix[i][j] > maxLength) {
-          maxLength = matrix[i][j];
-          endIndex = i - 1;
-        }
-      } else {
-        matrix[i][j] = 0;
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+        dp[i] = dp[j] + 1;
       }
     }
   }
 
-  // Extract the longest common substring using the endIndex and maxLength
-  const longestSubstring = str1.substr(endIndex - maxLength + 1, maxLength);
-  return longestSubstring;
+  let maxLength = 0;
+  let endIndex = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (dp[i] > maxLength) {
+      maxLength = dp[i];
+      endIndex = i;
+    }
+  }
+
+  const lis = [];
+  let currentLength = maxLength;
+
+  lis.unshift(arr[endIndex]);
+
+  for (let i = endIndex - 1; i >= 0; i--) {
+    if (arr[i] < arr[endIndex] && dp[i] === currentLength - 1) {
+      lis.unshift(arr[i]);
+      currentLength--;
+    }
+  }
+
+  return lis;
 }
 
 // Example usage:
-const str1 = "abcdef";
-const str2 = "xyzdef";
-console.log(longestCommonSubstring(str1, str2)); // Output: "def"
+const array = [3, 10, 2, 1, 20];
+console.log(findLongestIncreasingSubsequence(array)); // Output: [3, 10, 20]
