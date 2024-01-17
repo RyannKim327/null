677b50e6089051401bc9cdd8d7acc7e383cd65aa
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:45:02 +0800

    1-18-2024 5:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,103 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  // Add a value to the end of the list
-  append(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  // Add a value to the beginning of the list
-  prepend(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  // Remove the first occurrence of a value from the list
-  remove(value) {
-    if (!this.head) {
-      return;
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
     }
 
-    if (this.head.value === value) {
-      this.head = this.head.next;
-      if (!this.head) {
-        this.tail = null;
-      }
-      return;
-    }
+    // Insert a value into the binary search tree
+    insert(value) {
+        const newNode = new Node(value);
 
-    let currentNode = this.head;
-    while (currentNode.next) {
-      if (currentNode.next.value === value) {
-        currentNode.next = currentNode.next.next;
-        if (!currentNode.next) {
-          this.tail = currentNode;
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
         }
-        return;
-      }
-      currentNode = currentNode.next;
     }
-  }
 
-  // Check if a value exists in the list
-  contains(value) {
-    let currentNode = this.head;
-    while (currentNode) {
-      if (currentNode.value === value) {
-        return true;
-      }
-      currentNode = currentNode.next;
+    // Helper method to insert a node into the binary search tree
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
     }
-    return false;
-  }
 
-  // Print the values of the list
-  print() {
-    let currentNode  = this.head;
-    const values = [];
-    while (currentNode) {
-      values.push(currentNode.value);
-      currentNode = currentNode.next;
+    // Perform an in-order traversal of the binary search tree
+    inOrderTraversal(callback) {
+        this.inOrderTraversalNode(this.root, callback);
     }
-    console.log(values.join(' -> '));
-  }
+
+    // Helper method to perform an in-order traversal on a specific node
+    inOrderTraversalNode(node, callback) {
+        if (node !== null) {
+            this.inOrderTraversalNode(node.left, callback);
+            callback(node.value);
+            this.inOrderTraversalNode(node.right, callback);
+        }
+    }
+
+    // Search for a value in the binary search tree
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    // Helper method to search for a value in a specific node
+    searchNode(node, value) {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
 }
+class BSTNode {
+    constructor() {
+        this.bst = new BinarySearchTree();
+    }
 
-// Example usage
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-list.prepend(0);
-list.remove(2);
-console.log(list.contains(3)); // true
-console.log(list.contains(2)); // false
-list.print(); // 0 -> 1 -> 3
+    insert(value) {
+        this.bst.insert(value);
+    }
+
+    search(value) {
+        return this.bst.search(value);
+    }
+
+    inOrderTraversal(callback) {
+        this.bst.inOrderTraversal(callback);
+    }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(8);
+bst.insert(3);
+
+bst.inOrderTraversal(value => console.log(value));
+
+console.log(bst.search(8)); // Output: true
+console.log(bst.search(20)); // Output: false
