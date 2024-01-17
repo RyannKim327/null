14640e640a4a4a233060dff782a5f8152458ebd8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:53:55 +0800

    1-18-2024 5:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,8 @@
-function insertionSort(arr) {
-  const len = arr.length;
-
-  for (let i = 1; i < len; i++) {
-    let currentValue = arr[i];
-    let j = i - 1;
-
-    while (j >= 0 && arr[j] > currentValue) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-
-    arr[j + 1] = currentValue;
+function factorialRecursive(num) {
+  if (num === 0 || num === 1) {
+    return 1;
   }
-
-  return arr;
+  return num * factorialRecursive(num - 1);
 }
-const arr = [5, 2, 4, 6, 1, 3];
-console.log(insertionSort(arr)); // Outputs: [1, 2, 3, 4, 5, 6]
+const result = factorialRecursive(5);
+console.log(result); // Output: 120
