2c1cc2004a0356e4849774afed344d541b9a9cd0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:39:25 +0800

    1-18-2024 5:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,24 @@
-var str1 = "Hello";
-var str2 = " world!";
-var concatenated = str1 + str2;
-console.log(concatenated);
-var str1 = "Hello";
-var str2 = " world!";
-var concatenated = str1.concat(str2);
-console.log(concatenated);
+function shellSort(arr) {
+    let gap = Math.floor(arr.length / 2);
+
+    while (gap > 0) {
+        for (let i = gap; i < arr.length; i++) {
+            let temp = arr[i];
+            let j = i;
+
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = temp;
+        }
+        gap = Math.floor(gap / 2);
+    }
+
+    return arr;
+}
+
+// Example usage:
+const array = [9, 7, 5, 1, 3, 8, 2, 6, 4];
+console.log(shellSort(array));
