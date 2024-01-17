a3d9a70f78e368cd038a249a8c759cadc113bf58
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:12:05 +0800

    1-18-2024 5:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,15 @@
-function selectionSort(arr) {
-  const n = arr.length;
-
-  for (let i = 0; i < n - 1; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < n; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    const temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
+function getIntersectionNode(headA, headB) {
+  if (!headA || !headB) {
+    return null;
   }
-
-  return arr;
+  
+  let p1 = headA;
+  let p2 = headB;
+  
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : headB;
+    p2 = p2 ? p2.next : headA;
+  }
+  
+  return p1;
 }
-
-// Usage example:
-const arr = [64, 25, 12, 22, 11];
-const sortedArr = selectionSort(arr);
-console.log(sortedArr); // Output: [11, 12, 22, 25, 64]
