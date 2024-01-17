aec309c2a509bf9cc76921b0a1ba5425755ef357
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:31:36 +0800

    1-18-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,8 @@
-function isPalindrome(str) {
-  // Remove all non-alphanumeric characters and convert to lowercase
-  const cleanedStr = str.toLowerCase().replace(/[\W_]/g, '');
-
-  // Compare the cleaned string with its reversed version
-  return cleanedStr === cleanedStr.split('').reverse().join('');
-}
-const str1 = "racecar";
-console.log(isPalindrome(str1)); // Output: true
-
-const str2 = "hello";
-console.log(isPalindrome(str2)); // Output: false
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
