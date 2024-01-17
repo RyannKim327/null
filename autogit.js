993c0a76615252ba886e04f60cb3d5381046d456
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:54:18 +0800

    1-18-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,31 @@
-function factorialRecursive(num) {
-  if (num === 0 || num === 1) {
-    return 1;
-  }
-  return num * factorialRecursive(num - 1);
+function reverseString(string) {
+  // Split the string into an array of characters
+  var array = string.split('');
+  // Reverse the order of the array
+  var reverseArray = array.reverse();
+  // Join the reversed array back into a string
+  var reversedString = reverseArray.join('');
+  // Return the reversed string
+  return reversedString;
 }
-const result = factorialRecursive(5);
-console.log(result); // Output: 120
+
+var originalString = 'Hello, World!';
+var reversedString = reverseString(originalString);
+console.log(reversedString);
+function reverseString(string) {
+  var reversedString = '';
+  // Iterate through the string in reverse order
+  for (var i = string.length - 1; i >= 0; i--) {
+    // Append each character to the reversed string
+    reversedString += string[i];
+  }
+  // Return the reversed string
+  return reversedString;
+}
+
+var originalString = 'Hello, World!';
+var reversedString = reverseString(originalString);
+console.log(reversedString);
+var originalString = 'Hello, World!';
+var reversedString = originalString.split('').reverse().join('');
+console.log(reversedString);
