7dca0cbcd5daaeba34a95de047e29037b13596c3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:51:21 +0800

    1-18-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,8 @@
-function isPrime(number) {
-    // Check if the number is less than 2, which is not prime
-    if (number < 2) {
-        return false;
-    }
-
-    // Check for divisibility from 2 to the square root of the number
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-
-    return true;
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
-console.log(isPrime(7));  // Output: true
-console.log(isPrime(12)); // Output: false
-console.log(isPrime(37)); // Output: true
+
+// Example usage
+const input = 'Hello World';
+const output = removeVowels(input);
+console.log(output);  // Output: "Hll Wrld"
