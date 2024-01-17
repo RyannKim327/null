4bf6e65c54aeb8c861594e6abc8659bfd4d4acc9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:21:38 +0800

    1-18-2024 4:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,14 @@
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = [];
-for (let i = 0; i < array.length; i++) {
-  if (!uniqueArray.includes(array[i])) {
-    uniqueArray.push(array[i]);
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
   }
+
+  return true;
 }
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5, 6]
