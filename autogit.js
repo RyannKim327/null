23a8e5c74a1f81085ce1c4d6509752a954e5f408
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:59:29 +0800

    1-18-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,36 @@
-const cron = require('node-cron');
+function countingSort(arr) {
+  const len = arr.length;
 
-cron.schedule('*/5 * * * *', () => {
-  // This code will run every 5 minutes
+  if (len <= 1) {
+    return arr;
+  }
 
-  // Add your desired functionality here
-  console.log('Running cron job...');
-});
+  let max = arr[0];
+  for (let i = 1; i < len; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  const count = new Array(max + 1).fill(0);
+  const output = new Array(len);
+
+  for (let i = 0; i < len; i++) {
+    count[arr[i]]++;
+  }
+
+  for (let i = 1; i <= max; i++) {
+    count[i] += count[i - 1];
+  }
+
+  for (let i = len - 1; i >= 0; i--) {
+    const val = arr[i];
+    count[val]--;
+    output[count[val]] = val;
+  }
+
+  return output;
+}
+const array = [5, 2, 9, 5, 2, 3, 5];
+const sortedArray = countingSort(array);
+console.log(sortedArray); // Output: [2, 2, 3, 5, 5, 5, 9]
