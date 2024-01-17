08cd42c9635f47871cdbd7e6ea3be53425c8f5af
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:49:50 +0800

    1-18-2024 4:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,15 @@
-function findMax(arr) {
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
-function getDigit(num, place) {
-  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-}
-function radixSort(arr) {
-  let max = findMax(arr);
-  let maxDigitCount = Math.floor(Math.log10(max) + 1);
-
-  for (let i = 0; i < maxDigitCount; i++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-
-    for (let j = 0; j < arr.length; j++) {
-      let digit = getDigit(arr[j], i);
-      digitBuckets[digit].push(arr[j]);
-    }
-
-    arr = [].concat(...digitBuckets);
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
   }
 
-  return arr;
+  let result = 1;
+  for (let i = number; i >= 2; i--) {
+    result *= i;
+  }
+  return result;
 }
-let array = [170, 45, 75, 90, 802, 24, 2, 66];
-let sortedArray = radixSort(array);
-console.log(sortedArray);
+
+const number = 5;
+const factorialOfNumber = factorial(number);
+console.log(`The factorial of ${number} is ${factorialOfNumber}`);
