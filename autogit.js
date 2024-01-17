43534f3ad0ea01eec70acc84a55e0e0fe758c2c7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:33:13 +0800

    1-18-2024 4:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,22 @@
-function findMajorityElement(arr) {
-  const freqMap = {};
-
-  // Count the frequency of each element
-  for (let num of arr) {
-    freqMap[num] = (freqMap[num] || 0) + 1;
-  }
-
-  // Find the element with frequency greater than half the array length
-  for (let num in freqMap) {
-    if (freqMap[num] > arr.length / 2) {
-      return parseInt(num);
+function countOccurrences(string, character) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === character) {
+      count++;
     }
   }
-
-  return -1; // If no majority element is found
+  return count;
 }
 
-// Example usage:
-const arr = [2, 4, 2, 3, 2, 2, 6];
-const majorityElement = findMajorityElement(arr);
-console.log(`The majority element is: ${majorityElement}`);
+const str = "Hello, world!";
+const char = "o";
+const occurrences = countOccurrences(str, char);
+console.log(`The character '${char}' appears ${occurrences} time(s) in the string.`);
+function countOccurrences(string, character) {
+  return string.split(character).length - 1;
+}
+
+const str = "Hello, world!";
+const char = "o";
+const occurrences = countOccurrences(str, char);
+console.log(`The character '${char}' appears ${occurrences} time(s) in the string.`);
