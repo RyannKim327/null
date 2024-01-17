94884b8d772b67c5bb427fcbcbfe7907e599a4da
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:09:36 +0800

    1-18-2024 5:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,72 @@
-function factorialRecursive(num) {
-  if (num === 0) return 1;       // base case: factorial of 0 is 1
-  return num * factorialRecursive(num - 1);  // recursive call
-}
-
-console.log(factorialRecursive(5));  // Output: 120
-function factorialIterative(num) {
-  let result = 1;
-  for (let i = 1; i <= num; i++) {
-    result *= i;
+class QueueNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  return result;
 }
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
+}
+enqueue(value) {
+    const newNode = new QueueNode(value);
 
-console.log(factorialIterative(5));  // Output: 120
+    if (this.rear === null) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+
+    this.size++;
+  }
+dequeue() {
+    if (this.front === null) {
+      return null;
+    }
+
+    const dequeuedValue = this.front.value;
+
+    if (this.front === this.rear) {
+      this.front = null;
+      this.rear = null;
+    } else {
+      this.front = this.front.next;
+    }
+
+    this.size--;
+    return dequeuedValue;
+  }
+isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  peek() {
+    if (this.front === null) {
+      return null;
+    }
+
+    return this.front.value;
+  }
+const queue = new Queue();
+console.log(queue.isEmpty()); // true
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+console.log(queue.getSize()); // 3
+console.log(queue.peek()); // 1
+
+console.log(queue.dequeue()); // 1
+console.log(queue.dequeue()); // 2
+
+console.log(queue.peek()); // 3
+console.log(queue.getSize()); // 1
