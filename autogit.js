40152fd92ea3cdd66852c127b1acc6c6de37ed95
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:42:19 +0800

    1-18-2024 5:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,4 @@
-let str = "Hello, World!";
-let reversedString = str.split('').reverse().join('');
-console.log(reversedString);
-let str = "Hello, World!";
-let reversedString = '';
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedString += str.charAt(i);
-}
-console.log(reversedString);
-function reverseString(str) {
-  if (str === '')
-    return '';
-  else
-    return reverseString(str.substring(1)) + str.charAt(0);
-}
+const array = [5, 9, 2, 14, 8];
+const max = Math.max(...array);
 
-let str = "Hello, World!";
-let reversedString = reverseString(str);
-console.log(reversedString);
+console.log(max); // Output: 14
