275227d1e4bc6a4ca574e4bfdc95384e35223cc9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 05:00:09 +0800

    1-18-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,37 @@
-function countOccurrences(string, word) {
-  const regex = new RegExp(word, 'g');
-  const matches = string.match(regex);
-  return matches ? matches.length : 0;
+class Node {
+  constructor() {
+    this.label = '';
+    this.children = {};
+  }
 }
+class SuffixTree {
+  constructor() {
+    this.root = new Node();
+  }
 
-const sentence = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?';
-const word = 'wood';
-console.log(countOccurrences(sentence, word)); // Output: 2
-function countOccurrences(string, word) {
-  const wordsArray = string.split(' ');
-  let count = 0;
-  for (let i = 0; i < wordsArray.length; i++) {
-    if (wordsArray[i] === word) {
-      count++;
+  // Implement additional methods here
+}
+class SuffixTree {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(word) {
+    for (let i = 0; i < word.length; i++) {
+      let currentNode = this.root;
+      for (let j = i; j < word.length; j++) {
+        const currentChar = word[j];
+        if (!currentNode.children[currentChar]) {
+          currentNode.children[currentChar] = new Node();
+        }
+        currentNode = currentNode.children[currentChar];
+        currentNode.label += currentChar;
+      }
     }
   }
-  return count;
 }
+const suffixTree = new SuffixTree();
+suffixTree.insert('banana');
 
-const sentence = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?';
-const word = 'wood';
-console.log(countOccurrences(sentence, word)); // Output: 2
-function countOccurrences(string, word) {
-  const wordsArray = string.split(' ');
-  return wordsArray.reduce((count, currentWord) => {
-    return count + (currentWord === word ? 1 : 0);
-  }, 0);
-}
-
-const sentence = 'How much wood would a woodchuck chuck if a woodchuck could chuck wood?';
-const word = 'wood';
-console.log(countOccurrences(sentence, word)); // Output: 2
+console.log(suffixTree.root.children['b'].children['a'].children['n'].children['a']); // Output: Node { label: 'na', children: {} }
+console.log(suffixTree.root.children['b'].children['a']); // Output: Node { label: 'a', children: { n: Node { label: 'na', children: {} } } }
