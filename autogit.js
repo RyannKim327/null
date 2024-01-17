0125ab735aefafe06cdda1344ec6653fad7a5ed9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-18 04:27:46 +0800

    1-18-2024 4:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,4 @@
-// Function to generate a random number in a given range
-function getRandomNumber(min, max) {
-  // Generate a random decimal number between 0 and 1
-  var randomDecimal = Math.random();
+let str = "Hello, world! This is a sample string.";
+let noSpaces = str.replace(/\s/g, "");
 
-  // Scale the random decimal to the range [min, max]
-  var randomNum = randomDecimal * (max - min + 1) + min;
-
-  // Remove the fractional part to get an integer
-  randomNum = Math.floor(randomNum);
-
-  // Return the generated random number
-  return randomNum;
-}
-
-// Example usage:
-var minRange = 1;
-var maxRange = 10;
-var randomNumber = getRandomNumber(minRange, maxRange);
-console.log(randomNumber);
+console.log(noSpaces); // Output: Hello,world!Thisisasamplestring.
