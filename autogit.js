f62ceb86a1757b835bfeff3b887d7b54f8213ca9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:41:23 +0800

    1-19-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,23 @@
-const array = [1, 2, 3, 4, 5, 3, 2, 1];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 4, 5, 3, 2, 1];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 4, 5, 3, 2, 1];
-const uniqueArray = array.reduce((accumulator, value) => {
-  if (!accumulator.includes(value)) {
-    accumulator.push(value);
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+
+  var pivot = arr[0];
+  var lesser = [];
+  var greater = [];
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      lesser.push(arr[i]);
+    } else {
+      greater.push(arr[i]);
+    }
+  }
+
+  return quicksort(lesser).concat(pivot, quicksort(greater));
+}
+
+// Example usage:
+var array = [5, 8, 2, 9, 1, 10, 7];
+console.log(quicksort(array)); // Outputs: [1, 2, 5, 7, 8, 9, 10]
