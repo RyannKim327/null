7252349bf95d91bfe4198eb378dae9a19cbad47a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:22:46 +0800

    1-20-2024 6:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,10 @@
-function findMedianSortedArrays(nums1, nums2) {
-  const mergedArray = nums1.concat(nums2);
-  mergedArray.sort((a, b) => a - b);
-  
-  const len = mergedArray.length;
-  const mid = Math.floor(len / 2);
-
-  if (len % 2 === 0) {
-    return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
-  } else {
-    return mergedArray[mid];
-  }
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
-const nums1 = [1, 3];
-const nums2 = [2, 4];
 
-console.log(findMedianSortedArrays(nums1, nums2));  // Output: 2.5
+// Calling the function
+var base = 5;
+var height = 7;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
