7f673fb881e8975038ab8fa007ad1437d56b2078
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:38:51 +0800

    1-20-2024 6:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,14 @@
-function findLongestCommonSubsequence(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  // Create a 2D array to store the lengths of common subsequences
-  const dp = Array(m + 1).fill().map(() => Array(n + 1).fill(0));
-
-  // Build the dp array
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-
-  // Construct the longest common subsequence
-  let lcs = '';
-  let i = m;
-  let j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i - 1][j] >= dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
+const str = "Hello World";
+const words = str.split(" ");
+const reversedWords = words.reverse();
+const reversedString = reversedWords.join(" ");
+function reverseStringWords(str) {
+  const words = str.split(" ");
+  const reversedWords = words.reverse();
+  const reversedString = reversedWords.join(" ");
+  return reversedString;
 }
 
-// Example usage:
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const longestCommonSubsequence = findLongestCommonSubsequence(str1, str2);
-console.log(longestCommonSubsequence);  // Output: ADH
+const inputString = "Hello World";
+const result = reverseStringWords(inputString);
+console.log(result); // Output: "World Hello"
