ebcc85ad9e53c8e9943ffcb969a89e92872cdcd6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:39:43 +0800

    1-20-2024 6:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,3 @@
-function calculateStringLength(string) {
-  let count = 0;
-  for (let i = 0; string[i] !== undefined; i++) {
-    count++;
-  }
-  return count;
-}
-
-const str = "Hello, world!";
-const length = calculateStringLength(str);
-console.log(length); // Output: 13
+let str = "Hello World!";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: "hello world!"
