d244e0cc8a17f1fd0ce2d8d01dd504a3eee819b2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:42:00 +0800

    1-20-2024 6:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,8 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
 
-  add(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  isPalindrome() {
-    // Convert the linked list to an array
-    const array = [];
-    let current = this.head;
-    while (current) {
-      array.push(current.value);
-      current = current.next;
-    }
-
-    // Check if the array is a palindrome
-    const length = array.length;
-    for (let i = 0; i < length / 2; i++) {
-      if (array[i] !== array[length - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(2);
-linkedList.add(1);
-console.log(linkedList.isPalindrome()); // Output: true
+// Example usage:
+console.log(decimalToBinary(10));  // Output: "1010"
+console.log(decimalToBinary(27));  // Output: "11011"
+console.log(decimalToBinary(42));  // Output: "101010"
