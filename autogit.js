93f1bd0e617e1cd979a0d6b43b33935cb0292d0b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:51:48 +0800

    1-19-2024 22:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,155 +1,18 @@
-class BinaryHeap {
-  constructor() {
-    this.heap = [];
-  }
+function reverseWords(str) {
+  // Step 1: Split the string into an array of words
+  const wordsArray = str.split(' ');
 
-  // Helper functions
-  getParentIndex(i) {
-    return Math.floor((i - 1) / 2);
-  }
+  // Step 2: Reverse the order of the array
+  const reversedArray = wordsArray.reverse();
 
-  getLeftChildIndex(i) {
-    return 2 * i + 1;
-  }
+  // Step 3: Join the array elements into a string
+  const reversedString = reversedArray.join(' ');
 
-  getRightChildIndex(i) {
-    return 2 * i + 2;
-  }
-
-  hasParent(i) {
-    return this.getParentIndex(i) >= 0;
-  }
-
-  hasLeftChild(i) {
-    return this.getLeftChildIndex(i) < this.heap.length;
-  }
-
-  hasRightChild(i) {
-    return this.getRightChildIndex(i) < this.heap.length;
-  }
-
-  getParent(i) {
-    return this.heap[this.getParentIndex(i)];
-  }
-
-  getLeftChild(i) {
-    return this.heap[this.getLeftChildIndex(i)];
-  }
-
-  getRightChild(i) {
-    return this.heap[this.getRightChildIndex(i)];
-  }
-
-  swap(i, j) {
-    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-  }
-
-  // Heap operations
-  peek() {
-    if (this.heap.length === 0) {
-      throw new Error('Heap is empty');
-    }
-    return this.heap[0];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  push(value) {
-    this.heap.push(value);
-    this.heapifyUp();
-  }
-
-  pop() {
-    if (this.heap.length === 0) {
-      throw new Error('Heap is empty');
-    }
-    const minValue = this.heap[0];
-    this.heap[0] = this.heap[this.heap.length - 1];
-    this.heap.pop();
-    this.heapifyDown();
-    return minValue;
-  }
-
-  // Heapify Up
-  heapifyUp() {
-    let currentIndex = this.heap.length - 1;
-    while (
-      this.hasParent(currentIndex) &&
-      this.getParent(currentIndex) > this.heap[currentIndex]
-    ) {
-      const parentIndex = this.getParentIndex(currentIndex);
-      this.swap(currentIndex, parentIndex);
-      currentIndex = parentIndex;
-    }
-  }
-
-  // Heapify Down
-  heapifyDown() {
-    let currentIndex = 0;
-    while (this.hasLeftChild(currentIndex)) {
-      let smallestChildIndex = this.getLeftChildIndex(currentIndex);
-      if (
-        this.hasRightChild(currentIndex) &&
-        this.getRightChild(currentIndex) < this.getLeftChild(currentIndex)
-      ) {
-        smallestChildIndex = this.getRightChildIndex(currentIndex);
-      }
-
-      if (this.heap[currentIndex] < this.heap[smallestChildIndex]) {
-        break;
-      } else {
-        this.swap(currentIndex, smallestChildIndex);
-      }
-
-      currentIndex = smallestChildIndex;
-    }
-  }
+  return reversedString;
 }
-class PriorityQueue {
-  constructor() {
-    this.heap = new BinaryHeap();
-  }
 
-  enqueue(item, priority) {
-    const element = { item, priority };
-    this.heap.push(element);
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      throw new Error('Priority Queue is empty');
-    }
-    const element = this.heap.pop();
-    return element.item;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      throw new Error('Priority Queue is empty');
-    }
-    return this.heap.peek().item;
-  }
-
-  size() {
-    return this.heap.size();
-  }
-
-  isEmpty() {
-    return this.heap.isEmpty();
-  }
-}
-const pq = new PriorityQueue();
-
-pq.enqueue('Task 1', 3);
-pq.enqueue('Task 2', 1);
-pq.enqueue('Task 3', 2);
-
-console.log(pq.dequeue()); // Output: 'Task 2'
-console.log(pq.dequeue()); // Output: 'Task 3'
-console.log(pq.dequeue()); // Output: 'Task 1'
+// Example usage
+const originalString = "Hello world, how are you?";
+const reversedString = reverseWords(originalString);
+console.log(reversedString);
+you? are how world, Hello
