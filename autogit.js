0b3ec78a81ad0c922f03ca64f3e62587ba92c1c0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:19:09 +0800

    1-20-2024 6:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,5 @@
-function countOccurrences(string, character) {
-  let count = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === character) {
-      count++;
-    }
-  }
-  return count;
-}
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-const myString = 'Hello World';
-const myCharacter = 'o';
-const occurrences = countOccurrences(myString, myCharacter);
-console.log(occurrences);  // Output: 2
-function countOccurrences(string, character) {
-  const regex = new RegExp(character, 'g');
-  const matches = string.match(regex);
-  return matches ? matches.length : 0;
-}
-
-const myString = 'Hello World';
-const myCharacter = 'o';
-const occurrences = countOccurrences(myString, myCharacter);
-console.log(occurrences);  // Output: 2
+console.log(lowercaseStr);
+hello world
