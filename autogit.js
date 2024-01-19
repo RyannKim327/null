19accbb8a4346cb710dd56c16fa551c55651772a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:44:18 +0800

    1-19-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,43 @@
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // method to reverse the linked list
+  reverse() {
+    let current = this.head;
+    let prev = null;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
+}
+const linkedList = new LinkedList();
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+
+linkedList.head = node1;
+node1.next = node2;
+node2.next = node3;
+// Before reversing
+// 1 -> 2 -> 3
+
+linkedList.reverse();
+
+// After reversing
+// 3 -> 2 -> 1
