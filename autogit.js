cb23c598f722f05ac6dd74323fb6840d44ef724e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:32:19 +0800

    1-19-2024 22:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,11 @@
-const maxNum = Math.max(...array);
-const numDigits = Math.floor(Math.log10(maxNum)) + 1;
-function getDigit(num, place) {
-  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+Area = (base * height) / 2
+function calculateTriangleArea(base, height) {
+    var area = (base * height) / 2;
+    return area;
 }
-for (let digit = 0; digit < numDigits; digit++) {
-  // inner loop and sorting logic go here
-}
-const buckets = Array.from({ length: 10 }, () => []);
-for (let i = 0; i < array.length; i++) {
-  const digit = getDigit(array[i], digit);
-  buckets[digit].push(array[i]);
-}
-array = buckets.flat();
-function radixSort(array) {
-  const maxNum = Math.max(...array);
-  const numDigits = Math.floor(Math.log10(maxNum)) + 1;
 
-  function getDigit(num, place) {
-    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-  }
-
-  for (let digit = 0; digit < numDigits; digit++) {
-    const buckets = Array.from({ length: 10 }, () => []);
-
-    for (let i = 0; i < array.length; i++) {
-      const digitValue = getDigit(array[i], digit);
-      buckets[digitValue].push(array[i]);
-    }
-
-    array = buckets.flat();
-  }
-
-  return array;
-}
-const unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
-const sortedArray = radixSort(unsortedArray);
-console.log(sortedArray);
+// Example usage:
+var base = 5;
+var height = 6;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
