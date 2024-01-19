e918431ba2ca9e7d72b02b68876315052f6f2565
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:27:01 +0800

    1-20-2024 6:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,4 @@
-function isPrime(num) {
-  // 1 and numbers less than 1 are not prime
-  if (num <= 1) {
-    return false;
-  }
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-  // Check for divisibility from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false; // Number is divisible by i, hence not prime
-    }
-  }
-
-  return true; // Number is prime
-}
-
-// Example usage
-console.log(isPrime(7)); // true
-console.log(isPrime(12)); // false
-console.log(isPrime(29)); // true
+console.log(lowercaseStr); // Output: "hello world"
