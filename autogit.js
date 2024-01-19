5984554eda00bf596d79a536a1a2e24c1e6fff2a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:21:59 +0800

    1-19-2024 22:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,7 @@
-function findNthNodeFromEnd(head, n) {
-  let p1 = head;
-  let p2 = head;
-
-  // Move p2 n positions ahead
-  while (n > 0 && p2 !== null) {
-    p2 = p2.next;
-    n--;
-  }
-
-  // If the length of the list is less than n
-  if (n > 0) {
-    return null; // or any other desired value
-  }
-
-  // Move both pointers until p2 reaches the last node
-  while (p2 !== null && p2.next !== null) {
-    p1 = p1.next;
-    p2 = p2.next;
-  }
-
-  // p1 points to the nth node from the end
-  return p1.value;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+// Usage example
+var randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber); // Outputs a random number between 1 and 10
