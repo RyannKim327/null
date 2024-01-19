d1888afba69595f0cdd844b57c61de40145f81d1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:41:01 +0800

    1-19-2024 22:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,16 @@
-const array = [3, 9, 2, 4, 7, 5];
-
-const max = Math.max(...array);
-console.log(max); // Output: 9
+const array = [1, 2, 3, 4, 5, 3, 2, 1];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const array = [1, 2, 3, 4, 5, 3, 2, 1];
+const uniqueArray = array.filter((value, index, self) => {
+  return self.indexOf(value) === index;
+});
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const array = [1, 2, 3, 4, 5, 3, 2, 1];
+const uniqueArray = array.reduce((accumulator, value) => {
+  if (!accumulator.includes(value)) {
+    accumulator.push(value);
+  }
+  return accumulator;
+}, []);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
