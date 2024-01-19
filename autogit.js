c426149a5209aa0bd1ef99a5780714b77f4dfd9b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:33:08 +0800

    1-19-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,7 @@
-function partition(arr, left, right) {
-  const pivot = arr[Math.floor((left + right) / 2)];
-  while (left <= right) {
-    while (arr[left] < pivot) {
-      left++;
-    }
-    while (arr[right] > pivot) {
-      right--;
-    }
-    if (left <= right) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      left++;
-      right--;
-    }
-  }
-  return left;
-}
-
-function quickselect(arr, k, left = 0, right = arr.length - 1) {
-  if (left === right) {
-    return arr[left];
-  }
-  const pivotIndex = partition(arr, left, right);
-  if (k === pivotIndex) {
-    return arr[k];
-  } else if (k < pivotIndex) {
-    return quickselect(arr, k, left, pivotIndex - 1);
-  } else {
-    return quickselect(arr, k, pivotIndex, right);
-  }
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
 // Example usage
-const array = [5, 2, 8, 3, 1, 9, 4];
-const k = 3;
-const kthSmallest = quickselect(array, k - 1);
-console.log(`The ${k}th smallest element is ${kthSmallest}`);
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(42)); // Output: 101010
