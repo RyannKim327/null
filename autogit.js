7a78e4d40c4c085616ad6307f3f92a55f2e8d835
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 23:01:32 +0800

    1-19-2024 23:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,9 @@
-const CronJob = require('cron').CronJob;
+const string = 'Hello, World!';
+const substring = 'Hello';
 
-// Define the cron schedule pattern (runs every minute)
-const schedule = '* * * * *';
-
-// Create a new cron job
-const job = new CronJob(schedule, function() {
-  // This code will be executed when the cron job triggers
-  console.log('Cron job triggered!');
-}, null, true);
-
-// Start the cron job
-job.start();
-
-// Logging a message to indicate the job has started
-console.log('Cron job started!');
+if (string.includes(substring)) {
+  console.log('The string contains the substring.');
+} else {
+  console.log('The string does not contain the substring.');
+}
+The string contains the substring.
