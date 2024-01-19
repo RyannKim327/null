ee7f1229a915df0ad4f739917c4d3718e21e81ba
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:26:46 +0800

    1-20-2024 6:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,20 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function isPrime(num) {
+  // 1 and numbers less than 1 are not prime
+  if (num <= 1) {
+    return false;
+  }
+
+  // Check for divisibility from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; // Number is divisible by i, hence not prime
+    }
+  }
+
+  return true; // Number is prime
 }
-var randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
+
+// Example usage
+console.log(isPrime(7)); // true
+console.log(isPrime(12)); // false
+console.log(isPrime(29)); // true
