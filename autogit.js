d5b31a790db16b60a94eeeb96d2f97c771ed2b94
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:31:35 +0800

    1-20-2024 6:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,38 @@
-function depthLimitedSearch(initialState, maxDepth) {
-  function DLS(state, depth) {
-    if (isGoalState(state)) {
-      return state;
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
+
+function LinkedList() {
+  this.head = null;
+}
+
+LinkedList.prototype.addNode = function(value) {
+  const newNode = new LinkedListNode(value);
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
     }
-    
-    if (depth >= maxDepth) {
-      return null;
-    }
-    
-    var children = generateChildStates(state);
-    
-    for (var i = 0; i < children.length; i++) {
-      var result = DLS(children[i], depth + 1);
-      if (result !== null) {
-        return result;
-      }
-    }
-    
-    return null;
+    current.next = newNode;
   }
-  
-  return DLS(initialState, 0);
-}
+};
 
-// Example helper functions
-function isGoalState(state) {
-  // Implement your own goal state check logic
-  return false;
-}
+LinkedList.prototype.getLength = function() {
+  let count = 0;
+  let current = this.head;
+  while (current !== null) {
+    count++;
+    current = current.next;
+  }
+  return count;
+};
 
-function generateChildStates(state) {
-  // Implement your own state generation logic
-  return [];
-}
+// Usage example:
+const myList = new LinkedList();
+myList.addNode(1);
+myList.addNode(2);
+myList.addNode(3);
+console.log(myList.getLength()); // Output: 3
