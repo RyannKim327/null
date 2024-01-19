70923f27f9e7057cf69d811b3929b4a7ed592abb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:57:25 +0800

    1-19-2024 22:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,22 @@
-function findLongestCommonPrefix(strings) {
-  if (strings.length === 0) {
-    return "";
-  } else if (strings.length === 1) {
-    return strings[0];
-  }
-
-  let prefix = "";
-
-  for (let i = 0; i < strings[0].length; i++) {
-    const char = strings[0][i];
-
-    for (let j = 1; j < strings.length; j++) {
-      if (strings[j][i] !== char) {
-        return prefix;
-      }
+function insertionSort(arr) {
+  const len = arr.length;
+  
+  for (let i = 1; i < len; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-
-    prefix += char;
+    
+    arr[j + 1] = key;
   }
-
-  return prefix;
+  
+  return arr;
 }
+const array = [5, 2, 4, 6, 1, 3];
+console.log('Original array:', array);
 
-// Example usage
-const strings = ["apple", "app", "application"];
-console.log(findLongestCommonPrefix(strings)); // Output: "app"
+const sortedArray = insertionSort(array);
+console.log('Sorted array:', sortedArray);
