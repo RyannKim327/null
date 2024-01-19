d2cfc50404e38f44a393e3d1162b1d9853002880
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:39:10 +0800

    1-20-2024 6:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,21 @@
-const str = "Hello World";
-const words = str.split(" ");
-const reversedWords = words.reverse();
-const reversedString = reversedWords.join(" ");
-function reverseStringWords(str) {
-  const words = str.split(" ");
-  const reversedWords = words.reverse();
-  const reversedString = reversedWords.join(" ");
-  return reversedString;
+function getIntersectionNode(headA, headB) {
+  let set = new Set();
+  
+  // Traverse through the first linked list and store node references in a set
+  let currentA = headA;
+  while (currentA !== null) {
+    set.add(currentA);
+    currentA = currentA.next;
+  }
+  
+  // Traverse through the second linked list and check for intersection
+  let currentB = headB;
+  while (currentB !== null) {
+    if (set.has(currentB)) {
+      return currentB; // Intersection found
+    }
+    currentB = currentB.next;
+  }
+  
+  return null; // No intersection found
 }
-
-const inputString = "Hello World";
-const result = reverseStringWords(inputString);
-console.log(result); // Output: "World Hello"
