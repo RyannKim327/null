e3ebe06ed55e76ff9dd641bdaf73a0ddbc62890b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-19 22:32:36 +0800

    1-19-2024 22:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,21 @@
-Area = (base * height) / 2
-function calculateTriangleArea(base, height) {
-    var area = (base * height) / 2;
-    return area;
+function isPrime(number) {
+  // Check if number is less than 2
+  if (number < 2) {
+    return false;
+  }
+
+  // Iterate from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    // If the number is divisible by any i, it is not prime
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  // If the number is not divisible by any i, it is prime
+  return true;
 }
 
-// Example usage:
-var base = 5;
-var height = 6;
-var triangleArea = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + triangleArea);
+// Example usage
+console.log(isPrime(7)); // true
+console.log(isPrime(12)); // false
