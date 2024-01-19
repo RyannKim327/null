5e823a7473e8715c9d1645d93a84143586cdcccb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-20 06:25:59 +0800

    1-20-2024 6:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,5 @@
-function breadthFirstSearch(graph, startNode) {
-  const queue = [];
-  const visited = [];
+// Prompt the user for input
+const userInput = prompt('Enter your input');
 
-  queue.push(startNode);
-  visited[startNode] = true;
-
-  while (queue.length > 0) {
-    const node = queue.shift();
-    visited.push(node);
-
-    const neighbors = graph[node];
-    for (let neighbor of neighbors) {
-      if (!visited[neighbor]) {
-        queue.push(neighbor);
-        visited[neighbor] = true;
-      }
-    }
-  }
-
-  return visited;
-}
-
-// Example usage:
-const graph = {
-  A: ['B', 'C'],
-  B: ['A', 'D'],
-  C: ['A', 'E'],
-  D: ['B', 'E', 'F'],
-  E: ['C', 'D', 'F'],
-  F: ['D', 'E']
-};
-
-console.log(breadthFirstSearch(graph, 'A'));
+// Log the user input to the console
+console.log('User input:', userInput);
