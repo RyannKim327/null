079da0527a8992a203e29711f8fbedbbbafb58e8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:25:03 +0800

    1-21-2024 7:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,37 @@
-class Graph {
-  constructor() {
-    this.vertices = [];
-    this.adjacencyList = {};
-  }
-
-  addVertex(vertex) {
-    this.vertices.push(vertex);
-    this.adjacencyList[vertex] = [];
-  }
-
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
-  }
-
-  dfs(startVertex, visited = {}) {
-    console.log(startVertex);
-    visited[startVertex] = true;
-
-    for (const neighbor of this.adjacencyList[startVertex]) {
-      if (!visited[neighbor]) {
-        this.dfs(neighbor, visited);
-      }
-    }
+class Node {
+  constructor(value, children = []) {
+    this.value = value;
+    this.children = children;
   }
 }
 
-// Example usage
-const myGraph = new Graph();
-myGraph.addVertex('A');
-myGraph.addVertex('B');
-myGraph.addVertex('C');
-myGraph.addVertex('D');
-myGraph.addEdge('A', 'B');
-myGraph.addEdge('B', 'D');
-myGraph.addEdge('D', 'C');
+function depthLimitedSearch(node, target, depth) {
+  if (node.value === target) {
+    return node;
+  }
 
-myGraph.dfs('A');
+  if (depth === 0) {
+    return null;
+  }
+
+  for (let child of node.children) {
+    const result = depthLimitedSearch(child, target, depth - 1);
+    if (result) {
+      return result;
+    }
+  }
+
+  return null;
+}
+
+// Example usage:
+const G = new Node("G");
+const F = new Node("F");
+const E = new Node("E", [G]);
+const D = new Node("D", [F]);
+const C = new Node("C", [E]);
+const B = new Node("B", [D]);
+const A = new Node("A", [B, C]);
+
+const result = depthLimitedSearch(A, "G", 3);
+console.log(result);
