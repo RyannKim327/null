206f49532c68ac66642ad610914b7df699da604d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:19:40 +0800

    1-21-2024 7:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,38 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
+class GraphNode {
+  constructor(id) {
+    this.id = id;
+    this.neighbors = [];
+  }
+
+  addNeighbor(node) {
+    this.neighbors.push(node);
+  }
 }
+function dfs(startNode, visited) {
+  // Mark the current node as visited
+  visited.add(startNode);
 
-var originalString = "Hello, World!";
-var reversedString = reverseString(originalString);
+  // Process the current node (print it, manipulate its data, etc.)
+  console.log(startNode.id);
 
-console.log(reversedString); // Output: "!dlroW ,olleH"
+  // Traverse the neighbors
+  for (const neighbor of startNode.neighbors) {
+    if (!visited.has(neighbor)) {
+      dfs(neighbor, visited); // Recursively visit the neighbor
+    }
+  }
+}
+// Create nodes
+const nodeA = new GraphNode('A');
+const nodeB = new GraphNode('B');
+const nodeC = new GraphNode('C');
+const nodeD = new GraphNode('D');
+const nodeE = new GraphNode('E');
+
+// Connect nodes with edges
+nodeA.addNeighbor(nodeB);
+nodeA.addNeighbor(nodeC);
+nodeB.addNeighbor(nodeD);
+nodeD.addNeighbor(nodeE);
+const visited = new Set();
+dfs(nodeA, visited);
