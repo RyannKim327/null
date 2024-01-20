4532e7cbe9dddd68f44741834812a93dd90d8871
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:16:40 +0800

    1-21-2024 7:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,27 @@
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-  let next = null;
-  
-  while (current !== null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  
-  return prev;
 }
+
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.val;
+}
+
+// Example usage:
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+console.log(findMiddleElement(head)); // Output: 3
