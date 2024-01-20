88c35abab531ffb31fabbd6e65e888f898a21f3d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:22:39 +0800

    1-21-2024 7:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,72 @@
-const axios = require('axios');
+function biDirectionalSearch(start, goal) {
+  let visitedStart = new Set();
+  let visitedGoal = new Set();
+  let queueStart = [start];
+  let queueGoal = [goal];
+  let parentStart = {};
+  let parentGoal = {};
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
+  while (queueStart.length && queueGoal.length) {
+    const currentStart = queueStart.shift();
 
-axios.get(url)
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    if (currentStart === goal || visitedGoal.has(currentStart)) {
+      return getPath(currentStart, goal, parentStart, parentGoal);
+    }
+
+    visitedStart.add(currentStart);
+    for (const neighbor of getNeighbors(currentStart)) {
+      if (!visitedStart.has(neighbor)) {
+        queueStart.push(neighbor);
+        parentStart[neighbor] = currentStart;
+      }
+    }
+
+    const currentGoal = queueGoal.shift();
+
+    if (currentGoal === start || visitedStart.has(currentGoal)) {
+      return getPath(start, currentGoal, parentStart, parentGoal);
+    }
+
+    visitedGoal.add(currentGoal);
+    for (const neighbor of getNeighbors(currentGoal)) {
+      if (!visitedGoal.has(neighbor)) {
+        queueGoal.push(neighbor);
+        parentGoal[neighbor] = currentGoal;
+      }
+    }
+  }
+
+  return null;
+}
+
+function getPath(start, goal, parentStart, parentGoal) {
+  const path = [start, goal];
+  let current = start;
+  
+  while (current !== goal) {
+    const next = parentStart[current];
+    path.push(next);
+    current = next;
+  }
+  
+  current = goal;
+  while (current !== start) {
+    const next = parentGoal[current];
+    path.unshift(next);
+    current = next;
+  }
+  
+  return path;
+}
+
+function getNeighbors(node) {
+  // Implement the logic to get the neighbors of a node here
+  // Return an array of neighboring nodes
+  // For example, if the nodes are represented as an adjacency list, you can return the corresponding array
+}
+
+// Example usage
+const startNode = 'A';
+const goalNode = 'F';
+const path = biDirectionalSearch(startNode, goalNode);
+console.log('Path:', path);
