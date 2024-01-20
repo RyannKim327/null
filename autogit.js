f3c6657a27c8d1fa4ef6b3d10e149b2343b79ea1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:16:25 +0800

    1-21-2024 7:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,14 @@
-function sanitizeString(str) {
-  return str.toLowerCase().replace(/[^a-z0-9]/g, '');
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+  
+  while (current !== null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  
+  return prev;
 }
-function reverseString(str) {
-  return str.split('').reverse().join('');
-}
-function isPalindrome(str) {
-  const sanitizedString = sanitizeString(str);
-  const reversedString = reverseString(sanitizedString);
-  return sanitizedString === reversedString;
-}
-const str1 = 'race car';
-console.log(isPalindrome(str1)); // Output: true
-
-const str2 = 'hello world';
-console.log(isPalindrome(str2)); // Output: false
