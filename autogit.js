5848cd963aba91751e0db9deede99b0f98d1c5f2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:31:36 +0800

    1-21-2024 7:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,5 @@
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-
-  push(value) {
-    this.stack.push(value);
-  }
-
-  pop() {
-    return this.stack.pop();
-  }
-
-  peek() {
-    return this.stack[this.stack.length - 1];
-  }
-
-  isEmpty() {
-    return this.stack.length === 0;
-  }
-
-  size() {
-    return this.stack.length;
-  }
-}
-const stack = new Stack();
-
-stack.push(1);
-stack.push(2);
-stack.push(3);
-
-console.log(stack.peek()); // Output: 3
-
-console.log(stack.pop()); // Output: 3
-console.log(stack.pop()); // Output: 2
-
-console.log(stack.size()); // Output: 1
-
-console.log(stack.isEmpty()); // Output: false
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: "hello world"
+let str = "Hello World";
+console.log(str.toLowerCase()); // Output: "hello world"
