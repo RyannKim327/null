ea9854de8981eaddb1001ab5ee363f90632846b8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:22:03 +0800

    1-21-2024 7:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,101 +1,14 @@
-class BinaryHeap {
-  constructor() {
-    this.heap = [];
-  }
+function getRandomNumber(min, max) {
+  // Generate a random decimal number between 0 and 1
+  const randomDecimal = Math.random();
+
+  // Scale the random decimal number to the desired range
+  const randomNumber = randomDecimal * (max - min + 1) + min;
+
+  // Convert the scaled number to a whole number (integer)
+  return Math.floor(randomNumber);
 }
-BinaryHeap.prototype.parent = function (index) {
-  return Math.floor((index - 1) / 2);
-};
 
-BinaryHeap.prototype.leftChild = function (index) {
-  return 2 * index + 1;
-};
-
-BinaryHeap.prototype.rightChild = function (index) {
-  return 2 * index + 2;
-};
-BinaryHeap.prototype.insert = function (value) {
-  this.heap.push(value);
-  this.heapifyUp();
-};
-
-BinaryHeap.prototype.remove = function () {
-  if (this.isEmpty()) {
-    return undefined;
-  }
-  if (this.size() === 1) {
-    return this.heap.pop();
-  }
-
-  const root = this.heap[0];
-  this.heap[0] = this.heap.pop();
-  this.heapifyDown();
-
-  return root;
-};
-BinaryHeap.prototype.heapifyUp = function () {
-  let currentIndex = this.size() - 1;
-
-  while (
-    currentIndex > 0 &&
-    this.heap[currentIndex] < this.heap[this.parent(currentIndex)]
-  ) {
-    this.swap(currentIndex, this.parent(currentIndex));
-    currentIndex = this.parent(currentIndex);
-  }
-};
-BinaryHeap.prototype.heapifyDown = function () {
-  let currentIndex = 0;
-
-  while (this.hasLeftChild(currentIndex)) {
-    let smallestChildIndex = this.leftChild(currentIndex);
-
-    if (
-      this.hasRightChild(currentIndex) &&
-      this.heap[this.rightChild(currentIndex)] < this.heap[smallestChildIndex]
-    ) {
-      smallestChildIndex = this.rightChild(currentIndex);
-    }
-
-    if (this.heap[currentIndex] < this.heap[smallestChildIndex]) {
-      break;
-    } else {
-      this.swap(currentIndex, smallestChildIndex);
-    }
-
-    currentIndex = smallestChildIndex;
-  }
-};
-BinaryHeap.prototype.size = function () {
-  return this.heap.length;
-};
-
-BinaryHeap.prototype.isEmpty = function () {
-  return this.size() === 0;
-};
-
-BinaryHeap.prototype.swap = function (index1, index2) {
-  [this.heap[index1], this.heap[index2]] = [
-    this.heap[index2],
-    this.heap[index1],
-  ];
-};
-
-BinaryHeap.prototype.hasLeftChild = function (index) {
-  return this.leftChild(index) < this.size();
-};
-
-BinaryHeap.prototype.hasRightChild = function (index) {
-  return this.rightChild(index) < this.size();
-};
-const pq = new BinaryHeap();
-pq.insert(5);
-pq.insert(10);
-pq.insert(3);
-pq.insert(8);
-
-console.log(pq.remove()); // Output: 3
-console.log(pq.remove()); // Output: 5
-console.log(pq.remove()); // Output: 8
-console.log(pq.remove()); // Output: 10
-console.log(pq.remove()); // Output: undefined (empty queue)
+// Example usage: generating a random number between 1 and 10
+const result = getRandomNumber(1, 10);
+console.log(result);
