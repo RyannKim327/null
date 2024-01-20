32cf9efe83bbfea66b9301d7babf22231b19e9e7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 07:29:59 +0800

    1-21-2024 7:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,14 @@
-function isPrime(number) {
-  if (number <= 1) {
-    return false; // Numbers less than or equal to 1 are not prime
-  }
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false; // If a divisor is found, it is not prime
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
     }
-  }
-  return true; // If no divisors are found, it is prime
+    return count;
 }
-function findLargestPrimeFactor(number) {
-  let largestFactor = 1;
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0 && isPrime(i)) {
-      largestFactor = i;
-    }
-  }
-  return largestFactor;
-}
-let number = 1234567890; // Replace with your desired number
-let largestPrimeFactor = findLargestPrimeFactor(number);
-console.log("The largest prime factor of", number, "is", largestPrimeFactor);
+let string = "Hello, world!";
+let character = "o";
+let occurrences = countOccurrences(string, character);
+console.log(`The character "${character}" occurs ${occurrences} times in the string.`);
+The character "o" occurs 2 times in the string.
