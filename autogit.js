d729d60fe4a93209375e2345940cf621878359f3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:08:11 +0800

    1-21-2024 9:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,8 @@
-function getRandomNumber(min, max) {
-  // Calculate the range of the random number
-  let range = max - min;
-
-  // Generate a random number between 0 and range
-  let randomNumber = Math.random() * range;
-
-  // Round down the random number and add it to the minimum value
-  return Math.floor(randomNumber) + min;
-}
-
-// Example Usage
-let min = 1;
-let max = 10;
-let randomNum = getRandomNumber(min, max);
-console.log(randomNum);
+let string1 = "Hello";
+let string2 = " World";
+let result = string1 + string2; // "Hello World"
+console.log(result);
+let string1 = "Hello";
+let string2 = " World";
+let result = string1.concat(string2); // "Hello World"
+console.log(result);
