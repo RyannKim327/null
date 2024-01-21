39c1ea2b4c15cf2cac2ed031fd229e57c4b100f0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:26:31 +0800

    1-21-2024 8:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,43 @@
-function getLinkedListLength(head) {
-  let count = 0;
-  let currentNode = head;
-
-  while (currentNode !== null) {
-    count++;
-    currentNode = currentNode.next; // Assuming each node has a 'next' property pointing to the next node
+// Node class representing a single node in the linked list
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  return count;
 }
-const length = getLinkedListLength(head);
-console.log(length);
+
+// Function to reverse a linked list
+function reverseLinkedList(head) {
+  let prevNode = null;
+  let currNode = head;
+  
+  while (currNode !== null) {
+    const nextNode = currNode.next;
+    currNode.next = prevNode;
+    prevNode = currNode;
+    currNode = nextNode;
+  }
+  
+  return prevNode;
+}
+// Creating a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Reversing the linked list
+const reversedHead = reverseLinkedList(head);
+
+// Printing the reversed linked list
+let currNode = reversedHead;
+while (currNode !== null) {
+  console.log(currNode.value);
+  currNode = currNode.next;
+}
+5
+4
+3
+2
+1
