bb04b4682b0597e4e0aaa53908a429ab51064536
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:08:57 +0800

    1-21-2024 9:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,68 @@
-function reverseWords(str) {
-  // Step 1: Split the string into an array of words
-  const wordsArray = str.split(' ');
-
-  // Step 2: Reverse the order of the array
-  const reversedArray = wordsArray.reverse();
-
-  // Step 3: Join the elements of the reversed array into a new string
-  const reversedString = reversedArray.join(' ');
-
-  return reversedString;
+class SkipListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = [];
+  }
 }
+class SkipList {
+  constructor() {
+    this.head = new SkipListNode(-Infinity);
+    this.maxHeight = 1;
+  }
+}
+insert(value) {
+  const newNode = new SkipListNode(value);
+  const nodePath = this.createNodePath(value);
 
-// Usage example
-const sentence = "Hello, how are you?";
-const reversedSentence = reverseWords(sentence);
-console.log(reversedSentence);
-you? are how Hello, 
+  for (let i = 0; i < nodePath.length; i++) {
+    const nextNode = nodePath[i].next[i];
+    nodePath[i].next[i] = newNode;
+    newNode.next[i] = nextNode;
+  }
+
+  if (nodePath.length > this.maxHeight) {
+    this.maxHeight = nodePath.length;
+  }
+}
+createNodePath(value) {
+  const path = [this.head];
+  let node = this.head;
+  for (let i = this.maxHeight - 1; i >= 0; i--) {
+    while (node.next[i] && node.next[i].value < value) {
+      node = node.next[i];
+    }
+    path[i] = node;
+  }
+  return path;
+}
+search(value) {
+  let node = this.head;
+  for (let i = this.maxHeight - 1; i >= 0; i--) {
+    while (node.next[i] && node.next[i].value < value) {
+      node = node.next[i];
+    }
+
+    if (node.next[i] && node.next[i].value === value) {
+      return node.next[i];
+    }
+  }
+  return null;
+}
+remove(value) {
+  const nodePath = this.createNodePath(value);
+  const nodeToRemove = nodePath[0].next[0];
+
+  if (!nodeToRemove || nodeToRemove.value !== value) {
+    return; // Element not found
+  }
+
+  for (let i = 0; i < nodeToRemove.next.length; i++) {
+    const prevNode = nodePath[i];
+    const nextNode = nodeToRemove.next[i];
+    prevNode.next[i] = nextNode;
+  }
+
+  while (this.maxHeight > 1 && !this.head.next[this.maxHeight - 1]) {
+    this.maxHeight--;
+  }
+}
