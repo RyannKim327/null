7b9a3d4974e5cfd8dfa01f4b78e7000a831ef5d3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:42:45 +0800

    1-21-2024 8:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,10 @@
-function hasCycle(head) {
-  if (!head || !head.next) {
-    return false;
-  }
-  
-  let slow = head;
-  let fast = head.next;
-  
-  while (slow !== fast) {
-    if (!fast || !fast.next) {
-      return false;
-    }
-    
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  
-  return true;
-}
-// Definition for a linked list node
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+const cron = require('cron');
 
-// Create a linked list with a cycle
-const head = new ListNode(1);
-const node2 = new ListNode(2);
-const node3 = new ListNode(3);
-const node4 = new ListNode(4);
-const node5 = new ListNode(5);
+// Create a new cron job with the desired schedule
+const job = new cron.CronJob('0 0 0 * * *', () => {
+  // Code to be executed when the cron job runs
+  console.log('This job runs at midnight every day!');
+});
 
-head.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-node5.next = node2; // This creates a cycle
-
-console.log(hasCycle(head)); // Output: true
+// Start the cron job
+job.start();
