89a30f6c3446f1757b8c57a1405f4c58718af351
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:17:06 +0800

    1-21-2024 9:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,10 @@
-function heapify(arr, n, i) {
-  let largest = i;
-  let left = 2 * i + 1;
-  let right = 2 * i + 2;
+let array = [5, 2, 8, 1, 9];
 
-  if (left < n && arr[left] > arr[largest])
-    largest = left;
+array.sort((a, b) => a - b);
 
-  if (right < n && arr[right] > arr[largest])
-    largest = right;
+console.log(array); // Output: [1, 2, 5, 8, 9]
+let array = [5, 2, 8, 1, 9];
 
-  if (largest !== i) {
-    [arr[i], arr[largest]] = [arr[largest], arr[i]];
-    heapify(arr, n, largest);
-  }
-}
+array.sort((a, b) => b - a);
 
-
-function buildHeap(arr, n) {
-  let startIdx = Math.floor(n / 2) - 1;
-
-  for (let i = startIdx; i >= 0; i--) {
-    heapify(arr, n, i);
-  }
-}
-function heapSort(arr) {
-  let n = arr.length;
-
-  buildHeap(arr, n);
-
-  for (let i = n - 1; i >= 0; i--) {
-    [arr[0], arr[i]] = [arr[i], arr[0]];
-    heapify(arr, i, 0);
-  }
-}
-let array = [7, 2, 4, 1, 5, 3];
-heapSort(array);
-console.log(array); // [1, 2, 3, 4, 5, 7]
+console.log(array); // Output: [9, 8, 5, 2, 1]
