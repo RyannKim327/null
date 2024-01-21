7778647c76c05d3d72e849a9406b2f97abe9eb73
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:52:47 +0800

    1-21-2024 8:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,17 @@
-function BFS(graph, startNode) {
-  const queue = [];
-  const visited = new Set();
-  const traversalOrder = [];
-  queue.push(startNode);
-  visited.add(startNode);
-  while (queue.length > 0) {
-    const currentNode = queue.shift();
-    traversalOrder.push(currentNode);
+function beamSearch(initialState, beamWidth, scoreFunction, generateSuccessors, terminationCondition) {
+  let candidates = [initialState];
 
-    const neighbors = graph[currentNode];
-    for (let i = 0; i < neighbors.length; i++) {
-      const neighbor = neighbors[i];
-      if (!visited.has(neighbor)) {
-        queue.push(neighbor);
-        visited.add(neighbor);
-      }
+  while (!terminationCondition()) {
+    let successors = [];
+
+    for (let candidate of candidates) {
+      let candidateSuccessors = generateSuccessors(candidate);
+      successors.push(...candidateSuccessors);
     }
+
+    successors.sort((a, b) => scoreFunction(b) - scoreFunction(a));
+    candidates = successors.slice(0, beamWidth);
   }
-  return traversalOrder;
+
+  return candidates[0]; // Return the best candidate solution found
 }
-
-// Test example graph
-const graph = {
-  A: ['B', 'C'],
-  B: ['A', 'D'],
-  C: ['A', 'E'],
-  D: ['B'],
-  E: ['C']
-};
-
-console.log(BFS(graph, 'A'));
