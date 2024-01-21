232fb9d8d432839cd51ed84e7a619152c4e69365
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:45:45 +0800

    1-21-2024 8:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,70 +1,15 @@
-function largestPrimeFactor(number) {
-  // Your code here
-}
-function largestPrimeFactor(number) {
-  let largestFactor = 1;
-  // Your code here
-}
-function largestPrimeFactor(number) {
-  let largestFactor = 1;
-  
-  while (number % 2 === 0) {
-    largestFactor = 2;
-    number = number / 2;
-  }
-  
-  // Your code here
-}
-function largestPrimeFactor(number) {
-  let largestFactor = 1;
-  
-  while (number % 2 === 0) {
-    largestFactor = 2;
-    number = number / 2;
-  }
-  
-  for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
-    // Your code here
-  }
-}
-function largestPrimeFactor(number) {
-  let largestFactor = 1;
-  
-  while (number % 2 === 0) {
-    largestFactor = 2;
-    number = number / 2;
-  }
-  
-  for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
-    while (number % i === 0) {
-      largestFactor = i;
-      number = number / i;
+function countOccurrence(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      count++;
     }
   }
-  
-  // Your code here
+  return count;
 }
-function largestPrimeFactor(number) {
-  let largestFactor = 1;
-  
-  while (number % 2 === 0) {
-    largestFactor = 2;
-    number = number / 2;
-  }
-  
-  for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
-    while (number % i === 0) {
-      largestFactor = i;
-      number = number / i;
-    }
-  }
-  
-  if (number > 2) {
-    largestFactor = number;
-  }
-  
-  return largestFactor;
-}
-const number = 84;
-const largestPrimeFactorOfNumber = largestPrimeFactor(number);
-console.log(largestPrimeFactorOfNumber);
+
+// Example usage
+const text = "Hello, world!";
+const charToCount = "o";
+const occurrenceCount = countOccurrence(text, charToCount);
+console.log(`The character "${charToCount}" occurs ${occurrenceCount} times.`);
