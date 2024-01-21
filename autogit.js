3120f56f084c731a9b23966d4afdffb6a7cd6900
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:15:17 +0800

    1-21-2024 9:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,23 @@
-let str = "Hello World!";
-let withoutSpaces = str.replace(/\s/g, "");
-console.log(withoutSpaces); // Output: "HelloWorld!"
-let str = "Hello World!";
-let withoutSpaces = str.split(" ").join("");
-console.log(withoutSpaces); // Output: "HelloWorld!"
-let str = "Hello World!";
-let withoutSpaces = str.replace(" ", "");
-console.log(withoutSpaces); // Output: "HelloWorld!"
+function findFirstRepeatedChar(str) {
+  const charCount = {};
+  
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    
+    // If the character has already been encountered, it is the first repeated character
+    if (charCount[char]) {
+      return char;
+    }
+    
+    // Otherwise, mark the character as encountered by setting its count to 1
+    charCount[char] = 1;
+  }
+  
+  // If no repeated character is found, return undefined or any other value of your choice
+  return undefined;
+}
+
+// Example usage
+const inputString = "Hello World";
+const firstRepeatedChar = findFirstRepeatedChar(inputString);
+console.log(firstRepeatedChar); // Output: "l"
