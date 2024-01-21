6219c447a7863bb7d6a4f135445f18703f76dc00
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:59:21 +0800

    1-21-2024 8:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,72 @@
-function largestPrimeFactor(number) {
-  if (number < 2) {
-    return "No prime factors.";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+class BinarySearchTree {
+  // ...
 
-  let factor = 2;
+  insert(value) {
+    const newNode = new Node(value);
 
-  while (factor <= number) {
-    if (number % factor === 0) {
-      number /= factor;
+    if (this.root === null) {
+      this.root = newNode;
     } else {
-      factor++;
+      let current = this.root;
+
+      while (true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode;
+            break;
+          }
+          current = current.left;
+        } else {
+          if (current.right === null) {
+            current.right = newNode;
+            break;
+          }
+          current = current.right;
+        }
+      }
     }
   }
-
-  return factor;
 }
+class BinarySearchTree {
+  // ...
 
-// Example usage:
-console.log(largestPrimeFactor(84));  // Output: 7
-console.log(largestPrimeFactor(13195));  // Output: 29
+  search(value) {
+    let current = this.root;
+
+    while (current !== null) {
+      if (value === current.value) {
+        return current;
+      } else if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+
+    return null;
+  }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+console.log(bst.search(60)); // Output: Node { value: 60, left: null, right: null }
+console.log(bst.search(55)); // Output: null
