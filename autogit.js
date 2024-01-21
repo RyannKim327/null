b1404a5419c87efd00123c8ab924fa0aa0537f4b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:48:45 +0800

    1-21-2024 8:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,57 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lower case
-  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+class HashTable {
+  constructor(size = 100) {
+    this.size = size;
+    this.table = new Array(size);
+  }
 
-  // Create the reversed version of the string
-  var reversed = str.split("").reverse().join("");
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * (i + 1)) % this.size;
+    }
+    return hash;
+  }
 
-  // Compare the original string with its reversed version
-  return str === reversed;
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (let i = 0; i < this.table[index].length; i++) {
+      if (this.table[index][i][0] === key) {
+        return this.table[index][i][1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      return;
+    }
+    for (let i = 0; i < this.table[index].length; i++) {
+      if (this.table[index][i][0] === key) {
+        this.table[index].splice(i, 1);
+        return;
+      }
+    }
+  }
 }
+const hashTable = new HashTable();
+hashTable.set("name", "John");
+hashTable.set("age", 25);
 
-// Test the function
-console.log(isPalindrome("level")); // true
-console.log(isPalindrome("Hello")); // false
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(hashTable.get("name")); // Output: "John"
+console.log(hashTable.get("age")); // Output: 25
+
+hashTable.remove("age");
+console.log(hashTable.get("age")); // Output: undefined
