dae00b30019c75ef50ad46e3a36bb0c9fe3ea41c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:23:42 +0800

    1-21-2024 9:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,74 +1,26 @@
-function biDirectionalSearch(startNode, endNode) {
-  let forwardQueue = [startNode];
-  let backwardQueue = [endNode];
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-  let forwardVisited = {};
-  let backwardVisited = {};
+  const pivot = arr[Math.floor(arr.length / 2)];
+  const less = [];
+  const equal = [];
+  const greater = [];
 
-  let commonNode = null;
-
-  while (forwardQueue.length > 0 && backwardQueue.length > 0) {
-    let currentForwardNode = forwardQueue.shift();
-    if (backwardVisited[currentForwardNode]) {
-      commonNode = currentForwardNode;
-      break;
-    }
-    forwardVisited[currentForwardNode] = true;
-
-    let currentBackwardNode = backwardQueue.shift();
-    if (forwardVisited[currentBackwardNode]) {
-      commonNode = currentBackwardNode;
-      break;
-    }
-    backwardVisited[currentBackwardNode] = true;
-
-    // Add neighbors to forwardQueue
-    let forwardNeighbors = getAllNeighbors(currentForwardNode);
-    for (let neighbor of forwardNeighbors) {
-      if (!forwardVisited[neighbor]) {
-        forwardQueue.push(neighbor);
-      }
-    }
-
-    // Add neighbors to backwardQueue
-    let backwardNeighbors = getAllNeighbors(currentBackwardNode);
-    for (let neighbor of backwardNeighbors) {
-      if (!backwardVisited[neighbor]) {
-        backwardQueue.push(neighbor);
-      }
+  for (let element of arr) {
+    if (element < pivot) {
+      less.push(element);
+    } else if (element > pivot) {
+      greater.push(element);
+    } else {
+      equal.push(element);
     }
   }
 
-  if (commonNode) {
-    // Construct shortest path
-    let shortestPath = [];
-
-    let currentNode = startNode;
-    while (currentNode !== commonNode) {
-      shortestPath.push(currentNode);
-      currentNode = getNextNode(currentNode, forwardVisited);
-    }
-
-    shortestPath.push(commonNode);
-
-    currentNode = endNode;
-    while (currentNode !== commonNode) {
-      shortestPath.unshift(currentNode);
-      currentNode = getNextNode(currentNode, backwardVisited);
-    }
-
-    return shortestPath;
-  }
-
-  return null; // No path found
+  return [...quicksort(less), ...equal, ...quicksort(greater)];
 }
+const unsortedArray = [5, 3, 8, 4, 2, 9, 1, 7, 6];
+const sortedArray = quicksort(unsortedArray);
 
-// Helper functions to get neighbors and next node
-
-function getAllNeighbors(node) {
-  // Return an array of neighbors for a given node
-}
-
-function getNextNode(node, visited) {
-  // Return the next node in the shortest path using the visited object
-}
+console.log(sortedArray);
