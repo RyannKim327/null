449f86d8c1f0277a485b3558b13c5286307892d3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:56:16 +0800

    1-21-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,3 @@
-function selectionSort(arr) {
-  const length = arr.length;
-
-  for (let i = 0; i < length - 1; i++) {
-    let minIndex = i;
-
-    // Find the index of the smallest element in the unsorted portion
-    for (let j = i + 1; j < length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    // Swap the smallest element with the first element of the unsorted portion
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
-  }
-
-  return arr;
-}
-
-// Example usage
-const unsortedArray = [64, 25, 12, 22, 11];
-const sortedArray = selectionSort(unsortedArray);
-console.log(sortedArray); // Output: [11, 12, 22, 25, 64]
+let str = "Hello, world! How are you?";
+let newStr = str.replace(/\s/g, "");
+console.log(newStr); // Output: "Hello,world!Howareyou?"
