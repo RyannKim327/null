809e8df669e75fa66d9c673f1b335fff21711f31
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:13:39 +0800

    1-21-2024 9:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-// Function to check if a number is prime
-function isPrime(num) {
-  if (num < 2) return false;
-  
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  
-  return true;
-}
+let str = "42";
+let num = parseInt(str);
 
-// Function to find the largest prime factor
-function largestPrimeFactor(num) {
-  let largestFactor = 1;
-  
-  // Divide the number by 2 until it is odd
-  while (num % 2 === 0) {
-    largestFactor = 2;
-    num = num / 2;
-  }
-  
-  // Check for prime factors starting from 3
-  for (let i = 3; i <= Math.sqrt(num); i += 2) {
-    while (num % i === 0) {
-      largestFactor = i;
-      num = num / i;
-    }
-  }
-  
-  // When num is a prime number greater than 2
-  if (num > 2) {
-    largestFactor = num;
-  }
-  
-  return largestFactor;
-}
+console.log(num); // Output: 42
+console.log(typeof num); // Output: number
+let str = "1010";
+let num = parseInt(str, 2); // Parsing binary
 
-// Example usage
-const number = 600851475143;
-const result = largestPrimeFactor(number);
-console.log("Largest prime factor of", number, "is", result);
+console.log(num); // Output: 10
