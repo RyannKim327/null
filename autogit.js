317d3594cafb0770c158e01918de11bc6c531d47
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:34:29 +0800

    1-21-2024 8:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,19 @@
-function hasCycle(head) {
-  if (!head || !head.next) {
-    return false;
-  }
+const cron = require('cron');
 
-  let tortoise = head;
-  let hare = head.next;
-
-  while (tortoise !== hare) {
-    if (!hare || !hare.next) {
-      return false;
-    }
-
-    tortoise = tortoise.next;
-    hare = hare.next.next;
-  }
-
-  return true;
+// Define the task function
+function myTask() {
+  console.log('Running scheduled task...');
+  // Add your task logic here
 }
-// Example usage
-const node1 = { value: 1 };
-const node2 = { value: 2 };
-const node3 = { value: 3 };
-const node4 = { value: 4 };
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node2; // Creating a cycle
+// Schedule the task to run every minute
+const job = new cron.CronJob('* * * * *', myTask);
 
-console.log(hasCycle(node1)); // Output: true
+// Start the cron job
+job.start();
+
+// Stop the cron job after 5 minutes
+setTimeout(() => {
+  job.stop();
+  console.log('Cron job stopped.');
+}, 5 * 60 * 1000);
