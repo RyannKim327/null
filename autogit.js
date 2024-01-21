24ed820c5fdc4fb7e299c3ac70bb14573879d146
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:59:52 +0800

    1-21-2024 8:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,12 @@
-function shellSort(arr) {
-  // Shell sort logic goes here
+let str = "Hello, World!";
+let reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
+let str = "Hello, World!";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-function shellSort(arr) {
-  let gap = Math.floor(arr.length / 2);
-  // Shell sort logic goes here
-}
-function shellSort(arr) {
-  let gap = Math.floor(arr.length / 2);
-
-  while (gap > 0) {
-    // Shell sort logic goes here
-
-    gap = Math.floor(gap / 2);
-  }
-}
-function shellSort(arr) {
-  let gap = Math.floor(arr.length / 2);
-
-  while (gap > 0) {
-    for (let i = gap; i < arr.length; i++) {
-      let j = i;
-      let temp = arr[i];
- 
-      while (j >= gap && arr[j - gap] > temp) {
-        arr[j] = arr[j - gap];
-        j -= gap;
-      }
- 
-      arr[j] = temp;
-    }
-
-    gap = Math.floor(gap / 2);
-  }
-
-  return arr;
-}
-function shellSort(arr) {
-  let gap = Math.floor(arr.length / 2);
-
-  while (gap > 0) {
-    for (let i = gap; i < arr.length; i++) {
-      let j = i;
-      let temp = arr[i];
- 
-      while (j >= gap && arr[j - gap] > temp) {
-        arr[j] = arr[j - gap];
-        j -= gap;
-      }
- 
-      arr[j] = temp;
-    }
-
-    gap = Math.floor(gap / 2);
-  }
-
-  return arr;
-}
-
-// Usage:
-const unsortedArray = [9, 2, 5, 3, 8, 7, 1, 10, 4, 6];
-const sortedArray = shellSort(unsortedArray);
-console.log(sortedArray);
+console.log(reversedStr);
+let str = "Hello, World!";
+let reversedStr = [...str].reverse().join("");
+console.log(reversedStr);
