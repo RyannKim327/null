192e6f95604b79a3e906165d908982c74c9238f3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:41:26 +0800

    1-21-2024 8:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,13 @@
-function heapify(array, size, index) {
-  let largest = index;
-  const left = 2 * index + 1;
-  const right = 2 * index + 2;
+function getLinkedListLength(head) {
+  let length = 0;
+  let currentNode = head;
 
-  if (left < size && array[left] > array[largest]) {
-    largest = left;
+  while (currentNode !== null) {
+    length++;
+    currentNode = currentNode.next; // Assumes each node has a 'next' property
   }
 
-  if (right < size && array[right] > array[largest]) {
-    largest = right;
-  }
-
-  if (largest !== index) {
-    [array[index], array[largest]] = [array[largest], array[index]];
-    heapify(array, size, largest);
-  }
+  return length;
 }
-
-function buildMaxHeap(array) {
-  const size = array.length;
-
-  for (let i = Math.floor(size / 2) - 1; i >= 0; i--) {
-    heapify(array, size, i);
-  }
-}
-function heapSort(array) {
-  const size = array.length;
-
-  buildMaxHeap(array);
-
-  for (let i = size - 1; i > 0; i--) {
-    [array[0], array[i]] = [array[i], array[0]];
-    heapify(array, i, 0);
-  }
-
-  return array;
-}
-const array = [5, 9, 2, 1, 6, 3];
-console.log(heapSort(array)); // Output: [1, 2, 3, 5, 6, 9]
+const length = getLinkedListLength(head);
+console.log(length);
