613c0e457fdf716154a254d0092ae0a28d1bab84
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:17:36 +0800

    1-21-2024 9:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,56 @@
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = Array.from(new Set(array));
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = array.filter((value, index, self) => {
-  return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 3, 4, 5, 5];
-const uniqueArray = [];
-for (let i = 0; i < array.length; i++) {
-  if (uniqueArray.indexOf(array[i]) === -1) {
-    uniqueArray.push(array[i]);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
+
+function reverseLinkedList(head) {
+  let current = head;
+  let previous = null;
+  let next = null;
+
+  while (current !== null) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
+// Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+
+const head = new Node(1);
+const second = new Node(2);
+const third = new Node(3);
+const fourth = new Node(4);
+const fifth = new Node(5);
+
+head.next = second;
+second.next = third;
+third.next = fourth;
+fourth.next = fifth;
+
+console.log('Original List:');
+printList(head);
+
+const reversedHead = reverseLinkedList(head);
+
+console.log('Reversed List:');
+printList(reversedHead);
+
+function printList(node) {
+  let current = node;
+  let list = '';
+  while (current !== null) {
+    list += current.value + ' -> ';
+    current = current.next;
+  }
+  list += 'null';
+  console.log(list);
+}
+Original List:
+1 -> 2 -> 3 -> 4 -> 5 -> null
+Reversed List:
+5 -> 4 -> 3 -> 2 -> 1 -> null
