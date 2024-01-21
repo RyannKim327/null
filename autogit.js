66ffe001a31607b48c15f20b64c98b4b1978a1fc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:52:14 +0800

    1-21-2024 8:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,126 +1,32 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+function topologicalSort(graph) {
+  const visited = new Set();
+  const order = [];
+
+  function dfs(node) {
+    visited.add(node);
+
+    for (const neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor);
+      }
+    }
+
+    order.unshift(node);
   }
+
+  for (const node in graph) {
+    if (!visited.has(node)) {
+      dfs(node);
+    }
+  }
+
+  return order;
 }
-
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-  
-  insert(value) {
-    const newNode = new Node(value);
-    
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-  
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-  
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-  
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    }
-    
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    } else if (value > node.value) {
-      return this.searchNode(node.right, value);
-    } else {
-      return true;
-    }
-  }
-  
-  remove(value) {
-    this.root = this.removeNode(this.root, value);
-  }
-  
-  removeNode(node, value) {
-    if (node === null) {
-      return null;
-    }
-    
-    if (value < node.value) {
-      node.left = this.removeNode(node.left, value);
-      return node;
-    } else if (value > node.value) {
-      node.right = this.removeNode(node.right, value);
-      return node;
-    } else {
-      // Node to be deleted has no children
-      if (node.left === null && node.right === null) {
-        node = null;
-        return node;
-      }
-      
-      // Node to be deleted has one child
-      if (node.left === null) {
-        node = node.right;
-        return node;
-      } else if (node.right === null) {
-        node = node.left;
-        return node;
-      }
-      
-      // Node to be deleted has two children
-      const minNode = this.findMinNode(node.right);
-      node.value = minNode.value;
-      node.right = this.removeNode(node.right, minNode.value);
-      return node;
-    }
-  }
-  
-  findMinNode(node) {
-    if (node.left === null) {
-      return node;
-    } else {
-      return this.findMinNode(node.left);
-    }
-  }
-  
-  getRootNode() {
-    return this.root;
-  }
-}
-const bst = new BinarySearchTree();
-bst.insert(8);
-bst.insert(3);
-bst.insert(10);
-bst.insert(1);
-bst.insert(6);
-bst.insert(14);
-bst.insert(4);
-bst.insert(7);
-bst.insert(13);
-
-console.log(bst.search(7)); // true
-console.log(bst.search(9)); // false
-
-bst.remove(6);
-console.log(bst.search(6)); // false
-
-console.log(bst.getRootNode()); // Node object representing the root
+const graph = {
+  A: ['B', 'C'],
+  B: ['D'],
+  C: [],
+  D: ['E'],
+  E: [],
+};
+console.log(topologicalSort(graph));
