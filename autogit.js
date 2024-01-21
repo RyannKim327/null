d1f2863fef3ea54efb3a6bfb861e882ee5afd7bf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:23:56 +0800

    1-21-2024 9:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,11 @@
-function quicksort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const less = [];
-  const equal = [];
-  const greater = [];
-
-  for (let element of arr) {
-    if (element < pivot) {
-      less.push(element);
-    } else if (element > pivot) {
-      greater.push(element);
-    } else {
-      equal.push(element);
-    }
-  }
-
-  return [...quicksort(less), ...equal, ...quicksort(greater)];
-}
-const unsortedArray = [5, 3, 8, 4, 2, 9, 1, 7, 6];
-const sortedArray = quicksort(unsortedArray);
-
-console.log(sortedArray);
+// Fetch data from API
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Process the response data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.log('An error occurred:', error);
+  });
