926dadcb65c4d38d6427ca2e2eafcc3a66db13b7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 09:23:28 +0800

    1-21-2024 9:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,74 @@
-function factorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  
-  return result;
-}
-// Calculate the factorial of 5
-console.log(factorial(5)); // Output: 120
+function biDirectionalSearch(startNode, endNode) {
+  let forwardQueue = [startNode];
+  let backwardQueue = [endNode];
 
-// Calculate the factorial of 10
-console.log(factorial(10)); // Output: 3628800
+  let forwardVisited = {};
+  let backwardVisited = {};
+
+  let commonNode = null;
+
+  while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+    let currentForwardNode = forwardQueue.shift();
+    if (backwardVisited[currentForwardNode]) {
+      commonNode = currentForwardNode;
+      break;
+    }
+    forwardVisited[currentForwardNode] = true;
+
+    let currentBackwardNode = backwardQueue.shift();
+    if (forwardVisited[currentBackwardNode]) {
+      commonNode = currentBackwardNode;
+      break;
+    }
+    backwardVisited[currentBackwardNode] = true;
+
+    // Add neighbors to forwardQueue
+    let forwardNeighbors = getAllNeighbors(currentForwardNode);
+    for (let neighbor of forwardNeighbors) {
+      if (!forwardVisited[neighbor]) {
+        forwardQueue.push(neighbor);
+      }
+    }
+
+    // Add neighbors to backwardQueue
+    let backwardNeighbors = getAllNeighbors(currentBackwardNode);
+    for (let neighbor of backwardNeighbors) {
+      if (!backwardVisited[neighbor]) {
+        backwardQueue.push(neighbor);
+      }
+    }
+  }
+
+  if (commonNode) {
+    // Construct shortest path
+    let shortestPath = [];
+
+    let currentNode = startNode;
+    while (currentNode !== commonNode) {
+      shortestPath.push(currentNode);
+      currentNode = getNextNode(currentNode, forwardVisited);
+    }
+
+    shortestPath.push(commonNode);
+
+    currentNode = endNode;
+    while (currentNode !== commonNode) {
+      shortestPath.unshift(currentNode);
+      currentNode = getNextNode(currentNode, backwardVisited);
+    }
+
+    return shortestPath;
+  }
+
+  return null; // No path found
+}
+
+// Helper functions to get neighbors and next node
+
+function getAllNeighbors(node) {
+  // Return an array of neighbors for a given node
+}
+
+function getNextNode(node, visited) {
+  // Return the next node in the shortest path using the visited object
+}
