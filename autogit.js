e9c81f71f9adfb6178eee452faa346d470be1021
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:43:01 +0800

    1-21-2024 8:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,43 @@
-const cron = require('cron');
+class HashTable {
+  constructor() {
+    this.table = {};
+  }
 
-// Create a new cron job with the desired schedule
-const job = new cron.CronJob('0 0 0 * * *', () => {
-  // Code to be executed when the cron job runs
-  console.log('This job runs at midnight every day!');
-});
+  // Compute the hash code for a given key
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
 
-// Start the cron job
-job.start();
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const hashKey = this.hash(key);
+    this.table[hashKey] = value;
+  }
+
+  // Retrieve the value associated with a given key
+  get(key) {
+    const hashKey = this.hash(key);
+    return this.table[hashKey];
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const hashKey = this.hash(key);
+    delete this.table[hashKey];
+  }
+}
+
+// Example usage:
+const myHashTable = new HashTable();
+myHashTable.insert("apple", 10);
+myHashTable.insert("banana", 20);
+
+console.log(myHashTable.get("apple")); // Output: 10
+console.log(myHashTable.get("banana")); // Output: 20
+
+myHashTable.remove("apple");
+console.log(myHashTable.get("apple")); // Output: undefined
