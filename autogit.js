b245c1f396a47894742ae138cca0f82600124c96
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:30:41 +0800

    1-21-2024 8:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,11 @@
-let string = "Hello, World!";
-let reversedString = string.split("").reverse().join("");
-console.log(reversedString);
-let string = "Hello, World!";
-let reversedString = "";
-for (let i = string.length - 1; i >= 0; i--) {
-  reversedString += string[i];
-}
-console.log(reversedString);
-function reverseString(string) {
-  if (string === "")
-    return "";
-  else
-    return reverseString(string.substr(1)) + string.charAt(0);
-}
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-let string = "Hello, World!";
-let reversedString = reverseString(string);
-console.log(reversedString);
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
