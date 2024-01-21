4c518f66defea22fb4cc886bee6cdbd6511cd360
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:26:16 +0800

    1-21-2024 8:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,13 @@
-function findLongestCommonSubstring(str1, str2) {
-  // Initialize a 2-dimensional array to store the lengths of common substrings
-  const lengths = Array(str1.length + 1)
-    .fill(0)
-    .map(() => Array(str2.length + 1).fill(0));
+function getLinkedListLength(head) {
+  let count = 0;
+  let currentNode = head;
 
-  let maxLength = 0; // length of longest common substring
-  let endIndex = 0; // end index of longest common substring in str1
-
-  // Iterate through the strings to fill the lengths matrix
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        // If the characters match, increment the length of the common substring
-        lengths[i][j] = lengths[i - 1][j - 1] + 1;
-        if (lengths[i][j] > maxLength) {
-          // If the new substring is longer, update the maxLength and endIndex
-          maxLength = lengths[i][j];
-          endIndex = i - 1;
-        }
-      }
-    }
+  while (currentNode !== null) {
+    count++;
+    currentNode = currentNode.next; // Assuming each node has a 'next' property pointing to the next node
   }
 
-  // Extract the longest common substring using the endIndex
-  const longestCommonSubstring = str1.substr(endIndex - maxLength + 1, maxLength);
-  return longestCommonSubstring;
+  return count;
 }
-
-// Example usage:
-const str1 = "abcdefg";
-const str2 = "defgxyz";
-const longestCommonSubstring = findLongestCommonSubstring(str1, str2);
-console.log(longestCommonSubstring); // Output: "defg"
+const length = getLinkedListLength(head);
+console.log(length);
