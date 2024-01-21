a6cdfb6d24aa1e98b33f65c5e081cc8afa29d57a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:23:34 +0800

    1-21-2024 8:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,8 @@
-// Prompt the user for input
-var userInput = prompt("Enter a number:");
-
-// Convert the user input to a number
-var number = parseInt(userInput);
-
-// Check if the input is a valid number
-if (isNaN(number)) {
-  console.log("Invalid input! Please enter a valid number.");
-} else {
-  // Perform some calculations with the number
-  var square = number * number;
-  var cube = number * number * number;
-
-  // Display the results
-  console.log("Number: " + number);
-  console.log("Square: " + square);
-  console.log("Cube: " + cube);
-}
+const string1 = "Hello";
+const string2 = "World";
+const concatenatedString = string1 + " " + string2;
+console.log(concatenatedString); // Output: Hello World
+const string1 = "Hello";
+const string2 = "World";
+const concatenatedString = string1.concat(" ", string2);
+console.log(concatenatedString); // Output: Hello World
