775de46acf3e66d37cede8b5fe06b1bd82ef04b9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-21 08:37:48 +0800

    1-21-2024 8:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,11 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len - 1; i++) {
-    for (var j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swapping the elements
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
+// Function to generate a random number in a given range
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example usage:
-var array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original Array: " + array);
-console.log("Sorted Array: " + bubbleSort(array));
+// Example usage
+var minRange = 1;
+var maxRange = 10;
+
+var randomNumber = getRandomNumber(minRange, maxRange);
+console.log(randomNumber);
