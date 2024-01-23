5a15eb912ac90f299528bedec00307fb895d446f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:36:33 +0800

    1-24-2024 5:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,10 @@
-// Make a GET request to an API endpoint
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    // Process the data returned from the API
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the request
-    console.log('Error:', error);
-  });
+let string1 = "Hello";
+let string2 = "world";
+
+let result = string1 + " " + string2;
+console.log(result); // Output: "Hello world"
+let string1 = "Hello";
+let string2 = "world";
+
+let result = string1.concat(" ", string2);
+console.log(result); // Output: "Hello world"
