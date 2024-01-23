970644f3d78ca13fee5d15d6e91b8e8736f0db6c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:14:08 +0800

    1-24-2024 6:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,53 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+function tarjansSCC(graph) {
+  let index = 0;
+  const indexes = {};
+  const lowlinks = {};
+  const onStack = {};
+  const stack = [];
+  const components = [];
+
+  function dfs(node) {
+    indexes[node] = index;
+    lowlinks[node] = index;
+    index += 1;
+    stack.push(node);
+    onStack[node] = true;
+
+    for (const neighbor of graph[node]) {
+      if (typeof indexes[neighbor] === 'undefined') {
+        dfs(neighbor);
+        lowlinks[node] = Math.min(lowlinks[node], lowlinks[neighbor]);
+      } else if (onStack[neighbor]) {
+        lowlinks[node] = Math.min(lowlinks[node], indexes[neighbor]);
+      }
+    }
+
+    if (lowlinks[node] === indexes[node]) {
+      const component = [];
+      let w;
+      do {
+        w = stack.pop();
+        onStack[w] = false;
+        component.push(w);
+      } while (w !== node);
+      components.push(component);
     }
   }
-  return true;
+
+  for (const node in graph) {
+    if (typeof indexes[node] === 'undefined') {
+      dfs(node);
+    }
+  }
+
+  return components;
 }
+const graph = {
+  0: [1, 2],
+  1: [2],
+  2: [0, 3],
+  3: [3, 4],
+};
 
-// Example usage:
-const myArray = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(myArray));  // true
-
-const anotherArray = [5, 3, 2, 1, 4];
-console.log(isSortedAscending(anotherArray));  // false
+const components = tarjansSCC(graph);
+console.log(components);
