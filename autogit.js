ccdcde3e505c9f4912b994747c7129b635343a6e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:06:31 +0800

    1-24-2024 6:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,14 @@
-function searchPattern(text, pattern) {
-  const n = text.length;
-  const m = pattern.length;
-
-  // Preprocess the pattern
-  const lps = new Array(m).fill(0);
-  let len = 0;
-  let i = 1;
-
-  while (i < m) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
-    }
+const uniqueArray = Array.from(new Set(myArray));
+const uniqueArray = myArray.filter((item, index) => myArray.indexOf(item) === index);
+const uniqueArray = myArray.reduce((accumulator, currentValue) => {
+  if (!accumulator.includes(currentValue)) {
+    accumulator.push(currentValue);
   }
-
-  // Search for the pattern
-  let j = 0;
-  let i = 0;
-
-  while (i < n) {
-    if (pattern[j] === text[i]) {
-      j++;
-      i++;
-
-      if (j === m) {
-        console.log("Pattern found at index", i - j);
-        j = lps[j - 1];
-      }
-    } else if (j !== 0) {
-      j = lps[j - 1];
-    } else {
-      i++;
-    }
+  return accumulator;
+}, []);
+const uniqueArray = [];
+myArray.forEach((item) => {
+  if (uniqueArray.indexOf(item) === -1) {
+    uniqueArray.push(item);
   }
-}
+});
