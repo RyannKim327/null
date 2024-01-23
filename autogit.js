15269f4e96cab215f81859bc12ff327507d752d0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:56:13 +0800

    1-24-2024 5:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,14 @@
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    // Swap values
-    let temp = arr[minIndex];
-    arr[minIndex] = arr[i];
-    arr[i] = temp;
+function getStringLength(str) {
+  var length = 0;
+  
+  while (str[length] !== undefined) {
+    length++;
   }
-  return arr;
+  
+  return length;
 }
 
-// Testing the selectionSort function
-const arr = [64, 25, 12, 22, 11];
-console.log(selectionSort(arr)); // Output: [11, 12, 22, 25, 64]
+// Example usage:
+var myString = "Hello, world!";
+var length = getStringLength(myString);
+console.log(length); // Outputs: 13
