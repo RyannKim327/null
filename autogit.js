ca23c3d9422d3d2aa12bfa451c7126e46ceb2782
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:52:01 +0800

    1-24-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,17 @@
-let numbers = [9, 2, 5, 1, 7];
-numbers.sort(function(a, b) {
-  return a - b;
-});
-console.log(numbers);
+function interpolationSearch(arr, target, low, high) {
+  if (low <= high && target >= arr[low] && target <= arr[high]) {
+    let pos = low + Math.floor((target - arr[low]) * (high - low) / (arr[high] - arr[low]));
+
+    if (arr[pos] === target) {
+      return pos;
+    }
+
+    if (arr[pos] > target) {
+      return interpolationSearch(arr, target, low, pos - 1);
+    }
+
+    return interpolationSearch(arr, target, pos + 1, high);
+  }
+
+  return -1;
+}
