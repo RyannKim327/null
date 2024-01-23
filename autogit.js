d5cc4d139a1f1bfd8897a98f059ca83a0ee3ea02
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:57:56 +0800

    1-24-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,37 @@
-function findFirstRepeatedChar(inputString) {
-  var charCount = {};
+function longestCommonSubstring(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
 
-  // Traverse each character in the input string
-  for (var i = 0; i < inputString.length; i++) {
-    var char = inputString[i];
-
-    // Check if the character already exists in the map
-    if (charCount[char]) {
-      // If it does, return the first repeated character
-      return char;
-    }
-
-    // If the character doesn't exist, add it to the map
-    charCount[char] = true;
+  // Create a table to store lengths of longest common suffixes
+  const table = [];
+  for (let i = 0; i <= m; i++) {
+    table.push(new Array(n + 1).fill(0));
   }
 
-  // If no repeated character found
-  return null;
+  let maxLength = 0; // length of the longest common substring
+  let endIndex = 0; // end index of the longest common substring
+
+  // Fill the table and track the length and end index of longest common substring
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        table[i][j] = table[i - 1][j - 1] + 1;
+        if (table[i][j] > maxLength) {
+          maxLength = table[i][j];
+          endIndex = i - 1;
+        }
+      } else {
+        table[i][j] = 0;
+      }
+    }
+  }
+
+  // Extract the longest common substring using the end index and length
+  return str1.substring(endIndex - maxLength + 1, endIndex + 1);
 }
 
 // Example usage:
-var string = "Hello World";
-var repeatedChar = findFirstRepeatedChar(string);
-console.log("First repeated character: " + repeatedChar);
+const str1 = "ABCDXYZ";
+const str2 = "XYZABCD";
+const longestSubstring = longestCommonSubstring(str1, str2);
+console.log(longestSubstring); // Output: "ABCD"
