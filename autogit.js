c6229dc9832f255e09b2dda16648cca1136ddb68
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:14:27 +0800

    1-24-2024 6:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,5 @@
-// Function to perform a depth-first search
-function dfs(graph, node, visited, stack) {
-  visited.add(node);
+const numbers = [5, 10, 2, 15, 8];
 
-  // Visit all neighbors of the current node
-  for (let neighbor of graph[node]) {
-    if (!visited.has(neighbor)) {
-      dfs(graph, neighbor, visited, stack);
-    }
-  }
+const max = Math.max(...numbers);
 
-  // Push the current node to the stack
-  stack.push(node);
-}
-
-// Function to perform topological sort
-function topologicalSort(graph) {
-  const visited = new Set();
-  const stack = [];
-
-  // Visit all nodes in the graph
-  for (let node in graph) {
-    if (!visited.has(node)) {
-      dfs(graph, node, visited, stack);
-    }
-  }
-
-  // Reverse the order in the stack to get the sorted result
-  return stack.reverse();
-}
-
-// Example usage
-const graph = {
-  A: ['B', 'C'],
-  B: ['D'],
-  C: ['D', 'E'],
-  D: [],
-  E: ['F'],
-  F: []
-};
-
-const sorted = topologicalSort(graph);
-console.log(sorted); // Output: ["A", "C", "E", "F", "B", "D"]
+console.log(max); // Output: 15
