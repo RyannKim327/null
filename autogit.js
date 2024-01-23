b7e8e5fada2caca163b0545947d4d9d17bf0c24b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:01:23 +0800

    1-24-2024 6:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,71 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+class SkipListNode {
+  constructor(value, level) {
+    this.value = value;
+    this.level = level;
+    this.next = new Array(level + 1); // Array of next pointers
   }
+}
+class SkipList {
+  constructor() {
+    this.head = new SkipListNode(-Infinity, 0); // Dummy head node
+    this.tail = new SkipListNode(Infinity, 0); // Dummy tail node
+    this.level = 0; // Current maximum level of the skip list
+  }
+}
+randomLevel() {
+  let level = 0;
+  while (Math.random() < 0.5 && level < this.level + 1) {
+    level++;
+  }
+  return level;
+}
+insert(value) {
+  const newNode = new SkipListNode(value, this.randomLevel());
+  let currentNode = this.head;
 
-  const pivot = arr[arr.length - 1];
-  const leftArr = [];
-  const rightArr = [];
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      leftArr.push(arr[i]);
-    } else {
-      rightArr.push(arr[i]);
+  // Find the appropriate positions to insert the new node
+  for (let i = this.level; i >= 0; i--) {
+    while (currentNode.next[i] && currentNode.next[i].value < value) {
+      currentNode = currentNode.next[i];
+    }
+    if (i <= newNode.level) {
+      newNode.next[i] = currentNode.next[i];
+      currentNode.next[i] = newNode;
     }
   }
 
-  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+  // Update the maximum level of the skip list
+  if (newNode.level > this.level) {
+    this.level = newNode.level;
+  }
 }
+search(value) {
+  let currentNode = this.head;
 
-// Example usage
-const array = [5, 2, 9, 1, 3, 8];
-const sortedArray = quickSort(array);
+  // Traverse the skip list to find the node
+  for (let i = this.level; i >= 0; i--) {
+    while (currentNode.next[i] && currentNode.next[i].value < value) {
+      currentNode = currentNode.next[i];
+    }
+  }
 
-console.log(sortedArray);
+  // Check if the node's value matches the given value
+  if (currentNode.next[0] && currentNode.next[0].value === value) {
+    return currentNode.next[0];
+  } else {
+    return null;
+  }
+}
+remove(value) {
+  let currentNode = this.head;
+
+  // Find the node to be removed
+  for (let i = this.level; i >= 0; i--) {
+    while (currentNode.next[i] && currentNode.next[i].value < value) {
+      currentNode = currentNode.next[i];
+    }
+    if (currentNode.next[i] && currentNode.next[i].value === value) {
+      currentNode.next[i] = currentNode.next[i].next[i];
+    }
+  }
+}
