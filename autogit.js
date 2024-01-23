20286e718a3daa9f9c3432a0a7d42aebbcc27513
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:49:46 +0800

    1-24-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,50 @@
-function findMajorityElement(array) {
-  const hashmap = {};
-
-  // Count the frequency of each element
-  for (const element of array) {
-    if (hashmap[element]) {
-      hashmap[element]++;
-    } else {
-      hashmap[element] = 1;
-    }
-  }
-
-  // Find the element with the maximum frequency
-  let majorityElement = null;
-  let maxFrequency = 0;
-  for (const element in hashmap) {
-    if (hashmap[element] > maxFrequency) {
-      majorityElement = element;
-      maxFrequency = hashmap[element];
-    }
-  }
-
-  // Check if the majority element exists
-  if (maxFrequency > array.length / 2) {
-    return majorityElement;
-  } else {
-    return "No majority element found";
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
   }
 }
 
-// Example usage
-const array = [2, 2, 3, 4, 2, 2, 5];
-const majorityElement = findMajorityElement(array);
-console.log(majorityElement); // Output: 2
+function isPalindrome(head) {
+  if (!head || !head.next) {
+    return true;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  // Find the middle of the list
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Reverse second half of the list
+  slow.next = reverseList(slow.next);
+  slow = slow.next;
+
+  // Compare values of nodes
+  while (slow) {
+    if (head.val !== slow.val) {
+      return false;
+    }
+    head = head.next;
+    slow = slow.next;
+  }
+
+  return true;
+}
+
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+
+  while (curr) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev;
+}
