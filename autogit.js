6aac0fe407a2c0ff556246ed596961ba1385cc8f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:19:02 +0800

    1-24-2024 6:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,19 @@
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-function createLinkedList(values) {
-  let head = null;
-  let tail = null;
-
-  for (let val of values) {
-    const newNode = new ListNode(val);
-    if (!head) {
-      head = tail = newNode;
-    } else {
-      tail.next = newNode;
-      tail = newNode;
-    }
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
   }
 
-  return head;
-}
-function reverseLinkedList(head) {
-  let prev = null;
-  let curr = head;
-
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-
-  return prev;
-}
-function isLinkedListPalindrome(head) {
-  let fast = head;
-  let slow = head;
-
-  while (fast && fast.next) {
-    fast = fast.next.next;
-    slow = slow.next;
-  }
-
-  if (fast) {
-    slow = slow.next; // Ignoring middle element for odd-length lists
-  }
-
-  let reversed = reverseLinkedList(slow);
-
-  while (reversed) {
-    if (reversed.val !== head.val) {
+  // Check for divisibility from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
       return false;
     }
-    reversed = reversed.next;
-    head = head.next;
   }
 
+  // If the number is not divisible by any smaller number, it is prime
   return true;
 }
-const values = [1, 2, 3, 2, 1];
-const linkedList = createLinkedList(values);
 
-console.log(isLinkedListPalindrome(linkedList)); // Output: true
+// Example usage
+console.log(isPrime(17)); // Output: true
+console.log(isPrime(20)); // Output: false
