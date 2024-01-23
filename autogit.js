01e1f13a8ef97edd61b89650ec14e5be8515531c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:07:15 +0800

    1-24-2024 6:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,4 @@
-function findMajorityElement(arr) {
-  const map = {};
-  
-  for (let i = 0; i < arr.length; i++) {
-    const num = arr[i];
-    map[num] = (map[num] || 0) + 1;
-    
-    if (map[num] > arr.length / 2) {
-      return num;
-    }
-  }
-  
-  return null; // No majority element found
-}
+let array = [1, 2, 3, 4, 5];
+array.reverse();
 
-// Example usage:
-const arr = [2, 5, 2, 2, 3, 2, 4, 2, 1];
-const majorityElement = findMajorityElement(arr);
-console.log(majorityElement); // Output: 2
+console.log(array); // Output: [5, 4, 3, 2, 1]
