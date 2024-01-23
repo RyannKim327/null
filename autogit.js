c7388950b9a68c5c86ed6051482ad65ea9e71f0b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-23 22:06:16 +0800

    1-23-2024 22:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,16 @@
-class Graph {
-  constructor(numVertices) {
-    this.numVertices = numVertices;
-    this.adjList = new Array(numVertices).fill().map(() => []);
-  }
+const cron = require('cron');
 
-  addEdge(source, destination) {
-    this.adjList[source].push(destination);
-  }
+// Function to be executed
+function task() {
+  console.log('Executing task...');
+  // Add your task logic here
 }
-let index = 0;
-const stack = [];
-const components = [];
-function tarjanSCC(graph, callback) {
-  const { numVertices, adjList } = graph;
 
-  // Helper function to traverse the graph
-  function traverse(vertex) {
-    vertex.index = index;
-    vertex.lowLink = index++;
-    vertex.visited = true;
-    stack.push(vertex);
+// Schedule the task to run every minute
+const cronJob = new cron.CronJob('* * * * *', task);
 
-    for (const neighbor of adjList[vertex]) {
-      if (!neighbor.visited) {
-        traverse(neighbor);
-        vertex.lowLink = Math.min(vertex.lowLink, neighbor.lowLink);
-      } else if (stack.includes(neighbor)) {
-        vertex.lowLink = Math.min(vertex.lowLink, neighbor.index);
-      }
-    }
+// Start the cron job
+cronJob.start();
 
-    if (vertex.lowLink === vertex.index) {
-      const component = [];
-      let v;
-      do {
-        v = stack.pop();
-        component.push(v);
-      } while (v !== vertex);
-      callback(component);
-    }
-  }
-
-  // Call Tarjan's algorithm for each unvisited vertex
-  for (let i = 0; i < numVertices; i++) {
-    if (!adjList[i].visited) {
-      traverse(adjList[i]);
-    }
-  }
-}
-const graph = new Graph(6);
-graph.addEdge(0, 1);
-graph.addEdge(1, 2);
-graph.addEdge(2, 0);
-graph.addEdge(3, 4);
-graph.addEdge(4, 5);
-graph.addEdge(5, 3);
-
-tarjanSCC(graph, (component) => {
-  console.log(component.map((v) => v.index));
-});
+// Stop the cron job after 5 minutes
+setTimeout(() => cronJob.stop(), 5 * 60 * 1000);
