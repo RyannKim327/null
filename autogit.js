c43210ae5c47035f55c96737a15c73b390782262
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:48:29 +0800

    1-24-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,114 @@
-function areAnagrams(str1, str2) {
-  // Convert both strings to lowercase
-  str1 = str1.toLowerCase();
-  str2 = str2.toLowerCase();
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
 
-  // Remove non-alphabetic characters
-  str1 = str1.replace(/[^a-z]/g, "");
-  str2 = str2.replace(/[^a-z]/g, "");
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
 
-  // Sort the strings
-  str1 = str1.split("").sort().join("");
-  str2 = str2.split("").sort().join("");
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
 
-  // Compare the sorted strings
-  return str1 === str2;
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  hasParent(index) {
+    return this.getParentIndex(index) >= 0;
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heap.length;
+  }
+
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heap.length;
+  }
+
+  parent(index) {
+    return this.heap[this.getParentIndex(index)];
+  }
+
+  leftChild(index) {
+    return this.heap[this.getLeftChildIndex(index)];
+  }
+
+  rightChild(index) {
+    return this.heap[this.getRightChildIndex(index)];
+  }
+
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.hasParent(index) && this.parent(index).priority > this.heap[index].priority) {
+      const parentIndex = this.getParentIndex(index);
+      this.swap(parentIndex, index);
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (this.hasRightChild(index) && this.rightChild(index).priority < this.leftChild(index).priority) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index].priority < this.heap[smallerChildIndex].priority) {
+        break;
+      }
+
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.heap[0].item;
+  }
+
+  enqueue(item, priority) {
+    const node = { item, priority };
+    this.heap.push(node);
+    this.heapifyUp();
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item = this.heap[0].item;
+    this.swap(0, this.heap.length - 1);
+    this.heap.pop();
+    this.heapifyDown();
+    return item;
+  }
 }
 
 // Example usage
-const string1 = "listen";
-const string2 = "silent";
-console.log(areAnagrams(string1, string2));  // Output: true
+const pq = new PriorityQueue();
+pq.enqueue("Task 1", 2);
+pq.enqueue("Task 2", 1);
+pq.enqueue("Task 3", 3);
 
-const string3 = "night";
-const string4 = "thing";
-console.log(areAnagrams(string3, string4));  // Output: false
+console.log(pq.dequeue()); // Output: Task 2
+console.log(pq.dequeue()); // Output: Task 1
+console.log(pq.dequeue()); // Output: Task 3
+console.log(pq.dequeue()); // Output: null (queue is empty)
