41b9aed8f36c294420a930ee407f8045a8a0e0c0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:59:31 +0800

    1-24-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,17 @@
-let str = "Hello World!";
-let lowercaseStr = str.toLowerCase();
+function largestPrimeFactor(number) {
+  let largestFactor = 1;
 
-console.log(lowercaseStr); // Output: "hello world!"
+  for (let factor = 2; factor <= Math.sqrt(number); factor++) {
+    while (number % factor === 0) {
+      largestFactor = Math.max(largestFactor, factor);
+      number /= factor;
+    }
+  }
+
+  if (number > 1) {
+    largestFactor = Math.max(largestFactor, number);
+  }
+
+  return largestFactor;
+}
+console.log(largestPrimeFactor(123456789));  // Output: 3803
