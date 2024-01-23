5634b31a6eff03773aae9a75146d6476eefa672a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-23 22:04:53 +0800

    1-23-2024 22:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,8 @@
-class ListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-function findNthFromEnd(head, n) {
-  let first = head;
-  let second = head;
-
-  // Move the first pointer to the nth node from the beginning
-  for (let i = 0; i < n && first !== null; i++) {
-    first = first.next;
-  }
-
-  // If first pointer becomes null before reaching n, then the length of the list is less than n
-  if (first === null) {
-    return null;
-  }
-
-  // Move both pointers until first reaches the end of the list
-  while (first.next !== null) {
-    first = first.next;
-    second = second.next;
-  }
-
-  // At this point, the second pointer will be pointing to the nth node from the end
-  return second;
-}
-// Create a linked list
-let head = new ListNode(1);
-let second = new ListNode(2);
-let third = new ListNode(3);
-let fourth = new ListNode(4);
-let fifth = new ListNode(5);
-
-head.next = second;
-second.next = third;
-third.next = fourth;
-fourth.next = fifth;
-
-let n = 2;
-let nthNodeFromEnd = findNthFromEnd(head, n);
-
-console.log(`The ${n}th node from end is: ${nthNodeFromEnd.value}`);
+var string1 = "Hello";
+var string2 = "World";
+var result = string1 + " " + string2;
+console.log(result); // Output: "Hello World"
+var string1 = "Hello";
+var string2 = "World";
+var result = string1.concat(" ", string2);
+console.log(result); // Output: "Hello World"
