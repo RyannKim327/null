c65b613758dcee2551a70898ab2ff014af282f78
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:59:20 +0800

    1-24-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,4 @@
-function calculateMean(numbers) {
-    var sum = 0;
-    var count = numbers.length;
-    
-    for (var i = 0; i < count; i++) {
-        sum += numbers[i];
-    }
-    
-    var mean = sum / count;
-    return mean;
-}
+let str = "Hello World!";
+let lowercaseStr = str.toLowerCase();
 
-// Example usage:
-var numberList = [1, 3, 5, 7, 9];
-var meanValue = calculateMean(numberList);
-console.log(meanValue); // Output: 5
+console.log(lowercaseStr); // Output: "hello world!"
