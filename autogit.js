e04f7546dc00b212c97a0a3ffe5087a55c58ee78
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:50:21 +0800

    1-24-2024 5:49 [User mode]

--- autogit.js
+++
@@ -1,7 +0,0 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
-let arr = [1, 2, 3, 4, 5];
-let reversedArr = [...arr].reverse();
-console.log(reversedArr); // Output: [5, 4, 3, 2, 1]
-console.log(arr); // Output: [1, 2, 3, 4, 5]
