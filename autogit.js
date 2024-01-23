5b5767d68f6111b368bffa86365c874c19a8acb4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:48:40 +0800

    1-24-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,114 +1,21 @@
-class PriorityQueue {
-  constructor() {
-    this.heap = [];
-  }
+function largestPrimeFactor(number) {
+  let factor = 2;
+  let largestPrime = 1;
 
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2);
-  }
-
-  getLeftChildIndex(index) {
-    return 2 * index + 1;
-  }
-
-  getRightChildIndex(index) {
-    return 2 * index + 2;
-  }
-
-  hasParent(index) {
-    return this.getParentIndex(index) >= 0;
-  }
-
-  hasLeftChild(index) {
-    return this.getLeftChildIndex(index) < this.heap.length;
-  }
-
-  hasRightChild(index) {
-    return this.getRightChildIndex(index) < this.heap.length;
-  }
-
-  parent(index) {
-    return this.heap[this.getParentIndex(index)];
-  }
-
-  leftChild(index) {
-    return this.heap[this.getLeftChildIndex(index)];
-  }
-
-  rightChild(index) {
-    return this.heap[this.getRightChildIndex(index)];
-  }
-
-  swap(index1, index2) {
-    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-  }
-
-  heapifyUp() {
-    let index = this.heap.length - 1;
-    while (this.hasParent(index) && this.parent(index).priority > this.heap[index].priority) {
-      const parentIndex = this.getParentIndex(index);
-      this.swap(parentIndex, index);
-      index = parentIndex;
+  while (number > 1) {
+    if (number % factor === 0) {
+      largestPrime = factor;
+      number /= factor;
+    } else {
+      factor++;
     }
   }
 
-  heapifyDown() {
-    let index = 0;
-    while (this.hasLeftChild(index)) {
-      let smallerChildIndex = this.getLeftChildIndex(index);
-      if (this.hasRightChild(index) && this.rightChild(index).priority < this.leftChild(index).priority) {
-        smallerChildIndex = this.getRightChildIndex(index);
-      }
-
-      if (this.heap[index].priority < this.heap[smallerChildIndex].priority) {
-        break;
-      }
-
-      this.swap(index, smallerChildIndex);
-      index = smallerChildIndex;
-    }
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    return this.heap[0].item;
-  }
-
-  enqueue(item, priority) {
-    const node = { item, priority };
-    this.heap.push(node);
-    this.heapifyUp();
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    const item = this.heap[0].item;
-    this.swap(0, this.heap.length - 1);
-    this.heap.pop();
-    this.heapifyDown();
-    return item;
-  }
+  return largestPrime;
 }
 
-// Example usage
-const pq = new PriorityQueue();
-pq.enqueue("Task 1", 2);
-pq.enqueue("Task 2", 1);
-pq.enqueue("Task 3", 3);
-
-console.log(pq.dequeue()); // Output: Task 2
-console.log(pq.dequeue()); // Output: Task 1
-console.log(pq.dequeue()); // Output: Task 3
-console.log(pq.dequeue()); // Output: null (queue is empty)
+// Test the function
+const number = 13195;
+const largestFactor = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestFactor}.`);
+The largest prime factor of 13195 is 29.
