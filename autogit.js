cec4367913c18f6e1793c219405e2c7a9e1f992a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:49:56 +0800

    1-24-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,7 @@
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
-
-function isPalindrome(head) {
-  if (!head || !head.next) {
-    return true;
-  }
-
-  let slow = head;
-  let fast = head;
-
-  // Find the middle of the list
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  // Reverse second half of the list
-  slow.next = reverseList(slow.next);
-  slow = slow.next;
-
-  // Compare values of nodes
-  while (slow) {
-    if (head.val !== slow.val) {
-      return false;
-    }
-    head = head.next;
-    slow = slow.next;
-  }
-
-  return true;
-}
-
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-
-  while (curr) {
-    let next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-
-  return prev;
-}
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // Output: [5, 4, 3, 2, 1]
+let arr = [1, 2, 3, 4, 5];
+let reversedArr = [...arr].reverse();
+console.log(reversedArr); // Output: [5, 4, 3, 2, 1]
+console.log(arr); // Output: [1, 2, 3, 4, 5]
