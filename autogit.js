3f237284048438b3801817128aae6c763bc66023
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 05:51:47 +0800

    1-24-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,5 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
-  const sanitizedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-  // Reverse the string
-  const reversedStr = sanitizedStr.split('').reverse().join('');
-
-  // Compare the reversed string with the original string
-  return sanitizedStr === reversedStr;
-}
-const string1 = "A man, a plan, a canal: Panama";
-console.log(isPalindrome(string1)); // Output: true
-
-const string2 = "race car";
-console.log(isPalindrome(string2)); // Output: true
-
-const string3 = "Hello, world!";
-console.log(isPalindrome(string3)); // Output: false
+let numbers = [9, 2, 5, 1, 7];
+numbers.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers);
