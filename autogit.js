aac265fae53c97dfad899f7749299c21c035e29b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 06:16:05 +0800

    1-24-2024 6:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,3 @@
-function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
-
-  while (left < right) {
-    if (str[left] !== str[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-
-  return true;
-}
-
-// Example Usage:
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello")); // false
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+let str = "Hello World!";
+let lowerCaseStr = str.toLowerCase();
+console.log(lowerCaseStr); // Output: "hello world!"
