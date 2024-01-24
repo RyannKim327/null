708bc50a3844b2dd770b275b875d0a7badee62af
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:18:44 +0800

    1-25-2024 5:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,66 +1,177 @@
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
   }
 }
-
-class LinkedList {
+class PriorityQueue {
   constructor() {
-    this.head = null;
+    this.items = [];
   }
+}
+getLeftChildIndex(index) {
+  return 2 * index + 1;
+}
 
-  add(data) {
-    const newNode = new Node(data);
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current.next = newNode;
+getRightChildIndex(index) {
+  return 2 * index + 2;
+}
+getParentIndex(index) {
+  return Math.floor((index - 1) / 2);
+}
+enqueue(value, priority) {
+  const node = new Node(value, priority);
+  this.items.push(node);
+  this.heapifyUp();
+}
+heapifyUp() {
+  let index = this.items.length - 1;
+  while (index > 0) {
+    const parentIndex = this.getParentIndex(index);
+    if (this.items[index].priority <= this.items[parentIndex].priority) {
+      break;
     }
+    this.swap(index, parentIndex);
+    index = parentIndex;
   }
-
-  reverse() {
-    let prev = null;
-    let current = this.head;
-
-    while (current !== null) {
-      const nextNode = current.next;
-      current.next = prev;
-      prev = current;
-      current = nextNode;
-    }
-
-    this.head = prev;
+}
+swap(index1, index2) {
+  [this.items[index1], this.items[index2]] = [this.items[index2], this.items[index1]];
+}
+dequeue() {
+  if (this.isEmpty()) {
+    return null;
   }
-
-  print() {
-    let current = this.head;
-    const elements = [];
-    while (current !== null) {
-      elements.push(current.data);
-      current = current.next;
+  const root = this.items[0];
+  const lastNode = this.items.pop();
+  if (!this.isEmpty()) {
+    this.items[0] = lastNode;
+    this.heapifyDown();
+  }
+  return root.value;
+}
+heapifyDown() {
+  let index = 0;
+  while (this.getLeftChildIndex(index) < this.items.length) {
+    const leftChildIndex = this.getLeftChildIndex(index);
+    const rightChildIndex = this.getRightChildIndex(index);
+    const biggerChildIndex = this.getHigherPriorityChildIndex(
+      leftChildIndex,
+      rightChildIndex
+    );
+    if (this.items[index].priority >= this.items[biggerChildIndex].priority) {
+      break;
     }
-    console.log(elements.join(' -> '));
+    this.swap(index, biggerChildIndex);
+    index = biggerChildIndex;
+  }
+}
+getHigherPriorityChildIndex(leftChildIndex, rightChildIndex) {
+  if (
+    rightChildIndex >= this.items.length ||
+    this.items[leftChildIndex].priority >= this.items[rightChildIndex].priority
+  ) {
+    return leftChildIndex;
+  } else {
+    return rightChildIndex;
+  }
+}
+isEmpty() {
+  return this.items.length === 0;
+}
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
   }
 }
 
-// Example usage:
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
 
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-linkedList.add(5);
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
 
-console.log('Original Linked List:');
-linkedList.print();
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
 
-linkedList.reverse();
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
 
-console.log('Reversed Linked List:');
-linkedList.print();
+  enqueue(value, priority) {
+    const node = new Node(value, priority);
+    this.items.push(node);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.items.length - 1;
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+      if (this.items[index].priority <= this.items[parentIndex].priority) {
+        break;
+      }
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+
+  swap(index1, index2) {
+    [this.items[index1], this.items[index2]] = [
+      this.items[index2],
+      this.items[index1],
+    ];
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const root = this.items[0];
+    const lastNode = this.items.pop();
+    if (!this.isEmpty()) {
+      this.items[0] = lastNode;
+      this.heapifyDown();
+    }
+    return root.value;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.items.length) {
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+      const biggerChildIndex = this.getHigherPriorityChildIndex(
+        leftChildIndex,
+        rightChildIndex
+      );
+      if (
+        this.items[index].priority >= this.items[biggerChildIndex].priority
+      ) {
+        break;
+      }
+      this.swap(index, biggerChildIndex);
+      index = biggerChildIndex;
+    }
+  }
+
+  getHigherPriorityChildIndex(leftChildIndex, rightChildIndex) {
+    if (
+      rightChildIndex >= this.items.length ||
+      this.items[leftChildIndex].priority >= this.items[rightChildIndex].priority
+    ) {
+      return leftChildIndex;
+    } else {
+      return rightChildIndex;
+    }
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+}
