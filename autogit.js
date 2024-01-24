866f73793f0fa4b6e0098f3cd3a4b6e113995da5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:20:12 +0800

    1-25-2024 5:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,9 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  var isSwapped;
+let array = [1, 2, 3, 4, 5];
+array.reverse();
 
-  for (var i = 0; i < len - 1; i++) {
-    isSwapped = false;
+console.log(array); //[5, 4, 3, 2, 1]
+let array = [1, 2, 3, 4, 5];
+let reversedArray = array.slice().reverse();
 
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        isSwapped = true;
-      }
-    }
-
-    // If no swapping occurred in the inner loop, the array is already sorted
-    if (!isSwapped) {
-      break;
-    }
-  }
-
-  return arr;
-}
-
-// Usage example:
-var numbers = [5, 3, 8, 2, 1, 4];
-console.log("Before sorting:", numbers);
-
-var sortedNumbers = bubbleSort(numbers);
-console.log("After sorting:", sortedNumbers);
+console.log(array); //[1, 2, 3, 4, 5]
+console.log(reversedArray); //[5, 4, 3, 2, 1]
