e3eda7dae75cf542d4766171274c327c198d504e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:03:08 +0800

    1-25-2024 6:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,67 +1,18 @@
-function bidirectionalSearch(start, goal) {
-  const visitedStart = new Set();
-  const visitedGoal = new Set();
-  const queueStart = [];
-  const queueGoal = [];
+function findFirstRepeatedCharacter(str) {
+  const charSet = new Set(); // or create an empty object using {}
 
-  visitedStart.add(start);
-  queueStart.push([start]);
-  visitedGoal.add(goal);
-  queueGoal.push([goal]);
-
-  while (queueStart.length > 0 && queueGoal.length > 0) {
-    const pathStart = queueStart.shift();
-    const currentStart = pathStart[pathStart.length - 1];
-
-    if (currentStart === goal) {
-      return pathStart;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charSet.has(char)) {
+      return char;
     }
-
-    const neighborsStart = getNeighbors(currentStart);
-    for (const neighbor of neighborsStart) {
-      if (!visitedStart.has(neighbor)) {
-        queueStart.push([...pathStart, neighbor]);
-        visitedStart.add(neighbor);
-      }
-    }
-
-    const pathGoal = queueGoal.shift();
-    const currentGoal = pathGoal[pathGoal.length - 1];
-
-    if (currentGoal === start) {
-      return pathGoal.reverse();
-    }
-
-    const neighborsGoal = getNeighbors(currentGoal);
-    for (const neighbor of neighborsGoal) {
-      if (!visitedGoal.has(neighbor)) {
-        queueGoal.push([...pathGoal, neighbor]);
-        visitedGoal.add(neighbor);
-      }
-    }
-
-    const intersect = [...visitedStart].find(node => visitedGoal.has(node));
-    if (intersect) {
-      const pathIntersection = [...pathStart, intersect, ...pathGoal.reverse()];
-      return pathIntersection;
-    }
+    charSet.add(char);
   }
 
-  return "No path found";
+  return null; // or any other value you prefer
 }
 
-// Example usage
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['A', 'D', 'E'],
-  'C': ['A', 'F'],
-  'D': ['B'],
-  'E': ['B', 'F'],
-  'F': ['C', 'E'],
-};
-
-function getNeighbors(node) {
-  return graph[node] || [];
-}
-
-console.log(bidirectionalSearch('A', 'F'));
+// Example usage:
+const string = "hello world";
+const repeatedChar = findFirstRepeatedCharacter(string);
+console.log("First repeated character:", repeatedChar);
