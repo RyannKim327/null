351400859519a048ac8b18e3aafece642294037c
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:04:35 +0800

    1-25-2024 6:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,12 @@
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-
-  while (current) {
-    const next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
+function findStringLength(string) {
+  let count = 0;
+  while (string[count] !== undefined) {
+    count++;
   }
-
-  return prev;
-}
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-const head = new Node(1);
-const node2 = new Node(2);
-head.next = node2;
-const node3 = new Node(3);
-node2.next = node3;
-const tail = new Node(4);
-node3.next = tail;
-console.log('Original Linked List:')
-let current = head;
-while (current) {
-  console.log(current.value);
-  current = current.next;
+  return count;
 }
 
-const reversedHead = reverseLinkedList(head);
-
-console.log('Reversed Linked List:')
-current = reversedHead;
-while (current) {
-  console.log(current.value);
-  current = current.next;
-}
-Original Linked List:
-1
-2
-3
-4
-Reversed Linked List:
-4
-3
-2
-1
+// Example usage:
+const text = "Hello, World!";
+const length = findStringLength(text);
+console.log(length); // Output: 13
