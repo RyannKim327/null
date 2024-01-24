6ce2d66964e5ffaac1c97b8d5ad454e1d23cc6e5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:33:02 +0800

    1-24-2024 20:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,79 @@
-function longestCommonPrefix(strings) {
-  if (strings.length === 0) {
-    return "";
+class Graph {
+  constructor() {
+    this.nodes = {};
   }
 
-  let prefix = "";
+  addNode(name) {
+    this.nodes[name] = [];
+  }
 
-  for (let i = 0; i < strings[0].length; i++) {
-    const char = strings[0][i];
+  addEdge(startNode, endNode) {
+    this.nodes[startNode].push(endNode);
+    this.nodes[endNode].push(startNode);
+  }
+}
+function bidirectionalSearch(startNode, endNode, graph) {
+  // Create two queues, one for forward search and one for reverse search
+  const forwardQueue = [startNode];
+  const reverseQueue = [endNode];
 
-    for (let j = 1; j < strings.length; j++) {
-      if (strings[j][i] !== char) {
-        return prefix;
-      }
+  // Create two sets to keep track of visited nodes
+  const forwardVisited = new Set();
+  const reverseVisited = new Set();
+
+  // Start the search
+  while (forwardQueue.length && reverseQueue.length) {
+    // Perform forward search
+    const forwardCurrentNode = forwardQueue.shift();
+    forwardVisited.add(forwardCurrentNode);
+
+    // Check if we reach the end node
+    if (reverseVisited.has(forwardCurrentNode)) {
+      return true; // Path found
     }
 
-    prefix += char;
+    // Add unvisited neighbors to the forward queue
+    graph.nodes[forwardCurrentNode].forEach((neighbor) => {
+      if (!forwardVisited.has(neighbor)) {
+        forwardQueue.push(neighbor);
+      }
+    });
+
+    // Perform reverse search
+    const reverseCurrentNode = reverseQueue.shift();
+    reverseVisited.add(reverseCurrentNode);
+
+    // Check if we reach the start node
+    if (forwardVisited.has(reverseCurrentNode)) {
+      return true; // Path found
+    }
+
+    // Add unvisited neighbors to the reverse queue
+    graph.nodes[reverseCurrentNode].forEach((neighbor) => {
+      if (!reverseVisited.has(neighbor)) {
+        reverseQueue.push(neighbor);
+      }
+    });
   }
 
-  return prefix;
+  return false; // Path not found
 }
-const strings = ["flower", "flow", "flight"];
-const commonPrefix = longestCommonPrefix(strings);
+const graph = new Graph();
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addNode('D');
+graph.addNode('E');
+graph.addNode('F');
 
-console.log(commonPrefix); // Output: "fl"
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'D');
+graph.addEdge('D', 'E');
+graph.addEdge('E', 'F');
+
+const startNode = 'A';
+const endNode = 'F';
+
+const pathExists = bidirectionalSearch(startNode, endNode, graph);
+console.log(pathExists); // true
