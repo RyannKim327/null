c9520069794bb06080f889d142e8ad67564c5f15
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:27:43 +0800

    1-25-2024 5:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,36 @@
-var string1 = "Hello";
-var string2 = "world";
+class Node {
+  constructor() {
+    this.children = {};
+    // additional properties if needed
+  }
+}
+class SuffixTree {
+  constructor() {
+    this.root = new Node();
+    // additional properties if needed
+  }
 
-var result = string1 + " " + string2; // "Hello world"
-var string1 = "Hello";
-var string2 = "world";
+  // additional methods if needed
+}
+SuffixTree.prototype.addSuffix = function(suffix, index) {
+  let node = this.root;
+  for (let i = 0; i < suffix.length; i++) {
+    const char = suffix[i];
+    if (!node.children[char]) {
+      node.children[char] = new Node();
+    }
+    node = node.children[char];
+  }
+  
+  // Store additional information if needed (e.g., suffix index)
+};
 
-var result = string1.concat(" ", string2); // "Hello world"
+SuffixTree.prototype.build = function(str) {
+  for (let i = 0; i < str.length; i++) {
+    this.addSuffix(str.slice(i), i);
+  }
+};
+const suffixTree = new SuffixTree();
+const inputString = "banana";
+suffixTree.build(inputString);
+console.log(suffixTree); // Print the constructed tree for testing
