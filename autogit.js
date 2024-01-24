8e522ffe946251be8376fe9d97b98a0eac71a7df
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:33:17 +0800

    1-24-2024 20:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,79 +1,89 @@
-class Graph {
-  constructor() {
-    this.nodes = {};
-  }
-
-  addNode(name) {
-    this.nodes[name] = [];
-  }
-
-  addEdge(startNode, endNode) {
-    this.nodes[startNode].push(endNode);
-    this.nodes[endNode].push(startNode);
+class BTreeNode {
+  constructor(order, leaf = true) {
+    this.order = order; // Maximum number of keys a node can hold
+    this.keys = [];
+    this.child = [];
+    this.leaf = leaf;
   }
 }
-function bidirectionalSearch(startNode, endNode, graph) {
-  // Create two queues, one for forward search and one for reverse search
-  const forwardQueue = [startNode];
-  const reverseQueue = [endNode];
-
-  // Create two sets to keep track of visited nodes
-  const forwardVisited = new Set();
-  const reverseVisited = new Set();
-
-  // Start the search
-  while (forwardQueue.length && reverseQueue.length) {
-    // Perform forward search
-    const forwardCurrentNode = forwardQueue.shift();
-    forwardVisited.add(forwardCurrentNode);
-
-    // Check if we reach the end node
-    if (reverseVisited.has(forwardCurrentNode)) {
-      return true; // Path found
-    }
-
-    // Add unvisited neighbors to the forward queue
-    graph.nodes[forwardCurrentNode].forEach((neighbor) => {
-      if (!forwardVisited.has(neighbor)) {
-        forwardQueue.push(neighbor);
-      }
-    });
-
-    // Perform reverse search
-    const reverseCurrentNode = reverseQueue.shift();
-    reverseVisited.add(reverseCurrentNode);
-
-    // Check if we reach the start node
-    if (forwardVisited.has(reverseCurrentNode)) {
-      return true; // Path found
-    }
-
-    // Add unvisited neighbors to the reverse queue
-    graph.nodes[reverseCurrentNode].forEach((neighbor) => {
-      if (!reverseVisited.has(neighbor)) {
-        reverseQueue.push(neighbor);
-      }
-    });
+class BTree {
+  constructor(order) {
+    this.root = new BTreeNode(order, true);
+    this.order = order; // Maximum number of keys a node can hold
   }
 
-  return false; // Path not found
+  // Insert a key into the B-tree
+  insert(key) {
+    const root = this.root;
+    if (root.keys.length === 2 * this.order - 1) {
+      const newNode = new BTreeNode(this.order, false);
+
+      this.root = newNode;
+      newNode.child[0] = root;
+
+      this.splitChild(newNode, 0);
+      this.insertNonFull(newNode, key);
+    } else {
+      this.insertNonFull(root, key);
+    }
+  }
+
+  // Insert a key into a non-full node
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+    if (node.leaf) {
+      node.keys.push('');
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i + 1] = node.keys[i];
+        i--;
+      }
+      node.keys[i + 1] = key;
+    } else {
+      while (i >= 0 && key < node.keys[i]) {
+        i--;
+      }
+      i++;
+      if (node.child[i].keys.length === 2 * this.order - 1) {
+        this.splitChild(node, i);
+        if (key > node.keys[i]) {
+          i++;
+        }
+      }
+      this.insertNonFull(node.child[i], key);
+    }
+  }
+
+  // Split the child node at index 'i'
+  splitChild(parent, i) {
+    const order = this.order;
+    const child = parent.child[i];
+    const newNode = new BTreeNode(order, child.leaf);
+    parent.keys.splice(i, 0, child.keys[order - 1]);
+    parent.child.splice(i + 1, 0, newNode);
+    newNode.keys = child.keys.splice(order, order - 1);
+
+    if (!child.leaf) {
+      newNode.child = child.child.splice(order, order);
+    }
+  }
+
+  // Search a key in the B-tree
+  search(key, node = this.root) {
+    const i = 0;
+    while (i < node.keys.length && key > node.keys[i]) {
+      i++;
+    }
+    if (node.keys[i] === key) {
+      return node;
+    }
+    if (node.leaf) {
+      return null;
+    }
+    return this.search(key, node.child[i]);
+  }
 }
-const graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addNode('E');
-graph.addNode('F');
-
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'C');
-graph.addEdge('C', 'D');
-graph.addEdge('D', 'E');
-graph.addEdge('E', 'F');
-
-const startNode = 'A';
-const endNode = 'F';
-
-const pathExists = bidirectionalSearch(startNode, endNode, graph);
-console.log(pathExists); // true
+const bTree = new BTree(3);
+bTree.insert(1);
+bTree.insert(2);
+bTree.insert(3);
+console.log(bTree.search(2)); // outputs the node containing the key: 2
