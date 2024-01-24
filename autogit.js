0f1a77f08ae2a5779d8adf4c018ec67a33e6db46
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:00:43 +0800

    1-25-2024 6:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,22 @@
-function findLongestCommonSubsequence(str1, str2) {
-  const grid = Array(str1.length + 1)
-    .fill(null)
-    .map(() => Array(str2.length + 1).fill(0));
+function areAnagrams(str1, str2) {
+  // Remove whitespace and convert to lowercase
+  str1 = str1.replace(/\s/g, '').toLowerCase();
+  str2 = str2.replace(/\s/g, '').toLowerCase();
 
-  // Fill the grid using the LCS algorithm
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        grid[i][j] = grid[i - 1][j - 1] + 1;
-      } else {
-        grid[i][j] = Math.max(grid[i - 1][j], grid[i][j - 1]);
-      }
-    }
+  // Check if lengths are different
+  if (str1.length !== str2.length) {
+    return false;
   }
 
-  // Traverse the grid to get the longest common subsequence
-  let lcs = '';
-  let i = str1.length;
-  let j = str2.length;
+  // Convert strings to arrays and sort them
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
 
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (grid[i - 1][j] > grid[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
+  // Compare the sorted arrays
+  return sortedStr1 === sortedStr2;
 }
 
 // Example usage:
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const longestSubsequence = findLongestCommonSubsequence(str1, str2);
-console.log(longestSubsequence); // Output: ADH
+console.log(areAnagrams('listen', 'silent'));   // true
+console.log(areAnagrams('hello', 'world'));     // false
+console.log(areAnagrams('Debit card', 'Bad credit'));   // true
