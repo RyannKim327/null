272f093c747bf540bd21d7c75e9b6b3e2f5f8a1d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:41:23 +0800

    1-25-2024 5:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,13 @@
-const axios = require('axios');
+let arr = [1, 5, 3, 9, 2];
+let max = Math.max(...arr);
+console.log(max);  // Output: 9
+let arr = [1, 5, 3, 9, 2];
+let max = arr[0];  // Assume the first element is the maximum
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] > max) {
+    max = arr[i];  // Update max if a larger value is found
+  }
+}
+
+console.log(max);  // Output: 9
