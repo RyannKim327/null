a1dce8b35862d55b55124d9ffd50a65a102460b4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:51:05 +0800

    1-25-2024 5:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,39 @@
-function KMPSearch(pattern, text) {
-  const patternLength = pattern.length;
-  const textLength = text.length;
+function longestCommonSubsequence(s1, s2) {
+  const dp = Array(s1.length + 1)
+    .fill(0)
+    .map(() => Array(s2.length + 1).fill(0));
 
-  // Preprocess the pattern
-  const lps = new Array(patternLength).fill(0);
-  let len = 0;
-  let i = 1;
+  for (let i = 1; i <= s1.length; i++) {
+    for (let j = 1; j <= s2.length; j++) {
+      if (s1[i - 1] === s2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
 
-  while (i < patternLength) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
+  let lcs = '';
+  let i = s1.length;
+  let j = s2.length;
+
+  while (i > 0 && j > 0) {
+    if (s1[i - 1] === s2[j - 1]) {
+      lcs = s1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
     } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
+      j--;
     }
   }
 
-  // Perform the string matching
-  let j = 0;
-  let i = 0;
-
-  while (i < textLength) {
-    if (pattern[j] === text[i]) {
-      j++;
-      i++;
-    }
-
-    if (j === patternLength) {
-      console.log("Pattern found at index", i - j);
-      j = lps[j - 1];
-    } else if (i < textLength && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
+  return lcs;
 }
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-KMPSearch(pattern, text);
+
+// Usage example:
+const s1 = "AGGTAB";
+const s2 = "GXTXAYB";
+const result = longestCommonSubsequence(s1, s2);
+console.log(result); // Output: "GTAB"
