4e3fdce2497ab5398bc4fc7ddcfb94579aa33778
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:50:03 +0800

    1-24-2024 20:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,66 @@
-function areAnagrams(str1, str2) {
-  // Convert both strings to lowercase and remove non-alphanumeric characters
-  str1 = str1.toLowerCase().replace(/[^a-z0-9]/g, '');
-  str2 = str2.toLowerCase().replace(/[^a-z0-9]/g, '');
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-  // Sort the characters in both strings
-  str1 = str1.split('').sort().join('');
-  str2 = str2.split('').sort().join('');
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-  // Compare the sorted strings
-  return str1 === str2;
+  add(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+
+    while (current !== null) {
+      const nextNode = current.next;
+      current.next = prev;
+      prev = current;
+      current = nextNode;
+    }
+
+    this.head = prev;
+  }
+
+  print() {
+    let current = this.head;
+    const elements = [];
+    while (current !== null) {
+      elements.push(current.data);
+      current = current.next;
+    }
+    console.log(elements.join(' -> '));
+  }
 }
 
 // Example usage:
-const string1 = 'listen';
-const string2 = 'silent';
 
-if (areAnagrams(string1, string2)) {
-  console.log('The strings are anagrams.');
-} else {
-  console.log('The strings are not anagrams.');
-}
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+linkedList.add(5);
+
+console.log('Original Linked List:');
+linkedList.print();
+
+linkedList.reverse();
+
+console.log('Reversed Linked List:');
+linkedList.print();
