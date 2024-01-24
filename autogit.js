fc3cd668d4daed87b8432f546a00e7a4875b8ef0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:58:04 +0800

    1-25-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,41 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+function findLCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+
+  // Create a 2D array to store the lengths of common subsequences
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0));
+
+  // Build the dp array
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
     }
   }
-  return true;
-}
-const array1 = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(array1));  // Output: true
 
-const array2 = [5, 4, 3, 2, 1];
-console.log(isSortedAscending(array2));  // Output: false
+  // Read the LCS from the dp array
+  let i = m, j = n;
+  const lcs = [];
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs.unshift(str1[i - 1]);
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return lcs.join('');
+}
+const str1 = "ABCBDAB";
+const str2 = "BDCAB";
+const lcs = findLCS(str1, str2);
+console.log(lcs);  // Output: BCAB
