7cb92cf1eb659f4887440c96a7538dca47f9af54
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:19:04 +0800

    1-25-2024 5:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,5 @@
-function countingSort(arr) {
-  let min = arr[0];
-  let max = arr[0];
+// Prompt the user for input
+var userInput = prompt("Enter a value:");
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-
-  const countArray = new Array(max - min + 1).fill(0);
-
-  for (let i = 0; i < arr.length; i++) {
-    countArray[arr[i] - min]++;
-  }
-
-  for (let i = 1; i < countArray.length; i++) {
-    countArray[i] += countArray[i - 1];
-  }
-
-  const sortedArray = new Array(arr.length);
-
-  for (let i = arr.length - 1; i >= 0; i--) {
-    sortedArray[countArray[arr[i] - min] - 1] = arr[i];
-    countArray[arr[i] - min]--;
-  }
-
-  return sortedArray;
-}
-const array = [5, 3, 2, 6, 1, 4];
-const sortedArray = countingSort(array);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6]
+// Display the user's input in the console
+console.log("User input:", userInput);
