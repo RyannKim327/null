f964920805b70b0e06402d4fc7f796c649211803
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:19:36 +0800

    1-25-2024 5:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,11 @@
-class ListNode {
-  constructor(val, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
+function getLinkedListLength(head) {
+  let current = head;
+  let length = 0;
 
-function findMiddleElement(head) {
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
+  while (current !== null) {
+    length++;
+    current = current.next;
   }
 
-  return slow.val;
+  return length;
 }
-const node5 = new ListNode(5);
-const node4 = new ListNode(4, node5);
-const node3 = new ListNode(3, node4);
-const node2 = new ListNode(2, node3);
-const node1 = new ListNode(1, node2);
-
-console.log(findMiddleElement(node1)); // Outputs: 3
