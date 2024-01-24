2e237c2c5ac4996af744f3a5a15aa4f433dd5efe
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:24:12 +0800

    1-25-2024 5:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,7 @@
-function removeVowels(str) {
-  // Use the replace() method with a regular expression to remove vowels
-  var removed = str.replace(/[aeiou]/gi, '');
-
-  return removed;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
-var myString = "Hello, World!"; // Example string
-var result = removeVowels(myString);
-console.log(result);
-Hll, Wrld!
+// Example usage
+console.log(decimalToBinary(10));  // Output: 1010
+console.log(decimalToBinary(42));  // Output: 101010
