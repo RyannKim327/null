b3b8c5483024616030e7b9fb0421719e9a5a6d54
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:09:43 +0800

    1-25-2024 6:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,61 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+class HashTable {
+  constructor(size = 100) {
+    this.size = size;
+    this.table = new Array(size);
   }
 
-  const midIndex = Math.floor(arr.length / 2);
-  const leftHalf = arr.slice(0, midIndex);
-  const rightHalf = arr.slice(midIndex);
-
-  return merge(mergeSort(leftHalf), mergeSort(rightHalf));
-}
-
-function merge(arr1, arr2) {
-  const merged = [];
-  let i = 0;
-  let j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      merged.push(arr1[i]);
-      i++;
-    } else {
-      merged.push(arr2[j]);
-      j++;
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
     }
+    return hash % this.size;
   }
 
-  while (i < arr1.length) {
-    merged.push(arr1[i]);
-    i++;
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
   }
 
-  while (j < arr2.length) {
-    merged.push(arr2[j]);
-    j++;
+  get(key) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          return this.table[index][i][1];
+        }
+      }
+    }
+    return undefined;
   }
 
-  return merged;
+  delete(key) {
+    const index = this._hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          this.table[index].splice(i, 1);
+          if (this.table[index].length === 0) {
+            delete this.table[index];
+          }
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
+const hashTable = new HashTable();
+hashTable.set("name", "John");
+hashTable.set("age", 30);
+hashTable.set("city", "New York");
 
-// Example usage:
-const arr = [5, 2, 8, 12, 3, 1, 9];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr);
+console.log(hashTable.get("name")); // Output: John
+console.log(hashTable.get("age")); // Output: 30
+console.log(hashTable.get("city")); // Output: New York
+
+hashTable.delete("age");
+console.log(hashTable.get("age")); // Output: undefined
