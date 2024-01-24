09a92f9332177f4e03e89871e63b68f38ce4fd69
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:21:06 +0800

    1-25-2024 5:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,5 @@
-function findCommonElements(array1, array2) {
-  var commonElements = [];
-  for (var i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      commonElements.push(array1[i]);
-    }
-  }
-  return commonElements;
-}
-function findCommonElements(array1, array2) {
-  return array1.filter(function (value) {
-    return array2.includes(value);
-  });
-}
-function findCommonElements(array1, array2) {
-  var set = new Set(array2);
-  return array1.filter(function (value) {
-    return set.has(value);
-  });
-}
-var arr1 = [1, 2, 3, 4, 5];
-var arr2 = [4, 5, 6, 7, 8];
+const str = "Hello, world! This is a sample string.";
 
-var commonElements = findCommonElements(arr1, arr2);
-console.log(commonElements); // Output: [4, 5]
+const withoutSpaces = str.replace(/\s/g, '');
+console.log(withoutSpaces);
+Hello,world!Thisisasamplestring.
