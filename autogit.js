361a539831aecafd4cd772ee3db3a8be37a07e57
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:44:07 +0800

    1-24-2024 20:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,8 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const mid = Math.floor(arr.length / 2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid);
-
-  return merge(mergeSort(left), mergeSort(right));
+function reverseString(str) {
+  return str.split("").reverse().join("");
 }
 
-function merge(left, right) {
-  let result = [];
-  let i = 0;
-  let j = 0;
+var myString = "Hello, World!";
+var reversedString = reverseString(myString);
 
-  while (i < left.length && j < right.length) {
-    if (left[i] < right[j]) {
-      result.push(left[i]);
-      i++;
-    } else {
-      result.push(right[j]);
-      j++;
-    }
-  }
-
-  // Concatenate the remaining elements
-  return result.concat(left.slice(i)).concat(right.slice(j));
-}
-
-// Example usage:
-const arr = [5, 3, 8, 4, 2, 1, 10, 9];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr);
+console.log(reversedString); // Outputs: "!dlroW ,olleH"
