45fbbe1020d75d8a681b12764dffed0e1fffb339
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:45:02 +0800

    1-24-2024 20:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,10 @@
-function fibonacciSearch(arr, target) {
-  let fib1 = 0;
-  let fib2 = 1;
-  let fibM = fib1 + fib2;
-
-  while (fibM < arr.length) {
-    fib1 = fib2;
-    fib2 = fibM;
-    fibM = fib1 + fib2;
+function getStringLength(str) {
+  let length = 0;
+  while (str[length] !== undefined) {
+    length++;
   }
-
-  let offset = 0;
-  let index = Math.min(offset + fib1, arr.length - 1);
-
-  while (arr[index] !== target && fib1 > 1) {
-    if (target < arr[index]) {
-      fib2 = fib1;
-      fib1 = fibM - fib2;
-      fibM = fib2 - fib1;
-    } else {
-      fib1 = fib1 - fib2;
-      fib2 = fibM - fib1;
-      offset = index;
-      fibM = fib1 + fib2;
-    }
-
-    index = Math.min(offset + fib1, arr.length - 1);
-  }
-
-  if (arr[index] === target) {
-    return index;
-  } else {
-    return -1;
-  }
+  return length;
 }
+const myString = "Hello, world!";
+const length = getStringLength(myString);
+console.log(length); // Output: 13
