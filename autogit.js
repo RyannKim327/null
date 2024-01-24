00a4ec3dfa57d906e061f45f25dd8af2b72f6f06
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:55:12 +0800

    1-25-2024 5:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,41 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Do something with the retrieved data
-  })
-  .catch(error => {
-    console.log('An error occurred:', error);
-  });
+function findLCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  
+  // Create a 2D array to store LCS lengths
+  const dp = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
+  
+  // Building the LCS matrix
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+  }
+  
+  // Backtrack to find the LCS
+  let i = m, j = n;
+  const lcs = [];
+  
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs.unshift(str1[i - 1]);
+      i--;
+      j--;
+    } else if (dp[i][j - 1] > dp[i - 1][j]) {
+      j--;
+    } else {
+      i--;
+    }
+  }
+  
+  return lcs.join('');
+}
+const str1 = 'abcdaf';
+const str2 = 'acbcf';
+
+const lcs = findLCS(str1, str2);
+console.log(lcs);  // Output: "abcf"
