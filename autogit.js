497265dbc580bec8caefd4413e077a9451b76972
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:50:39 +0800

    1-25-2024 5:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,42 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
-            count++;
-        }
+function fibonacciSearch(arr, key) {
+  function generateFibonacci(n) {
+    const fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
     }
-    return count;
+    return fib;
+  }
+
+  const fib = generateFibonacci(arr.length);
+
+  let offset = -1;
+  let prevOffset = -1;
+  let temp = fib[fib.length - 1];
+
+  while (temp > 1) {
+    if (arr[offset + prevOffset] === key) {
+      return offset + prevOffset;
+    }
+
+    if (arr[offset + prevOffset] < key) {
+      offset = prevOffset;
+      temp -= fib[fib.indexOf(temp) - 1];
+    } else {
+      prevOffset = offset - prevOffset;
+      temp -= fib[fib.indexOf(temp) - 2];
+    }
+  }
+
+  if (arr[offset + 1] === key) {
+    return offset + 1;
+  }
+  
+  return -1;
 }
 
-// Example usage
-const myString = "Hello, World!";
-const charToCount = "o";
-const occurrences = countOccurrences(myString, charToCount);
-console.log(occurrences); // Output: 2
-function countOccurrences(str, char) {
-    const charArray = str.split(char);
-    return charArray.length - 1;
-}
+// Example usage:
+const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+const key = 12;
+const index = fibonacciSearch(arr, key);
 
-// Example usage
-const myString = "Hello, World!";
-const charToCount = "o";
-const occurrences = countOccurrences(myString, charToCount);
-console.log(occurrences); // Output: 2
-function countOccurrences(str, char) {
-    const regex = new RegExp(char, 'g');
-    const matches = str.match(regex);
-    return matches ? matches.length : 0;
-}
-
-// Example usage
-const myString = "Hello, World!";
-const charToCount = "o";
-const occurrences = countOccurrences(myString, charToCount);
-console.log(occurrences); // Output: 2
+console.log(`Key ${key} found at index ${index}`);
