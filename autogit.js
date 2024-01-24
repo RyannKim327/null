96f34086ec852287febf02df44ace26149e2bd9d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:46:33 +0800

    1-25-2024 5:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,158 @@
-function findFirstNonRepeatingChar(str) {
-  const charCount = {};
-  
-  // Count the frequency of each character
-  for (let char of str) {
-    if (charCount[char]) {
-      charCount[char]++;
-    } else {
-      charCount[char] = 1;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  // Find the first non-repeating character
-  for (let char of str) {
-    if (charCount[char] === 1) {
-      return char;
-    }
-  }
-  
-  // Return null if no non-repeating character is found
-  return null;
 }
 
-// Example usage
-const input = 'aabbcdeff';
-const result = findFirstNonRepeatingChar(input);
-console.log(result);  // Output: 'c'
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // Removing a leaf node
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      // Removing a node with one child
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // Removing a node with two children
+      const aux = this.findMinNode(node.right);
+      node.value = aux.value;
+      node.right = this.removeNode(node.right, aux.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  inorderTraversal(callback) {
+    this.inorderTraversalNode(this.root, callback);
+  }
+
+  inorderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inorderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inorderTraversalNode(node.right, callback);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log("Inorder traversal:");
+bst.inorderTraversal(value => console.log(value));
+
+console.log("Searching for value 6:", bst.search(6));
+console.log("Searching for value 11:", bst.search(11));
+
+bst.remove(6);
+console.log("After removing value 6, inorder traversal:");
+bst.inorderTraversal(value => console.log(value));
+Inorder traversal:
+1
+3
+4
+6
+7
+8
+10
+13
+14
+Searching for value 6: true
+Searching for value 11: false
+After removing value 6, inorder traversal:
+1
+3
+4
+7
+8
+10
+13
+14
