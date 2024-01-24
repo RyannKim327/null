4e6e75a745f44165935339cd80c21e25f8be09de
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:41:50 +0800

    1-24-2024 20:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,24 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
+function countOccurrences(string, character) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === character) {
+      count++;
+    }
   }
+  return count;
 }
 
-function findMiddleElement(head) {
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  return slow;
+let myString = "Hello, World!";
+let myCharacter = "l";
+let occurrenceCount = countOccurrences(myString, myCharacter);
+console.log(occurrenceCount); // Output: 3
+function countOccurrences(string, character) {
+  let regex = new RegExp(character, "g");
+  let matches = string.match(regex);
+  return matches ? matches.length : 0;
 }
+
+let myString = "Hello, World!";
+let myCharacter = "l";
+let occurrenceCount = countOccurrences(myString, myCharacter);
+console.log(occurrenceCount); // Output: 3
