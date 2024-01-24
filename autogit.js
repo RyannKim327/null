091394b539cc45f1ada9ea8f09255e6782774e73
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:19:48 +0800

    1-24-2024 20:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,25 @@
-function hash(key, size) {
-  let hash = 5381;
-  for (let i = 0; i < key.length; i++) {
-    hash = (hash * 33) ^ key.charCodeAt(i);
-  }
-  return hash % size;
-}
-class HashTable {
-  constructor(size) {
-    this.size = size;
-    this.table = new Array(size);
+function longestCommonPrefix(strings) {
+  let commonPrefix = '';
+
+  if (strings.length === 0) {
+    return commonPrefix;
   }
 
-  // ...
-}
-put(key, value) {
-  const index = hash(key, this.size);
-  if (!this.table[index]) {
-    this.table[index] = [];
-  }
-  this.table[index].push({ key, value });
-}
-get(key) {
-  const index = hash(key, this.size);
-  if (this.table[index]) {
-    const buckets = this.table[index];
-    for (let i = 0; i < buckets.length; i++) {
-      if (buckets[i].key === key) {
-        return buckets[i].value;
+  commonPrefix = strings[0];
+
+  for (let i = 0; i < commonPrefix.length; i++) {
+    for (let j = 1; j < strings.length; j++) {
+      if (commonPrefix[i] !== strings[j][i] || i === strings[j].length) {
+        return commonPrefix;
       }
     }
+    commonPrefix += strings[0][i];
   }
-  return undefined;
+
+  return commonPrefix;
 }
-remove(key) {
-  const index = hash(key, this.size);
-  if (this.table[index]) {
-    const buckets = this.table[index];
-    for (let i = 0; i < buckets.length; i++) {
-      if (buckets[i].key === key) {
-        buckets.splice(i, 1);
-        break;
-      }
-    }
-  }
-}
-const myHashTable = new HashTable(10);
 
-myHashTable.put('name', 'John');
-myHashTable.put('age', 25);
-
-console.log(myHashTable.get('name')); // Output: John
-
-myHashTable.remove('age');
-
-console.log(myHashTable.get('age')); // Output: undefined
+// Example usage:
+const inputStrings = ['apple', 'application', 'appstore'];
+const result = longestCommonPrefix(inputStrings);
+console.log(result); // Output: 'app'
