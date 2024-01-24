365682fa312b6e77bbe4866e5d46d47838e0f10e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:45:23 +0800

    1-25-2024 5:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,54 @@
-const string = "Hello, World!";
-const substring = "World";
+class Graph {
+  constructor() {
+    this.vertices = {};
+    this.edges = [];
+  }
 
-if (string.includes(substring)) {
-  console.log("Substring found!");
-} else {
-  console.log("Substring not found.");
-}
-const string = "Hello, World!";
-const substring = "World";
+  addVertex(id) {
+    this.vertices[id] = true;
+  }
 
-if (string.indexOf(substring) !== -1) {
-  console.log("Substring found!");
-} else {
-  console.log("Substring not found.");
+  addEdge(source, destination, weight) {
+    this.edges.push({ source, destination, weight });
+  }
 }
-const string = "Hello, World!";
-const substring = /World/;
+function bellmanFord(graph, source) {
+  // Step 1: Initialize distances
+  const distances = {};
+  for (let vertex in graph.vertices) {
+    distances[vertex] = vertex === source ? 0 : Infinity;
+  }
 
-if (substring.test(string)) {
-  console.log("Substring found!");
-} else {
-  console.log("Substring not found.");
+  // Step 2: Relax edges repeatedly
+  for (let i = 0; i < Object.keys(graph.vertices).length - 1; i++) {
+    for (let j = 0; j < graph.edges.length; j++) {
+      const { source, destination, weight } = graph.edges[j];
+      if (distances[source] + weight < distances[destination]) {
+        distances[destination] = distances[source] + weight;
+      }
+    }
+  }
+
+  // Step 3: Check for negative-weight cycles
+  for (let i = 0; i < graph.edges.length; i++) {
+    const { source, destination, weight } = graph.edges[i];
+    if (distances[source] + weight < distances[destination]) {
+      throw new Error("Graph contains a negative-weight cycle");
+    }
+  }
+
+  return distances;
 }
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+
+graph.addEdge("A", "B", 5);
+graph.addEdge("A", "C", 2);
+graph.addEdge("C", "B", -6);
+graph.addEdge("B", "D", 3);
+const sourceVertex = "A";
+const shortestPaths = bellmanFord(graph, sourceVertex);
+console.log(shortestPaths);
