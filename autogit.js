247879e324a8436653b168b6a52c9c72fecfe0ba
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:47:16 +0800

    1-25-2024 5:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,144 @@
-function heapify(array, length, index) {
-  let largest = index;
-  let leftChild = 2 * index + 1;
-  let rightChild = 2 * index + 2;
-
-  if (leftChild < length && array[leftChild] > array[largest]) {
-    largest = leftChild;
-  }
-
-  if (rightChild < length && array[rightChild] > array[largest]) {
-    largest = rightChild;
-  }
-
-  if (largest !== index) {
-    [array[index], array[largest]] = [array[largest], array[index]];
-    heapify(array, length, largest);
+// Red-Black Tree Node
+class Node {
+  constructor(key, color) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+    this.color = color;
   }
 }
 
-function heapSort(array) {
-  const length = array.length;
-
-  for (let i = Math.floor(length / 2) - 1; i >= 0; i--) {
-    heapify(array, length, i);
+// Red-Black Tree
+class RedBlackTree {
+  constructor() {
+    this.root = null;
   }
 
-  for (let i = length - 1; i > 0; i--) {
-    [array[0], array[i]] = [array[i], array[0]];
-    heapify(array, i, 0);
+  // Insert a new node
+  insert(key) {
+    const node = new Node(key, "red");
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (current) {
+        if (key < current.key) {
+          if (!current.left) {
+            current.left = node;
+            node.parent = current;
+            break;
+          }
+          current = current.left;
+        } else {
+          if (!current.right) {
+            current.right = node;
+            node.parent = current;
+            break;
+          }
+          current = current.right;
+        }
+      }
+      this.fixInsertion(node);
+    }
   }
 
-  return array;
+  // Fix the tree after insertion
+  fixInsertion(node) {
+    while (node.parent && node.parent.color === "red") {
+      if (node.parent === node.parent.parent.left) {
+        let uncle = node.parent.parent.right;
+        if (uncle && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.right) {
+            node = node.parent;
+            this.rotateLeft(node);
+          }
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateRight(node.parent.parent);
+        }
+      } else {
+        let uncle = node.parent.parent.left;
+        if (uncle && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.left) {
+            node = node.parent;
+            this.rotateRight(node);
+          }
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateLeft(node.parent.parent);
+        }
+      }
+    }
+    this.root.color = "black";
+  }
+
+  // Left rotation
+  rotateLeft(node) {
+    let temp = node.right;
+    node.right = temp.left;
+    if (temp.left) {
+      temp.left.parent = node;
+    }
+    temp.parent = node.parent;
+    if (!node.parent) {
+      this.root = temp;
+    } else if (node === node.parent.left) {
+      node.parent.left = temp;
+    } else {
+      node.parent.right = temp;
+    }
+    temp.left = node;
+    node.parent = temp;
+  }
+
+  // Right rotation
+  rotateRight(node) {
+    let temp = node.left;
+    node.left = temp.right;
+    if (temp.right) {
+      temp.right.parent = node;
+    }
+    temp.parent = node.parent;
+    if (!node.parent) {
+      this.root = temp;
+    } else if (node === node.parent.right) {
+      node.parent.right = temp;
+    } else {
+      node.parent.left = temp;
+    }
+    temp.right = node;
+    node.parent = temp;
+  }
+
+  // Print the tree in-order
+  printInOrder(node = this.root) {
+    if (node) {
+      this.printInOrder(node.left);
+      console.log(node.key);
+      this.printInOrder(node.right);
+    }
+  }
 }
 
 // Example usage
-const arr = [9, 12, 5, 2, 10, 3];
-console.log(heapSort(arr)); // Output: [2, 3, 5, 9, 10, 12]
+const rbTree = new RedBlackTree();
+rbTree.insert(10);
+rbTree.insert(5);
+rbTree.insert(15);
+rbTree.insert(3);
+rbTree.insert(8);
+rbTree.insert(12);
+rbTree.insert(17);
+
+rbTree.printInOrder();
