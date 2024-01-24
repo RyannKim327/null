87c23538469c17a9c5680ad72624728276341865
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:08:26 +0800

    1-25-2024 6:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,94 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
+class Graph {
+  constructor() {
+    this.adjList = new Map();
   }
-  return true;
+
+  addVertex(vertex) {
+    this.adjList.set(vertex, []);
+  }
+
+  addEdge(v1, v2) {
+    this.adjList.get(v1).push(v2);
+    this.adjList.get(v2).push(v1);
+  }
+
+  getNeighbors(vertex) {
+    return this.adjList.get(vertex);
+  }
+}
+
+function biDirectionalSearch(graph, start, goal) {
+  const forwardQueue = [start];
+  const backwardQueue = [goal];
+  const forwardVisited = new Set();
+  const backwardVisited = new Set();
+  const forwardParent = new Map();
+  const backwardParent = new Map();
+
+  while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+    const currentForward = forwardQueue.shift();
+    const currentBackward = backwardQueue.shift();
+
+    if (backwardVisited.has(currentForward) || forwardVisited.has(currentBackward)) {
+      const path = getPath(currentForward, currentBackward, forwardParent, backwardParent);
+      return path;
+    }
+
+    forwardVisited.add(currentForward);
+    backwardVisited.add(currentBackward);
+
+    const forwardNeighbors = graph.getNeighbors(currentForward);
+    const backwardNeighbors = graph.getNeighbors(currentBackward);
+
+    forwardNeighbors.forEach((neighbor) => {
+      if (!forwardVisited.has(neighbor)) {
+        forwardQueue.push(neighbor);
+        forwardParent.set(neighbor, currentForward);
+      }
+    });
+
+    backwardNeighbors.forEach((neighbor) => {
+      if (!backwardVisited.has(neighbor)) {
+        backwardQueue.push(neighbor);
+        backwardParent.set(neighbor, currentBackward);
+      }
+    });
+  }
+
+  return null;
+}
+
+function getPath(start, goal, forwardParent, backwardParent) {
+  const path = [];
+  let current = start;
+  while (current) {
+    path.push(current);
+    current = forwardParent.get(current);
+  }
+  path.reverse();
+  current = backwardParent.get(goal);
+  while (current) {
+    path.push(current);
+    current = backwardParent.get(current);
+  }
+  return path;
 }
 
 // Example usage:
-const array1 = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(array1));  // Output: true
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
 
-const array2 = [5, 2, 7, 1, 8];
-console.log(isSortedAscending(array2));  // Output: false
+const startNode = 'A';
+const goalNode = 'E';
+
+const path = biDirectionalSearch(graph, startNode, goalNode);
+console.log(path);
