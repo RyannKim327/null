5f64e9ca7ba7c0f607b9101334f040e8454d7e27
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:38:38 +0800

    1-24-2024 20:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,21 @@
-function countOccurrences(str, char) {
-  var count = 0;
-  for (var i = 0; i < str.length; i++) {
-    if (str.charAt(i) === char) {
-      count++;
-    }
+function binarySearchRecursive(arr, target, start = 0, end = arr.length - 1) {
+  if (start > end) {
+    return -1;
   }
-  return count;
+
+  const mid = Math.floor((start + end) / 2);
+
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] > target) {
+    return binarySearchRecursive(arr, target, start, mid - 1);
+  } else {
+    return binarySearchRecursive(arr, target, mid + 1, end);
+  }
 }
 
-var myString = "Hello, World!";
-var myChar = "o";
-var occurrences = countOccurrences(myString, myChar);
-
-console.log("Number of occurrences of '" + myChar + "' in the string: " + occurrences);
+// Example usage:
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const target = 5;
+const index = binarySearchRecursive(arr, target);
+console.log(index); // Output: 4
