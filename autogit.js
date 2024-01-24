38d421211833611a414e66ecd14d82ec3ff775b3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:43:17 +0800

    1-24-2024 20:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,24 @@
-function findFirstNonRepeatingChar(str) {
-  const charCount = {};
-  
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (charCount[char]) {
-      charCount[char]++;
-    } else {
-      charCount[char] = 1;
+function selectionSort(arr) {
+  // Loop through each element of the array
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i;
+
+    // Find the index of the minimum value in the unsorted part of the array
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the minimum value with the first unsorted element
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
     }
   }
-  
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (charCount[char] === 1) {
-      return char;
-    }
-  }
-  
-  return null; // If no non-repeating character is found
+
+  return arr;
 }
 
-// Testing the function
-const str = "abracadabra";
-const firstNonRepeatingChar = findFirstNonRepeatingChar(str);
-console.log(firstNonRepeatingChar); // Output: "b"
+// Test the implementation
+const array = [64, 25, 12, 22, 11];
+console.log(selectionSort(array)); // [11, 12, 22, 25, 64]
