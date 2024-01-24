67ec9732e9460b8ee41cc4e652f647c30f4dd234
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:41:15 +0800

    1-25-2024 5:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-function interpolationSearch(array, target) {
-  // ...
-}
-  let start = 0;
-  let end = array.length - 1;
-  while (start <= end) {
-    // ...
-  }
-    let pos = Math.floor(start + (((target - array[start]) * (end - start)) / (array[end] - array[start]))));
-    if (array[pos] === target) {
-      return pos;
-    }
-    if (array[pos] > target) {
-      end = pos - 1;
-    }
-    if (array[pos] < target) {
-      start = pos + 1;
-    }
-  return -1;
-function interpolationSearch(array, target) {
-  let start = 0;
-  let end = array.length - 1;
+const axios = require('axios');
 
-  while (start <= end) {
-    let pos = Math.floor(
-      start + (((target - array[start]) * (end - start)) / (array[end] - array[start]))
-    );
-
-    if (array[pos] === target) {
-      return pos;
-    }
-
-    if (array[pos] > target) {
-      end = pos - 1;
-    }
-
-    if (array[pos] < target) {
-      start = pos + 1;
-    }
-  }
-
-  return -1;
-}
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
