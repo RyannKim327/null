c23555a153ede18a3ccdb112ffa2dba6065aadb3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:26:27 +0800

    1-25-2024 5:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,18 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, "");
+function reverseWords(string) {
+  // Step 1: Split the string into an array of words
+  var wordsArray = string.split(' ');
+
+  // Step 2: Reverse the array of words
+  var reversedArray = wordsArray.reverse();
+
+  // Step 3: Join the reversed array into a string
+  var reversedString = reversedArray.join(' ');
+
+  return reversedString;
 }
 
 // Example usage
-const input = "Hello, world!";
-const result = removeVowels(input);
-console.log(result); // Output: "Hll, wrld!"
+var inputString = "Hello world, how are you?";
+var reversedString = reverseWords(inputString);
+console.log(reversedString);
+you? are how world, Hello
