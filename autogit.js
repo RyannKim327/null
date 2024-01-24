9153cc7b0155aefd04741d0d2115542432e2afe6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:54:35 +0800

    1-25-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,47 @@
-let arr = [5, 2, 8, 1, 4];
-arr = arr.sort((a, b) => a - b);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
 
-console.log(arr); // Output: [1, 2, 4, 5, 8]
+  addChild(node) {
+    this.children.push(node);
+  }
+
+  breadthFirstSearch() {
+    const queue = [this]; // Start with the root node in the queue
+    const visited = [];
+
+    while (queue.length > 0) {
+      const currentNode = queue.shift(); // Dequeue the first node
+
+      // Process the current node
+      visited.push(currentNode.value);
+
+      // Enqueue all children of the current node
+      for (const child of currentNode.children) {
+        queue.push(child);
+      }
+    }
+
+    return visited;
+  }
+}
+
+// Usage example:
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+const nodeF = new Node('F');
+const nodeG = new Node('G');
+
+nodeA.addChild(nodeB);
+nodeA.addChild(nodeC);
+nodeB.addChild(nodeD);
+nodeB.addChild(nodeE);
+nodeC.addChild(nodeF);
+nodeC.addChild(nodeG);
+
+console.log(nodeA.breadthFirstSearch()); // Output: ['A', 'B', 'C', 'D', 'E', 'F', 'G']
