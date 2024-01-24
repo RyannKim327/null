ef59e2f9b5631c24117235b3f38541c26aeec48b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:45:40 +0800

    1-25-2024 5:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,21 @@
-const numbers = [5, 2, 9, 1, 7];
-const max = Math.max(...numbers);
+function insertionSort(array) {
+  const length = array.length;
 
-console.log(max);  // Output: 9
+  for (let i = 1; i < length; i++) {
+    const temp = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = temp;
+  }
+
+  return array;
+}
+
+// Test the function
+const arr = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5, 6]
