98507887857aeaba1431f5c63f79f52037d20821
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:08:55 +0800

    1-25-2024 6:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,23 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('An error occurred:', error);
-  });
+function selectionSort(arr) {
+  const len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    if (min !== i) {
+      [arr[i], arr[min]] = [arr[min], arr[i]]; // Swap elements
+    }
+  }
+
+  return arr;
+}
+
+// Example usage:
+const arr = [8, 5, 2, 9, 1, 7, 3];
+console.log(selectionSort(arr)); // [1, 2, 3, 5, 7, 8, 9]
