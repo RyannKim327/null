6f75ecd00b601762daca520134c31e543496deff
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:31:30 +0800

    1-25-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,22 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    // Handle the data obtained from the response
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the fetch request
-    console.error('Error:', error);
-  });
+function findMajorityElement(nums) {
+  let majorityIndex = 0;
+  let count = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[majorityIndex]) {
+      count++;
+    } else {
+      count--;
+    }
+
+    if (count === 0) {
+      majorityIndex = i;
+      count = 1;
+    }
+  }
+
+  return nums[majorityIndex];
+}
+const nums = [2, 2, 1, 1, 1, 2, 2];
+const majorityElement = findMajorityElement(nums);
+console.log(majorityElement); // Output: 2
