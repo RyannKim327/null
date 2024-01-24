c79b3f172024f19ea681183c9d201574d3ab4166
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:34:08 +0800

    1-25-2024 5:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,9 @@
-function selectionSort(array) {
-  const length = array.length;
-
-  for (let i = 0; i < length; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
-      }
-    }
-
-    if (minIndex !== i) {
-      let temp = array[i];
-      array[i] = array[minIndex];
-      array[minIndex] = temp;
-    }
-  }
-
-  return array;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example usage:
-const unsortedArray = [64, 25, 12, 22, 11];
-console.log(selectionSort(unsortedArray)); // Output: [11, 12, 22, 25, 64]
+// Usage example
+const min = 1;
+const max = 100;
+const randomNumber = getRandomNumber(min, max);
+console.log(randomNumber);
