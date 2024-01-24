31b27a23b8fd8d8c704befc76bc344a2bceeae82
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:52:12 +0800

    1-25-2024 5:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,18 @@
-function getRandomNumber(min, max) {
-  // Calculate the range size
-  var range = max - min + 1;
-  
-  // Generate a random number within the range
-  var randomNumber = Math.floor(Math.random() * range) + min;
-  
-  return randomNumber;
+function reverseWords(str) {
+  // Split the string into an array of words
+  var words = str.split(' ');
+
+  // Reverse the order of the array
+  var reversedWords = words.reverse();
+
+  // Join the words back together
+  var reversedString = reversedWords.join(' ');
+
+  return reversedString;
 }
 
 // Example usage
-var min = 1;
-var max = 10;
-var randomNumber = getRandomNumber(min, max);
-
-console.log(randomNumber);
+var inputString = 'Hello world, how are you?';
+var reversedString = reverseWords(inputString);
+console.log(reversedString);
+you? are how world, Hello
