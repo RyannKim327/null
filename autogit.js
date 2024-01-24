bd4a5a7862b6a731a6325cf7ef5779b3326f0117
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:58:12 +0800

    1-25-2024 5:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,30 @@
-function findLCS(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  // Create a 2D array to store the lengths of common subsequences
-  const dp = Array(m + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0));
-
-  // Build the dp array
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-      }
-    }
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  // Read the LCS from the dp array
-  let i = m, j = n;
-  const lcs = [];
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs.unshift(str1[i - 1]);
-      i--;
-      j--;
-    } else if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs.join('');
 }
-const str1 = "ABCBDAB";
-const str2 = "BDCAB";
-const lcs = findLCS(str1, str2);
-console.log(lcs);  // Output: BCAB
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+
+node1.next = node2;
+node2.next = node3;
+
+// Linked list: 1 -> 2 -> 3
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+
+  while (current !== null) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  
+  return prev;
+}
+const reversedHead = reverseLinkedList(node1);
+
+// The linked list is now reversed: 3 -> 2 -> 1
