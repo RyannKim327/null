0b15aa8c3be3661cc1ad0488c3257025dcd91eb0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:12:28 +0800

    1-25-2024 6:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,14 @@
-class HashTable {
-  constructor(size = 100) {
-    this.size = size;
-    this.table = new Array(size);
+function reverseLinkedList(head) {
+  let previous = null;
+  let current = head;
+  let next = null;
+
+  while (current !== null) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
   }
 
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
-    }
-    return hash % this.size;
-  }
-
-  set(key, value) {
-    const index = this._hash(key);
-    if (!this.table[index]) {
-      this.table[index] = [];
-    }
-    this.table[index].push([key, value]);
-  }
-
-  get(key) {
-    const index = this._hash(key);
-    if (this.table[index]) {
-      for (let i = 0; i < this.table[index].length; i++) {
-        if (this.table[index][i][0] === key) {
-          return this.table[index][i][1];
-        }
-      }
-    }
-    return undefined;
-  }
-
-  delete(key) {
-    const index = this._hash(key);
-    if (this.table[index]) {
-      for (let i = 0; i < this.table[index].length; i++) {
-        if (this.table[index][i][0] === key) {
-          this.table[index].splice(i, 1);
-          if (this.table[index].length === 0) {
-            delete this.table[index];
-          }
-          return true;
-        }
-      }
-    }
-    return false;
-  }
+  return previous;
 }
-const hashTable = new HashTable();
-hashTable.set("name", "John");
-hashTable.set("age", 30);
-hashTable.set("city", "New York");
-
-console.log(hashTable.get("name")); // Output: John
-console.log(hashTable.get("age")); // Output: 30
-console.log(hashTable.get("city")); // Output: New York
-
-hashTable.delete("age");
-console.log(hashTable.get("age")); // Output: undefined
