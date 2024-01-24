68f3b2d363d98f3d732d906b092e19221d2a3772
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:38:07 +0800

    1-24-2024 20:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,58 @@
-function shellSort(arr) {
-  const len = arr.length;
-  let interval = Math.floor(len / 2); // Initialize the interval sequence
-
-  while (interval > 0) {
-    for (let i = interval; i < len; i++) {
-      let temp = arr[i];
-      let j = i;
-
-      while (j >= interval && arr[j - interval] > temp) {
-        arr[j] = arr[j - interval];
-        j -= interval;
-      }
-
-      arr[j] = temp;
-    }
-
-    interval = Math.floor(interval / 2); // Reduce the interval
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
   }
 
-  return arr;
-}
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 
-// Usage example:
-const array = [5, 3, 8, 4, 2, 1];
-const sortedArray = shellSort(array);
-console.log(sortedArray);
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
+function depthFirstSearch(graph, startVertex) {
+  const visited = {};
+  const result = [];
+
+  function dfs(vertex) {
+    if (!vertex) return null;
+    visited[vertex] = true;
+    result.push(vertex);
+
+    graph.adjacencyList[vertex].forEach(neighbor => {
+      if (!visited[neighbor]) {
+        dfs(neighbor);
+      }
+    });
+  }
+
+  dfs(startVertex);
+  return result;
+}
+// Create a new graph
+const graph = new Graph();
+
+// Add vertices to the graph
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+// Add edges to the graph
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+// Perform DFS starting from vertex 'A'
+const dfsResult = depthFirstSearch(graph, 'A');
+console.log(dfsResult); // Output: ["A", "B", "D", "E", "C", "F"]
