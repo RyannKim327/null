996254a0c334f4a9ddd7474172addee4a5147b05
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:30:13 +0800

    1-24-2024 20:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,18 @@
-function generateLPS(pattern) {
-  const lps = [0]; // lps[0] is always 0
-  let len = 0; // length of the previous longest prefix suffix
+function findFirstRepeatedChar(str) {
+  const charFrequency = {};
 
-  let i = 1;
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charFrequency[char]) {
+      return char;
     }
-  }
-  return lps;
-}
-function KMP(text, pattern) {
-  const lps = generateLPS(pattern);
-
-  let i = 0; // index for text
-  let j = 0; // index for pattern
-
-  const indexes = [];
-
-  while (i < text.length) {
-    if (pattern[j] === text[i]) {
-      i++;
-      j++;
-    }
-
-    if (j === pattern.length) {
-      indexes.push(i - j);
-      j = lps[j - 1];
-    } else if (i < text.length && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
+    charFrequency[char] = true;
   }
 
-  return indexes;
+  return null; // If no repeated character is found
 }
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
 
-const indexes = KMP(text, pattern);
-console.log(indexes); // Output: [10]
+// Example usage:
+const input = "Hello World";
+const repeatedChar = findFirstRepeatedChar(input);
+console.log(repeatedChar); // Output: "l"
