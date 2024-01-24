9c49bcbbea6152696394fc6d54ae7a90629174bd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:05:43 +0800

    1-25-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,7 @@
-const array = [5, 8, 1, 9, 3];
-const max = Math.max(...array);
-console.log(max); // Output: 9
-const array = [5, 8, 1, 9, 3];
-const max = array.reduce((acc, curr) => (curr > acc ? curr : acc));
-console.log(max); // Output: 9
+let str = "Hello World!";
+let noSpaces = str.replace(/\s/g, "");
+console.log(noSpaces); // Output: HelloWorld!
+let str = "  Hello World!  ";
+let trimmed = str.trim();
+let noSpaces = trimmed.replace(/\s/g, "");
+console.log(noSpaces); // Output: HelloWorld!
