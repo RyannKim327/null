79d3c4a2b08fc42359155cceff82eb30bff6f57d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:14:23 +0800

    1-25-2024 6:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,20 @@
-// Make a GET request to an API endpoint using fetch
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    // Do something with the retrieved data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the request
-    console.log('Error:', error);
-  });
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let key = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = key;
+  }
+
+  return array;
+}
+
+// Usage example:
+const unsortedArray = [6, 3, 8, 1, 9, 2];
+const sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 3, 6, 8, 9]
