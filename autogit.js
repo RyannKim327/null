829569ce168f3370dd5b3213847378ac3126f758
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:14:28 +0800

    1-25-2024 6:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,4 @@
-function insertionSort(array) {
-  for (let i = 1; i < array.length; i++) {
-    let key = array[i];
-    let j = i - 1;
-
-    while (j >= 0 && array[j] > key) {
-      array[j + 1] = array[j];
-      j--;
-    }
-
-    array[j + 1] = key;
-  }
-
-  return array;
-}
-
-// Usage example:
-const unsortedArray = [6, 3, 8, 1, 9, 2];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 6, 8, 9]
+let str = "Hello, World!";
+let reversed = str.split('').reverse().join('');
+console.log(reversed);
+!dlroW ,olleH
