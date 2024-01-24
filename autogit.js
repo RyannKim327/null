798ab626adb1b6d2e5d400e90147aba82ac256b3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:04:29 +0800

    1-25-2024 6:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,51 @@
-function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
 
-  while (left < right) {
-    if (str.charAt(left) !== str.charAt(right)) {
-      return false;
-    }
-
-    left++;
-    right--;
+  while (current) {
+    const next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
 
-  return true;
+  return prev;
+}
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+const head = new Node(1);
+const node2 = new Node(2);
+head.next = node2;
+const node3 = new Node(3);
+node2.next = node3;
+const tail = new Node(4);
+node3.next = tail;
+console.log('Original Linked List:')
+let current = head;
+while (current) {
+  console.log(current.value);
+  current = current.next;
 }
 
-// Example usage
-console.log(isPalindrome('racecar')); // true
-console.log(isPalindrome('hello')); // false
+const reversedHead = reverseLinkedList(head);
+
+console.log('Reversed Linked List:')
+current = reversedHead;
+while (current) {
+  console.log(current.value);
+  current = current.next;
+}
+Original Linked List:
+1
+2
+3
+4
+Reversed Linked List:
+4
+3
+2
+1
