65aca644bd8c6c9b81c1a9eea1bead3ee0636341
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:33:35 +0800

    1-24-2024 20:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,12 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+function getStringLength(str) {
+  let count = 0;
+  while (str[count] !== undefined) {
+    count++;
+  }
+  return count;
+}
+
+// Example usage:
+const myString = "Hello, World!";
+const length = getStringLength(myString);
+console.log(length); // Output: 13
