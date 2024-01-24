c93b5bd0417120ddaea075c3fc04de4666ac63f2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:34:15 +0800

    1-25-2024 5:34 [User mode]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Usage example
-const min = 1;
-const max = 100;
-const randomNumber = getRandomNumber(min, max);
-console.log(randomNumber);
