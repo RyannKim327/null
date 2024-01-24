41b2122c5a0d225fa15798c9c9efa6e90a021573
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:48:32 +0800

    1-24-2024 20:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,42 @@
-// Make a GET request using fetch
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    // Process the response data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.log(error);
-  });
+function LinkedListNode(value) {
+  this.value = value;
+  this.next = null;
+}
 
-// Make a POST request using fetch
-fetch('https://api.example.com/data', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ name: 'John', age: 30 }),
-})
-  .then(response => response.json())
-  .then(data => {
-    // Process the response data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.log(error);
-  });
+function LinkedList() {
+  this.head = null;
+
+  // Method to append a node to the linked list
+  this.append = function (value) {
+    var newNode = new LinkedListNode(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      var current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  };
+
+  // Method to find the length of the linked list
+  this.getLength = function () {
+    var current = this.head;
+    var count = 0;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  };
+}
+
+// Example usage
+var list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+
+console.log(list.getLength()); // Output: 4
