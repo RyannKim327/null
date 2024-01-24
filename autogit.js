c98a23c41f0ed74c85fd5f2a1af24b13f6cdb012
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:28:45 +0800

    1-25-2024 5:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,127 @@
-// Definition for a binary tree node
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
+// Node class
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-// Function to count the number of leaf nodes in a binary tree
-function countLeafNodes(root) {
-  if (!root) {
-    return 0;
+// BinarySearchTree class
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // helper method to insert a node in BST
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // recursively insert a node in BST
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      // insert in the left subtree
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      // insert in the right subtree
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // helper method to perform in-order traversal
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  // recursively perform in-order traversal
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  // helper method to perform pre-order traversal
+  preOrderTraversal(callback) {
+    this.preOrderTraversalNode(this.root, callback);
+  }
+
+  // recursively perform pre-order traversal
+  preOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      callback(node.value);
+      this.preOrderTraversalNode(node.left, callback);
+      this.preOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  // helper method to perform post-order traversal
+  postOrderTraversal(callback) {
+    this.postOrderTraversalNode(this.root, callback);
+  }
+
+  // recursively perform post-order traversal
+  postOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.postOrderTraversalNode(node.left, callback);
+      this.postOrderTraversalNode(node.right, callback);
+      callback(node.value);
+    }
+  }
+
+  // method to search for a node in BST
+  search(value) {
+    return this.searchNode(this.root, value);
   }
   
-  // If the current node is a leaf node, return 1
-  if (!root.left && !root.right) {
-    return 1;
-  } else {
-    // Recursively count the leaf nodes in the left and right subtrees
-    return countLeafNodes(root.left) + countLeafNodes(root.right);
+  // recursively search for a node in BST
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
   }
 }
 
-// Test the function
-const root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(6);
+// Usage example
+const bst = new BinarySearchTree();
+bst.insert(12);
+bst.insert(6);
+bst.insert(15);
+bst.insert(3);
+bst.insert(8);
 
-console.log("Number of leaf nodes:", countLeafNodes(root));
+console.log("In-order traversal:");
+bst.inOrderTraversal((value) => console.log(value));
+
+console.log("Pre-order traversal:");
+bst.preOrderTraversal((value) => console.log(value));
+
+console.log("Post-order traversal:");
+bst.postOrderTraversal((value) => console.log(value));
+
+console.log("Search for value 6:", bst.search(6));
+console.log("Search for value 10:", bst.search(10));
