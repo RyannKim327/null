bd3adb72e380760f4bfcb8071477d152b30951e3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:41:11 +0800

    1-24-2024 20:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,8 @@
-function reverseStringWords(str) {
-  // Step 1: Split the string into an array of words
-  var wordsArray = str.split(' ');
-
-  // Step 2: Reverse the order of array elements
-  var reversedArray = wordsArray.reverse();
-
-  // Step 3: Join the reversed array elements into a string
-  var reversedString = reversedArray.join(' ');
-
-  // Return the reversed string
-  return reversedString;
-}
-
-// Example usage
-var inputString = "Hello World, how are you?";
-var reversedString = reverseStringWords(inputString);
-console.log(reversedString);
-you? are how World, Hello
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
