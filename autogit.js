122e67692abd78945ccb8f4537f93133134b2d30
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:17:07 +0800

    1-24-2024 20:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,34 @@
-const cron = require('node-cron');
+function findMajorityElement(arr) {
+  let count = 0;
+  let majorityElement;
 
-// Define a function to be executed
-const task = () => {
-  console.log('Running task at', new Date());
-};
+  for (let i = 0; i < arr.length; i++) {
+    if (count === 0) {
+      majorityElement = arr[i];
+      count++;
+    } else if (arr[i] === majorityElement) {
+      count++;
+    } else {
+      count--;
+    }
+  }
 
-// Schedule the task to run every minute
-cron.schedule('* * * * *', task);
+  if (count > 0) {
+    // Validating if the majority element occurs more than n/2 times
+    count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === majorityElement) {
+        count++;
+      }
+    }
+    if (count > arr.length / 2) {
+      return majorityElement;
+    }
+  }
+
+  return -1; // No majority element found
+}
+const arr = [2, 4, 3, 4, 4, 2, 4]; // array with a majority element
+
+const majorityElement = findMajorityElement(arr);
+console.log(majorityElement); // Output: 4
