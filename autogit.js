ab44538614ae9baab531a37a9278c96ac7a6dff7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:02:28 +0800

    1-25-2024 6:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,66 @@
 class HashTable {
-  constructor(size) {
+  constructor(size = 100) {
     this.size = size;
-    this.table = new Array(size);
+    this.buckets = Array(this.size);
   }
 
-  // Hash function to convert a key into an index
   hash(key) {
-    let hashValue = 0;
+    let hash = 0;
     for (let i = 0; i < key.length; i++) {
-      hashValue += key.charCodeAt(i);
+      hash += key.charCodeAt(i);
     }
-    return hashValue % this.size;
+    return hash % this.size;
   }
 
-  // Insert a key-value pair into the hash table
-  insert(key, value) {
+  set(key, value) {
     const index = this.hash(key);
-    if (!this.table[index]) {
-      this.table[index] = [];
+    
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
     }
-    this.table[index].push([key, value]);
-  }
 
-  // Get the value associated with a key
-  get(key) {
-    const index = this.hash(key);
-    if (!this.table[index]) {
-      return undefined;
-    }
-    for (let i = 0; i < this.table[index].length; i++) {
-      if (this.table[index][i][0] === key) {
-        return this.table[index][i][1];
+    for (const pair of this.buckets[index]) {
+      if (pair[0] === key) {
+        pair[1] = value;
+        return;
       }
     }
+
+    this.buckets[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (!this.buckets[index]) return undefined;
+
+    for (const pair of this.buckets[index]) {
+      if (pair[0] === key) {
+        return pair[1];
+      }
+    }
+
     return undefined;
   }
 
-  // Remove a key-value pair from the hash table
-  remove(key) {
+  delete(key) {
     const index = this.hash(key);
-    if (!this.table[index]) {
-      return undefined;
-    }
-    for (let i = 0; i < this.table[index].length; i++) {
-      if (this.table[index][i][0] === key) {
-        return this.table[index].splice(i, 1)[0][1];
+    if (!this.buckets[index]) return undefined;
+
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === key) {
+        return this.buckets[index].splice(i, 1)[0][1];
       }
     }
+
     return undefined;
   }
 }
+const table = new HashTable();
+table.set("name", "John");
+table.set("age", 25);
 
-// Usage
-const hashTable = new HashTable(10);
-hashTable.insert("firstName", "John");
-hashTable.insert("lastName", "Doe");
-console.log(hashTable.get("firstName")); // Output: John
-console.log(hashTable.get("lastName")); // Output: Doe
-console.log(hashTable.get("age")); // Output: undefined
-hashTable.remove("lastName");
-console.log(hashTable.get("lastName")); // Output: undefined
+console.log(table.get("name")); // Outputs: "John"
+console.log(table.get("age")); // Outputs: 25
+
+console.log(table.delete("age")); // Outputs: 25
+console.log(table.get("age")); // Outputs: undefined
