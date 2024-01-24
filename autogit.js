0206da19f9af00ba370d93a364c3e14555dada26
RyannKim327 <rksesgundo123@gmail.com>
2024-01-24 20:17:16 +0800

    1-24-2024 20:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,41 @@
-function findMajorityElement(arr) {
-  let count = 0;
-  let majorityElement;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (count === 0) {
-      majorityElement = arr[i];
-      count++;
-    } else if (arr[i] === majorityElement) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  if (count > 0) {
-    // Validating if the majority element occurs more than n/2 times
-    count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === majorityElement) {
-        count++;
-      }
-    }
-    if (count > arr.length / 2) {
-      return majorityElement;
-    }
-  }
-
-  return -1; // No majority element found
+function largestPrimeFactor(number) {
+  // code goes here
 }
-const arr = [2, 4, 3, 4, 4, 2, 4]; // array with a majority element
+function largestPrimeFactor(number) {
+  let factor = 2;
+  let largestFactor = 1;
+  // code continues...
+}
+function largestPrimeFactor(number) {
+  let factor = 2;
+  let largestFactor = 1;
 
-const majorityElement = findMajorityElement(arr);
-console.log(majorityElement); // Output: 4
+  while (number > 1) {
+    if (number % factor === 0) {
+      largestFactor = factor;
+      number /= factor;
+    } else {
+      factor++;
+    }
+  }
+  
+  return largestFactor;
+}
+function largestPrimeFactor(number) {
+  let factor = 2;
+  let largestFactor = 1;
+
+  while (number > 1) {
+    if (number % factor === 0) {
+      largestFactor = factor;
+      number /= factor;
+    } else {
+      factor++;
+    }
+  }
+  
+  return largestFactor;
+}
+
+// Example usage:
+console.log(largestPrimeFactor(13195));  // Output: 29
