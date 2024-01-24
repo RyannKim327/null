3b0df6559d0f69d5a9c8464ddfecbc91ae55fee5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:18:58 +0800

    1-25-2024 5:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,177 +1,35 @@
-class Node {
-  constructor(value, priority) {
-    this.value = value;
-    this.priority = priority;
-  }
-}
-class PriorityQueue {
-  constructor() {
-    this.items = [];
-  }
-}
-getLeftChildIndex(index) {
-  return 2 * index + 1;
-}
+function countingSort(arr) {
+  let min = arr[0];
+  let max = arr[0];
 
-getRightChildIndex(index) {
-  return 2 * index + 2;
-}
-getParentIndex(index) {
-  return Math.floor((index - 1) / 2);
-}
-enqueue(value, priority) {
-  const node = new Node(value, priority);
-  this.items.push(node);
-  this.heapifyUp();
-}
-heapifyUp() {
-  let index = this.items.length - 1;
-  while (index > 0) {
-    const parentIndex = this.getParentIndex(index);
-    if (this.items[index].priority <= this.items[parentIndex].priority) {
-      break;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    this.swap(index, parentIndex);
-    index = parentIndex;
-  }
-}
-swap(index1, index2) {
-  [this.items[index1], this.items[index2]] = [this.items[index2], this.items[index1]];
-}
-dequeue() {
-  if (this.isEmpty()) {
-    return null;
-  }
-  const root = this.items[0];
-  const lastNode = this.items.pop();
-  if (!this.isEmpty()) {
-    this.items[0] = lastNode;
-    this.heapifyDown();
-  }
-  return root.value;
-}
-heapifyDown() {
-  let index = 0;
-  while (this.getLeftChildIndex(index) < this.items.length) {
-    const leftChildIndex = this.getLeftChildIndex(index);
-    const rightChildIndex = this.getRightChildIndex(index);
-    const biggerChildIndex = this.getHigherPriorityChildIndex(
-      leftChildIndex,
-      rightChildIndex
-    );
-    if (this.items[index].priority >= this.items[biggerChildIndex].priority) {
-      break;
-    }
-    this.swap(index, biggerChildIndex);
-    index = biggerChildIndex;
-  }
-}
-getHigherPriorityChildIndex(leftChildIndex, rightChildIndex) {
-  if (
-    rightChildIndex >= this.items.length ||
-    this.items[leftChildIndex].priority >= this.items[rightChildIndex].priority
-  ) {
-    return leftChildIndex;
-  } else {
-    return rightChildIndex;
-  }
-}
-isEmpty() {
-  return this.items.length === 0;
-}
-class Node {
-  constructor(value, priority) {
-    this.value = value;
-    this.priority = priority;
-  }
-}
-
-class PriorityQueue {
-  constructor() {
-    this.items = [];
-  }
-
-  getLeftChildIndex(index) {
-    return 2 * index + 1;
-  }
-
-  getRightChildIndex(index) {
-    return 2 * index + 2;
-  }
-
-  getParentIndex(index) {
-    return Math.floor((index - 1) / 2);
-  }
-
-  enqueue(value, priority) {
-    const node = new Node(value, priority);
-    this.items.push(node);
-    this.heapifyUp();
-  }
-
-  heapifyUp() {
-    let index = this.items.length - 1;
-    while (index > 0) {
-      const parentIndex = this.getParentIndex(index);
-      if (this.items[index].priority <= this.items[parentIndex].priority) {
-        break;
-      }
-      this.swap(index, parentIndex);
-      index = parentIndex;
+    if (arr[i] > max) {
+      max = arr[i];
     }
   }
 
-  swap(index1, index2) {
-    [this.items[index1], this.items[index2]] = [
-      this.items[index2],
-      this.items[index1],
-    ];
+  const countArray = new Array(max - min + 1).fill(0);
+
+  for (let i = 0; i < arr.length; i++) {
+    countArray[arr[i] - min]++;
   }
 
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    const root = this.items[0];
-    const lastNode = this.items.pop();
-    if (!this.isEmpty()) {
-      this.items[0] = lastNode;
-      this.heapifyDown();
-    }
-    return root.value;
+  for (let i = 1; i < countArray.length; i++) {
+    countArray[i] += countArray[i - 1];
   }
 
-  heapifyDown() {
-    let index = 0;
-    while (this.getLeftChildIndex(index) < this.items.length) {
-      const leftChildIndex = this.getLeftChildIndex(index);
-      const rightChildIndex = this.getRightChildIndex(index);
-      const biggerChildIndex = this.getHigherPriorityChildIndex(
-        leftChildIndex,
-        rightChildIndex
-      );
-      if (
-        this.items[index].priority >= this.items[biggerChildIndex].priority
-      ) {
-        break;
-      }
-      this.swap(index, biggerChildIndex);
-      index = biggerChildIndex;
-    }
+  const sortedArray = new Array(arr.length);
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    sortedArray[countArray[arr[i] - min] - 1] = arr[i];
+    countArray[arr[i] - min]--;
   }
 
-  getHigherPriorityChildIndex(leftChildIndex, rightChildIndex) {
-    if (
-      rightChildIndex >= this.items.length ||
-      this.items[leftChildIndex].priority >= this.items[rightChildIndex].priority
-    ) {
-      return leftChildIndex;
-    } else {
-      return rightChildIndex;
-    }
-  }
-
-  isEmpty() {
-    return this.items.length === 0;
-  }
+  return sortedArray;
 }
+const array = [5, 3, 2, 6, 1, 4];
+const sortedArray = countingSort(array);
+console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6]
