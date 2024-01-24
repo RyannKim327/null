40e063c67ab67c94381e528143168b5a1cbc1412
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 06:07:22 +0800

    1-25-2024 6:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,33 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  var swapped;
-
-  do {
-    swapped = false;
-    for (var i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
+function findMajorityElement(nums) {
+  let majorityElement = nums[0];
+  let count = 1;
+  
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      majorityElement = nums[i];
+      count = 1;
+    } else if (nums[i] === majorityElement) {
+      count++;
+    } else {
+      count--;
     }
-  } while (swapped);
-
-  return arr;
+  }
+  
+  // Verify if the found majority element occurs more than n/2 times
+  count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === majorityElement) {
+      count++;
+    }
+  }
+  if (count > nums.length / 2) {
+    return majorityElement;
+  }
+  
+  return null; // No majority element found
 }
 
-// Example usage
-var arr = [5, 3, 8, 1, 2];
-console.log("Original array:", arr); // [5, 3, 8, 1, 2]
-console.log("Sorted array:", bubbleSort(arr)); // [1, 2, 3, 5, 8]
+// Example usage:
+const nums = [2, 3, 4, 2, 2, 2, 5];
+const majorityElement = findMajorityElement(nums);
+console.log("Majority Element:", majorityElement);
