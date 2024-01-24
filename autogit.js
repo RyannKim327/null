eebabcdcc4e27c78d21f7fe52201f73dad978cc3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:34:52 +0800

    1-25-2024 5:34 [User mode]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-// Define an array of integers
-const numbers = [5, 2, 8, 1, 4];
-
-// Sort the array in ascending order
-numbers.sort(function(a, b) {
-  return a - b;
-});
-
-console.log(numbers); // Output: [1, 2, 4, 5, 8]
