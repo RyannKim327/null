583314d5bc134f310358080d3770fb041036b462
RyannKim327 <rksesgundo123@gmail.com>
2024-01-25 05:24:06 +0800

    1-25-2024 5:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,11 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
+function removeVowels(str) {
+  // Use the replace() method with a regular expression to remove vowels
+  var removed = str.replace(/[aeiou]/gi, '');
 
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const less = [];
-  const equal = [];
-  const greater = [];
-
-  for (const element of arr) {
-    if (element < pivot) {
-      less.push(element);
-    } else if (element === pivot) {
-      equal.push(element);
-    } else {
-      greater.push(element);
-    }
-  }
-
-  return quickSort(less).concat(equal, quickSort(greater));
+  return removed;
 }
 
-// Example usage:
-const unsortedArray = [5, 2, 6, 1, 3, 9, 4];
-const sortedArray = quickSort(unsortedArray);
-
-console.log(sortedArray);  // output: [1, 2, 3, 4, 5, 6, 9]
+var myString = "Hello, World!"; // Example string
+var result = removeVowels(myString);
+console.log(result);
+Hll, Wrld!
