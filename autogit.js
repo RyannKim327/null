aa8cdede66c30f7ac5baacfd3d900d57e3883fe1
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:58:01 +0800

    1-26-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,26 @@
-function burrowsWheelerTransform(str) {
-  var rotations = [];
-  var len = str.length;
+function binarySearch(arr, target, startIndex = 0) {
+  let endIndex = arr.length - 1;
 
-  // Create all rotations of the input string
-  for (var i = 0; i < len; i++) {
-    rotations.push(str);
-    str = str.slice(1) + str[0];
+  while (startIndex <= endIndex) {
+    const midIndex = Math.floor((startIndex + endIndex) / 2);
+
+    if (arr[midIndex] === target) {
+      return midIndex;
+    }
+
+    if (arr[midIndex] < target) {
+      startIndex = midIndex + 1;
+    } else {
+      endIndex = midIndex - 1;
+    }
   }
-
-  // Sort the rotations lexicographically
-  rotations.sort();
-
-  // Extract the last characters of each rotation
-  var transformedStr = '';
-  for (var j = 0; j < len; j++) {
-    transformedStr += rotations[j][len - 1];
-  }
-
-  return transformedStr;
+  
+  return -1;
 }
-var inputStr = 'banana';
-var transformedStr = burrowsWheelerTransform(inputStr);
-console.log(transformedStr);  // Outputs: 'annb$a'
+
+// Example usage:
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const target = 6;
+const index = binarySearch(arr, target);
+
+console.log(index); // Output: 5 (index of the target element in the array)
