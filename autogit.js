9e0bf88d697011dc43427389e3c42e2923278b47
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 06:05:41 +0800

    1-26-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,3 @@
-function radixSort(arr) {
-  const maxNum = Math.max(...arr);
-  const maxDigits = String(maxNum).length;
-  
-  for (let i = 0; i < maxDigits; i++) {
-    const buckets = Array.from({ length: 10 }, () => []);
-    
-    for (let j = 0; j < arr.length; j++) {
-      const digit = Math.floor(arr[j] / Math.pow(10, i)) % 10;
-      buckets[digit].push(arr[j]);
-    }
-    
-    arr = [].concat(...buckets);
-  }
-  
-  return arr;
-}
-
-// Example usage:
-const unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
-const sortedArray = radixSort(unsortedArray);
-console.log(sortedArray);
+const str = "Hello, World!";
+const reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
