6b9817af1eae304bcd9c1bdfb968301592d1ef58
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:59:22 +0800

    1-26-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-const array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
-const array = [1, 2, 3, 4, 5];
+let str = '  Hello,  world!  ';
+let trimmedStr = str.replace(/\s/g, '');
 
-for (let i = 0; i < Math.floor(array.length / 2); i++) {
-  const temp = array[i];
-  array[i] = array[array.length - 1 - i];
-  array[array.length - 1 - i] = temp;
-}
-
-console.log(array); // Output: [5, 4, 3, 2, 1]
-const array = [1, 2, 3, 4, 5];
-const reversedArray = [...array].reverse();
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+console.log(trimmedStr); // Output: "Hello,world!"
