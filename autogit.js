f0b6ca0e093be36ab01d1dbf067d89464530526d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:52:11 +0800

    1-26-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,114 @@
-function hasCycle(head) {
-  let slow = head; // Slow pointer moves one node at a time
-  let fast = head; // Fast pointer moves two nodes at a time
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-  while (fast !== null && fast.next !== null) {
-    // Move slow pointer one step
-    slow = slow.next;
-
-    // Move fast pointer two steps
-    fast = fast.next.next;
-
-    // If slow and fast pointers meet, there is a cycle
-    if (slow === fast) {
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+  
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+  
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.removeNode(node.right, minNode.value);
+      return node;
+    }
+  }
+  
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+  
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+  
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    } else if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
       return true;
     }
   }
-
-  // If fast pointer reaches the end (null), no cycle exists
-  return false;
 }
+const tree = new BinarySearchTree();
+
+tree.insert(8);
+tree.insert(3);
+tree.insert(10);
+tree.insert(1);
+tree.insert(6);
+tree.insert(14);
+tree.insert(4);
+tree.insert(7);
+tree.insert(13);
+
+console.log(tree.search(6)); // Output: true
+console.log(tree.search(12)); // Output: false
+
+tree.remove(3);
+console.log(tree.search(3)); // Output: false
