6c2ef3322a3bd097a2fe7debcd6b2e5033b4fc81
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 06:04:55 +0800

    1-26-2024 6:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,23 @@
-function burrowsWheelerTransform(input) {
-  // Add EOF (End of File) marker to the input
-  const text = input + '\0';
-
-  // Create an array to store rotations of the input
-  const rotations = [];
-  
-  // Generate all rotations of the input
-  for (let i = 0; i < text.length; i++) {
-    rotations.push(text.slice(i) + text.slice(0, i));
+function factorialLoop(num) {
+  if (num === 0 || num === 1) {
+    return 1;
   }
 
-  // Sort the rotations lexicographically
-  rotations.sort();
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
 
-  // Extract the last characters of each rotation
-  const transformed = rotations.map((rotation) => rotation.slice(-1));
-
-  // Find the original position/index of the input in the sorted rotations
-  const index = rotations.indexOf(text);
-
-  // Return the transformed string and the index
-  return [transformed.join(''), index];
+  return result;
 }
-const input = 'your_input_string';
-const [transformed, index] = burrowsWheelerTransform(input);
 
-console.log('Transformed: ', transformed);
-console.log('Original Index: ', index);
+console.log(factorialLoop(5)); // Output: 120
+function factorialRecursive(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  
+  return num * factorialRecursive(num - 1);
+}
+
+console.log(factorialRecursive(5)); // Output: 120
