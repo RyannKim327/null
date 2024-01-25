1f3350975b62d4571fe7bd97c95bc8d6f8c05acc
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:57:51 +0800

    1-26-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,24 @@
-class Graph {
-  constructor() {
-    this.vertices = [];
-    this.adjList = new Map();
+function burrowsWheelerTransform(str) {
+  var rotations = [];
+  var len = str.length;
+
+  // Create all rotations of the input string
+  for (var i = 0; i < len; i++) {
+    rotations.push(str);
+    str = str.slice(1) + str[0];
   }
 
-  addVertex(v) {
-    this.vertices.push(v);
-    this.adjList.set(v, []);
+  // Sort the rotations lexicographically
+  rotations.sort();
+
+  // Extract the last characters of each rotation
+  var transformedStr = '';
+  for (var j = 0; j < len; j++) {
+    transformedStr += rotations[j][len - 1];
   }
 
-  addEdge(v, w) {
-    this.adjList.get(v).push(w);
-    this.adjList.get(w).push(v);
-  }
+  return transformedStr;
 }
-function dfs(graph, startVertex, callback) {
-  const visited = new Set();
-
-  function dfsRecursive(vertex) {
-    visited.add(vertex);
-    callback(vertex);
-
-    const neighbors = graph.adjList.get(vertex);
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        dfsRecursive(neighbor);
-      }
-    }
-  }
-
-  dfsRecursive(startVertex);
-}
-// Create a new graph
-const graph = new Graph();
-
-// Add vertices
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
-
-// Add edges
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'D');
-graph.addEdge('B', 'C');
-graph.addEdge('D', 'E');
-graph.addEdge('E', 'F');
-
-// Perform depth-first search starting from vertex 'A'
-dfs(graph, 'A', (vertex) => console.log(vertex));
+var inputStr = 'banana';
+var transformedStr = burrowsWheelerTransform(inputStr);
+console.log(transformedStr);  // Outputs: 'annb$a'
