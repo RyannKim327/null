a42f9fcdec43eb113744fb529a93c878f2d47060
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:51:28 +0800

    1-26-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,48 @@
-function calculateFibonacci(n) {
-  if (n <= 1) {
-    return n;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.neighbors = [];
   }
-  return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
 }
-function calculateFibonacci(n) {
-  if (n <= 1) {
-    return n;
-  }
-  return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
-}
-
-function fibonacciSearch(arr, key, n) {
-  let offset = 0;
-  let i = 0;
-  let fibMinus2 = 0;
-  let fibMinus1 = 1;
-  let fib = fibMinus2 + fibMinus1;
-
-  while (fib < n) {
-    fibMinus2 = fibMinus1;
-    fibMinus1 = fib;
-    fib = fibMinus2 + fibMinus1;
-    i++;
-  }
-
-  while (fibMinus2 > 0) {
-    const index = Math.min(offset + fibMinus2, n - 1);
-
-    if (arr[index] > key) {
-      fib = fibMinus2;
-      fibMinus2 = fibMinus1 - fibMinus2;
-      fibMinus1 = fib - fibMinus2;
-      offset = index;
-    } else if (arr[index] < key) {
-      fib = fibMinus1;
-      fibMinus1 = fibMinus2;
-      fibMinus2 = fib - fibMinus1;
-    } else {
-      return index; // Found at index
+function breadthFirstSearch(startNode) {
+  // Create a visited set to keep track of visited nodes
+  const visited = new Set();
+  
+  // Create a queue to store nodes for traversal
+  const queue = [startNode];
+  
+  // Mark the start node as visited
+  visited.add(startNode);
+  
+  // Process nodes in the queue
+  while (queue.length > 0) {
+    // Remove the first node from the queue
+    const currentNode = queue.shift();
+    
+    // Process the current node (you can do whatever you need to do with it)
+    console.log(currentNode.value);
+    
+    // Iterate over the neighbors of the current node
+    for (const neighbor of currentNode.neighbors) {
+      // If the neighbor has not been visited, mark it as visited and enqueue it
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push(neighbor);
+      }
     }
   }
-
-  return -1; // Not found
 }
-const arr = [1, 3, 5, 7, 11, 13, 17, 19];
-const key = 11;
-const n = arr.length;
+// Create nodes
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
 
-const index = fibonacciSearch(arr, key, n);
+// Create connections between nodes
+nodeA.neighbors.push(nodeB, nodeC);
+nodeB.neighbors.push(nodeD, nodeE);
+nodeC.neighbors.push(nodeE);
 
-if (index !== -1) {
-  console.log(`Key ${key} found at index ${index}`);
-} else {
-  console.log(`Key ${key} not found`);
-}
+// Call the breadth-first search algorithm
+breadthFirstSearch(nodeA);
