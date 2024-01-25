00af08a5f6d2920243b1e61a03c2364baeca59de
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:51:49 +0800

    1-26-2024 5:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,58 @@
-let string1 = "Hello";
-let string2 = "World";
+function tarjanSCC(graph) {
+  const stack = [];
+  let index = 0;
+  const components = [];
 
-let result = string1 + " " + string2;
-console.log(result); // Output: Hello World
-let string1 = "Hello";
-let string2 = "World";
+  function tarjanDFS(node) {
+    node.index = index;
+    node.lowLink = index;
+    index++;
+    stack.push(node);
+    node.onStack = true;
 
-let result = string1.concat(" ", string2);
-console.log(result); // Output: Hello World
+    for (const neighbor of graph[node.id]) {
+      if (neighbor.index === undefined) {
+        tarjanDFS(neighbor);
+        node.lowLink = Math.min(node.lowLink, neighbor.lowLink);
+      } else if (neighbor.onStack) {
+        node.lowLink = Math.min(node.lowLink, neighbor.index);
+      }
+    }
+
+    if (node.lowLink === node.index) {
+      const component = [];
+      let neighbor;
+      
+      do {
+        neighbor = stack.pop();
+        neighbor.onStack = false;
+        component.push(neighbor);
+      } while (neighbor !== node);
+
+      components.push(component);
+    }
+  }
+
+  for (const node of Object.values(graph)) {
+    if (node.index === undefined) {
+      tarjanDFS(node);
+    }
+  }
+
+  return components;
+}
+
+// Example usage:
+const graph = {
+  A: ['B'],
+  B: ['C', 'E', 'F'],
+  C: ['D', 'G'],
+  D: ['C', 'H'],
+  E: ['A', 'F'],
+  F: ['G'],
+  G: ['F'],
+  H: ['D', 'G'],
+};
+
+const stronglyConnectedComponents = tarjanSCC(graph);
+console.log(stronglyConnectedComponents);
