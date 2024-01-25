cd05d52c06c1fe9cb19cbeafdff8b479b40f0371
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:59:16 +0800

    1-26-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,15 @@
-class Node {
-  constructor() {
-    this.keys = [];
-    this.children = [];
-  }
+const array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output: [5, 4, 3, 2, 1]
+const array = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < Math.floor(array.length / 2); i++) {
+  const temp = array[i];
+  array[i] = array[array.length - 1 - i];
+  array[array.length - 1 - i] = temp;
 }
-class BTree {
-  constructor(order) {
-    this.root = new Node();
-    this.order = order;
-  }
 
-  // Other methods here (insert, search, delete)
-}
-class BTree {
-  // ...
-
-  insert(key) {
-    const node = this.root;
-    if (node.keys.length === (2 * this.order) - 1) {
-      const newNode = new Node();
-      this.root = newNode;
-      newNode.children.push(node);
-      this.splitChild(newNode, 0);
-      this.insertNonFull(newNode, key);
-    } else {
-      this.insertNonFull(node, key);
-    }
-  }
-
-  // Utility methods here (splitChild, insertNonFull)
-}
-class BTree {
-  // ...
-
-  splitChild(parent, index) {
-    const node = parent.children[index];
-    const newNode = new Node();
-    parent.keys.splice(index, 0, node.keys[this.order - 1]);
-    parent.children.splice(index + 1, 0, newNode);
-    newNode.keys = node.keys.splice(this.order, this.order - 1);
-    if (node.children.length) {
-      newNode.children = node.children.splice(this.order, this.order);
-    }
-  }
-
-  insertNonFull(node, key) {
-    let i = node.keys.length - 1;
-    if (!node.children.length) {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      node.keys.splice(i + 1, 0, key);
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      i++;
-      if (node.children[i].keys.length === (2 * this.order) - 1) {
-        this.splitChild(node, i);
-        if (key > node.keys[i]) {
-          i++;
-        }
-      }
-      this.insertNonFull(node.children[i], key);
-    }
-  }
-}
+console.log(array); // Output: [5, 4, 3, 2, 1]
+const array = [1, 2, 3, 4, 5];
+const reversedArray = [...array].reverse();
+console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
