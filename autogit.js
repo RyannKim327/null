e2de77bd5f5c7fe0d5cfc2689294e4e1c7dd229a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:52:17 +0800

    1-26-2024 5:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,114 +1,12 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+const axios = require('axios');
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-  
-  insert(value) {
-    const newNode = new Node(value);
-    
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-  
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-  
-  remove(value) {
-    this.root = this.removeNode(this.root, value);
-  }
-  
-  removeNode(node, value) {
-    if (node === null) {
-      return null;
-    } else if (value < node.value) {
-      node.left = this.removeNode(node.left, value);
-      return node;
-    } else if (value > node.value) {
-      node.right = this.removeNode(node.right, value);
-      return node;
-    } else {
-      if (node.left === null && node.right === null) {
-        node = null;
-        return node;
-      }
-      
-      if (node.left === null) {
-        node = node.right;
-        return node;
-      } else if (node.right === null) {
-        node = node.left;
-        return node;
-      }
-      
-      const minNode = this.findMinNode(node.right);
-      node.value = minNode.value;
-      node.right = this.removeNode(node.right, minNode.value);
-      return node;
-    }
-  }
-  
-  findMinNode(node) {
-    if (node.left === null) {
-      return node;
-    } else {
-      return this.findMinNode(node.left);
-    }
-  }
-  
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-  
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    } else if (value < node.value) {
-      return this.searchNode(node.left, value);
-    } else if (value > node.value) {
-      return this.searchNode(node.right, value);
-    } else {
-      return true;
-    }
-  }
-}
-const tree = new BinarySearchTree();
-
-tree.insert(8);
-tree.insert(3);
-tree.insert(10);
-tree.insert(1);
-tree.insert(6);
-tree.insert(14);
-tree.insert(4);
-tree.insert(7);
-tree.insert(13);
-
-console.log(tree.search(6)); // Output: true
-console.log(tree.search(12)); // Output: false
-
-tree.remove(3);
-console.log(tree.search(3)); // Output: false
+// Make a GET request to a JSON placeholder API
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    // Handle the response data
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
