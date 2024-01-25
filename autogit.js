be504b19872c4c1aa91c207348bf8cda29921706
RyannKim327 <rksesgundo123@gmail.com>
2024-01-26 05:57:43 +0800

    1-26-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,53 @@
-function randomSort(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+class Graph {
+  constructor() {
+    this.vertices = [];
+    this.adjList = new Map();
   }
-  return arr;
-}
 
-// Example usage
-const myArray = [1, 2, 3, 4, 5];
-console.log("Original Array:", myArray);
-console.log("Randomly Sorted Array:", randomSort(myArray));
+  addVertex(v) {
+    this.vertices.push(v);
+    this.adjList.set(v, []);
+  }
+
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+    this.adjList.get(w).push(v);
+  }
+}
+function dfs(graph, startVertex, callback) {
+  const visited = new Set();
+
+  function dfsRecursive(vertex) {
+    visited.add(vertex);
+    callback(vertex);
+
+    const neighbors = graph.adjList.get(vertex);
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        dfsRecursive(neighbor);
+      }
+    }
+  }
+
+  dfsRecursive(startVertex);
+}
+// Create a new graph
+const graph = new Graph();
+
+// Add vertices
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+// Add edges
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'D');
+graph.addEdge('B', 'C');
+graph.addEdge('D', 'E');
+graph.addEdge('E', 'F');
+
+// Perform depth-first search starting from vertex 'A'
+dfs(graph, 'A', (vertex) => console.log(vertex));
