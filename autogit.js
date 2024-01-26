d0eabcfa4f51c9b8d59b514b785c57f2f74de4a9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:58:27 +0800

    1-27-2024 6:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,28 @@
-function mergeSortIterative(arr) {
-  var mergeSize = 1;
-  var arrLength = arr.length;
+function beamSearch(initialState, beamWidth, maxDepth) {
+  let beam = [{ state: initialState, score: 0 }];
   
-  function merge(arr, left, mid, right) {
-    var leftArr = arr.slice(left, mid + 1);
-    var rightArr = arr.slice(mid + 1, right + 1);
-
-    var i = 0, j = 0, k = left;
-  
-    while (i < leftArr.length && j < rightArr.length) {
-      if (leftArr[i] <= rightArr[j]) {
-        arr[k] = leftArr[i];
-        i++;
-      } else {
-        arr[k] = rightArr[j];
-        j++;
+  for (let depth = 0; depth < maxDepth; depth++) {
+    let newBeam = [];
+    
+    for (let i = 0; i < beam.length; i++) {
+      let { state, score } = beam[i];
+      
+      // Generate possible next states from current state
+      let nextStates = generateNextStates(state);
+      
+      for (let j = 0; j < nextStates.length; j++) {
+        let nextState = nextStates[j];
+        let nextScore = scoreFunction(nextState);
+        newBeam.push({ state: nextState, score: nextScore });
       }
-      k++;
     }
-  
-    while (i < leftArr.length) {
-      arr[k] = leftArr[i];
-      i++;
-      k++;
-    }
-  
-    while (j < rightArr.length) {
-      arr[k] = rightArr[j];
-      j++;
-      k++;
-    }
+    
+    // Sort the new beam by score
+    newBeam.sort((a, b) => b.score - a.score);
+    
+    // Truncate the beam to the top-scoring solutions
+    beam = newBeam.slice(0, beamWidth);
   }
   
-  while (mergeSize < arrLength) {
-    for (var start = 0; start < arrLength - mergeSize; start += 2 * mergeSize) {
-      var mid = start + mergeSize - 1;
-      var end = Math.min(start + 2 * mergeSize - 1, arrLength - 1);
-      merge(arr, start, mid, end);
-    }
-    mergeSize *= 2;
-  }
-  
-  return arr;
+  return beam[0].state; // Return the best solution found
 }
-
-// Example usage:
-var array = [5, 8, 1, 6, 2, 9, 3];
-console.log("Original Array: ", array);
-console.log("Sorted Array: ", mergeSortIterative(array));
