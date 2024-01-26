4ccc17b3dc0379b8953323a9bf896b01dd0b88ec
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:59:38 +0800

    1-27-2024 6:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,132 +1,41 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
 }
+function findNthFromEnd(head, n) {
+  // Two pointers to track the nth node from the end and the end of the list
+  let p1 = head;
+  let p2 = head;
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(value) {
-    const newNode = new Node(value);
-
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    }
-
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    } else if (value > node.value) {
-      return this.searchNode(node.right, value);
-    } else {
-      return true;
-    }
-  }
-
-  remove(value) {
-    this.root = this.removeNode(this.root, value);
-  }
-
-  removeNode(node, key) {
-    if (node === null) {
+  // Move p2 n positions ahead
+  for (let i = 0; i < n; i++) {
+    if (p2 === null) {
+      // If the list has fewer than n nodes, return null
       return null;
     }
-
-    if (key < node.value) {
-      node.left = this.removeNode(node.left, key);
-      return node;
-    } else if (key > node.value) {
-      node.right = this.removeNode(node.right, key);
-      return node;
-    } else {
-      if (node.left === null && node.right === null) {
-        node = null;
-        return node;
-      }
-
-      if (node.left === null) {
-        node = node.right;
-        return node;
-      } else if (node.right === null) {
-        node = node.left;
-        return node;
-      }
-
-      const aux = this.findMinNode(node.right);
-      node.value = aux.value;
-      node.right = this.removeNode(node.right, aux.value);
-      return node;
-    }
+    p2 = p2.next;
   }
 
-  findMinNode(node) {
-    if (node.left === null) {
-      return node;
-    } else {
-      return this.findMinNode(node.left);
-    }
+  // Move both pointers until p2 reaches the end of the list
+  while (p2 !== null) {
+    p1 = p1.next;
+    p2 = p2.next;
   }
 
-  inorderTraversal(callback) {
-    this.inorderTraversalNode(this.root, callback);
-  }
-
-  inorderTraversalNode(node, callback) {
-    if (node !== null) {
-      this.inorderTraversalNode(node.left, callback);
-      callback(node.value);
-      this.inorderTraversalNode(node.right, callback);
-    }
-  }
+  // p1 will be pointing to the nth node from the end
+  return p1;
 }
-const bst = new BinarySearchTree();
-bst.insert(8);
-bst.insert(3);
-bst.insert(10);
-bst.insert(1);
-bst.insert(6);
-bst.insert(14);
-bst.insert(4);
-bst.insert(7);
-bst.insert(13);
+// Create a sample linked list 1 -> 2 -> 3 -> 4 -> 5
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
 
-console.log(bst.search(6)); // Output: true
-console.log(bst.search(11)); // Output: false
+// Call the function to find the 2nd node from the end
+const n = 2;
+const nthNodeFromEnd = findNthFromEnd(head, n);
 
-bst.remove(6);
-console.log(bst.search(6)); // Output: false
-
-bst.inorderTraversal((value) => console.log(value));
-// Output: 1, 3, 4, 7, 8, 10, 13, 14
+console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.val}`);
