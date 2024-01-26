6e235b37e0347e58acedc3d70432fff4d98bc18d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:06:12 +0800

    1-27-2024 7:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,59 @@
-let str = "   Hello,    World!   ";
+function binarySearch(arr, target) {
 
-// Remove whitespace using regular expression
-let newStr = str.replace(/\s/g, "");
-console.log(newStr);
-"Hello,World!"
-let str = "   Hello,    World!   ";
+}
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+}
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
 
-// Remove leading and trailing whitespace
-let newStr = str.trim();
-console.log(newStr);
-"Hello,    World!"
+  while (start <= end) {
+
+  }
+}
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+  }
+}
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1; // Return -1 if the target is not found
+}
+function binarySearch(arr, target) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  return -1;
+}
