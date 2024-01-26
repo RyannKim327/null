409b196e368d97fe7f6ca628cb62787ff83d3cfa
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:08:45 +0800

    1-27-2024 7:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,106 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.height = 1;
-  }
-}
-class AVLTree {
-  constructor() {
-    this.root = null;
-  }
+function largestPrimeFactor(number) {
+  let factor = 2;
 
-  getHeight(node) {
-    if (node === null) {
-      return 0;
-    }
-    return node.height;
-  }
-
-  getBalanceFactor(node) {
-    if (node === null) {
-      return 0;
-    }
-    return this.getHeight(node.left) - this.getHeight(node.right);
-  }
-
-  updateHeight(node) {
-    if (node === null) {
-      return;
-    }
-    node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-  }
-
-  rotateRight(y) {
-    const x = y.left;
-    const T2 = x.right;
-
-    x.right = y;
-    y.left = T2;
-
-    this.updateHeight(y);
-    this.updateHeight(x);
-
-    return x;
-  }
-
-  rotateLeft(x) {
-    const y = x.right;
-    const T2 = y.left;
-
-    y.left = x;
-    x.right = T2;
-
-    this.updateHeight(x);
-    this.updateHeight(y);
-
-    return y;
-  }
-
-  insert(node, value) {
-    if (node === null) {
-      return new Node(value);
-    }
-
-    if (value < node.value) {
-      node.left = this.insert(node.left, value);
+  while (factor <= number) {
+    if (number % factor === 0) {
+      number /= factor;
     } else {
-      node.right = this.insert(node.right, value);
+      factor++;
     }
-
-    this.updateHeight(node);
-
-    const balanceFactor = this.getBalanceFactor(node);
-
-    if (balanceFactor > 1) {
-      if (value < node.left.value) {
-        return this.rotateRight(node);
-      } else {
-        node.left = this.rotateLeft(node.left);
-        return this.rotateRight(node);
-      }
-    }
-
-    if (balanceFactor < -1) {
-      if (value > node.right.value) {
-        return this.rotateLeft(node);
-      } else {
-        node.right = this.rotateRight(node.right);
-        return this.rotateLeft(node);
-      }
-    }
-
-    return node;
   }
 
-  insert(value) {
-    this.root = this.insert(this.root, value);
-  }
-
-  // Implement other methods like delete and search here...
+  return factor;
 }
-const avlTree = new AVLTree();
-avlTree.insert(10);
-avlTree.insert(20);
-avlTree.insert(30);
+
+// Example usage
+const number = 123456789; // The number you want to find the largest prime factor of
+const largestFactor = largestPrimeFactor(number);
+console.log("Largest Prime Factor: " + largestFactor);
