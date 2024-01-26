a0be78593b3de4274d98fb22cd3d03924942f88d
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:49:16 +0800

    1-27-2024 6:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,25 @@
-const str = "Hello, World!";
-const reversedStr = str.split("").reverse().join("");
-console.log(reversedStr);
+const axios = require('axios');
+
+// Make a GET request to an API endpoint
+axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+  // Handle success
+  console.log('Response data:', response.data);
+}).catch(error => {
+  // Handle error
+  console.error('Error:', error.message);
+});
+
+// Make a POST request to an API endpoint
+const data = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1,
+};
+
+axios.post('https://jsonplaceholder.typicode.com/posts', data).then(response => {
+  // Handle success
+  console.log('Created new post:', response.data);
+}).catch(error => {
+  // Handle error
+  console.error('Error:', error.message);
+});
