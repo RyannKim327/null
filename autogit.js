7446233abfa965dcad0ccd57e451dab8b48ddd1f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:09:34 +0800

    1-27-2024 7:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,69 +1,9 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+let array = [1, 2, 3, 4, 5];
+array.reverse();
 
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
+console.log(array); // Output: [5, 4, 3, 2, 1]
+let array = [1, 2, 3, 4, 5];
+let reversedArray = array.slice().reverse();
 
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.size === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
-
-  dequeue() {
-    if (this.size === 0) {
-      throw new Error('Queue is empty');
-    }
-    const value = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-    if (this.size === 0) {
-      this.tail = null;
-    }
-    return value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  peek() {
-    if (this.size === 0) {
-      throw new Error('Queue is empty');
-    }
-    return this.head.value;
-  }
-}
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.getSize()); // Output: 3
-console.log(queue.peek()); // Output: 1
-
-console.log(queue.dequeue()); // Output: 1
-
-console.log(queue.getSize()); // Output: 2
-console.log(queue.peek()); // Output: 2
-
-console.log(queue.isEmpty()); // Output: false
+console.log(array); // Output: [1, 2, 3, 4, 5]
+console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
