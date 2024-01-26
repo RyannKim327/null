230a52742f00278dce879698ab3d36dc19751ac3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:07:28 +0800

    1-27-2024 7:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,42 @@
-const array = [2, 8, 5, 1, 7];
-const max = Math.max(...array);
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+  
+  push(element) {
+    this.data.push(element);
+  }
+  
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.data.pop();
+  }
+  
+  peek() {
+    if (this.isEmpty()) {
+      return "Stack is empty";
+    }
+    return this.data[this.data.length - 1];
+  }
+  
+  isEmpty() {
+    return this.data.length === 0;
+  }
+  
+  size() {
+    return this.data.length;
+  }
+}
+const stack = new Stack();
 
-console.log(max); // Output: 8
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.pop());   // Output: 30
+console.log(stack.pop());   // Output: 20
+console.log(stack.peek());  // Output: 10
+console.log(stack.size());  // Output: 1
+console.log(stack.isEmpty());// Output: false
