4805ddfeb9f84a93a4474fc02ffcc7ba7266537f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:59:29 +0800

    1-27-2024 6:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,132 @@
-function findMajorityElement(arr) {
-  const hashMap = new Map();
-
-  // Count the frequency of each element
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    if (hashMap.has(element)) {
-      hashMap.set(element, hashMap.get(element) + 1);
-    } else {
-      hashMap.set(element, 1);
-    }
-  }
-
-  // Find the element with maximum frequency
-  let majorityElement, maxFrequency = 0;
-  hashMap.forEach((frequency, element) => {
-    if (frequency > maxFrequency) {
-      majorityElement = element;
-      maxFrequency = frequency;
-    }
-  });
-
-  // Check if the maximum frequency is a majority
-  if (maxFrequency > arr.length / 2) {
-    return majorityElement;
-  } else {
-    return null; // No majority element
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-// Example usage
-const nums = [1, 2, 2, 3, 2, 3, 2];
-const majorityElement = findMajorityElement(nums);
-console.log(majorityElement);  // Output: 2
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, key) {
+    if (node === null) {
+      return null;
+    }
+
+    if (key < node.value) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.value) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const aux = this.findMinNode(node.right);
+      node.value = aux.value;
+      node.right = this.removeNode(node.right, aux.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  inorderTraversal(callback) {
+    this.inorderTraversalNode(this.root, callback);
+  }
+
+  inorderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inorderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inorderTraversalNode(node.right, callback);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log(bst.search(6)); // Output: true
+console.log(bst.search(11)); // Output: false
+
+bst.remove(6);
+console.log(bst.search(6)); // Output: false
+
+bst.inorderTraversal((value) => console.log(value));
+// Output: 1, 3, 4, 7, 8, 10, 13, 14
