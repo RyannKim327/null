dbe86b72779b13224eab762051512462028a5ac4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:10:38 +0800

    1-27-2024 7:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,18 @@
-function largestPrimeFactor(number) {
-  let largestPrime = 2;
-  
-  while (number % 2 === 0) {
-    number /= 2;
-  }
-  
-  for (let i = 3; i <= Math.sqrt(number); i += 2) {
-    while (number % i === 0) {
-      largestPrime = i;
-      number /= i;
-    }
-  }
-  
-  if (number > 2) {
-    largestPrime = number;
-  }
-  
-  return largestPrime;
+// Function to generate a random number within a given range
+function getRandomNumber(min, max) {
+  // Generate a random decimal number between 0 and 1
+  var randomDecimal = Math.random();
+
+  // Scale the range to the desired min and max values
+  var randomNumber = randomDecimal * (max - min + 1) + min;
+
+  // Convert the decimal to an integer by using Math.floor()
+  return Math.floor(randomNumber);
 }
 
-// Usage example
-const number = 123456789;
-const largestPrime = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is ${largestPrime}`);
-The largest prime factor of 123456789 is 3803
+// Usage Example
+var minRange = 1; // Minimum value of the range
+var maxRange = 10; // Maximum value of the range
+
+var randomNum = getRandomNumber(minRange, maxRange);
+console.log(randomNum); // Print the random number to the console
