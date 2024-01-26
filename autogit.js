f83028578f993c15d9d5a54d34c46c8c54fe6a24
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:56:03 +0800

    1-27-2024 6:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,21 @@
-// Represents the graph as an adjacency list
-class Graph {
-  constructor(numOfVertices) {
-    this.numOfVertices = numOfVertices;
-    this.edges = [];
-  }
+function findFirstRepeatedChar(str) {
+  let charCount = {};
 
-  addEdge(src, dest, weight) {
-    this.edges.push([src, dest, weight]);
-  }
-}
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
 
-// Calculates the shortest paths from a given source vertex
-function bellmanFord(graph, src) {
-  // Step 1: Initialize distances from source to all other vertices as infinite
-  let distances = new Array(graph.numOfVertices);
-  for (let i = 0; i < graph.numOfVertices; i++) {
-    distances[i] = Infinity;
-  }
-  distances[src] = 0;
-
-  // Step 2: Relax all edges |V| - 1 times
-  for (let i = 1; i <= graph.numOfVertices - 1; i++) {
-    for (let [src, dest, weight] of graph.edges) {
-      if (distances[src] + weight < distances[dest]) {
-        distances[dest] = distances[src] + weight;
-      }
+    // if character already exists in charCount, it is repeated
+    if (charCount[char]) {
+      return char;
+    } else {
+      charCount[char] = 1;
     }
   }
 
-  // Step 3: Check for negative-weight cycles
-  for (let [src, dest, weight] of graph.edges) {
-    if (distances[src] + weight < distances[dest]) {
-      throw new Error("Graph contains a negative-weight cycle");
-    }
-  }
-
-  return distances;
+  return null; // return null if no repeated character found
 }
 
 // Example usage:
-let graph = new Graph(5);
-graph.addEdge(0, 1, -1);
-graph.addEdge(0, 2, 4);
-graph.addEdge(1, 2, 3);
-graph.addEdge(1, 3, 2);
-graph.addEdge(1, 4, 2);
-graph.addEdge(3, 2, 5);
-graph.addEdge(3, 1, 1);
-graph.addEdge(4, 3, -3);
-
-let src = 0;
-let distances = bellmanFord(graph, src);
-console.log("Shortest distances from source:", distances);
+const str = "hello";
+const repeatedChar = findFirstRepeatedChar(str);
+console.log(repeatedChar); // Output: 'l'
