cb96dbd92e9bd48b7af75fbc97f09fba11cefb37
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:11:50 +0800

    1-27-2024 7:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,30 @@
-function factorialLoop(num) {
-  let result = 1;
+function findLongestIncreasingSubsequence(arr) {
+  const n = arr.length;
+  const dp = new Array(n).fill(1);
+  let maxLength = 1;
   
-  for(let i = num; i >= 1; i--) {
-    result *= i;
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        maxLength = Math.max(maxLength, dp[i]);
+      }
+    }
   }
-  
-  return result;
+
+  const lis = [];
+  let length = maxLength;
+  for (let i = n - 1; i >= 0; i--) {
+    if (dp[i] === length) {
+      lis.unshift(arr[i]);
+      length--;
+    }
+  }
+
+  return lis;
 }
 
-console.log(factorialLoop(5));  // Output: 120
-function factorialRecursive(num) {
-  if(num === 0 || num === 1) {
-    return 1;
-  } else {
-    return num * factorialRecursive(num - 1);
-  }
-}
-
-console.log(factorialRecursive(5));  // Output: 120
+// Example usage:
+const array = [2, 4, 3, 5, 1, 7, 6, 9, 8];
+const longestIncreasingSubsequence = findLongestIncreasingSubsequence(array);
+console.log(longestIncreasingSubsequence);
