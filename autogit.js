70533fb59017e3b0e62f8b358381927264bf4a52
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:57:35 +0800

    1-27-2024 6:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,12 @@
-function computeLPSArray(pattern) {
-  const lps = Array(pattern.length).fill(0);
-  let len = 0, i = 1;
+const axios = require('axios');
 
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
-    }
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
   }
+};
 
-  return lps;
-}
-function KMPSearch(text, pattern) {
-  const matches = [];
-  const m = pattern.length;
-  const n = text.length;
-  let i = 0, j = 0;
-
-  const lps = computeLPSArray(pattern);
-
-  while (i < n) {
-    if (pattern[j] === text[i]) {
-      i++;
-      j++;
-    }
-
-    if (j === m) {
-      matches.push(i - j);
-      j = lps[j - 1];
-    } else if (i < n && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-
-  return matches;
-}
-const text = "ABCABCDABABCDABCDABDE";
-const pattern = "ABCDABD";
-const matches = KMPSearch(text, pattern);
-console.log(matches); // Output: [10]
+fetchData();
