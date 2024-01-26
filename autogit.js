67ee7c2e3f99acef395a36678c1b3957db271047
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:58:37 +0800

    1-27-2024 6:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,52 @@
-function beamSearch(initialState, beamWidth, maxDepth) {
-  let beam = [{ state: initialState, score: 0 }];
-  
-  for (let depth = 0; depth < maxDepth; depth++) {
-    let newBeam = [];
-    
-    for (let i = 0; i < beam.length; i++) {
-      let { state, score } = beam[i];
-      
-      // Generate possible next states from current state
-      let nextStates = generateNextStates(state);
-      
-      for (let j = 0; j < nextStates.length; j++) {
-        let nextState = nextStates[j];
-        let nextScore = scoreFunction(nextState);
-        newBeam.push({ state: nextState, score: nextScore });
+class HashTable {
+  constructor() {
+    this.table = {};
+  }
+
+  // Hash function to convert a key into a hash value
+  hash(key) {
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashValue += key.charCodeAt(i);
+    }
+    return hashValue;
+  }
+
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const hashKey = this.hash(key);
+    if (!this.table.hasOwnProperty(hashKey)) {
+      this.table[hashKey] = {};
+    }
+    this.table[hashKey][key] = value;
+  }
+
+  // Retrieve a value from the hash table given a key
+  get(key) {
+    const hashKey = this.hash(key);
+    if (this.table.hasOwnProperty(hashKey) && this.table[hashKey].hasOwnProperty(key)) {
+      return this.table[hashKey][key];
+    }
+    return undefined;
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const hashKey = this.hash(key);
+    if (this.table.hasOwnProperty(hashKey) && this.table[hashKey].hasOwnProperty(key)) {
+      delete this.table[hashKey][key];
+      if (Object.keys(this.table[hashKey]).length === 0) {
+        delete this.table[hashKey];
       }
     }
-    
-    // Sort the new beam by score
-    newBeam.sort((a, b) => b.score - a.score);
-    
-    // Truncate the beam to the top-scoring solutions
-    beam = newBeam.slice(0, beamWidth);
   }
-  
-  return beam[0].state; // Return the best solution found
 }
+
+// Example usage
+const hashtable = new HashTable();
+hashtable.insert("name", "John");
+hashtable.insert("age", 25);
+console.log(hashtable.get("name")); // Output: John
+console.log(hashtable.get("age")); // Output: 25
+hashtable.remove("age");
+console.log(hashtable.get("age")); // Output: undefined
