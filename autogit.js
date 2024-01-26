3259af461ca51c8447db0bb12b95b2c214bb8589
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:09:46 +0800

    1-27-2024 7:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,71 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.adjList = new Map();
+    this.index = 0;
+    this.stack = [];
+    this.ids = new Array(vertices).fill(-1);
+    this.lowLink = new Array(vertices).fill(-1);
+    this.onStack = new Array(vertices).fill(false);
+    this.scc = [];
+  }
 
-console.log(array); // Output: [5, 4, 3, 2, 1]
-let array = [1, 2, 3, 4, 5];
-let reversedArray = array.slice().reverse();
+  addEdge(u, v) {
+    if (!this.adjList.has(u)) {
+      this.adjList.set(u, []);
+    }
+    this.adjList.get(u).push(v);
+  }
 
-console.log(array); // Output: [1, 2, 3, 4, 5]
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
+  findSCC() {
+    for (let i = 0; i < this.vertices; i++) {
+      if (this.ids[i] === -1) {
+        this.dfs(i);
+      }
+    }
+    return this.scc;
+  }
+
+  dfs(node) {
+    this.stack.push(node);
+    this.onStack[node] = true;
+    this.ids[node] = this.lowLink[node] = this.index++;
+  
+    if (this.adjList.has(node)) {
+      const neighbors = this.adjList.get(node);
+      for (let i = 0; i < neighbors.length; i++) {
+        const neighbor = neighbors[i];
+        if (this.ids[neighbor] === -1) {
+          this.dfs(neighbor);
+        }
+        if (this.onStack[neighbor]) {
+          this.lowLink[node] = Math.min(this.lowLink[node], this.lowLink[neighbor]);
+        }
+      }
+    }
+
+    if (this.ids[node] == this.lowLink[node]) {
+      const component = [];
+      let w;
+      do {
+        w = this.stack.pop();
+        this.onStack[w] = false;
+        component.push(w);
+      } while (w !== node);
+      this.scc.push(component);
+    }
+  }
+}
+const graph = new Graph(7); // number of vertices
+
+graph.addEdge(0, 1);
+graph.addEdge(1, 2);
+graph.addEdge(2, 0);
+graph.addEdge(2, 3);
+graph.addEdge(3, 4);
+graph.addEdge(4, 5);
+graph.addEdge(5, 3);
+graph.addEdge(5, 6);
+
+const stronglyConnectedComponents = graph.findSCC();
+console.log(stronglyConnectedComponents);
