f272bb733539f90c35a50d5fc62502b346b91972
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:59:46 +0800

    1-27-2024 6:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,30 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-function findNthFromEnd(head, n) {
-  // Two pointers to track the nth node from the end and the end of the list
-  let p1 = head;
-  let p2 = head;
+function findFirstNonRepeatingChar(str) {
+  // Create an empty map to store character frequencies
+  const charFrequencies = new Map();
 
-  // Move p2 n positions ahead
-  for (let i = 0; i < n; i++) {
-    if (p2 === null) {
-      // If the list has fewer than n nodes, return null
-      return null;
+  // Iterate through the string and update character frequencies
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charFrequencies.has(char)) {
+      charFrequencies.set(char, charFrequencies.get(char) + 1);
+    } else {
+      charFrequencies.set(char, 1);
     }
-    p2 = p2.next;
   }
 
-  // Move both pointers until p2 reaches the end of the list
-  while (p2 !== null) {
-    p1 = p1.next;
-    p2 = p2.next;
+  // Iterate through the string again and find the first non-repeating character
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (charFrequencies.get(char) === 1) {
+      return char;
+    }
   }
 
-  // p1 will be pointing to the nth node from the end
-  return p1;
+  // If no non-repeating character is found, return null
+  return null;
 }
-// Create a sample linked list 1 -> 2 -> 3 -> 4 -> 5
-const head = new ListNode(1);
-head.next = new ListNode(2);
-head.next.next = new ListNode(3);
-head.next.next.next = new ListNode(4);
-head.next.next.next.next = new ListNode(5);
 
-// Call the function to find the 2nd node from the end
-const n = 2;
-const nthNodeFromEnd = findNthFromEnd(head, n);
-
-console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.val}`);
+// Example usage:
+const str = "aabbcde";
+const firstNonRepeatingChar = findFirstNonRepeatingChar(str);
+console.log(firstNonRepeatingChar);  // Output: 'c'
