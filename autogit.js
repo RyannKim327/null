367db5a26878618b5f3a89988430b9b73a528983
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 06:50:37 +0800

    1-27-2024 6:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,15 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
   }
+  return count;
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+let myString = "Hello, World!";
+let myCharacter = "o";
 
-  // Method to add nodes to the linked list
-  addNode(data) {
-    const newNode = new Node(data);
-
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  // Method to find the middle element
-  findMiddleElement() {
-    if (this.head === null) {
-      return null;
-    }
-
-    let slow = this.head;
-    let fast = this.head;
-
-    while (fast !== null && fast.next !== null) {
-      fast = fast.next.next;
-      slow = slow.next;
-    }
-
-    return slow.data;
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
-
-console.log(linkedList.findMiddleElement()); // Output: 3
+let occurrences = countOccurrences(myString, myCharacter);
+console.log(`The character "${myCharacter}" occurs ${occurrences} times in the string.`);
