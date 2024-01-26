3b734bef56eb082a4ebcfd6c749f82a115e45293
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:13:53 +0800

    1-27-2024 7:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,47 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-function isPalindromeLinkedList(head) {
-  if (!head || !head.next) {
-    // If the linked list is empty or has only one node, it is a palindrome
-    return true;
-  }
-
-  // Find the middle element of the linked list
-  let slow = head;
-  let fast = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-
-  // Reverse the second half of the linked list
-  let prev = null;
-  let curr = slow;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-
-  // Compare the first half and reversed second half
-  let firstHalf = head;
-  let secondHalf = prev;
-  while (firstHalf && secondHalf) {
-    if (firstHalf.data !== secondHalf.data) {
-      // The linked list is not a palindrome
-      return false;
+function findFirstRepeatedChar(str) {
+  const charCount = {};
+  
+  for (const char of str) {
+    if (charCount[char]) {
+      return char;
     }
-    firstHalf = firstHalf.next;
-    secondHalf = secondHalf.next;
+    charCount[char] = true;
   }
-
-  // Restore the original order of the second half
-  curr = prev;
-  prev = null;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
-  }
-  slow.next = prev;
-
-  // The linked list is a palindrome
-  return true;
+  
+  return null;
 }
-const list = new Node(1);
-list.next = new Node(2);
-list.next.next = new Node(3);
-list.next.next.next = new Node(2);
-list.next.next.next.next = new Node(1);
 
-console.log(isPalindromeLinkedList(list)); // Output: true
+const inputString = "Hello World";
+const result = findFirstRepeatedChar(inputString);
+console.log(result); // Output: 'l'
+function findFirstRepeatedChar(str) {
+  const charArray = Array.from(str);
+  const visitedChars = [];
+  
+  for (const char of charArray) {
+    if (visitedChars.includes(char)) {
+      return char;
+    }
+    visitedChars.push(char);
+  }
+  
+  return null;
+}
+
+const inputString = "Hello World";
+const result = findFirstRepeatedChar(inputString);
+console.log(result); // Output: 'l'
+function findFirstRepeatedChar(str) {
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charAt(i);
+    if (str.indexOf(char) !== str.lastIndexOf(char)) {
+      return char;
+    }
+  }
+  
+  return null;
+}
+
+const inputString = "Hello World";
+const result = findFirstRepeatedChar(inputString);
+console.log(result); // Output: 'l'
