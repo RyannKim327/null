9e4c2226e15e3501895c942af04eee70f68c200e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-27 07:04:55 +0800

    1-27-2024 7:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,13 @@
-function findLCS(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  const dp = Array(m + 1)
-    .fill(null)
-    .map(() => Array(n + 1).fill(0));
-
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
+function factorial(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  else {
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
     }
+    return result;
   }
-
-  let lcsLength = dp[m][n];
-  let lcs = '';
-
-  let i = m;
-  let j = n;
-
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
 }
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const lcs = findLCS(str1, str2);
-console.log(lcs);  // Output: ADH
+
+console.log(factorial(5)); // Output: 120
