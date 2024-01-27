af8e7e9aa22dc8eb153aac69fee63dfa14ce40c3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:46:11 +0800

    1-28-2024 6:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,9 @@
-function radixSort(arr) {
-  const getMaxDigits = (arr) => {
-    let maxDigits = 0;
-    for (let i = 0; i < arr.length; i++) {
-      maxDigits = Math.max(maxDigits, String(arr[i]).length);
-    }
-    return maxDigits;
-  };
-
-  const distributeElements = (arr, digit) => {
-    const buckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      const digitValue = Math.floor(arr[i] / Math.pow(10, digit)) % 10;
-      buckets[digitValue].push(arr[i]);
-    }
-    return buckets;
-  };
-
-  const collectElements = (buckets) => {
-    const newArr = [];
-    for (let i = 0; i < buckets.length; i++) {
-      newArr.push(...buckets[i]);
-    }
-    return newArr;
-  };
-
-  const maxDigits = getMaxDigits(arr);
-  for (let i = 0; i < maxDigits; i++) {
-    const buckets = distributeElements(arr, i);
-    arr = collectElements(buckets);
-  }
-
-  return arr;
-}
-
-// Example usage:
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr));
+let str = "Hello World!";
+let withoutSpaces = str.replace(/\s/g, "");
+console.log(withoutSpaces);
+let str = "Hello World!";
+let withoutSpaces = str.split(" ").join("");
+console.log(withoutSpaces);
+let str = "Hello World!";
+let withoutSpaces = str.replaceAll(" ", "");
+console.log(withoutSpaces);
