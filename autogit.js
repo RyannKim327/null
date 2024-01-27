266d72243c41ca2101d44fc462b981a0868d2fe2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:39:02 +0800

    1-28-2024 6:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,31 @@
-let str = "Hello, World!";
-let reversedStr = str.split('').reverse().join('');
-console.log(reversedStr);
-let str = "Hello, World!";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
+function findMajorityElement(array) {
+  const frequency = {};
+
+  // Count the frequency of each element
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    frequency[element] = (frequency[element] || 0) + 1;
+  }
+
+  // Find the element with the highest frequency
+  let majorityElement;
+  let maxFrequency = 0;
+  for (const element in frequency) {
+    if (frequency[element] > maxFrequency) {
+      majorityElement = element;
+      maxFrequency = frequency[element];
+    }
+  }
+
+  // Check if it is a majority element
+  if (maxFrequency > array.length / 2) {
+    return majorityElement;
+  }
+
+  return null;
 }
-console.log(reversedStr);
-let str = "Hello, World!";
-let reversedStr = Array.from(str).reduce((acc, char) => char + acc, '');
-console.log(reversedStr);
+
+// Example usage
+const array = [1, 2, 2, 3, 2, 4, 2];
+const majorityElement = findMajorityElement(array);
+console.log(majorityElement); // Output: 2
