ad956f9066dce1d1a88d72eb7aced048677a4627
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:59:32 +0800

    1-28-2024 6:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,10 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.children = {};
-    this.isWord = false;
-  }
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
-class Trie {
-  constructor() {
-    this.root = new Node("");
-  }
 
-  // Other methods will go here
-}
-insert(word) {
-  let node = this.root;
-  for (let char of word) {
-    if (!node.children[char]) {
-      node.children[char] = new Node(char);
-    }
-    node = node.children[char];
-  }
-  node.isWord = true;
-}
-search(word) {
-  let node = this.root;
-  for (let char of word) {
-    if (!node.children[char]) {
-      return false;
-    }
-    node = node.children[char];
-  }
-  return node.isWord;
-}
-startsWith(prefix) {
-  let node = this.root;
-  for (let char of prefix) {
-    if (!node.children[char]) {
-      return false;
-    }
-    node = node.children[char];
-  }
-  return true;
-}
-const myTrie = new Trie();
-myTrie.insert("apple");
-myTrie.insert("banana");
-console.log(myTrie.search("apple")); // true
-console.log(myTrie.search("banana")); // true
-console.log(myTrie.startsWith("app")); // true
-console.log(myTrie.startsWith("ban")); // true
-console.log(myTrie.search("orange")); // false
-console.log(myTrie.startsWith("ora")); // false
+// Example usage:
+var triangleBase = 5;
+var triangleHeight = 10;
+var triangleArea = calculateTriangleArea(triangleBase, triangleHeight);
+console.log("The area of the triangle is: " + triangleArea);
