30b9d41280c6d0bf0c1b23f7289bec1c749d462f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:55:14 +0800

    1-28-2024 6:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,127 @@
-function depthLimitedSearchIterative(root, target, depthLimit) {
-  const stack = [{ node: root, depth: 0 }];
+class Node {
+  constructor(value, color) {
+    this.value = value;
+    this.color = color;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+class RedBlackTree {
+  constructor() {
+    this.root = null;
+  }
 
-  while (stack.length > 0) {
-    const { node, depth } = stack.pop();
+  // Rest of the methods will be implemented here
+}
+insert(value) {
+  const newNode = new Node(value, "red");
 
-    if (node.value === target) {
-      return node;
+  if (!this.root) {
+    this.root = newNode;
+  } else {
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        if (currentNode.left) {
+          currentNode = currentNode.left;
+        } else {
+          currentNode.left = newNode;
+          newNode.parent = currentNode;
+          break;
+        }
+      } else {
+        if (currentNode.right) {
+          currentNode = currentNode.right;
+        } else {
+          currentNode.right = newNode;
+          newNode.parent = currentNode;
+          break;
+        }
+      }
     }
+    this.fixTreeAfterInsert(newNode);
+  }
 
-    if (depth < depthLimit) {
-      // Add child nodes to the stack in reverse order
-      for (let i = node.children.length - 1; i >= 0; i--) {
-        stack.push({ node: node.children[i], depth: depth + 1 });
+  newNode.color = "black";  // Root node must be black
+}
+fixTreeAfterInsert(node) {
+  while (node.parent && node.parent.color === "red") {
+    if (node.parent === node.parent.parent.left) {
+      const uncle = node.parent.parent.right;
+      if (uncle && uncle.color === "red") {
+        node.parent.color = "black";
+        uncle.color = "black";
+        node.parent.parent.color = "red";
+        node = node.parent.parent;
+      } else {
+        if (node === node.parent.right) {
+          node = node.parent;
+          this.rotateLeft(node);
+        }
+        node.parent.color = "black";
+        node.parent.parent.color = "red";
+        this.rotateRight(node.parent.parent);
+      }
+    } else {
+      const uncle = node.parent.parent.left;
+      if (uncle && uncle.color === "red") {
+        node.parent.color = "black";
+        uncle.color = "black";
+        node.parent.parent.color = "red";
+        node = node.parent.parent;
+      } else {
+        if (node === node.parent.left) {
+          node = node.parent;
+          this.rotateRight(node);
+        }
+        node.parent.color = "black";
+        node.parent.parent.color = "red";
+        this.rotateLeft(node.parent.parent);
       }
     }
   }
 
-  return null; // Target not found within depth limit
+  this.root.color = "black";
+}
+rotateLeft(node) {
+  const rightChild = node.right;
+  node.right = rightChild.left;
+
+  if (rightChild.left) {
+    rightChild.left.parent = node;
+  }
+  rightChild.parent = node.parent;
+
+  if (!node.parent) {
+    this.root = rightChild;
+  } else if (node === node.parent.left) {
+    node.parent.left = rightChild;
+  } else {
+    node.parent.right = rightChild;
+  }
+
+  rightChild.left = node;
+  node.parent = rightChild;
+}
+
+rotateRight(node) {
+  const leftChild = node.left;
+  node.left = leftChild.right;
+
+  if (leftChild.right) {
+    leftChild.right.parent = node;
+  }
+  leftChild.parent = node.parent;
+
+  if (!node.parent) {
+    this.root = leftChild;
+  } else if (node === node.parent.left) {
+    node.parent.left = leftChild;
+  } else {
+    node.parent.right = leftChild;
+  }
+
+  leftChild.right = node;
+  node.parent = leftChild;
 }
