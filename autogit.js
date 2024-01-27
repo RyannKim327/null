8970278c49a161653661e0cc6daf19a5b64817ac
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:59:26 +0800

    1-28-2024 6:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,53 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  var swapped;
-  
-  do {
-    swapped = false;
-    for (var i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        // Swap elements using a temporary variable
-        var temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
-      }
-    }
-    len--;
-  } while (swapped);
-  
-  return arr;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.children = {};
+    this.isWord = false;
+  }
 }
-var arr = [4, 2, 7, 1, 3];
-console.log(bubbleSort(arr)); // Output: [1, 2, 3, 4, 7]
+class Trie {
+  constructor() {
+    this.root = new Node("");
+  }
+
+  // Other methods will go here
+}
+insert(word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node.children[char]) {
+      node.children[char] = new Node(char);
+    }
+    node = node.children[char];
+  }
+  node.isWord = true;
+}
+search(word) {
+  let node = this.root;
+  for (let char of word) {
+    if (!node.children[char]) {
+      return false;
+    }
+    node = node.children[char];
+  }
+  return node.isWord;
+}
+startsWith(prefix) {
+  let node = this.root;
+  for (let char of prefix) {
+    if (!node.children[char]) {
+      return false;
+    }
+    node = node.children[char];
+  }
+  return true;
+}
+const myTrie = new Trie();
+myTrie.insert("apple");
+myTrie.insert("banana");
+console.log(myTrie.search("apple")); // true
+console.log(myTrie.search("banana")); // true
+console.log(myTrie.startsWith("app")); // true
+console.log(myTrie.startsWith("ban")); // true
+console.log(myTrie.search("orange")); // false
+console.log(myTrie.startsWith("ora")); // false
