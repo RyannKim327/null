abb735cd85ec4ef95259f78128b2e2ba46a01bfe
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:43:13 +0800

    1-28-2024 6:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,112 +1,27 @@
-class Node {
-  constructor(key) {
-    this.key = key;
-    this.height = 1;
-    this.left = null;
-    this.right = null;
+class ListNode {
+  constructor(val, next = null) {
+    this.val = val;
+    this.next = next;
   }
 }
-class AVLTree {
-  constructor() {
-    this.root = null;
+
+function findNthNodeFromEnd(head, n) {
+  let slowPtr = head;
+  let fastPtr = head;
+
+  // Move fastPtr n positions ahead
+  for (let i = 0; i < n; i++) {
+    if (fastPtr === null) {
+      return null; // n is greater than the length of the list
+    }
+    fastPtr = fastPtr.next;
   }
 
-  // Method to insert a new key into the AVL tree
-  insert(key) {
-    // Implement the logic for key insertion here
+  // Move both pointers one position at a time
+  while (fastPtr !== null) {
+    slowPtr = slowPtr.next;
+    fastPtr = fastPtr.next;
   }
 
-  // Method to delete a key from the AVL tree
-  delete(key) {
-    // Implement the logic for key deletion here
-  }
-
-  // Method to balance the AVL tree
-  balance(node) {
-    // Implement the logic to balance the tree here
-  }
-
-  // Utility methods (optional) - height, getMinValue, getMaxValue, etc.
-}
-insert(key) {
-  this.root = this.insertNode(this.root, key);
-}
-
-insertNode(node, key) {
-  // Standard BST insertion
-  if (node === null) {
-    return new Node(key);
-  }
-
-  if (key < node.key) {
-    node.left = this.insertNode(node.left, key);
-  } else if (key > node.key) {
-    node.right = this.insertNode(node.right, key);
-  } else {
-    // Duplicate keys are not allowed
-    return node;
-  }
-
-  // Update height of this node
-  node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-
-  // Balance the tree
-  return this.balance(node);
-}
-balance(node) {
-  // Calculate the balance factor of the node
-  const balanceFactor = this.getBalanceFactor(node);
-
-  // Left Left Case: Perform a right rotation
-  if (balanceFactor > 1 && this.getBalanceFactor(node.left) >= 0) {
-    return this.rotateRight(node);
-  }
-
-  // Right Right Case: Perform a left rotation
-  if (balanceFactor < -1 && this.getBalanceFactor(node.right) <= 0) {
-    return this.rotateLeft(node);
-  }
-
-  // Left Right Case: Perform a left-right rotation
-  if (balanceFactor > 1 && this.getBalanceFactor(node.left) < 0) {
-    node.left = this.rotateLeft(node.left);
-    return this.rotateRight(node);
-  }
-
-  // Right Left Case: Perform a right-left rotation
-  if (balanceFactor < -1 && this.getBalanceFactor(node.right) > 0) {
-    node.right = this.rotateRight(node.right);
-    return this.rotateLeft(node);
-  }
-
-  return node;
-}
-rotateLeft(node) {
-  const rightChild = node.right;
-  const rightLeftChild = rightChild.left;
-
-  // Perform rotation
-  rightChild.left = node;
-  node.right = rightLeftChild;
-
-  // Update heights
-  node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-  rightChild.height = 1 + Math.max(this.getHeight(rightChild.left), this.getHeight(rightChild.right));
-
-  return rightChild;
-}
-
-rotateRight(node) {
-  const leftChild = node.left;
-  const leftRightChild = leftChild.right;
-
-  // Perform rotation
-  leftChild.right = node;
-  node.left = leftRightChild;
-
-  // Update heights
-  node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-  leftChild.height = 1 + Math.max(this.getHeight(leftChild.left), this.getHeight(leftChild.right));
-
-  return leftChild;
+  return slowPtr;
 }
