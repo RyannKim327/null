0498a788d271d8e4e40c89419a794a762d831181
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:49:44 +0800

    1-28-2024 6:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,18 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+function findMedianSortedArrays(nums1, nums2) {
+  const combinedArray = nums1.concat(nums2);
+  combinedArray.sort((a, b) => a - b);
+
+  const length = combinedArray.length;
+  const middle = Math.floor(length / 2);
+
+  if (length % 2 === 0) {
+    return (combinedArray[middle - 1] + combinedArray[middle]) / 2;
+  } else {
+    return combinedArray[middle];
+  }
+}
+const nums1 = [1, 3];
+const nums2 = [2, 4];
+
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median); // Output: 2.5
