914a59ed9afd92643bf1418fba06f8b724fa2782
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:50:15 +0800

    1-28-2024 6:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,63 @@
-const CronJob = require('cron').CronJob;
-
-// Define the cron schedule
-const cronExpression = '0 * * * *'; // every hour
-
-// Function to be executed by the cron job
-function myCronFunction() {
-  console.log('Cron job executed!');
-  // Add your desired functionality here
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-// Create a new cron job
-const cronJob = new CronJob(cronExpression, myCronFunction);
+  addNode(value) {
+    const newNode = new Node(value);
 
-// Start the cron job
-cronJob.start();
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
 
-console.log('Cron job scheduled!');
+  isPalindrome() {
+    let slow = this.head;
+    let fast = this.head;
+    let stack = [];
+
+    while (fast !== null && fast.next !== null) {
+      stack.push(slow.value);
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // If the list length is odd, skip the middle node
+    if (fast !== null) {
+      slow = slow.next;
+    }
+
+    while (slow !== null) {
+      const top = stack.pop();
+
+      if (top !== slow.value) {
+        return false;
+      }
+
+      slow = slow.next;
+    }
+
+    return true;
+  }
+}
+const linkedList = new LinkedList();
+
+linkedList.addNode('a');
+linkedList.addNode('b');
+linkedList.addNode('c');
+linkedList.addNode('b');
+linkedList.addNode('a');
+
+console.log(linkedList.isPalindrome()); // Output: true
