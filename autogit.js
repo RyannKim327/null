4369446296b6909e0f4dcfc740acb88fb3f299c6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:39:54 +0800

    1-28-2024 6:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,23 @@
-function longestCommonPrefix(strings) {
-  if (strings.length === 0) {
-    return '';
-  }
-  
-  let prefix = '';
-  const firstString = strings[0];
-
-  for (let i = 0; i < firstString.length; i++) {
-    const char = firstString[i];
-
-    for (let j = 1; j < strings.length; j++) {
-      if (i >= strings[j].length || strings[j][i] !== char) {
-        return prefix;
-      }
-    }
-
-    prefix += char;
+function binarySearchRecursive(arr, target, start, end) {
+  if (start > end) {
+    return -1;
   }
 
-  return prefix;
+  const middle = Math.floor((start + end) / 2);
+
+  if (arr[middle] === target) {
+    return middle;
+  }
+
+  if (arr[middle] < target) {
+    return binarySearchRecursive(arr, target, middle + 1, end);
+  } else {
+    return binarySearchRecursive(arr, target, start, middle - 1);
+  }
 }
 
 // Example usage:
-const strings = ['flower', 'flow', 'flight'];
-console.log(longestCommonPrefix(strings)); // Output: 'fl'
+const array = [2, 4, 7, 10, 12, 15, 21, 23];
+const target = 10;
+const index = binarySearchRecursive(array, target, 0, array.length - 1);
+console.log(index); // Output: 3 (index of target value 10 in the array)
