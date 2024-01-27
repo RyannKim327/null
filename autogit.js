9b28fe7baf58b59de882eeaf473751f50cd1d921
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:46:32 +0800

    1-28-2024 6:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,19 @@
-function largestPrimeFactor(number) {
-  let factor = 2;
-  let largestFactor = 1;
+function insertionSort(arr) {
+  const len = arr.length;
+  for (let i = 1; i < len; i++) {
+    let key = arr[i]; // current element to be compared
 
-  while (factor <= number) {
-    if (number % factor === 0) {
-      largestFactor = factor;
-      number = number / factor;
-    } else {
-      factor++;
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      // move elements greater than key to one position ahead
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = key; // insert the key element in its correct position
   }
-
-  return largestFactor;
+  return arr;
 }
-const number = 84;
-const largestFactor = largestPrimeFactor(number);
-console.log('Largest Prime Factor:', largestFactor);
-Largest Prime Factor: 7
+
+// Usage example:
+const array = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(array)); // Output: [1, 2, 3, 4, 5, 6]
