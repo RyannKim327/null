5e4dddbf6567dd1e93e5dfc5066109fe484fa7c5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:41:33 +0800

    1-28-2024 6:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,77 +1,43 @@
-function findKthSmallest(arr, k) {
-  arr.sort((a, b) => a - b);
-  return arr[k - 1];
-}
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
+function bellmanFord(graph, source) {
+  const dist = {};
 
-  insert(value) {
-    this.heap.push(value);
-    this.bubbleUp();
+  // Step 2: Initialization
+  for (let node in graph) {
+    dist[node] = Infinity;
   }
+  dist[source] = 0;
 
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    while (index > 0) {
-      let element = this.heap[index];
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.heap[parentIndex];
-      if (parent <= element) break;
-      this.heap[index] = parent;
-      this.heap[parentIndex] = element;
-      index = parentIndex;
+  // Step 3: Relax edges repeatedly
+  for (let i = 0; i < Object.keys(graph).length - 1; i++) {
+    for (let node in graph) {
+      for (let edge of graph[node]) {
+        const { target, weight } = edge;
+        if (dist[node] + weight < dist[target]) {
+          dist[target] = dist[node] + weight;
+        }
+      }
     }
   }
 
-  extractMin() {
-    const min = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.sinkDown(0);
-    return min;
-  }
-
-  sinkDown(index) {
-    const leftChildIndex = 2 * index + 1;
-    const rightChildIndex = 2 * index + 2;
-    let smallestIndex = index;
-    const length = this.heap.length;
-
-    if (leftChildIndex < length && this.heap[leftChildIndex] < this.heap[smallestIndex]) {
-      smallestIndex = leftChildIndex;
-    }
-
-    if (rightChildIndex < length && this.heap[rightChildIndex] < this.heap[smallestIndex]) {
-      smallestIndex = rightChildIndex;
-    }
-
-    if (smallestIndex !== index) {
-      [this.heap[smallestIndex], this.heap[index]] = [this.heap[index], this.heap[smallestIndex]];
-      this.sinkDown(smallestIndex);
+  // Step 4: Check for negative cycles
+  for (let node in graph) {
+    for (let edge of graph[node]) {
+      const { target, weight } = edge;
+      if (dist[node] + weight < dist[target]) {
+        throw new Error("Graph contains a negative cycle!");
+      }
     }
   }
-}
 
-function findKthSmallest(arr, k) {
-  const heap = new MinHeap();
-  arr.forEach((num) => heap.insert(num));
-  for (let i = 0; i < k - 1; i++) {
-    heap.extractMin();
-  }
-  return heap.extractMin();
+  return dist;
 }
-function findKthSmallest(arr, k) {
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const smaller = arr.filter((num) => num < pivot);
-  const equal = arr.filter((num) => num === pivot);
-  const greater = arr.filter((num) => num > pivot);
+const graph = {
+  A: [{ target: "B", weight: -1 }, { target: "C", weight: 4 }],
+  B: [{ target: "C", weight: 3 }, { target: "D", weight: 2 }, { target: "E", weight: 2 }],
+  C: [],
+  D: [{ target: "B", weight: 1 }, { target: "C", weight: 5 }],
+  E: [{ target: "D", weight: -3 }],
+};
 
-  if (k <= smaller.length) {
-    return findKthSmallest(smaller, k);
-  } else if (k <= smaller.length + equal.length) {
-    return pivot;
-  } else {
-    return findKthSmallest(greater, k - smaller.length - equal.length);
-  }
-}
+const source = "A";
+console.log(bellmanFord(graph, source));
