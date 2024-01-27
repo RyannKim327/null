f46359a3a6cdf486b2cca01fff1029acddbdd697
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:50:22 +0800

    1-28-2024 6:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function reverseWords(str) {
+  // Step 1: Split the string into an array of words
+  var words = str.split(" ");
+
+  // Step 2: Reverse the order of the array
+  words.reverse();
+
+  // Step 3: Join the words back together into a string
+  var reversedStr = words.join(" ");
+
+  return reversedStr;
 }
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
 
-  addNode(value) {
-    const newNode = new Node(value);
-
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  isPalindrome() {
-    let slow = this.head;
-    let fast = this.head;
-    let stack = [];
-
-    while (fast !== null && fast.next !== null) {
-      stack.push(slow.value);
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-
-    // If the list length is odd, skip the middle node
-    if (fast !== null) {
-      slow = slow.next;
-    }
-
-    while (slow !== null) {
-      const top = stack.pop();
-
-      if (top !== slow.value) {
-        return false;
-      }
-
-      slow = slow.next;
-    }
-
-    return true;
-  }
-}
-const linkedList = new LinkedList();
-
-linkedList.addNode('a');
-linkedList.addNode('b');
-linkedList.addNode('c');
-linkedList.addNode('b');
-linkedList.addNode('a');
-
-console.log(linkedList.isPalindrome()); // Output: true
+// Example usage
+var sentence = "Hello, world! How are you?";
+var reversedSentence = reverseWords(sentence);
+console.log(reversedSentence);
+you? are How world! Hello,
