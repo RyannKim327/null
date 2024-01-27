626a3989797e1a870bb8a6a54362d03bd0ef5265
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 06:52:58 +0800

    1-28-2024 6:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,21 @@
-function mergeArrays(arr1, arr2) {
-  let mergedArray = [];
-  let i = 0;
-  let j = 0;
-
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      mergedArray.push(arr1[i]);
-      i++;
-    } else {
-      mergedArray.push(arr2[j]);
-      j++;
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  
+  // Check if number is divisible by any number from 2 to square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
-
-  while (i < arr1.length) {
-    mergedArray.push(arr1[i]);
-    i++;
-  }
-
-  while (j < arr2.length) {
-    mergedArray.push(arr2[j]);
-    j++;
-  }
-
-  return mergedArray;
-}
-function findMedian(arr1, arr2) {
-  const mergedArray = mergeArrays(arr1, arr2);
-  const length = mergedArray.length;
   
-  if (length % 2 === 1) {
-    return mergedArray[Math.floor(length / 2)];
-  } else {
-    const mid = length / 2;
-    return (mergedArray[mid] + mergedArray[mid - 1]) / 2;
-  }
+  return true;
 }
-const arr1 = [1, 3, 5];
-const arr2 = [2, 4, 6, 8];
-const median = findMedian(arr1, arr2);
-console.log(median); // Output: 4
+const number = 29;  // Example number to check
+
+if (isPrime(number)) {
+  console.log(number + ' is prime.');
+} else {
+  console.log(number + ' is not prime.');
+}
