a5bb73ac57969b59d1e90c4ba29ef6baffac0783
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 07:02:01 +0800

    1-28-2024 7:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,8 @@
-const array = [1, 2, 3, 2, 4, 1, 5];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 2, 4, 1, 5];
-const uniqueArray = array.filter((value, index, arr) => arr.indexOf(value) === index);
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 2, 4, 1, 5];
-const uniqueArray = array.reduce((accumulator, value) => {
-  if (!accumulator.includes(value)) {
-    accumulator.push(value);
-  }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 2, 4, 1, 5];
-const uniqueArray = [];
-for (let i = 0; i < array.length; i++) {
-  if (uniqueArray.indexOf(array[i]) === -1) {
-    uniqueArray.push(array[i]);
-  }
+const str = 'Hello, World!';
+const substring = 'lo';
+
+if (str.includes(substring)) {
+  console.log('Substring found!');
+} else {
+  console.log('Substring not found!');
 }
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
