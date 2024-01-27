703acc978d6650dfad481ebac347be3a9f7a66c9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-28 07:03:50 +0800

    1-28-2024 7:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,59 @@
-const string = 'Hello, World!';
-const substring = 'World';
-
-if (string.includes(substring)) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
-const string = 'Hello, World!';
-const substring = /World/;
 
-if (substring.test(string)) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  print() {
+    const values = [];
+    let current = this.head;
+
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+
+    console.log(values.join(' -> '));
+  }
 }
+
+// Usage example:
+const list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend(0);
+
+list.print(); // Output: 0 -> 1 -> 2 -> 3
