f18a86e77f0dce4d2fe16c685e419fcbd8dbc388
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:52:44 +0800

    1-29-2024 20:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-function LinkedListNode(value) {
-  this.value = value;
-  this.next = null;
+const string = "Hello, world!";
+const substring = "world";
+
+if (string.includes(substring)) {
+  console.log("Substring found!");
+} else {
+  console.log("Substring not found!");
 }
-
-function LinkedList() {
-  this.head = null;
-}
-
-LinkedList.prototype.addNode = function(value) {
-  var newNode = new LinkedListNode(value);
-
-  if (this.head === null) {
-    this.head = newNode;
-  } else {
-    var currentNode = this.head;
-    while (currentNode.next !== null) {
-      currentNode = currentNode.next;
-    }
-    currentNode.next = newNode;
-  }
-};
-
-LinkedList.prototype.getLength = function() {
-  var currentNode = this.head;
-  var length = 0;
-
-  while (currentNode !== null) {
-    length++;
-    currentNode = currentNode.next;
-  }
-
-  return length;
-};
-
-// Example usage
-var list = new LinkedList();
-list.addNode(1);
-list.addNode(2);
-list.addNode(3);
-list.addNode(4);
-console.log(list.getLength()); // Output: 4
