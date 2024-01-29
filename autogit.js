e5c7fb743115dc5ca198f3e6f05b629e34d2e1a9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:30:33 +0800

    1-29-2024 20:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,70 @@
-function calculateMean(numbers) {
-  if(numbers.length === 0) {
-    return 0; // or display an error message
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  const mean = (sum / numbers.length).toFixed(2);
-
-  return mean;
 }
 
-// Example usage
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log(mean); // Output: 3
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  addToTail(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+  
+  removeFromTail() {
+    if (!this.head) {
+      return null;
+    }
+    
+    let currentNode = this.head;
+    let previousNode = null;
+
+    while (currentNode.next) {
+      previousNode = currentNode;
+      currentNode = currentNode.next;
+    }
+    
+    if (previousNode) {
+      previousNode.next = null;
+      this.tail = previousNode;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return currentNode.value;
+  }
+  
+  print() {
+    let currentNode = this.head;
+    const values = [];
+
+    while (currentNode) {
+      values.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    
+    console.log(values.join(' -> '));
+  }
+}
+const list = new LinkedList();
+
+list.addToTail(5);
+list.addToTail(10);
+list.addToTail(15);
+list.print(); // Output: 5 -> 10 -> 15
+
+list.removeFromTail();
+list.print(); // Output: 5 -> 10
