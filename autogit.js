e798ece71214388adc07730bac910f52c99950a3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:40:23 +0800

    1-29-2024 20:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,23 @@
-function findMajorityElement(arr) {
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    let count = 0;
-    for (let j = 0; j < length; j++) {
-      if (arr[j] === arr[i]) {
-        count++;
-      }
-    }
-    if (count > length / 2) {
-      return arr[i];
-    }
-  }
-  return -1; // No majority element found
+function prepareString(str) {
+  return str.replace(/\s/g, '').toLowerCase();
 }
-
-const array = [1, 2, 3, 3, 3, 4, 5, 3, 3];
-console.log(findMajorityElement(array)); // Output: 3
-function findMajorityElement(arr) {
-  let count = 0;
-  let majorityElement = null;
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (count === 0) {
-      majorityElement = arr[i];
-    }
-    if (arr[i] === majorityElement) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-  
-  return majorityElement;
+function sortString(str) {
+  return str.split('').sort().join('');
 }
+function areAnagrams(str1, str2) {
+  const prepStr1 = prepareString(str1);
+  const prepStr2 = prepareString(str2);
+  const sortedStr1 = sortString(prepStr1);
+  const sortedStr2 = sortString(prepStr2);
+  
+  return sortedStr1 === sortedStr2;
+}
+const string1 = "listen";
+const string2 = "silent";
 
-const array = [1, 2, 3, 3, 3, 4, 5, 3, 3];
-console.log(findMajorityElement(array)); // Output: 3
+if (areAnagrams(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
+listen and silent are anagrams.
