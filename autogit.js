212c09bcc78b0ddeae96db9dd6a63f6ac01b1de0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:54:35 +0800

    1-29-2024 20:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-function countCharOccurrences(str, char) {
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (str.charAt(i) === char) {
-      count++;
-    }
-  }
-  return count;
-}
-const inputString = "Hello, World!";
-const charToCount = "o";
-
-const occurrences = countCharOccurrences(inputString, charToCount);
-console.log(`The character "${charToCount}" appears ${occurrences} times.`);
-The character "o" appears 2 times.
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
