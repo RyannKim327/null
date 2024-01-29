19e8bad7cf37057cf641cac0ed6342cad1fe97be
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 06:06:05 +0800

    1-30-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,13 @@
-function generateBadCharTable(pattern) {
-  const table = {};
-  const patternLength = pattern.length - 1;
-  for (let i = 0; i < patternLength; i++) {
-    table[pattern[i]] = patternLength - i;
-  }
-  return table;
-}
-function searchBMH(text, pattern) {
-  const patternLength = pattern.length;
-  const textLength = text.length;
+function isPalindrome(str) {
+  let i = 0;
+  let j = str.length - 1;
 
-  const table = generateBadCharTable(pattern);
-  let shift = 0;
-  
-  while (shift <= textLength - patternLength) {
-    let mismatchIndex = patternLength - 1;
-
-    while (pattern[mismatchIndex] === text[shift + mismatchIndex]) {
-      if (mismatchIndex === 0) {
-        // Pattern found at the current shift
-        return shift;
-      }
-      mismatchIndex--;
+  while (i < j) {
+    if (str[i] !== str[j]) {
+      return false;
     }
-
-    const badCharShift = table[text[shift + patternLength - 1]];
-    shift += badCharShift || patternLength;
+    i++;
+    j--;
   }
-
-  // Pattern not found in text
-  return -1;
-}
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-const pattern = "ipsum";
-
-const position = searchBMH(text, pattern);
-if (position !== -1) {
-  console.log(`Pattern found at index ${position}`);
-} else {
-  console.log("Pattern not found in text");
+  return true;
 }
