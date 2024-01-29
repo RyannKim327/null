f96769e4359bd47e7568708cf0d5b4a71aed4f5e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:46:13 +0800

    1-29-2024 20:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,3 @@
-function factorialRecursive(num) {
-  if (num === 0) {
-    // Base case: factorial of 0 is 1
-    return 1;
-  } else {
-    // Recursive case: factorial of num is num multiplied by factorial of num-1
-    return num * factorialRecursive(num - 1);
-  }
-}
-// Calculate factorial of 5
-const result = factorialRecursive(5);
-console.log(result);  // Output: 120
+let str = 'Hello World';
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr);
