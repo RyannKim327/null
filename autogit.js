d17178cb7fef4207f9c782f6420ea54552ca6b68
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:42:53 +0800

    1-29-2024 20:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,5 @@
-function findMajorityElement(array) {
-  const frequency = {};
-  const n = array.length;
+let str = "123";
+let num = parseInt(str);
 
-  // Count the frequencies of each element
-  for (let i = 0; i < n; i++) {
-    const element = array[i];
-    frequency[element] = (frequency[element] || 0) + 1;
-  }
-
-  // Find the element with the highest frequency
-  let majorityElement;
-  let maxFrequency = 0;
-  for (const element in frequency) {
-    if (frequency[element] > maxFrequency) {
-      majorityElement = element;
-      maxFrequency = frequency[element];
-    }
-  }
-
-  // Check if the frequency of the majority element is greater than n/2
-  if (maxFrequency > n / 2) {
-    return majorityElement;
-  } else {
-    return "No majority element found.";
-  }
-}
-
-// Example usage
-const array = [2, 2, 3, 4, 2, 2];
-console.log(findMajorityElement(array)); // Output: 2
+console.log(num); // Output: 123
+console.log(typeof num); // Output: number
