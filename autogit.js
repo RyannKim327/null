f35e7927fc5c9b8112dc938e56ebcd86d93d5452
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:32:35 +0800

    1-29-2024 20:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,83 +1,96 @@
-class Graph {
-  constructor(vertices) {
-    this.vertices = vertices; // number of vertices
-    this.edges = []; // array to store the edges
+class BTreeNode {
+  constructor(order) {
+    this.order = order; // Maximum number of children (suggested value: odd number)
+    this.keys = [];     // Array to store keys
+    this.children = []; // Array to store child nodes
+    this.isLeaf = true; // Flag to determine if the node is a leaf
   }
 
-  addEdge(src, dest, weight) {
-    this.edges.push({ src, dest, weight });
-  }
-}
-function initialize(graph, source) {
-  const distances = new Array(graph.vertices);
-  const parent = new Array(graph.vertices);
-
-  // Initialize distances and parent arrays
-  for (let i = 0; i < graph.vertices; i++) {
-    distances[i] = Infinity;
-    parent[i] = -1;
+  // Find the index of the first key greater than or equal to the given key
+  findIndex(key) {
+    let pos = 0;
+    while (pos < this.keys.length && this.keys[pos] < key) {
+      pos++;
+    }
+    return pos;
   }
 
-  // Distance from source to itself is 0
-  distances[source] = 0;
+  // Insert key into the node
+  insertNonFull(key) {
+    let pos = this.findIndex(key);
 
-  return { distances, parent };
-}
-function bellmanFord(graph, source) {
-  const { distances, parent } = initialize(graph, source);
+    if (this.isLeaf) {
+      this.keys.splice(pos, 0, key);
+    } else {
+      if (this.children[pos].keys.length === (2 * this.order - 1)) {
+        this.splitChild(pos);
 
-  // Relax all edges |V| - 1 times
-  for (let i = 0; i < graph.vertices - 1; i++) {
-    for (let j = 0; j < graph.edges.length; j++) {
-      const { src, dest, weight } = graph.edges[j];
-
-      if (distances[src] + weight < distances[dest]) {
-        distances[dest] = distances[src] + weight;
-        parent[dest] = src;
+        if (key > this.keys[pos]) {
+          pos++;
+        }
       }
+
+      this.children[pos].insertNonFull(key);
     }
   }
 
-  // Check for negative-weight cycles
-  for (let i = 0; i < graph.edges.length; i++) {
-    const { src, dest, weight } = graph.edges[i];
+  // Split the child node at the specified index
+  splitChild(index) {
+    const child = this.children[index];
+    const newChild = new BTreeNode(this.order);
 
-    if (distances[src] + weight < distances[dest]) {
-      console.log("Graph contains negative-weight cycle");
-      return null;
+    this.keys.splice(index, 0, child.keys[this.order - 1]);
+    this.children.splice(index + 1, 0, newChild);
+
+    newChild.keys = child.keys.splice(this.order, this.order - 1);
+
+    if (!child.isLeaf) {
+      newChild.isLeaf = false;
+      newChild.children = child.children.splice(this.order, this.order);
     }
   }
 
-  // Return the calculated distances and parent array
-  return { distances, parent };
-}
-const graph = new Graph(5);
-graph.addEdge(0, 1, -1);
-graph.addEdge(0, 2, 4);
-graph.addEdge(1, 2, 3);
-graph.addEdge(1, 3, 2);
-graph.addEdge(1, 4, 2);
-graph.addEdge(3, 2, 5);
-graph.addEdge(3, 1, 1);
-graph.addEdge(4, 3, -3);
+  // Search for a key in the node
+  search(key) {
+    let pos = this.findIndex(key);
 
-const source = 0;
-const result = bellmanFord(graph, source);
+    if (this.keys[pos] === key) {
+      return true;
+    }
 
-if (result) {
-  const { distances, parent } = result;
+    if (this.isLeaf) {
+      return false;
+    }
 
-  console.log("Vertex\tDistance\tPath");
-  for (let i = 0; i < graph.vertices; i++) {
-    console.log(
-      `${source} -> ${i}\t${distances[i]}\t\t${getPath(i, parent)}`
-    );
+    return this.children[pos].search(key);
   }
 }
 
-function getPath(vertex, parent) {
-  if (parent[vertex] === -1) {
-    return vertex.toString();
+class BTree {
+  constructor(order) {
+    this.order = order; // Maximum number of children in each node
+    this.root = new BTreeNode(order);
   }
-  return getPath(parent[vertex], parent) + " -> " + vertex;
+
+  insert(key) {
+    if (this.root.keys.length === (2 * this.order - 1)) {
+      const newRoot = new BTreeNode(this.order);
+      newRoot.children.push(this.root);
+      newRoot.isLeaf = false;
+      this.root = newRoot;
+      newRoot.splitChild(0);
+    }
+    this.root.insertNonFull(key);
+  }
+
+  search(key) {
+    return this.root.search(key);
+  }
 }
+const bTree = new BTree(3); // Create a B-tree with order 3
+bTree.insert(10);
+bTree.insert(20);
+bTree.insert(5);
+
+console.log(bTree.search(20)); // Outputs: true
+console.log(bTree.search(15)); // Outputs: false
