bb5d09ff14d8414478df7e244d4a01ca3cc4e2d6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:35:33 +0800

    1-29-2024 20:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,19 @@
-function findLCS(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
+function bubbleSort(array) {
+  const length = array.length;
 
-  // Create a matrix to store the lengths of LCS
-  const matrix = Array(m + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0));
-
-  // Fill the matrix in a bottom-up manner
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        // Characters match
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-      } else {
-        // Characters don't match, take the maximum of the previous LCS lengths
-        matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
+  for (let i = 0; i < length; i++) {
+    // Last i elements are already in place
+    for (let j = 0; j < length - i - 1; j++) {
+      // Swap if the current element is greater than the next element
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
       }
     }
   }
 
-  // Read the LCS from the matrix
-  let lcs = '';
-  let i = m;
-  let j = n;
-  while (i > 0 && j > 0) {
-    // If the characters match, it belongs to the LCS
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    }
-    // Move to the cell with the maximum LCS length
-    else if (matrix[i - 1][j] > matrix[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
+  return array;
 }
 
 // Example usage:
-const str1 = 'ABAZDC';
-const str2 = 'BACBAD';
-const longestCommonSubsequence = findLCS(str1, str2);
-console.log(longestCommonSubsequence); // Output: "ABAD"
+const numbers = [9, 5, 7, 3, 1, 8];
+console.log(bubbleSort(numbers)); // Output: [1, 3, 5, 7, 8, 9]
