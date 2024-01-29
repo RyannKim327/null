e446c374010d4926b975c2cd0e90382972850682
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:51:36 +0800

    1-29-2024 20:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,71 @@
-class Graph {
-  constructor() {
-    this.nodes = {};
-  }
-
-  addNode(node) {
-    this.nodes[node] = [];
-  }
-
-  addEdge(source, destination) {
-    this.nodes[source].push(destination);
-    this.nodes[destination].push(source);
-  }
-
-  getNeighbors(node) {
-    return this.nodes[node];
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
-function depthFirstSearch(graph, start, visited = new Set()) {
-  visited.add(start);
-  console.log(start);
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-  const neighbors = graph.getNeighbors(start);
-  for (const neighbor of neighbors) {
-    if (!visited.has(neighbor)) {
-      depthFirstSearch(graph, neighbor, visited);
+  // Function to insert a value into the tree
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // Helper function to recursively insert a value
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Function to search for a value in the tree
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  // Helper function to recursively search for a value
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (node.value === value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
     }
   }
 }
-const graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addNode('E');
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
 
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('D', 'E');
-depthFirstSearch(graph, 'A');
+console.log(bst.search(6));  // Output: true
+console.log(bst.search(11)); // Output: false
