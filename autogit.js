ea10d90ba58d0415afb5e98bfca2a2816ba1784b
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:37:11 +0800

    1-29-2024 20:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,16 @@
-const str = "Hello, World!";
-const reversedStr = str.split("").reverse().join("");
-console.log(reversedStr); // Output: "!dlroW ,olleH"
-function reverseString(str) {
-  let reversedStr = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-  }
-  return reversedStr;
+function reverseWords(str) {
+  // Splitting the string into an array of words
+  var wordsArray = str.split(" ");
+
+  // Reversing the array of words
+  var reversedWordsArray = wordsArray.reverse();
+
+  // Joining the reversed array back into a string
+  var reversedString = reversedWordsArray.join(" ");
+
+  return reversedString;
 }
 
-const str = "Hello, World!";
-console.log(reverseString(str)); // Output: "!dlroW ,olleH"
-function reverseString(str) {
-  if (str === '') {
-    return '';
-  }
-  return reverseString(str.substr(1)) + str.charAt(0);
-}
-
-const str = "Hello, World!";
-console.log(reverseString(str)); // Output: "!dlroW ,olleH"
+// Example usage
+var originalString = "Hello, world!";
+console.log(reverseWords(originalString)); // Output: "world! Hello,"
