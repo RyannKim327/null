0e854973cd1ea98467fb8d5f73194c891d413156
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:28:46 +0800

    1-29-2024 20:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,8 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-function calculateSum(root) {
-  if (root === null) {
-    return 0;
-  }
-
-  let sum = root.value;
-  sum += calculateSum(root.left);
-  sum += calculateSum(root.right);
-
-  return sum;
-}
-
-// Example usage:
-const tree = new Node(1);
-tree.left = new Node(2);
-tree.right = new Node(3);
-tree.left.left = new Node(4);
-tree.left.right = new Node(5);
-
-const sum = calculateSum(tree);
-console.log('Sum of all nodes:', sum);
+let str1 = "Hello";
+let str2 = "world";
+let result = str1 + " " + str2;
+console.log(result); // Output: "Hello world"
+let str1 = "Hello";
+let str2 = "world";
+str1 += " " + str2;
+console.log(str1); // Output: "Hello world"
