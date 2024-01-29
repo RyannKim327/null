19c27f46ed1d9820bdf79f84b7207a583950d947
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 21:03:03 +0800

    1-29-2024 21:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,11 @@
-function selectionSort(arr) {
-  // Your code goes here
-}
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    // Swap the found minimum element with the first element
-    let temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
-  return arr;
-}
-const arr = [64, 25, 12, 22, 11];
-console.log(selectionSort(arr));
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    // Swap the found minimum element with the first element
-    let temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
-  return arr;
-}
+function getIntersectionNode(headA, headB) {
+  let p1 = headA;
+  let p2 = headB;
 
-const arr = [64, 25, 12, 22, 11];
-console.log(selectionSort(arr));
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : headB;
+    p2 = p2 ? p2.next : headA;
+  }
+
+  return p1;
+}
