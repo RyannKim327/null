ccd82dc85e8a22ee0a286134a28cff349b47a626
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:37:02 +0800

    1-29-2024 20:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,22 @@
+const str = "Hello, World!";
+const reversedStr = str.split("").reverse().join("");
+console.log(reversedStr); // Output: "!dlroW ,olleH"
 function reverseString(str) {
-    return str.split("").reverse().join("");
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
-console.log(reverseString("Hello, World!")); // Output: !dlroW ,olleH
+const str = "Hello, World!";
+console.log(reverseString(str)); // Output: "!dlroW ,olleH"
 function reverseString(str) {
-    let reversedStr = "";
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversedStr += str[i];
-    }
-    return reversedStr;
+  if (str === '') {
+    return '';
+  }
+  return reverseString(str.substr(1)) + str.charAt(0);
 }
 
-console.log(reverseString("Hello, World!")); // Output: !dlroW ,olleH
-function reverseString(str) {
-    if (str === "")
-        return "";
-    else
-        return reverseString(str.substr(1)) + str.charAt(0);
-}
-
-console.log(reverseString("Hello, World!")); // Output: !dlroW ,olleH
+const str = "Hello, World!";
+console.log(reverseString(str)); // Output: "!dlroW ,olleH"
