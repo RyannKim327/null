2dda21867f15009ee72e9629404d843873b715cd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:49:14 +0800

    1-29-2024 20:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,14 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
-}
-
-// Usage example:
-const input = "Hello World!";
-const output = removeVowels(input);
-console.log(output); // Output: "Hll Wrld!"
+// Make a GET request to the JSONPlaceholder API
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    // Display the fetched data
+    data.forEach(post => {
+      console.log(`Post ID: ${post.id}`);
+      console.log(`Title: ${post.title}`);
+      console.log(`Body: ${post.body}\n`);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
