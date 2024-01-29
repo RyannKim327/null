d459ae861b03443cd7be5bea3acbdc04740a2fa6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:50:20 +0800

    1-29-2024 20:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,17 @@
-var str1 = "Hello";
-var str2 = " World";
-var combinedString = str1 + str2;
-
-console.log(combinedString); // Output: Hello World
-var str1 = "Hello";
-var str2 = " World";
-var combinedString = str1.concat(str2);
-
-console.log(combinedString); // Output: Hello World
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray);
+// Output: [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = array.filter((element, index) => array.indexOf(element) === index);
+console.log(uniqueArray);
+// Output: [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = array.reduce((accumulator, current) => {
+  if (!accumulator.includes(current)) {
+    accumulator.push(current);
+  }
+  return accumulator;
+}, []);
+console.log(uniqueArray);
+// Output: [1, 2, 3, 4, 5, 6]
