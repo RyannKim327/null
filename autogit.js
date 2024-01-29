0b8698137c6d0a27300a195c3fb6ec16f8615665
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:58:54 +0800

    1-29-2024 20:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,69 +1,24 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
+const axios = require('axios');
 
-  // Method to insert a new value into the tree
-  insert(value) {
-    const newNode = new Node(value);
+// Make a GET request
+axios.get('https://api.example.com/data')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
+// Make a POST request
+const requestBody = {
+  username: 'exampleUser',
+  password: 'examplePassword'
+};
 
-  // Helper method to recursively insert a node
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  // Method to search for a value in the tree
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  // Helper method to recursively search for a node
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    }
-
-    if (value === node.value) {
-      return true;
-    } else if (value < node.value) {
-      return this.searchNode(node.left, value);
-    } else {
-      return this.searchNode(node.right, value);
-    }
-  }
-}
-const tree = new BinaryTree();
-tree.insert(5);
-tree.insert(3);
-tree.insert(8);
-tree.insert(1);
-tree.insert(4);
-
-console.log(tree.search(4)); // Output: true
-console.log(tree.search(7)); // Output: false
+axios.post('https://api.example.com/login', requestBody)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
