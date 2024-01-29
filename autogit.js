8d00f376c58a046340842b05c2832433bcac2048
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:31:37 +0800

    1-29-2024 20:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,14 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters
+  const alphanumeric = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  
+  // Compare with the reversed string
+  return alphanumeric === alphanumeric.split('').reverse().join('');
 }
 
-var inputString = "Hello World";
-var result = removeVowels(inputString);
-console.log(result); // Output: Hll Wrld
+// Example usage
+const testString1 = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(testString1)); // Output: true
+
+const testString2 = "Hello, World!";
+console.log(isPalindrome(testString2)); // Output: false
