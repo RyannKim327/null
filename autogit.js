e41ef7fbd81855fea8d53b2c92eb22adf636b52f
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:59:38 +0800

    1-29-2024 20:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,17 @@
-const cron = require('node-cron');
-
-// Define a function to be executed periodically
-const task = () => {
-  console.log('Running cron job...');
-  // Your code here
-};
-
-// Schedule the task to run every minute
-cron.schedule('* * * * *', task);
-
-// You can also use cron expressions for more specific schedules
-// Examples:
-// - Run every hour, at the 30th minute: '30 * * * *'
-// - Run every day at 9 AM: '0 9 * * *'
-// - Run every Monday at 3 PM: '0 15 * * 1'
-
-// Keep the script running indefinitely
-console.log('Cron job scheduled.');
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+let index = array.indexOf(elementToRemove);
+if (index > -1) {
+  array.splice(index, 1);
+}
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+array = array.filter(item => item !== elementToRemove);
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] === elementToRemove) {
+    array.splice(i, 1);
+    break; // optional, remove this line if you want to remove all occurrences.
+  }
+}
