ba8f7ddcfffe67f8ee745b15af47da8b28df04fd
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 21:03:57 +0800

    1-29-2024 21:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,12 @@
-function ListNode(value, next) {
-  this.value = value;
-  this.next = next;
-}
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-  let next = null;
-
-  while (current != null) {
-    next = current.next; // temporarily store the next node
-    current.next = prev; // reverse the next pointer
-
-    // move prev and current to the next nodes
-    prev = current;
-    current = next;
+function factorialRecursive(num) {
+  // Base case: factorial of 0 and 1 is 1
+  if (num === 0 || num === 1) {
+    return 1;
   }
 
-  // update the head to point to the new first node
-  head = prev;
-
-  return head; // return the reversed linked list
-}
-function ListNode(value, next) {
-  this.value = value;
-  this.next = next;
+  // Recursive case: call the function again with num-1
+  return num * factorialRecursive(num - 1);
 }
 
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-  let next = null;
-
-  while (current != null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  head = prev;
-
-  return head;
-}
-
-// Create the original linked list
-const node3 = new ListNode(3, null);
-const node2 = new ListNode(2, node3);
-const node1 = new ListNode(1, node2);
-
-// Reverse the linked list
-const reversed = reverseLinkedList(node1);
-
-// Print the reversed linked list
-let currentNode = reversed;
-while (currentNode != null) {
-  console.log(currentNode.value);
-  currentNode = currentNode.next;
-}
+// Example usage:
+console.log(factorialRecursive(5)); // Output: 120
