bdfe3d2016b9a3f15c886952d9b47d87da34a0aa
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:58:04 +0800

    1-29-2024 20:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,37 @@
-class Tarjan {
-  constructor(vertices) {
-    this.vertices = vertices;
-    this.index = 0;
-    this.stack = [];
-    this.ids = new Array(vertices.length).fill(-1);
-    this.low = new Array(vertices.length);
-    this.onStack = new Array(vertices.length).fill(false);
-    this.scc = [];
-  }
-  
-  findSCC() {
-    for (let i = 0; i < this.vertices.length; i++) {
-      if (this.ids[i] === -1) {
-        this.tarjanDFS(i);
-      }
-    }
-    return this.scc;
-  }
-  
-  tarjanDFS(vertex) {
-    this.ids[vertex] = this.index;
-    this.low[vertex] = this.index;
-    this.index++;
-    this.stack.push(vertex);
-    this.onStack[vertex] = true;
-    
-    for (const adjacentVertex of this.vertices[vertex]) {
-      if (this.ids[adjacentVertex] === -1) {
-        this.tarjanDFS(adjacentVertex);
-        this.low[vertex] = Math.min(this.low[vertex], this.low[adjacentVertex]);
-      } else if (this.onStack[adjacentVertex]) {
-        this.low[vertex] = Math.min(this.low[vertex], this.ids[adjacentVertex]);
-      }
-    }
-    
-    if (this.ids[vertex] === this.low[vertex]) {
-      const component = [];
-      let v;
-      
-      do {
-        v = this.stack.pop();
-        this.onStack[v] = false;
-        component.push(v);
-      } while (v !== vertex);
-      
-      this.scc.push(component);
-    }
-  }
+function findCommonElements(arr1, arr2) {
+  return arr1.filter(item => arr2.includes(item));
 }
-const graph = [[1, 2], [0], [3], [4, 5], [3, 6], [5], [4]];
-const tarjan = new Tarjan(graph);
-const scc = tarjan.findSCC();
-const graph = [[1, 2], [0], [3], [4, 5], [3, 6], [5], [4]];
-const tarjan = new Tarjan(graph);
-const scc = tarjan.findSCC();
 
-console.log(scc);  // [[6, 4, 5], [0, 1, 2], [3]]
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
+function findCommonElements(arr1, arr2) {
+  const set1 = new Set(arr1);
+  const commonElements = arr2.filter(item => set1.has(item));
+  
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
+function findCommonElements(arr1, arr2) {
+  const commonElements = arr1.reduce((common, item) => {
+    if (arr2.includes(item)) {
+      common.push(item);
+    }
+    return common;
+  }, []);
+  
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
+
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
