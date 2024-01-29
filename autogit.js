82103f10a7795d046a53245022eb44ce6a0e5e39
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:29:07 +0800

    1-29-2024 20:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,49 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function insertionSort(arr) {
+  // ... implementation goes here
+}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    // ... continue implementation
   }
 }
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-  
-  addNode(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let currentNode = this.head;
-      while (currentNode.next) {
-        currentNode = currentNode.next;
-      }
-      currentNode.next = newNode;
-    }
-  }
-  
-  isPalindrome() {
-    let slow = this.head;
-    let fast = this.head;
-    const stack = [];
-  
-    while (fast && fast.next) {
-      stack.push(slow.value);
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-  
-    if (fast) {
-      slow = slow.next; // Move slow pointer to the middle element for odd-length lists
-    }
-  
-    while (slow) {
-      if (slow.value !== stack.pop()) {
-        return false;
-      }
-      slow = slow.next;
-    }
-  
-    return true;
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    // ... continue implementation
   }
 }
-const linkedList = new LinkedList();
-linkedList.addNode('a');
-linkedList.addNode('b');
-linkedList.addNode('c');
-linkedList.addNode('b');
-linkedList.addNode('a');
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
+    // ... continue implementation
+  }
+}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
 
-console.log(linkedList.isPalindrome()); // Output: true
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
 
-linkedList.addNode('d');
+    // ... continue implementation
+  }
+}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
 
-console.log(linkedList.isPalindrome()); // Output: false
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentValue;
+  }
+
+  return arr;
+}
