21dbe30c539c5a2b951fdef93771d6710bc52ceb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:56:14 +0800

    1-29-2024 20:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,28 @@
-const array = [9, 5, 7, 2, 4, 1, 6, 3, 8];
+class ListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-console.log("Original Array:", array);
-console.log("Sorted Array:", heapSort(array));
-Original Array: [9, 5, 7, 2, 4, 1, 6, 3, 8]
-Sorted Array: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.value;
+}
+// Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+// Find the middle element
+const middleElement = findMiddleElement(head);
+console.log(middleElement); // Output: 3
