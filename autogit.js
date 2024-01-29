68c3109a2e511c00afd0c94c6f74174ed1bd15b5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:37:44 +0800

    1-29-2024 20:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,21 @@
-function isPalindrome(head) {
-  if (!head || !head.next) {
-    return true; // Empty list or single node is a palindrome
-  }
-
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    fast = fast.next.next;
-    slow = slow.next;
-  }
-
-  slow = reverseLinkedList(slow);
-
-  while (head && slow) {
-    if (head.val !== slow.val) {
-      return false;
+function bubbleSort(arr) {
+  var len = arr.length;
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
     }
-    head = head.next;
-    slow = slow.next;
-  }
-
-  return true;
+    len--;
+  } while (swapped);
+  return arr;
 }
 
-function reverseLinkedList(node) {
-  let prev = null;
-  let current = node;
-  let next = null;
-
-  while (current) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  return prev;
-}
+// Example usage:
+var numbers = [5, 3, 8, 4, 2];
+console.log(bubbleSort(numbers)); // output: [2, 3, 4, 5, 8]
