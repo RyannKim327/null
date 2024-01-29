5cc834cdbce2fc5259b5a93f05d1bd38d59c11a8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 21:03:25 +0800

    1-29-2024 21:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,15 @@
-function rabinKarp(text, pattern) {
-    const textLength = text.length;
-    const patternLength = pattern.length;
-    const prime = 101;
+function isPrime(number) {
+  if (number <= 1) { // 0 and 1 are not prime
+    return false;
+  }
 
-    if (patternLength > textLength) {
-        return -1;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false; // If divisible by any number from 2 to sqrt(number), it's not prime
     }
+  }
 
-    function hash(str, length) {
-        let hashValue = 0;
-        for (let i = 0; i < length; i++) {
-            hashValue += str.charCodeAt(i) * Math.pow(prime, i);
-        }
-        return hashValue;
-    }
-
-    function rehash(oldHash, oldChar, newChar, patternLength) {
-        return (oldHash - oldChar.charCodeAt(0)) / prime + newChar.charCodeAt(0) * Math.pow(prime, patternLength - 1);
-    }
-
-    const patternHash = hash(pattern, patternLength);
-    let textHash = hash(text, patternLength);
-
-    for (let i = 0; i <= textLength - patternLength; i++) {
-        if (textHash === patternHash && text.substring(i, i + patternLength) === pattern) {
-            return i; // Pattern found at index i
-        }
-        if (i < textLength - patternLength) {
-            textHash = rehash(textHash, text[i], text[i + patternLength], patternLength);
-        }
-    }
-
-    return -1;
+  return true; // If not divisible by any number, it's prime
 }
-const text = "This is a sample text";
-const pattern = "sample";
-
-const result = rabinKarp(text, pattern);
-console.log(result); // Output: 10
+const number = 17;
+console.log(isPrime(number)); // Output: true
