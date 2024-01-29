3c453fe27dd3739d5bd2e8d26251363611b35bb9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-29 20:45:40 +0800

    1-29-2024 20:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,19 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array);
-[5, 4, 3, 2, 1]
+function largestPrimeFactor(number) {
+  let factor = 2;
+  
+  while (factor * factor <= number) {
+    if (number % factor === 0) {
+      number = number / factor;
+    } else {
+      factor++;
+    }
+  }
+  
+  return number;
+}
+
+// Example usage
+const number = 13195;
+const largestFactor = largestPrimeFactor(number);
+
+console.log("Largest prime factor of", number, "is", largestFactor);
