a5381f0d962fc091e32684c58b8588edd9a98ae8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:24:00 +0800

    1-30-2024 19:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,67 @@
-function findMajorityElement(nums) {
-  let count = 0;
-  let majorityElement;
-
-  // Step 1: Find the potential majority element
-  for (let i = 0; i < nums.length; i++) {
-    if (count === 0) {
-      majorityElement = nums[i];
-    }
-
-    if (nums[i] === majorityElement) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Step 2: Verify if the potential majority element is actually the majority
-  count = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === majorityElement) {
-      count++;
-    }
-  }
-
-  if (count > nums.length / 2) {
-    return majorityElement;
-  } else {
-    return "No majority element found.";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-// Example usage:
-const array = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(array);
-console.log("Majority Element:", majorityElement);
+  addNode(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+function isPalindrome(list) {
+  // Step 1: Find the middle of the linked list
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Step 2: Reverse the second half of the linked list
+  let prev = null;
+  let current = slow;
+  while (current) {
+    const nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
+
+  // Step 3: Compare the first half and reversed second half of the linked list
+  let firstHalf = list.head;
+  let secondHalf = prev;
+
+  while (secondHalf) {
+    if (firstHalf.value !== secondHalf.value) {
+      return false;
+    }
+    firstHalf = firstHalf.next;
+    secondHalf = secondHalf.next;
+  }
+
+  return true;
+}
+const myList = new LinkedList();
+myList.addNode(1);
+myList.addNode(2);
+myList.addNode(3);
+myList.addNode(2);
+myList.addNode(1);
+
+console.log(isPalindrome(myList)); // Output: true
