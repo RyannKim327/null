724d0e1d9fb29f1b46230bed6c4af77ed6950933
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:38:29 +0800

    1-30-2024 19:38 [User mode]

---
+++ autogit.js
@@ -0,0 +1,12 @@
+function getStringLength(str) {
+  let length = 0;
+  while (str[length] !== undefined) {
+    length++;
+  }
+  return length;
+}
+
+// Example usage
+const myString = "Hello, world!";
+const length = getStringLength(myString);
+console.log(length); // Output: 13
