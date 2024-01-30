7f6fffbe032e53868c0398fb599fbba7b036ab25
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:29:17 +0800

    1-30-2024 19:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,124 +1,34 @@
-// Node class representing each element in the linked list
+// Define the binary tree node structure
 class Node {
-    constructor(data) {
-        this.data = data; // value of the node
-        this.next = null; // reference to the next node
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-// Linked list class to manage the nodes
-class LinkedList {
-    constructor() {
-        this.head = null; // first node in the list
-        this.tail = null; // last node in the list
-    }
-
-    // Add a new node to the end of the linked list
-    append(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-    }
-
-    // Print all the nodes in the linked list
-    print() {
-        let currentNode = this.head;
-
-        while (currentNode) {
-            console.log(currentNode.data);
-            currentNode = currentNode.next;
-        }
-    }
-
-    // Get the length of the linked list
-    length() {
-        let count = 0;
-        let currentNode = this.head;
-
-        while (currentNode) {
-            count++;
-            currentNode = currentNode.next;
-        }
-
-        return count;
-    }
-
-    // Insert a new node at a specific position in the linked list
-    insertAt(position, data) {
-        if (position < 0 || position > this.length()) {
-            throw new Error('Invalid position');
-        }
-
-        const newNode = new Node(data);
-
-        if (position === 0) {
-            newNode.next = this.head;
-            this.head = newNode;
-        } else {
-            let previousNode = null;
-            let currentNode = this.head;
-            let currentPosition = 0;
-
-            while (currentPosition < position) {
-                previousNode = currentNode;
-                currentNode = currentNode.next;
-                currentPosition++;
-            }
-
-            newNode.next = currentNode;
-            previousNode.next = newNode;
-        }
-    }
-
-    // Remove a node at a specific position in the linked list
-    removeAt(position) {
-        if (position < 0 || position >= this.length()) {
-            throw new Error('Invalid position');
-        }
-
-        let previousNode = null;
-        let currentNode = this.head;
-        let currentPosition = 0;
-
-        if (position === 0) {
-            this.head = currentNode.next;
-        } else {
-            while (currentPosition < position) {
-                previousNode = currentNode;
-                currentNode = currentNode.next;
-                currentPosition++;
-            }
-
-            previousNode.next = currentNode.next;
-
-            if (position === this.length() - 1) {
-                this.tail = previousNode;
-            }
-        }
-    }
-
-    // Clear the linked list
-    clear() {
-        this.head = null;
-        this.tail = null;
-    }
+// Function to count the number of leaf nodes in a binary tree
+function countLeafNodes(root) {
+  if (root === null) {
+    return 0; // If the root is null, there are no leaf nodes
+  } else if (root.left === null && root.right === null) {
+    return 1; // If the current node has no children, it is a leaf node
+  } else {
+    // Recursively count the leaf nodes in the left and right subtrees
+    return countLeafNodes(root.left) + countLeafNodes(root.right);
+  }
 }
 
-// Usage example:
-const linkedList = new LinkedList();
-linkedList.append(10);
-linkedList.append(20);
-linkedList.append(30);
-linkedList.print(); // Output: 10 20 30
+// Example usage:
+// Create the binary tree
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
 
-linkedList.insertAt(1, 15);
-linkedList.print(); // Output: 10 15 20 30
-
-linkedList.removeAt(2);
-linkedList.print(); // Output: 10 15 30
+// Count the number of leaf nodes
+const leafNodeCount = countLeafNodes(root);
+console.log("Number of leaf nodes:", leafNodeCount);
