e98e35b4d60aba224e5524776b3cf59ec9338576
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:14:59 +0800

    1-30-2024 19:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,9 @@
-const array = [1, 2, 3, 4, 2, 1];
-const uniqueArray = Array.from(new Set(array));
-console.log(uniqueArray); // Output: [1, 2, 3, 4]
-const array = [1, 2, 3, 4, 2, 1];
-const uniqueArray = array.filter((element, index) => array.indexOf(element) === index);
-console.log(uniqueArray); // Output: [1, 2, 3, 4]
-const array = [1, 2, 3, 4, 2, 1];
-const uniqueArray = array.reduce((accumulator, currentValue) => {
-  if (!accumulator.includes(currentValue)) {
-    accumulator.push(currentValue);
-  }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // Output: [1, 2, 3, 4]
+let str = "Hello, World! How are you?";
+let result = str.replace(/\s/g, '');
+console.log(result);
+let str = "Hello, World! How are you?";
+let result = str.split(' ').join('');
+console.log(result);
+let str = "Hello, World! How are you?";
+let result = str.split(/\s/g).join('');
+console.log(result);
