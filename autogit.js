ff9d538c0f4e942b747db1f175d9d2749aaba0a7
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:17:45 +0800

    1-30-2024 19:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,4 @@
-function getRandomNumber(min, max) {
-  min = Math.ceil(min); // Round up to the nearest whole number
-  max = Math.floor(max); // Round down to the nearest whole number
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+
+console.log(lowercaseStr); // Output: "hello world"
