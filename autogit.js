dd6f465d0895ad4392edeb8343bdaea34f1d5041
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:38:55 +0800

    1-30-2024 19:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,74 +1,10 @@
-function Graph() {
-  this.nodes = {};
+area = (base * height) / 2;
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
-Graph.prototype.addNode = function (node) {
-  this.nodes[node] = [];
-};
-Graph.prototype.addEdge = function (src, dest) {
-  this.nodes[src].push(dest);
-};
-Graph.prototype.tarjanSCC = function () {
-  this.index = 0;
-  this.stack = [];
-  this.visited = {};
-  this.lowLink = {};
-  this.scc = [];
 
-  for (var node in this.nodes) {
-    if (!this.visited[node]) {
-      this.tarjan(node);
-    }
-  }
-
-  return this.scc;
-};
-Graph.prototype.tarjan = function (node) {
-  this.visited[node] = this.index;
-  this.lowLink[node] = this.index;
-  this.index++;
-  this.stack.push(node);
-
-  var neighbors = this.nodes[node];
-
-  for (var i = 0; i < neighbors.length; i++) {
-    var neighbor = neighbors[i];
-
-    if (!this.visited[neighbor]) {
-      this.tarjan(neighbor);
-      this.lowLink[node] = Math.min(this.lowLink[node], this.lowLink[neighbor]);
-    } else if (this.stack.includes(neighbor)) {
-      this.lowLink[node] = Math.min(this.lowLink[node], this.visited[neighbor]);
-    }
-  }
-
-  if (this.lowLink[node] === this.visited[node]) {
-    var component = [];
-    var nextNode;
-
-    do {
-      nextNode = this.stack.pop();
-      component.push(nextNode);
-    } while (nextNode !== node);
-
-    this.scc.push(component);
-  }
-};
-// Example usage
-var graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addNode('E');
-graph.addNode('F');
-
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'C');
-graph.addEdge('C', 'A');
-graph.addEdge('B', 'D');
-graph.addEdge('D', 'E');
-graph.addEdge('E', 'F');
-graph.addEdge('F', 'D');
-
-var sccs = graph.tarjanSCC();
-console.log(sccs);
+var base = 5;
+var height = 7;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
