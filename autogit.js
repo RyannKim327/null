d1cae0c03930dc58922e157c31228b62133d5929
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:19:14 +0800

    1-30-2024 19:19 [User mode]

---
+++ autogit.js
@@ -0,0 +1,10 @@
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
+}
+
+// Example usage
+var base = 5;
+var height = 7;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
