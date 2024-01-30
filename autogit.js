1748243ae49e0db5424d1cc2cdd07324c0dda171
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:33:03 +0800

    1-30-2024 19:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,42 @@
-const array = [10, 5, 20, 15, 8];
-const max = Math.max(...array);
+function generateFibonacciSequence(limit) {
+  const sequence = [0, 1];
+  
+  while (sequence[sequence.length - 1] < limit) {
+    const nextNumber = sequence[sequence.length - 1] + sequence[sequence.length - 2];
+    sequence.push(nextNumber);
+  }
+  
+  return sequence;
+}
+function fibonacciSearch(array, target) {
+  const fibonacciSeq = generateFibonacciSequence(array.length);
+  let fib2 = fibonacciSeq[fibonacciSeq.length - 2];
+  let fib1 = fibonacciSeq[fibonacciSeq.length - 3];
+  let offset = -1;
 
-console.log(max); // Output: 20
+  while (fib2 > 1) {
+    const index = Math.min(offset + fib1, array.length - 1);
+
+    if (array[index] < target) {
+      fib2 = fib1;
+      fib1 = fibonacciSeq[fibonacciSeq.indexOf(fib2) - 1];
+      offset = index;
+    } else if (array[index] > target) {
+      fib2 = fib2 - fib1;
+      fib1 = fib1 - fib2;
+    } else {
+      return index;
+    }
+  }
+
+  if (fib1 && array[offset + 1] === target) {
+    return offset + 1;
+  }
+
+  return -1;
+}
+const array = [1, 3, 5, 7, 9, 11, 13, 15];
+const target = 7;
+const index = fibonacciSearch(array, target);
+
+console.log(`Target ${target} found at index ${index}`);
