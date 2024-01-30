d2ec91633979b2a1f2183fd2ef9f682851d2f5b9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:16:59 +0800

    1-30-2024 19:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,66 +1,57 @@
-class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-class BinaryTree {
-  constructor() {
-    this.root = null;
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
   }
 
-  // Method to insert a value into the binary tree
-  insert(value) {
-    // If the tree is empty, create a new node and set it as the root
-    if (this.root === null) {
-      this.root = new TreeNode(value);
-    } else {
-      this._insertNode(this.root, value);
+  // Hash function that converts a key into an index
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
     }
+    return hash % this.size;
   }
 
-  // Private helper method to recursively insert a value into the tree
-  _insertNode(node, value) {
-    if (value < node.value) {
-      // If the value is less than the current node, go to the left subtree
-      if (node.left === null) {
-        node.left = new TreeNode(value);
-      } else {
-        this._insertNode(node.left, value);
-      }
-    } else {
-      // If the value is greater than or equal to the current node, go to the right subtree
-      if (node.right === null) {
-        node.right = new TreeNode(value);
-      } else {
-        this._insertNode(node.right, value);
+  // Insert a key-value pair into the hash table
+  insert(key, value) {
+    const index = this.hash(key);
+    if (this.table[index] === undefined) {
+      this.table[index] = [];
+    }
+    this.table[index].push({ key, value });
+  }
+
+  // Retrieve a value from the hash table given a key
+  get(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i].key === key) {
+          return this.table[index][i].value;
+        }
       }
     }
+    return undefined;
   }
 
-  // Method to traverse the tree in-order (left, root, right)
-  inOrderTraversal() {
-    this._inOrder(this.root);
-  }
-
-  // Private helper method to recursively perform in-order traversal
-  _inOrder(node) {
-    if (node !== null) {
-      this._inOrder(node.left);
-      console.log(node.value);
-      this._inOrder(node.right);
+  // Remove a value from the hash table given a key
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i].key === key) {
+          this.table[index].splice(i, 1);
+          return;
+        }
+      }
     }
   }
 }
-const tree = new BinaryTree();
-
-tree.insert(5);
-tree.insert(3);
-tree.insert(7);
-tree.insert(2);
-tree.insert(4);
-tree.insert(6);
-tree.insert(8);
-
-tree.inOrderTraversal(); // Outputs: 2, 3, 4, 5, 6, 7, 8
+const hashTable = new HashTable(10);
+hashTable.insert('key1', 'value1');
+hashTable.insert('key2', 'value2');
+console.log(hashTable.get('key1'));  // Output: value1
+console.log(hashTable.get('key2'));  // Output: value2
+hashTable.remove('key1');
+console.log(hashTable.get('key1'));  // Output: undefined
