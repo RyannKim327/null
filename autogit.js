ac498e51932def0798669534dfddf183f1b692d4
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:16:38 +0800

    1-30-2024 19:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,48 @@
-let str = "123";
-let num = parseInt(str);
+class Stack {
+  constructor() {
+    this.items = []; // The array to store stack elements
+  }
 
-console.log(num); // Output: 123
-console.log(typeof num); // Output: number
+  // Push element to the top of the stack
+  push(element) {
+    this.items.push(element);
+  }
+
+  // Remove and return the top element from the stack
+  pop() {
+    if (this.items.length === 0) {
+      return 'Underflow';
+    }
+    return this.items.pop();
+  }
+
+  // Get the top element of the stack without removing it
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // Get the number of elements in the stack
+  size() {
+    return this.items.length;
+  }
+
+  // Print the stack elements
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+// Usage example:
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.pop();
+console.log(stack.peek()); // Output: 20
+console.log(stack.size()); // Output: 2
+stack.print(); // Output: 10, 20
