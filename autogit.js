62e85d3eb97e0f3ba2a15ffc07ca6d1df3168f4e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:28:45 +0800

    1-30-2024 19:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,18 @@
-const axios = require('axios');
-
-// Function to make GET request using axios
-async function requestData(url) {
-  try {
-    const response = await axios.get(url);
-    console.log('Response:', response.data);
-    // Do something with the response data
-  } catch (error) {
-    console.error('Error:', error.message);
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements using a temporary variable
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
   }
+  return arr;
 }
 
-// Example usage
-const apiUrl = 'https://jsonplaceholder.typicode.com/posts';
-requestData(apiUrl);
+// Test the bubbleSort function
+var arr = [5, 3, 8, 4, 2];
+console.log(bubbleSort(arr));  // Output: [2, 3, 4, 5, 8]
