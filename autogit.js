877806d21fdf84b424ab37c9ff7ce056a1a3f943
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:23:10 +0800

    1-30-2024 19:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,156 +1,125 @@
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null; // Pointer to the next node
+  constructor(value, level) {
+    this.value = value;
+    this.forward = new Array(level + 1);
   }
 }
-
-class LinkedList {
+class SkipList {
   constructor() {
-    this.head = null; // Pointer to the first node
-    this.tail = null; // Pointer to the last node
-    this.length = 0; // Number of nodes in the list
+    this.level = 0;
+    this.head = new Node(null, 0);
   }
 
-  // Add a node to the end of the list
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+  // Helper function to generate random level for a node
+  generateRandomLevel() {
+    let level = 0;
+    while (Math.random() < 0.5 && level < this.level + 1) {
+      level++;
     }
-
-    this.length++;
+    return level;
   }
 
-  // Insert a node at a specific position in the list
-  insert(position, data) {
-    if (position < 0 || position > this.length) {
-      return false; // Invalid position
-    }
-
-    const newNode = new Node(data);
-
-    if (position === 0) {
-      newNode.next = this.head;
-      this.head = newNode;
-      if (!this.tail) {
-        // If the list was initially empty
-        this.tail = newNode;
-      }
-    } else if (position === this.length) {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    } else {
-      let current = this.head;
-      let previous = null;
-      let index = 0;
-
-      while (index < position) {
-        previous = current;
-        current = current.next;
-        index++;
-      }
-
-      newNode.next = current;
-      previous.next = newNode;
-    }
-
-    this.length++;
-    return true;
-  }
-
-  // Remove a node at a specific position in the list
-  removeAt(position) {
-    if (position < 0 || position >= this.length) {
-      return null; // Invalid position
-    }
-
+  // Insert a value into the Skip List
+  insert(value) {
+    const update = new Array(this.level + 1);
     let current = this.head;
-    let previous = null;
-    let index = 0;
 
-    if (position === 0) {
-      this.head = current.next;
-      if (position === this.length - 1) {
-        // If the head was the only node
-        this.tail = null;
+    // Find the appropriate position to insert the new node
+    for (let i = this.level; i >= 0; i--) {
+      while (current.forward[i] && current.forward[i].value < value) {
+        current = current.forward[i];
       }
-    } else {
-      while (index < position) {
-        previous = current;
-        current = current.next;
-        index++;
-      }
-
-      previous.next = current.next;
-      if (position === this.length - 1) {
-        // If removing the last node, update tail
-        this.tail = previous;
-      }
+      update[i] = current;
     }
 
-    this.length--;
-    return current.data;
+    current = current.forward[0];
+
+    if (!current || current.value !== value) {
+      const newNodeLevel = this.generateRandomLevel();
+      const newNode = new Node(value, newNodeLevel);
+
+      // If the new node has a higher level than the current skip list level, update the update array and the skip list level
+      if (newNodeLevel > this.level) {
+        for (let i = this.level + 1; i <= newNodeLevel; i++) {
+          update[i] = this.head;
+        }
+        this.level = newNodeLevel;
+      }
+
+      // Insert the new node into the skip list
+      for (let i = 0; i <= newNodeLevel; i++) {
+        newNode.forward[i] = update[i].forward[i];
+        update[i].forward[i] = newNode;
+      }
+    }
   }
 
-  // Get the data at a specific position in the list
-  get(position) {
-    if (position < 0 || position >= this.length) {
-      return null; // Invalid position
-    }
-
+  // Search for a value in the Skip List
+  search(value) {
     let current = this.head;
-    let index = 0;
+    for (let i = this.level; i >= 0; i--) {
+      while (current.forward[i] && current.forward[i].value < value) {
+        current = current.forward[i];
+      }
+    }
+    current = current.forward[0];
+    if (current && current.value === value) {
+      return true;
+    }
+    return false;
+  }
 
-    while (index < position) {
-      current = current.next;
-      index++;
+  // Remove a value from the Skip List
+  remove(value) {
+    const update = new Array(this.level + 1);
+    let current = this.head;
+
+    // Find the node to be removed
+    for (let i = this.level; i >= 0; i--) {
+      while (current.forward[i] && current.forward[i].value < value) {
+        current = current.forward[i];
+      }
+      update[i] = current;
     }
 
-    return current.data;
+    current = current.forward[0];
+
+    if (current && current.value === value) {
+      // Remove the node from the skip list
+      for (let i = 0; i <= this.level; i++) {
+        if (update[i].forward[i] === current) {
+          update[i].forward[i] = current.forward[i];
+        }
+      }
+
+      // Update the skip list level if necessary
+      while (this.level > 0 && this.head.forward[this.level] === null) {
+        this.level--;
+      }
+
+      return true;
+    }
+
+    return false;
   }
 
-  // Check if the list is empty
-  isEmpty() {
-    return this.length === 0;
-  }
-
-  // Get the number of nodes in the list
-  size() {
-    return this.length;
-  }
-
-  // Print the data of all nodes in the list
+  // Print the Skip List
   print() {
     let current = this.head;
-    let result = '';
-
-    while (current) {
-      result += current.data + ' ';
-      current = current.next;
+    while (current.forward[0]) {
+      console.log(current.forward[0].value);
+      current = current.forward[0];
     }
-
-    console.log(result.trim());
   }
 }
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
+const skipList = new SkipList();
+skipList.insert(3);
+skipList.insert(7);
+skipList.insert(2);
+skipList.insert(1);
 
-list.print(); // Output: 1 2 3
+console.log(skipList.search(3)); // Output: true
+console.log(skipList.search(5)); // Output: false
 
-list.insert(1, 4);
-list.print(); // Output: 1 4 2 3
-
-list.removeAt(2);
-list.print(); // Output: 1 4 3
-
-console.log(list.get(1)); // Output: 4
-console.log(list.size()); // Output: 3
-console.log(list.isEmpty()); // Output: false
+skipList.remove(2);
+skipList.print(); // Output: 1, 3, 7
