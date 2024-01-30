6889655cfc047241162b3930dd9b27e3b7651bfb
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:39:59 +0800

    1-30-2024 19:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,12 @@
-function findMajorityElement(array) {
-  let majorityElement = array[0];
-  let count = 1;
-
-  for (let i = 1; i < array.length; i++) {
-    if (count === 0) {
-      majorityElement = array[i];
-      count = 1;
-    } else if (array[i] === majorityElement) {
-      count++;
-    } else {
-      count--;
-    }
+function getStringLength(str) {
+  let count = 0;
+  for (let i = 0; str[i] !== undefined; i++) {
+    count++;
   }
-
-  // At this point, we have a potential majority element.
-  // We need to verify if it actually occurs more than n/2 times.
-
-  count = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === majorityElement) {
-      count++;
-    }
-  }
-
-  if (count > array.length / 2) {
-    return majorityElement;
-  } else {
-    return -1; // No majority element exists.
-  }
+  return count;
 }
 
 // Example usage:
-const array = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(array);
-
-if (majorityElement !== -1) {
-  console.log("The majority element is: " + majorityElement);
-} else {
-  console.log("There is no majority element.");
-}
+const myString = "Hello, world!";
+const length = getStringLength(myString);
+console.log(length); // Output: 13
