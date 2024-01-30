c65af2a507fd4e4a7f1621579f6851d41762c4bf
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:22:19 +0800

    1-30-2024 19:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,108 @@
-function beamSearch(problem, width, maxIterations) {
-  let beam = [problem.initialSolution()];
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
 
-  for (let iteration = 0; iteration < maxIterations; iteration++) {
-    let successors = [];
+  insert(value) {
+    const newNode = new TreeNode(value);
 
-    for (let solution of beam) {
-      let newSuccessors = problem.generateSuccessors(solution);
-      successors.push(...newSuccessors);
-    }
-
-    successors.sort((a, b) => problem.evaluate(a) - problem.evaluate(b));
-    beam = successors.slice(0, width);
-
-    if (problem.isTermination(beam[0])) {
-      break;
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
     }
   }
 
-  return beam[0];
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (node.value === value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
+  preOrderTraversal(callback) {
+    this.preOrderTraversalNode(this.root, callback);
+  }
+
+  preOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      callback(node.value);
+      this.preOrderTraversalNode(node.left, callback);
+      this.preOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
+    }
+  }
+
+  postOrderTraversal(callback) {
+    this.postOrderTraversalNode(this.root, callback);
+  }
+
+  postOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.postOrderTraversalNode(node.left, callback);
+      this.postOrderTraversalNode(node.right, callback);
+      callback(node.value);
+    }
+  }
 }
+const tree = new BinaryTree();
 
-// Example usage
+tree.insert(7);
+tree.insert(4);
+tree.insert(9);
+tree.insert(1);
+tree.insert(6);
 
-const problem = {
-  initialSolution: () => [0], // Your initial solution representation
-  generateSuccessors: (solution) => [solution.concat([1]), solution.concat([2])], // Generate successors based on current solution
-  evaluate: (solution) => solution.reduce((a, b) => a + b), // Your evaluation function, higher is better
-  isTermination: (solution) => solution.length === 5 // Terminate search when solution length is 5
-};
+console.log(tree.search(6)); // Output: true
+console.log(tree.search(8)); // Output: false
 
-const width = 2; // Width of the beam, top-k solutions to keep
-const maxIterations = 10; // Maximum iterations before terminating
-
-const optimalSolution = beamSearch(problem, width, maxIterations);
-console.log("Optimal solution:", optimalSolution);
+tree.preOrderTraversal(value => console.log(value)); // Output: 7 4 1 6 9
+tree.inOrderTraversal(value => console.log(value)); // Output: 1 4 6 7 9
+tree.postOrderTraversal(value => console.log(value)); // Output: 1 6 4 9 7
