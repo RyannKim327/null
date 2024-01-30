0cf6766c92d615fb9f4a90fbc126119b40d1e578
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:29:10 +0800

    1-30-2024 19:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,124 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap elements using a temporary variable
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+// Node class representing each element in the linked list
+class Node {
+    constructor(data) {
+        this.data = data; // value of the node
+        this.next = null; // reference to the next node
     }
-  }
-  return arr;
 }
 
-// Test the bubbleSort function
-var arr = [5, 3, 8, 4, 2];
-console.log(bubbleSort(arr));  // Output: [2, 3, 4, 5, 8]
+// Linked list class to manage the nodes
+class LinkedList {
+    constructor() {
+        this.head = null; // first node in the list
+        this.tail = null; // last node in the list
+    }
+
+    // Add a new node to the end of the linked list
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+
+    // Print all the nodes in the linked list
+    print() {
+        let currentNode = this.head;
+
+        while (currentNode) {
+            console.log(currentNode.data);
+            currentNode = currentNode.next;
+        }
+    }
+
+    // Get the length of the linked list
+    length() {
+        let count = 0;
+        let currentNode = this.head;
+
+        while (currentNode) {
+            count++;
+            currentNode = currentNode.next;
+        }
+
+        return count;
+    }
+
+    // Insert a new node at a specific position in the linked list
+    insertAt(position, data) {
+        if (position < 0 || position > this.length()) {
+            throw new Error('Invalid position');
+        }
+
+        const newNode = new Node(data);
+
+        if (position === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            let previousNode = null;
+            let currentNode = this.head;
+            let currentPosition = 0;
+
+            while (currentPosition < position) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+                currentPosition++;
+            }
+
+            newNode.next = currentNode;
+            previousNode.next = newNode;
+        }
+    }
+
+    // Remove a node at a specific position in the linked list
+    removeAt(position) {
+        if (position < 0 || position >= this.length()) {
+            throw new Error('Invalid position');
+        }
+
+        let previousNode = null;
+        let currentNode = this.head;
+        let currentPosition = 0;
+
+        if (position === 0) {
+            this.head = currentNode.next;
+        } else {
+            while (currentPosition < position) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+                currentPosition++;
+            }
+
+            previousNode.next = currentNode.next;
+
+            if (position === this.length() - 1) {
+                this.tail = previousNode;
+            }
+        }
+    }
+
+    // Clear the linked list
+    clear() {
+        this.head = null;
+        this.tail = null;
+    }
+}
+
+// Usage example:
+const linkedList = new LinkedList();
+linkedList.append(10);
+linkedList.append(20);
+linkedList.append(30);
+linkedList.print(); // Output: 10 20 30
+
+linkedList.insertAt(1, 15);
+linkedList.print(); // Output: 10 15 20 30
+
+linkedList.removeAt(2);
+linkedList.print(); // Output: 10 15 30
