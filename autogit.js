16048097221e928990b291a61427c871c1ab19ea
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:35:35 +0800

    1-30-2024 19:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,11 @@
-const graph = {}; // Adjacency list representation of the graph
-const stack = []; // Stack to keep track of visited vertices
-const visited = {}; // Object to keep track of visited vertices
-const lowLink = {}; // Object to store the low link values
-const ids = {}; // Object to assign unique ids to each vertex
-let id = 0; // Variable to assign a unique id to each vertex
-const scc = []; // Array to store the strongly connected components
-function tarjansAlgorithm(node) {
-  visited[node] = true;
-  lowLink[node] = ids[node] = id++;
-  stack.push(node);
+let str1 = "Hello";
+let str2 = "world";
 
-  const neighbors = graph[node];
+// Using the "+" operator
+let result1 = str1 + " " + str2;
+console.log(result1); // Output: Hello world
 
-  for (let i = 0; i < neighbors.length; i++) {
-    const neighbor = neighbors[i];
-
-    if (!visited[neighbor]) {
-      tarjansAlgorithm(neighbor);
-      lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
-    } else if (stack.includes(neighbor)) {
-      lowLink[node] = Math.min(lowLink[node], ids[neighbor]);
-    }
-  }
-
-  if (lowLink[node] === ids[node]) {
-    const component = [];
-    let currNode;
-
-    do {
-      currNode = stack.pop();
-      component.push(currNode);
-    } while (currNode !== node);
-
-    scc.push(component);
-  }
-}
-function findStronglyConnectedComponents() {
-  for (const node in graph) {
-    if (!visited[node]) {
-      tarjansAlgorithm(node);
-    }
-  }
-}
-// Example graph
-graph['A'] = ['B'];
-graph['B'] = ['C', 'E', 'F'];
-graph['C'] = ['D', 'G'];
-graph['D'] = ['C', 'H'];
-graph['E'] = ['A', 'F'];
-graph['F'] = ['G'];
-graph['G'] = ['F'];
-graph['H'] = ['D', 'G'];
-
-findStronglyConnectedComponents();
-
-console.log(scc);
+// Using the "+=" operator
+let result2 = str1;
+result2 += " " + str2;
+console.log(result2); // Output: Hello world
