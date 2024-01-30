b51c9de14f4917eab395b3880ee5ffec43e745f3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-30 19:39:09 +0800

    1-30-2024 19:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,47 @@
-area = (base * height) / 2;
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
-}
+class Graph {
+  constructor() {
+    this.nodes = new Map();
+  }
 
-var base = 5;
-var height = 7;
-var triangleArea = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + triangleArea);
+  addNode(node) {
+    this.nodes.set(node, []);
+  }
+
+  addEdge(node1, node2) {
+    this.nodes.get(node1).push(node2);
+    this.nodes.get(node2).push(node1);
+  }
+
+  getNeighbors(node) {
+    return this.nodes.get(node);
+  }
+}
+function depthFirstSearch(graph, startNode) {
+  const visited = new Set();
+
+  function dfs(node) {
+    visited.add(node);
+    console.log(node); // or do any other processing
+
+    const neighbors = graph.getNeighbors(node);
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor);
+      }
+    }
+  }
+
+  dfs(startNode);
+}
+const graph = new Graph();
+graph.addNode('A');
+graph.addNode('B');
+graph.addNode('C');
+graph.addNode('D');
+graph.addNode('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'D');
+graph.addEdge('D', 'E');
+depthFirstSearch(graph, 'A');
