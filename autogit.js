c60f4e2c6723f8576f3174c4a1f86e05a31780d2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:03:58 +0800

    1-31-2024 22:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,46 @@
-let str = "Hello, World! This is a string with spaces.";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-let stringWithoutSpaces = str.split(' ').join('');
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
 
-console.log(stringWithoutSpaces);
-Hello,World!Thisisastringwithspaces.
-let str = "Hello, World! This is a string with spaces.";
+  while (current !== null) {
+    let nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
 
-let stringWithoutSpaces = str.replace(/\s/g, '');
+  return prev;
+}
+// Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+let head = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+let node5 = new Node(5);
 
-console.log(stringWithoutSpaces);
-Hello,World!Thisisastringwithspaces.
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+// Reverse the linked list
+let newHead = reverseLinkedList(head);
+
+// Print the reversed linked list
+let current = newHead;
+while (current !== null) {
+  console.log(current.value);
+  current = current.next;
+}
+5
+4
+3
+2
+1
