bde1e65d11750e5e61c501cfe7a338ad01ccb576
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:04:50 +0800

    1-31-2024 22:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,7 @@
-function areAnagrams(str1, str2) {
-  // Remove non-alphabetic characters and convert to lowercase
-  str1 = str1.replace(/[^a-z]/gi, '').toLowerCase();
-  str2 = str2.replace(/[^a-z]/gi, '').toLowerCase();
-
-  // Sort the strings
-  const sortedStr1 = str1.split('').sort().join('');
-  const sortedStr2 = str2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
 
 // Example usage
-const string1 = 'listen';
-const string2 = 'silent';
-
-if (areAnagrams(string1, string2)) {
-  console.log('The strings are anagrams.');
-} else {
-  console.log('The strings are not anagrams.');
-}
+console.log(decimalToBinary(10));  // Output: 1010
+console.log(decimalToBinary(27));  // Output: 11011
