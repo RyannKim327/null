b4b31adf9ae95e75ff75dd6f81a4959818d45eb3
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:44:08 +0800

    1-31-2024 22:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,25 @@
-function mergeSort(inputArray) {
-  let subarraySize = 1;
-  let tempArray = [];
-  
-  while (subarraySize < inputArray.length) {
-    let startIndex = 0;
-    let tempIndex = 0;
-    
-    while (startIndex < inputArray.length) {
-      const midIndex = Math.min(startIndex + subarraySize - 1, inputArray.length - 1);
-      const leftIndex = startIndex;
-      const rightIndex = midIndex + 1;
-      
-      const leftArray = [];
-      const rightArray = [];
-      
-      while (leftIndex <= midIndex) {
-        leftArray.push(inputArray[leftIndex]);
-        leftIndex++;
-      }
-      
-      while (rightIndex <= midIndex + subarraySize && rightIndex < inputArray.length) {
-        rightArray.push(inputArray[rightIndex]);
-        rightIndex++;
-      }
-      
-      let index = startIndex;
-      while (leftArray.length > 0 && rightArray.length > 0) {
-        if (leftArray[0] <= rightArray[0]) {
-          inputArray[index] = leftArray.shift();
-        } else {
-          inputArray[index] = rightArray.shift();
-        }
-        index++;
-      }
-      
-      while (leftArray.length > 0) {
-        inputArray[index] = leftArray.shift();
-        index++;
-      }
-      
-      while (rightArray.length > 0) {
-        inputArray[index] = rightArray.shift();
-        index++;
-      }
-      
-      startIndex += subarraySize * 2;
-    }
-    
-    subarraySize *= 2;
-    tempArray = inputArray.slice();
+class BTreeNode {
+  constructor(keys = [], children = []) {
+    this.keys = keys;
+    this.children = children;
+  }
+}
+
+class BTree {
+  constructor(order) {
+    this.order = order; // Maximum number of keys in each node
+    this.root = new BTreeNode();
   }
   
-  return inputArray;
+  insert(key) {
+    // TODO: Implement the insertion operation
+  }
+  
+  delete(key) {
+    // TODO: Implement the deletion operation
+  }
+  
+  search(key) {
+    // TODO: Implement the search operation
+  }
 }
-const array = [5, 8, 2, 10, 4];
-console.log(mergeSort(array)); // Output: [2, 4, 5, 8, 10]
