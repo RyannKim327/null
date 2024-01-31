123e126c142020007ebd360ba2e8e00bb6a73150
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:12:10 +0800

    1-31-2024 22:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,11 @@
-class Graph {
-  constructor(vertices) {
-    this.vertices = vertices;
-    this.edges = [];
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
 
-  addEdge(source, destination, weight) {
-    this.edges.push({ source, destination, weight });
-  }
+  return slow.value;
 }
-function bellmanFord(graph, source) {
-  // Step 1: Initialize distances from source to all other nodes as INFINITE
-  let distances = {};
-  for (let vertex of graph.vertices) {
-    distances[vertex] = Infinity;
-  }
-  distances[source] = 0;
-
-  // Step 2: Relax edges repeatedly
-  for (let i = 0; i < graph.vertices.length - 1; i++) {
-    for (let { source, destination, weight } of graph.edges) {
-      if (distances[source] + weight < distances[destination]) {
-        distances[destination] = distances[source] + weight;
-      }
-    }
-  }
-
-  // Step 3: Check for negative-weight cycles
-  for (let { source, destination, weight } of graph.edges) {
-    if (distances[source] + weight < distances[destination]) {
-      throw new Error("Graph contains a negative-weight cycle");
-    }
-  }
-
-  return distances;
-}
-const graph = new Graph(["A", "B", "C", "D", "E"]);
-graph.addEdge("A", "B", -1);
-graph.addEdge("A", "C", 4);
-graph.addEdge("B", "C", 3);
-graph.addEdge("B", "D", 2);
-graph.addEdge("B", "E", 2);
-graph.addEdge("D", "B", 1);
-graph.addEdge("D", "C", 5);
-graph.addEdge("E", "D", -3);
-const source = "A";
-const distances = bellmanFord(graph, source);
-console.log(distances);
