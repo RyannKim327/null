759decc688cf3b8cc28c550984ff57512c374bed
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:41:33 +0800

    1-31-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,14 @@
-function findNthNodeFromEnd(head, n) {
-  let ptr1 = head;
-  let ptr2 = head;
-
-  // Move ptr1 n steps forward
-  for (let i = 0; i < n; i++) {
-    if (ptr1 === null) {
-      // If the list is shorter than n, return null
-      return null;
-    }
-    ptr1 = ptr1.next;
-  }
-
-  // Move both pointers until ptr1 reaches the end
-  while (ptr1 !== null) {
-    ptr1 = ptr1.next;
-    ptr2 = ptr2.next;
-  }
-
-  // ptr2 now points to the nth node from the end
-  return ptr2;
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
+
+console.log(reverseString('Hello World')); // Output: dlroW olleH
+function reverseString(str) {
+  let reversedString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+}
+
+console.log(reverseString('Hello World')); // Output: dlroW olleH
