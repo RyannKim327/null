2b6ae65e0d91959b676c90a81314118ef6d4ceb6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:42:42 +0800

    1-31-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,12 @@
-class Node {
-  constructor() {
-    // Each node contains a map of characters to child nodes
-    this.children = new Map();
-  }
-}
+let str = '   Hello,   World!   ';
+let trimmedStr = str.replace(/\s/g, '');
 
-class SuffixTree {
-  constructor(string) {
-    this.root = new Node(); // The root of the suffix tree
-    this.string = string; // The input string
-    this.buildTree();
-  }
+console.log(trimmedStr); // Output: "Hello,World!"
+let str = '   Hello,   World!   ';
+let trimmedStr = str.trim();
 
-  // Function to build the suffix tree
-  buildTree() {
-    // Add all suffixes of the input string to the tree
-    for (let i = 0; i < this.string.length; i++) {
-      this.addSuffix(i);
-    }
-  }
+console.log(trimmedStr); // Output: "Hello,   World!"
+let str = '   Hello,   World!   ';
+let trimmedStr = str.replace(/\s/g, '').trim();
 
-  // Function to add a suffix to the tree
-  addSuffix(suffixStart) {
-    let node = this.root; // Start at the root
-    const suffix = this.string.slice(suffixStart); // Get the suffix from the input string
-
-    // Traverse the tree for the suffix
-    for (let i = 0; i < suffix.length; i++) {
-      const char = suffix[i];
-      if (!node.children.has(char)) {
-        // If a character is not found, create a new child node
-        node.children.set(char, new Node());
-      }
-      
-      // Move to the next node
-      node = node.children.get(char);
-    }
-  }
-}
-const inputString = "banana";
-const suffixTree = new SuffixTree(inputString);
+console.log(trimmedStr); // Output: "Hello,World!"
