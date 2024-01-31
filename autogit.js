789f5899f4d6084f2b4db04f60e58197b75b189e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:10:52 +0800

    1-31-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,9 @@
-const axios = require('axios');
-
-// Make a GET request
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log('GET Request - Response:', response.data);
+// Fetch API request to get random cat facts
+fetch('https://cat-fact.herokuapp.com/facts/random')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Random Cat Fact:', data.text);
   })
   .catch(error => {
-    console.error('GET Request - Error:', error);
-  });
-
-// Make a POST request
-const newPost = {
-  title: 'New Post',
-  body: 'This is the body of a new post',
-  userId: 1
-};
-
-axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
-  .then(response => {
-    console.log('POST Request - Response:', response.data);
-  })
-  .catch(error => {
-    console.error('POST Request - Error:', error);
+    console.log('Error fetching cat fact:', error);
   });
