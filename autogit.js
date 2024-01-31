443644782678e2cc9f2de37ddcbfb3cd92363f3e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:33:13 +0800

    1-31-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,16 @@
-function findLCS(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-
-  // Create a matrix to store the lengths of LCS
-  const lcsMatrix = Array(m + 1)
-    .fill(0)
-    .map(() => Array(n + 1).fill(0));
-
-  // Build the lcsMatrix in a bottom-up manner
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        lcsMatrix[i][j] = 1 + lcsMatrix[i - 1][j - 1];
-      } else {
-        lcsMatrix[i][j] = Math.max(lcsMatrix[i - 1][j], lcsMatrix[i][j - 1]);
-      }
-    }
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray);
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = array.filter((value, index, self) => {
+  return self.indexOf(value) === index;
+});
+console.log(uniqueArray);
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = array.reduce((acc, value) => {
+  if (!acc.includes(value)) {
+    acc.push(value);
   }
-
-  // Find the longest common subsequence by backtracking the lcsMatrix
-  let lcs = "";
-  let i = m;
-  let j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (lcsMatrix[i - 1][j] > lcsMatrix[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
-}
-
-// Example usage:
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-console.log(findLCS(str1, str2)); // Output: ADH
+  return acc;
+}, []);
+console.log(uniqueArray);
