b767a928e50abd47b20d481c2bd5732ce20aa664
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:19:01 +0800

    1-31-2024 22:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,20 @@
-let str = "123";
-let num = parseInt(str);
-
-console.log(num); // Output: 123
-console.log(typeof num); // Output: number
-let hexString = "1A";
-let decimalNum = parseInt(hexString, 16);
-
-console.log(decimalNum); // Output: 26
+function findNthNodeFromEnd(n) {
+  let mainPtr = head;
+  let refPtr = head;
+  
+  // Move refPtr n positions ahead
+  for (let i = 0; i < n; i++) {
+    if (refPtr === null) {
+      return null; // Linked list is shorter than n
+    }
+    refPtr = refPtr.next;
+  }
+  
+  // Move both pointers synchronously
+  while (refPtr !== null) {
+    mainPtr = mainPtr.next;
+    refPtr = refPtr.next;
+  }
+  
+  return mainPtr;
+}
