7902a8dfb9544b94d06b8a0b282139a86331f22e
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:02:01 +0800

    1-31-2024 22:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,20 @@
-const numbers = [5, 1, 3, 2, 4];
-
-numbers.sort(function(a, b) {
-  return a - b;
-});
-
-console.log(numbers);
+function findCommonElements(array1, array2) {
+  const commonElements = [];
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i]) && !commonElements.includes(array1[i])) {
+      commonElements.push(array1[i]);
+    }
+  }
+  
+  return commonElements;
+}
+function findCommonElements(array1, array2) {
+  const commonElements = array1.filter(element => array2.includes(element));
+  return commonElements;
+}
+function findCommonElements(array1, array2) {
+  const set1 = new Set(array1);
+  const commonElements = array2.filter(element => set1.has(element));
+  return commonElements;
+}
