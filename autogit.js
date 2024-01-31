a41e9e07739fdab10b4a6f6f892a90188f8e704a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:24:26 +0800

    1-31-2024 22:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,9 @@
-function buildPrefixTable(pattern) {
-  const prefixTable = [0];
-  let prefixLen = 0;
-  let i = 1;
-
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[prefixLen]) {
-      prefixLen++;
-      prefixTable[i] = prefixLen;
-      i++;
-    } else {
-      if (prefixLen !== 0) {
-        prefixLen = prefixTable[prefixLen - 1];
-      } else {
-        prefixTable[i] = 0;
-        i++;
-      }
-    }
-  }
-
-  return prefixTable;
-}
-
-function stringMatch(text, pattern) {
-  const prefixTable = buildPrefixTable(pattern);
-
-  let i = 0; // Index for text
-  let j = 0; // Index for pattern
-
-  while (i < text.length) {
-    if (pattern[j] === text[i]) {
-      j++;
-      i++;
-    }
-
-    if (j === pattern.length) {
-      console.log('Pattern found at index', i - j);
-      j = prefixTable[j - 1];
-    } else if (i < text.length && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = prefixTable[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-}
-
-// Example usage
-const text = 'ABABDABACDABABCABAB';
-const pattern = 'ABABCABAB';
-stringMatch(text, pattern);
+var str = "this is a string with spaces";
+var stringWithoutSpaces = str.split(" ").join("");
+console.log(stringWithoutSpaces);
+var str = "this is a string with spaces";
+var stringWithoutSpaces = str.replace(/\s/g, "");
+console.log(stringWithoutSpaces);
+var str = "this is a string with spaces";
+var stringWithoutSpaces = str.split("").join("");
+console.log(stringWithoutSpaces);
