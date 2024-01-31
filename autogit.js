b15eaf8d0d9cd102f93948ddc1f301ed4dc32fad
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:24:44 +0800

    1-31-2024 22:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,23 @@
-// Represents a node in a tree/graph
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.children = [];
+function factorialLoop(num) {
+  if (num === 0 || num === 1) {
+    return 1;
   }
-
-  // Add a child node
-  addChild(child) {
-    this.children.push(child);
+  
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
   }
+  
+  return result;
 }
 
-// Depth-limited search algorithm
-function depthLimitedSearch(node, target, depthLimit) {
-  if (node.value === target) {
-    return true; // Found the target
+console.log(factorialLoop(5)); // Output: 120
+function factorialRecursive(num) {
+  if (num === 0 || num === 1) {
+    return 1;
   }
-
-  if (depthLimit === 0) {
-    return false; // Reached depth limit
-  }
-
-  for (let child of node.children) {
-    if (depthLimitedSearch(child, target, depthLimit - 1)) {
-      return true; // Found the target in child node
-    }
-  }
-
-  return false; // Target not found
+  
+  return num * factorialRecursive(num - 1);
 }
 
-// Usage & Example
-const root = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-const node5 = new Node(5);
-root.addChild(node2);
-root.addChild(node3);
-node2.addChild(node4);
-node2.addChild(node5);
-
-console.log(depthLimitedSearch(root, 4, 2)); // Output: true
-console.log(depthLimitedSearch(root, 5, 1)); // Output: false
+console.log(factorialRecursive(5)); // Output: 120
