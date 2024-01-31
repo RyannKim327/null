acdc4f141387d4551af44e5cd524fd1abc003a66
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:00:23 +0800

    1-31-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,14 @@
-function getDigit(num, position) {
-  return Math.floor(Math.abs(num) / Math.pow(10, position)) % 10;
-}
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+function getStringLength(str) {
+  let count = 0;
+  
+  for (let i = 0; str[i] !== undefined; i++) {
+    count++;
   }
-  return maxDigits;
+  
+  return count;
 }
-function radixSort(nums) {
-  const maxDigitCount = mostDigits(nums);
-  for (let k = 0; k < maxDigitCount; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < nums.length; i++) {
-      const digit = getDigit(nums[i], k);
-      digitBuckets[digit].push(nums[i]);
-    }
-    nums = [].concat(...digitBuckets);
-  }
-  return nums;
-}
-console.log(radixSort([170, 45, 75, 90, 802, 24, 2, 66])); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+
+// Example usage:
+const string = "Hello, World!";
+const length = getStringLength(string);
+console.log(length); // Output: 13
