a1e9d449fcc32932d8223c0fe73912e95a452fe8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:22:33 +0800

    1-31-2024 22:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,138 @@
-class BinaryTree {
+class Node {
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
+}
 
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Method to insert a value into the tree
   insert(value) {
-    if (value <= this.value) {
-      if (this.left === null) {
-        this.left = new BinaryTree(value);
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
       } else {
-        this.left.insert(value);
+        this.insertNode(node.left, newNode);
       }
     } else {
-      if (this.right === null) {
-        this.right = new BinaryTree(value);
+      if (node.right === null) {
+        node.right = newNode;
       } else {
-        this.right.insert(value);
+        this.insertNode(node.right, newNode);
       }
     }
   }
 
-  has(value) {
-    if (value === this.value) {
-      return true;
-    } else if (value < this.value) {
-      if (this.left === null) {
-        return false;
-      } else {
-        return this.left.has(value);
-      }
+  // Method to search for a specific value in the tree
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
     } else {
-      if (this.right === null) {
-        return false;
-      } else {
-        return this.right.has(value);
+      return true;
+    }
+  }
+
+  // Method to remove a value from the tree
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    } else if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // Case 1: No child
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
       }
+
+      // Case 2: One child
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // Case 3: Two children
+      const aux = this.findMinNode(node.right);
+      node.value = aux.value;
+      node.right = this.removeNode(node.right, aux.value);
+      return node;
+    }
+  }
+
+  // Helper method to find the node with minimum value
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  // Method to traverse the tree in-order (left-root-right)
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
     }
   }
 }
-const tree = new BinaryTree(10);
-tree.insert(5);
-tree.insert(15);
-tree.insert(2);
-tree.insert(7);
+const bst = new BinarySearchTree();
 
-console.log(tree.has(7)); // Output: true
-console.log(tree.has(12)); // Output: false
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log(bst.search(6)); // Output: true
+console.log(bst.search(11)); // Output: false
+
+bst.remove(6);
+console.log(bst.search(6)); // Output: false
+
+bst.inOrderTraversal(value => console.log(value)); // Output: 1 3 4 7 8 10 13 14
