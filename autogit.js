f22991c61d1761b0a27a5acb4fb0f4110ff5fc10
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:25:24 +0800

    1-31-2024 22:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,154 @@
-function findCommonElements(arr1, arr2) {
-  return arr1.filter(element => arr2.includes(element));
+// Define a Node class to represent each node in the linked list
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(arr1, arr2) {
-  return arr1.filter(element => arr2.indexOf(element) !== -1);
-}
+// Define a LinkedList class to represent the linked list
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(arr1, arr2) {
-  const set1 = new Set(arr1);
-  const set2 = new Set(arr2);
-  const result = [];
+  // Method to add a new node to the end of the list
+  append(value) {
+    const newNode = new Node(value);
 
-  for (const element of set1) {
-    if (set2.has(element)) {
-      result.push(element);
+    if (!this.head) {
+      // If the list is empty, set the new node as both head and tail
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // If the list is not empty, append the new node after the tail
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
   }
-  
-  return result;
+
+  // Method to insert a new node at a specified position in the list
+  insertAt(value, index) {
+    const newNode = new Node(value);
+
+    if (index === 0) {
+      // If inserting at the beginning, set the new node as the new head
+      newNode.next = this.head;
+      this.head = newNode;
+      if (!this.tail) {
+        // If the list was empty, update the tail as well
+        this.tail = newNode;
+      }
+    } else {
+      let currentIndex = 0;
+      let currentNode = this.head;
+      let prevNode = null;
+
+      while (currentNode && currentIndex < index) {
+        // Traverse the list to the desired position
+        prevNode = currentNode;
+        currentNode = currentNode.next;
+        currentIndex++;
+      }
+
+      if (currentIndex === index) {
+        // Insert the new node at the specified position
+        newNode.next = currentNode;
+        prevNode.next = newNode;
+
+        if (!currentNode) {
+          // If inserting at the end, update the tail as well
+          this.tail = newNode;
+        }
+      } else {
+        console.log('Index out of range.');
+      }
+    }
+  }
+
+  // Method to remove a node at a specified position from the list
+  removeAt(index) {
+    if (!this.head) {
+      // If the list is empty, nothing to remove
+      console.log('List is already empty.');
+      return;
+    }
+
+    if (index === 0) {
+      // If removing the head node
+      this.head = this.head.next;
+      if (!this.head) {
+        // If the list had only one node, update the tail as well
+        this.tail = null;
+      }
+      return;
+    }
+
+    let currentIndex = 0;
+    let currentNode = this.head;
+    let prevNode = null;
+
+    while (currentNode && currentIndex < index) {
+      // Traverse the list to the desired position
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    if (currentIndex === index && currentNode) {
+      // Remove the node at the specified position
+      prevNode.next = currentNode.next;
+
+      if (!currentNode.next) {
+        // If removing the last node, update the tail as well
+        this.tail = prevNode;
+      }
+    } else {
+      console.log('Index out of range.');
+    }
+  }
+
+  // Method to retrieve the node value at a specified position in the list
+  getAt(index) {
+    let currentIndex = 0;
+    let currentNode = this.head;
+
+    while (currentNode && currentIndex < index) {
+      // Traverse the list to the desired position
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+    if (currentIndex === index && currentNode) {
+      return currentNode.value;
+    } else {
+      console.log('Index out of range.');
+    }
+  }
+
+  // Method to print the linked list
+  print() {
+    let currentNode = this.head;
+    const list = [];
+
+    while (currentNode) {
+      // Traverse the list and store node values in an array
+      list.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    console.log(list.join(' -> '));
+  }
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+// Example usage:
+const list = new LinkedList();
+list.append(5);
+list.append(10);
+list.append(15);
+list.append(20);
+list.insertAt(25, 1);
+list.removeAt(3);
+list.print(); // Output: 5 -> 25 -> 10 -> 20
+console.log(list.getAt(2)); // Output: 10
