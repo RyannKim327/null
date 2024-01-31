6cf6e7a6c191dfe846486e59063f2e2b14bdad80
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:44:45 +0800

    1-31-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,18 @@
-function bfs(graph, startNode) {
-  // Initialize the queue with the start node and mark it as visited
-  const queue = [startNode];
-  const visited = new Set([startNode]);
-
-  while (queue.length > 0) {
-    // Dequeue a node from the front of the queue
-    const currentNode = queue.shift();
-
-    // Process the dequeued node (e.g., print or perform any operation)
-    console.log(currentNode);
-
-    // Enqueue all unvisited neighboring nodes and mark them as visited
-    const neighbors = graph[currentNode];
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        queue.push(neighbor);
-        visited.add(neighbor);
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap elements
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
       }
     }
   }
+  return arr;
 }
-const graph = {
-  A: ['B', 'C'],
-  B: ['D', 'E'],
-  C: ['F'],
-  D: [],
-  E: ['F'],
-  F: []
-};
-bfs(graph, 'A');
+
+// Example usage:
+var array = [5, 3, 8, 2, 1, 4];
+console.log(bubbleSort(array));
