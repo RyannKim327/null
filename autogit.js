2d7c922c20ed089f0b35d264fd6a6ef4474fa941
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:04:23 +0800

    1-31-2024 22:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,75 @@
-class Graph {
+// Node class representing a single node in the binary tree
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// BinaryTree class representing the overall binary tree
+class BinaryTree {
   constructor() {
-    this.adjList = new Map();
+    this.root = null;
   }
 
-  addNode(node) {
-    this.adjList.set(node, []);
+  // Insert a value into the binary tree
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
   }
 
-  addEdge(src, dest) {
-    this.adjList.get(src).push(dest);
-  }
-
-  dfs(startNode) {
-    const visited = new Set();
-
-    this._dfsHelper(startNode, visited);
-  }
-
-  _dfsHelper(node, visited) {
-    visited.add(node);
-    console.log(node);
-
-    const neighbors = this.adjList.get(node);
-    
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        this._dfsHelper(neighbor, visited);
+  // Helper method to insert a value recursively
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
       }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Traverse the binary tree in in-order (left-root-right) order
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  // Helper method for in-order traversal
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
     }
   }
 }
 
-// Usage example:
+// Example usage
+const tree = new BinaryTree();
+tree.insert(8);
+tree.insert(4);
+tree.insert(12);
+tree.insert(2);
+tree.insert(6);
+tree.insert(10);
+tree.insert(14);
 
-const graph = new Graph();
+// In-order traversal callback function
+const printValue = (value) => {
+  console.log(value);
+};
 
-// Add nodes to the graph
-graph.addNode(1);
-graph.addNode(2);
-graph.addNode(3);
-graph.addNode(4);
-graph.addNode(5);
-
-// Add edges
-graph.addEdge(1, 2);
-graph.addEdge(1, 3);
-graph.addEdge(2, 4);
-graph.addEdge(3, 5);
-
-// Perform DFS starting from node 1
-graph.dfs(1);
+// Perform in-order traversal
+tree.inOrderTraversal(printValue);
