e5186a3c6f471912dee39e1c72d548ee19919a77
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:02:08 +0800

    1-31-2024 22:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,8 @@
-function findCommonElements(array1, array2) {
-  const commonElements = [];
-  
-  for (let i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i]) && !commonElements.includes(array1[i])) {
-      commonElements.push(array1[i]);
-    }
-  }
-  
-  return commonElements;
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
-function findCommonElements(array1, array2) {
-  const commonElements = array1.filter(element => array2.includes(element));
-  return commonElements;
-}
-function findCommonElements(array1, array2) {
-  const set1 = new Set(array1);
-  const commonElements = array2.filter(element => set1.has(element));
-  return commonElements;
-}
+
+// Example usage:
+var myString = "Hello, World!";
+var reversedString = reverseString(myString);
+console.log(reversedString);
