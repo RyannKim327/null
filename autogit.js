5f0db871e36aa953de92bf46fae35d0cba92fca6
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:29:32 +0800

    1-31-2024 22:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,25 @@
-class ListNode {
-  constructor(value) {
-    this.val = value;
-    this.next = null;
+function findLongestCommonPrefix(strs) {
+  let prefix = "";
+
+  if (strs === null || strs.length === 0) {
+    return prefix;
   }
+
+  for (let i = 0; i < strs[0].length; i++) {
+    const currentChar = strs[0][i];
+
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== currentChar) {
+        return prefix;
+      }
+    }
+
+    prefix += currentChar;
+  }
+
+  return prefix;
 }
+const strings = ["apple", "app", "applause"];
+const longestPrefix = findLongestCommonPrefix(strings);
 
-function getIntersectionNode(headA, headB) {
-  if (!headA || !headB) {
-    return null;
-  }
-
-  let pA = headA;
-  let pB = headB;
-
-  while (pA !== pB) {
-    pA = pA === null ? headB : pA.next;
-    pB = pB === null ? headA : pB.next;
-  }
-
-  return pA; // or pB, they would either be the same node or null
-}
-// list1: 1 → 2 → 3 → 6 → 7
-const node1 = new ListNode(1);
-const node2 = new ListNode(2);
-const node3 = new ListNode(3);
-const node6 = new ListNode(6);
-const node7 = new ListNode(7);
-node1.next = node2;
-node2.next = node3;
-node3.next = node6;
-node6.next = node7;
-
-// list2: 4 → 5 → 6 → 7
-const node4 = new ListNode(4);
-const node5 = new ListNode(5);
-node4.next = node5;
-node5.next = node6;
-
-const intersectionNode = getIntersectionNode(node1, node4);
-console.log(intersectionNode ? intersectionNode.val : "No intersection"); // Output: 6
+console.log(longestPrefix); // Output: "app"
