e08a0254523e2dcb72e0d23ac3c91944590ebd19
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:37:48 +0800

    1-31-2024 22:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,92 @@
-function mergeSortIterative(array) {
-  let sortedArray = array.slice();
-  const len = sortedArray.length;
-  let subArraySize = 1;
-
-  function merge(left, mid, right, array) {
-    const leftArray = array.slice(left, mid + 1);
-    const rightArray = array.slice(mid + 1, right + 1);
-    let leftIndex = 0;
-    let rightIndex = 0;
-    let sortedIndex = left;
-
-    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
-      if (leftArray[leftIndex] <= rightArray[rightIndex]) {
-        array[sortedIndex] = leftArray[leftIndex];
-        leftIndex++;
-      } else {
-        array[sortedIndex] = rightArray[rightIndex];
-        rightIndex++;
-      }
-      sortedIndex++;
-    }
-
-    while (leftIndex < leftArray.length) {
-      array[sortedIndex] = leftArray[leftIndex];
-      leftIndex++;
-      sortedIndex++;
-    }
-
-    while (rightIndex < rightArray.length) {
-      array[sortedIndex] = rightArray[rightIndex];
-      rightIndex++;
-      sortedIndex++;
-    }
+class BTreeNode {
+  constructor(t, leaf = true) {
+    this.t = t; // Minimum degree (defines the number of keys the node can hold)
+    this.keys = []; // Array to hold the keys
+    this.children = []; // Array to hold the child nodes
+    this.leaf = leaf; // Is true when node is a leaf node, otherwise false
   }
-
-  while (subArraySize < len) {
-    let left = 0;
-
-    while (left + subArraySize < len) {
-      let mid = left + subArraySize - 1;
-      let right = Math.min(left + 2 * subArraySize - 1, len - 1);
-      merge(left, mid, right, sortedArray);
-      left += 2 * subArraySize;
-    }
-
-    subArraySize *= 2;
-  }
-
-  return sortedArray;
 }
-const array = [7, 2, 4, 1, 5, 3];
-const sortedArray = mergeSortIterative(array);
-console.log(sortedArray); // [1, 2, 3, 4, 5, 7]
+
+class BTree {
+  constructor(t) {
+    this.root = null; // Root node
+    this.t = t; // Minimum degree (defines the number of keys the nodes can hold)
+  }
+
+  // Insert a key into the tree
+  insert(key) {
+    if (this.root === null) {
+      // Tree is empty, create a new root node
+      this.root = new BTreeNode(this.t, true);
+      this.root.keys.push(key);
+    } else {
+      if (this.root.keys.length === (2 * this.t) - 1) {
+        // Root node is full, grow the tree taller
+        const newRoot = new BTreeNode(this.t, false);
+        newRoot.children.push(this.root);
+        this.splitChild(newRoot, 0, this.root);
+        this.root = newRoot;
+      }
+      this.insertNonFull(this.root, key);
+    }
+  }
+
+  // Insert a key into a non-full node
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+    if (node.leaf) {
+      // Shift the keys greater than the new key to the right
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i + 1] = node.keys[i];
+        i -= 1;
+      }
+      node.keys[i + 1] = key;
+    } else {
+      // Find the child node to insert the key
+      while (i >= 0 && key < node.keys[i]) {
+        i -= 1;
+      }
+      i += 1;
+      if (node.children[i].keys.length === (2 * this.t) - 1) {
+        // Child node is full, split it
+        this.splitChild(node, i, node.children[i]);
+        if (key > node.keys[i]) {
+          i += 1;
+        }
+      }
+      this.insertNonFull(node.children[i], key);
+    }
+  }
+
+  // Split a full child node into two nodes
+  splitChild(parent, index, child) {
+    const newNode = new BTreeNode(this.t, child.leaf);
+    parent.keys.splice(index, 0, child.keys[this.t - 1]);
+    parent.children.splice(index + 1, 0, newNode);
+    newNode.keys = child.keys.splice(this.t, this.t - 1);
+    if (!child.leaf) {
+      newNode.children = child.children.splice(this.t, this.t);
+    }
+  }
+
+  // Search for a key in the tree
+  search(key, node = this.root) {
+    let i = 0;
+    while (i < node.keys.length && key > node.keys[i]) {
+      i += 1;
+    }
+    if (node.keys[i] === key) {
+      return true;
+    } else if (node.leaf) {
+      return false;
+    } else {
+      return this.search(key, node.children[i]);
+    }
+  }
+}
+const bTree = new BTree(3); // Create a B-tree with minimum degree 3
+bTree.insert(10);
+bTree.insert(20);
+bTree.insert(5);
+console.log(bTree.search(10)); // Output: true
+console.log(bTree.search(30)); // Output: false
