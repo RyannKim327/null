4fdcaa97cf6563551f4ae0b01f8cf1bd49e36be5
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:00:16 +0800

    1-31-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,27 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-
-console.log(array); // Output: [5, 4, 3, 2, 1]
-let array = [1, 2, 3, 4, 5];
-let reversedArray = array.slice().reverse();
-
-console.log(reversedArray); // Output: [5, 4, 3, 2, 1]
-console.log(array);         // Output: [1, 2, 3, 4, 5]
+function getDigit(num, position) {
+  return Math.floor(Math.abs(num) / Math.pow(10, position)) % 10;
+}
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+function radixSort(nums) {
+  const maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      const digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
+    }
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
+}
+console.log(radixSort([170, 45, 75, 90, 802, 24, 2, 66])); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
