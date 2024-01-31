14a73c55f19cc86773334d5d268e59d85be4fa71
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:10:14 +0800

    1-31-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,31 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  return true;
 }
-const array1 = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(array1));  // Output: true
 
-const array2 = [5, 4, 3, 2, 1];
-console.log(isSortedAscending(array2));  // Output: false
+function getLinkedListLength(head) {
+  let length = 0;
+  let current = head;
 
-const array3 = [1, 3, 2, 4, 5];
-console.log(isSortedAscending(array3));  // Output: false
+  while (current !== null) {
+    length++;
+    current = current.next;
+  }
+
+  return length;
+}
+// Create a linked list with three nodes
+const head = new ListNode(1);
+const secondNode = new ListNode(2);
+const thirdNode = new ListNode(3);
+
+// Connect the nodes
+head.next = secondNode;
+secondNode.next = thirdNode;
+
+// Get the length of the linked list
+const length = getLinkedListLength(head);
+
+console.log(length); // Output: 3
