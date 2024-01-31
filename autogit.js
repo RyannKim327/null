62b66a83d9ced7cb50fbf37632ff6a0dd9f12aa0
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:29:48 +0800

    1-31-2024 22:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,141 @@
-function findLongestCommonPrefix(strs) {
-  let prefix = "";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
-  if (strs === null || strs.length === 0) {
-    return prefix;
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
   }
 
-  for (let i = 0; i < strs[0].length; i++) {
-    const currentChar = strs[0][i];
+  insert(value) {
+    const newNode = new Node(value);
 
-    for (let j = 1; j < strs.length; j++) {
-      if (strs[j][i] !== currentChar) {
-        return prefix;
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
       }
     }
-
-    prefix += currentChar;
   }
 
-  return prefix;
-}
-const strings = ["apple", "app", "applause"];
-const longestPrefix = findLongestCommonPrefix(strings);
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
 
-console.log(longestPrefix); // Output: "app"
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      // No child
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      // One child
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      // Two children
+      const aux = this.findMinNode(node.right);
+      node.value = aux.value;
+      node.right = this.removeNode(node.right, aux.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  getRootNode() {
+    return this.root;
+  }
+
+  inorderTraversal(callback) {
+    this.inorderTraversalNode(this.root, callback);
+  }
+
+  inorderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inorderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inorderTraversalNode(node.right, callback);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
+
+console.log("Inorder Traversal:");
+bst.inorderTraversal(value => console.log(value));
+
+console.log("Is 6 present in the tree?", bst.search(6)); // true
+
+console.log("Removing node 7");
+bst.remove(7);
+
+console.log("Inorder Traversal after removal:");
+bst.inorderTraversal(value => console.log(value));
