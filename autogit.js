2353f0549c9ccd77e88cf84f8ca5f3843a5ccc33
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:36:18 +0800

    1-31-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,14 @@
-function quicksort(arr) {
-  if (arr.length < 2) {
-    return arr;
-  } else {
-    const pivot = arr[0];
-    const smaller = arr.slice(1).filter((element) => element <= pivot);
-    const greater = arr.slice(1).filter((element) => element > pivot);
-    
-    return [...quicksort(smaller), pivot, ...quicksort(greater)];
-  }
-}
+function reverseLinkedList(head) {
+  let previous = null;
+  let current = head;
+  let next = null;
 
-// Example usage:
-const array = [7, 2, 10, -5, 4, 8, 0];
-console.log(quicksort(array)); // Output: [-5, 0, 2, 4, 7, 8, 10]
+  while (current !== null) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
