7032a41d61204da758ef287f6ffd0d1070f4e13a
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 21:59:56 +0800

    1-31-2024 21:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,18 @@
-function isAnagram(str1, str2) {
-  // Convert strings to lowercase and remove whitespace
-  str1 = str1.toLowerCase().replace(/\s/g, '');
-  str2 = str2.toLowerCase().replace(/\s/g, '');
-
-  // Convert strings to arrays, sort them, and convert back to strings
-  const sortedStr1 = str1.split('').sort().join('');
-  const sortedStr2 = str2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
+function countOccurrences(string, character) {
+  let count = 0;
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === character) {
+      count++;
+    }
+  }
+  
+  return count;
 }
 
 // Example usage
-const string1 = 'listen';
-const string2 = 'silent';
+const myString = "Hello, World!";
+const characterToCount = "o";
 
-console.log(isAnagram(string1, string2)); // Output: true
+const occurrenceCount = countOccurrences(myString, characterToCount);
+console.log("Number of occurrences:", occurrenceCount);
