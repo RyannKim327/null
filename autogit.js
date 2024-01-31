4b4467a5f8a10165cb2da36bb9eee80288dabe28
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:41:50 +0800

    1-31-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,57 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
+function stringMatch(text, pattern) {
+    const n = text.length;
+    const m = pattern.length;
+    const lps = new Array(m).fill(0);
+    let i = 0, j = 0;
+    computeLPSArray(pattern, m, lps);
+    const indices = [];
+
+    while (j < n) {
+        if (pattern[i] === text[j]) {
+            i++;
+            j++;
+        }
+
+        if (i === m) {
+            indices.push(j - i);
+            i = lps[i - 1];
+        }
+        else if (j < n && pattern[i] !== text[j]) {
+            if (i !== 0) {
+                i = lps[i - 1];
+            }
+            else {
+                j++;
+            }
+        }
+    }
+
+    return indices;
 }
 
-console.log(reverseString('Hello World')); // Output: dlroW olleH
-function reverseString(str) {
-  let reversedString = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedString += str[i];
-  }
-  return reversedString;
-}
+function computeLPSArray(pattern, m, lps) {
+    let length = 0;
+    let i = 1;
+    lps[0] = 0;
 
-console.log(reverseString('Hello World')); // Output: dlroW olleH
+    while (i < m) {
+        if (pattern[i] === pattern[length]) {
+            length++;
+            lps[i] = length;
+            i++;
+        }
+        else {
+            if (length !== 0) {
+                length = lps[length - 1];
+            }
+            else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+const text = "ABCABCDABABCDABCDABDE";
+const pattern = "ABCDABD";
+const indices = stringMatch(text, pattern);
+console.log(indices); // Output: [5, 14]
