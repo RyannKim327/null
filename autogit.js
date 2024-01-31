7f79e1469d4ae320ab88a82f1602262967f2cce2
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:14:27 +0800

    1-31-2024 22:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,45 @@
-const str = 'Hello, World!';
-const substring = 'llo';
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
 
-// Using includes() method
-if (str.includes(substring)) {
-  console.log(`"${str}" contains "${substring}"`);
-} else {
-  console.log(`"${str}" does not contain "${substring}"`);
-}
+  while (current !== null) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
 
-// Using regular expression
-const regex = new RegExp(substring);
-if (regex.test(str)) {
-  console.log(`"${str}" contains "${substring}"`);
-} else {
-  console.log(`"${str}" does not contain "${substring}"`);
+  return prev;
 }
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+// Create nodes
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+
+// Connect nodes to form a linked list
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+// Set head as the starting node of the linked list
+const head = node1;
+// Reverse the linked list
+const reversedHead = reverseLinkedList(head);
+
+// Print the reversed linked list
+let current = reversedHead;
+while (current !== null) {
+  console.log(current.data);
+  current = current.next;
+}
+4
+3
+2
+1
