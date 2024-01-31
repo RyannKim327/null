f82316ce7848749f39cb3e86b31e51100f1de9f8
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:26:23 +0800

    1-31-2024 22:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,43 @@
-// Function to merge two sorted arrays
-function merge(leftArr, rightArr) {
-  const result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
+function mergeArrays(arr1, arr2) {
+  const merged = [];
+  let i = 0;
+  let j = 0;
 
-  // Merge elements from leftArr and rightArr into the result array in sorted order
-  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-    if (leftArr[leftIndex] < rightArr[rightIndex]) {
-      result.push(leftArr[leftIndex]);
-      leftIndex++;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] <= arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
     } else {
-      result.push(rightArr[rightIndex]);
-      rightIndex++;
+      merged.push(arr2[j]);
+      j++;
     }
   }
 
-  // Take care of any remaining elements in either array
-  while (leftIndex < leftArr.length) {
-    result.push(leftArr[leftIndex]);
-    leftIndex++;
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
   }
 
-  while (rightIndex < rightArr.length) {
-    result.push(rightArr[rightIndex]);
-    rightIndex++;
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
   }
 
-  return result;
+  return merged;
 }
+function findMedianSortedArrays(nums1, nums2) {
+  const merged = mergeArrays(nums1, nums2);
+  const len = merged.length;
 
-// Iterative merge sort function
-function mergeSortIterative(arr) {
-  const len = arr.length;
-  const tempArr = [...arr];
-  const result = [...arr];
-
-  // Merge subarrays of size 1, then merge subarrays of increasing size
-  for (let size = 1; size < len; size *= 2) {
-    for (let leftStart = 0; leftStart < len; leftStart += 2 * size) {
-      const mid = leftStart + size;
-      const rightEnd = Math.min(leftStart + 2 * size, len);
-      const leftArr = tempArr.slice(leftStart, mid);
-      const rightArr = tempArr.slice(mid, rightEnd);
-      const mergedArr = merge(leftArr, rightArr);
-      result.splice(leftStart, mergedArr.length, ...mergedArr);
-    }
-    
-    // Swap tempArr and result for the next iteration
-    const temp = tempArr;
-    tempArr = result;
-    result = temp;
+  if (len % 2 === 1) {
+    return merged[Math.floor(len / 2)];
+  } else {
+    const mid = len / 2;
+    return (merged[mid - 1] + merged[mid]) / 2;
   }
-
-  return result;
 }
+const nums1 = [1, 3, 5];
+const nums2 = [2, 4, 6];
 
-// Example usage:
-const arr = [8, 4, 1, 6, 3, 9, 2, 7, 5];
-const sortedArr = mergeSortIterative(arr);
-console.log(sortedArr); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median); // Output: 3.5
