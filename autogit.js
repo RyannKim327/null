bdf14ab9b87e2275667b9a2a97d9f7fc9116f3b9
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:41:24 +0800

    1-31-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,22 @@
-class BinaryHeap {
-  constructor() {
-    this.heap = [];
-  }
+function findNthNodeFromEnd(head, n) {
+  let ptr1 = head;
+  let ptr2 = head;
 
-  push(value, priority) {
-    const element = { value, priority };
-    this.heap.push(element);
-    this.heapifyUp();
-  }
-
-  pop() {
-    if (this.heap.length === 0) {
+  // Move ptr1 n steps forward
+  for (let i = 0; i < n; i++) {
+    if (ptr1 === null) {
+      // If the list is shorter than n, return null
       return null;
     }
-
-    const highestPriorityElement = this.heap[0];
-    const lastElement = this.heap.pop();
-
-    if (this.heap.length > 0) {
-      this.heap[0] = lastElement;
-      this.heapifyDown();
-    }
-
-    return highestPriorityElement.value;
+    ptr1 = ptr1.next;
   }
 
-  heapifyUp() {
-    let index = this.heap.length - 1;
-    const element = this.heap[index];
-
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      const parent = this.heap[parentIndex];
-
-      if (element.priority >= parent.priority) {
-        break;
-      }
-
-      this.heap[index] = parent;
-      index = parentIndex;
-    }
-
-    this.heap[index] = element;
+  // Move both pointers until ptr1 reaches the end
+  while (ptr1 !== null) {
+    ptr1 = ptr1.next;
+    ptr2 = ptr2.next;
   }
 
-  heapifyDown() {
-    let index = 0;
-    const element = this.heap[index];
-    const length = this.heap.length;
-
-    while (index < length) {
-      const leftChildIndex = (2 * index) + 1;
-      const rightChildIndex = (2 * index) + 2;
-      let swapIndex = null;
-
-      if (leftChildIndex < length) {
-        const leftChild = this.heap[leftChildIndex];
-
-        if (leftChild.priority < element.priority) {
-          swapIndex = leftChildIndex;
-        }
-      }
-
-      if (rightChildIndex < length) {
-        const rightChild = this.heap[rightChildIndex];
-
-        if (
-          (swapIndex === null && rightChild.priority < element.priority) ||
-          (swapIndex !== null && rightChild.priority < this.heap[swapIndex].priority)
-        ) {
-          swapIndex = rightChildIndex;
-        }
-      }
-
-      if (swapIndex === null) {
-        break;
-      }
-
-      this.heap[index] = this.heap[swapIndex];
-      this.heap[swapIndex] = element;
-      index = swapIndex;
-    }
-  }
+  // ptr2 now points to the nth node from the end
+  return ptr2;
 }
-const priorityQueue = new BinaryHeap();
-priorityQueue.push("Task 1", 3);
-priorityQueue.push("Task 2", 2);
-priorityQueue.push("Task 3", 1);
-
-const firstTask = priorityQueue.pop();
-console.log(firstTask); // Output: "Task 3"
-
-const secondTask = priorityQueue.pop();
-console.log(secondTask); // Output: "Task 2"
-
-const thirdTask = priorityQueue.pop();
-console.log(thirdTask); // Output: "Task 1"
