18991c88f6c2c9f70b7304c06963aaf85efd0a05
RyannKim327 <rksesgundo123@gmail.com>
2024-01-31 22:31:00 +0800

    1-31-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,129 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  return arr;
 }
 
-// Test the sort algorithm
-var array = [4, 2, 7, 1, 3];
-console.log("Before sorting: ", array);
-console.log("After sorting: ", bubbleSort(array));
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+  }
+
+  insert(index, value) {
+    if (index <= 0) {
+      this.prepend(value);
+      return;
+    }
+
+    const newNode = new Node(value);
+    let current = this.head;
+    let previous = null;
+    let count = 0;
+
+    while (current && count < index) {
+      previous = current;
+      current = current.next;
+      count++;
+    }
+
+    if (count === index) {
+      previous.next = newNode;
+      newNode.next = current;
+
+      if (!current) {
+        this.tail = newNode;
+      }
+    } else {
+      throw new Error("Index out of bounds");
+    }
+  }
+
+  remove(index) {
+    if (!this.head) {
+      throw new Error("List is empty");
+    }
+
+    if (index === 0) {
+      this.head = this.head.next;
+
+      if (!this.head) {
+        this.tail = null;
+      }
+
+      return;
+    }
+
+    let current = this.head;
+    let previous = null;
+    let count = 0;
+
+    while (current && count < index) {
+      previous = current;
+      current = current.next;
+      count++;
+    }
+
+    if (count === index && current) {
+      previous.next = current.next;
+
+      if (!current.next) {
+        this.tail = previous;
+      }
+    } else {
+      throw new Error("Index out of bounds");
+    }
+  }
+
+  toArray() {
+    const arr = [];
+    let current = this.head;
+
+    while (current) {
+      arr.push(current.value);
+      current = current.next;
+    }
+
+    return arr;
+  }
+
+  print() {
+    console.log(this.toArray().join(" -> "));
+  }
+}
+const list = new LinkedList();
+
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend(0);
+list.insert(2, 99);
+list.print(); // 0 -> 1 -> 99 -> 2 -> 3
+
+list.remove(1);
+list.print(); // 0 -> 99 -> 2 -> 3
