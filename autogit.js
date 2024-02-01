c3f43c48951ef13d895a3e2c99bc5dde4a1e34e7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:59:22 +0800

    2-1-2024 18:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,5 @@
-function topologicalSort(graph) {
-  const orderedNodes = [];
-  const visited = new Set();
+let str = "Hello, world! This is a string with spaces.";
+let result = str.replace(/\s/g, "");
 
-  function dfs(node) {
-    visited.add(node);
-
-    const neighbors = graph[node] || [];
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        dfs(neighbor);
-      }
-    }
-
-    orderedNodes.unshift(node);
-  }
-
-  for (const node in graph) {
-    if (!visited.has(node)) {
-      dfs(node);
-    }
-  }
-
-  return orderedNodes;
-}
-
-// Example usage:
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['D', 'E'],
-  'C': ['F'],
-  'D': [],
-  'E': ['F'],
-  'F': []
-};
-
-const orderedNodes = topologicalSort(graph);
-console.log(orderedNodes); // Output: ['A', 'C', 'F', 'B', 'E', 'D']
+console.log(result);
+Hello,world!Thisisastringwithspaces.
