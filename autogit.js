b4b72dd4deaae466b5f814412a8571e0d11d77c0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:28:22 +0800

    2-1-2024 18:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,38 @@
-function shuffleArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
   }
-  return arr;
 }
+HashTable.prototype.hash = function(key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
+  }
+  return hash % this.size;
+};
+HashTable.prototype.insert = function(key, value) {
+  const index = this.hash(key);
+  if (!this.buckets[index]) {
+    this.buckets[index] = [];
+  }
+  this.buckets[index].push({ key, value });
+};
 
-function isSorted(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+HashTable.prototype.get = function(key) {
+  const index = this.hash(key);
+  if (this.buckets[index]) {
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i].key === key) {
+        return this.buckets[index][i].value;
+      }
     }
   }
-  return true;
-}
-
-function bogoSort(arr) {
-  while (!isSorted(arr)) {
-    arr = shuffleArray(arr);
-  }
-  return arr;
-}
-
-// Usage example:
-const unsortedArray = [5, 2, 9, 1, 3];
-const sortedArray = bogoSort(unsortedArray);
-console.log(sortedArray);
+  return undefined;
+};
+const hashTable = new HashTable(10);
+hashTable.insert("apple", 42);
+hashTable.insert("banana", 17);
+console.log(hashTable.get("apple"));   // Output: 42
+console.log(hashTable.get("banana"));  // Output: 17
+console.log(hashTable.get("orange"));  // Output: undefined
