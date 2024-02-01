a95bc78b171059a0f8016f627980e14167be54f3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:04:23 +0800

    2-1-2024 19:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,122 +1,28 @@
-class Node {
-  constructor(value = null, level = 0) {
-    this.value = value;
-    this.next = new Array(level + 1);
-  }
-}
-class SkipList {
-  constructor() {
-    this.head = new Node();
-    this.maxLevel = 0;
-  }
-
-  // Generate a random level for a new node
-  getRandomLevel() {
-    let level = 0;
-    while (Math.random() < 0.5 && level < this.maxLevel + 1) {
-      level++;
-    }
-    return level;
-  }
-
-  // Insert a new node into the skip list
-  insert(value) {
-    const newNode = new Node(value, this.getRandomLevel());
-    const update = new Array(this.maxLevel + 1);
-    let current = this.head;
-
-    for (let i = this.maxLevel; i >= 0; i--) {
-      while (current.next[i] && current.next[i].value < value) {
-        current = current.next[i];
-      }
-      update[i] = current;
-    }
-
-    for (let i = 0; i <= newNode.next.length - 1; i++) {
-      newNode.next[i] = update[i].next[i];
-      update[i].next[i] = newNode;
-    }
-
-    if (newNode.next.length > this.maxLevel) {
-      this.maxLevel = newNode.next.length - 1;
-    }
-  }
-
-  // Search for a value in the skip list
-  search(value) {
-    let current = this.head;
+function findFirstRepeatedCharacter(str) {
+  var visited = {};
+  
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i];
     
-    for (let i = this.maxLevel; i >= 0; i--) {
-      while (current.next[i] && current.next[i].value < value) {
-        current = current.next[i];
-      }
-    }
-
-    current = current.next[0];
-    if (current && current.value === value) {
-      return current;
+    // If the character is already in the visited object, it's a repeat
+    if (visited[char]) {
+      return char;
     }
     
-    return null;
+    // Otherwise, mark the character as visited
+    visited[char] = true;
   }
-
-  // Remove a value from the skip list
-  remove(value) {
-    const update = new Array(this.maxLevel + 1);
-    let current = this.head;
-
-    for (let i = this.maxLevel; i >= 0; i--) {
-      while (current.next[i] && current.next[i].value < value) {
-        current = current.next[i];
-      }
-      update[i] = current;
-    }
-
-    current = current.next[0];
-
-    if (current && current.value === value) {
-      for (let i = 0; i <= current.next.length - 1; i++) {
-        if (update[i].next[i] !== current) {
-          break;
-        }
-        update[i].next[i] = current.next[i];
-      }
-
-      // Adjust the max level if necessary
-      while (this.maxLevel > 0 && this.head.next[this.maxLevel] === null) {
-        this.maxLevel--;
-      }
-
-      return true;
-    }
-
-    return false;
-  }
-
-  // Print the skip list
-  print() {
-    for (let level = this.maxLevel; level >= 0; level--) {
-      let output = `Level ${level}: `;
-      let current = this.head.next[level];
-      while (current) {
-        output += `${current.value} `;
-        current = current.next[level];
-      }
-      console.log(output);
-    }
-  }
+  
+  // If no repeated character is found, return null
+  return null;
 }
-const list = new SkipList();
 
-list.insert(3);
-list.insert(7);
-list.insert(2);
-list.insert(5);
+// Test case
+var string = "abcdefgah";
+var repeatedChar = findFirstRepeatedCharacter(string);
 
-list.print(); // Prints the skip list
-
-console.log(list.search(7)); // Searches for the value 7
-console.log(list.search(4)); // Searches for the value 4
-
-console.log(list.remove(2)); // Removes the value 2
-list.print(); // Prints the updated skip list
+if (repeatedChar) {
+  console.log("The first repeated character is: " + repeatedChar);
+} else {
+  console.log("No repeated character found.");
+}
