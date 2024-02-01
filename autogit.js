e14a079aeb856a38d22fada1b09ea6d7fbc13fa6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:02:58 +0800

    2-1-2024 19:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,9 @@
-function isAnagram(str1, str2) {
-    // convert strings to lowercase and remove spaces/punctuation
-    str1 = str1.toLowerCase().replace(/[^a-z0-9]/g, '');
-    str2 = str2.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-    // sort the strings
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
-
-    // compare the sorted strings
-    return sortedStr1 === sortedStr2;
-}
-
-// example usage
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-    console.log("The strings are anagrams.");
-} else {
-    console.log("The strings are not anagrams.");
-}
+let str = "Hello World!";
+let newStr = str.replace(/\s/g, ""); // /\s/ is the regular expression for whitespace characters, and the "g" flag makes it global
+console.log(newStr); // Output: "HelloWorld!"
+let str = "Hello World!";
+let newStr = str.split(" ").join("");
+console.log(newStr); // Output: "HelloWorld!"
+let str = "Hello World!";
+let newStr = [...str].filter((char) => char !== " ").join("");
+console.log(newStr); // Output: "HelloWorld!"
