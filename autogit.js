ebcde6919b931fa97876280cc816c45af56286a7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:29:32 +0800

    2-1-2024 18:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,4 @@
-function removeVowels(str) {
-  // Use regular expression to match all vowels (both lowercase and uppercase)
-  // and replace them with an empty string
-  return str.replace(/[aeiou]/gi, '');
-}
-
-// Example usage
-console.log(removeVowels("Hello, World!")); // Output: Hll, Wrld!
-console.log(removeVowels("JavaScript")); // Output: JvScrpt
+let str = "   Hello,  World!   ";
+let trimmedStr = str.replace(/\s/g, '');
+console.log(trimmedStr);
+Hello,World!
