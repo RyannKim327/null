5a1b0ca561e8236eb459c8a0296335c8b1d0d0dd
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:37:06 +0800

    2-1-2024 18:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,22 @@
-let str = "Hello, World! This is a string with spaces.";
-let stringWithoutSpaces = str.split(' ').join('');
-console.log(stringWithoutSpaces);
-let str = "Hello, World! This is a string with spaces.";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces);
-let str = "Hello, World! This is a string with spaces.";
-let stringWithoutSpaces = "";
-for (let i = 0; i < str.length; i++) {
-  if (str[i] !== ' ') {
-    stringWithoutSpaces += str[i];
+function selectionSort(arr) {
+  // Iterate through the entire array
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i; // Assume the current element is the minimum
+
+    // Find the index of the minimum element in the unsorted part of the array
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the found minimum element with the first element
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
   }
+
+  return arr;
 }
-console.log(stringWithoutSpaces);
+const arr = [64, 25, 12, 22, 11];
+console.log(selectionSort(arr)); // Outputs: [11, 12, 22, 25, 64]
