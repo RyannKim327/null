16c8b3f80d6e9df75d6f915fb3eac4e28a2260da
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:12:48 +0800

    2-1-2024 19:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,96 +1,13 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function calculateMean(numbers) {
+  if (numbers.length === 0) {
+    return 0; // Return 0 for an empty list, but you can choose another value if desired
   }
+
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  const mean = sum / numbers.length;
+
+  return mean;
 }
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-
-    this.size++;
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-
-    const removedNode = this.head;
-    this.head = this.head.next;
-    removedNode.next = null;
-
-    if (this.head === null) {
-      this.tail = null;
-    }
-
-    this.size--;
-
-    return removedNode.value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      return null;
-    }
-
-    return this.head.value;
-  }
-
-  print() {
-    if (this.isEmpty()) {
-      console.log("Queue is empty");
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode !== null) {
-      console.log(currentNode.value);
-      currentNode = currentNode.next;
-    }
-  }
-}
-const queue = new Queue();
-console.log(queue.isEmpty()); // true
-
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-console.log(queue.getSize()); // 3
-console.log(queue.peek()); // 10
-
-queue.print();
-// Output:
-// 10
-// 20
-// 30
-
-console.log(queue.dequeue()); // 10
-console.log(queue.getSize()); // 2
-
-console.log(queue.dequeue()); // 20
-console.log(queue.dequeue()); // 30
-console.log(queue.isEmpty()); // true
+const myList = [1, 2, 3, 4, 5];
+const mean = calculateMean(myList);
+console.log(mean); // Output: 3
