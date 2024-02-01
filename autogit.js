78b0cff7397797c7ac2b6f39954c72326bbb46e2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:49:14 +0800

    2-1-2024 18:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,5 @@
-class HashTable {
-  constructor() {
-    this.table = {};
-  }
-}
-HashTable.prototype.hash = function(key) {
-  return key.hashCode(); // Built-in hashCode function for strings
-};
-HashTable.prototype.add = function(key, value) {
-  const index = this.hash(key);
-  this.table[index] = value;
-};
+let stringNum = "42";
+let integerNum = parseInt(stringNum);
 
-HashTable.prototype.get = function(key) {
-  const index = this.hash(key);
-  return this.table[index];
-};
-const hashTable = new HashTable();
-hashTable.add("name", "John");
-hashTable.add("age", 30);
-
-console.log(hashTable.get("name")); // Output: John
-console.log(hashTable.get("age")); // Output: 30
+console.log(integerNum); // Output: 42
+console.log(typeof integerNum); // Output: number
