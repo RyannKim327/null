2a4de18e40ceabe57cccef5108d1eb0a84370b60
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:48:32 +0800

    2-1-2024 18:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,12 @@
-function ListNode(val, next) {
-  this.val = val;
-  this.next = next;
+let str = "Hello World";
+let reversed = str.split("").reverse().join("");
+console.log(reversed);
+let str = "Hello World";
+let reversed = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversed += str[i];
 }
-
-function isPalindrome(head) {
-  let values = [];
-  let currentNode = head;
-
-  while (currentNode) {
-    values.push(currentNode.val);
-    currentNode = currentNode.next;
-  }
-
-  let left = 0;
-  let right = values.length - 1;
-
-  while (left < right) {
-    if (values[left] !== values[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-
-  return true;
-}
-
-// Example usage:
-let linkedList = new ListNode(1);
-linkedList.next = new ListNode(2);
-linkedList.next.next = new ListNode(3);
-linkedList.next.next.next = new ListNode(2);
-linkedList.next.next.next.next = new ListNode(1);
-console.log(isPalindrome(linkedList)); // Output: true
+console.log(reversed);
+let str = "Hello World";
+let reversed = str.split("").reduce((accumulator, char) => char + accumulator, "");
+console.log(reversed);
