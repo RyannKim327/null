46abbc1f00640ba3b5c98489031d2c84645cf38d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:07:17 +0800

    2-1-2024 19:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,15 @@
-// Import required Java classes
-var URL = Java.type("java.net.URL");
-var HttpURLConnection = Java.type("java.net.HttpURLConnection");
-var BufferedReader = Java.type("java.io.BufferedReader");
-var InputStreamReader = Java.type("java.io.InputStreamReader");
+function findSecondLargest(array) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
 
-// Define AsyncTask class
-var ConnectTask = Java.extend(Java.type("android.os.AsyncTask"), {
-  doInBackground: function() {
-    var result = "";
-
-    try {
-      var url = new URL("http://www.example.com"); // replace with your server URL
-      var connection = url.openConnection();
-      connection.setRequestMethod("GET");
-
-      var inputStream = connection.getInputStream();
-      var reader = new BufferedReader(new InputStreamReader(inputStream));
-
-      var line = "";
-      while ((line = reader.readLine()) != null) {
-        result += line;
-      }
-
-      reader.close();
-      inputStream.close();
-      connection.disconnect();
-    } catch (e) {
-      console.error("Error occurred: " + e);
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+      secondLargest = largest;
+      largest = array[i];
+    } else if (array[i] > secondLargest && array[i] !== largest) {
+      secondLargest = array[i];
     }
-
-    return result;
-  },
-
-  onPostExecute: function(result) {
-    console.log("Response from server: " + result);
-    // Execute further actions on the UI thread with the received result
   }
-});
 
-// Create instance of ConnectTask and execute it
-var connectTask = new ConnectTask();
-connectTask.execute();
+  return secondLargest;
+}
