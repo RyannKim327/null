f417e5cb31bf9fcb30dfea6fceede9a8b10e9dee
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:23:59 +0800

    2-1-2024 18:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,19 @@
-function isPalindrome(str) {
-  // Convert the string to lowercase and remove non-alphanumeric characters
-  const alphaNumericStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-
-  let start = 0;
-  let end = alphaNumericStr.length - 1;
-
-  while (start < end) {
-    if (alphaNumericStr[start] !== alphaNumericStr[end]) {
-      return false; // Not a palindrome
-    }
-    start++;
-    end--;
+function bwtTransform(input) {
+  let transformArray = [];
+  
+  for (let i = 0; i < input.length; i++) {
+    transformArray.push(input.substr(i) + input.substr(0, i));
   }
 
-  return true; // Palindrome
+  transformArray.sort();
+
+  let transformedString = '';
+  for (let i = 0; i < transformArray.length; i++) {
+    transformedString += transformArray[i].charAt(input.length - 1);
+  }
+
+  return transformedString;
 }
+let inputString = 'example';
+let transformedString = bwtTransform(inputString);
+console.log(transformedString); // Output: eelpmax
