8f9bda3e1ab6b07ea000a796de044757d3ff6d83
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:51:12 +0800

    2-1-2024 18:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,4 @@
-function beamSearch(startState, beamWidth, maxIterations, getSuccessorStates, evaluateState) {
-  let beam = [startState];
+let stringWithSpaces = "Hello, world! This is a string with spaces.";
+let stringWithoutSpaces = stringWithSpaces.replace(/\s/g, "");
 
-  for (let i = 0; i < maxIterations; i++) {
-    let nextBeam = [];
-
-    for (let s of beam) {
-      let successorStates = getSuccessorStates(s);
-
-      for (let nextState of successorStates) {
-        let score = evaluateState(nextState);
-        let nextStateObj = { state: nextState, score: score, parent: s };
-        nextBeam.push(nextStateObj);
-      }
-    }
-
-    nextBeam.sort((a, b) => b.score - a.score);
-    beam = nextBeam.slice(0, beamWidth);
-
-    if (beam.length === 0) {
-      break;
-    }
-  }
-
-  return beam[0].state;
-}
+console.log(stringWithoutSpaces);  // Output: "Hello,world!Thisisastringwithspaces."
