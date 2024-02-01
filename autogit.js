8075f40877b4292db9e088424bd78ad28ef9ee0e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:13:50 +0800

    2-1-2024 19:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,5 @@
-function longestCommonSubsequence(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-  
-  // Create a 2D array to store the lengths of common subsequences
-  const dp = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
+const numbers = [3, 1, 5, 2, 4];
 
-  // Building the dp table
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-  
-  // Backtracking to find the longest common subsequence
-  let result = '';
-  let i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      result = str1[i - 1] + result;
-      i--;
-      j--;
-    } else {
-      if (dp[i - 1][j] > dp[i][j - 1]) {
-        i--;
-      } else {
-        j--;
-      }
-    }
-  }
+numbers.sort((a, b) => a - b);
 
-  return result;
-}
-
-// Example usage
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-const lcs = longestCommonSubsequence(str1, str2);
-console.log(lcs); // Outputs "ADH"
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
