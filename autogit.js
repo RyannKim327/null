dc66fd31b545966f271123c1dca5854878734af2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:11:52 +0800

    2-1-2024 19:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,21 @@
-function mergeArrays(arr1, arr2) {
-  let merged = [];
-  let i = 0, j = 0;
+function isPrime(number) {
+  // Check if the number is less than 2
+  if (number < 2) {
+    return false;
+  }
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      merged.push(arr1[i]);
-      i++;
-    } else {
-      merged.push(arr2[j]);
-      j++;
+  // Iterate from 2 to the square root of the number
+  // If any number divides the input without leaving a remainder, it's not prime
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
 
-  // Add remaining elements from arr1
-  while (i < arr1.length) {
-    merged.push(arr1[i]);
-    i++;
-  }
-
-  // Add remaining elements from arr2
-  while (j < arr2.length) {
-    merged.push(arr2[j]);
-    j++;
-  }
-
-  return merged;
+  // If no divisors are found, the number is prime
+  return true;
 }
-function findMedian(arr) {
-  const n = arr.length;
-  const mid = Math.floor(n / 2);
 
-  if (n % 2 === 0) {
-    // Even number of elements
-    return (arr[mid - 1] + arr[mid]) / 2;
-  } else {
-    // Odd number of elements
-    return arr[mid];
-  }
-}
-const arr1 = [1, 3, 5];
-const arr2 = [2, 4, 6];
-
-const merged = mergeArrays(arr1, arr2);
-const median = findMedian(merged);
-
-console.log(median); // Output: 3.5
+// Usage example
+console.log(isPrime(7)); // Output: true
+console.log(isPrime(10)); // Output: false
