8b0b09cc98504caeee56d7fa89bbbbf417a5ba72
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:12:55 +0800

    2-1-2024 19:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,3 @@
-function calculateMean(numbers) {
-  if (numbers.length === 0) {
-    return 0; // Return 0 for an empty list, but you can choose another value if desired
-  }
-
-  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-  const mean = sum / numbers.length;
-
-  return mean;
-}
-const myList = [1, 2, 3, 4, 5];
-const mean = calculateMean(myList);
-console.log(mean); // Output: 3
+var str = "Hello   World";
+var result = str.replace(/\s/g, "");
+console.log(result);  // Output: "HelloWorld"
