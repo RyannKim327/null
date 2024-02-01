6c56f62a0d89e6b4cbea34bcbbd082998490de90
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:33:40 +0800

    2-1-2024 18:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,106 +1,21 @@
-class BTreeNode {
-  constructor(t, leaf) {
-    this.t = t; // Minimum degree
-    this.keys = []; // Array of keys stored in the node
-    this.children = []; // Array of child pointers
-    this.leaf = leaf; // Is true if the node is a leaf, false otherwise
-  }
-}
-class BTree {
-  constructor(t) {
-    this.root = null; // Root node of the B-tree
-    this.t = t; // Minimum degree
-  }
-}
-BTree.prototype.insert = function(key) {
-  if (!this.root) {
-    // Tree is empty, create the root node
-    this.root = new BTreeNode(this.t, true);
-    this.root.keys.push(key);
-  } else {
-    if (this.root.keys.length === (2 * this.t - 1)) {
-      // Root is full, split it
-      const newRoot = new BTreeNode(this.t, false);
-      newRoot.children.push(this.root);
-      this.splitChild(newRoot, 0, this.root);
-  
-      // Insert the key into the appropriate child
-      this.insertNonFull(newRoot, key);
-  
-      // Update the root
-      this.root = newRoot;
-    } else {
-      // Root is not full, insert the key in the appropriate child
-      this.insertNonFull(this.root, key);
-    }
-  }
-};
+function findLongestIncreasingSubsequence(arr) {
+  const n = arr.length;
+  const lis = Array(n).fill(1);
+  let result = 1;
 
-BTree.prototype.insertNonFull = function(node, key) {
-  let i = node.keys.length - 1;
-  
-  if (node.leaf) {
-    // Node is a leaf, insert the key at the right position
-    while (i >= 0 && key < node.keys[i]) {
-      i--;
-    }
-    node.keys.splice(i + 1, 0, key);
-  } else {
-    // Node is not a leaf, find the appropriate child to insert the key
-    while (i >= 0 && key < node.keys[i]) {
-      i--;
-    }
-    i++;
-    if (node.children[i].keys.length === (2 * this.t - 1)) {
-      // Child is full, split it
-      this.splitChild(node, i, node.children[i]);
-      if (key > node.keys[i]) {
-        i++;
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+        lis[i] = lis[j] + 1;
+        if (lis[i] > result) result = lis[i];
       }
     }
-    this.insertNonFull(node.children[i], key);
   }
-};
 
-BTree.prototype.splitChild = function(parent, index, child) {
-  const newNode = new BTreeNode(this.t, child.leaf);
-  
-  // Move keys from the child to the new node
-  newNode.keys = child.keys.splice(this.t, child.keys.length - 1);
-  
-  // Move children from the child to the new node
-  if (!child.leaf) {
-    newNode.children = child.children.splice(this.t, child.children.length);
-  }
-  
-  // Insert the new node into the parent
-  parent.keys.splice(index, 0, child.keys.pop());
-  parent.children.splice(index + 1, 0, newNode);
-};
-BTree.prototype.search = function(key) {
-  return this.searchNode(this.root, key);
-};
+  return result;
+}
 
-BTree.prototype.searchNode = function(node, key) {
-  let i = 0;
-  while (i < node.keys.length && key > node.keys[i]) {
-    i++;
-  }
-  
-  if (node.keys[i] === key) {
-    return true;
-  } else if (node.leaf) {
-    // Key is not found
-    return false;
-  } else {
-    // Continue searching in the appropriate child
-    return this.searchNode(node.children[i], key);
-  }
-};
-const bTree = new BTree(3); // Create a B-tree with minimum degree of 3
-bTree.insert(5);
-bTree.insert(10);
-bTree.insert(15);
-
-console.log(bTree.search(10)); // Output: true
-console.log(bTree.search(20)); // Output: false
+// Example usage:
+const arr = [10, 22, 9, 33, 21, 50, 41, 60];
+const longestSubsequenceLength = findLongestIncreasingSubsequence(arr);
+console.log(`Length of Longest Increasing Subsequence: ${longestSubsequenceLength}`);
