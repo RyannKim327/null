6cb5534ee9ff15623cdefd14cf004f11aa5b746c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:51:20 +0800

    2-1-2024 18:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,16 @@
-let stringWithSpaces = "Hello, world! This is a string with spaces.";
-let stringWithoutSpaces = stringWithSpaces.replace(/\s/g, "");
+function findMedianSortedArrays(nums1, nums2) {
+  const merged = nums1.concat(nums2);
+  const length = merged.length;
 
-console.log(stringWithoutSpaces);  // Output: "Hello,world!Thisisastringwithspaces."
+  if (length % 2 === 1) {
+    return merged[Math.floor(length / 2)];
+  } else {
+    const midIndex = length / 2;
+    return (merged[midIndex - 1] + merged[midIndex]) / 2;
+  }
+}
+const nums1 = [1, 3, 5];
+const nums2 = [2, 4];
+
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median);  // Output: 3
