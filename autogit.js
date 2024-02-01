146e408d9ff186c868e71ca4782f8cba7b6938f3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:07:46 +0800

    2-1-2024 19:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,5 @@
-function shellSort(arr) {
-  const len = arr.length;
-  let gap = Math.floor(len / 2);
+// Prompt the user for their name
+const name = prompt("What is your name?");
 
-  while (gap > 0) {
-    for (let i = gap; i < len; i++) {
-      let temp = arr[i];
-      let j = i;
-
-      while (j >= gap && arr[j - gap] > temp) {
-        arr[j] = arr[j - gap];
-        j -= gap;
-      }
-
-      arr[j] = temp;
-    }
-
-    gap = Math.floor(gap / 2);
-  }
-
-  return arr;
-}
-const unsortedArray = [7, 2, 3, 8, 5, 1, 6, 4];
-const sortedArray = shellSort(unsortedArray);
-console.log(sortedArray);
+// Print a welcome message
+console.log(`Hello, ${name}! Welcome to the website.`);
