da6eec1c21ef147d0918f17d3a4df3be8260ac20
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:37:52 +0800

    2-1-2024 18:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,102 +1,42 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function findKthSmallestElement(arr, k) {
+  arr.sort((a, b) => a - b); // Sort the array in ascending order
+  return arr[k - 1]; // Return the kth element (subtract 1 as the index is zero-based)
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
+function findKthSmallestElement(arr, k) {
+  if (k < 1 || k > arr.length) {
+    return null; // Invalid input
   }
 
-  isEmpty() {
-    return this.head === null;
-  }
+  function partition(arr, left, right) {
+    const pivotValue = arr[right];
+    let i = left;
 
-  addToHead(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  addToTail(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  removeFromHead() {
-    if (this.isEmpty()) {
-      return undefined;
-    }
-
-    const value = this.head.value;
-    this.head = this.head.next;
-
-    if (this.head === null) {
-      this.tail = null;
-    }
-
-    return value;
-  }
-
-  removeFromTail() {
-    if (this.isEmpty()) {
-      return undefined;
-    }
-
-    let currentNode = this.head;
-    let previousNode = null;
-
-    while (currentNode.next !== null) {
-      previousNode = currentNode;
-      currentNode = currentNode.next;
-    }
-
-    if (previousNode === null) {
-      // Only one element in the list
-      this.head = null;
-      this.tail = null;
-    } else {
-      previousNode.next = null;
-      this.tail = previousNode;
-    }
-
-    return currentNode.value;
-  }
-
-  search(value) {
-    let currentNode = this.head;
-
-    while (currentNode !== null) {
-      if (currentNode.value === value) {
-        return true;
+    for (let j = left; j < right; j++) {
+      if (arr[j] < pivotValue) {
+        [arr[i], arr[j]] = [arr[j], arr[i]]; // Swap elements
+        i++;
       }
-      currentNode = currentNode.next;
     }
 
-    return false;
+    [arr[i], arr[right]] = [arr[right], arr[i]]; // Swap pivot with partition position
+    return i;
   }
+
+  function quickselect(arr, left, right, k) {
+    if (left === right) {
+      return arr[left];
+    }
+
+    const pivotIndex = partition(arr, left, right);
+
+    if (k === pivotIndex) {
+      return arr[k];
+    } else if (k < pivotIndex) {
+      return quickselect(arr, left, pivotIndex - 1, k);
+    } else {
+      return quickselect(arr, pivotIndex + 1, right, k);
+    }
+  }
+
+  return quickselect(arr, 0, arr.length - 1, k - 1); // Subtract 1 as the index is zero-based
 }
-const list = new LinkedList();
-list.addToHead(3);
-list.addToHead(2);
-list.addToHead(1);
-
-console.log(list.search(2)); // true
-console.log(list.search(4)); // false
-
-list.removeFromHead();
-console.log(list.search(1)); // false
