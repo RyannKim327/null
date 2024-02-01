4e8cdf3fa00db86190b28ea9fa10f9168f6a8da9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:58:49 +0800

    2-1-2024 18:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,29 @@
-let str = "42";
-let num = parseInt(str);
-
-console.log(num); // Output: 42
-console.log(typeof num); // Output: number
+function getDigit(num, pos) {
+  return Math.floor(Math.abs(num) / Math.pow(10, pos)) % 10;
+}
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+function mostDigits(arr) {
+  let maxDigits = 0;
+  for (let i = 0; i < arr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
+  }
+  return maxDigits;
+}
+function radixSort(arr) {
+  const maxDigits = mostDigits(arr);
+  for (let k = 0; k < maxDigits; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < arr.length; i++) {
+      const digit = getDigit(arr[i], k);
+      digitBuckets[digit].push(arr[i]);
+    }
+    arr = [].concat(...digitBuckets);
+  }
+  return arr;
+}
+const array = [23, 345, 5467, 12, 2345, 9852];
+const sortedArray = radixSort(array);
+console.log(sortedArray); // Output: [12, 23, 345, 2345, 5467, 9852]
