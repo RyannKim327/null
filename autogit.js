fab191d3b299f7cda08d34fb3090ecfe99fdcf7e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:29:38 +0800

    2-1-2024 18:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,3 @@
-let str = "   Hello,  World!   ";
-let trimmedStr = str.replace(/\s/g, '');
-console.log(trimmedStr);
-Hello,World!
+const numbers = [5, 2, 9, 1, 7];
+numbers.sort((a, b) => a - b);
+console.log(numbers); // Output: [1, 2, 5, 7, 9]
