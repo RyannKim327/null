6079a0c130d7b6c6be5f7f7660153df2594a4715
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:56:52 +0800

    2-1-2024 18:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,81 +1,24 @@
-function boyerMooreSearch(text, pattern) {
-  const badMatchTable = {};
-  const goodSuffixTable = [];
-  const patternLength = pattern.length;
+function bubbleSort(arr) {
+  var len = arr.length;
+  var isSwapped;
   
-  // Populate badMatchTable
-  for (let i = 0; i < patternLength - 1; i++) {
-    badMatchTable[pattern[i]] = patternLength - i - 1;
-  }
+  do {
+    isSwapped = false;
 
-  // Populate goodSuffixTable
-  buildGoodSuffixTable(pattern, goodSuffixTable);
+    for (var i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        isSwapped = true;
+      }
+    }
+  } while (isSwapped);
   
-  let i = patternLength - 1;
-
-  while (i < text.length) {
-    let j = patternLength - 1;
-    let k = i;
-
-    while (j >= 0 && text[k] === pattern[j]) {
-      j--;
-      k--;
-    }
-
-    if (j === -1) {
-      return k + 1; // Match found, return the starting index
-    } else {
-      const badMatchShift = badMatchTable[text[k]] || patternLength;
-      const goodSuffixShift = goodSuffixTable[j];
-
-      i += Math.max(badMatchShift, goodSuffixShift);
-    }
-  }
-
-  return -1; // No match found
+  return arr;
 }
 
-function buildGoodSuffixTable(pattern, goodSuffixTable) {
-  const patternLength = pattern.length;
-  let lastPrefixPosition = patternLength;
-
-  for (let i = patternLength - 1; i >= 0; i--) {
-    if (isPrefix(pattern, i + 1)) {
-      lastPrefixPosition = i + 1;
-    }
-
-    goodSuffixTable[i] = lastPrefixPosition + (patternLength - 1 - i);
-  }
-
-  for (let i = 0; i < patternLength - 1; i++) {
-    const suffixLength = getSuffixLength(pattern, i);
-    if (pattern[i - suffixLength] !== pattern[patternLength - 1 - suffixLength]) {
-      goodSuffixTable[patternLength - 1 - suffixLength] = patternLength - 1 - i + suffixLength;
-    }
-  }
-}
-
-function isPrefix(pattern, startIndex) {
-  const patternLength = pattern.length;
-  for (let i = startIndex, j = 0; i < patternLength; i++, j++) {
-    if (pattern[i] !== pattern[j]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-function getSuffixLength(pattern, startIndex) {
-  let length = 0;
-  let j = pattern.length - 1;
-  for (let i = startIndex; i >= 0 && pattern[i] === pattern[j]; i--, j--) {
-    length++;
-  }
-  return length;
-}
-
-// Example usage:
-const text = "This is an example text for searching";
-const pattern = "example";
-const index = boyerMooreSearch(text, pattern);
-console.log("Pattern found at index:", index);
+// Testing the bubbleSort function
+var array = [5, 3, 8, 2, 1, 4];
+console.log("Before sorting:", array);
+console.log("After sorting:", bubbleSort(array));
