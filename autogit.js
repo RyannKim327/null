a197bdbfa04c212416725a93c1a8f618ebb3dfdc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:16:05 +0800

    2-1-2024 19:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,19 @@
-area = (base * height) / 2;
-function calculateTriangleArea(base, height) {
-  var area = (base * height) / 2;
-  return area;
+function cleanString(str) {
+  return str.replace(/[^a-z]/gi, '').toLowerCase().split('').sort().join('');
 }
+function areAnagrams(str1, str2) {
+  // Clean the strings
+  const cleanedStr1 = cleanString(str1);
+  const cleanedStr2 = cleanString(str2);
 
-// Example usage:
-var base = 5;
-var height = 10;
-console.log("The area of the triangle is: " + calculateTriangleArea(base, height));
+  // Compare the cleaned strings
+  return cleanedStr1 === cleanedStr2;
+}
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (areAnagrams(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
