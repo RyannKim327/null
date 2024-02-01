208f2c441bc46c64dc87c414580c835e878300d1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:05:26 +0800

    2-1-2024 19:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,67 +1,14 @@
-function biDirectionalSearch(graph, startNode, goalNode) {
-    // Step 1: Define the graph
-
-    // Step 2: Forward search
-    const forwardVisited = new Set();
-    const forwardQueue = [[startNode, null]];
-
-    while (forwardQueue.length > 0) {
-        const [currentNode, parent] = forwardQueue.shift();
-        forwardVisited.add(currentNode);
-
-        if (currentNode === goalNode) {
-            return constructPath(parent, currentNode);
-        }
-
-        for (const neighbor of graph[currentNode]) {
-            if (!forwardVisited.has(neighbor)) {
-                forwardQueue.push([neighbor, currentNode]);
-            }
-        }
-    }
-
-    // Step 3: Backward search
-    const backwardVisited = new Set();
-    const backwardQueue = [[goalNode, null]];
-
-    while (backwardQueue.length > 0) {
-        const [currentNode, parent] = backwardQueue.shift();
-        backwardVisited.add(currentNode);
-
-        if (currentNode === startNode) {
-            return constructPath(parent, currentNode).reverse();
-        }
-
-        for (const neighbor of graph[currentNode]) {
-            if (!backwardVisited.has(neighbor)) {
-                backwardQueue.push([neighbor, currentNode]);
-            }
-        }
-    }
-
-    return null; // No path found
-
-    // Step 5: Build the final path
-    function constructPath(parent, currentNode) {
-        const path = [currentNode];
-        while (parent !== null) {
-            path.push(parent);
-            parent = backwardVisited.has(parent) ? graph[parent][0] : graph[parent];
-        }
-        return path;
-    }
+function findStringLength(str) {
+  var length = 0;
+  
+  for (var i = 0; i < str.length; i++) {
+    length++;
+  }
+  
+  return length;
 }
-const graph = {
-    A: ['B', 'D'],
-    B: ['A', 'C'],
-    C: ['B', 'E'],
-    D: ['A', 'E'],
-    E: ['C', 'D', 'F'],
-    F: ['E']
-};
 
-const startNode = 'A';
-const goalNode = 'F';
+var myString = "Hello, World!";
+var stringLength = findStringLength(myString);
 
-const path = biDirectionalSearch(graph, startNode, goalNode);
-console.log('Shortest path:', path);
+console.log("Length of string:", stringLength);
