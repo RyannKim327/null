5febea702882d182f0bd56e1c5c625cbae5fb18e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:03:24 +0800

    2-1-2024 19:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,27 @@
-function longestCommonPrefix(strings) {
-  if (strings.length === 0) {
-    return '';
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  let prefix = strings[0];
-  
-  for (let i = 1; i < strings.length; i++) {
-    while (strings[i].indexOf(prefix) !== 0) {
-      prefix = prefix.substring(0, prefix.length - 1);
-      
-      if (prefix === '') {
-        return '';
-      }
-    }
-  }
-  
-  return prefix;
 }
-const strings = ['apple', 'app', 'application', 'aptitude'];
-const commonPrefix = longestCommonPrefix(strings);
-console.log(commonPrefix); // Output: 'app'
+
+function countLeafNodes(root) {
+  if (root === null) {
+    return 0;
+  }
+  
+  if (root.left === null && root.right === null) {
+    return 1;
+  }
+  
+  return countLeafNodes(root.left) + countLeafNodes(root.right);
+}
+const root = new Node(3);
+root.left = new Node(9);
+root.right = new Node(20);
+root.right.left = new Node(15);
+root.right.right = new Node(7);
+
+const leafNodeCount = countLeafNodes(root);
+console.log('Number of leaf nodes:', leafNodeCount); // Output: Number of leaf nodes: 3
