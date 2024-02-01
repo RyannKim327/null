f2be873f741c01c4ca556de272182920b8a45b80
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:43:46 +0800

    2-1-2024 18:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,45 @@
-let str = "  Hello,   World!  ";
-let trimmedStr = str.replace(/\s/g, '');
-console.log(trimmedStr); // Output: "Hello,World!"
-let str = "  Hello,   World!  ";
-let trimmedStr = str.split(' ').join('');
-console.log(trimmedStr); // Output: "Hello,World!"
-let str = "  Hello,   World!  ";
-let trimmedStr = str.trim().replace(/\s+/g, '');
-console.log(trimmedStr); // Output: "Hello,World!"
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new ListNode(data);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+    }
+  }
+
+  getLength() {
+    let currentNode = this.head;
+    let count = 0;
+
+    while (currentNode) {
+      count++;
+      currentNode = currentNode.next;
+    }
+
+    return count;
+  }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+console.log(linkedList.getLength()); // Output: 3
