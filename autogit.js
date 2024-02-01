a0b444e3b8af21277b672fd5480eab4871d2d060
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:33:12 +0800

    2-1-2024 18:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,3 @@
-let str = "HELLO WORLD";
-let lowerCaseStr = str.toLowerCase();
-
-console.log(lowerCaseStr); // Output: hello world
+const unsortedArray = [6, 5, 3, 1, 8, 7, 2, 4];
+const sortedArray = heapSort(unsortedArray);
+console.log(sortedArray); // [1, 2, 3, 4, 5, 6, 7, 8]
