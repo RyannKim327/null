28827296f5ce5c6a0ae14a231cbcabfb7b5f0f90
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:42:20 +0800

    2-1-2024 18:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,18 @@
-function factorial(number) {
-  if (number === 0 || number === 1) {
-    return 1;
-  } else {
-    let result = 1;
-    for (let i = 2; i <= number; i++) {
-      result *= i;
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swapping elements
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
     }
-    return result;
   }
+  return arr;
 }
 
-// Example Usage
-console.log(factorial(5)); // Output: 120
+// Example usage
+var numbers = [5, 3, 8, 4, 2];
+console.log(bubbleSort(numbers)); // Output: [2, 3, 4, 5, 8]
