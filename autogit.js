6687a661d687886e3ccbbe00f80f2e469e7036eb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:25:25 +0800

    2-1-2024 18:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,34 @@
-const str = "  Hello,    World!  ";
-const trimmedStr = str.trim();
-console.log(trimmedStr);  // Output: "Hello,    World!"
-const str = "  Hello,    World!  ";
-const trimmedStr = str.replace(/\s/g, "");
-console.log(trimmedStr);  // Output: "Hello,World!"
+function findCommonElements(array1, array2) {
+  const commonElements = [];
+
+  for (let element of array1) {
+    if (array2.includes(element)) {
+      commonElements.push(element);
+    }
+  }
+
+  return commonElements;
+}
+function findCommonElements(array1, array2) {
+  return array1.filter(element => array2.includes(element));
+}
+function findCommonElements(array1, array2) {
+  const set2 = new Set(array2);
+  return array1.filter(element => set2.has(element));
+}
+function findCommonElements(array1, array2) {
+  const obj = {};
+  const commonElements = [];
+
+  for (let element of array1) {
+    obj[element] = true;
+  }
+
+  for (let element of array2) {
+    if (obj[element]) {
+      commonElements.push(element);
+    }
+  }
+
+  return commonElements;
+}
