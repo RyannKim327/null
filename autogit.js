cc97c9506a3d1384785ad8e609ea0b07d62eb121
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:59:30 +0800

    2-1-2024 18:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,27 @@
-let str = "Hello, world! This is a string with spaces.";
-let result = str.replace(/\s/g, "");
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-console.log(result);
-Hello,world!Thisisastringwithspaces.
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
+// Create a linked list
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+const linkedList = new ListNode(1);
+linkedList.next = new ListNode(2);
+linkedList.next.next = new ListNode(3);
+linkedList.next.next.next = new ListNode(4);
+
+// Find the middle element
+const middleElement = findMiddleElement(linkedList);
+console.log(middleElement.val); // Output: 3
