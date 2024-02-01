b4825298cae5794184199a8002e8970d04dcc221
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:28:39 +0800

    2-1-2024 18:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,130 @@
-class HashTable {
-  constructor(size) {
-    this.size = size;
-    this.buckets = new Array(size);
+class AVLNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.height = 1;
   }
 }
-HashTable.prototype.hash = function(key) {
-  let hash = 0;
-  for (let i = 0; i < key.length; i++) {
-    hash += key.charCodeAt(i);
-  }
-  return hash % this.size;
-};
-HashTable.prototype.insert = function(key, value) {
-  const index = this.hash(key);
-  if (!this.buckets[index]) {
-    this.buckets[index] = [];
-  }
-  this.buckets[index].push({ key, value });
-};
 
-HashTable.prototype.get = function(key) {
-  const index = this.hash(key);
-  if (this.buckets[index]) {
-    for (let i = 0; i < this.buckets[index].length; i++) {
-      if (this.buckets[index][i].key === key) {
-        return this.buckets[index][i].value;
-      }
-    }
+class AVLTree {
+  constructor() {
+    this.root = null;
   }
-  return undefined;
-};
-const hashTable = new HashTable(10);
-hashTable.insert("apple", 42);
-hashTable.insert("banana", 17);
-console.log(hashTable.get("apple"));   // Output: 42
-console.log(hashTable.get("banana"));  // Output: 17
-console.log(hashTable.get("orange"));  // Output: undefined
+
+  // Get the height of a node
+  getHeight(node) {
+    if (node === null) {
+      return 0;
+    }
+    return node.height;
+  }
+
+  // Get the balance factor of a node
+  getBalanceFactor(node) {
+    if (node === null) {
+      return 0;
+    }
+    return this.getHeight(node.left) - this.getHeight(node.right);
+  }
+
+  // Update the height of a node
+  updateHeight(node) {
+    if (node === null) {
+      return;
+    }
+    node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+  }
+
+  // Perform left rotation on a node
+  leftRotate(node) {
+    const newRoot = node.right;
+    node.right = newRoot.left;
+    newRoot.left = node;
+
+    this.updateHeight(node);
+    this.updateHeight(newRoot);
+
+    return newRoot;
+  }
+
+  // Perform right rotation on a node
+  rightRotate(node) {
+    const newRoot = node.left;
+    node.left = newRoot.right;
+    newRoot.right = node;
+
+    this.updateHeight(node);
+    this.updateHeight(newRoot);
+
+    return newRoot;
+  }
+
+  // Balance the tree at a specific node
+  balance(node) {
+    this.updateHeight(node);
+
+    if (this.getBalanceFactor(node) > 1) {
+      if (this.getBalanceFactor(node.left) < 0) {
+        node.left = this.leftRotate(node.left);
+      }
+      return this.rightRotate(node);
+    }
+
+    if (this.getBalanceFactor(node) < -1) {
+      if (this.getBalanceFactor(node.right) > 0) {
+        node.right = this.rightRotate(node.right);
+      }
+      return this.leftRotate(node);
+    }
+
+    return node;
+  }
+
+  // Insert a value into the tree
+  insert(value) {
+    this.root = this._insertRecursive(this.root, value);
+  }
+
+  _insertRecursive(node, value) {
+    if (node === null) {
+      return new AVLNode(value);
+    }
+
+    if (value < node.value) {
+      node.left = this._insertRecursive(node.left, value);
+    } else {
+      node.right = this._insertRecursive(node.right, value);
+    }
+
+    return this.balance(node);
+  }
+
+  // Print the tree in-order
+  printInOrder() {
+    this._printInOrderRecursive(this.root);
+    console.log();
+  }
+
+  _printInOrderRecursive(node) {
+    if (node === null) {
+      return;
+    }
+
+    this._printInOrderRecursive(node.left);
+    process.stdout.write(node.value + " ");
+    this._printInOrderRecursive(node.right);
+  }
+}
+
+// Usage example
+const avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(5);
+avlTree.insert(15);
+avlTree.insert(3);
+avlTree.insert(8);
+avlTree.insert(12);
+avlTree.insert(17);
+
+avlTree.printInOrder(); // Output: 3 5 8 10 12 15 17
