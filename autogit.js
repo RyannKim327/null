c35a34e52ddeeb2d58b6a4c52bc8844249c78ab6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:54:58 +0800

    2-1-2024 18:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,21 @@
-async function connectToServer() {
-  try {
-    const response = await fetch('http://your-server-url');
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIndex = i;
 
-    if (!response.ok) {
-      throw new Error('Connection failed');
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
     }
 
-    const data = await response.json();
-
-    console.log('Received data:', data);
-  } catch (error) {
-    console.error('Error connecting to server:', error);
+    if (minIndex !== i) {
+      let temp = array[i];
+      array[i] = array[minIndex];
+      array[minIndex] = temp;
+    }
   }
-}
 
-connectToServer();
+  return array;
+}
+let arr = [64, 25, 12, 22, 11];
+console.log(selectionSort(arr));
