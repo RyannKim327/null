c34168e1f770e9a692a27c7413e2ac286d5f5ca9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 18:28:11 +0800

    2-1-2024 18:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,92 +1,28 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+  return arr;
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  isEmpty() {
-    return this.head === null;
-  }
-
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
+function isSorted(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
   }
-
-  append(value) {
-    const newNode = new Node(value);
-
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  delete(value) {
-    if (this.isEmpty()) {
-      return;
-    }
-
-    if (this.head.value === value) {
-      this.head = this.head.next;
-
-      if (this.head === null) {
-        this.tail = null;
-      }
-
-      return;
-    }
-
-    let current = this.head;
-    while (current.next !== null) {
-      if (current.next.value === value) {
-        current.next = current.next.next;
-
-        if (current.next === null) {
-          this.tail = current;
-        }
-
-        return;
-      }
-
-      current = current.next;
-    }
-  }
-
-  display() {
-    let current = this.head;
-    const values = [];
-
-    while (current !== null) {
-      values.push(current.value);
-      current = current.next;
-    }
-
-    console.log(values);
-  }
+  return true;
 }
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-list.prepend(0);
-list.display();  // Output: [0, 1, 2, 3]
-list.delete(2);
-list.display();  // Output: [0, 1, 3]
+
+function bogoSort(arr) {
+  while (!isSorted(arr)) {
+    arr = shuffleArray(arr);
+  }
+  return arr;
+}
+
+// Usage example:
+const unsortedArray = [5, 2, 9, 1, 3];
+const sortedArray = bogoSort(unsortedArray);
+console.log(sortedArray);
