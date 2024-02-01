1af5233744d1af3a25ebf59ffba80e27e4a15f2a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:05:48 +0800

    2-1-2024 19:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,50 @@
-function longestCommonSubstring(s1, s2) {
-  // Initialize a matrix to store the lengths of common substrings
-  const matrix = Array(s1.length + 1)
-    .fill(0)
-    .map(() => Array(s2.length + 1).fill(0));
-
-  let longestSubstringLength = 0; // Length of the longest common substring
-  let endIndex = 0; // Ending index of the longest common substring in s1
-
-  // Traverse the strings and update the matrix
-  for (let i = 1; i <= s1.length; i++) {
-    for (let j = 1; j <= s2.length; j++) {
-      if (s1[i - 1] === s2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-
-        if (matrix[i][j] > longestSubstringLength) {
-          longestSubstringLength = matrix[i][j];
-          endIndex = i - 1;
-        }
-      } else {
-        matrix[i][j] = 0;
-      }
-    }
+class Graph {
+  constructor() {
+    this.vertices = {};
   }
 
-  // Extract the longest common substring
-  const longestSubstring = s1.substr(endIndex - longestSubstringLength + 1, longestSubstringLength);
-  return longestSubstring;
+  addVertex(v) {
+    this.vertices[v] = [];
+  }
+
+  addEdge(v, w) {
+    this.vertices[v].push(w);
+  }
+
+  dfs(startVertex) {
+    const visited = {};
+
+    // Helper function for recursive DFS traversal
+    const dfsUtil = (vertex) => {
+      visited[vertex] = true;
+      console.log(vertex); // Do whatever you want with the vertex
+  
+      const neighbors = this.vertices[vertex];
+      for (let i = 0; i < neighbors.length; i++) {
+        const neighbor = neighbors[i];
+        if (!visited[neighbor]) {
+          dfsUtil(neighbor);
+        }
+      }
+    };
+
+    dfsUtil(startVertex);
+  }
 }
 
-// Example usage:
-const s1 = "abcdef";
-const s2 = "cde";
-const longestSubstring = longestCommonSubstring(s1, s2);
-console.log(longestSubstring); // Output: "cde"
+// Example usage
+const g = new Graph();
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
+
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('B', 'E');
+g.addEdge('C', 'F');
+
+g.dfs('A');
