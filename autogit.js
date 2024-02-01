3655eae9944611cd0346bb327247a0f2a229cefb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:06:45 +0800

    2-1-2024 19:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,9 @@
-function findMedianSortedArrays(nums1, nums2) {
-  const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
-  const length = mergedArray.length;
-  const middleIndex = Math.floor(length / 2);
-
-  if (length % 2 === 1) {
-    return mergedArray[middleIndex];
-  } else {
-    return (mergedArray[middleIndex - 1] + mergedArray[middleIndex]) / 2;
-  }
-}
-
-// Example usage:
-const nums1 = [1, 3, 5];
-const nums2 = [2, 4, 6];
-const median = findMedianSortedArrays(nums1, nums2);
-console.log(median); // Output: 3.5
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log('Received data:', data);
+    // Do something with the data here
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
