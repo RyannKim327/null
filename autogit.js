2ce3f1418bdada1d3b3c3ddd673f8f90fb6b6943
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:15:50 +0800

    2-1-2024 19:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,26 @@
-function findMedianSortedArrays(nums1, nums2) {
-  const merged = mergeArrays(nums1, nums2);
-  const n = merged.length;
-  
-  if (n % 2 === 0) {
-    // Median for an even number of elements is the average of the middle two
-    const middle = n / 2;
-    return (merged[middle - 1] + merged[middle]) / 2;
-  } else {
-    // Median for an odd number of elements is the middle element
-    return merged[Math.floor(n / 2)];
-  }
+function removeDuplicates(array) {
+  return array.filter((element, index) => array.indexOf(element) === index);
 }
 
-function mergeArrays(nums1, nums2) {
-  const merged = [];
-  let i = 0;
-  let j = 0;
-  
-  while (i < nums1.length && j < nums2.length) {
-    if (nums1[i] < nums2[j]) {
-      merged.push(nums1[i]);
-      i++;
-    } else {
-      merged.push(nums2[j]);
-      j++;
+const originalArray = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+const uniqueArray = removeDuplicates(originalArray);
+console.log(uniqueArray);
+function removeDuplicates(array) {
+  return Array.from(new Set(array));
+}
+
+const originalArray = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+const uniqueArray = removeDuplicates(originalArray);
+console.log(uniqueArray);
+function removeDuplicates(array) {
+  return array.reduce((unique, element) => {
+    if (!unique.includes(element)) {
+      unique.push(element);
     }
-  }
-  
-  // Add remaining elements if there are any
-  while (i < nums1.length) {
-    merged.push(nums1[i]);
-    i++;
-  }
-  
-  while (j < nums2.length) {
-    merged.push(nums2[j]);
-    j++;
-  }
-  
-  return merged;
+    return unique;
+  }, []);
 }
-const nums1 = [1, 3];
-const nums2 = [2, 4, 6];
 
-const median = findMedianSortedArrays(nums1, nums2);
-
-console.log(median); // Output: 3.0
+const originalArray = [1, 2, 3, 4, 4, 5, 6, 6, 7];
+const uniqueArray = removeDuplicates(originalArray);
+console.log(uniqueArray);
