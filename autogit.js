951345397c56aeb79ccac9200f973bcd8e8c0d25
RyannKim327 <rksesgundo123@gmail.com>
2024-02-01 19:06:24 +0800

    2-1-2024 19:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,93 +1,30 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
+function findMiddleElement(head) {
+  let slowPointer = head;
+  let fastPointer = head;
+
+  while (fastPointer !== null && fastPointer.next !== null) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
   }
 
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  prepend(data) {
-    const newNode = new Node(data);
-    newNode.next = this.head;
-    this.head = newNode;
-  }
-
-  insertAfter(data, prevNodeData) {
-    const newNode = new Node(data);
-    let current = this.head;
-    while (current) {
-      if (current.data === prevNodeData) {
-        newNode.next = current.next;
-        current.next = newNode;
-        break;
-      }
-      current = current.next;
-    }
-  }
-
-  delete(data) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-      return;
-    }
-
-    let current = this.head;
-    let prev = null;
-
-    while (current) {
-      if (current.data === data) {
-        prev.next = current.next;
-        break;
-      }
-      prev = current;
-      current = current.next;
-    }
-  }
-
-  display() {
-    let current = this.head;
-    let result = '';
-
-    while (current) {
-      result += current.data + ' -> ';
-      current = current.next;
-    }
-
-    result += 'null';
-    console.log(result);
-  }
+  return slowPointer;
 }
 
 // Example usage:
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-list.prepend(0);
-list.insertAfter(2.5, 2);
-list.display(); // Output: 0 -> 1 -> 2 -> 2.5 -> 3 -> null
 
-list.delete(1);
-list.display(); // Output: 0 -> 2 -> 2.5 -> 3 -> null
+// Creating a linked list: 1 -> 2 -> 3 -> 4 -> 5
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+const middleElement = findMiddleElement(head);
+console.log(middleElement.value); // Output: 3
