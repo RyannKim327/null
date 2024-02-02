5d625713a66c9e61fcbc8f71f322cc74f3d881e0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:54:36 +0800

    2-2-2024 18:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,45 @@
-const numbers = [5, 3, 8, 1, 2, 9, 4, 7, 6];
+function prefixTable(pattern) {
+  const table = [0];
+  let prefixLen = 0;
 
-// Using the sort() method with a comparison function
-numbers.sort((a, b) => a - b);
+  for (let i = 1; i < pattern.length; i++) {
+    while (prefixLen > 0 && pattern[i] !== pattern[prefixLen]) {
+      prefixLen = table[prefixLen - 1];
+    }
 
-console.log(numbers);
-[1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if (pattern[i] === pattern[prefixLen]) {
+      prefixLen++;
+    }
+
+    table[i] = prefixLen;
+  }
+
+  return table;
+}
+function KMP(str, pattern) {
+  const table = prefixTable(pattern);
+  let strIndex = 0;
+  let patternIndex = 0;
+
+  while (strIndex < str.length) {
+    if (str[strIndex] === pattern[patternIndex]) {
+      if (patternIndex === pattern.length - 1) {
+        return strIndex - pattern.length + 1;
+      }
+
+      patternIndex++;
+      strIndex++;
+    } else if (patternIndex > 0) {
+      patternIndex = table[patternIndex - 1];
+    } else {
+      strIndex++;
+    }
+  }
+
+  return -1;
+}
+const inputStr = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+
+const result = KMP(inputStr, pattern);
+console.log(result); // Output: 10
