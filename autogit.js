08819bcfc8ffd636f01cc28eeb8ef38f56be9bd6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:48:23 +0800

    2-2-2024 18:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,20 @@
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['C', 'D'],
-  'C': ['E'],
-  'D': ['F'],
-  'E': [],
-  'F': []
-};
-const visited = {};
-const result = [];
-function topologicalSort() {
-  for (let node in graph) {
-    if (!visited[node]) {
-      dfs(node);
+function findFirstRepeatedChar(str) {
+  const hashmap = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (hashmap[char]) {
+      return char;
     }
+
+    hashmap[char] = true;
   }
+
+  return null; // No repeated character found
 }
 
-function dfs(node) {
-  visited[node] = true;
-
-  graph[node].forEach(neighbor => {
-    if (!visited[neighbor]) {
-      dfs(neighbor);
-    }
-  });
-
-  result.unshift(node);
-}
-topologicalSort();
-console.log(result); // ['A', 'B', 'C', 'E', 'D', 'F']
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['C', 'D'],
-  'C': ['E'],
-  'D': ['F'],
-  'E': [],
-  'F': []
-};
-
-const visited = {};
-const result = [];
-
-function topologicalSort() {
-  for (let node in graph) {
-    if (!visited[node]) {
-      dfs(node);
-    }
-  }
-}
-
-function dfs(node) {
-  visited[node] = true;
-
-  graph[node].forEach(neighbor => {
-    if (!visited[neighbor]) {
-      dfs(neighbor);
-    }
-  });
-
-  result.unshift(node);
-}
-
-topologicalSort();
-console.log(result); // ['A', 'B', 'C', 'E', 'D', 'F']
+// Example usage
+const str = "Hello";
+const repeatedChar = findFirstRepeatedChar(str);
+console.log(repeatedChar); // Output: "l"
