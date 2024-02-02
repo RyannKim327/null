d69a8392b5bdfcdff613f216a9797cf0904373e4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:44:01 +0800

    2-2-2024 18:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,10 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function getStringLength(str) {
+  let count = 0;
+  for (let i = 0; str[i] !== undefined; i++) {
+    count++;
   }
+  return count;
 }
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  // Insert a new node at the end of the linked list
-  append(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  // Insert a new node at the beginning of the linked list
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-}
-
-// Example usage
-const list = new LinkedList();
-list.append(2);
-list.append(5);
-list.prepend(1);
-
-console.log(list);
+const string = "Hello, world!";
+const length = getStringLength(string);
+console.log(length); // Output: 13
