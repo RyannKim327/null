0fe14c67f2cf9c599c5f63e9190fc71868a04797
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:44:29 +0800

    2-2-2024 18:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,24 @@
-const axios = require('axios');
+function isPrime(number) {
+  if (number < 2) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
-axios.get('https://api.example.com/data')
-  .then(response => {
-    // Handle data retrieval success
-    console.log(response.data);
-  })
-  .catch(error => {
-    // Handle data retrieval failure
-    console.error(error);
-  });
+function largestPrimeFactor(number) {
+  let largestPrime = 1;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0 && isPrime(i)) {
+      largestPrime = i;
+    }
+  }
+  return largestPrime === 1 ? number : largestPrime;
+}
+
+// Example usage
+const number = 1234567890;
+const largestPrime = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestPrime}`);
