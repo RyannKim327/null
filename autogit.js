bba9ca7b918c35c472e0380a464d7e5bb8bbfb90
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:52:38 +0800

    2-2-2024 18:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,41 @@
-function findFirstNonRepeatingChar(str) {
-  // Step 1: Create an object to store character counts
-  const charCount = {};
-  
-  // Step 2: Iterate through the string and count occurrence of each character
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    charCount[char] = (charCount[char] || 0) + 1;
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
   }
-  
-  // Step 3: Iterate through the string and return the first character with count 1
-  for (let i = 0; i < str.length; i++) {
-    const char = str[i];
-    if (charCount[char] === 1) {
-      return char;
+}
+HashTable.prototype.hash = function(key) {
+  let hash = 0;
+  for (let i = 0; i < key.length; i++) {
+    hash += key.charCodeAt(i);
+  }
+  return hash % this.size;
+};
+HashTable.prototype.insert = function(key, value) {
+  const index = this.hash(key);
+  if (!this.buckets[index]) {
+    this.buckets[index] = [];
+  }
+  this.buckets[index].push({ key, value });
+};
+HashTable.prototype.get = function(key) {
+  const index = this.hash(key);
+  if (this.buckets[index]) {
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i].key === key) {
+        return this.buckets[index][i].value;
+      }
     }
   }
-  
-  // If there are no non-repeating characters, return null or any other value as needed
-  return null;
-}
+  return undefined;
+};
+const hashTable = new HashTable(10);
 
-// Usage example:
-const str = "abacabad";
-const firstNonRepeatingChar = findFirstNonRepeatingChar(str);
-console.log("First non-repeating character:", firstNonRepeatingChar);
+hashTable.insert("apple", 10);
+hashTable.insert("banana", 20);
+hashTable.insert("orange", 30);
+
+console.log(hashTable.get("apple"));    // output: 10
+console.log(hashTable.get("banana"));   // output: 20
+console.log(hashTable.get("orange"));   // output: 30
+console.log(hashTable.get("grape"));    // output: undefined
