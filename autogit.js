078f215a5e04c37445cefdf6fd490e505abefd8e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-02 18:45:20 +0800

    2-2-2024 18:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,44 @@
-function longestCommonPrefix(strs) {
-  if (strs.length === 0) {
-    return '';
-  }
-  
-  let prefix = '';
-  
-  for (let i = 0; i < strs[0].length; i++) {
-    const char = strs[0][i];
-    
-    for (let j = 1; j < strs.length; j++) {
-      if (i >= strs[j].length || strs[j][i] !== char) {
-        return prefix;
+function longestCommonSubsequence(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+
+  // Create a 2D array with dimensions (m+1)x(n+1)
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0));
+
+  // Fill the 2D array using dynamic programming
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1; // Increment the count
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Take the maximum of the previous counts
       }
     }
-    
-    prefix += char;
   }
-  
-  return prefix;
+
+  // Backtrack through the 2D array to find the longest common subsequence
+  let i = m;
+  let j = n;
+  const subsequence = [];
+
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      subsequence.unshift(str1[i - 1]); // Add the character to the beginning of the subsequence
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return subsequence.join('');
 }
 
 // Example usage:
-const strings = ['flower', 'flow', 'flight'];
-const commonPrefix = longestCommonPrefix(strings);
-console.log(commonPrefix);  // Output: "fl"
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+console.log(longestCommonSubsequence(str1, str2)); // Output: ADH
