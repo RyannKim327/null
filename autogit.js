df76373cc70d882dba223ca182c600d92872cd08
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:28:21 +0800

    2-3-2024 23:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,112 +1,27 @@
 class Node {
-  constructor(data) {
-    this.data = data;
+  constructor(value) {
+    this.value = value;
     this.next = null;
   }
 }
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
 
-class LinkedList {
-  constructor() {
-    this.head = null;
+node1.next = node2;
+node2.next = node3;
+function reverseLinkedList(head) {
+  let previous = null;
+  let current = head;
+  let next = null;
+
+  while (current != null) {
+    next = current.next;  // Save the next node
+    current.next = previous;  // Reverse the link
+    previous = current;
+    current = next;
   }
+
+  return previous;  // New head of the reversed list
 }
-LinkedList.prototype.insert = function (data) {
-  const newNode = new Node(data);
-  if (!this.head) {
-    this.head = newNode;
-  } else {
-    let current = this.head;
-    while (current.next) {
-      current = current.next;
-    }
-    current.next = newNode;
-  }
-};
-LinkedList.prototype.isPalindrome = function () {
-  let slowPtr = this.head;
-  let fastPtr = this.head;
-  let prevSlowPtr = null;
-  let midNode = null;
-  let result = true;
-
-  if (this.head !== null && this.head.next !== null) {
-    while (fastPtr !== null && fastPtr.next !== null) {
-      fastPtr = fastPtr.next.next;
-
-      prevSlowPtr = slowPtr;
-      slowPtr = slowPtr.next;
-    }
-
-    if (fastPtr !== null) {
-      midNode = slowPtr;
-      slowPtr = slowPtr.next;
-    }
-
-    let secondHalfPtr = slowPtr;
-    prevSlowPtr.next = null;
-    let reversePtr = this.reverse(secondHalfPtr);
-
-    result = this.compareLists(this.head, reversePtr);
-
-    let reverseSecondHalfPtr = this.reverse(reversePtr);
-    if (midNode !== null) {
-      prevSlowPtr.next = midNode;
-      midNode.next = reverseSecondHalfPtr;
-    } else {
-      prevSlowPtr.next = reverseSecondHalfPtr;
-    }
-  }
-  
-  return result;
-};
-LinkedList.prototype.reverse = function (headNode) {
-  let prevNode = null;
-  let currentNode = headNode;
-  let nextNode = null;
-
-  while (currentNode !== null) {
-    nextNode = currentNode.next;
-    currentNode.next = prevNode;
-    prevNode = currentNode;
-    currentNode = nextNode;
-  }
-
-  return prevNode;
-};
-
-LinkedList.prototype.compareLists = function (head1, head2) {
-  let temp1 = head1;
-  let temp2 = head2;
-
-  while (temp1 && temp2) {
-    if (temp1.data === temp2.data) {
-      temp1 = temp1.next;
-      temp2 = temp2.next;
-    } else {
-      return false;
-    }
-  }
-
-  if (temp1 === null && temp2 === null) {
-    return true;
-  }
-
-  return false;
-};
-const linkedList = new LinkedList();
-linkedList.insert('a');
-linkedList.insert('b');
-linkedList.insert('c');
-linkedList.insert('b');
-linkedList.insert('a');
-
-console.log(linkedList.isPalindrome()); // Output: true
-
-const linkedList2 = new LinkedList();
-linkedList2.insert(1);
-linkedList2.insert(2);
-linkedList2.insert(3);
-linkedList2.insert(4);
-linkedList2.insert(5);
-
-console.log(linkedList2.isPalindrome()); // Output: false
+const reversedHead = reverseLinkedList(node1);
