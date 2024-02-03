157b8739dae7e49c24b7b2e754f3be8705925840
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:09:10 +0800

    2-3-2024 23:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,16 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
-  }
+function findMedianSortedArrays(nums1, nums2) {
+  const merged = [...nums1, ...nums2].sort((a, b) => a - b);
+  const mid = Math.floor(merged.length / 2);
 
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
-    }
-  }
-
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
-  }
-
-  dfsRecursive(startVertex) {
-    const result = [];
-    const visited = {};
-    const adjacencyList = this.adjacencyList;
-
-    function dfs(vertex) {
-      if (!vertex) return null;
-      visited[vertex] = true;
-      result.push(vertex);
-
-      adjacencyList[vertex].forEach(neighbor => {
-        if (!visited[neighbor]) {
-          return dfs(neighbor);
-        }
-      });
-    }
-
-    dfs(startVertex);
-    return result;
+  if (merged.length % 2 === 0) {
+    return (merged[mid - 1] + merged[mid]) / 2;
+  } else {
+    return merged[mid];
   }
 }
-const graph = new Graph();
 
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
-
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'E');
-graph.addEdge('D', 'F');
-graph.addEdge('E', 'F');
-
-console.log(graph.dfsRecursive('A')); // Output: ["A", "B", "D", "E", "C", "F"]
+// Example usage:
+const nums1 = [1, 3];
+const nums2 = [2, 4];
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median); // Output: 2.5
