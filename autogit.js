188d70f2e1cc8db426fb12536d4dc3df02a5b415
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:25:51 +0800

    2-3-2024 23:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,15 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  // Add a new node to the end of the list
-  append(data) {
-    const newNode = new Node(data);
-    if (this.head === null) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next !== null) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  // Get the length of the linked list
-  getLength() {
+function countCharacter(str, character) {
     let count = 0;
-    let current = this.head;
-    while (current !== null) {
-      count++;
-      current = current.next;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === character) {
+            count++;
+        }
     }
     return count;
-  }
 }
 
-// Create a new linked list
-const list = new LinkedList();
-
-// Add nodes to the list
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-list.append(5);
-
-// Get the length of the linked list
-const length = list.getLength();
-console.log(length); // Output: 5
+// Example usage:
+const myString = "Hello, world!";
+const myCharacter = "o";
+const occurrenceCount = countCharacter(myString, myCharacter);
+console.log(occurrenceCount); // Output: 2
