cfd6e22b3e43c46810aa970128b6c7576ba7cd5a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:17:11 +0800

    2-3-2024 23:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,30 @@
-class HashTable {
-  constructor() {
-    this.hashMap = {};
-  }
-
-  // Function to calculate the hash key
-  hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
-    }
-    return hash;
-  }
-
-  // Function to add a key-value pair to the hash table
-  insert(key, value) {
-    const hashKey = this.hash(key);
-    if (!this.hashMap[hashKey]) {
-      this.hashMap[hashKey] = {};
-    }
-    this.hashMap[hashKey][key] = value;
-  }
-
-  // Function to get the value associated with a key
-  get(key) {
-    const hashKey = this.hash(key);
-    if (this.hashMap[hashKey] && this.hashMap[hashKey][key]) {
-      return this.hashMap[hashKey][key];
-    }
-    return null;
-  }
-
-  // Function to remove a key-value pair from the hash table
-  remove(key) {
-    const hashKey = this.hash(key);
-    if (this.hashMap[hashKey] && this.hashMap[hashKey][key]) {
-      delete this.hashMap[hashKey][key];
+function depthLimitedSearch(initialState, depthLimit, otherParams) {
+  function recursiveDLS(state, depth) {
+    if (state is goal) {
+      return solution or true;
+    } else if (depth === 0) {
+      return cutoff;
+    } else {
+      var cutoffOccurred = false
+      var successors = generateSuccessors(state);
+      
+      for (var i = 0; i < successors.length; i++) {
+        var result = recursiveDLS(successors[i], depth - 1);
+        
+        if (result === cutoff) {
+          cutoffOccurred = true;
+        } else if (result !== false) {
+          return result;
+        }
+      }
+      
+      if (cutoffOccurred) {
+        return cutoff;
+      } else {
+        return false;
+      }
     }
   }
+  
+  return recursiveDLS(initialState, depthLimit);
 }
-
-// Example usage:
-
-const hashtable = new HashTable();
-
-hashtable.insert("name", "John");
-hashtable.insert("age", 25);
-
-console.log(hashtable.get("name")); // Output: John
-console.log(hashtable.get("age")); // Output: 25
-
-hashtable.remove("age");
-console.log(hashtable.get("age")); // Output: null
