aedf0a62db24769442323c31be73b9d025483930
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:20:14 +0800

    2-3-2024 23:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,14 @@
-const array = [3, 7, 2, 9, 1, 5];
-
-const max = Math.max(...array);
-
-console.log(max);
-9
+function getRandomNumber(min, max) {
+  // Generate a random decimal between 0 and 1
+  var randomDecimal = Math.random();
+  
+  // Scale the random decimal to the desired range
+  var randomInRange = randomDecimal * (max - min + 1) + min;
+  
+  // Truncate the decimal to get an integer
+  var randomInteger = Math.floor(randomInRange);
+  
+  return randomInteger;
+}
+var randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber); // Outputs a random number between 1 and 10 (inclusive)
