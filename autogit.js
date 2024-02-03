66d5fac074a8e5344ecd9085025cd836df9cf6ea
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:27:02 +0800

    2-3-2024 23:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,5 @@
-function findLongestCommonPrefix(strings) {
-  let prefix = '';
+// Prompt the user for input
+var userInput = prompt("Enter a value:");
 
-  if (strings.length === 0 || strings.length === 1) {
-    return prefix;
-  }
-
-  for (let i = 0; i < strings[0].length; i++) {
-    const char = strings[0][i];
-
-    for (let j = 1; j < strings.length; j++) {
-      if (i >= strings[j].length || strings[j][i] !== char) {
-        return prefix;
-      }
-    }
-
-    prefix += char;
-  }
-
-  return prefix;
-}
-const strings = ['apple', 'app', 'append', 'apricot'];
-const longestPrefix = findLongestCommonPrefix(strings);
-console.log(longestPrefix);  // Output: 'ap'
+// Alert the entered value
+alert("You entered: " + userInput);
