d88c756ea3579f10dc7e7ec813225c72c26fc7eb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:16:10 +0800

    2-3-2024 23:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,73 +1,19 @@
-function interpolationSearch(array, target, compareFn) {
-  // Code implementation goes here
+let str = "Hello, World!";
+let reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
+let str = "Hello, World!";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-function interpolationSearch(array, target, compareFn) {
-  let start = 0;
-  let end = array.length - 1;
-  // Code implementation goes here
+console.log(reversedStr);
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
 }
-function interpolationSearch(array, target, compareFn) {
-  let start = 0;
-  let end = array.length - 1;
 
-  while (start <= end) {
-    // Code implementation goes here
-  }
-}
-function interpolationSearch(array, target, compareFn) {
-  let start = 0;
-  let end = array.length - 1;
-
-  while (start <= end) {
-    const position = start + ((target - array[start]) / (array[end] - array[start])) * (end - start);
-    // Code implementation goes here
-  }
-}
-function interpolationSearch(array, target, compareFn) {
-  let start = 0;
-  let end = array.length - 1;
-
-  while (start <= end) {
-    const position = start + ((target - array[start]) / (array[end] - array[start])) * (end - start);
-
-    if (position < start || position > end)
-      break;
-
-    if (compareFn && compareFn(array[position], target) === 0)
-      return position;
-    else if (array[position] === target)
-      return position;
-    else if (compareFn && compareFn(array[position], target) < 0)
-      start = position + 1;
-    else
-      end = position - 1;
-  }
-
-  return -1;
-}
-function interpolationSearch(array, target, compareFn) {
-  let start = 0;
-  let end = array.length - 1;
-
-  while (start <= end) {
-    const position = start + ((target - array[start]) / (array[end] - array[start])) * (end - start);
-
-    if (position < start || position > end)
-      break;
-
-    if (compareFn && compareFn(array[position], target) === 0)
-      return position;
-    else if (array[position] === target)
-      return position;
-    else if (compareFn && compareFn(array[position], target) < 0)
-      start = position + 1;
-    else
-      end = position - 1;
-  }
-
-  return -1;
-}
-const array = [1, 2, 4, 6, 8, 10];
-const target = 6;
-const index = interpolationSearch(array, target);
-console.log(index);  // Output: 3
+let str = "Hello, World!";
+let reversedStr = reverseString(str);
+console.log(reversedStr);
