cb1e7f87499a327a5a26f7e1026d45c799c5fdd2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:14:50 +0800

    2-3-2024 23:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,12 @@
-function interpolationSearch(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
-  
-  while (left <= right) {
-    let pos = Math.floor(left + ((target - arr[left]) * (right - left)) / (arr[right] - arr[left]));
-
-    if (arr[pos] === target) {
-      return pos;
-    }
-
-    if (arr[pos] < target) {
-      left = pos + 1;
-    } else {
-      right = pos - 1;
-    }
+function stringLength(str) {
+  let length = 0;
+  while (str[length] !== undefined) {
+    length++;
   }
-
-  return -1;
+  return length;
 }
-const arr = [2, 4, 6, 8, 10, 12, 14, 16];
-const target = 10;
 
-console.log(interpolationSearch(arr, target)); // Output: 4
+// Example usage
+const myString = "Hello, world!";
+const length = stringLength(myString);
+console.log(length); // Output: 13
