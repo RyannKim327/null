2d629c6258b457bca2ff4d382c56bc16739eb185
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:20:06 +0800

    2-3-2024 23:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,6 @@
-function shellSort(array) {
-  const len = array.length;
-  let gap = Math.floor(len / 2);
+const array = [3, 7, 2, 9, 1, 5];
 
-  while (gap > 0) {
-    for (let i = gap; i < len; i++) {
-      let temp = array[i];
-      let j = i;
+const max = Math.max(...array);
 
-      while (j >= gap && array[j - gap] > temp) {
-        array[j] = array[j - gap];
-        j -= gap;
-      }
-
-      array[j] = temp;
-    }
-
-    gap = Math.floor(gap / 2);
-  }
-
-  return array;
-}
-const unsortedArray = [5, 9, 1, 3, 6, 2, 8, 4, 7];
-const sortedArray = shellSort(unsortedArray);
-
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(max);
+9
