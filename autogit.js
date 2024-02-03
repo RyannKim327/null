45ab1510c0b4717447594350756548d5fca05357
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:13:32 +0800

    2-3-2024 23:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,70 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, "");
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-// Example usage:
-const inputString = "Hello World";
-const stringWithoutVowels = removeVowels(inputString);
-console.log(stringWithoutVowels); // Output: "Hll Wrld"
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value === node.value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+}
+
+// Example usage
+const tree = new BinaryTree();
+tree.insert(4); //     4
+tree.insert(2); //    / \
+tree.insert(6); //   2   6
+tree.insert(1); //  / \   \
+tree.insert(3); // 1   3   8
+
+console.log(tree.search(6)); // true
+console.log(tree.search(9)); // false
