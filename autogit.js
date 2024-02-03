0703393534714c2949f7943fea4349f80353b571
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:20:34 +0800

    2-3-2024 23:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,16 @@
-function isPrime(number) {
-  if (number <= 1) {
-    return false; // Numbers less than or equal to 1 are not prime
-  }
-  
-  for (let divisor = 2; divisor <= Math.sqrt(number); divisor++) {
-    if (number % divisor === 0) {
-      return false; // Divisible by a number other than 1 and itself
-    }
-  }
-  
-  return true; // Number is prime
-}
-const number = 7; // The number to check
+function findFirstRepeatedCharacter(str) {
+  let charMap = {};
 
-if (isPrime(number)) {
-  console.log(number + ' is a prime number.');
-} else {
-  console.log(number + ' is not a prime number.');
+  for (let char of str) {
+    if (char in charMap) {
+      return char;
+    }
+    charMap[char] = true;
+  }
+
+  return null;
 }
+
+// Example usage:
+let str = "hello";
+console.log(findFirstRepeatedCharacter(str)); // Output: l
