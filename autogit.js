4583247b649decc42612f9d8f2dbb643fb4c22aa
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:15:09 +0800

    2-3-2024 23:15 [User mode]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
