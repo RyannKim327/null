31db64f8b6b347e630561e96dda7cd2a4fc9f9bf
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:31:01 +0800

    2-3-2024 23:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,13 @@
-function bubbleSort(arr) {
-    let len = arr.length;
-    
-    for (let i = 0; i < len - 1; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    
-    return arr;
+function removeNonAlphanumeric(str) {
+  return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 }
-// Test the bubbleSort function
-let arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + bubbleSort(arr));
-Original array: 64,34,25,12,22,11,90
-Sorted array: 11,12,22,25,34,64,90
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+function isPalindrome(str) {
+  const cleanStr = removeNonAlphanumeric(str);
+  const reversedStr = reverseString(cleanStr);
+  return cleanStr === reversedStr;
+}
+const word = "A man, a plan, a canal: Panama";
+console.log(isPalindrome(word));  // Output: true
