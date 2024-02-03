dbe28c858090ae66ed67fa07df80bce79486c330
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:08:11 +0800

    2-3-2024 23:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,104 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
+class BTreeNode {
+  constructor(t, leaf = true) {
+    this.t = t;
+    this.keys = [];
+    this.children = [];
+    this.leaf = leaf;
   }
-}
 
-function isLinkedListPalindrome(head) {
-  if (!head || !head.next) {
-    return true;
-  }
-  
-  let slow = head;
-  let fast = head;
-  
-  // Finding the middle element of the linked list
-  while (fast.next && fast.next.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-  }
-  
-  // Reversing the second half of the linked list
-  let secondHalf = slow.next;
-  let prev = null;
-  
-  while (secondHalf) {
-    const next = secondHalf.next;
-    secondHalf.next = prev;
-    prev = secondHalf;
-    secondHalf = next;
-  }
-  
-  slow.next = prev;
-  
-  // Comparing values from the first and second halves of the linked list
-  let firstHalf = head;
-  secondHalf = slow.next;
-  
-  while (secondHalf) {
-    if (firstHalf.data !== secondHalf.data) {
-      return false;
+  // Insert a key into the current node
+  insertNonFull(key) {
+    let i = this.keys.length - 1;
+
+    if (this.leaf) {
+      while (i >= 0 && key < this.keys[i]) {
+        i--;
+      }
+      this.keys.splice(i + 1, 0, key);
+    } else {
+      while (i >= 0 && key < this.keys[i]) {
+        i--;
+      }
+      i++;
+
+      if (this.children[i].keys.length === 2 * this.t - 1) {
+        this.splitChild(i);
+
+        if (key > this.keys[i]) {
+          i++;
+        }
+      }
+      this.children[i].insertNonFull(key);
     }
-    
-    firstHalf = firstHalf.next;
-    secondHalf = secondHalf.next;
   }
-  
-  return true;
+
+  // Split the child node into two
+  splitChild(i) {
+    const child = this.children[i];
+    const newNode = new BTreeNode(child.t, child.leaf);
+
+    this.keys.splice(i, 0, child.keys[child.keys.length - 1]);
+    this.children.splice(i + 1, 0, newNode);
+
+    newNode.keys = child.keys.splice(child.keys.length / 2);
+    if (!child.leaf) {
+      newNode.children = child.children.splice(child.children.length / 2);
+    }
+  }
+
+  // Search for a key in the tree
+  search(key) {
+    let i = 0;
+    while (i < this.keys.length && key > this.keys[i]) {
+      i++;
+    }
+
+    if (this.keys[i] === key) {
+      return this;
+    } else if (this.leaf) {
+      return null;
+    } else {
+      return this.children[i].search(key);
+    }
+  }
 }
 
-// Creating a sample linked list
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(2);
-head.next.next.next.next = new Node(1);
+class BTree {
+  constructor(t) {
+    this.root = new BTreeNode(t, true);
+    this.t = t;
+  }
 
-console.log(isLinkedListPalindrome(head)); // Output: true
+  // Insert a key into the tree
+  insert(key) {
+    const root = this.root;
+    if (root.keys.length === 2 * this.t - 1) {
+      const newRoot = new BTreeNode(this.t, false);
+      newRoot.children.push(root);
+      this.root = newRoot;
+      newRoot.splitChild(0);
+      newRoot.insertNonFull(key);
+    } else {
+      root.insertNonFull(key);
+    }
+  }
 
-head.next.next.next.data = 4;
+  // Search for a key in the tree
+  search(key) {
+    return this.root.search(key);
+  }
+}
+// Create a B-tree with a degree of 3
+const tree = new BTree(3);
 
-console.log(isLinkedListPalindrome(head)); // Output: false
+// Insert some keys
+tree.insert(10);
+tree.insert(20);
+tree.insert(5);
+tree.insert(6);
+tree.insert(12);
+tree.insert(30);
+
+// Search for a key
+const result = tree.search(6);
+console.log(result ? "Key found" : "Key not found"); // Output: "Key found"
