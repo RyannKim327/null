be23e460039ce7d311a617d68c85cfe1604ff9c2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:11:57 +0800

    2-3-2024 23:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,135 +1,43 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+function mergeSortIterative(arr) {
+  const len = arr.length;
+  const sortedArr = arr.slice(); // Copy the original array to avoid modifying it
+  
+  for (let width = 1; width < len; width *= 2) {
+    for (let i = 0; i < len; i += 2 * width) {
+      const left = i;
+      const mid = Math.min(i + width, len);
+      const right = Math.min(i + 2 * width, len);
+      merge(sortedArr, left, mid, right);
+    }
   }
+  
+  return sortedArr;
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
+function merge(arr, left, mid, right) {
+  const leftArr = arr.slice(left, mid);
+  const rightArr = arr.slice(mid, right);
 
-  insert(value) {
-    const newNode = new Node(value);
+  let i = 0;
+  let j = 0;
+  let k = left;
 
-    if (this.root === null) {
-      this.root = newNode;
+  while (i < leftArr.length && j < rightArr.length) {
+    if (leftArr[i] <= rightArr[j]) {
+      arr[k++] = leftArr[i++];
     } else {
-      this.insertNode(this.root, newNode);
+      arr[k++] = rightArr[j++];
     }
   }
 
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
+  while (i < leftArr.length) {
+    arr[k++] = leftArr[i++];
   }
 
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  searchNode(node, value) {
-    if (node === null || node.value === value) {
-      return node;
-    }
-
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    }
-
-    return this.searchNode(node.right, value);
-  }
-
-  remove(value) {
-    this.root = this.removeNode(this.root, value);
-  }
-
-  removeNode(node, value) {
-    if (node === null) {
-      return null;
-    }
-
-    if (value < node.value) {
-      node.left = this.removeNode(node.left, value);
-      return node;
-    } else if (value > node.value) {
-      node.right = this.removeNode(node.right, value);
-      return node;
-    } else {
-      if (node.left === null && node.right === null) {
-        node = null;
-        return null;
-      }
-
-      if (node.left === null) {
-        node = node.right;
-        return node;
-      }
-
-      if (node.right === null) {
-        node = node.left;
-        return node;
-      }
-
-      const minNode = this.findMinNode(node.right);
-      node.value = minNode.value;
-      node.right = this.removeNode(node.right, minNode.value);
-      return node;
-    }
-  }
-
-  findMinNode(node) {
-    if (node.left === null) {
-      return node;
-    }
-
-    return this.findMinNode(node.left);
-  }
-
-  getRootNode() {
-    return this.root;
-  }
-
-  inorderTraversal(callback) {
-    this.inorderTraversalNode(this.root, callback);
-  }
-
-  inorderTraversalNode(node, callback) {
-    if (node !== null) {
-      this.inorderTraversalNode(node.left, callback);
-      callback(node.value);
-      this.inorderTraversalNode(node.right, callback);
-    }
+  while (j < rightArr.length) {
+    arr[k++] = rightArr[j++];
   }
 }
-const bst = new BinarySearchTree();
-
-bst.insert(8);
-bst.insert(3);
-bst.insert(10);
-bst.insert(1);
-bst.insert(6);
-
-console.log("Inorder Traversal:");
-bst.inorderTraversal(value => console.log(value));
-
-console.log("Search 6:", bst.search(6));
-console.log("Search 9:", bst.search(9));
-
-bst.remove(3);
-
-console.log("Inorder Traversal after removing 3:");
-bst.inorderTraversal(value => console.log(value));
+const array = [9, 2, 7, 12, 3, 6, 8, 1];
+const sortedArray = mergeSortIterative(array);
+console.log(sortedArray);
