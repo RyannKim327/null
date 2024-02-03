e849b9a2913b788c44ede954b4579c885e53c7c9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:22:24 +0800

    2-3-2024 23:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,48 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
-  const alphanumericStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-  // Reverse the string
-  const reversedStr = alphanumericStr.split('').reverse().join('');
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 
-  // Compare the original and reversed strings
-  return alphanumericStr === reversedStr;
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  dfs(startingVertex) {
+    const stack = [startingVertex];
+    const visited = new Set();
+
+    visited.add(startingVertex);
+
+    while (stack.length > 0) {
+      const currentVertex = stack.pop();
+      console.log(currentVertex); // Print the current vertex
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited.has(neighbor)) {
+          stack.push(neighbor);
+          visited.add(neighbor);
+        }
+      });
+    }
+  }
 }
 
-// Example usage
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
-console.log(isPalindrome("race car")); // true
-console.log(isPalindrome("hello world")); // false
+// Example usage:
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.dfs("A");
