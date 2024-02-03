26cac78d4446caa3cda7863ed88f1c5a5407025e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:17:01 +0800

    2-3-2024 23:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,53 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
+class HashTable {
+  constructor() {
+    this.hashMap = {};
+  }
+
+  // Function to calculate the hash key
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
+
+  // Function to add a key-value pair to the hash table
+  insert(key, value) {
+    const hashKey = this.hash(key);
+    if (!this.hashMap[hashKey]) {
+      this.hashMap[hashKey] = {};
+    }
+    this.hashMap[hashKey][key] = value;
+  }
+
+  // Function to get the value associated with a key
+  get(key) {
+    const hashKey = this.hash(key);
+    if (this.hashMap[hashKey] && this.hashMap[hashKey][key]) {
+      return this.hashMap[hashKey][key];
+    }
+    return null;
+  }
+
+  // Function to remove a key-value pair from the hash table
+  remove(key) {
+    const hashKey = this.hash(key);
+    if (this.hashMap[hashKey] && this.hashMap[hashKey][key]) {
+      delete this.hashMap[hashKey][key];
     }
   }
-  return true;
 }
-const array1 = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(array1));  // Output: true
 
-const array2 = [5, 4, 3, 2, 1];
-console.log(isSortedAscending(array2));  // Output: false
+// Example usage:
+
+const hashtable = new HashTable();
+
+hashtable.insert("name", "John");
+hashtable.insert("age", 25);
+
+console.log(hashtable.get("name")); // Output: John
+console.log(hashtable.get("age")); // Output: 25
+
+hashtable.remove("age");
+console.log(hashtable.get("age")); // Output: null
