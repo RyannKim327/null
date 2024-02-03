ede8f9662b2ab9c8eef3f21c11928e4fa41e8dd9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:34:05 +0800

    2-3-2024 23:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,21 @@
-function boyerMooreSearch(text, pattern) {
-    // Preprocessing
-    var lastTable = buildLastTable(pattern);
-    var n = text.length;
-    var m = pattern.length;
-    var i = m - 1; // Text pointer
-    var j = m - 1; // Pattern pointer
+function getNthNodeFromEnd(list, n) {
+  let p1 = list;
+  let p2 = list;
 
-    while (i < n) {
-        if (text[i] === pattern[j]) {
-            if (j === 0) {
-                // Pattern matches
-                return i;
-            }
-            // Matched, continue comparing
-            i--;
-            j--;
-        } else {
-            // Mismatch, shift the pattern
-            i = i + m - Math.min(j, 1 + lastTable[text[i].charCodeAt(0)]);
-            j = m - 1;
-        }
+  // Move p2 n positions ahead
+  for (let i = 0; i < n; i++) {
+    if (p2 === null) {
+      return null; // n is greater than list length
     }
+    p2 = p2.next;
+  }
 
-    // Pattern not found
-    return -1;
+  // Move both pointers until p2 reaches the end
+  while (p2 !== null && p2.next !== null) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  // p1 points to the nth node from the end
+  return p1.value; // or return p1 for the node itself
 }
-
-function buildLastTable(pattern) {
-    var lastTable = {};
-    var m = pattern.length;
-
-    // Initialize all characters as -1
-    for (var i = 0; i < 256; i++) {
-        lastTable[i] = -1;
-    }
-
-    // Update last occurrence of characters in the pattern
-    for (var j = 0; j < m; j++) {
-        lastTable[pattern[j].charCodeAt(0)] = j;
-    }
-
-    return lastTable;
-}
-
-// Usage example
-var text = "Hello, world!";
-var pattern = "world";
-var index = boyerMooreSearch(text, pattern);
-console.log(index); // Output: 7 (index of the pattern in the text)
