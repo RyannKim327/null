8471c62b2973bf656f0de263295ab8ebfa3b8fe1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:23:18 +0800

    2-3-2024 23:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,8 @@
-function getLinkedListLength(head) {
-  let length = 0;
-  let currentNode = head;
-
-  while (currentNode !== null) {
-    length++;
-    currentNode = currentNode.next;
-  }
-
-  return length;
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-const myList = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: null
-    }
-  }
-};
 
-const length = getLinkedListLength(myList);
-console.log(length); // Output: 3
+// Example usage:
+console.log(decimalToBinary(10)); // Output: "1010"
+console.log(decimalToBinary(27)); // Output: "11011"
+console.log(decimalToBinary(44)); // Output: "101100"
