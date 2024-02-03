eb42f1270741a7a2500990787942de22d9ce264d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:12:12 +0800

    2-3-2024 23:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,11 @@
-function reverseLinkedList(head) {
-    let previousNode = null;
-    let currentNode = head;
-
-    while (currentNode !== null) {
-        let nextNode = currentNode.next;
-        currentNode.next = previousNode;
-        previousNode = currentNode;
-        currentNode = nextNode;
-    }
-
-    return previousNode;
-}
-let reversedHead = reverseLinkedList(head);
+// Make an HTTP GET request using fetch
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    // Process the received data
+    console.log(data);
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error('Error:', error);
+  });
