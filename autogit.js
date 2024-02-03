98cc5d4ae8b6271dd70a2770ea545dcb14d25646
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:26:09 +0800

    2-3-2024 23:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,22 @@
-function buildPrefixTable(pattern) {
-  const prefixTable = [0]; // Initialize with the first element as 0
-  let prefixLength = 0;
-  
-  for (let i = 1; i < pattern.length; i++) {
-    while (prefixLength > 0 && pattern[i] !== pattern[prefixLength]) {
-      prefixLength = prefixTable[prefixLength - 1];
+function isLinkedListPalindrome(head) {
+  let values = [];
+  let currentNode = head;
+
+  while (currentNode) {
+    values.push(currentNode.val);
+    currentNode = currentNode.next;
+  }
+
+  let left = 0;
+  let right = values.length - 1;
+
+  while (left < right) {
+    if (values[left] !== values[right]) {
+      return false;
     }
-    
-    if (pattern[i] === pattern[prefixLength]) {
-      prefixLength++;
-    }
-    
-    prefixTable[i] = prefixLength;
+    left++;
+    right--;
   }
   
-  return prefixTable;
+  return true;
 }
-function search(text, pattern) {
-  const prefixTable = buildPrefixTable(pattern);
-  const matches = [];
-  let textIndex = 0;
-  let patternIndex = 0;
-  
-  while (textIndex < text.length) {
-    if (text[textIndex] === pattern[patternIndex]) {
-      textIndex++;
-      patternIndex++;
-      
-      if (patternIndex === pattern.length) {
-        matches.push(textIndex - patternIndex);
-        patternIndex = prefixTable[patternIndex - 1];
-      }
-    } else {
-      if (patternIndex !== 0) {
-        patternIndex = prefixTable[patternIndex - 1];
-      } else {
-        textIndex++;
-      }
-    }
-  }
-  
-  return matches;
-}
-const text = 'ABABDABACDABABCABAB';
-const pattern = 'ABABCABAB';
-const matchedPositions = search(text, pattern);
-console.log(matchedPositions);
