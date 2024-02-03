7f0a74a13fcf813ad03e0302af4c1a182d999d45
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:20:26 +0800

    2-3-2024 23:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,20 @@
-const array = [1, 2, 3, 4, 4, 5, 5];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray);
-const array = [1, 2, 3, 4, 4, 5, 5];
-const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArray);
-const array = [1, 2, 3, 4, 4, 5, 5];
-const uniqueArray = array.reduce((accumulator, currentValue) => {
-  if (!accumulator.includes(currentValue)) {
-    accumulator.push(currentValue);
+function isPrime(number) {
+  if (number <= 1) {
+    return false; // Numbers less than or equal to 1 are not prime
   }
-  return accumulator;
-}, []);
-console.log(uniqueArray);
+  
+  for (let divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+    if (number % divisor === 0) {
+      return false; // Divisible by a number other than 1 and itself
+    }
+  }
+  
+  return true; // Number is prime
+}
+const number = 7; // The number to check
+
+if (isPrime(number)) {
+  console.log(number + ' is a prime number.');
+} else {
+  console.log(number + ' is not a prime number.');
+}
