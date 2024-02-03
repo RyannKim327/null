c56f0bbd7b131826d188af9fdfdcb4e8b390ac94
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:12:06 +0800

    2-3-2024 23:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,14 @@
-function mergeSortIterative(arr) {
-  const len = arr.length;
-  const sortedArr = arr.slice(); // Copy the original array to avoid modifying it
-  
-  for (let width = 1; width < len; width *= 2) {
-    for (let i = 0; i < len; i += 2 * width) {
-      const left = i;
-      const mid = Math.min(i + width, len);
-      const right = Math.min(i + 2 * width, len);
-      merge(sortedArr, left, mid, right);
+function reverseLinkedList(head) {
+    let previousNode = null;
+    let currentNode = head;
+
+    while (currentNode !== null) {
+        let nextNode = currentNode.next;
+        currentNode.next = previousNode;
+        previousNode = currentNode;
+        currentNode = nextNode;
     }
-  }
-  
-  return sortedArr;
+
+    return previousNode;
 }
-
-function merge(arr, left, mid, right) {
-  const leftArr = arr.slice(left, mid);
-  const rightArr = arr.slice(mid, right);
-
-  let i = 0;
-  let j = 0;
-  let k = left;
-
-  while (i < leftArr.length && j < rightArr.length) {
-    if (leftArr[i] <= rightArr[j]) {
-      arr[k++] = leftArr[i++];
-    } else {
-      arr[k++] = rightArr[j++];
-    }
-  }
-
-  while (i < leftArr.length) {
-    arr[k++] = leftArr[i++];
-  }
-
-  while (j < rightArr.length) {
-    arr[k++] = rightArr[j++];
-  }
-}
-const array = [9, 2, 7, 12, 3, 6, 8, 1];
-const sortedArray = mergeSortIterative(array);
-console.log(sortedArray);
+let reversedHead = reverseLinkedList(head);
