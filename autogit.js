cf00078019ddb8daf9d28c542698a35347c2f850
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:25:45 +0800

    2-3-2024 23:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,51 @@
-const str = "Hello, World!";
-const reversedStr = str.split("").reverse().join("");
-console.log(reversedStr); // Output: "!dlroW ,olleH"
-const str = "Hello, World!";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
-}
-console.log(reversedStr); // Output: "!dlroW ,olleH"
-function reverseString(str) {
-  if (str === "") {
-    return "";
-  } else {
-    return reverseString(str.substr(1)) + str.charAt(0);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
 }
 
-const str = "Hello, World!";
-const reversedStr = reverseString(str);
-console.log(reversedStr); // Output: "!dlroW ,olleH"
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Add a new node to the end of the list
+  append(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Get the length of the linked list
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+}
+
+// Create a new linked list
+const list = new LinkedList();
+
+// Add nodes to the list
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+
+// Get the length of the linked list
+const length = list.getLength();
+console.log(length); // Output: 5
