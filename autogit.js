b66bdb305087c267f8db176e3ee5cb8a3a8f1357
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:33:06 +0800

    2-3-2024 23:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,16 @@
-function findMajorityElement(nums) {
-  const frequency = {};
-  const majorityThreshold = Math.floor(nums.length / 2);
-
-  for (let i = 0; i < nums.length; i++) {
-    if (frequency[nums[i]]) {
-      frequency[nums[i]]++;
-    } else {
-      frequency[nums[i]] = 1;
-    }
-
-    if (frequency[nums[i]] > majorityThreshold) {
-      return nums[i];
-    }
-  }
-
-  return null; // No majority element found
+let arr = [1, 2, 3, 4, 5];
+let index = arr.indexOf(3); // Get the index of the element to remove
+if (index !== -1) {
+  arr.splice(index, 1); // Remove 1 element at the found index
 }
-
-// Example usage:
-const arr = [2, 4, 5, 2, 3, 2, 2];
-const majorityElement = findMajorityElement(arr);
-console.log(majorityElement); // Output: 2
+console.log(arr); // Output: [1, 2, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+arr = arr.filter(element => element !== 3); // Remove the element with a value of 3
+console.log(arr); // Output: [1, 2, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+arr.forEach((element, index) => {
+  if (element === 3) {
+    arr.splice(index, 1); // Remove 1 element at the current index
+  }
+});
+console.log(arr); // Output: [1, 2, 4, 5]
