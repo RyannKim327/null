37d534b4e48b69d5cc20a04b273c5b6211a0cdb8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-03 23:25:30 +0800

    2-3-2024 23:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,3 @@
-const numbers = [3, 1, 5, 2, 4];
-numbers.sort((a, b) => a - b);
-console.log(numbers); // Output: [1, 2, 3, 4, 5]
-const numbers = [3, 1, 5, 2, 4];
-const sortedNumbers = [].concat(numbers).sort((a, b) => a - b);
-console.log(sortedNumbers); // Output: [1, 2, 3, 4, 5]
-const numbers = [3, 1, 5, 2, 4];
-const sortedNumbers = [...numbers].sort((a, b) => a - b);
-console.log(sortedNumbers); // Output: [1, 2, 3, 4, 5]
-numbers.sort((a, b) => b - a);
+const array = [3, 6, 2, 8, 1, 9, 5];
+const max = Math.max(...array);
+console.log(max); // Output: 9
