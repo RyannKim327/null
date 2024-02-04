ce3e0fdbc10d8f80d64e5ae9392806501e094796
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:36:07 +0800

    2-4-2024 22:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,40 @@
-class Graph {
-  constructor() {
-    this.nodes = new Set();
-    this.edges = {};
-  }
+function breadthFirstSearch(graph, startNode) {
+  // Step 2: Create a queue and add the start node to it
+  const queue = [startNode];
 
-  addNode(node) {
-    this.nodes.add(node);
-    this.edges[node] = {};
-  }
-
-  addEdge(src, dest, weight) {
-    this.edges[src][dest] = weight;
-    this.edges[dest][src] = weight;
-  }
-
-  getNeighbors(node) {
-    return Object.keys(this.edges[node]);
-  }
-}
-function dijkstra(graph, start) {
-  const distances = {};
+  // Step 3: Create a visited set and mark the start node as visited
   const visited = new Set();
+  visited.add(startNode);
 
-  // Initialize all distances to infinity except the start node
-  for (const node of graph.nodes) {
-    distances[node] = Infinity;
-  }
-  distances[start] = 0;
+  // Step 4: Perform breadth-first search
+  while (queue.length > 0) {
+    // Dequeue a node from the queue
+    const currentNode = queue.shift();
 
-  while (visited.size < graph.nodes.size) {
-    const nextNode = getNextNode(distances, visited);
+    // Process the node (e.g., print or do something with it)
+    console.log(currentNode);
 
-    // Mark the current node as visited
-    visited.add(nextNode);
+    // Get all adjacent nodes of the current node
+    const neighbors = graph[currentNode];
 
-    // Update distances to neighbors
-    const neighbors = graph.getNeighbors(nextNode);
-    for (const neighbor of neighbors) {
-      const weight = graph.edges[nextNode][neighbor];
-      const distance = distances[nextNode] + weight;
-      if (distance < distances[neighbor]) {
-        distances[neighbor] = distance;
+    // Visit each adjacent node if not already visited
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighborNode = neighbors[i];
+      if (!visited.has(neighborNode)) {
+        visited.add(neighborNode);
+        queue.push(neighborNode);
       }
     }
   }
-
-  return distances;
 }
-function getNextNode(distances, visited) {
-  let minDistance = Infinity;
-  let nextNode;
+// Step 1: Define the graph
+const graph = {
+  A: ['B', 'C'],
+  B: ['A', 'D'],
+  C: ['A', 'D'],
+  D: ['B', 'C', 'E'],
+  E: ['D']
+};
 
-  for (const node in distances) {
-    if (!visited.has(node) && distances[node] < minDistance) {
-      minDistance = distances[node];
-      nextNode = node;
-    }
-  }
-
-  return nextNode;
-}
-// Create a graph
-const graph = new Graph();
-graph.addNode("A");
-graph.addNode("B");
-graph.addNode("C");
-graph.addNode("D");
-graph.addNode("E");
-
-graph.addEdge("A", "B", 4);
-graph.addEdge("A", "C", 2);
-graph.addEdge("B", "E", 3);
-graph.addEdge("C", "D", 2);
-graph.addEdge("D", "E", 3);
-
-// Find shortest paths starting from "A"
-const startNode = "A";
-const shortestPaths = dijkstra(graph, startNode);
-console.log(shortestPaths);
+// Step 5: Call the breadthFirstSearch function
+breadthFirstSearch(graph, 'A');
