2c17e11d50ecde198a485810673cc3ef542618b1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:48:51 +0800

    2-4-2024 22:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,23 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    // Do something with the returned data
-  })
-  .catch(error => {
-    console.log('Error:', error);
-  });
+function largestPrimeFactor(number) {
+  let largestPrime = 2;
+
+  while (number % 2 === 0) {
+    number /= 2;
+  }
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    while (number % i === 0) {
+      largestPrime = i;
+      number /= i;
+    }
+  }
+
+  if (number > 2) {
+    largestPrime = number;
+  }
+
+  return largestPrime;
+}
+
+// Example usage:
+console.log(largestPrimeFactor(13195)); // Output: 29
