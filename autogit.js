c0e377ad526fa1332655aff27ec82bacde99a352
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:35:57 +0800

    2-4-2024 22:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,80 @@
-function burrowsWheelerTransform(text) {
-  // Add end of text marker
-  text += '$';
-
-  // Generate all rotations of the text
-  const rotations = generateRotations(text);
-
-  // Sort the rotations lexicographically
-  rotations.sort();
-
-  // Extract the last characters of each rotation
-  let result = '';
-  for (const rotation of rotations) {
-    result += rotation.charAt(text.length - 1);
+class Graph {
+  constructor() {
+    this.nodes = new Set();
+    this.edges = {};
   }
-  return result;
-}
-function generateRotations(text) {
-  const rotations = [];
-  for (let i = 0; i < text.length; i++) {
-    rotations.push(text.slice(i) + text.slice(0, i));
+
+  addNode(node) {
+    this.nodes.add(node);
+    this.edges[node] = {};
   }
-  return rotations;
-}
-const inputText = 'banana';
-const bwtResult = burrowsWheelerTransform(inputText);
-console.log(bwtResult);
-function burrowsWheelerTransform(text) {
-  // Add end of text marker
-  text += '$';
 
-  // Generate all rotations of the text
-  const rotations = generateRotations(text);
-
-  // Sort the rotations lexicographically
-  rotations.sort();
-
-  // Extract the last characters of each rotation
-  let result = '';
-  for (const rotation of rotations) {
-    result += rotation.charAt(text.length - 1);
+  addEdge(src, dest, weight) {
+    this.edges[src][dest] = weight;
+    this.edges[dest][src] = weight;
   }
-  return result;
-}
 
-function generateRotations(text) {
-  const rotations = [];
-  for (let i = 0; i < text.length; i++) {
-    rotations.push(text.slice(i) + text.slice(0, i));
+  getNeighbors(node) {
+    return Object.keys(this.edges[node]);
   }
-  return rotations;
 }
+function dijkstra(graph, start) {
+  const distances = {};
+  const visited = new Set();
 
-// Test the BWT function
-const inputText = 'banana';
-const bwtResult = burrowsWheelerTransform(inputText);
-console.log(bwtResult);
+  // Initialize all distances to infinity except the start node
+  for (const node of graph.nodes) {
+    distances[node] = Infinity;
+  }
+  distances[start] = 0;
+
+  while (visited.size < graph.nodes.size) {
+    const nextNode = getNextNode(distances, visited);
+
+    // Mark the current node as visited
+    visited.add(nextNode);
+
+    // Update distances to neighbors
+    const neighbors = graph.getNeighbors(nextNode);
+    for (const neighbor of neighbors) {
+      const weight = graph.edges[nextNode][neighbor];
+      const distance = distances[nextNode] + weight;
+      if (distance < distances[neighbor]) {
+        distances[neighbor] = distance;
+      }
+    }
+  }
+
+  return distances;
+}
+function getNextNode(distances, visited) {
+  let minDistance = Infinity;
+  let nextNode;
+
+  for (const node in distances) {
+    if (!visited.has(node) && distances[node] < minDistance) {
+      minDistance = distances[node];
+      nextNode = node;
+    }
+  }
+
+  return nextNode;
+}
+// Create a graph
+const graph = new Graph();
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+graph.addNode("E");
+
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("D", "E", 3);
+
+// Find shortest paths starting from "A"
+const startNode = "A";
+const shortestPaths = dijkstra(graph, startNode);
+console.log(shortestPaths);
