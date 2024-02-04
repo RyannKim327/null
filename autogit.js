1f73d54abef7a41030dd653e051eff350eb5daf7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:41:22 +0800

    2-4-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,90 +1,37 @@
-class Graph {
-  constructor() {
-    this.adjList = {};
-  }
-}
-class Graph {
-  constructor() {
-    this.adjList = {};
-  }
+function topologicalSort(graph) {
+  const sortedOrder = [];
+  const visited = new Set();
 
-  addVertex(vertex) {
-    if (!this.adjList[vertex]) {
-      this.adjList[vertex] = [];
-    }
-  }
-}
-class Graph {
-  constructor() {
-    this.adjList = {};
-  }
+  function dfs(node) {
+    visited.add(node);
 
-  addVertex(vertex) {
-    if (!this.adjList[vertex]) {
-      this.adjList[vertex] = [];
-    }
-  }
-
-  addEdge(vertex1, vertex2) {
-    if (!this.adjList[vertex1] || !this.adjList[vertex2]) {
-      throw new Error('Vertex does not exist in the graph.');
-    }
-    this.adjList[vertex1].push(vertex2);
-  }
-}
-class Graph {
-  constructor() {
-    this.adjList = {};
-  }
-
-  addVertex(vertex) {
-    if (!this.adjList[vertex]) {
-      this.adjList[vertex] = [];
-    }
-  }
-
-  addEdge(vertex1, vertex2) {
-    if (!this.adjList[vertex1] || !this.adjList[vertex2]) {
-      throw new Error('Vertex does not exist in the graph.');
-    }
-    this.adjList[vertex1].push(vertex2);
-  }
-
-  topologicalSort() {
-    let visited = {};
-    let stack = [];
-
-    for (let vertex in this.adjList) {
-      if (!visited[vertex]) {
-        this.topologicalSortUtil(vertex, visited, stack);
+    const neighbors = graph[node];
+    if (neighbors) {
+      for (let neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          dfs(neighbor);
+        }
       }
     }
-
-    return stack.reverse();
+    
+    sortedOrder.unshift(node);
   }
 
-  topologicalSortUtil(vertex, visited, stack) {
-    visited[vertex] = true;
-
-    for (let adjacentVertex of this.adjList[vertex]) {
-      if (!visited[adjacentVertex]) {
-        this.topologicalSortUtil(adjacentVertex, visited, stack);
-      }
+  for (let node in graph) {
+    if (!visited.has(node)) {
+      dfs(node);
     }
-
-    stack.push(vertex);
   }
+
+  return sortedOrder;
 }
-let graph = new Graph();
+const graph = {
+  1: [2, 3],
+  2: [4],
+  3: [4, 5],
+  4: [],
+  5: []
+};
 
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addEdge('A', 'D');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'A');
-graph.addEdge('C', 'B');
-
-let sorted = graph.topologicalSort();
-console.log(sorted); // Output: ['C', 'B', 'A', 'D']
+const sortedOrder = topologicalSort(graph);
+console.log(sortedOrder);
