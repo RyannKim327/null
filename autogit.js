b219b5852f0fb1026312cbfb35abe68ad6ede47c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 23:07:13 +0800

    2-4-2024 23:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,118 +1,18 @@
-// Node class for AVL tree
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-    this.height = 1;
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
   }
+
+  // Check for divisibility from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-// AVL Tree class
-class AVLTree {
-  constructor() {
-    this.root = null;
-  }
-
-  // Add a value to the tree
-  add(value) {
-    this.root = this.insertNode(this.root, value);
-  }
-
-  // Perform a left rotation
-  rotateLeft(node) {
-    const newRoot = node.right;
-    node.right = newRoot.left;
-    newRoot.left = node;
-    node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-    newRoot.height = Math.max(this.getHeight(newRoot.right), node.height) + 1;
-    return newRoot;
-  }
-
-  // Perform a right rotation
-  rotateRight(node) {
-    const newRoot = node.left;
-    node.left = newRoot.right;
-    newRoot.right = node;
-    node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-    newRoot.height = Math.max(this.getHeight(newRoot.left), node.height) + 1;
-    return newRoot;
-  }
-
-  // Get the height of a node
-  getHeight(node) {
-    if (!node) {
-      return 0;
-    }
-    return node.height;
-  }
-
-  // Get the balance factor of a node
-  getBalanceFactor(node) {
-    if (!node) {
-      return 0;
-    }
-    return this.getHeight(node.left) - this.getHeight(node.right);
-  }
-
-  // Insert a value into the tree
-  insertNode(node, value) {
-    if (!node) {
-      return new Node(value);
-    }
-
-    if (value < node.value) {
-      node.left = this.insertNode(node.left, value);
-    } else if (value > node.value) {
-      node.right = this.insertNode(node.right, value);
-    } else {
-      // Duplicate values are not allowed in AVL trees
-      return node;
-    }
-
-    node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-
-    const balanceFactor = this.getBalanceFactor(node);
-
-    // Left Left Case
-    if (balanceFactor > 1 && value < node.left.value) {
-      return this.rotateRight(node);
-    }
-
-    // Right Right Case
-    if (balanceFactor < -1 && value > node.right.value) {
-      return this.rotateLeft(node);
-    }
-
-    // Left Right Case
-    if (balanceFactor > 1 && value > node.left.value) {
-      node.left = this.rotateLeft(node.left);
-      return this.rotateRight(node);
-    }
-
-    // Right Left Case
-    if (balanceFactor < -1 && value < node.right.value) {
-      node.right = this.rotateRight(node.right);
-      return this.rotateLeft(node);
-    }
-
-    return node;
-  }
-
-  // Utility function to traverse the tree in inorder
-  inorder(node) {
-    if (node) {
-      this.inorder(node.left);
-      console.log(node.value);
-      this.inorder(node.right);
-    }
-  }
-}
-const avlTree = new AVLTree();
-avlTree.add(10);
-avlTree.add(20);
-avlTree.add(30);
-avlTree.add(40);
-avlTree.add(50);
-
-avlTree.inorder(avlTree.root);
+// Example usage
+console.log(isPrime(7));  // true
+console.log(isPrime(10)); // false
