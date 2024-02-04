d8073f4c28ecb2131ee42b458f0b4240c8b5deaa
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:58:13 +0800

    2-4-2024 22:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,71 @@
-function findFirstRepeatedChar(str) {
-  const charMap = new Map();
-
-  for (let char of str) {
-    if (charMap.has(char)) {
-      return char;
-    }
-    charMap.set(char, true);
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.adjList = new Map();
   }
 
-  return null; // Return null if no repeated character is found
+  addEdge(u, v) {
+    if (!this.adjList.has(u)) {
+      this.adjList.set(u, []);
+    }
+    this.adjList.get(u).push(v);
+  }
 }
+function tarjanSCC(graph) {
+  let index = 0;
+  let stack = [];
+  let ids = new Map();
+  let lowLink = new Map();
+  let onStack = new Set();
+  let result = [];
 
-const inputString = "hello world";
-const firstRepeatedChar = findFirstRepeatedChar(inputString);
-console.log(firstRepeatedChar); // Output: "l"
+  for (let v of graph.vertices) {
+    if (!ids.has(v)) {
+      dfs(v);
+    }
+  }
+
+  function dfs(vertex) {
+    ids.set(vertex, index);
+    lowLink.set(vertex, index);
+    index++;
+    stack.push(vertex);
+    onStack.add(vertex);
+
+    if (graph.adjList.has(vertex)) {
+      for (let neighbor of graph.adjList.get(vertex)) {
+        if (!ids.has(neighbor)) {
+          dfs(neighbor);
+          lowLink.set(vertex, Math.min(lowLink.get(vertex), lowLink.get(neighbor)));
+        } else if (onStack.has(neighbor)) {
+          lowLink.set(vertex, Math.min(lowLink.get(vertex), ids.get(neighbor)));
+        }
+      }
+    }
+
+    if (ids.get(vertex) === lowLink.get(vertex)) {
+      let component = [];
+      let w;
+      do {
+        w = stack.pop();
+        onStack.delete(w);
+        component.push(w);
+      } while (w !== vertex);
+
+      result.push(component);
+    }
+  }
+
+  return result;
+}
+let graph = new Graph(['A', 'B', 'C', 'D', 'E', 'F']);
+
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'A');
+graph.addEdge('D', 'C');
+graph.addEdge('D', 'E');
+graph.addEdge('E', 'F');
+graph.addEdge('F', 'D');
+let components = tarjanSCC(graph);
+console.log(components);
