67aeacad5e546b680c03d1b5268ea0936a467d08
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 23:04:25 +0800

    2-4-2024 23:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,87 +1,71 @@
-class BTreeNode {
-  constructor(leaf = true) {
-    this.keys = [];
-    this.childs = [];
-    this.leaf = leaf;
-  }
-}
-class BTree {
-  constructor(order) {
-    this.root = new BTreeNode(true);
-    this.order = order;
-  }
+function bidirectionalSearch(graph, start, goal) {
+  // Create forward and backward search queues
+  const forwardQueue = [start];
+  const backwardQueue = [goal];
 
-  search(key, node = this.root) {
-    let i = 0;
-    while (i < node.keys.length && key > node.keys[i]) {
-      i++;
+  // Create forward and backward visited sets
+  const forwardVisited = new Set();
+  const backwardVisited = new Set();
+
+  // Track parents for path reconstruction
+  const forwardParents = new Map();
+  const backwardParents = new Map();
+
+  while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+    // Perform forward search step
+    const forwardNode = forwardQueue.shift();
+    forwardVisited.add(forwardNode);
+
+    // Check if forward search finds a node in the backward search visited set
+    if (backwardVisited.has(forwardNode)) {
+      return reconstructPath(forwardNode, forwardParents, backwardParents);
     }
 
-    if (i < node.keys.length && key === node.keys[i]) {
-      return true;
-    } else if (node.leaf) {
-      return false;
-    } else {
-      return this.search(key, node.childs[i]);
-    }
-  }
-}
-class BTree {
-  // ...
-
-  insert(key) {
-    const node = this.root;
-    if (node.keys.length === this.order - 1) {
-      const newNode = new BTreeNode(false);
-      this.root = newNode;
-      newNode.childs.push(node);
-      this._splitChild(newNode, 0);
-      this._insertNonFull(newNode, key);
-    } else {
-      this._insertNonFull(node, key);
-    }
-  }
-
-  _insertNonFull(node, key) {
-    let i = node.keys.length - 1;
-    if (node.leaf) {
-      node.keys.push(null);
-      while (i >= 0 && key < node.keys[i]) {
-        node.keys[i + 1] = node.keys[i];
-        i--;
+    // Expand forward node and enqueue its neighbors
+    const forwardNeighbors = graph.getNeighbors(forwardNode);
+    for (const neighbor of forwardNeighbors) {
+      if (!forwardVisited.has(neighbor)) {
+        forwardQueue.push(neighbor);
+        forwardParents.set(neighbor, forwardNode);
       }
-      node.keys[i + 1] = key;
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
+    }
+
+    // Perform backward search step
+    const backwardNode = backwardQueue.shift();
+    backwardVisited.add(backwardNode);
+
+    // Check if backward search finds a node in the forward search visited set
+    if (forwardVisited.has(backwardNode)) {
+      return reconstructPath(backwardNode, forwardParents, backwardParents);
+    }
+
+    // Expand backward node and enqueue its neighbors
+    const backwardNeighbors = graph.getNeighbors(backwardNode);
+    for (const neighbor of backwardNeighbors) {
+      if (!backwardVisited.has(neighbor)) {
+        backwardQueue.push(neighbor);
+        backwardParents.set(neighbor, backwardNode);
       }
-      i++;
-      if (node.childs[i].keys.length === this.order - 1) {
-        this._splitChild(node, i);
-        if (key > node.keys[i]) {
-          i++;
-        }
-      }
-      this._insertNonFull(node.childs[i], key);
     }
   }
 
-  _splitChild(parent, index) {
-    const node = parent.childs[index];
-    const newNode = new BTreeNode(node.leaf);
-    parent.keys.splice(index, 0, node.keys[this.order - 1]);
-    parent.childs.splice(index + 1, 0, newNode);
-    newNode.keys = node.keys.splice(this.order, this.order - 1);
-    if (!node.leaf) {
-      newNode.childs = node.childs.splice(this.order, this.order);
-    }
-  }
+  return null; // No path found
 }
-const bTree = new BTree(3);
-bTree.insert(10);
-bTree.insert(20);
-bTree.insert(5);
-bTree.insert(6);
 
-console.log(bTree.search(10)); // true
-console.log(bTree.search(30)); // false
+function reconstructPath(node, forwardParents, backwardParents) {
+  const path = [node];
+
+  let currentNode = node;
+  while (forwardParents.has(currentNode)) {
+    currentNode = forwardParents.get(currentNode);
+    path.unshift(currentNode);
+  }
+
+  currentNode = node;
+  while (backwardParents.has(currentNode)) {
+    currentNode = backwardParents.get(currentNode);
+    path.push(currentNode);
+  }
+
+  return path;
+}
