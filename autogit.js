bbcc41e232162f1e4cb6e01796c4bcbed579856a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:54:23 +0800

    2-4-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,11 @@
-function longestCommonSubstring(str1, str2) {
-  const matrix = Array(str1.length + 1)
-    .fill(0)
-    .map(() => Array(str2.length + 1).fill(0));
+function getIntersection(list1, list2) {
+  let p1 = list1;
+  let p2 = list2;
 
-  let maxLength = 0;
-  let endIndex = 0;
-
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-        if (matrix[i][j] > maxLength) {
-          maxLength = matrix[i][j];
-          endIndex = i - 1; // or j - 1
-        }
-      }
-    }
+  while (p1 !== p2) {
+    p1 = p1 ? p1.next : list2; // Move to the next node or start of list2
+    p2 = p2 ? p2.next : list1; // Move to the next node or start of list1
   }
 
-  return str1.substr(endIndex - maxLength + 1, maxLength);
+  return p1; // Return null (no intersection) or the intersection node
 }
-const str1 = "abcdef";
-const str2 = "xyzdef";
-const commonSubstring = longestCommonSubstring(str1, str2);
-console.log(commonSubstring); // Output: "def"
