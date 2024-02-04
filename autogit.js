527d610cd1f99507c54035dc842e258332ac1dbd
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:50:41 +0800

    2-4-2024 22:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,17 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
+function largestPrimeFactor(number) {
+  let i = 2;
+ 
+  while (i <= number) {
+    if (number % i === 0) {
+      number /= i;
+    } else {
+      i++;
+    }
+  }
+ 
+  return i;
 }
-
-// Example usage
-const input = 'Hello, World!';
-const output = removeVowels(input);
-console.log(output);  // Output: Hll, Wrld!
+const number = 84;
+const largestFactor = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestFactor}`);
+The largest prime factor of 84 is 7
