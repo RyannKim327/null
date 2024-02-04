946e83bae07530d2d54ea15f06dd7b17bc891808
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:50:02 +0800

    2-4-2024 22:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,158 @@
-function generateFibSeries(n) {
-  let fib = [0, 1];
-  while (fib[fib.length - 1] < n) {
-    fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
-  }
-  return fib;
-}
-let fibSeries = generateFibSeries(n);
-let offset = 0;
-let prevOffset = 0;
-let i = fibSeries.length - 1;
-while (offset < n) {
-  let index = Math.min(offset + prevOffset, n - 1);
-
-  if (arr[index] == value) {
-    return index; // The search value is found at index
-  }
-
-  if (arr[index] < value) {
-    // If the element is smaller, move the two Fibonacci indices two steps down
-    offset = index + 1;
-    i = i - 2;
-  } else {
-    // If the element is larger, move the two Fibonacci indices one step down
-    prevOffset = offset;
-    offset = offset - fibSeries[i - 1];
-    i = i - 1;
+// Red-Black Tree Node
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.color = "red";
+    this.parent = null;
+    this.left = null;
+    this.right = null;
   }
 }
-function fibonacciSearch(arr, value, n) {
-  function generateFibSeries(n) {
-    let fib = [0, 1];
-    while (fib[fib.length - 1] < n) {
-      fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
-    }
-    return fib;
+
+// Red-Black Tree
+class RedBlackTree {
+  constructor() {
+    this.root = null;
   }
 
-  let fibSeries = generateFibSeries(n);
-  let offset = 0;
-  let prevOffset = 0;
-  let i = fibSeries.length - 1;
+  // Insert new value
+  insert(value) {
+    const newNode = new Node(value);
 
-  while (offset < n) {
-    let index = Math.min(offset + prevOffset, n - 1);
-
-    if (arr[index] == value) {
-      return index;
+    if (this.root === null) {
+      this.root = newNode;
+      this.root.color = "black";
+      return;
     }
 
-    if (arr[index] < value) {
-      offset = index + 1;
-      i = i - 2;
-    } else {
-      prevOffset = offset;
-      offset = offset - fibSeries[i - 1];
-      i = i - 1;
+    let currentNode = this.root;
+    let parentNode = null;
+
+    while (currentNode !== null) {
+      parentNode = currentNode;
+
+      if (newNode.value < currentNode.value)
+        currentNode = currentNode.left;
+      else
+        currentNode = currentNode.right;
     }
+
+    newNode.parent = parentNode;
+
+    if (newNode.value < parentNode.value)
+      parentNode.left = newNode;
+    else
+      parentNode.right = newNode;
+
+    this.fixViolations(newNode);
   }
 
-  return -1; // Value not found
+  // Fix violations to maintain red-black properties
+  fixViolations(node) {
+    while (node !== this.root && node.parent.color === "red") {
+      if (node.parent === node.parent.parent.left) {
+        const uncle = node.parent.parent.right;
+
+        if (uncle !== null && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.right) {
+            node = node.parent;
+            this.rotateLeft(node);
+          }
+
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateRight(node.parent.parent);
+        }
+      } else {
+        const uncle = node.parent.parent.left;
+
+        if (uncle !== null && uncle.color === "red") {
+          node.parent.color = "black";
+          uncle.color = "black";
+          node.parent.parent.color = "red";
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.left) {
+            node = node.parent;
+            this.rotateRight(node);
+          }
+
+          node.parent.color = "black";
+          node.parent.parent.color = "red";
+          this.rotateLeft(node.parent.parent);
+        }
+      }
+    }
+
+    this.root.color = "black";
+  }
+
+  // Left rotation
+  rotateLeft(node) {
+    const rightChild = node.right;
+    node.right = rightChild.left;
+
+    if (rightChild.left !== null) {
+      rightChild.left.parent = node;
+    }
+
+    rightChild.parent = node.parent;
+
+    if (node.parent === null)
+      this.root = rightChild;
+    else if (node === node.parent.left)
+      node.parent.left = rightChild;
+    else
+      node.parent.right = rightChild;
+
+    rightChild.left = node;
+    node.parent = rightChild;
+  }
+
+  // Right rotation
+  rotateRight(node) {
+    const leftChild = node.left;
+    node.left = leftChild.right;
+
+    if (leftChild.right !== null) {
+      leftChild.right.parent = node;
+    }
+
+    leftChild.parent = node.parent;
+
+    if (node.parent === null)
+      this.root = leftChild;
+    else if (node === node.parent.left)
+      node.parent.left = leftChild;
+    else
+      node.parent.right = leftChild;
+
+    leftChild.right = node;
+    node.parent = leftChild;
+  }
+
+  // (Optional) Print the red-black tree in-order
+  inOrderTraversal(node) {
+    if (node !== null) {
+      this.inOrderTraversal(node.left);
+      console.log(node.value);
+      this.inOrderTraversal(node.right);
+    }
+  }
 }
-let arr = [1, 2, 3, 5, 8, 13, 21, 34];
-let value = 13;
-let n = arr.length;
+const tree = new RedBlackTree();
 
-let index = fibonacciSearch(arr, value, n);
-console.log(index); // Output: 5
+tree.insert(9);
+tree.insert(5);
+tree.insert(13);
+tree.insert(2);
+tree.insert(7);
+tree.insert(11);
+tree.insert(15);
+
+tree.inOrderTraversal(tree.root);
