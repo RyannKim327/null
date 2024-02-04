deb62117941fabe820d311ca2728e5605ffbc38b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:55:17 +0800

    2-4-2024 22:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,20 @@
-function hasCycle(head) {
-  if (!head || !head.next) {
-    return false; // If the list is empty or has only one node, there can be no cycle
-  }
+function areAnagrams(str1, str2) {
+  // Remove non-alphabetic characters and convert to lowercase
+  str1 = str1.replace(/[^a-zA-Z]/g, "").toLowerCase();
+  str2 = str2.replace(/[^a-zA-Z]/g, "").toLowerCase();
 
-  let slow = head; // The slow pointer moves one node at a time
-  let fast = head; // The fast pointer moves two nodes at a time
+  // Convert strings to arrays and sort them
+  const arr1 = str1.split("").sort();
+  const arr2 = str2.split("").sort();
 
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-
-    if (slow === fast) {
-      return true; // If the slow and fast pointers meet, there is a cycle
-    }
-  }
-
-  return false; // If the fast pointer reaches the end of the list, there is no cycle
+  // Compare the sorted arrays
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
 }
-// Linked List Node
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+const string1 = "listen";
+const string2 = "silent";
+
+if (areAnagrams(string1, string2)) {
+  console.log("The strings are anagrams!");
+} else {
+  console.log("The strings are not anagrams.");
 }
-
-// Creating a sample linked list with a cycle
-const node1 = new ListNode(1);
-const node2 = new ListNode(2);
-const node3 = new ListNode(3);
-const node4 = new ListNode(4);
-const node5 = new ListNode(5);
-
-// Connect the nodes to form a linked list with a cycle
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-node5.next = node3; // This creates the cycle
-
-console.log(hasCycle(node1)); // Output: true
