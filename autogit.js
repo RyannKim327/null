44526fb096726c7bf882a3e518326f7b6c9e0507
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:36:38 +0800

    2-4-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,3 @@
-const CronJob = require('cron').CronJob;
-
-// Define the cron expression to run the task
-const cronExpression = '0 */5 * * * *'; // Runs every 5 minutes
-
-// Define the task to be executed
-const task = () => {
-  console.log('Running the task...');
-  // Your task logic goes here
-};
-
-// Create a new cron job instance
-const cronJob = new CronJob(cronExpression, task);
-
-// Start the cron job
-cronJob.start();
-
-console.log(`Cron job scheduled to run every 5 minutes.`);
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: "hello world"
