c7e08a03f4cbcca5e80f25d616f8da384b9eaef0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:54:38 +0800

    2-4-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,42 @@
-function isPalindrome(str) {
-  // Remove non-alphanumeric characters and convert to lowercase
-  str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  
-  // Reverse the string
-  let reverseStr = str.split('').reverse().join('');
-  
-  // Compare reversed string with original string
-  return reverseStr === str;
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false; // If the list is empty or has only one node, there can be no cycle
+  }
+
+  let slow = head; // The slow pointer moves one node at a time
+  let fast = head; // The fast pointer moves two nodes at a time
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true; // If the slow and fast pointers meet, there is a cycle
+    }
+  }
+
+  return false; // If the fast pointer reaches the end of the list, there is no cycle
+}
+// Linked List Node
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-// Example usage
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
-console.log(isPalindrome("Hello World")); // false
+// Creating a sample linked list with a cycle
+const node1 = new ListNode(1);
+const node2 = new ListNode(2);
+const node3 = new ListNode(3);
+const node4 = new ListNode(4);
+const node5 = new ListNode(5);
+
+// Connect the nodes to form a linked list with a cycle
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node3; // This creates the cycle
+
+console.log(hasCycle(node1)); // Output: true
