15296fc0e3b5c28ce5036cd7cd549fb749f31003
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:36:46 +0800

    2-4-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,24 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Output: "hello world"
+function bubbleSort(arr) {
+  const len = arr.length;
+  let swapped;
+  
+  do {
+    swapped = false;
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap elements
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  
+  return arr;
+}
+
+// Example usage:
+const unsortedArray = [9, 5, 2, 8, 1, 0, 6, 4, 3, 7];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray);  // Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
