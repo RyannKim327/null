52d42101f036a14d39fab767ed43845e1e7fbab4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:52:15 +0800

    2-4-2024 22:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,4 @@
-function depthLimitedSearch(node, goal, depthLimit) {
-  if (node === goal) {
-    return true; // Goal found
-  }
-
-  if (depthLimit <= 0) {
-    return false; // Reached depth limit
-  }
-
-  for (let child of node.children) {
-    if (depthLimitedSearch(child, goal, depthLimit - 1)) {
-      return true; // Goal found in child subtree
-    }
-  }
-
-  return false; // Goal not found in current subtree
-}
-const rootNode = /* initialize your root node */;
-const goalNode = /* initialize your goal node */;
-const depthLimit = 5; // Set your desired depth limit
-
-const isGoalFound = depthLimitedSearch(rootNode, goalNode, depthLimit);
-console.log(`Goal found? ${isGoalFound}`);
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
