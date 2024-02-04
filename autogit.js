60f71665ee3a0b1b2c67af6c0589a0d48f911499
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:39:15 +0800

    2-4-2024 22:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,19 @@
-let str = "Hello, World!";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Output: hello, world!
+const array = [1, 1, 2, 3, 3, 4, 5, 5];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray);
+[1, 2, 3, 4, 5]
+const array = [1, 1, 2, 3, 3, 4, 5, 5];
+const uniqueArray = array.filter((element, index) => {
+  return array.indexOf(element) === index;
+});
+console.log(uniqueArray);
+[1, 2, 3, 4, 5]
+const array = [1, 1, 2, 3, 3, 4, 5, 5];
+const uniqueArray = array.reduce((accumulator, currentValue) => {
+  if (!accumulator.includes(currentValue)) {
+    accumulator.push(currentValue);
+  }
+  return accumulator;
+}, []);
+console.log(uniqueArray);
+[1, 2, 3, 4, 5]
