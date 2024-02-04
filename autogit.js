36962249cbc5e17fbb9f96f3bc3b596c37911248
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:56:58 +0800

    2-4-2024 22:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,42 @@
-function selectionSort(array) {
-  const length = array.length;
-
-  for (let i = 0; i < length; i++) {
-    let minIndex = i;
-
-    for (let j = i + 1; j < length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
+function longestCommonSubsequence(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  
+  // Create a 2D array to store the lengths of LCS
+  const dp = Array(m + 1)
+    .fill(null)
+    .map(() => Array(n + 1).fill(0));
+  
+  // Calculate the lengths of LCS for all substrings
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
-
-    if (minIndex !== i) {
-      // Swap values
-      const temp = array[i];
-      array[i] = array[minIndex];
-      array[minIndex] = temp;
+  }
+  
+  // Backtrack to construct the LCS
+  let lcs = '';
+  let i = m, j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
     }
   }
-
-  return array;
+  
+  return lcs;
 }
-const unsortedArray = [64, 25, 12, 22, 11];
-const sortedArray = selectionSort(unsortedArray);
-console.log(sortedArray); // Output: [11, 12, 22, 25, 64]
+const str1 = 'ABCD';
+const str2 = 'AEBD';
+
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: ABD
