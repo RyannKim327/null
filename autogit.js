287234d36206c3e5281286eaefc9b0b521fde813
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 23:05:57 +0800

    2-4-2024 23:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,47 @@
-function factorial(number) {
-  if (number === 0 || number === 1) {
-    return 1;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  let result = 1;
-  for (let i = 2; i <= number; i++) {
-    result *= i;
-  }
-
-  return result;
 }
 
-const number = 5;
-const factorialResult = factorial(number);
-console.log(`The factorial of ${number} is: ${factorialResult}`);
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+
+function findLinkedListLength(list) {
+  let length = 0;
+  let current = list.head;
+  
+  while (current) {
+    length++;
+    current = current.next;
+  }
+  
+  return length;
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+
+console.log(findLinkedListLength(linkedList));  // Output: 4
