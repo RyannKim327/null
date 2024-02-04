4f9c8ecfca4788111fb99ed838062abb4e749e5a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:33:16 +0800

    2-4-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,88 +1,9 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  prepend(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  delete(data) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-      if (this.head === null) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let current = this.head;
-
-    while (current.next) {
-      if (current.next.data === data) {
-        current.next = current.next.next;
-        if (current.next === null) {
-          this.tail = current;
-        }
-        return;
-      }
-      current = current.next;
-    }
-  }
-
-  print() {
-    let current = this.head;
-    const result = [];
-
-    while (current) {
-      result.push(current.data);
-      current = current.next;
-    }
-
-    console.log(result.join(" -> "));
-  }
-}
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-
-list.prepend(0);
-
-list.print(); // Output: 0 -> 1 -> 2 -> 3
-
-list.delete(2);
-
-list.print(); // Output: 0 -> 1 -> 3
+let str = "  Hello,     World!  ";
+let trimmedStr = str.replace(/\s/g, "");
+console.log(trimmedStr); // Output: "Hello,World!"
+let str = "  Hello,     World!  ";
+let trimmedStr = str.trim();
+console.log(trimmedStr); // Output: "Hello,     World!"
+let str = "  Hello,     World!  ";
+let trimmedStr = str.replace(/\s+/g, "");
+console.log(trimmedStr); // Output: "Hello,World!"
