1b3a4bc2f4020c239543df066c7785464899f24e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 23:05:21 +0800

    2-4-2024 23:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,25 @@
-function longestCommonSubsequence(str1, str2) {
-  // Create a 2D array of size (str1.length + 1) x (str2.length + 1)
-  const matrix = Array(str1.length + 1)
-    .fill(0)
-    .map(() => Array(str2.length + 1).fill(0));
+function shellSort(arr) {
+  const len = arr.length;
+  let gap = Math.floor(len / 2);
 
-  // Fill the matrix
-  for (let i = 1; i <= str1.length; i++) {
-    for (let j = 1; j <= str2.length; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        matrix[i][j] = matrix[i - 1][j - 1] + 1;
-      } else {
-        matrix[i][j] = Math.max(matrix[i][j - 1], matrix[i - 1][j]);
+  while (gap > 0) {
+    for (let i = gap; i < len; i++) {
+      const temp = arr[i];
+      let innerIndex = i;
+
+      while (innerIndex >= gap && arr[innerIndex - gap] > temp) {
+        arr[innerIndex] = arr[innerIndex - gap];
+        innerIndex -= gap;
       }
+
+      arr[innerIndex] = temp;
     }
+
+    gap = Math.floor(gap / 2);
   }
 
-  // Trace back the matrix to find the LCS
-  let i = str1.length;
-  let j = str2.length;
-  const lcs = [];
-
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs.unshift(str1[i - 1]);
-      i--;
-      j--;
-    } else if (matrix[i][j - 1] > matrix[i - 1][j]) {
-      j--;
-    } else {
-      i--;
-    }
-  }
-
-  return lcs.join('');
+  return arr;
 }
-
-// Example usage
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const result = longestCommonSubsequence(str1, str2);
-console.log(result); // Output: ADH
+const arr = [9, 4, 1, 7, 3, 6, 2, 8, 5];
+const sortedArr = shellSort(arr);
+console.log(sortedArr); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
