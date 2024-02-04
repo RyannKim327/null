d082216f19e147654a43e698e8226b005d9b7a5c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-04 22:36:56 +0800

    2-4-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,54 @@
-function bubbleSort(arr) {
-  const len = arr.length;
-  let swapped;
+function findKthSmallest(arr, k) {
+  if (arr.length < k || k <= 0) {
+    return null;
+  }
   
-  do {
-    swapped = false;
-    for (let i = 0; i < len - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        // Swap elements
-        const temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = true;
+  // Modified quicksort algorithm to find the kth smallest element
+  function quickselect(arr, left, right, k) {
+    if (left === right) {
+      return arr[left];
+    }
+    
+    var pivotIndex = partition(arr, left, right);
+    
+    if (k === pivotIndex) {
+      return arr[k];
+    } else if (k < pivotIndex) {
+      return quickselect(arr, left, pivotIndex - 1, k);
+    } else {
+      return quickselect(arr, pivotIndex + 1, right, k);
+    }
+  }
+  
+  // Helper function for partitioning the array
+  function partition(arr, left, right) {
+    var pivotValue = arr[right];
+    var i = left;
+    
+    for (var j = left; j < right; j++) {
+      if (arr[j] < pivotValue) {
+        swap(arr, i, j);
+        i++;
       }
     }
-  } while (swapped);
+    
+    swap(arr, i, right);
+    return i;
+  }
+
+  // Helper function for swapping array elements
+  function swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
   
-  return arr;
+  // Call quickselect to find the kth smallest element
+  return quickselect(arr, 0, arr.length - 1, k - 1);
 }
 
-// Example usage:
-const unsortedArray = [9, 5, 2, 8, 1, 0, 6, 4, 3, 7];
-const sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray);  // Output: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Example usage
+var arr = [7, 10, 4, 3, 20, 15];
+var k = 3;
+var kthSmallest = findKthSmallest(arr, k);
+console.log(kthSmallest);  // Output: 7
