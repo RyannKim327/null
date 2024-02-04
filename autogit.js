859c324884224e8dd2296150643c90631187e2a7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-05 05:39:31 +0800

    02-05-24 05:39

--- autogit.js
+++ autogit.js
@@ -1,18 +1,1 @@
-function isPrime(number) {
-  if (number <= 1) {
-    return false;
-  }
-
-  // Check for divisibility from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-// Example usage
-console.log(isPrime(7));  // true
-console.log(isPrime(10)); // false
+console.log("Hehe")
