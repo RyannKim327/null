48a4e4626881f6a147fc84c4d1198367e28207d7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:44:12 +0800

    2-6-2024 22:44 [User mode]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-// Remove leading and trailing whitespace
-let str = "   Hello, World!   ";
-let trimmedStr = str.trim();
-console.log(trimmedStr); // Output: "Hello, World!"
-
-// Remove all whitespace within the string
-let stringWithWhiteSpace = " This is a  sample  string ";
-let stringWithoutWhiteSpace = stringWithWhiteSpace.replace(/\s/g, '');
-console.log(stringWithoutWhiteSpace); // Output: "Thisisasamplestring"
