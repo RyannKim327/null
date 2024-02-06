ba65781e18d031cedf49e243b29a16699f4cfdec
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:30:14 +0800

    2-6-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,5 @@
-function findMajorityElement(arr) {
-  let candidate;
-  let count = 0;
+let str = "Hello, World!";
+let lowerCaseStr = str.toLowerCase();
 
-  for (let num of arr) {
-    if (count === 0) {
-      candidate = num;
-      count = 1;
-    } else if (num === candidate) {
-      count++;
-    } else {
-      count--;
-    }
-  }
-
-  // Check if the candidate is the majority element
-  count = 0;
-  for (let num of arr) {
-    if (num === candidate) count++;
-  }
-
-  if (count > arr.length / 2) {
-    return candidate;
-  } else {
-    return "No majority element found";
-  }
-}
-
-// Example usage
-const arr = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(arr);
-console.log(majorityElement);
+console.log(lowerCaseStr);
+// Output: hello, world!
