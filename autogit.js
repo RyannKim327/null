e3d1e3d849c6493b8861236ec7465e9bdd4d50e3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 21:57:32 +0800

    2-6-2024 21:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,10 @@
-// Node class
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-// LinkedList class
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  // Add a node to the end of the linked list
-  append(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  // Add a node to the beginning of the linked list
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  // Remove a node from the linked list
-  remove(value) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.value === value) {
-      this.head = this.head.next;
-      if (!this.head) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next) {
-      if (currentNode.next.value === value) {
-        if (currentNode.next === this.tail) {
-          this.tail = currentNode;
-        }
-        currentNode.next = currentNode.next.next;
-        return;
-      }
-      currentNode = currentNode.next;
-    }
-  }
-
-  // Search for a node with a specific value
-  search(value) {
-    let currentNode = this.head;
-    while (currentNode) {
-      if (currentNode.value === value) {
-        return currentNode;
-      }
-      currentNode = currentNode.next;
-    }
-    return null;
-  }
-
-  // Convert the linked list to an array
-  toArray() {
-    const nodes = [];
-    let currentNode = this.head;
-    while (currentNode) {
-      nodes.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    return nodes;
-  }
-}
-const list = new LinkedList();
-list.append(10); // [10]
-list.append(20); // [10, 20]
-list.prepend(5); // [5, 10, 20]
-list.remove(10); // [5, 20]
-console.log(list.search(20)); // Node { value: 20, next: null }
-console.log(list.toArray()); // [5, 20]
+let str = "Hello World! This is a test string.";
+let result = str.replace(/\s/g, ""); // Removes all whitespace characters
+console.log(result);
+let str = "Hello World! This is a test string.";
+let result = str.split(" ").join("");
+console.log(result);
+let str = "Hello World! This is a test string.";
+let result = [...str].filter(char => char !== " ").join("");
+console.log(result);
+HelloWorld!Thisisateststring.
