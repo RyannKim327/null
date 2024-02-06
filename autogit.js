4115847b248be3a22ed89077a71a84e7028122c3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:44:39 +0800

    2-6-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,76 +1,28 @@
-class BTreeNode {
-  constructor(leaf = true) {
-    this.keys = [];
-    this.children = [];
-    this.leaf = leaf;
-  }
-
-  isFull(order) {
-    return this.keys.length === 2 * order - 1;
-  }
-}
-class BTree {
-  constructor(order) {
-    this.root = new BTreeNode(true);
-    this.order = order;
-  }
-
-  insert(key) {
-    const root = this.root;
-    if (root.isFull(this.order)) {
-      const newNode = new BTreeNode(false);
-      newNode.children.push(root);
-      this.root = newNode;
-      this.splitChild(newNode, 0);
-      this.insertNonFull(newNode, key);
-    } else {
-      this.insertNonFull(root, key);
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
-  }
+}
 
-  insertNonFull(node, key) {
-    let i = node.keys.length - 1;
-    if (node.leaf) {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      node.keys.splice(i + 1, 0, key);
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      i++;
-      if (node.children[i].isFull(this.order)) {
-        this.splitChild(node, i);
-        if (key > node.keys[i]) {
-          i++;
+function getIntersectionNode(headA, headB) {
+    const set = new Set();
+
+    // Traverse through the first linked list and store all node references in the Set
+    let node = headA;
+    while (node) {
+        set.add(node);
+        node = node.next;
+    }
+
+    // Traverse through the second linked list and check for intersection
+    node = headB;
+    while (node) {
+        if (set.has(node)) {
+            return node; // Intersection found
         }
-      }
-      this.insertNonFull(node.children[i], key);
-    }
-  }
-
-  splitChild(parent, index) {
-    const order = this.order;
-    const child = parent.children[index];
-    const newNode = new BTreeNode(child.leaf);
-
-    parent.keys.splice(index, 0, child.keys[order - 1]);
-
-    newNode.keys = child.keys.splice(order, order - 1);
-
-    if (!child.leaf) {
-      newNode.children = child.children.splice(order, order);
+        node = node.next;
     }
 
-    parent.children.splice(index + 1, 0, newNode);
-  }
+    return null; // No intersection found
 }
-const bTree = new BTree(3); // Create a B-tree of order 3
-
-bTree.insert(10);
-bTree.insert(20);
-bTree.insert(5);
-bTree.insert(6);
-
-console.log(bTree.root); // Prints the root node of the B-tree
