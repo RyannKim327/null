91536e148aaffe802a60d1d451165e1e0c9113f5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:30:44 +0800

    2-6-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,42 @@
-function longestCommonPrefix(strings) {
-  let prefix = '';
-
-  if (strings.length === 0) {
-    return prefix;
+function buildHeap(arr) {
+  var heapSize = arr.length;
+  for (var i = Math.floor(heapSize / 2); i >= 0; i--) {
+    heapify(arr, heapSize, i);
   }
-
-  strings.sort();
-
-  for (let i = 0; i < strings[0].length; i++) {
-    if (strings[0][i] === strings[strings.length - 1][i]) {
-      prefix += strings[0][i];
-    } else {
-      break;
-    }
-  }
-
-  return prefix;
 }
-const strings = ['flower', 'flow', 'flight'];
-console.log(longestCommonPrefix(strings));   // Output: 'fl'
+function heapify(arr, heapSize, i) {
+  var largest = i;
+  var leftChild = 2 * i + 1;
+  var rightChild = 2 * i + 2;
+
+  if (leftChild < heapSize && arr[leftChild] > arr[largest]) {
+    largest = leftChild;
+  }
+
+  if (rightChild < heapSize && arr[rightChild] > arr[largest]) {
+    largest = rightChild;
+  }
+
+  if (largest !== i) {
+    swap(arr, i, largest);
+    heapify(arr, heapSize, largest);
+  }
+}
+function swap(arr, i, j) {
+  var temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+function heapSort(arr) {
+  buildHeap(arr);
+  var heapSize = arr.length;
+
+  for (var i = arr.length - 1; i > 0; i--) {
+    swap(arr, 0, i);
+    heapSize--;
+    heapify(arr, heapSize, 0);
+  }
+}
+var array = [6, 5, 3, 1, 8, 7, 2, 4];
+heapSort(array);
+console.log(array);  // Output: [1, 2, 3, 4, 5, 6, 7, 8]
