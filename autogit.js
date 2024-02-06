173b8e98fb9a152a7f52e850faeaf4ed728233f3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:36:56 +0800

    2-6-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,14 @@
-function rabinKarpSearch(text, pattern) {
-  const textLength = text.length;
-  const patternLength = pattern.length;
-  const primeNumber = 101; // Choose a prime number for modulo operation
-  const hashPattern = hash(pattern, patternLength, primeNumber);
-  let hashText = hash(text, patternLength, primeNumber);
+function isPalindrome(str) {
+  let left = 0;
+  let right = str.length - 1;
 
-  for (let i = 0; i <= textLength - patternLength; i++) {
-    if (hashPattern === hashText) {
-      if (text.substring(i, i + patternLength) === pattern) {
-        return i; // Match found, return the starting index
-      }
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
     }
-    // Recalculate hash value for next substring
-    if (i < textLength - patternLength) {
-      hashText =
-        ((hashText - text.charCodeAt(i) * Math.pow(primeNumber, patternLength - 1)) * primeNumber +
-          text.charCodeAt(i + patternLength)) %
-        primeNumber;
-      if (hashText < 0) {
-        hashText += primeNumber;
-      }
-    }
+    left++;
+    right--;
   }
-  return -1; // Match not found
-}
 
-function hash(str, strLength, primeNumber) {
-  let hashValue = 0;
-  for (let i = 0; i < strLength; i++) {
-    hashValue += str.charCodeAt(i) * Math.pow(primeNumber, i);
-  }
-  return hashValue % primeNumber;
+  return true;
 }
-
-// Usage example
-const text = 'ABCDEFGHIJK';
-const pattern = 'DEF';
-console.log(rabinKarpSearch(text, pattern)); // Output: 3 (index where the pattern 'DEF' starts)
