42158fea4fe6a12e14137bf53274800d10171de7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:16:49 +0800

    2-6-2024 22:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,9 @@
-function quicksort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const left = [];
-  const right = [];
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else if (arr[i] > pivot) {
-      right.push(arr[i]);
-    }
-  }
-  
-  return [...quicksort(left), pivot, ...quicksort(right)];
-}
-
-// Example usage:
-const unsortedArray = [5, 2, 6, 3, 1, 4];
-const sortedArray = quicksort(unsortedArray);
-console.log(sortedArray); // [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray);
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
+console.log(uniqueArray);
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = array.reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], []);
+console.log(uniqueArray);
