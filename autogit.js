419b99ddddb653df4b22ad2ade7db7566ef846fe
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:39:51 +0800

    2-6-2024 22:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,26 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, '');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
-var input = 'Hello, World!';
-var output = removeVowels(input);
+const reversed = reverseString("Hello, world!");
+console.log(reversed); // Output: "!dlrow ,olleH"
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
-console.log(output); // Output: Hll, Wrld!
+const reversed = reverseString("Hello, world!");
+console.log(reversed); // Output: "!dlrow ,olleH"
+function reverseString(str) {
+  if (str === '') {
+    return '';
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0);
+  }
+}
+
+const reversed = reverseString("Hello, world!");
+console.log(reversed); // Output: "!dlrow ,olleH"
