b59145d68747f0716cae9b1e380d8b5e4bee5c9a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:25:27 +0800

    2-6-2024 22:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,15 @@
-function interpolationSearch(arr, target, start = 0) {
-  let end = arr.length - 1;
-
-  while (start <= end) {
-    const estimatedPos =
-      Math.floor(((target - arr[start]) / (arr[end] - arr[start])) * (end - start)) +
-      start;
-
-    if (estimatedPos < start || estimatedPos > end) {
-      return -1;
+function bubbleSort(array) {
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+                swapped = true;
+            }
+        }
     }
-
-    if (arr[estimatedPos] === target) {
-      return estimatedPos;
-    }
-
-    if (arr[estimatedPos] < target) {
-      start = estimatedPos + 1;
-    } else {
-      end = estimatedPos - 1;
-    }
-  }
-
-  return -1;
+    return array;
 }
-
-// Testing the interpolationSearch function
-const arr = [2, 5, 7, 10, 15, 18, 20];
-const target = 10;
-const index = interpolationSearch(arr, target);
-console.log(`Found ${target} at index ${index}`);
+const numbers = [5, 3, 8, 2, 1, 4];
+console.log(bubbleSort(numbers)); // Output: [1, 2, 3, 4, 5, 8]
