3e57cecac3c720b5bfde62927edeaf7cd16bcaed
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:18:37 +0800

    2-6-2024 22:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,12 @@
-function insertionSort(arr) {
-  const len = arr.length;
-  for (let i = 1; i < len; i++) {
-    let current = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > current) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = current;
-  }
-  return arr;
+let str = "Hello, World!";
+let reversedStr = str.split('').reverse().join('');
+console.log(reversedStr); // Output: "!dlroW ,olleH"
+let str = "Hello, World!";
+let reversedStr = '';
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-
-// Example usage:
-const unsortedArray = [5, 3, 8, 1, 2, 7];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 5, 7, 8]
+console.log(reversedStr); // Output: "!dlroW ,olleH"
+let str = "Hello, World!";
+let reversedStr = Array.from(str).reduce((acc, char) => char + acc, '');
+console.log(reversedStr); // Output: "!dlroW ,olleH"
