02909926795205483694b97bb361eca5ef0eaddf
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:19:50 +0800

    2-6-2024 22:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,105 +1,44 @@
-class BTreeNode {
-  constructor(t, leaf = true) {
-    this.keys = [];
-    this.children = [];
-    this.leaf = leaf;
-    this.t = t;
+function boyerMooreHorspool(haystack, needle) {
+  // Step 2: Preprocessing
+  const needleLength = needle.length;
+  const haystackLength = haystack.length;
+
+  // Step 3: Preprocessing Bad Character Table
+  const badCharTable = {};
+
+  for (let i = 0; i < needleLength - 1; i++) {
+    badCharTable[needle[i]] = needleLength - 1 - i;
   }
 
-  insertNonFull(key) {
-    let i = this.keys.length - 1;
+  // Step 4: Searching
+  let haystackIndex = needleLength - 1;
 
-    if (this.leaf) {
-      while (i >= 0 && key < this.keys[i]) {
-        this.keys[i + 1] = this.keys[i];
-        i--;
-      }
-      this.keys[i + 1] = key;
+  while (haystackIndex < haystackLength) {
+    let needleIndex = needleLength - 1;
+
+    while (needleIndex >= 0 && haystack[haystackIndex] === needle[needleIndex]) {
+      haystackIndex--;
+      needleIndex--;
+    }
+
+    if (needleIndex === -1) {
+      return haystackIndex + 1; // Match found
     } else {
-      while (i >= 0 && key < this.keys[i]) {
-        i--;
-      }
+      const charInHaystack = haystack[haystackIndex];
+      const skipLen = badCharTable[charInHaystack] || needleLength;
 
-      if (this.children[i + 1].keys.length === (2 * this.t) - 1) {
-        this.splitChild(i + 1, this.children[i + 1]);
-
-        if (key > this.keys[i + 1]) {
-          i++;
-        }
-      }
-      
-      this.children[i + 1].insertNonFull(key);
+      haystackIndex += skipLen;
     }
   }
 
-  splitChild(i, node) {
-    const splitIndex = Math.floor(node.keys.length / 2);
-    const splitValue = node.keys[splitIndex];
-    const newNode = new BTreeNode(this.t, node.leaf);
-
-    for (let j = splitIndex + 1; j < node.keys.length; j++) {
-      newNode.keys.push(node.keys[j]);
-    }
-
-    if (!node.leaf) {
-      for (let j = splitIndex + 1; j < node.children.length; j++) {
-        newNode.children.push(node.children[j]);
-      }
-    }
-
-    node.keys.length = splitIndex;
-
-    if (!node.leaf) {
-      node.children.length = splitIndex + 1;
-    }
-
-    this.keys.splice(i, 0, splitValue);
-    this.children.splice(i + 1, 0, newNode);
-  }
-
-  search(key) {
-    let i = 0;
-    while (i < this.keys.length && key > this.keys[i]) {
-      i++;
-    }
-
-    if (this.keys[i] === key) {
-      return this;
-    }
-
-    if (this.leaf) {
-      return null;
-    }
-
-    return this.children[i].search(key);
-  }
+  return -1; // No match found
 }
+const haystack = "The quick brown fox jumps over the lazy dog";
+const needle = "fox";
 
-class BTree {
-  constructor(t) {
-    this.root = new BTreeNode(t, true);
-    this.t = t;
-  }
-
-  insert(key) {
-    if (this.root.keys.length === (2 * this.t) - 1) {
-      const oldRoot = this.root;
-      this.root = new BTreeNode(this.t, false);
-      this.root.children.push(oldRoot);
-      this.root.splitChild(0, oldRoot);
-      this.root.insertNonFull(key);
-    } else {
-      this.root.insertNonFull(key);
-    }
-  }
-
-  search(key) {
-    return this.root.search(key);
-  }
+const result = boyerMooreHorspool(haystack, needle);
+if (result !== -1) {
+  console.log("Match found at index:", result);
+} else {
+  console.log("No match found");
 }
-const bTree = new BTree(3);
-bTree.insert(10);
-bTree.insert(20);
-bTree.insert(5);
-const node = bTree.search(10);
-console.log(node.keys); // Output: [10]
