2f82db9f701d1df0c72479a1a5a2e40f30b7d2e6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 21:59:33 +0800

    2-6-2024 21:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,8 @@
-function findCommonElements(array1, array2) {
-  var commonElements = [];
-  
-  for (var i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      commonElements.push(array1[i]);
-    }
-  }
-  
-  return commonElements;
-}
+let str = "   Hello,    World!   ";
+let trimmedStr = str.replace(/\s/g, '');
 
-var array1 = [1, 2, 3, 4, 5];
-var array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  return array1.filter(element => array2.includes(element));
-}
+console.log(trimmedStr); // Output: "Hello,World!"
+let str = "   Hello,    World!   ";
+let trimmedStr = str.trim();
 
-var array1 = [1, 2, 3, 4, 5];
-var array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
-function findCommonElements(array1, array2) {
-  var set1 = new Set(array1);
-  return array2.filter(element => set1.has(element));
-}
-
-var array1 = [1, 2, 3, 4, 5];
-var array2 = [4, 5, 6, 7, 8];
-console.log(findCommonElements(array1, array2)); // Output: [4, 5]
+console.log(trimmedStr); // Output: "Hello,    World!"
