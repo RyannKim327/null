00446bb88f060bf079db51982124712daa80e174
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:13:02 +0800

    2-6-2024 22:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,18 @@
-function findMax(arr) {
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
-function radixSort(arr) {
-  const max = findMax(arr);
-  const maxDigits = max.toString().length;
+function bubbleSort(arr) {
+  const len = arr.length;
 
-  for (let digit = 0; digit < maxDigits; digit++) {
-    // Create 10 buckets (0-9) for each place value
-    const buckets = Array.from({ length: 10 }, () => []);
-    
-    // Distribute elements into buckets based on the current digit
-    for (let i = 0; i < arr.length; i++) {
-      const num = arr[i];
-      const digitValue = Math.floor((num / Math.pow(10, digit)) % 10);
-      buckets[digitValue].push(num);
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swap the elements
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
     }
-    
-    // Gather elements from buckets in order
-    arr = [].concat(...buckets);
   }
 
   return arr;
 }
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+
+// Example usage
+const array = [5, 3, 8, 2, 1, 4];
+console.log(bubbleSort(array)); // Output: [1, 2, 3, 4, 5, 8]
