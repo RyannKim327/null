b0f2857cd39bc7e38cc6bca9d4c7b54f16358028
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:12:44 +0800

    2-6-2024 22:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,4 @@
-function topologicalSort(graph) {
-  const visited = new Set();
-  const order = [];
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-  function dfs(node) {
-    visited.add(node);
-
-    graph[node].forEach((adjNode) => {
-      if (!visited.has(adjNode)) {
-        dfs(adjNode);
-      }
-    });
-
-    order.unshift(node); // Add the node to the beginning of the order list
-  }
-
-  Object.keys(graph).forEach((node) => {
-    if (!visited.has(node)) {
-      dfs(node);
-    }
-  });
-
-  return order;
-}
-
-// Example usage:
-const graph = {
-  A: ['B', 'C'],
-  B: ['D'],
-  C: ['D', 'E'],
-  D: ['F'],
-  E: ['F'],
-  F: []
-};
-
-const result = topologicalSort(graph);
-console.log(result);
+console.log(lowercaseStr); // Output: "hello world"
