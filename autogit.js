72a256e62599fd8a65b13ab4a9846ab7b88dd51e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 21:53:33 +0800

    2-6-2024 21:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,93 @@
-class Graph {
-  constructor(numNodes) {
-    this.numNodes = numNodes;
-    this.adjList = new Array(numNodes).fill(null).map(() => []);
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+  
+  // Other methods will be implemented here
+}
+swap(i, j) {
+  const temp = this.heap[i];
+  this.heap[i] = this.heap[j];
+  this.heap[j] = temp;
+}
+parentIndex(index) {
+  return Math.floor((index - 1) / 2);
+}
+leftChildIndex(index) {
+  return index * 2 + 1;
+}
+rightChildIndex(index) {
+  return index * 2 + 2;
+}
+hasParent(index) {
+  return this.parentIndex(index) >= 0;
+}
+hasLeftChild(index) {
+  return this.leftChildIndex(index) < this.heap.length;
+}
+hasRightChild(index) {
+  return this.rightChildIndex(index) < this.heap.length;
+}
+parent(index) {
+  return this.heap[this.parentIndex(index)];
+}
+leftChild(index) {
+  return this.heap[this.leftChildIndex(index)];
+}
+rightChild(index) {
+  return this.heap[this.rightChildIndex(index)];
+}
+peek() {
+  if (this.heap.length === 0) {
+    throw new Error('Priority queue is empty');
+  }
+  return this.heap[0];
+}
+poll() {
+  if (this.heap.length === 0) {
+    throw new Error('Priority queue is empty');
   }
 
-  addEdge(node1, node2) {
-    this.adjList[node1].push(node2);
-  }
-
-  topologicalSortUtil(node, visited, stack) {
-    visited[node] = true;
-
-    // Recur for all the adjacent nodes
-    for (const adjacentNode of this.adjList[node]) {
-      if (!visited[adjacentNode]) {
-        this.topologicalSortUtil(adjacentNode, visited, stack);
-      }
-    }
-
-    // After visiting all adjacent nodes, push the current node to stack
-    stack.push(node);
-  }
-
-  topologicalSort() {
-    const visited = new Array(this.numNodes).fill(false);
-    const stack = [];
-
-    for (let node = 0; node < this.numNodes; node++) {
-      if (!visited[node]) {
-        this.topologicalSortUtil(node, visited, stack);
-      }
-    }
-
-    // Print the order of the nodes
-    while (stack.length > 0) {
-      console.log(stack.pop());
-    }
+  const item = this.heap[0];
+  this.heap[0] = this.heap[this.heap.length - 1];
+  this.heap.pop();
+  this.heapifyDown();
+  return item;
+}
+add(item) {
+  this.heap.push(item);
+  this.heapifyUp();
+}
+heapifyUp() {
+  let index = this.heap.length - 1;
+  while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
+    const parentIndex = this.parentIndex(index);
+    this.swap(parentIndex, index);
+    index = parentIndex;
   }
 }
+heapifyDown() {
+  let index = 0;
+  while (this.hasLeftChild(index)) {
+    let smallerChildIndex = this.leftChildIndex(index);
+    if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
+      smallerChildIndex = this.rightChildIndex(index);
+    }
 
-// Example usage
-const graph = new Graph(6);
-graph.addEdge(5, 2);
-graph.addEdge(5, 0);
-graph.addEdge(4, 0);
-graph.addEdge(4, 1);
-graph.addEdge(2, 3);
-graph.addEdge(3, 1);
-
-console.log("Topological Sort:");
-graph.topologicalSort();
+    if (this.heap[index] < this.heap[smallerChildIndex]) {
+      break;
+    } else {
+      this.swap(index, smallerChildIndex);
+    }
+    index = smallerChildIndex;
+  }
+}
+const pq = new PriorityQueue();
+pq.add(5);
+pq.add(3);
+pq.add(8);
+pq.add(2);
+console.log(pq.poll());  // Output: 2
+console.log(pq.poll());  // Output: 3
+console.log(pq.poll());  // Output: 5
+console.log(pq.poll());  // Output: 8
