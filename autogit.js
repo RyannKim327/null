242e746f9672387a95f2e87054d81528ffc3419e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:34:22 +0800

    2-6-2024 22:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,127 @@
-function findFirstRepeatedChar(str) {
-  const charFrequency = {};
-  
-  for (let char of str) {
-    if (charFrequency[char]) {
-      return char;
-    }
-    charFrequency[char] = true;
+class BinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getLeftChildIndex(parentIndex) {
+    return 2 * parentIndex + 1;
   }
   
-  return null; // return null if no repeated character is found
-}
+  getRightChildIndex(parentIndex) {
+    return 2 * parentIndex + 2;
+  }
 
-// Example usage
-const inputString = "Hello, World!";
-const firstRepeatedChar = findFirstRepeatedChar(inputString);
-console.log("First repeated character:", firstRepeatedChar);
-First repeated character: l
+  getParentIndex(childIndex) {
+    return Math.floor((childIndex - 1) / 2);
+  }
+
+  hasLeftChild(index) {
+    return this.getLeftChildIndex(index) < this.heap.length;
+  }
+  
+  hasRightChild(index) {
+    return this.getRightChildIndex(index) < this.heap.length;
+  }
+
+  hasParent(index) {
+    return this.getParentIndex(index) >= 0;
+  }
+
+  leftChild(index) {
+    return this.heap[this.getLeftChildIndex(index)];
+  }
+  
+  rightChild(index) {
+    return this.heap[this.getRightChildIndex(index)];
+  }
+
+  parent(index) {
+    return this.heap[this.getParentIndex(index)];
+  }
+
+  swap(index1, index2) {
+    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      throw new Error('Queue is empty');
+    }
+    return this.heap[0];
+  }
+
+  poll() {
+    if (this.heap.length === 0) {
+      throw new Error('Queue is empty');
+    }
+    if (this.heap.length === 1) {
+      return this.heap.pop();
+    }
+    const item = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return item;
+  }
+
+  add(item) {
+    this.heap.push(item);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
+      const parentIndex = this.getParentIndex(index);
+      this.swap(parentIndex, index);
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.hasLeftChild(index)) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+
+      if (this.heap[index] < this.heap[smallerChildIndex]) {
+        break;
+      } else {
+        this.swap(index, smallerChildIndex);
+      }
+      index = smallerChildIndex;
+    }
+  }
+}
+class PriorityQueue {
+  constructor() {
+    this.heap = new BinaryHeap();
+  }
+  
+  isEmpty() {
+    return this.heap.heap.length === 0;
+  }
+  
+  peek() {
+    return this.heap.peek();
+  }
+  
+  enqueue(item) {
+    this.heap.add(item);
+  }
+  
+  dequeue() {
+    return this.heap.poll();
+  }
+}
+const queue = new PriorityQueue();
+queue.enqueue(5);
+queue.enqueue(3);
+queue.enqueue(8);
+queue.enqueue(1);
+
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 3
+console.log(queue.dequeue()); // Output: 5
+console.log(queue.dequeue()); // Output: 8
