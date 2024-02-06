9c7ae252edd86961c2cbb5c7acd5eaa7e2bb36e5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 20:20:37 +0800

    2-6-2024 20:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,6 @@
-class TreeNode {
-  constructor(val, left = null, right = null) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
-}
-
-function maxDepth(root) {
-  if (root === null) {
-    return 0;  // If the root is null, the depth is 0
-  } else {
-    const leftDepth = maxDepth(root.left);   // Get the maximum depth of the left subtree
-    const rightDepth = maxDepth(root.right); // Get the maximum depth of the right subtree
-
-    return Math.max(leftDepth, rightDepth) + 1; // Return the maximum depth + 1 for the current node
-  }
-}
+let str = "42";
+let num = parseInt(str);
+console.log(num);  // Output: 42
+let str = "42";
+let num = +str;
+console.log(num);  // Output: 42
