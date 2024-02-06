8835fe57958a2600402df6d81525c2da1bee2073
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:41:43 +0800

    2-6-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,19 @@
-function removeVowels(str) {
-  return str.replace(/[aeiou]/gi, "");
+function findSecondLargest(arr) {
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+  }
+
+  return secondMax;
 }
 
-// Usage example
-var inputString = "Hello World!";
-var result = removeVowels(inputString);
-console.log(result); // Output: Hll Wrld!
+// Example usage:
+const array = [5, 2, 8, 9, 1, 3];
+console.log(findSecondLargest(array)); // Output: 8
