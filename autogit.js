72414a1c1a3a407d2374d7b81db6e985a7a9fa10
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:35:00 +0800

    2-6-2024 22:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,21 @@
-function findFirstRepeatedCharacter(str) {
-  const characters = {};
-
-  for (let char of str) {
-    if (characters[char]) {
-      return char;
+function findCommonElements(array1, array2) {
+  const commonElements = [];
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      commonElements.push(array1[i]);
     }
-
-    characters[char] = true;
   }
-
-  return null; // Return null if no repeated character found
+  return commonElements;
 }
-
-// Example usage:
-const string = "hello world";
-const firstRepeatedChar = findFirstRepeatedCharacter(string);
-
-console.log(firstRepeatedChar); // Output: 'l'
+function findCommonElements(array1, array2) {
+  return array1.filter(element => array2.includes(element));
+}
+function findCommonElements(array1, array2) {
+  const set = new Set(array2);
+  return array1.filter(element => set.has(element));
+}
+function findCommonElements(array1, array2) {
+  const set1 = new Set(array1);
+  const set2 = new Set(array2);
+  return Array.from(new Set([...set1].filter(x => set2.has(x))));
+}
