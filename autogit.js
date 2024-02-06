e3455b7aae920a1e326cf36d1b2cfba3b187d950
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 20:21:24 +0800

    2-6-2024 20:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,73 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+class BinarySearchTree {
+  // ...
 
-const commonElements = array1.filter(element => array2.includes(element));
+  insert(value) {
+    const newNode = new Node(value);
 
-console.log(commonElements); // Output: [4, 5]
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
 
-const commonElements = array1.filter(element => array2.indexOf(element) !== -1);
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+}
+class BinarySearchTree {
+  // ...
 
-console.log(commonElements); // Output: [4, 5]
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      // Value not found
+      return false;
+    }
+
+    if (value === node.value) {
+      // Value found
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+
+console.log(bst.search(3)); // true
+console.log(bst.search(5)); // false
