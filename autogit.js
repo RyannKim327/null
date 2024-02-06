c0977fbe315cd2c96ff51d21a6c59853544c22e3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:42:23 +0800

    2-6-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,4 @@
-function firstNonRepeatingChar(str) {
-   var charCount = {};
-   
-   for (var i = 0; i < str.length; i++) {
-      var char = str[i];
-      if (charCount[char]) {
-         charCount[char]++;
-      } else {
-         charCount[char] = 1;
-      }
-   }
+let str = "HELLO WORLD";
+let lowercaseStr = str.toLowerCase();
 
-   for (var i = 0; i < str.length; i++) {
-      var char = str[i];
-      if (charCount[char] === 1) {
-         return char;
-      }
-   }
-   
-   return null; // No non-repeating characters found
-}
-
-// Example usage
-var inputString = "aabbccddeeffg";
-var result = firstNonRepeatingChar(inputString);
-console.log(result); // Output: "g"
+console.log(lowercaseStr); // Output: hello world
