5838503869224f2251f76b4290d538c42cdaa974
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 21:54:10 +0800

    2-6-2024 21:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,93 +1,83 @@
-class PriorityQueue {
+// Define a class for a linked list node
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// Define a class for a linked list
+class LinkedList {
   constructor() {
-    this.heap = [];
-  }
-  
-  // Other methods will be implemented here
-}
-swap(i, j) {
-  const temp = this.heap[i];
-  this.heap[i] = this.heap[j];
-  this.heap[j] = temp;
-}
-parentIndex(index) {
-  return Math.floor((index - 1) / 2);
-}
-leftChildIndex(index) {
-  return index * 2 + 1;
-}
-rightChildIndex(index) {
-  return index * 2 + 2;
-}
-hasParent(index) {
-  return this.parentIndex(index) >= 0;
-}
-hasLeftChild(index) {
-  return this.leftChildIndex(index) < this.heap.length;
-}
-hasRightChild(index) {
-  return this.rightChildIndex(index) < this.heap.length;
-}
-parent(index) {
-  return this.heap[this.parentIndex(index)];
-}
-leftChild(index) {
-  return this.heap[this.leftChildIndex(index)];
-}
-rightChild(index) {
-  return this.heap[this.rightChildIndex(index)];
-}
-peek() {
-  if (this.heap.length === 0) {
-    throw new Error('Priority queue is empty');
-  }
-  return this.heap[0];
-}
-poll() {
-  if (this.heap.length === 0) {
-    throw new Error('Priority queue is empty');
+    this.head = null;
+    this.tail = null;
   }
 
-  const item = this.heap[0];
-  this.heap[0] = this.heap[this.heap.length - 1];
-  this.heap.pop();
-  this.heapifyDown();
-  return item;
-}
-add(item) {
-  this.heap.push(item);
-  this.heapifyUp();
-}
-heapifyUp() {
-  let index = this.heap.length - 1;
-  while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-    const parentIndex = this.parentIndex(index);
-    this.swap(parentIndex, index);
-    index = parentIndex;
-  }
-}
-heapifyDown() {
-  let index = 0;
-  while (this.hasLeftChild(index)) {
-    let smallerChildIndex = this.leftChildIndex(index);
-    if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
-      smallerChildIndex = this.rightChildIndex(index);
-    }
+  // Add a new node to the end of the linked list
+  append(value) {
+    const newNode = new ListNode(value);
 
-    if (this.heap[index] < this.heap[smallerChildIndex]) {
-      break;
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-      this.swap(index, smallerChildIndex);
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    index = smallerChildIndex;
+  }
+
+  // Remove and return the value of the first node in the linked list
+  remove() {
+    if (!this.head) {
+      return null; // Empty list
+    }
+
+    const value = this.head.value;
+    this.head = this.head.next;
+
+    if (!this.head) {
+      this.tail = null; // Reset tail if list becomes empty
+    }
+
+    return value;
+  }
+
+  // Check if the linked list is empty
+  isEmpty() {
+    return this.head === null;
   }
 }
-const pq = new PriorityQueue();
-pq.add(5);
-pq.add(3);
-pq.add(8);
-pq.add(2);
-console.log(pq.poll());  // Output: 2
-console.log(pq.poll());  // Output: 3
-console.log(pq.poll());  // Output: 5
-console.log(pq.poll());  // Output: 8
+
+// Define a class for a queue using a linked list
+class Queue {
+  constructor() {
+    this.list = new LinkedList();
+  }
+
+  // Add an element to the end of the queue
+  enqueue(value) {
+    this.list.append(value);
+  }
+
+  // Remove and return the element at the front of the queue
+  dequeue() {
+    return this.list.remove();
+  }
+
+  // Check if the queue is empty
+  isEmpty() {
+    return this.list.isEmpty();
+  }
+}
+
+// Example usage
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.dequeue()); // Output: 2
+console.log(queue.isEmpty()); // Output: false
+console.log(queue.dequeue()); // Output: 3
+console.log(queue.isEmpty()); // Output: true
