98c9dfff92e99e3c6d28e4de5a1a19986d3ea47b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:23:36 +0800

    2-6-2024 22:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,48 @@
-function isPalindrome(str) {
-  // Clean and normalize the input string
-  const cleanStr = str.replace(/[\W_]/g, '').toLowerCase();
-  
-  // Reverse the cleaned string
-  const reversedStr = cleanStr.split('').reverse().join('');
-  
-  // Check if the reversed string is equal to the original cleaned string
-  return cleanStr === reversedStr;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = newNode;
+    }
+  }
+
+  getLength() {
+    let count = 0;
+    let current = this.head;
+
+    while (current) {
+      count++;
+      current = current.next;
+    }
+
+    return count;
+  }
 }
 
 // Example usage
-const input = "A man, a plan, a canal, Panama";
-console.log(isPalindrome(input));  // Output: true
+const list = new LinkedList();
+list.addNode(5);
+list.addNode(10);
+list.addNode(15);
+
+console.log(list.getLength()); // Output: 3
