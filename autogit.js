1beb5bae895cf00cfbe705c3b62056f616a5bd5e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:32:17 +0800

    2-6-2024 22:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,21 @@
-let mergedArray = array1.concat(array2);
-mergedArray.sort((a, b) => a - b);
-let length = mergedArray.length;
-let median;
-if (length % 2 === 1) {
-  median = mergedArray[Math.floor(length / 2)];
-} else {
-  let midIndex = length / 2;
-  median = (mergedArray[midIndex - 1] + mergedArray[midIndex]) / 2;
-}
-function findMedian(array1, array2) {
-  let mergedArray = array1.concat(array2);
-  mergedArray.sort((a, b) => a - b);
+function findMedianSortedArrays(nums1, nums2) {
+  const mergedArray = [...nums1, ...nums2].sort((a, b) => a - b);
+  const length = mergedArray.length;
+  const middleIndex = Math.floor(length / 2);
 
-  let length = mergedArray.length;
-  let median;
-  if (length % 2 === 1) {
-    median = mergedArray[Math.floor(length / 2)];
+  if (length % 2 === 0) {
+    // Even length case
+    const median1 = mergedArray[middleIndex - 1];
+    const median2 = mergedArray[middleIndex];
+    return (median1 + median2) / 2;
   } else {
-    let midIndex = length / 2;
-    median = (mergedArray[midIndex - 1] + mergedArray[midIndex]) / 2;
+    // Odd length case
+    return mergedArray[middleIndex];
   }
-
-  return median;
 }
 
 // Example usage
-let array1 = [1, 3, 5];
-let array2 = [2, 4, 6];
-let median = findMedian(array1, array2);
-console.log(median); // Output: 3.5
+const nums1 = [1, 3];
+const nums2 = [2, 4, 5];
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median); // Output: 3
