d214b810af3449b43b2e7900f39f340022cdb158
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:24:28 +0800

    2-6-2024 22:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,18 @@
-function computeLPS(pattern) {
-  const len = pattern.length;
-  const lps = new Array(len).fill(0);
+// Import the Axios library
+const axios = require('axios');
 
-  let i = 1;
-  let j = 0;
+// Define the API endpoint URL
+const apiUrl = 'https://api.example.com/';
 
-  while (i < len) {
-    if (pattern[i] === pattern[j]) {
-      lps[i] = j + 1;
-      i++;
-      j++;
-    } else {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
-    }
-  }
-
-  return lps;
-}
-
-function stringMatch(text, pattern) {
-  const tLen = text.length;
-  const pLen = pattern.length;
-  const lps = computeLPS(pattern);
-
-  let i = 0;
-  let j = 0;
-
-  while (i < tLen) {
-    if (text[i] === pattern[j]) {
-      i++;
-      j++;
-
-      if (j === pLen) {
-        console.log('Pattern found at index', i - j);
-        j = lps[j - 1];
-      }
-    } else {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-}
-const text = 'ABABDABACDABABCABAB';
-const pattern = 'ABABCABAB';
-
-stringMatch(text, pattern);
+// Make a GET request to the API
+axios.get(apiUrl)
+  .then(response => {
+    // Handle the response data
+    console.log('API Response:', response.data);
+  })
+  .catch(error => {
+    // Handle the error
+    console.error('Error:', error.message);
+  });
+npm install axios
+yarn add axios
