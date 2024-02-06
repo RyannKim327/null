5bc84e231ae107d91f9cddcb825521e6d74c8b72
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:12:54 +0800

    2-6-2024 22:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,32 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+function radixSort(arr) {
+  const max = findMax(arr);
+  const maxDigits = max.toString().length;
 
-console.log(lowercaseStr); // Output: "hello world"
+  for (let digit = 0; digit < maxDigits; digit++) {
+    // Create 10 buckets (0-9) for each place value
+    const buckets = Array.from({ length: 10 }, () => []);
+    
+    // Distribute elements into buckets based on the current digit
+    for (let i = 0; i < arr.length; i++) {
+      const num = arr[i];
+      const digitValue = Math.floor((num / Math.pow(10, digit)) % 10);
+      buckets[digitValue].push(num);
+    }
+    
+    // Gather elements from buckets in order
+    arr = [].concat(...buckets);
+  }
+
+  return arr;
+}
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
