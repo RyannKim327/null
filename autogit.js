27041b896f3df2e6b5a40e63d601675780bc85a8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:30:30 +0800

    2-6-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,21 @@
-let str = "42";
-let num = parseInt(str);
+function longestCommonPrefix(strings) {
+  let prefix = '';
 
-console.log(num); // Output: 42
-let str = "42";
-let num = Number(str);
+  if (strings.length === 0) {
+    return prefix;
+  }
 
-console.log(num); // Output: 42
+  strings.sort();
+
+  for (let i = 0; i < strings[0].length; i++) {
+    if (strings[0][i] === strings[strings.length - 1][i]) {
+      prefix += strings[0][i];
+    } else {
+      break;
+    }
+  }
+
+  return prefix;
+}
+const strings = ['flower', 'flow', 'flight'];
+console.log(longestCommonPrefix(strings));   // Output: 'fl'
