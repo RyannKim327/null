cad3d7e98be032404365b4e1f6f95c675b098df7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:35:08 +0800

    2-6-2024 22:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,14 @@
-function findCommonElements(array1, array2) {
-  const commonElements = [];
-  for (let i = 0; i < array1.length; i++) {
-    if (array2.includes(array1[i])) {
-      commonElements.push(array1[i]);
-    }
+function decimalToBinary(decimal) {
+  let binaryDigits = [];
+  
+  while (decimal > 0) {
+    let remainder = decimal % 2;
+    binaryDigits.unshift(remainder);
+    decimal = Math.floor(decimal / 2);
   }
-  return commonElements;
+  
+  return binaryDigits.join('');
 }
-function findCommonElements(array1, array2) {
-  return array1.filter(element => array2.includes(element));
-}
-function findCommonElements(array1, array2) {
-  const set = new Set(array2);
-  return array1.filter(element => set.has(element));
-}
-function findCommonElements(array1, array2) {
-  const set1 = new Set(array1);
-  const set2 = new Set(array2);
-  return Array.from(new Set([...set1].filter(x => set2.has(x))));
-}
+let decimalNumber = 42;
+let binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber);  // Output: "101010"
