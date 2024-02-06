1f3836598eadbc57fe6fbeb1069b390227c1d888
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:38:07 +0800

    2-6-2024 22:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,21 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+function selectionSort(array) {
+  const length = array.length;
+
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [array[i], array[minIndex]] = [array[minIndex], array[i]];
+    }
   }
+
+  return array;
 }
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-
-  while (current) {
-    const next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  return prev;
-}
-// Create the linked list: 1 -> 2 -> 3 -> 4 -> 5
-const head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-// Reverse the linked list
-const reversedHead = reverseLinkedList(head);
-
-// Print the reversed linked list
-let current = reversedHead;
-while (current) {
-  console.log(current.value);
-  current = current.next;
-}
-5
-4
-3
-2
-1
+const myArray = [9, 5, 1, 4, 3, 2, 8, 7, 6];
+console.log(selectionSort(myArray)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
