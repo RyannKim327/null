0c692b0cfae71c197eafe531fdba05798df583d6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:23:07 +0800

    2-6-2024 22:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,85 +1,22 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+function countOccurrences(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str.charAt(i) === char) {
+      count++;
+    }
   }
+  return count;
 }
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
 
-  // Insert a value into the tree
-  insert(value) {
-    const newNode = new Node(value);
-
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  // Search for a value in the tree
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  searchNode(node, value) {
-    if (node === null) {
-      return false;
-    }
-
-    if (value === node.value) {
-      return true;
-    }
-
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    }
-
-    return this.searchNode(node.right, value);
-  }
-
-  // Perform a tree traversal (e.g., inorder, preorder, postorder)
-  inorder() {
-    this.inorderTraversal(this.root);
-  }
-
-  inorderTraversal(node) {
-    if (node !== null) {
-      this.inorderTraversal(node.left);
-      console.log(node.value);
-      this.inorderTraversal(node.right);
-    }
-  }
+const myString = "Hello world!";
+const targetChar = "o";
+const occurrences = countOccurrences(myString, targetChar);
+console.log(`The character "${targetChar}" occurs ${occurrences} times in "${myString}".`);
+function countOccurrences(str, char) {
+  return str.split(char).length - 1;
 }
-const tree = new BinaryTree();
 
-tree.insert(5);
-tree.insert(3);
-tree.insert(7);
-tree.insert(2);
-tree.insert(4);
-
-console.log(tree.search(4)); // Output: true
-console.log(tree.search(6)); // Output: false
-
-tree.inorder(); // Output: 2 3 4 5 7
+const myString = "Hello world!";
+const targetChar = "o";
+const occurrences = countOccurrences(myString, targetChar);
+console.log(`The character "${targetChar}" occurs ${occurrences} times in "${myString}".`);
