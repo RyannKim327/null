e11876bdbf312f93f95ecdfb204a65ee57e19cd0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:16:18 +0800

    2-6-2024 22:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,15 @@
-let str = "42";
-let num = parseInt(str);
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  str = str.replace(/[\W_]/g, '').toLowerCase();
 
-console.log(num); // Output: 42
-let str = "42";
-let num = Number(str);
+  // Reverse the modified string
+  const reversedStr = str.split('').reverse().join('');
 
-console.log(num); // Output: 42
+  // Compare reversed string with the original string
+  return str === reversedStr;
+}
+
+// Example usage:
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+console.log(isPalindrome("race car")); // Output: true
+console.log(isPalindrome("hello world")); // Output: false
