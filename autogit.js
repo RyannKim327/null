6f5bf844889e04783d79222a4e3f6fb841f940c6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:33:20 +0800

    2-6-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,31 @@
-const axios = require('axios');
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
 
-axios.get('https://api.example.com/data')
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true; // Cycle detected
+    }
+  }
+
+  return false; // No cycle found
+}
+const hasCycle = (head) => {
+  // Implementation of the algorithm
+}
+
+const linkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: { ... } // Can point to any other node in the linked list, creating a cycle
+    }
+  }
+};
+
+console.log(hasCycle(linkedList)); // Outputs: true or false
