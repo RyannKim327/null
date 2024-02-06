2004cbc494acce39f94752c755ff305d98883ace
RyannKim327 <rksesgundo123@gmail.com>
2024-02-06 22:33:31 +0800

    2-6-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,8 @@
-function hasCycle(head) {
-  let slow = head;
-  let fast = head;
+let array = [1, 2, 3, 4, 5];
+console.log("Original Array:", array);
 
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
+array.reverse();
 
-    if (slow === fast) {
-      return true; // Cycle detected
-    }
-  }
-
-  return false; // No cycle found
-}
-const hasCycle = (head) => {
-  // Implementation of the algorithm
-}
-
-const linkedList = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: { ... } // Can point to any other node in the linked list, creating a cycle
-    }
-  }
-};
-
-console.log(hasCycle(linkedList)); // Outputs: true or false
+console.log("Reversed Array:", array);
+Original Array: [1, 2, 3, 4, 5]
+Reversed Array: [5, 4, 3, 2, 1]
