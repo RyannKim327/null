826a5ad5618696b4982d76800944b4443c061615
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:41:45 +0800

    2-8-2024 4:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,8 @@
-function longestCommonPrefix(strings) {
-  let prefix = '';
-
-  if (strings === null || strings.length === 0) {
-    return prefix;
-  }
-
-  for (let i = 0; i < strings[0].length; i++) {
-    const char = strings[0][i];
-
-    for (let j = 1; j < strings.length; j++) {
-      if (i >= strings[j].length || strings[j][i] !== char) {
-        return prefix;
-      }
-    }
-
-    prefix += char;
-  }
-
-  return prefix;
-}
-const strings = ['apple', 'app', 'apricot'];
-console.log(longestCommonPrefix(strings)); // Output: 'ap'
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
