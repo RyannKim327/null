c4e08072f1e0f08bc1edbc360b45088487cfafd7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:52:10 +0800

    2-8-2024 4:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,67 +1,17 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
+function countOccurrences(str, char) {
+  let count = 0;
 
-  enqueue(value) {
-    const newNode = new Node(value);
-
-    if (this.size === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
     }
-
-    this.size++;
   }
 
-  dequeue() {
-    if (this.size === 0) {
-      return null;
-    }
-
-    const removedNode = this.head;
-
-    if (this.size === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.next;
-    }
-
-    this.size--;
-    return removedNode.value;
-  }
+  return count;
 }
-class Queue {
-  constructor() {
-    this.linkedList = new LinkedList();
-  }
 
-  enqueue(value) {
-    this.linkedList.enqueue(value);
-  }
+const myString = "Hello, World!";
+const myChar = "l";
+const occurrences = countOccurrences(myString, myChar);
 
-  dequeue() {
-    return this.linkedList.dequeue();
-  }
-}
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-console.log(queue.dequeue()); // Output: 3
-console.log(queue.dequeue()); // Output: null (queue is empty)
+console.log(`The character "${myChar}" appears ${occurrences} times in "${myString}".`);
