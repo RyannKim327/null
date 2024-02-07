40d2129c92787d6f7e7046c3f19231b4a8288500
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:05:22 +0800

    2-8-2024 5:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,39 @@
-function isPalindrome(head) {
-  if (!head || !head.next) {
-    // An empty list or a single node is considered a palindrome
-    return true;
-  }
-
-  let slow = head;
-  let fast = head;
-  let prev = null;
-
-  // Use the two-pointer technique to find the middle node
-  while (fast && fast.next) {
-    fast = fast.next.next;
-
-    // Reverse the first half of the remaining linked list
-    const next = slow.next;
-    slow.next = prev;
-    prev = slow;
-    slow = next;
-  }
-
-  // If the linked list has an odd number of nodes, skip the middle node
-  if (fast) {
-    slow = slow.next;
-  }
-
-  // Compare the first and second halves of the linked list
-  while (prev) {
-    if (prev.val !== slow.val) {
-      // The values don't match, so it's not a palindrome
-      return false;
+function findFirstNonRepeatingChar(string) {
+    // Implementation goes here
+}
+function findFirstNonRepeatingChar(string) {
+    const charCount = {};
+    // Rest of the code
+}
+function findFirstNonRepeatingChar(string) {
+    const charCount = {};
+    for (let char of string) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
     }
-    prev = prev.next;
-    slow = slow.next;
-  }
+    // Rest of the code
+}
+function findFirstNonRepeatingChar(string) {
+    const charCount = {};
+    for (let char of string) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
 
-  // All the values match, so it's a palindrome
-  return true;
+    for (let char of string) {
+        if (charCount[char] === 1) {
+            return char;
+        }
+    }
+
+    return null; // If no non-repeating character found
 }
-class ListNode {
-  constructor(val = 0, next = null) {
-    this.val = val;
-    this.next = next;
-  }
-}
+const inputString = "abbacdeb";
+const firstNonRepeatingChar = findFirstNonRepeatingChar(inputString);
+console.log(firstNonRepeatingChar); // Output: 'c'
