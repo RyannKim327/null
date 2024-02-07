6073912f04ce0b146d3dd2ad09276a07a7b4ead3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:32:40 +0800

    2-8-2024 5:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,18 @@
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-let index = array.indexOf(elementToRemove);
-
-if (index > -1) {
-  array.splice(index, 1);
+function selectionSort(arr) {
+  const length = arr.length;
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+  }
+  return arr;
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-let filteredArray = array.filter((element) => element !== elementToRemove);
-
-console.log(filteredArray); // Output: [1, 2, 4, 5]
+// Example usage:
+const unsortedArray = [64, 25, 12, 22, 11];
+const sortedArray = selectionSort(unsortedArray);
+console.log(sortedArray);
