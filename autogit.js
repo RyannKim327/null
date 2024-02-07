1379852a6ec6b1769dff4678f2948318d2d41288
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:02:04 +0800

    2-8-2024 5:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,100 +1,24 @@
-function findKthSmallest(arr, k) {
-    arr.sort((a, b) => a - b);
-    return arr[k - 1];
-}
+function bubbleSort(arr) {
+    var len = arr.length;
+    var swapped;
 
-const array = [5, 8, 2, 7, 1, 6, 9];
-const k = 3;
-const kthSmallest = findKthSmallest(array, k);
-console.log(kthSmallest); // Output: 3
-function swap(arr, i, j) {
-    const temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-
-function minHeapify(arr, i, heapSize) {
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-    let smallest = i;
-
-    if (left < heapSize && arr[left] < arr[smallest]) {
-        smallest = left;
-    }
-
-    if (right < heapSize && arr[right] < arr[smallest]) {
-        smallest = right;
-    }
-
-    if (smallest !== i) {
-        swap(arr, i, smallest);
-        minHeapify(arr, smallest, heapSize);
-    }
-}
-
-function buildMinHeap(arr) {
-    const n = arr.length;
-    const startIndex = Math.floor(n / 2) - 1;
-
-    for (let i = startIndex; i >= 0; i--) {
-        minHeapify(arr, i, n);
-    }
-}
-
-function findKthSmallest(arr, k) {
-    buildMinHeap(arr);
-    let heapSize = arr.length;
-
-    for (let i = 0; i < k - 1; i++) {
-        swap(arr, 0, heapSize - 1);
-        heapSize--;
-        minHeapify(arr, 0, heapSize);
-    }
-
-    return arr[0];
-}
-
-const array = [5, 8, 2, 7, 1, 6, 9];
-const k = 3;
-const kthSmallest = findKthSmallest(array, k);
-console.log(kthSmallest); // Output: 3
-function partition(arr, left, right) {
-    const pivotValue = arr[right];
-    let i = left;
-
-    for (let j = left; j < right; j++) {
-        if (arr[j] < pivotValue) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-            i++;
+    do {
+        swapped = false;
+        for (var i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
         }
-    }
+        len--;
+    } while (swapped);
 
-    [arr[i], arr[right]] = [arr[right], arr[i]];
-    return i;
+    return arr;
 }
 
-function quickselect(arr, left, right, k) {
-    if (left === right) {
-        return arr[left];
-    }
-
-    const pivotIndex = Math.floor(Math.random() * (right - left + 1)) + left;
-    const partitionIndex = partition(arr, left, right);
-
-    if (k === partitionIndex) {
-        return arr[partitionIndex];
-    } else if (k < partitionIndex) {
-        return quickselect(arr, left, partitionIndex - 1, k);
-    } else {
-        return quickselect(arr, partitionIndex + 1, right, k);
-    }
-}
-
-function findKthSmallest(arr, k) {
-    return quickselect(arr, 0, arr.length - 1, k - 1);
-}
-
-const array = [5, 8, 2, 7, 1, 6, 9];
-const k = 3;
-const kthSmallest = findKthSmallest(array, k);
-console.log(kthSmallest); // Output: 3
+// Example usage:
+var array = [8, 5, 2, 9, 1, 4, 3, 7, 6];
+console.log("Before sorting: " + array);
+console.log("After sorting: " + bubbleSort(array));
