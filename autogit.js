3dd23a1c59da920684e7789668b47906b91750e1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:11:57 +0800

    2-8-2024 5:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,31 @@
-let str = "123";
-let num = parseInt(str);
-console.log(num); // Output: 123
-let str = "456";
-let num = Number(str);
-console.log(num); // Output: 456
+function findMajorityElement(arr) {
+  const countMap = {};
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (element in countMap) {
+      countMap[element]++;
+    } else {
+      countMap[element] = 1;
+    }
+  }
+
+  let majorityElement = null;
+  let majorityCount = 0;
+  for (const element in countMap) {
+    if (countMap[element] > majorityCount) {
+      majorityElement = element;
+      majorityCount = countMap[element];
+    }
+  }
+
+  if (majorityCount > arr.length / 2) {
+    return majorityElement;
+  } else {
+    return null;
+  }
+}
+
+// Example usage
+const array = [1, 2, 3, 3, 3, 3, 4, 3, 5];
+const majorityElement = findMajorityElement(array);
+console.log(majorityElement);
