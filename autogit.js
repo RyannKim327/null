bdb48c22140d0f4d65bd25c8005ce366eb18dc54
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:45:28 +0800

    2-8-2024 4:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,14 @@
-function buildPrefixTable(pattern) {
-  const prefixTable = [0];
-  let prefixLength = 0;
-  let i = 1;
-
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[prefixLength]) {
-      prefixLength++;
-      prefixTable[i] = prefixLength;
-      i++;
-    } else {
-      if (prefixLength !== 0) {
-        prefixLength = prefixTable[prefixLength - 1];
-      } else {
-        prefixTable[i] = 0;
-        i++;
-      }
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
     }
-  }
-
-  return prefixTable;
+    return count;
 }
-function kmpSearch(text, pattern) {
-  const prefixTable = buildPrefixTable(pattern);
-  const indices = [];
+const myString = "Hello World";
+const myCharacter = "o";
 
-  let textIndex = 0;
-  let patternIndex = 0;
-
-  while (textIndex < text.length) {
-    if (pattern[patternIndex] === text[textIndex]) {
-      textIndex++;
-      patternIndex++;
-    }
-
-    if (patternIndex === pattern.length) {
-      indices.push(textIndex - patternIndex);
-      patternIndex = prefixTable[patternIndex - 1];
-    } else if (
-      textIndex < text.length &&
-      pattern[patternIndex] !== text[textIndex]
-    ) {
-      if (patternIndex !== 0) {
-        patternIndex = prefixTable[patternIndex - 1];
-      } else {
-        textIndex++;
-      }
-    }
-  }
-
-  return indices;
-}
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const indices = kmpSearch(text, pattern);
-console.log(indices);
+const occurrenceCount = countOccurrences(myString, myCharacter);
+console.log(occurrenceCount);  // Output: 2
