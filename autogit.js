52b2fe5a1f7f31e5f7ff4c31ed953281bac32641
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:33:51 +0800

    2-8-2024 5:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,4 @@
-function selectionSort(array) {
-  const length = array.length;
-  for (let i = 0; i < length - 1; i++) {
-    let min = array[i];
-    let minIndex = i;
-    for (let j = i + 1; j < length; j++) {
-      if (array[j] < min) {
-        min = array[j];
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      [array[i], array[minIndex]] = [array[minIndex], array[i]];
-    }
-  }
-  return array;
-}
-const unsortedArray = [5, 2, 9, 1, 7, 3];
-const sortedArray = selectionSort(unsortedArray);
-console.log(sortedArray);
-// Output: [1, 2, 3, 5, 7, 9]
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+
+console.log(lowercaseStr);  // Output: hello world
