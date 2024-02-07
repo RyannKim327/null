5c3c87b1a5b14dae5c3746f09bfa39a4662adf2d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:51:20 +0800

    2-8-2024 4:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,4 @@
-const numbers = [5, 2, 9, 1, 7];
+let str = "   Hello,    World!   ";
+let newStr = str.replace(/\s/g, "");
 
-// Sort the array in ascending order
-numbers.sort((a, b) => a - b);
-console.log(numbers); // Output: [1, 2, 5, 7, 9]
-
-// Sort the array in descending order
-numbers.sort((a, b) => b - a);
-console.log(numbers); // Output: [9, 7, 5, 2, 1]
-const numbers = [5, 2, 9, 1, 7];
-
-// Create a copy of the original array
-const sortedNumbers = [...numbers].sort((a, b) => a - b);
-
-console.log(numbers); // Output: [5, 2, 9, 1, 7]
-console.log(sortedNumbers); // Output: [1, 2, 5, 7, 9]
+console.log(newStr); // Output: "Hello,World!"
