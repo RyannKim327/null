daa28b1d16cbd956b3e8e85896565e16aa10f13d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:41:27 +0800

    2-8-2024 4:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,12 @@
-function largestPrimeFactor(number) {
-   // your logic goes here
+Area = (base * height) / 2
+// Function to calculate the area of a triangle
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
-let factor = 2;
-while (factor <= number) {
-   // your logic goes here
-}
-while (factor <= number) {
-   if (number % factor === 0) {
-      number /= factor;
-   } else {
-      factor++;
-   }
-}
-return factor;
-function largestPrimeFactor(number) {
-   let factor = 2;
 
-   while (factor <= number) {
-      if (number % factor === 0) {
-         number /= factor;
-      } else {
-         factor++;
-      }
-   }
-
-   return factor;
-}
-console.log(largestPrimeFactor(13195));  // Output: 29
+// Example usage
+var base = 5;
+var height = 7;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
