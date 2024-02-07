131a9655acbce07348aca05715eceaa1d6f14a2e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:59:57 +0800

    2-8-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,45 @@
-const numbers = [1, 5, 2, 9, 3, 7];
-const max = Math.max(...numbers);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-console.log(max); // Output: 9
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+}
+
+// Example usage:
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+
+console.log(list.getLength()); // Output: 4
