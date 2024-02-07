826c53d048a479fc5f4632658d50342efff3ddd2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:06:47 +0800

    2-8-2024 5:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,17 @@
-let str = "42";
-let num = parseInt(str);
-
-console.log(num);  // Output: 42
-console.log(typeof num);  // Output: number
+const arr = [1, 2, 3, 2, 4, 3, 5];
+const uniqueArray = arr.filter((value, index) => arr.indexOf(value) === index);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 2, 4, 3, 5];
+const uniqueArray = Array.from(new Set(arr));
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 2, 4, 3, 5];
+const uniqueArray = [...new Set(arr)];
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const arr = [1, 2, 3, 2, 4, 3, 5];
+const uniqueArray = [];
+for (const element of arr) {
+  if (!uniqueArray.includes(element)) {
+    uniqueArray.push(element);
+  }
+}
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
