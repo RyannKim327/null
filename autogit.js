4540f27ec073ba49b932048eadb22d376036093a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:31:56 +0800

    2-8-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,120 @@
-function KMP(text, pattern) {
-  const n = text.length;
-  const m = pattern.length;
-  const lps = new Array(m).fill(0);
-
-  // Preprocessing
-  let len = 0; // Length of the previous longest prefix suffix
-  let i = 1;
-  while (i < m) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-
-  // Matching
-  let j = 0; // Index in pattern
-  let i = 0; // Index in text
-  const matches = [];
-  while (i < n) {
-    if (text[i] === pattern[j]) {
-      i++;
-      j++;
-    }
-
-    if (j === m) {
-      matches.push(i - j);
-      j = lps[j - 1];
-    } else if (i < n && text[i] !== pattern[j]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-
-  return matches;
 }
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value === node.value) {
+      return true;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else {
+      return this.searchNode(node.right, value);
+    }
+  }
+
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      node.right = this.removeNode(node.right, minNode.value);
+      return node;
+    }
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+}
+// Create a new binary tree
+const tree = new BinaryTree();
+
+// Insert nodes
+tree.insert(10);
+tree.insert(7);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+
+// Search for a value
+console.log(tree.search(7));  // Output: true
+console.log(tree.search(12)); // Output: false
+
+// Remove a node
+tree.remove(7);
+console.log(tree.search(7));  // Output: false
