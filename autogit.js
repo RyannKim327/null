dee425dde344324d2e2a1d0b9976354281d7a84c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:43:45 +0800

    2-8-2024 4:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,6 @@
-const graph = {};
-function addEdge(node1, node2) {
-  if (!graph[node1]) graph[node1] = [];
-  if (!graph[node2]) graph[node2] = [];
-  graph[node1].push(node2);
-  graph[node2].push(node1);
-}
-function depthFirstSearch(startNode) {
-  const visited = {};
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
 
-  function dfs(node) {
-    visited[node] = true;
-    console.log(node); // Print the visited node
+const commonElements = array1.filter(element => array2.includes(element));
 
-    for (const neighbor of graph[node]) {
-      if (!visited[neighbor]) {
-        dfs(neighbor);
-      }
-    }
-  }
-
-  dfs(startNode);
-}
-addEdge(1, 2);
-addEdge(1, 3);
-addEdge(2, 4);
-addEdge(2, 5);
-addEdge(3, 6);
-addEdge(3, 7);
-depthFirstSearch(1);
+console.log(commonElements); // Output: [4, 5]
