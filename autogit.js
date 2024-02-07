7354e4a0179b7efc2798f57dd42706d29bfc3137
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:51:03 +0800

    2-8-2024 4:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,39 @@
-let str = "42";
-let num = parseInt(str);
+function findLongestIncreasingSubsequence(arr) {
+  const n = arr.length;
+  const dp = new Array(n).fill(1);
 
-console.log(num); // Output: 42
-let str = "42";
-let num = Number(str);
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+        dp[i] = dp[j] + 1;
+      }
+    }
+  }
 
-console.log(num); // Output: 42
+  let maxIndex = 0;
+  for (let i = 1; i < n; i++) {
+    if (dp[i] > dp[maxIndex]) {
+      maxIndex = i;
+    }
+  }
+
+  // Construct the longest increasing subsequence
+  const lis = [];
+  let index = dp[maxIndex] - 1;
+  lis[index] = arr[maxIndex];
+  index--;
+  for (let i = maxIndex - 1; i >= 0; i--) {
+    if (arr[i] < arr[maxIndex] && dp[i] === dp[maxIndex] - 1) {
+      lis[index] = arr[i];
+      index--;
+      maxIndex = i;
+    }
+  }
+
+  return lis;
+}
+
+// Example usage:
+const array = [10, 22, 9, 33, 21, 50, 41, 60, 80];
+const lis = findLongestIncreasingSubsequence(array);
+console.log("Longest Increasing Subsequence:", lis);
