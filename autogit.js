3d6e2d9cc39044003aa6d6e989004b654bd0d0ce
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:48:24 +0800

    2-8-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,49 @@
-function fibSeries(n) {
-  let fib = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-  return fib;
 }
-function fibonacciSearch(arr, key) {
-  let fib = fibSeries(30); // Generate Fibonacci series up to desired length
 
-  let offset = -1;
-  let fibIdx = fib.length - 1;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-  while (fibIdx > 1) {
-    // Find the smallest Fibonacci number equal to or greater than the array size
-    let i = Math.min(offset + fib[fibIdx - 2], arr.length - 1);
+  append(data) {
+    let newNode = new Node(data);
 
-    if (arr[i] < key) {
-      // Move offset to the right
-      fibIdx = fibIdx - 1;
-      offset = i;
-    } else if (arr[i] > key) {
-      // Move offset to the left
-      fibIdx = fibIdx - 2;
-    } else {
-      // Found the element
-      return i;
+    if (!this.head) {
+      this.head = newNode;
+      return;
     }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
   }
 
-  // Check the last element
-  if (arr[offset + 1] === key) {
-    return offset + 1;
+  getLength() {
+    let count = 0;
+    let current = this.head;
+
+    while (current) {
+      count++;
+      current = current.next;
+    }
+
+    return count;
   }
-
-  // Element not found
-  return -1;
 }
-// Example usage
-let arr = [2, 4, 7, 12, 16, 23, 38, 56, 72, 91];
-let key = 23;
 
-let index = fibonacciSearch(arr, key);
-if (index !== -1) {
-  console.log("Element found at index:", index);
-} else {
-  console.log("Element not found in the array.");
-}
+// Usage example
+let linkedList = new LinkedList();
+linkedList.append(5);
+linkedList.append(10);
+linkedList.append(15);
+linkedList.append(20);
+
+console.log('Length:', linkedList.getLength()); // Output: Length: 4
