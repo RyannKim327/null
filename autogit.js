9b6bae27d0e57182b69d40d72719684e824ca191
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:22:25 +0800

    2-8-2024 5:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,8 @@
-function LinkedListNode(value) {
-  this.value = value;
-  this.next = null;
-}
-
-function LinkedList() {
-  this.head = null;
-}
-
-LinkedList.prototype.addNode = function(value) {
-  var node = new LinkedListNode(value);
-  
-  if (!this.head) {
-    this.head = node;
-  } else {
-    var current = this.head;
-    
-    while (current.next) {
-      current = current.next;
-    }
-    
-    current.next = node;
-  }
-}
-
-LinkedList.prototype.getLength = function() {
-  var current = this.head;
-  var count = 0;
-  
-  while (current) {
-    count++;
-    current = current.next;
-  }
-  
-  return count;
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
 }
 
 // Example usage
-var list = new LinkedList();
-list.addNode(1);
-list.addNode(2);
-list.addNode(3);
-
-console.log(list.getLength()); // Output: 3
+const input = 'Hello, World!';
+const result = removeVowels(input);
+console.log(result); // Output: Hll, Wrld!
