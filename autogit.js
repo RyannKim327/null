b288f4b38aaebd05be028a5cb2ef4fb91f1f2815
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:57:43 +0800

    2-8-2024 4:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,3 @@
-// Prompt the user for input
-var input = prompt("Enter a number:");
-
-// Convert the input to a number
-var number = parseFloat(input);
-
-// Check if the input is a valid number
-if (!isNaN(number)) {
-  // Print the square of the number
-  var square = number * number;
-  console.log("The square of " + number + " is " + square);
-} else {
-  console.log("Invalid input. Please enter a number.");
-}
+const array = [1, 5, 3, 9, 2];
+const max = Math.max(...array);
+console.log(max); // Output: 9
