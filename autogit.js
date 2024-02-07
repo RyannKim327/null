48b106c630cb1099c000ea9081902c0e68b1118c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:14:54 +0800

    2-8-2024 5:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,23 @@
-function badCharacterHeuristic(pattern) {
-  const table = {};
-
-  for (let i = 0; i < pattern.length - 1; i++) {
-    table[pattern[i]] = i;
+function isPrime(number) {
+  // Check if the number is less than 2
+  if (number < 2) {
+    return false;
   }
 
-  return table;
-}
-
-function goodSuffixHeuristic(pattern) {
-  const m = pattern.length;
-  const suff = new Array(m + 1).fill(0);
-  const prefix = new Array(m + 1).fill(false);
-
-  for (let i = 0; i < m; i++) {
-    suff[i] = 0;
-    prefix[i] = false;
-  }
-
-  let g = m - 1;
-  let f = 0;
-
-  for (let i = m - 1; i >= 0; i--) {
-    if (i > g && suff[i + m - 1 - f] !== i - g) {
-      suff[i] = Math.min(suff[i + m - 1 - f], i - g);
-    } else {
-      if (i < g) {
-        g = i;
-      }
-      f = i;
-
-      while (g >= 0 && pattern[g] === pattern[g + m - 1 - f]) {
-        g--;
-      }
-
-      suff[i] = f - g;
+  // Iterate from 2 to the square root of the number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    // If the number is divisible by any other number, it is not prime
+    if (number % i === 0) {
+      return false;
     }
   }
 
-  for (let i = 0; i < m - 1; i++) {
-    suff[m - 1 - suff[i]] = i;
-  }
-
-  return { suffix: suff, prefix: prefix };
+  // If no factors are found, the number is prime
+  return true;
 }
-function boyerMooreSearch(text, pattern) {
-  const n = text.length;
-  const m = pattern.length;
 
-  const table = badCharacterHeuristic(pattern);
-  const { suffix, prefix } = goodSuffixHeuristic(pattern);
-
-  let i = 0; // Starting index of the pattern in text
-
-  while (i <= n - m) {
-    let j = m - 1; // Index of the last character in the pattern
-
-    while (j >= 0 && pattern[j] === text[i + j]) {
-      j--;
-    }
-
-    if (j < 0) {
-      // Pattern found
-      return i;
-    } else {
-      // Shift pattern based on bad character and good suffix heuristics
-      const badCharacterShift = j - table[text[i + j]] || 1;
-      const goodSuffixShift = suffix[j + 1] || m - prefix[j];
-
-      i += Math.max(badCharacterShift, goodSuffixShift);
-    }
-  }
-
-  // Pattern not found
-  return -1;
-}
-const text = "Lorem ipsum dolor sit amet consectetur adipiscing elit";
-const pattern = "consectetur";
-
-const result = boyerMooreSearch(text, pattern);
-console.log(`Pattern found at index ${result}`);
+// Test the function
+console.log(isPrime(7));     // Output: true
+console.log(isPrime(10));    // Output: false
+console.log(isPrime(17));    // Output: true
+console.log(isPrime(20));    // Output: false
