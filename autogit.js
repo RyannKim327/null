7ccf0a08fce5d3f88b8987aa8c705fcdcc22c9eb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:59:48 +0800

    2-8-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,4 @@
-function findMajorityElement(nums) {
-  let majorityElement = nums[0];
-  let count = 1;
+const numbers = [1, 5, 2, 9, 3, 7];
+const max = Math.max(...numbers);
 
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] === majorityElement) {
-      count++;
-    } else {
-      count--;
-    }
-
-    if (count === 0) {
-      majorityElement = nums[i];
-      count = 1;
-    }
-  }
-
-  // Verify if the majority element occurs more than n/2 times
-  count = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === majorityElement) {
-      count++;
-    }
-
-    if (count > nums.length / 2) {
-      return majorityElement;
-    }
-  }
-
-  // If there is no majority element
-  return null;
-}
-
-// Example usage
-const nums = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(nums);
-console.log(majorityElement); // Output: 2
+console.log(max); // Output: 9
