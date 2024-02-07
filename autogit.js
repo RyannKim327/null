789518d672c0a78630ca157f32e987559ca42fb0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:24:03 +0800

    2-8-2024 5:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,46 @@
-function largestPrimeFactor(number) {
-  let factor = 2;
-  
-  while (factor <= number) {
-    if (number % factor === 0) {
-      number /= factor;
+function mergeArrays(arr1, arr2) {
+  let merged = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      merged.push(arr1[i]);
+      i++;
     } else {
-      factor++;
+      merged.push(arr2[j]);
+      j++;
     }
   }
-  
-  return factor;
-}
 
-// Example usage
-console.log(largestPrimeFactor(600851475143));  // Output: 6857
+  while (i < arr1.length) {
+    merged.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    merged.push(arr2[j]);
+    j++;
+  }
+
+  return merged;
+}
+function findMedian(nums) {
+  const n = nums.length;
+
+  if (n % 2 === 0) {
+    // If the length is even, average the two middle elements
+    const mid = n / 2;
+    return (nums[mid - 1] + nums[mid]) / 2;
+  } else {
+    // If the length is odd, return the middle element
+    return nums[Math.floor(n / 2)];
+  }
+}
+const arr1 = [1, 3, 5];
+const arr2 = [2, 4, 6];
+
+const merged = mergeArrays(arr1, arr2);
+const median = findMedian(merged);
+
+console.log(median); // Output: 3.5
