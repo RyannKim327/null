77cd80f4d672dd7340e87c74b003838ee85c00d5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:15:03 +0800

    2-8-2024 5:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,14 @@
-function isPrime(number) {
-  // Check if the number is less than 2
-  if (number < 2) {
-    return false;
+function findMiddleElement(head) {
+  let slowPtr = head;  // Pointer that moves one node at a time
+  let fastPtr = head;  // Pointer that moves two nodes at a time
+
+  while (fastPtr !== null && fastPtr.next !== null) {
+    slowPtr = slowPtr.next;         // Move slow pointer by one step
+    fastPtr = fastPtr.next.next;    // Move fast pointer by two steps
   }
 
-  // Iterate from 2 to the square root of the number
-  for (let i = 2; i <= Math.sqrt(number); i++) {
-    // If the number is divisible by any other number, it is not prime
-    if (number % i === 0) {
-      return false;
-    }
-  }
-
-  // If no factors are found, the number is prime
-  return true;
+  return slowPtr;  // Return the middle element
 }
-
-// Test the function
-console.log(isPrime(7));     // Output: true
-console.log(isPrime(10));    // Output: false
-console.log(isPrime(17));    // Output: true
-console.log(isPrime(20));    // Output: false
+const linkedList = { value: 1, next: { value: 2, next: { value: 3, next: { value: 4, next: { value: 5, next: null } } } } };
+const middleNode = findMiddleElement(linkedList);
+console.log(middleNode);  // Output: { value: 3, next: { value: 4, next: { value: 5, next: null } } }
