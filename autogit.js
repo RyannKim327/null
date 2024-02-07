6003dc0672a7c27af3b91a62ac6edb96fffb12d8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:20:11 +0800

    2-8-2024 5:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,18 @@
-function mergeSortIterative(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
+function findSecondLargest(array) {
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
 
-  // Create subarrays of size 1
-  let subarrays = arr.map((element) => [element]);
-
-  // Merge pairs of subarrays until there is only one sorted array
-  while (subarrays.length > 1) {
-    let mergedSubarrays = [];
-
-    for (let i = 0; i < subarrays.length; i += 2) {
-      if (i + 1 < subarrays.length) {
-        // Merge two subarrays
-        let merged = merge(subarrays[i], subarrays[i + 1]);
-        mergedSubarrays.push(merged);
-      } else {
-        // If there is an odd number of subarrays, pass it through
-        mergedSubarrays.push(subarrays[i]);
-      }
-    }
-
-    subarrays = mergedSubarrays;
-  }
-
-  return subarrays[0];
-}
-
-function merge(leftArr, rightArr) {
-  let mergedArr = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-
-  // Merge elements from the two subarrays in sorted order
-  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
-    if (leftArr[leftIndex] < rightArr[rightIndex]) {
-      mergedArr.push(leftArr[leftIndex]);
-      leftIndex++;
-    } else {
-      mergedArr.push(rightArr[rightIndex]);
-      rightIndex++;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+      secondLargest = largest;
+      largest = array[i];
+    } else if (array[i] > secondLargest && array[i] < largest) {
+      secondLargest = array[i];
     }
   }
 
-  // Copy the remaining elements from the left subarray
-  while (leftIndex < leftArr.length) {
-    mergedArr.push(leftArr[leftIndex]);
-    leftIndex++;
-  }
-
-  // Copy the remaining elements from the right subarray
-  while (rightIndex < rightArr.length) {
-    mergedArr.push(rightArr[rightIndex]);
-    rightIndex++;
-  }
-
-  return mergedArr;
+  return secondLargest;
 }
-let arr = [5, 2, 9, 1, 7, 6, 3];
-let sortedArr = mergeSortIterative(arr);
-console.log(sortedArr); // [1, 2, 3, 5, 6, 7, 9]
+const numbers = [5, 2, 9, 1, 7, 3];
+const secondLargest = findSecondLargest(numbers);
+console.log(secondLargest); // Output: 7
