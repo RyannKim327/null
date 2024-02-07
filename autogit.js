4a2ca9d8bd279d75fd2e05e568f772059e7dacb4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:02:15 +0800

    2-8-2024 5:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,44 @@
-function bubbleSort(arr) {
-    var len = arr.length;
-    var swapped;
+function longestCommonSubsequence(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
 
-    do {
-        swapped = false;
-        for (var i = 0; i < len - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                var temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
-            }
-        }
-        len--;
-    } while (swapped);
+  // Create a matrix to store the lengths of longest common subsequences
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0));
 
-    return arr;
+  // Build the matrix using dynamic programming
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  // Retrieve the longest common subsequence
+  const longestSubsequence = [];
+  let i = m,
+    j = n;
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      longestSubsequence.unshift(str1[i - 1]);
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return longestSubsequence.join('');
 }
 
 // Example usage:
-var array = [8, 5, 2, 9, 1, 4, 3, 7, 6];
-console.log("Before sorting: " + array);
-console.log("After sorting: " + bubbleSort(array));
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: ADH
