36b5de7586a03710bdde15d605210e69bc925ab8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:23:01 +0800

    2-8-2024 5:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,9 @@
-let str = "Hello, World!";
-let lowercaseStr = str.toLowerCase();
+const array = [1, 6, 2, 9, 4, 7, 3];
 
-console.log(lowercaseStr);  // Output: hello, world!
+// Using spread operator
+const max1 = Math.max(...array);
+console.log(max1); // Output: 9
+
+// Using apply() method
+const max2 = Math.max.apply(null, array);
+console.log(max2); // Output: 9
