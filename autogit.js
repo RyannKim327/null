f95214485e0e00aaed8a7dc5b0252e60d693f067
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:09:15 +0800

    2-8-2024 5:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,17 @@
-var string1 = "Hello";
-var string2 = "World";
-var result = string1 + " " + string2;
-console.log(result); // Output: Hello World
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[Math.floor(arr.length / 2)];
+
+  const less = arr.filter((value) => value < pivot);
+  const equal = arr.filter((value) => value === pivot);
+  const greater = arr.filter((value) => value > pivot);
+
+  return [...quicksort(less), ...equal, ...quicksort(greater)];
+}
+const array = [5, 8, 2, 1, 6, 3, 7, 4];
+const sortedArray = quicksort(array);
+console.log(sortedArray);
+[1, 2, 3, 4, 5, 6, 7, 8]
