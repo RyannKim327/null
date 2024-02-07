c3d5b1e970c7413cc0063409d447b61226f622b9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:53:34 +0800

    2-8-2024 4:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,28 @@
-function mergeSortIterative(array) {
-  const length = array.length;
-  const tempArray = new Array(length);
-  
-  for (let blockSize = 1; blockSize < length; blockSize *= 2) {
-    for (let start = 0; start < length - blockSize; start += 2 * blockSize) {
-      const mid = start + blockSize - 1;
-      const end = Math.min(start + 2 * blockSize - 1, length - 1);
-      merge(array, tempArray, start, mid, end);
-    }
-  }
-  
-  return array;
-}
-
-function merge(array, tempArray, start, mid, end) {
-  let i = start;
-  let j = mid + 1;
-  
-  for (let k = start; k <= end; k++) {
-    tempArray[k] = array[k];
-  }
-  
-  for (let k = start; k <= end; k++) {
-    if (i > mid) {
-      array[k] = tempArray[j];
-      j++;
-    } else if (j > end) {
-      array[k] = tempArray[i];
-      i++;
-    } else if (tempArray[j] < tempArray[i]) {
-      array[k] = tempArray[j];
-      j++;
-    } else {
-      array[k] = tempArray[i];
-      i++;
-    }
+class Stack {
+  constructor() {
+    this.items = []; // The array to store stack elements
   }
 }
-const array = [5, 9, 2, 8, 3, 7, 1, 6, 4];
-console.log("Original Array:", array);
+push(element) {
+  this.items.push(element);
+}
+pop() {
+  if (this.isEmpty()) {
+    return "Stack is empty!";
+  }
+  return this.items.pop();
+}
+peek() {
+  return this.items[this.items.length - 1];
+}
+isEmpty() {
+  return this.items.length === 0;
+}
+const stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
 
-const sortedArray = mergeSortIterative(array);
-console.log("Sorted Array:", sortedArray);
+console.log(stack.pop()); // Output: 30
+console.log(stack.peek()); // Output: 20
+console.log(stack.isEmpty()); // Output: false
