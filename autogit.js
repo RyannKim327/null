841e24cdaf5ca3a4331f22a6767be2f6ec18645c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 05:13:50 +0800

    2-8-2024 5:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,85 +1,26 @@
-function fibonacciSearch(array, key, length) {
-  // ...
-}
-function fibonacciSearch(array, key, length) {
-  let fibNums = [0, 1];
-  // ...
-}
-function fibonacciSearch(array, key, length) {
-  let fibNums = [0, 1];
-
-  while (fibNums[fibNums.length - 1] < length) {
-    fibNums.push(fibNums[fibNums.length - 1] + fibNums[fibNums.length - 2]);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  // ...
 }
-function fibonacciSearch(array, key, length) {
-  let fibNums = [0, 1];
 
-  while (fibNums[fibNums.length - 1] < length) {
-    fibNums.push(fibNums[fibNums.length - 1] + fibNums[fibNums.length - 2]);
-  }
-
-  let fibIndex = -1;
-  let k = fibNums.length - 1;
-  while (fibNums[k] > length) {
-    k--;
-  }
-
-  fibIndex = k;
-  // ...
-}
-function fibonacciSearch(array, key, length) {
-  // ...
-
-  let offset = -1;
-  let prev = 0;
-  // ...
-}
-function fibonacciSearch(array, key, length) {
-  // ...
-
-  let offset = -1;
-  let prev = 0;
-  
-  while (fibIndex > 1) {
-    let i = Math.min(offset + fibNums[fibIndex - 2], length - 1);
-
-    if (array[i] < key) {
-      fibIndex -= 1;
-      offset = i;
-      prev = array[i];
-    } else if (array[i] > key) {
-      fibIndex -= 2;
-    } else {
-      return i; // Key found at index i
-    }
-  }
-  // ...
-}
-function fibonacciSearch(array, key, length) {
-  // ...
-
-  let offset = -1;
-  let prev = 0;
-
-  while (fibIndex > 1) {
-    let i = Math.min(offset + fibNums[fibIndex - 2], length - 1);
-
-    if (array[i] < key) {
-      fibIndex -= 1;
-      offset = i;
-      prev = array[i];
-    } else if (array[i] > key) {
-      fibIndex -= 2;
-    } else {
-      return i; // Key found at index i
-    }
-  }
-
-  if (prev && array[offset + 1] === key) {
-    return offset + 1;
+function findMaxDepth(node) {
+  if (node == null) {
+    return 0;
   } else {
-    return -1; // Key not found
+    let leftDepth = findMaxDepth(node.left);
+    let rightDepth = findMaxDepth(node.right);
+    return Math.max(leftDepth, rightDepth) + 1;
   }
 }
+
+// Example usage:
+let root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+
+console.log(findMaxDepth(root)); // Output: 3
