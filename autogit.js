171ed668bd52bd958d15de9f8104d63999f1d905
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:48:35 +0800

    2-8-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,9 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Output: hello world
+function removeVowels(str) {
+  // Use regular expression to remove all vowels (both lowercase and uppercase)
+  return str.replace(/[aeiouAEIOU]/g, '');
+}
+
+// Example usage
+const inputString = 'Hello, World!';
+const stringWithoutVowels = removeVowels(inputString);
+console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
