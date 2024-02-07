7cda30eaa863157c50376328dc9536516a0ec948
RyannKim327 <rksesgundo123@gmail.com>
2024-02-08 04:46:33 +0800

    2-8-2024 4:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,49 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-
-  // Split the array into subarrays
-  let temp = arr.map(function (value) {
-    return [value];
-  });
-
-  // Merge the subarrays
-  while (temp.length > 1) {
-    let merged = [];
-
-    for (let i = 0; i < temp.length; i += 2) {
-      // Merge two subarrays
-      if (temp[i + 1]) {
-        merged.push(merge(temp[i], temp[i + 1]));
-      } else {
-        merged.push(temp[i]);
-      }
-    }
-
-    temp = merged;
-  }
-
-  return temp[0]; // Return the sorted array
 }
 
-// Helper function to merge two sorted arrays
-function merge(arr1, arr2) {
-  let merged = [];
-  let i = 0;
-  let j = 0;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      merged.push(arr1[i]);
-      i++;
+  append(data) {
+    const newNode = new Node(data);
+  
+    if (!this.head) {
+      this.head = newNode;
     } else {
-      merged.push(arr2[j]);
-      j++;
+      let current = this.head;
+  
+      while (current.next) {
+        current = current.next;
+      }
+  
+      current.next = newNode;
     }
   }
 
-  // Push the remaining elements of arr1, if any
-  while (i < arr1.length) {
-    merged.push(arr1[i]);
-    i++;
+  getLength() {
+    let count = 0;
+    let current = this.head;
+  
+    while (current) {
+      count++;
+      current = current.next;
+    }
+  
+    return count;
   }
-
-  // Push the remaining elements of arr2, if any
-  while (j < arr2.length) {
-    merged.push(arr2[j]);
-    j++;
-  }
-
-  return merged;
 }
 
-// Example usage:
-let array = [5, 3, 8, 4, 2, 1, 6, 7];
-let sortedArray = mergeSort(array);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+// Example usage
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+
+console.log(list.getLength()); // Output: 4
