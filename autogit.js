62ed57841e6fd4fe0e51faa831827028c0896987
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:50:56 +0800

    2-9-2024 12:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,22 @@
-function radixSort(array) {
-  const maxNum = Math.max(...array);
-  const numPasses = Math.floor(Math.log10(maxNum) + 1);
-
-  // Create 10 buckets
-  const buckets = Array.from({ length: 10 }, () => []);
-
-  for (let pass = 0; pass < numPasses; pass++) {
-    // Distribute numbers into appropriate buckets
-    for (const num of array) {
-      const digit = getDigit(num, pass);
-      buckets[digit].push(num);
-    }
-
-    // Concatenate numbers from buckets back into the array
-    array = [].concat(...buckets);
-
-    // Clear buckets for next pass
-    for (const bucket of buckets) {
-      bucket.length = 0;
-    }
+function hasCycle(head) {
+  if (!head || !head.next) {
+    // If the list is empty or has only one node, there can't be a cycle
+    return false;
   }
 
-  return array;
-}
+  let slow = head;  // Tortoise pointer
+  let fast = head.next;  // Hare pointer
 
-function getDigit(num, pass) {
-  return Math.floor(Math.abs(num) / Math.pow(10, pass)) % 10;
+  while (slow !== fast) {
+    if (!fast || !fast.next) {
+      // If either pointer reaches the end of the list, there is no cycle
+      return false;
+    }
+
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // If the fast pointer catches up with the slow pointer, a cycle is found
+  return true;
 }
-const unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
-const sortedArray = radixSort(unsortedArray);
-console.log(sortedArray);
-[2, 24, 45, 66, 75, 90, 170, 802]
