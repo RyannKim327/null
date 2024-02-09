abd315394e661859965e790ac637779bcc74999b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:46:20 +0800

    2-9-2024 12:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,23 @@
-function generateFibonacci(n) {
-  let fib = [0, 1];
-  for (let i = 2; fib[i - 1] < n; i++) {
-    fib[i] = fib[i - 1] + fib[i - 2];
-  }
-  return fib;
-}
-function fibonacciSearch(arr, x) {
-  let fib = generateFibonacci(arr.length);
-  
-  let offset = -1; // Initialize the offset
-  
-  // Find the smallest Fibonacci number that is greater than or equal to arr.length
-  let fibIndex = 0;
-  while (fib[fibIndex] < arr.length) {
-    fibIndex++;
-  }
-  
-  let i = fib[fibIndex - 2]; // Index of the first element to compare
-  
-  // Perform the search
-  while (i < arr.length) {
-    if (arr[i] === x) {
-      return i; // Found the element at index i
-    } else if (arr[i] < x) {
-      i = i + fib[fibIndex - 1]; // Move to the right
-      fibIndex -= 2;
-    } else {
-      i = i - fib[fibIndex - 2]; // Move to the left
-      fibIndex -= 1;
+function getNodeFromEnd(list, n) {
+  let p1 = list;
+  let p2 = list;
+
+  // Move p2 n positions ahead
+  for (let i = 0; i < n; i++) {
+    if (p2 === null) {
+      return "List has fewer than n nodes";
     }
+    p2 = p2.next;
   }
-  
-  return -1; // Element not found
+
+  // Move both pointers until p2 reaches the end
+  while (p2 !== null) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  return p1;
 }
-let arr = [1, 3, 5, 8, 11, 15, 21, 34, 55, 89];
-let x = 15;
-let index = fibonacciSearch(arr, x);
-if (index !== -1) {
-  console.log(`Element ${x} found at index ${index}`);
-} else {
-  console.log('Element not found');
-}
+const head = { value: 1, next: { value: 2, next: { value: 3, next: { value: 4, next: null } } } };
+
+console.log(getNodeFromEnd(head, 2)); // Output: { value: 3, next: { value: 4, next: null } }
