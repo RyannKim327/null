6510aab1bbf2b1e37fdcc4d56b0008ead198ad45
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:53:31 +0800

    2-9-2024 12:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,2 @@
-let numbers = [4, 2, 9, 1, 7];
-numbers.sort((a, b) => a - b);
-console.log(numbers); // Output: [1, 2, 4, 7, 9]
-let numbers = [4, 2, 9, 1, 7];
-let sortedNumbers = [];
-while (numbers.length > 0) {
-  let min = Math.min(...numbers);
-  sortedNumbers.push(min);
-  numbers.splice(numbers.indexOf(min), 1);
-}
-console.log(sortedNumbers); // Output: [1, 2, 4, 7, 9]
-let numbers = [4, 2, 9, 1, 7];
-function sortNumbers(a, b) {
-  return a - b;
-}
-let sortedNumbers = numbers.sort(sortNumbers);
-console.log(sortedNumbers); // Output: [1, 2, 4, 7, 9]
+const inputString = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(inputString)); // Output: true
