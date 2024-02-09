7e3109aec05140405cfb948e5dae7dfc995d7ff3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:48:41 +0800

    2-9-2024 12:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,10 @@
-function longestCommonSubsequence(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-  
-  // Create a 2D array of size (m+1)x(n+1) and initialize it with zeros
-  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-  
-  // Fill the dp array
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-      }
-    }
-  }
-  
-  // Backtrack to find the longest common subsequence
-  let lcs = '';
-  let i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i][j - 1] > dp[i - 1][j]) {
-      j--;
-    } else {
-      i--;
-    }
-  }
-  
-  return lcs;
+function countOccurrences(string, character) {
+  return string.split(character).length - 1;
 }
 
-// Example usage:
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
+// Example usage
+const myString = "Hello, how are you?";
+const myCharacter = "o";
+const occurrences = countOccurrences(myString, myCharacter);
 
-console.log(longestCommonSubsequence(str1, str2)); // Output: ADH
+console.log(`The character '${myCharacter}' occurs ${occurrences} times in the string.`);
