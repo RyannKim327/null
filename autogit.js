5c5db2dfd1e8a07acaf3a40339545accec7bbdbf
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:49:59 +0800

    2-9-2024 12:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,55 @@
-// Prompt the user to enter their name
-let name = prompt("Enter your name:");
-
-// Display a greeting message with the user's name
-console.log("Hello, " + name + "!");
-
-// Prompt the user to enter their age
-let age = prompt("Enter your age:");
-
-// Convert the age string to a number
-age = parseInt(age);
-
-// Display a message based on the user's age
-if (age < 18) {
-  console.log("You are underage.");
-} else {
-  console.log("You are an adult.");
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  isPalindrome() {
+    const values = [];
+    let current = this.head;
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+
+    let start = 0;
+    let end = values.length - 1;
+    while (start < end) {
+      if (values[start] !== values[end]) {
+        return false;
+      }
+      start++;
+      end--;
+    }
+    return true;
+  }
+}
+
+// Example usage:
+const linkedList = new LinkedList();
+linkedList.push('a');
+linkedList.push('b');
+linkedList.push('c');
+linkedList.push('b');
+linkedList.push('a');
+
+console.log(linkedList.isPalindrome()); // Output: true
