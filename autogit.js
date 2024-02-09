ea1308d8fdcd9ebe919ea24d051088ede3decbc9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:01:19 +0800

    2-9-2024 13:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,4 @@
-function bwtTransform(input) {
-  // Generate all possible rotations
-  let rotations = [];
-  for (let i = 0; i < input.length; i++) {
-    rotations.push(input.slice(i) + input.slice(0, i));
-  }
-
-  // Sort the rotations
-  rotations.sort();
-
-  // Get the last characters of each rotation
-  let bwt = '';
-  for (let i = 0; i < rotations.length; i++) {
-    bwt += rotations[i][input.length - 1];
-  }
-
-  return bwt;
-}
-let input = 'banana';
-let bwtResult = bwtTransform(input);
-console.log(bwtResult);
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
