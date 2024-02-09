efb62beeb245f13aba392c5ce16e546fdd1a5d5c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:03:45 +0800

    2-9-2024 13:3 [User mode]

--- autogit.js
+++
@@ -1,7 +0,0 @@
-function decimalToBinary(decimalNumber) {
-  return decimalNumber.toString(2);
-}
-
-// Example usage
-console.log(decimalToBinary(10));   // Output: "1010"
-console.log(decimalToBinary(42));   // Output: "101010"
