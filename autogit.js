69d2bb748c7e6108ffdfe31c8a32d709e945ebf5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:01:47 +0800

    2-9-2024 13:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,18 @@
-function findLCS(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
-  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-
-  // Building the LCS matrix
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
-    }
-  }
-
-  // Backtracking to find the LCS
-  let i = m;
-  let j = n;
-  const lcs = [];
-
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs.unshift(str1[i - 1]);
-      i--;
-      j--;
-    } else if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs.join('');
-}
-
-// Example usage
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const lcs = findLCS(str1, str2);
-console.log(lcs); // Output: 'ADH'
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Random JavaScript Code</title>
+    <script>
+        function displayInput() {
+            var userInput = document.getElementById("input").value;
+            alert("You entered: " + userInput);
+        }
+    </script>
+</head>
+<body>
+    <h1>Random JavaScript Code</h1>
+    <label for="input">Enter some text:</label>
+    <input type="text" id="input">
+    <button onclick="displayInput()">Display</button>
+</body>
+</html>
