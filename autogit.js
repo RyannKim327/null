ef646643819145d4634de8b92ddc4fd253d82593
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:05:08 +0800

    2-9-2024 13:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,23 @@
-let string1 = 'Hello';
-let string2 = 'World';
+function bubbleSort(arr) {
+  var len = arr.length;
+  var swapped;
+  
+  do {
+    swapped = false;
+    for (var i = 0; i < len-1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  
+  return arr;
+}
 
-let result = string1 + ' ' + string2;
-console.log(result); // Output: Hello World
-let string1 = 'Hello';
-let string2 = 'World';
-
-let result = string1.concat(' ', string2);
-console.log(result); // Output: Hello World
+// Usage example:
+var numbers = [8, 3, 2, 10, 4];
+console.log("Original array: " + numbers);
+console.log("Sorted array: " + bubbleSort(numbers));
