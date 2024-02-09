8682a7b7763c6635aba13c3de89d411a9e74bb25
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:49:00 +0800

    2-9-2024 12:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,46 @@
-class HashTable {
-  constructor() {
-     this.table = {};
-  }
-
-  // Convert the key into a numeric hash code
-  hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
-    }
-    return hash;
-  }
-
-  // Insert a key-value pair into the hash table
-  insert(key, value) {
-    const hash = this.hash(key);
-    this.table[hash] = value;
-  }
-
-  // Retrieve a value based on the key
-  get(key) {
-    const hash = this.hash(key);
-    return this.table[hash];
-  }
-
-  // Remove a key-value pair from the hash table
-  remove(key) {
-    const hash = this.hash(key);
-    if (this.table.hasOwnProperty(hash)) {
-      delete this.table[hash];
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+enqueue(value) {
+  const newNode = new Node(value);
 
-// Example usage
-const myHashTable = new HashTable();
-myHashTable.insert('name', 'John');
-myHashTable.insert('age', 25);
+  if (!this.head) {
+    this.head = newNode;
+    this.tail = newNode;
+  } else {
+    this.tail.next = newNode;
+    this.tail = newNode;
+  }
+}
+dequeue() {
+  if (!this.head) {
+    return null;
+  }
 
-console.log(myHashTable.get('name')); // Output: John
-console.log(myHashTable.get('age'));  // Output: 25
+  const dequeuedNode = this.head;
 
-myHashTable.remove('name');
-console.log(myHashTable.get('name')); // Output: undefined
+  if (this.head === this.tail) {
+    this.head = null;
+    this.tail = null;
+  } else {
+    this.head = this.head.next;
+  }
+
+  return dequeuedNode.value;
+}
+peek() {
+  if (!this.head) {
+    return null;
+  }
+
+  return this.head.value;
+}
