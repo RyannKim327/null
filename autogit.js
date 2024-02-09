268f5ff1fac1ea355900257c23a89f582b1c4669
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:55:48 +0800

    2-9-2024 12:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,85 @@
-function countOccurrences(str, character) {
-  let count = 0;
-  
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] === character) {
-      count++;
-    }
+class Node {
+  constructor() {
+    this.value = '';
+    this.isEndOfWord = false;
+    this.children = {};
   }
-  
-  return count;
 }
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
 
-// Example usage
-const myString = 'Hello, world!';
-const myCharacter = 'o';
+  insert(word) {
+    let currentNode = this.root;
+    
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
 
-const occurrences = countOccurrences(myString, myCharacter);
-console.log(`The character "${myCharacter}" occurs ${occurrences} times in the string.`);
+      if (!currentNode.children[char]) {
+        currentNode.children[char] = new Node();
+        currentNode.children[char].value = char;
+      }
+
+      currentNode = currentNode.children[char];
+    }
+
+    currentNode.isEndOfWord = true;
+  }
+
+  search(word) {
+    let currentNode = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+
+      if (!currentNode.children[char]) {
+        return false;
+      }
+
+      currentNode = currentNode.children[char];
+    }
+
+    return currentNode.isEndOfWord;
+  }
+
+  delete(word) {
+    const deleteHelper = (node, word, index) => {
+      if (index === word.length) {
+        node.isEndOfWord = false;
+
+        // If the node has no children, it can be safely deleted
+        return Object.keys(node.children).length === 0;
+      }
+
+      const char = word[index];
+
+      if (!node.children[char]) {
+        return false; // Word does not exist in trie
+      }
+
+      const canDeleteNode = deleteHelper(node.children[char], word, index + 1);
+
+      if (canDeleteNode) {
+        delete node.children[char];
+
+        // If the node has no children and is not the end of a word, it can be safely deleted
+        return !node.isEndOfWord && Object.keys(node.children).length === 0;
+      }
+
+      return false;
+    };
+
+    deleteHelper(this.root, word, 0);
+  }
+}
+const trie = new Trie();
+
+trie.insert('apple');
+trie.insert('banana');
+console.log(trie.search('apple')); // Output: true
+console.log(trie.search('banana')); // Output: true
+console.log(trie.search('orange')); // Output: false
+
+trie.delete('apple');
+console.log(trie.search('apple')); // Output: false
