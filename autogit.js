8c5b4630b2c4e91419d486ae6d4243b356e656ad
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:57:38 +0800

    2-9-2024 12:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,50 @@
-let str = "Hello World";
-let reversedStr = str.split("").reverse().join("");
-console.log(reversedStr);
-dlroW olleH
-let str = "Hello World";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
+class Graph {
+  constructor(vertices) {
+    this.vertices = vertices;
+    this.adjacencyList = new Map();
+    for (const vertex of vertices) {
+      this.adjacencyList.set(vertex, []);
+    }
+  }
+
+  addEdge(source, destination) {
+    this.adjacencyList.get(source).push(destination);
+  }
+
+  getNeighbors(vertex) {
+    return this.adjacencyList.get(vertex);
+  }
+
+  // Add any additional methods here as needed
 }
-console.log(reversedStr);
-dlroW olleH
+function topologicalSort(graph) {
+  const visited = new Set();
+  const stack = [];
+
+  for (const vertex of graph.vertices) {
+    if (!visited.has(vertex)) {
+      dfs(vertex);
+    }
+  }
+
+  function dfs(vertex) {
+    visited.add(vertex);
+
+    for (const neighbor of graph.getNeighbors(vertex)) {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor);
+      }
+    }
+
+    stack.unshift(vertex); // Add the vertex to the beginning of the stack
+  }
+
+  return stack;
+}
+const graph = new Graph(['A', 'B', 'C', 'D', 'E']);
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'D');
+graph.addEdge('D', 'E');
+const orderedVertices = topologicalSort(graph);
+console.log(orderedVertices); // Output: ['A', 'B', 'C', 'D', 'E']
