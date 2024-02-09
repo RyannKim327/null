5785ff48f103c4d9384ce6815907f17edf6caca6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:10:24 +0800

    2-9-2024 13:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,5 @@
-function calculateMean(numbers) {
-  if (numbers.length === 0) {
-    return 0;
-  }
+// Prompt the user for input
+var userInput = prompt("Please enter something:");
 
-  var sum = numbers.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue;
-  });
-
-  var mean = sum / numbers.length;
-  return mean;
-}
-
-// Example usage:
-var numbers = [5, 12, 3, 8, 9];
-var mean = calculateMean(numbers);
-console.log("Mean: " + mean);
+// Display the user's input
+console.log("You entered: " + userInput);
