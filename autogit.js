de8665f765591a7bb4d5d349d2cc760005783682
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:04:54 +0800

    2-9-2024 13:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,12 @@
-class ListNode {
-  constructor(val){
-    this.val = val;
-    this.next = null;
-  }
-}
+const array = [1, 2, 3, 3, 4, 4, 5];
+const uniqueArray = Array.from(new Set(array));
 
-function getIntersectionNode(headA, headB) {
-  if (!headA || !headB) {
-    return null; // No intersection if any of the lists is empty
-  }
-  
-  let lengthA = getLength(headA);
-  let lengthB = getLength(headB);
-  let diff = Math.abs(lengthA - lengthB);
-  
-  if (lengthA > lengthB) {
-    while (diff > 0) {
-      headA = headA.next;
-      diff--;
-    }
-  } else {
-    while (diff > 0) {
-      headB = headB.next;
-      diff--;
-    }
-  }
-  
-  while (headA && headB) {
-    if (headA === headB) {
-      return headA; // Intersection found
-    }
-    
-    headA = headA.next;
-    headB = headB.next;
-  }
-  
-  return null; // No intersection found
-  
-}
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const array = [1, 2, 3, 3, 4, 4, 5];
+const uniqueArray = array.filter((value, index) => array.indexOf(value) === index);
 
-function getLength(node) {
-  let length = 0;
-  
-  while (node) {
-    length++;
-    node = node.next;
-  }
-  
-  return length;
-}
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const array = [1, 2, 3, 3, 4, 4, 5];
+const uniqueArray = Object.keys(array.reduce((acc, value) => ((acc[value] = true), acc), {})).map(Number);
+
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
