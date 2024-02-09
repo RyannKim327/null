bde53d467b1a2a5818a88966fdcbb7ac1bdc6de4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:01:40 +0800

    2-9-2024 13:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,41 @@
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
-const array = [1, 2, 3, 4, 4, 5, 6, 6];
-const uniqueArray = [];
-const tempObject = {};
-for (const item of array) {
-  if (!tempObject[item]) {
-    uniqueArray.push(item);
-    tempObject[item] = true;
+function findLCS(str1, str2) {
+  const m = str1.length;
+  const n = str2.length;
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+  // Building the LCS matrix
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
   }
+
+  // Backtracking to find the LCS
+  let i = m;
+  let j = n;
+  const lcs = [];
+
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs.unshift(str1[i - 1]);
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return lcs.join('');
 }
-console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
+
+// Example usage
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const lcs = findLCS(str1, str2);
+console.log(lcs); // Output: 'ADH'
