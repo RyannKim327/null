4a215e962b2170b36c1884d92eb4c9def8a7a10a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:57:28 +0800

    2-9-2024 12:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,11 @@
-function findCommonElements(arr1, arr2) {
-  let common = [];
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
-      common.push(arr1[i]);
-    }
-  }
-  return common;
+let str = "Hello World";
+let reversedStr = str.split("").reverse().join("");
+console.log(reversedStr);
+dlroW olleH
+let str = "Hello World";
+let reversedStr = "";
+for (let i = str.length - 1; i >= 0; i--) {
+  reversedStr += str[i];
 }
-function findCommonElements(arr1, arr2) {
-  return arr1.filter((element) => arr2.includes(element));
-}
-function findCommonElements(arr1, arr2) {
-  const set1 = new Set(arr1);
-  return [...new Set(arr2.filter((item) => set1.has(item)))];
-}
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-
-const commonElements = findCommonElements(array1, array2);
-console.log(commonElements); // Output: [4, 5]
+console.log(reversedStr);
+dlroW olleH
