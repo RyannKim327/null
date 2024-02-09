b961feb91ee5187c08be0ee2c0dc84e6b3ca068e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:01:28 +0800

    2-9-2024 13:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,16 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = [...new Set(array)];
+console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
+console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 3, 4, 4, 5, 6, 6];
+const uniqueArray = [];
+const tempObject = {};
+for (const item of array) {
+  if (!tempObject[item]) {
+    uniqueArray.push(item);
+    tempObject[item] = true;
+  }
+}
+console.log(uniqueArray);  // Output: [1, 2, 3, 4, 5, 6]
