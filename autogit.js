dad7dd66eabca4bb6a63edb18e3b366b8f7cdf1a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 12:56:24 +0800

    2-9-2024 12:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,108 +1,51 @@
-class BTreeNode {
-  constructor(t, leaf = true) {
-    this.t = t;
-    this.keys = [];
-    this.children = [];
-    this.leaf = leaf;
+class HashTable {
+  constructor(size = 100) {
+    this.size = size;
+    this.buckets = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % this.size;
+  }
+
+  insert(key, value) {
+    const index = this._hash(key);
+    if (!this.buckets[index]) {
+      this.buckets[index] = {};
+    }
+    this.buckets[index][key] = value;
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (!this.buckets[index]) {
+      return undefined;
+    }
+    return this.buckets[index][key];
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+    if (!this.buckets[index]) {
+      return undefined;
+    }
+    const value = this.buckets[index][key];
+    delete this.buckets[index][key];
+    return value;
   }
 }
+const hashTable = new HashTable();
 
-class BTree {
-  constructor(t) {
-    this.root = new BTreeNode(t);
-    this.t = t;
-  }
+hashTable.insert("name", "John");
+hashTable.insert("age", 25);
 
-  insert(key) {
-    const rootNode = this.root;
-    if (rootNode.keys.length === (2 * this.t) - 1) {
-      const newRoot = new BTreeNode(this.t, false);
-      this.root = newRoot;
-      newRoot.children.push(rootNode);
-      this.splitChild(newRoot, 0);
-      this.insertNonFull(newRoot, key);
-    } else {
-      this.insertNonFull(rootNode, key);
-    }
-  }
+console.log(hashTable.get("name")); // Output: John
+console.log(hashTable.get("age")); // Output: 25
 
-  insertNonFull(node, key) {
-    let i = node.keys.length - 1;
+hashTable.remove("age");
 
-    if (node.leaf) {
-      while (i >= 0 && key < node.keys[i]) {
-        node.keys[i + 1] = node.keys[i];
-        i--;
-      }
-      node.keys[i + 1] = key;
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      i++;
-
-      if (node.children[i].keys.length === (2 * this.t) - 1) {
-        this.splitChild(node, i);
-
-        if (key > node.keys[i]) {
-          i++;
-        }
-      }
-      this.insertNonFull(node.children[i], key);
-    }
-  }
-
-  splitChild(parent, index) {
-    const node = parent.children[index];
-    const newNode = new BTreeNode(this.t, node.leaf);
-    parent.keys.splice(index, 0, node.keys[this.t - 1]);
-    parent.children.splice(index + 1, 0, newNode);
-
-    newNode.keys = node.keys.splice(this.t, node.keys.length - 1);
-    if (!node.leaf) {
-      newNode.children = node.children.splice(this.t, node.children.length - 1);
-    }
-  }
-
-  search(node, key) {
-    let i = 0;
-    while (i < node.keys.length && key > node.keys[i]) {
-      i++;
-    }
-
-    if (node.keys[i] === key) {
-      return true;
-    }
-    if (node.leaf) {
-      return false;
-    }
-    return this.search(node.children[i], key);
-  }
-
-  traverse(node) {
-    const result = [];
-    for (let i = 0; i < node.keys.length; i++) {
-      if (!node.leaf) {
-        result.push(...this.traverse(node.children[i]));
-      }
-      result.push(node.keys[i]);
-    }
-    if (!node.leaf) {
-      result.push(...this.traverse(node.children[node.keys.length]));
-    }
-    return result;
-  }
-}
-
-// Example usage:
-const btree = new BTree(3);
-btree.insert(5);
-btree.insert(3);
-btree.insert(8);
-btree.insert(7);
-btree.insert(1);
-
-console.log(btree.search(btree.root, 8)); // Output: true
-console.log(btree.search(btree.root, 2)); // Output: false
-
-console.log(btree.traverse(btree.root)); // Output: [1, 3, 5, 7, 8]
+console.log(hashTable.get("age")); // Output: undefined
