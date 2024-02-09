806d9cdf50064d0065578990fbc0efbb85da271c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:03:33 +0800

    2-9-2024 13:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,7 @@
-const array = [1, 2, 3, 4, 5];
-const elementToRemove = 3;
-
-const index = array.indexOf(elementToRemove);
-if (index > -1) {
-  array.splice(index, 1);
+function decimalToBinary(decimalNumber) {
+  return decimalNumber.toString(2);
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
-const array = [1, 2, 3, 4, 5];
-const elementToRemove = 3;
-
-const filteredArray = array.filter((element) => element !== elementToRemove);
-
-console.log(filteredArray); // Output: [1, 2, 4, 5]
-const array = [1, 2, 3, 4, 5];
-const elementToRemove = 3;
-
-const index = array.indexOf(elementToRemove);
-if (index > -1) {
-  array = array.slice(0, index).concat(array.slice(index + 1));
-}
-
-console.log(array); // Output: [1, 2, 4, 5]
+// Example usage
+console.log(decimalToBinary(10));   // Output: "1010"
+console.log(decimalToBinary(42));   // Output: "101010"
