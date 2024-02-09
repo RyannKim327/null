64fa4d84f6c3d2f38e9f2440405ae3eb1f420556
RyannKim327 <rksesgundo123@gmail.com>
2024-02-09 13:06:15 +0800

    2-9-2024 13:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,78 +1,8 @@
-class SuffixTree {
-  constructor() {
-    // Implementation goes here
-  }
-
-  // Methods go here
-}
-constructor() {
-  this.root = {
-    children: {},
-  };
-}
-insert(word) {
-  // Implementation goes here
-}
-for (let i = 0; i < word.length; i++) {
-  // Implementation goes here
-}
-let currentNode = this.root;
-for (let i = 0; i < word.length; i++) {
-  const char = word[i];
-  if (!currentNode.children[char]) {
-    currentNode.children[char] = { children: {} };
-  }
-  currentNode = currentNode.children[char];
-}
-currentNode.children['$'] = { children: {} };
-search(word) {
-  // Implementation goes here
-}
-let currentNode = this.root;
-for (let i = 0; i < word.length; i++) {
-  const char = word[i];
-  if (!currentNode.children[char]) {
-    return false;
-  }
-  currentNode = currentNode.children[char];
-}
-return currentNode.children['$'] !== undefined;
-class SuffixTree {
-  constructor() {
-    this.root = {
-      children: {},
-    };
-  }
-
-  insert(word) {
-    let currentNode = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (!currentNode.children[char]) {
-        currentNode.children[char] = { children: {} };
-      }
-      currentNode = currentNode.children[char];
-    }
-    currentNode.children['$'] = { children: {} };
-  }
-
-  search(word) {
-    let currentNode = this.root;
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (!currentNode.children[char]) {
-        return false;
-      }
-      currentNode = currentNode.children[char];
-    }
-    return currentNode.children['$'] !== undefined;
+function factorialRecursive(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorialRecursive(n - 1);
   }
 }
-const suffixTree = new SuffixTree();
-suffixTree.insert('banana');
-suffixTree.insert('apple');
-suffixTree.insert('orange');
-
-console.log(suffixTree.search('ana')); // true
-console.log(suffixTree.search('orange')); // true
-console.log(suffixTree.search('berry')); // false
+console.log(factorialRecursive(5)); // Output: 120
