d5646d268d76c067100b9511a7f19fe980f3a14c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:55:00 +0800

    2-13-2024 4:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,91 +1,9 @@
-// Create a node class
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function removeVowels(str) {
+  // Use the regular expression /[aeiou]/gi to match all vowels, regardless of case
+  // 'g' flag ensures global match (find all occurrences), 'i' flag ignores case
+  return str.replace(/[aeiou]/gi, "");
 }
 
-// Create a linked list class
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  // Add a new node to the head of the list
-  addToHead(data) {
-    const newNode = new Node(data);
-    newNode.next = this.head;
-    this.head = newNode;
-  }
-
-  // Add a new node to the tail of the list
-  addToTail(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next) {
-      currentNode = currentNode.next;
-    }
-
-    currentNode.next = newNode;
-  }
-
-  // Remove a node from the list based on the data
-  removeNode(data) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.data === data) {
-      this.head = this.head.next;
-      return;
-    }
-
-    let currentNode = this.head;
-    let previousNode = null;
-
-    while (currentNode && currentNode.data !== data) {
-      previousNode = currentNode;
-      currentNode = currentNode.next;
-    }
-
-    if (currentNode) {
-      previousNode.next = currentNode.next;
-    }
-  }
-
-  // Print the list
-  printList() {
-    let currentNode = this.head;
-    let result = "";
-
-    while (currentNode) {
-      result += currentNode.data + " -> ";
-      currentNode = currentNode.next;
-    }
-
-    result += "null";
-    console.log(result);
-  }
-}
-
-// Usage example
-const myList = new LinkedList();
-
-myList.addToHead(3);
-myList.addToHead(2);
-myList.addToHead(1);
-
-myList.addToTail(4);
-
-myList.printList(); // Output: 1 -> 2 -> 3 -> 4 -> null
-
-myList.removeNode(2);
-
-myList.printList(); // Output: 1 -> 3 -> 4 -> null
+var inputString = "Hello World!";
+var result = removeVowels(inputString);
+console.log(result);  // Output: Hll Wrld!
