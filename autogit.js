1399a01c547eb6e317d7595af48261fee7d89100
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:37:50 +0800

    2-13-2024 4:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,4 @@
-function factorial(n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is ${result}.`);
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+
+console.log(lowercaseStr);  // Output: "hello world"
