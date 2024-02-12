53a54ac4dca66a2e1e936db1f78823160d48ac9d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:37:58 +0800

    2-13-2024 4:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,20 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
+let str = "A man, a plan, a canal: Panama";
+str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+let reversedStr = str.split('').reverse().join('');
+if (str === reversedStr) {
+    console.log("The string is a palindrome.");
+} else {
+    console.log("The string is not a palindrome.");
+}
+function isPalindrome(str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reversedStr = str.split('').reverse().join('');
+    return str === reversedStr;
+}
 
-console.log(lowercaseStr);  // Output: "hello world"
+let inputString = "A man, a plan, a canal: Panama";
+if (isPalindrome(inputString)) {
+    console.log("The string is a palindrome.");
+} else {
+    console.log("The string is not a palindrome.");
+}
