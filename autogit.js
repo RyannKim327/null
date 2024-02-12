15fe5b559612c0129bfa505790d2dcfe4047507a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:03:06 +0800

    2-13-2024 5:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,23 @@
-let str = "Hello, World!";
-let lowerCaseStr = str.toLowerCase();
-console.log(lowerCaseStr); // Output: hello, world!
+function shellSort(arr) {
+  let gap = Math.floor(arr.length / 2);
+  
+  while (gap > 0) {
+    for (let i = gap; i < arr.length; i++) {
+      let temp = arr[i];
+      let j = i;
+
+      while (j >= gap && arr[j - gap] > temp) {
+        arr[j] = arr[j - gap];
+        j -= gap;
+      }
+      
+      arr[j] = temp;
+    }
+    
+    gap = Math.floor(gap / 2);
+  }
+  
+  return arr;
+}
+const array = [6, 2, 9, 1, 5, 3, 10];
+console.log(shellSort(array)); // Output: [1, 2, 3, 5, 6, 9, 10]
