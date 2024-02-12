f487715f04985942601c506e2027679d2acead1e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:41:39 +0800

    2-13-2024 4:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,74 @@
-let str = "Hello, world!";
-let reversedStr = str.split("").reverse().join("");
-console.log(reversedStr);
-let str = "Hello, world!";
-let reversedStr = "";
-for (let i = str.length - 1; i >= 0; i--) {
-  reversedStr += str[i];
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
 }
-console.log(reversedStr);
-let str = "Hello, world!";
-let reversedStr = Array.from(str).reduce((acc, char) => char + acc, '');
-console.log(reversedStr);
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let currentNode = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+
+      if (!currentNode.children[char]) {
+        currentNode.children[char] = new TrieNode();
+      }
+
+      currentNode = currentNode.children[char];
+    }
+
+    currentNode.isEndOfWord = true;
+  }
+
+  search(word) {
+    let currentNode = this.root;
+
+    for (let i = 0; i < word.length; i++) {
+      const char = word[i];
+
+      if (!currentNode.children[char]) {
+        return false;
+      }
+
+      currentNode = currentNode.children[char];
+    }
+
+    return currentNode.isEndOfWord;
+  }
+
+  startsWith(prefix) {
+    let currentNode = this.root;
+
+    for (let i = 0; i < prefix.length; i++) {
+      const char = prefix[i];
+
+      if (!currentNode.children[char]) {
+        return false;
+      }
+
+      currentNode = currentNode.children[char];
+    }
+
+    return true;
+  }
+}
+const trie = new Trie();
+
+trie.insert("apple");
+trie.insert("banana");
+trie.insert("orange");
+
+console.log(trie.search("apple")); // Output: true
+console.log(trie.search("banana")); // Output: true
+console.log(trie.search("orange")); // Output: true
+console.log(trie.search("grape")); // Output: false
+
+console.log(trie.startsWith("app")); // Output: true
+console.log(trie.startsWith("ban")); // Output: true
+console.log(trie.startsWith("ora")); // Output: true
+console.log(trie.startsWith("gr")); // Output: false
