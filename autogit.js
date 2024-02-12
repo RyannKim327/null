bd72c9e6af8626d74aed858c7f460e7a419d8f08
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:42:02 +0800

    2-13-2024 4:41 [User mode]

--- autogit.js
+++
@@ -1,8 +0,0 @@
-var string1 = "Hello";
-var string2 = " world!";
-var result = string1 + string2;
-console.log(result); // Output: Hello world!
-var string1 = "Hello";
-var string2 = " world!";
-var result = string1.concat(string2);
-console.log(result); // Output: Hello world!
