7e712f5218578c66a05afb7dbce85536b6cf6dbd
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:45:00 +0800

    2-13-2024 4:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,45 @@
-function bubbleSort(arr) {
-  const n = arr.length;
-  
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swap arr[j] and arr[j + 1]
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+function tarjanSCC(graph) {
+  let index = 0;
+  const stack = [];
+  const indices = new Array(graph.length);
+  const lowLinks = new Array(graph.length);
+  const onStack = new Array(graph.length).fill(false);
+  const components = [];
+
+  function strongConnect(node) {
+    indices[node] = index;
+    lowLinks[node] = index;
+    index++;
+    stack.push(node);
+    onStack[node] = true;
+
+    graph[node].forEach(neighbor => {
+      if (indices[neighbor] === undefined) {
+        strongConnect(neighbor);
+        lowLinks[node] = Math.min(lowLinks[node], lowLinks[neighbor]);
+      } else if (onStack[neighbor]) {
+        lowLinks[node] = Math.min(lowLinks[node], indices[neighbor]);
       }
+    });
+
+    if (lowLinks[node] === indices[node]) {
+      const component = [];
+      let curNode;
+      do {
+        curNode = stack.pop();
+        onStack[curNode] = false;
+        component.push(curNode);
+      } while (curNode !== node);
+
+      components.push(component);
     }
   }
-  
-  return arr;
-}
 
-// Example usage:
-const numbers = [5, 3, 8, 2, 1, 4];
-console.log(bubbleSort(numbers)); // Output: [1, 2, 3, 4, 5, 8]
+  for (let i = 0; i < graph.length; i++) {
+    if (indices[i] === undefined) {
+      strongConnect(i);
+    }
+  }
+
+  return components;
+}
