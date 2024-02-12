7d00b1865ecf6e2badc2ca38585dbe41b9c0720c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:45:28 +0800

    2-13-2024 4:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,23 @@
-class Graph {
-  constructor() {
-    this.vertices = [];
-    this.adjList = new Map();
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+console.log(reverseString('Hello World')); // Output: "dlroW olleH"
+function reverseString(str) {
+  let reversedString = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
   }
+  return reversedString;
+}
 
-  addVertex(v) {
-    this.vertices.push(v);
-    this.adjList.set(v, []);
-  }
-
-  addEdge(v, w) {
-    this.adjList.get(v).push(w);
-    this.adjList.get(w).push(v);
-  }
-
-  breadthFirstSearch(startingNode) {
-    const visited = new Set();
-    const queue = [];
-
-    visited.add(startingNode);
-    queue.push(startingNode);
-
-    while (queue.length > 0) {
-      const currentVertex = queue.shift();
-      console.log(currentVertex); // Process the node (in this case, print its value)
-
-      const neighbors = this.adjList.get(currentVertex);
-      for (const neighbor of neighbors) {
-        if (!visited.has(neighbor)) {
-          queue.push(neighbor);
-          visited.add(neighbor);
-        }
-      }
-    }
+console.log(reverseString('Hello World')); // Output: "dlroW olleH"
+function reverseString(str) {
+  if (str === '') {
+    return '';
+  } else {
+    return reverseString(str.substr(1)) + str.charAt(0);
   }
 }
 
-// Example usage:
-const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-
-graph.breadthFirstSearch('A');
+console.log(reverseString('Hello World')); // Output: "dlroW olleH"
