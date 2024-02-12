c5503705bd09473572ca61d7e3861f38d8c34dea
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:49:38 +0800

    2-13-2024 4:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,77 +1,19 @@
-class TrieNode {
-  constructor() {
-    this.children = new Map();
-    this.isEndOfWord = false;
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // Swapping elements
+        var temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
   }
+  return arr;
 }
-class Trie {
-  constructor() {
-    this.root = new TrieNode();
-  }
 
-  insert(word) {
-    let currentNode = this.root;
-
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      let node = currentNode.children.get(char);
-
-      if (!node) {
-        node = new TrieNode();
-        currentNode.children.set(char, node);
-      }
-
-      currentNode = node;
-    }
-
-    currentNode.isEndOfWord = true;
-  }
-
-  search(word) {
-    let currentNode = this.root;
-
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      const node = currentNode.children.get(char);
-
-      if (!node) {
-        return false;
-      }
-
-      currentNode = node;
-    }
-
-    return currentNode.isEndOfWord;
-  }
-
-  startsWith(prefix) {
-    let currentNode = this.root;
-
-    for (let i = 0; i < prefix.length; i++) {
-      const char = prefix[i];
-      const node = currentNode.children.get(char);
-
-      if (!node) {
-        return false;
-      }
-
-      currentNode = node;
-    }
-
-    return true;
-  }
-}
-const trie = new Trie();
-
-trie.insert("apple");
-trie.insert("banana");
-trie.insert("app");
-
-console.log(trie.search("apple")); // true
-console.log(trie.search("app")); // true
-console.log(trie.search("banana")); // true
-console.log(trie.search("ap")); // false
-
-console.log(trie.startsWith("app")); // true
-console.log(trie.startsWith("ban")); // true
-console.log(trie.startsWith("bat")); // false
+// Example usage:
+var unsortedArray = [4, 2, 7, 1, 3];
+var sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 3, 4, 7]
