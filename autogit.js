fd1414d69ba28832b8c37e0056dd073410918bd4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:04:01 +0800

    2-13-2024 5:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,22 @@
-function Graph(numNodes) {
-  this.numNodes = numNodes;
-  this.adjList = new Array(numNodes).fill(null).map(() => []);
-}
+function insertionSort(arr) {
+  const len = arr.length;
 
-Graph.prototype.addEdge = function (from, to) {
-  this.adjList[from].push(to);
-};
+  for (let i = 1; i < len; i++) {
+    const currentValue = arr[i];
 
-Graph.prototype.getAdjList = function () {
-  return this.adjList;
-};
-function tarjanSCC(graph) {
-  let index = 0;
-  let stack = [];
-  let onStack = new Array(graph.numNodes).fill(false);
-  let lowLink = new Array(graph.numNodes).fill(0);
-  let indexMap = new Array(graph.numNodes).fill(-1);
-  let result = [];
-
-  function strongConnect(node) {
-    indexMap[node] = index;
-    lowLink[node] = index;
-    index++;
-    stack.push(node);
-    onStack[node] = true;
-
-    const adjList = graph.getAdjList();
-    for (let i = 0; i < adjList[node].length; i++) {
-      const neighbor = adjList[node][i];
-      if (indexMap[neighbor] === -1) {
-        strongConnect(neighbor);
-        lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
-      } else if (onStack[neighbor]) {
-        lowLink[node] = Math.min(lowLink[node], indexMap[neighbor]);
-      }
+    let j = i - 1;
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
     }
 
-    if (lowLink[node] === indexMap[node]) {
-      const component = [];
-      let w;
-      do {
-        w = stack.pop();
-        onStack[w] = false;
-        component.push(w);
-      } while (w !== node);
-      result.push(component);
-    }
+    arr[j + 1] = currentValue;
   }
 
-  for (let node = 0; node < graph.numNodes; node++) {
-    if (indexMap[node] === -1) {
-      strongConnect(node);
-    }
-  }
-
-  return result;
+  return arr;
 }
-const graph = new Graph(8);
-graph.addEdge(0, 1);
-graph.addEdge(1, 2);
-graph.addEdge(2, 0);
-graph.addEdge(3, 4);
-graph.addEdge(4, 5);
-graph.addEdge(5, 3);
-graph.addEdge(2, 3);
-graph.addEdge(3, 6);
-graph.addEdge(6, 7);
-graph.addEdge(7, 4);
 
-const components = tarjanSCC(graph);
-console.log(components);
+// Example usage:
+const numbers = [5, 2, 4, 6, 1, 3];
+const sortedNumbers = insertionSort(numbers);
+console.log(sortedNumbers); // Output: [1, 2, 3, 4, 5, 6]
