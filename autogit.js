c5421542b30bf2de942872b415871f5d5bd9e0e9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:55:05 +0800

    2-13-2024 4:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,5 @@
-function removeVowels(str) {
-  // Use the regular expression /[aeiou]/gi to match all vowels, regardless of case
-  // 'g' flag ensures global match (find all occurrences), 'i' flag ignores case
-  return str.replace(/[aeiou]/gi, "");
-}
+var str = "Hello, world! How are you?";
+var result = str.replace(/\s/g, "");
 
-var inputString = "Hello World!";
-var result = removeVowels(inputString);
-console.log(result);  // Output: Hll Wrld!
+console.log(result);
+Hello,world!Howareyou?
