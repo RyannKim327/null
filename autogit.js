1d745ec9c067e9683d661f28137c6c9f1563d778
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:42:32 +0800

    2-13-2024 4:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,13 @@
-const array = [1, 2, 3, 4, 3, 2, 1];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // [1, 2, 3, 4]
-const array = [1, 2, 3, 4, 3, 2, 1];
-const uniqueArray = array.filter((value, index, self) => {
-    return self.indexOf(value) === index;
-});
-console.log(uniqueArray); // [1, 2, 3, 4]
-const array = [1, 2, 3, 4, 3, 2, 1];
-const uniqueArray = [];
-for (const element of array) {
-    if (!uniqueArray.includes(element)) {
-        uniqueArray.push(element);
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
+  }
+  return true;
 }
-console.log(uniqueArray); // [1, 2, 3, 4]
+const array1 = [1, 2, 3, 4, 5];
+console.log(isSortedAscending(array1));  // Output: true
+
+const array2 = [5, 3, 7, 2, 1];
+console.log(isSortedAscending(array2));  // Output: false
