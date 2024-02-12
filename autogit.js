16bed8392c04de0f90a2b206b882f3113d7a2df3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:46:07 +0800

    2-13-2024 4:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,69 +1,21 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinaryTree {
-  constructor() {
-    this.root = null;
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
   }
 
-  insert(value) {
-    const newNode = new Node(value);
-
-    if (!this.root) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (!node.left) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (!node.right) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  search(value) {
-    return this.searchNode(this.root, value);
-  }
-
-  searchNode(node, value) {
-    if (node === null) {
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
       return false;
     }
-
-    if (value === node.value) {
-      return true;
-    }
-
-    if (value < node.value) {
-      return this.searchNode(node.left, value);
-    }
-
-    return this.searchNode(node.right, value);
   }
+
+  return true;
 }
-const bst = new BinaryTree();
+const number = 37;
 
-bst.insert(5);
-bst.insert(3);
-bst.insert(8);
-bst.insert(2);
-bst.insert(4);
-
-console.log(bst.search(4)); // Output: true
-console.log(bst.search(9)); // Output: false
+if (isPrime(number)) {
+  console.log(`${number} is a prime number.`);
+} else {
+  console.log(`${number} is not a prime number.`);
+}
+37 is a prime number.
