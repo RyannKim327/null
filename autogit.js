46bbaeb4b7b8f7edb9232e30a96b96789548c7ac
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:54:12 +0800

    2-13-2024 4:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,27 @@
-function tarjanSCC(graph) {
-  let index = 0;
-  let stack = [];
-  let id = 0;
-  let ids = [];
-  let lowLink = [];
-  let onStack = [];
-  let result = [];
+function findLongestCommonPrefix(strings) {
+  if (strings.length === 0) {
+    return '';
+  }
 
-  function dfs(node) {
-    ids[node] = index;
-    lowLink[node] = index;
-    index++;
-    stack.push(node);
-    onStack[node] = true;
+  let prefix = strings[0];
 
-    for (let neighbor of graph[node]) {
-      if (ids[neighbor] === undefined) {
-        dfs(neighbor);
-      }
-      if (onStack[neighbor]) {
-        lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
-      }
+  for (let i = 1; i < strings.length; i++) {
+    const currentString = strings[i];
+    let j = 0;
+
+    while (j < prefix.length && j < currentString.length && prefix[j] === currentString[j]) {
+      j++;
     }
 
-    if (ids[node] === lowLink[node]) {
-      let component = [];
-      let currNode;
-      do {
-        currNode = stack.pop();
-        onStack[currNode] = false;
-        component.push(currNode);
-      } while (currNode !== node);
-      result.push(component);
+    prefix = prefix.slice(0, j);
+
+    if (prefix === '') {
+      break;
     }
   }
 
-  for (let node = 0; node < graph.length; node++) {
-    if (ids[node] === undefined) {
-      dfs(node);
-    }
-  }
-
-  return result;
+  return prefix;
 }
-let graph = [
-  [1],       // Node 0 is connected to Node 1
-  [2],       // Node 1 is connected to Node 2
-  [0, 3],    // Node 2 is connected to Node 0 and Node 3
-  [4, 5],    // Node 3 is connected to Node 4 and Node 5
-  [3],       // Node 4 is connected to Node 3
-  [5, 6],    // Node 5 is connected to Node 5 and Node 6
-  [6, 7],    // Node 6 is connected to Node 6, Node 7
-  [7, 5],    // Node 7 is connected to Node 7 and Node 5
-];
-
-let components = tarjanSCC(graph);
-console.log(components);
-[
-  [5, 6, 7],
-  [3, 4],
-  [0, 1, 2]
-]
+const strings = ['flower', 'flow', 'flight'];
+const longestCommonPrefix = findLongestCommonPrefix(strings);
+console.log(longestCommonPrefix); // Output: 'fl'
