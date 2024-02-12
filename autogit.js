adc5a2c823e18dcb1750daf8f9fa549f84b7eed3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:49:48 +0800

    2-13-2024 4:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,63 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swapping elements
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+function createLinkedList(values) {
+  let head = null;
+  let tail = null;
+
+  for (let i = 0; i < values.length; i++) {
+    let newNode = new Node(values[i]);
+
+    if (!head) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
     }
   }
-  return arr;
-}
 
-// Example usage:
-var unsortedArray = [4, 2, 7, 1, 3];
-var sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 7]
+  return head;
+}
+function isLinkedListPalindrome(head) {
+  // Step 1: Find the middle of the linked list
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Step 2: Reverse the second half of the linked list
+  let prev = null;
+  let curr = slow;
+
+  while (curr) {
+    let nextNode = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextNode;
+  }
+
+  // Step 3: Compare the first half of the linked list with the reversed second half
+  let firstHalf = head;
+  let secondHalf = prev;
+
+  while (secondHalf) {
+    if (firstHalf.value !== secondHalf.value) {
+      return false;
+    }
+
+    firstHalf = firstHalf.next;
+    secondHalf = secondHalf.next;
+  }
+
+  return true;
+}
+let myList = createLinkedList([1, 2, 3, 2, 1]);
+
+console.log(isLinkedListPalindrome(myList)); // Output: true
