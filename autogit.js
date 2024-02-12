5949d71083c3c4b50a6d60e8e628b3d6415c0ed1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:01:00 +0800

    2-13-2024 5:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,10 @@
-let str = "HELLO WORLD";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Output: hello world
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
+}
+
+// Example usage
+var base = 5;
+var height = 6;
+var triangleArea = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + triangleArea);
