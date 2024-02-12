c63bcfd18d513482a7c8e725233cbf9474683dcc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:45:12 +0800

    2-13-2024 4:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,59 @@
-function tarjanSCC(graph) {
-  let index = 0;
-  const stack = [];
-  const indices = new Array(graph.length);
-  const lowLinks = new Array(graph.length);
-  const onStack = new Array(graph.length).fill(false);
-  const components = [];
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+}
+getParentIndex(index) {
+  return Math.floor((index - 1) / 2);
+}
+getLeftChildIndex(index) {
+  return 2 * index + 1;
+}
+getRightChildIndex(index) {
+  return 2 * index + 2;
+}
+swap(index1, index2) {
+  [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+}
+enqueue(value, priority) {
+  const element = { value, priority };
+  this.heap.push(element);
+  this.bubbleUp();
+}
+bubbleUp() {
+  let index = this.heap.length - 1;
+  while (index > 0) {
+    const parentIndex = this.getParentIndex(index);
+    if (this.heap[index].priority <= this.heap[parentIndex].priority) break;
+    this.swap(index, parentIndex);
+    index = parentIndex;
+  }
+}
+dequeue() {
+  if (this.heap.length === 0) return null;
+  if (this.heap.length === 1) return this.heap.pop().value;
 
-  function strongConnect(node) {
-    indices[node] = index;
-    lowLinks[node] = index;
-    index++;
-    stack.push(node);
-    onStack[node] = true;
+  const maxValue = this.heap[0].value;
+  this.heap[0] = this.heap.pop();
+  this.sinkDown(0);
+  return maxValue;
+}
+sinkDown(index) {
+  const leftChildIndex = this.getLeftChildIndex(index);
+  const rightChildIndex = this.getRightChildIndex(index);
+  const length = this.heap.length;
+  let largestIndex = index;
 
-    graph[node].forEach(neighbor => {
-      if (indices[neighbor] === undefined) {
-        strongConnect(neighbor);
-        lowLinks[node] = Math.min(lowLinks[node], lowLinks[neighbor]);
-      } else if (onStack[neighbor]) {
-        lowLinks[node] = Math.min(lowLinks[node], indices[neighbor]);
-      }
-    });
-
-    if (lowLinks[node] === indices[node]) {
-      const component = [];
-      let curNode;
-      do {
-        curNode = stack.pop();
-        onStack[curNode] = false;
-        component.push(curNode);
-      } while (curNode !== node);
-
-      components.push(component);
-    }
+  if (leftChildIndex < length && this.heap[leftChildIndex].priority > this.heap[largestIndex].priority) {
+    largestIndex = leftChildIndex;
   }
 
-  for (let i = 0; i < graph.length; i++) {
-    if (indices[i] === undefined) {
-      strongConnect(i);
-    }
+  if (rightChildIndex < length && this.heap[rightChildIndex].priority > this.heap[largestIndex].priority) {
+    largestIndex = rightChildIndex;
   }
 
-  return components;
+  if (largestIndex !== index) {
+    this.swap(index, largestIndex);
+    this.sinkDown(largestIndex);
+  }
 }
