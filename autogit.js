b73d5f7b491a86031a8f608c2d7d5c6c7cc8c1d9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:57:43 +0800

    2-13-2024 4:57 [User mode]

--- autogit.js
+++
@@ -1,8 +0,0 @@
-const string = 'Hello, World!';
-const substring = 'World';
-
-if (string.includes(substring)) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
-}
