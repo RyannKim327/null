c2b53c0a1f8201b0bcc072d2503b38ca9b8e465a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:43:43 +0800

    2-13-2024 4:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,12 @@
-function bwt(input) {
-  // Construct an array of all rotations of the input string
-  const rotations = [];
-  for (let i = 0; i < input.length; i++) {
-    rotations.push(input.slice(i) + input.slice(0, i));
-  }
-
-  // Sort the rotations lexicographically
-  rotations.sort();
-
-  // Extract the last characters from each rotation to construct the BWT result
-  const bwtResult = rotations.map(rotation => rotation.charAt(rotation.length - 1)).join('');
-  
-  return bwtResult;
-}
-const inputString = 'banana';
-const bwtResult = bwt(inputString);
-console.log('BWT Result:', bwtResult);
-function bwt(input) {
-  const rotations = [];
-  for (let i = 0; i < input.length; i++) {
-    rotations.push(input.slice(i) + input.slice(0, i));
-  }
-
-  rotations.sort();
-
-  const bwtResult = rotations.map(rotation => rotation.charAt(rotation.length - 1)).join('');
-
-  return bwtResult;
-}
-
-const inputString = 'banana';
-const bwtResult = bwt(inputString);
-console.log('BWT Result:', bwtResult);
-BWT Result: annb$aa
+let str = "Hello World!";
+let newStr = str.replace(/\s/g, "");
+console.log(newStr); // Output: "HelloWorld!"
+let str = "Hello World!";
+let newStr = str.split(" ").join("");
+console.log(newStr); // Output: "HelloWorld!"
+let str = "Hello World!";
+let newStr = str.replaceAll(" ", "");
+console.log(newStr); // Output: "HelloWorld!"
+let str = "   Hello World!    ";
+let newStr = str.trim();
+console.log(newStr); // Output: "Hello World!"
