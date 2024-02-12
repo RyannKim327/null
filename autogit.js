dc568b4ae5c7c09a135cd4a5175920dfc8f6d062
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:05:22 +0800

    2-13-2024 5:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,97 @@
-let str = "Hello, world! How are you?";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-// Remove all spaces using replace() with a regular expression
-let result = str.replace(/\s/g, "");
+  // Helper method to create a new node
+  createNode(value) {
+    return new Node(value);
+  }
 
-console.log(result);
-Hello,world!Howareyou?
+  // Method to insert a value into the tree
+  insert(value) {
+    const newNode = this.createNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // Helper method to recursively insert a value in the tree
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Method to search for a value in the tree
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  // Helper method to recursively search for a value in the tree
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    }
+
+    return true; // value found
+  }
+
+  // Method to traverse the tree in-order (left, root, right)
+  inOrderTraversal(callback) {
+    this.inOrderTraversalNode(this.root, callback);
+  }
+
+  // Helper method to recursively traverse the tree in-order
+  inOrderTraversalNode(node, callback) {
+    if (node !== null) {
+      this.inOrderTraversalNode(node.left, callback);
+      callback(node.value);
+      this.inOrderTraversalNode(node.right, callback);
+    }
+  }
+}
+// Create a new instance of the binary search tree
+const bst = new BinarySearchTree();
+
+// Insert values into the tree
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(12);
+bst.insert(17);
+
+// Search for a value in the tree
+console.log(bst.search(7)); // true
+console.log(bst.search(20)); // false
+
+// Traverse the tree in-order
+bst.inOrderTraversal((value) => console.log(value));
