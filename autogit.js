225a8398902bc9ee03aba2718ebd0ae5d31461ad
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:41:10 +0800

    2-13-2024 4:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,80 @@
-class Node {
-  constructor(value, level = 0) {
-    this.value = value;
-    this.next = new Array(level + 1);
-  }
-}
-class SkipList {
+class PriorityQueue {
   constructor() {
-    this.head = new Node(-Infinity);
-    this.maxLevel = 0;
+    this.heap = [];
+  }
+  // other methods...
+}
+swapElements(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+getParentIndex(index) {
+  return Math.floor((index - 1) / 2);
+}
+
+getLeftChildIndex(index) {
+  return (2 * index) + 1;
+}
+
+getRightChildIndex(index) {
+  return (2 * index) + 2;
+}
+hasParent(index) {
+  return this.getParentIndex(index) >= 0;
+}
+
+hasLeftChild(index) {
+  return this.getLeftChildIndex(index) < this.heap.length;
+}
+
+hasRightChild(index) {
+  return this.getRightChildIndex(index) < this.heap.length;
+}
+getParent(index) {
+  return this.heap[this.getParentIndex(index)];
+}
+
+getLeftChild(index) {
+  return this.heap[this.getLeftChildIndex(index)];
+}
+
+getRightChild(index) {
+  return this.heap[this.getRightChildIndex(index)];
+}
+bubbleUp(index) {
+  while (this.hasParent(index) && this.heap[index] < this.getParent(index)) {
+    const parentIndex = this.getParentIndex(index);
+    this.swapElements(this.heap, index, parentIndex);
+    index = parentIndex;
   }
 }
-SkipList.prototype.insert = function(value) {
-  const newNode = new Node(value, this.randomLevel());
-  
-  while (this.maxLevel < newNode.next.length) {
-    this.head.next.push(null);
-    this.maxLevel++;
-  }
-  
-  let current = this.head;
-  
-  for (let i = this.maxLevel; i >= 0; i--) {
-    while (current.next[i] && current.next[i].value < value) {
-      current = current.next[i];
+bubbleDown(index) {
+  while (this.hasLeftChild(index)) {
+    let smallerChildIndex = this.getLeftChildIndex(index);
+    if (this.hasRightChild(index) && this.getRightChild(index) < this.getLeftChild(index)) {
+      smallerChildIndex = this.getRightChildIndex(index);
     }
-    newNode.next[i] = current.next[i];
-    current.next[i] = newNode;
-  }
-};
-SkipList.prototype.search = function(value) {
-  let current = this.head;
-  
-  for (let i = this.maxLevel; i >= 0; i--) {
-    while (current.next[i] && current.next[i].value < value) {
-      current = current.next[i];
+    if (this.heap[index] < this.heap[smallerChildIndex]) {
+      break;
     }
+    this.swapElements(this.heap, index, smallerChildIndex);
+    index = smallerChildIndex;
   }
-  
-  current = current.next[0];
-  
-  if (current && current.value === value) {
-    return current;
+}
+enqueue(value) {
+  this.heap.push(value);
+  this.bubbleUp(this.heap.length - 1);
+}
+dequeue() {
+  if (this.heap.length === 0) {
+    return null;
   }
-  
-  return null;
-};
-SkipList.prototype.remove = function(value) {
-  let current = this.head;
-  let removed = null;
-  
-  for (let i = this.maxLevel; i >= 0; i--) {
-    while (current.next[i] && current.next[i].value < value) {
-      current = current.next[i];
-    }
-    if (current.next[i] && current.next[i].value === value) {
-      removed = current.next[i];
-      current.next[i] = current.next[i].next[i];
-    }
+  if (this.heap.length === 1) {
+    return this.heap.pop();
   }
-  
-  return removed;
-};
-SkipList.prototype.randomLevel = function() {
-  let level = 0;
-  while (Math.random() < 0.5) {
-    level++;
-  }
-  return level;
-};
-const list = new SkipList();
-
-list.insert(10);
-list.insert(5);
-list.insert(15);
-
-console.log(list.search(5)); // Output: Node { value: 5, next: [ Node ] }
-console.log(list.search(20)); // Output: null
-
-list.remove(5);
-
-console.log(list.search(5)); // Output: null
+  const priorityElement = this.heap[0];
+  this.heap[0] = this.heap.pop();
+  this.bubbleDown(0);
+  return priorityElement;
+}
