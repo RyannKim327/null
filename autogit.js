86a24cd3944cbf9d962aef02e89aa27896e4e24c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:02:10 +0800

    2-13-2024 5:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,25 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function bubbleSort(arr) {
+  var len = arr.length;
+  var swapped;
+  
+  do {
+    swapped = false;
+    
+    for (var i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        var temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  
+  return arr;
 }
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
 
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.isEmpty()) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-    this.size++;
-  }
+// Usage:
+var unsortedArray = [5, 3, 8, 2, 1, 4];
+var sortedArray = bubbleSort(unsortedArray);
 
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-
-    const removedNode = this.head;
-    this.head = this.head.next;
-    this.size--;
-
-    if (this.isEmpty()) {
-      this.tail = null;
-    }
-
-    return removedNode.value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  getSize() {
-    return this.size;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    return this.head.value;
-  }
-}
-const queue = new Queue();
-queue.enqueue('a');
-queue.enqueue('b');
-queue.enqueue('c');
-console.log(queue.dequeue()); // Output: 'a'
-console.log(queue.peek()); // Output: 'b'
-console.log(queue.getSize()); // Output: 2
-console.log(queue.isEmpty()); // Output: false
+console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
