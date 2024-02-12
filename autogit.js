823940828379bcdae803152e46b65a9b0b9f0d0c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:37:40 +0800

    2-13-2024 4:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,11 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
-  
-  insert(value) {
-    const newNode = new Node(value);
-    
-    if (!this.root) {
-      this.root = newNode;
-      return;
-    }
-    
-    let current = this.root;
-    while (true) {
-      if (value < current.value) {
-        if (!current.left) {
-          current.left = newNode;
-          break;
-        }
-        current = current.left;
-      } else {
-        if (!current.right) {
-          current.right = newNode;
-          break;
-        }
-        current = current.right;
-      }
-    }
-  }
-  
-  search(value) {
-    let current = this.root;
-    
-    while (current) {
-      if (value === current.value) {
-        return true;
-      }
-      
-      if (value < current.value) {
-        current = current.left;
-      } else {
-        current = current.right;
-      }
-    }
-    
-    return false;
-  }
-}
-const tree = new BinaryTree();
-tree.insert(5);
-tree.insert(3);
-tree.insert(8);
-tree.insert(2);
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-console.log(tree.search(3));  // Output: true
-console.log(tree.search(6));  // Output: false
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
