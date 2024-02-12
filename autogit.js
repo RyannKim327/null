57342007cf1c5d988d8d98d9d1213a57e3f9804e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:47:36 +0800

    2-13-2024 4:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,90 +1,163 @@
-class TrieNode {
-  constructor() {
-    this.children = {};
-    this.isEndOfWord = false;
+// Class for a single node in the Red-Black Tree
+class RBTreeNode {
+  constructor(value, color, parent, left, right) {
+    this.value = value;       // Value stored in the node
+    this.color = color;       // Color of the node (either 'red' or 'black')
+    this.parent = parent;     // Reference to the parent node
+    this.left = left;         // Reference to the left child node
+    this.right = right;       // Reference to the right child node
   }
 }
-class Trie {
+
+// Class for the Red-Black Tree
+class RedBlackTree {
   constructor() {
-    this.root = new TrieNode();
+    this.root = null;         // Reference to the root node of the tree
   }
   
-  insert(word) {
-    let currentNode = this.root;
-    
-    // Loop through each character of the word
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      
-      // If the character doesn't exist, create a new TrieNode
-      if (!currentNode.children.hasOwnProperty(char)) {
-        currentNode.children[char] = new TrieNode();
+  // Method to insert a value into the tree
+  insert(value) {
+    const newNode = new RBTreeNode(value, 'red');
+  
+    // If the tree is empty, make the new node as the root and color it black
+    if (this.root === null) {
+      newNode.color = 'black';
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        // If the value is lesser, go left
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.left;
+        }
+        // If the value is greater or equal, go right
+        else {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.right;
+        }
       }
-      
-      // Move to the next TrieNode
-      currentNode = currentNode.children[char];
+      this.fixInsertionViolation(newNode);
     }
-    
-    // Mark the last TrieNode as the end of a word
-    currentNode.isEndOfWord = true;
   }
   
-  search(word) {
-    let currentNode = this.root;
-    
-    // Loop through each character of the word
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      
-      // If the character doesn't exist, the word doesn't exist in the Trie
-      if (!currentNode.children.hasOwnProperty(char)) {
-        return false;
+  // Method to fix the Red-Black Tree property violations after inserting a node
+  fixInsertionViolation(node) {
+    while (node && node !== this.root && node.parent.color === 'red') {
+      let uncle;
+      if (node.parent === node.parent.parent.left) {
+        // Parent is a left child, uncle is the right child of the grandparent
+        uncle = node.parent.parent.right;
+  
+        // Case 1: Uncle is red - Recolor and move up to the grandparent
+        if (uncle && uncle.color === 'red') {
+          node.parent.color = 'black';
+          uncle.color = 'black';
+          node.parent.parent.color = 'red';
+          node = node.parent.parent;
+        }
+        else {
+          // Case 2: Uncle is black and node is a right child - Rotate left and convert to Case 3
+          if (node === node.parent.right) {
+            node = node.parent;
+            this.rotateLeft(node);
+          }
+  
+          // Case 3: Uncle is black and node is a left child - Recolor and rotate right
+          node.parent.color = 'black';
+          node.parent.parent.color = 'red';
+          this.rotateRight(node.parent.parent);
+        }
+      } else {
+        // Parent is a right child, uncle is the left child of the grandparent
+        uncle = node.parent.parent.left;
+  
+        // Case 1: Uncle is red - Recolor and move up to the grandparent
+        if (uncle && uncle.color === 'red') {
+          node.parent.color = 'black';
+          uncle.color = 'black';
+          node.parent.parent.color = 'red';
+          node = node.parent.parent;
+        }
+        else {
+          // Case 2: Uncle is black and node is a left child - Rotate right and convert to Case 3
+          if (node === node.parent.left) {
+            node = node.parent;
+            this.rotateRight(node);
+          }
+  
+          // Case 3: Uncle is black and node is a right child - Recolor and rotate left
+          node.parent.color = 'black';
+          node.parent.parent.color = 'red';
+          this.rotateLeft(node.parent.parent);
+        }
       }
-      
-      // Move to the next TrieNode
-      currentNode = currentNode.children[char];
     }
-    
-    // Return true if the last TrieNode is the end of a word
-    return currentNode.isEndOfWord;
+    // Color the root node black
+    if (this.root) {
+      this.root.color = 'black';
+    }
   }
   
-  startsWith(prefix) {
-    let currentNode = this.root;
-    
-    // Loop through each character of the prefix
-    for (let i = 0; i < prefix.length; i++) {
-      const char = prefix[i];
-      
-      // If the character doesn't exist, the prefix doesn't exist in the Trie
-      if (!currentNode.children.hasOwnProperty(char)) {
-        return false;
-      }
-      
-      // Move to the next TrieNode
-      currentNode = currentNode.children[char];
+  // Method to perform a left rotation on a node
+  rotateLeft(node) {
+    const rightChild = node.right;
+    node.right = rightChild.left;
+  
+    if (rightChild.left) {
+      rightChild.left.parent = node;
     }
-    
-    // Return true since the prefix exists in the Trie
-    return true;
+  
+    rightChild.parent = node.parent;
+  
+    if (node.parent === null) {
+      this.root = rightChild;
+    } else if (node === node.parent.left) {
+      node.parent.left = rightChild;
+    } else {
+      node.parent.right = rightChild;
+    }
+  
+    rightChild.left = node;
+    node.parent = rightChild;
+  }
+  
+  // Method to perform a right rotation on a node
+  rotateRight(node) {
+    const leftChild = node.left;
+    node.left = leftChild.right;
+  
+    if (leftChild.right) {
+      leftChild.right.parent = node;
+    }
+  
+    leftChild.parent = node.parent;
+  
+    if (node.parent === null) {
+      this.root = leftChild;
+    } else if (node === node.parent.left) {
+      node.parent.left = leftChild;
+    } else {
+      node.parent.right = leftChild;
+    }
+  
+    leftChild.right = node;
+    node.parent = leftChild;
   }
 }
-// Create a new instance of the Trie
-const trie = new Trie();
-
-// Insert words into the Trie
-trie.insert("apple");
-trie.insert("banana");
-trie.insert("orange");
-
-// Search for words in the Trie
-console.log(trie.search("apple"));   // true
-console.log(trie.search("banana"));  // true
-console.log(trie.search("orange"));  // true
-console.log(trie.search("pear"));    // false
-
-// Check prefixes in the Trie
-console.log(trie.startsWith("app"));   // true
-console.log(trie.startsWith("ban"));   // true
-console.log(trie.startsWith("oran"));  // true
-console.log(trie.startsWith("pea"));   // false
+const tree = new RedBlackTree();
+tree.insert(10);
+tree.insert(7);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(12);
+tree.insert(18);
