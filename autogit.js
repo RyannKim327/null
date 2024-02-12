4306a640524ac87c5a74dbaa177a921a7a286973
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:48:49 +0800

    2-13-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,90 +1,25 @@
-class LinkedListNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  // Add a node to the end of the linked list
-  addNode(value) {
-    const newNode = new LinkedListNode(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  // Remove the first node with a given value
-  removeNode(value) {
-    if (!this.head) {
-      return;
-    }
-
-    // If the head node is the one to remove
-    if (this.head.value === value) {
-      this.head = this.head.next;
-
-      // If the list becomes empty, update the tail
-      if (!this.head) {
-        this.tail = null;
+function shellSort(arr) {
+  let gap = Math.floor(arr.length / 2);
+  
+  while (gap > 0) {
+    for (let i = gap; i < arr.length; i++) {
+      let temp = arr[i];
+      let j = i;
+      
+      while (j >= gap && arr[j - gap] > temp) {
+        arr[j] = arr[j - gap];
+        j -= gap;
       }
-
-      return;
+      
+      arr[j] = temp;
     }
-
-    let currentNode = this.head;
-    let prevNode = null;
-
-    // Traverse the list to find the node to remove
-    while (currentNode !== null && currentNode.value !== value) {
-      prevNode = currentNode;
-      currentNode = currentNode.next;
-    }
-
-    // If the node is found, remove it
-    if (currentNode !== null && currentNode.value === value) {
-      prevNode.next = currentNode.next;
-
-      // If the tail node is removed, update the tail
-      if (currentNode === this.tail) {
-        this.tail = prevNode;
-      }
-    }
+    
+    gap = Math.floor(gap / 2);
   }
-
-  // Print the values of all nodes in the linked list
-  printList() {
-    let currentNode = this.head;
-    const values = [];
-
-    while (currentNode !== null) {
-      values.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-
-    console.log(values.join(' -> '));
-  }
+  
+  return arr;
 }
 
 // Example usage:
-const linkedList = new LinkedList();
-
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-
-linkedList.printList(); // 1 -> 2 -> 3
-
-linkedList.removeNode(2);
-
-linkedList.printList(); // 1 -> 3
+const arr = [9, 5, 1, 8, 4, 3, 2, 7, 6];
+console.log(shellSort(arr)); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
