15fb3232414819362d494267e27e4464e5334581
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 05:02:03 +0800

    2-13-2024 5:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,64 @@
-const cron = require('cron');
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
 
-// Define your cron job
-const job = new cron.CronJob('0 0 12 * * *', () => {
-  // This function will be executed every day at 12:00 PM
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
 
-  // Write your logic here
-  console.log('Running task...');
-  // Do something meaningful
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
 
-}, null, true, 'UTC');
+    const removedNode = this.head;
+    this.head = this.head.next;
+    this.size--;
 
-// Start the cron job
-job.start();
+    if (this.isEmpty()) {
+      this.tail = null;
+    }
 
-// Run indefinitely
-setInterval(() => {}, 1000);
+    return removedNode.value;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.head.value;
+  }
+}
+const queue = new Queue();
+queue.enqueue('a');
+queue.enqueue('b');
+queue.enqueue('c');
+console.log(queue.dequeue()); // Output: 'a'
+console.log(queue.peek()); // Output: 'b'
+console.log(queue.getSize()); // Output: 2
+console.log(queue.isEmpty()); // Output: false
