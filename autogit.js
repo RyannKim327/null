a97a91d304c07cb991de50a2fb55535f670e8cf9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:59:16 +0800

    2-13-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,110 @@
-function findFirstRepeatedChar(str) {
-  var charSet = new Set();
-
-  for (var i = 0; i < str.length; i++) {
-    var char = str[i];
-    if (charSet.has(char)) {
-      return char;
-    }
-    charSet.add(char);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-
-  return null;  // If no repeated character is found
 }
 
-// Example usage:
-var inputString = "Hello World!";
-var result = findFirstRepeatedChar(inputString);
-console.log(result);  // Output: l
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    }
+
+    return true;
+  }
+
+  delete(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+  deleteNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      node.left = this.deleteNode(node.left, value);
+    } else if (value > node.value) {
+      node.right = this.deleteNode(node.right, value);
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+      } else if (node.left === null) {
+        node = node.right;
+      } else if (node.right === null) {
+        node = node.left;
+      } else {
+        const minNode = this.findMinNode(node.right);
+        node.value = minNode.value;
+        node.right = this.deleteNode(node.right, minNode.value);
+      }
+    }
+
+    return node;
+  }
+
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    }
+
+    return this.findMinNode(node.left);
+  }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+console.log(bst.search(60)); // true
+console.log(bst.search(90)); // false
+
+bst.delete(20);
+console.log(bst.search(20)); // false
