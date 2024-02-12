92d68fa9fe0600b04bb5dfd7907a51014f937e7a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-13 04:57:49 +0800

    2-13-2024 4:57 [User mode]

---
+++ autogit.js
@@ -0,0 +1,8 @@
+function removeVowels(str) {
+  return str.replace(/[aeiou]/gi, '');
+}
+
+// Example usage
+const input = "Hello, World!";
+const result = removeVowels(input);
+console.log(result); // Output: Hll, Wrld!
