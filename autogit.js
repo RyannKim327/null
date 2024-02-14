03e2eb12f239ce1c1604953a45d04dd05ac5c01b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:57:24 +0800

    2-14-2024 9:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,15 @@
-function bubbleSort(arr) {
-  let len = arr.length;
-  
-  for (let i = 0; i < len; i++) {
-    for (let j = 0; j < len - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        // Swapping elements
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  
-  return arr;
-}
+// Prompt the user for input
+var num1 = prompt("Enter the first number: "); // Input for the first number
+var num2 = prompt("Enter the second number: "); // Input for the second number
 
-// Example usage
-let array = [5, 8, 2, 1, 6, 3, 7, 4];
-console.log("Before sorting:", array);
-console.log("After sorting:", bubbleSort(array));
+// Convert the user input to numbers
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+// Perform a simple calculation
+var sum = num1 + num2;
+var product = num1 * num2;
+
+// Display the result
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+console.log("The product of " + num1 + " and " + num2 + " is: " + product);
