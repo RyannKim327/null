36b5fff0fbb4f78db277225901f990176afd83aa
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:50:17 +0800

    2-14-2024 9:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,10 @@
-function biDirectionalSearch(graph, startNode, goalNode) {
-  // Create forward and backward queues
-  let forwardQueue = [startNode];
-  let backwardQueue = [goalNode];
+let string1 = "Hello";
+let string2 = "World";
 
-  // Create forward and backward visited sets
-  let forwardVisited = new Set();
-  let backwardVisited = new Set();
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString); // Output: Hello World
+let string1 = "Hello";
+let string2 = "World";
 
-  // Create forward and backward parents maps
-  let forwardParents = {};
-  let backwardParents = {};
-
-  // Initialize start and goal nodes
-  forwardVisited.add(startNode);
-  backwardVisited.add(goalNode);
-  forwardParents[startNode] = null;
-  backwardParents[goalNode] = null;
-
-  // Perform the search
-  while (forwardQueue.length && backwardQueue.length) {
-    let currentForward = forwardQueue.shift();
-    let currentBackward = backwardQueue.shift();
-
-    // Check for common nodes
-    if (forwardVisited.has(currentBackward) || backwardVisited.has(currentForward)) {
-      let commonNode = forwardVisited.has(currentBackward) ? currentBackward : currentForward;
-      return reconstructPath(forwardParents, backwardParents, commonNode);
-    }
-
-    // Expand forward search
-    for (let neighbor of graph[currentForward]) {
-      if (!forwardVisited.has(neighbor)) {
-        forwardVisited.add(neighbor);
-        forwardParents[neighbor] = currentForward;
-        forwardQueue.push(neighbor);
-      }
-    }
-
-    // Expand backward search
-    for (let neighbor of graph[currentBackward]) {
-      if (!backwardVisited.has(neighbor)) {
-        backwardVisited.add(neighbor);
-        backwardParents[neighbor] = currentBackward;
-        backwardQueue.push(neighbor);
-      }
-    }
-  }
-
-  // No path found
-  return null;
-}
-
-function reconstructPath(forwardParents, backwardParents, commonNode) {
-  let path = [commonNode];
-
-  // Traverse from start to common node
-  let current = commonNode;
-  while (forwardParents[current]) {
-    current = forwardParents[current];
-    path.unshift(current);
-  }
-
-  // Traverse from goal to common node
-  current = commonNode;
-  while (backwardParents[current]) {
-    current = backwardParents[current];
-    path.push(current);
-  }
-
-  return path;
-}
+let concatenatedString = string1.concat(" ", string2);
+console.log(concatenatedString); // Output: Hello World
