8260605cd0cef4abf2948625f7407fdc55ee53d6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:56:03 +0800

    2-14-2024 9:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,58 @@
-const axios = require('axios');
+class HashTable {
+  constructor() {
+    this.table = {}; // object to store key-value pairs
+  }
 
-// Define the URL endpoint you want to make a GET request to
-const url = 'https://jsonplaceholder.typicode.com/posts';
+  // Generate a hash for the given key
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash;
+  }
 
-// Make the HTTP GET request using Axios
-axios.get(url)
-  .then(response => {
-    // Handle the response data
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the request
-    console.error('Error:', error);
-  });
+  // Add a key-value pair to the hash table
+  put(key, value) {
+    const index = this.hash(key);
+    this.table[index] = value;
+  }
+
+  // Retrieve a value based on the key
+  get(key) {
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table.hasOwnProperty(index)) {
+      delete this.table[index];
+    }
+  }
+
+  // Check if a key exists in the hash table
+  contains(key) {
+    const index = this.hash(key);
+    return this.table.hasOwnProperty(index);
+  }
+
+  // Get all keys in the hash table
+  getKeys() {
+    return Object.keys(this.table);
+  }
+}
+
+// Test the hash table implementation
+const myHashTable = new HashTable();
+myHashTable.put('a', 'apple');
+myHashTable.put('b', 'banana');
+myHashTable.put('c', 'cat');
+
+console.log(myHashTable.get('a')); // Output: apple
+console.log(myHashTable.contains('b')); // Output: true
+
+myHashTable.remove('c');
+
+console.log(myHashTable.getKeys()); // Output: ['a', 'b']
