b861dab4cb99ecf14b2ab92781bc149e5942a49c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:58:10 +0800

    2-14-2024 9:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,12 @@
-function findCommonElements(arr1, arr2) {
-  return arr1.filter(element => arr2.includes(element));
-}
+const axios = require('axios');
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-const commonElements = findCommonElements(array1, array2);
-
-console.log(commonElements); // Output: [4, 5]
-function findCommonElements(arr1, arr2) {
-  const set1 = new Set(arr1);
-  const commonElements = arr2.filter(element => set1.has(element));
-  return commonElements;
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-const commonElements = findCommonElements(array1, array2);
-
-console.log(commonElements); // Output: [4, 5]
-function findCommonElements(arr1, arr2) {
-  return arr1.reduce((common, element) => {
-    if (arr2.includes(element)) {
-      common.push(element);
-    }
-    return common;
-  }, []);
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-const commonElements = findCommonElements(array1, array2);
-
-console.log(commonElements); // Output: [4, 5]
+// Make a GET request
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    // Handle successful response
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle error
+    console.error(error);
+  });
