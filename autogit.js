7b6e95fee81b3e831f188ded1af2c3167ce1a51b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:51:16 +0800

    2-14-2024 9:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,85 +1,16 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-
-  append(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-  }
-
-  prepend(value) {
-    const newNode = new Node(value);
-
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
-    }
-  }
-
-  delete(value) {
-    if (!this.head) {
-      return;
-    }
-
-    if (this.head.value === value) {
-      this.head = this.head.next;
-      if (!this.head) {
-        this.tail = null;
-      }
-      return;
-    }
-
-    let currentNode = this.head;
-    while (currentNode.next) {
-      if (currentNode.next.value === value) {
-        currentNode.next = currentNode.next.next;
-        if (!currentNode.next) {
-          this.tail = currentNode;
-        }
-        return;
-      }
-      currentNode = currentNode.next;
-    }
-  }
-  
-  print() {
-    const values = [];
-    let currentNode = this.head;
-    while (currentNode) {
-      values.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-    console.log(values.join(' -> '));
-  }
-}
-
-// Example usage:
-
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.prepend(0);
-linkedList.append(3);
-linkedList.print(); // Output: 0 -> 1 -> 2 -> 3
-
-linkedList.delete(1);
-linkedList.print(); // Output: 0 -> 2 -> 3
+let str = "Hello World!";
+let withoutSpaces = str.split(' ').join('');
+console.log(withoutSpaces); // Output: HelloWorld!
+let str = "Hello World!";
+let withoutSpaces = str.replace(/ /g, '');
+console.log(withoutSpaces); // Output: HelloWorld!
+let str = "Hello World!";
+let withoutSpaces = str.split(' ').join('');
+console.log(withoutSpaces); // Output: HelloWorld!
+let str = "Hello World!";
+let withoutSpaces = str.replace(/\s/g, '');
+console.log(withoutSpaces); // Output: HelloWorld!
+let str = "  Hello World!  ";
+let trimmedString = str.trim();
+let withoutSpaces = trimmedString.split(' ').join('');
+console.log(withoutSpaces); // Output: HelloWorld!
