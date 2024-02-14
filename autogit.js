73f3f1af6b28c2814a45a72d5be421a3615a1a9d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:51:25 +0800

    2-14-2024 9:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,60 @@
-let str = "Hello World!";
-let withoutSpaces = str.split(' ').join('');
-console.log(withoutSpaces); // Output: HelloWorld!
-let str = "Hello World!";
-let withoutSpaces = str.replace(/ /g, '');
-console.log(withoutSpaces); // Output: HelloWorld!
-let str = "Hello World!";
-let withoutSpaces = str.split(' ').join('');
-console.log(withoutSpaces); // Output: HelloWorld!
-let str = "Hello World!";
-let withoutSpaces = str.replace(/\s/g, '');
-console.log(withoutSpaces); // Output: HelloWorld!
-let str = "  Hello World!  ";
-let trimmedString = str.trim();
-let withoutSpaces = trimmedString.split(' ').join('');
-console.log(withoutSpaces); // Output: HelloWorld!
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+function BinarySearchTree() {
+  this.root = null;
+}
+BinarySearchTree.prototype.insert = function(value) {
+  var newNode = new Node(value);
+
+  if (this.root === null) {
+    this.root = newNode;
+  } else {
+    insertNode(this.root, newNode);
+  }
+}
+
+function insertNode(node, newNode) {
+  if (newNode.value < node.value) {
+    if (node.left === null) {
+      node.left = newNode;
+    } else {
+      insertNode(node.left, newNode);
+    }
+  } else {
+    if (node.right === null) {
+      node.right = newNode;
+    } else {
+      insertNode(node.right, newNode);
+    }
+  }
+}
+
+BinarySearchTree.prototype.search = function(value) {
+  return searchNode(this.root, value);
+}
+
+function searchNode(node, value) {
+  if (node === null) {
+    return false;
+  }
+
+  if (value === node.value) {
+    return true;
+  } else if (value < node.value) {
+    return searchNode(node.left, value);
+  } else {
+    return searchNode(node.right, value);
+  }
+}
+var binarySearchTree = new BinarySearchTree();
+binarySearchTree.insert(8);
+binarySearchTree.insert(3);
+binarySearchTree.insert(10);
+binarySearchTree.insert(1);
+binarySearchTree.insert(6);
+
+console.log(binarySearchTree.search(6)); // Output: true
+console.log(binarySearchTree.search(9)); // Output: false
