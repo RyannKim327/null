b20a3281d4ff9eeb5ffe6b7a97bb3dcdbb9c77a7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:13:20 +0800

    2-14-2024 10:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,13 @@
-const str = 'Hello, world!';
-
-if (str.includes('world')) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
+Area = (base * height) / 2
+// Function to calculate the area of a triangle
+function calculateTriangleArea(base, height) {
+  var area = (base * height) / 2;
+  return area;
 }
-const str = 'Hello, world!';
-const substring = /world/;
 
-if (str.match(substring)) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
-}
+// Example usage:
+var base = 5; // length of the base of the triangle
+var height = 3; // height of the triangle
+
+var area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
