d33f3b63c04aefba148a7af21042632ddf428ff3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:51:38 +0800

    2-14-2024 9:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,9 @@
-function isArraySortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Usage example:
-const array1 = [1, 2, 3, 4, 5];
-console.log(isArraySortedAscending(array1));  // Output: true
-
-const array2 = [5, 4, 3, 2, 1];
-console.log(isArraySortedAscending(array2));  // Output: false
+fetch('https://api.example.com/random')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Do whatever you want with the fetched data
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
