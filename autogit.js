5771305732a38b22c850c99edcb5d35403f3b6d6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:02:18 +0800

    2-14-2024 10:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,18 @@
-function findFirstRepeatedChar(str) {
-  // Create an empty set to keep track of characters seen so far
-  const charSet = new Set();
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
 
-  // Loop through each character in the string
-  for (let char of str) {
-    // Check if the character is already in the set
-    if (charSet.has(char)) {
-      // If yes, return the repeated character
-      return char;
+    while (j >= 0 && arr[j] > currentValue) {
+      arr[j + 1] = arr[j];
+      j--;
     }
 
-    // If the character is not in the set, add it to the set
-    charSet.add(char);
+    arr[j + 1] = currentValue;
   }
 
-  // If no repeated character found, return null
-  return null;
+  return arr;
 }
-
-// Example usage:
-const inputString = "abcdaef";
-const repeatedChar = findFirstRepeatedChar(inputString);
-console.log("First repeated character:", repeatedChar);
+const array = [5, 2, 4, 6, 1, 3];
+const sortedArray = insertionSort(array);
+console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6]
