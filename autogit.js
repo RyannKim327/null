7d0a408920309f171602fec14e46b83711efdec4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:56:15 +0800

    2-14-2024 9:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,136 @@
-class HashTable {
-  constructor() {
-    this.table = {}; // object to store key-value pairs
-  }
-
-  // Generate a hash for the given key
-  hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash += key.charCodeAt(i);
-    }
-    return hash;
-  }
-
-  // Add a key-value pair to the hash table
-  put(key, value) {
-    const index = this.hash(key);
-    this.table[index] = value;
-  }
-
-  // Retrieve a value based on the key
-  get(key) {
-    const index = this.hash(key);
-    return this.table[index];
-  }
-
-  // Remove a key-value pair from the hash table
-  remove(key) {
-    const index = this.hash(key);
-    if (this.table.hasOwnProperty(index)) {
-      delete this.table[index];
-    }
-  }
-
-  // Check if a key exists in the hash table
-  contains(key) {
-    const index = this.hash(key);
-    return this.table.hasOwnProperty(index);
-  }
-
-  // Get all keys in the hash table
-  getKeys() {
-    return Object.keys(this.table);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-// Test the hash table implementation
-const myHashTable = new HashTable();
-myHashTable.put('a', 'apple');
-myHashTable.put('b', 'banana');
-myHashTable.put('c', 'cat');
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+  
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+  
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+  
+  searchNode(node, value) {
+    if (node === null) {
+      return false;
+    }
+    
+    if (value === node.value) {
+      return true;
+    }
+    
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    }
+    
+    return this.searchNode(node.right, value);
+  }
+  
+  remove(value) {
+    this.root = this.removeNode(this.root, value);
+  }
+  
+  removeNode(node, value) {
+    if (node === null) {
+      return null;
+    }
+    
+    if (value < node.value) {
+      node.left = this.removeNode(node.left, value);
+      return node;
+    } else if (value > node.value) {
+      node.right = this.removeNode(node.right, value);
+      return node;
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      
+      const minNode = this.findMinNode(node.right);
+      node.value = minNode.value;
+      
+      node.right = this.removeNode(node.right, minNode.value);
+      return node;
+    }
+  }
+  
+  findMinNode(node) {
+    if (node.left === null) {
+      return node;
+    }
+    
+    return this.findMinNode(node.left);
+  }
+  
+  getRootNode() {
+    return this.root;
+  }
+  
+  inorderTraversal(node, callback) {
+    if (node !== null) {
+      this.inorderTraversal(node.left, callback);
+      callback(node.value);
+      this.inorderTraversal(node.right, callback);
+    }
+  }
+}
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
+bst.insert(4);
+bst.insert(7);
+bst.insert(13);
 
-console.log(myHashTable.get('a')); // Output: apple
-console.log(myHashTable.contains('b')); // Output: true
+console.log('Inorder Traversal:');
+bst.inorderTraversal(bst.getRootNode(), value => console.log(value));
 
-myHashTable.remove('c');
+console.log('Search 7:', bst.search(7));
+console.log('Search 12:', bst.search(12));
 
-console.log(myHashTable.getKeys()); // Output: ['a', 'b']
+bst.remove(6);
+console.log('Inorder Traversal after removal:');
+bst.inorderTraversal(bst.getRootNode(), value => console.log(value));
