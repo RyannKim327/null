9bbcea95487229698bdab2f03f06594f540c2f82
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:58:54 +0800

    2-14-2024 9:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,5 @@
-function findFirstNonRepeatingChar(str) {
-  // Step 1: Count character frequencies
-  const charFrequencies = {};
-  for (const char of str) {
-    if (charFrequencies[char]) {
-      charFrequencies[char]++;
-    } else {
-      charFrequencies[char] = 1;
-    }
-  }
+let stringNumber = "42";
+let convertedNumber = parseInt(stringNumber);
 
-  // Step 2: Find first non-repeating character
-  for (const char of str) {
-    if (charFrequencies[char] === 1) {
-      return char;
-    }
-  }
-
-  // If there are no non-repeating characters
-  return null;
-}
-
-// Example usage:
-const input = "hello world";
-const firstNonRepeatingChar = findFirstNonRepeatingChar(input);
-console.log(firstNonRepeatingChar); // Output: 'h'
+console.log(convertedNumber); // Output: 42
+console.log(typeof convertedNumber); // Output: number
