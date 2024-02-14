05f4a0ce874a8d41be419081d6210ff506e1737e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:51:32 +0800

    2-14-2024 9:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,15 @@
-function Node(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-}
-function BinarySearchTree() {
-  this.root = null;
-}
-BinarySearchTree.prototype.insert = function(value) {
-  var newNode = new Node(value);
-
-  if (this.root === null) {
-    this.root = newNode;
-  } else {
-    insertNode(this.root, newNode);
-  }
-}
-
-function insertNode(node, newNode) {
-  if (newNode.value < node.value) {
-    if (node.left === null) {
-      node.left = newNode;
-    } else {
-      insertNode(node.left, newNode);
-    }
-  } else {
-    if (node.right === null) {
-      node.right = newNode;
-    } else {
-      insertNode(node.right, newNode);
+function isArraySortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
   }
+  return true;
 }
 
-BinarySearchTree.prototype.search = function(value) {
-  return searchNode(this.root, value);
-}
+// Usage example:
+const array1 = [1, 2, 3, 4, 5];
+console.log(isArraySortedAscending(array1));  // Output: true
 
-function searchNode(node, value) {
-  if (node === null) {
-    return false;
-  }
-
-  if (value === node.value) {
-    return true;
-  } else if (value < node.value) {
-    return searchNode(node.left, value);
-  } else {
-    return searchNode(node.right, value);
-  }
-}
-var binarySearchTree = new BinarySearchTree();
-binarySearchTree.insert(8);
-binarySearchTree.insert(3);
-binarySearchTree.insert(10);
-binarySearchTree.insert(1);
-binarySearchTree.insert(6);
-
-console.log(binarySearchTree.search(6)); // Output: true
-console.log(binarySearchTree.search(9)); // Output: false
+const array2 = [5, 4, 3, 2, 1];
+console.log(isArraySortedAscending(array2));  // Output: false
