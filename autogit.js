c4e66095da66027e4af50fdb789a7ce54cb6b8f5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:50:33 +0800

    2-14-2024 9:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,18 @@
-function computeLPSArray(pattern) {
-  const lps = [0];
-  let len = 0;
-  let i = 1;
+function reverseWords(str) {
+  // Split the string into an array of words
+  var wordsArray = str.split(' ');
 
-  while (i < pattern.length) {
-    if (pattern[i] === pattern[len]) {
-      len++;
-      lps[i] = len;
-      i++;
-    } else {
-      if (len !== 0) {
-        len = lps[len - 1];
-      } else {
-        lps[i] = 0;
-        i++;
-      }
-    }
-  }
+  // Reverse the order of the array
+  var reversedArray = wordsArray.reverse();
 
-  return lps;
+  // Join the array elements back into a string
+  var reversedString = reversedArray.join(' ');
+
+  return reversedString;
 }
-function KMP(text, pattern) {
-  const results = [];
-  const lps = computeLPSArray(pattern);
-  let i = 0;
-  let j = 0;
 
-  while (i < text.length) {
-    if (pattern[j] === text[i]) {
-      i++;
-      j++;
-    }
+// Example usage
+var sentence = "Hello world, how are you?";
+var reversedSentence = reverseWords(sentence);
 
-    if (j === pattern.length) {
-      results.push(i - j);
-      j = lps[j - 1];
-    } else if (i < text.length && pattern[j] !== text[i]) {
-      if (j !== 0) {
-        j = lps[j - 1];
-      } else {
-        i++;
-      }
-    }
-  }
-  
-  return results;
-}
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const matchPositions = KMP(text, pattern);
-console.log(matchPositions); // Output: [10]
+console.log(reversedSentence); // Output: "you? are how world, Hello"
