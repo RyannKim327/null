70b219f3ced4cfd613e69831c3150b486982d07e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:50:27 +0800

    2-14-2024 9:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,52 @@
-let string1 = "Hello";
-let string2 = "World";
+function computeLPSArray(pattern) {
+  const lps = [0];
+  let len = 0;
+  let i = 1;
 
-let concatenatedString = string1 + " " + string2;
-console.log(concatenatedString); // Output: Hello World
-let string1 = "Hello";
-let string2 = "World";
+  while (i < pattern.length) {
+    if (pattern[i] === pattern[len]) {
+      len++;
+      lps[i] = len;
+      i++;
+    } else {
+      if (len !== 0) {
+        len = lps[len - 1];
+      } else {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
 
-let concatenatedString = string1.concat(" ", string2);
-console.log(concatenatedString); // Output: Hello World
+  return lps;
+}
+function KMP(text, pattern) {
+  const results = [];
+  const lps = computeLPSArray(pattern);
+  let i = 0;
+  let j = 0;
+
+  while (i < text.length) {
+    if (pattern[j] === text[i]) {
+      i++;
+      j++;
+    }
+
+    if (j === pattern.length) {
+      results.push(i - j);
+      j = lps[j - 1];
+    } else if (i < text.length && pattern[j] !== text[i]) {
+      if (j !== 0) {
+        j = lps[j - 1];
+      } else {
+        i++;
+      }
+    }
+  }
+  
+  return results;
+}
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const matchPositions = KMP(text, pattern);
+console.log(matchPositions); // Output: [10]
