f821d6759d19d65bf271945e34d118a395984192
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:05:43 +0800

    2-14-2024 10:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,66 @@
-const mergedArray = array1.concat(array2);
-mergedArray.sort((a, b) => a - b);
-const middleIndex = Math.floor(mergedArray.length / 2);
-if (mergedArray.length % 2 === 0) {
-  // Array has an even number of elements
-  const median = (mergedArray[middleIndex - 1] + mergedArray[middleIndex]) / 2;
-  console.log('Median:', median);
-} else {
-  // Array has an odd number of elements
-  const median = mergedArray[middleIndex];
-  console.log('Median:', median);
-}
-function findMedian(array1, array2) {
-  const mergedArray = array1.concat(array2);
-  mergedArray.sort((a, b) => a - b);
-  const middleIndex = Math.floor(mergedArray.length / 2);
-
-  if (mergedArray.length % 2 === 0) {
-    return (mergedArray[middleIndex - 1] + mergedArray[middleIndex]) / 2;
-  } else {
-    return mergedArray[middleIndex];
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+class Queue {
+  constructor() {
+    this.head = null; // reference to the front of the queue
+    this.tail = null; // reference to the end of the queue
+  }
 
-// Usage
-const array1 = [1, 3, 5];
-const array2 = [2, 4, 6];
+  // Add an item to the end of the queue
+  enqueue(value) {
+    const newNode = new Node(value);
+    
+    if (this.isEmpty()) {
+      // If the queue is empty, update both head and tail references
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // If the queue is not empty, update the tail reference
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
 
-const median = findMedian(array1, array2);
-console.log('Median:', median);
+  // Remove and return the item from the front of the queue
+  dequeue() {
+    if (this.isEmpty()) {
+      return null; // If the queue is empty, return null
+    }
+
+    const removedNode = this.head;
+    this.head = this.head.next; // Move the head reference to the next node
+
+    if (this.head === null) {
+      // If the head becomes null after dequeue, update the tail reference
+      this.tail = null;
+    }
+    
+    return removedNode.value;
+  }
+
+  // Check if the queue is empty
+  isEmpty() {
+    return this.head === null;
+  }
+}
+const queue = new Queue();
+
+queue.enqueue(1);    // [1]
+queue.enqueue(2);    // [1, 2]
+queue.enqueue(3);    // [1, 2, 3]
+
+console.log(queue.dequeue());    // 1, [2, 3]
+console.log(queue.dequeue());    // 2, [3]
+
+console.log(queue.isEmpty());    // false
+
+queue.enqueue(4);    // [3, 4]
+
+console.log(queue.dequeue());    // 3, [4]
+console.log(queue.dequeue());    // 4, []
+
+console.log(queue.isEmpty());    // true
