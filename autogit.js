7b8337dcb1a4b25dba771a206b955e3aac2dba67
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:08:25 +0800

    2-14-2024 10:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,125 @@
-function createGraph(vertices) {
-  let graph = [];
-
-  for (let i = 0; i < vertices; i++) {
-    graph.push([]);
+// Define the RedBlackTree class
+class RedBlackTree {
+  constructor() {
+    this.root = null;
   }
 
-  return graph;
-}
-function addEdge(graph, source, destination, weight) {
-  graph[source].push({ destination, weight });
-}
-function bellmanFord(graph, vertices, source) {
-  // Step 1: Initialize distances
-  let distances = Array(vertices).fill(Infinity);
-  distances[source] = 0;
+  // Node class for the tree nodes
+  class Node {
+    constructor(value, color) {
+      this.value = value;
+      this.left = null;
+      this.right = null;
+      this.parent = null;
+      // 'color' can be either 'red' or 'black'
+      this.color = color;
+    }
+  }
 
-  // Step 2: Relax all edges
-  for (let i = 0; i < vertices - 1; i++) {
-    for (let j = 0; j < vertices; j++) {
-      for (let k = 0; k < graph[j].length; k++) {
-        let { destination, weight } = graph[j][k];
-        if (distances[j] + weight < distances[destination]) {
-          distances[destination] = distances[j] + weight;
+  // Function to insert a value into the tree
+  insert(value) {
+    const newNode = new Node(value, 'red');
+    if (this.root === null) {
+      this.root = newNode;
+      newNode.color = 'black';
+    } else {
+      let currentNode = this.root;
+      while (currentNode) {
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            newNode.parent = currentNode;
+            break;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+      this.fixInsertViolation(newNode);
+    }
+  }
+
+  // Function to fix insertion violations and restore the red-black tree properties
+  fixInsertViolation(node) {
+    while (node.parent !== null && node.parent.color === 'red') {
+      if (node.parent === node.parent.parent.left) {
+        const uncle = node.parent.parent.right;
+        if (uncle && uncle.color === 'red') {
+          node.parent.color = 'black';
+          uncle.color = 'black';
+          node.parent.parent.color = 'red';
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.right) {
+            node = node.parent;
+            this.leftRotate(node);
+          }
+          node.parent.color = 'black';
+          node.parent.parent.color = 'red';
+          this.rightRotate(node.parent.parent);
+        }
+      } else {
+        const uncle = node.parent.parent.left;
+        if (uncle && uncle.color === 'red') {
+          node.parent.color = 'black';
+          uncle.color = 'black';
+          node.parent.parent.color = 'red';
+          node = node.parent.parent;
+        } else {
+          if (node === node.parent.left) {
+            node = node.parent;
+            this.rightRotate(node);
+          }
+          node.parent.color = 'black';
+          node.parent.parent.color = 'red';
+          this.leftRotate(node.parent.parent);
         }
       }
     }
+    this.root.color = 'black';
   }
 
-  // Step 3: Check for negative cycle
-  for (let j = 0; j < vertices; j++) {
-    for (let k = 0; k < graph[j].length; k++) {
-      let { destination, weight } = graph[j][k];
-      if (distances[j] + weight < distances[destination]) {
-        // Negative cycle found, return an empty array
-        return [];
-      }
+  // Function to perform a left rotation
+  leftRotate(node) {
+    const pivot = node.right;
+    node.right = pivot.left;
+    if (pivot.left !== null) {
+      pivot.left.parent = node;
     }
+    pivot.parent = node.parent;
+    if (node.parent === null) {
+      this.root = pivot;
+    } else if (node === node.parent.left) {
+      node.parent.left = pivot;
+    } else {
+      node.parent.right = pivot;
+    }
+    pivot.left = node;
+    node.parent = pivot;
   }
 
-  return distances;
+  // Function to perform a right rotation
+  rightRotate(node) {
+    const pivot = node.left;
+    node.left = pivot.right;
+    if (pivot.right !== null) {
+      pivot.right.parent = node;
+    }
+    pivot.parent = node.parent;
+    if (node.parent === null) {
+      this.root = pivot;
+    } else if (node === node.parent.right) {
+      node.parent.right = pivot;
+    } else {
+      node.parent.left = pivot;
+    }
+    pivot.right = node;
+    node.parent = pivot;
+  }
 }
-let graph = createGraph(5);
-addEdge(graph, 0, 1, 6);
-addEdge(graph, 0, 2, 7);
-addEdge(graph, 1, 2, 8);
-addEdge(graph, 1, 3, -4);
-addEdge(graph, 1, 4, 5);
-addEdge(graph, 2, 3, 9);
-addEdge(graph, 2, 4, -3);
-addEdge(graph, 3, 1, 2);
-addEdge(graph, 4, 3, 7);
-
-let shortestPaths = bellmanFord(graph, 5, 0);
-console.log(shortestPaths);
