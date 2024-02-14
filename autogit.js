5c50c5e672a45ce7a9a6f6a1c2d29e1c8f2677be
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:49:59 +0800

    2-14-2024 9:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,5 @@
-function longestCommonSubsequence(str1, str2) {
-  const m = str1.length;
-  const n = str2.length;
+// Prompt the user for input
+const userInput = prompt("Enter your name:");
 
-  // Create a 2D grid to store the LCS lengths
-  const dp = new Array(m + 1);
-  for (let i = 0; i <= m; i++) {
-    dp[i] = new Array(n + 1).fill(0);
-  }
-
-  // Compute LCS lengths
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-      }
-    }
-  }
-
-  // Reconstruct the LCS
-  let lcs = '';
-  let i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i][j - 1] > dp[i - 1][j]) {
-      j--;
-    } else {
-      i--;
-    }
-  }
-
-  return lcs;
-}
-
-// Example usage:
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const result = longestCommonSubsequence(str1, str2);
-console.log(result);  // Output: ADH
+// Display the user input on the console
+console.log("User input:", userInput);
