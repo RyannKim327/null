0942728b5f192fd5aba3ccc1296660ec5ebac510
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:05:01 +0800

    2-14-2024 10:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,8 @@
-function findFirstNonRepeatingChar(str) {
-    // logic goes here
-}
-    var charCount = {};
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
-    }
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-    return null;
-function findFirstNonRepeatingChar(str) {
-    var charCount = {};
-
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
-    }
-
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-
-    return null;
-}
-var str = "aabbcde";
-var result = findFirstNonRepeatingChar(str);
-console.log(result); // Output: "c"
+const string1 = "Hello";
+const string2 = "World";
+const result = string1 + " " + string2;
+console.log(result);  // Output: "Hello World"
+const string1 = "Hello";
+const string2 = "World";
+const result = string1.concat(" ", string2);
+console.log(result);  // Output: "Hello World"
