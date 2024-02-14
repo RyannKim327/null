509c3629b2783f49393c2bbc51fd62d7ad4fa8f0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:53:44 +0800

    2-14-2024 9:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,9 @@
-function countWordOccurrences(str, word) {
-  // Use regular expression to match word boundaries (\b) and case-insensitive flag (i)
-  const regex = new RegExp(`\\b${word}\\b`, 'gi');
-  
-  // Use match() method to get an array of all the matched occurrences
-  const matches = str.match(regex);
-  
-  // Return the number of occurrences
-  return matches ? matches.length : 0;
-}
+const axios = require('axios');
 
-// Example usage
-const text = "I have a cat. My cat is brown. The cat is cute.";
-const word = "cat";
-const count = countWordOccurrences(text, word);
-console.log(`"${word}" occurs ${count} time(s) in the text.`);
+axios.get('https://api.example.com/data')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
