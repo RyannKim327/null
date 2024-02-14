7b2e1703ac0d293d375fd9935c4fcada186b05ce
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:53:59 +0800

    2-14-2024 9:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,23 @@
-function isPalindrome(str) {
-  let left = 0;
-  let right = str.length - 1;
+function longestCommonSubsequence(str1, str2) {
+  const len1 = str1.length;
+  const len2 = str2.length;
+  const dp = new Array(len1 + 1).fill([]).map(() => new Array(len2 + 1).fill(''));
 
-  while (left < right) {
-    if (str.charAt(left) !== str.charAt(right)) {
-      return false;
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + str1[i - 1];
+      } else {
+        dp[i][j] = dp[i - 1][j].length > dp[i][j - 1].length ? dp[i - 1][j] : dp[i][j - 1];
+      }
     }
-
-    left++;
-    right--;
   }
 
-  return true;
+  return dp[len1][len2];
 }
 
-// Example usage
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello")); // false
+// Example usage:
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: ADH
