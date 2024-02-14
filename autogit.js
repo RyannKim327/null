3d2ce7660567b8ff031079414d69860cad4be89a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 09:56:35 +0800

    2-14-2024 9:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,11 @@
-const mergedArray = array1.concat(array2).sort((a, b) => a - b);
-const length = mergedArray.length;
-const middle = Math.floor(length / 2);
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
 
-if (length % 2 === 0) {
-  const median = (mergedArray[middle - 1] + mergedArray[middle]) / 2;
-  console.log(median);
-} else {
-  const median = mergedArray[middle];
-  console.log(median);
-}
-function findMedian(array1, array2) {
-  const mergedArray = array1.concat(array2).sort((a, b) => a - b);
-  const length = mergedArray.length;
-  const middle = Math.floor(length / 2);
-
-  if (length % 2 === 0) {
-    const median = (mergedArray[middle - 1] + mergedArray[middle]) / 2;
-    return median;
-  } else {
-    const median = mergedArray[middle];
-    return median;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
   }
-}
 
-const array1 = [1, 3, 5];
-const array2 = [2, 4, 6];
-console.log(findMedian(array1, array2));
+  return slow;
+}
