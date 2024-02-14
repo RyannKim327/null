ac254d5ac5231ce7992d765f5412e75e7fa2d0f8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-14 10:06:27 +0800

    2-14-2024 10:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,101 @@
-function countOccurrences(word, string) {
-  // Escape special characters in the word for use in a regular expression
-  const escapedWord = word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-
-  // Create a regular expression with the word and "g" flag for global search
-  const regex = new RegExp('\\b' + escapedWord + '\\b', 'gi');
-
-  // Use match() method on the string and count the number of matches
-  const matches = string.match(regex);
-  const count = matches ? matches.length : 0;
-
-  return count;
+class Node {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+    this.children = {};
+    this.suffixLink = null;
+  }
 }
+class SuffixTree {
+  constructor() {
+    this.root = new Node(-1, -1); // Root node
+    this.lastNewNode = null;
+    this.activeNode = this.root;
+    this.activeEdge = -1;
+    this.activeLength = 0;
+    this.remainingSuffixCount = 0;
+    this.end = -1;
+    this.length = 0;
+    this.string = ''; // Original string
+  }
 
-// Example usage
-const sentence = 'She sells seashells by the seashore.';
-const word = 'se';
+  // Function for inserting a string into the tree
+  insertString(string) {
+    this.string = string;
+    this.length = string.length;
+    for (let i = 0; i < this.length; i++) {
+      this.extendSuffixTree(i);
+    }
+  }
 
-const occurrences = countOccurrences(word, sentence);
-console.log(`The word "${word}" appears ${occurrences} time(s) in the sentence.`);
+  // Function for extending the suffix tree based on the current index
+  extendSuffixTree(pos) {
+    this.end = pos;
+    this.remainingSuffixCount++;
+    this.lastNewNode = null;
+
+    while (this.remainingSuffixCount > 0) {
+
+      if (this.activeLength === 0) {
+        this.activeEdge = pos;
+      }
+
+      if (!this.activeNode.children[this.string.charAt(this.activeEdge)]) {
+        this.activeNode.children[this.string.charAt(this.activeEdge)] = new Node(pos, this.length);
+        
+        if (this.lastNewNode !== null) {
+          this.lastNewNode.suffixLink = this.activeNode;
+          this.lastNewNode = null;
+        }
+      } else {
+        const nextNode = this.activeNode.children[this.string.charAt(this.activeEdge)];
+        if (this.doTraversal(nextNode)) {
+          continue;
+        }
+        if (this.string.charAt(nextNode.start + this.activeLength) === this.string.charAt(pos)) {
+          if (this.lastNewNode !== null && this.activeNode !== this.root) {
+            this.lastNewNode.suffixLink = this.activeNode;
+            this.lastNewNode = null;
+          }
+          this.activeLength++;
+          break;
+        }
+
+        const newNode = new Node(nextNode.start, nextNode.start + this.activeLength - 1);
+        const splitNode = new Node(pos, this.length);
+        this.activeNode.children[this.string.charAt(this.activeEdge)] = newNode;
+        newNode.children[this.string.charAt(pos)] = splitNode;
+        nextNode.start += this.activeLength;
+
+        newNode.children[this.string.charAt(nextNode.start)] = nextNode;
+
+        if (this.lastNewNode !== null) {
+          this.lastNewNode.suffixLink = newNode;
+        }
+        this.lastNewNode = newNode;
+      }
+
+      this.remainingSuffixCount--;
+      if (this.activeNode === this.root && this.activeLength > 0) {
+        this.activeLength--;
+        this.activeEdge = pos - this.remainingSuffixCount + 1;
+      } else if (this.activeNode !== this.root) {
+        this.activeNode = this.activeNode.suffixLink;
+      }
+    }
+  }
+
+  doTraversal(currentNode) {
+    const edgeLength = currentNode.end - currentNode.start + 1;
+
+    if (this.activeLength >= edgeLength) {
+      this.activeEdge += edgeLength;
+      this.activeLength -= edgeLength;
+      this.activeNode = currentNode;
+      return true;
+    }
+    return false;
+  }
+}
+const suffixTree = new SuffixTree();
+suffixTree.insertString("your_string_here");
