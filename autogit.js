f80dfc02b9504e7d027d8d317a5bcb518aed69f1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:15:28 +0800

    2-17-2024 16:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,30 @@
-function largestPrimeFactor(number) {
-    let factor = 2;
+function longestIncreasingSubsequence(arr) {
+    if (arr.length === 0) return 0;
 
-    while (factor <= number) {
-        if (number % factor === 0) {
-            number /= factor;
-        } else {
-            factor++;
+    const n = arr.length;
+    const lis = Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+                lis[i] = lis[j] + 1;
+            }
         }
     }
 
-    return factor;
+    let maxLength = Math.max(...lis);
+    let result = [];
+    let maxIndex = lis.indexOf(maxLength);
+
+    for (let i = maxIndex; i >= 0; i--) {
+        if (lis[i] === maxLength && (result.length === 0 || arr[i] < result[result.length - 1])) {
+            result.unshift(arr[i]);
+            maxLength--;
+        }
+    }
+
+    return result;
 }
 
-const number = 600851475143;
-const largestPrime = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is ${largestPrime}`);
+const arr = [10, 22, 9, 33, 21, 50, 41, 60, 80];
+console.log(longestIncreasingSubsequence(arr)); // Output: [10, 22, 33, 50, 60, 80]
