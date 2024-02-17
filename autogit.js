0bd037185f4186633afd7b6f77fed0ad0a782437
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:17:00 +0800

    2-17-2024 16:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,56 @@
-function bubbleSort(arr) {
-    var len = arr.length;
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+// Hash Table class
+class HashTable {
+  constructor() {
+    this.table = new Array(100); // Array to store key-value pairs
+  }
+
+  // Hash function to generate a hash value for the key
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * 31) % this.table.length;
     }
-    return arr;
+    return hash;
+  }
+
+  // Insert a key-value pair into the hash table
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
+  }
+
+  // Get the value associated with the key from the hash table
+  get(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      for (let i = 0; i < this.table[index].length; i++) {
+        if (this.table[index][i][0] === key) {
+          return this.table[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  // Remove a key-value pair from the hash table
+  remove(key) {
+    const index = this.hash(key);
+    if (this.table[index]) {
+      this.table[index] = this.table[index].filter(item => item[0] !== key);
+    }
+  }
 }
 
-// Usage
-var unsortedArray = [64, 34, 25, 12, 22, 11, 90];
-var sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray);
+// Example usage
+const myHashTable = new HashTable();
+myHashTable.set("apple", 5);
+myHashTable.set("banana", 8);
+
+console.log(myHashTable.get("apple")); // Output: 5
+console.log(myHashTable.get("banana")); // Output: 8
+
+myHashTable.remove("apple");
+console.log(myHashTable.get("apple")); // Output: undefined
