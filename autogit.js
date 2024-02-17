6ac44cac4cd3998514f4f4492d24257fbb02f6d0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:46:16 +0800

    2-17-2024 16:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,16 @@
-function shellSort(arr) {
-    let n = arr.length;
-    let gap = Math.floor(n / 2);
+let str = "Hello, World!";
+let subStr = "Hello";
 
-    while (gap > 0) {
-        for (let i = gap; i < n; i++) {
-            let temp = arr[i];
-            let j = i;
-
-            while (j >= gap && arr[j - gap] > temp) {
-                arr[j] = arr[j - gap];
-                j -= gap;
-            }
-
-            arr[j] = temp;
-        }
-
-        gap = Math.floor(gap / 2);
-    }
-
-    return arr;
+if (str.includes(subStr)) {
+    console.log('The string contains the substring');
+} else {
+    console.log('The string does not contain the substring');
 }
+let str = "Hello, World!";
+let subStr = "Hello";
 
-// Testing the shellSort function
-let arr = [12, 34, 54, 2, 3];
-console.log(shellSort(arr));
+if (str.indexOf(subStr) !== -1) {
+    console.log('The string contains the substring');
+} else {
+    console.log('The string does not contain the substring');
+}
