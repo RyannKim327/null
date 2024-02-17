be099df5e24bf6b144e14e4f9d7a83b82084a64d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:44:36 +0800

    2-17-2024 16:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,8 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentValue = arr[i];
-        let j = i - 1;
-        
-        while (j >= 0 && arr[j] > currentValue) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        
-        arr[j + 1] = currentValue;
-    }
-    
-    return arr;
-}
+let str = "42";
+let int = parseInt(str);
 
-// Example usage
-let unsortedArray = [5, 3, 8, 4, 2, 6, 1, 7];
-let sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(int); // Output: 42
+let str = "3.14";
+let float = parseFloat(str);
+
+console.log(float); // Output: 3.14
