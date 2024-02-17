0650c8cd7f85590142370fc7061944790f6b37e0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:18:10 +0800

    2-17-2024 16:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,78 +1,13 @@
-class PriorityQueue {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(value, priority) {
-    const node = { value, priority };
-    this.heap.push(node);
-    this.bubbleUp();
-  }
-
-  extractMin() {
-    const min = this.heap[0];
-    const end = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = end;
-      this.sinkDown();
-    }
-    return min;
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    const node = this.heap[index];
-
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.heap[parentIndex];
-
-      if (node.priority >= parent.priority) break;
-
-      this.heap[parentIndex] = node;
-      this.heap[index] = parent;
-      index = parentIndex;
+function isSortedAscending(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      return false;
     }
   }
-
-  sinkDown() {
-    let index = 0;
-    const length = this.heap.length;
-    const node = this.heap[0];
-
-    while (true) {
-      let leftChildIdx = 2 * index + 1;
-      let rightChildIdx = 2 * index + 2;
-      let leftChild, rightChild;
-      let swap = null;
-
-      if (leftChildIdx < length) {
-        leftChild = this.heap[leftChildIdx];
-        if (leftChild.priority < node.priority) {
-          swap = leftChildIdx;
-        }
-      }
-
-      if (rightChildIdx < length) {
-        rightChild = this.heap[rightChildIdx];
-        if ((swap === null && rightChild.priority < node.priority) || (swap !== null && rightChild.priority < leftChild.priority)) {
-          swap = rightChildIdx;
-        }
-      }
-
-      if (swap === null) break;
-
-      this.heap[index] = this.heap[swap];
-      this.heap[swap] = node;
-      index = swap;
-    }
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  size() {
-    return this.heap.length;
-  }
+  return true;
 }
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [5, 4, 3, 2, 1];
+
+console.log(isSortedAscending(array1)); // Output: true
+console.log(isSortedAscending(array2)); // Output: false
