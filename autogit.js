c684aabedf6ac2267236042afa2cf0b9eda4c211
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:30:40 +0800

    2-17-2024 16:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,25 @@
-function dijkstra(graph, startNode) {
-    const distances = {};
-    const visited = {};
-    const unvisited = {};
-
-    Object.keys(graph).forEach(node => {
-        distances[node] = Infinity;
-        unvisited[node] = graph[node];
-    });
-
-    distances[startNode] = 0;
-
-    while (Object.keys(unvisited).length > 0) {
-        const currentNode = findNodeWithShortestDistance(unvisited, distances);
-        const neighbors = unvisited[currentNode];
-
-        Object.keys(neighbors).forEach(neighbor => {
-            const distance = distances[currentNode] + neighbors[neighbor];
-            if (distance < distances[neighbor]) {
-                distances[neighbor] = distance;
-            }
-        });
-
-        visited[currentNode] = unvisited[currentNode];
-        delete unvisited[currentNode];
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
     }
 
-    return distances;
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-function findNodeWithShortestDistance(nodes, distances) {
-    return Object.keys(nodes).reduce((minNode, node) =>
-        distances[node] < distances[minNode] ? node : minNode
-    );
-}
+// Example usage
+const unsortedArray = [5, 3, 8, 4, 2, 1, 9, 7, 6];
+const sortedArray = quickSort(unsortedArray);
 
-// Example graph representation:
-const graph = {
-    A: { B: 3, C: 2 },
-    B: { A: 3, C: 4, D: 5 },
-    C: { A: 2, B: 4, D: 1 },
-    D: { B: 5, C: 1 }
-};
-
-const startNode = 'A';
-
-const shortestDistances = dijkstra(graph, startNode);
-console.log(shortestDistances);
+console.log(sortedArray);
