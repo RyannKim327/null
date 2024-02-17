cdd78f37908b2fb919e2ac4bbcb54530ba1325b4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:33:30 +0800

    2-17-2024 16:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,53 @@
-const str = 'Hello, World!';
-const subStr = 'World';
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
 
-if (str.includes(subStr)) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
-}
-const str = 'Hello, World!';
-const subStr = 'World';
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
 
-if (str.indexOf(subStr) !== -1) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
-}
-const str = 'Hello, World!';
-const subStr = 'World';
-const regex = new RegExp(subStr);
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
 
-if (regex.test(str)) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
+  depthFirstSearch(startingNode) {
+    const visited = {};
+    const result = [];
+
+    const dfs = (vertex) => {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      
+      this.adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    };
+
+    dfs(startingNode);
+    return result;
+  }
 }
+
+// Example usage
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+console.log(graph.depthFirstSearch('A')); // Output: [ 'A', 'B', 'D', 'E', 'C', 'F' ]
