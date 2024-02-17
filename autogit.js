775a61bac671a98789c738bf6f8049e9a9ac9564
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:24:35 +0800

    2-17-2024 16:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,52 @@
-let str = "Hello World!";
-let lowercaseStr = str.toLowerCase();
+function buildPatternTable(pattern) {
+    let table = Array(pattern.length).fill(0);
+    let j = 0;
+    
+    for (let i = 1; i < pattern.length; i++) {
+        if (pattern[i] === pattern[j]) {
+            j++;
+            table[i] = j;
+        } else {
+            if (j !== 0) {
+                j = table[j - 1];
+                i--;
+            }
+        }
+    }
+    
+    return table;
+}
 
-console.log(lowercaseStr); // Outputs: "hello world!"
+function kmpSearch(text, pattern) {
+    let patternTable = buildPatternTable(pattern);
+    let i = 0;
+    let j = 0;
+    
+    while (i < text.length) {
+        if (text[i] === pattern[j]) {
+            i++;
+            j++;
+            if (j === pattern.length) {
+                return i - j;
+            }
+        } else {
+            if (j !== 0) {
+                j = patternTable[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    return -1;
+}
+
+let text = "This is a simple example";
+let pattern = "example";
+let index = kmpSearch(text, pattern);
+
+if (index !== -1) {
+    console.log(`Pattern found at index ${index}`);
+} else {
+    console.log("Pattern not found");
+}
