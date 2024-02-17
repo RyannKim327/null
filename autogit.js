2a54f2a94c085d02bb3f3fa69683d8005dca1838
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:20:20 +0800

    2-17-2024 16:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,56 @@
-const axios = require('axios');
+function buildLPSArray(pattern) {
+    const lps = [0];
+    let len = 0;
+    let i = 1;
+    
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+    
+    return lps;
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+function kmpSearch(text, pattern) {
+    const lps = buildLPSArray(pattern);
+    const matches = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < text.length) {
+        if (pattern[j] === text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j === pattern.length) {
+            matches.push(i - j);
+            j = lps[j - 1];
+        } else if (i < text.length && pattern[j] !== text[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return matches;
+}
+
+// Example usage
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const matches = kmpSearch(text, pattern);
+
+console.log("Pattern found at indices: ", matches);
