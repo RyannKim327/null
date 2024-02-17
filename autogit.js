e79f8602ca5520b8d934d0852e5647356c7f446b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:20:10 +0800

    2-17-2024 16:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,9 @@
-function maxSubarraySum(arr) {
-    let maxSum = arr[0];
-    let currentSum = arr[0];
+const axios = require('axios');
 
-    for (let i = 1; i < arr.length; i++) {
-        currentSum = Math.max(arr[i], currentSum + arr[i]);
-        maxSum = Math.max(maxSum, currentSum);
-    }
-
-    return maxSum;
-}
-
-// Example
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const maxSum = maxSubarraySum(arr);
-console.log(maxSum); // Output should be 6, which corresponds to the maximum sum subarray [4, -1, 2, 1]
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
