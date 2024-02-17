08148d88d725b15038c1f76a5e4489238613df1a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:13:17 +0800

    2-17-2024 16:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,55 @@
-function bubbleSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+function kmpSearch(text, pattern) {
+    const patternLength = pattern.length;
+    const textLength = text.length;
+
+    // Generate the lps (longest proper prefix which is also a suffix) array for the pattern
+    const lps = new Array(patternLength).fill(0);
+    let j = 0;
+
+    generateLPSArray();
+
+    let i = 0;
+    while (i < textLength) {
+        if (pattern[j] === text[i]) {
+            j++;
+            i++;
+        }
+
+        if (j === patternLength) {
+            console.log("Pattern found at index " + (i - j));
+            j = lps[j - 1];
+        } else if (i < textLength && pattern[j] !== text[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
             }
         }
     }
-    return arr;
+
+    function generateLPSArray() {
+        let len = 0;
+        let i = 1;
+
+        while (i < patternLength) {
+            if (pattern[i] === pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len !== 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = len;
+                    i++;
+                }
+            }
+        }
+    }
 }
 
-const arr = [5, 3, 8, 1, 2, 7];
-console.log("Original array:", arr);
-console.log("Sorted array:", bubbleSort(arr));
+// Example usage
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+
+kmpSearch(text, pattern);
