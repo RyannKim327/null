feb32cee3f469971dc5e2f613e3d1ed7dd5e8755
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:27:56 +0800

    2-17-2024 16:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,64 @@
-function factorial(n) {
-    if (n === 0) {
-        return 1;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-    return n * factorial(n - 1);
 }
 
-// Test the factorial function
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is: ${result}`);
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.size++;
+    }
+
+    remove(value) {
+        let current = this.head;
+        
+        if (current.value === value) {
+            this.head = current.next;
+            this.size--;
+        } else {
+            let previous = current;
+            while (current.next) {
+                if (current.value === value) {
+                    previous.next = current.next;
+                    this.size--;
+                    break;
+                }
+                previous = current;
+                current = current.next;
+            }
+        }
+    }
+
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.print(); // Output: 1 2 3
+list.remove(2);
+list.print(); // Output: 1 3
