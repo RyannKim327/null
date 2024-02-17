7f7ce7ee81d4ebaa867aa9402b3e18a58afd4b8d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:15:19 +0800

    2-17-2024 16:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,17 @@
-let str = "Hello, World!";
-let lowerCaseStr = str.toLowerCase();
-console.log(lowerCaseStr); // Output: "hello, world!"
+function largestPrimeFactor(number) {
+    let factor = 2;
+
+    while (factor <= number) {
+        if (number % factor === 0) {
+            number /= factor;
+        } else {
+            factor++;
+        }
+    }
+
+    return factor;
+}
+
+const number = 600851475143;
+const largestPrime = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestPrime}`);
