b1935f5311adc959a381c176c22a1468e26d7a8d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:59:23 +0800

    2-17-2024 16:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,18 @@
-const axios = require('axios');
+function largestPrimeFactor(number) {
+    let factor = 2;
+    
+    while (factor <= number) {
+        if (number % factor === 0) {
+            number /= factor;
+        } else {
+            factor++;
+        }
+    }
+    
+    return factor;
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log('Response data:', response.data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+const number = 13195; // The number for which you want to find the largest prime factor
+const largestPrime = largestPrimeFactor(number);
+
+console.log(`The largest prime factor of ${number} is ${largestPrime}`);
