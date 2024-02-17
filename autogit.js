dcbddb4f8b616f0fc10af94607bdd3bce460b8f6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:58:19 +0800

    2-17-2024 16:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,5 @@
-function maxSubArray(arr) {
-    let maxEndingHere = arr[0];
-    let maxSoFar = arr[0];
+let array = [10, 5, 20, 15, 30];
 
-    for (let i = 1; i < arr.length; i++) {
-        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = Math.max(maxSoFar, maxEndingHere);
-    }
+let max = Math.max(...array);
 
-    return maxSoFar;
-}
-
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-console.log(maxSubArray(arr)); // Output: 6 (The subarray [4, -1, 2, 1] has the maximum sum)
+console.log("Maximum value in the array is: " + max);
