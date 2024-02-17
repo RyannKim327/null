cec3ddc242ecafb6f6c6ba05200a4e95039dbec7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:33:16 +0800

    2-17-2024 16:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,48 @@
-function reverseWords(str) {
-    return str.split(' ').reverse().join(' ');
+function findMedianSortedArrays(nums1, nums2) {
+    let mergedArray = mergeArrays(nums1, nums2);
+    let length = mergedArray.length;
+    
+    if (length % 2 === 0) {
+        let middleIndex = length / 2;
+        return (mergedArray[middleIndex - 1] + mergedArray[middleIndex]) / 2;
+    } else {
+        let middleIndex = Math.floor(length / 2);
+        return mergedArray[middleIndex];
+    }
 }
 
-const input = "Hello world!";
-const reversed = reverseWords(input);
-console.log(reversed); // Output: "world! Hello"
+function mergeArrays(nums1, nums2) {
+    let mergedArray = [];
+    let i = 0, j = 0;
+    
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+            mergedArray.push(nums1[i]);
+            i++;
+        } else {
+            mergedArray.push(nums2[j]);
+            j++;
+        }
+    }
+    
+    while (i < nums1.length) {
+        mergedArray.push(nums1[i]);
+        i++;
+    }
+    
+    while (j < nums2.length) {
+        mergedArray.push(nums2[j]);
+        j++;
+    }
+    
+    return mergedArray;
+}
+
+// Test the function with sample inputs
+const nums1 = [1, 3];
+const nums2 = [2];
+console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2
+
+const nums3 = [1, 2];
+const nums4 = [3, 4];
+console.log(findMedianSortedArrays(nums3, nums4)); // Output: 2.5
