51e102d4bb71cd4007374e164fb3a6338e299b9f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:58:39 +0800

    2-17-2024 16:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,24 @@
-let str = "   Hello    World    ";
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-// Using regular expression to remove whitespaces
-let trimmedStr = str.replace(/\s/g, '');
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
 
-console.log(trimmedStr); // Output: "HelloWorld"
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// Example usage
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const sortedArray = quickSort(array);
+console.log(sortedArray);
