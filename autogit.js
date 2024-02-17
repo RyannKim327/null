274178971253acbdb7e39e5c9aaef6e4ad242b2d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:34:32 +0800

    2-17-2024 16:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,35 @@
-let str = "Hello, World! This is a test string.";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces);
+// Define the graph as an adjacency list
+const graph = {
+  A: ['B', 'C'],
+  B: ['A', 'D', 'E'],
+  C: ['A', 'F'],
+  D: ['B'],
+  E: ['B', 'F'],
+  F: ['C', 'E']
+};
+
+// Breadth-First Search function
+function bfs(graph, startNode) {
+  let visited = {};
+  let queue = [];
+
+  queue.push(startNode);
+  visited[startNode] = true;
+
+  while (queue.length > 0) {
+    let currentNode = queue.shift();
+    console.log(currentNode);
+
+    let neighbors = graph[currentNode];
+    for (let i = 0; i < neighbors.length; i++) {
+      let neighbor = neighbors[i];
+      if (!visited[neighbor]) {
+        queue.push(neighbor);
+        visited[neighbor] = true;
+      }
+    }
+  }
+}
+
+// Using BFS from node 'A'
+bfs(graph, 'A');
