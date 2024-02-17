24d83ba2e2986f4041bb3cfc880e7527a49b4bde
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:36:33 +0800

    2-17-2024 16:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,68 @@
-const array = [1, 2, 3, 4, 2, 3, 5];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 4, 2, 3, 5];
-const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
-const array = [1, 2, 3, 4, 2, 3, 5];
-const uniqueArray = array.reduce((acc, current) => {
-    if (acc.indexOf(current) === -1) {
-        acc.push(current);
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-    return acc;
-}, []);
-console.log(uniqueArray); // [1, 2, 3, 4, 5]
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        let newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(data) {
+        return this.searchNode(this.root, data);
+    }
+
+    searchNode(node, data) {
+        if (node === null) {
+            return false;
+        }
+
+        if (data < node.data) {
+            return this.searchNode(node.left, data);
+        } else if (data > node.data) {
+            return this.searchNode(node.right, data);
+        } else {
+            return true;
+        }
+    }
+}
+
+// Usage example
+let binaryTree = new BinaryTree();
+binaryTree.insert(10);
+binaryTree.insert(5);
+binaryTree.insert(15);
+binaryTree.insert(3);
+binaryTree.insert(7);
+
+console.log(binaryTree.search(7)); // Output: true
+console.log(binaryTree.search(12)); // Output: false
