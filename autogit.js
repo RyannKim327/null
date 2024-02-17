13a062a29bb80873898a8f9a32e3ddf00f231029
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:30:09 +0800

    2-17-2024 16:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,5 @@
+const cron = require('node-cron');
 
-function maxSubarraySum(arr) {
-    if (arr.length === 0) {
-        return 0;
-    }
-
-    let maxEndingHere = arr[0];
-    let maxSoFar = arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = Math.max(maxSoFar, maxEndingHere);
-    }
-
-    return maxSoFar;
-}
-
-// Example usage
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-console.log(maxSubarraySum(arr)); // Output: 6
-
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute');
+});
