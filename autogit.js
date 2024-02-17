7f44777f765c7d5d0802ec20912eb033fde42650
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:51:34 +0800

    2-17-2024 16:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,46 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-const originalString = "hello";
-const reversedString = reverseString(originalString);
-console.log(reversedString); // Output: "olleh"
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+  }
+
+  findMiddle() {
+    let slow = this.head;
+    let fast = this.head;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow.value;
+  }
+}
+
+// Usage
+let ll = new LinkedList();
+ll.add(1);
+ll.add(2);
+ll.add(3);
+ll.add(4);
+ll.add(5);
+
+console.log(ll.findMiddle()); // Output: 3
