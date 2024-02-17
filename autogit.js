bde347ce93c08a4ae4276a88b8af47fb821106d2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:23:41 +0800

    2-17-2024 16:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,27 @@
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-let index = array.indexOf(elementToRemove);
-if (index > -1) {
-    array.splice(index, 1);
+function largestPrimeFactor(number) {
+    let largestPrime = 1;
+    
+    // Start from 2 (smallest prime number) and iterate up to the square root of the number
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            // Check if i is a prime number
+            let isPrime = true;
+            for (let j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime) {
+                largestPrime = i;
+            }
+        }
+    }
+    
+    return largestPrime;
 }
+const number = 56;
+const largestPrime = largestPrimeFactor(number);
 
-console.log(array); // Output: [1, 2, 4, 5]
+console.log(`The largest prime factor of ${number} is: ${largestPrime}`);
