585dfe4b5161ff3d689f1b0258427092559d885c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:17:39 +0800

    2-17-2024 16:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,25 @@
-function binarySearch(arr, target, start = 0, end = arr.length - 1) {
-    if (start > end) {
-        return -1; // If the target is not found
-    }
+let str = "Hello, world!";
+let subStr = "Hello";
 
-    const mid = Math.floor((start + end) / 2);
-
-    if (arr[mid] === target) {
-        return mid; // If the target is found
-    } else if (arr[mid] < target) {
-        return binarySearch(arr, target, mid + 1, end); // Search the right half
-    } else {
-        return binarySearch(arr, target, start, mid - 1); // Search the left half
-    }
-}
-
-// Example usage
-const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15, 17];
-const target = 7;
-const index = binarySearch(sortedArray, target);
-
-if (index !== -1) {
-    console.log(`Target ${target} found at index ${index}`);
+if (str.includes(subStr)) {
+    console.log("The string contains the substring");
 } else {
-    console.log(`Target ${target} not found in the array`);
+    console.log("The string does not contain the substring");
+}
+let str = "Hello, world!";
+let subStr = "Hello";
+
+if (str.indexOf(subStr) !== -1) {
+    console.log("The string contains the substring");
+} else {
+    console.log("The string does not contain the substring");
+}
+let str = "Hello, world!";
+let subStr = "Hello";
+let regex = new RegExp(subStr);
+
+if (regex.test(str)) {
+    console.log("The string contains the substring");
+} else {
+    console.log("The string does not contain the substring");
 }
