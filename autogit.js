7d528166edf45a16e99608e610c19b9bec343c32
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:34:22 +0800

    2-17-2024 16:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,3 @@
-function calculateMean(numbers) {
-    const sum = numbers.reduce((acc, current) => acc + current, 0);
-    const mean = sum / numbers.length;
-    return mean;
-}
-
-// Example usage
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log(`Mean: ${mean}`);
+let str = "Hello, World! This is a test string.";
+let stringWithoutSpaces = str.replace(/\s/g, '');
+console.log(stringWithoutSpaces);
