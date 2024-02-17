2227ab9d407f39390228abb774b9b69a0fa72284
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:28:39 +0800

    2-17-2024 16:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,41 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
-            count++;
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+
+    const dp = [];
+    for (let i = 0; i <= m; i++) {
+        dp[i] = [];
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 || j === 0) {
+                dp[i][j] = 0;
+            } else if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
     }
-    return count;
+
+    let i = m;
+    let j = n;
+    let index = dp[m][n];
+    const lcs = [];
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs[--index] = str1[i - 1];
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return lcs.join('');
 }
 
-// Example of using the function
-let myString = 'hello world';
-let charToCount = 'o';
-let occurrences = countOccurrences(myString, charToCount);
-console.log(`The character "${charToCount}" appears ${occurrences} times in the string.`);
+const str1 = "ABCBDAB";
+const str2 = "BDCAB";
+console.log(longestCommonSubsequence(str1, str2)); // Output: BCAB
