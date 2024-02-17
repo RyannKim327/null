4209aa0920d47f056200040c91400143deacd7e1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:36:49 +0800

    2-17-2024 16:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,8 @@
-function longestCommonSubstring(str1, str2) {
-    const dp = Array.from({ length: str1.length + 1 }, () => Array(str2.length + 1).fill(0));
-    let maxLength = 0;
-    let endIndex = 0;
+let numString = "42";
+let numInt = parseInt(numString);
 
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
-        }
-    }
+console.log(numInt); // Output: 42
+let hexString = "1A";
+let hexInt = parseInt(hexString, 16);
 
-    if (maxLength === 0) {
-        return "";
-    }
-
-    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
-}
-
-// Example usage
-const str1 = "abcdef";
-const str2 = "bcdeft";
-console.log(longestCommonSubstring(str1, str2)); // Output: "cde"
+console.log(hexInt); // Output: 26
