7bd9c8118bf68e87f04180a0193d7b6c3c2c26c8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:39:46 +0800

    2-17-2024 16:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,76 @@
-function firstNonRepeatingCharacter(str) {
-    // Initialize an empty object to store character counts
-    var charCount = {};
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
+    }
 
-    // Iterate over the characters in the string and count how many times each character appears
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
+    insert(value, priority) {
+        const node = { value, priority };
+        this.heap.push(node);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        const min = this.heap[0];
+        const lastNode = this.heap.pop();
+
+        if (this.heap.length > 0) {
+            this.heap[0] = lastNode;
+            this.bubbleDown(0);
+        }
+
+        return min.value;
+    }
+
+    peek() {
+        return this.heap.length > 0 ? this.heap[0].value : null;
+    }
+
+    bubbleUp(index) {
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index].priority < this.heap[parentIndex].priority) {
+                [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+                index = parentIndex;
+            } else {
+                break;
+            }
         }
     }
 
-    // Find the first character that appears only once
-    for (var j = 0; j < str.length; j++) {
-        var char = str[j];
-        if (charCount[char] === 1) {
-            return char;
+    bubbleDown(index) {
+        while (index < this.heap.length) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let smallerChildIndex = index;
+
+            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].priority < this.heap[smallerChildIndex].priority) {
+                smallerChildIndex = leftChildIndex;
+            }
+
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[smallerChildIndex].priority) {
+                smallerChildIndex = rightChildIndex;
+            }
+
+            if (smallerChildIndex !== index) {
+                [this.heap[index], this.heap[smallerChildIndex]] = [this.heap[smallerChildIndex], this.heap[index]];
+                index = smallerChildIndex;
+            } else {
+                break;
+            }
         }
     }
-
-    // If no non-repeating character is found, return null
-    return null;
 }
 
-// Test the function
-var inputString = "hello";
-var firstNonRepeatingChar = firstNonRepeatingCharacter(inputString);
-console.log("First non-repeating character: " + firstNonRepeatingChar);
+// Sample Usage
+const pq = new PriorityQueue();
+pq.insert('task1', 4);
+pq.insert('task2', 1);
+pq.insert('task3', 3);
+
+console.log(pq.extractMin()); // Output: task2
+console.log(pq.extractMin()); // Output: task3
+console.log(pq.extractMin()); // Output: task1
