fffe9c9e2e9b8088e31576547483a6a18eee03fe
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:18:46 +0800

    2-17-2024 16:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,62 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    enqueue(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+class TarjanSCC {
+    constructor(graph) {
+        this.graph = graph;
+        this.index = 0;
+        this.stack = [];
+        this.groups = [];
+        this.lowLink = [];
+        this.indexes = [];
+        this.onStack = [];
+        
+        for (let i = 0; i < this.graph.length; i++) {
+            this.groups[i] = [];
+            this.lowLink[i] = -1;
+            this.indexes[i] = -1;
+            this.onStack[i] = false;
         }
-        this.size++;
     }
 
-    dequeue() {
-        if (!this.head) {
-            return null;
+    findSCC() {
+        for (let i = 0; i < this.graph.length; i++) {
+            if (this.indexes[i] === -1) {
+                this.strongConnect(i);
+            }
         }
-        const value = this.head.value;
-        this.head = this.head.next;
-        this.size--;
-        if (this.size === 0) {
-            this.tail = null;
+
+        return this.groups;
+    }
+
+    strongConnect(node) {
+        this.indexes[node] = this.index;
+        this.lowLink[node] = this.index;
+        this.index++;
+        this.stack.push(node);
+        this.onStack[node] = true;
+
+        for (let neighbor of this.graph[node]) {
+            if (this.indexes[neighbor] === -1) {
+                this.strongConnect(neighbor);
+                this.lowLink[node] = Math.min(this.lowLink[node], this.lowLink[neighbor]);
+            } else if (this.onStack[neighbor]) {
+                this.lowLink[node] = Math.min(this.lowLink[node], this.indexes[neighbor]);
+            }
         }
-        return value;
-    }
 
-    peek() {
-        if (!this.head) {
-            return null;
-        }
-        return this.head.value;
-    }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    print() {
-        let current = this.head;
-        while (current) {
-            console.log(current.value);
-            current = current.next;
+        if (this.lowLink[node] === this.indexes[node]) {
+            let group = [];
+            let member = null;
+            do {
+                member = this.stack.pop();
+                this.onStack[member] = false;
+                group.push(member);
+            } while (member !== node);
+            this.groups.push(group);
         }
     }
 }
 
-// Usage
-const queue = new Queue();
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-queue.print(); // Output: 10, 20, 30
-console.log(queue.dequeue()); // Output: 10
-console.log(queue.peek()); // Output: 20
-console.log(queue.isEmpty()); // Output: false
+// Example usage
+const graph = [[1], [2], [0], [3, 4], [0, 2]];
+const tarjan = new TarjanSCC(graph);
+const sccGroups = tarjan.findSCC();
+console.log(sccGroups);
