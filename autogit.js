322bd58945d7c8b3eb9f1beb6a8f8bab95cafd57
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:50:15 +0800

    2-17-2024 16:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,21 @@
-function isPrime(num) {
-    if (num <= 1) {
-        return false;
-    }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
+function bubbleSort(arr) {
+    const n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                const temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
         }
-    }
-    return true;
+    } while (swapped);
+
+    return arr;
 }
 
-// Test the function
-console.log(isPrime(5)); // true
-console.log(isPrime(10)); // false
+const arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + bubbleSort(arr));
