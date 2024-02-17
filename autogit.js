788fb5e0f8e4761f65175ed9a6b5bd0e352a3381
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:34:09 +0800

    2-17-2024 16:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,65 @@
-function countOccurrences(str, char) {
-    var count = 0;
-    for (var i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
-            count++;
-        }
+function tarjanSCC(graph) {
+  let index = 0;
+  let stack = [];
+  let indexMap = new Map();
+  let lowLink = new Map();
+  let onStack = new Set();
+  let result = [];
+
+  function strongConnect(node) {
+    indexMap.set(node, index);
+    lowLink.set(node, index);
+    index++;
+    stack.push(node);
+    onStack.add(node);
+
+    for (let neighbor of graph[node]) {
+      if (!indexMap.has(neighbor)) {
+        strongConnect(neighbor);
+        lowLink.set(node, Math.min(lowLink.get(node), lowLink.get(neighbor)));
+      } else if (onStack.has(neighbor)) {
+        lowLink.set(node, Math.min(lowLink.get(node), indexMap.get(neighbor)));
+      }
     }
-    return count;
+
+    if (lowLink.get(node) === indexMap.get(node)) {
+      let component = [];
+      let popNode = stack.pop();
+      onStack.delete(popNode);
+      component.push(popNode);
+      
+      while (popNode !== node) {
+        popNode = stack.pop();
+        onStack.delete(popNode);
+        component.push(popNode);
+      }
+
+      result.push(component);
+    }
+  }
+
+  for (let node of Object.keys(graph)) {
+    if (!indexMap.has(node)) {
+      strongConnect(node);
+    }
+  }
+
+  return result;
 }
 
-var str = "hello world";
-var char = "l";
-var occurrences = countOccurrences(str, char);
-console.log("Occurrences of '" + char + "' in '" + str + "': " + occurrences); // Output: Occurrences of 'l' in 'hello world': 3
+// Example usage
+let graph = {
+  'A': ['B'],
+  'B': ['C', 'E'],
+  'C': ['A', 'D'],
+  'D': ['F'],
+  'E': ['F'],
+  'F': ['G'],
+  'G': ['E', 'H'],
+  'H': ['I'],
+  'I': ['J'],
+  'J': ['H']
+};
+
+let stronglyConnectedComponents = tarjanSCC(graph);
+console.log(stronglyConnectedComponents);
