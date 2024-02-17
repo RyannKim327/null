0dd52480fd9d2a6ec25d5e20ce646ef540dbba14
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:59:16 +0800

    2-17-2024 16:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,9 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    const dp = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => 0));
+const axios = require('axios');
 
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    let result = '';
-    let i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            result = str1[i - 1] + result;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result;
-}
-
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-const lcs = longestCommonSubsequence(str1, str2);
-console.log('Longest Common Subsequence:', lcs);
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log('Response data:', response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
