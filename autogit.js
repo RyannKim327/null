ca4c456f930ebdc5fc04f93d1f50799dbb0de870
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:57:41 +0800

    2-17-2024 16:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,57 @@
-function findKthSmallestElement(arr, k) {
-    // Sort the array in ascending order
-    arr.sort((a, b) => a - b);
+function biDirectionalSearch(startNode, endNode) {
+    let visitedFromStart = new Set();
+    let visitedFromEnd = new Set();
+    let queueFromStart = [startNode];
+    let queueFromEnd = [endNode];
 
-    // Return the kth smallest element (k-1 because array is 0-indexed)
-    return arr[k - 1];
+    visitedFromStart.add(startNode);
+    visitedFromEnd.add(endNode);
+
+    while (queueFromStart.length > 0 && queueFromEnd.length > 0) {
+        const currentNodeFromStart = queueFromStart.shift();
+        const currentNodeFromEnd = queueFromEnd.shift();
+
+        // Check if the nodes meet in the middle
+        if (visitedFromStart.has(currentNodeFromEnd) || visitedFromEnd.has(currentNodeFromStart)) {
+            console.log("Path found!");
+            return true;
+        }
+
+        // Expand next nodes from start node
+        for (let neighbor of currentNodeFromStart.neighbors) {
+            if (!visitedFromStart.has(neighbor)) {
+                visitedFromStart.add(neighbor);
+                queueFromStart.push(neighbor);
+            }
+        }
+
+        // Expand next nodes from end node
+        for (let neighbor of currentNodeFromEnd.neighbors) {
+            if (!visitedFromEnd.has(neighbor)) {
+                visitedFromEnd.add(neighbor);
+                queueFromEnd.push(neighbor);
+            }
+        }
+    }
+
+    console.log("No path found");
+    return false;
 }
 
-// Test the function
-const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-const k = 3;
-const kthSmallestElement = findKthSmallestElement(array, k);
+// Example usage
+class Node {
+    constructor(value, neighbors) {
+        this.value = value;
+        this.neighbors = neighbors;
+    }
+}
 
-console.log(`The ${k}th smallest element in the array is: ${kthSmallestElement}`);
+const node1 = new Node(1, []);
+const node2 = new Node(2, [node1]);
+const node3 = new Node(3, [node1]);
+const node4 = new Node(4, [node2]);
+const node5 = new Node(5, [node2, node3]);
+const node6 = new Node(6, [node4, node5]);
+const node7 = new Node(7, [node5]);
+
+biDirectionalSearch(node1, node7);
