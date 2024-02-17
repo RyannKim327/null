6354bd429b48ed2df069e69d683e23895eff99bd
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:34:00 +0800

    2-17-2024 16:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,14 @@
-// Define the binary tree node structure
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+function countOccurrences(str, char) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
+    }
+    return count;
 }
 
-// Function to count the number of leaf nodes in a binary tree
-function countLeafNodes(node) {
-  if (node === null) {
-    return 0;
-  }
-
-  if (node.left === null && node.right === null) {
-    return 1;
-  }
-
-  return countLeafNodes(node.left) + countLeafNodes(node.right);
-}
-
-// Example binary tree
-const root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-
-const leafNodesCount = countLeafNodes(root);
-console.log("Number of leaf nodes in the binary tree: ", leafNodesCount);
+var str = "hello world";
+var char = "l";
+var occurrences = countOccurrences(str, char);
+console.log("Occurrences of '" + char + "' in '" + str + "': " + occurrences); // Output: Occurrences of 'l' in 'hello world': 3
