22be4c44536790a7477e4aba72ffe7e2f845f228
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:49:57 +0800

    2-17-2024 16:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,12 @@
-function isAnagram(str1, str2) {
-    // Remove spaces and punctuation, and convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-    // Sort the characters in the strings
-    str1 = str1.split('').sort().join('');
-    str2 = str2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return str1 === str2;
+function findSecondLargest(arr) {
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+    
+    return arr[1];
 }
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example usage
+const arr = [10, 5, 8, 20, 15];
+const secondLargest = findSecondLargest(arr);
+console.log("Second largest element is: " + secondLargest);
