8cd8bd13bdc53bbcbf5c8c57424ccc0083820b34
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:22:53 +0800

    2-17-2024 16:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,30 @@
-function longestCommonSubstring(str1, str2) {
-    const dp = Array.from({ length: str1.length + 1 }, () =>
-        Array.from({ length: str2.length + 1 }, () => 0)
-    );
-
-    let maxLength = 0;
-    let endIndex = 0;
-
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
-        }
-    }
-
-    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-const str1 = "abcdefg";
-const str2 = "xyzabcd";
+function calculateNodeSum(root) {
+  if (root === null) {
+    return 0;
+  }
 
-console.log(longestCommonSubstring(str1, str2)); // Output: "abc"
+  let sum = root.value;
+  sum += calculateNodeSum(root.left);
+  sum += calculateNodeSum(root.right);
+
+  return sum;
+}
+
+// Sample binary tree
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+const treeSum = calculateNodeSum(root);
+
+console.log("Sum of all nodes in the binary tree:", treeSum);
