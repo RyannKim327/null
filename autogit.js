145092dfd8d533946619b7895fc0095430d796da
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:23:14 +0800

    2-17-2024 16:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,12 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const cron = require('node-cron');
 
-// Example: Generate a random number between 1 and 100
-const randomNumber = getRandomNumber(1, 100);
-console.log(randomNumber);
+// Define the task to be executed
+const task = () => {
+  console.log('Task executed at: ', new Date());
+};
+
+// Schedule the task to run every minute
+cron.schedule('* * * * *', task);
+
+console.log('Cron job scheduled!');
+npm install node-cron
