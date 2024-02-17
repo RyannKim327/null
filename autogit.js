4a5a0744ba6041809028d61fe62239f650de972f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:30:24 +0800

    2-17-2024 16:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,23 @@
-function findLongestIncreasingSubsequence(arr) {
-    let n = arr.length;
-    let lis = Array(n).fill(1);
-
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-                lis[i] = lis[j] + 1;
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return '';
+    
+    let prefix = '';
+    
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+        
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return prefix;
             }
         }
+        
+        prefix += char;
     }
-
-    let maxLength = Math.max(...lis);
-    let result = [];
-
-    for (let i = n - 1; i >= 0; i--) {
-        if (lis[i] === maxLength) {
-            result.unshift(arr[i]);
-            maxLength--;
-        }
-    }
-
-    return result;
+    
+    return prefix;
 }
 
-// Example
-const arr = [3, 10, 2, 1, 20];
-const longestIncreasingSubsequence = findLongestIncreasingSubsequence(arr);
-console.log(longestIncreasingSubsequence); // Output: [3, 10, 20]
+// Example usage:
+const strings = ['apple', 'ape', 'apricot'];
+console.log(longestCommonPrefix(strings)); // Output: 'ap'
