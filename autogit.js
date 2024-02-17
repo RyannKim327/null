e93114ddad53933bb8b1e32c53146e92d365075f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:16:51 +0800

    2-17-2024 16:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,18 @@
-function boyerMoore(text, pattern) {
-    let textLength = text.length;
-    let patternLength = pattern.length;
-    
-    if (patternLength === 0) {
-        return -1;
-    }
-
-    let badMatchTable = {};
-    for(let i = 0; i < patternLength - 1; i++) {
-        badMatchTable[pattern[i]] = patternLength - i - 1;
-    }
-    
-    let index = patternLength - 1; // Start index at end of pattern
-    while (index < textLength) {
-        let patternIndex = patternLength - 1;
-        let textIndex = index;
-        
-        while (patternIndex >= 0 && text[textIndex] === pattern[patternIndex]) {
-            patternIndex--;
-            textIndex--;
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
-        
-        if (patternIndex === -1) {
-            return textIndex + 1; // Pattern found
-        }
-        
-        let badMatchSkip = badMatchTable[text[index]] || patternLength;
-        index += badMatchSkip;
     }
-
-    return -1; // Pattern not found
+    return arr;
 }
 
-// Test the Boyer-Moore algorithm
-let text = "Hello, this is a test string for searching.";
-let pattern = "test";
-let result = boyerMoore(text, pattern);
-
-if (result !== -1) {
-    console.log(`Pattern found at index ${result}`);
-} else {
-    console.log(`Pattern not found`);
-}
+// Usage
+var unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+var sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray);
