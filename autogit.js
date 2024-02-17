85109f88305dfad7aef5096a49bdf978755e90bc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:54:11 +0800

    2-17-2024 16:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,16 @@
-let str = "Hello, World! This has spaces.";
-let stringWithoutSpaces = str.replace(/\s/g, "");
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return '';
 
-console.log(stringWithoutSpaces);
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+        }
+    }
+
+    return prefix;
+}
+
+// Example usage
+const strings = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strings)); // Output: "fl"
