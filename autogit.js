8412c1489a4de2387476e69bfec6b06309c949c2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:22:46 +0800

    2-17-2024 16:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,29 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+function longestCommonSubstring(str1, str2) {
+    const dp = Array.from({ length: str1.length + 1 }, () =>
+        Array.from({ length: str2.length + 1 }, () => 0)
+    );
 
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
+    let maxLength = 0;
+    let endIndex = 0;
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
         }
     }
 
-    return quickSort(left).concat(pivot, quickSort(right));
+    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
 }
 
-// Example usage
-const arr = [5, 3, 7, 2, 8, 4, 1];
-const sortedArray = quickSort(arr);
-console.log(sortedArray);
+const str1 = "abcdefg";
+const str2 = "xyzabcd";
+
+console.log(longestCommonSubstring(str1, str2)); // Output: "abc"
