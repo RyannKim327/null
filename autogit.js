409c568b43fe05aa10984f36996e20079f4f882f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:46:32 +0800

    2-17-2024 16:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,43 @@
-function findFirstRepeatedChar(str) {
-    let charMap = {};
+function fibonacciSearch(arr, x) {
+    let fibM2 = 0;
+    let fibM1 = 1;
+    let fibM = fibM2 + fibM1;
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
+    while (fibM < arr.length) {
+        fibM2 = fibM1;
+        fibM1 = fibM;
+        fibM = fibM2 + fibM1;
+    }
+
+    let offset = -1;
+
+    while (fibM > 1) {
+        let i = Math.min(offset + fibM2, arr.length - 1);
+
+        if (arr[i] < x) {
+            fibM = fibM1;
+            fibM1 = fibM2;
+            fibM2 = fibM - fibM1;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibM = fibM2;
+            fibM1 -= fibM2;
+            fibM2 = fibM - fibM1;
         } else {
-            charMap[char] = 1;
+            return i;
         }
     }
 
-    return null;
+    if (fibM1 && arr[offset + 1] === x) {
+        return offset + 1;
+    }
+
+    return -1;
 }
 
-let str = "hello world";
-let repeatedChar = findFirstRepeatedChar(str);
+// Example:
+const arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100];
+const x = 85;
 
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("No repeated characters found");
-}
+const index = fibonacciSearch(arr, x);
+console.log(`Element found at index: ${index}`);
