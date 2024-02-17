b7a39b6b596451e834c6f1637e97d1ce926a5145
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:23:50 +0800

    2-17-2024 16:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,45 @@
-function largestPrimeFactor(number) {
-    let largestPrime = 1;
-    
-    // Start from 2 (smallest prime number) and iterate up to the square root of the number
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            // Check if i is a prime number
-            let isPrime = true;
-            for (let j = 2; j <= Math.sqrt(i); j++) {
-                if (i % j === 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            
-            if (isPrime) {
-                largestPrime = i;
-            }
-        }
-    }
-    
-    return largestPrime;
-}
-const number = 56;
-const largestPrime = largestPrimeFactor(number);
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
 
-console.log(`The largest prime factor of ${number} is: ${largestPrime}`);
+  addChild(node) {
+    this.children.push(node);
+    return this;
+  }
+}
+
+function depthLimitedSearchIterative(root, target, limit) {
+  let stack = [];
+  stack.push({ node: root, depth: 0 });
+
+  while (stack.length > 0) {
+    const { node, depth } = stack.pop();
+
+    if (node.name === target) {
+      return node;
+    }
+
+    if (depth < limit) {
+      for (let child of node.children) {
+        stack.push({ node: child, depth: depth + 1 });
+      }
+    }
+  }
+
+  return null;
+}
+
+// Usage
+let rootNode = new Node("A");
+let nodeB = new Node("B");
+let nodeC = new Node("C");
+let nodeD = new Node("D");
+
+rootNode.addChild(nodeB);
+rootNode.addChild(nodeC);
+nodeB.addChild(nodeD);
+
+let result = depthLimitedSearchIterative(rootNode, "D", 2);
+console.log(result);
