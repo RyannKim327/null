8451070518af37056270fd7bae30d1c6f9378319
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:31:00 +0800

    2-17-2024 16:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,64 @@
 class Node {
-    constructor(start, end) {
-        this.children = {};
-        this.start = start; // start index of the substring represented by this node
-        this.end = end; // end index of the substring represented by this node
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
     }
 }
-class SuffixTree {
-    constructor(text) {
-        this.root = new Node(-1, -1);
-        this.text = text;
-        this.buildTree();
+
+class RedBlackTree {
+    constructor() {
+        this.root = null;
     }
 
-    buildTree() {
-        // Implement the Ukkonen's algorithm here to build the suffix tree
+    insert(value) {
+        const newNode = new Node(value, "red");
+        if (!this.root) {
+            this.root = newNode;
+            this.root.color = "black";
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(rootNode, newNode) {
+        if (newNode.value < rootNode.value) {
+            if (rootNode.left === null) {
+                rootNode.left = newNode;
+                newNode.parent = rootNode;
+            } else {
+                this.insertNode(rootNode.left, newNode);
+            }
+        } else {
+            if (rootNode.right === null) {
+                rootNode.right = newNode;
+                newNode.parent = rootNode;
+            } else {
+                this.insertNode(rootNode.right, newNode);
+            }
+        }
+    }
+
+    // Other methods like delete, rotate, balance can be added here
+
+    // Print the tree in-order
+    inOrderTraversal(node) {
+        if (node !== null) {
+            this.inOrderTraversal(node.left);
+            console.log(node.value);
+            this.inOrderTraversal(node.right);
+        }
     }
 }
+
+// Usage
+const tree = new RedBlackTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(5);
+tree.insert(11);
+tree.insert(3);
+
+tree.inOrderTraversal(tree.root);
