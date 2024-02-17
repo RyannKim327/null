e77c94dc73ecd50eb0af884fd0acf8c8f0082ed2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:43:58 +0800

    2-17-2024 16:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,26 @@
-class Graph {
-  constructor() {
-    this.nodes = new Map();
-  }
+function longestCommonSubstring(str1, str2) {
+    let longest = 0;
+    let result = '';
 
-  addNode(name) {
-    this.nodes.set(name, []);
-  }
+    const dp = Array.from(Array(str1.length + 1), () => Array(str2.length + 1).fill(0));
 
-  addEdge(startNode, endNode) {
-    if (!this.nodes.has(startNode) || !this.nodes.has(endNode)) {
-      throw new Error('Nodes not found in graph');
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+
+                if (dp[i][j] > longest) {
+                    longest = dp[i][j];
+                    result = str1.substring(i - longest, i);
+                }
+            }
+        }
     }
 
-    this.nodes.get(startNode).push(endNode);
-    this.nodes.get(endNode).push(startNode);
-  }
-
-  biDirectionalSearch(startNode, endNode) {
-    let visitedFromStart = new Set([startNode]);
-    let visitedFromEnd = new Set([endNode]);
-    let queueFromStart = [startNode];
-    let queueFromEnd = [endNode];
-
-    while (queueFromStart.length > 0 && queueFromEnd.length > 0) {
-      let currentStart = queueFromStart.shift();
-      let currentEnd = queueFromEnd.shift();
-
-      if (visitedFromEnd.has(currentStart) || visitedFromStart.has(currentEnd)) {
-        return true;
-      }
-
-      for (let neighbor of this.nodes.get(currentStart)) {
-        if (!visitedFromStart.has(neighbor)) {
-          visitedFromStart.add(neighbor);
-          queueFromStart.push(neighbor);
-        }
-      }
-
-      for (let neighbor of this.nodes.get(currentEnd)) {
-        if (!visitedFromEnd.has(neighbor)) {
-          visitedFromEnd.add(neighbor);
-          queueFromEnd.push(neighbor);
-        }
-      }
-    }
-
-    return false;
-  }
+    return result;
 }
 
-// Example usage
-let graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'C');
-graph.addEdge('C', 'D');
-
-let result = graph.biDirectionalSearch('A', 'D');
-console.log(result); // Output: true
+// Example
+const str1 = "abcdef";
+const str2 = "zxbcdf";
+console.log(longestCommonSubstring(str1, str2)); // Output: "bcdf"
