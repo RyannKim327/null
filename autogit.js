3abe4c0073eddb0cbb7e8956c48fc3e0154a4d02
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:31:21 +0800

    2-17-2024 16:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,26 @@
-function findFirstRepeatedChar(str) {
-    let charSet = new Set();
-    
-    for (let char of str) {
-        if (charSet.has(char)) {
-            return char; // Found the first repeated character
-        } else {
-            charSet.add(char);
+function selectionSort(arr) {
+    const n = arr.length;
+
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
-    
-    return null; // No repeated characters found
+
+    return arr;
 }
 
-let inputString = "abcaebd";
-let repeatedChar = findFirstRepeatedChar(inputString);
-
-if (repeatedChar) {
-    console.log(`The first repeated character is: ${repeatedChar}`);
-} else {
-    console.log("No repeated characters found in the string.");
-}
+// Example usage
+const unsortedArray = [64, 25, 12, 22, 11];
+const sortedArray = selectionSort(unsortedArray);
+console.log(sortedArray); // Output: [11, 12, 22, 25, 64]
