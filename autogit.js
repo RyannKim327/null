1b25699967f72b0d6c3e0b1df2a56f2c17e8ae5c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:15:34 +0800

    2-17-2024 16:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,17 @@
-function longestIncreasingSubsequence(arr) {
-    if (arr.length === 0) return 0;
-
-    const n = arr.length;
-    const lis = Array(n).fill(1);
-
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-                lis[i] = lis[j] + 1;
-            }
+function isPrimeNumber(num) {
+    if (num <= 1) {
+        return false;
+    }
+    
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
         }
     }
-
-    let maxLength = Math.max(...lis);
-    let result = [];
-    let maxIndex = lis.indexOf(maxLength);
-
-    for (let i = maxIndex; i >= 0; i--) {
-        if (lis[i] === maxLength && (result.length === 0 || arr[i] < result[result.length - 1])) {
-            result.unshift(arr[i]);
-            maxLength--;
-        }
-    }
-
-    return result;
+    
+    return true;
 }
 
-const arr = [10, 22, 9, 33, 21, 50, 41, 60, 80];
-console.log(longestIncreasingSubsequence(arr)); // Output: [10, 22, 33, 50, 60, 80]
+// Test the function
+console.log(isPrimeNumber(7)); // Output: true
+console.log(isPrimeNumber(10)); // Output: false
