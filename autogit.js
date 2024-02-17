39b95386debe6cbccab5fb6199dee1a8cc02ee78
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:39:04 +0800

    2-17-2024 16:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,38 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    isEmpty() {
-        return this.size() === 0;
-    }
-
-    insert(value, priority) {
-        const node = { value, priority };
-        this.heap.push(node);
-        this.percolateUp(this.size() - 1);
-    }
-
-    extractMin() {
-        if (this.isEmpty()) {
-            return null;
-        }
-
-        const minNode = this.heap[0];
-        const lastNode = this.heap.pop();
-
-        if (this.size() > 0) {
-            this.heap[0] = lastNode;
-            this.percolateDown(0);
-        }
-
-        return minNode;
-    }
-
-    percolateUp(index) {
-        let parentIndex = Math.floor((index - 1) / 2);
-
-        while (index > 0 && this.heap[index].priority < this.heap[parentIndex].priority) {
-            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-            index = parentIndex;
-            parentIndex = Math.floor((index - 1) / 2);
-        }
-    }
-
-    percolateDown(index) {
-        let leftChildIndex = 2 * index + 1;
-        let rightChildIndex = 2 * index + 2;
-        let minIndex = index;
-
-        if (leftChildIndex < this.size() && this.heap[leftChildIndex].priority < this.heap[minIndex].priority) {
-            minIndex = leftChildIndex;
-        }
-
-        if (rightChildIndex < this.size() && this.heap[rightChildIndex].priority < this.heap[minIndex].priority) {
-            minIndex = rightChildIndex;
-        }
-
-        if (minIndex !== index) {
-            [this.heap[minIndex], this.heap[index]] = [this.heap[index], this.heap[minIndex]];
-            this.percolateDown(minIndex);
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
 }
 
-// Example usage
-const pq = new PriorityQueue();
-pq.insert("Task 1", 4);
-pq.insert("Task 2", 2);
-pq.insert("Task 3", 7);
+function reverseLinkedList(head) {
+    let prev = null;
+    let current = head;
+    let next = null;
 
-console.log(pq.extractMin()); // Outputs: { value: 'Task 2', priority: 2 }
+    while (current !== null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
+
+// Example usage
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+
+node1.next = node2;
+node2.next = node3;
+
+let reversedHead = reverseLinkedList(node1);
+
+// Print the reversed linked list
+let current = reversedHead;
+while (current !== null) {
+    console.log(current.value);
+    current = current.next;
+}
