cb7c46dede2cced492c6355e06acf9347b93b5ef
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:43:28 +0800

    2-17-2024 16:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,12 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function findSecondLargest(arr) {
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+    
+    return arr[1];
 }
 
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    enqueue(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-    }
-
-    dequeue() {
-        if (!this.head) {
-            return null;
-        }
-
-        const data = this.head.data;
-        this.head = this.head.next;
-        if (!this.head) {
-            this.tail = null;
-        }
-
-        return data;
-    }
-
-    isEmpty() {
-        return this.head === null;
-    }
-
-    peek() {
-        if (!this.head) {
-            return null;
-        }
-
-        return this.head.data;
-    }
-}
-
-// Usage example:
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.peek()); // Output: 2
-console.log(queue.isEmpty()); // Output: false
+// Example usage
+let numbers = [10, 5, 8, 20, 15];
+let secondLargest = findSecondLargest(numbers);
+console.log("Second largest element is: " + secondLargest);
