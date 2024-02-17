2b952378cf8c8e6ad8e429f22a5ea30df301d584
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:13:42 +0800

    2-17-2024 16:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,63 @@
-let str = "Hello, World!";
-let substring = "Hello";
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
+  }
 
-if (str.includes(substring)) {
-    console.log("The string contains the substring.");
-} else {
-    console.log("The string does not contain the substring.");
-}
-let str = "Hello, World!";
-let substring = "Hello";
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.size;
+    }
+    return hash;
+  }
 
-if (str.indexOf(substring) !== -1) {
-    console.log("The string contains the substring.");
-} else {
-    console.log("The string does not contain the substring.");
+  set(key, value) {
+    const index = this._hash(key);
+
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+
+    this.table[index].push({ key, value });
+  }
+
+  get(key) {
+    const index = this._hash(key);
+
+    if (!this.table[index]) {
+      return undefined;
+    }
+
+    for (let entry of this.table[index]) {
+      if (entry.key === key) {
+        return entry.value;
+      }
+    }
+
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this._hash(key);
+
+    if (!this.table[index]) {
+      return undefined;
+    }
+
+    this.table[index] = this.table[index].filter(entry => entry.key !== key);
+  }
 }
+
+// Example usage
+const ht = new HashTable(10);
+
+ht.set('apple', 'red');
+ht.set('banana', 'yellow');
+ht.set('grape', 'purple');
+
+console.log(ht.get('apple')); // Output: red
+console.log(ht.get('banana')); // Output: yellow
+
+ht.remove('apple');
+console.log(ht.get('apple')); // Output: undefined
