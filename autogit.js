73991a9e2ab227d99fd07e683aec64aa42b28c4c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:37:29 +0800

    2-17-2024 16:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,14 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-        this.height = 1;
-    }
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    
+    // Reverse the cleaned string
+    const reversedStr = cleanedStr.split('').reverse().join('');
+    
+    // Check if the cleaned string is the same as its reversed version
+    return cleanedStr === reversedStr;
 }
 
-class AVLTree {
-    constructor() {
-        this.root = null;
-    }
-
-    getHeight(node) {
-        if (node == null) {
-            return 0;
-        }
-        return node.height;
-    }
-
-    getBalanceFactor(node) {
-        if (node == null) {
-            return 0;
-        }
-        return this.getHeight(node.left) - this.getHeight(node.right);
-    }
-
-    rightRotate(y) {
-        let x = y.left;
-        let T2 = x.right;
-
-        x.right = y;
-        y.left = T2;
-
-        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
-        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
-
-        return x;
-    }
-
-    leftRotate(x) {
-        let y = x.right;
-        let T2 = y.left;
-
-        y.left = x;
-        x.right = T2;
-
-        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
-        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
-
-        return y;
-    }
-
-    insert(data) {
-        this.root = this._insert(this.root, data);
-    }
-
-    _insert(node, data) {
-        if (node == null) {
-            return new Node(data);
-        }
-
-        if (data < node.data) {
-            node.left = this._insert(node.left, data);
-        } else if (data > node.data) {
-            node.right = this._insert(node.right, data);
-        } else {
-            return node;
-        }
-
-        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-
-        let balance = this.getBalanceFactor(node);
-
-        if (balance > 1 && data < node.left.data) {
-            return this.rightRotate(node);
-        }
-
-        if (balance < -1 && data > node.right.data) {
-            return this.leftRotate(node);
-        }
-
-        if (balance > 1 && data > node.left.data) {
-            node.left = this.leftRotate(node.left);
-            return this.rightRotate(node);
-        }
-
-        if (balance < -1 && data < node.right.data) {
-            node.right = this.rightRotate(node.right);
-            return this.leftRotate(node);
-        }
-
-        return node;
-    }
-
-    // Other methods like search, delete, printTree can be added here
-}
+// Test the function
+const testString = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(testString)); // Output: true
