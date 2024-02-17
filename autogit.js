ff2dae1766176073719527e53527fdaf31deb0e5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:48:30 +0800

    2-17-2024 16:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,8 @@
-function isAnagram(str1, str2) {
-  // Remove non-alphabetic characters and convert to lowercase
-  const cleanStr1 = str1.replace(/[^A-Za-z]/g, '').toLowerCase();
-  const cleanStr2 = str2.replace(/[^A-Za-z]/g, '').toLowerCase();
-
-  // Sort the characters
-  const sortedStr1 = cleanStr1.split('').sort().join('');
-  const sortedStr2 = cleanStr2.split('').sort().join('');
-
-  // Compare the sorted strings
-  return sortedStr1 === sortedStr2;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-if (isAnagram(string1, string2)) {
-  console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-  console.log(`${string1} and ${string2} are not anagrams.`);
-}
+const inputString = "Hello, World!";
+const stringWithoutVowels = removeVowels(inputString);
+
+console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
