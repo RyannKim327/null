bf72752d73e3ce026b0e4d890d15f3b4b215e50d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:42:19 +0800

    2-17-2024 16:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,19 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result); // Output: Hello World
+function findFirstRepeatedChar(str) {
+    var charMap = {};
+    
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i];
+        
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+
+    return null;
+}
+
+var str = "aabbccddeeff";
+var firstRepeatedChar = findFirstRepeatedChar(str);
+console.log("First repeated character in the string is:", firstRepeatedChar);
