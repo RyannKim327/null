622f10f640a0e8a46db10d5d586e3f7edf29626c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:28:47 +0800

    2-17-2024 16:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,14 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-
-    const dp = [];
-    for (let i = 0; i <= m; i++) {
-        dp[i] = [];
-        for (let j = 0; j <= n; j++) {
-            if (i === 0 || j === 0) {
-                dp[i][j] = 0;
-            } else if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+let arr = [1, 2, 3, 3, 4, 4, 5];
+let uniqueArr = [...new Set(arr)];
+console.log(uniqueArr);
+let arr = [1, 2, 3, 3, 4, 4, 5];
+let uniqueArr = arr.filter((value, index, self) => self.indexOf(value) === index);
+console.log(uniqueArr);
+let arr = [1, 2, 3, 3, 4, 4, 5];
+let uniqueArr = [];
+arr.forEach((element) => {
+    if (!uniqueArr.includes(element)) {
+        uniqueArr.push(element);
     }
-
-    let i = m;
-    let j = n;
-    let index = dp[m][n];
-    const lcs = [];
-
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs[--index] = str1[i - 1];
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return lcs.join('');
-}
-
-const str1 = "ABCBDAB";
-const str2 = "BDCAB";
-console.log(longestCommonSubsequence(str1, str2)); // Output: BCAB
+});
+console.log(uniqueArr);
