6fb39b9e01b28ca5144efd6ccede10739ed88cb2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:59:11 +0800

    2-17-2024 16:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,36 @@
-function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        let minIndex = i;
-        
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+    const dp = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => 0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        
-        if (minIndex !== i) {
-            let temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+    }
+
+    let result = '';
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            result = str1[i - 1] + result;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
         }
     }
-    
-    return arr;
+
+    return result;
 }
 
-// Example usage
-const array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: ", array);
-console.log("Sorted array: ", selectionSort(array));
+const str1 = 'ABCDGH';
+const str2 = 'AEDFHR';
+const lcs = longestCommonSubsequence(str1, str2);
+console.log('Longest Common Subsequence:', lcs);
