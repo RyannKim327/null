fcea3109425556a05967d85fa3daf508a9525fa2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:34:15 +0800

    2-17-2024 16:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,10 @@
-function tarjanSCC(graph) {
-  let index = 0;
-  let stack = [];
-  let indexMap = new Map();
-  let lowLink = new Map();
-  let onStack = new Set();
-  let result = [];
-
-  function strongConnect(node) {
-    indexMap.set(node, index);
-    lowLink.set(node, index);
-    index++;
-    stack.push(node);
-    onStack.add(node);
-
-    for (let neighbor of graph[node]) {
-      if (!indexMap.has(neighbor)) {
-        strongConnect(neighbor);
-        lowLink.set(node, Math.min(lowLink.get(node), lowLink.get(neighbor)));
-      } else if (onStack.has(neighbor)) {
-        lowLink.set(node, Math.min(lowLink.get(node), indexMap.get(neighbor)));
-      }
-    }
-
-    if (lowLink.get(node) === indexMap.get(node)) {
-      let component = [];
-      let popNode = stack.pop();
-      onStack.delete(popNode);
-      component.push(popNode);
-      
-      while (popNode !== node) {
-        popNode = stack.pop();
-        onStack.delete(popNode);
-        component.push(popNode);
-      }
-
-      result.push(component);
-    }
-  }
-
-  for (let node of Object.keys(graph)) {
-    if (!indexMap.has(node)) {
-      strongConnect(node);
-    }
-  }
-
-  return result;
+function calculateMean(numbers) {
+    const sum = numbers.reduce((acc, current) => acc + current, 0);
+    const mean = sum / numbers.length;
+    return mean;
 }
 
 // Example usage
-let graph = {
-  'A': ['B'],
-  'B': ['C', 'E'],
-  'C': ['A', 'D'],
-  'D': ['F'],
-  'E': ['F'],
-  'F': ['G'],
-  'G': ['E', 'H'],
-  'H': ['I'],
-  'I': ['J'],
-  'J': ['H']
-};
-
-let stronglyConnectedComponents = tarjanSCC(graph);
-console.log(stronglyConnectedComponents);
+const numbers = [1, 2, 3, 4, 5];
+const mean = calculateMean(numbers);
+console.log(`Mean: ${mean}`);
