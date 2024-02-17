c55d3de60ac9ddfdf9ee66a21860258b3168d4bd
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:37:55 +0800

    2-17-2024 16:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,47 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert the string to lowercase
-    const alphanumericStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    
-    // Compare the string with its reverse
-    return alphanumericStr === alphanumericStr.split('').reverse().join('');
+// Node class represents each node in the linked list
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-// Test the isPalindrome function
-const testString = "A man, a plan, a canal, Panama!";
-if (isPalindrome(testString)) {
-    console.log(`${testString} is a palindrome.`);
-} else {
-    console.log(`${testString} is not a palindrome.`);
+// LinkedList class to manage the linked list
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to insert a new node at the end of the linked list
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    // Method to print all elements of the linked list
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.print(); // Output: 1, 2, 3
