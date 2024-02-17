22e8249b7f37bcf65e17d7b4aae9ea2203a49407
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:18:54 +0800

    2-17-2024 16:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,15 @@
-class TarjanSCC {
-    constructor(graph) {
-        this.graph = graph;
-        this.index = 0;
-        this.stack = [];
-        this.groups = [];
-        this.lowLink = [];
-        this.indexes = [];
-        this.onStack = [];
-        
-        for (let i = 0; i < this.graph.length; i++) {
-            this.groups[i] = [];
-            this.lowLink[i] = -1;
-            this.indexes[i] = -1;
-            this.onStack[i] = false;
-        }
+function maxSubarraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
     }
 
-    findSCC() {
-        for (let i = 0; i < this.graph.length; i++) {
-            if (this.indexes[i] === -1) {
-                this.strongConnect(i);
-            }
-        }
-
-        return this.groups;
-    }
-
-    strongConnect(node) {
-        this.indexes[node] = this.index;
-        this.lowLink[node] = this.index;
-        this.index++;
-        this.stack.push(node);
-        this.onStack[node] = true;
-
-        for (let neighbor of this.graph[node]) {
-            if (this.indexes[neighbor] === -1) {
-                this.strongConnect(neighbor);
-                this.lowLink[node] = Math.min(this.lowLink[node], this.lowLink[neighbor]);
-            } else if (this.onStack[neighbor]) {
-                this.lowLink[node] = Math.min(this.lowLink[node], this.indexes[neighbor]);
-            }
-        }
-
-        if (this.lowLink[node] === this.indexes[node]) {
-            let group = [];
-            let member = null;
-            do {
-                member = this.stack.pop();
-                this.onStack[member] = false;
-                group.push(member);
-            } while (member !== node);
-            this.groups.push(group);
-        }
-    }
+    return maxSoFar;
 }
 
 // Example usage
-const graph = [[1], [2], [0], [3, 4], [0, 2]];
-const tarjan = new TarjanSCC(graph);
-const sccGroups = tarjan.findSCC();
-console.log(sccGroups);
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubarraySum(arr)); // Output: 6
