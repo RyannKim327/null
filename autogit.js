fd80b2363a1f80a8ca0deb9a4f1dee96104d9e87
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:33:39 +0800

    2-17-2024 16:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,41 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
-  }
-
-  addEdge(v1, v2) {
-    this.adjacencyList[v1].push(v2);
-    this.adjacencyList[v2].push(v1);
-  }
-
-  depthFirstSearch(startingNode) {
-    const visited = {};
-    const result = [];
-
-    const dfs = (vertex) => {
-      if (!vertex) return null;
-      visited[vertex] = true;
-      result.push(vertex);
-      
-      this.adjacencyList[vertex].forEach(neighbor => {
-        if (!visited[neighbor]) {
-          return dfs(neighbor);
+function breadthFirstSearch(graph, startNode, targetNode) {
+    let visited = {};
+    let queue = [startNode];
+    
+    while (queue.length > 0) {
+        let currentNode = queue.shift();
+        
+        if (currentNode === targetNode) {
+            return true;
         }
-      });
-    };
-
-    dfs(startingNode);
-    return result;
-  }
+        
+        if (!visited[currentNode]) {
+            visited[currentNode] = true;
+            
+            let neighbors = graph[currentNode];
+            for (let i = 0; i < neighbors.length; i++) {
+                let neighbor = neighbors[i];
+                if (!visited[neighbor]) {
+                    queue.push(neighbor);
+                }
+            }
+        }
+    }
+    
+    return false;
 }
 
-// Example usage
-const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
+// Example graph represented as an adjacency list
+let graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+};
 
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'E');
-graph.addEdge('D', 'F');
-graph.addEdge('E', 'F');
+let startNode = 'A';
+let targetNode = 'F';
 
-console.log(graph.depthFirstSearch('A')); // Output: [ 'A', 'B', 'D', 'E', 'C', 'F' ]
+console.log(breadthFirstSearch(graph, startNode, targetNode)); // Output: true
