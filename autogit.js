f9e984521022de24025e6b8202f00b954cb34834
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:24:12 +0800

    2-17-2024 16:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,42 @@
-let userInput = prompt("Enter something:"); 
-console.log("You entered: ", userInput);
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insert(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage
+const list = new LinkedList();
+list.insert(1);
+list.insert(2);
+list.insert(3);
+
+list.print();
