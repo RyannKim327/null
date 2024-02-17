e7d06c0192e99d8b71808af5b4311d7e71169b58
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:28:16 +0800

    2-17-2024 16:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,89 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
+class SkipList {
+    constructor(maxLevels) {
+        this.maxLevels = maxLevels;
+        this.head = new Node(-Infinity, maxLevels);
     }
 
-    addNode(data) {
-        let newNode = new Node(data);
-
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-
-            while (current.next !== null) {
-                current = current.next;
-            }
-
-            current.next = newNode;
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < 0.5 && level < this.maxLevels) {
+            level++;
         }
+        return level;
     }
 
-    getLength() {
+    insert(value) {
+        let level = this.randomLevel();
+        let newNode = new Node(value, level);
         let current = this.head;
-        let length = 0;
+        
+        for (let i = this.maxLevels; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            if (i <= level) {
+                newNode.forward[i] = current.forward[i];
+                current.forward[i] = newNode;
+            }
+        }
+    }
 
-        while (current !== null) {
-            length++;
-            current = current.next;
+    search(value) {
+        let current = this.head;
+        for (let i = this.maxLevels; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+        current = current.forward[0];
+        if (current !== null && current.value === value) {
+            return current;
+        } else {
+            return null;
+        }
+    }
+
+    delete(value) {
+        let current = this.head;
+        let toDelete = null;
+
+        for (let i = this.maxLevels; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            if (current.forward[i] !== null && current.forward[i].value === value) {
+                toDelete = current.forward[i];
+                current.forward[i] = current.forward[i].forward[i];
+            }
         }
 
-        return length;
+        if (toDelete !== null) {
+            // Clean up levels of deleted node
+            for (let i = 0; i <= this.maxLevels; i++) {
+                if (current.forward[i] === null) {
+                    break;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
-// Usage
-let linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
+// Example usage:
+const skipList = new SkipList(3);
+skipList.insert(1);
+skipList.insert(4);
+skipList.insert(2);
 
-console.log(linkedList.getLength()); // Output: 3
+console.log(skipList.search(2)); // Node { value: 2, forward: [ null, Node ] }
+skipList.delete(4);
+console.log(skipList.search(4)); // null
