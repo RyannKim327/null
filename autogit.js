c19b2da5a5245eab2b60a903801b0f768f118056
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:42:31 +0800

    2-17-2024 16:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,105 @@
-function findFirstRepeatedChar(str) {
-    var charMap = {};
-    
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = 1;
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
     }
-
-    return null;
 }
 
-var str = "aabbccddeeff";
-var firstRepeatedChar = findFirstRepeatedChar(str);
-console.log("First repeated character in the string is:", firstRepeatedChar);
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+
+    rotateRight(y) {
+        let x = y.left;
+        let T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        this.updateHeight(y);
+        this.updateHeight(x);
+
+        return x;
+    }
+
+    rotateLeft(x) {
+        let y = x.right;
+        let T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        this.updateHeight(x);
+        this.updateHeight(y);
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this.insertNode(this.root, value);
+    }
+
+    insertNode(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this.insertNode(node.left, value);
+        } else if (value > node.value) {
+            node.right = this.insertNode(node.right, value);
+        } else {
+            return node; // Duplicate values not allowed
+        }
+
+        this.updateHeight(node);
+        let balanceFactor = this.getBalanceFactor(node);
+
+        if (balanceFactor > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balanceFactor < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balanceFactor > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balanceFactor < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+}
+
+// Example usage
+let avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(20);
+avlTree.insert(30);
+avlTree.insert(15);
+
+console.log(avlTree);
