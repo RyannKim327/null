b0fe82b5b16e7365b74c3b5d0566e269dad7e360
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:40:04 +0800

    2-17-2024 16:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,5 @@
 let str1 = "Hello";
 let str2 = "World";
-let concatenatedString = str1 + " " + str2;
-console.log(concatenatedString); // Output: Hello World
-let str1 = "Hello";
-let str2 = "World";
-let concatenatedString = str1.concat(" ", str2);
-console.log(concatenatedString); // Output: Hello World
+let result = str1 + " " + str2;
+
+console.log(result); // Output: Hello World
