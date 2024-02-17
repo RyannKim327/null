f4756410ceaf25a7c0fbdf1e18d5f691afc7d4ae
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:12:16 +0800

    2-17-2024 16:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,59 @@
-let str = "   Hello,   World!   ";
-let trimmedStr = str.replace(/\s/g, "");
+class Node {
+    constructor() {
+        this.children = {};
+    }
+}
 
-console.log(trimmedStr); // Output: "Hello,World!"
+class SuffixTree {
+    constructor(text) {
+        this.root = new Node();
+        this.text = text;
+        this.buildSuffixTree();
+    }
+
+    buildSuffixTree() {
+        for (let i = 0; i < this.text.length; i++) {
+            let suffix = this.text.substring(i);
+            this.insertSuffix(suffix, i);
+        }
+    }
+
+    insertSuffix(suffix, index) {
+        let node = this.root;
+
+        for (let i = 0; i < suffix.length; i++) {
+            let char = suffix[i];
+
+            if (!node.children[char]) {
+                node.children[char] = new Node();
+            }
+
+            node = node.children[char];
+        }
+
+        // Store the index of the suffix in the leaf node
+        node.index = index;
+    }
+
+    searchPattern(pattern) {
+        let node = this.root;
+
+        for (let i = 0; i < pattern.length; i++) {
+            let char = pattern[i];
+
+            if (node.children[char]) {
+                node = node.children[char];
+            } else {
+                return null;
+            }
+        }
+
+        return node.index;
+    }
+}
+
+// Example usage
+let text = "banana";
+let suffixTree = new SuffixTree(text);
+
+console.log(suffixTree.searchPattern("ana")); // Output: 1
