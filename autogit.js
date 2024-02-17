88ebb09749a11444293b936ba89b7fbef951fec1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:14:13 +0800

    2-17-2024 16:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,24 @@
-function largestPrimeFactor(number) {
-    let largestPrime = 1;
-    
-    // Start checking for prime factors from 2
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        // Check if the number is divisible by i
-        while (number % i === 0) {
-            largestPrime = i;
-            number /= i;
-        }
-    }
-    
-    // If the remaining number is greater than 1, update largestPrime
-    if (number > largestPrime) {
-        largestPrime = number;
-    }
-    
-    return largestPrime;
-}
-let number = 123456789;
-let result = largestPrimeFactor(number);
+function quicksort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        const pivot = arr[0];
+        const left = [];
+        const right = [];
 
-console.log(`The largest prime factor of ${number} is: ${result}`);
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
+        }
+
+        return quicksort(left).concat(pivot, quicksort(right));
+    }
+}
+
+// Example usage:
+const arr = [5, 2, 9, 3, 7, 6, 1, 8, 4];
+const sortedArr = quicksort(arr);
+console.log(sortedArr);
