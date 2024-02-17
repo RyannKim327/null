f9fdfa7d9f315b312d81972fa04eac74c9ad8556
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:49:52 +0800

    2-17-2024 16:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,22 @@
-const axios = require('axios');
+function isAnagram(str1, str2) {
+    // Remove spaces and punctuation, and convert to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
-async function fetchData() {
-  try {
-    const response = await axios.get('https://api.example.com/data');
-    console.log(response.data);
-    return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+    // Sort the characters in the strings
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
+
+    // Compare the sorted strings
+    return str1 === str2;
 }
 
-fetchData();
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
