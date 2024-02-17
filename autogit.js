e68a8457f61e5ebe15287a3847173d12d6a6f9dc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:48:37 +0800

    2-17-2024 16:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,27 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-const inputString = "Hello, World!";
-const stringWithoutVowels = removeVowels(inputString);
+function findMiddleElement(head) {
+    let slow = head;
+    let fast = head;
 
-console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow.value;
+}
+
+// Example linked list
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+console.log(findMiddleElement(head)); // Output: 3
