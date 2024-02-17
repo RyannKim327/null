e578c84b461f6723a2c781b4b98a295d36839bb5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:25:54 +0800

    2-17-2024 16:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,19 @@
-class BTreeNode {
-  constructor(t, leaf) {
-    this.t = t; // degree of the B-tree
-    this.keys = [];
-    this.children = [];
-    this.leaf = leaf;
-  }
-}
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
 
-class BTree {
-  constructor(t) {
-    this.root = null;
-    this.t = t;
-  }
+    let slow = head;
+    let fast = head.next;
 
-  insert(key) {
-    if (this.root === null) {
-      this.root = new BTreeNode(this.t, true);
-      this.root.keys.push(key);
-    } else {
-      if (this.root.keys.length === 2 * this.t - 1) {
-        let newRoot = new BTreeNode(this.t, false);
-        newRoot.children.push(this.root);
-        this.splitChild(newRoot, 0);
-
-        let i = 0;
-        if (newRoot.keys[0] < key) {
-          i++;
+    while (fast && fast.next) {
+        if (slow === fast) {
+            return true;
         }
-        this.insertNonFull(newRoot.children[i], key);
 
-        this.root = newRoot;
-      } else {
-        this.insertNonFull(this.root, key);
-      }
+        slow = slow.next;
+        fast = fast.next.next;
     }
-  }
 
-  insertNonFull(node, key) {
-    let i = node.keys.length - 1;
-
-    if (node.leaf) {
-      while (i >= 0 && key < node.keys[i]) {
-        node.keys[i+1] = node.keys[i];
-        i--;
-      }
-      node.keys[i+1] = key;
-    } else {
-      while (i >= 0 && key < node.keys[i]) {
-        i--;
-      }
-      i++;
-      if (node.children[i].keys.length === 2 * this.t - 1) {
-        this.splitChild(node, i);
-        if (key > node.keys[i]) {
-          i++;
-        }
-      }
-      this.insertNonFull(node.children[i], key);
-    }
-  }
-
-  splitChild(parent, index) {
-    let t = this.t;
-    let child = parent.children[index];
-    let newChild = new BTreeNode(t, child.leaf);
-    parent.keys.splice(index, 0, child.keys[t-1]);
-    parent.children.splice(index+1, 0, newChild);
-
-    newChild.keys = child.keys.splice(t);
-    if (!child.leaf) {
-      newChild.children = child.children.splice(t);
-    }
-  }
+    return false;
 }
-
-// Example usage
-let btree = new BTree(2);
-btree.insert(10);
-btree.insert(20);
-btree.insert(5);
-btree.insert(6);
