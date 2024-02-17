317ea96143720335192e2201121b7ec591ff2cb0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:58:13 +0800

    2-17-2024 16:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,14 @@
-class ListNode {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+function maxSubArray(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
     }
+
+    return maxSoFar;
 }
 
-function getIntersectionNode(headA, headB) {
-    let lenA = getLength(headA);
-    let lenB = getLength(headB);
-
-    let currA = headA;
-    let currB = headB;
-
-    // Move the pointers to the same start position
-    while (lenA > lenB) {
-        currA = currA.next;
-        lenA--;
-    }
-
-    while (lenB > lenA) {
-        currB = currB.next;
-        lenB--;
-    }
-
-    // Traverse both lists until a common node is found
-    while (currA !== currB) {
-        currA = currA.next;
-        currB = currB.next;
-    }
-
-    return currA;
-}
-
-function getLength(node) {
-    let length = 0;
-    while (node) {
-        length++;
-        node = node.next;
-    }
-    return length;
-}
-
-// Example Usage
-let node1 = new ListNode(1);
-let node2 = new ListNode(2);
-let commonNode = new ListNode(3);
-let node4 = new ListNode(4);
-
-node1.next = node2;
-node2.next = commonNode;
-node4.next = commonNode;
-
-let intersectionNode = getIntersectionNode(node1, node4);
-console.log(intersectionNode.value); // Output: 3
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArray(arr)); // Output: 6 (The subarray [4, -1, 2, 1] has the maximum sum)
