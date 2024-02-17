28928c3214afcdc7fd2e5969d42c54a0d66a0bba
RyannKim327 <rksesgundo123@gmail.com>
2024-02-17 16:25:48 +0800

    2-17-2024 16:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,82 @@
-function bmHorspoolSearch(text, pattern) {
-    const patternLength = pattern.length;
-    const textLength = text.length;
-
-    if (patternLength === 0) {
-        return -1;
-    }
-
-    const badMatchTable = new Array(256).fill(patternLength);
-
-    for (let i = 0; i < patternLength - 1; i++) {
-        badMatchTable[pattern.charCodeAt(i)] = patternLength - 1 - i;
-    }
-
-    let textIndex = patternLength - 1;
-
-    while (textIndex < textLength) {
-        let patternIndex = patternLength - 1;
-        let matchIndex = textIndex;
-
-        while (patternIndex >= 0 && text[matchIndex] === pattern[patternIndex]) {
-            patternIndex--;
-            matchIndex--;
-        }
-
-        if (patternIndex === -1) {
-            return matchIndex + 1; // Match found
-        }
-
-        textIndex += badMatchTable[text.charCodeAt(textIndex)];
-    }
-
-    return -1; // Match not found
+class BTreeNode {
+  constructor(t, leaf) {
+    this.t = t; // degree of the B-tree
+    this.keys = [];
+    this.children = [];
+    this.leaf = leaf;
+  }
 }
 
-// Test the function
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-const pattern = "consectetur";
-const matchIndex = bmHorspoolSearch(text, pattern);
+class BTree {
+  constructor(t) {
+    this.root = null;
+    this.t = t;
+  }
 
-if (matchIndex !== -1) {
-    console.log(`Pattern found at index ${matchIndex}`);
-} else {
-    console.log("Pattern not found");
+  insert(key) {
+    if (this.root === null) {
+      this.root = new BTreeNode(this.t, true);
+      this.root.keys.push(key);
+    } else {
+      if (this.root.keys.length === 2 * this.t - 1) {
+        let newRoot = new BTreeNode(this.t, false);
+        newRoot.children.push(this.root);
+        this.splitChild(newRoot, 0);
+
+        let i = 0;
+        if (newRoot.keys[0] < key) {
+          i++;
+        }
+        this.insertNonFull(newRoot.children[i], key);
+
+        this.root = newRoot;
+      } else {
+        this.insertNonFull(this.root, key);
+      }
+    }
+  }
+
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+
+    if (node.leaf) {
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i+1] = node.keys[i];
+        i--;
+      }
+      node.keys[i+1] = key;
+    } else {
+      while (i >= 0 && key < node.keys[i]) {
+        i--;
+      }
+      i++;
+      if (node.children[i].keys.length === 2 * this.t - 1) {
+        this.splitChild(node, i);
+        if (key > node.keys[i]) {
+          i++;
+        }
+      }
+      this.insertNonFull(node.children[i], key);
+    }
+  }
+
+  splitChild(parent, index) {
+    let t = this.t;
+    let child = parent.children[index];
+    let newChild = new BTreeNode(t, child.leaf);
+    parent.keys.splice(index, 0, child.keys[t-1]);
+    parent.children.splice(index+1, 0, newChild);
+
+    newChild.keys = child.keys.splice(t);
+    if (!child.leaf) {
+      newChild.children = child.children.splice(t);
+    }
+  }
 }
+
+// Example usage
+let btree = new BTree(2);
+btree.insert(10);
+btree.insert(20);
+btree.insert(5);
+btree.insert(6);
