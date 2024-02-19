5441b4f902f38d67f6a2ee7044d0d07001dfc033
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:50:48 +0800

    2-19-2024 19:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,68 @@
-// Node class to represent each node in the linked list
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
     }
 }
 
-// Linked list class to manage the list
-class LinkedList {
+class Trie {
     constructor() {
-        this.head = null;
-        this.size = 0;
+        this.root = new TrieNode();
     }
 
-    // Method to add a new node to the end of the list
-    add(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
+    insert(word) {
+        let currentNode = this.root;
+        
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i];
+            
+            if (!currentNode.children[char]) {
+                currentNode.children[char] = new TrieNode();
             }
-            current.next = newNode;
+            
+            currentNode = currentNode.children[char];
         }
-        this.size++;
+        
+        currentNode.isEndOfWord = true;
     }
 
-    // Method to print the linked list
-    print() {
-        let current = this.head;
-        while (current) {
-            console.log(current.value);
-            current = current.next;
+    search(word) {
+        let currentNode = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            const char = word[i];
+            
+            if (!currentNode.children[char]) {
+                return false;
+            }
+            
+            currentNode = currentNode.children[char];
         }
+        
+        return currentNode.isEndOfWord;
+    }
+
+    startsWith(prefix) {
+        let currentNode = this.root;
+
+        for (let i = 0; i < prefix.length; i++) {
+            const char = prefix[i];
+            
+            if (!currentNode.children[char]) {
+                return false;
+            }
+            
+            currentNode = currentNode.children[char];
+        }
+        
+        return true;
     }
 }
 
 // Usage example
-const list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(3);
-list.print();
+const trie = new Trie();
+trie.insert("apple");
+trie.insert("application");
+console.log(trie.search("apple")); // Output: true
+console.log(trie.search("app")); // Output: false
+console.log(trie.startsWith("app")); // Output: true
