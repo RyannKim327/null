b7a9595e66ef613e844c88d5ac33ace49bfe6eb4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:41:35 +0800

    2-19-2024 19:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,108 +1,22 @@
-class AVLNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.height = 1;
+function findFirstRepeatedCharacter(str) {
+    let charMap = {};
+
+    for (let char of str) {
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = true;
+        }
     }
+
+    return null; // If no repeated character found
 }
 
-class AVLTree {
-    constructor() {
-        this.root = null;
-    }
+let inputString = "hello world";
+let repeatedChar = findFirstRepeatedCharacter(inputString);
 
-    getHeight(node) {
-        if (node === null) {
-            return 0;
-        }
-        return node.height;
-    }
-
-    getBalanceFactor(node) {
-        if (node === null) {
-            return 0;
-        }
-        return this.getHeight(node.left) - this.getHeight(node.right);
-    }
-
-    updateHeight(node) {
-        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-    }
-
-    rotateRight(y) {
-        const x = y.left;
-        const T2 = x.right;
-
-        x.right = y;
-        y.left = T2;
-
-        this.updateHeight(y);
-        this.updateHeight(x);
-
-        return x;
-    }
-
-    rotateLeft(x) {
-        const y = x.right;
-        const T2 = y.left;
-
-        y.left = x;
-        x.right = T2;
-
-        this.updateHeight(x);
-        this.updateHeight(y);
-
-        return y;
-    }
-
-    insert(value) {
-        const insertHelper = (node, value) => {
-            if (node === null) {
-                return new AVLNode(value);
-            }
-
-            if (value < node.value) {
-                node.left = insertHelper(node.left, value);
-            } else {
-                node.right = insertHelper(node.right, value);
-            }
-
-            this.updateHeight(node);
-
-            const balance = this.getBalanceFactor(node);
-
-            // Left Left Case
-            if (balance > 1 && value < node.left.value) {
-                return this.rotateRight(node);
-            }
-
-            // Right Right Case
-            if (balance < -1 && value > node.right.value) {
-                return this.rotateLeft(node);
-            }
-
-            // Left Right Case
-            if (balance > 1 && value > node.left.value) {
-                node.left = this.rotateLeft(node.left);
-                return this.rotateRight(node);
-            }
-
-            // Right Left Case
-            if (balance < -1 && value < node.right.value) {
-                node.right = this.rotateRight(node.right);
-                return this.rotateLeft(node);
-            }
-
-            return node;
-        };
-
-        this.root = insertHelper(this.root, value);
-    }
+if (repeatedChar) {
+    console.log("The first repeated character is: " + repeatedChar);
+} else {
+    console.log("No repeated character found in the string.");
 }
-
-// Example usage
-const avlTree = new AVLTree();
-avlTree.insert(10);
-avlTree.insert(20);
-avlTree.insert(30);
