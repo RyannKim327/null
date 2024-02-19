a014c21fe116032ea65cc713fbf4e0d59462831c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:46:31 +0800

    2-19-2024 19:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,11 @@
-function isPalindrome(str) {
-    let i = 0;
-    let j = str.length - 1;
-
-    while (i < j) {
-        if (str[i] !== str[j]) {
-            return false;
-        }
-        i++;
-        j--;
+function getStringLength(str) {
+    let count = 0;
+    while (str[count] !== undefined) {
+        count++;
     }
-
-    return true;
+    return count;
 }
 
-// Test the function
-const str1 = "racecar";
-const str2 = "hello";
-
-console.log(isPalindrome(str1)); // true
-console.log(isPalindrome(str2)); // false
+let str = "Hello, World!";
+let length = getStringLength(str);
+console.log(length); // Output: 13
