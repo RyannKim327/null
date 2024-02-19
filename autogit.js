fc174c80a861f785ba1034048d9c96c4ce8a55ed
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:43:12 +0800

    2-19-2024 19:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,21 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function countingSort(arr) {
+    const max = Math.max(...arr);
+    const countArray = new Array(max + 1).fill(0);
+
+    arr.forEach(num => countArray[num]++);
+  
+    let sortedIndex = 0; // Index for sorted array
+    countArray.forEach((count, num) => {
+        while (count > 0) {
+            arr[sortedIndex++] = num;
+            count--;
+        }
+    });
+
+    return arr;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.size = 0;
-    }
-
-    add(data) {
-        const newNode = new Node(data);
-        let current;
-
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            current = this.head;
-
-            while (current.next) {
-                current = current.next;
-            }
-
-            current.next = newNode;
-        }
-
-        this.size++;
-    }
-
-    remove(data) {
-        let current = this.head;
-        let prevNode = null;
-
-        while (current !== null) {
-            if (current.data === data) {
-                if (prevNode === null) {
-                    this.head = current.next;
-                } else {
-                    prevNode.next = current.next;
-                }
-
-                this.size--;
-                return current.data;
-            }
-
-            prevNode = current;
-            current = current.next;
-        }
-
-        return null;
-    }
-
-    print() {
-        let current = this.head;
-        let result = '';
-
-        while (current !== null) {
-            result += current.data + ' ';
-            current = current.next;
-        }
-
-        console.log(result.trim());
-    }
-}
-
-// Example Usage:
-const linkedList = new LinkedList();
-
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-
-linkedList.print(); // Output: 1 2 3
-
-linkedList.remove(2);
-
-linkedList.print(); // Output: 1 3
+// Example usage
+const unsortedArray = [4, 2, 2, 8, 3, 3, 1];
+const sortedArray = countingSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 2, 3, 3, 4, 8]
