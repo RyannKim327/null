ae2eb3093edc471ff81f3ad28dd79b4003c699e6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:43:33 +0800

    2-19-2024 19:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,4 @@
-class Node {
-    constructor() {
-        this.children = {};
-    }
-}
-class SuffixTree {
-    constructor() {
-        this.root = new Node();
-    }
-
-    // Function to add a suffix to the suffix tree
-    addSuffix(suffix) {
-        let currentNode = this.root;
-
-        for (let i = 0; i < suffix.length; i++) {
-            const char = suffix[i];
-
-            if (!currentNode.children[char]) {
-                currentNode.children[char] = new Node();
-            }
-
-            currentNode = currentNode.children[char];
-        }
-    }
-}
-function buildSuffixTree(text) {
-    const suffixTree = new SuffixTree();
-
-    for (let i = 0; i < text.length; i++) {
-        suffixTree.addSuffix(text.slice(i));
-    }
-
-    return suffixTree;
-}
+var string1 = "Hello";
+var string2 = "World";
+var result = string1 + " " + string2;
+console.log(result); // Output: Hello World
