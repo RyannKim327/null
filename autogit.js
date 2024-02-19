27a8d88e7647e62f32e2f982531c286fc1c4c3d4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:08:52 +0800

    2-19-2024 20:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,29 @@
-class Graph {
-  constructor() {
-    this.nodes = {};
-  }
-
-  addNode(node) {
-    this.nodes[node] = [];
-  }
-
-  addEdge(node1, node2) {
-    this.nodes[node1].push(node2);
-    this.nodes[node2].push(node1);
-  }
-}
-
-function biDirectionalSearch(graph, source, target) {
-  let visitedFromSource = {};
-  let visitedFromTarget = {};
-
-  let queueSource = [source];
-  let queueTarget = [target];
-
-  visitedFromSource[source] = true;
-  visitedFromTarget[target] = true;
-
-  while (queueSource.length > 0 && queueTarget.length > 0) {
-    let currentNodeSource = queueSource.shift();
-    let currentNodeTarget = queueTarget.shift();
-
-    if (visitedFromTarget[currentNodeSource] || visitedFromSource[currentNodeTarget]) {
-      return true;
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-
-    for (let neighbor of graph.nodes[currentNodeSource]) {
-      if (!visitedFromSource[neighbor]) {
-        visitedFromSource[neighbor] = true;
-        queueSource.push(neighbor);
-      }
-    }
-
-    for (let neighbor of graph.nodes[currentNodeTarget]) {
-      if (!visitedFromTarget[neighbor]) {
-        visitedFromTarget[neighbor] = true;
-        queueTarget.push(neighbor);
-      }
-    }
-  }
-
-  return false;
+    
+    return -1; // element not found
 }
 
 // Example usage
-let graph = new Graph();
-graph.addNode('A');
-graph.addNode('B');
-graph.addNode('C');
-graph.addNode('D');
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'C');
-graph.addEdge('C', 'D');
+const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+const target = 10;
+const index = binarySearch(arr, target);
 
-console.log(biDirectionalSearch(graph, 'A', 'D')); // Output: true
+if (index !== -1) {
+    console.log(`Element found at index: ${index}`);
+} else {
+    console.log("Element not found");
+}
