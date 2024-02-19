beee721fe8c8c58f2f2358f9db8508b37b41bb4f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:04:56 +0800

    2-19-2024 20:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,28 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-console.log(lowercaseStr); // Output: "hello world"
+function findMiddleElement(head) {
+  let slowPointer = head;
+  let fastPointer = head;
+
+  while (fastPointer && fastPointer.next) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  return slowPointer.value;
+}
+
+// Create a sample linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Finding the middle element of the linked list
+console.log(findMiddleElement(head)); // Output: 3
