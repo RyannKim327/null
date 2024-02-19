deeb90cd6e2b081c1cba54bc6d7324438f033cf3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:44:39 +0800

    2-19-2024 19:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,57 @@
-function maxSubarraySum(arr) {
-  let maxEndingHere = arr[0];
-  let maxSoFar = arr[0];
-  
-  for (let i = 1; i < arr.length; i++) {
-    maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-    maxSoFar = Math.max(maxSoFar, maxEndingHere);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-  
-  return maxSoFar;
 }
 
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]; // Example array
-console.log(maxSubarraySum(arr)); // Output: 6 (the maximum sum subarray is [4, -1, 2, 1])
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(value) {
+    let newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  findNthNodeFromEnd(n) {
+    let slow = this.head;
+    let fast = this.head;
+
+    // Move the fast pointer to the nth node from the beginning
+    for (let i = 0; i < n; i++) {
+      if (fast === null) {
+        return null; // Not enough nodes
+      }
+      fast = fast.next;
+    }
+
+    // Move both pointers until the fast pointer reaches the end
+    while (fast !== null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    return slow;
+  }
+}
+
+// Test the function
+let linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+
+let nthNodeFromEnd = linkedList.findNthNodeFromEnd(2);
+console.log(nthNodeFromEnd.value); // Output: 4
