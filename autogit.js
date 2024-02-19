a38e17c5013896a26211b163a9745ff1c52dc688
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:04:40 +0800

    2-19-2024 20:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,5 @@
-function bubbleSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    return arr;
-}
+// Taking input from user
+let userInput = prompt("Enter something: ");
 
-// Test the bubbleSort function
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: ", arr);
-console.log("Sorted array: ", bubbleSort(arr));
+// Displaying the user input
+console.log("You entered: " + userInput);
