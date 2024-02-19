49829973c616da1dc08c7894de4c5705112a248f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:59:54 +0800

    2-19-2024 19:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,28 @@
-function tarjanStronglyConnectedComponents(graph) {
-    let index = 0;
-    let stack = [];
-    let indexes = new Map();
-    let lowlinks = new Map();
-    let onStack = new Set();
-    let sccs = [];
-
-    function strongConnect(node) {
-        indexes.set(node, index);
-        lowlinks.set(node, index);
-        index++;
-        stack.push(node);
-        onStack.add(node);
-
-        graph[node].forEach(neighbor => {
-            if (!indexes.has(neighbor)) {
-                strongConnect(neighbor);
-                lowlinks.set(node, Math.min(lowlinks.get(node), lowlinks.get(neighbor)));
-            } else if (onStack.has(neighbor)) {
-                lowlinks.set(node, Math.min(lowlinks.get(node), indexes.get(neighbor)));
+function longestCommonSubstring(str1, str2) {
+    let longestSubstringLength = 0;
+    let endIndex = 0;
+  
+    const dp = Array(str1.length + 1)
+        .fill(0)
+        .map(() => Array(str2.length + 1).fill(0));
+  
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > longestSubstringLength) {
+                    longestSubstringLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
             }
-        });
-
-        if (lowlinks.get(node) === indexes.get(node)) {
-            let scc = [];
-            let poppedNode;
-            do {
-                poppedNode = stack.pop();
-                onStack.delete(poppedNode);
-                scc.push(poppedNode);
-            } while (poppedNode !== node);
-            sccs.push(scc);
         }
     }
-
-    Object.keys(graph).forEach(node => {
-        if (!indexes.has(node)) {
-            strongConnect(node);
-        }
-    });
-
-    return sccs;
+  
+    return str1.substring(endIndex - longestSubstringLength + 1, endIndex + 1);
 }
-const graph = {
-    A: ['B', 'C'],
-    B: ['D'],
-    C: ['A', 'E'],
-    D: ['C'],
-    E: ['F'],
-    F: ['G'],
-    G: ['E', 'H'],
-    H: ['I'],
-    I: ['J'],
-    J: ['H']
-};
 
-const sccs = tarjanStronglyConnectedComponents(graph);
-console.log(sccs);
+const str1 = "abcdefg";
+const str2 = "bcdeftg";
+console.log(longestCommonSubstring(str1, str2)); // Output: "cdef"
