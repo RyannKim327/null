a4f7f5f37914b20bcf4eb64668282c5f14044080
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:41:51 +0800

    2-19-2024 19:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,12 @@
-class Node {
-    constructor() {
-        this.children = {};
+function calculateFactorial(num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * calculateFactorial(num - 1);
     }
 }
 
-class SuffixTree {
-    constructor(text) {
-        this.root = new Node();
-        for (let i = 0; i < text.length; i++) {
-            this.addSuffix(text.slice(i));
-        }
-    }
-
-    addSuffix(suffix) {
-        let current = this.root;
-        for (let i = 0; i < suffix.length; i++) {
-            let char = suffix[i];
-            if (!current.children[char]) {
-                current.children[char] = new Node();
-            }
-            current = current.children[char];
-        }
-    }
-
-    search(pattern) {
-        let current = this.root;
-        for (let i = 0; i < pattern.length; i++) {
-            let char = pattern[i];
-            if (!current.children[char]) {
-                return false;
-            }
-            current = current.children[char];
-        }
-        return true;
-    }
-}
+// Test the function with an example number
+const number = 5;
+const result = calculateFactorial(number);
+console.log(`The factorial of ${number} is: ${result}`);
