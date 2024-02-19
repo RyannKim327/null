b26eaa4ff06258adf1b0880f40c9c02bf20c7113
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:57:56 +0800

    2-19-2024 19:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,45 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
+class Node { 
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+    
+    insert(data) {
+        const newNode = new Node(data);
+        
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
         }
     }
-    return count;
+    
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
 }
 
 // Example usage
-let str = "Hello, World!";
-let char = "o";
-let occurrences = countOccurrences(str, char);
-console.log(`The character "${char}" occurs ${occurrences} times in the string.`);
+const binaryTree = new BinaryTree();
+binaryTree.insert(10);
+binaryTree.insert(5);
+binaryTree.insert(15);
