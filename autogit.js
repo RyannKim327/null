b4c89fec656c3811ea7c16d08a3bdb253ec3ff7e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:41:44 +0800

    2-19-2024 19:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,37 @@
-function findFirstRepeatedCharacter(str) {
-    let charMap = {};
+class Node {
+    constructor() {
+        this.children = {};
+    }
+}
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
+class SuffixTree {
+    constructor(text) {
+        this.root = new Node();
+        for (let i = 0; i < text.length; i++) {
+            this.addSuffix(text.slice(i));
         }
     }
 
-    return null; // If no repeated character found
-}
+    addSuffix(suffix) {
+        let current = this.root;
+        for (let i = 0; i < suffix.length; i++) {
+            let char = suffix[i];
+            if (!current.children[char]) {
+                current.children[char] = new Node();
+            }
+            current = current.children[char];
+        }
+    }
 
-let inputString = "hello world";
-let repeatedChar = findFirstRepeatedCharacter(inputString);
-
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("No repeated character found in the string.");
+    search(pattern) {
+        let current = this.root;
+        for (let i = 0; i < pattern.length; i++) {
+            let char = pattern[i];
+            if (!current.children[char]) {
+                return false;
+            }
+            current = current.children[char];
+        }
+        return true;
+    }
 }
