37034a72ab2231ac29e5b2d7a0978bdd9c36210e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:55:59 +0800

    2-19-2024 19:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,26 @@
-function mergeSort(arr) {
-    if (arr.length < 2) return arr;
+function areAnagrams(str1, str2) {
+    // Remove non-alphabetic characters and convert to lowercase
+    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
 
-    const len = arr.length;
-    const sortedArr = [...arr];
-    const buffer = new Array(len);
+    // Convert strings to arrays of characters
+    let arr1 = str1.split('');
+    let arr2 = str2.split('');
 
-    for (let size = 1; size < len; size *= 2) {
-        for (let leftStart = 0; leftStart < len; leftStart += 2 * size) {
-            const left = leftStart;
-            const middle = Math.min(left + size, len);
-            const right = Math.min(left + 2 * size, len);
+    // Sort arrays
+    arr1.sort();
+    arr2.sort();
 
-            merge(sortedArr, buffer, left, middle, right);
-        }
-
-        // Swap the arrays
-        const temp = sortedArr;
-        sortedArr = buffer;
-        buffer = temp;
-    }
-
-    return sortedArr;
-}
-
-function merge(arr, buffer, left, middle, right) {
-    let i = left;
-    let j = middle;
-
-    for (let k = left; k < right; k++) {
-        if (i < middle && (j >= right || arr[i] <= arr[j])) {
-            buffer[k] = arr[i];
-            i++;
-        } else {
-            buffer[k] = arr[j];
-            j++;
-        }
-    }
+    // Compare sorted arrays
+    return arr1.join('') === arr2.join('');
 }
 
 // Example usage
-const arr = [5, 3, 8, 6, 2, 7, 1, 4];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr);
+const string1 = "listen";
+const string2 = "silent";
+
+if (areAnagrams(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
