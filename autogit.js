f7c9d56042f1e468690a315830d959b6d6ba8f29
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:05:10 +0800

    2-19-2024 20:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function largestPrimeFactor(number) {
+    let factor = 2;
+    
+    while (factor <= number) {
+        if (number % factor === 0) {
+            number /= factor;
+        } else {
+            factor++;
+        }
+    }
+    
+    return factor;
 }
 
-function findMiddleElement(head) {
-  let slowPointer = head;
-  let fastPointer = head;
-
-  while (fastPointer && fastPointer.next) {
-    slowPointer = slowPointer.next;
-    fastPointer = fastPointer.next.next;
-  }
-
-  return slowPointer.value;
-}
-
-// Create a sample linked list
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-// Finding the middle element of the linked list
-console.log(findMiddleElement(head)); // Output: 3
+// Example usage:
+const number = 84;
+const largestPrime = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestPrime}`);
