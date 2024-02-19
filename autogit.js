a8a3297e4cf192e94efb34d7972fc5578b474aeb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:00:55 +0800

    2-19-2024 20:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,30 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function areAnagrams(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const charCount = {};
+
+    for (let char of str1) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let char of str2) {
+        if (!charCount[char]) {
+            return false;
+        }
+        charCount[char] -= 1;
+    }
+
+    return true;
+}
+
+// Example usage
+const str1 = "listen";
+const str2 = "silent";
+
+if (areAnagrams(str1, str2)) {
+    console.log(`${str1} and ${str2} are anagrams.`);
+} else {
+    console.log(`${str1} and ${str2} are not anagrams.`);
+}
