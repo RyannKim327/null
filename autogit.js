153de329864a32f62ebac3a2071067e6fb7c9a43
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 20:09:32 +0800

    2-19-2024 20:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,8 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-    
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return -1; // element not found
-}
+let myString = "123";
+let myInteger = parseInt(myString);
 
-// Example usage
-const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
-const target = 10;
-const index = binarySearch(arr, target);
+console.log(myInteger); // Output: 123
+let myHex = "1A";
+let myNumber = parseInt(myHex, 16);
 
-if (index !== -1) {
-    console.log(`Element found at index: ${index}`);
-} else {
-    console.log("Element not found");
-}
+console.log(myNumber); // Output: 26
