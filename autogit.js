ea48db89aec7f22e52f34ab0e7e09bf752764a2b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:57:48 +0800

    2-19-2024 19:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,15 @@
-function findMajorityElement(nums) {
+function countOccurrences(str, char) {
     let count = 0;
-    let candidate = null;
-
-    for (let num of nums) {
-        if (count === 0) {
-            candidate = num;
-            count = 1;
-        } else if (num === candidate) {
-            count++;
-        } else {
-            count--;
-        }
-    }
-
-    // Validate the candidate
-    count = 0;
-    for (let num of nums) {
-        if (num === candidate) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
             count++;
         }
     }
-
-    if (count > nums.length / 2) {
-        return candidate;
-    } else {
-        return null; // No majority element found
-    }
+    return count;
 }
 
 // Example usage
-const nums = [2, 2, 1, 1, 1, 2, 2];
-const majorityElement = findMajorityElement(nums);
-if (majorityElement !== null) {
-    console.log(`The majority element is: ${majorityElement}`);
-} else {
-    console.log('No majority element found');
-}
+let str = "Hello, World!";
+let char = "o";
+let occurrences = countOccurrences(str, char);
+console.log(`The character "${char}" occurs ${occurrences} times in the string.`);
