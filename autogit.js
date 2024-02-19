ead1e2b2a86ca181eb2e6f9379dd6a0b272a7299
RyannKim327 <rksesgundo123@gmail.com>
2024-02-19 19:46:45 +0800

    2-19-2024 19:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,36 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+
+    let slow = head;
+    let fast = head.next;
+
+    while (fast && fast.next) {
+        if (slow === fast) {
+            return true;
+        }
+
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return false;
+}
+
+// Usage
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+node1.next = node2;
+node2.next = node3;
+node3.next = node1;
+
+console.log(hasCycle(node1)); // Output: true
