046fe8aac3cc5f8bc9f46d42ebc8038ebf81ef0b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:54:17 +0800

    2-20-2024 20:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,17 @@
-function heapSort(arr) {
-    buildMaxHeap(arr);
-
-    let endIndex = arr.length - 1;
-    while (endIndex > 0) {
-        // Swap root with last element
-        [arr[0], arr[endIndex]] = [arr[endIndex], arr[0]]; 
-        endIndex--;
-        
-        heapifyDown(arr, 0, endIndex);
+function isAnagram(str1, str2) {
+    const sanitizeString = function (str) {
+        return str.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
     }
 
-    return arr;
-}
-
-function buildMaxHeap(arr) {
-    let startIndex = Math.floor(arr.length / 2) - 1;
-    for (let i = startIndex; i >= 0; i--) {
-        heapifyDown(arr, i, arr.length - 1);
-    }
-}
-
-function heapifyDown(arr, startIndex, endIndex) {
-    let rootIndex = startIndex;
-    while (true) {
-        let childIndex = 2 * rootIndex + 1;
-        if (childIndex > endIndex) break;
-
-        if (childIndex + 1 <= endIndex && arr[childIndex] < arr[childIndex + 1]) {
-            childIndex++;
-        }
-
-        if (arr[rootIndex] < arr[childIndex]) {
-            [arr[rootIndex], arr[childIndex]] = [arr[childIndex], arr[rootIndex]];
-            rootIndex = childIndex;
-        } else {
-            break;
-        }
-    }
+    return sanitizeString(str1) === sanitizeString(str2);
 }
 
 // Example usage
-let arr = [12, 11, 13, 5, 6, 7];
-console.log(heapSort(arr));  // Output: [5, 6, 7, 11, 12, 13]
+const str1 = "listen";
+const str2 = "silent";
+
+if(isAnagram(str1, str2)) {
+    console.log(`${str1} and ${str2} are anagrams.`);
+} else {
+    console.log(`${str1} and ${str2} are not anagrams.`);
+}
