da286b381120311e70cdde79b47d52df41e6f149
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:49:15 +0800

    2-20-2024 20:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,13 @@
-function isAnagram(str1, str2) {
-  // Remove any non-alphanumeric characters and convert to lowercase
-  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
-  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-  // Check if the lengths are different
-  if (cleanStr1.length !== cleanStr2.length) {
-    return false;
-  }
-
-  // Create character frequency objects for both strings
-  const freq1 = {};
-  const freq2 = {};
-
-  // Populate character frequency objects
-  for (let char of cleanStr1) {
-    freq1[char] = (freq1[char] || 0) + 1;
-  }
-
-  for (let char of cleanStr2) {
-    freq2[char] = (freq2[char] || 0) + 1;
-  }
-
-  // Compare character frequency objects
-  for (let char in freq1) {
-    if (freq1[char] !== freq2[char]) {
-      return false;
+function isArraySorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
     }
-  }
-
-  return true;
+    return true;
 }
+const myArray = [1, 2, 3, 4, 5];
+console.log(isArraySorted(myArray)); // Output will be: true
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-console.log(isAnagram(string1, string2)); // true
+const myUnsortedArray = [5, 2, 8, 1];
+console.log(isArraySorted(myUnsortedArray)); // Output will be: false
