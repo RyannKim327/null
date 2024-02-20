a67ba277f53a521b0144a2b64858325bfcca3612
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:31:43 +0800

    2-20-2024 20:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,5 @@
-// Define the Node class to represent each node in the linked list
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+let str1 = 'Hello';
+let str2 = ' World';
+let result = str1 + str2;
 
-// Define the LinkedList class
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
-
-  // Method to add a new node to the linked list
-  add(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-    this.size++;
-  }
-
-  // Method to display the linked list
-  display() {
-    let current = this.head;
-    while (current) {
-      console.log(current.data);
-      current = current.next;
-    }
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.display();
+console.log(result); // Output: Hello World
