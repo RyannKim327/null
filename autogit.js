7788f400df7bb9c1a41f5857d2b18657ace6c69d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:15:41 +0800

    2-20-2024 20:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,48 @@
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
+function buildPatternTable(pattern) {
+    let patternTable = [0];
+    let prefixIndex = 0;
+    let suffixIndex = 1;
 
-let index = array.indexOf(elementToRemove);
-if (index > -1) {
-    array.splice(index, 1);
+    while (suffixIndex < pattern.length) {
+        if (pattern[suffixIndex] === pattern[prefixIndex]) {
+            prefixIndex++;
+            patternTable[suffixIndex] = prefixIndex;
+            suffixIndex++;
+        } else if (prefixIndex === 0) {
+            patternTable[suffixIndex] = 0;
+            suffixIndex++;
+        } else {
+            prefixIndex = patternTable[prefixIndex - 1];
+        }
+    }
+
+    return patternTable;
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
+function kmpSearch(text, pattern) {
+    let patternTable = buildPatternTable(pattern);
+    let textIndex = 0;
+    let patternIndex = 0;
+
+    while (textIndex < text.length) {
+        if (text[textIndex] === pattern[patternIndex]) {
+            if (patternIndex === pattern.length - 1) {
+                return textIndex - patternIndex;
+            }
+            textIndex++;
+            patternIndex++;
+        } else if (patternIndex === 0) {
+            textIndex++;
+        } else {
+            patternIndex = patternTable[patternIndex - 1];
+        }
+    }
+
+    return -1;
+}
+
+// Example usage
+let text = "ababcabab";
+let pattern = "abab";
+let index = kmpSearch(text, pattern);
+console.log(index); // Output: 4
