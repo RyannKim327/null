f545d590a74a8dee2fa5361824bf95fa6c8d6fc8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:23:10 +0800

    2-20-2024 20:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,15 @@
-function isPalindrome(str) {
-    for (let i = 0, j = str.length - 1; i < j; i++, j--) {
-        // Ignore non-alphanumeric characters
-        while (!isAlphaNumeric(str[i])) i++;
-        while (!isAlphaNumeric(str[j])) j--;
-
-        // Convert characters to lowercase for comparison
-        let char1 = str[i].toLowerCase();
-        let char2 = str[j].toLowerCase();
-
-        // If characters don't match, return false
-        if (char1 !== char2) {
+function isSortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
             return false;
         }
     }
     return true;
 }
 
-function isAlphaNumeric(char) {
-    return /[a-zA-Z0-9]/.test(char);
-}
+// Example usage:
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 7, 2, 9, 10];
 
-// Test cases
-console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
-console.log(isPalindrome("race a car")); // Output: false
+console.log(isSortedAscending(array1)); // Output: true
+console.log(isSortedAscending(array2)); // Output: false
