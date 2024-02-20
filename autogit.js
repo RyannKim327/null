67354e3c92a529b69bd258a59d96222461724220
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:39:46 +0800

    2-20-2024 20:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,34 @@
-function longestCommonSubstring(str1, str2) {
-    const matrix = Array.from({ length: str1.length + 1 }, () => Array.from({ length: str2.length + 1 }, () => 0));
-    let maxLength = 0;
-    let endIndex = 0;
-
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                matrix[i][j] = matrix[i - 1][j - 1] + 1;
-                if (matrix[i][j] > maxLength) {
-                    maxLength = matrix[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                matrix[i][j] = 0;
-            }
-        }
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-
-    if (maxLength === 0) {
-        return '';
-    }
-
-    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
 }
 
-const str1 = 'JavaScript is awesome';
-const str2 = 'Python is awesome too';
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-console.log(longestCommonSubstring(str1, str2)); // Output: ' is awesome'
+    findMiddle() {
+        let slowPointer = this.head;
+        let fastPointer = this.head;
+
+        while (fastPointer !== null && fastPointer.next !== null) {
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+        }
+
+        return slowPointer.data;
+    }
+}
+
+// Sample usage
+let list = new LinkedList();
+list.head = new Node(1);
+list.head.next = new Node(2);
+list.head.next.next = new Node(3);
+list.head.next.next.next = new Node(4);
+list.head.next.next.next.next = new Node(5);
+
+console.log(list.findMiddle()); // Output: 3
