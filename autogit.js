53817e0ef8fe8e7c4257e992eb1f3c2fc0052c83
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:26:42 +0800

    2-20-2024 20:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,26 @@
-function fibonacciSearch(arr, x) {
-    let fibMMm2 = 0;
-    let fibMMm1 = 1;
-    let fibM = fibMMm2 + fibMMm1;
+function selectionSort(arr) {
+    const n = arr.length;
 
-    while (fibM < arr.length) {
-        fibMMm2 = fibMMm1;
-        fibMMm1 = fibM;
-        fibM = fibMMm2 + fibMMm1;
-    }
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
 
-    let offset = -1;
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
 
-    while (fibM > 1) {
-        let i = Math.min(offset + fibMMm2, arr.length - 1);
-
-        if (arr[i] < x) {
-            fibM = fibMMm1;
-            fibMMm1 = fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-            offset = i;
-        } else if (arr[i] > x) {
-            fibM = fibMMm2;
-            fibMMm1 = fibMMm1 - fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-        } else {
-            return i;
+        if (minIndex !== i) {
+            const temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 
-    if (fibMMm1 && arr[offset + 1] === x) {
-        return offset + 1;
-    }
-
-    return -1;
+    return arr;
 }
 
-const array = [2, 4, 6, 8, 10, 12];
-const x = 6;
-const index = fibonacciSearch(array, x);
-
-if (index !== -1) {
-    console.log(`Element found at index ${index}`);
-} else {
-    console.log("Element not found in the array");
-}
+// Example
+const arr = [64, 25, 12, 22, 11];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + selectionSort(arr));
