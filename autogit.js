115e24c78d10c21366e845b18bae97e31206cb79
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:31:55 +0800

    2-20-2024 20:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,54 @@
-let str1 = 'Hello';
-let str2 = ' World';
-let result = str1 + str2;
+// Define a Node class to create individual nodes in the linked list
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-console.log(result); // Output: Hello World
+// Define a LinkedList class to manage the linked list
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Method to add a new node to the end of the linked list
+  append(data) {
+    let newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  // Method to find the length of the linked list
+  getLength() {
+    let current = this.head;
+    let length = 0;
+
+    while (current) {
+      length++;
+      current = current.next;
+    }
+
+    return length;
+  }
+}
+
+// Create a linked list and add some nodes to it
+let linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+// Find the length of the linked list
+let length = linkedList.getLength();
+console.log("Length of the linked list: " + length);
