3d63ffad8e37d4540e59814f4c91bd6d4dbd92e9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:59:30 +0800

    2-20-2024 20:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,63 @@
-let arr = [5, 10, 3, 8, 1];
-let max = Math.max(...arr);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-console.log(max); // Output: 10
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  printList() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current !== null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
+}
+
+// Example usage
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+
+console.log("Original list:");
+list.printList();
+
+list.reverse();
+
+console.log("Reversed list:");
+list.printList();
