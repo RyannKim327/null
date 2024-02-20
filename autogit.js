6ceabeb700d2cd4efa3d8cab874fa92010e15170
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:43:30 +0800

    2-20-2024 20:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,51 @@
-class PriorityQueue {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(value, priority) {
-    const item = { value, priority };
-    this.heap.push(item);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  extractMin() {
-    const min = this.heap[0];
-    const last = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = last;
-      this.trickleDown(0);
+// Node class represents each node in the linked list
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-    return min;
-  }
-
-  bubbleUp(index) {
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[parentIndex].priority <= this.heap[index].priority) {
-        break;
-      }
-      [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
-      index = parentIndex;
-    }
-  }
-
-  trickleDown(index) {
-    while (index < this.heap.length) {
-      const leftChild = index * 2 + 1;
-      const rightChild = index * 2 + 2;
-      let smallest = index;
-
-      if (leftChild < this.heap.length && this.heap[leftChild].priority < this.heap[smallest].priority) {
-        smallest = leftChild;
-      }
-
-      if (rightChild < this.heap.length && this.heap[rightChild].priority < this.heap[smallest].priority) {
-        smallest = rightChild;
-      }
-
-      if (smallest === index) {
-        break;
-      }
-
-      [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
-      index = smallest;
-    }
-  }
 }
 
-// Example Usage
-const pq = new PriorityQueue();
-pq.insert("Task 1", 3);
-pq.insert("Task 2", 1);
-pq.insert("Task 3", 2);
+// LinkedList class represents the linked list
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-console.log(pq.extractMin()); // { value: 'Task 2', priority: 1 }
-console.log(pq.extractMin()); // { value: 'Task 3', priority: 2 }
-console.log(pq.extractMin()); // { value: 'Task 1', priority: 3 }
+    // Function to add a new node to the end of the linked list
+    append(data) {
+        let newNode = new Node(data);
+
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Function to get the length of the linked list
+    getLength() {
+        let current = this.head;
+        let length = 0;
+
+        while (current !== null) {
+            length++;
+            current = current.next;
+        }
+
+        return length;
+    }
+}
+
+// Example usage
+let linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+
+console.log("Length of the linked list: " + linkedList.getLength());  // Output: Length of the linked list: 4
