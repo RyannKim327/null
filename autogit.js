6c70bd7940ed4a5252a1c2993afe428a8fc65a30
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:14:32 +0800

    2-20-2024 20:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,7 @@
-function findMajorityElement(arr) {
-    let frequency = new Map();
-    
-    for (let num of arr) {
-        if (frequency.has(num)) {
-            frequency.set(num, frequency.get(num) + 1);
-        } else {
-            frequency.set(num, 1);
-        }
-        
-        if (frequency.get(num) > arr.length / 2) {
-            return num;
-        }
-    }
-    
-    return null;
-}
+let array = [1, 2, 3, 4, 5];
 
-// Example
-const arr = [1, 2, 2, 2, 3, 2, 2, 4, 2];
-const majorityElement = findMajorityElement(arr);
-if (majorityElement !== null) {
-    console.log(`The majority element is: ${majorityElement}`);
-} else {
-    console.log('No majority element found');
-}
+// Remove element at index 2
+let index = 2;
+array.splice(index, 1);
+
+console.log(array); // Output: [1, 2, 4, 5]
