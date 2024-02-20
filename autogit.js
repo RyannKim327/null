3f00cfeee4f28ddd5e58f96c916a24d58b1779a5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:48:35 +0800

    2-20-2024 20:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,49 @@
-function isPalindrome(str) {
-    // Remove special characters and spaces from the string
-    const cleanStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-
-    // Reverse the string
-    const reversedStr = cleanStr.split('').reverse().join('');
-
-    // Check if the original string is equal to its reversed version
-    return cleanStr === reversedStr;
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
-// Test the function
-const string1 = "A man, a plan, a canal, Panama";
-console.log(isPalindrome(string1)); // Output: true
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-const string2 = "hello world";
-console.log(isPalindrome(string2)); // Output: false
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+
+  getLength() {
+    let count = 0;
+    let current = this.head;
+
+    while (current) {
+      count++;
+      current = current.next;
+    }
+
+    return count;
+  }
+}
+
+// Creating a linked list
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+
+// Getting the length of the linked list
+const length = linkedList.getLength();
+console.log(length); // Output: 3
