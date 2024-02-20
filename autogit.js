ed4324d44804bf9fefe2ccaf324beecd9a9f1b67
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:52:43 +0800

    2-20-2024 20:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,38 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
+function findMedianSortedArrays(nums1, nums2) {
+    let mergedArray = [];
+    let i = 0, j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+            mergedArray.push(nums1[i]);
+            i++;
+        } else {
+            mergedArray.push(nums2[j]);
+            j++;
+        }
+    }
+
+    while (i < nums1.length) {
+        mergedArray.push(nums1[i]);
+        i++;
+    }
+
+    while (j < nums2.length) {
+        mergedArray.push(nums2[j]);
+        j++;
+    }
+
+    const totalLength = mergedArray.length;
+    const mid = Math.floor(totalLength / 2);
+
+    if (totalLength % 2 === 0) {
+        return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
+    } else {
+        return mergedArray[mid];
+    }
+}
+
+// Example
+const nums1 = [1, 3];
+const nums2 = [2];
+console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2
