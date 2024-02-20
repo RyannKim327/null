6676eb91ae13ff5477a11eaa8deaa6a67b24d8d4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:54:37 +0800

    2-20-2024 20:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,28 @@
-function isAnagram(str1, str2) {
-    const sanitizeString = function (str) {
-        return str.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+function longestIncreasingSubsequence(arr) {
+    let n = arr.length;
+    let lis = new Array(n).fill(1);
+
+    for (let i = 1; i < n; i++) {
+        for (let j = 0; j < i; j++) {
+            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
+                lis[i] = lis[j] + 1;
+            }
+        }
     }
 
-    return sanitizeString(str1) === sanitizeString(str2);
+    let maxLength = Math.max(...lis);
+    let longestSubsequence = [];
+
+    for (let i = n - 1; i >= 0; i--) {
+        if (lis[i] === maxLength) {
+            longestSubsequence.unshift(arr[i]);
+            maxLength--;
+        }
+    }
+
+    return longestSubsequence;
 }
 
-// Example usage
-const str1 = "listen";
-const str2 = "silent";
-
-if(isAnagram(str1, str2)) {
-    console.log(`${str1} and ${str2} are anagrams.`);
-} else {
-    console.log(`${str1} and ${str2} are not anagrams.`);
-}
+// Example
+let arr = [10, 22, 9, 33, 21, 50, 41, 60, 80];
+console.log(longestIncreasingSubsequence(arr)); // Output: [ 10, 22, 33, 50, 60, 80 ]
