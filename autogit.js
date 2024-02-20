49df5861e80d6d239d628af0363138da8b433cf2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:32:57 +0800

    2-20-2024 20:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,38 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function buildBadCharTable(pattern) {
+    const table = new Array(256).fill(-1);
+    for (let i = 0; i < pattern.length; i++) {
+        table[pattern.charCodeAt(i)] = i;
     }
+    return table;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+function boyerMooreSearch(text, pattern) {
+    const badCharTable = buildBadCharTable(pattern);
+    const patternLength = pattern.length;
+    const textLength = text.length;
+    let shift = 0;
 
-    add(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
+    while (shift <= textLength - patternLength) {
+        let j = patternLength - 1;
+        while (j >= 0 && pattern[j] === text[shift + j]) {
+            j--;
+        }
+        if (j < 0) {
+            return shift; // pattern found
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
+            shift += Math.max(1, j - badCharTable[text.charCodeAt(shift + j)]);
         }
     }
+
+    return -1; // pattern not found
 }
 
-function reverseLinkedList(head) {
-    let prev = null;
-    let current = head;
-    while (current) {
-        let next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
+const text = "THIS IS A TEST TEXT";
+const pattern = "TEST";
+const index = boyerMooreSearch(text, pattern);
+
+if (index !== -1) {
+    console.log(`Pattern found at index ${index}`);
+} else {
+    console.log("Pattern not found");
 }
-
-function areLinkedListsEqual(list1, list2) {
-    let current1 = list1;
-    let current2 = list2;
-    
-    while (current1 && current2) {
-        if (current1.data !== current2.data) {
-            return false;
-        }
-        current1 = current1.next;
-        current2 = current2.next;
-    }
-    
-    return true;
-}
-
-function isPalindromeLinkedList(list) {
-    let slow = list.head;
-    let fast = list.head;
-    let prev = null;
-    
-    while (fast && fast.next) {
-        prev = slow;
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    
-    prev.next = null;
-    
-    let secondHalf = reverseLinkedList(slow);
-    
-    return areLinkedListsEqual(list.head, secondHalf);
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(2);
-linkedList.add(1);
-
-console.log(isPalindromeLinkedList(linkedList)); // Output: true
