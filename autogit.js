9596a40a4b61b1db2115b6530d20e51e62206f18
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:02:38 +0800

    2-20-2024 21:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,52 @@
-function findFirstRepeatedCharacter(str) {
-    let charSet = new Set();
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
+  }
 
-    for (let char of str) {
-        if (charSet.has(char)) {
-            return char;
-        }
-        charSet.add(char);
+  hashFunction(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
     }
+    return hash % this.size;
+  }
 
-    return "No repeated character found.";
+  set(key, value) {
+    const index = this.hashFunction(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hashFunction(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (const pair of this.table[index]) {
+      if (pair[0] === key) {
+        return pair[1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this.hashFunction(key);
+    if (!this.table[index]) {
+      return;
+    }
+    const updatedTable = this.table[index].filter(pair => pair[0] !== key);
+    this.table[index] = updatedTable;
+  }
 }
 
-let inputString = "hello";
-let repeatedCharacter = findFirstRepeatedCharacter(inputString);
-
-console.log("The first repeated character is: ", repeatedCharacter);
+// Example usage
+const myHashTable = new HashTable(10);
+myHashTable.set("apple", 5);
+myHashTable.set("orange", 10);
+console.log(myHashTable.get("apple")); // Output: 5
+myHashTable.remove("apple");
+console.log(myHashTable.get("apple")); // Output: undefined
