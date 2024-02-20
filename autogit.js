ec72760439125d0dcea4943403c95b45746ac352
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:04:59 +0800

    2-20-2024 21:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,7 @@
-function preprocessPattern(pattern) {
-    const badChar = {};
-    const patternLength = pattern.length;
-
-    for (let i = 0; i < patternLength - 1; i++) {
-        badChar[pattern[i]] = i;
-    }
-
-    return badChar;
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function boyerMoore(text, pattern) {
-    const badChar = preprocessPattern(pattern);
-    const n = text.length;
-    const m = pattern.length;
-    let i = 0;
-
-    while (i <= n - m) {
-        let j = m - 1;
-
-        while (j >= 0 && pattern[j] === text[i + j]) {
-            j--;
-        }
-
-        if (j < 0) {
-            console.log("Pattern found at index " + i);
-            i += (i + m < n) ? m - badChar[text[i + m]] : 1;
-        } else {
-            i += Math.max(1, j - badChar[text[i + j]]);
-        }
-    }
-}
-
-// Test the algorithm
-const text = "ABAAABCD";
-const pattern = "ABC";
-boyerMoore(text, pattern);
+// Generate a random number between 1 and 100
+let randomNumber = getRandomNumber(1, 100);
+console.log(randomNumber);
