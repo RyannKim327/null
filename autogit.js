0c4f19d90d542fe90976115f3b0c666ac0fc742a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:38:55 +0800

    2-20-2024 20:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,5 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let key = arr[i];
-        let j = i - 1;
+// Taking user input
+let userInput = prompt("Enter some text: ");
 
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-
-        arr[j + 1] = key;
-    }
-
-    return arr;
-}
-
-// Test the insertion sort algorithm
-const arr = [12, 11, 13, 5, 6];
-console.log("Original Array: " + arr);
-console.log("Sorted Array: " + insertionSort(arr));
+// Displaying the input
+console.log("You entered: " + userInput);
