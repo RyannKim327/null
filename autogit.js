e60d014df0af7984386331cf80e57c9eae3be779
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:29:47 +0800

    2-20-2024 20:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,51 @@
-class Node {
-    constructor(value, color) {
-        this.value = value;
-        this.color = color;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-    }
-}
-
-class RedBlackTree {
-    constructor() {
-        this.root = null;
+class HashTable {
+    constructor(size) {
+        this.size = size;
+        this.table = new Array(size);
     }
 
-    insert(value) {
-        if (this.root === null) {
-            this.root = new Node(value, 'black');
-        } else {
-            this._insertNode(this.root, value);
+    hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * (i + 1)) % this.size;
         }
+        return hash;
     }
 
-    _insertNode(node, value) {
-        if (value < node.value) {
-            if (node.left === null) {
-                node.left = new Node(value, 'red');
-                node.left.parent = node;
-            } else {
-                this._insertNode(node.left, value);
-            }
-        } else if (value > node.value) {
-            if (node.right === null) {
-                node.right = new Node(value, 'red');
-                node.right.parent = node;
-            } else {
-                this._insertNode(node.right, value);
+    set(key, value) {
+        const index = this.hash(key);
+        if (!this.table[index]) {
+            this.table[index] = [];
+        }
+        this.table[index].push({ key, value });
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        if (this.table[index]) {
+            for (let i = 0; i < this.table[index].length; i++) {
+                if (this.table[index][i].key === key) {
+                    return this.table[index][i].value;
+                }
             }
         }
-
-        this._fixTree(node);
+        return null;
     }
 
-    _fixTree(node) {
-        // Implement the rotation and recoloring logic here to fix the red-black tree properties
+    remove(key) {
+        const index = this.hash(key);
+        if (this.table[index]) {
+            this.table[index] = this.table[index].filter(entry => entry.key !== key);
+        }
     }
-    
-    // Add other methods like delete, find, rotate, etc.
 }
 
 // Usage
-const rbTree = new RedBlackTree();
-rbTree.insert(10);
-rbTree.insert(5);
-rbTree.insert(15);
+const hashtable = new HashTable(10);
+hashtable.set('name', 'Alice');
+hashtable.set('age', 30);
+
+console.log(hashtable.get('name')); // Output: Alice
+console.log(hashtable.get('age')); // Output: 30
+hashtable.remove('age');
+console.log(hashtable.get('age')); // Output: null
