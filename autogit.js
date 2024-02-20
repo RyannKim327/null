929eeee09970af75fe28f52d1efcca7bec070d5a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:53:38 +0800

    2-20-2024 20:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,44 @@
-function findMedianSortedArrays(nums1, nums2) {
-    let mergedArray = [];
-    let i = 0, j = 0;
+function heapSort(arr) {
+    buildMaxHeap(arr);
 
-    while (i < nums1.length && j < nums2.length) {
-        if (nums1[i] < nums2[j]) {
-            mergedArray.push(nums1[i]);
-            i++;
-        } else {
-            mergedArray.push(nums2[j]);
-            j++;
-        }
+    let endIndex = arr.length - 1;
+    while (endIndex > 0) {
+        // Swap root with last element
+        [arr[0], arr[endIndex]] = [arr[endIndex], arr[0]]; 
+        endIndex--;
+        
+        heapifyDown(arr, 0, endIndex);
     }
 
-    while (i < nums1.length) {
-        mergedArray.push(nums1[i]);
-        i++;
-    }
+    return arr;
+}
 
-    while (j < nums2.length) {
-        mergedArray.push(nums2[j]);
-        j++;
-    }
-
-    const totalLength = mergedArray.length;
-    const mid = Math.floor(totalLength / 2);
-
-    if (totalLength % 2 === 0) {
-        return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
-    } else {
-        return mergedArray[mid];
+function buildMaxHeap(arr) {
+    let startIndex = Math.floor(arr.length / 2) - 1;
+    for (let i = startIndex; i >= 0; i--) {
+        heapifyDown(arr, i, arr.length - 1);
     }
 }
 
-// Example
-const nums1 = [1, 3];
-const nums2 = [2];
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2
+function heapifyDown(arr, startIndex, endIndex) {
+    let rootIndex = startIndex;
+    while (true) {
+        let childIndex = 2 * rootIndex + 1;
+        if (childIndex > endIndex) break;
+
+        if (childIndex + 1 <= endIndex && arr[childIndex] < arr[childIndex + 1]) {
+            childIndex++;
+        }
+
+        if (arr[rootIndex] < arr[childIndex]) {
+            [arr[rootIndex], arr[childIndex]] = [arr[childIndex], arr[rootIndex]];
+            rootIndex = childIndex;
+        } else {
+            break;
+        }
+    }
+}
+
+// Example usage
+let arr = [12, 11, 13, 5, 6, 7];
+console.log(heapSort(arr));  // Output: [5, 6, 7, 11, 12, 13]
