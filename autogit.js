2f4ccb6f353c7cff98b56ab4874bbd885283a13b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:46:47 +0800

    2-20-2024 20:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,19 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result); // Output: Hello World
-let str1 = "Hello";
-let str2 = "World";
-let result = str1.concat(" ", str2);
-console.log(result); // Output: Hello World
+function isPalindrome(str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase(); // Remove non-alphanumeric characters and convert to lowercase
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false; // Characters at opposite ends don't match, not a palindrome
+    }
+    left++;
+    right--;
+  }
+
+  return true; // If the while loop completes, the string is a palindrome
+}
+
+// Test the function
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+console.log(isPalindrome("race a car")); // Output: false
