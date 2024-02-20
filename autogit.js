c947ca3e4dd04a61ee70da2ee5c4ce184f4bbfce
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:34:45 +0800

    2-20-2024 20:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,22 @@
-function selectionSort(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        let minIndex = i;
-        
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+function countingSort(arr) {
+    const max = Math.max(...arr);
+    const counts = new Array(max + 1).fill(0);
+    const sorted = [];
+
+    for (let num of arr) {
+        counts[num]++;
+    }
+
+    for (let i = 0; i < counts.length; i++) {
+        for (let j = 0; j < counts[i]; j++) {
+            sorted.push(i);
         }
     }
-    
-    return arr;
+
+    return sorted;
 }
 
 // Example usage
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log(selectionSort(arr));
+const arr = [4, 2, 2, 8, 3, 3, 1];
+const sortedArr = countingSort(arr);
+console.log(sortedArr);  // Output: [1, 2, 2, 3, 3, 4, 8]
