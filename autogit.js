9c14f4fe986e42bc71459c3087e22759a4557e47
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:58:04 +0800

    2-20-2024 20:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,27 @@
-function maxSubArray(arr) {
-    let maxEndingHere = arr[0];
-    let maxSoFar = arr[0];
+function longestCommonSubstring(str1, str2) {
+    let dp = Array.from({ length: str1.length + 1 }, () => Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIdx = 0;
 
-    for (let i = 1; i < arr.length; i++) {
-        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIdx = i - 1;
+                }
+            }
+        }
     }
 
-    return maxSoFar;
+    if (maxLength === 0) {
+        return null;
+    }
+
+    return str1.substr(endIdx - maxLength + 1, maxLength);
 }
 
-// Example usage:
-const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const maxSum = maxSubArray(array);
-console.log(maxSum); // Output: 6
+let str1 = "abcdef";
+let str2 = "bcdeft";
+console.log(longestCommonSubstring(str1, str2));  // Output: "cdef"
