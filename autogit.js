88fb9a389a574f46fc02696c38082bbcd771fc32
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:37:55 +0800

    2-20-2024 20:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,45 @@
-function findStringLength(str) {
-    let count = 0;
-    for (let char of str) {
-        count++;
+class Node {
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+        this.children = {};
+        this.suffixLink = null;
     }
-    return count;
 }
 
-let inputString = "Hello, World!";
-let length = findStringLength(inputString);
+class SuffixTree {
+    constructor(str) {
+        this.root = new Node(-1, -1);
 
-console.log("Length of the string is: " + length);
+        for (let i = 0; i < str.length; i++) {
+            this.addSuffix(str.substring(i), i);
+        }
+    }
+
+    addSuffix(suffix, startIndex) {
+        let current = this.root;
+        for (let i = 0; i < suffix.length; i++) {
+            let char = suffix[i];
+            if (!(char in current.children)) {
+                current.children[char] = new Node(startIndex + i, startIndex); // Represent suffix by start and end indices
+            }
+            current = current.children[char];
+        }
+    }
+
+    search(substring) {
+        let current = this.root;
+        for (let char of substring) {
+            if (!(char in current.children)) {
+                return false;
+            }
+            current = current.children[char];
+        }
+        return true;
+    }
+}
+
+// Usage
+const suffixTree = new SuffixTree("banana");
+console.log(suffixTree.search("ana")); // true
+console.log(suffixTree.search("zzz")); // false
