2df04f3ad0c71e89f0eadbf7b62ebee1c7801c97
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:49:22 +0800

    2-20-2024 20:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,20 @@
-function isArraySorted(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-const myArray = [1, 2, 3, 4, 5];
-console.log(isArraySorted(myArray)); // Output will be: true
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Input Example</title>
+</head>
+<body>
+    <h1>User Input Example</h1>
+    <label for="userInput">Enter your name:</label>
+    <input type="text" id="userInput">
+    <button onclick="displayInput()">Submit</button>
+    <p id="output"></p>
 
-const myUnsortedArray = [5, 2, 8, 1];
-console.log(isArraySorted(myUnsortedArray)); // Output will be: false
+    <script>
+        function displayInput() {
+            var userInput = document.getElementById('userInput').value;
+            document.getElementById('output').innerText = 'Hello, ' + userInput + '!';
+        }
+    </script>
+</body>
+</html>
