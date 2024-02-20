250375d46412c46572558b747795394edb68e7be
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:21:04 +0800

    2-20-2024 20:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,17 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
+function findMedianSortedArrays(nums1, nums2) {
+    const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
+    const length = mergedArray.length;
+    
+    if (length % 2 !== 0) {
+        return mergedArray[Math.floor(length / 2)];
+    } else {
+        const mid = length / 2;
+        return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
+    }
 }
 
-// Example usage
-const originalString = "Hello, world!";
-const reversedString = reverseString(originalString);
+const nums1 = [1, 3];
+const nums2 = [2, 4];
 
-console.log(reversedString);  // Output: "!dlrow ,olleH"
+const median = findMedianSortedArrays(nums1, nums2);
+console.log("The median is: ", median);
