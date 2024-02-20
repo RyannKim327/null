160e9f93ceedc2c76b83ae02415743fc077178ab
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:36:16 +0800

    2-20-2024 20:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,70 @@
-function longestCommonSubsequence(str1, str2) {
-    // create a 2D array to store the lengths of common subsequences
-    const dp = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-    // fill the dp array
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
             } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
             }
         }
     }
 
-    // construct the longest common subsequence
-    let i = str1.length, j = str2.length;
-    let lcs = '';
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
+    search(value) {
+        return this.searchNode(this.root, value);
     }
 
-    return lcs;
+    searchNode(node, value) {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
 }
 
-const str1 = 'ABCBDAB';
-const str2 = 'BDCAB';
-console.log(longestCommonSubsequence(str1, str2)); // Output: BCAB
+// Example usage
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+bst.insert(12);
+
+console.log(bst.search(7)); // Output: true
+console.log(bst.search(11)); // Output: false
