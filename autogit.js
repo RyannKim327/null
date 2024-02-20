2dbfed2cc9b11fab4ec9c9b6306a47c5a0669d5a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:29:37 +0800

    2-20-2024 20:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,55 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 0) return '';
-    
-    for (let i = 0; i < strs[0].length; i++) {
-        for (let j = 1; j < strs.length; j++) {
-            if (strs[j][i] !== strs[0][i]) {
-                return strs[0].slice(0, i);
-            }
-        }
+class Node {
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
     }
-    
-    return strs[0];
 }
 
-// Example
-let strings = ['flower', 'flow', 'flight'];
-console.log(longestCommonPrefix(strings)); // Output: "fl"
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        if (this.root === null) {
+            this.root = new Node(value, 'black');
+        } else {
+            this._insertNode(this.root, value);
+        }
+    }
+
+    _insertNode(node, value) {
+        if (value < node.value) {
+            if (node.left === null) {
+                node.left = new Node(value, 'red');
+                node.left.parent = node;
+            } else {
+                this._insertNode(node.left, value);
+            }
+        } else if (value > node.value) {
+            if (node.right === null) {
+                node.right = new Node(value, 'red');
+                node.right.parent = node;
+            } else {
+                this._insertNode(node.right, value);
+            }
+        }
+
+        this._fixTree(node);
+    }
+
+    _fixTree(node) {
+        // Implement the rotation and recoloring logic here to fix the red-black tree properties
+    }
+    
+    // Add other methods like delete, find, rotate, etc.
+}
+
+// Usage
+const rbTree = new RedBlackTree();
+rbTree.insert(10);
+rbTree.insert(5);
+rbTree.insert(15);
