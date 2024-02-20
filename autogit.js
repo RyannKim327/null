24250ff49d1303e03deac738a2dc857114eb4798
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:00:43 +0800

    2-20-2024 21:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,14 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+// Taking user input
+const userInput = prompt("Please enter a string:");
+
+// Function to reverse a string
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+// Reversing the user input
+const reversedString = reverseString(userInput);
 
-  addNode(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  findMiddleElement() {
-    let slow = this.head;
-    let fast = this.head;
-
-    while (fast && fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-
-    return slow.value;
-  }
-}
-
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
-
-const middleElement = linkedList.findMiddleElement();
-console.log("Middle Element:", middleElement);
+// Displaying the reversed string
+console.log(`Original string: ${userInput}`);
+console.log(`Reversed string: ${reversedString}`);
