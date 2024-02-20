2e6bdc650db99dc6ed2a853ac8bf5500d3863068
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:35:05 +0800

    2-20-2024 20:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,12 @@
-class HashTable {
-    constructor() {
-        this.table = {};
-    }
-
-    // Hash function to generate a hash key
-    hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash += key.charCodeAt(i);
-        }
-        return hash % 100; // Modulo 100 to limit the size of the hash key
-    }
-
-    // Insert a key-value pair into the hash table
-    insert(key, value) {
-        const hashKey = this.hash(key);
-        if (!this.table[hashKey]) {
-            this.table[hashKey] = {};
-        }
-        this.table[hashKey][key] = value;
-    }
-
-    // Retrieve a value from the hash table given a key
-    get(key) {
-        const hashKey = this.hash(key);
-        if (this.table[hashKey] && this.table[hashKey][key]) {
-            return this.table[hashKey][key];
-        } else {
-            return undefined;
-        }
-    }
-
-    // Delete a key-value pair from the hash table
-    remove(key) {
-        const hashKey = this.hash(key);
-        if (this.table[hashKey] && this.table[hashKey][key]) {
-            delete this.table[hashKey][key];
-        }
-    }
+function validateEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailPattern.test(email);
 }
 
-// Example usage
-const ht = new HashTable();
-ht.insert("name", "Alice");
-ht.insert("age", 30);
-
-console.log(ht.get("name")); // Output: Alice
-console.log(ht.get("age")); // Output: 30
-
-ht.remove("age");
-console.log(ht.get("age")); // Output: undefined
+// Test the function
+const email = "test@example.com";
+if (validateEmail(email)) {
+    console.log("Email is valid");
+} else {
+    console.log("Email is invalid");
+}
