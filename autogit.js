dadbe3d2697a83f44ca03300cf465ab5dfa49054
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:41:05 +0800

    2-20-2024 20:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,30 @@
-class BinaryHeap {
-    constructor(comparator) {
-        this.heap = [];
-        this.comparator = comparator;
+function countingSort(arr) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let range = max - min + 1;
+    let count = new Array(range).fill(0);
+    let output = new Array(arr.length);
+
+    for (let i = 0; i < arr.length; i++) {
+        count[arr[i] - min]++;
     }
 
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp(this.heap.length - 1);
+    for (let i = 1; i < count.length; i++) {
+        count[i] += count[i - 1];
     }
 
-    extract() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-
-        const maxValue = this.heap[0];
-        const lastValue = this.heap.pop();
-        
-        if (this.heap.length > 0) {
-            this.heap[0] = lastValue;
-            this.heapifyDown(0);
-        }
-
-        return maxValue;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
     }
 
-    heapifyUp(index) {
-        let parent = Math.floor((index - 1) / 2);
-        while (index > 0 && this.comparator(this.heap[parent], this.heap[index]) < 0) {
-            [this.heap[parent], this.heap[index]] = [this.heap[index], this.heap[parent]];
-            index = parent;
-            parent = Math.floor((index - 1) / 2);
-        }
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = output[i];
     }
 
-    heapifyDown(index) {
-        let left = 2 * index + 1;
-        let right = 2 * index + 2;
-        let largest = index;
-
-        if (left < this.heap.length && this.comparator(this.heap[left], this.heap[largest]) > 0) {
-            largest = left;
-        }
-
-        if (right < this.heap.length && this.comparator(this.heap[right], this.heap[largest]) > 0) {
-            largest = right;
-        }
-
-        if (largest !== index) {
-            [this.heap[largest], this.heap[index]] = [this.heap[index], this.heap[largest]];
-            this.heapifyDown(largest);
-        }
-    }
-}
-
-// Example comparator function for max priority queue
-function maxComparator(a, b) {
-    return a - b;
+    return arr;
 }
 
 // Example usage
-const maxPriorityQueue = new BinaryHeap(maxComparator);
-maxPriorityQueue.insert(5);
-maxPriorityQueue.insert(3);
-maxPriorityQueue.insert(7);
-maxPriorityQueue.insert(2);
-
-console.log(maxPriorityQueue.extract()); // Output: 7
-console.log(maxPriorityQueue.extract()); // Output: 5
-console.log(maxPriorityQueue.extract()); // Output: 3
-console.log(maxPriorityQueue.extract()); // Output: 2
+let arr = [4, 2, 2, 8, 3, 3, 1];
+console.log(countingSort(arr)); // Output: [1, 2, 2, 3, 3, 4, 8]
