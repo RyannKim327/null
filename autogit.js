49934fbca8a313a1d3597397ef7590306639f76c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:46:04 +0800

    2-20-2024 20:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-function isSortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
+let str = "Hello World!";
+let lowercaseStr = str.toLowerCase();
 
-// Example usage
-const unsortedArray = [3, 1, 5, 7, 2];
-const sortedArray = [1, 2, 3, 5, 7];
-
-console.log(isSortedAscending(unsortedArray)); // Output: false
-console.log(isSortedAscending(sortedArray)); // Output: true
+console.log(lowercaseStr); // Output: hello world!
