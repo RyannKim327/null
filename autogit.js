ea0738fb40e272782a3cc052a2d6ccf3a534c398
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:30:10 +0800

    2-20-2024 20:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,24 @@
-let str = "Hello, world!";
-let subStr = "Hello";
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
 
-if (str.includes(subStr)) {
-    console.log("Substring found in the string");
-} else {
-    console.log("Substring not found in the string");
-}
-let str = "Hello, world!";
-let subStr = "Hello";
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
 
-if (str.indexOf(subStr) !== -1) {
-    console.log("Substring found in the string");
-} else {
-    console.log("Substring not found in the string");
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
+
+// Example usage:
+const array = [3, 0, 2, 5, -1, 4, 1];
+const sortedArray = quickSort(array);
+console.log(sortedArray); // Output: [-1, 0, 1, 2, 3, 4, 5]
