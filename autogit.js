20466139938151049cf94cb7b133c3caff7a4afa
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:30:41 +0800

    2-20-2024 20:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,22 @@
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
+function findFirstRepeatedChar(str) {
+    let charMap = {};
 
-  const pivot = arr[arr.length - 1];
-  const left = [];
-  const right = [];
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+    for (let char of str) {
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = true;
+        }
     }
-  }
 
-  return [...quickSort(left), pivot, ...quickSort(right)];
+    return null;
 }
 
-// Example usage:
-const array = [3, 0, 2, 5, -1, 4, 1];
-const sortedArray = quickSort(array);
-console.log(sortedArray); // Output: [-1, 0, 1, 2, 3, 4, 5]
+let inputString = "hello world";
+let repeatedChar = findFirstRepeatedChar(inputString);
+
+if (repeatedChar) {
+    console.log(`The first repeated character in "${inputString}" is "${repeatedChar}"`);
+} else {
+    console.log(`No repeated characters found in "${inputString}"`);
+}
