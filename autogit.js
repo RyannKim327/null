e5cdaf7b782d274605265681af88cb5ce075e4a4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:59:48 +0800

    2-20-2024 20:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,38 @@
-let arr = [1, 2, 3, 4, 5];
-let index = arr.indexOf(3); // Get the index of the element you want to remove
-if (index !== -1) {
-  arr.splice(index, 1); // Remove one element at the index
+// Function to get the digit at a specific position in a number
+function getDigit(num, i) {
+    return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
-console.log(arr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let filteredArr = arr.filter(item => item !== 3); // Create a new array excluding the element to remove
-console.log(filteredArr); // Output: [1, 2, 4, 5]
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-let newArr = arr.filter(item => item !== elementToRemove);
-console.log(newArr); // Output: [1, 2, 4, 5]
+
+// Function to count the number of digits in a number
+function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// Function to find the number of digits in the largest number in the input array
+function mostDigits(nums) {
+    let maxDigits = 0;
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+    }
+    return maxDigits;
+}
+
+// Radix Sort function
+function radixSort(nums) {
+    let maxDigitCount = mostDigits(nums);
+    for (let k = 0; k < maxDigitCount; k++) {
+        let digitBuckets = Array.from({ length: 10 }, () => []);
+        for (let i = 0; i < nums.length; i++) {
+            let digit = getDigit(nums[i], k);
+            digitBuckets[digit].push(nums[i]);
+        }
+        nums = [].concat(...digitBuckets);
+    }
+    return nums;
+}
+
+// Example usage
+let unsortedArray = [170, 45, 75, 90, 802, 24, 2, 66];
+let sortedArray = radixSort(unsortedArray);
+console.log(sortedArray);
