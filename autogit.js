19918313f51e8647ee431e1d3564e92c11a890a1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:03:50 +0800

    2-20-2024 21:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,45 @@
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    depthFirstSearch(startingVertex, visitFn) {
+        const visited = {};
+        const dfs = (vertex) => {
+            if (!vertex) return null;
+            visited[vertex] = true;
+            visitFn(vertex);
+            this.adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    dfs(neighbor);
+                }
+            });
+        };
+
+        dfs(startingVertex);
+    }
 }
+
+// Usage example
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.depthFirstSearch('A', (vertex) => {
+    console.log(vertex);
+});
