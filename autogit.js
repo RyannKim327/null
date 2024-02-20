7018a7538bb9a1f27c6372b74ae244ca4d321830
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:36:55 +0800

    2-20-2024 20:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,44 @@
-function areAnagrams(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[\W_]/g, '').toLowerCase();
-    str2 = str2.replace(/[\W_]/g, '').toLowerCase();
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-    // Check if the length of the strings is the same
-    if (str1.length !== str2.length) {
-        return false;
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
     }
 
-    // Sort the characters of the strings and compare them
-    let sortedStr1 = str1.split('').sort().join('');
-    let sortedStr2 = str2.split('').sort().join('');
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
 
-    return sortedStr1 === sortedStr2;
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
 }
 
-// Test the function
-let string1 = "listen";
-let string2 = "silent";
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example of how to use the LinkedList class
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+console.log(linkedList.getLength()); // Output: 3
