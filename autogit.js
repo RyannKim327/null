6756c0ad4a27567f059d40bdc80fafd68ebff36d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:34:26 +0800

    2-20-2024 20:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,33 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-
-    if (num % 2 === 0 || num % 3 === 0) return false;
-
-    let i = 5;
-    while (i * i <= num) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
-        i += 6;
+class Node {
+    constructor() {
+        this.children = {};
+        this.start = null;
+        this.end = null;
+        this.suffixLink = null;
+    }
+}
+class SuffixTree {
+    constructor(text) {
+        this.root = new Node();
+        this.text = text;
+        this.activeNode = this.root;
+        this.activeEdge = 0;
+        this.activeLength = 0;
+        this.remainingSuffixCount = 0;
     }
 
-    return true;
-}
-
-function largestPrimeFactor(number) {
-    let factor = 2;
-    let largestPrime = 0;
-
-    while (number > 1) {
-        if (number % factor === 0) {
-            number /= factor;
-            if (isPrime(factor)) {
-                largestPrime = factor;
-            }
-        } else {
-            factor++;
+    buildSuffixTree() {
+        for (let i = 0; i < this.text.length; i++) {
+            this.addSuffix(i);
         }
     }
 
-    return largestPrime;
-}
+    addSuffix(index) {
+        // Implement adding a suffix to the suffix tree
+    }
 
-const number = 13195; // Input number
-const result = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is: ${result}`);
+    // Other helper functions to implement:
+    // - walkDown
+    // - extendSuffixTree
+    // - setSuffixIndex
+}
