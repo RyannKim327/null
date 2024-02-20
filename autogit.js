5b71ca33d67d79796c7f91651004d809d75936de
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:57:21 +0800

    2-20-2024 20:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,6 @@
-class Stack {
-  constructor() {
-    this.stackArray = [];
-  }
-
-  // Push element to the top of the stack
-  push(element) {
-    this.stackArray.push(element);
-  }
-
-  // Pop element from the top of the stack
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.stackArray.pop();
-  }
-
-  // Peek at the top element of the stack
-  peek() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.stackArray[this.stackArray.length - 1];
-  }
-
-  // Check if the stack is empty
-  isEmpty() {
-    return this.stackArray.length === 0;
-  }
-
-  // Get the size of the stack
-  size() {
-    return this.stackArray.length;
-  }
-}
-
-// Example usage
-const myStack = new Stack();
-myStack.push(1);
-myStack.push(2);
-myStack.push(3);
-
-console.log(myStack.pop()); // Output: 3
-console.log(myStack.peek()); // Output: 2
-console.log(myStack.size()); // Output: 2
-console.log(myStack.isEmpty()); // Output: false
+let str = "42";
+let num = parseInt(str);
+console.log(num); // Output: 42
+let str = "42";
+let num = Number(str);
+console.log(num); // Output: 42
