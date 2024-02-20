415fa554d1a1778949e48d474848b51f8401b78d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:30:52 +0800

    2-20-2024 20:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,39 @@
-function findFirstRepeatedChar(str) {
-    let charMap = {};
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
     }
+  }
 
-    return null;
+  return false;
 }
 
-let inputString = "hello world";
-let repeatedChar = findFirstRepeatedChar(inputString);
+// Example usage
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
 
-if (repeatedChar) {
-    console.log(`The first repeated character in "${inputString}" is "${repeatedChar}"`);
-} else {
-    console.log(`No repeated characters found in "${inputString}"`);
-}
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node2; // cycle
+
+console.log(hasCycle(node1)); // Output: true
