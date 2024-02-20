3b4eb1ba482dd656c17ed2a0814a2ded35e6379a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:00:58 +0800

    2-20-2024 21:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,6 @@
-function removeVowels(input) {
-  return input.replace(/[aeiouAEIOU]/g, '');
-}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-let inputString = 'Hello World';
-let result = removeVowels(inputString);
-console.log(result); // Output: 'Hll Wrld'
+const commonElements = array1.filter(element => array2.includes(element));
+
+console.log(commonElements); // Output: [3, 4, 5]
