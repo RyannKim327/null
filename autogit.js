f74c82c5a5ec53bae7f19eb3678c0985c953ea5d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:39:54 +0800

    2-20-2024 20:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,12 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function calculateTriangleArea(a, b, c) {
+    const s = (a + b + c) / 2;
+    const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    return area;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    findMiddle() {
-        let slowPointer = this.head;
-        let fastPointer = this.head;
-
-        while (fastPointer !== null && fastPointer.next !== null) {
-            slowPointer = slowPointer.next;
-            fastPointer = fastPointer.next.next;
-        }
-
-        return slowPointer.data;
-    }
-}
-
-// Sample usage
-let list = new LinkedList();
-list.head = new Node(1);
-list.head.next = new Node(2);
-list.head.next.next = new Node(3);
-list.head.next.next.next = new Node(4);
-list.head.next.next.next.next = new Node(5);
-
-console.log(list.findMiddle()); // Output: 3
+// Example usage
+const a = 5;
+const b = 6;
+const c = 7;
+const area = calculateTriangleArea(a, b, c);
+console.log("The area of the triangle is: " + area);
