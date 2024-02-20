14ad01a6b439ce8bbbbea1d4d2a1dd6d5904cc62
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:58:54 +0800

    2-20-2024 20:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,15 @@
-const cron = require('node-cron');
+function isSortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-cron.schedule('* * * * *', () => {
-    console.log('Running a task every minute');
-});
-npm install node-cron
+// Test the function
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [5, 4, 3, 2, 1];
+
+console.log(isSortedAscending(array1)); // true
+console.log(isSortedAscending(array2)); // false
