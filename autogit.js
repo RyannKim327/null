b44b59a49127a52774c8cd809c93d9b2e30f2a80
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:59:10 +0800

    2-20-2024 20:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,5 @@
-class Graph {
-  constructor() {
-    this.graph = {};
-  }
+const cron = require('node-cron');
 
-  addNode(node) {
-    if (!this.graph[node]) {
-      this.graph[node] = [];
-    }
-  }
-
-  addEdge(node1, node2) {
-    this.addNode(node1);
-    this.addNode(node2);
-    this.graph[node1].push(node2);
-  }
-
-  dfs(startNode) {
-    let visited = {};
-    let result = [];
-
-    const dfsUtil = (node) => {
-      visited[node] = true;
-      result.push(node);
-
-      this.graph[node].forEach((neighbor) => {
-        if (!visited[neighbor]) {
-          dfsUtil(neighbor);
-        }
-      });
-    };
-
-    dfsUtil(startNode);
-
-    return result;
-  }
-}
-
-// Example usage
-const graph = new Graph();
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('B', 'E');
-graph.addEdge('C', 'F');
-graph.addEdge('C', 'G');
-
-console.log(graph.dfs('A'));
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute!');
+});
