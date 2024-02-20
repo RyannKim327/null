9318f17f5a77a0cd9774ace23e0a616279b3f7c9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 20:24:53 +0800

    2-20-2024 20:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,101 @@
 class Node {
-    constructor(value, left = null, right = null) {
+    constructor(value) {
         this.value = value;
-        this.left = left;
-        this.right = right;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
     }
 }
 
-function sumTree(root) {
-    if (root === null) {
-        return 0;
+class AVLTree {
+    constructor() {
+        this.root = null;
     }
 
-    return root.value + sumTree(root.left) + sumTree(root.right);
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+
+    rightRotate(y) {
+        const x = y.left;
+        const T = x.right;
+
+        x.right = y;
+        y.left = T;
+
+        this.updateHeight(y);
+        this.updateHeight(x);
+
+        return x;
+    }
+
+    leftRotate(x) {
+        const y = x.right;
+        const T = y.left;
+
+        y.left = x;
+        x.right = T;
+
+        this.updateHeight(x);
+        this.updateHeight(y);
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = this._insert(node.right, value);
+        } else {
+            // Duplicate values not allowed
+            return node;
+        }
+
+        this.updateHeight(node);
+
+        const balanceFactor = this.getBalanceFactor(node);
+
+        if (balanceFactor > 1 && value < node.left.value) {
+            return this.rightRotate(node);
+        }
+
+        if (balanceFactor < -1 && value > node.right.value) {
+            return this.leftRotate(node);
+        }
+
+        if (balanceFactor > 1 && value > node.left.value) {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+
+        if (balanceFactor < -1 && value < node.right.value) {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
+        }
+
+        return node;
+    }
 }
-
-// Example binary tree
-const tree = new Node(1,
-    new Node(2,
-        new Node(4),
-        new Node(5)),
-    new Node(3,
-        new Node(6),
-        new Node(7)
-    )
-);
-
-const sum = sumTree(tree);
-console.log("Sum of all nodes in the binary tree: ", sum);
