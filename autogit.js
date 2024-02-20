d2eba2014bea974ff2890fd53804e56f5bc1c11d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:04:51 +0800

    2-20-2024 21:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,37 @@
-const axios = require('axios');
+function preprocessPattern(pattern) {
+    const badChar = {};
+    const patternLength = pattern.length;
 
-axios.get('https://api.example.com/data')
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    });
+    for (let i = 0; i < patternLength - 1; i++) {
+        badChar[pattern[i]] = i;
+    }
+
+    return badChar;
+}
+
+function boyerMoore(text, pattern) {
+    const badChar = preprocessPattern(pattern);
+    const n = text.length;
+    const m = pattern.length;
+    let i = 0;
+
+    while (i <= n - m) {
+        let j = m - 1;
+
+        while (j >= 0 && pattern[j] === text[i + j]) {
+            j--;
+        }
+
+        if (j < 0) {
+            console.log("Pattern found at index " + i);
+            i += (i + m < n) ? m - badChar[text[i + m]] : 1;
+        } else {
+            i += Math.max(1, j - badChar[text[i + j]]);
+        }
+    }
+}
+
+// Test the algorithm
+const text = "ABAAABCD";
+const pattern = "ABC";
+boyerMoore(text, pattern);
