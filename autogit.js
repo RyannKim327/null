205792e38a89e1b794c753ff24a7ecffefa88117
RyannKim327 <rksesgundo123@gmail.com>
2024-02-20 21:03:11 +0800

    2-20-2024 21:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,56 @@
-function bubbleSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+function tarjanSCC(graph) {
+    let index = 0;
+    let stack = [];
+    let visited = new Set();
+    let lowLink = {};
+    let result = [];
+
+    function strongConnect(node) {
+        lowLink[node] = index;
+        index++;
+        stack.push(node);
+        visited.add(node);
+
+        for (let neighbor of graph[node]) {
+            if (!visited.has(neighbor)) {
+                strongConnect(neighbor);
+                lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
+            } else if (stack.includes(neighbor)) {
+                lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
             }
         }
+
+        if (lowLink[node] === index) {
+            let component = [];
+            let top = null;
+            do {
+                top = stack.pop();
+                component.push(top);
+            } while (top !== node);
+            result.push(component);
+        }
     }
-    return arr;
+
+    for (let node in graph) {
+        if (!visited.has(node)) {
+            strongConnect(node);
+        }
+    }
+
+    return result;
 }
 
-const arr = [6, 3, 8, 2, 10];
-console.log("Original Array:", arr);
-console.log("Sorted Array:", bubbleSort(arr));
+// Example usage
+const graph = {
+    'A': ['B'],
+    'B': ['C', 'E', 'F'],
+    'C': ['D', 'G'],
+    'D': ['C', 'H'],
+    'E': ['A', 'F'],
+    'F': ['G'],
+    'G': ['F'],
+    'H': ['D', 'G']
+};
+
+const scc = tarjanSCC(graph);
+console.log(scc);
