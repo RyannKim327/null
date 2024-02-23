9f2e5b5f4ab0ef86be5939baa078567c7d102162
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:50:48 +0800

    2-23-2024 23:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,7 @@
-function burrowsWheelerTransform(input) {
-    // Create an array of rotations of the input string
-    let rotations = [];
-    for (let i = 0; i < input.length; i++) {
-        let rotation = input.slice(i) + input.slice(0, i);
-        rotations.push(rotation);
-    }
+let arr = [1, 2, 3, 4, 2, 3, 5, 6];
 
-    // Sort the rotations lexicographically
-    rotations.sort();
+let uniqueArr = arr.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+});
 
-    // Extract the last characters of each rotation to construct the transformed string
-    let transformedString = rotations.map(rotation => rotation[rotation.length - 1]).join('');
-
-    // Find the original string index in the sorted rotations
-    let originalIndex = rotations.indexOf(input);
-
-    return { transformedString, originalIndex };
-}
-
-function burrowsWheelerInverseTransform(transformedString, originalIndex) {
-    let table = [];
-    
-    for (let i = 0; i < transformedString.length; i++) {
-        table.push({ char: transformedString[i], index: i });
-    }
-    
-    table.sort((a, b) => a.char.localeCompare(b.char));
-    
-    let currentIndex = originalIndex;
-    let result = '';
-    
-    for (let i = 0; i < transformedString.length; i++) {
-        let currentChar = table[currentIndex].char;
-        result = currentChar + result;
-        currentIndex = table[currentIndex].index;
-    }
-    
-    return result;
-}
-
-// Example usage
-let inputString = 'banana';
-let { transformedString, originalIndex } = burrowsWheelerTransform(inputString);
-let originalString = burrowsWheelerInverseTransform(transformedString, originalIndex);
-
-console.log('Input String: ', inputString);
-console.log('Transformed String: ', transformedString);
-console.log('Original String: ', originalString);
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5, 6]
