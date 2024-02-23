30e0bfbc197ed9234dfe1c455c85382e0f989cb3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:19:08 +0800

    2-24-2024 0:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,43 @@
 class Node {
-    constructor(value) {
-        this.value = value;
+    constructor(data) {
+        this.data = data;
         this.next = null;
     }
 }
 
-function findIntersection(list1, list2) {
-    let set = new Set();
-
-    // Iterate through the first linked list and store nodes in a set
-    let current = list1;
-    while (current !== null) {
-        set.add(current);
-        current = current.next;
+class LinkedList {
+    constructor() {
+        this.head = null;
     }
 
-    // Iterate through the second linked list and check if the node exists in the set
-    current = list2;
-    while (current !== null) {
-        if (set.has(current)) {
-            return current; // Intersection found
+    // Add a new node to the end of the list
+    append(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
-        current = current.next;
     }
 
-    return null; // No intersection found
+    // Print all the nodes in the list
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
 
-// Example
-let commonNode = new Node(8);
-let list1 = new Node(1);
-list1.next = new Node(2);
-list1.next.next = new Node(3);
-list1.next.next.next = commonNode;
+// Usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
 
-let list2 = new Node(4);
-list2.next = new Node(5);
-list2.next.next = commonNode;
-
-let result = findIntersection(list1, list2);
-if (result) {
-    console.log("Intersection Node Value: " + result.value);
-} else {
-    console.log("No intersection found.");
-}
+linkedList.printList();
