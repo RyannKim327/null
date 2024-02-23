1af82759a67ef93f171c52aa451adaf0a9c2327a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:17:56 +0800

    2-24-2024 0:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,6 @@
-function topologicalSort(graph) {
-    const visited = {};
-    const stack = [];
+let str = "  Hello,   World!  ";
 
-    function dfs(node) {
-        visited[node] = true;
-        for (let neighbor of graph[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor);
-            }
-        }
-        stack.push(node);
-    }
+// Remove whitespace using a regular expression
+let newStr = str.replace(/\s/g, '');
 
-    for (let node in graph) {
-        if (!visited[node]) {
-            dfs(node);
-        }
-    }
-
-    return stack.reverse();
-}
-
-// Example usage:
-const graph = {
-    1: [2, 3],
-    2: [4],
-    3: [4, 5],
-    4: [],
-    5: []
-};
-
-const result = topologicalSort(graph);
-console.log(result); // Output: [1, 3, 5, 2, 4]
+console.log(newStr); // Output: "Hello,World!"
