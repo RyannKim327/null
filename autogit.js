02e2a7687d94b19565a458fc58f5375aa6b2e545
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:01:00 +0800

    2-24-2024 0:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,31 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+// Define a function to make an HTTP request to connect to an Android async task
+function connectToAsyncTask() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    const url = 'http://your-android-async-task-endpoint';
+    xhr.open('GET', url, true);
+    
+    xhr.onload = function() {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    
+    xhr.onerror = function() {
+      reject(xhr.statusText);
+    };
+    
+    xhr.send();
+  });
+}
+
+// Call the function to connect to the Android async task and handle the response
+connectToAsyncTask()
+  .then(response => {
+    console.log('Connected to Android async task:', response);
+  })
+  .catch(error => {
+    console.error('Error connecting to Android async task:', error);
+  });
