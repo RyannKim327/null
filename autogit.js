48a9178ac07c65f485a357fd0f869742d579eec3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:04:08 +0800

    2-24-2024 0:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,17 @@
-function findStringLength(str) {
-    let count = 0;
-    for (let i = 0; str[i] !== undefined; i++) {
-        count++;
-    }
-    return count;
+function reverseWords(str) {
+    // Split the string into an array of words
+    let words = str.split(' ');
+
+    // Reverse the array of words
+    words.reverse();
+
+    // Join the array of words back into a string
+    let reversedStr = words.join(' ');
+
+    return reversedStr;
 }
 
-let myString = "Hello, World!";
-let length = findStringLength(myString);
-console.log(length); // Output: 13
+// Usage
+let originalStr = "Hello World";
+let reversedStr = reverseWords(originalStr);
+console.log(reversedStr); // Output: "World Hello"
