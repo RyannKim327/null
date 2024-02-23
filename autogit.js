e257ee5aa9cb8764a8be78532da2bebf777cd287
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:52:05 +0800

    2-23-2024 23:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(user => {
-      console.log(user.name);
-    })
-  })
-  .catch(error => {
-    console.log('Error fetching data: ', error);
-  });
+function findMedianSortedArrays(nums1, nums2) {
+    const mergedArray = [...nums1, ...nums2].sort((a, b) => a - b);
+    const length = mergedArray.length;
+    
+    if (length % 2 !== 0) {
+        return mergedArray[Math.floor(length / 2)];
+    } else {
+        const midIndex = length / 2;
+        return (mergedArray[midIndex - 1] + mergedArray[midIndex]) / 2;
+    }
+}
+
+const nums1 = [1, 3];
+const nums2 = [2, 4];
+
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median);
