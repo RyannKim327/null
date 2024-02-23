fea117bc858b400c9af2fc334463eb5acb7d1d9c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:47:51 +0800

    2-23-2024 23:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,22 @@
-function largestPrimeFactor(number) {
-    let largestFactor = 1;
-
-    // Divide the number by 2 repeatedly to extract out all the factors of 2
-    while (number % 2 === 0) {
-        largestFactor = 2;
-        number = number / 2;
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
     }
-
-    // Check for odd factors starting from 3
-    for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
-        while (number % i === 0) {
-            largestFactor = i;
-            number = number / i;
+    
+    // Check for divisibility from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
         }
     }
-
-    // If the remaining number is a prime number greater than 2
-    if (number > 2) {
-        largestFactor = number;
-    }
-
-    return largestFactor;
+    
+    return true;
 }
 
-// Example usage
-const number = 84;
-console.log(`The largest prime factor of ${number} is: ${largestPrimeFactor(number)}`);
+// Usage
+const number = 17;
+if (isPrime(number)) {
+    console.log(`${number} is a prime number`);
+} else {
+    console.log(`${number} is not a prime number`);
+}
