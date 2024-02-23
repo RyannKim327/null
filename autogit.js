317d0ebddc2f08c8fa93dfec027e5f2f9caffe52
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:07:04 +0800

    2-24-2024 0:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,10 @@
-function computeLPSArray(pattern) {
-    const patternLength = pattern.length;
-    let lps = [0];
-    let len = 0;
-    let i = 1;
+let string1 = "Hello, ";
+let string2 = "world!";
+let concatenatedString = string1 + string2;
 
-    while (i < patternLength) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len !== 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
+console.log(concatenatedString); // Output: Hello, world!
+let string1 = "Hello, ";
+let string2 = "world!";
+let concatenatedString = string1.concat(string2);
 
-    return lps;
-}
-
-function KMP(text, pattern) {
-    const textLength = text.length;
-    const patternLength = pattern.length;
-    const lps = computeLPSArray(pattern);
-    let i = 0;
-    let j = 0;
-    let indices = [];
-
-    while (i < textLength) {
-        if (text[i] === pattern[j]) {
-            i++;
-            j++;
-        }
-
-        if (j === patternLength) {
-            indices.push(i - j);
-            j = lps[j - 1];
-        } else if (i < textLength && text[i] !== pattern[j]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-
-    return indices;
-}
-
-// Test the KMP algorithm
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const indices = KMP(text, pattern);
-console.log("Pattern found at indices:", indices);
+console.log(concatenatedString); // Output: Hello, world!
