26aab41d13b84fc3b626815ddb946ca6bbef9d76
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:46:33 +0800

    2-23-2024 23:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,22 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function findFirstRepeatedChar(str) {
+    let charMap = {};
+
+    for (let char of str) {
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = 1;
+        }
+    }
+    
+    return null;
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.size = 0;
-  }
+let str = "hello world";
+let repeatedChar = findFirstRepeatedChar(str);
 
-  add(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-    this.size++;
-  }
-
-  findMiddle() {
-    let slowPointer = this.head;
-    let fastPointer = this.head;
-
-    while (fastPointer !== null && fastPointer.next !== null) {
-      slowPointer = slowPointer.next;
-      fastPointer = fastPointer.next.next;
-    }
-
-    return slowPointer;
-  }
+if (repeatedChar) {
+    console.log("First repeated character: " + repeatedChar);
+} else {
+    console.log("No repeated characters found");
 }
-
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-linkedList.add(5);
-
-const middleNode = linkedList.findMiddle();
-console.log(middleNode.value); // Output the middle element
-
