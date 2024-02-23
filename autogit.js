c323980e2c0444b63cac085ed0296a85d40e3b62
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:49:40 +0800

    2-23-2024 23:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,77 +1,20 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
+function findMaxSubarray(arr) {
+    if (arr.length === 0) {
+        return 0;
     }
+
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
 }
 
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
-
-    insert(data) {
-        const newNode = new Node(data);
-
-        if (this.root === null) {
-            this.root = newNode;
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
-
-    insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-            if (node.left === null) {
-                node.left = newNode;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-            } else {
-                this.insertNode(node.right, newNode);
-            }
-        }
-    }
-
-    search(data) {
-        return this.searchNode(this.root, data);
-    }
-
-    searchNode(node, data) {
-        if (node === null) {
-            return false;
-        }
-
-        if (data < node.data) {
-            return this.searchNode(node.left, data);
-        } else if (data > node.data) {
-            return this.searchNode(node.right, data);
-        } else {
-            return true;
-        }
-    }
-
-    inOrderTraversal(node) {
-        if (node !== null) {
-            this.inOrderTraversal(node.left);
-            console.log(node.data);
-            this.inOrderTraversal(node.right);
-        }
-    }
-}
-
-// Usage
-const bst = new BinarySearchTree();
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
-bst.insert(8);
-
-console.log(bst.search(5)); // true
-console.log(bst.search(20)); // false
-
-bst.inOrderTraversal(bst.root);
+// Test the function with an example array
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const maxSubarraySum = findMaxSubarray(arr);
+console.log("Maximum sum subarray is:", maxSubarraySum);
