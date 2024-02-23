c570e661c195bb18b19a1139a64e116aeedec496
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:59:31 +0800

    2-23-2024 23:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,19 @@
-function findStringLength(str) {
-    let count = 0;
-    for (let i = 0; str[i] !== undefined; i++) {
-        count++;
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    return count;
+    return arr;
 }
 
-let string = "Hello, World!";
-console.log(findStringLength(string)); // Output: 13
+// Test the bubble sort algorithm
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + bubbleSort(arr));
