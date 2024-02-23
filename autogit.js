10f96255f0e1d65a062f014a75e821a5b227c475
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:56:12 +0800

    2-23-2024 23:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,28 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Outputs: [5, 4, 3, 2, 1]
+function longestCommonSubstring(str1, str2) {
+  let maxLength = 0;
+  let endIndex = 0;
+  const dp = Array.from({ length: str1.length + 1 }, () => Array(str2.length + 1).fill(0));
+
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        if (dp[i][j] > maxLength) {
+          maxLength = dp[i][j];
+          endIndex = i - 1;
+        }
+      }
+    }
+  }
+
+  if (maxLength === 0) {
+    return '';
+  }
+
+  return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+}
+
+// Example usage
+const str1 = 'abcdef';
+const str2 = 'abc';
+console.log(longestCommonSubstring(str1, str2)); // Output: 'abc'
