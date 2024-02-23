5d66bd2ab1379816184d033ce0ed790c85089fc0
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:57:47 +0800

    2-23-2024 23:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,26 @@
-function isArraySortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
+function largestPrimeFactor(num) {
+    let largestPrime = 1;
+    
+    // Remove all factors of 2
+    while (num % 2 === 0) {
+        largestPrime = 2;
+        num = num / 2;
+    }
+    
+    // Check for factors starting from 3
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        while (num % i === 0) {
+            largestPrime = i;
+            num = num / i;
         }
     }
-    return true;
+    
+    if (num > 2) {
+        largestPrime = num;
+    }
+    
+    return largestPrime;
 }
 
-// Example usage
-const sortedArray = [1, 2, 3, 4, 5];
-const unsortedArray = [5, 2, 8, 1, 9];
-
-console.log(isArraySortedAscending(sortedArray)); // Output: true
-console.log(isArraySortedAscending(unsortedArray)); // Output: false
+const number = 13195;
+console.log(largestPrimeFactor(number)); // Output: 29
