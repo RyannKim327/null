c658c83f2be179039a6162b7e8c081e0d355d518
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:01:38 +0800

    2-24-2024 0:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,7 @@
-function isPrime(num) {
-  if (num < 2) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function largestPrimeFactor(num) {
-  let largestPrime = 0;
-  
-  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
-    if (num % i === 0) {
-      let factor = num / i;
-      if (isPrime(factor)) {
-        largestPrime = factor;
-        break;
-      }
-      if (isPrime(i)) {
-        largestPrime = i;
-      }
-    }
-  }
-  
-  return largestPrime;
-}
-
-// Example usage
-const num = 13195;
-console.log(largestPrimeFactor(num)); // Output: 29
+// Generate a random number between 1 and 10
+let randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber);
