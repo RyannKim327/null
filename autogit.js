6e4f9e2f66b707ab7e4d2042eedde912ebc30443
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:15:04 +0800

    2-24-2024 0:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,8 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return -1;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
 // Example usage
-const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15, 17];
-const targetValue = 7;
-const index = binarySearch(sortedArray, targetValue);
-
-if (index !== -1) {
-    console.log(`Found ${targetValue} at index ${index}`);
-} else {
-    console.log(`${targetValue} not found in the array`);
-}
+let string = 'Hello, World!';
+let stringWithoutVowels = removeVowels(string);
+console.log(stringWithoutVowels); // Output: 'Hll, Wrld!'
