ab9b54778f2dbd8c7f8988e8e1eef61a4b917e87
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:57:30 +0800

    2-23-2024 23:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,5 @@
-function fibonacciSearch(arr, key) {
-    let fibMMm2 = 0; // (m-2)'th Fibonacci No.
-    let fibMMm1 = 1; // (m-1)'th Fibonacci No.
-    let fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
+let array = [1, 2, 3, 4, 2, 3, 5];
 
-    let n = arr.length;
+let uniqueArray = Array.from(new Set(array));
 
-    while (fibM < n) {
-        fibMMm2 = fibMMm1;
-        fibMMm1 = fibM;
-        fibM = fibMMm2 + fibMMm1;
-    }
-
-    let offset = -1;
-
-    while (fibM > 1) {
-
-        let i = Math.min(offset + fibMMm2, n - 1);
-
-        if (arr[i] < key) {
-            fibM = fibMMm1;
-            fibMMm1 = fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-            offset = i;
-        }
-        else if (arr[i] > key) {
-            fibM = fibMMm2;
-            fibMMm1 = fibMMm1 - fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-        }
-        else {
-            return i;
-        }
-    }
-
-    if (fibMMm1 && arr[n-1] === key) {
-        return n-1;
-    }
-
-    return -1;
-}
-
-// Example usage
-let arr = [2, 3, 5, 8, 13, 21, 34, 55, 89];
-let key = 21;
-
-let index = fibonacciSearch(arr, key);
-
-console.log(index); // Output: 5
+console.log(uniqueArray); // Output will be [1, 2, 3, 4, 5]
