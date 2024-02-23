2f62516db7b5b6c773cdce9cbdaec6ed408d8dd4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:45:17 +0800

    2-23-2024 23:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,37 @@
-const axios = require('axios');
+function heapify(arr, n, i) {
+    let largest = i;
+    let left = 2 * i + 1;
+    let right = 2 * i + 2;
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest !== i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, n, largest);
+    }
+}
+
+function heapSort(arr) {
+    const n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapify(arr, i, 0);
+    }
+
+    return arr;
+}
+
+// Example usage
+const arr = [12, 11, 13, 5, 6, 7];
+console.log(heapSort(arr)); // Output: [5, 6, 7, 11, 12, 13]
