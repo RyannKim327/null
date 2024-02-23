2ebadf406fb356da44a8c042b303a9b954d130ae
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:15:34 +0800

    2-24-2024 0:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,49 @@
-let arr = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
+class Graph {
+    constructor() {
+        this.nodes = [];
+        this.adjList = {};
+    }
 
-let index = arr.indexOf(elementToRemove);
+    addNode(node) {
+        this.nodes.push(node);
+        this.adjList[node] = [];
+    }
 
-if (index > -1) {
-    arr.splice(index, 1);
+    addEdge(node1, node2) {
+        this.adjList[node1].push(node2);
+        this.adjList[node2].push(node1);
+    }
+
+    depthFirstSearch(startNode) {
+        const visited = {};
+        const result = [];
+
+        const dfs = (node) => {
+            if (!node) return;
+
+            visited[node] = true;
+            result.push(node);
+
+            this.adjList[node].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    dfs(neighbor);
+                }
+            });
+        };
+
+        dfs(startNode);
+        return result;
+    }
 }
 
-console.log(arr); // [1, 2, 4, 5]
+// Usage
+const graph = new Graph();
+graph.addNode(1);
+graph.addNode(2);
+graph.addNode(3);
+graph.addNode(4);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+
+console.log(graph.depthFirstSearch(1)); // Output: [1, 2, 4, 3]
