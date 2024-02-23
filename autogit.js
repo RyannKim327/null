b81d7bf7b4b059bcee85575c3c780c8c54dd9ee4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:02:26 +0800

    2-24-2024 0:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,4 @@
-function heapSort(arr) {
-    // Build max heap
-    function heapify(arr, n, i) {
-        let largest = i;
-        let left = 2 * i + 1;
-        let right = 2 * i + 2;
-
-        if (left < n && arr[left] > arr[largest]) {
-            largest = left;
-        }
-
-        if (right < n && arr[right] > arr[largest]) {
-            largest = right;
-        }
-
-        if (largest !== i) {
-            [arr[i], arr[largest]] = [arr[largest], arr[i]];
-            heapify(arr, n, largest);
-        }
-    }
-
-    // Build max heap
-    for (let i = Math.floor(arr.length / 2) - 1; i >= 0; i--) {
-        heapify(arr, arr.length, i);
-    }
-
-    // Heap sort
-    for (let i = arr.length - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapify(arr, i, 0);
-    }
-
-    return arr;
-}
-
-// Test the heap sort algorithm
-let arr = [12, 11, 13, 5, 6, 7];
-console.log("Original array: ", arr);
-console.log("Sorted array (using Heap Sort): ", heapSort(arr));
+let str1 = 'Hello';
+let str2 = 'World';
+let combinedString = str1 + ' ' + str2;
+console.log(combinedString); // Output: Hello World
