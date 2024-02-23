e70507edd14afb2d0bd2bc123ee944d3538c0b89
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:48:22 +0800

    2-23-2024 23:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,64 @@
-let str = "Hello, World! This is a test string.";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces);
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addNode(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        let next = null;
+
+        while (current !== null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        this.head = prev;
+    }
+
+    printList() {
+        let current = this.head;
+        while (current !== null) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+
+// Example usage
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+list.addNode(5);
+
+console.log("Original Linked List:");
+list.printList();
+
+list.reverse();
+
+console.log("Reversed Linked List:");
+list.printList();
