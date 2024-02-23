ff59ba5c23045277a413322bfe6650a5c613a04e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:14:10 +0800

    2-24-2024 0:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,93 +1,31 @@
-class Node {
-    constructor(order, isLeaf = true) {
-        this.order = order;
-        this.keys = [];
-        this.children = [];
-        this.isLeaf = isLeaf;
+function interpolationSearch(arr, x) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high && x >= arr[low] && x <= arr[high]) {
+        let pos = Math.floor(low + ((x - arr[low]) * (high - low)) / (arr[high] - arr[low]));
+
+        if (arr[pos] === x) {
+            return pos;
+        }
+
+        if (arr[pos] < x) {
+            low = pos + 1;
+        } else {
+            high = pos - 1;
+        }
     }
+
+    return -1;
 }
 
-class BTree {
-    constructor(order) {
-        this.root = new Node(order);
-        this.order = order;
-    }
+// Example usage
+const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+const x = 12;
+const index = interpolationSearch(arr, x);
 
-    search(key) {
-        return this.searchRecursive(this.root, key);
-    }
-
-    searchRecursive(node, key) {
-        let i = 0;
-        while (i < node.keys.length && key > node.keys[i]) {
-            i++;
-        }
-        if (node.keys[i] === key) {
-            return node;
-        }
-        if (node.isLeaf) {
-            return null;
-        } else {
-            return this.searchRecursive(node.children[i], key);
-        }
-    }
-
-    insert(key) {
-        let node = this.root;
-        if (node.keys.length === (2 * this.order) - 1) {
-            let newRoot = new Node(this.order, false);
-            newRoot.children.push(this.root);
-            this.splitChild(newRoot, 0);
-            this.root = newRoot;
-            this.insertNonFull(newRoot, key);
-        } else {
-            this.insertNonFull(node, key);
-        }
-    }
-
-    insertNonFull(node, key) {
-        let i = node.keys.length - 1;
-        if (node.isLeaf) {
-            while (i >= 0 && key < node.keys[i]) {
-                node.keys[i + 1] = node.keys[i];
-                i--;
-            }
-            node.keys[i + 1] = key;
-        } else {
-            while (i >= 0 && key < node.keys[i]) {
-                i--;
-            }
-            i++;
-            if (node.children[i].keys.length === (2 * this.order) - 1) {
-                this.splitChild(node, i);
-                if (key > node.keys[i]) {
-                    i++;
-                }
-            }
-            this.insertNonFull(node.children[i], key);
-        }
-    }
-
-    splitChild(parent, index) {
-        let order = this.order;
-        let child = parent.children[index];
-        let newChild = new Node(order, child.isLeaf);
-        parent.keys.splice(index, 0, child.keys[order - 1]);
-        parent.children.splice(index + 1, 0, newChild);
-
-        newChild.keys = child.keys.splice(order);
-        if (!child.isLeaf) {
-            newChild.children = child.children.splice(order);
-        }
-    }
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log("Element not found");
 }
-
-// Usage
-let bTree = new BTree(2);
-bTree.insert(1);
-bTree.insert(3);
-bTree.insert(5);
-bTree.insert(6);
-bTree.insert(9);
-
-console.log(bTree.search(5)); // Outputs the node containing key 5
