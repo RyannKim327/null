96210b9cf5425f9e1fe8d000ea427cbf7fce6fd5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:47:35 +0800

    2-23-2024 23:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,5 @@
-function heapify(arr, n, i) {
-    let largest = i;
-    let left = 2 * i + 1;
-    let right = 2 * i + 2;
+let str = "Hello, World! This is a test string with spaces.";
 
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
-    }
+let stringWithoutSpaces = str.replace(/\s/g, "");
 
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-
-    if (largest !== i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapify(arr, n, largest);
-    }
-}
-
-function heapSort(arr) {
-    const n = arr.length;
-
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-
-    for (let i = n - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapify(arr, i, 0);
-    }
-
-    return arr;
-}
-
-const arr = [12, 11, 13, 5, 6, 7];
-console.log("Original array:");
-console.log(arr);
-
-const sortedArr = heapSort(arr);
-
-console.log("Sorted array:");
-console.log(sortedArr);
+console.log(stringWithoutSpaces);
