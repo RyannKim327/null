947dfe8538432f9d2a5a00d82f4dad873be1dad5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:52:40 +0800

    2-23-2024 23:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,22 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
+function firstNonRepeatingCharacter(str) {
+    let charCount = {};
 
-    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+    // Count frequency of each character
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
 
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
+    // Find the first non-repeating character
+    for (let char of str) {
+        if (charCount[char] === 1) {
+            return char;
         }
     }
 
-    let lcs = '';
-    let i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return lcs;
+    return null; // Return null if no non-repeating character found
 }
 
 // Example usage
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-console.log(longestCommonSubsequence(str1, str2)); // Output: "ADH"
+const str = "leetcode";
+const result = firstNonRepeatingCharacter(str);
+console.log(result); // Output: "l"
