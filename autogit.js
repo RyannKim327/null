c5fa0edfb1e31127e6ee3c71747763ef0f3be740
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:53:43 +0800

    2-23-2024 23:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,52 @@
-async function connectToAndroidApp() {
-  const response = await fetch('http://your-android-app-url/api/data', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      // Add any necessary headers here
+// Node class to represent each node in the binary tree
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-  });
-
-  if (!response.ok) {
-    throw new Error('Unable to connect to Android app');
-  }
-
-  const data = await response.json();
-  console.log(data);
 }
 
-connectToAndroidApp();
+// Binary Tree class
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Function to insert a new node in the binary tree
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+}
+
+// Example usage
+const tree = new BinaryTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+tree.insert(7);
+
+console.log(tree.root);
