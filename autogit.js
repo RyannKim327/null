e71f069cab281acadcdf29e37f9942bab102385e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:48:07 +0800

    2-23-2024 23:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,103 @@
-function isPrime(number) {
-    if (number <= 1) {
-        return false;
+class Node {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
     }
-    
-    // Check for divisibility from 2 to the square root of the number
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
+}
+
+class SkipList {
+    constructor(maxLevel, p) {
+        this.maxLevel = maxLevel;
+        this.p = p;
+        this.level = 0;
+        this.head = new Node(null, maxLevel);
+    }
+
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.p && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        const update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current === null || current.value !== value) {
+            const newLevel = this.randomLevel();
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.head;
+                }
+                this.level = newLevel;
+            }
+
+            const newNode = new Node(value, newLevel);
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    search(value) {
+        let current = this.head;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+
+        current = current.forward[0];
+        if (current !== null && current.value === value) {
+            return true;
+        } else {
             return false;
         }
     }
-    
-    return true;
+
+    remove(value) {
+        const update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current !== null && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].forward[i] !== current) break;
+                update[i].forward[i] = current.forward[i];
+            }
+
+            while (this.level > 0 && this.head.forward[this.level] === null) {
+                this.level--;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 // Usage
-const number = 17;
-if (isPrime(number)) {
-    console.log(`${number} is a prime number`);
-} else {
-    console.log(`${number} is not a prime number`);
-}
+const skipList = new SkipList(4, 0.5);
+skipList.insert(3);
+skipList.insert(6);
+skipList.insert(2);
