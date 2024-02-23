3aeb0279ce3bd8ed12f4a5565a6f4da10bfbf3c6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:08:42 +0800

    2-24-2024 0:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,3 @@
-function isPalindrome(str) {
-    let i = 0;
-    let j = str.length - 1;
-
-    while (i < j) {
-        if (str[i] !== str[j]) {
-            return false;
-        }
-        i++;
-        j--;
-    }
-
-    return true;
-}
-
-// Test the function
-const str1 = "radar";
-const str2 = "hello";
-
-console.log(isPalindrome(str1)); // Output: true
-console.log(isPalindrome(str2)); // Output: false
+let str = "Hello, World!";
+let stringWithoutSpaces = str.replace(/\s/g, "");
+console.log(stringWithoutSpaces);
