86f370372c32ba8951bd7629ff6dc97a53fa9796
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:49:13 +0800

    2-23-2024 23:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,40 @@
-// Take user input for a number
-let number = prompt("Enter a number:");
-
-// Check if the number is even or odd
-if (number % 2 === 0) {
-    console.log(`${number} is even.`);
-} else {
-    console.log(`${number} is odd.`);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
+
+function hasCycle(head) {
+  if (!head) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+// Example usage
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+// Uncomment the line below to create a cycle
+// node4.next = node2;
+
+console.log(hasCycle(node1)); // Should return true if there is a cycle
