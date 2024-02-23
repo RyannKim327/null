90f031cb89b5b908c2446c099246fb5ab3b23cf4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:20:12 +0800

    2-24-2024 0:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,100 @@
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+}
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+
+    rotateRight(node) {
+        const newRoot = node.left;
+        node.left = newRoot.right;
+        newRoot.right = node;
+        
+        this.updateHeight(node);
+        this.updateHeight(newRoot);
+        
+        return newRoot;
+    }
+
+    rotateLeft(node) {
+        const newRoot = node.right;
+        node.right = newRoot.left;
+        newRoot.left = node;
+
+        this.updateHeight(node);
+        this.updateHeight(newRoot);
+        
+        return newRoot;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = this._insert(node.right, value);
+        } else {
+            // Duplicate values not allowed
+            return node;
+        }
+
+        this.updateHeight(node);
+
+        const balanceFactor = this.getBalanceFactor(node);
+
+        if (balanceFactor > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balanceFactor < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balanceFactor > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balanceFactor < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+}
+
+// Example usage
+const avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(5);
+avlTree.insert(15);
