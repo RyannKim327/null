2db73204de8c1023cff460883eef1b55ac868e17
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:53:34 +0800

    2-23-2024 23:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,18 @@
-function reverseWords(str) {
-    return str.split(" ").reverse().join(" ");
+async function connectToAndroidApp() {
+  const response = await fetch('http://your-android-app-url/api/data', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      // Add any necessary headers here
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('Unable to connect to Android app');
+  }
+
+  const data = await response.json();
+  console.log(data);
 }
 
-// Example
-const originalString = "Hello World! This is a sample string.";
-const reversedString = reverseWords(originalString);
-console.log(reversedString);
+connectToAndroidApp();
