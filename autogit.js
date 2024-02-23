4aa5577d5e2925e72db3a956e8f4a36b6c9c16c5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:51:03 +0800

    2-23-2024 23:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,5 @@
-function isPalindrome(str) {
-    // Remove special characters and convert the string to lowercase
-    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+const array = [10, 5, 20, 15, 30];
 
-    // Reverse the string
-    let reversedStr = str.split('').reverse().join('');
+const max = Math.max(...array);
 
-    // Check if the original string is equal to the reversed string
-    return str === reversedStr;
-}
-let inputString = "A man, a plan, a canal, Panama!";
-if (isPalindrome(inputString)) {
-    console.log(inputString + " is a palindrome!");
-} else {
-    console.log(inputString + " is not a palindrome.");
-}
+console.log(max); // Output: 30
