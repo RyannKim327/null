44094a98702e60100eeffc49dc5923ff5aaebf6e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:17:00 +0800

    2-24-2024 0:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,17 @@
-function buildPatternTable(pattern) {
-  const table = new Array(pattern.length).fill(0);
-  let j = 0;
-  
-  for (let i = 1; i < pattern.length; i++) {
-    if (pattern[i] === pattern[j]) {
-      j++;
-      table[i] = j;
-    } else {
-      if (j) {
-        j = table[j - 1];
-        i--;
-      } else {
-        table[i] = 0;
-      }
-    }
-  }
-  
-  return table;
+function reverseWords(str) {
+    // Split the string into an array of words
+    let wordsArr = str.split(' ');
+
+    // Reverse the array
+    wordsArr.reverse();
+
+    // Join the array back into a string
+    let reversedStr = wordsArr.join(' ');
+
+    return reversedStr;
 }
 
-function kmpSearch(text, pattern) {
-  const table = buildPatternTable(pattern);
-  let i = 0;
-  let j = 0;
-
-  while (i < text.length) {
-    if (text[i] === pattern[j]) {
-      if (j === pattern.length - 1) {
-        return i - j;
-      }
-      i++;
-      j++;
-    } else if (j > 0) {
-      j = table[j - 1];
-    } else {
-      i++;
-    }
-  }
-  
-  return -1;
-}
-
-// Test the implementation
-const text = "ababcababcabcabcab";
-const pattern = "abcabcabc";
-const index = kmpSearch(text, pattern);
-
-if (index !== -1) {
-  console.log(`Pattern found at index ${index}`);
-} else {
-  console.log("Pattern not found");
-}
+// Test the function
+let originalStr = 'Hello world, it is a beautiful day';
+let reversedStr = reverseWords(originalStr);
+console.log(reversedStr); // Output: 'day beautiful a is it world, Hello'
