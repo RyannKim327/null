56eae5c4ad60a0314070d9479cafaf72921affa4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:45:00 +0800

    2-23-2024 23:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-function boyerMooreSearch(text, pattern) {
-    const textLength = text.length;
-    const patternLength = pattern.length;
-    const last = buildLastTable(pattern);
+const axios = require('axios');
 
-    let i = patternLength - 1;
-    let j = patternLength - 1;
-
-    while (i < textLength) {
-        if (text[i] === pattern[j]) {
-            if (j === 0) {
-                return i;
-            }
-            i--;
-            j--;
-        } else {
-            i += patternLength - Math.min(j, 1 + last[text.charCodeAt(i)]);
-            j = patternLength - 1;
-        }
-    }
-
-    return -1;
-}
-
-function buildLastTable(pattern) {
-    const last = new Array(256).fill(-1);
-    for (let i = 0; i < pattern.length; i++) {
-        last[pattern.charCodeAt(i)] = i;
-    }
-    return last;
-}
-
-// Test the Boyer-Moore search function
-const text = "ABAAABCD";
-const pattern = "ABC";
-
-const index = boyerMooreSearch(text, pattern);
-
-if (index !== -1) {
-    console.log(`Pattern found at index: ${index}`);
-} else {
-    console.log("Pattern not found in the text.");
-}
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
