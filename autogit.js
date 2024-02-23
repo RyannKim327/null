b4537faff4f503271e3089990aba6d6d01991e23
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:55:06 +0800

    2-23-2024 23:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,3 @@
-function mergeSort(array) {
-    if (array.length <= 1) {
-        return array;
-    }
-
-    const middle = Math.floor(array.length / 2);
-    const left = array.slice(0, middle);
-    const right = array.slice(middle);
-
-    return merge(mergeSort(left), mergeSort(right));
-}
-
-function merge(left, right) {
-    let result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
-
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            result.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
-
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-}
-
-const array = [8, 3, 1, 7, 0, 10, 2];
-console.log(mergeSort(array));
+let str = "Hello, World!";
+let stringWithoutSpaces = str.replace(/\s/g, '');
+console.log(stringWithoutSpaces);
