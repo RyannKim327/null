cc1a040f8092fa8ecd3f323a0ce5595e7acb4748
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:20:26 +0800

    2-24-2024 0:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,11 @@
-function removeVowels(str) {
-  return str.replace(/[aeiouAEIOU]/g, '');
+function getLengthOfStr(str) {
+    let count = 0;
+    while (str[count] !== undefined) {
+        count++;
+    }
+    return count;
 }
 
-// Example usage
-const input = "Hello, World!";
-const result = removeVowels(input);
-
-console.log(result); // Output: "Hll, Wrld!"
+let myString = "Hello, World!";
+let length = getLengthOfStr(myString);
+console.log(length); // This will output the length of the string
