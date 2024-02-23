524f05dca4a0eacb27140c5d465df300ff4c6fe2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:01:53 +0800

    2-24-2024 0:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,76 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
+    }
+
+    enqueue(item, priority) {
+        const node = { item, priority };
+        this.heap.push(node);
+        this.bubbleUp(this.heap.length - 1);
+    }
+
+    dequeue() {
+        const min = this.heap[0];
+        const lastNode = this.heap.pop();
+
+        if (this.heap.length > 0) {
+            this.heap[0] = lastNode;
+            this.trickleDown(0);
+        }
+
+        return min.item;
+    }
+
+    bubbleUp(index) {
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+            if (this.heap[index].priority < this.heap[parentIndex].priority) {
+                [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    trickleDown(index) {
+        while (index < this.heap.length) {
+            const left = 2 * index + 1;
+            const right = 2 * index + 2;
+            let smallest = index;
+
+            if (left < this.heap.length && this.heap[left].priority < this.heap[smallest].priority) {
+                smallest = left;
+            }
+
+            if (right < this.heap.length && this.heap[right].priority < this.heap[smallest].priority) {
+                smallest = right;
+            }
+
+            if (smallest !== index) {
+                [this.heap[index], this.heap[smallest]] = [this.heap[smallest], this.heap[index]];
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+    isEmpty() {
+        return this.heap.length === 0;
+    }
+
+    size() {
+        return this.heap.length;
+    }
 }
 
-// Generate a random number between 1 and 10
-let randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
+// Example usage
+const pq = new PriorityQueue();
+pq.enqueue('Task 1', 2);
+pq.enqueue('Task 2', 1);
+pq.enqueue('Task 3', 3);
+
+while (!pq.isEmpty()) {
+    console.log(pq.dequeue());
+}
