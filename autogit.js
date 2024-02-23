d7786220282d5a58436d3dfa00a109b0a96b580e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:52:32 +0800

    2-23-2024 23:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,37 @@
-function calculateMean(list) {
-    let sum = 0;
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
 
-    for (let i = 0; i < list.length; i++) {
-        sum += list[i];
+    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
     }
 
-    return sum / list.length;
+    let lcs = '';
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return lcs;
 }
 
 // Example usage
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-
-console.log("Mean:", mean);
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+console.log(longestCommonSubsequence(str1, str2)); // Output: "ADH"
