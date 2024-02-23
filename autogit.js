3c5378354aac8cb6679b6910fd4c25c3539a7e07
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:53:26 +0800

    2-23-2024 23:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,8 @@
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
-        for (let i = 0; i < size; i++) {
-            this.table[i] = [];
-        }
-    }
-
-    hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash += key.charCodeAt(i);
-        }
-        return hash % this.size;
-    }
-
-    set(key, value) {
-        const index = this.hash(key);
-        this.table[index].push({ key, value });
-    }
-
-    get(key) {
-        const index = this.hash(key);
-        for (const item of this.table[index]) {
-            if (item.key === key) {
-                return item.value;
-            }
-        }
-        return undefined;
-    }
-
-    remove(key) {
-        const index = this.hash(key);
-        this.table[index] = this.table[index].filter(item => item.key !== key);
-    }
+function reverseWords(str) {
+    return str.split(" ").reverse().join(" ");
 }
 
-// Example usage
-const ht = new HashTable(10);
-ht.set("apple", "red");
-ht.set("banana", "yellow");
-console.log(ht.get("apple")); // Output: red
-ht.remove("banana");
-console.log(ht.get("banana")); // Output: undefined
+// Example
+const originalString = "Hello World! This is a sample string.";
+const reversedString = reverseWords(originalString);
+console.log(reversedString);
