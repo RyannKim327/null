4736091936ea680c78b594d117c7bc84bd39f1ee
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:03:08 +0800

    2-24-2024 0:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,16 @@
-class Node {
-    constructor(state, parent, cost, heuristic) {
-        this.state = state;
-        this.parent = parent;
-        this.cost = cost;
-        this.heuristic = heuristic;
-    }
-}
-
-class BeamSearch {
-    constructor(beamWidth, initialState, heuristic) {
-        this.beamWidth = beamWidth;
-        this.initialNode = new Node(initialState, null, 0, heuristic(initialState));
-    }
-
-    search() {
-        let candidates = [this.initialNode];
-
-        while (candidates.length > 0) {
-            candidates.sort((a, b) => a.cost + a.heuristic - b.cost - b.heuristic);
-            candidates = candidates.slice(0, this.beamWidth);
-
-            let newCandidates = [];
-
-            for (let candidate of candidates) {
-                // Generate successor states
-                let successors = this.generateSuccessors(candidate.state);
-
-                for (let successor of successors) {
-                    let child = new Node(successor.state, candidate, candidate.cost + successor.cost, this.heuristic(successor.state));
-                    newCandidates.push(child);
-                }
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
-
-            candidates = newCandidates;
         }
-
-        // Retrieve the best solution
-        let bestNode = candidates.reduce((a, b) => a.cost + a.heuristic < b.cost + b.heuristic ? a : b);
-        let solution = [];
-
-        while (bestNode != null) {
-            solution.unshift(bestNode.state);
-            bestNode = bestNode.parent;
-        }
-
-        return solution;
     }
-
-    generateSuccessors(state) {
-        // Generate successor states based on the current state
-        // Return an array of { state, cost } objects
-    }
+    return arr;
 }
 
-// Define a heuristic function
-function heuristic(state) {
-    // Evaluate the quality of the state
-    // Return a heuristic score
-}
-
-// Usage
-const initialState = // Define your initial state
-const beamWidth = 5;
-const beamSearch = new BeamSearch(beamWidth, initialState, heuristic);
-
-const solution = beamSearch.search();
-console.log(solution);
+const array = [5, 3, 8, 1, 2];
+console.log('Original Array:', array);
+console.log('Sorted Array:', bubbleSort(array));
