9c4d8bf9f6e7a2995094eb81fd9ff404021e3a5f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:57:57 +0800

    2-23-2024 23:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,65 @@
-function largestPrimeFactor(num) {
-    let largestPrime = 1;
-    
-    // Remove all factors of 2
-    while (num % 2 === 0) {
-        largestPrime = 2;
-        num = num / 2;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    
-    // Check for factors starting from 3
-    for (let i = 3; i <= Math.sqrt(num); i += 2) {
-        while (num % i === 0) {
-            largestPrime = i;
-            num = num / i;
-        }
-    }
-    
-    if (num > 2) {
-        largestPrime = num;
-    }
-    
-    return largestPrime;
 }
 
-const number = 13195;
-console.log(largestPrimeFactor(number)); // Output: 29
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+}
+
+// Usage example
+const tree = new BinaryTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+console.log(tree.search(5)); // Output: true
+console.log(tree.search(20)); // Output: false
