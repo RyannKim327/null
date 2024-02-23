44aa9cf3045e1a87a4aaa4ffd075bb8a5a7283fc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:47:18 +0800

    2-23-2024 23:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,28 @@
-let str1 = "Hello";
-let str2 = "World";
-let concatenatedString = str1 + " " + str2;
-console.log(concatenatedString); // Output: Hello World
-let str1 = "Hello";
-let str2 = "World";
-let concatenatedString = str1.concat(" ", str2);
-console.log(concatenatedString); // Output: Hello World
+function radixSort(arr) {
+    const getDigit = (num, place) => Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+    const getMaxDigits = (arr) => Math.max(...arr).toString().length;
+
+    const radixSortHelper = (arr) => {
+        const maxDigitCount = getMaxDigits(arr);
+        let result = arr;
+
+        for (let k = 0; k < maxDigitCount; k++) {
+            let digitBuckets = Array.from({ length: 10 }, () => []);
+
+            for (let i = 0; i < result.length; i++) {
+                digitBuckets[getDigit(result[i], k)].push(result[i]);
+            }
+
+            result = [].concat(...digitBuckets);
+        }
+
+        return result;
+    };
+
+    return radixSortHelper(arr);
+}
+
+// Example usage:
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr));
