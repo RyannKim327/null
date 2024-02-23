bc9d22149a568d85e9ee44fccd461063c0e81a92
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:16:51 +0800

    2-24-2024 0:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,53 @@
-// Make a GET request to a random API
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function buildPatternTable(pattern) {
+  const table = new Array(pattern.length).fill(0);
+  let j = 0;
+  
+  for (let i = 1; i < pattern.length; i++) {
+    if (pattern[i] === pattern[j]) {
+      j++;
+      table[i] = j;
+    } else {
+      if (j) {
+        j = table[j - 1];
+        i--;
+      } else {
+        table[i] = 0;
+      }
+    }
+  }
+  
+  return table;
+}
+
+function kmpSearch(text, pattern) {
+  const table = buildPatternTable(pattern);
+  let i = 0;
+  let j = 0;
+
+  while (i < text.length) {
+    if (text[i] === pattern[j]) {
+      if (j === pattern.length - 1) {
+        return i - j;
+      }
+      i++;
+      j++;
+    } else if (j > 0) {
+      j = table[j - 1];
+    } else {
+      i++;
+    }
+  }
+  
+  return -1;
+}
+
+// Test the implementation
+const text = "ababcababcabcabcab";
+const pattern = "abcabcabc";
+const index = kmpSearch(text, pattern);
+
+if (index !== -1) {
+  console.log(`Pattern found at index ${index}`);
+} else {
+  console.log("Pattern not found");
+}
