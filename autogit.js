d86ad9e133a0ab44939679084e518a8482432dc4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:20:35 +0800

    2-24-2024 0:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,42 @@
-function getLengthOfStr(str) {
-    let count = 0;
-    while (str[count] !== undefined) {
-        count++;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
     }
-    return count;
 }
 
-let myString = "Hello, World!";
-let length = getLengthOfStr(myString);
-console.log(length); // This will output the length of the string
+function breadthFirstSearch(root) {
+    if (!root) {
+        return;
+    }
+
+    const queue = [root];
+    const visited = new Set();
+
+    while (queue.length > 0) {
+        const currentNode = queue.shift();
+
+        if (!visited.has(currentNode)) {
+            console.log(currentNode.value);
+            visited.add(currentNode);
+
+            for (let child of currentNode.children) {
+                queue.push(child);
+            }
+        }
+    }
+}
+
+// Example usage
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+const nodeF = new Node('F');
+
+nodeA.children = [nodeB, nodeC];
+nodeB.children = [nodeD, nodeE];
+nodeC.children = [nodeF];
+
+breadthFirstSearch(nodeA);
