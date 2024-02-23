422b0c77394478be854cc1925649e09432ad0970
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:47:44 +0800

    2-23-2024 23:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,28 @@
-let str = "Hello, World! This is a test string with spaces.";
+function largestPrimeFactor(number) {
+    let largestFactor = 1;
 
-let stringWithoutSpaces = str.replace(/\s/g, "");
+    // Divide the number by 2 repeatedly to extract out all the factors of 2
+    while (number % 2 === 0) {
+        largestFactor = 2;
+        number = number / 2;
+    }
 
-console.log(stringWithoutSpaces);
+    // Check for odd factors starting from 3
+    for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
+        while (number % i === 0) {
+            largestFactor = i;
+            number = number / i;
+        }
+    }
+
+    // If the remaining number is a prime number greater than 2
+    if (number > 2) {
+        largestFactor = number;
+    }
+
+    return largestFactor;
+}
+
+// Example usage
+const number = 84;
+console.log(`The largest prime factor of ${number} is: ${largestPrimeFactor(number)}`);
