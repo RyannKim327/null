1c6ba184a4606c688709130635ba1d6f61e8222f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:13:14 +0800

    2-24-2024 0:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,27 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function longestCommonSubstring(str1, str2) {
+    let dp = new Array(str1.length + 1).fill(0).map(() => new Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
+
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            }
+        }
+    }
+
+    if (maxLength === 0) {
+        return "";
+    } else {
+        return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+    }
 }
 
-// Generate a random number between 1 and 100
-const randomNumber = getRandomNumber(1, 100);
-console.log(randomNumber);
+let str1 = "abcdef";
+let str2 = "bcdf";
+console.log(longestCommonSubstring(str1, str2)); // Output: "bcd"
