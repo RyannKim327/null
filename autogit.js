79c2f4ef2f18311637b0fdb3f91dae79c17303a2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:19:58 +0800

    2-24-2024 0:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,6 @@
-function firstNonRepeatingChar(str) {
-    let charCount = {};
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
 
-    // Count occurrences of each character
-    for (let char of str) {
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
-    }
-
-    // Find the first character with count = 1
-    for (let char of str) {
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-
-    return null; // If no non-repeating character found
-}
-
-// Test the function
-let str = "hello world";
-let firstNonRepeating = firstNonRepeatingChar(str);
-console.log("First non-repeating character is:", firstNonRepeating);
+fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
