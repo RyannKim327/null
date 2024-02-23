4e0ef86dc29d88aee18f7c40d3d944b11b66d441
RyannKim327 <rksesgundo123@gmail.com>
2024-02-24 00:16:40 +0800

    2-24-2024 0:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,5 @@
-function isSortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Test the function
-const sortedArray = [1, 2, 3, 4, 5];
-const unsortedArray = [5, 4, 3, 2, 1];
-
-console.log(isSortedAscending(sortedArray)); // Output: true
-console.log(isSortedAscending(unsortedArray)); // Output: false
+// Make a GET request to a random API
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
