2954af161faa81b2c7700a4c1ab7cf775ff53ec4
RyannKim327 <rksesgundo123@gmail.com>
2024-02-23 23:48:58 +0800

    2-23-2024 23:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,11 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+function findKthSmallestElement(arr, k) {
+    arr.sort((a, b) => a - b); // Sort the array in ascending order
+    return arr[k - 1]; // Return the k-th smallest element (index k-1)
 }
 
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
+// Example of how to use the function
+const array = [7, 10, 4, 3, 20, 15];
+const k = 2; // Find the 2nd smallest element
 
-    enqueue(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.size++;
-    }
-
-    dequeue() {
-        if (!this.head) {
-            return null;
-        }
-        const dequeuedValue = this.head.value;
-        this.head = this.head.next;
-        this.size--;
-        return dequeuedValue;
-    }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    peek() {
-        if (!this.head) {
-            return null;
-        }
-        return this.head.value;
-    }
-
-    print() {
-        let current = this.head;
-        while (current) {
-            console.log(current.value);
-            current = current.next;
-        }
-    }
-}
-
-// Example usage
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-queue.print(); // Output: 1, 2, 3
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.peek()); // Output: 2
+const kthSmallestElement = findKthSmallestElement(array, k);
+console.log(`The ${k}-th smallest element is: ${kthSmallestElement}`);
