7cb31559d3aa9194956205a5ba5ce997099716d9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:08:27 +0800

    2-26-2024 19:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,24 @@
-let str = "Hello, World!";
-let newStr = str.replace(/\s/g, '');
-console.log(newStr); // Output: "Hello,World!"
+function longestCommonPrefix(strings) {
+    if (strings.length === 0) {
+        return '';
+    }
+    
+    let prefix = strings[0];
+    
+    for (let i = 1; i < strings.length; i++) {
+        let currentString = strings[i];
+        let j = 0;
+        
+        while (j < prefix.length && j < currentString.length && prefix[j] === currentString[j]) {
+            j++;
+        }
+        
+        prefix = prefix.slice(0, j);
+    }
+    
+    return prefix;
+}
+
+// Example usage
+const strings = ['flower', 'flour', 'flow'];
+console.log(longestCommonPrefix(strings)); // Output: 'flo'
