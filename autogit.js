8a4a6e82f27ea8252c19d4275cbc536a3ba8cb48
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:50:26 +0800

    2-26-2024 18:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,71 @@
-function mergeSortIterative(arr) {
-    const stack = [];
-    for (let i = 0; i < arr.length; i++) {
-        stack.push([i, i]);
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    
-    while (stack.length > 1) {
-        const left1 = stack.pop();
-        const left2 = stack.pop();
-        stack.push(merge(arr, left1[0], left1[1], left2[0], left2[1]));
-    }
-
-    return arr;
 }
 
-function merge(arr, start1, end1, start2, end2) {
-    const merged = [];
-    let index = 0;
-    let i = start1;
-    let j = start2;
-    
-    while (i <= end1 && j <= end2) {
-        if (arr[i] <= arr[j]) {
-            merged[index++] = arr[i++];
-        } else {
-            merged[index++] = arr[j++];
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+
+        let current = this.root;
+
+        while (true) {
+            if (value === current.value) {
+                return undefined;
+            }
+
+            if (value < current.value) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
         }
     }
 
-    while (i <= end1) {
-        merged[index++] = arr[i++];
-    }
+    search(value) {
+        let current = this.root;
 
-    while (j <= end2) {
-        merged[index++] = arr[j++];
-    }
+        while (current) {
+            if (value === current.value) {
+                return current;
+            }
 
-    for (let k = start1, l = 0; k <= end2; k++, l++) {
-        arr[k] = merged[l];
-    }
+            if (value < current.value) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
 
-    return [start1, end2];
+        return null;
+    }
 }
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
 
-// Example usage
-const arr = [38, 27, 43, 3, 9, 82, 10];
-console.log("Unsorted array:", arr);
-mergeSortIterative(arr);
-console.log("Sorted array:", arr);
+console.log(bst.search(5)); // Output: Node { value: 5, left: Node { value: 3, ...}, right: Node { value: 7, ... } }
+console.log(bst.search(20)); // Output: null
