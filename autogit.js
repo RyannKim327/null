00ea0c41ecc07c00f20a1cf3eb6ab01e5ad8ed24
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:04:52 +0800

    2-26-2024 19:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,4 @@
-function findFirstRepeatedChar(str) {
-    let charMap = {};
-
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
-    }
-
-    return null;
-}
-
-// Example usage
-let inputString = "hello world";
-let repeatedChar = findFirstRepeatedChar(inputString);
-
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("No repeated characters found.");
-}
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
