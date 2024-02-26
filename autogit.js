7c617b69b753cc244f1cdc1785abf3420ce5895b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:35:11 +0800

    2-26-2024 18:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,46 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result); // Output: Hello World
+function biDirectionalSearch(startNode, goalNode) {
+    let forwardVisited = new Set();
+    let backwardVisited = new Set();
+    
+    let forwardQueue = [startNode];
+    let backwardQueue = [goalNode];
+    
+    while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+        // Forward search
+        let currentNodeForward = forwardQueue.shift();
+        forwardVisited.add(currentNodeForward);
+        
+        // Check if intersection
+        if (backwardVisited.has(currentNodeForward)) {
+            // Path found
+            return true;
+        }
+        
+        // Explore neighbors of currentNodeForward
+        for (let neighbor of currentNodeForward.neighbors) {
+            if (!forwardVisited.has(neighbor)) {
+                forwardQueue.push(neighbor);
+            }
+        }
+        
+        // Backward search
+        let currentNodeBackward = backwardQueue.shift();
+        backwardVisited.add(currentNodeBackward);
+        
+        // Check if intersection
+        if (forwardVisited.has(currentNodeBackward)) {
+            // Path found
+            return true;
+        }
+        
+        // Explore neighbors of currentNodeBackward
+        for (let neighbor of currentNodeBackward.neighbors) {
+            if (!backwardVisited.has(neighbor)) {
+                backwardQueue.push(neighbor);
+            }
+        }
+    }
+    
+    // Path not found
+    return false;
+}
