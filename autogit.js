3519231e6f06b9c7faef4575bbdb5d4b16292c2d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:47:23 +0800

    2-26-2024 18:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,18 @@
-// Function to get the digit of a number at a specific position
-function getDigit(num, i) {
-  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+function reverseString(str) {
+    // Step 1: Convert string to array
+    var arr = str.split("");
+
+    // Step 2: Reverse the array
+    var reversedArr = arr.reverse();
+
+    // Step 3: Join the array back to a string
+    var reversedStr = reversedArr.join("");
+
+    return reversedStr;
 }
 
-// Function to get the number of digits in a number
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
+// Pass the string you want to reverse to the function
+var originalStr = "Hello, World!";
+var reversedStr = reverseString(originalStr);
 
-// Function to get the maximum number of digits in an array of numbers
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
-  }
-  return maxDigits;
-}
-
-function radixSort(nums) {
-  const maxDigitCount = mostDigits(nums);
-  for (let k = 0; k < maxDigitCount; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < nums.length; i++) {
-      let digit = getDigit(nums[i], k);
-      digitBuckets[digit].push(nums[i]);
-    }
-    nums = [].concat(...digitBuckets);
-  }
-  return nums;
-}
-
-// Test the radix sort function
-const testArray = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(testArray)); // Outputs [2, 24, 45, 66, 75, 90, 170, 802]
+console.log(reversedStr); // Output: "!dlroW ,olleH"
