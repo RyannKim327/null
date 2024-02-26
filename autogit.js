6a13f92cb41cb232d9af32572c8cd9ef738db635
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:24:02 +0800

    2-26-2024 18:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,119 @@
-function calculateMean(numbers) {
-    const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    const mean = sum / numbers.length;
-    return mean;
+class AVLNode {
+    constructor(value) {
+        this.value = value;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+    }
 }
 
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log('Mean:', mean);
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    rotateRight(y) {
+        let x = y.left;
+        let T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+
+        return x;
+    }
+
+    rotateLeft(x) {
+        let y = x.right;
+        let T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this.insertNode(this.root, value);
+    }
+
+    insertNode(node, value) {
+        if (node === null) {
+            return new AVLNode(value);
+        }
+
+        if (value < node.value) {
+            node.left = this.insertNode(node.left, value);
+        } else if (value > node.value) {
+            node.right = this.insertNode(node.right, value);
+        } else {
+            return node; // Duplicate values are not allowed
+        }
+
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+
+        let balance = this.getBalanceFactor(node);
+
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    printInorder(node) {
+        if (node != null) {
+            this.printInorder(node.left);
+            console.log(node.value);
+            this.printInorder(node.right);
+        }
+    }
+}
+let avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(20);
+avlTree.insert(30);
+avlTree.insert(40);
+avlTree.insert(50);
+avlTree.insert(25);
+
+avlTree.printInorder(avlTree.root);
+10
+20
+25
+30
+40
+50
