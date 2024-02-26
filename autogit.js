f775fcd34a6a8947b7fe1bf890f052790c695c39
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:25:29 +0800

    2-26-2024 18:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,94 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result); // Output: Hello World
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+
+  enqueue(element, priority) {
+    const item = { element, priority };
+    this.heap.push(item);
+    this.bubbleUp();
+  }
+
+  dequeue() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    const min = this.heap[0];
+    const end = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.sinkDown();
+    }
+
+    return min.element;
+  }
+
+  bubbleUp() {
+    let index = this.heap.length - 1;
+    const element = this.heap[index];
+
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.heap[parentIndex];
+
+      if (element.priority >= parent.priority) {
+        break;
+      }
+
+      this.heap[parentIndex] = element;
+      this.heap[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.heap.length;
+    const element = this.heap[0];
+
+    while (true) {
+      let leftChildIdx = 2 * index + 1;
+      let rightChildIdx = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.heap[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if (rightChildIdx < length) {
+        rightChild = this.heap[rightChildIdx];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if (swap === null) {
+        break;
+      }
+
+      this.heap[index] = this.heap[swap];
+      this.heap[swap] = element;
+      index = swap;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+    return this.heap[0].element;
+  }
+}
