8978894e2f6d7fe871f54506f9f0bd7ed9c14b9a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:31:14 +0800

    2-26-2024 18:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,16 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    push(data) {
-        let newNode = new Node(data);
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-
-    findNthNodeFromEnd(n) {
-        let mainPointer = this.head;
-        let refPointer = this.head;
-
-        // Move refPointer by n steps
-        for (let i = 0; i < n; i++) {
-            if (refPointer === null) {
-                return null; // List is shorter than n
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; // Swap elements
             }
-            refPointer = refPointer.next;
         }
-
-        // Move both pointers until refPointer reaches the end
-        while (refPointer !== null) {
-            mainPointer = mainPointer.next;
-            refPointer = refPointer.next;
-        }
-
-        return mainPointer;
     }
+    return arr;
 }
 
-// Sample usage
-let ll = new LinkedList();
-ll.push(1);
-ll.push(2);
-ll.push(3);
-ll.push(4);
-ll.push(5);
-
-let n = 2; // Find the 2nd node from the end
-let nthNodeFromEnd = ll.findNthNodeFromEnd(n);
-
-if (nthNodeFromEnd !== null) {
-    console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.data}`);
-} else {
-    console.log(`List is shorter than ${n}`);
-}
+// Example usage:
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray); // Output: [11, 12, 22, 25, 34, 64, 90]
