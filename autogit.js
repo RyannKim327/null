58e4bb8af2456045a9c634820317fb90bf70e81a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:06:29 +0800

    2-26-2024 19:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,23 @@
-function buildPrefixTable(pattern) {
-    const prefixTable = new Array(pattern.length);
-    prefixTable[0] = 0;
-    let prefixLength = 0;
-    let i = 1;
-
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[prefixLength]) {
-            prefixLength++;
-            prefixTable[i] = prefixLength;
-            i++;
-        } else {
-            if (prefixLength !== 0) {
-                prefixLength = prefixTable[prefixLength - 1];
-            } else {
-                prefixTable[i] = 0;
-                i++;
-            }
-        }
+function factorialIterative(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+        result *= i;
     }
-
-    return prefixTable;
+    return result;
 }
 
-function kmpSearch(text, pattern) {
-    const prefixTable = buildPrefixTable(pattern);
-    let textIndex = 0;
-    let patternIndex = 0;
-
-    while (textIndex < text.length) {
-        if (pattern[patternIndex] === text[textIndex]) {
-            patternIndex++;
-            textIndex++;
-        }
-
-        if (patternIndex === pattern.length) {
-            console.log('Pattern found at index ' + (textIndex - patternIndex));
-            patternIndex = prefixTable[patternIndex - 1];
-        } else if (textIndex < text.length && pattern[patternIndex] !== text[textIndex]) {
-            if (patternIndex !== 0) {
-                patternIndex = prefixTable[patternIndex - 1];
-            } else {
-                textIndex++;
-            }
-        }
+// Example usage
+const num = 5;
+const factorial = factorialIterative(num);
+console.log(`Factorial of ${num} is: ${factorial}`);
+function factorialRecursive(num) {
+    if (num === 0) {
+        return 1;
     }
+    return num * factorialRecursive(num - 1);
 }
 
-// Test the KMP algorithm
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-kmpSearch(text, pattern);
+// Example usage
+const num = 5;
+const factorial = factorialRecursive(num);
+console.log(`Factorial of ${num} is: ${factorial}`);
