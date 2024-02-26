508e34cb748ded5314908844d753961a8502a577
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:10:48 +0800

    2-26-2024 19:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,3 @@
-function selectionSort(arr) {
-    const n = arr.length;
-
-    for (let i = 0; i < n - 1; i++) {
-        let minIndex = i;
-
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-        }
-    }
-
-    return arr;
-}
-
-// Example usage
-const array = [64, 25, 12, 22, 11];
-console.log("Original Array: ", array);
-console.log("Sorted Array: ", selectionSort(array));
+let str = "This is a sample string with spaces";
+let stringWithoutSpaces = str.replace(/\s/g, '');
+console.log(stringWithoutSpaces);
