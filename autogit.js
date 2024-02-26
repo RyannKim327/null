9055541452cbba9863a469850d6889d12f228cdf
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:22:26 +0800

    2-26-2024 18:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,49 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-console.log(result); // Output: Hello World
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    getLength() {
+        let current = this.head;
+        let count = 0;
+
+        while (current) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+const length = linkedList.getLength();
+console.log('Length of the linked list:', length);
