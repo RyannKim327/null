b18c89a7d8111078e1e60e04109d47be277d0b7a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:26:12 +0800

    2-26-2024 18:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,49 @@
-// Define the structure for a node in the linked list
-class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
-}
+function fibonacciSearch(arr, x) {
+    let fibM2 = 0; // (m-2)'th Fibonacci No.
+    let fibM1 = 1; // (m-1)'th Fibonacci No.
+    let fibM = fibM2 + fibM1; // m'th Fibonacci No.
 
-// Define the LinkedList class
-class LinkedList {
-    constructor() {
-        this.head = null;
+    // finding the smallest Fibonacci Number greater than or equal to array length
+    while (fibM < arr.length) {
+        fibM2 = fibM1;
+        fibM1 = fibM;
+        fibM = fibM2 + fibM1;
     }
 
-    // Function to add a node to the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
+    let offset = -1;
+
+    while (fibM > 1) {
+        let i = Math.min(offset + fibM2, arr.length - 1);
+
+        if (arr[i] < x) {
+            fibM = fibM1;
+            fibM1 = fibM2;
+            fibM2 = fibM - fibM1;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibM = fibM2;
+            fibM1 -= fibM2;
+            fibM2 = fibM - fibM1;
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
+            return i;
         }
     }
 
-    // Function to find the length of the linked list
-    getLength() {
-        let current = this.head;
-        let length = 0;
-        while (current) {
-            length++;
-            current = current.next;
-        }
-        return length;
+    if (fibM1 && arr[offset + 1] == x) {
+        return offset + 1;
     }
+
+    return -1;
 }
 
-// Creating a linked list
-const list = new LinkedList();
-list.addNode(1);
-list.addNode(2);
-list.addNode(3);
+// Example Usage
+let arr = [2, 3, 5, 8, 11, 15, 20, 30, 40];
+let x = 15;
 
-// Finding the length of the linked list
-const length = list.getLength();
-console.log("Length of the linked list: ", length);
+let index = fibonacciSearch(arr, x);
+
+if (index != -1) {
+    console.log(`Element found at index: ${index}`);
+} else {
+    console.log("Element not found");
+}
