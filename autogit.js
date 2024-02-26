16389770e04816736e512f6fbd6ab4388ca663c5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:53:09 +0800

    2-26-2024 18:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,3 @@
-function beamSearch(startNode, beamWidth, maxDepth, getSuccessors, isGoal) {
-    let beam = [startNode];
-    
-    for (let depth = 0; depth < maxDepth; depth++) {
-        let newBeam = [];
-        
-        for (let node of beam) {
-            let successors = getSuccessors(node);
-            
-            for (let successor of successors) {
-                newBeam.push(successor);
-            }
-        }
-        
-        newBeam.sort((a, b) => a.cost - b.cost);
-        newBeam = newBeam.slice(0, beamWidth);
-        
-        beam = newBeam;
-        
-        let found = beam.find(node => isGoal(node));
-        if (found) {
-            return found;
-        }
-    }
-    
-    return null;
-}
+let str = "Hello, world!";
+let noSpaces = str.replace(/\s/g, "");
+console.log(noSpaces); // Output: "Hello,world!"
