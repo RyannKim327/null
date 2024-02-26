704f863101677622fc6716c2329ca2c4f3a37be8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:11:55 +0800

    2-26-2024 19:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,111 @@
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len; i++) {
-    for (var j = 0; j < len - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  enqueue(element) {
+    this.heap.push(element);
+    this.heapifyUp();
+  }
+
+  dequeue() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    const min = this.heap[0];
+    const last = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.heapifyDown();
+    }
+
+    return min;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      const element = this.heap[index];
+      const parentIndex = Math.floor((index - 1) / 2);
+      const parent = this.heap[parentIndex];
+
+      if (parent <= element) {
+        break;
       }
+
+      this.heap[parentIndex] = element;
+      this.heap[index] = parent;
+      index = parentIndex;
     }
   }
-  return arr;
+
+  heapifyDown() {
+    let index = 0;
+
+    while (index < this.heap.length) {
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+      let smallest = index;
+
+      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
+        smallest = leftChildIndex;
+      }
+
+      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
+        smallest = rightChildIndex;
+      }
+
+      if (smallest === index) {
+        break;
+      }
+
+      const temp = this.heap[index];
+      this.heap[index] = this.heap[smallest];
+      this.heap[smallest] = temp;
+
+      index = smallest;
+    }
+  }
+
+  peek() {
+    return this.heap[0];
+  }
 }
 
-// Example usage
-var array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + array);
-console.log("Sorted array: " + bubbleSort(array));
+// Priority Queue implementation using a MinHeap
+class PriorityQueue {
+  constructor() {
+    this.minHeap = new MinHeap();
+  }
+
+  enqueue(element) {
+    this.minHeap.enqueue(element);
+  }
+
+  dequeue() {
+    return this.minHeap.dequeue();
+  }
+
+  peek() {
+    return this.minHeap.peek();
+  }
+
+  isEmpty() {
+    return this.minHeap.heap.length === 0;
+  }
+}
+
+// Example usage:
+const pq = new PriorityQueue();
+pq.enqueue(3);
+pq.enqueue(2);
+pq.enqueue(5);
+pq.enqueue(1);
+
+console.log(pq.dequeue());  // Output: 1
+console.log(pq.dequeue());  // Output: 2
+console.log(pq.dequeue());  // Output: 3
+console.log(pq.dequeue());  // Output: 5
