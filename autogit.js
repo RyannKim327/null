83ed6576e58508dbf1dd09b9eee9b4c4c85e2da3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:53:16 +0800

    2-26-2024 18:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,31 @@
-let str = "Hello, world!";
-let noSpaces = str.replace(/\s/g, "");
-console.log(noSpaces); // Output: "Hello,world!"
+function shellSort(arr) {
+    let n = arr.length;
+    let gap = Math.floor(n/2);
+
+    while (gap > 0) {
+        for (let i = gap; i < n; i++) {
+            let temp = arr[i];
+            let j = i;
+
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = temp;
+        }
+
+        gap = Math.floor(gap / 2);
+    }
+
+    return arr;
+}
+
+// Usage
+let arr = [12, 34, 54, 2, 3];
+console.log("Original Array:");
+console.log(arr);
+
+console.log("\nSorted Array:");
+arr = shellSort(arr);
+console.log(arr);
