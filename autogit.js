4c79eeceb0b17fec8e9fac5bdf5358e5ad0b0429
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:39:40 +0800

    2-26-2024 18:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,30 @@
-function findMajorityElement(arr) {
-    let countMap = {};
-    let majorityElement = null;
+function longestCommonSubstring(str1, str2) {
+  let matrix = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
+  let maxLength = 0;
+  let endPos = 0;
 
-    arr.forEach((element) => {
-        countMap[element] = (countMap[element] || 0) + 1;
-        if (countMap[element] > arr.length / 2) {
-            majorityElement = element;
-            return;
+  for (let i = 1; i <= str1.length; i++) {
+    for (let j = 1; j <= str2.length; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        matrix[i][j] = matrix[i - 1][j - 1] + 1;
+        if (matrix[i][j] > maxLength) {
+          maxLength = matrix[i][j];
+          endPos = i - 1;
         }
-    });
+      } else {
+        matrix[i][j] = 0;
+      }
+    }
+  }
 
-    return majorityElement;
+  if (maxLength === 0) {
+    return '';
+  }
+
+  return str1.substring(endPos - maxLength + 1, endPos + 1);
 }
 
-// Example usage
-const arr = [2, 3, 2, 2, 4, 2, 2];
-const majorityElement = findMajorityElement(arr);
-
-if (majorityElement) {
-    console.log(`The majority element is: ${majorityElement}`);
-} else {
-    console.log("No majority element found");
-}
+// Test the function
+let str1 = 'abcdef';
+let str2 = 'zabcde';
+console.log(longestCommonSubstring(str1, str2)); // Output: 'abc'
