f2d80812760a692891247a966e566012983cc8a8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:35:00 +0800

    2-26-2024 18:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,4 @@
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-
-function maxDepth(root) {
-  if (!root) {
-    return 0;
-  }
-
-  const leftDepth = maxDepth(root.left);
-  const rightDepth = maxDepth(root.right);
-
-  return Math.max(leftDepth, rightDepth) + 1;
-}
-
-// Example usage
-const root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-
-console.log(maxDepth(root)); // Output: 3
+let str1 = "Hello";
+let str2 = "World";
+let result = str1 + " " + str2;
+console.log(result); // Output: Hello World
