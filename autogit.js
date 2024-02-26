60c504b9c65f6e4716556caead3377889afdba58
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:44:36 +0800

    2-26-2024 18:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,23 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array);  // Output: [5, 4, 3, 2, 1]
+function burrowsWheelerTransform(input) {
+    let table = [];
+    
+    // Generate all rotations of the input string
+    for (let i = 0; i < input.length; i++) {
+        let rotated = input.slice(i) + input.slice(0, i);
+        table.push(rotated);
+    }
+    
+    // Sort the rotations
+    table.sort();
+    
+    // Extract the last characters of the sorted rotations to get the transformed string
+    let transformed = table.map(rotation => rotation.slice(-1)).join('');
+    
+    return transformed;
+}
+
+// Test the implementation
+let inputString = "banana";
+let transformedString = burrowsWheelerTransform(inputString);
+console.log("Input: " + inputString);
+console.log("Transformed: " + transformedString);
