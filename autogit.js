19b192cb27c9aa2dae47a9512f6fc9854211c128
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:51:04 +0800

    2-26-2024 18:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,79 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-console.log(result); // Output: Hello World
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+
+    inOrderTraversal(node, callback) {
+        if (node !== null) {
+            this.inOrderTraversal(node.left, callback);
+            callback(node.value);
+            this.inOrderTraversal(node.right, callback);
+        }
+    }
+}
+
+// Example of using the BinaryTree class
+const binaryTree = new BinaryTree();
+binaryTree.insert(5);
+binaryTree.insert(3);
+binaryTree.insert(7);
+binaryTree.insert(2);
+binaryTree.insert(4);
+
+console.log(binaryTree.search(4)); // Output: true
+
+binaryTree.inOrderTraversal(binaryTree.root, (value) => {
+    console.log(value);
+});
