3085db4c036c35480a5a0fe7231741ecc4d390f1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:02:19 +0800

    2-26-2024 19:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,43 @@
-let str = "Hello, world!";
-let substring = "world";
+function burrowsWheelerTransform(input) {
+    // Create a list of all possible rotations of the input string
+    let rotations = [];
+    for (let i = 0; i < input.length; i++) {
+        let rotation = input.slice(i) + input.slice(0, i);
+        rotations.push(rotation);
+    }
 
-if(str.includes(substring)) {
-    console.log("Substring found in the string!");
-} else {
-    console.log("Substring not found in the string!");
-}
-let str = "Hello, world!";
-let substring = "world";
+    // Sort the list of rotations
+    rotations.sort();
 
-if(str.indexOf(substring) !== -1) {
-    console.log("Substring found in the string!");
-} else {
-    console.log("Substring not found in the string!");
+    // Extract the last characters of each rotation to create the transformed string
+    let transformedString = rotations.map(rotation => rotation[rotation.length - 1]).join('');
+
+    // Find the index of the original string in the sorted rotations to get the original index
+    let originalIndex = rotations.indexOf(input);
+
+    return { transformedString, originalIndex };
 }
+
+function inverseBurrowsWheelerTransform(transformedString, originalIndex) {
+    // Create an empty table to store the sorted characters
+    let table = transformedString.split('').sort();
+
+    // Create an empty array to store the characters of the original string
+    let originalString = new Array(transformedString.length);
+
+    // Fill in the table with the transformed string and sort it at the same time
+    for (let i = 0; i < transformedString.length; i++) {
+        originalString[i] = table[originalIndex];
+        originalIndex = transformedString.indexOf(originalString[i], table.indexOf(originalString[i]) + 1);
+    }
+
+    return originalString.join('');
+}
+
+// Test the Burrows-Wheeler Transform algorithm
+let input = "banana";
+let { transformedString, originalIndex } = burrowsWheelerTransform(input);
+console.log("Transformed string: ", transformedString);
+
+let originalString = inverseBurrowsWheelerTransform(transformedString, originalIndex);
+console.log("Original string: ", originalString);
