6adee510761f62af8bb1e701eb387900d5f52623
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:43:56 +0800

    2-26-2024 18:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,44 @@
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-
-  // Push element to the top of the stack
-  push(element) {
-    this.stack.push(element);
-  }
-
-  // Remove and return the top element of the stack
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
+function generateFibonacci(n) {
+    let fibArray = [0, 1];
+    while (fibArray[fibArray.length - 1] < n) {
+        fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
     }
-    return this.stack.pop();
-  }
-
-  // Return the top element of the stack without removing it
-  peek() {
-    return this.stack[this.stack.length - 1];
-  }
-
-  // Check if the stack is empty
-  isEmpty() {
-    return this.stack.length === 0;
-  }
-
-  // Get the size of the stack
-  size() {
-    return this.stack.length;
-  }
-
-  // Print the stack
-  printStack() {
-    console.log(this.stack);
-  }
+    return fibArray;
 }
 
-// Example usage
-const stack = new Stack();
+function fibonacciSearch(arr, x) {
+    let fibArray = generateFibonacci(arr.length);
 
-stack.push(1);
-stack.push(2);
-stack.push(3);
-stack.printStack(); // Output: [1, 2, 3]
+    let offset = 0;
+    let i = fibArray.length - 1;
 
-console.log(stack.pop()); // Output: 3
-console.log(stack.peek()); // Output: 2
-console.log(stack.size()); // Output: 2
-console.log(stack.isEmpty()); // Output: false
+    while (i > 1) {
+        let index = Math.min(offset + fibArray[i - 2], arr.length - 1);
+
+        if (arr[index] < x) {
+            i--;
+            offset = index;
+        } else if (arr[index] > x) {
+            i -= 2;
+        } else {
+            return index;
+        }
+    }
+
+    if (arr[offset] === x) {
+        return offset;
+    }
+
+    return -1;
+}
+
+// Sample usage
+let arr = [2, 3, 4, 10, 40];
+let x = 10;
+let index = fibonacciSearch(arr, x);
+
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log("Element not found");
+}
