a528c71a827f92057aa09dfb6b016448562e32a9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:31:06 +0800

    2-26-2024 18:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,56 @@
-function findMedianSortedArrays(nums1, nums2) {
-    const merged = nums1.concat(nums2).sort((a, b) => a - b);
-    const len = merged.length;
-    
-    if (len % 2 === 0) {
-        return (merged[len / 2 - 1] + merged[len / 2]) / 2;
-    } else {
-        return merged[Math.floor(len / 2)];
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
-const nums1 = [1, 3];
-const nums2 = [2];
-const median = findMedianSortedArrays(nums1, nums2);
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-console.log("Median:", median);
+    push(data) {
+        let newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    findNthNodeFromEnd(n) {
+        let mainPointer = this.head;
+        let refPointer = this.head;
+
+        // Move refPointer by n steps
+        for (let i = 0; i < n; i++) {
+            if (refPointer === null) {
+                return null; // List is shorter than n
+            }
+            refPointer = refPointer.next;
+        }
+
+        // Move both pointers until refPointer reaches the end
+        while (refPointer !== null) {
+            mainPointer = mainPointer.next;
+            refPointer = refPointer.next;
+        }
+
+        return mainPointer;
+    }
+}
+
+// Sample usage
+let ll = new LinkedList();
+ll.push(1);
+ll.push(2);
+ll.push(3);
+ll.push(4);
+ll.push(5);
+
+let n = 2; // Find the 2nd node from the end
+let nthNodeFromEnd = ll.findNthNodeFromEnd(n);
+
+if (nthNodeFromEnd !== null) {
+    console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.data}`);
+} else {
+    console.log(`List is shorter than ${n}`);
+}
