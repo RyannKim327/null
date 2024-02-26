88f6c1628652b75d5a1e7517f705840a5835ec9d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:13:07 +0800

    2-26-2024 19:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,48 @@
-function factorial(n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
+class Graph {
+    constructor(vertices) {
+        this.adjacencyList = new Map();
+        for (let v of vertices) {
+            this.adjacencyList.set(v, []);
+        }
+    }
+
+    addEdge(v1, v2) {
+        this.adjacencyList.get(v1).push(v2);
+    }
+
+    topologicalSortUtil(v, visited, stack) {
+        visited.add(v);
+        for (let neighbor of this.adjacencyList.get(v)) {
+            if (!visited.has(neighbor)) {
+                this.topologicalSortUtil(neighbor, visited, stack);
+            }
+        }
+        stack.push(v);
+    }
+
+    topologicalSort() {
+        let visited = new Set();
+        let stack = [];
+
+        for (let v of this.adjacencyList.keys()) {
+            if (!visited.has(v)) {
+                this.topologicalSortUtil(v, visited, stack);
+            }
+        }
+
+        return stack.reverse();
     }
 }
 
-// Test the factorial function
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is: ${result}`);
+// Example usage
+let vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
+let g = new Graph(vertices);
+g.addEdge('A', 'C');
+g.addEdge('C', 'E');
+g.addEdge('E', 'F');
+g.addEdge('B', 'D');
+g.addEdge('B', 'E');
+g.addEdge('D', 'F');
+
+let result = g.topologicalSort();
+console.log(result);
