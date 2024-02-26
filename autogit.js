0955ff1cf94e05e0882ec519e33887e901e7c8f3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:05:24 +0800

    2-26-2024 19:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,79 @@
-function selectionSort(arr) {
-    const n = arr.length;
-    
-    for(let i = 0; i < n - 1; i++) {
-        let minIndex = i;
-        for(let j = i+1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        
-        if(minIndex !== i) {
-            const temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-    
-    return arr;
 }
 
-// Example usage
-const array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original Array: ", array);
-console.log("Sorted Array: ", selectionSort(array));
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        let newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(node, data) {
+        if (node === null) {
+            return false;
+        } else if (data < node.data) {
+            return this.search(node.left, data);
+        } else if (data > node.data) {
+            return this.search(node.right, data);
+        } else {
+            return true;
+        }
+    }
+
+    inorder(node) {
+        if (node !== null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+}
+
+// Example Usage
+let bst = new BinarySearchTree();
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+console.log('Inorder traversal');
+bst.inorder(bst.getRootNode());
+
+console.log('Search for 40:', bst.search(bst.getRootNode(), 40)); // true
+console.log('Search for 100:', bst.search(bst.getRootNode(), 100)); // false
