9e67c37a21687f2a8d2a423db95f9144fec0d1a2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:55:09 +0800

    2-26-2024 18:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,62 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-    
-    return count;
 }
 
-// Usage example
-const str = "Hello, world!";
-const char = "o";
-const occurrences = countOccurrences(str, char);
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-console.log(`The character '${char}' occurs ${occurrences} times in the string.`);
+    addNode(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        let temp = null;
+
+        while (current !== null) {
+            temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        this.head = prev;
+    }
+
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.value);
+            current = current.next;
+        }
+    }
+}
+
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+
+console.log("Original Linked List:");
+linkedList.printList();
+
+linkedList.reverse();
+
+console.log("Reversed Linked List:");
+linkedList.printList();
