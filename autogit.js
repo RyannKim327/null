2f16660ca7754136a3954ddac55f7831cf8ef03c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:42:15 +0800

    2-26-2024 18:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,20 @@
-let str = "Hello, World! This is a test string.";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces); // Output: "Hello,World!Thisisateststring."
+function isAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+}
+
+// Test the function
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
