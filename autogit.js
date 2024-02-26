190f58beb5fae92d8ab3ed93adf070c1e07fb879
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:45:27 +0800

    2-26-2024 18:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,4 @@
-function burrowsWheelerTransform(input) {
-    var table = [];
-    
-    // Generate all rotations of the input string
-    for (var i = 0; i < input.length; i++) {
-        var rotated = input.slice(i) + input.slice(0, i);
-        table.push(rotated);
-    }
-    
-    // Sort the rotations
-    table.sort();
-    
-    // Extract the last character of each rotation to get the transformed string
-    var transformed = '';
-    for (var i = 0; i < table.length; i++) {
-        transformed += table[i][input.length - 1];
-    }
-    
-    return transformed;
-}
+let array = [1, 2, 3, 4, 3, 2, 1];
+let uniqueArray = Array.from(new Set(array));
 
-function inverseBurrowsWheelerTransform(input) {
-    var table = [];
-    
-    // Generate all rotations of the transformed string
-    for (var i = 0; i < input.length; i++) {
-        table.push(input);
-        input = input.slice(1) + input.slice(0, 1);
-    }
-    
-    // Sort the rotations
-    table.sort();
-    
-    // Find the original string
-    var original = table.find(rotation => rotation.endsWith('$'));
-    return original.slice(0, -1); // Remove the special character at the end
-}
-
-// Example
-var originalString = "banana$";
-var transformedString = burrowsWheelerTransform(originalString);
-var reversedString = inverseBurrowsWheelerTransform(transformedString);
-
-console.log("Original String: " + originalString);
-console.log("Transformed String: " + transformedString);
-console.log("Reversed String: " + reversedString);
+console.log(uniqueArray); // Output: [1, 2, 3, 4]
