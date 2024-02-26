84db617302701b1efc6514ecea5510c820bf94fb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:35:38 +0800

    2-26-2024 18:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,3 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = current;
-    }
-    return arr;
-}
-
-// Example usage
-let arr = [5, 2, 4, 6, 1, 3];
-console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5, 6]
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
+console.log(lowercaseStr); // Output: hello world
