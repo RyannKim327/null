466cc002a72b7c8005da92739a46ca80aedf5777
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:12:07 +0800

    2-26-2024 19:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,111 +1,46 @@
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  enqueue(element) {
-    this.heap.push(element);
-    this.heapifyUp();
-  }
-
-  dequeue() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-
-    const min = this.heap[0];
-    const last = this.heap.pop();
-
-    if (this.heap.length > 0) {
-      this.heap[0] = last;
-      this.heapifyDown();
-    }
-
-    return min;
-  }
-
-  heapifyUp() {
-    let index = this.heap.length - 1;
-
-    while (index > 0) {
-      const element = this.heap[index];
-      const parentIndex = Math.floor((index - 1) / 2);
-      const parent = this.heap[parentIndex];
-
-      if (parent <= element) {
-        break;
-      }
-
-      this.heap[parentIndex] = element;
-      this.heap[index] = parent;
-      index = parentIndex;
-    }
-  }
-
-  heapifyDown() {
-    let index = 0;
-
-    while (index < this.heap.length) {
-      const leftChildIndex = 2 * index + 1;
-      const rightChildIndex = 2 * index + 2;
-      let smallest = index;
-
-      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
-        smallest = leftChildIndex;
-      }
-
-      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
-        smallest = rightChildIndex;
-      }
-
-      if (smallest === index) {
-        break;
-      }
-
-      const temp = this.heap[index];
-      this.heap[index] = this.heap[smallest];
-      this.heap[smallest] = temp;
-
-      index = smallest;
-    }
-  }
-
-  peek() {
-    return this.heap[0];
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
   }
 }
 
-// Priority Queue implementation using a MinHeap
-class PriorityQueue {
-  constructor() {
-    this.minHeap = new MinHeap();
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  let next = null;
+
+  while (current !== null) {
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
   }
 
-  enqueue(element) {
-    this.minHeap.enqueue(element);
-  }
-
-  dequeue() {
-    return this.minHeap.dequeue();
-  }
-
-  peek() {
-    return this.minHeap.peek();
-  }
-
-  isEmpty() {
-    return this.minHeap.heap.length === 0;
-  }
+  head = prev; // Update the head to be the last element after reversal
+  return head;
 }
 
-// Example usage:
-const pq = new PriorityQueue();
-pq.enqueue(3);
-pq.enqueue(2);
-pq.enqueue(5);
-pq.enqueue(1);
+// Test the reverseLinkedList function
+// Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+let node5 = new Node(5);
+let node4 = new Node(4, node5);
+let node3 = new Node(3, node4);
+let node2 = new Node(2, node3);
+let node1 = new Node(1, node2);
 
-console.log(pq.dequeue());  // Output: 1
-console.log(pq.dequeue());  // Output: 2
-console.log(pq.dequeue());  // Output: 3
-console.log(pq.dequeue());  // Output: 5
+console.log("Original linked list:");
+let current = node1;
+while (current !== null) {
+  console.log(current.data);
+  current = current.next;
+}
+
+let reversedHead = reverseLinkedList(node1);
+
+console.log("\nReversed linked list:");
+current = reversedHead;
+while (current !== null) {
+  console.log(current.data);
+  current = current.next;
+}
