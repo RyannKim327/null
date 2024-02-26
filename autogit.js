05aa638db8b1b6281609d8e1c015bb031dedfe8f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:25:51 +0800

    2-26-2024 18:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,49 @@
-function createBadMatchTable(pattern) {
-    const table = {};
-    const patternLength = pattern.length;
-
-    for (let i = 0; i < patternLength - 1; i++) {
-        table[pattern[i]] = patternLength - i - 1;
+// Define the structure for a node in the linked list
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
     }
-
-    return table;
 }
 
-function boyerMooreHorspool(text, pattern) {
-    const badMatchTable = createBadMatchTable(pattern);
-    const textLength = text.length;
-    const patternLength = pattern.length;
-    
-    let i = patternLength - 1;
+// Define the LinkedList class
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-    while (i < textLength) {
-        let j = patternLength - 1;
-        let k = i;
-
-        while (j >= 0 && text[k] === pattern[j]) {
-            k--;
-            j--;
-        }
-
-        if (j === -1) {
-            return k + 1; // Match found
+    // Function to add a node to the linked list
+    addNode(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
         } else {
-            const badMatchOffset = badMatchTable[text[i]] || patternLength;
-            i += badMatchOffset;
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
     }
 
-    return -1; // No match found
+    // Function to find the length of the linked list
+    getLength() {
+        let current = this.head;
+        let length = 0;
+        while (current) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
 }
 
-// Example Usage
-const text = "This is a sample text for searching.";
-const pattern = "sample";
-const index = boyerMooreHorspool(text, pattern);
+// Creating a linked list
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
 
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+// Finding the length of the linked list
+const length = list.getLength();
+console.log("Length of the linked list: ", length);
