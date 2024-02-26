8ffab01ce447628c963dbea22c37fab74c7e7339
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:32:19 +0800

    2-26-2024 18:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,51 @@
-class SuffixTreeNode {
-    constructor() {
-        this.children = {};
+function mergeSortIterative(arr) {
+  const n = arr.length;
+  
+  for (let currSize = 1; currSize <= n - 1; currSize = 2 * currSize) {
+    for (let leftStart = 0; leftStart < n - 1; leftStart += 2 * currSize) {
+      const mid = Math.min(leftStart + currSize - 1, n - 1);
+      const rightEnd = Math.min(leftStart + 2 * currSize - 1, n - 1);
+
+      merge(arr, leftStart, mid, rightEnd);
     }
+  }
+
+  return arr;
 }
 
-class SuffixTree {
-    constructor(inputString) {
-        this.root = new SuffixTreeNode();
-        for (let i = 0; i < inputString.length; i++) {
-            this.addSuffix(inputString.substring(i), i);
-        }
-    }
+function merge(arr, left, mid, right) {
+  const n1 = mid - left + 1;
+  const n2 = right - mid;
 
-    addSuffix(suffix, index) {
-        let currentNode = this.root;
-        for (let i = 0; i < suffix.length; i++) {
-            let char = suffix[i];
-            if (!currentNode.children[char]) {
-                currentNode.children[char] = new SuffixTreeNode();
-            }
-            currentNode = currentNode.children[char];
-        }
-        // Store the index of the suffix if needed
-        // currentNode.index = index;
+  const leftArr = arr.slice(left, left + n1);
+  const rightArr = arr.slice(mid + 1, mid + 1 + n2);
+
+  let i = 0, j = 0, k = left;
+
+  while (i < n1 && j < n2) {
+    if (leftArr[i] <= rightArr[j]) {
+      arr[k] = leftArr[i];
+      i++;
+    } else {
+      arr[k] = rightArr[j];
+      j++;
     }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = leftArr[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = rightArr[j];
+    j++;
+    k++;
+  }
 }
 
-// Example usage
-const suffixTree = new SuffixTree("banana");
-console.log(suffixTree);
+// Test the function
+const arr = [38, 27, 43, 3, 9, 82, 10];
+console.log(mergeSortIterative(arr)); // Output: [3, 9, 10, 27, 38, 43, 82]
