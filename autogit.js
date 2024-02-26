c8efebefe930519d2c563a5c2a63ea64bf0ba704
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:44:43 +0800

    2-26-2024 18:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,8 @@
-function burrowsWheelerTransform(input) {
-    let table = [];
-    
-    // Generate all rotations of the input string
-    for (let i = 0; i < input.length; i++) {
-        let rotated = input.slice(i) + input.slice(0, i);
-        table.push(rotated);
-    }
-    
-    // Sort the rotations
-    table.sort();
-    
-    // Extract the last characters of the sorted rotations to get the transformed string
-    let transformed = table.map(rotation => rotation.slice(-1)).join('');
-    
-    return transformed;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Test the implementation
-let inputString = "banana";
-let transformedString = burrowsWheelerTransform(inputString);
-console.log("Input: " + inputString);
-console.log("Transformed: " + transformedString);
+let inputString = "Hello, World!";
+let result = removeVowels(inputString);
+
+console.log(result); // Output: "Hll, Wrld!"
