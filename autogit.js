a7654338a74c208c6fbbebf1c0d6b8b1f7ef5906
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:48:55 +0800

    2-26-2024 18:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,9 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert the string to lowercase
-    const cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
-    
-    // Compare the clean string with its reverse
-    return cleanStr === cleanStr.split('').reverse().join('');
-}
+const axios = require('axios');
 
-// Example usage
-const inputString = "A man, a plan, a canal, Panama";
-if (isPalindrome(inputString)) {
-    console.log("The input string is a palindrome.");
-} else {
-    console.log("The input string is not a palindrome.");
-}
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log('Response data:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
