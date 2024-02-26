ac7a58b217b9dccbc0ea9d533cccbec763bbf935
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:08:47 +0800

    2-26-2024 19:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,76 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
     }
 
-    return -1; // Return -1 if target is not found in the array
+    enqueue(data) {
+        const newNode = new Node(data);
+
+        if (!this.front) {
+            this.front = newNode;
+            this.rear = newNode;
+        } else {
+            this.rear.next = newNode;
+            this.rear = newNode;
+        }
+
+        this.size++;
+    }
+
+    dequeue() {
+        if (!this.front) return null;
+
+        const data = this.front.data;
+
+        if (this.front === this.rear) {
+            this.front = null;
+            this.rear = null;
+        } else {
+            this.front = this.front.next;
+        }
+
+        this.size--;
+
+        return data;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    printQueue() {
+        let current = this.front;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
 
 // Example usage
-const arr = [1, 3, 5, 7, 9, 11, 13, 15, 17];
-const target = 9;
-const index = binarySearch(arr, target);
+const queue = new Queue();
 
-if (index !== -1) {
-    console.log(`Target ${target} found at index ${index}`);
-} else {
-    console.log(`Target ${target} not found in the array`);
-}
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log("Dequeuing...", queue.dequeue());
+console.log("Dequeuing...", queue.dequeue());
+
+console.log("Queue size:", queue.getSize());
+console.log("Is queue empty?", queue.isEmpty());
+
+queue.printQueue();
