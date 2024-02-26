b157a4bcabd1e0290da7262bb66a2bbdddd836ec
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:30:53 +0800

    2-26-2024 18:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,16 @@
-function isPalindrome(str) {
-    let left = 0;
-    let right = str.length - 1;
-
-    while (left < right) {
-        if (str[left] !== str[right]) {
-            return false;
-        }
-        left++;
-        right--;
+function findMedianSortedArrays(nums1, nums2) {
+    const merged = nums1.concat(nums2).sort((a, b) => a - b);
+    const len = merged.length;
+    
+    if (len % 2 === 0) {
+        return (merged[len / 2 - 1] + merged[len / 2]) / 2;
+    } else {
+        return merged[Math.floor(len / 2)];
     }
-
-    return true;
 }
 
-let input = "madam";
-if (isPalindrome(input)) {
-    console.log(input + " is a palindrome.");
-} else {
-    console.log(input + " is not a palindrome.");
-}
+const nums1 = [1, 3];
+const nums2 = [2];
+const median = findMedianSortedArrays(nums1, nums2);
+
+console.log("Median:", median);
