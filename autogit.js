622820ce7eef9e13bf13621f579754aa26e447f6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:00:08 +0800

    2-26-2024 18:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,30 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+function findIntersection(headA, headB) {
+    let hash = new Set();
+    
+    let currA = headA;
+    while (currA) {
+        hash.add(currA);
+        currA = currA.next;
+    }
+    
+    let currB = headB;
+    while (currB) {
+        if (hash.has(currB)) {
+            return currB;
+        }
+        currB = currB.next;
+    }
+    
+    return null;
+}
+function findIntersection(headA, headB) {
+    let currA = headA;
+    let currB = headB;
+    
+    while (currA !== currB) {
+        currA = currA ? currA.next : headB;
+        currB = currB ? currB.next : headA;
+    }
+    
+    return currA;
+}
