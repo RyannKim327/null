186ced47bbb98f9ae6dd64eca08fb45c6a22f492
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:52:14 +0800

    2-26-2024 18:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,7 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+let numbers = [5, 2, 8, 1, 4];
 
-function isPalindrome(head) {
-  let values = [];
-  let current = head;
-  
-  // Push node values into an array
-  while (current !== null) {
-    values.push(current.value);
-    current = current.next;
-  }
-  
-  // Check if the values form a palindrome
-  let start = 0;
-  let end = values.length - 1;
-  
-  while (start < end) {
-    if (values[start] !== values[end]) {
-      return false;
-    }
-    start++;
-    end--;
-  }
-  
-  return true;
-}
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-// Test the function
-let a = new Node('r');
-let b = new Node('a');
-let c = new Node('c');
-let d = new Node('e');
-let e = new Node('c');
-let f = new Node('a');
-let g = new Node('r');
-
-a.next = b;
-b.next = c;
-c.next = d;
-d.next = e;
-e.next = f;
-f.next = g;
-
-console.log(isPalindrome(a)); // Output: true
+console.log(numbers); // Output: [1, 2, 4, 5, 8]
