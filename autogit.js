b730a6ed50d7b404affda086c0a64e5959aefd1f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:01:36 +0800

    2-26-2024 19:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,5 @@
-function horspoolSearch(text, pattern) {
-    const skipTable = createSkipTable(pattern);
-    let i = 0;
+let array = [5, 12, 34, 8, 10];
 
-    while (i <= text.length - pattern.length) {
-        let j = pattern.length - 1;
+let max = Math.max(...array);
 
-        while (j >= 0 && pattern[j] === text[i + j]) {
-            j--;
-        }
-
-        if (j < 0) {
-            return i;
-        } else {
-            const skip = skipTable[text[i + pattern.length - 1]] || pattern.length;
-            i += skip;
-        }
-    }
-
-    return -1;
-}
-
-function createSkipTable(pattern) {
-    const table = {};
-    const patternLength = pattern.length;
-
-    for (let i = 0; i < patternLength - 1; i++) {
-        table[pattern[i]] = patternLength - 1 - i;
-    }
-
-    return table;
-}
-
-// Example usage
-const text = "The quick brown fox jumps over the lazy dog";
-const pattern = "fox";
-const index = horspoolSearch(text, pattern);
-
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+console.log("The maximum value in the array is: " + max);
