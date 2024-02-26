829bc47410b0874e924dce3c4cef5caad369983a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:28:23 +0800

    2-26-2024 18:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,4 @@
-function findMedianSortedArrays(nums1, nums2) {
-    const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
-    const length = mergedArray.length;
-
-    if (length % 2 !== 0) {
-        return mergedArray[Math.floor(length / 2)];
-    } else {
-        const mid = length / 2;
-        return (mergedArray[mid - 1] + mergedArray[mid]) / 2;
-    }
-}
-
-// Example usage
-const nums1 = [1, 3];
-const nums2 = [2];
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
