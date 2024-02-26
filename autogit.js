cc9d5400edffc7031ca30e3b5496d93e7a2f2391
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:28:07 +0800

    2-26-2024 18:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,25 @@
-function decimalToBinary(decimal) {
-    return decimal.toString(2);
+
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false; // If characters at left and right index don't match, it's not a palindrome
+        }
+
+        left++;
+        right--;
+    }
+    
+    return true;
 }
 
-// Example Usage
-const decimalNumber = 10;
-const binaryNumber = decimalToBinary(decimalNumber);
-console.log(binaryNumber); // Output: 1010
+// Test the function
+const inputString = "racecar";
+if (isPalindrome(inputString)) {
+    console.log(`${inputString} is a palindrome.`);
+} else {
+    console.log(`${inputString} is not a palindrome.`);
+}
+
