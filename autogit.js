6b91b8df1877fb10c8575869f945604473b2b6cc
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:59:40 +0800

    2-26-2024 18:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,3 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert the string to lowercase
-    const alphanumericStr = str.toLowerCase().replace(/[\W_]/g, '');
-
-    // Reverse the string
-    const reversedStr = alphanumericStr.split('').reverse().join('');
-
-    // Check if the original string is equal to its reverse
-    return alphanumericStr === reversedStr;
-}
-
-// Test the function
-const str1 = "A man, a plan, a canal, Panama";
-console.log(isPalindrome(str1)); // Output: true
-
-const str2 = "Hello, world!";
-console.log(isPalindrome(str2)); // Output: false
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // Output: [5, 4, 3, 2, 1]
