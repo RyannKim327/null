57323ef3e2934aa7870f41fa8fc85814f24564bb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:37:56 +0800

    2-26-2024 18:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,24 @@
 function quicksort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const pivot = arr[0];
-  const left = [];
-  const right = [];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+    if (arr.length <= 1) {
+        return arr;
     }
-  }
 
-  return [...quicksort(left), pivot, ...quicksort(right)];
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quicksort(left), pivot, ...quicksort(right)];
 }
 
-// Example usage
-const array = [3, 6, 8, 10, 1, 2, 1];
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
 const sortedArray = quicksort(array);
+
 console.log(sortedArray);
