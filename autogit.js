fd1facab2f88515dff2192b57b76f700b3ecd378
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:32:53 +0800

    2-26-2024 18:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,91 +1,21 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
+function isPalindrome(str) {
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i < j) {
+    if (str[i] !== str[j]) {
+      return false;
     }
+    i++;
+    j--;
+  }
 
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-
-    getLeftChildIndex(index) {
-        return 2 * index + 1;
-    }
-
-    getRightChildIndex(index) {
-        return 2 * index + 2;
-    }
-
-    swap(index1, index2) {
-        let temp = this.heap[index1];
-        this.heap[index1] = this.heap[index2];
-        this.heap[index2] = temp;
-    }
-
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp(this.heap.length - 1);
-    }
-
-    heapifyUp(index) {
-        let parentIndex = this.getParentIndex(index);
-        while (index > 0 && this.heap[index] < this.heap[parentIndex]) {
-            this.swap(index, parentIndex);
-            index = parentIndex;
-            parentIndex = this.getParentIndex(index);
-        }
-    }
-
-    extractMin() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-        let minValue = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown(0);
-        return minValue;
-    }
-
-    heapifyDown(index) {
-        let leftChildIndex = this.getLeftChildIndex(index);
-        let rightChildIndex = this.getRightChildIndex(index);
-        let smallest = index;
-
-        if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallest]) {
-            smallest = leftChildIndex;
-        }
-
-        if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallest]) {
-            smallest = rightChildIndex;
-        }
-
-        if (smallest !== index) {
-            this.swap(index, smallest);
-            this.heapifyDown(smallest);
-        }
-    }
-
-    peek() {
-        return this.heap[0];
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    isEmpty() {
-        return this.heap.length === 0;
-    }
+  return true;
 }
 
-// Example usage:
-const pq = new PriorityQueue();
-pq.insert(5);
-pq.insert(2);
-pq.insert(8);
-pq.insert(1);
-
-console.log(pq.extractMin()); // Output: 1
-console.log(pq.extractMin()); // Output: 2
+const str = "madam";
+if (isPalindrome(str)) {
+  console.log("The string is a palindrome.");
+} else {
+  console.log("The string is not a palindrome.");
+}
