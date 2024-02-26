2fdad6e2b639602919d8d78cf997a1f7f8b6927d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:37:05 +0800

    2-26-2024 18:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,47 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentElement = arr[i];
-        let j = i - 1;
-
-        while (j >= 0 && arr[j] > currentElement) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = currentElement;
+function fibonacciSearch(arr, value) {
+    let fib2 = 0;
+    let fib1 = 1;
+    let fib = fib1 + fib2;
+    
+    while (fib < arr.length) {
+        fib2 = fib1;
+        fib1 = fib;
+        fib = fib1 + fib2;
     }
-
-    return arr;
+    
+    let offset = -1;
+    
+    while (fib > 1) {
+        let i = Math.min(offset + fib2, arr.length - 1);
+        
+        if (arr[i] < value) {
+            fib = fib1;
+            fib1 = fib2;
+            fib2 = fib - fib1;
+            offset = i;
+        } else if (arr[i] > value) {
+            fib = fib2;
+            fib1 -= fib2;
+            fib2 = fib - fib1;
+        } else {
+            return i;
+        }
+    }
+    
+    if (fib1 && arr[offset + 1] === value) {
+        return offset + 1;
+    }
+    
+    return -1;
 }
 
 // Example usage
-const unsortedArray = [5, 2, 4, 6, 1, 3];
-const sortedArray = insertionSort(unsortedArray);
-console.log(sortedArray);
+const arr = [1, 2, 3, 5, 8, 13, 21, 34, 55];
+const value = 13;
+const index = fibonacciSearch(arr, value);
+
+if (index !== -1) {
+    console.log(`Found ${value} at index ${index}`);
+} else {
+    console.log(`${value} not found in the array`);
+}
