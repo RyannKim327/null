f0f630c1a4ce39d73e0973aec14fb4b9f5d24cec
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:13:32 +0800

    2-26-2024 19:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,8 @@
-function longestCommonPrefix(strings) {
-    if (strings.length === 0) return "";
-
-    for (let i = 0; i < strings[0].length; i++) {
-        for (let j = 1; j < strings.length; j++) {
-            if (strings[j][i] !== strings[0][i]) {
-                return strings[0].substring(0, i);
-            }
-        }
-    }
-
-    return strings[0];
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 // Example usage
-const strings = ["flower", "flight", "flour"];
-console.log(longestCommonPrefix(strings)); // Output: "fl"
+const originalString = "hello";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "olleh"
