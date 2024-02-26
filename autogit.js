845636b27c0f7938250ea8ffa6c33a772ed16ed6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:13:41 +0800

    2-26-2024 19:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,42 @@
-function reverseString(str) {
-  return str.split('').reverse().join('');
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num <= 3) return true;
+  if (num % 2 === 0 || num % 3 === 0) return false;
+  
+  let i = 5;
+  while (i * i <= num) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+    i += 6;
+  }
+  
+  return true;
+}
+
+function largestPrimeFactor(num) {
+  let maxPrime = 1;
+  
+  while (num % 2 === 0) {
+    maxPrime = 2;
+    num = num / 2;
+  }
+  
+  for (let i = 3; i <= Math.sqrt(num); i += 2) {
+    while (num % i === 0) {
+      if (isPrime(i)) {
+        maxPrime = i;
+      }
+      num = num / i;
+    }
+  }
+  
+  if (num > 2 && isPrime(num)) {
+    maxPrime = num;
+  }
+  
+  return maxPrime;
 }
 
 // Example usage
-const originalString = "hello";
-const reversedString = reverseString(originalString);
-console.log(reversedString); // Output: "olleh"
+const number = 84;
+const largestPrime = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is: ${largestPrime}`);
