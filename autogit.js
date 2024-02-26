92aaa8d1409a6646f290b93b494dd0081f9587e1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:56:37 +0800

    2-26-2024 18:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,8 @@
-function maxSubArray(arr) {
-    if (!arr.length) {
-        return 0;
-    }
-
-    let maxSum = arr[0];
-    let currentSum = arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        currentSum = Math.max(arr[i], currentSum + arr[i]);
-        maxSum = Math.max(maxSum, currentSum);
-    }
-
-    return maxSum;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Example
-const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-const result = maxSubArray(array);
-console.log(result); // Output: 6 (the subarray [4, -1, 2, 1] has the maximum sum of 6)
+let inputString = "Hello, World!";
+let outputString = removeVowels(inputString);
+
+console.log(outputString); // Output: "Hll, Wrld!"
