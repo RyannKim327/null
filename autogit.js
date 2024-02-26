f567e2c21e0ffbce7885dbd449ac6dc1d8771d4e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:51:27 +0800

    2-26-2024 18:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,8 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    add(data) {
-        const newNode = new Node(data);
-
-        if (this.head === null) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next !== null) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    getLength() {
-        let current = this.head;
-        let count = 0;
-
-        while (current !== null) {
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
-}
-
-// Creating a linked list
-const list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(3);
-list.add(4);
-
-// Finding the length of the linked list
-const length = list.getLength();
-console.log('Length of the linked list:', length);
+fetch('https://api.quotable.io/random')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data.content);
+  })
+  .catch(error => {
+    console.error('An error occurred', error);
+  });
