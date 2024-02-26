25df7f442375b89ec48181c097cee4d281817608
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:06:21 +0800

    2-26-2024 19:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,74 +1,52 @@
-class PriorityQueue {
-  constructor() {
-    this.heap = [];
-  }
+function buildPrefixTable(pattern) {
+    const prefixTable = new Array(pattern.length);
+    prefixTable[0] = 0;
+    let prefixLength = 0;
+    let i = 1;
 
-  enqueue(value, priority) {
-    const node = { value, priority };
-    this.heap.push(node);
-    this.bubbleUp();
-  }
-
-  dequeue() {
-    const min = this.heap[0];
-    const end = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = end;
-      this.sinkDown();
-    }
-    return min;
-  }
-
-  bubbleUp() {
-    let index = this.heap.length - 1;
-    const node = this.heap[index];
-    while (index > 0) {
-      let parentIndex = Math.floor((index - 1) / 2);
-      let parent = this.heap[parentIndex];
-      if (node.priority >= parent.priority) break;
-      this.heap[parentIndex] = node;
-      this.heap[index] = parent;
-      index = parentIndex;
-    }
-  }
-
-  sinkDown() {
-    let index = 0;
-    const length = this.heap.length;
-    const node = this.heap[0];
-    while (true) {
-      let leftChildIndex = 2 * index + 1;
-      let rightChildIndex = 2 * index + 2;
-      let leftChild, rightChild;
-      let swap = null;
-
-      if (leftChildIndex < length) {
-        leftChild = this.heap[leftChildIndex];
-        if (leftChild.priority < node.priority) {
-          swap = leftChildIndex;
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[prefixLength]) {
+            prefixLength++;
+            prefixTable[i] = prefixLength;
+            i++;
+        } else {
+            if (prefixLength !== 0) {
+                prefixLength = prefixTable[prefixLength - 1];
+            } else {
+                prefixTable[i] = 0;
+                i++;
+            }
         }
-      }
-
-      if (rightChildIndex < length) {
-        rightChild = this.heap[rightChildIndex];
-        if ((swap === null && rightChild.priority < node.priority) ||
-          (swap !== null && rightChild.priority < leftChild.priority)) {
-          swap = rightChildIndex;
-        }
-      }
-
-      if (swap === null) break;
-      this.heap[index] = this.heap[swap];
-      this.heap[swap] = node;
-      index = swap;
     }
-  }
+
+    return prefixTable;
 }
-const pq = new PriorityQueue();
-pq.enqueue('task 1', 3);
-pq.enqueue('task 2', 1);
-pq.enqueue('task 3', 2);
 
-console.log(pq.dequeue()); // { value: 'task 2', priority: 1 }
-console.log(pq.dequeue()); // { value: 'task 3', priority: 2 }
-console.log(pq.dequeue()); // { value: 'task 1', priority: 3 }
+function kmpSearch(text, pattern) {
+    const prefixTable = buildPrefixTable(pattern);
+    let textIndex = 0;
+    let patternIndex = 0;
+
+    while (textIndex < text.length) {
+        if (pattern[patternIndex] === text[textIndex]) {
+            patternIndex++;
+            textIndex++;
+        }
+
+        if (patternIndex === pattern.length) {
+            console.log('Pattern found at index ' + (textIndex - patternIndex));
+            patternIndex = prefixTable[patternIndex - 1];
+        } else if (textIndex < text.length && pattern[patternIndex] !== text[textIndex]) {
+            if (patternIndex !== 0) {
+                patternIndex = prefixTable[patternIndex - 1];
+            } else {
+                textIndex++;
+            }
+        }
+    }
+}
+
+// Test the KMP algorithm
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+kmpSearch(text, pattern);
