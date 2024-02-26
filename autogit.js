5d40e399270584facb542ca14393a6493d66bfd8
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 18:38:04 +0800

    2-26-2024 18:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,22 @@
 function quicksort(arr) {
     if (arr.length <= 1) {
         return arr;
-    }
-
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+    } else {
+        const pivot = arr[0];
+        const left = [];
+        const right = [];
+        
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
         }
+        
+        return quicksort(left).concat(pivot, quicksort(right));
     }
-
-    return [...quicksort(left), pivot, ...quicksort(right)];
 }
 
-const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-const sortedArray = quicksort(array);
-
-console.log(sortedArray);
+const arr = [3, 6, 8, 10, 1, 2, 1];
+console.log(quicksort(arr)); // Output: [1, 1, 2, 3, 6, 8, 10]
