a5b50e6aa0257736a1a9f7a357708024fa544063
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:07:32 +0800

    2-26-2024 19:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,47 @@
-let str = "Hello, world! This is a test string.";
-let stringWithoutSpaces = str.replace(/\s/g, "");
-console.log(stringWithoutSpaces);
+function fibonacciSearch(arr, key) {
+    let fibM2 = 0;
+    let fibM1 = 1;
+    let fib = fibM2 + fibM1;
+
+    while (fib < arr.length) {
+        fibM2 = fibM1;
+        fibM1 = fib;
+        fib = fibM2 + fibM1;
+    }
+
+    let offset = -1;
+
+    while (fib > 1) {
+        let i = Math.min(offset + fibM2, arr.length - 1);
+
+        if (arr[i] < key) {
+            fib = fibM1;
+            fibM1 = fibM2;
+            fibM2 = fib - fibM1;
+            offset = i;
+        } else if (arr[i] > key) {
+            fib = fibM2;
+            fibM1 = fibM1 - fibM2;
+            fibM2 = fib - fibM1;
+        } else {
+            return i;
+        }
+    }
+
+    if (fibM1 && arr[offset + 1] === key) {
+        return offset + 1;
+    }
+
+    return -1;
+}
+
+// Usage
+const arr = [2, 3, 4, 10, 40];
+const key = 10;
+const index = fibonacciSearch(arr, key);
+
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log("Element not found");
+}
