6a8863be8289a41922607e1317d84d3ab0043242
RyannKim327 <rksesgundo123@gmail.com>
2024-02-26 19:11:09 +0800

    2-26-2024 19:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,20 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
+function bubbleSort(arr) {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap the elements
+                const temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(data) {
-    const newNode = new Node(data);
-
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.data < node.data) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  search(data) {
-    return this.searchNode(this.root, data);
-  }
-
-  searchNode(node, data) {
-    if (node === null) {
-      return false;
-    }
-
-    if (data < node.data) {
-      return this.searchNode(node.left, data);
-    } else if (data > node.data) {
-      return this.searchNode(node.right, data);
-    } else {
-      return true;
-    }
-  }
-}
-
-// Usage
-const bst = new BinarySearchTree();
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
-bst.insert(3);
-bst.insert(7);
-
-console.log(bst.search(5)); // Output: true
-console.log(bst.search(12)); // Output: false
+// Example usage
+const array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + array);
+const sortedArray = bubbleSort(array);
+console.log("Sorted array: " + sortedArray);
