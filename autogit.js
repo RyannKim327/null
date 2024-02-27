cc08adb5b226bcaaa87c8658e37fd7d339e31513
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:06:36 +0800

    2-28-2024 6:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,20 @@
-area = (base * height) / 2
-function calculateTriangleArea(base, height) {
-    return (base * height) / 2;
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
 }
 
 // Example usage
-let base = 5;
-let height = 8;
-let area = calculateTriangleArea(base, height);
-
-console.log("The area of the triangle is: " + area);
+let array = [12, 11, 13, 5, 6];
+insertionSort(array);
+console.log(array);
