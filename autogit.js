9b5755393d433178f194f07aa265230004db24d3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:10:15 +0800

    2-28-2024 6:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,3 @@
-function quicksort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  } else {
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < pivot) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i]);
-      }
-    }
-
-    return quicksort(left).concat(pivot, quicksort(right));
-  }
-}
-
-const unsortedArray = [5, 2, 9, 3, 7, 6, 1, 8, 4];
-const sortedArray = quicksort(unsortedArray);
-
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let str = "  Hello   World  ";
+let newStr = str.replace(/\s/g, ''); // The \s matches any whitespace character, and the 'g' flag makes the replacement global
+console.log(newStr); // Output: "HelloWorld"
