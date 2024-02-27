8639bdb84cd8d2fc07557d8cfffe8485c391f817
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:25:21 +0800

    2-28-2024 6:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,2 @@
-function isArraySorted(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-// Example usage
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [1, 3, 2, 4, 5];
-
-console.log(isArraySorted(arr1)); // Output: true
-console.log(isArraySorted(arr2)); // Output: false
+const userInput = prompt("Enter something:");
+console.log("You entered: " + userInput);
