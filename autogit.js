f4f776a12301568cdba0ed99a6a3190c2275db72
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:10:43 +0800

    2-28-2024 6:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,52 @@
-function largestPrimeFactor(number) {
-    let factors = [];
-    let i = 2;
-    
-    while (i <= number) {
-        if (number % i === 0) {
-            factors.push(i);
-            number /= i;
-        } else {
-            i++;
-        }
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  // Push element to the top of the stack
+  push(element) {
+    this.items.push(element);
+  }
+
+  // Pop element from the top of the stack
+  pop() {
+    if (this.isEmpty()) {
+      return "Underflow";
     }
-    
-    return factors[factors.length - 1];
+    return this.items.pop();
+  }
+
+  // Peek the top element of the stack
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  // Check if the stack is empty
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  // Print the elements of the stack
+  printStack() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + " ";
+    }
+    return str;
+  }
 }
 
-const number = 13195;
-const largestPrime = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is ${largestPrime}`);
+// Example usage
+let stack = new Stack();
+console.log(stack.isEmpty()); // true
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack.printStack()); // 1 2 3
+
+console.log(stack.peek()); // 3
+
+console.log(stack.pop()); // 3
+console.log(stack.printStack()); // 1 2
