2cfc12af25ef3bfba2656eafdffca2e35d2d47b3
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:34:12 +0800

    2-28-2024 6:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,51 @@
-class Graph {
-    constructor() {
-        this.nodes = {};
-    }
-
-    addNode(node) {
-        this.nodes[node] = [];
-    }
-
-    addEdge(node1, node2, weight) {
-        this.nodes[node1].push({ node: node2, weight: weight });
-        this.nodes[node2].push({ node: node1, weight: weight });
-    }
-
-    dijkstra(startNode) {
-        let distances = {};
-        let pq = {};
-
-        for (let node in this.nodes) {
-            distances[node] = Infinity;
-            pq[node] = Infinity;
-        }
-
-        distances[startNode] = 0;
-        pq[startNode] = 0;
-
-        while (Object.keys(pq).length !== 0) {
-            let minNode = Object.keys(pq).reduce((a, b) => pq[a] < pq[b] ? a : b);
-            delete pq[minNode];
-
-            for (let neighbor of this.nodes[minNode]) {
-                let totalDistance = distances[minNode] + neighbor.weight;
-                if (totalDistance < distances[neighbor.node]) {
-                    distances[neighbor.node] = totalDistance;
-                    pq[neighbor.node] = totalDistance;
-                }
-            }
-        }
-
-        return distances;
+class Node {
+    constructor(value, color) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.color = color || 'red';
     }
 }
 
-// Example usage:
-let g = new Graph();
-g.addNode('A');
-g.addNode('B');
-g.addNode('C');
-g.addNode('D');
-g.addEdge('A', 'B', 1);
-g.addEdge('A', 'C', 4);
-g.addEdge('B', 'C', 2);
-g.addEdge('B', 'D', 5);
-g.addEdge('C', 'D', 1);
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
 
-console.log(g.dijkstra('A'));
+    insert(value) {
+        let newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            this.root.color = 'black';
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(root, newNode) {
+        if (newNode.value < root.value) {
+            if (!root.left) {
+                root.left = newNode;
+                newNode.parent = root;
+            } else {
+                this.insertNode(root.left, newNode);
+            }
+        } else {
+            if (!root.right) {
+                root.right = newNode;
+                newNode.parent = root;
+            } else {
+                this.insertNode(root.right, newNode);
+            }
+        }
+    }
+}
+
+// Usage
+const tree = new RedBlackTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(5);
+
+console.log(tree.root); // Output the root node of the red-black tree
