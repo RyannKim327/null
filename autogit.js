a852eb3a23ab4a1709164d544a6d05312f92a3f1
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:07:32 +0800

    2-28-2024 6:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,104 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+class AVLTreeNode {
+    constructor(value) {
+        this.value = value;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+    }
 }
 
-let inputString = "Hello, World!";
-let result = removeVowels(inputString);
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
 
-console.log(result);  // Output: "Hll, Wrld!"
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+
+    rotateRight(y) {
+        const x = y.left;
+        const T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        this.updateHeight(y);
+        this.updateHeight(x);
+
+        return x;
+    }
+
+    rotateLeft(x) {
+        const y = x.right;
+        const T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        this.updateHeight(x);
+        this.updateHeight(y);
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this.insertRec(this.root, value);
+    }
+
+    insertRec(node, value) {
+        if (node === null) {
+            return new AVLTreeNode(value);
+        }
+
+        if (value < node.value) {
+            node.left = this.insertRec(node.left, value);
+        } else if (value > node.value) {
+            node.right = this.insertRec(node.right, value);
+        } else {
+            return node; // value already exists
+        }
+
+        this.updateHeight(node);
+
+        const balance = this.getBalanceFactor(node);
+
+        // Left Left Case
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        // Right Right Case
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        // Left Right Case
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        // Right Left Case
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+}
