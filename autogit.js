8e21d0ee2b62d0f450187c4b9b6553e82a5f7b9b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:53:53 +0800

    2-28-2024 5:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,5 @@
-// Initial array
-let array = [1, 2, 3, 4, 5];
+// Prompt the user for input
+let userInput = prompt("Please enter some text:");
 
-// Element to remove
-let elementToRemove = 3;
-
-// Find the index of the element in the array
-let index = array.indexOf(elementToRemove);
-
-// Check if the element exists in the array
-if (index !== -1) {
-    // Remove the element at the specified index
-    array.splice(index, 1);
-}
-
-console.log(array); // Output: [1, 2, 4, 5]
+// Display the input back to the user
+alert("You entered: " + userInput);
