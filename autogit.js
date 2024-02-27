e47dc0f7502eb269425794f16bbe3b31b047580c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:04:07 +0800

    2-28-2024 6:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,65 @@
-let str = "42";
-let num = parseInt(str);
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-console.log(num); // Output: 42
-let str = "3.14";
-let num = parseFloat(str);
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
-console.log(num); // Output: 3.14
+  // Function to add a new node to the linked list
+  addNode(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Function to reverse the linked list
+  reverse() {
+    let previous = null;
+    let current = this.head;
+    let next = null;
+
+    while (current !== null) {
+      next = current.next;
+      current.next = previous;
+      previous = current;
+      current = next;
+    }
+
+    this.head = previous;
+  }
+
+  // Function to print the linked list
+  printList() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+// Usage
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+
+console.log("Original Linked List:");
+list.printList();
+
+list.reverse();
+
+console.log("\nReversed Linked List:");
+list.printList();
