e41a9a821dfea767b6abe0c8a35ed8342a201786
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:33:34 +0800

    2-28-2024 6:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,90 @@
-class ListNode {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+// Define a TreeNode object
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-function getIntersectionNode(headA, headB) {
-    let currA = headA;
-    let currB = headB;
-    
-    while (currA !== currB) {
-        currA = currA ? currA.next : headB;
-        currB = currB ? currB.next : headA;
+// Define a BinarySearchTree object
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // Insert a value into the binary search tree
+  insert(value) {
+    const newNode = new TreeNode(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
     }
-    
-    return currA;
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.value < node.value) {
+      if (!node.left) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (!node.right) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  // Search for a value in the binary search tree
+  search(value) {
+    return this.searchNode(this.root, value);
+  }
+
+  searchNode(node, value) {
+    if (!node) {
+      return false;
+    }
+
+    if (value < node.value) {
+      return this.searchNode(node.left, value);
+    } else if (value > node.value) {
+      return this.searchNode(node.right, value);
+    } else {
+      return true;
+    }
+  }
+
+  // Inorder traversal - returns the values in sorted order
+  inorder() {
+    return this.inorderTraversal(this.root, []);
+  }
+
+  inorderTraversal(node, result) {
+    if (node) {
+      this.inorderTraversal(node.left, result);
+      result.push(node.value);
+      this.inorderTraversal(node.right, result);
+    }
+
+    return result;
+  }
 }
 
-// Example usage
-let node1 = new ListNode(1);
-let node2 = new ListNode(2);
-let node3 = new ListNode(3);
-let node4 = new ListNode(4);
-let node5 = new ListNode(5);
+// Usage
+const bst = new BinarySearchTree();
+bst.insert(8);
+bst.insert(3);
+bst.insert(10);
+bst.insert(1);
+bst.insert(6);
+bst.insert(14);
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
+console.log(bst.search(6)); // Output: true
+console.log(bst.search(11)); // Output: false
 
-let nodeA1 = new ListNode(10);
-let nodeA2 = new ListNode(11);
-
-nodeA1.next = nodeA2;
-nodeA2.next = node3; // Intersection point
-
-let intersectionNode = getIntersectionNode(node1, nodeA1);
-console.log(intersectionNode); // This will output the common node (node3 in this case)
+console.log(bst.inorder()); // Output: [1, 3, 6, 8, 10, 14]
