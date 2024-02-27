9a6bc78fbff31c894001092c7407c414d683242a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:52:51 +0800

    2-28-2024 5:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,8 @@
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
-    }
-}
-
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-
-    insert(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new TrieNode();
-            }
-            node = node.children[char];
-        }
-        node.isEndOfWord = true;
-    }
-
-    search(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return node.isEndOfWord;
-    }
-
-    startsWith(prefix) {
-        let node = this.root;
-        for (let char of prefix) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return true;
-    }
-}
-
-// Example usage
-let trie = new Trie();
-trie.insert("apple");
-console.log(trie.search("apple")); // true
-console.log(trie.search("app")); // false
-console.log(trie.startsWith("app")); // true
-trie.insert("app");
-console.log(trie.search("app")); // true
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = Array.from(new Set(arr));
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = arr.filter((value, index, self) => self.indexOf(value) === index);
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = arr.reduce((unique, item) => {
+    return unique.includes(item) ? unique : [...unique, item];
+}, []);
