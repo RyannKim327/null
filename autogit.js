c317c221c554bb4a3f796fc3b32ed4fed92e6dca
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:17:41 +0800

    2-28-2024 6:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,8 @@
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
-    }
-}
+let str = "42";
+let num = parseInt(str);
 
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
+console.log(num); // 42
+let binaryStr = "1010";
+let num = parseInt(binaryStr, 2);
 
-    insert(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new TrieNode();
-            }
-            node = node.children[char];
-        }
-        node.isEndOfWord = true;
-    }
-
-    search(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return node.isEndOfWord;
-    }
-
-    startsWith(prefix) {
-        let node = this.root;
-        for (let char of prefix) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return true;
-    }
-}
-
-// Example Usage
-let trie = new Trie();
-trie.insert("apple");
-console.log(trie.search("apple"));  // true
-console.log(trie.search("app"));    // false
-console.log(trie.startsWith("app")); // true
+console.log(num); // 10
