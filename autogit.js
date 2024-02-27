55431f0eda6a3ce04295d87310178de0343deb57
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:24:26 +0800

    2-28-2024 6:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,22 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function findFirstRepeatedChar(str) {
+    let charCount = {};
+
+    for (let char of str) {
+        if (charCount[char]) {
+            return char;
+        } else {
+            charCount[char] = 1;
+        }
     }
+
+    return null;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+let inputString = "hello world";
+let firstRepeatedChar = findFirstRepeatedChar(inputString);
 
-    append(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-
-    findMiddle() {
-        let slow = this.head;
-        let fast = this.head;
-
-        while (fast && fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow.data;
-    }
+if (firstRepeatedChar) {
+    console.log(`The first repeated character is: ${firstRepeatedChar}`);
+} else {
+    console.log("No repeated characters found in the string.");
 }
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
-linkedList.append(5);
-
-console.log(linkedList.findMiddle()); // Output: 3
