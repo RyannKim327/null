c92f19d555cde01042c0fb8e82b806432429b79b
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:50:52 +0800

    2-28-2024 5:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,8 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-function getIntersectionNode(headA, headB) {
-  if (!headA || !headB) {
-    return null;
-  }
+let originalString = "Hello, World!";
+let stringWithoutVowels = removeVowels(originalString);
 
-  let visitedNodes = new Set();
-  let currentNodeA = headA;
-
-  // Traverse the first linked list and store nodes in a Set
-  while (currentNodeA) {
-    visitedNodes.add(currentNodeA);
-    currentNodeA = currentNodeA.next;
-  }
-
-  // Traverse the second linked list and check for intersection
-  let currentNodeB = headB;
-  while (currentNodeB) {
-    if (visitedNodes.has(currentNodeB)) {
-      return currentNodeB;
-    }
-    currentNodeB = currentNodeB.next;
-  }
-
-  return null; // No intersection found
-}
-
-// Example usage:
-// Construct two linked lists
-let listA = new Node(4);
-listA.next = new Node(1);
-listA.next.next = new Node(8);
-listA.next.next.next = new Node(4);
-listA.next.next.next.next = new Node(5);
-
-let listB = new Node(5);
-listB.next = new Node(0);
-listB.next.next = new Node(1);
-listB.next.next.next = listA.next.next;
-
-const intersectionNode = getIntersectionNode(listA, listB);
-if (intersectionNode) {
-  console.log("Intersection node value:", intersectionNode.value);
-} else {
-  console.log("No intersection node found");
-}
+console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
