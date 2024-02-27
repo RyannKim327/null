6011bda0c2cfce39f6e71e4ebf3684e18ad13ef9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:29:54 +0800

    2-28-2024 6:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,19 @@
-const axios = require('axios');
-
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
+    
+    let tortoise = head;
+    let hare = head.next;
+    
+    while (tortoise !== hare) {
+        if (!hare || !hare.next) {
+            return false;
+        }
+        
+        tortoise = tortoise.next;
+        hare = hare.next.next;
+    }
+    
+    return true;
+}
