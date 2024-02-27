8075e72d7f060fdd7e3d49a063e97d712c73c10d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:56:06 +0800

    2-28-2024 5:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,75 +1,68 @@
-class Graph {
-    constructor() {
-        this.nodes = new Map();
-    }
-    
-    addNode(node) {
-        this.nodes.set(node, new Set());
-    }
-
-    addEdge(node1, node2) {
-        if (this.nodes.has(node1) && this.nodes.has(node2)) {
-            this.nodes.get(node1).add(node2);
-            this.nodes.get(node2).add(node1);
-        } else {
-            console.error("Nodes not found in the graph.");
-        }
-    }
-
-    biDirectionalSearch(startNode, endNode) {
-        let forwardQueue = [startNode];
-        let backwardQueue = [endNode];
-        let visitedForward = new Set([startNode]);
-        let visitedBackward = new Set([endNode]);
-        let intersectionNode = null;
-
-        while (forwardQueue.length > 0 && backwardQueue.length > 0) {
-            intersectionNode = this.visitNodes(forwardQueue, visitedForward, visitedBackward);
-            if (intersectionNode) {
-                console.log("Shortest Path Found!");
-                return true;
-            }
-
-            intersectionNode = this.visitNodes(backwardQueue, visitedBackward, visitedForward);
-            if (intersectionNode) {
-                console.log("Shortest Path Found!");
-                return true;
-            }
-        }
-
-        console.log("No path found.");
-        return false;
-    }
-
-    visitNodes(queue, visitedStart, visitedEnd) {
-        let currentNode = queue.shift();
-        if (visitedEnd.has(currentNode)) {
-            return currentNode;
-        }
-
-        for (let neighbor of this.nodes.get(currentNode)) {
-            if (!visitedStart.has(neighbor)) {
-                visitedStart.add(neighbor);
-                queue.push(neighbor);
-            }
-        }
-
-        return null;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
-// Example usage
-let graph = new Graph();
-graph.addNode("A");
-graph.addNode("B");
-graph.addNode("C");
-graph.addNode("D");
-graph.addNode("E");
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
 
-graph.addEdge("A", "B");
-graph.addEdge("B", "C");
-graph.addEdge("C", "D");
-graph.addEdge("D", "E");
-graph.addEdge("C", "E");
+    insert(value) {
+        const newNode = new Node(value);
 
-graph.biDirectionalSearch("A", "E");
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+}
+
+// Usage example
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+bst.insert(7);
+
+console.log(bst.search(5)); // Output: true
+console.log(bst.search(12)); // Output: false
