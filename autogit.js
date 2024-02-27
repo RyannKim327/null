f2ddb394e3c75e69db21e73fa469b44f831ce745
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:25:01 +0800

    2-28-2024 6:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,6 @@
-function findFirstRepeatedChar(str) {
-    const charMap = {};
+let numbers = [5, 2, 8, 4, 1];
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
-    }
-
-    return null;
-}
-
-const inputString = "hello world";
-const repeatedChar = findFirstRepeatedChar(inputString);
-
-if (repeatedChar) {
-    console.log(`The first repeated character in the string is: ${repeatedChar}`);
-} else {
-    console.log("No repeated characters found in the string.");
-}
+console.log(numbers);
