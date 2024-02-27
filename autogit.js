c7acba4b8abc7802f8995d3244a68a1553f81451
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:59:28 +0800

    2-28-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,5 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// Taking input from the user
+let userInput = prompt("Enter something:");
 
-// Example: Generate a random number between 1 and 100
-var randomNumber = getRandomNumber(1, 100);
-console.log(randomNumber);
+// Displaying the input back to the user
+console.log("You entered: " + userInput);
