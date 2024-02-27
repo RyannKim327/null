50e60db4e474f188d29dca229ecd004937154a45
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:01:33 +0800

    2-28-2024 6:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,7 @@
-let str = "   Hello,   World!   ";
-let newStr = str.replace(/\s/g, '');
-console.log(newStr); // Output: "Hello,World!"
-let str = "   Hello,   World!   ";
-let newStr = str.split('').filter(char => char.trim() !== '').join('');
-console.log(newStr); // Output: "Hello,World!"
-let str = "   Hello,   World!   ";
-let newStr = str.trim();
-console.log(newStr); // Output: "Hello,   World!"
+let numbers = [4, 2, 5, 1, 3];
+
+numbers.sort(function(a, b) {
+    return a - b;
+});
+
+console.log(numbers);
