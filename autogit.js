c8161f0f1807e050ebeb373ed84c50194e2f38c9
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:55:54 +0800

    2-28-2024 5:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,75 @@
-function maxSubarraySum(arr) {
-    let maxEndingHere = arr[0];
-    let maxSoFar = arr[0];
-
-    for (let i = 1; i < arr.length; i++) {
-        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+class Graph {
+    constructor() {
+        this.nodes = new Map();
+    }
+    
+    addNode(node) {
+        this.nodes.set(node, new Set());
     }
 
-    return maxSoFar;
+    addEdge(node1, node2) {
+        if (this.nodes.has(node1) && this.nodes.has(node2)) {
+            this.nodes.get(node1).add(node2);
+            this.nodes.get(node2).add(node1);
+        } else {
+            console.error("Nodes not found in the graph.");
+        }
+    }
+
+    biDirectionalSearch(startNode, endNode) {
+        let forwardQueue = [startNode];
+        let backwardQueue = [endNode];
+        let visitedForward = new Set([startNode]);
+        let visitedBackward = new Set([endNode]);
+        let intersectionNode = null;
+
+        while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+            intersectionNode = this.visitNodes(forwardQueue, visitedForward, visitedBackward);
+            if (intersectionNode) {
+                console.log("Shortest Path Found!");
+                return true;
+            }
+
+            intersectionNode = this.visitNodes(backwardQueue, visitedBackward, visitedForward);
+            if (intersectionNode) {
+                console.log("Shortest Path Found!");
+                return true;
+            }
+        }
+
+        console.log("No path found.");
+        return false;
+    }
+
+    visitNodes(queue, visitedStart, visitedEnd) {
+        let currentNode = queue.shift();
+        if (visitedEnd.has(currentNode)) {
+            return currentNode;
+        }
+
+        for (let neighbor of this.nodes.get(currentNode)) {
+            if (!visitedStart.has(neighbor)) {
+                visitedStart.add(neighbor);
+                queue.push(neighbor);
+            }
+        }
+
+        return null;
+    }
 }
 
-// Example usage:
-const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-console.log(maxSubarraySum(arr)); // Output: 6
+// Example usage
+let graph = new Graph();
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+graph.addNode("E");
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.addEdge("C", "D");
+graph.addEdge("D", "E");
+graph.addEdge("C", "E");
+
+graph.biDirectionalSearch("A", "E");
