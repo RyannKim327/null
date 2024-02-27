615a783c0f20f6e230372abbaafa6e0317950cf6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:27:09 +0800

    2-28-2024 6:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,4 @@
-function topologicalSort(graph) {
-    const visited = {};
-    const stack = [];
+let str = "42";
+let num = parseInt(str);
 
-    // Helper function to perform DFS
-    function dfs(node) {
-        visited[node] = true;
-        if (graph[node]) {
-            for (const neighbor of graph[node]) {
-                if (!visited[neighbor]) {
-                    dfs(neighbor);
-                }
-            }
-        }
-        stack.push(node);
-    }
-
-    // Perform DFS on each unvisited node
-    for (const node in graph) {
-        if (!visited[node]) {
-            dfs(node);
-        }
-    }
-
-    // Return the nodes in topologically sorted order
-    return stack.reverse();
-}
-
-// Example graph representation using adjacency list
-const graph = {
-    0: [1, 2],
-    1: [2],
-    2: [],
-    3: [2, 4],
-    4: []
-};
-
-const result = topologicalSort(graph);
-console.log(result);
+console.log(num);  // Output: 42
