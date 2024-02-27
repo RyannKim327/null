ad718786ff7aac910977b28f61720417919a3ba6
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:22:15 +0800

    2-28-2024 6:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,76 +1,61 @@
-class SkipNode {
-    constructor(value, level) {
-        this.value = value;
-        this.forward = new Array(level + 1).fill(null);
+class TarjansAlgorithm {
+  constructor(graph) {
+    this.graph = graph;
+    this.index = 0;
+    this.stack = [];
+    this.lowLinks = {};
+    this.indexes = {};
+    this.stronglyConnectedComponents = [];
+  }
+
+  findStronglyConnectedComponents() {
+    for(let node of Object.keys(this.graph)) {
+      if(!this.indexes[node]) {
+        this.strongConnect(node);
+      }
     }
+    return this.stronglyConnectedComponents;
+  }
+
+  strongConnect(node) {
+    this.indexes[node] = this.index;
+    this.lowLinks[node] = this.index;
+    this.index++;
+    this.stack.push(node);
+
+    for(let neighbor of this.graph[node]) {
+      if(!this.indexes[neighbor]) {
+        this.strongConnect(neighbor);
+        this.lowLinks[node] = Math.min(this.lowLinks[node], this.lowLinks[neighbor]);
+      } else if(this.stack.includes(neighbor)) {
+        this.lowLinks[node] = Math.min(this.lowLinks[node], this.indexes[neighbor]);
+      }
+    }
+
+    if(this.lowLinks[node] === this.indexes[node]) {
+      let component = [];
+      let poppedNode;
+      do {
+        poppedNode = this.stack.pop();
+        component.push(poppedNode);
+      } while(poppedNode !== node);
+      this.stronglyConnectedComponents.push(component);
+    }
+  }
 }
 
-class SkipList {
-    constructor(maxLevel) {
-        this.maxLevel = maxLevel;
-        this.head = new SkipNode(-1, maxLevel);
-        this.level = 0;
-    }
+// Example usage:
+const graph = {
+  1: [2],
+  2: [3, 4],
+  3: [1],
+  4: [5],
+  5: [6],
+  6: [4, 7],
+  7: [6, 8],
+  8: [5, 7]
+};
 
-    randomLevel() {
-        let level = 0;
-        while (Math.random() < 0.5 && level < this.maxLevel) {
-            level++;
-        }
-        return level;
-    }
-
-    insert(value) {
-        const update = new Array(this.maxLevel + 1).fill(null);
-        let current = this.head;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-            update[i] = current;
-        }
-
-        current = current.forward[0];
-
-        if (current === null || current.value !== value) {
-            const newLevel = this.randomLevel();
-            if (newLevel > this.level) {
-                for (let i = this.level + 1; i <= newLevel; i++) {
-                    update[i] = this.head;
-                }
-                this.level = newLevel;
-            }
-
-            const newNode = new SkipNode(value, newLevel);
-            for (let i = 0; i <= newLevel; i++) {
-                newNode.forward[i] = update[i].forward[i];
-                update[i].forward[i] = newNode;
-            }
-        }
-    }
-
-    search(value) {
-        let current = this.head;
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-        }
-        current = current.forward[0];
-        if (current !== null && current.value === value) {
-            console.log("Found value: " + value);
-        } else {
-            console.log("Value not found");
-        }
-    }
-}
-
-// Example usage
-const skipList = new SkipList(3);
-skipList.insert(3);
-skipList.insert(6);
-skipList.insert(7);
-skipList.insert(9);
-skipList.search(6);
-skipList.search(8);
+const tarjan = new TarjansAlgorithm(graph);
+const stronglyConnectedComponents = tarjan.findStronglyConnectedComponents();
+console.log(stronglyConnectedComponents);
