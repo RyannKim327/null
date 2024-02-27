cd074b6cdde3d364cddd2d92f8431348944b4c4e
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:59:22 +0800

    2-28-2024 5:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,71 +1,7 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-class BinaryTree {
-    constructor() {
-        this.root = null;
-    }
-
-    insert(value) {
-        const newNode = new Node(value);
-
-        if (!this.root) {
-            this.root = newNode;
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
-
-    insertNode(node, newNode) {
-        if (newNode.value < node.value) {
-            if (node.left === null) {
-                node.left = newNode;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-            } else {
-                this.insertNode(node.right, newNode);
-            }
-        }
-    }
-
-    search(value) {
-        return this.searchNode(this.root, value);
-    }
-
-    searchNode(node, value) {
-        if (!node) {
-            return false;
-        }
-
-        if (node.value === value) {
-            return true;
-        }
-
-        if (value < node.value) {
-            return this.searchNode(node.left, value);
-        } else {
-            return this.searchNode(node.right, value);
-        }
-    }
-}
-
-// Example usage
-const binaryTree = new BinaryTree();
-
-binaryTree.insert(5);
-binaryTree.insert(3);
-binaryTree.insert(7);
-binaryTree.insert(1);
-binaryTree.insert(4);
-
-console.log(binaryTree.search(4)); // Output: true
-console.log(binaryTree.search(6)); // Output: false
+// Example: Generate a random number between 1 and 100
+var randomNumber = getRandomNumber(1, 100);
+console.log(randomNumber);
