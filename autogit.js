8b5adeadc4c1506452ad5f3e772736af154e770c
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:03:19 +0800

    2-28-2024 6:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,36 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.children = [];
-  }
-
-  addChild(node) {
-    this.children.push(node);
+    this.next = null;
   }
 }
 
-function biDirectionalSearch(startNode, endNode) {
-  let startQueue = [startNode];
-  let endQueue = [endNode];
-  let startVisited = new Set();
-  let endVisited = new Set();
-
-  startVisited.add(startNode);
-  endVisited.add(endNode);
-
-  while (startQueue.length > 0 && endQueue.length > 0) {
-    let startCurrent = startQueue.shift();
-    let endCurrent = endQueue.shift();
-
-    if (endVisited.has(startCurrent)) {
-      console.log("Path found!");
-      return;
-    }
-
-    if (startVisited.has(endCurrent)) {
-      console.log("Path found!");
-      return;
-    }
-
-    for (let child of startCurrent.children) {
-      if (!startVisited.has(child)) {
-        startVisited.add(child);
-        startQueue.push(child);
-      }
-    }
-
-    for (let child of endCurrent.children) {
-      if (!endVisited.has(child)) {
-        endVisited.add(child);
-        endQueue.push(child);
-      }
-    }
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false;
   }
 
-  console.log("Path not found!");
+  let slow = head;
+  let fast = head.next;
+
+  while (fast && fast.next) {
+    if (slow === fast) {
+      return true;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
 }
 
 // Example usage
+// Create a linked list with a cycle
 let node1 = new Node(1);
 let node2 = new Node(2);
 let node3 = new Node(3);
+node1.next = node2;
+node2.next = node3;
+node3.next = node1; // Creating a cycle
 
-node1.addChild(node2);
-node2.addChild(node3);
-
-biDirectionalSearch(node1, node3);
+console.log(hasCycle(node1)); // Output: true
