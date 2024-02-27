5d874727cf566667e4cf1b93a5475479c3e764f2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:29:32 +0800

    2-28-2024 6:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,6 @@
-class Graph {
-    constructor() {
-        this.vertices = {};
-    }
+let numbers = [5, 2, 9, 1, 5, 6];
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-    addVertex(vertex) {
-        if (!this.vertices[vertex]) {
-            this.vertices[vertex] = [];
-        }
-    }
-
-    addEdge(vertex1, vertex2) {
-        if (!this.vertices[vertex1] || !this.vertices[vertex2]) {
-            throw new Error('Vertex not found in graph');
-        }
-        this.vertices[vertex1].push(vertex2);
-        this.vertices[vertex2].push(vertex1);
-    }
-
-    depthFirstSearch(startingVertex) {
-        const visited = {};
-        const result = [];
-
-        const dfs = (vertex) => {
-            visited[vertex] = true;
-            result.push(vertex);
-
-            this.vertices[vertex].forEach((neighbor) => {
-                if (!visited[neighbor]) {
-                    dfs(neighbor);
-                }
-            });
-        };
-
-        dfs(startingVertex);
-        return result;
-    }
-}
-
-// Example usage:
-const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-
-const dfsResult = graph.depthFirstSearch('A');
-console.log(dfsResult); // Output: ['A', 'B', 'D', 'C', 'E']
+console.log(numbers); // Output: [1, 2, 5, 5, 6, 9]
