db387799321b3457b92c89e5ba4f0857ce37b046
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:06:30 +0800

    2-28-2024 6:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,11 @@
-class ListNode {
-    constructor(val, next = null) {
-        this.val = val;
-        this.next = next;
-    }
+area = (base * height) / 2
+function calculateTriangleArea(base, height) {
+    return (base * height) / 2;
 }
 
-const getIntersectionNode = (headA, headB) => {
-    const set = new Set();
-    
-    // Traverse the first linked list and store each node's value in a Set
-    let curr = headA;
-    while (curr !== null) {
-        set.add(curr);
-        curr = curr.next;
-    }
-    
-    // Traverse the second linked list and check if the value exists in the Set
-    curr = headB;
-    while (curr !== null) {
-        if (set.has(curr)) {
-            return curr;
-        }
-        curr = curr.next;
-    }
-    
-    return null; // No intersection found
-};
+// Example usage
+let base = 5;
+let height = 8;
+let area = calculateTriangleArea(base, height);
 
-// Example Usage
-const list1 = new ListNode(4, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))));
-const list2 = new ListNode(5, new ListNode(6, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))));
-const intersectionNode = getIntersectionNode(list1, list2);
-console.log(intersectionNode);
+console.log("The area of the triangle is: " + area);
