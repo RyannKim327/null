3cd4ae1e01409145674dd234cb194f9b6e7b0d4d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:10:09 +0800

    2-28-2024 6:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,24 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    
-    const dp = [];
-    for (let i = 0; i <= m; i++) {
-        dp[i] = [];
-        for (let j = 0; j <= n; j++) {
-            if (i === 0 || j === 0) {
-                dp[i][j] = 0;
-            } else if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+function quicksort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  } else {
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
     }
-    
-    let index = dp[m][n];
-    let lcs = Array(index);
-    let i = m, j = n;
-    
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs[index - 1] = str1[i - 1];
-            i--;
-            j--;
-            index--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    
-    return lcs.join('');
+
+    return quicksort(left).concat(pivot, quicksort(right));
+  }
 }
 
-const str1 = 'AGGTAB';
-const str2 = 'GXTXAYB';
+const unsortedArray = [5, 2, 9, 3, 7, 6, 1, 8, 4];
+const sortedArray = quicksort(unsortedArray);
 
-const result = longestCommonSubsequence(str1, str2);
-console.log(result); // Output: GTAB
+console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
