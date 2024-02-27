ff6cd6f4b8cd8d1c33d7f751070fca61a2a834c2
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:12:19 +0800

    2-28-2024 6:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,22 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-function linkedListToArray(head) {
-  let current = head;
-  const values = [];
-  
-  while (current !== null) {
-    values.push(current.value);
-    current = current.next;
-  }
-  
-  return values;
-}
-function isPalindromeLinkedList(head) {
-  const values = linkedListToArray(head);
-  const len = values.length;
-  
-  for (let i = 0; i < len / 2; i++) {
-    if (values[i] !== values[len - 1 - i]) {
-      return false;
-    }
-  }
-  
-  return true;
-}
-// Example linked list
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(2);
-const node5 = new Node(1);
+function isAnagram(str1, str2) {
+    // Remove non-alphabetic characters and convert to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
+    // Convert strings to arrays, sort and join them back
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
 
-console.log(isPalindromeLinkedList(node1)); // Output: true
+    // Check if the sorted strings are equal
+    return str1 === str2;
+}
+
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
