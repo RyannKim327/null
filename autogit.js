909b208d67bece49d4def0dbb3c9c2a9f14aa9ab
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:16:32 +0800

    2-28-2024 6:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,84 +1,15 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.color = 'red'; // New nodes are always red by default
+function isArraySortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
     }
+    return true;
 }
 
-class RedBlackTree {
-    constructor() {
-        this.root = null;
-    }
+// Test the function
+const sortedArray = [1, 2, 3, 4, 5];
+const unsortedArray = [5, 4, 3, 2, 1];
 
-    insert(value) {
-        const newNode = new Node(value);
-        this.root = this._insertNode(this.root, newNode);
-        this.root.color = 'black'; // Root is always black
-    }
-
-    _insertNode(root, newNode) {
-        if (!root) {
-            return newNode;
-        }
-
-        if (newNode.value < root.value) {
-            root.left = this._insertNode(root.left, newNode);
-        } else {
-            root.right = this._insertNode(root.right, newNode);
-        }
-
-        // Perform rotation and recoloring
-        if (this._isRed(root.right) && !this._isRed(root.left)) {
-            root = this._rotateLeft(root);
-        }
-        if (this._isRed(root.left) && this._isRed(root.left.left)) {
-            root = this._rotateRight(root);
-        }
-        if (this._isRed(root.left) && this._isRed(root.right)) {
-            this._flipColors(root);
-        }
-
-        return root;
-    }
-
-    _isRed(node) {
-        if (!node) {
-            return false; // Null nodes are black
-        }
-        return node.color === 'red';
-    }
-
-    _rotateLeft(node) {
-        const newRoot = node.right;
-        node.right = newRoot.left;
-        newRoot.left = node;
-        newRoot.color = node.color;
-        node.color = 'red';
-        return newRoot;
-    }
-
-    _rotateRight(node) {
-        const newRoot = node.left;
-        node.left = newRoot.right;
-        newRoot.right = node;
-        newRoot.color = node.color;
-        node.color = 'red';
-        return newRoot;
-    }
-
-    _flipColors(node) {
-        node.color = 'red';
-        node.left.color = 'black';
-        node.right.color = 'black';
-    }
-}
-
-// Usage
-const rbTree = new RedBlackTree();
-rbTree.insert(10);
-rbTree.insert(5);
-rbTree.insert(15);
-rbTree.insert(3);
-rbTree.insert(7);
+console.log(isArraySortedAscending(sortedArray)); // Output: true
+console.log(isArraySortedAscending(unsortedArray)); // Output: false
