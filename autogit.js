caf591e3f773191d5899fcfa69d893389398d91a
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:29:23 +0800

    2-28-2024 6:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,54 @@
-function countOccurrences(inputString, word) {
-    // Using a regular expression to match the word globally (g) and case insensitive (i)
-    const regex = new RegExp('\\b' + word + '\\b', 'gi');
-    
-    // Using match() method to get an array of all matches
-    const matches = inputString.match(regex);
-    
-    // Counting the number of matches
-    const count = matches ? matches.length : 0;
+class Graph {
+    constructor() {
+        this.vertices = {};
+    }
 
-    return count;
+    addVertex(vertex) {
+        if (!this.vertices[vertex]) {
+            this.vertices[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        if (!this.vertices[vertex1] || !this.vertices[vertex2]) {
+            throw new Error('Vertex not found in graph');
+        }
+        this.vertices[vertex1].push(vertex2);
+        this.vertices[vertex2].push(vertex1);
+    }
+
+    depthFirstSearch(startingVertex) {
+        const visited = {};
+        const result = [];
+
+        const dfs = (vertex) => {
+            visited[vertex] = true;
+            result.push(vertex);
+
+            this.vertices[vertex].forEach((neighbor) => {
+                if (!visited[neighbor]) {
+                    dfs(neighbor);
+                }
+            });
+        };
+
+        dfs(startingVertex);
+        return result;
+    }
 }
 
-const inputString = 'Hello, hello, HELLO, World!';
-const word = 'hello';
-const occurrences = countOccurrences(inputString, word);
+// Example usage:
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
 
-console.log(`The word "${word}" occurs ${occurrences} times in the string.`);
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+
+const dfsResult = graph.depthFirstSearch('A');
+console.log(dfsResult); // Output: ['A', 'B', 'D', 'C', 'E']
