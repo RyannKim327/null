f1ec42e7811696112ba54a848c90cf106f00dfcf
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:11:16 +0800

    2-28-2024 6:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,28 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function findMiddleElement(head) {
+  let slowPointer = head;
+  let fastPointer = head;
+
+  while (fastPointer !== null && fastPointer.next !== null) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  return slowPointer.value;
+}
+
+// Example linked list
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Finding the middle element
+console.log(findMiddleElement(head)); // Output: 3
