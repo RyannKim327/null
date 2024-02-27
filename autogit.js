752fdbff45f1db4132f4df376a2f973f85dbcf23
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:57:28 +0800

    2-28-2024 5:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,60 @@
-function calculateTriangleArea(base, height) {
-    return 0.5 * base * height;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-// Example usage
-const base = 5;
-const height = 8;
-const area = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + area);
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+    while (current !== null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    this.head = prev;
+  }
+
+  printList() {
+    let current = this.head;
+    while (current !== null) {
+      console.log(current.value);
+      current = current.next;
+    }
+  }
+}
+
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+
+console.log("Original Linked List:");
+linkedList.printList();
+
+console.log("Reversed Linked List:");
+linkedList.reverse();
+linkedList.printList();
