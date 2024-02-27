e3b7744da76286ffd685021992c5e0b9f6d8fb77
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:49:04 +0800

    2-28-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,24 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-class BinaryTree {
-    constructor() {
-        this.root = null;
-    }
-
-    insert(data) {
-        let newNode = new Node(data);
-
-        if (this.root === null) {
-            this.root = newNode;
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
-
-    insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-            if (node.left === null) {
-                node.left = newNode;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-            } else {
-                this.insertNode(node.right, newNode);
+function selectionSort(arr) {
+    var minIndex, temp;
+    var n = arr.length;
+    
+    for (var i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (var j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+        
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
-
-    search(node, data) {
-        if (node === null) {
-            return false;
-        }
-
-        if (data < node.data) {
-            return this.search(node.left, data);
-        } else if (data > node.data) {
-            return this.search(node.right, data);
-        } else {
-            return true;
-        }
-    }
+    
+    return arr;
 }
 
-// Usage
-const tree = new BinaryTree();
-
-tree.insert(10);
-tree.insert(5);
-tree.insert(15);
-tree.insert(3);
-tree.insert(7);
-
-console.log(tree.search(tree.root, 5)); // Output: true
-console.log(tree.search(tree.root, 20)); // Output: false
+// Example usage
+var arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + selectionSort(arr));
