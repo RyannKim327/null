e2e766345a27910ffd81dc81f2eb92db41a15995
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:18:34 +0800

    2-28-2024 6:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,42 @@
-function factorial(n) {
-    // Base case
-    if (n === 0 || n === 1) {
-        return 1;
+function findMedianSortedArrays(nums1, nums2) {
+    let mergedArray = mergeArrays(nums1, nums2);
+
+    const n = mergedArray.length;
+    if (n % 2 === 0) {
+        return (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2;
+    } else {
+        return mergedArray[Math.floor(n / 2)];
     }
-    
-    // Recursive case
-    return n * factorial(n - 1);
 }
 
-// Example usage
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is ${result}.`);
+function mergeArrays(nums1, nums2) {
+    let merged = [];
+    let i = 0, j = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] < nums2[j]) {
+            merged.push(nums1[i]);
+            i++;
+        } else {
+            merged.push(nums2[j]);
+            j++;
+        }
+    }
+
+    while (i < nums1.length) {
+        merged.push(nums1[i]);
+        i++;
+    }
+
+    while (j < nums2.length) {
+        merged.push(nums2[j]);
+        j++;
+    }
+
+    return merged;
+}
+
+// Example
+const nums1 = [1, 3];
+const nums2 = [2];
+console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.0
