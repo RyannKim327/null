3cc00f6a016ec31d2846ea69a6329150789b67a7
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:48:37 +0800

    2-28-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,18 @@
-function longestCommonSubstring(str1, str2) {
-    const matrix = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
-    let longestSubstring = 0;
-    let longestSubstringEndIndex = 0;
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false; // If the list is empty or has only one node, there is no cycle
+    }
 
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                matrix[i][j] = matrix[i - 1][j - 1] + 1;
+    let slow = head;
+    let fast = head.next;
 
-                if (matrix[i][j] > longestSubstring) {
-                    longestSubstring = matrix[i][j];
-                    longestSubstringEndIndex = i - 1;
-                }
-            }
+    while (slow !== fast) {
+        if (!fast || !fast.next) {
+            return false; // If fast pointer reaches the end, there is no cycle
         }
+        slow = slow.next;
+        fast = fast.next.next;
     }
 
-    if (longestSubstring === 0) {
-        return '';
-    }
-
-    return str1.slice(longestSubstringEndIndex - longestSubstring + 1, longestSubstringEndIndex + 1);
+    return true; // If slow and fast pointers meet, there is a cycle
 }
-
-// Example usage
-const str1 = 'abcdef';
-const str2 = 'bcdeft';
-console.log(longestCommonSubstring(str1, str2));  // Output: 'cdef'
