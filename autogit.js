50a169727c453d446256a379b5c6a648117dfc66
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:13:29 +0800

    2-28-2024 6:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function decimalToBinary(decimal) {
+    return (decimal >>> 0).toString(2);
 }
 
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-  let next = null;
-
-  while (current !== null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  return prev;
-}
-
-// Example: Create a linked list and reverse it
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-
-console.log("Original linked list:");
-let current = head;
-while (current !== null) {
-  console.log(current.data);
-  current = current.next;
-}
-
-head = reverseLinkedList(head);
-
-console.log("Reversed linked list:");
-current = head;
-while (current !== null) {
-  console.log(current.data);
-  current = current.next;
-}
+// Example
+let decimalNumber = 42;
+let binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber);
