f5a788524cbfe37d9e2cb0a88db519b1ef0aad02
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:30:11 +0800

    2-28-2024 6:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,80 @@
-function hasCycle(head) {
-    if (!head || !head.next) {
-        return false;
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const getIntersectionNode = (headA, headB) => {
+  if (!headA || !headB) {
+    return null;
+  }
+
+  let lenA = getLength(headA);
+  let lenB = getLength(headB);
+
+  let currA = headA;
+  let currB = headB;
+
+  // Align the pointers to start at the same position
+  while (lenA > lenB) {
+    currA = currA.next;
+    lenA--;
+  }
+
+  while (lenB > lenA) {
+    currB = currB.next;
+    lenB--;
+  }
+
+  // Traverse both lists in parallel
+  while (currA !== currB) {
+    currA = currA.next;
+    currB = currB.next;
+
+    if (currA === null && currB === null) {
+      return null; // No intersection found
     }
-    
-    let tortoise = head;
-    let hare = head.next;
-    
-    while (tortoise !== hare) {
-        if (!hare || !hare.next) {
-            return false;
-        }
-        
-        tortoise = tortoise.next;
-        hare = hare.next.next;
+
+    if (currA === null) {
+      currA = headB; // Reset currA to headB
     }
-    
-    return true;
+
+    if (currB === null) {
+      currB = headA; // Reset currB to headA
+    }
+  }
+
+  return currA; // Return the intersection node
+};
+
+const getLength = (head) => {
+  let len = 0;
+  let curr = head;
+
+  while (curr) {
+    len++;
+    curr = curr.next;
+  }
+
+  return len;
+};
+
+// Example usage
+const list1 = new ListNode(4);
+list1.next = new ListNode(1);
+list1.next.next = new ListNode(8);
+list1.next.next.next = new ListNode(4);
+list1.next.next.next.next = new ListNode(5);
+
+const list2 = new ListNode(5);
+list2.next = new ListNode(6);
+list2.next.next = list1.next.next; // Intersection point
+
+const intersection = getIntersectionNode(list1, list2);
+
+if (intersection) {
+  console.log("Intersection found at node with value: " + intersection.val);
+} else {
+  console.log("No intersection found");
 }
