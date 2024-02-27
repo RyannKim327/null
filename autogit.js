c05d04c9103b779895d714c3f5b62bfcf8ac204d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:48:30 +0800

    2-28-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,29 @@
-const str = "Hello, world!";
-const substring = "world";
+function longestCommonSubstring(str1, str2) {
+    const matrix = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
+    let longestSubstring = 0;
+    let longestSubstringEndIndex = 0;
 
-if(str.indexOf(substring) !== -1) {
-    console.log("String contains the substring");
-} else {
-    console.log("String does not contain the substring");
-}
-const str = "Hello, world!";
-const substring = "world";
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                matrix[i][j] = matrix[i - 1][j - 1] + 1;
 
-if(str.includes(substring)) {
-    console.log("String contains the substring");
-} else {
-    console.log("String does not contain the substring");
+                if (matrix[i][j] > longestSubstring) {
+                    longestSubstring = matrix[i][j];
+                    longestSubstringEndIndex = i - 1;
+                }
+            }
+        }
+    }
+
+    if (longestSubstring === 0) {
+        return '';
+    }
+
+    return str1.slice(longestSubstringEndIndex - longestSubstring + 1, longestSubstringEndIndex + 1);
 }
+
+// Example usage
+const str1 = 'abcdef';
+const str2 = 'bcdeft';
+console.log(longestCommonSubstring(str1, str2));  // Output: 'cdef'
