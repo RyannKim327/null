be691a188562fa50b991ffd9365f49271490c705
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:49:28 +0800

    2-28-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,27 @@
-let str = "Hello World";
-let lowercaseStr = str.toLowerCase();
-console.log(lowercaseStr); // Outputs: "hello world"
+import android.os.AsyncTask;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebTask extends AsyncTask<Void, Void, Void> {
+
+    private WebView webView;
+
+    public WebTask(WebView webView) {
+        this.webView = webView;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        // Connect to a website
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.example.com");
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        // Run JavaScript function after the website is loaded
+        webView.evaluateJavascript("javascript: document.getElementById('elementId').innerHTML = 'Hello, World!';", null);
+    }
+}
