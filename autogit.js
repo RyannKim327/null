54bb15681671d362784e950d91c9da0ac0fbe69f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:25:34 +0800

    2-28-2024 6:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,29 @@
-const userInput = prompt("Enter something:");
-console.log("You entered: " + userInput);
+function longestCommonSubstring(str1, str2) {
+    // Initialize a 2D array to store the length of common substrings
+    const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
+
+    // Populate the 2D array with the length of common substrings
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            }
+        }
+    }
+
+    // Extract the longest common substring using the end index
+    const longestCommonSubstr = str1.substring(endIndex - maxLength + 1, endIndex + 1);
+
+    return longestCommonSubstr;
+}
+
+// Test the function with two strings
+const str1 = "abcdefg";
+const str2 = "bcde";
+console.log(longestCommonSubstring(str1, str2)); // Output: "cde"
