277d12933f6ae7617e53bffc42735b052c51da8d
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:49:44 +0800

    2-28-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,26 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+function countingSort(arr) {
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let count = new Array(max - min + 1).fill(0);
+    let output = new Array(arr.length);
+    
+    for (let i = 0; i < arr.length; i++) {
+        count[arr[i] - min]++;
     }
+    
+    for (let i = 1; i < count.length; i++) {
+        count[i] += count[i - 1];
+    }
+    
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
+    }
+    
+    return output;
 }
 
-function getIntersectionNode(headA, headB) {
-    if (!headA || !headB) {
-        return null;
-    }
-
-    let nodesA = [];
-    let nodesB = [];
-
-    let currentA = headA;
-    while (currentA) {
-        nodesA.push(currentA);
-        currentA = currentA.next;
-    }
-
-    let currentB = headB;
-    while (currentB) {
-        nodesB.push(currentB);
-        currentB = currentB.next;
-    }
-
-    let intersectionNode = null;
-    for (let i = nodesA.length - 1, j = nodesB.length - 1; i >= 0 && j >= 0; i--, j--) {
-        if (nodesA[i] === nodesB[j]) {
-            intersectionNode = nodesA[i];
-        } else {
-            break;
-        }
-    }
-
-    return intersectionNode;
-}
+// Test the countingSort function
+let arr = [4, 2, 2, 8, 3, 3, 1];
+console.log("Original array:", arr);
+console.log("Sorted array:", countingSort(arr));
