03c476a26d64a47045a45ff67e5d6d7608d8af78
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:11:07 +0800

    2-28-2024 6:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,3 @@
-let str = "  Hello,   World!   ";
-let newStr = str.replace(/\s/g, "");
-console.log(newStr); // Output: "Hello,World!"
+let array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output: [5, 4, 3, 2, 1]
