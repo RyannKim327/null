84d98cffbe2c6f5bd89edd8b6aba11aa4ac1641f
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:18:48 +0800

    2-28-2024 6:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,47 @@
-function findMedianSortedArrays(nums1, nums2) {
-    let mergedArray = mergeArrays(nums1, nums2);
-
-    const n = mergedArray.length;
-    if (n % 2 === 0) {
-        return (mergedArray[n / 2 - 1] + mergedArray[n / 2]) / 2;
-    } else {
-        return mergedArray[Math.floor(n / 2)];
-    }
-}
-
-function mergeArrays(nums1, nums2) {
-    let merged = [];
-    let i = 0, j = 0;
-
-    while (i < nums1.length && j < nums2.length) {
-        if (nums1[i] < nums2[j]) {
-            merged.push(nums1[i]);
-            i++;
-        } else {
-            merged.push(nums2[j]);
-            j++;
+// A utility function to get the maximum value in the input array
+function getMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
     }
-
-    while (i < nums1.length) {
-        merged.push(nums1[i]);
-        i++;
-    }
-
-    while (j < nums2.length) {
-        merged.push(nums2[j]);
-        j++;
-    }
-
-    return merged;
+    return max;
 }
 
-// Example
-const nums1 = [1, 3];
-const nums2 = [2];
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+// A function to do counting sort of arr[] according to the digit represented by exp.
+function countingSort(arr, exp) {
+    const output = new Array(arr.length);
+    const count = new Array(10).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+        count[Math.floor(arr[i] / exp) % 10]++;
+    }
+
+    for (let i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        output[count[Math.floor(arr[i] / exp) % 10] - 1] = arr[i];
+        count[Math.floor(arr[i] / exp) % 10]--;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = output[i];
+    }
+}
+
+function radixSort(arr) {
+    const max = getMax(arr);
+
+    for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+        countingSort(arr, exp);
+    }
+
+    return arr;
+}
+
+// Example usage
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
