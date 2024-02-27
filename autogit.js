7ac47e7651194e9a7529e653353b20374c4dccbb
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 05:58:10 +0800

    2-28-2024 5:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,13 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function findStringLength(str) {
+    let count = 0;
+    // Iterate through each character of the string
+    for (let char of str) {
+        count++;
+    }
+    return count;
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  add(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  toArray() {
-    let current = this.head;
-    const array = [];
-    while (current) {
-      array.push(current.value);
-      current = current.next;
-    }
-    return array;
-  }
-
-  isPalindrome() {
-    const array = this.toArray();
-    const reversedArray = array.slice().reverse();
-    return JSON.stringify(array) === JSON.stringify(reversedArray);
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.add('a');
-linkedList.add('b');
-linkedList.add('c');
-linkedList.add('b');
-linkedList.add('a');
-
-console.log(linkedList.isPalindrome()); // Output: true
+// Test the function
+let str = "Hello, World!";
+let length = findStringLength(str);
+console.log(length); // Output: 13
