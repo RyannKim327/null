9e6099914b53950af6778e61d280d000abb75b35
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:15:12 +0800

    2-28-2024 6:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,17 @@
-function findFirstRepeatedCharacter(str) {
-    // Create an empty object to keep track of characters we have encountered
-    let charMap = {};
-
-    // Iterate through each character in the string
-    for (let char of str) {
-        // If the character is already in charMap, return it as the first repeated character
-        if (charMap[char]) {
-            return char;
-        } else {
-            // Otherwise, mark the character as encountered by adding it to charMap
-            charMap[char] = true;
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
         }
     }
-
-    // If no repeated character is found, return null
-    return null;
+    return arr;
 }
 
-// Test the function with a sample string
-let str = "abcdeabcd";
-let firstRepeatedChar = findFirstRepeatedCharacter(str);
-console.log("The first repeated character is: " + firstRepeatedChar);
+// Example Usage
+let array = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(array));
