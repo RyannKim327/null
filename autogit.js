e722b3fe6652a7923aec23934db59edb923b8cf5
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:12:10 +0800

    2-28-2024 6:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,42 @@
-function BoyerMooreHorspool(text, pattern) {
-  const patternLength = pattern.length;
-  const textLength = text.length;
-
-  if (patternLength === 0) {
-    return -1;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
-
-  if (patternLength > textLength) {
-    return -1;
-  }
-
-  const badMatchTable = new Array(256).fill(patternLength);
-  
-  for (let i = 0; i < patternLength - 1; i++) {
-    badMatchTable[pattern.charCodeAt(i)] = patternLength - 1 - i;
-  }
-
-  let i = patternLength - 1;
-  while (i < textLength) {
-    let j = patternLength - 1;
-    let k = i;
-
-    while (j >= 0 && text[k] === pattern[j]) {
-      j--;
-      k--;
-    }
-
-    if (j === -1) {
-      return k + 1;
-    }
-
-    i += badMatchTable[text.charCodeAt(i)];
-  }
-
-  return -1;
 }
+function linkedListToArray(head) {
+  let current = head;
+  const values = [];
+  
+  while (current !== null) {
+    values.push(current.value);
+    current = current.next;
+  }
+  
+  return values;
+}
+function isPalindromeLinkedList(head) {
+  const values = linkedListToArray(head);
+  const len = values.length;
+  
+  for (let i = 0; i < len / 2; i++) {
+    if (values[i] !== values[len - 1 - i]) {
+      return false;
+    }
+  }
+  
+  return true;
+}
+// Example linked list
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(2);
+const node5 = new Node(1);
 
-// Example usage:
-const text = 'exampletextforexample';
-const pattern = 'example';
-const index = BoyerMooreHorspool(text, pattern);
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
-console.log('Pattern found at index:', index);
+console.log(isPalindromeLinkedList(node1)); // Output: true
