dae83634ab95268c7a1ebb625fcd2a295d5c6a41
RyannKim327 <rksesgundo123@gmail.com>
2024-02-28 06:06:56 +0800

    2-28-2024 6:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,49 @@
-function isAnagram(str1, str2) {
-    // Remove non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^a-z]/g, '').toLowerCase();
-    str2 = str2.replace(/[^a-z]/g, '').toLowerCase();
+function dijkstra(graph, startNode) {
+  const distances = {};
+  const visited = {};
+  const previous = {};
+  let unvisited = Object.keys(graph);
 
-    // Sort the characters of both strings
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
+  unvisited.forEach(node => {
+    distances[node] = Infinity;
+    previous[node] = null;
+  });
 
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
+  distances[startNode] = 0;
+
+  while (unvisited.length > 0) {
+    const currentNode = minDistanceNode(unvisited, distances);
+    unvisited = unvisited.filter(node => node !== currentNode);
+
+    for (let neighbor in graph[currentNode]) {
+      const distance = distances[currentNode] + graph[currentNode][neighbor];
+
+      if (distance < distances[neighbor]) {
+        distances[neighbor] = distance;
+        previous[neighbor] = currentNode;
+      }
+    }
+  }
+
+  return { distances, previous };
 }
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
+function minDistanceNode(nodes, distances) {
+  return nodes.reduce((minNode, node) =>
+    distances[node] < distances[minNode] ? node : minNode
+  );
 }
+
+// Example graph
+const graph = {
+  A: { B: 3, C: 4 },
+  B: { A: 3, C: 1, D: 7 },
+  C: { A: 4, B: 1, D: 2 },
+  D: { B: 7, C: 2 }
+};
+
+const startNode = 'A';
+const { distances, previous } = dijkstra(graph, startNode);
+
+console.log(distances);
+console.log(previous);
