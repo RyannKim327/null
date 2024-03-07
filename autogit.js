97fe8c0fe4f156ea9b33202b2baa7d7edbcf5dc8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-07 13:08:29 -0400

    3-8-2024 1:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,26 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
+function isAnagram(str1, str2) {
+  // Remove any non-alphabetic characters and convert the strings to lowercase
+  str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
 
-    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+  // Check if the strings are equal in length
+  if (str1.length !== str2.length) {
+    return false;
+  }
 
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
+  // Sort the characters of the strings and compare them
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
 
-    let i = m, j = n;
-    let result = "";
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            result = str1[i - 1] + result;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result;
+  return sortedStr1 === sortedStr2;
 }
 
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-const lcs = longestCommonSubsequence(str1, str2);
-console.log("Longest Common Subsequence: ", lcs);
+// Test the function
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (isAnagram(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
