e4f94edf2c1f7a9a3f6e04a25efe10639f87b96b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:41:18 +0800

    3-14-2024 6:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-function computePrefixArray(pattern) {
-    let prefixArray = [0];
-    let j = 0;
-
-    for (let i = 1; i < pattern.length; i++) {
-        while (j > 0 && pattern.charAt(i) !== pattern.charAt(j)) {
-            j = prefixArray[j - 1];
-        }
-        if (pattern.charAt(i) === pattern.charAt(j)) {
-            j++;
-        }
-        prefixArray[i] = j;
-    }
-
-    return prefixArray;
-}
-function kmpSearch(text, pattern) {
-    const prefixArray = computePrefixArray(pattern);
-    let j = 0;
-
-    for (let i = 0; i < text.length; i++) {
-        while (j > 0 && text.charAt(i) !== pattern.charAt(j)) {
-            j = prefixArray[j - 1];
-        }
-        if (text.charAt(i) === pattern.charAt(j)) {
-            j++;
-        }
-        if (j === pattern.length) {
-            return i - j + 1; // pattern found at index i - j + 1
-        }
-    }
-
-    return -1; // pattern not found
-}
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const index = kmpSearch(text, pattern);
-
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+let str = "   Hello, World!   ";
+str = str.replace(/\s/g, '');
+console.log(str); // Output: "Hello,World!"
+let str = "   Hello, World!   ";
+str = str.split(' ').join('');
+console.log(str); // Output: "Hello,World!"
+let str = "   Hello, World!   ";
+str = str.trim();
+console.log(str); // Output: "Hello, World!"
