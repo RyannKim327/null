41914b5155ff9308f251f0cb50d2b2b1924c0036
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:40:28 +0800

    3-14-2024 6:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,48 @@
-function buildPatternTable(pattern) {
-    let table = [0];
-    let prefixEnd = 0;
-    let suffixEnd = 1;
-    
-    while (suffixEnd < pattern.length) {
-        if (pattern[suffixEnd] === pattern[prefixEnd]) {
-            table[suffixEnd] = prefixEnd + 1;
-            suffixEnd++;
-            prefixEnd++;
-        } else if (prefixEnd === 0) {
-            table[suffixEnd] = 0;
-            suffixEnd++;
-        } else {
-            prefixEnd = table[prefixEnd - 1];
-        }
+class Graph {
+    constructor() {
+        this.adjList = new Map();
     }
-    
-    return table;
-}
 
-function kmpSearch(text, pattern) {
-    let patternTable = buildPatternTable(pattern);
-    let textIndex = 0;
-    let patternIndex = 0;
-    
-    while (textIndex < text.length) {
-        if (text[textIndex] === pattern[patternIndex]) {
-            if (patternIndex === pattern.length - 1) {
-                return textIndex - pattern.length + 1;
+    addVertex(v) {
+        this.adjList.set(v, []);
+    }
+
+    addEdge(v, w) {
+        this.adjList.get(v).push(w);
+    }
+
+    depthFirstSearch(startingNode) {
+        let visited = {};
+        this._dfs(startingNode, visited);
+    }
+
+    _dfs(node, visited) {
+        visited[node] = true;
+        console.log(node);
+
+        let neighbors = this.adjList.get(node);
+
+        for (let i = 0; i < neighbors.length; i++) {
+            let currentNode = neighbors[i];
+            if(!visited[currentNode]) {
+                this._dfs(currentNode, visited);
             }
-            patternIndex++;
-            textIndex++;
-        } else if (patternIndex > 0) {
-            patternIndex = patternTable[patternIndex - 1];
-        } else {
-            patternIndex = 0;
-            textIndex++;
         }
     }
-    
-    return -1;
 }
 
-let text = "ababcababa";
-let pattern = "aba";
-let index = kmpSearch(text, pattern);
+// Example usage
+const graph = new Graph();
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
 
-console.log(`Pattern found at index: ${index}`);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+graph.addEdge(2, 5);
+
+console.log('Depth First Search:');
+graph.depthFirstSearch(1);
