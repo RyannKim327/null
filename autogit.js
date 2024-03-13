5008eea88f51e2967c0a74de3d3f324227708d4a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:40:53 +0800

    3-14-2024 6:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,12 @@
-class SkipListNodenode {
-    constructor(value, levels) {
-        this.value = value;
-        this.levels = new Array(levels).fill(null);
-    }
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+
+// Find the index of the element to remove
+let index = array.indexOf(elementToRemove);
+
+if (index > -1) {
+    // Remove the element at the index
+    array.splice(index, 1);
 }
-class SkipList {
-    constructor(maxLevels, probability) {
-        this.maxLevels = maxLevels;
-        this.probability = probability;
-        this.head = new SkipListNode(-1, maxLevels);
-        this.levelCount = 1;
-    }
 
-    insert(value) {
-        const node = new SkipListNode(value, this.randomLevel());
-        const update = new Array(this.maxLevels).fill(null);
-
-        let current = this.head;
-        for (let i = this.levelCount - 1; i >= 0; i--) {
-            while (current.levels[i] !== null && current.levels[i].value < value) {
-                current = current.levels[i];
-            }
-            update[i] = current;
-        }
-
-        for (let i = 0; i < node.levels.length; i++) {
-            node.levels[i] = update[i].levels[i];
-            update[i].levels[i] = node;
-        }
-
-        if (node.levels[0] !== null) {
-            node.levels[0].prev = node;
-        }
-
-        if (this.levelCount < node.levels.length) {
-            this.levelCount = node.levels.length;
-        }
-    }
-
-    search(value) {
-        let current = this.head;
-        for (let i = this.levelCount - 1; i >= 0; i--) {
-            while (current.levels[i] !== null && current.levels[i].value < value) {
-                current = current.levels[i];
-            }
-        }
-
-        if (current.levels[0] !== null && current.levels[0].value === value) {
-            return current.levels[0];
-        } else {
-            return null;
-        }
-    }
-
-    randomLevel() {
-        let level = 1;
-        while (Math.random() < this.probability && level < this.maxLevels) {
-            level++;
-        }
-        return level;
-    }
-}
+console.log(array); // Output: [1, 2, 4, 5]
