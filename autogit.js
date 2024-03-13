86490c72d8dffcd45b982d6a4528adf11765c4c1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:41:54 +0800

    3-14-2024 6:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,80 +1,99 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+    constructor(start, end) {
+        this.start = start;
+        this.end = end;
+        this.children = {};
+        this.suffixLink = null;
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-        this.tail = null;
+class SuffixTree {
+    constructor(str) {
+        this.root = new Node(-1, -1);
+        this.activeNode = this.root;
+        this.activeEdge = 0;
+        this.activeLength = 0;
+        this.remainingSuffixCount = 0;
+        this.end = -1;
+        this.str = str;
+
+        this.buildSuffixTree();
     }
 
-    append(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+    buildSuffixTree() {
+        for (let i = 0; i < this.str.length; i++) {
+            this.extendSuffixTree(i);
         }
     }
 
-    remove() {
-        if (!this.head) {
-            return null;
-        }
+    extendSuffixTree(pos) {
+        this.end = pos;
 
-        const data = this.head.data;
-        this.head = this.head.next;
-        return data;
+        this.remainingSuffixCount++;
+        let lastCreatedNode = null;
+
+        while (this.remainingSuffixCount > 0) {
+            if (this.activeLength === 0) {
+                this.activeEdge = pos;
+            }
+
+            if (!this.activeNode.children[this.str[this.activeEdge]]) {
+                this.activeNode.children[this.str[this.activeEdge]] = new Node(pos, this.end);
+                if (lastCreatedNode !== null) {
+                    lastCreatedNode.suffixLink = this.activeNode;
+                    lastCreatedNode = null;
+                }
+            } else {
+                let next = this.activeNode.children[this.str[this.activeEdge]];
+                if (this.walkDown(next)) {
+                    continue;
+                }
+
+                if (this.str[next.start + this.activeLength] == this.str[pos]) {
+                    if (lastCreatedNode !== null && this.activeNode !== this.root) {
+                        lastCreatedNode.suffixLink = this.activeNode;
+                        lastCreatedNode = null;
+                    }
+
+                    this.activeLength++;
+                    break;
+                }
+
+                let splitEnd = next.start + this.activeLength - 1;
+                let splitNode = new Node(next.start, splitEnd);
+                this.activeNode.children[this.str[this.activeEdge]] = splitNode;
+                splitNode.children[this.str[pos]] = new Node(pos, this.end);
+                next.start += this.activeLength;
+                splitNode.children[this.str[next.start]] = next;
+
+                if (lastCreatedNode !== null) {
+                    lastCreatedNode.suffixLink = splitNode;
+                }
+
+                lastCreatedNode = splitNode;
+            }
+
+            this.remainingSuffixCount--;
+
+            if (this.activeNode === this.root && this.activeLength > 0) {
+                this.activeLength--;
+                this.activeEdge = pos - this.remainingSuffixCount + 1;
+            } else if (this.activeNode !== this.root) {
+                this.activeNode = this.activeNode.suffixLink || this.root;
+            }
+        }
+    }
+
+    walkDown(next) {
+        if (this.activeLength >= next.end - next.start + 1) {
+            this.activeEdge += next.end - next.start + 1;
+            this.activeLength -= next.end - next.start + 1;
+            this.activeNode = next;
+            return true;
+        }
+        return false;
     }
 }
 
-class Queue {
-    constructor() {
-        this.list = new LinkedList();
-    }
-
-    enqueue(data) {
-        this.list.append(data);
-    }
-
-    dequeue() {
-        return this.list.remove();
-    }
-
-    peek() {
-        return this.list.head ? this.list.head.data : null;
-    }
-
-    isEmpty() {
-        return this.list.head === null;
-    }
-
-    print() {
-        let current = this.list.head;
-        const elements = [];
-        while (current) {
-            elements.push(current.data);
-            current = current.next;
-        }
-        console.log(elements.join(' <- '));
-    }
-}
-
-// Example usage:
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-queue.print(); // Output: 1 <- 2 <- 3
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.peek());    // Output: 2
-
-queue.print(); // Output: 2 <- 3
+// Example usage
+const suffixTree = new SuffixTree("banana");
