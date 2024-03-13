23fc4c292c4b329d40801668db2390c3ede3b735
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:42:13 +0800

    3-14-2024 6:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,41 @@
-function bubbleSort(arr) {
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+class Node {
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
-    return arr;
 }
 
-const myArray = [5, 3, 8, 2, 1, 4];
-console.log('Before sorting:', myArray);
-console.log('After sorting:', bubbleSort(myArray));
+class BinaryTree {
+    constructor(root) {
+        this.root = root;
+    }
+
+    sumAllNodes(node) {
+        if (node === null) {
+            return 0;
+        }
+
+        return node.value + this.sumAllNodes(node.left) + this.sumAllNodes(node.right);
+    }
+
+    getSumOfAllNodes() {
+        return this.sumAllNodes(this.root);
+    }
+}
+
+// Creating a binary tree
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+node1.left = node2;
+node1.right = node3;
+node2.left = node4;
+node2.right = node5;
+
+const binaryTree = new BinaryTree(node1);
+
+console.log("Sum of all nodes in the binary tree: ", binaryTree.getSumOfAllNodes());
