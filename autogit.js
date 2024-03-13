d6d0aa5a47813e46d958612ec6c007775fe29ceb
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:43:27 +0800

    3-14-2024 6:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,8 @@
-function depthLimitedSearch(node, goal, depthLimit) {
-    return recursiveDLS(node, goal, depthLimit);
+function reverseString(str) {
+    return str.split("").reverse().join("");
 }
 
-function recursiveDLS(node, goal, depthLimit) {
-    if (node.state === goal) {
-        return node;
-    } else if (depthLimit === 0) {
-        return 'cutoff';
-    } else {
-        let cutoffOccurred = false;
-        let children = expand(node);
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
 
-        for (let i = 0; i < children.length; i++) {
-            let result = recursiveDLS(children[i], goal, depthLimit - 1);
-
-            if (result === 'cutoff') {
-                cutoffOccurred = true;
-            } else if (result !== 'failure') {
-                return result;
-            }
-        }
-
-        if (cutoffOccurred) {
-            return 'cutoff';
-        } else {
-            return 'failure';
-        }
-    }
-}
-
-function expand(node) {
-    // Here you can define how to generate child nodes based on the current node
-    // For example, generate child nodes by applying possible actions to the current node
-    return [];
-}
-
-// Example usage
-class Node {
-    constructor(state) {
-        this.state = state;
-    }
-}
-
-let rootNode = new Node(0);
-let goal = 10;
-let depthLimit = 3;
-
-let result = depthLimitedSearch(rootNode, goal, depthLimit);
-console.log(result);
+console.log(reversedString);
