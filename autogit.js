d74b183e4862d728b12b00d494bb789eb799ca1e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:44:02 +0800

    3-14-2024 6:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-
-  push(item) {
-    this.stack.push(item);
-  }
-
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.stack.pop();
-  }
-
-  isEmpty() {
-    return this.stack.length === 0;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      return "Stack is empty";
-    }
-    return this.stack[this.stack.length - 1];
-  }
-
-  printStack() {
-    console.log(this.stack);
-  }
+function decimalToBinary(decimal) {
+    return decimal.toString(2);
 }
 
 // Example usage
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
-
-stack.printStack(); // Output: [1, 2, 3]
-
-console.log(stack.pop()); // Output: 3
-console.log(stack.peek()); // Output: 2
+const decimalNumber = 42;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(`Binary representation of ${decimalNumber} is: ${binaryNumber}`);
