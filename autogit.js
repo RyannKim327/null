4a3836ac5fae094a4c3201cfedb907726b82c333
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:40:19 +0800

    3-14-2024 6:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,49 @@
-const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+function buildPatternTable(pattern) {
+    let table = [0];
+    let prefixEnd = 0;
+    let suffixEnd = 1;
+    
+    while (suffixEnd < pattern.length) {
+        if (pattern[suffixEnd] === pattern[prefixEnd]) {
+            table[suffixEnd] = prefixEnd + 1;
+            suffixEnd++;
+            prefixEnd++;
+        } else if (prefixEnd === 0) {
+            table[suffixEnd] = 0;
+            suffixEnd++;
+        } else {
+            prefixEnd = table[prefixEnd - 1];
+        }
+    }
+    
+    return table;
+}
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const firstUser = data[0];
-    console.log('First user:', firstUser);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+function kmpSearch(text, pattern) {
+    let patternTable = buildPatternTable(pattern);
+    let textIndex = 0;
+    let patternIndex = 0;
+    
+    while (textIndex < text.length) {
+        if (text[textIndex] === pattern[patternIndex]) {
+            if (patternIndex === pattern.length - 1) {
+                return textIndex - pattern.length + 1;
+            }
+            patternIndex++;
+            textIndex++;
+        } else if (patternIndex > 0) {
+            patternIndex = patternTable[patternIndex - 1];
+        } else {
+            patternIndex = 0;
+            textIndex++;
+        }
+    }
+    
+    return -1;
+}
+
+let text = "ababcababa";
+let pattern = "aba";
+let index = kmpSearch(text, pattern);
+
+console.log(`Pattern found at index: ${index}`);
