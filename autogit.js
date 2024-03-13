054f0dc9fe2649df987809f7fffa494862a9cafd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-14 06:45:01 +0800

    3-14-2024 6:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,70 +1,8 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.left = null;
-        this.right = null;
-    }
-}
+let str = "  Hello, World!   ";
+let trimmedStr = str.trim();
 
-class BinarySearchTree {
-    constructor() {
-        this.root = null;
-    }
+console.log(trimmedStr); // Output: "Hello, World!"
+let str = "  Hello,    World!   ";
+let trimmedStr = str.replace(/\s/g, '');
 
-    insert(data) {
-        let newNode = new Node(data);
-
-        if (this.root === null) {
-            this.root = newNode;
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
-
-    insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-            if (node.left === null) {
-                node.left = newNode;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-            } else {
-                this.insertNode(node.right, newNode);
-            }
-        }
-    }
-
-    search(data) {
-        return this.searchNode(this.root, data);
-    }
-
-    searchNode(node, data) {
-        if (node === null) {
-            return false;
-        }
-
-        if (data < node.data) {
-            return this.searchNode(node.left, data);
-        } else if (data > node.data) {
-            return this.searchNode(node.right, data);
-        } else {
-            return true;
-        }
-    }
-}
-
-// Example Usage:
-const bst = new BinarySearchTree();
-
-bst.insert(10);
-bst.insert(5);
-bst.insert(15);
-bst.insert(3);
-bst.insert(7);
-bst.insert(12);
-
-console.log(bst.search(7)); // Output: true
-console.log(bst.search(20)); // Output: false
+console.log(trimmedStr); // Output: "Hello,World!"
