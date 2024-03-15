453474bf3b5f58fde6037013f30c813352baa384
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:44:59 +0800

    3-15-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,24 @@
-function reverseWords(str) {
-    return str.split(" ").reverse().join(" ");
+function selectionSort(arr) {
+    const n = arr.length;
+    
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        
+        for (let j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    
+    return arr;
 }
 
-const originalString = "Hello world, this is a test";
-const reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: "test a is this world, Hello"
+// Example usage
+const array = [64, 25, 12, 22, 11];
+console.log("Original array: ", array);
+console.log("Sorted array: ", selectionSort(array));
