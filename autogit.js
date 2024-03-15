142109b40f204a012219f73850c04f029e2ef84e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:54:40 +0800

    3-15-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,51 @@
-// Make a GET request to a website and log the response
-fetch('https://api.example.com/data')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
+
+  addVertex(vertex) {
+    this.adjList.set(vertex, []);
+  }
+
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+  }
+
+  dfsUtil(vertex, visited) {
+    visited.add(vertex);
+
+    console.log(vertex);
+
+    let neighbours = this.adjList.get(vertex);
+
+    for (let neighbour of neighbours) {
+      if (!visited.has(neighbour)) {
+        this.dfsUtil(neighbour, visited);
+      }
     }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('There was a problem with your fetch operation:', error);
-  });
+  }
+
+  dfs(startVertex) {
+    let visited = new Set();
+
+    this.dfsUtil(startVertex, visited);
+  }
+}
+
+// Test the implementation
+
+let graph = new Graph();
+
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('B', 'E');
+
+console.log('Depth First Traversal:');
+graph.dfs('A');
