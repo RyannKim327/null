c929a2a08d972ca7326d3f0b0d0b887be9c7d1c4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:44:27 +0800

    3-15-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,18 @@
-function dijkstra(graph, source) {
-    let distance = {};
-    let visited = {};
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr = str.replace(/[\W_]/g, '').toLowerCase();
     
-    // Initialize distance to all nodes as Infinity
-    for (let node in graph) {
-        distance[node] = Infinity;
-    }
+    // Reverse the cleaned string
+    const reversedStr = cleanStr.split('').reverse().join('');
     
-    // The distance from the source to itself is 0
-    distance[source] = 0;
-    
-    // Main loop
-    while (true) {
-        let minDistance = Infinity;
-        let minNode = null;
-        
-        // Find the node with the smallest distance
-        for (let node in graph) {
-            if (!visited[node] && distance[node] < minDistance) {
-                minDistance = distance[node];
-                minNode = node;
-            }
-        }
-        
-        if (minNode === null) {
-            break;
-        }
-        
-        // Update the distances of neighbors of the current node
-        for (let neighbor in graph[minNode]) {
-            let weight = graph[minNode][neighbor];
-            let totalDistance = distance[minNode] + weight;
-            
-            if (totalDistance < distance[neighbor]) {
-                distance[neighbor] = totalDistance;
-            }
-        }
-        
-        visited[minNode] = true;
-    }
-    
-    return distance;
+    // Check if the original string is equal to the reversed string
+    return cleanStr === reversedStr;
 }
 
-// Example graph
-let graph = {
-    'A': { 'B': 5, 'C': 3 },
-    'B': { 'A': 5, 'C': 1, 'D': 2 },
-    'C': { 'A': 3, 'B': 1, 'D': 4 },
-    'D': { 'B': 2, 'C': 4 }
-};
-
-// Find shortest paths from node 'A'
-let shortestPaths = dijkstra(graph, 'A');
-
-console.log(shortestPaths);
+// Test the function
+const testString = "A man, a plan, a canal, Panama!";
+if (isPalindrome(testString)) {
+    console.log(testString + " is a palindrome.");
+} else {
+    console.log(testString + " is not a palindrome.");
+}
