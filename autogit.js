b67f590d50e2c55797c4af32d5fa59d83cc40d25
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:45:28 +0800

    3-15-2024 22:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,53 @@
-// Taking input from the user
-var userInput = prompt("Enter something: ");
+function buildPatternTable(pattern) {
+    let table = Array(pattern.length).fill(0);
+    let j = 0;
+    
+    for (let i = 1; i < pattern.length; i++) {
+        if (pattern[i] === pattern[j]) {
+            table[i] = j + 1;
+            j++;
+        } else {
+            if (j !== 0) {
+                j = table[j - 1];
+                i--;
+            } else {
+                table[i] = 0;
+            }
+        }
+    }
+    
+    return table;
+}
 
-// Displaying the input on the console
-console.log("User input: " + userInput);
+function knuthMorrisPratt(text, pattern) {
+    let table = buildPatternTable(pattern);
+    
+    let i = 0;
+    let j = 0;
+    let matches = [];
+    
+    while (i < text.length) {
+        if (text[i] === pattern[j]) {
+            i++;
+            j++;
+            if (j === pattern.length) {
+                matches.push(i - j);
+                j = 0;
+            }
+        } else {
+            if (j !== 0) {
+                j = table[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    return matches;
+}
+
+// Example usage
+let text = "ABABDABACDABABCABAB";
+let pattern = "ABABCABAB";
+let matches = knuthMorrisPratt(text, pattern);
+console.log(matches);
