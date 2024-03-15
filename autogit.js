22302fe3758169d3b275839919461dd548b465dc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:54:14 +0800

    3-15-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,15 @@
-// Get user input
-var userInput = prompt("Enter something:");
+function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-// Display the user input
-console.log("You entered: " + userInput);
+// Example
+const myArray = [1, 2, 3, 4, 5];
+console.log(isSorted(myArray)); // Output: true
+
+const unsortedArray = [5, 4, 3, 2, 1];
+console.log(isSorted(unsortedArray)); // Output: false
