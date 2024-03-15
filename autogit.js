d2fc0313f66274affb0039ea6369c41fef70629d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:35:07 +0800

    3-15-2024 22:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,49 @@
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-// Example usage
-const email = 'example@example.com';
-if (validateEmail(email)) {
-    console.log('Email is valid');
-} else {
-    console.log('Email is not valid');
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  findMiddleElement() {
+    let slow = this.head;
+    let fast = this.head;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow.data;
+  }
 }
+
+// Create a linked list
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+list.addNode(5);
+
+// Find the middle element
+const middleElement = list.findMiddleElement();
+console.log("Middle Element:", middleElement);
