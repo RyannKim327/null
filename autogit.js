d05310b2146b05b841fcbaf09efd9d9b48636a5d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:42:44 +0800

    3-15-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,14 @@
-function decimalToBinary(decimalNumber) {
-    return (decimalNumber >>> 0).toString(2);
+function isSortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Usage
-console.log(decimalToBinary(10)); // Output: 1010
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [5, 4, 3, 2, 1];
+
+console.log(isSortedAscending(array1)); // Output: true
+console.log(isSortedAscending(array2)); // Output: false
