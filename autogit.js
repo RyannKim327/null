ea2068b2b99daf4e886d76a7e58ad0a26280ac92
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:53:01 +0800

    3-15-2024 22:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,44 @@
-function findFirstRepeatedChar(str) {
-    let charMap = {};
-    
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
+class Stack {
+    constructor() {
+        this.stack = [];
     }
-    
-    return null; // If there are no repeated characters
+
+    // Push element onto the stack
+    push(element) {
+        this.stack.push(element);
+    }
+
+    // Pop element from the stack
+    pop() {
+        if (this.stack.length === 0) {
+            return "Stack is empty";
+        }
+        return this.stack.pop();
+    }
+
+    // Peek at the top element of the stack
+    peek() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    // Check if the stack is empty
+    isEmpty() {
+        return this.stack.length === 0;
+    }
+
+    // Get the size of the stack
+    size() {
+        return this.stack.length;
+    }
 }
 
-let inputString = "hello world";
-let firstRepeatedChar = findFirstRepeatedChar(inputString);
+// Example usage
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
 
-if (firstRepeatedChar) {
-    console.log("The first repeated character is: " + firstRepeatedChar);
-} else {
-    console.log("There are no repeated characters in the string.");
-}
+console.log(stack.pop());  // Outputs 3
+console.log(stack.peek()); // Outputs 2
+console.log(stack.size()); // Outputs 2
+console.log(stack.isEmpty()); // Outputs false
