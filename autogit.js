4b58f68497c1068b479b85b105a6c580130b5f7b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:48:45 +0800

    3-15-2024 22:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,121 +1,29 @@
 class Node {
-    constructor(data, color) {
-        this.data = data;
-        this.color = color;
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
-        this.parent = null;
     }
 }
 
-class RedBlackTree {
-    constructor() {
-        this.root = null;
+// Function to calculate sum of all nodes in a binary tree using depth-first traversal
+function calculateSum(root) {
+    if (root === null) {
+        return 0;
     }
 
-    insert(data) {
-        let newNode = new Node(data, 'red');
-        if (this.root === null) {
-            this.root = newNode;
-            this.root.color = 'black';
-        } else {
-            this.insertNode(this.root, newNode);
-        }
-    }
-
-    insertNode(node, newNode) {
-        if (newNode.data < node.data) {
-            if (node.left === null) {
-                node.left = newNode;
-                newNode.parent = node;
-            } else {
-                this.insertNode(node.left, newNode);
-            }
-        } else {
-            if (node.right === null) {
-                node.right = newNode;
-                newNode.parent = node;
-            } else {
-                this.insertNode(node.right, newNode);
-            }
-        }
-        
-        this.fixTree(newNode);
-    }
-
-    fixTree(node) {
-        while (node !== this.root && node.parent.color === 'red') {
-            if (node.parent === node.parent.parent.left) {
-                let uncle = node.parent.parent.right;
-                if (uncle && uncle.color === 'red') {
-                    node.parent.color = 'black';
-                    uncle.color = 'black';
-                    node.parent.parent.color = 'red';
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.right) {
-                        node = node.parent;
-                        this.rotateLeft(node);
-                    }
-                    node.parent.color = 'black';
-                    node.parent.parent.color = 'red';
-                    this.rotateRight(node.parent.parent);
-                }
-            } else {
-                let uncle = node.parent.parent.left;
-                if (uncle && uncle.color === 'red') {
-                    node.parent.color = 'black';
-                    uncle.color = 'black';
-                    node.parent.parent.color = 'red';
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.left) {
-                        node = node.parent;
-                        this.rotateRight(node);
-                    }
-                    node.parent.color = 'black';
-                    node.parent.parent.color = 'red';
-                    this.rotateLeft(node.parent.parent);
-                }
-            }
-        }
-        
-        this.root.color = 'black';
-    }
-
-    rotateLeft(node) {
-        let temp = node.right;
-        node.right = temp.left;
-        if (temp.left !== null) {
-            temp.left.parent = node;
-        }
-        temp.parent = node.parent;
-        if (node.parent === null) {
-            this.root = temp;
-        } else if (node === node.parent.left) {
-            node.parent.left = temp;
-        } else {
-            node.parent.right = temp;
-        }
-        temp.left = node;
-        node.parent = temp;
-    }
-
-    rotateRight(node) {
-        let temp = node.left;
-        node.left = temp.right;
-        if (temp.right !== null) {
-            temp.right.parent = node;
-        }
-        temp.parent = node.parent;
-        if (node.parent === null) {
-            this.root = temp;
-        } else if (node === node.parent.right) {
-            node.parent.right = temp;
-        } else {
-            node.parent.left = temp;
-        }
-        temp.right = node;
-        node.parent = temp;
-    }
+    return root.value + calculateSum(root.left) + calculateSum(root.right);
 }
+
+// Create a binary tree
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+// Calculate the sum
+const totalSum = calculateSum(root);
+console.log(totalSum); // Output should be 28 for the provided example tree
