e8aba741deee0fdfdde6804548fc7543c51cdfcf
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:47:57 +0800

    3-15-2024 22:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,8 @@
-function quicksort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return quicksort(left).concat(pivot, quicksort(right));
-}
-
-// Testing the quicksort function
-const arr = [4, 2, 7, 1, 3, 6, 5];
-const sortedArr = quicksort(arr);
-console.log(sortedArr);
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
