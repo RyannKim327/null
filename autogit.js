8453db5dca3bf1de402d727777be54342b98d3d2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:40:27 +0800

    3-15-2024 22:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,75 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(value, priority) {
+        const node = { value, priority };
+        this.heap.push(node);
+        this.heapifyUp();
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        const minNode = this.heap[0];
+        const lastNode = this.heap.pop();
+
+        if (this.heap.length > 0) {
+            this.heap[0] = lastNode;
+            this.heapifyDown();
+        }
+
+        return minNode.value;
+    }
+
+    heapifyUp() {
+        let index = this.heap.length - 1;
+
+        while (index > 0) {
+            const parentIndex = Math.floor((index - 1) / 2);
+
+            if (this.heap[parentIndex].priority > this.heap[index].priority) {
+                [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]];
+                index = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    heapifyDown() {
+        let index = 0;
+
+        while (true) {
+            const leftChildIndex = 2 * index + 1;
+            const rightChildIndex = 2 * index + 2;
+            let smallestChildIndex = index;
+
+            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].priority < this.heap[smallestChildIndex].priority) {
+                smallestChildIndex = leftChildIndex;
+            }
+
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[smallestChildIndex].priority) {
+                smallestChildIndex = rightChildIndex;
+            }
+
+            if (smallestChildIndex !== index) {
+                [this.heap[index], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[index]];
+                index = smallestChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
 }
+const pq = new PriorityQueue();
+pq.insert("Task 1", 3);
+pq.insert("Task 2", 1);
+pq.insert("Task 3", 5);
+pq.insert("Task 4", 2);
 
-let inputString = "Hello World!";
-let result = removeVowels(inputString);
-
-console.log(result); // Output: "Hll Wrld!"
+console.log(pq.extractMin()); // Output: Task 2
+console.log(pq.extractMin()); // Output: Task 4
