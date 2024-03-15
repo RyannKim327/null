a6c5c41a179b98713edc00f3f45650931b8ad5f0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:41:08 +0800

    3-15-2024 22:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,100 @@
-class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+class Node {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
+    }
 }
 
-function sumBinaryTree(root) {
-  if (root === null) {
-    return 0;
-  }
+class SkipList {
+    constructor(maxLevel, probability) {
+        this.maxLevel = maxLevel;
+        this.probability = probability;
+        this.header = new Node(null, maxLevel);
+        this.level = 0;
+    }
 
-  return root.value + sumBinaryTree(root.left) + sumBinaryTree(root.right);
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.probability && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        const update = new Array(this.maxLevel + 1);
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current === null || current.value !== value) {
+            const newLevel = this.randomLevel();
+
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i < newLevel + 1; i++) {
+                    update[i] = this.header;
+                }
+                this.level = newLevel;
+            }
+
+            const newNode = new Node(value, newLevel);
+
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    search(value) {
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+
+        current = current.forward[0];
+
+        if (current !== null && current.value === value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    delete(value) {
+        const update = new Array(this.maxLevel + 1);
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current !== null && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].forward[i] !== current) {
+                    break;
+                }
+                update[i].forward[i] = current.forward[i];
+            }
+
+            while (this.level > 0 && this.header.forward[this.level] === null) {
+                this.level--;
+            }
+        }
+    }
 }
-
-// Example binary tree
-let root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-
-let sum = sumBinaryTree(root);
-console.log("Sum of all nodes in the binary tree: " + sum); // Output: 15
