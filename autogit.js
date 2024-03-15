d219beb689895aecba3dae0690525f523a065b93
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:47:08 +0800

    3-15-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,60 @@
-function insertionSort(arr) {
-    const n = arr.length;
-    for (let i = 1; i < n; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = current;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-    return arr;
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addNode(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    findNthNodeFromEnd(n) {
+        let firstPointer = this.head;
+        let secondPointer = this.head;
+
+        // Move the first pointer by n steps
+        for (let i = 0; i < n; i++) {
+            if (firstPointer === null) {
+                return null; // n is greater than the length of the list
+            }
+            firstPointer = firstPointer.next;
+        }
+
+        // Move both pointers until the first pointer reaches the end of the list
+        while (firstPointer !== null) {
+            firstPointer = firstPointer.next;
+            secondPointer = secondPointer.next;
+        }
+
+        return secondPointer;
+    }
 }
 
 // Example usage
-const arr = [5, 2, 4, 6, 1, 3];
-const sortedArr = insertionSort(arr);
-console.log(sortedArr);  // Output: [1, 2, 3, 4, 5, 6]
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+
+// Find the 2nd node from the end
+const n = 2;
+const nthNodeFromEnd = linkedList.findNthNodeFromEnd(n);
+
+console.log(`Value of the ${n}th node from the end: ${nthNodeFromEnd ? nthNodeFromEnd.value : null}`);
