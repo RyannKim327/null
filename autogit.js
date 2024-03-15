381433df0f4fff5f6830bebb45a0afeba15d2d2b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:38:26 +0800

    3-15-2024 22:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,89 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+class Graph {
+  constructor() {
+    this.nodes = [];
+    this.edges = {};
+  }
+
+  addNode(node) {
+    this.nodes.push(node);
+    this.edges[node] = [];
+  }
+
+  addEdge(node1, node2, cost) {
+    this.edges[node1].push({ node: node2, cost });
+    this.edges[node2].push({ node: node1, cost });
+  }
 }
 
-// Example:
-var randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber); // Output will be a random number between 1 and 10 (inclusive)
+class PriorityQueue {
+  constructor() {
+    this.elements = [];
+  }
+
+  enqueue(element, priority) {
+    this.elements.push({ element, priority });
+    this.elements.sort((a, b) => a.priority - b.priority);
+  }
+
+  dequeue() {
+    return this.elements.shift().element;
+  }
+
+  isEmpty() {
+    return this.elements.length === 0;
+  }
+}
+
+function heuristic(node1, node2) {
+  // Calculate the Euclidean distance between two nodes as the heuristic function
+  return Math.sqrt((node1.x - node2.x) ** 2 + (node1.y - node2.y) ** 2);
+}
+
+function astar(graph, start, goal) {
+  let cameFrom = {};
+  let costSoFar = {};
+  let frontier = new PriorityQueue();
+
+  frontier.enqueue(start, 0);
+  cameFrom[start] = null;
+  costSoFar[start] = 0;
+
+  while (!frontier.isEmpty()) {
+    let current = frontier.dequeue();
+
+    if (current === goal) {
+      break;
+    }
+
+    for (let { node, cost } of graph.edges[current]) {
+      let newCost = costSoFar[current] + cost;
+      if (!(node in costSoFar) || newCost < costSoFar[node]) {
+        costSoFar[node] = newCost;
+        let priority = newCost + heuristic(goal, node);
+        frontier.enqueue(node, priority);
+        cameFrom[node] = current;
+      }
+    }
+  }
+
+  let path = [goal];
+  while (path[path.length - 1] !== start) {
+    path.push(cameFrom[path[path.length - 1]]);
+  }
+  
+  return path.reverse();
+}
+
+// Usage example
+let graph = new Graph();
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+graph.addEdge("A", "B", 1);
+graph.addEdge("A", "C", 3);
+graph.addEdge("B", "D", 5);
+graph.addEdge("C", "D", 1);
+
+let path = astar(graph, "A", "D");
+console.log(path);
