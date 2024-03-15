f5f5b4a547c47ece57d0fbf9477e46a6e8347863
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:54:08 +0800

    3-15-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,5 @@
-function heapify(arr, n, i) {
-    let largest = i;
-    let left = 2 * i + 1;
-    let right = 2 * i + 2;
+// Get user input
+var userInput = prompt("Enter something:");
 
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
-    }
-
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-
-    if (largest !== i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapify(arr, n, largest);
-    }
-}
-
-function heapSort(arr) {
-    const n = arr.length;
-
-    // Build max heap
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-
-    // Heap sort
-    for (let i = n - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapify(arr, i, 0);
-    }
-
-    return arr;
-}
-
-// Test the heap sort implementation
-const arr = [12, 11, 13, 5, 6, 7];
-console.log("Original array: ", arr);
-console.log("Sorted array:", heapSort(arr));
+// Display the user input
+console.log("You entered: " + userInput);
