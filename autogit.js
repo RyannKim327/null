96025d7281e46b9d436418e6e0c77bbcd35b9e50
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:52:54 +0800

    3-15-2024 22:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,22 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function findFirstRepeatedChar(str) {
+    let charMap = {};
+    
+    for (let char of str) {
+        if (charMap[char]) {
+            return char;
+        } else {
+            charMap[char] = true;
+        }
+    }
+    
+    return null; // If there are no repeated characters
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+let inputString = "hello world";
+let firstRepeatedChar = findFirstRepeatedChar(inputString);
 
-  append(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  findMiddleElement() {
-    if (!this.head) {
-      return null;
-    }
-    
-    let slow = this.head;
-    let fast = this.head;
-    
-    while (fast && fast.next) {
-      slow = slow.next;
-      fast = fast.next.next;
-    }
-    
-    return slow.data;
-  }
+if (firstRepeatedChar) {
+    console.log("The first repeated character is: " + firstRepeatedChar);
+} else {
+    console.log("There are no repeated characters in the string.");
 }
-
-// Usage
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-list.append(5);
-
-console.log(list.findMiddleElement()); // Output: 3
