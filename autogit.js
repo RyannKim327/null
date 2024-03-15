2490078b1675433063c5da4e7100878d1b8ae2dd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:39:00 +0800

    3-15-2024 22:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,7 @@
-function longestCommonSubstring(str1, str2) {
-    const table = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
-    let longest = 0;
-    let endIndex = 0;
+const cron = require('cron');
 
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                table[i][j] = table[i - 1][j - 1] + 1;
-                if (table[i][j] > longest) {
-                    longest = table[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                table[i][j] = 0;
-            }
-        }
-    }
+const job = new cron.CronJob('* * * * *', function() {
+  console.log('This job runs every minute');
+}, null, true);
 
-    return str1.substring(endIndex - longest + 1, endIndex + 1);
-}
-
-const str1 = "abcdefg";
-const str2 = "xbcde";
-console.log(longestCommonSubstring(str1, str2)); // Output: "bcde"
+job.start();
