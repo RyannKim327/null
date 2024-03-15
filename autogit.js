a6c5b48ae61785b293a3f5fe42ca16480874d0eb
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:50:25 +0800

    3-15-2024 22:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,26 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function isAnagram(str1, str2) {
+    // Remove any non-alphanumeric characters and convert the strings to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+
+    // Check if the length of the strings are different
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Sort the characters of the strings and compare them
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+}
+
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
