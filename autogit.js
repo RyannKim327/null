dd08f2da906ffdbf454f3385ed7ce6bc35f3c3e6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:50:01 +0800

    3-15-2024 22:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,79 @@
-function insertionSort(arr) {
-    for (var i = 1; i < arr.length; i++) {
-        var current = arr[i];
-        var j = i - 1;
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = current;
+class BinaryHeap {
+    constructor() {
+        this.heap = [];
     }
-    return arr;
+
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    insert(value, priority) {
+        const newNode = { value, priority };
+        this.heap.push(newNode);
+        this.heapifyUp();
+    }
+
+    heapifyUp() {
+        let currentIndex = this.heap.length - 1;
+        while (currentIndex > 0) {
+            const parentIndex = this.getParentIndex(currentIndex);
+            if (this.heap[currentIndex].priority < this.heap[parentIndex].priority) {
+                [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+                currentIndex = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+
+    remove() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown();
+
+        return root;
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+        while (this.getLeftChildIndex(currentIndex) < this.heap.length) {
+            const leftChildIndex = this.getLeftChildIndex(currentIndex);
+            const rightChildIndex = this.getRightChildIndex(currentIndex);
+            let nextIndex = leftChildIndex;
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[leftChildIndex].priority) {
+                nextIndex = rightChildIndex;
+            }
+            if (this.heap[currentIndex].priority > this.heap[nextIndex].priority) {
+                [this.heap[currentIndex], this.heap[nextIndex]] = [this.heap[nextIndex], this.heap[currentIndex]];
+                currentIndex = nextIndex;
+            } else {
+                break;
+            }
+        }
+    }
 }
 
-// Example usage
-var arr = [12, 11, 13, 5, 6];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + insertionSort(arr));
+// Example usage:
+const priorityQueue = new BinaryHeap();
+priorityQueue.insert("Task 1", 2);
+priorityQueue.insert("Task 2", 1);
+priorityQueue.insert("Task 3", 3);
+
+console.log(priorityQueue.remove()); // { value: 'Task 2', priority: 1 }
+console.log(priorityQueue.remove()); // { value: 'Task 1', priority: 2 }
+console.log(priorityQueue.remove()); // { value: 'Task 3', priority: 3 }
