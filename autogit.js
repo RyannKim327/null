2dc99de5dd180ab3649ee3abe2db25e153ec81e0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:48:23 +0800

    3-15-2024 22:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,17 @@
-function validateEmail(email) {
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return emailPattern.test(email);
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-// Example usage
-const email = "test@example.com";
-if (validateEmail(email)) {
-    console.log("Email is valid");
-} else {
-    console.log("Email is invalid");
-}
+let arr = [64, 25, 12, 22, 11];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + bubbleSort(arr));
