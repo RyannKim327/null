8a2db80b41a8e161766920e3d57010e6928a6455
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:46:38 +0800

    3-15-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,18 @@
-class Node {
-    constructor(value, level) {
-        this.value = value;
-        this.forward = new Array(level + 1).fill(null);
+function countOccurrences(str, char) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
     }
+
+    return count;
 }
 
-class SkipList {
-    constructor(maxLevel, probability) {
-        this.maxLevel = maxLevel;
-        this.probability = probability;
-        this.level = 0;
-        this.head = new Node(null, this.maxLevel);
-    }
-    
-    randomLevel() {
-        let level = 0;
-        while (Math.random() < this.probability && level < this.maxLevel) {
-            level++;
-        }
-        return level;
-    }
-    
-    insert(value) {
-        let update = new Array(this.maxLevel + 1).fill(null);
-        let current = this.head;
-        
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-            update[i] = current;
-        }
-        
-        current = current.forward[0];
-        
-        let newLevel = this.randomLevel();
-        
-        if (newLevel > this.level) {
-            for (let i = this.level + 1; i <= newLevel; i++) {
-                update[i] = this.head;
-            }
-            this.level = newLevel;
-        }
-        
-        let newNode = new Node(value, newLevel);
-        
-        for (let i = 0; i <= newLevel; i++) {
-            newNode.forward[i] = update[i].forward[i];
-            update[i].forward[i] = newNode;
-        }
-    }
-    
-    search(value) {
-        let current = this.head;
-        
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-        }
-        
-        current = current.forward[0];
-        
-        if (current !== null && current.value === value) {
-            return current;
-        } else {
-            return null;
-        }
-    }
-    
-    delete(value) {
-        let update = new Array(this.maxLevel + 1).fill(null);
-        let current = this.head;
-        
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-            update[i] = current;
-        }
-        
-        current = current.forward[0];
-        
-        if (current !== null && current.value === value) {
-            for (let i = 0; i <= this.level; i++) {
-                if (update[i].forward[i] !== current) {
-                    break;
-                }
-                update[i].forward[i] = current.forward[i];
-            }
-            
-            while (this.level > 0 && this.head.forward[this.level] === null) {
-                this.level--;
-            }
-        }
-    }
-}
+// Example usage
+const str = "hello, world!";
+const char = "o";
+const result = countOccurrences(str, char);
+
+console.log(`'${char}' occurs ${result} times in '${str}'.`);
