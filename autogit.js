5cb58cc7a1a12ae76f9c8d7cc99366af5ad6e5ed
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:41:48 +0800

    3-15-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,15 @@
-let str1 = "Hello";
-let str2 = "World";
+function findKthSmallest(arr, k) {
+    // Sort the array
+    arr.sort(function(a, b) {
+        return a - b;
+    });
 
-let concatenatedString = str1 + " " + str2;
-console.log(concatenatedString); // Output: Hello World
+    // Return the kth smallest element
+    return arr[k - 1];
+}
+
+// Example usage
+const arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+const k = 3;
+const kthSmallest = findKthSmallest(arr, k);
+console.log(`The ${k}th smallest element is: ${kthSmallest}`);
