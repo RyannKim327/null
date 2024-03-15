0896f6c465348c5c685d8cbd829563e09e36b83d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:41:15 +0800

    3-15-2024 22:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,100 +1,15 @@
-class Node {
-    constructor(value, level) {
-        this.value = value;
-        this.forward = new Array(level + 1).fill(null);
+function maxSubArray(arr) {
+    let maxSum = arr[0];
+    let currentSum = arr[0];
+    
+    for (let i = 1; i < arr.length; i++) {
+        currentSum = Math.max(arr[i], currentSum + arr[i]);
+        maxSum = Math.max(maxSum, currentSum);
     }
+    
+    return maxSum;
 }
 
-class SkipList {
-    constructor(maxLevel, probability) {
-        this.maxLevel = maxLevel;
-        this.probability = probability;
-        this.header = new Node(null, maxLevel);
-        this.level = 0;
-    }
-
-    randomLevel() {
-        let level = 0;
-        while (Math.random() < this.probability && level < this.maxLevel) {
-            level++;
-        }
-        return level;
-    }
-
-    insert(value) {
-        const update = new Array(this.maxLevel + 1);
-        let current = this.header;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-            update[i] = current;
-        }
-
-        current = current.forward[0];
-
-        if (current === null || current.value !== value) {
-            const newLevel = this.randomLevel();
-
-            if (newLevel > this.level) {
-                for (let i = this.level + 1; i < newLevel + 1; i++) {
-                    update[i] = this.header;
-                }
-                this.level = newLevel;
-            }
-
-            const newNode = new Node(value, newLevel);
-
-            for (let i = 0; i <= newLevel; i++) {
-                newNode.forward[i] = update[i].forward[i];
-                update[i].forward[i] = newNode;
-            }
-        }
-    }
-
-    search(value) {
-        let current = this.header;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-        }
-
-        current = current.forward[0];
-
-        if (current !== null && current.value === value) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    delete(value) {
-        const update = new Array(this.maxLevel + 1);
-        let current = this.header;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.forward[i] !== null && current.forward[i].value < value) {
-                current = current.forward[i];
-            }
-            update[i] = current;
-        }
-
-        current = current.forward[0];
-
-        if (current !== null && current.value === value) {
-            for (let i = 0; i <= this.level; i++) {
-                if (update[i].forward[i] !== current) {
-                    break;
-                }
-                update[i].forward[i] = current.forward[i];
-            }
-
-            while (this.level > 0 && this.header.forward[this.level] === null) {
-                this.level--;
-            }
-        }
-    }
-}
+// Example
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArray(arr)); // Output should be 6 (from subarray [4, -1, 2, 1])
