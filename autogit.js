ff4744311472276eabc26d551b8e94c77dbe75f9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:52:20 +0800

    3-15-2024 22:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,5 @@
-function areAnagrams(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^a-z]/g, '').toLowerCase();
-    str2 = str2.replace(/[^a-z]/g, '').toLowerCase();
+const cron = require('node-cron');
 
-    // Check if the lengths of the two strings are equal
-    if (str1.length !== str2.length) {
-        return false;
-    }
-
-    // Sort the characters in both strings and compare them
-    return str1.split('').sort().join('') === str2.split('').sort().join('');
-}
-
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute!');
+});
