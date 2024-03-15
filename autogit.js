e2b033aefdc48d3039dcf8e1ce39bb90ca44f63a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:46:57 +0800

    3-15-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,18 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
-    }
-
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-
-    getLeftChildIndex(index) {
-        return 2 * index + 1;
-    }
-
-    getRightChildIndex(index) {
-        return 2 * index + 2;
-    }
-
-    swap(index1, index2) {
-        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-    }
-
-    insert(data) {
-        this.heap.push(data);
-        this.bubbleUp();
-    }
-
-    bubbleUp() {
-        let index = this.heap.length - 1;
-        while (index > 0) {
-            const parentIndex = this.getParentIndex(index);
-            if (this.heap[parentIndex] > this.heap[index]) {
-                break;
-            }
-            this.swap(parentIndex, index);
-            index = parentIndex;
+function insertionSort(arr) {
+    const n = arr.length;
+    for (let i = 1; i < n; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = current;
     }
-
-    extractMax() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-
-        const max = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.sinkDown(0);
-        return max;
-    }
-
-    sinkDown(index) {
-        let largest = index;
-        const leftIndex = this.getLeftChildIndex(index);
-        const rightIndex = this.getRightChildIndex(index);
-        const size = this.heap.length;
-
-        if (leftIndex < size && this.heap[leftIndex] > this.heap[largest]) {
-            largest = leftIndex;
-        }
-
-        if (rightIndex < size && this.heap[rightIndex] > this.heap[largest]) {
-            largest = rightIndex;
-        }
-
-        if (largest !== index) {
-            this.swap(index, largest);
-            this.sinkDown(largest);
-        }
-    }
+    return arr;
 }
 
 // Example usage
-const pq = new PriorityQueue();
-pq.insert(5);
-pq.insert(10);
-pq.insert(3);
-
-console.log(pq.extractMax()); // Output: 10
-console.log(pq.extractMax()); // Output: 5
-console.log(pq.extractMax()); // Output: 3
+const arr = [5, 2, 4, 6, 1, 3];
+const sortedArr = insertionSort(arr);
+console.log(sortedArr);  // Output: [1, 2, 3, 4, 5, 6]
