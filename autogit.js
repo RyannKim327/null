4c9a88d258397c6406d21af052ec04597875255d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:54:21 +0800

    3-15-2024 22:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,15 @@
-function isSorted(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
+function hasCycle(head) {
+    let slow = head;
+    let fast = head;
+    
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        
+        if (slow === fast) {
+            return true; // Cycle detected
         }
     }
-    return true;
+    
+    return false; // No cycle found
 }
-
-// Example
-const myArray = [1, 2, 3, 4, 5];
-console.log(isSorted(myArray)); // Output: true
-
-const unsortedArray = [5, 4, 3, 2, 1];
-console.log(isSorted(unsortedArray)); // Output: false
