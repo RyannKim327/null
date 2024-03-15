8ef7a4a1ce4f6da80feed2f367447b1436efcb98
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:43:04 +0800

    3-15-2024 22:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,9 @@
-// Definition for a singly-linked list node
-class ListNode {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-// Function to find the length of a linked list
-function findLengthOfLinkedList(head) {
-    let length = 0;
-    let current = head;
-
-    while (current !== null) {
-        length++;
-        current = current.next;
-    }
-
-    return length;
-}
-
-// Example linked list
-let head = new ListNode(1);
-head.next = new ListNode(2);
-head.next.next = new ListNode(3);
-head.next.next.next = new ListNode(4);
-head.next.next.next.next = new ListNode(5);
-
-// Find the length of the linked list
-const length = findLengthOfLinkedList(head);
-console.log("Length of the linked list: " + length); // Output: Length of the linked list: 5
+let str = "42";
+let number = parseInt(str);
+console.log(number); // Output: 42
+let str = "1010";
+let binaryNumber = parseInt(str, 2); // converting binary to integer
+console.log(binaryNumber); // Output: 10
+let str = "42";
+let number = Number(str);
+console.log(number); // Output: 42
