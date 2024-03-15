84ac9ab8ce302b8ad6084bbd5ff1ac946f77ee9d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:47:31 +0800

    3-15-2024 22:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,17 @@
-function buildLPS(pattern) {
-    let lps = new Array(pattern.length).fill(0);
-    let j = 0, i = 1;
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return '';
     
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[j]) {
-            lps[i] = j + 1;
-            i++;
-            j++;
-        } else {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
+    let prefix = strs[0];
+    
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
         }
     }
     
-    return lps;
+    return prefix;
 }
 
-function KMP(text, pattern) {
-    let lps = buildLPS(pattern);
-    let i = 0, j = 0;
-    let matches = [];
-    
-    while (i < text.length) {
-        if (text[i] === pattern[j]) {
-            i++;
-            j++;
-        }
-        
-        if (j === pattern.length) {
-            matches.push(i - j);
-            j = lps[j - 1];
-        } else if (i < text.length && text[i] !== pattern[j]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-    
-    return matches;
-}
-
-// Example usage
-let text = "ABABDABACDABABCABAB";
-let pattern = "ABABCABAB";
-let matches = KMP(text, pattern);
-console.log(matches);
+// Example
+const strings = ["flower", "flow", "flight"];
+console.log(longestCommonPrefix(strings)); // Output: "fl"
