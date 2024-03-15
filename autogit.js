dd5d97cbfd58c88d10d80ddf85a01283e25868ff
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:47:43 +0800

    3-15-2024 22:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,70 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 0) return '';
-    
-    let prefix = strs[0];
-    
-    for (let i = 1; i < strs.length; i++) {
-        while (strs[i].indexOf(prefix) != 0) {
-            prefix = prefix.substring(0, prefix.length - 1);
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-    
-    return prefix;
 }
 
-// Example
-const strings = ["flower", "flow", "flight"];
-console.log(longestCommonPrefix(strings)); // Output: "fl"
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    enqueue(value) {
+        const newNode = new Node(value);
+        if (!this.front) {
+            this.front = newNode;
+            this.rear = newNode;
+        } else {
+            this.rear.next = newNode;
+            this.rear = newNode;
+        }
+        this.size++;
+    }
+
+    dequeue() {
+        if (!this.front) {
+            return null;
+        }
+
+        const removedNode = this.front;
+        this.front = this.front.next;
+        if (!this.front) {
+            this.rear = null;
+        }
+        this.size--;
+
+        return removedNode.value;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    peek() {
+        return this.front ? this.front.value : null;
+    }
+
+    print() {
+        let current = this.front;
+        const values = [];
+        while (current) {
+            values.push(current.value);
+            current = current.next;
+        }
+        console.log(values.join(' <- '));
+    }
+}
+
+// Example usage:
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.print(); // Output: 1 <- 2 <- 3
+queue.dequeue();
+queue.print(); // Output: 2 <- 3
+console.log(queue.peek()); // Output: 2
+console.log(queue.isEmpty()); // Output: false
