453d6b94917bb6ff9b898bdf093fc58f05f4773e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:39:06 +0800

    3-15-2024 22:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,9 @@
-const cron = require('cron');
+function calculateTriangleArea(base, height) {
+    return 0.5 * base * height;
+}
 
-const job = new cron.CronJob('* * * * *', function() {
-  console.log('This job runs every minute');
-}, null, true);
-
-job.start();
+// Example usage
+const base = 5;
+const height = 8;
+const area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
