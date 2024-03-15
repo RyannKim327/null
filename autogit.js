055019bd5a609d64c7cf3f248857003f46f7342a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:50:43 +0800

    3-15-2024 22:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,47 @@
-function longestCommonSubsequence(str1, str2) {
-    const dp = Array.from({ length: str1.length + 1 }, () =>
-        Array.from({ length: str2.length + 1 }).fill(0)
-    );
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = 1 + dp[i - 1][j - 1];
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    let i = str1.length, j = str2.length;
-    let lcs = '';
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return lcs;
+  addChild(child) {
+    this.children.push(child);
+  }
 }
 
-const str1 = 'AGGTAB';
-const str2 = 'GXTXAYB';
-console.log(longestCommonSubsequence(str1, str2)); // Output: GTAB
+function breadthLimitedSearch(root, limit) {
+  let queue = [root];
+  let level = 1;
+
+  while (queue.length > 0) {
+    let node = queue.shift();
+    console.log(node.data);
+
+    if (level < limit) {
+      node.children.forEach(child => {
+        queue.push(child);
+      });
+    }
+
+    if (queue.length === 0 && level < limit) {
+      level++;
+      queue = root.children.slice();
+    }
+  }
+}
+
+// Example usage
+let root = new Node('A');
+let nodeB = new Node('B');
+let nodeC = new Node('C');
+let nodeD = new Node('D');
+let nodeE = new Node('E');
+let nodeF = new Node('F');
+
+root.addChild(nodeB);
+root.addChild(nodeC);
+nodeB.addChild(nodeD);
+nodeB.addChild(nodeE);
+nodeE.addChild(nodeF);
+
+breadthLimitedSearch(root, 2);
