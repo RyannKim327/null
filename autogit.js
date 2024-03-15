d043a68a653f61c1b4de66f07e8ba03891045705
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:53:18 +0800

    3-15-2024 22:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,54 @@
-function mean(numbers) {
-    var sum = 0;
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = new Array(size);
+  }
 
-    for (var i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
+  hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * (i + 1)) % this.size;
     }
+    return hash;
+  }
 
-    return sum / numbers.length;
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (let i = 0; i < this.table[index].length; i++) {
+      if (this.table[index][i][0] === key) {
+        return this.table[index][i][1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      return;
+    }
+    this.table[index] = this.table[index].filter(item => item[0] !== key);
+  }
 }
 
 // Example usage
-var numbers = [1, 2, 3, 4, 5];
-var meanValue = mean(numbers);
-console.log(meanValue); // Output: 3
+const ht = new HashTable(10);
+ht.set("name", "John");
+ht.set("age", 30);
+
+console.log(ht.get("name")); // Output: John
+console.log(ht.get("age")); // Output: 30
+
+ht.remove("age");
+console.log(ht.get("age")); // Output: undefined
