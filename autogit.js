78817400e89d08a6a008ce3ba0f94f4199f1c66e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:44:46 +0800

    3-15-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,90 @@
-function areAnagrams(str1, str2) {
-    // Remove non-alphabetic characters and convert to lowercase
-    const cleanStr1 = str1.replace(/[^a-z]/ig, '').toLowerCase();
-    const cleanStr2 = str2.replace(/[^a-z]/ig, '').toLowerCase();
-
-    // Sort the characters in the strings
-    const sortedStr1 = cleanStr1.split('').sort().join('');
-    const sortedStr2 = cleanStr2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
+// Node class for skip list
+class Node {
+    constructor(value, level) {
+        this.value = value;
+        this.next = new Array(level).fill(null);
+    }
 }
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
+// SkipList class
+class SkipList {
+    constructor(maxLevels, probability) {
+        this.maxLevels = maxLevels;
+        this.probability = probability;
+        this.header = new Node(null, maxLevels);
+        this.level = 0;
+    }
+    
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.probability && level < this.maxLevels - 1) {
+            level++;
+        }
+        return level;
+    }
+    
+    insert(value) {
+        const newNode = new Node(value, this.randomLevel());
+        
+        let current = this.header;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] != null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            if (i <= newNode.next.length) {
+                newNode.next[i] = current.next[i];
+                current.next[i] = newNode;
+            }
+        }
+        
+        this.level = Math.max(this.level, newNode.next.length - 1);
+    }
+    
+    search(value) {
+        let current = this.header;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] != null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+        }
+        current = current.next[0];
+        
+        if (current != null && current.value === value) {
+            return current;
+        } else {
+            return null;
+        }
+    }
+    
+    delete(value) {
+        let current = this.header;
+        const update = new Array(this.level + 1);
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] != null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            update[i] = current;
+        }
+        
+        current = current.next[0];
+        if (current != null && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].next[i] !== current) break;
+                update[i].next[i] = current.next[i];
+            }
+        }
+    }
+    
 }
+
+// Test the SkipList
+const skipList = new SkipList(4, 0.5);
+skipList.insert(3);
+skipList.insert(6);
+skipList.insert(7);
+skipList.insert(9);
+
+console.log(skipList.search(6)); // Outputs: Node { value: 6, next: [ null, null, null, null ] }
+
+skipList.delete(6);
+console.log(skipList.search(6)); // Outputs: null
