f53fac151765db74c6963978bbf365a0fee393ba
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:45:11 +0800

    3-15-2024 22:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,47 @@
-function selectionSort(arr) {
-    const n = arr.length;
-    
-    for (let i = 0; i < n - 1; i++) {
-        let minIndex = i;
+function fibonacciSearch(arr, x) {
+    let fibN2 = 0; 
+    let fibN1 = 1; 
+    let fibN = fibN2 + fibN1;
+
+    while (fibN < arr.length) {
+        fibN2 = fibN1;
+        fibN1 = fibN;
+        fibN = fibN2 + fibN1;
+    }
+
+    let offset = -1;
+
+    while (fibN > 1) {
+        let i = Math.min(offset + fibN2, arr.length - 1);
         
-        for (let j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        if (arr[i] < x) {
+            fibN = fibN1;
+            fibN1 = fibN2;
+            fibN2 = fibN - fibN1;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibN = fibN2;
+            fibN1 = fibN1 - fibN2;
+            fibN2 = fibN - fibN1;
+        } else {
+            return i;
         }
     }
-    
-    return arr;
+
+    if (fibN1 && arr[offset + 1] === x) {
+        return offset + 1;
+    }
+
+    return -1; // Element not found
 }
 
 // Example usage
-const array = [64, 25, 12, 22, 11];
-console.log("Original array: ", array);
-console.log("Sorted array: ", selectionSort(array));
+const arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100];
+const x = 85;
+const index = fibonacciSearch(arr, x);
+
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log("Element not found");
+}
