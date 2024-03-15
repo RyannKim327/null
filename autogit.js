55a1ba974e5cb8661905f0f1411fd13d622de38e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:33:08 +0800

    3-15-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,10 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+function findStringLength(str) {
+    let count = 0;
+    while (str[count] !== undefined) {
+        count++;
     }
+    return count;
 }
 
-function sumBinaryTree(root) {
-    if (root === null) {
-        return 0;
-    }
-
-    // Perform depth-first traversal using recursion
-    const leftSum = sumBinaryTree(root.left);
-    const rightSum = sumBinaryTree(root.right);
-
-    // Sum up the values of the current node and its children
-    return root.value + leftSum + rightSum;
-}
-
-// Example binary tree
-const root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-
-console.log("Sum of all nodes in the binary tree:", sumBinaryTree(root)); // Output: 28
+const str = "Hello, World!";
+console.log(findStringLength(str)); // Output: 13
