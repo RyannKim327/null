68e4377df6ef56dbcd44fe025a53c4777820086c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:34:30 +0800

    3-15-2024 22:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,80 @@
-function largestPrimeFactor(number) {
-    let largestPrime = 1;
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        while (number % i === 0) {
-            largestPrime = i;
-            number = number / i;
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    if (number > 1) {
-        largestPrime = number;
-    }
-    return largestPrime;
 }
 
-let num = 13195;
-console.log("The largest prime factor of " + num + " is " + largestPrimeFactor(num));
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+
+    inorderTraversal(node, result = []) {
+        if (node !== null) {
+            this.inorderTraversal(node.left, result);
+            result.push(node.value);
+            this.inorderTraversal(node.right, result);
+        }
+        return result;
+    }
+}
+
+// Usage example
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log(bst.search(7)); // Output: true
+console.log(bst.search(20)); // Output: false
+
+const inorderResult = bst.inorderTraversal(bst.root);
+console.log(inorderResult); // Output: [3, 5, 7, 10, 15]
