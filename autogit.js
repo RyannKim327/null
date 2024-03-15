6f66cdce99e01566a43920c028ae885c5a869d1b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:40:12 +0800

    3-15-2024 22:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,8 @@
-function computeLPSArray(pattern) {
-    let lps = new Array(pattern.length).fill(0);
-    let len = 0;
-    let i = 1;
-    
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len !== 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
-    
-    return lps;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-function KMPSearch(text, pattern) {
-    let n = text.length;
-    let m = pattern.length;
-    let lps = computeLPSArray(pattern);
-    
-    let i = 0; // index for text
-    let j = 0; // index for pattern
-    
-    while (i < n) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-        
-        if (j === m) {
-            console.log("Pattern found at index " + (i - j));
-            j = lps[j - 1];
-        } else if (i < n && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-}
+let inputString = "Hello World!";
+let result = removeVowels(inputString);
 
-let text = "ABABDABACDABABCABAB";
-let pattern = "ABABCABAB";
-
-KMPSearch(text, pattern);
+console.log(result); // Output: "Hll Wrld!"
