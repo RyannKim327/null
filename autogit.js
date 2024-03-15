6f5715d8d6bb13613c0ac90612ad28643b332b12
RyannKim327 <rksesgundo123@gmail.com>
2024-03-15 22:46:50 +0800

    3-15-2024 22:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,82 @@
-function countOccurrences(str, char) {
-    let count = 0;
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
+    }
 
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
-            count++;
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return 2 * index + 1;
+    }
+
+    getRightChildIndex(index) {
+        return 2 * index + 2;
+    }
+
+    swap(index1, index2) {
+        [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
+    }
+
+    insert(data) {
+        this.heap.push(data);
+        this.bubbleUp();
+    }
+
+    bubbleUp() {
+        let index = this.heap.length - 1;
+        while (index > 0) {
+            const parentIndex = this.getParentIndex(index);
+            if (this.heap[parentIndex] > this.heap[index]) {
+                break;
+            }
+            this.swap(parentIndex, index);
+            index = parentIndex;
         }
     }
 
-    return count;
+    extractMax() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        if (this.heap.length === 1) {
+            return this.heap.pop();
+        }
+
+        const max = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.sinkDown(0);
+        return max;
+    }
+
+    sinkDown(index) {
+        let largest = index;
+        const leftIndex = this.getLeftChildIndex(index);
+        const rightIndex = this.getRightChildIndex(index);
+        const size = this.heap.length;
+
+        if (leftIndex < size && this.heap[leftIndex] > this.heap[largest]) {
+            largest = leftIndex;
+        }
+
+        if (rightIndex < size && this.heap[rightIndex] > this.heap[largest]) {
+            largest = rightIndex;
+        }
+
+        if (largest !== index) {
+            this.swap(index, largest);
+            this.sinkDown(largest);
+        }
+    }
 }
 
 // Example usage
-const str = "hello, world!";
-const char = "o";
-const result = countOccurrences(str, char);
+const pq = new PriorityQueue();
+pq.insert(5);
+pq.insert(10);
+pq.insert(3);
 
-console.log(`'${char}' occurs ${result} times in '${str}'.`);
+console.log(pq.extractMax()); // Output: 10
+console.log(pq.extractMax()); // Output: 5
+console.log(pq.extractMax()); // Output: 3
