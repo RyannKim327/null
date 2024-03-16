3987027dd8bc640d5112dc1bd9e0c7bcc895d5b6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-16 21:37:31 +0800

    3-16-2024 21:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,18 @@
 function bubbleSort(arr) {
-    let n = arr.length;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-    } while (swapped);
-    
+    }
     return arr;
 }
 
-// Example
-let arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + bubbleSort(arr));
+const array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original Array: ", array);
+const sortedArray = bubbleSort(array);
+console.log("Sorted Array: ", sortedArray);
