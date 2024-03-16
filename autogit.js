7687c1d13e6130108696a290892f5a762e8a274b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-16 21:39:19 +0800

    3-16-2024 21:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,3 @@
-const fetchUserData = async () => {
-  try {
-    const response = await fetch('https://api.example.com/userdata');
-    const data = await response.json();
-    
-    // Process the data here, e.g., update UI, store in database, etc.
-
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-  }
-};
-
-fetchUserData();
-MyWebView.setWebViewClient(new WebViewClient() {
-    @Override
-    public void onPageFinished(WebView view, String url) {
-        super.onPageFinished(view, url);
-        // Call the JavaScript function after the page is loaded
-        MyWebView.loadUrl("javascript:fetchUserData()");
-    }
-});
+let str = "  Hello, world!  ";
+let trimmedStr = str.replace(/\s/g, "");
+console.log(trimmedStr);
