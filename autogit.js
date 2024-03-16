204b389b94751e70112b23045e80fc53b95b4176
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 07:42:25 +0800

    3-17-2024 7:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,75 +1,47 @@
-class PriorityQueue {
-  constructor() {
-    this.heap = [];
-  }
+function createPrefixTable(pattern) {
+    const prefixTable = [0];
+    let prefixLength = 0;
 
-  enqueue(value, priority) {
-    const node = { value, priority };
-    this.heap.push(node);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
+    for (let i = 1; i < pattern.length; i++) {
+        while (prefixLength > 0 && pattern.charAt(i) !== pattern.charAt(prefixLength)) {
+            prefixLength = prefixTable[prefixLength - 1];
+        }
+        if (pattern.charAt(i) === pattern.charAt(prefixLength)) {
+            prefixLength++;
+        }
+        prefixTable[i] = prefixLength;
     }
 
-    const minNode = this.heap[0];
-    const lastNode = this.heap.pop();
-    if (this.heap.length > 0) {
-      this.heap[0] = lastNode;
-      this.bubbleDown(0);
-    }
-    
-    return minNode.value;
-  }
-
-  bubbleUp(index) {
-    while (index > 0) {
-      const parentIndex = Math.floor((index - 1) / 2);
-      if (this.heap[index].priority < this.heap[parentIndex].priority) {
-        [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]];
-        index = parentIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  bubbleDown(index) {
-    while (index < this.heap.length) {
-      const leftChildIndex = 2 * index + 1;
-      const rightChildIndex = 2 * index + 2;
-      let nextIndex = index;
-
-      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].priority < this.heap[nextIndex].priority) {
-        nextIndex = leftChildIndex;
-      }
-
-      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[nextIndex].priority) {
-        nextIndex = rightChildIndex;
-      }
-
-      if (nextIndex !== index) {
-        [this.heap[index], this.heap[nextIndex]] = [this.heap[nextIndex], this.heap[index]];
-        index = nextIndex;
-      } else {
-        break;
-      }
-    }
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
+    return prefixTable;
 }
+function kmpSearch(text, pattern) {
+    const prefixTable = createPrefixTable(pattern);
+    let matches = [];
 
-// Usage
-const pq = new PriorityQueue();
-pq.enqueue('task1', 3);
-pq.enqueue('task2', 1);
-pq.enqueue('task3', 2);
+    let textIndex = 0;
+    let patternIndex = 0;
 
-console.log(pq.dequeue()); // task2
-console.log(pq.dequeue()); // task3
-console.log(pq.dequeue()); // task1
+    while (textIndex < text.length) {
+        if (pattern.charAt(patternIndex) === text.charAt(textIndex)) {
+            textIndex++;
+            patternIndex++;
+            if (patternIndex === pattern.length) {
+                matches.push(textIndex - patternIndex);
+                patternIndex = prefixTable[patternIndex - 1];
+            }
+        } else {
+            if (patternIndex !== 0) {
+                patternIndex = prefixTable[patternIndex - 1];
+            } else {
+                textIndex++;
+            }
+        }
+    }
+
+    return matches;
+}
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const matches = kmpSearch(text, pattern);
+
+console.log("Pattern found at positions:", matches);
