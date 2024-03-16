c51abd2c96de09acf6a2f68538569c26aff8c03e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 07:41:38 +0800

    3-17-2024 7:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,13 @@
-let str = "  Hello, world!  ";
-let trimmedStr = str.replace(/\s/g, "");
-console.log(trimmedStr);
+const axios = require('axios');
+
+axios.post('https://jsonplaceholder.typicode.com/posts', {
+  title: 'foo',
+  body: 'bar',
+  userId: 1
+})
+.then(response => {
+  console.log('Post created:', response.data);
+})
+.catch(error =>{
+  console.error('Error creating post:', error);
+});
