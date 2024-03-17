60761e1ab371d904ae6a65a11248b5c4dd1db744
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:09:19 +0800

    3-17-2024 9:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,47 @@
-class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
+function fibonacciSearch(arr, x) {
+    let fibMMm2 = 0; // (m-2)'th Fibonacci Number
+    let fibMMm1 = 1; // (m-1)'th Fibonacci Number
+    let fibM = fibMMm2 + fibMMm1; // m'th Fibonacci Number
+
+    let length = arr.length;
+
+    while (fibM < length) {
+        fibMMm2 = fibMMm1;
+        fibMMm1 = fibM;
+        fibM = fibMMm2 + fibMMm1;
     }
+
+    let offset = -1;
+
+    while (fibM > 1) {
+        let i = Math.min(offset + fibMMm2, length - 1);
+
+        if (arr[i] < x) {
+            fibM = fibMMm1;
+            fibMMm1 = fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibM = fibMMm2;
+            fibMMm1 = fibMMm1 - fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+        } else {
+            return i;
+        }
+    }
+
+    if (fibMMm1 && arr[offset + 1] === x) {
+        return offset + 1;
+    }
+
+    return -1;
 }
+let arr = [1, 3, 5, 7, 9, 11, 13, 15];
+let x = 9;
+let result = fibonacciSearch(arr, x);
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    insertAtEnd(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-
-        current.next = newNode;
-    }
-
-    getLength() {
-        let count = 0;
-        let current = this.head;
-
-        while (current) {
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
+if (result !== -1) {
+    console.log(`Element found at index ${result}`);
+} else {
+    console.log('Element not found in the array');
 }
-
-// Usage
-const linkedList = new LinkedList();
-linkedList.insertAtEnd(1);
-linkedList.insertAtEnd(2);
-linkedList.insertAtEnd(3);
-
-console.log(linkedList.getLength()); // Output: 3
