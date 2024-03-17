ff55ab1b6155b0468241821d6bec09e91fb59493
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:11:18 +0800

    3-17-2024 9:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,22 @@
-let str = "Hello, world! This is a string with spaces.";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces);
+function burrowsWheelerTransform(inputString) {
+    let matrix = [];
+    
+    // Generate all rotations of the input string
+    for (let i = 0; i < inputString.length; i++) {
+        let rotation = inputString.slice(i) + inputString.slice(0, i);
+        matrix.push(rotation);
+    }
+    
+    // Sort the rotations lexicographically
+    matrix.sort();
+    
+    // Extract the last characters of each rotation
+    let transformedString = matrix.map(rotation => rotation.slice(-1)).join('');
+    
+    return transformedString;
+}
+
+// Example usage
+let inputString = "banana";
+let bwtResult = burrowsWheelerTransform(inputString);
+console.log("Burrows-Wheeler transformed string:", bwtResult);
