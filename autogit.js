82dd03b064573eb00d29e07167a1c7807666453a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:58:30 +0800

    3-17-2024 8:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,35 @@
-function factorial(n) {
-    if (n === 0) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
+function topologicalSort(graph) {
+    const visited = new Set();
+    const result = [];
+
+    function visit(node) {
+        if (visited.has(node)) return;
+        visited.add(node);
+
+        graph[node].forEach(neighbor => {
+            visit(neighbor);
+        });
+
+        result.unshift(node);
     }
+
+    for (const node in graph) {
+        visit(node);
+    }
+
+    return result;
 }
 
-// Test the factorial function
-const num = 5;
-const result = factorial(num);
-console.log(`The factorial of ${num} is ${result}`);
+// Example graph
+const graph = {
+    'A': ['C', 'D'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['G'],
+    'F': ['G'],
+    'G': []
+};
+
+const sortedNodes = topologicalSort(graph);
+console.log(sortedNodes);
