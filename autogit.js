89358d2c489efbd73334ae9833d7875e4b8b26d0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:55:41 +0800

    3-17-2024 8:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,13 @@
-function selectionSort(arr) {
-  // Loop through the entire array
-  for (let i = 0; i < arr.length; i++) {
-    // Find the index of the minimum element in the remaining unsorted portion of the array
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    // Swap the minimum element with the current element
-    if (minIndex !== i) {
-      let temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
-    }
-  }
-  return arr;
+const emailRegExp = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[\w-]{2,4}$/;
+function validateEmail(email) {
+  const emailRegExp = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[\w-]{2,4}$/;
+  
+  return emailRegExp.test(email);
 }
 
-// Test the selectionSort function
-let arr = [64, 25, 12, 22, 11];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + selectionSort(arr));
+const email = "example@example.com";
+if (validateEmail(email)) {
+  console.log("Email address is valid");
+} else {
+  console.log("Email address is invalid");
+}
