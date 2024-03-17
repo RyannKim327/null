45945d2bb97ccaefdcb92848429f00f9530b4aa9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:52:14 +0800

    3-17-2024 8:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,28 @@
-function reverseWords(str) {
-    // Split the string into an array of words
-    let wordsArray = str.split(" ");
-
-    // Reverse the array of words
-    let reversedArray = wordsArray.reverse();
-
-    // Join the array back into a string
-    let reversedString = reversedArray.join(" ");
-
-    return reversedString;
+class Node {
+    constructor() {
+        this.edges = {};
+    }
 }
 
-let originalString = "Hello World, how are you?";
-let reversedString = reverseWords(originalString);
+class Edge {
+    constructor(start, end, label) {
+        this.start = start;
+        this.end = end;
+        this.label = label;
+    }
+}
+class SuffixTree {
+    constructor() {
+        this.root = new Node();
+    }
 
-console.log(reversedString); // Output: "you? are how World, Hello"
+    // Methods to insert a string into the suffix tree
+    insert(suffix) {
+        // Implement insertion logic here
+    }
+
+    // Method to search for a substring in the suffix tree
+    search(substring) {
+        // Implement search logic here
+    }
+}
