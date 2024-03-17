757139604d5ba72dd3e799636ce68e16b32b47c8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:12:42 +0800

    3-17-2024 9:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,9 @@
-function isPrime(number) {
-    if (number <= 1) {
-        return false;
-    }
-
-    if (number <= 3) {
-        return true;
-    }
-
-    if (number % 2 === 0 || number % 3 === 0) {
-        return false;
-    }
-
-    for (let i = 5; i * i <= number; i += 6) {
-        if (number % i === 0 || number % (i + 2) === 0) {
-            return false;
-        }
-    }
-
-    return true;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Test the function
-const number = 17; // Change this to test different numbers
-if (isPrime(number)) {
-    console.log(number + ' is a prime number');
-} else {
-    console.log(number + ' is not a prime number');
-}
+// Example usage
+const inputString = "Hello, World!";
+const stringWithoutVowels = removeVowels(inputString);
+
+console.log(stringWithoutVowels);  // Output: "Hll, Wrld!"
