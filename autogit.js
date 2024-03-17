63f7d1091b425b79ce5597d7909e1809df398dbb
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:48:44 +0800

    3-18-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,100 +1,111 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
-    }
-
-    // Helper functions
-    swap(i, j) {
-        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-    }
-
-    getParentIndex(index) {
-        return Math.floor((index - 1) / 2);
-    }
-
-    getLeftChildIndex(index) {
-        return 2 * index + 1;
-    }
-
-    getRightChildIndex(index) {
-        return 2 * index + 2;
-    }
-
-    // Functions to maintain the heap property
-    heapifyUp() {
-        let index = this.heap.length - 1;
-        while (index > 0) {
-            const parentIndex = this.getParentIndex(index);
-            if (this.heap[parentIndex] < this.heap[index]) {
-                this.swap(parentIndex, index);
-                index = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
-
-    heapifyDown() {
-        let index = 0;
-        while (this.getLeftChildIndex(index) < this.heap.length) {
-            const leftChildIndex = this.getLeftChildIndex(index);
-            const rightChildIndex = this.getRightChildIndex(index);
-            let swapIndex = index;
-
-            if (this.heap[swapIndex] < this.heap[leftChildIndex]) {
-                swapIndex = leftChildIndex;
-            }
-            if (rightChildIndex < this.heap.length && this.heap[swapIndex] < this.heap[rightChildIndex]) {
-                swapIndex = rightChildIndex;
-            }
-
-            if (swapIndex !== index) {
-                this.swap(index, swapIndex);
-                index = swapIndex;
-            } else {
-                break;
-            }
-        }
-    }
-
-    // Priority Queue API
-    insert(value) {
-        this.heap.push(value);
-        this.heapifyUp();
-    }
-
-    extractMax() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        if (this.heap.length === 1) {
-            return this.heap.pop();
-        }
-
-        const maxValue = this.heap[0];
-        this.heap[0] = this.heap.pop();
-        this.heapifyDown();
-
-        return maxValue;
-    }
-
-    peek() {
-        return this.heap[0];
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    isEmpty() {
-        return this.heap.length === 0;
+class RBNode {
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
     }
 }
 
-// Example usage
-const pq = new PriorityQueue();
-pq.insert(5);
-pq.insert(2);
-pq.insert(10);
-console.log(pq.extractMax()); // 10
-console.log(pq.extractMax()); // 5
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new RBNode(value, "red");
+        this.root = this.insertNode(this.root, newNode);
+        this.root.color = "black";
+    }
+
+    insertNode(node, newNode) {
+        if (node === null) return newNode;
+
+        if (newNode.value < node.value) {
+            node.left = this.insertNode(node.left, newNode);
+            node.left.parent = node;
+        } else if (newNode.value > node.value) {
+            node.right = this.insertNode(node.right, newNode);
+            node.right.parent = node;
+        }
+
+        if (node.left && node.left.color === "red" && node.right && node.right.color === "red") {
+            if (node.left.left && node.left.left.color === "red") {
+                node = this.rotateRight(node);
+            } else {
+                node = this.rotateLeftRight(node);
+            }
+        }
+
+        if (node.parent) {
+            if (node === node.parent.left) {
+                if (node.color === "red" && node.parent.left.color === "red") {
+                    if (node.parent.right && node.parent.right.color === "red") {
+                        node.parent.color = "red";
+                        node.parent.left.color = "black";
+                        node.parent.right.color = "black";
+                    }
+                }
+            } else {
+                if (node.color === "red" && node.parent.right.color === "red") {
+                    if (node.parent.left && node.parent.left.color === "red") {
+                        node.parent.color = "red";
+                        node.parent.left.color = "black";
+                        node.parent.right.color = "black";
+                    }
+                }
+            }
+        }
+
+        return node;
+    }
+
+    rotateLeft(node) {
+        const temp = node.right;
+        node.right = temp.left;
+        if (temp.left) {
+            temp.left.parent = node;
+        }
+        temp.parent = node.parent;
+        if (!node.parent) {
+            this.root = temp;
+        } else if (node === node.parent.left) {
+            node.parent.left = temp;
+        } else {
+            node.parent.right = temp;
+        }
+        temp.left = node;
+        node.parent = temp;
+        return temp;
+    }
+
+    rotateRight(node) {
+        const temp = node.left;
+        node.left = temp.right;
+        if (temp.right) {
+            temp.right.parent = node;
+        }
+        temp.parent = node.parent;
+        if (!node.parent) {
+            this.root = temp;
+        } else if (node === node.parent.left) {
+            node.parent.left = temp;
+        } else {
+            node.parent.right = temp;
+        }
+        temp.right = node;
+        node.parent = temp;
+        return temp;
+    }
+
+    rotateLeftRight(node) {
+        node.left = this.rotateLeft(node.left);
+        return this.rotateRight(node);
+    }
+
+    rotateRightLeft(node) {
+        node.right = this.rotateRight(node.right);
+        return this.rotateLeft(node);
+    }
+}
