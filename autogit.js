07b598509f0ea7cf2f1802d917aa0262fe360002
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:05:44 +0800

    3-17-2024 9:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,5 @@
-let arr = [1, 2, 3, 4, 4, 5, 6, 6, 7, 8];
-let uniqueArr = [...new Set(arr)];
-console.log(uniqueArr); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
-let arr = [1, 2, 3, 4, 4, 5, 6, 6, 7, 8];
-let uniqueArr = arr.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArr); // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+const numbers = [10, 5, 20, 15, 30];
+
+const max = Math.max(...numbers);
+
+console.log(`The maximum value in the array is: ${max}`);
