a68b3c53433f8605c148d7b2cc30ec45430156e1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:30:06 +0800

    3-18-2024 5:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,65 @@
 class Node {
-  constructor(data, color) {
-    this.data = data;
-    this.color = color;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-  }
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
 }
 
-class RedBlackTree {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(data) {
-    const newNode = new Node(data, "red");
-    this.root = this.insertNode(this.root, newNode);
-    // Ensure root node is black
-    this.root.color = "black";
-  }
-
-  insertNode(node, newNode) {
-    // If tree is empty, return the new node
-    if (node === null) {
-      return newNode;
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
     }
 
-    // Insert new node
-    if (newNode.data < node.data) {
-      node.left = this.insertNode(node.left, newNode);
-      node.left.parent = node;
-    } else if (newNode.data > node.data) {
-      node.right = this.insertNode(node.right, newNode);
-      node.right.parent = node;
+    insert(data) {
+        let newNode = new Node(data);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
     }
 
-    // Perform rotations and color flips to maintain red-black tree properties
-    if (this.isRed(node.right) && !this.isRed(node.left)) {
-      node = this.rotateLeft(node);
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
     }
-    if (this.isRed(node.left) && this.isRed(node.left.left)) {
-      node = this.rotateRight(node);
-    }
-    if (this.isRed(node.left) && this.isRed(node.right)) {
-      this.flipColors(node);
+
+    search(data) {
+        return this.searchNode(this.root, data);
     }
 
-    return node;
-  }
-
-  isRed(node) {
-    if (node === null) {
-      return false;
+    searchNode(node, data) {
+        if (node === null) {
+            return false;
+        } else if (data < node.data) {
+            return this.searchNode(node.left, data);
+        } else if (data > node.data) {
+            return this.searchNode(node.right, data);
+        } else {
+            return true;
+        }
     }
-    return node.color === "red";
-  }
-
-  rotateLeft(node) {
-    const temp = node.right;
-    node.right = temp.left;
-    temp.left = node;
-    temp.color = node.color;
-    node.color = "red";
-    return temp;
-  }
-
-  rotateRight(node) {
-    const temp = node.left;
-    node.left = temp.right;
-    temp.right = node;
-    temp.color = node.color;
-    node.color = "red";
-    return temp;
-  }
-
-  flipColors(node) {
-    node.color = "red";
-    node.left.color = "black";
-    node.right.color = "black";
-  }
 }
+
+// Example usage
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(8);
+bst.insert(3);
+
+console.log(bst.search(8)); // Output: true
+console.log(bst.search(12)); // Output: false
