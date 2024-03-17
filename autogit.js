a8ab3c23d42cdf9cbd8fb821a105c1add7bf05c4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:44:06 +0800

    3-18-2024 5:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,40 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+  
+    const dp = [];
+    for (let i = 0; i <= m; i++) {
+        dp[i] = Array(n + 1).fill(0);
+    }
+  
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+  
+    let lcs = '';
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+  
+    return lcs;
+}
+
+const str1 = "ABCBDAB";
+const str2 = "BDCAB";
+const result = longestCommonSubsequence(str1, str2);
+console.log(result); // Output: BCAB
