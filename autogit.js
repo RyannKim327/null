517159ac44a44612b369c5dc6039ba07e862fe4a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:11:12 +0800

    3-17-2024 9:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,3 @@
-// Define an array of integers
-let numbers = [5, 3, 8, 1, 2];
-
-// Sort the array in ascending order
-numbers.sort(function(a, b) {
-    return a - b;
-});
-
-// Print the sorted array
-console.log(numbers); // Output: [1, 2, 3, 5, 8]
+let str = "Hello, world! This is a string with spaces.";
+let stringWithoutSpaces = str.replace(/\s/g, '');
+console.log(stringWithoutSpaces);
