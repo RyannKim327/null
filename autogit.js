aa048d4454cae08395c0a205b2bb377320d490b2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:47:22 +0800

    3-17-2024 8:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,44 @@
-function findFirstRepeatedChar(str) {
-    const seen = new Set();
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-    for (const char of str) {
-        if (seen.has(char)) {
-            return char;
-        }
-        seen.add(char);
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
+  }
 
-    return null; // No repeated character found
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
 }
 
-const inputStr = "hello world";
-const repeatedChar = findFirstRepeatedChar(inputStr);
+// Example usage
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
 
-if (repeatedChar) {
-    console.log(`First repeated character in '${inputStr}' is '${repeatedChar}'`);
-} else {
-    console.log("No repeated characters found in the input string.");
-}
+console.log(linkedList.getLength()); // Output: 4
