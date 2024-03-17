8406be367f63e8f6f66ecc3db4aa0d64ccc8185d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:47:49 +0800

    3-17-2024 8:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,8 @@
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-let index = array.indexOf(elementToRemove);
-if (index > -1) {
-    array.splice(index, 1);
-}
-
-console.log(array); // Output: [1, 2, 4, 5]
-let array = [1, 2, 3, 4, 5];
-let indexToRemove = 2; // Index of element 3
-
-array.splice(indexToRemove, 1);
-
-console.log(array); // Output: [1, 2, 4, 5]
+let str1 = "Hello";
+let str2 = " World";
+let result = str1 + str2;
+console.log(result); // Output: Hello World
+let str1 = "Hello";
+let str2 = " World";
+let result = str1.concat(str2);
+console.log(result); // Output: Hello World
