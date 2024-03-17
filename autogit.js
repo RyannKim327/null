8b908ec9cfc243b83da53d2e2b37adcdb27c56f3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:09:12 +0800

    3-17-2024 9:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,47 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertAtEnd(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    getLength() {
+        let count = 0;
+        let current = this.head;
+
+        while (current) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
+
+// Usage
+const linkedList = new LinkedList();
+linkedList.insertAtEnd(1);
+linkedList.insertAtEnd(2);
+linkedList.insertAtEnd(3);
+
+console.log(linkedList.getLength()); // Output: 3
