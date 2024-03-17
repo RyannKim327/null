1bded2f050437c0f533c3dd6a2fa33077e57e2a6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:44:26 +0800

    3-17-2024 8:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,18 @@
-function hasCycle(head) {
-    if (!head || !head.next) {
+function isPrime(num) {
+    if(num <= 1) {
         return false;
     }
 
-    let slow = head;
-    let fast = head.next;
-
-    while (fast && fast.next) {
-        if (slow === fast) {
-            return true;
+    // Check if num is divisible by any number from 2 to the square root of num
+    for(let i = 2; i <= Math.sqrt(num); i++) {
+        if(num % i === 0) {
+            return false;
         }
-        slow = slow.next;
-        fast = fast.next.next;
     }
 
-    return false;
-}
-// Define a simple Node class for the linked list
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+    return true;
 }
 
-// Build a linked list with a cycle
-let node1 = new Node(1);
-let node2 = new Node(2);
-let node3 = new Node(3);
-node1.next = node2;
-node2.next = node3;
-node3.next = node1; // Create a cycle
-
-console.log(hasCycle(node1)); // Output: true
+// Test the function
+console.log(isPrime(17));  // Output: true
+console.log(isPrime(15));  // Output: false
