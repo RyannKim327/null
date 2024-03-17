0e469e2375bb0b30baf1254af799856b61a3a39e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:49:12 +0800

    3-18-2024 5:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,19 @@
-var str1 = "Hello";
-var str2 = "World";
-var result = str1 + " " + str2;
-console.log(result); // Output: "Hello World"
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Test the isPrime function
+const number = 17;
+if (isPrime(number)) {
+  console.log(`${number} is a prime number.`);
+} else {
+  console.log(`${number} is not a prime number.`);
+}
