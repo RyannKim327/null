cf73ebe956cce088edf17f7750139b999b452e5a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:40:03 +0800

    3-18-2024 5:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,73 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+function biDirectionalSearch(graph, startNode, endNode) {
+    let forwardQueue = [startNode];
+    let backwardQueue = [endNode];
+    let visitedForward = new Set();
+    let visitedBackward = new Set();
+    
+    while (forwardQueue.length > 0 && backwardQueue.length > 0) {
+        let forwardNode = forwardQueue.shift();
+        let backwardNode = backwardQueue.shift();
+        
+        if (visitedForward.has(forwardNode) || visitedBackward.has(backwardNode)) {
+            continue;
         }
+        
+        visitedForward.add(forwardNode);
+        visitedBackward.add(backwardNode);
+        
+        if (forwardNode === backwardNode) {
+            return getPath(graph, startNode, endNode, forwardNode);
+        }
+        
+        let forwardNeighbors = getNeighbors(graph, forwardNode);
+        let backwardNeighbors = getNeighbors(graph, backwardNode);
+        
+        forwardQueue.push(...forwardNeighbors.filter(node => !visitedForward.has(node)));
+        backwardQueue.push(...backwardNeighbors.filter(node => !visitedBackward.has(node)));
     }
-
-    return -1;
+    
+    return "No path found";
 }
 
-// Example
-const array = [1, 3, 5, 7, 9, 11, 13];
-const target = 7;
-const index = binarySearch(array, target);
-
-if (index !== -1) {
-    console.log(`Target ${target} found at index ${index}`);
-} else {
-    console.log(`Target ${target} not found in the array`);
+function getNeighbors(graph, node) {
+    return graph[node];
 }
+
+function getPath(graph, startNode, endNode, commonNode) {
+    let path = [];
+    let currentNode = commonNode;
+    
+    while (currentNode !== startNode) {
+        path.unshift(currentNode);
+        currentNode = graph[currentNode].parent;
+    }
+    
+    currentNode = commonNode;
+    
+    while (currentNode !== endNode) {
+        path.push(currentNode);
+        currentNode = graph[currentNode].parent;
+    }
+    
+    path.push(endNode);
+    
+    return path;
+}
+
+// Example graph
+let graph = {
+    A: ['B', 'C'],
+    B: ['A', 'D'],
+    C: ['A', 'E'],
+    D: ['B', 'F'],
+    E: ['C'],
+    F: ['D', 'G'],
+    G: ['F']
+};
+
+let startNode = 'A';
+let endNode = 'G';
+
+let path = biDirectionalSearch(graph, startNode, endNode);
+
+console.log(path);
