a8e2524156c1ee0fc084662953b615c6788c06b9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:49:51 +0800

    3-17-2024 8:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,62 @@
-let str = "Hello, World!";
-let substring = "World";
-
-if (str.includes(substring)) {
-    console.log("Substring found in the string");
-} else {
-    console.log("Substring not found in the string");
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
-let str = "Hello, World!";
-let substring = "World";
 
-if (str.indexOf(substring) !== -1) {
-    console.log("Substring found in the string");
-} else {
-    console.log("Substring not found in the string");
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  reverse() {
+    let prev = null;
+    let current = this.head;
+    let next = null;
+
+    while (current) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+
+    this.head = prev;
+  }
+
+  print() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
 }
+
+// Example
+const list = new LinkedList();
+list.add(1);
+list.add(2);
+list.add(3);
+
+console.log('Original linked list:');
+list.print();
+
+list.reverse();
+
+console.log('Reversed linked list:');
+list.print();
