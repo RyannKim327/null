9ae6c61b831acbbe30a0c20bbf8195832050321d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:37:18 +0800

    3-18-2024 5:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,70 @@
-class Stack {
-  constructor() {
-    this.stack = [];
-  }
-
-  push(item) {
-    this.stack.push(item);
-  }
-
-  pop() {
-    if (this.isEmpty()) {
-      return "Stack Underflow";
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
     }
-    return this.stack.pop();
-  }
-
-  isEmpty() {
-    return this.stack.length === 0;
-  }
 }
 
-// Example Usage:
-const stack = new Stack();
+function getIntersectionNode(headA, headB) {
+    if (!headA || !headB) {
+        return null;
+    }
 
-stack.push(1);
-stack.push(2);
-stack.push(3);
+    let lenA = getLength(headA);
+    let lenB = getLength(headB);
 
-console.log(stack.pop()); // Output: 3
-console.log(stack.pop()); // Output: 2
-console.log(stack.pop()); // Output: 1
-console.log(stack.pop()); // Output: Stack Underflow
+    let currA = headA;
+    let currB = headB;
+
+    // Traverse the longer list by the difference in lengths
+    while (lenA > lenB) {
+        currA = currA.next;
+        lenA--;
+    }
+
+    while (lenB > lenA) {
+        currB = currB.next;
+        lenB--;
+    }
+
+    // Traverse both lists in parallel
+    while (currA && currB) {
+        if (currA === currB) {
+            return currA;
+        }
+        currA = currA.next;
+        currB = currB.next;
+    }
+
+    return null;
+}
+
+function getLength(head) {
+    let length = 0;
+    let current = head;
+    while (current) {
+        length++;
+        current = current.next;
+    }
+    return length;
+}
+
+// Example
+// Intersected linked lists:
+// 1 -> 2 -> 3 -> 4 -> 5 -> 6
+//                   \
+//                    7 -> 8 -> 9
+let headA = new ListNode(1);
+headA.next = new ListNode(2);
+headA.next.next = new ListNode(3);
+headA.next.next.next = new ListNode(4);
+headA.next.next.next.next = new ListNode(5);
+headA.next.next.next.next.next = new ListNode(6);
+
+let headB = new ListNode(7);
+headB.next = new ListNode(8);
+headB.next.next = new ListNode(9);
+headB.next.next.next = headA.next.next.next.next; // Intersection point
+
+let intersectionNode = getIntersectionNode(headA, headB);
+console.log(intersectionNode); // Output: ListNode { val: 5, next: ListNode { val: 6, next: null } }
