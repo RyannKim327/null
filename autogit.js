5183fcced247fdb2184f2066c9384b929d5d50cb
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:40:28 +0800

    3-18-2024 5:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,10 @@
-function findMajorityElement(nums) {
-    let count = 0;
-    let candidate = null;
+const axios = require('axios');
 
-    for (let num of nums) {
-        if (count === 0) {
-            candidate = num;
-        }
-
-        count += (num === candidate) ? 1 : -1;
-    }
-
-    return candidate;
-}
-
-// Test the function
-let nums = [1, 2, 2, 2, 3, 2, 4, 2, 2];
-console.log(findMajorityElement(nums)); // Output: 2
+// Make a GET request to a sample API
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
