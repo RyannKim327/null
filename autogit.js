9a6cea3e4fe7b083f9e4baaa803c341e0504fca2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:04:53 +0800

    3-17-2024 9:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,60 @@
-function findMajorityElement(arr) {
-    let hash = {};
-    let majorityElement = null;
-    let majorityCount = 0;
+class HashTable {
+  constructor(size = 100) {
+    this.size = size;
+    this.data = new Array(size);
+  }
 
-    // Count occurrences of each element in the array
-    for (let num of arr) {
-        hash[num] = (hash[num] || 0) + 1;
-        if (hash[num] > majorityCount) {
-            majorityElement = num;
-            majorityCount = hash[num];
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.size;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+
+    if (!this.data[index]) {
+      this.data[index] = [];
+    }
+
+    this.data[index].push([key, value]);
+  }
+  
+  get(key) {
+    const index = this._hash(key);
+
+    if (!this.data[index]) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.data[index].length; i++) {
+      if (this.data[index][i][0] === key) {
+        return this.data[index][i][1];
+      }
+    }
+
+    return undefined;
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        for (let j = 0; j < this.data[i].length; j++) {
+          keysArray.push(this.data[i][j][0]);
         }
+      }
     }
-
-    // Check if the majority element is indeed the majority
-    if (majorityCount > arr.length / 2) {
-        return majorityElement;
-    } else {
-        return "No majority element found";
-    }
+    return keysArray;
+  }
 }
+const myHashTable = new HashTable();
+myHashTable.set('firstName', 'John');
+myHashTable.set('lastName', 'Doe');
 
-// Example
-const array = [2, 2, 1, 1, 1, 2, 2];
-console.log(findMajorityElement(array)); // Output: 2
+console.log(myHashTable.get('firstName')); // Output: John
+console.log(myHashTable.get('age')); // Output: undefined
+
+console.log(myHashTable.keys()); // Output: ['firstName', 'lastName']
