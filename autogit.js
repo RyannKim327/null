168ccef9b9eccd47b00825cdce716a1f1523fd59
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:09:42 +0800

    3-17-2024 9:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,17 @@
-function checkAnagram(str1, str2) {
-    // Remove spaces and punctuation, convert to lowercase, and split into arrays
-    const cleanStr1 = str1.replace(/[^\w]/g, "").toLowerCase().split("");
-    const cleanStr2 = str2.replace(/[^\w]/g, "").toLowerCase().split("");
-
-    // Sort both arrays alphabetically and convert back to strings
-    const sortedStr1 = cleanStr1.sort().join("");
-    const sortedStr2 = cleanStr2.sort().join("");
-
-    // Compare the sorted strings
-    if (sortedStr1 === sortedStr2) {
-        return true;
-    } else {
-        return false;
+function countCharacter(str, char) {
+    let count = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
+        }
     }
+    
+    return count;
 }
 
-// Test the function
-const string1 = "Listen";
-const string2 = "Silent";
-
-if (checkAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example usage
+const str = "hello world";
+const charToCount = 'o';
+const occurrences = countCharacter(str, charToCount);
+console.log(`Number of occurrences of '${charToCount}' in '${str}': ${occurrences}`);
