ed315f862a071113a0c8aa75a9a33dda126fb686
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:45:48 +0800

    3-18-2024 5:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-function preprocessPattern(pattern) {
-    let prefix = new Array(pattern.length).fill(0);
-    let j = 0;
-    
-    for (let i = 1; i < pattern.length; i++) {
-        while (j > 0 && pattern[i] !== pattern[j]) {
-            j = prefix[j - 1];
-        }
-        
-        if (pattern[i] === pattern[j]) {
-            j++;
-        }
-        
-        prefix[i] = j;
-    }
-    
-    return prefix;
-}
+const axios = require('axios');
 
-function kmpSearch(text, pattern) {
-    let prefix = preprocessPattern(pattern);
-    let j = 0;
-
-    for (let i = 0; i < text.length; i++) {
-        while (j > 0 && text[i] !== pattern[j]) {
-            j = prefix[j - 1];
-        }
-
-        if (text[i] === pattern[j]) {
-            j++;
-        }
-
-        if (j === pattern.length) {
-            console.log(`Pattern found at index ${i - j + 1}`);
-            j = prefix[j - 1];
-        }
-    }
-}
-
-// Test the algorithm
-let text = "ABABDABACDABABCABAB";
-let pattern = "ABABCABAB";
-kmpSearch(text, pattern);
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
