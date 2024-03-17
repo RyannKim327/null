1c5732c4de939104148f5fd040836d9193bd8dcc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:16:11 +0800

    3-17-2024 9:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,46 @@
-class BeamSearch {
-  constructor(beamWidth) {
-    this.beamWidth = beamWidth;
-  }
-
-  search(startNode, expandFunction, goalFunction) {
-    let frontier = [[startNode, 0]];
-    let nextFrontier = [];
-
-    while (frontier.length > 0) {
-      frontier.forEach(([node, cost]) => {
-        const children = expandFunction(node);
-        children.forEach(([child, childCost]) => {
-          const totalCost = cost + childCost;
-          nextFrontier.push([child, totalCost]);
-        });
-      });
-
-      nextFrontier.sort((a, b) => a[1] - b[1]);
-      frontier = nextFrontier.slice(0, this.beamWidth);
-      nextFrontier = [];
-
-      const foundGoal = frontier.find(([node, _]) => goalFunction(node));
-      if (foundGoal) {
-        return foundGoal[0];
-      }
-    }
-
-    return null;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
 }
+function convertListToArray(head) {
+  let arr = [];
+  while (head !== null) {
+    arr.push(head.value);
+    head = head.next;
+  }
+  return arr;
+}
+function isPalindrome(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  
+  while (left < right) {
+    if (arr[left] !== arr[right]) {
+      return false;
+    }
+    
+    left++;
+    right--;
+  }
+  
+  return true;
+}
+function isLinkedListPalindrome(head) {
+  const arr = convertListToArray(head);
+  return isPalindrome(arr);
+}
+// Construct a linked list
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(2);
+const node5 = new Node(1);
 
-// Usage example:
-const beamSearch = new BeamSearch(2);
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
 
-// Define expand function
-const expandFunction = (node) => {
-  return [
-    [node + 1, 1],
-    [node + 2, 2],
-  ];
-};
-
-// Define goal function
-const goalFunction = (node) => {
-  return node === 5;
-};
-
-const startNode = 0;
-const result = beamSearch.search(startNode, expandFunction, goalFunction);
-console.log(result);
+console.log(isLinkedListPalindrome(node1)); // Output: true
