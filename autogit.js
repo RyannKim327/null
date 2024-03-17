6c92a773a60422580e734c49eb2693fe316e27ff
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:59:29 +0800

    3-17-2024 8:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,15 @@
-let str = "42";
-let num = parseInt(str);
-console.log(num); // Output: 42
-let str = "42";
-let num = Number(str);
-console.log(num); // Output: 42
+function maxSubArraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+}
+
+// Example usage
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArraySum(arr)); // Output: 6 (corresponding to subarray [4, -1, 2, 1])
