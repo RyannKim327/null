3d5ec0539d94b97a7d7160eeeb7fa73957847466
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:07:28 +0800

    3-17-2024 9:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,49 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-console.log(result); // This will output "Hello World"
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  findMiddleElement() {
+    let fast = this.head;
+    let slow = this.head;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow.value;
+  }
+}
+
+// Usage
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+
+const middleElement = linkedList.findMiddleElement();
+console.log(middleElement); // Output: 3
