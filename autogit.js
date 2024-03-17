b3af041e086435230fd33b7f17bcc7dd47923c6e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:07:13 +0800

    3-17-2024 9:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,46 @@
-function decimalToBinary(decimal) {
-    return (decimal >>> 0).toString(2);
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-// Example
-let decimalNumber = 42;
-let binaryNumber = decimalToBinary(decimalNumber);
-console.log(binaryNumber); // Output: 101010
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addNode(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    getLength() {
+        let current = this.head;
+        let count = 0;
+
+        while (current) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
+
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+
+console.log("Length of linked list: ", linkedList.getLength());
