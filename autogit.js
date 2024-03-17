15b6abebb2376ade6e25d9e0d7c9b1470f1cb261
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:12:33 +0800

    3-17-2024 9:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,89 +1,29 @@
-class Node {
-    constructor(order, isLeaf) {
-        this.keys = [];
-        this.children = [];
-        this.isLeaf = isLeaf;
-        this.order = order;
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
     }
+
+    if (number <= 3) {
+        return true;
+    }
+
+    if (number % 2 === 0 || number % 3 === 0) {
+        return false;
+    }
+
+    for (let i = 5; i * i <= number; i += 6) {
+        if (number % i === 0 || number % (i + 2) === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
-class BTree {
-    constructor(order) {
-        this.root = new Node(order, true);
-        this.order = order;
-    }
 
-    insert(key) {
-        let root = this.root;
-        if (root.keys.length === 2 * this.order - 1) {
-            let newRoot = new Node(this.order, false);
-            newRoot.children.push(root);
-            this.splitChild(newRoot, 0);
-            this.insertNonFull(newRoot, key);
-            this.root = newRoot;
-        } else {
-            this.insertNonFull(root, key);
-        }
-    }
-
-    insertNonFull(node, key) {
-        let i = node.keys.length - 1;
-        if (node.isLeaf) {
-            while (i >= 0 && key < node.keys[i]) {
-                node.keys[i + 1] = node.keys[i];
-                i--;
-            }
-            node.keys[i + 1] = key;
-        } else {
-            while (i >= 0 && key < node.keys[i]) {
-                i--;
-            }
-            i++;
-            if (node.children[i].keys.length === 2 * this.order - 1) {
-                this.splitChild(node, i);
-                if (key > node.keys[i]) {
-                    i++;
-                }
-            }
-            this.insertNonFull(node.children[i], key);
-        }
-    }
-
-    splitChild(node, i) {
-        let order = this.order;
-        let child = node.children[i];
-        let newChild = new Node(order, child.isLeaf);
-        node.children.splice(i + 1, 0, newChild);
-        node.keys.splice(i, 0, child.keys[order - 1]);
-        newChild.keys = child.keys.splice(order, order - 1);
-        if (!child.isLeaf) {
-            newChild.children = child.children.splice(order, order);
-        }
-    }
-
-    search(key) {
-        return this.searchNode(this.root, key);
-    }
-
-    searchNode(node, key) {
-        let i = 0;
-        while (i < node.keys.length && key > node.keys[i]) {
-            i++;
-        }
-        if (i < node.keys.length && key === node.keys[i]) {
-            return node;
-        }
-        if (node.isLeaf) {
-            return null;
-        } else {
-            return this.searchNode(node.children[i], key);
-        }
-    }
+// Test the function
+const number = 17; // Change this to test different numbers
+if (isPrime(number)) {
+    console.log(number + ' is a prime number');
+} else {
+    console.log(number + ' is not a prime number');
 }
-let btree = new BTree(3);
-btree.insert(5);
-btree.insert(10);
-btree.insert(3);
-btree.insert(7);
-btree.insert(12);
-
-console.log(btree.search(7));
