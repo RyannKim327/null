0eb0f869d3dfc802ce3d07a4d43999d699fe266e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:04:07 +0800

    3-17-2024 9:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,27 @@
-function largestPrimeFactor(number) {
-    let factor = 2;
-
-    while (factor <= number) {
-        if (number % factor === 0) {
-            number /= factor;
-        } else {
-            factor++;
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-
-    return factor;
 }
 
-const number = 600851475143;
-const largestPrimeFactorValue = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is: ${largestPrimeFactorValue}`);
+function findMiddleElement(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow.value;
+}
+
+// Create a sample linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+console.log(findMiddleElement(head)); // Output: 3
