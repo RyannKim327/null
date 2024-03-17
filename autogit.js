27f5264672081d2d3486f1d723f131dacb33a956
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:40:54 +0800

    3-18-2024 5:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,36 @@
-const array1 = [1, 2, 3, 4];
-const array2 = [3, 4, 5, 6];
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
 
-const commonElements = array1.filter(element => array2.includes(element));
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
 
-console.log(commonElements);
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    let result = '';
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            result = str1[i - 1] + result;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return result;
+}
+
+const str1 = "ABCBDAB";
+const str2 = "BDCAB";
+console.log(longestCommonSubsequence(str1, str2)); // Output: "BCAB"
