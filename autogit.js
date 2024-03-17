f1990f69d37b4aaf79dfc6dc3777bfa373e40f51
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:48:24 +0800

    3-17-2024 8:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,16 @@
-function bubbleSort(arr) {
-    var len = arr.length;
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
         }
+        arr[j + 1] = current;
     }
     return arr;
 }
 
 // Example usage:
-var arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + bubbleSort(arr));
+let arr = [5, 2, 4, 6, 1, 3];
+console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5, 6]
