0f233b1bbae88bc950f9e877b5e678b52c09c26f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:49:44 +0800

    3-17-2024 8:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,16 @@
-// Define a Node class to represent each node in the linked list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+let str = "Hello, World!";
+let substring = "World";
+
+if (str.includes(substring)) {
+    console.log("Substring found in the string");
+} else {
+    console.log("Substring not found in the string");
 }
+let str = "Hello, World!";
+let substring = "World";
 
-// Define a LinkedList class to represent the linked list
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    // Function to add a new node to the end of the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Function to calculate the length of the linked list
-    getLength() {
-        let length = 0;
-        let current = this.head;
-        while (current) {
-            length++;
-            current = current.next;
-        }
-        return length;
-    }
+if (str.indexOf(substring) !== -1) {
+    console.log("Substring found in the string");
+} else {
+    console.log("Substring not found in the string");
 }
-
-// Create a new linked list
-const linkedList = new LinkedList();
-
-// Add nodes to the linked list
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-
-// Calculate the length of the linked list
-const length = linkedList.getLength();
-console.log("Length of the linked list: " + length);
