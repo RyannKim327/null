6b810078336b67822ed962d6a153b653c7502c66
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:18 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,3 @@
-async function connectToDevice() {
-  try {
-    const response = await fetch('http://yourandroiddeviceip:port/connect');
-    const data = await response.json();
-    
-    console.log('Connected to Android device:', data);
-    
-    // Continue with more operations here...
-  } catch (error) {
-    console.error('Error connecting to Android device:', error);
-  }
-}
-
-connectToDevice();
+let str = "Hello, World!";
+let stringWithoutSpaces = str.replace(/\s/g, '');
+console.log(stringWithoutSpaces);
