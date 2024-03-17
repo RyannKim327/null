baa1d39bfc19e0715a5e4b8951475d0a083baa08
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:47:26 +0800

    3-17-2024 8:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,14 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function reverseWords(str) {
+    // Split the string into an array of words
+    let words = str.split(" ");
+    
+    // Reverse the array
+    words.reverse();
+
+    // Join the array back into a string
+    return words.join(" ");
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  addNode(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  getLength() {
-    let count = 0;
-    let current = this.head;
-    while (current) {
-      count++;
-      current = current.next;
-    }
-    return count;
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-
-console.log(linkedList.getLength()); // Output: 4
+let originalString = "Hello World";
+let reversedString = reverseWords(originalString);
+console.log(reversedString); // Output: "World Hello"
