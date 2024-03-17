b30c6cba279267d7a36d746b62e28cfc9690223b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:43:12 +0800

    3-17-2024 8:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,25 @@
-function findMedianSortedArrays(nums1, nums2) {
-    const merged = nums1.concat(nums2).sort((a, b) => a - b);
-    const n = merged.length;
-    
-    if (n % 2 === 0) {
-        const mid1 = merged[n / 2 - 1];
-        const mid2 = merged[n / 2];
-        return (mid1 + mid2) / 2;
-    } else {
-        return merged[Math.floor(n / 2)];
+function isPrime(num) {
+    if (num <= 1) {
+        return false;
     }
+    if (num === 2) {
+        return true;
+    }
+    if (num % 2 === 0) {
+        return false;
+    }
+    for (let i = 3; i <= Math.sqrt(num); i += 2) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Example usage
-const nums1 = [1, 3];
-const nums2 = [2];
-const median = findMedianSortedArrays(nums1, nums2);
-console.log(median); // Output: 2
+// Test the function
+const number = 17; // Change this number to test
+if (isPrime(number)) {
+    console.log(`${number} is a prime number.`);
+} else {
+    console.log(`${number} is not a prime number.`);
+}
