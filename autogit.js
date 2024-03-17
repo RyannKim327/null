12b00a6498918810c4998e5e225562f138f7caf5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:50:33 +0800

    3-17-2024 8:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,9 @@
-function isAnagram(str1, str2) {
-  // Remove any non-alphanumeric characters and convert to lowercase
-  const cleanStr1 = str1.replace(/[^\w]/g, "").toLowerCase();
-  const cleanStr2 = str2.replace(/[^\w]/g, "").toLowerCase();
-  
-  // Return false if the strings are not of equal length
-  if (cleanStr1.length !== cleanStr2.length) {
-    return false;
-  }
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
 
-  // Sort the characters in the strings and compare them
-  const sortedStr1 = cleanStr1.split('').sort().join('');
-  const sortedStr2 = cleanStr2.split('').sort().join('');
-
-  return sortedStr1 === sortedStr2;
+let index = array.indexOf(elementToRemove);
+if (index > -1) {
+    array.splice(index, 1);
 }
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-  console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-  console.log(`${string1} and ${string2} are not anagrams.`);
-}
+console.log(array); // Output: [1, 2, 4, 5]
