e7f28fb8441f0e3ee133903e1b45d5ed80227e53
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:00:35 +0800

    3-17-2024 9:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,9 @@
-function validateEmail(email) {
-    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return re.test(email);
+function calculateTriangleArea(base, height) {
+    return 0.5 * base * height;
 }
 
-const email = "example@email.com";
-if (validateEmail(email)) {
-    console.log("Email is valid");
-} else {
-    console.log("Email is not valid");
-}
+// Example usage
+const base = 5;
+const height = 10;
+const area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
