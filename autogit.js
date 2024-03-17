79104be1f5f1e6c378933e38bef0dd596646b1d8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:13:24 +0800

    3-17-2024 9:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,47 @@
-function burrowsWheelerTransform(input) {
-  // Add a unique end-of-text symbol
-  input += '$';
-  
-  // Generate all cyclic permutations of the input string
-  const rotations = [...input].map((_, index) => input.slice(index) + input.slice(0, index));
-  
-  // Sort the rotations lexicographically
-  const sortedRotations = rotations.sort();
-  
-  // Extract the last characters of the sorted rotations to form the Burrows-Wheeler transformed string
-  const transformedString = sortedRotations.map(rotation => rotation.slice(-1)).join('');
-  
-  // Find the position of the original input in the sorted rotations to get the index of the original string
-  const originalIndex = sortedRotations.indexOf(input);
-  
-  // Return the transformed string and the index of the original input
-  return { transformedString, originalIndex };
+class ListNode {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-// Test the implementation
-const inputString = 'hello world';
-const { transformedString, originalIndex } = burrowsWheelerTransform(inputString);
+function findIntersection(list1, list2) {
+    let set = new Set();
+    
+    // Store nodes of list1 in a set
+    let current = list1;
+    while (current) {
+        set.add(current);
+        current = current.next;
+    }
+    
+    // Traverse list2 and check if any node is in set
+    current = list2;
+    while (current) {
+        if (set.has(current)) {
+            return current;
+        }
+        current = current.next;
+    }
+    
+    return null; // No intersection found
+}
 
-console.log('Input String:', inputString);
-console.log('Transformed String:', transformedString);
-console.log('Original Index:', originalIndex);
+// Example linked lists
+let node1 = new ListNode(1);
+let node2 = new ListNode(2);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+
+let nodeA = new ListNode('A');
+let nodeB = new ListNode('B');
+
+nodeA.next = nodeB;
+nodeB.next = node3;
+
+let intersectionNode = findIntersection(node1, nodeA);
+console.log('Intersection node value:', intersectionNode.value);
