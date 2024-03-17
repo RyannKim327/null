bb6108ec92fa7650dbf92860f740d8e2aa895729
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:01:16 +0800

    3-17-2024 9:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,96 @@
-function calculateAreaOfTriangle(base, height) {
-    return 0.5 * base * height;
+class Node {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
+    }
 }
 
-// Example usage
-const base = 5;
-const height = 8;
-const area = calculateAreaOfTriangle(base, height);
+class SkipList {
+    constructor(maxLevel, probability) {
+        this.maxLevel = maxLevel;
+        this.probability = probability;
+        this.level = 0;
+        this.head = new Node(-Infinity, maxLevel);
+    }
 
-console.log("The area of the triangle is: " + area);
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.probability && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current === null || current.value !== value) {
+            let newLevel = this.randomLevel();
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.head;
+                }
+                this.level = newLevel;
+            }
+
+            let newNode = new Node(value, newLevel);
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    search(value) {
+        let current = this.head;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+        current = current.forward[0];
+
+        if (current !== null && current.value === value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    remove(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] !== null && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current !== null && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].forward[i] !== current) {
+                    break;
+                }
+                update[i].forward[i] = current.forward[i];
+            }
+
+            while (this.level > 0 && this.head.forward[this.level] === null) {
+                this.level--;
+            }
+        }
+    }
+}
