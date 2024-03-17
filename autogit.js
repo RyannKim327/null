22f10c5c05a83102efbeae2560613eee0cf236c2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:29 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,11 @@
-class Stack {
-    constructor() {
-        this.items = [];
+function findStringLength(str) {
+    let count = 0;
+    while (str[count] !== undefined) {
+        count++;
     }
-
-    // Push element onto the stack
-    push(element) {
-        this.items.push(element);
-    }
-
-    // Pop element from the stack
-    pop() {
-        if (this.items.length === 0) {
-            return "Underflow";
-        }
-        return this.items.pop();
-    }
-
-    // Peek at the top element of the stack
-    peek() {
-        return this.items[this.items.length - 1];
-    }
-
-    // Check if the stack is empty
-    isEmpty() {
-        return this.items.length === 0;
-    }
-
-    // Print the stack
-    printStack() {
-        let str = "";
-        for (let i = 0; i < this.items.length; i++) {
-            str += this.items[i] + " ";
-        }
-        return str;
-    }
+    return count;
 }
 
-// Usage
-const stack = new Stack();
-console.log(stack.isEmpty()); // true
-
-stack.push(10);
-stack.push(20);
-stack.push(30);
-
-console.log(stack.printStack()); // 10 20 30
-console.log(stack.peek()); // 30
-
-stack.pop();
-console.log(stack.printStack()); // 10 20
+const myString = "Hello, World!";
+const length = findStringLength(myString);
+console.log("Length of the string: " + length); // Output: Length of the string: 13
