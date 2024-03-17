eb3633ea6eaeac0249b1121d46de7d1e35b1f0dd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:15:41 +0800

    3-17-2024 9:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,8 @@
-function isArraySorted(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [5, 4, 3, 2, 1];
+let inputString = "Hello, World!";
+let result = removeVowels(inputString);
 
-console.log(isArraySorted(array1)); // Output: true
-console.log(isArraySorted(array2)); // Output: false
+console.log(result); // Output will be "Hll, Wrld!"
