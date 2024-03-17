caf780c1ca8689890167b6e2e37e76a671481c2d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:43:01 +0800

    3-17-2024 8:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,8 @@
-function createPrefixTable(pattern) {
-    const prefixTable = [0];
-    let prefixLength = 0;
-
-    for (let i = 1; i < pattern.length; i++) {
-        while (prefixLength > 0 && pattern.charAt(i) !== pattern.charAt(prefixLength)) {
-            prefixLength = prefixTable[prefixLength - 1];
-        }
-        if (pattern.charAt(i) === pattern.charAt(prefixLength)) {
-            prefixLength++;
-        }
-        prefixTable[i] = prefixLength;
-    }
-
-    return prefixTable;
+function findKthSmallest(arr, k) {
+    arr.sort((a, b) => a - b);
+    return arr[k - 1];
 }
-function kmpSearch(text, pattern) {
-    const prefixTable = createPrefixTable(pattern);
-    let matches = [];
 
-    let textIndex = 0;
-    let patternIndex = 0;
-
-    while (textIndex < text.length) {
-        if (pattern.charAt(patternIndex) === text.charAt(textIndex)) {
-            textIndex++;
-            patternIndex++;
-            if (patternIndex === pattern.length) {
-                matches.push(textIndex - patternIndex);
-                patternIndex = prefixTable[patternIndex - 1];
-            }
-        } else {
-            if (patternIndex !== 0) {
-                patternIndex = prefixTable[patternIndex - 1];
-            } else {
-                textIndex++;
-            }
-        }
-    }
-
-    return matches;
-}
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const matches = kmpSearch(text, pattern);
-
-console.log("Pattern found at positions:", matches);
+let array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+let k = 4;
+console.log(`The ${k}th smallest element in the array is: ${findKthSmallest(array, k)}`);
