a9ed3cc4b0e84c3135bc4ab48baf08c2d71f9e98
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:46:07 +0800

    3-18-2024 5:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,52 @@
-function firstNonRepeatingChar(str) {
-    for (let i = 0; i < str.length; i++) {
-        let char = str.charAt(i);
-        if (str.indexOf(char) === str.lastIndexOf(char)) {
-            return char;
+class Graph {
+    constructor() {
+        this.adjList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjList[vertex]) {
+            this.adjList[vertex] = [];
         }
     }
-    return null; // Return null if there is no non-repeating character
+
+    addEdge(source, destination) {
+        this.adjList[source].push(destination);
+        // For undirected graph, you may also want to add the reverse edge
+        // this.adjList[destination].push(source);
+    }
+
+    dfs(startVertex) {
+        const visited = {};
+        this._dfsHelper(startVertex, visited);
+    }
+
+    _dfsHelper(vertex, visited) {
+        visited[vertex] = true;
+        console.log(vertex);
+
+        for (const neighbor of this.adjList[vertex]) {
+            if (!visited[neighbor]) {
+                this._dfsHelper(neighbor, visited);
+            }
+        }
+    }
 }
 
-let str = "hello world";
-let firstNonRepeating = firstNonRepeatingChar(str);
+// Create a graph
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
 
-console.log("First non-repeating character: ", firstNonRepeating);
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('B', 'E');
+graph.addEdge('C', 'F');
+
+// Perform DFS starting from vertex 'A'
+console.log('DFS traversal:');
+graph.dfs('A');
