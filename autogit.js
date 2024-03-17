e8f9133b18ef82e644ef14c8456e7e08cf84b0e6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:48:19 +0800

    3-17-2024 8:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,76 +1,18 @@
-// Node class to represent a node in the red-black tree
-class Node {
-    constructor(value, color, left = null, right = null, parent = null) {
-        this.value = value;
-        this.color = color; // Red or Black
-        this.left = left;
-        this.right = right;
-        this.parent = parent;
-    }
-}
-
-// RedBlackTree class to represent the red-black tree and its operations
-class RedBlackTree {
-    constructor() {
-        this.root = null;
-    }
-
-    // Insert a value into the red-black tree
-    insert(value) {
-        let node = new Node(value, "RED");
-
-        if (!this.root) {
-            this.root = node;
-        } else {
-            this.insertNode(this.root, node);
-        }
-
-        this.fixViolations(node);
-    }
-
-    // Helper method to insert a node into the red-black tree
-    insertNode(root, node) {
-        if (node.value < root.value) {
-            if (root.left === null) {
-                root.left = node;
-                node.parent = root;
-            } else {
-                this.insertNode(root.left, node);
-            }
-        } else if (node.value > root.value) {
-            if (root.right === null) {
-                root.right = node;
-                node.parent = root;
-            } else {
-                this.insertNode(root.right, node);
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-
-    // Fix any violations of the red-black tree properties after insertion
-    fixViolations(node) {
-        while (node !== this.root && node.color === "RED" && node.parent.color === "RED") {
-            // Implement rotation and recoloring rules for fixing violations here
-        }
-
-        this.root.color = "BLACK";
-    }
-
-    // Print the red-black tree in-order traversal
-    inOrderTraversal(node) {
-        if (node !== null) {
-            this.inOrderTraversal(node.left);
-            console.log(node.value + " " + node.color);
-            this.inOrderTraversal(node.right);
-        }
-    }
+    return arr;
 }
 
 // Example usage:
-let rbTree = new RedBlackTree();
-rbTree.insert(10);
-rbTree.insert(20);
-rbTree.insert(30);
-rbTree.insert(40);
-
-rbTree.inOrderTraversal(rbTree.root);
+var arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + bubbleSort(arr));
