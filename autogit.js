bf283b9ae770036f5134b95b595624229cb68c41
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:57:30 +0800

    3-17-2024 8:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,17 @@
-// Function to perform beam search
-function beamSearch(startNode, beamWidth, maxDepth, getSuccessors, heuristicFunction) {
-    let currentNodes = [startNode];
-    
-    for (let depth = 0; depth < maxDepth; depth++) {
-        let nextNodes = [];
-        
-        for (let node of currentNodes) {
-            let successors = getSuccessors(node);
-            
-            for (let successor of successors) {
-                successor.parent = node;
-                successor.heuristic = heuristicFunction(successor);
-                nextNodes.push(successor);
-            }
+function countOccurrences(str, word) {
+    let count = 0;
+    let words = str.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].toLowerCase() === word.toLowerCase()) {
+            count++;
         }
-        
-        nextNodes.sort((a, b) => a.heuristic - b.heuristic);
-        currentNodes = nextNodes.slice(0, beamWidth);
     }
-    
-    return currentNodes;
+
+    return count;
 }
 
-// Example usage
-let startNode = { state: 'A' };
-
-function getSuccessors(node) {
-    // Implement this function to generate successor nodes for a given node
-    return [{ state: 'B' }, { state: 'C' }];
-}
-
-function heuristicFunction(node) {
-    // Implement this function to calculate a heuristic value for a given node
-    return 0;
-}
-
-let result = beamSearch(startNode, 2, 3, getSuccessors, heuristicFunction);
-console.log(result);
+let str = 'This is a test string. Test is a good word to test with.';
+let word = 'test';
+let occurrences = countOccurrences(str, word);
+console.log(`The word "${word}" occurs ${occurrences} times in the string.`);
