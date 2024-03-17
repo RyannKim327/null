514632bc1a4b6bc9ec766d1fd7b189903465a2c1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:44:21 +0800

    3-17-2024 8:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,33 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
 function hasCycle(head) {
-  if (!head || !head.next) {
-    return false;
-  }
-
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-
-    if (slow === fast) {
-      return true;
+    if (!head || !head.next) {
+        return false;
     }
-  }
 
-  return false;
+    let slow = head;
+    let fast = head.next;
+
+    while (fast && fast.next) {
+        if (slow === fast) {
+            return true;
+        }
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return false;
+}
+// Define a simple Node class for the linked list
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-// Example usage
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-
+// Build a linked list with a cycle
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
 node1.next = node2;
 node2.next = node3;
 node3.next = node1; // Create a cycle
