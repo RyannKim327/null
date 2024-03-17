880257fe18f46e190532cdef051ac185c9cbde3d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:48:54 +0800

    3-18-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,27 @@
-function decimalToBinary(decimal) {
-    return decimal.toString(2);
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(42)); // Output: 101010
+function findMiddleElement(head) {
+    let slowPointer = head;
+    let fastPointer = head;
+
+    while (fastPointer !== null && fastPointer.next !== null) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+
+    return slowPointer.data;
+}
+
+// Test the implementation
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+console.log(findMiddleElement(head)); // Output: 3
