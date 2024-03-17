09046b9c79a4a0304780e952759463bef56830b2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:35:06 +0800

    3-18-2024 5:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,110 @@
-npm install node-cron
-const cron = require('node-cron');
-
-// Define a task to be executed
-const task = () => {
-    console.log('Running task at:', new Date());
+class Node {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
+    }
 }
 
-// Schedule the task to run every minute
-cron.schedule('* * * * *', task);
+class SkipList {
+    constructor(maxLevel, probability) {
+        this.maxLevel = maxLevel;
+        this.probability = probability;
+        this.header = new Node(-1, maxLevel);
+        this.level = 0;
+    }
+
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.probability && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (!current || current.value !== value) {
+            let newLevel = this.randomLevel();
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.header;
+                }
+                this.level = newLevel;
+            }
+
+            let newNode = new Node(value, newLevel);
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    search(value) {
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+
+        current = current.forward[0];
+
+        if (current && current.value === value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    delete(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.header;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].forward[i] !== current) {
+                    break;
+                }
+                update[i].forward[i] = current.forward[i];
+            }
+
+            while (this.level > 0 && this.header.forward[this.level] === null) {
+                this.level--;
+            }
+        }
+    }
+}
+let skipList = new SkipList(5, 0.5);
+
+skipList.insert(3);
+skipList.insert(6);
+skipList.insert(2);
+
+console.log(skipList.search(3)); // Output: true
+console.log(skipList.search(5)); // Output: false
+
+skipList.delete(6);
+
+console.log(skipList.search(6)); // Output: false
