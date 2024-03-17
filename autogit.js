d95c1689da5384402355a22d1348bf352792d8ea
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:05:15 +0800

    3-17-2024 9:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,3 @@
-class Node {
-    constructor() {
-        this.outgoingEdges = {};
-    }
-}
-
-class SuffixTree {
-    constructor() {
-        this.root = new Node();
-    }
-
-    addString(str) {
-        // Implement the logic to add a string to the suffix tree
-    }
-
-    searchSubstring(subStr) {
-        // Implement the logic to search for a substring in the suffix tree
-    }
-}
-
-// Usage
-const suffixTree = new SuffixTree();
-suffixTree.addString("banana");
-
+let str = "Hello, world!";
+let newStr = str.replace(/\s/g, "");
+console.log(newStr); // Output: "Hello,world!"
