e2f00f2bf6ee02085d8a55df93fa1564f0d2bb18
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:40:18 +0800

    3-18-2024 5:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,31 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+function interpolationSearch(arr, x) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high && x >= arr[low] && x <= arr[high]) {
+        let position = low + Math.floor(((x - arr[low]) * (high - low)) / (arr[high] - arr[low]));
+
+        if (arr[position] === x) {
+            return position;
+        }
+
+        if (arr[position] < x) {
+            low = position + 1;
+        } else {
+            high = position - 1;
+        }
+    }
+
+    return -1;
 }
 
-function getIntersectionNode(headA, headB) {
-  let set = new Set();
-  let intersectionHead = null;
-  let intersectionTail = null;
+// Testing the interpolation search algorithm
+let arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+let x = 12;
+let index = interpolationSearch(arr, x);
 
-  // Iterate through the first linked list and store each node in a Set
-  let currNode = headA;
-  while (currNode) {
-    set.add(currNode);
-    currNode = currNode.next;
-  }
-
-  // Iterate through the second linked list and check if each node is present in the Set
-  currNode = headB;
-  while (currNode) {
-    if (set.has(currNode)) {
-      if (!intersectionHead) {
-        intersectionHead = new ListNode(currNode.val);
-        intersectionTail = intersectionHead;
-      } else {
-        intersectionTail.next = new ListNode(currNode.val);
-        intersectionTail = intersectionTail.next;
-      }
-    }
-    currNode = currNode.next;
-  }
-
-  return intersectionHead;
+if (index !== -1) {
+    console.log(`${x} found at index ${index}`);
+} else {
+    console.log(`${x} not found in the array`);
 }
