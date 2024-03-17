4051b0823a5e92444124f9d0249419228c7455da
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:12:26 +0800

    3-17-2024 9:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,89 @@
-function mergeSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
+class Node {
+    constructor(order, isLeaf) {
+        this.keys = [];
+        this.children = [];
+        this.isLeaf = isLeaf;
+        this.order = order;
+    }
+}
+class BTree {
+    constructor(order) {
+        this.root = new Node(order, true);
+        this.order = order;
     }
 
-    const merge = (left, right) => {
-        let result = [];
-        let leftIndex = 0;
-        let rightIndex = 0;
-
-        while (leftIndex < left.length && rightIndex < right.length) {
-            if (left[leftIndex] < right[rightIndex]) {
-                result.push(left[leftIndex]);
-                leftIndex++;
-            } else {
-                result.push(right[rightIndex]);
-                rightIndex++;
-            }
+    insert(key) {
+        let root = this.root;
+        if (root.keys.length === 2 * this.order - 1) {
+            let newRoot = new Node(this.order, false);
+            newRoot.children.push(root);
+            this.splitChild(newRoot, 0);
+            this.insertNonFull(newRoot, key);
+            this.root = newRoot;
+        } else {
+            this.insertNonFull(root, key);
         }
+    }
 
-        return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-    };
-
-    const split = (arr) => {
-        const result = arr.map(item => [item]);
-
-        while (result.length > 1) {
-            const temp = [];
-            for (let i = 0; i < result.length; i += 2) {
-                if (result[i + 1]) {
-                    temp.push(merge(result[i], result[i + 1]));
-                } else {
-                    temp.push(result[i]);
+    insertNonFull(node, key) {
+        let i = node.keys.length - 1;
+        if (node.isLeaf) {
+            while (i >= 0 && key < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i];
+                i--;
+            }
+            node.keys[i + 1] = key;
+        } else {
+            while (i >= 0 && key < node.keys[i]) {
+                i--;
+            }
+            i++;
+            if (node.children[i].keys.length === 2 * this.order - 1) {
+                this.splitChild(node, i);
+                if (key > node.keys[i]) {
+                    i++;
                 }
             }
-            result = temp;
+            this.insertNonFull(node.children[i], key);
         }
+    }
 
-        return result[0];
-    };
+    splitChild(node, i) {
+        let order = this.order;
+        let child = node.children[i];
+        let newChild = new Node(order, child.isLeaf);
+        node.children.splice(i + 1, 0, newChild);
+        node.keys.splice(i, 0, child.keys[order - 1]);
+        newChild.keys = child.keys.splice(order, order - 1);
+        if (!child.isLeaf) {
+            newChild.children = child.children.splice(order, order);
+        }
+    }
 
-    return split(arr);
+    search(key) {
+        return this.searchNode(this.root, key);
+    }
+
+    searchNode(node, key) {
+        let i = 0;
+        while (i < node.keys.length && key > node.keys[i]) {
+            i++;
+        }
+        if (i < node.keys.length && key === node.keys[i]) {
+            return node;
+        }
+        if (node.isLeaf) {
+            return null;
+        } else {
+            return this.searchNode(node.children[i], key);
+        }
+    }
 }
+let btree = new BTree(3);
+btree.insert(5);
+btree.insert(10);
+btree.insert(3);
+btree.insert(7);
+btree.insert(12);
 
-// Usage
-const arr = [4, 11, 5, 8, 2, 10, 6, 3, 9, 7, 1];
-const sortedArray = mergeSort(arr);
-console.log(sortedArray);
+console.log(btree.search(7));
