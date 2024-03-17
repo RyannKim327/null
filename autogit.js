33318551a2d179132577c43231c4147c5e955f59
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:09:24 +0800

    3-17-2024 9:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,15 @@
-function fibonacciSearch(arr, x) {
-    let fibMMm2 = 0; // (m-2)'th Fibonacci Number
-    let fibMMm1 = 1; // (m-1)'th Fibonacci Number
-    let fibM = fibMMm2 + fibMMm1; // m'th Fibonacci Number
-
-    let length = arr.length;
-
-    while (fibM < length) {
-        fibMMm2 = fibMMm1;
-        fibMMm1 = fibM;
-        fibM = fibMMm2 + fibMMm1;
-    }
-
-    let offset = -1;
-
-    while (fibM > 1) {
-        let i = Math.min(offset + fibMMm2, length - 1);
-
-        if (arr[i] < x) {
-            fibM = fibMMm1;
-            fibMMm1 = fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-            offset = i;
-        } else if (arr[i] > x) {
-            fibM = fibMMm2;
-            fibMMm1 = fibMMm1 - fibMMm2;
-            fibMMm2 = fibM - fibMMm1;
-        } else {
-            return i;
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
         }
     }
-
-    if (fibMMm1 && arr[offset + 1] === x) {
-        return offset + 1;
-    }
-
-    return -1;
+    return count;
 }
-let arr = [1, 3, 5, 7, 9, 11, 13, 15];
-let x = 9;
-let result = fibonacciSearch(arr, x);
 
-if (result !== -1) {
-    console.log(`Element found at index ${result}`);
-} else {
-    console.log('Element not found in the array');
-}
+const str = "hello world";
+const charToCount = "o";
+const occurrences = countOccurrences(str, charToCount);
+
+console.log(`The character "${charToCount}" occurs ${occurrences} times in the string "${str}".`);
