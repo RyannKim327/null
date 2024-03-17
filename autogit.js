baae061ccb3a848d1b840b3093cbce0e9c57d37d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:34:49 +0800

    3-18-2024 5:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,9 @@
-class Graph {
-    constructor() {
-        this.adjacencyList = {};
-    }
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
 
-    addVertex(vertex) {
-        if (!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex] = [];
-        }
-    }
-
-    addEdge(v1, v2) {
-        if (!this.adjacencyList[v1] || !this.adjacencyList[v2]) {
-            throw new Error("Vertex not found in the graph");
-        }
-
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
-
-    depthFirstSearch(start) {
-        const visited = {};
-        const result = [];
-
-        const dfs = (vertex) => {
-            if (!vertex) return null;
-            visited[vertex] = true;
-            result.push(vertex);
-
-            this.adjacencyList[vertex].forEach((neighbor) => {
-                if (!visited[neighbor]) {
-                    dfs(neighbor);
-                }
-            });
-        };
-
-        dfs(start);
-
-        return result;
-    }
+let index = array.indexOf(elementToRemove); // Get the index of the element to be removed
+if (index > -1) {
+    array.splice(index, 1); // Remove 1 element starting from the index
 }
 
-// Usage
-const graph = new Graph();
-graph.addVertex("A");
-graph.addVertex("B");
-graph.addVertex("C");
-graph.addVertex("D");
-graph.addEdge("A", "B");
-graph.addEdge("A", "C");
-graph.addEdge("B", "D");
-console.log(graph.depthFirstSearch("A"));
+console.log(array); // Output: [1, 2, 4, 5]
