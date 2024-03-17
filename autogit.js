708cd7e1112c7723017d829419d9253b1c2fc3d2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:33:59 +0800

    3-18-2024 5:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,29 @@
-var xmlhttp = new XMLHttpRequest();
+function longestCommonSubstring(str1, str2) {
+    const matrix = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
+    let longest = 0;
+    let longestIndex = 0;
 
-xmlhttp.onreadystatechange = function() {
-  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    console.log(xmlhttp.responseText);
-  }
-};
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                if (matrix[i][j] > longest) {
+                    longest = matrix[i][j];
+                    longestIndex = i;
+                }
+            } else {
+                matrix[i][j] = 0;
+            }
+        }
+    }
 
-xmlhttp.open("GET", "https://api.example.com/data", true);
-xmlhttp.send();
+    if (longest === 0) {
+        return '';
+    }
+
+    return str1.slice(longestIndex - longest, longestIndex);
+}
+
+const str1 = 'hello';
+const str2 = 'world';
+console.log(longestCommonSubstring(str1, str2)); // Output: 'l'
