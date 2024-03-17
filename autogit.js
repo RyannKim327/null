3d9895fd7d28b290a7c85de4b7817cf00246dc90
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:45:04 +0800

    3-18-2024 5:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,4 @@
-function mergeSortIterative(arr) {
-    if (arr.length < 2) {
-        return arr;
-    }
+let str = "Hello World!";
+let lowercaseStr = str.toLowerCase();
 
-    const n = arr.length;
-    for (let size = 1; size < n; size *= 2) {
-        for (let left = 0; left < n - 1; left += 2 * size) {
-            const mid = Math.min(left + size - 1, n - 1);
-            const right = Math.min(left + 2 * size - 1, n - 1);
-            merge(arr, left, mid, right);
-        }
-    }
-    return arr;
-}
-
-function merge(arr, left, mid, right) {
-    const leftArr = arr.slice(left, mid + 1);
-    const rightArr = arr.slice(mid + 1, right + 1);
-
-    let i = 0,
-        j = 0,
-        k = left;
-
-    while (i < leftArr.length && j < rightArr.length) {
-        if (leftArr[i] <= rightArr[j]) {
-            arr[k++] = leftArr[i++];
-        } else {
-            arr[k++] = rightArr[j++];
-        }
-    }
-
-    while (i < leftArr.length) {
-        arr[k++] = leftArr[i++];
-    }
-
-    while (j < rightArr.length) {
-        arr[k++] = rightArr[j++];
-    }
-}
-
-// Example usage
-const arr = [38, 27, 43, 3, 9, 82, 10];
-console.log(mergeSortIterative(arr)); // Output: [3, 9, 10, 27, 38, 43, 82]
+console.log(lowercaseStr); // Output: hello world!
