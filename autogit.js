bc32e483a3897d4ba330fd64d65077daa3df3ab3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:58:55 +0800

    3-17-2024 8:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,95 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
+class Node {
+    constructor(order, isLeaf) {
+        this.keys = [];
+        this.children = [];
+        this.isLeaf = isLeaf || false;
+        this.order = order;
+    }
+}
 
-    while (left <= right) {
-        let mid = left + Math.floor((right - left) / 2);
+class BTree {
+    constructor(order) {
+        this.root = new Node(order, true);
+        this.order = order;
+    }
 
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
+    insert(key) {
+        let rootNode = this.root;
+
+        if (rootNode.keys.length === 2 * this.order - 1) {
+            let newRoot = new Node(this.order);
+            newRoot.children.push(rootNode);
+            this.splitChild(newRoot, 0);
+            this.root = newRoot;
+            this.insertNonFull(newRoot, key);
         } else {
-            right = mid - 1;
+            this.insertNonFull(rootNode, key);
         }
     }
 
-    return -1; // Element not found
+    insertNonFull(node, key) {
+        let i = node.keys.length - 1;
+
+        if (node.isLeaf) {
+            while (i >= 0 && key < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i];
+                i--;
+            }
+            node.keys[i + 1] = key;
+        } else {
+            while (i >= 0 && key < node.keys[i]) {
+                i--;
+            }
+            i++;
+            if (node.children[i].keys.length === 2 * this.order - 1) {
+                this.splitChild(node, i);
+                if (key > node.keys[i]) {
+                    i++;
+                }
+            }
+            this.insertNonFull(node.children[i], key);
+        }
+    }
+
+    splitChild(parent, index) {
+        let order = this.order;
+        let child = parent.children[index];
+        let newChild = new Node(order, child.isLeaf);
+        parent.keys.splice(index, 0, child.keys[order - 1]);
+        parent.children.splice(index + 1, 0, newChild);
+
+        newChild.keys = child.keys.splice(order);
+        if (!child.isLeaf) {
+            newChild.children = child.children.splice(order);
+        }
+    }
+
+    search(key) {
+        return this.searchInNode(this.root, key);
+    }
+
+    searchInNode(node, key) {
+        let i = 0;
+        while (i < node.keys.length && key > node.keys[i]) {
+            i++;
+        }
+        if (node.keys[i] === key) {
+            return true;
+        }
+        if (node.isLeaf) {
+            return false;
+        } else {
+            return this.searchInNode(node.children[i], key);
+        }
+    }
 }
 
-// Example usage
-const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15];
-const targetElement = 7;
-const resultIndex = binarySearch(sortedArray, targetElement);
+// Example usage:
+let bTree = new BTree(3);
+bTree.insert(10);
+bTree.insert(20);
+bTree.insert(5);
+bTree.insert(6);
 
-if (resultIndex !== -1) {
-    console.log(`Element found at index ${resultIndex}`);
-} else {
-    console.log(`Element not found in the array`);
-}
+console.log(bTree.search(5)); // Output: true
+console.log(bTree.search(15)); // Output: false
