19ffaa94376839e7fe418692f1cdb9c09e32775a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:35 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,27 @@
-function findStringLength(str) {
-    let count = 0;
-    while (str[count] !== undefined) {
-        count++;
+function shellSort(arr) {
+    let increment = Math.floor(arr.length / 2);
+
+    while (increment > 0) {
+        for (let i = increment; i < arr.length; i++) {
+            let j = i;
+            let temp = arr[i];
+
+            while (j >= increment && arr[j - increment] > temp) {
+                arr[j] = arr[j - increment];
+                j = j - increment;
+            }
+
+            arr[j] = temp;
+        }
+
+        increment = Math.floor(increment / 2);
     }
-    return count;
+
+    return arr;
 }
 
-const myString = "Hello, World!";
-const length = findStringLength(myString);
-console.log("Length of the string: " + length); // Output: Length of the string: 13
+// Test the shellSort function
+let arr = [12, 34, 54, 2, 3];
+console.log("Original Array: ", arr);
+arr = shellSort(arr);
+console.log("Sorted Array: ", arr);
