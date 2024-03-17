bced961d310b4d20298c00a84a20f541a4ed9e5c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:03:27 +0800

    3-17-2024 9:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,37 @@
-let str = "Hello, World!";
-let subStr = "Hello";
-
-if (str.includes(subStr)) {
-    console.log("The string contains the substring.");
-} else {
-    console.log("The string does not contain the substring.");
+function longestCommonSubsequence(str1, str2) {
+    let m = str1.length;
+    let n = str2.length;
+    
+    let dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    
+    let lcs = "";
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    
+    return lcs;
 }
+
+let str1 = "ABCDGH";
+let str2 = "AEDFHR";
+
+console.log(longestCommonSubsequence(str1, str2)); // Output: "ADH"
