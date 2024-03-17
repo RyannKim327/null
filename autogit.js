cd29f1ceff329fed8a80040669c5487a5f3b2ea9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:54:04 +0800

    3-17-2024 8:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,15 @@
-// Node class to represent nodes in a linked list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function maxSubarraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+    
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
     }
+    
+    return maxSoFar;
 }
 
-// LinkedList class
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    // Method to add a new node to the end of the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-
-            while (current.next) {
-                current = current.next;
-            }
-
-            current.next = newNode;
-        }
-    }
-
-    // Method to find and return the length of the linked list
-    getLength() {
-        let current = this.head;
-        let count = 0;
-
-        while (current) {
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
-}
-
-// Creating a sample linked list and adding nodes
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-
-// Getting the length of the linked list
-const length = linkedList.getLength();
-console.log("Length of the linked list: ", length);
+// Test the function
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubarraySum(arr)); // Output: 6
