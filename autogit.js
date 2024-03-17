7e9f15b73d16bc21e538a7795567a9e2b6cdeb88
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:59:04 +0800

    3-17-2024 8:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,36 @@
-class Node {
-    constructor(order, isLeaf) {
+class BTreeNode {
+    constructor(order, leaf) {
+        this.order = order;
+        this.leaf = leaf;
         this.keys = [];
         this.children = [];
-        this.isLeaf = isLeaf || false;
-        this.order = order;
     }
 }
 
 class BTree {
     constructor(order) {
-        this.root = new Node(order, true);
         this.order = order;
+        this.root = new BTreeNode(order, true);
     }
 
     insert(key) {
-        let rootNode = this.root;
+        let root = this.root;
 
-        if (rootNode.keys.length === 2 * this.order - 1) {
-            let newRoot = new Node(this.order);
-            newRoot.children.push(rootNode);
+        if (root.keys.length === (2 * this.order) - 1) {
+            let newRoot = new BTreeNode(this.order, false);
+            newRoot.children.push(root);
             this.splitChild(newRoot, 0);
             this.root = newRoot;
             this.insertNonFull(newRoot, key);
         } else {
-            this.insertNonFull(rootNode, key);
+            this.insertNonFull(root, key);
         }
     }
 
     insertNonFull(node, key) {
         let i = node.keys.length - 1;
 
-        if (node.isLeaf) {
+        if (node.leaf) {
             while (i >= 0 && key < node.keys[i]) {
                 node.keys[i + 1] = node.keys[i];
                 i--;
@@ -40,13 +40,15 @@ class BTree {
             while (i >= 0 && key < node.keys[i]) {
                 i--;
             }
+
             i++;
-            if (node.children[i].keys.length === 2 * this.order - 1) {
+            if (node.children[i].keys.length === (2 * this.order) - 1) {
                 this.splitChild(node, i);
                 if (key > node.keys[i]) {
                     i++;
                 }
             }
+
             this.insertNonFull(node.children[i], key);
         }
     }
@@ -54,42 +56,13 @@ class BTree {
     splitChild(parent, index) {
         let order = this.order;
         let child = parent.children[index];
-        let newChild = new Node(order, child.isLeaf);
+        let newChild = new BTreeNode(order, child.leaf);
         parent.keys.splice(index, 0, child.keys[order - 1]);
         parent.children.splice(index + 1, 0, newChild);
+        newChild.keys = child.keys.splice(order, order - 1);
 
-        newChild.keys = child.keys.splice(order);
-        if (!child.isLeaf) {
-            newChild.children = child.children.splice(order);
-        }
-    }
-
-    search(key) {
-        return this.searchInNode(this.root, key);
-    }
-
-    searchInNode(node, key) {
-        let i = 0;
-        while (i < node.keys.length && key > node.keys[i]) {
-            i++;
-        }
-        if (node.keys[i] === key) {
-            return true;
-        }
-        if (node.isLeaf) {
-            return false;
-        } else {
-            return this.searchInNode(node.children[i], key);
+        if (!child.leaf) {
+            newChild.children = child.children.splice(order, order);
         }
     }
 }
-
-// Example usage:
-let bTree = new BTree(3);
-bTree.insert(10);
-bTree.insert(20);
-bTree.insert(5);
-bTree.insert(6);
-
-console.log(bTree.search(5)); // Output: true
-console.log(bTree.search(15)); // Output: false
