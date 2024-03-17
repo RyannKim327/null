e111b60019231fb66ab7624cca13bb25d9a56f14
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:06:35 +0800

    3-17-2024 9:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,17 @@
-function removeVowels(str) {
-   return str.replace(/[aeiouAEIOU]/g, '');
+function reverseWords(str) {
+    // Split the string into an array of words
+    let words = str.split(' ');
+
+    // Reverse the array of words
+    words.reverse();
+
+    // Join the array back into a string
+    let reversedStr = words.join(' ');
+
+    return reversedStr;
 }
 
-// Example
-console.log(removeVowels("Hello World")); // Output: "Hll Wrld"
+// Example usage
+let originalStr = "Hello World!";
+let reversedStr = reverseWords(originalStr);
+console.log(reversedStr);  // Output: "World! Hello"
