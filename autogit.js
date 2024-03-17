02f4f03e983da37f9934ec0b4b0a9368742fcb2b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:11:27 +0800

    3-17-2024 9:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,86 @@
-function burrowsWheelerTransform(inputString) {
-    let matrix = [];
-    
-    // Generate all rotations of the input string
-    for (let i = 0; i < inputString.length; i++) {
-        let rotation = inputString.slice(i) + inputString.slice(0, i);
-        matrix.push(rotation);
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
     }
-    
-    // Sort the rotations lexicographically
-    matrix.sort();
-    
-    // Extract the last characters of each rotation
-    let transformedString = matrix.map(rotation => rotation.slice(-1)).join('');
-    
-    return transformedString;
 }
 
-// Example usage
-let inputString = "banana";
-let bwtResult = burrowsWheelerTransform(inputString);
-console.log("Burrows-Wheeler transformed string:", bwtResult);
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    rotateRight(node) {
+        const newRoot = node.left;
+        node.left = newRoot.right;
+        newRoot.right = node;
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+        newRoot.height = Math.max(this.getHeight(newRoot.left), this.getHeight(newRoot.right)) + 1;
+        return newRoot;
+    }
+
+    rotateLeft(node) {
+        const newRoot = node.right;
+        node.right = newRoot.left;
+        newRoot.left = node;
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+        newRoot.height = Math.max(this.getHeight(newRoot.left), this.getHeight(newRoot.right)) + 1;
+        return newRoot;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
+        } else {
+            node.right = this._insert(node.right, value);
+        }
+
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+
+        const balance = this.getBalanceFactor(node);
+
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+}
