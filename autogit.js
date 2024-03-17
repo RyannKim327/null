22b5761c67827bf005ba4072bc8852412e1f3fe6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:41:58 +0800

    3-18-2024 5:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,14 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function calculateMean(numbers) {
+    const sum = numbers.reduce((acc, val) => acc + val, 0);
+    const mean = sum / numbers.length;
+  
+    return mean;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+// Example list of numbers
+const numbers = [1, 2, 3, 4, 5];
 
-    append(data) {
-        const newNode = new Node(data);
+// Calculate the mean
+const mean = calculateMean(numbers);
 
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-
-        current.next = newNode;
-    }
-
-    getLength() {
-        let length = 0;
-        let current = this.head;
-
-        while (current) {
-            length++;
-            current = current.next;
-        }
-
-        return length;
-    }
-}
-
-// Example usage
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-
-console.log(list.getLength()); // Output: 3
+console.log("Mean:", mean);
