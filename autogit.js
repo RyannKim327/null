07adbc037a92af91d93aa3ac330fef937fc93108
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:04:31 +0800

    3-17-2024 9:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,8 @@
-function computeLPSArray(pattern) {
-    let lps = [0];
-    let len = 0;
-    let i = 1;
-
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len !== 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
-
-    return lps;
+function decimalToBinary(decimal) {
+    return (decimal >>> 0).toString(2);
 }
 
-function KMPSearch(text, pattern) {
-    const n = text.length;
-    const m = pattern.length;
-    const lps = computeLPSArray(pattern);
-    let i = 0;
-    let j = 0;
-
-    while (i < n) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-
-        if (j === m) {
-            console.log("Pattern found at index " + (i - j));
-            j = lps[j - 1];
-        } else if (i < n && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-}
-
-// Example usage
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-
-KMPSearch(text, pattern);
+// Example
+const decimalNumber = 10;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber); // Output: 1010
