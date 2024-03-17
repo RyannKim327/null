3f9b31ee8dc8fb0bd31a762d76d6ad3e41072bb0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:45:50 +0800

    3-17-2024 8:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,5 @@
-function bfs(graph, startNode, endNode) {
-    // Create a queue for BFS
-    let queue = [];
-    queue.push([startNode]);
+const cron = require('node-cron');
 
-    // Keep track of visited nodes
-    let visited = new Set();
-
-    // Iterate through the queue
-    while (queue.length > 0) {
-        let path = queue.shift();
-        let node = path[path.length - 1];
-
-        if (node === endNode) {
-            return path;
-        }
-
-        if (!visited.has(node)) {
-            visited.add(node);
-
-            let neighbors = graph[node];
-            for (let neighbor of neighbors) {
-                let newPath = [...path, neighbor];
-                queue.push(newPath);
-            }
-        }
-    }
-
-    // If no path is found
-    return null;
-}
-
-// Example graph representation using an adjacency list
-const graph = {
-    A: ['B', 'C'],
-    B: ['A', 'D', 'E'],
-    C: ['A', 'F'],
-    D: ['B'],
-    E: ['B', 'F'],
-    F: ['C', 'E']
-};
-
-const startNode = 'A';
-const endNode = 'F';
-const shortestPath = bfs(graph, startNode, endNode);
-
-if (shortestPath) {
-    console.log(`Shortest path from ${startNode} to ${endNode}: ${shortestPath.join(' -> ')}`);
-} else {
-    console.log(`No path found from ${startNode} to ${endNode}`);
-}
+cron.schedule('*/5 * * * *', () => {
+  console.log('This function runs every 5 minutes');
+});
