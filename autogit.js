b5c23dd0a71d9e348c71119e163529d017e8745f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:43:09 +0800

    3-18-2024 5:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,27 @@
-function insertionSort(arr) {
-    for (var i = 1; i < arr.length; i++) {
-        var key = arr[i];
-        var j = i - 1;
-        
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+function longestCommonSubstring(str1, str2) {
+    let dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
+  
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
         }
-        
-        arr[j + 1] = key;
     }
-    return arr;
+
+    return str1.substring(endIndex - maxLength, endIndex);
 }
 
-// Example usage
-var arr = [12, 11, 13, 5, 6];
-console.log("Array before sorting: " + arr);
-console.log("Array after sorting: " + insertionSort(arr));
+// Test the function
+let str1 = "ABABC";
+let str2 = "BABC";
+console.log(longestCommonSubstring(str1, str2)); // Output: "BABC"
