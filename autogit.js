c5a19f80daaf185f32da72efb038057a8ae55676
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:53:21 +0800

    3-17-2024 8:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,8 @@
-function bfs(graph, startNode) {
-    let visited = {};
-    let queue = [startNode];
-    let result = [];
-
-    visited[startNode] = true;
-
-    while (queue.length > 0) {
-        let currentNode = queue.shift();
-        result.push(currentNode);
-
-        for (let neighbor of graph[currentNode]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                queue.push(neighbor);
-            }
-        }
-    }
-
-    return result;
-}
-
-// Example graph
-const graph = {
-    A: ['B', 'C'],
-    B: ['A', 'D', 'E'],
-    C: ['A', 'F'],
-    D: ['B'],
-    E: ['B', 'F'],
-    F: ['C', 'E']
-};
-
-// Starting node
-const startNode = 'A';
-
-// Perform BFS
-const bfsResult = bfs(graph, startNode);
-console.log(bfsResult);
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
