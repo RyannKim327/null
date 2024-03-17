3a6818ed84d3539b440d245e53097d227b51fbba
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:43:51 +0800

    3-17-2024 8:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,8 @@
-class ListNode {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+let numbers = [10, 5, 20, 15, 30];
+let max = Math.max.apply(null, numbers);
 
-const getIntersectionNode = (headA, headB) => {
-  if (!headA || !headB) {
-    return null;
-  }
+console.log(max); // Output: 30
+let numbers = [10, 5, 20, 15, 30];
+let max = Math.max(...numbers);
 
-  let pA = headA;
-  let pB = headB;
-
-  while (pA !== pB) {
-    pA = pA ? pA.next : headB;
-    pB = pB ? pB.next : headA;
-  }
-
-  return pA;
-};
-// Create linked list 1: 1 -> 3 -> 5 -> 7 -> 9
-let headA = new ListNode(1);
-headA.next = new ListNode(3);
-headA.next.next = new ListNode(5);
-headA.next.next.next = new ListNode(7);
-headA.next.next.next.next = new ListNode(9);
-
-// Create linked list 2: 2 -> 4 -> 7 -> 9
-let headB = new ListNode(2);
-headB.next = new ListNode(4);
-headB.next.next = headA.next.next.next; // connecting to the intersecting node 7
-
-let intersectionNode = getIntersectionNode(headA, headB);
-console.log(intersectionNode ? intersectionNode.val : null);
+console.log(max); // Output: 30
