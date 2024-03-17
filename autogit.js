c832971a5052164d965ed595849061e52455457e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:07:01 +0800

    3-17-2024 9:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,16 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function maxSubarraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
     }
+
+    return maxSoFar;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    // Insert a new node at the beginning of the linked list
-    insert(data) {
-        const newNode = new Node(data);
-        newNode.next = this.head;
-        this.head = newNode;
-    }
-
-    // Reverse the linked list
-    reverse() {
-        let prev = null;
-        let current = this.head;
-        let next = null;
-
-        while (current !== null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        this.head = prev;
-    }
-
-    // Utility method to print the linked list
-    printList() {
-        let current = this.head;
-        while (current !== null) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.insert(1);
-linkedList.insert(2);
-linkedList.insert(3);
-
-console.log('Original linked list:');
-linkedList.printList();
-
-linkedList.reverse();
-
-console.log('Reversed linked list:');
-linkedList.printList();
+// Example usage:
+const array = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const maxSum = maxSubarraySum(array);
+console.log(maxSum); // Output: 6
