af8c3d47914621afe455c9117f1a6fc732f7dac7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:49 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,38 @@
-// Node class to represent each node in the linked list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
 
-// LinkedList class to manage the nodes and operations on the list
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+    const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
 
-    // Method to insert a new node at the end of the list
-    insert(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
-            current.next = newNode;
         }
     }
 
-    // Method to display the elements in the list
-    display() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
+    let i = m, j = n;
+    let lcs = '';
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
         }
     }
+
+    return lcs;
 }
 
-// Example of using the LinkedList class
-const linkedList = new LinkedList();
-linkedList.insert(1);
-linkedList.insert(2);
-linkedList.insert(3);
-linkedList.display();
+const str1 = "AGGTAB";
+const str2 = "GXTXAYB";
+
+console.log(longestCommonSubsequence(str1, str2)); // Output: GTAB
