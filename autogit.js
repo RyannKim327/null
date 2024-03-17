47f8248f789bca3ad6e0855315b1648aea0f1e6a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:52:57 +0800

    3-17-2024 8:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,42 @@
-function getIntersectionNode(headA, headB) {
-    let set = new Set();
-    
-    let currA = headA;
-    while (currA) {
-        set.add(currA);
-        currA = currA.next;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-    
-    let currB = headB;
-    while (currB) {
-        if (set.has(currB)) {
-            return currB;
+}
+
+function findNthFromEnd(head, n) {
+    let fast = head;
+    let slow = head;
+
+    // Move fast pointer n nodes ahead
+    for (let i = 0; i < n; i++) {
+        if (fast === null) {
+            return null; // List length is less than n
         }
-        currB = currB.next;
+        fast = fast.next;
     }
-    
-    return null; // If no intersection is found
+
+    // Move both pointers until fast reaches the end
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    return slow;
+}
+
+// Example usage
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+let n = 2;
+let nthNodeFromEnd = findNthFromEnd(head, n);
+if (nthNodeFromEnd !== null) {
+    console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.data}`);
+} else {
+    console.log(`List length is less than ${n}`);
 }
