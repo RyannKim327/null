24a23e1d6d23807a336e9f506617cf515e15196a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:30:23 +0800

    3-18-2024 5:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,3 @@
-function isArraySortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Test the function
-const sortedArray = [1, 2, 3, 4, 5];
-const unsortedArray = [1, 3, 2, 4, 5];
-
-console.log(isArraySortedAscending(sortedArray)); // Output: true
-console.log(isArraySortedAscending(unsortedArray)); // Output: false
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // Output: [5, 4, 3, 2, 1]
