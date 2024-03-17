5e6bc5a1788b43ee3ecd4db9878f93ffdfde1188
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:39:47 +0800

    3-18-2024 5:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,98 @@
-function biDirectionalSearch(startNode, targetNode) {
-    let queueFromStart = [startNode];
-    let queueFromTarget = [targetNode];
-    let visitedFromStart = new Set();
-    let visitedFromTarget = new Set();
-    
-    visitedFromStart.add(startNode);
-    visitedFromTarget.add(targetNode);
-    
-    while (queueFromStart.length && queueFromTarget.length) {
-        let nodeFromStart = queueFromStart.shift();
-        let nodeFromTarget = queueFromTarget.shift();
-        
-        if (visitedFromStart.has(nodeFromTarget) || visitedFromTarget.has(nodeFromStart)) {
-            return true; // Found a common node, path exists
-        }
-        
-        for (let neighbor of nodeFromStart.neighbors) {
-            if (!visitedFromStart.has(neighbor)) {
-                visitedFromStart.add(neighbor);
-                queueFromStart.push(neighbor);
-            }
-        }
-        
-        for (let neighbor of nodeFromTarget.neighbors) {
-            if (!visitedFromTarget.has(neighbor)) {
-                visitedFromTarget.add(neighbor);
-                queueFromTarget.push(neighbor);
-            }
-        }
+class AVLNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
     }
-    
-    return false; // Path does not exist
+}
+
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+    }
+
+    rotateRight(y) {
+        const x = y.left;
+        const T = x.right;
+
+        x.right = y;
+        y.left = T;
+
+        this.updateHeight(y);
+        this.updateHeight(x);
+
+        return x;
+    }
+
+    rotateLeft(x) {
+        const y = x.right;
+        const T = y.left;
+
+        y.left = x;
+        x.right = T;
+
+        this.updateHeight(x);
+        this.updateHeight(y);
+
+        return y;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (node === null) {
+            return new AVLNode(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
+        } else {
+            node.right = this._insert(node.right, value);
+        }
+
+        this.updateHeight(node);
+
+        const balance = this.getBalanceFactor(node);
+
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
 }
