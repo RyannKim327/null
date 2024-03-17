950d884e21c6f3d6923554952fdced4847d73dba
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:45:33 +0800

    3-17-2024 8:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,9 @@
-class Graph {
-  constructor() {
-    this.adjList = new Map();
-  }
-
-  addVertex(v) {
-    this.adjList.set(v, []);
-  }
-
-  addEdge(from, to) {
-    this.adjList.get(from).push(to);
-    this.adjList.get(to).push(from); // Undirected graph
-  }
-
-  depthFirstSearch(start) {
-    const visited = new Set();
-    this._dfs(start, visited);
-  }
-
-  _dfs(vertex, visited) {
-    visited.add(vertex);
-    console.log(vertex);
-
-    const neighbors = this.adjList.get(vertex);
-    for (const neighbor of neighbors) {
-      if (!visited.has(neighbor)) {
-        this._dfs(neighbor, visited);
-      }
-    }
-  }
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-// Example usage
-const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
+// Test the function
+const originalString = 'Hello, World!';
+const reversedString = reverseString(originalString);
 
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-
-graph.depthFirstSearch('A');
+console.log(reversedString); // Outputs: "!dlroW ,olleH"
