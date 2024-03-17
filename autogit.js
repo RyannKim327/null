291af158c0b0824a3ee29b62323252a5d9cb5d3e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:14 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,14 @@
-function isAnagram(str1, str2) {
-    // Remove non-alphanumeric characters and convert to lowercase
-    const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-    // Sort the strings alphabetically
-    const sortedStr1 = cleanStr1.split('').sort().join('');
-    const sortedStr2 = cleanStr2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
+async function connectToDevice() {
+  try {
+    const response = await fetch('http://yourandroiddeviceip:port/connect');
+    const data = await response.json();
+    
+    console.log('Connected to Android device:', data);
+    
+    // Continue with more operations here...
+  } catch (error) {
+    console.error('Error connecting to Android device:', error);
+  }
 }
 
-// Test the function
-const string1 = 'listen';
-const string2 = 'silent';
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+connectToDevice();
