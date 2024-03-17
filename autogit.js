c30f3469f66376617bec10045fb7b6bcb4451448
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 09:12:59 +0800

    3-17-2024 9:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,22 @@
-let originalString = "Hello, World!";
-let lowercaseString = originalString.toLowerCase();
+function isAnagram(str1, str2) {
+    // Clean and lowercase both strings
+    str1 = str1.toLowerCase().replace(/[^a-z]+/g, '');
+    str2 = str2.toLowerCase().replace(/[^a-z]+/g, '');
 
-console.log(lowercaseString); // Output: hello, world!
+    // Sort the characters of both strings
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
+
+    // Compare the sorted strings
+    return str1 === str2;
+}
+
+// Test the function
+const string1 = "Listen";
+const string2 = "Silent";
+
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
