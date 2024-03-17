821f0b7c855efb7b0fefe7686d2b438ebb08be3b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:48:17 +0800

    3-18-2024 5:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,12 @@
-function tarjanSCC(graph) {
-  let index = 0;
-  let stack = [];
-  let onStack = new Set();
-  let indexes = {};
-  let lowlink = {};
-  let sccs = [];
+let numbers = [4, 2, 10, 8, 6];
 
-  function strongConnect(node) {
-    indexes[node] = index;
-    lowlink[node] = index;
-    index++;
-    stack.push(node);
-    onStack.add(node);
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-    graph[node].forEach(neighbor => {
-      if (indexes[neighbor] === undefined) {
-        strongConnect(neighbor);
-        lowlink[node] = Math.min(lowlink[node], lowlink[neighbor]);
-      } else if (onStack.has(neighbor)) {
-        lowlink[node] = Math.min(lowlink[node], indexes[neighbor]);
-      }
-    });
+console.log(numbers); // [2, 4, 6, 8, 10]
+numbers.sort(function(a, b) {
+    return b - a;
+});
 
-    if (indexes[node] === lowlink[node]) {
-      let scc = [];
-      let poppedNode = null;
-      do {
-        poppedNode = stack.pop();
-        onStack.delete(poppedNode);
-        scc.push(poppedNode);
-      } while (poppedNode !== node);
-      sccs.push(scc);
-    }
-  }
-
-  Object.keys(graph).forEach(node => {
-    if (indexes[node] === undefined) {
-      strongConnect(node);
-    }
-  });
-
-  return sccs;
-}
-
-// Example usage
-const graph = {
-  0: [1],
-  1: [2, 4],
-  2: [0, 3],
-  3: [2],
-  4: [5],
-  5: [0, 6],
-  6: [4, 7],
-  7: [6]
-};
-
-const sccs = tarjanSCC(graph);
-console.log(sccs);
+console.log(numbers); // [10, 8, 6, 4, 2]
