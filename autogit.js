5809bae526c31464baac7a40b926f78dc77e3bbc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:48:30 +0800

    3-17-2024 8:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,7 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = current;
-    }
-    return arr;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example usage:
-let arr = [5, 2, 4, 6, 1, 3];
-console.log(insertionSort(arr)); // Output: [1, 2, 3, 4, 5, 6]
+// Generate a random number between 1 and 10
+const randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber);
