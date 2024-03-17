2808a246030b80c0b159edb0bcb8e27aab098b7e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-18 05:43:03 +0800

    3-18-2024 5:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,19 @@
-let arr = [1, 2, 3, 4, 2, 3, 5];
+function insertionSort(arr) {
+    for (var i = 1; i < arr.length; i++) {
+        var key = arr[i];
+        var j = i - 1;
+        
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        
+        arr[j + 1] = key;
+    }
+    return arr;
+}
 
-let uniqueArray = arr.filter((item, index) => arr.indexOf(item) === index);
-
-console.log(uniqueArray);
+// Example usage
+var arr = [12, 11, 13, 5, 6];
+console.log("Array before sorting: " + arr);
+console.log("Array after sorting: " + insertionSort(arr));
