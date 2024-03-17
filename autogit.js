ac10c8f1ca3ed2a6a19c73341caebd7da198644f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:56:25 +0800

    3-17-2024 8:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,51 @@
-let str = "Hello, World!";
-let stringWithoutSpaces = str.replace(/\s/g, '');
-console.log(stringWithoutSpaces);
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    // Push element onto the stack
+    push(element) {
+        this.items.push(element);
+    }
+
+    // Pop element from the stack
+    pop() {
+        if (this.items.length === 0) {
+            return "Underflow";
+        }
+        return this.items.pop();
+    }
+
+    // Peek at the top element of the stack
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    // Check if the stack is empty
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    // Print the stack
+    printStack() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++) {
+            str += this.items[i] + " ";
+        }
+        return str;
+    }
+}
+
+// Usage
+const stack = new Stack();
+console.log(stack.isEmpty()); // true
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack.printStack()); // 10 20 30
+console.log(stack.peek()); // 30
+
+stack.pop();
+console.log(stack.printStack()); // 10 20
