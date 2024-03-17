d400149835fd813fa53ca064336d2043a6805f10
RyannKim327 <rksesgundo123@gmail.com>
2024-03-17 08:51:33 +0800

    3-17-2024 8:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,26 @@
-function isPrime(number) {
-    if (number <= 1) {
-        return false;
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-
-    if (number <= 3) {
-        return true;
-    }
-
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-
-    return true;
 }
 
-// Test the function
-console.log(isPrime(7)); // Output: true
-console.log(isPrime(12)); // Output: false
+function sumBinaryTree(root) {
+    if (root === null) {
+        return 0;
+    }
+    
+    return root.value + sumBinaryTree(root.left) + sumBinaryTree(root.right);
+}
+
+// Example binary tree
+let root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+// Calculate sum of all nodes in the binary tree
+let sum = sumBinaryTree(root);
+console.log("Sum of all nodes in the binary tree:", sum);
