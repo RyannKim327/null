8cfa7b60139593c15cfec8935827d9ade90fc7e1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-19 20:00:39 +0800

    3-19-2024 20:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,7 @@
-function isPrime(number) {
-    if (number <= 1) {
-        return false;
-    }
-    if (number <= 3) {
-        return true;
-    }
-    if (number % 2 === 0 || number % 3 === 0) {
-        return false;
-    }
+const cron = require('node-cron');
 
-    let i = 5;
-    while (i * i <= number) {
-        if (number % i === 0 || number % (i + 2) === 0) {
-            return false;
-        }
-        i += 6;
-    }
-
-    return true;
-}
-
-// Test the function
-const number = 17;
-if (isPrime(number)) {
-    console.log(`${number} is a prime number`);
-} else {
-    console.log(`${number} is not a prime number`);
-}
+cron.schedule('*/5 * * * *', () => {
+  console.log('Running a task every 5 minutes');
+});
+npm install node-cron
+node cron-example.js
