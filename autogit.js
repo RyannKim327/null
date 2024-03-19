98d8880044d5fac01ac3e43ec6bece2577591e86
RyannKim327 <rksesgundo123@gmail.com>
2024-03-20 06:10:14 +0800

    3-20-2024 6:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,12 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
+function factorial(num) {
+    if (num === 0) {
+        return 1;
+    } else {
+        return num * factorial(num - 1);
+    }
+}
 
-const commonElements = array1.filter(element => array2.includes(element));
-
-console.log(commonElements); // Output: [3, 4, 5]
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-const set1 = new Set(array1);
-const commonElements = array2.filter(element => set1.has(element));
-
-console.log(commonElements); // Output: [3, 4, 5]
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-const commonElements = array1.filter(element => array2.indexOf(element) !== -1);
-
-console.log(commonElements); // Output: [3, 4, 5]
+// Test the factorial function
+const number = 5;
+const result = factorial(number);
+console.log(`The factorial of ${number} is ${result}`);
