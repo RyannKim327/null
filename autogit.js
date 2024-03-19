0421971e8e5f098195ceae845c8d56ef97c139db
RyannKim327 <rksesgundo123@gmail.com>
2024-03-20 06:09:37 +0800

    3-20-2024 6:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,89 +1,45 @@
-function generateBadCharTable(pattern) {
-    const table = {};
-    const patternLength = pattern.length;
-
-    for (let i = 0; i < patternLength - 1; i++) {
-        table[pattern[i]] = patternLength - 1 - i;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-
-    return table;
 }
 
-function generateGoodSuffixTable(pattern) {
-    const table = new Array(pattern.length).fill(0);
-    const goodSuffixes = [];
-
-    // Fill the table with 0 as the default value
-    for (let i = 0; i < pattern.length; i++) {
-        table[i] = 0;
+class LinkedList {
+    constructor() {
+        this.head = null;
     }
 
-    let i = pattern.length;
-    let j = pattern.length + 1;
-
-    while (i > 0) {
-        if (pattern[i - 1] === pattern[j - 1]) {
-            i--;
-            j--;
-            table[i] = j - pattern.length;
+    addNode(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
         } else {
-            j = goodSuffixes[j];
-            if (j < i) {
-                i = j;
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
             }
+            current.next = newNode;
         }
     }
 
-    j = goodSuffixes[0];
-
-    for (i = 0; i < pattern.length; i++) {
-        if (table[i] === 0) {
-            table[i] = j;
+    getLength() {
+        let current = this.head;
+        let length = 0;
+        while (current) {
+            length++;
+            current = current.next;
         }
-
-        if (i === j) {
-            j = goodSuffixes[j];
-        }
+        return length;
     }
-
-    return table;
 }
 
-function boyerMooreSearch(text, pattern) {
-    const badCharTable = generateBadCharTable(pattern);
-    const goodSuffixTable = generateGoodSuffixTable(pattern);
+// Create a linked list
+let list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
 
-    const textLength = text.length;
-    const patternLength = pattern.length;
-    let shift = 0;
-
-    while (shift <= textLength - patternLength) {
-        let j = patternLength - 1;
-
-        while (j >= 0 && pattern[j] === text[shift + j]) {
-            j--;
-        }
-
-        if (j < 0) {
-            return shift; // Pattern found at index shift
-        } else {
-            const badCharShift = badCharTable[text[shift + j]] || patternLength;
-            const goodSuffixShift = goodSuffixTable[j];
-
-            shift += Math.max(badCharShift, goodSuffixShift);
-        }
-    }
-
-    return -1; // Pattern not found
-}
-
-// Example usage:
-const text = "ABAAABCD";
-const pattern = "ABC";
-const index = boyerMooreSearch(text, pattern);
-
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+// Get the length of the linked list
+console.log(list.getLength()); // Output: 4
