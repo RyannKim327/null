8f164111f119f2cd490787c6c6db247045d4b960
RyannKim327 <rksesgundo123@gmail.com>
2024-03-20 06:10:21 +0800

    3-20-2024 6:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,21 @@
-function factorial(num) {
-    if (num === 0) {
-        return 1;
-    } else {
-        return num * factorial(num - 1);
+function isAnagram(str1, str2) {
+    // Remove any non-alphabetic characters and convert the strings to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+
+    // Check if the lengths of the strings are equal
+    if (str1.length !== str2.length) {
+        return false;
     }
+
+    // Sort the characters of the strings and compare them
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
 }
 
-// Test the factorial function
-const number = 5;
-const result = factorial(number);
-console.log(`The factorial of ${number} is ${result}`);
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+console.log(isAnagram(string1, string2)); // Output: true
