51b802d49de948433e0ddf90454acd5905a43150
RyannKim327 <rksesgundo123@gmail.com>
2024-03-19 20:00:57 +0800

    3-19-2024 20:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,22 @@
-let str = "Hello, World! How are you?";
-let stringWithoutSpaces = str.replace(/\s/g, "");
+function isAnagram(str1, str2) {
+  // Remove non-alphabetic characters and convert to lowercase
+  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
-console.log(stringWithoutSpaces); // Output: "Hello,World!Howareyou?"
+  // Sort the characters of both strings
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  // Compare the sorted strings
+  return sortedStr1 === sortedStr2;
+}
+
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
