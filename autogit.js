0abda1d0360eab8866083f21f1dfd213ebd4f49f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-20 06:11:29 +0800

    3-20-2024 6:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,49 @@
-function isAnagram(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+// Node class for the linked list
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
 
-    // Check if the lengths of the strings are equal
-    if (str1.length !== str2.length) {
-        return false;
+// Linked list class
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  // Function to add a new node to the linked list
+  addNode(data) {
+    const newNode = new Node(data);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
     }
+  }
 
-    // Sort the characters in the strings and compare them
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
-
-    return sortedStr1 === sortedStr2;
+  // Function to find the length of the linked list
+  getLength() {
+    let count = 0;
+    let current = this.head;
+    while (current !== null) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
 }
 
-// Test the function
-const str1 = 'listen';
-const str2 = 'silent';
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
 
-if (isAnagram(str1, str2)) {
-    console.log(`${str1} and ${str2} are anagrams.`);
-} else {
-    console.log(`${str1} and ${str2} are not anagrams.`);
-}
+// Find the length of the linked list
+const length = linkedList.getLength();
+console.log("Length of the linked list: ", length);
