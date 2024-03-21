7fa6aaf94528b3d70472ae5ca41faaefe5e280db
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:39:04 +0800

    3-21-2024 19:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,56 @@
-const cron = require('node-cron');
+function computeLPSArray(pattern) {
+    const lps = new Array(pattern.length).fill(0);
+    let len = 0;
+    let i = 1;
 
-cron.schedule('* * * * *', () => {
-  console.log('Running a task every minute');
-});
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len !== 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+}
+
+function KMP(text, pattern) {
+    const lps = computeLPSArray(pattern);
+    const result = [];
+
+    let i = 0;
+    let j = 0;
+
+    while (i < text.length) {
+        if (pattern[j] === text[i]) {
+            i++;
+            j++;
+        }
+
+        if (j === pattern.length) {
+            result.push(i - j);
+            j = lps[j - 1];
+        } else if (i < text.length && pattern[j] !== text[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+
+    return result;
+}
+
+// Example
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const indexes = KMP(text, pattern);
+console.log("Pattern found at indexes:", indexes);
