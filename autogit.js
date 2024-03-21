5373f0fcd1fef0f5a48a296b4f482dc24d4555f4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:04:16 +0800

    3-21-2024 20:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,15 @@
-function isAnagram(str1, str2) {
-    // Check if the strings are of different lengths
-    if (str1.length !== str2.length) {
-        return false;
-    }
-
-    // Create objects to store character frequencies for each string
-    const charCount1 = {};
-    const charCount2 = {};
-
-    // Count the frequency of characters in str1
-    for (let char of str1) {
-        charCount1[char] = (charCount1[char] || 0) + 1;
-    }
-
-    // Count the frequency of characters in str2
-    for (let char of str2) {
-        charCount2[char] = (charCount2[char] || 0) + 1;
-    }
-
-    // Compare the character frequencies of both strings
-    for (let char in charCount1) {
-        if (charCount1[char] !== charCount2[char]) {
+function isSortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
             return false;
         }
     }
-
     return true;
 }
 
 // Test the function
-const string1 = "listen";
-const string2 = "silent";
-console.log(isAnagram(string1, string2)); // Output: true
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 4, 3, 2, 1];
+
+console.log(isSortedAscending(arr1)); // Output: true
+console.log(isSortedAscending(arr2)); // Output: false
