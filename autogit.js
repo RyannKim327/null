43d01b15abff5515bfad7c64afa56e66a87f8c5d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:36:56 +0800

    3-21-2024 19:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,59 @@
-// Helper function to get the nth digit of a number
-function getDigit(num, nth) {
-  return Math.floor((num / Math.pow(10, nth)) % 10);
-}
-
-// Helper function to get the number of digits in a number
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
-
-// Helper function to get the largest number of digits in an array of numbers
-function mostDigits(nums) {
-  let maxDigits = 0;
-  for (let i = 0; i < nums.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
-  }
-  return maxDigits;
-}
-
-// Radix Sort function
-function radixSort(arr) {
-  const maxDigits = mostDigits(arr);
-  
-  for (let k = 0; k < maxDigits; k++) {
-    let buckets = Array.from({ length: 10 }, () => []);
+function initialize(graph, source) {
+    let distances = {};
     
-    for (let i = 0; i < arr.length; i++) {
-      let digit = getDigit(arr[i], k);
-      buckets[digit].push(arr[i]);
+    for(let vertex in graph) {
+        distances[vertex] = vertex === source ? 0 : Infinity;
     }
     
-    arr = [].concat(...buckets);
-  }
-  
-  return arr;
+    return distances;
 }
 
-// Test the radix sort algorithm
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr));
+function relax(edge, distances) {
+    let {source, destination, weight} = edge;
+    
+    if(distances[source] + weight < distances[destination]) {
+        distances[destination] = distances[source] + weight;
+    }
+}
+
+function bellmanFord(graph, source) {
+    let distances = initialize(graph, source);
+    let edges = [];
+    
+    // Flatten the graph edges into a list
+    for(let node in graph) {
+        for(let neighbor of graph[node]) {
+            edges.push({source: node, destination: neighbor[0], weight: neighbor[1]});
+        }
+    }
+    
+    // Relax all edges |V| - 1 times
+    for(let i = 0; i < Object.keys(graph).length - 1; i++) {
+        for(let edge of edges) {
+            relax(edge, distances);
+        }
+    }
+    
+    // Check for negative weight cycles
+    for(let edge of edges) {
+        let {source, destination, weight} = edge;
+        if(distances[source] + weight < distances[destination]) {
+            console.log("Graph contains negative weight cycle");
+            return;
+        }
+    }
+    
+    return distances;
+}
+
+// Test the implementation
+let graph = {
+    'A': [['B', -1], ['C', 4]],
+    'B': [['C', 3], ['D', 2], ['E', 2]],
+    'C': [],
+    'D': ['B', 1],
+    'E': [['D', -3]]
+}
+
+let sourceNode = 'A';
+console.log(bellmanFord(graph, sourceNode));
