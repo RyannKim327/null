2752d5e94e7b850b80c535b84b7ea0d44ec75a60
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:45:56 +0800

    3-21-2024 19:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,66 @@
 class Node {
     constructor(value) {
         this.value = value;
-        this.next = null;
+        this.left = null;
+        this.right = null;
     }
 }
 
-function reverseLinkedList(head) {
-    let current = head;
-    let prev = null;
-    let next = null;
-
-    while (current !== null) {
-        next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
+class BinaryTree {
+    constructor() {
+        this.root = null;
     }
 
-    return prev;
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
 }
+
+// Usage example
+const binaryTree = new BinaryTree();
+binaryTree.insert(10);
+binaryTree.insert(5);
+binaryTree.insert(15);
+
+console.log(binaryTree.search(10)); // Output: true
+console.log(binaryTree.search(20)); // Output: false
