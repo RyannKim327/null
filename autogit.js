5f0b3ad1f6e4023fda9615fefedf8bd4739c4650
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:43:38 +0800

    3-21-2024 19:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,57 @@
-function calculateTriangleArea(base, height) {
-  return 0.5 * base * height;
+function tarjanSCC(graph) {
+    let index = 0;
+    let stack = [];
+    let indexMap = new Map();
+    let lowLinkMap = new Map();
+    let result = [];
+
+    function tarjanDFS(node) {
+        indexMap.set(node, index);
+        lowLinkMap.set(node, index);
+        index++;
+        stack.push(node);
+
+        for (let neighbor of graph[node]) {
+            if (!indexMap.has(neighbor)) {
+                tarjanDFS(neighbor);
+                lowLinkMap.set(node, Math.min(lowLinkMap.get(node), lowLinkMap.get(neighbor)));
+            } else if (stack.includes(neighbor)) {
+                lowLinkMap.set(node, Math.min(lowLinkMap.get(node), indexMap.get(neighbor)));
+            }
+        }
+
+        if (indexMap.get(node) === lowLinkMap.get(node)) {
+            let scc = [];
+            let popNode;
+            do {
+                popNode = stack.pop();
+                scc.push(popNode);
+            } while (popNode !== node);
+            result.push(scc);
+        }
+    }
+
+    for (let node in graph) {
+        if (!indexMap.has(node)) {
+            tarjanDFS(node);
+        }
+    }
+
+    return result;
 }
 
 // Example usage:
-const base = 10;
-const height = 5;
-const area = calculateTriangleArea(base, height);
-console.log('The area of the triangle is: ' + area);
+const graph = {
+    'A': ['B'],
+    'B': ['C'],
+    'C': ['A', 'D'],
+    'D': ['E'],
+    'E': ['F'],
+    'F': ['D', 'G'],
+    'G': ['H'],
+    'H': ['F', 'I'],
+    'I': []
+};
+
+const sccResult = tarjanSCC(graph);
+console.log(sccResult);
