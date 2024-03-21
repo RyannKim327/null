b384b1a88a1c73e488891ca67c58fa6401711360
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:58:52 +0800

    3-21-2024 19:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,22 @@
-let str1 = "Hello";
-let str2 = "World";
-let result = str1 + " " + str2;
-console.log(result); // Output: "Hello World"
+function countingSort(arr) {
+    let max = Math.max(...arr);
+    let countArray = new Array(max + 1).fill(0);
+    let sortedArray = [];
+
+    arr.forEach(num => {
+        countArray[num]++;
+    });
+
+    countArray.forEach((count, num) => {
+        for (let i = 0; i < count; i++) {
+            sortedArray.push(num);
+        }
+    });
+
+    return sortedArray;
+}
+
+// Example usage
+let arr = [4, 2, 2, 8, 3, 3, 1];
+let sortedArr = countingSort(arr);
+console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
