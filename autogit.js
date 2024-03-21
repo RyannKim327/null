18c077a81119c731c235cbb015cb5f7ed5f7a2d6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:26:12 +0800

    3-21-2024 19:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,4 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+let str = "Hello World";
+let lowercaseStr = str.toLowerCase();
 
-function countLeafNodes(root) {
-    if (!root) {
-        return 0;
-    }
-    
-    if (!root.left && !root.right) {
-        return 1;
-    }
-    
-    return countLeafNodes(root.left) + countLeafNodes(root.right);
-}
-
-// Example binary tree
-const root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-root.right.left = new Node(6);
-root.right.right = new Node(7);
-root.right.right.right = new Node(8);
-
-const leafNodeCount = countLeafNodes(root);
-console.log("Number of leaf nodes in the binary tree:", leafNodeCount);
+console.log(lowercaseStr); // Output: hello world
