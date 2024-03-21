ce215e24e228e4ad4101c6f8483f0025af086c8f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:34:07 +0800

    3-21-2024 19:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,42 @@
-function isPalindrome(str) {
-    let start = 0;
-    let end = str.length - 1;
-
-    while (start < end) {
-        if (str[start] !== str[end]) {
-            return false;
-        }
-        start++;
-        end--;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
-
-    return true;
 }
 
-// Test the function
-console.log(isPalindrome("madam")); // Output: true
-console.log(isPalindrome("hello")); // Output: false
+function findNthFromEnd(head, n) {
+    let p1 = head;
+    let p2 = head;
+
+    // Move p2 n nodes ahead
+    for (let i = 0; i < n; i++) {
+        if (p2 === null) {
+            return null; // n is greater than the length of the list
+        }
+        p2 = p2.next;
+    }
+
+    // Move p1 and p2 simultaneously until p2 reaches the end
+    while (p2 !== null) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+
+    return p1;
+}
+
+// Example linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+let n = 2;
+let nthNodeFromEnd = findNthFromEnd(head, n);
+if (nthNodeFromEnd !== null) {
+    console.log(`The ${n}th node from end is: ${nthNodeFromEnd.value}`);
+} else {
+    console.log(`N is greater than the length of the list`);
+}
