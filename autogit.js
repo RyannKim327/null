e95e659b1b6a25c381137c6ab6aef17b33980cee
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:54:16 +0800

    3-21-2024 19:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,9 @@
-function radixSort(arr) {
-    const getMax = (arr) => {
-        let max = 0;
-        arr.forEach(num => {
-            max = Math.max(max, num);
-        });
-        return max;
-    };
-    
-    const digitCount = (num) => {
-        if (num === 0) return 1;
-        return Math.floor(Math.log10(num)) + 1;
-    };
-    
-    const getDigit = (num, place) => {
-        return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-    };
-    
-    const radixSortHelper = (arr) => {
-        const maxDigits = digitCount(getMax(arr));
-        for (let k = 0; k < maxDigits; k++) {
-            const buckets = Array.from({ length: 10 }, () => []);
-            for (let i = 0; i < arr.length; i++) {
-                const digit = getDigit(arr[i], k);
-                buckets[digit].push(arr[i]);
-            }
-            arr = [].concat(...buckets);
-        }
-        return arr;
-    };
-    
-    return radixSortHelper(arr);
+function findCommonElements(arr1, arr2) {
+  return arr1.filter(element => arr2.includes(element));
 }
 
-// Example usage
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-const sortedArr = radixSort(arr);
-console.log(sortedArr);
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [3, 4, 5, 6, 7];
+
+let commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
