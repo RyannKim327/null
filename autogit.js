acde895fa3522a82359a58a1d5127fb681dbdb7e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:08:02 +0800

    3-21-2024 20:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,8 @@
-function findIntersection(list1, list2) {
-    let nodeSet = new Set();
-    let intersection = [];
-
-    // Traverse the first linked list and store the nodes in a Set
-    let current = list1;
-    while (current) {
-        nodeSet.add(current);
-        current = current.next;
-    }
-
-    // Traverse the second linked list and check if each node is in the Set
-    current = list2;
-    while (current) {
-        if (nodeSet.has(current)) {
-            intersection.push(current);
-        }
-        current = current.next;
-    }
-
-    return intersection;
+function decToBin(dec) {
+    return (dec >>> 0).toString(2);
 }
+
+// Example
+let decimalNumber = 42;
+let binaryNumber = decToBin(decimalNumber);
+console.log(binaryNumber);  // Output: 101010
