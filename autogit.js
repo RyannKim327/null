fc367f7e88af59637b4dc2e9018ee2c1dbc67921
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:51:19 +0800

    3-21-2024 19:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,3 @@
-function firstNonRepeatingCharacter(str) {
-    // Create an object to store the count of each character
-    var charCount = {};
-
-    // Count the occurrences of each character in the string
-    for (var i = 0; i < str.length; i++) {
-        var char = str[i];
-        if (charCount[char]) {
-            charCount[char]++;
-        } else {
-            charCount[char] = 1;
-        }
-    }
-
-    // Loop through the string to find the first non-repeating character
-    for (var j = 0; j < str.length; j++) {
-        var char = str[j];
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-
-    return null; // If no non-repeating character is found
-}
-
-// Test the function
-var str = "hello world";
-console.log(firstNonRepeatingCharacter(str)); // Output: "h"
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // Output: [5, 4, 3, 2, 1]
