5db9c9d7e44e35111cbf4270f8f5507e5357367d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:48:53 +0800

    3-21-2024 19:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,38 @@
-function isAnagram(str1, str2) {
-    // Remove non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
-    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
-
-    // Sort the characters in both strings
-    let sortedStr1 = str1.split('').sort().join('');
-    let sortedStr2 = str2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
+// Node class to represent each node in the graph
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  }
 }
 
-// Test the function
-let string1 = "listen";
-let string2 = "silent";
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
+// Depth-first search function
+function depthFirstSearch(node, visited = new Set()) {
+  if (!node) {
+    return;
+  }
+
+  // Visit the current node
+  console.log(node.value);
+  visited.add(node);
+
+  // Visit all the neighbors of the current node
+  for (let child of node.children) {
+    if (!visited.has(child)) {
+      depthFirstSearch(child, visited);
+    }
+  }
 }
+
+// Example usage
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+node1.children = [node2, node3];
+node2.children = [node4];
+node3.children = [node5];
+
+depthFirstSearch(node1);
