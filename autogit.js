b3a311c6d901b2a09a233e287ca586a4fb8adffc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:52:58 +0800

    3-21-2024 19:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,23 @@
-function mergeSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
-    const n = arr.length;
-    const sorted = arr.slice();
-    const buffer = new Array(n);
-
-    for (let size = 1; size < n; size *= 2) {
-        for (let leftStart = 0; leftStart < n; leftStart += 2*size) {
-            const leftEnd = Math.min(leftStart + size, n);
-            const rightStart = leftEnd;
-            const rightEnd = Math.min(rightStart + size, n);
-
-            let i = leftStart;
-            let j = rightStart;
-            let k = leftStart;
-
-            while (i < leftEnd && j < rightEnd) {
-                if (sorted[i] <= sorted[j]) {
-                    buffer[k++] = sorted[i++];
-                } else {
-                    buffer[k++] = sorted[j++];
-                }
-            }
-
-            while (i < leftEnd) {
-                buffer[k++] = sorted[i++];
-            }
-
-            while (j < rightEnd) {
-                buffer[k++] = sorted[j++];
-            }
+function findMajorityElement(arr) {
+    let map = {};
+    
+    for (let num of arr) {
+        map[num] = (map[num] || 0) + 1;
+        
+        if (map[num] > arr.length / 2) {
+            return num;
         }
-
-        const temp = sorted;
-        sorted = buffer;
-        buffer = temp;
     }
-
-    return sorted;
+    
+    return -1; // no majority element found
 }
 
-// Usage
-const arr = [5, 3, 8, 6, 2, 7, 1, 4];
-const sortedArr = mergeSort(arr);
-console.log(sortedArr);
+// Example
+let arr = [2, 2, 3, 3, 3, 2, 3];
+let majorityElement = findMajorityElement(arr);
+
+if (majorityElement !== -1) {
+    console.log(`The majority element in the array is: ${majorityElement}`);
+} else {
+    console.log(`No majority element found in the array.`);
+}
