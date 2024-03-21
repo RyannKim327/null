b30b04d24436d4cf4133cec547d178dc35b2c7ca
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:45:09 +0800

    3-21-2024 19:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,49 @@
-function reverseWords(str) {
-    // Split the string into an array of words
-    let words = str.split(" ");
+function FibonacciSearch(arr, x) {
+    let fib1 = 0;
+    let fib2 = 1;
+    let fibM = fib1 + fib2;
 
-    // Reverse the array of words
-    words = words.reverse();
+    let n = arr.length;
 
-    // Join the array back into a string
-    let reversedStr = words.join(" ");
+    while (fibM < n) {
+        fib1 = fib2;
+        fib2 = fibM;
+        fibM = fib1 + fib2;
+    }
 
-    return reversedStr;
+    let offset = -1;
+
+    while (fibM > 1) {
+        let i = Math.min(offset + fib1, n - 1);
+
+        if (arr[i] < x) {
+            fibM = fib2;
+            fib2 = fib1;
+            fib1 = fibM - fib2;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibM = fib1;
+            fib2 -= fib1;
+            fib1 = fibM - fib2;
+        } else {
+            return i;
+        }
+    }
+
+    if (fib2 && arr[offset + 1] == x) {
+        return offset + 1;
+    }
+
+    return -1;
 }
 
-// Test the function
-let originalStr = "Hello world!";
-let reversedStr = reverseWords(originalStr);
-console.log(reversedStr); // Output: "world! Hello"
+// Usage
+let arr = [2, 3, 4, 10, 40];
+let x = 10;
+let result = FibonacciSearch(arr, x);
+
+if (result != -1) {
+    console.log(`Element found at index ${result}`);
+} else {
+    console.log("Element not found");
+}
