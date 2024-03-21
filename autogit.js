50214c7bdaec2fed7b215077e3ace6d9666fb648
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:37:29 +0800

    3-21-2024 19:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,17 @@
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-
-let index = array.indexOf(elementToRemove);
-if (index > -1) {
-    array.splice(index, 1);
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-console.log(array); // Output: [1, 2, 4, 5]
+var arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original array: " + arr);
+console.log("Sorted array: " + bubbleSort(arr));
