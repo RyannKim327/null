db5e44136a23d21495f72b3faeb69e0f84bcc656
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:49:35 +0800

    3-21-2024 19:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,39 @@
-let str = "Hello, World! This is a string with spaces.";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-// Remove all spaces from the string
-let stringWithoutSpaces = str.replace(/\s/g, '');
+function findIntersection(list1, list2) {
+  let set = new Set();
 
-console.log(stringWithoutSpaces); // Output: Hello,World!Thisisastringwithspaces.
+  let current1 = list1;
+  while (current1) {
+    set.add(current1);
+    current1 = current1.next;
+  }
+
+  let current2 = list2;
+  while (current2) {
+    if (set.has(current2)) {
+      return current2;
+    }
+    current2 = current2.next;
+  }
+
+  return null;
+}
+
+// Example usage
+// Construct linked lists
+let list1 = new Node(1);
+list1.next = new Node(2);
+let intersectionNode = new Node(3);
+list1.next.next = intersectionNode;
+
+let list2 = new Node(4);
+list2.next = intersectionNode;
+
+let intersection = findIntersection(list1, list2);
+console.log("Intersection node value: " + intersection.value); // Output: Intersection node value: 3
