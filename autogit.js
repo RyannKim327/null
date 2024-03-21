1716290f2fe935348bdcfc4a47373097b9e4036d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:35:46 +0800

    3-21-2024 19:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,16 @@
-function Node(value) {
-    this.value = value;
-    this.visited = false;
-    this.neighbors = [];
+function findSecondLargest(arr) {
+    // Sort the array in descending order
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+
+    // Return the second element in the sorted array
+    return arr[1];
 }
 
-function Graph() {
-    this.nodes = {};
-  
-    this.addNode = function(value) {
-        if (!this.nodes[value]) {
-            this.nodes[value] = new Node(value);
-        }
-    };
+// Example array
+const arr = [10, 5, 8, 20, 15];
 
-    this.addEdge = function(from, to) {
-        this.nodes[from].neighbors.push(to);
-    };
-}
-
-function topologicalSortHelper(node, stack) {
-    node.visited = true;
-
-    for (let neighbor of node.neighbors) {
-        if (!neighbor.visited) {
-            topologicalSortHelper(neighbor, stack);
-        }
-    }
-
-    stack.push(node.value);
-}
-
-function topologicalSort(graph) {
-    const stack = [];
-  
-    for (let nodeValue in graph.nodes) {
-        const node = graph.nodes[nodeValue];
-        if (!node.visited) {
-            topologicalSortHelper(node, stack);
-        }
-    }
-
-    return stack.reverse();
-}
-
-// Example usage
-const graph = new Graph();
-graph.addNode(1);
-graph.addNode(2);
-graph.addNode(3);
-graph.addNode(4);
-graph.addEdge(1, 2);
-graph.addEdge(1, 3);
-graph.addEdge(2, 4);
-graph.addEdge(3, 4);
-
-const sortedNodes = topologicalSort(graph);
-console.log(sortedNodes); // Output: [1, 3, 2, 4]
+// Find the second largest element
+const secondLargest = findSecondLargest(arr);
+console.log("Second largest element: ", secondLargest);
