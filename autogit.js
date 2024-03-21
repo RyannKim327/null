aaf560190f40009079e6618cf735cc3cdc163700
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:25:17 +0800

    3-21-2024 19:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,11 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function isPalindrome(str) {
+    // Remove special characters and convert to lowercase
+    str = str.toLowerCase().replace(/[\W_]/g, '');
+    
+    // Compare the string with its reverse
+    return str === str.split('').reverse().join('');
 }
 
-function findMiddleElement(head) {
-    let slowPointer = head;
-    let fastPointer = head;
-
-    while (fastPointer !== null && fastPointer.next !== null) {
-        slowPointer = slowPointer.next;
-        fastPointer = fastPointer.next.next;
-    }
-
-    return slowPointer.data;
-}
-
-// Test the implementation
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-console.log(findMiddleElement(head)); // Output: 3
+// Test the function
+console.log(isPalindrome("A man, a plan, a canal, Panama")); //true
+console.log(isPalindrome("Hello World")); //false
