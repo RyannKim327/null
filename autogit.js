0c3469e147305180d9c6c7f466b8e1feadec7dd0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:32:05 +0800

    3-21-2024 19:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,34 @@
-function bubbleSort(arr) {
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+function topologicalSort(graph) {
+  const visited = new Set();
+  const stack = [];
+
+  function dfs(node) {
+    visited.add(node);
+    graph[node].forEach((neighbor) => {
+      if (!visited.has(neighbor)) {
+        dfs(neighbor);
+      }
+    });
+    stack.unshift(node);
+  }
+
+  for (let node in graph) {
+    if (!visited.has(node)) {
+      dfs(node);
     }
-    return arr;
+  }
+
+  return stack;
 }
 
-// Example usage
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log(bubbleSort(arr)); // Output: [11, 12, 22, 25, 34, 64, 90]
+// Example graph representation using adjacency list
+const graph = {
+  'A': ['B', 'C'],
+  'B': ['D'],
+  'C': ['D'],
+  'D': ['E'],
+  'E': []
+};
+
+const result = topologicalSort(graph);
+console.log(result); // Output: ['A', 'C', 'B', 'D', 'E']
