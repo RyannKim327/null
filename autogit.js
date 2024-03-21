84f2e1654503e786ef309b56b778922e486293a4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:36:46 +0800

    3-21-2024 19:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,41 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+// Helper function to get the nth digit of a number
+function getDigit(num, nth) {
+  return Math.floor((num / Math.pow(10, nth)) % 10);
+}
+
+// Helper function to get the number of digits in a number
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+// Helper function to get the largest number of digits in an array of numbers
+function mostDigits(nums) {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+}
+
+// Radix Sort function
+function radixSort(arr) {
+  const maxDigits = mostDigits(arr);
+  
+  for (let k = 0; k < maxDigits; k++) {
+    let buckets = Array.from({ length: 10 }, () => []);
+    
+    for (let i = 0; i < arr.length; i++) {
+      let digit = getDigit(arr[i], k);
+      buckets[digit].push(arr[i]);
+    }
+    
+    arr = [].concat(...buckets);
+  }
+  
+  return arr;
+}
+
+// Test the radix sort algorithm
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr));
