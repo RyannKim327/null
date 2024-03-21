635ed9ba67994cb40b8d000588ecca4c6b20733e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:56:49 +0800

    3-21-2024 19:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,58 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert to lowercase
-    str = str.toLowerCase().replace(/[\W_]/g, '');
-    
-    // Compare the original string with its reverse
-    return str === str.split('').reverse().join('');
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-// Test the function
-const string1 = "A man, a plan, a canal, Panama!";
-console.log(isPalindrome(string1)); // Output: true
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-const string2 = "Hello, world!";
-console.log(isPalindrome(string2)); // Output: false
+    addNode(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    findNthFromEnd(n) {
+        let pointer1 = this.head;
+        let pointer2 = this.head;
+
+        // Move pointer2 n nodes ahead
+        for (let i = 0; i < n; i++) {
+            if (pointer2 === null) {
+                return null; // List is shorter than n nodes
+            }
+            pointer2 = pointer2.next;
+        }
+
+        // Move both pointers until pointer2 reaches the end
+        while (pointer2 !== null) {
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+        }
+
+        return pointer1; // pointer1 is now pointing to the nth node from the end
+    }
+}
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+
+const n = 2;
+const nthNode = linkedList.findNthFromEnd(n); // Find the 2nd node from the end
+console.log(nthNode.value); // Output: 4
