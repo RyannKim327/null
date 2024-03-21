013bd6046543393979c288a0a7aea7d4dba07cf9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:53:12 +0800

    3-21-2024 19:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,24 @@
-function findSecondLargest(arr) {
-  arr.sort(function(a, b){return a - b});
-  return arr[arr.length - 2];
+function quicksort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        const pivot = arr[0];
+        const left = [];
+        const right = [];
+
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
+            }
+        }
+
+        return quicksort(left).concat(pivot, quicksort(right));
+    }
 }
 
 // Example usage
-let array = [10, 5, 8, 20];
-let secondLargest = findSecondLargest(array);
-console.log("Second largest element: ", secondLargest);
+const unsortedArray = [5, 2, 9, 3, 7, 6];
+const sortedArray = quicksort(unsortedArray);
+console.log(sortedArray);
