ed5ff1b2c55da42d3979b9ca4b853a814a0064ad
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:30:56 +0800

    3-21-2024 19:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,6 @@
-function isSortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
+let array = [1, 2, 3, 4, 1, 2, 5];
 
-// Test the function
-const arr1 = [1, 2, 3, 4, 5];
-console.log(isSortedAscending(arr1)); // Output: true
+// Create a Set from the array to remove duplicates
+let uniqueArray = [...new Set(array)];
 
-const arr2 = [5, 4, 3, 2, 1];
-console.log(isSortedAscending(arr2)); // Output: false
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
