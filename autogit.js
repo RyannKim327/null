dfaa13be45bb97b4a1e03ebe16fd11c70e52bd03
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:02:32 +0800

    3-21-2024 20:2 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,59 +1,15 @@
-// Define a graph class
-class Graph {
-    constructor() {
-        this.adjList = new Map();
-    }
-
-    addVertex(v) {
-        this.adjList.set(v, []);
-    }
-
-    addEdge(v, w) {
-        this.adjList.get(v).push(w);
-    }
-
-    // Depth-First Search function
-    dfsUtil(v, visited) {
-        visited[v] = true;
-        console.log(v);
-
-        let neighbors = this.adjList.get(v);
-
-        for (let i = 0; i < neighbors.length; i++) {
-            let neighbor = neighbors[i];
-            if (!visited[neighbor]) {
-                this.dfsUtil(neighbor, visited);
-            }
+function countOccurrences(str, char) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
         }
     }
-
-    dfs(startingNode) {
-        let visited = [];
-        for (let i = 0; i < this.adjList.size; i++) {
-            visited[i] = false;
-        }
-
-        this.dfsUtil(startingNode, visited);
-    }
+    return count;
 }
 
-// Create a new graph
-let graph = new Graph();
+var myString = "Hello, World!";
+var charToCount = "o";
+var occurrences = countOccurrences(myString, charToCount);
 
-// Add vertices
-graph.addVertex(0);
-graph.addVertex(1);
-graph.addVertex(2);
-graph.addVertex(3);
-graph.addVertex(4);
-graph.addVertex(5);
-
-// Add edges
-graph.addEdge(0, 1);
-graph.addEdge(0, 2);
-graph.addEdge(1, 3);
-graph.addEdge(1, 4);
-graph.addEdge(2, 5);
-
-// Perform depth-first search starting from vertex 0
-graph.dfs(0);
+console.log("The character '" + charToCount + "' occurs " + occurrences + " times in the string.");
