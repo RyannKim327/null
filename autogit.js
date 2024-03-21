10565a1cacf810f44b68fc5ba3747c53eebefc79
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:30:42 +0800

    3-21-2024 19:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,22 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+function areAnagrams(str1, str2) {
+    // Convert strings to lowercase and remove non-alphabetic characters
+    str1 = str1.toLowerCase().replace(/[^a-z]/g, '');
+    str2 = str2.toLowerCase().replace(/[^a-z]/g, '');
+
+    // Sort characters of both strings alphabetically
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    // Compare the sorted strings
+    return sortedStr1 === sortedStr2;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+// Test the function
+const string1 = 'listen';
+const string2 = 'silent';
 
-    append(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-
-        current.next = newNode;
-    }
-
-    reverse() {
-        let prev = null;
-        let current = this.head;
-        let next = null;
-
-        while (current) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        this.head = prev;
-    }
-
-    printList() {
-        let current = this.head;
-        while (current) {
-            console.log(current.value);
-            current = current.next;
-        }
-    }
+if (areAnagrams(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
 }
-
-// Example usage:
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
-
-console.log("Original Linked List:");
-linkedList.printList();
-
-linkedList.reverse();
-
-console.log("Reversed Linked List:");
-linkedList.printList();
