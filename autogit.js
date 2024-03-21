a212c494110844942cb79fcf8f54bc7b818982be
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:36:29 +0800

    3-21-2024 19:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,4 @@
-function findMedianSortedArrays(nums1, nums2) {
-    // Concatenate the two arrays and sort them
-    const mergedArray = nums1.concat(nums2).sort((a, b) => a - b);
-
-    const n = mergedArray.length;
-
-    // Check if the length of the merged arrays is odd or even to find the median
-    if (n % 2 === 0) {
-        // If the length is even, return the average of the two middle elements
-        return (mergedArray[n/2 - 1] + mergedArray[n/2]) / 2;
-    } else {
-        // If the length is odd, return the middle element
-        return mergedArray[Math.floor(n/2)];
-    }
-}
-
-// Example
-const nums1 = [1, 3];
-const nums2 = [2];
-
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
