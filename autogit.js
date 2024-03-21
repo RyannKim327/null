ee5e0e2147b504d9b05c1128f9a48467120b79be
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:06:54 +0800

    3-21-2024 20:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,25 @@
-let str = "  Hello,   World!  ";
-let trimmedStr = str.replace(/\s/g, '');
-console.log(trimmedStr); // Output: "Hello,World!"
+function findIntersection(headA, headB) {
+    let seenNodes = new Set();
+
+    let currentA = headA;
+    while (currentA) {
+        seenNodes.add(currentA);
+        currentA = currentA.next;
+    }
+
+    let currentB = headB;
+    while (currentB) {
+        if (seenNodes.has(currentB)) {
+            return currentB;
+        }
+        currentB = currentB.next;
+    }
+
+    return null; // No intersection found
+}
+let intersectionNode = findIntersection(headA, headB);
+if (intersectionNode) {
+    console.log("Intersection node value: " + intersectionNode.value);
+} else {
+    console.log("No intersection found.");
+}
