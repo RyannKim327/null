f4164bc8c65acdc3cc37258a2eef9e70278399db
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:07:33 +0800

    3-21-2024 20:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,20 @@
-const axios = require('axios');
+function isAnagram(str1, str2) {
+    // Remove any special characters and convert to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    // Check if the lengths of the strings are different
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Sort the characters of the strings and compare them
+    let sortedStr1 = str1.split('').sort().join('');
+    let sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
+}
+
+// Test the function
+console.log(isAnagram('listen', 'silent')); // Output: true
+console.log(isAnagram('hello', 'world')); // Output: false
