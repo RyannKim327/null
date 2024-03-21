6adea8d03075ce4a584f60d055bbdba1eb586b8a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:50:52 +0800

    3-21-2024 19:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,3 @@
-// Generate a random number in a given range
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Example: Generate a random number between 1 and 10
-const randomNumber = getRandomNumber(1, 10);
-console.log(randomNumber);
+let array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output will be [5, 4, 3, 2, 1]
