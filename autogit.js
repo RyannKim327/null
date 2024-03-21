73cc419ea1eeb1fe7451fc102cd09b6043d66923
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:40:02 +0800

    3-21-2024 19:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,48 @@
-// Node class to represent individual elements in the queue
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function fibonacciSearch(arr, key) {
+    let fibMMm2 = 0; // (m-2)'th Fibonacci number
+    let fibMMm1 = 1; // (m-1)'th Fibonacci number
+    let fibM = fibMMm2 + fibMMm1; // m'th Fibonacci number
+  
+    // Find the smallest Fibonacci Number greater than or equal to the length of the array
+    while (fibM < arr.length) {
+        fibMMm2 = fibMMm1;
+        fibMMm1 = fibM;
+        fibM = fibMMm2 + fibMMm1;
     }
-}
-
-// Queue class to implement the queue using a linked list
-class Queue {
-    constructor() {
-        this.front = null;
-        this.rear = null;
-        this.size = 0;
-    }
-
-    // Method to add an element to the queue
-    enqueue(data) {
-        const newNode = new Node(data);
-        if (!this.front) {
-            this.front = newNode;
-            this.rear = newNode;
+  
+    let offset = -1;
+  
+    while (fibM > 1) {
+        const i = Math.min(offset + fibMMm2, arr.length - 1);
+  
+        if (arr[i] < key) {
+            fibM = fibMMm1;
+            fibMMm1 = fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
+            offset = i;
+        } else if (arr[i] > key) {
+            fibM = fibMMm2;
+            fibMMm1 -= fibMMm2;
+            fibMMm2 = fibM - fibMMm1;
         } else {
-            this.rear.next = newNode;
-            this.rear = newNode;
+            return i;
         }
-        this.size++;
     }
-
-    // Method to remove and return the element at the front of the queue
-    dequeue() {
-        if (!this.front) {
-            return null;
-        }
-        const data = this.front.data;
-        this.front = this.front.next;
-        if (!this.front) {
-            this.rear = null;
-        }
-        this.size--;
-        return data;
+  
+    if (fibMMm1 && arr[offset + 1] === key) {
+        return offset + 1;
     }
-
-    // Method to check if the queue is empty
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    // Method to get the size of the queue
-    getSize() {
-        return this.size;
-    }
+  
+    return -1;
 }
 
-// Example usage:
-const queue = new Queue();
+// Example usage
+const arr = [10, 22, 30, 40, 50, 60, 70, 80, 90, 100];
+const key = 60;
+const index = fibonacciSearch(arr, key);
 
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-console.log(queue.getSize()); // Output: 1
-
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log("Element not found");
+}
