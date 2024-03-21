ee0aafecc9f4eeac1bb0ef529986c29b7fc6b0be
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:29:52 +0800

    3-21-2024 19:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,29 @@
-function findSecondLargest(arr) {
-    arr.sort((a, b) => b - a);
-    return arr[1];
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-const numbers = [4, 10, 5, 8, 2];
-const secondLargest = findSecondLargest(numbers);
-console.log(secondLargest); // Outputs 8
+function findMiddleElement(head) {
+    let slowPtr = head;
+    let fastPtr = head;
+
+    while (fastPtr !== null && fastPtr.next !== null) {
+        slowPtr = slowPtr.next;
+        fastPtr = fastPtr.next.next;
+    }
+
+    return slowPtr.data;
+}
+
+// Example of creating a linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Call the function to find the middle element
+let middleElement = findMiddleElement(head);
+console.log('Middle element of the linked list: ' + middleElement);
