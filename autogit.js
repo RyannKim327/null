2c579636eeab2b95d3b79f3328fead9dc5295da4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:56:56 +0800

    3-21-2024 19:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,58 +1,26 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+// Definition for a binary tree node
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
+// Function to find the maximum depth of a binary tree
+function maxDepth(root) {
+    if (root === null) {
+        return 0;
     }
 
-    addNode(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
+    let leftDepth = maxDepth(root.left);
+    let rightDepth = maxDepth(root.right);
 
-    findNthFromEnd(n) {
-        let pointer1 = this.head;
-        let pointer2 = this.head;
-
-        // Move pointer2 n nodes ahead
-        for (let i = 0; i < n; i++) {
-            if (pointer2 === null) {
-                return null; // List is shorter than n nodes
-            }
-            pointer2 = pointer2.next;
-        }
-
-        // Move both pointers until pointer2 reaches the end
-        while (pointer2 !== null) {
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
-        }
-
-        return pointer1; // pointer1 is now pointing to the nth node from the end
-    }
+    return Math.max(leftDepth, rightDepth) + 1;
 }
 
-// Example usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
+// Example binary tree
+let root = new TreeNode(3);
+root.left = new TreeNode(9);
+root.right = new TreeNode(20);
+root.right.left = new TreeNode(15);
+root.right.right = new TreeNode(7);
 
-const n = 2;
-const nthNode = linkedList.findNthFromEnd(n); // Find the 2nd node from the end
-console.log(nthNode.value); // Output: 4
+console.log(maxDepth(root)); // Output: 3
