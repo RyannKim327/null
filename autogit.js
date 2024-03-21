24c8322355a568a3599600f85c5ce7aaecb4af55
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:47:35 +0800

    3-21-2024 19:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,110 @@
-const fetchAndroidBackendData = async () => {
-    try {
-        const response = await fetch('http://your-android-backend-url/data');
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Error fetching data from Android backend:', error);
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
     }
-};
+}
 
-fetchAndroidBackendData();
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(data) {
+        const newNode = new Node(data);
+        
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (node.data > newNode.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    remove(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key) {
+        if (node === null) {
+            return null;
+        } else if (key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if (key > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+            if (node.left === null && node.right === null) {
+                node = null;
+                return node;
+            }
+
+            if (node.left === null) {
+                node = node.right;
+                return node;
+            } else if (node.right === null) {
+                node = node.left;
+                return node;
+            }
+
+            const aux = this.findMinNode(node.right);
+            node.data = aux.data;
+
+            node.right = this.removeNode(node.right, aux.data);
+            return node;
+        }
+    }
+
+    findMinNode(node) {
+        if (node.left === null) {
+            return node;
+        } else {
+            return this.findMinNode(node.left);
+        }
+    }
+
+    inorder(node) {
+        if (node !== null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
+    }
+
+    getRootNode() {
+        return this.root;
+    }
+}
+
+// Usage of the Binary Search Tree
+const bst = new BST();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log("Inorder traversal of the binary search tree:");
+bst.inorder(bst.getRootNode());
+
+bst.remove(5);
+
+console.log("Inorder traversal after deleting 5:");
+bst.inorder(bst.getRootNode());
