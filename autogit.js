f51a0e3b5eedfc3afb6a8dc090402ba48aa196a7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:32:12 +0800

    3-21-2024 19:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,5 @@
-function topologicalSort(graph) {
-  const visited = new Set();
-  const stack = [];
-
-  function dfs(node) {
-    visited.add(node);
-    graph[node].forEach((neighbor) => {
-      if (!visited.has(neighbor)) {
-        dfs(neighbor);
-      }
-    });
-    stack.unshift(node);
-  }
-
-  for (let node in graph) {
-    if (!visited.has(node)) {
-      dfs(node);
-    }
-  }
-
-  return stack;
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// Example graph representation using adjacency list
-const graph = {
-  'A': ['B', 'C'],
-  'B': ['D'],
-  'C': ['D'],
-  'D': ['E'],
-  'E': []
-};
-
-const result = topologicalSort(graph);
-console.log(result); // Output: ['A', 'C', 'B', 'D', 'E']
+let randomNumber = getRandomNumber(1, 100);
+console.log(randomNumber); // Output a random number between 1 and 100
