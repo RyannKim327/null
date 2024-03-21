2175c418e734bc219987fac470d4dcf70d8fd690
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:47:56 +0800

    3-21-2024 19:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,30 @@
-function bubbleSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - i - 1; j++) {
-            // Swap if the element found is greater than the next element
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+function findMajorityElement(arr) {
+    let map = {};
+    let majorityElement = null;
+    let maxCount = 0;
+
+    // Count occurrences of each element in the array
+    arr.forEach(num => {
+        if (map[num]) {
+            map[num]++;
+        } else {
+            map[num] = 1;
         }
+        
+        if (map[num] > maxCount) {
+            maxCount = map[num];
+            majorityElement = num;
+        }
+    });
+
+    // Check if the majority element count is greater than half the length of the array
+    if (maxCount > arr.length / 2) {
+        return majorityElement;
+    } else {
+        return "No majority element found";
     }
-    return arr;
 }
 
-// Test the bubbleSort function
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Unsorted array:", arr);
-console.log("Sorted array:", bubbleSort(arr));
+// Test the function
+const array = [2, 2, 3, 4, 2, 2, 5, 2, 2];
+console.log(findMajorityElement(array)); // Output: 2
