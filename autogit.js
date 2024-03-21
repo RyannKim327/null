48bb1fa68d567e66120b12c04af477f67f23a99e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:01:19 +0800

    3-21-2024 20:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,8 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentVal = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > currentVal) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = currentVal;
-    }
-    return arr;
-}
+var array = [1, 2, 3, 4, 5];
+var max = Math.max.apply(null, array);
 
-// Example
-let arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-console.log("Before sorting:", arr);
-insertionSort(arr);
-console.log("After sorting:", arr);
+console.log(max); // Output: 5
+var array = [1, 2, 3, 4, 5];
+var max = Math.max(...array);
+
+console.log(max); // Output: 5
