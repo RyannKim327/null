a5e0e6a23e8b28211829f83e73c7d8479c4dff28
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:45:41 +0800

    3-21-2024 19:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,4 @@
-class Stack {
-    constructor() {
-        this.stackArray = [];
-    }
+let array = [1, 2, 3, 4, 5];
+array.reverse();
 
-    // Push an element onto the stack
-    push(element) {
-        this.stackArray.push(element);
-    }
-
-    // Remove and return the top element of the stack
-    pop() {
-        if (this.isEmpty()) {
-            return "Stack is empty";
-        }
-        return this.stackArray.pop();
-    }
-
-    // Return the top element of the stack without removing it
-    peek() {
-        return this.stackArray[this.stackArray.length - 1];
-    }
-
-    // Check if the stack is empty
-    isEmpty() {
-        return this.stackArray.length === 0;
-    }
-
-    // Return the size of the stack
-    size() {
-        return this.stackArray.length;
-    }
-}
-
-// Example Usage
-const myStack = new Stack();
-myStack.push(5);
-myStack.push(10);
-myStack.push(15);
-
-console.log(myStack.pop()); // 15
-console.log(myStack.peek()); // 10
-console.log(myStack.size()); // 2
+console.log(array); // Output: [5, 4, 3, 2, 1]
