bcdde0af5c7fd7515cfd1305fd609afb4ff30e46
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:37:18 +0800

    3-21-2024 19:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,61 @@
-function findSecondLargest(arr) {
-    arr.sort((a, b) => b - a);
-    return arr[1];
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-// Test the function
-let array = [10, 20, 5, 15];
-console.log("Second largest element is: ", findSecondLargest(array));
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        while (current) {
+            let next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        this.head = prev;
+    }
+
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+
+console.log("Original linked list:");
+linkedList.printList();
+
+linkedList.reverse();
+
+console.log("\nReversed linked list:");
+linkedList.printList();
