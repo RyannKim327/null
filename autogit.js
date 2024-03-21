3d84fb040e7b22aee12656f5162f3136800263e0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:30:08 +0800

    3-21-2024 19:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,79 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+  constructor() {
+    this.children = {};
+  }
 }
 
-function findMiddleElement(head) {
-    let slowPtr = head;
-    let fastPtr = head;
+class SuffixTree {
+  constructor(input) {
+    this.root = new Node();
+    this.input = input;
+    this.activeNode = this.root;
+    this.activeEdge = 0;
+    this.activeLength = 0;
+    this.remaining = 0;
+    this.end = 0;
+    this.splitEnd = 0;
+    this.size = -1;
 
-    while (fastPtr !== null && fastPtr.next !== null) {
-        slowPtr = slowPtr.next;
-        fastPtr = fastPtr.next.next;
+    this.buildSuffixTree();
+  }
+
+  buildSuffixTree() {
+    for (let i = 0; i < this.input.length; i++) {
+      this.extendSuffixTree(i);
     }
+  }
 
-    return slowPtr.data;
+  extendSuffixTree(pos) {
+    this.size++;
+    this.remaining++;
+    let lastCreatedNode = null;
+
+    while (this.remaining > 0) {
+      if (this.activeLength === 0) {
+        this.activeEdge = pos;
+      }
+
+      if (!this.activeNode.children[this.input[this.activeEdge]]) {
+        this.activeNode.children[this.input[this.activeEdge]] = new Node();
+        if (lastCreatedNode != null) {
+          lastCreatedNode.suffixLink = this.activeNode;
+          lastCreatedNode = null;
+        }
+      } else {
+        let next = this.activeNode.children[this.input[this.activeEdge]];
+        if (this.canonizeSuffix(pos, next)) continue;
+      }
+
+      this.end = pos;
+      this.remaining--;
+
+      if (this.activeNode === this.root && this.activeLength > 0) {
+        this.activeLength--;
+        this.activeEdge = pos - this.remaining + 1;
+      } else {
+        this.activeNode =
+          this.activeNode.suffixLink || this.root;
+      }
+      this.canonizeSuffix(pos, this.activeNode);
+
+    }
+  }
+
+  canonizeSuffix(pos, node) {
+    if (this.activeLength >= node.children[this.input[this.activeEdge]].length) {
+      this.activeEdge += node.children[this.input[this.activeEdge]].length;
+      this.activeLength -= node.children[this.input[this.activeEdge]].length;
+      this.activeNode = node;
+      return true;
+    }
+    return false;
+  }
 }
 
-// Example of creating a linked list
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
+// Example usage
+const inputString = "banana";
+const suffixTree = new SuffixTree(inputString);
+console.log(suffixTree);
 
-// Call the function to find the middle element
-let middleElement = findMiddleElement(head);
-console.log('Middle element of the linked list: ' + middleElement);
