075439b11cb89eb5c40dfd5280a6832983a3f25b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:53:05 +0800

    3-21-2024 19:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,9 @@
-function findMajorityElement(arr) {
-    let map = {};
-    
-    for (let num of arr) {
-        map[num] = (map[num] || 0) + 1;
-        
-        if (map[num] > arr.length / 2) {
-            return num;
-        }
-    }
-    
-    return -1; // no majority element found
+function findSecondLargest(arr) {
+  arr.sort(function(a, b){return a - b});
+  return arr[arr.length - 2];
 }
 
-// Example
-let arr = [2, 2, 3, 3, 3, 2, 3];
-let majorityElement = findMajorityElement(arr);
-
-if (majorityElement !== -1) {
-    console.log(`The majority element in the array is: ${majorityElement}`);
-} else {
-    console.log(`No majority element found in the array.`);
-}
+// Example usage
+let array = [10, 5, 8, 20];
+let secondLargest = findSecondLargest(array);
+console.log("Second largest element: ", secondLargest);
