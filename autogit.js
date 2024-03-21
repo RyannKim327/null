2b1aae3df69427bd9344df5a9eebd0b42a6c61ff
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:35:01 +0800

    3-21-2024 19:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,37 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
-        }
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const sortedArrays = arr.map(item => [item]); // Convert each item in the array to an array of one element
+
+  while (sortedArrays.length > 1) {
+    const mergedArray = [];
+    for (let i = 0; i < sortedArrays.length; i += 2) {
+      if (i + 1 < sortedArrays.length) {
+        mergedArray.push(merge(sortedArrays[i], sortedArrays[i + 1]));
+      } else {
+        mergedArray.push(sortedArrays[i]);
+      }
     }
-    return count;
+    sortedArrays.length = 0;
+    sortedArrays.push(...mergedArray);
+  }
+
+  return sortedArrays[0];
 }
 
-// Example usage
-let str = "hello, world!";
-let char = "l";
-let charCount = countOccurrences(str, char);
-console.log(`The character '${char}' occurs ${charCount} times in the string.`);
+function merge(arr1, arr2) {
+  let merged = [];
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      merged.push(arr1.shift());
+    } else {
+      merged.push(arr2.shift());
+    }
+  }
+
+  return [...merged, ...arr1, ...arr2];
+}
+
+// Test the mergeSort function
+const arr = [38, 27, 43, 3, 9, 82, 10];
+console.log(mergeSort(arr)); // Output: [3, 9, 10, 27, 38, 43, 82]
