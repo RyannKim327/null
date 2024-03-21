be9208995e47919d1251a26c57b653912452c345
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:30:17 +0800

    3-21-2024 19:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,79 +1,21 @@
-class Node {
-  constructor() {
-    this.children = {};
-  }
+function isAnagram(str1, str2) {
+    // Clean the strings (remove spaces and convert to lowercase)
+    const cleanStr1 = str1.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const cleanStr2 = str2.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+    // Check if the lengths of the cleaned strings match
+    if (cleanStr1.length !== cleanStr2.length) {
+        return false;
+    }
+
+    // Sort the characters and compare the sorted strings
+    const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
 }
 
-class SuffixTree {
-  constructor(input) {
-    this.root = new Node();
-    this.input = input;
-    this.activeNode = this.root;
-    this.activeEdge = 0;
-    this.activeLength = 0;
-    this.remaining = 0;
-    this.end = 0;
-    this.splitEnd = 0;
-    this.size = -1;
-
-    this.buildSuffixTree();
-  }
-
-  buildSuffixTree() {
-    for (let i = 0; i < this.input.length; i++) {
-      this.extendSuffixTree(i);
-    }
-  }
-
-  extendSuffixTree(pos) {
-    this.size++;
-    this.remaining++;
-    let lastCreatedNode = null;
-
-    while (this.remaining > 0) {
-      if (this.activeLength === 0) {
-        this.activeEdge = pos;
-      }
-
-      if (!this.activeNode.children[this.input[this.activeEdge]]) {
-        this.activeNode.children[this.input[this.activeEdge]] = new Node();
-        if (lastCreatedNode != null) {
-          lastCreatedNode.suffixLink = this.activeNode;
-          lastCreatedNode = null;
-        }
-      } else {
-        let next = this.activeNode.children[this.input[this.activeEdge]];
-        if (this.canonizeSuffix(pos, next)) continue;
-      }
-
-      this.end = pos;
-      this.remaining--;
-
-      if (this.activeNode === this.root && this.activeLength > 0) {
-        this.activeLength--;
-        this.activeEdge = pos - this.remaining + 1;
-      } else {
-        this.activeNode =
-          this.activeNode.suffixLink || this.root;
-      }
-      this.canonizeSuffix(pos, this.activeNode);
-
-    }
-  }
-
-  canonizeSuffix(pos, node) {
-    if (this.activeLength >= node.children[this.input[this.activeEdge]].length) {
-      this.activeEdge += node.children[this.input[this.activeEdge]].length;
-      this.activeLength -= node.children[this.input[this.activeEdge]].length;
-      this.activeNode = node;
-      return true;
-    }
-    return false;
-  }
-}
-
-// Example usage
-const inputString = "banana";
-const suffixTree = new SuffixTree(inputString);
-console.log(suffixTree);
-
+// Test the function
+const str1 = 'listen';
+const str2 = 'silent';
+console.log(isAnagram(str1, str2)); // Output: true
