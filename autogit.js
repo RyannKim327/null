f261b6d7a38f3451cad6a552f67a38d46ace3bd6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:47:12 +0800

    3-21-2024 19:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,8 @@
-function areAnagrams(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase();
-    
-    // Check if the sorted versions of the strings are equal
-    return cleanStr1.split('').sort().join('') === cleanStr2.split('').sort().join('');
-}
+const removeVowels = (str) => {
+    return str.replace(/[aeiouAEIOU]/g, '');
+};
 
-// Test the function
-const string1 = "rail safety";
-const string2 = "fairy tales";
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example usage
+const inputString = "Hello World";
+const result = removeVowels(inputString);
+console.log(result); // Output: "Hll Wrld"
