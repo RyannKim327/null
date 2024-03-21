2e26aac876549ac2e62107c3c00d3464301b083a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:37:23 +0800

    3-21-2024 19:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,9 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+let array = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+
+let index = array.indexOf(elementToRemove);
+if (index > -1) {
+    array.splice(index, 1);
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    add(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    reverse() {
-        let prev = null;
-        let current = this.head;
-        while (current) {
-            let next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        this.head = prev;
-    }
-
-    printList() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
-}
-
-
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-
-console.log("Original linked list:");
-linkedList.printList();
-
-linkedList.reverse();
-
-console.log("\nReversed linked list:");
-linkedList.printList();
+console.log(array); // Output: [1, 2, 4, 5]
