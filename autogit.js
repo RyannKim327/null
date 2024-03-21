270cb86b0932e59223d23f48f1d13263c8f02057
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:50:47 +0800

    3-21-2024 19:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,70 +1,8 @@
-class BTreeNode {
-    constructor(t, leaf) {
-        this.t = t;
-        this.leaf = leaf;
-        this.keys = [];
-        this.children = [];
-    }
+// Generate a random number in a given range
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-class BTree {
-    constructor(t) {
-        this.root = new BTreeNode(t, true);
-        this.t = t;
-    }
-
-    insert(key) {
-        let root = this.root;
-
-        if (root.keys.length === (2 * this.t) - 1) {
-            let newRoot = new BTreeNode(this.t, false);
-            newRoot.children.push(root);
-            this.splitChild(newRoot, 0);
-            this.insertNonFull(newRoot, key);
-            this.root = newRoot;
-        } else {
-            this.insertNonFull(root, key);
-        }
-    }
-
-    insertNonFull(node, key) {
-        let i = node.keys.length - 1;
-
-        if (node.leaf) {
-            while (i >= 0 && key < node.keys[i]) {
-                node.keys[i + 1] = node.keys[i];
-                i--;
-            }
-
-            node.keys[i + 1] = key;
-        } else {
-            while (i >= 0 && key < node.keys[i]) {
-                i--;
-            }
-
-            i++;
-            if (node.children[i].keys.length === (2 * this.t) - 1) {
-                this.splitChild(node, i);
-                if (key > node.keys[i]) {
-                    i++;
-                }
-            }
-
-            this.insertNonFull(node.children[i], key);
-        }
-    }
-
-    splitChild(parent, index) {
-        let t = this.t;
-        let child = parent.children[index];
-        let newChild = new BTreeNode(t, child.leaf);
-
-        parent.keys.splice(index, 0, child.keys[t - 1]);
-        parent.children.splice(index + 1, 0, newChild);
-
-        newChild.keys = child.keys.splice(t);
-        if (!child.leaf) {
-            newChild.children = child.children.splice(t);
-        }
-    }
-}
+// Example: Generate a random number between 1 and 10
+const randomNumber = getRandomNumber(1, 10);
+console.log(randomNumber);
