4f32de7ce4ad7f91afdc3e8547a5867f4a510412
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 20:00:17 +0800

    3-21-2024 20:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,15 @@
-// Make a GET request to an API
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    // Log the data received from the API
-    console.log(data);
-  })
-  .catch(error => {
-    // Log any errors that occur during the request
-    console.error('Error:', error);
-  });
+function maxSubArray(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+    
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+    
+    return maxSoFar;
+}
+
+// Example
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArray(arr)); // Output: 6 (subarray [4, -1, 2, 1])
