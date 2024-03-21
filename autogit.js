345df12024928c3553cd9cc17f07d29c2531adbe
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:46:40 +0800

    3-21-2024 19:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,15 @@
-function merge(left, right) {
-    let result = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
+function countOccurrences(str, word) {
+    // Use regex to split the string by non-word characters and filter out empty strings
+    const wordsArray = str.split(/\b\W+\b/).filter(Boolean);
 
-    while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-            result.push(left[leftIndex]);
-            leftIndex++;
-        } else {
-            result.push(right[rightIndex]);
-            rightIndex++;
-        }
-    }
+    // Use the filter method to filter out the words that match the target word
+    const matchingWords = wordsArray.filter((w) => w.toLowerCase() === word.toLowerCase());
 
-    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    return matchingWords.length;
 }
 
-function mergeSortIterative(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+const text = "Hello world. This is a sample text with word occurrences. Word word word.";
+const wordToCount = "word";
+const occurrences = countOccurrences(text, wordToCount);
 
-    let sortedArr = arr.map(item => [item]);
-
-    while (sortedArr.length > 1) {
-        let nextSortArr = [];
-        for (let i = 0; i < sortedArr.length; i += 2) {
-            if (i + 1 < sortedArr.length) {
-                nextSortArr.push(merge(sortedArr[i], sortedArr[i + 1]));
-            } else {
-                nextSortArr.push(sortedArr[i]);
-            }
-        }
-        sortedArr = nextSortArr;
-    }
-
-    return sortedArr[0];
-}
-
-// Example usage
-const arr = [5, 3, 8, 6, 2, 7, 1, 4];
-console.log(mergeSortIterative(arr));
+console.log(`The word "${wordToCount}" occurs ${occurrences} times in the text.`);
