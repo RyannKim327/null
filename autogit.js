426e365f353bf80a6ff52fe15d23ee7883670fe6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:31:43 +0800

    3-21-2024 19:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,21 @@
-function longestCommonSubstring(str1, str2) {
-    const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
-    let maxLength = 0;
-    let endIndex = 0;
-
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
     }
-
-    return str1.slice(endIndex - maxLength + 1, endIndex + 1);
+    return arr;
 }
 
-// Example usage
-const str1 = 'helloworld';
-const str2 = 'hellojsworld';
-const result = longestCommonSubstring(str1, str2);
-console.log(result); // Output: 'hello'
+// Example usage:
+const arr = [64, 25, 12, 22, 11];
+const sortedArr = selectionSort(arr);
+console.log(sortedArr); // Output: [11, 12, 22, 25, 64]
