1fb4b2171dae71a94715fb38c563baf3762ba651
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:44:59 +0800

    3-21-2024 19:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,17 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
+function reverseWords(str) {
+    // Split the string into an array of words
+    let words = str.split(" ");
+
+    // Reverse the array of words
+    words = words.reverse();
+
+    // Join the array back into a string
+    let reversedStr = words.join(" ");
+
+    return reversedStr;
 }
 
-// Test the reverseString function
-const originalString = "Hello, world!";
-const reversedString = reverseString(originalString);
-
-console.log("Original string:", originalString);
-console.log("Reversed string:", reversedString);
+// Test the function
+let originalStr = "Hello world!";
+let reversedStr = reverseWords(originalStr);
+console.log(reversedStr); // Output: "world! Hello"
