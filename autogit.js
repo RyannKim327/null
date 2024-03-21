224d80b9a877f489c3f36614989fad8ca9ccd6a3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:35:11 +0800

    3-21-2024 19:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,50 @@
-function mergeSort(arr) {
-  if (arr.length <= 1) return arr;
-
-  const sortedArrays = arr.map(item => [item]); // Convert each item in the array to an array of one element
-
-  while (sortedArrays.length > 1) {
-    const mergedArray = [];
-    for (let i = 0; i < sortedArrays.length; i += 2) {
-      if (i + 1 < sortedArrays.length) {
-        mergedArray.push(merge(sortedArrays[i], sortedArrays[i + 1]));
-      } else {
-        mergedArray.push(sortedArrays[i]);
-      }
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
     }
-    sortedArrays.length = 0;
-    sortedArrays.push(...mergedArray);
-  }
 
-  return sortedArrays[0];
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    depthFirstSearch(startingVertex) {
+        const visited = {};
+        const result = [];
+
+        const dfs = (vertex) => {
+            if (!vertex) {
+                return null;
+            }
+            visited[vertex] = true;
+            result.push(vertex);
+
+            this.adjacencyList[vertex].forEach(neighbor => {
+                if (!visited[neighbor]) {
+                    return dfs(neighbor);
+                }
+            });
+        };
+
+        dfs(startingVertex);
+
+        return result;
+    }
 }
 
-function merge(arr1, arr2) {
-  let merged = [];
-  while (arr1.length && arr2.length) {
-    if (arr1[0] < arr2[0]) {
-      merged.push(arr1.shift());
-    } else {
-      merged.push(arr2.shift());
-    }
-  }
-
-  return [...merged, ...arr1, ...arr2];
-}
-
-// Test the mergeSort function
-const arr = [38, 27, 43, 3, 9, 82, 10];
-console.log(mergeSort(arr)); // Output: [3, 9, 10, 27, 38, 43, 82]
+// Example Usage
+const graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+console.log(graph.depthFirstSearch('A')); // Output: ['A', 'B', 'D', 'C']
