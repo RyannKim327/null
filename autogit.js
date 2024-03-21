7adb01607d0353dca808582b1ad0632a75a66c91
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:38:00 +0800

    3-21-2024 19:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,6 @@
-function mergeSort(arr) {
-    const n = arr.length;
-    for (let size = 1; size < n; size *= 2) {
-        for (let leftStart = 0; leftStart < n - 1; leftStart += 2 * size) {
-            const mid = Math.min(leftStart + size - 1, n - 1);
-            const rightEnd = Math.min(leftStart + 2 * size - 1, n - 1);
-            merge(arr, leftStart, mid, rightEnd);
-        }
-    }
-    return arr;
-}
+let array = [1, 2, 3, 4, 5];
+let indexToRemove = 2; // Index of the element you want to remove
 
-function merge(arr, leftStart, mid, rightEnd) {
-    const left = arr.slice(leftStart, mid + 1);
-    const right = arr.slice(mid + 1, rightEnd + 1);
-    let i = 0, j = 0, k = leftStart;
-    
-    while (i < left.length && j < right.length) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
-            i++;
-        } else {
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-    
-    while (i < left.length) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
-    
-    while (j < right.length) {
-        arr[k] = right[j];
-        j++;
-        k++;
-    }
-}
+array.splice(indexToRemove, 1);
 
-// Example usage
-const arr = [38, 27, 43, 3, 9, 82, 10];
-console.log(mergeSort(arr)); // Output: [3, 9, 10, 27, 38, 43, 82]
+console.log(array); // Output: [1, 2, 4, 5]
