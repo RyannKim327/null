31da8979b8548041fe46f0c58720fb8915995035
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:34:52 +0800

    3-21-2024 19:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,15 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Example usage
+let str = "hello, world!";
+let char = "l";
+let charCount = countOccurrences(str, char);
+console.log(`The character '${char}' occurs ${charCount} times in the string.`);
