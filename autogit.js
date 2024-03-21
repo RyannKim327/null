1ab8c570c1e8f23268ecd05a6af92ff66e0044f4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:56:29 +0800

    3-21-2024 19:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,14 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    str = str.toLowerCase().replace(/[\W_]/g, '');
+    
+    // Compare the original string with its reverse
+    return str === str.split('').reverse().join('');
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+// Test the function
+const string1 = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(string1)); // Output: true
 
-  insert(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  isPalindrome() {
-    let values = [];
-    let current = this.head;
-
-    // Push values of linked list into an array
-    while (current) {
-      values.push(current.value);
-      current = current.next;
-    }
-
-    // Use two pointers approach to compare values
-    let start = 0;
-    let end = values.length - 1;
-
-    while (start < end) {
-      if (values[start] !== values[end]) {
-        return false;
-      }
-      start++;
-      end--;
-    }
-
-    return true;
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.insert(1);
-linkedList.insert(2);
-linkedList.insert(3);
-linkedList.insert(2);
-linkedList.insert(1);
-
-console.log(linkedList.isPalindrome()); // Output: true
+const string2 = "Hello, world!";
+console.log(isPalindrome(string2)); // Output: false
