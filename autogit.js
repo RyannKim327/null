23aa4bb203f27e84f697beacf45b3744d2c2bb12
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:52:31 +0800

    3-21-2024 19:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,97 @@
-const str = 'Hello, World!';
-const substring = 'Hello';
-
-if (str.includes(substring)) {
-  console.log('String contains the substring');
-} else {
-  console.log('String does not contain the substring');
+class SkipListNode {
+    constructor(value, level) {
+        this.value = value;
+        this.forward = new Array(level + 1).fill(null);
+    }
 }
-const str = 'Hello, World!';
-const substring = 'World';
 
-if (str.indexOf(substring) !== -1) {
-  console.log('String contains the substring');
-} else {
-  console.log('String does not contain the substring');
+class SkipList {
+    constructor(maxLevel, p) {
+        this.maxLevel = maxLevel;
+        this.p = p;
+        this.level = 0;
+        this.head = new SkipListNode(-1, maxLevel);
+    }
+
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.p && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (!current || current.value != value) {
+            let newLevel = this.randomLevel();
+
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.head;
+                }
+                this.level = newLevel;
+            }
+
+            let newNode = new SkipListNode(value, newLevel);
+
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.forward[i] = update[i].forward[i];
+                update[i].forward[i] = newNode;
+            }
+        }
+    }
+
+    search(value) {
+        let current = this.head;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+        }
+
+        current = current.forward[0];
+
+        if (current && current.value === value) {
+            return current;
+        }
+
+        return null;
+    }
+
+    delete(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.forward[i] && current.forward[i].value < value) {
+                current = current.forward[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.forward[0];
+
+        if (current && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].forward[i] !== current) break;
+                update[i].forward[i] = current.forward[i];
+            }
+
+            while (this.level > 0 && this.head.forward[this.level] === null) {
+                this.level--;
+            }
+        }
+    }
 }
