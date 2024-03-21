5aa230b189c38856dbdc4dd6eaaa4d7c9727b25e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:45:37 +0800

    3-21-2024 19:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,43 @@
-let userInput = prompt("Please enter something:");
-console.log("You entered: " + userInput);
+class Stack {
+    constructor() {
+        this.stackArray = [];
+    }
+
+    // Push an element onto the stack
+    push(element) {
+        this.stackArray.push(element);
+    }
+
+    // Remove and return the top element of the stack
+    pop() {
+        if (this.isEmpty()) {
+            return "Stack is empty";
+        }
+        return this.stackArray.pop();
+    }
+
+    // Return the top element of the stack without removing it
+    peek() {
+        return this.stackArray[this.stackArray.length - 1];
+    }
+
+    // Check if the stack is empty
+    isEmpty() {
+        return this.stackArray.length === 0;
+    }
+
+    // Return the size of the stack
+    size() {
+        return this.stackArray.length;
+    }
+}
+
+// Example Usage
+const myStack = new Stack();
+myStack.push(5);
+myStack.push(10);
+myStack.push(15);
+
+console.log(myStack.pop()); // 15
+console.log(myStack.peek()); // 10
+console.log(myStack.size()); // 2
