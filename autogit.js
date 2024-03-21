d2cd5aea0352c371b8244047df57a57f432a73cd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:49:40 +0800

    3-21-2024 19:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,8 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, ''); // The regular expression [aeiouAEIOU] matches all vowels (both lower and uppercase) globally in the string and replaces them with an empty string.
 }
 
-function findIntersection(list1, list2) {
-  let set = new Set();
+let inputString = "Hello, World!";
+let result = removeVowels(inputString);
 
-  let current1 = list1;
-  while (current1) {
-    set.add(current1);
-    current1 = current1.next;
-  }
-
-  let current2 = list2;
-  while (current2) {
-    if (set.has(current2)) {
-      return current2;
-    }
-    current2 = current2.next;
-  }
-
-  return null;
-}
-
-// Example usage
-// Construct linked lists
-let list1 = new Node(1);
-list1.next = new Node(2);
-let intersectionNode = new Node(3);
-list1.next.next = intersectionNode;
-
-let list2 = new Node(4);
-list2.next = intersectionNode;
-
-let intersection = findIntersection(list1, list2);
-console.log("Intersection node value: " + intersection.value); // Output: Intersection node value: 3
+console.log(result); // Output: "Hll, Wrld!"
