3e3a8f7a3652649f5ed5805a50d481809a69cedd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:54:33 +0800

    3-21-2024 19:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,47 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 0) return '';
-    
-    let prefix = '';
-    for (let i = 0; i < strs[0].length; i++) {
-        const char = strs[0][i];
-        if (strs.some(str => str[i] !== char)) {
-            break;
-        }
-        prefix += char;
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    if (this.items.length === 0) {
+      return "Underflow";
     }
-    
-    return prefix;
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  printStack() {
+    let stackString = "";
+    for (let i = 0; i < this.items.length; i++) {
+      stackString += this.items[i] + " ";
+    }
+    console.log("Stack: " + stackString);
+  }
 }
-const strings = ["flower", "flour", "florida"];
-const commonPrefix = longestCommonPrefix(strings);
-console.log(commonPrefix); // Output: "flo"
+
+// Usage
+const stack = new Stack();
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+stack.printStack(); // Output: Stack: 1 2 3
+
+console.log("Popped element: " + stack.pop()); // Output: Popped element: 3
+
+console.log("Top element: " + stack.peek()); // Output: Top element: 2
+
+console.log("Is the stack empty: " + stack.isEmpty()); // Output: Is the stack empty: false
