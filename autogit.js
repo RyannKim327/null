d8e656c14cf466d0deae019c544ff1a7e6a0e177
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:51:54 +0800

    3-21-2024 19:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,9 @@
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
-    }
-}
+const axios = require('axios');
 
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-
-    insert(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                node.children[char] = new TrieNode();
-            }
-            node = node.children[char];
-        }
-        node.isEndOfWord = true;
-    }
-
-    search(word) {
-        let node = this.root;
-        for (let char of word) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return node.isEndOfWord;
-    }
-
-    startsWith(prefix) {
-        let node = this.root;
-        for (let char of prefix) {
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return true;
-    }
-}
-
-// Usage
-const trie = new Trie();
-trie.insert("apple");
-trie.insert("app");
-console.log(trie.search("apple"));   // Output: true
-console.log(trie.search("app"));     // Output: true
-console.log(trie.startsWith("app")); // Output: true
-console.log(trie.search("ap"));      // Output: false
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
