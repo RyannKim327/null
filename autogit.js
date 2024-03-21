24e3b67972b2c34a51a2e98b984ac68596075afe
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:37:42 +0800

    3-21-2024 19:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,53 @@
-function bubbleSort(arr) {
-    var len = arr.length;
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+class Graph {
+  constructor(vertices, edges) {
+    this.vertices = vertices;
+    this.edges = edges;
+  }
+
+  bellmanFord(source) {
+    let distances = {};
+  
+    // Initialize distances from source to all other vertices as Infinity
+    for (let vertex of this.vertices) {
+      distances[vertex] = Infinity;
     }
-    return arr;
+  
+    distances[source] = 0;
+  
+    // Relax all edges |V| - 1 times
+    for (let i = 0; i < this.vertices.length - 1; i++) {
+      for (let [start, end, weight] of this.edges) {
+        if (distances[start] + weight < distances[end]) {
+          distances[end] = distances[start] + weight;
+        }
+      }
+    }
+  
+    // Check for negative weight cycles
+    for (let [start, end, weight] of this.edges) {
+      if (distances[start] + weight < distances[end]) {
+        console.log("Graph contains negative weight cycle");
+        return;
+      }
+    }
+  
+    return distances;
+  }
 }
 
-var arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + bubbleSort(arr));
+// Example usage:
+let vertices = ["A", "B", "C", "D", "E"];
+let edges = [
+  ["A", "C", 3],
+  ["A", "D", 2],
+  ["B", "A", 2],
+  ["B", "C", 4],
+  ["C", "D", 1],
+  ["D", "E", 3],
+  ["E", "B", -5]
+];
+
+let graph = new Graph(vertices, edges);
+let shortestDistances = graph.bellmanFord("A");
+
+console.log(shortestDistances);
