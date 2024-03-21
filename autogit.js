266b4d7b07cbf3a429729f6e4e552e4e3f3ef99c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:40:56 +0800

    3-21-2024 19:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,72 @@
+class BiDirectionalSearch {
+    constructor(startNode, endNode) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.frontierStart = [startNode];
+        this.frontierEnd = [endNode];
+        this.visitedStart = new Set();
+        this.visitedEnd = new Set();
+    }
+
+    search() {
+        while (this.frontierStart.length > 0 && this.frontierEnd.length > 0) {
+            const result = this.bfs(this.frontierStart, this.visitedStart, this.visitedEnd);
+            if (result) {
+                return result;
+            }
+
+            result = this.bfs(this.frontierEnd, this.visitedEnd, this.visitedStart);
+            if (result) {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+    bfs(frontier, visited, targetVisited) {
+        const node = frontier.shift();
+
+        if (targetVisited.has(node)) {
+            return node;
+        }
+
+        visited.add(node);
+
+        for (let neighbor of node.neighbors) {
+            if (!visited.has(neighbor)) {
+                frontier.push(neighbor);
+            }
+        }
+
+        return null;
+    }
+}
+
+// Usage
 class Node {
-    constructor(value, next = null) {
+    constructor(value) {
         this.value = value;
-        this.next = next;
+        this.neighbors = [];
     }
 }
 
-function findMiddleElement(head) {
-    let slow = head;
-    let fast = head;
+const startNode = new Node('A');
+const endNode = new Node('B');
 
-    while (fast !== null && fast.next !== null) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
+startNode.neighbors.push(new Node('C'));
+startNode.neighbors.push(new Node('D'));
+startNode.neighbors.push(new Node('E'));
 
-    return slow.value;
+endNode.neighbors.push(new Node('F'));
+endNode.neighbors.push(new Node('G'));
+endNode.neighbors.push(new Node('C'));
+
+const biDirectionalSearch = new BiDirectionalSearch(startNode, endNode);
+const result = biDirectionalSearch.search();
+
+if (result) {
+    console.log('Path found!');
+} else {
+    console.log('Path not found.');
 }
-
-// Example usage
-const node1 = new Node(1);
-const node2 = new Node(2);
-const node3 = new Node(3);
-const node4 = new Node(4);
-const node5 = new Node(5);
-
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-
-console.log(findMiddleElement(node1)); // Output: 3
