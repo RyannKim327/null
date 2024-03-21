c844ea3d5916a11c264d3befb4d3abd7dfac5c87
RyannKim327 <rksesgundo123@gmail.com>
2024-03-21 19:52:00 +0800

    3-21-2024 19:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,20 @@
-const axios = require('axios');
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let current = array[i];
+    let j = i - 1;
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+
+    array[j + 1] = current;
+  }
+
+  return array;
+}
+
+// Example usage:
+const unsortedArray = [7, 3, 1, 9, 5];
+const sortedArray = insertionSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 3, 5, 7, 9]
