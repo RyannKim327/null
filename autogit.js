104b813f2d0c42dab42c42bd4520cc0b1067dfbd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:42:35 +0800

    3-22-2024 16:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,9 @@
-// Sample graph represented as an adjacency list
-const graph = {
-  A: ['B', 'C'],
-  B: ['D', 'E'],
-  C: ['F'],
-  D: [],
-  E: ['G'],
-  F: [],
-  G: []
-};
-
-function depthLimitedSearch(startNode, graph, depthLimit) {
-  let stack = [{ node: startNode, depth: 0 }];
-  
-  while (stack.length > 0) {
-    const { node, depth } = stack.pop();
-    console.log(`Visiting node ${node} at depth ${depth}`);
-    
-    if (depth >= depthLimit) {
-      continue;
-    }
-    
-    const neighbors = graph[node];
-    for (let i = neighbors.length - 1; i >= 0; i--) {
-      const neighbor = neighbors[i];
-      stack.push({ node: neighbor, depth: depth + 1 });
-    }
-  }
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Perform depth-limited search starting from node 'A' with a depth limit of 2
-depthLimitedSearch('A', graph, 2);
+// Example
+let originalString = "Hello, World!";
+let stringWithoutVowels = removeVowels(originalString);
+
+console.log(stringWithoutVowels);  // Output: "Hll, Wrld!"
