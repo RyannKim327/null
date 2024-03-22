3ecccce4c0a152092a03912071e8b66e405e2690
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:43:55 +0800

    3-22-2024 16:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,31 @@
-let str = "123";
-let num = parseInt(str);
+function isPalindrome(str) {
+    let i = 0;
+    let j = str.length - 1;
 
-console.log(num); // Output: 123
-let str = "123";
-let num = +str;
+    while (i < j) {
+        // Ignore non-alphanumeric characters
+        while (!isAlphanumeric(str[i]) && i < j) {
+            i++;
+        }
+        while (!isAlphanumeric(str[j]) && i < j) {
+            j--;
+        }
 
-console.log(num); // Output: 123
+        if (str[i].toLowerCase() !== str[j].toLowerCase()) {
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+function isAlphanumeric(char) {
+    return /[a-zA-Z0-9]/.test(char);
+}
+
+// Test cases
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
+console.log(isPalindrome("race a car")); // false
