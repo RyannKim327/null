e7ab12c018ff931de135e1d3d4e039e49ee4799c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:58:44 +0800

    3-22-2024 17:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,69 @@
-// Async function to fetch data from a remote server in Android
-async function fetchData() {
-  try {
-    let response = await fetch('https://api.example.com/data');
-    let data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return null;
+class BTreeNode {
+  constructor(order, leaf = true) {
+    this.order = order;
+    this.keys = [];
+    this.children = [];
+    this.leaf = leaf;
   }
 }
 
-// Function to initiate the async task in Android
-function startAsyncTask() {
-  fetchData()
-    .then((data) => {
-      // Handle the data received from the server
-      console.log('Data received:', data);
+class BTree {
+  constructor(order) {
+    this.root = new BTreeNode(order);
+    this.order = order;
+  }
 
-      // Add your Android-specific code here to process the data
-    })
-    .catch((error) => {
-      console.error('Error in async task:', error);
-    });
+  insert(key) {
+    let node = this.root;
+
+    if (node.keys.length === 2 * this.order - 1) {
+      let newRoot = new BTreeNode(this.order, false);
+      newRoot.children.push(node);
+      this.splitChild(newRoot, 0);
+      this.root = newRoot;
+      this.insertNonFull(newRoot, key);
+    } else {
+      this.insertNonFull(node, key);
+    }
+  }
+
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+
+    if (node.leaf) {
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i + 1] = node.keys[i];
+        i--;
+      }
+
+      node.keys[i + 1] = key;
+    } else {
+      while (i >= 0 && key < node.keys[i]) {
+        i--;
+      }
+
+      i++;
+      if (node.children[i].keys.length === 2 * this.order - 1) {
+        this.splitChild(node, i);
+        if (key > node.keys[i]) {
+          i++;
+        }
+      }
+
+      this.insertNonFull(node.children[i], key);
+    }
+  }
+
+  splitChild(parent, index) {
+    let y = parent.children[index];
+    let z = new BTreeNode(this.order, y.leaf);
+
+    parent.keys.splice(index, 0, y.keys[this.order - 1]);
+    parent.children.splice(index + 1, 0, z);
+
+    z.keys = y.keys.splice(this.order);
+    z.children = y.children.splice(this.order);
+
+    y.keys.pop();
+  }
 }
-
-// Call the startAsyncTask function when the page loads or some event occurs
-startAsyncTask();
