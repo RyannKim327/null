84162ad19739557b433538b1187744eecef99fa0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:48:03 +0800

    3-22-2024 16:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,11 @@
-let userInput = prompt("Please enter your name:");
-alert("Hello, " + userInput + "! Welcome to our website!");
+function findStringLength(str) {
+    let count = 0;
+    for (let i = 0; str[i] !== undefined; i++) {
+        count++;
+    }
+    return count;
+}
+
+let str = "Hello, World!";
+let length = findStringLength(str);
+console.log(length); // Output: 13
