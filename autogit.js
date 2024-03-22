2ec32ae3cc300e03561e8b6eebf72e8958cae61d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:56:08 +0800

    3-22-2024 17:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,57 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    addNode(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
+function createLPSArray(pattern) {
+    const lps = [0];
+    let length = 0;
+    let i = 1;
+    
+    while (i < pattern.length) {
+        if (pattern[i] === pattern[length]) {
+            length++;
+            lps[i] = length;
+            i++;
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
+            if (length !== 0) {
+                length = lps[length - 1];
+            } else {
+                lps[i] = 0;
+                i++;
             }
-            current.next = newNode;
         }
     }
-
-    findMiddle() {
-        let slow = this.head;
-        let fast = this.head;
-
-        while (fast && fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow.value;
-    }
+    
+    return lps;
 }
 
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
+function KMPSearch(text, pattern) {
+    const lps = createLPSArray(pattern);
+    const result = [];
+    
+    let i = 0;
+    let j = 0;
+    
+    while (i < text.length) {
+        if (pattern[j] === text[i]) {
+            i++;
+            j++;
+        }
+        
+        if (j === pattern.length) {
+            result.push(i - j);
+            j = lps[j - 1];
+        } else if (i < text.length && pattern[j] !== text[i]) {
+            if (j !== 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+    
+    return result;
+}
 
-// Find the middle element
-const middleElement = linkedList.findMiddle();
-console.log("Middle element:", middleElement);
+// Example usage
+const text = "ABABDABACDABABCABAB";
+const pattern = "ABABCABAB";
+const result = KMPSearch(text, pattern);
+
+console.log(result); // Output: [10]
