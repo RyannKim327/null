c7e64956527603327f8398cef845a232dd7baa4c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 18:01:31 +0800

    3-22-2024 18:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,78 +1,58 @@
-class BTreeNode {
-    constructor(t, leaf) {
-        this.t = t; // Min degree
-        this.keys = [];
-        this.children = leaf ? null : [];
-        this.isLeaf = leaf;
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    // Add element to the top of the stack
+    push(element) {
+        this.items.push(element);
+    }
+
+    // Remove and return the top element from the stack
+    pop() {
+        if (this.items.length === 0) {
+            return "Underflow";
+        }
+        return this.items.pop();
+    }
+
+    // Return the top element without removing it
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    // Check if the stack is empty
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    // Return the size of the stack
+    size() {
+        return this.items.length;
+    }
+
+    // Print the stack elements
+    printStack() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++) {
+            str += this.items[i] + " ";
+        }
+        return str;
     }
 }
 
-class BTree {
-    constructor(t) {
-        this.root = null;
-        this.t = t;
-    }
+// Example usage of the Stack class
+const stack = new Stack();
 
-    // Insert a key into the tree
-    insert(key) {
-        if (this.root === null) {
-            this.root = new BTreeNode(this.t, true);
-            this.root.keys.push(key);
-        } else {
-            if (this.root.keys.length === 2 * this.t - 1) {
-                let newRoot = new BTreeNode(this.t, false);
-                newRoot.children.push(this.root);
-                this.splitChild(newRoot, 0);
-                this.root = newRoot;
-            }
-            this.insertNonFull(this.root, key);
-        }
-    }
+console.log(stack.isEmpty()); // true
 
-    // Insert a key into a non-full node
-    insertNonFull(node, key) {
-        let i = node.keys.length - 1;
-        if (node.isLeaf) {
-            while (i >= 0 && key < node.keys[i]) {
-                node.keys[i + 1] = node.keys[i];
-                i--;
-            }
-            node.keys[i + 1] = key;
-        } else {
-            while (i >= 0 && key < node.keys[i]) {
-                i--;
-            }
-            i++;
-            if (node.children[i].keys.length === 2 * this.t - 1) {
-                this.splitChild(node, i);
-                if (key > node.keys[i]) {
-                    i++;
-                }
-            }
-            this.insertNonFull(node.children[i], key);
-        }
-    }
+stack.push(10);
+stack.push(20);
+stack.push(30);
 
-    // Split child of a node
-    splitChild(node, i) {
-        let t = this.t;
-        let y = node.children[i];
-        let z = new BTreeNode(t, y.isLeaf);
-        node.children.splice(i + 1, 0, z);
-        node.keys.splice(i, 0, y.keys[t - 1]);
+console.log(stack.printStack()); // 10 20 30
 
-        z.keys = y.keys.splice(t, t - 1);
+console.log(stack.peek()); // 30
 
-        if (!y.isLeaf) {
-            z.children = y.children.splice(t, t);
-        }
-    }
-}
-
-// Usage
-const btree = new BTree(3); // Create a B-tree with min degree 3
-btree.insert(10);
-btree.insert(20);
-btree.insert(5);
-btree.insert(6);
-btree.insert(12);
+console.log(stack.pop()); // 30
+console.log(stack.printStack()); // 10 20
