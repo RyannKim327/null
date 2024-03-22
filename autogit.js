4d590fce5f9e9f9f1a3918824c6edc57b121a168
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:44:10 +0800

    3-22-2024 16:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,62 @@
-function isPalindrome(str) {
-    let i = 0;
-    let j = str.length - 1;
-
-    while (i < j) {
-        // Ignore non-alphanumeric characters
-        while (!isAlphanumeric(str[i]) && i < j) {
-            i++;
+function biDirectionalSearch(graph, startNode, endNode) {
+    // Initialize the forward and backward queues and visited sets
+    let forwardQueue = [startNode];
+    let backwardQueue = [endNode];
+    
+    let forwardVisited = new Set();
+    let backwardVisited = new Set();
+    
+    forwardVisited.add(startNode);
+    backwardVisited.add(endNode);
+    
+    while (forwardQueue.length && backwardQueue.length) {
+        // Perform forward search
+        let currentNode = forwardQueue.shift();
+        
+        // Check if the current node is present in both directions
+        if (backwardVisited.has(currentNode)) {
+            return currentNode; // If found, return the intersection node
         }
-        while (!isAlphanumeric(str[j]) && i < j) {
-            j--;
+        
+        graph[currentNode].forEach(neighbour => {
+            if (!forwardVisited.has(neighbour)) {
+                forwardVisited.add(neighbour);
+                forwardQueue.push(neighbour);
+            }
+        });
+        
+        // Perform backward search
+        currentNode = backwardQueue.shift();
+        
+        // Check if the current node is present in both directions
+        if (forwardVisited.has(currentNode)) {
+            return currentNode; // If found, return the intersection node
         }
-
-        if (str[i].toLowerCase() !== str[j].toLowerCase()) {
-            return false;
-        }
-
-        i++;
-        j--;
+        
+        graph[currentNode].forEach(neighbour => {
+            if (!backwardVisited.has(neighbour)) {
+                backwardVisited.add(neighbour);
+                backwardQueue.push(neighbour);
+            }
+        });
     }
-
-    return true;
+    
+    return null; // If no intersection node found
 }
 
-function isAlphanumeric(char) {
-    return /[a-zA-Z0-9]/.test(char);
-}
+// Example graph representation
+const graph = {
+    A: ['B', 'C'],
+    B: ['A', 'D'],
+    C: ['A', 'E'],
+    D: ['B', 'F'],
+    E: ['C', 'G'],
+    F: ['D'],
+    G: ['E']
+};
 
-// Test cases
-console.log(isPalindrome("A man, a plan, a canal, Panama")); // true
-console.log(isPalindrome("race a car")); // false
+const startNode = 'A';
+const endNode = 'G';
+
+const intersectionNode = biDirectionalSearch(graph, startNode, endNode);
+console.log('Intersection node:', intersectionNode);
