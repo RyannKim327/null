e54acafa17644b852e99ca40f2e9c554add58353
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:57:01 +0800

    3-22-2024 17:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,41 @@
-// Taking user input using prompt
-let userInput = prompt("Enter something:");
+class Node {
+    constructor() {
+        this.children = {};
+        this.suffixLink = null;
+    }
+}
+class SuffixTree {
+    constructor(input) {
+        this.root = new Node();
+        this.input = input;
+        this.buildSuffixTree();
+    }
 
-// Displaying the user input
-console.log("You entered: " + userInput);
+    buildSuffixTree() {
+        for (let i = 0; i < this.input.length; i++) {
+            this.addSuffix(this.input.substring(i));
+        }
+    }
+
+    addSuffix(suffix) {
+        let current = this.root;
+        for (let i = 0; i < suffix.length; i++) {
+            let char = suffix.charAt(i);
+            if (!current.children[char]) {
+                current.children[char] = new Node();
+            }
+            current = current.children[char];
+        }
+    }
+}
+searchSubString(substring) {
+    let current = this.root;
+    for (let i = 0; i < substring.length; i++) {
+        let char = substring.charAt(i);
+        if (!current.children[char]) {
+            return false;
+        }
+        current = current.children[char];
+    }
+    return true;
+}
