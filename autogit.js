96f76c5b3a900f725d82311aa0736ca8111e9ca5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:56:48 +0800

    3-22-2024 17:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,5 @@
-function findFirstRepeatedChar(str) {
-    let charSet = new Set();
-    
-    for (let char of str) {
-        if (charSet.has(char)) {
-            return char;
-        } else {
-            charSet.add(char);
-        }
-    }
-    
-    return null; // If no repeated character is found
-}
+// Taking user input using prompt
+let userInput = prompt("Enter something:");
 
-let str = "hello world";
-let repeatedChar = findFirstRepeatedChar(str);
-
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("There are no repeated characters in the string.");
-}
+// Displaying the user input
+console.log("You entered: " + userInput);
