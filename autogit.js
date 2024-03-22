015a532fb3e02b2c3ed3ebd3c972c1ac39156545
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:43:33 +0800

    3-22-2024 16:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,7 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
+const cron = require('node-cron');
 
-function findNthNodeFromEnd(head, n) {
-  let slow = head;
-  let fast = head;
-
-  // Move fast pointer n positions ahead
-  for (let i = 0; i < n; i++) {
-    if (fast === null) {
-      return null; // n is greater than the length of the list
-    }
-    fast = fast.next;
-  }
-
-  // Move both pointers until fast reaches the end
-  while (fast !== null) {
-    slow = slow.next;
-    fast = fast.next;
-  }
-
-  return slow;
-}
-
-// Example usage
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-let n = 2;
-let nthNodeFromEnd = findNthNodeFromEnd(head, n);
-
-if (nthNodeFromEnd !== null) {
-  console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.value}`);
-} else {
-  console.log(`List doesn't have ${n} nodes.`);
-}
+// Schedule a task to run every minute
+cron.schedule('* * * * *', () => {
+  console.log('This task runs every minute');
+});
+npm install node-cron
