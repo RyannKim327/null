054d0c9032e5601abe084d96ce80c895787dcc98
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:56:14 +0800

    3-22-2024 17:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,9 @@
-function createLPSArray(pattern) {
-    const lps = [0];
-    let length = 0;
-    let i = 1;
-    
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[length]) {
-            length++;
-            lps[i] = length;
-            i++;
-        } else {
-            if (length !== 0) {
-                length = lps[length - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
-    
-    return lps;
+function findCommonElements(arr1, arr2) {
+    return arr1.filter(element => arr2.includes(element));
 }
 
-function KMPSearch(text, pattern) {
-    const lps = createLPSArray(pattern);
-    const result = [];
-    
-    let i = 0;
-    let j = 0;
-    
-    while (i < text.length) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-        
-        if (j === pattern.length) {
-            result.push(i - j);
-            j = lps[j - 1];
-        } else if (i < text.length && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-    
-    return result;
-}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-// Example usage
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const result = KMPSearch(text, pattern);
-
-console.log(result); // Output: [10]
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
