e35f2658bae218ebdc59695219fba0e33041188d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 16:44:18 +0800

    3-22-2024 16:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,67 @@
-function biDirectionalSearch(graph, startNode, endNode) {
-    // Initialize the forward and backward queues and visited sets
-    let forwardQueue = [startNode];
-    let backwardQueue = [endNode];
-    
-    let forwardVisited = new Set();
-    let backwardVisited = new Set();
-    
-    forwardVisited.add(startNode);
-    backwardVisited.add(endNode);
-    
-    while (forwardQueue.length && backwardQueue.length) {
-        // Perform forward search
-        let currentNode = forwardQueue.shift();
-        
-        // Check if the current node is present in both directions
-        if (backwardVisited.has(currentNode)) {
-            return currentNode; // If found, return the intersection node
-        }
-        
-        graph[currentNode].forEach(neighbour => {
-            if (!forwardVisited.has(neighbour)) {
-                forwardVisited.add(neighbour);
-                forwardQueue.push(neighbour);
-            }
-        });
-        
-        // Perform backward search
-        currentNode = backwardQueue.shift();
-        
-        // Check if the current node is present in both directions
-        if (forwardVisited.has(currentNode)) {
-            return currentNode; // If found, return the intersection node
-        }
-        
-        graph[currentNode].forEach(neighbour => {
-            if (!backwardVisited.has(neighbour)) {
-                backwardVisited.add(neighbour);
-                backwardQueue.push(neighbour);
-            }
-        });
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    
-    return null; // If no intersection node found
 }
 
-// Example graph representation
-const graph = {
-    A: ['B', 'C'],
-    B: ['A', 'D'],
-    C: ['A', 'E'],
-    D: ['B', 'F'],
-    E: ['C', 'G'],
-    F: ['D'],
-    G: ['E']
-};
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
 
-const startNode = 'A';
-const endNode = 'G';
+    insert(value) {
+        const newNode = new Node(value);
 
-const intersectionNode = biDirectionalSearch(graph, startNode, endNode);
-console.log('Intersection node:', intersectionNode);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+}
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log(bst.search(15)); // Output: true
+console.log(bst.search(8)); // Output: false
