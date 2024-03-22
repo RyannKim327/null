e66a217debdab7b9aaa55eaba9c571f1069c1181
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:56:33 +0800

    3-22-2024 17:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,26 @@
-function validateEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
+function isPalindrome(str) {
+    if (!str) {
+        return false;
+    }
+    
+    let i = 0;
+    let j = str.length - 1;
+    
+    while (i < j) {
+        if (str[i] !== str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    
+    return true;
 }
 
-// Test the function
-console.log(validateEmail("example@example.com")); // true
-console.log(validateEmail("example@.com")); // false
+// Test with a string
+const inputString = "racecar";
+if (isPalindrome(inputString)) {
+    console.log(inputString + " is a palindrome.");
+} else {
+    console.log(inputString + " is not a palindrome.");
+}
