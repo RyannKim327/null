df2b0a93e414a73b8db4a32e16946d49345df8a7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:45:46 +0800

    3-22-2024 17:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,5 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+let str1 = "Hello";
+let str2 = "World";
+let result = str1 + " " + str2;
 
-function findMiddleElement(head) {
-    let slowPointer = head;
-    let fastPointer = head;
-
-    while (fastPointer != null && fastPointer.next != null) {
-        slowPointer = slowPointer.next;
-        fastPointer = fastPointer.next.next;
-    }
-
-    return slowPointer.value;
-}
-
-// Example of creating a linked list
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-console.log(findMiddleElement(head)); // Output: 3
+console.log(result); // Output: "Hello World"
