2bda49cbc3094d019729a955ef030334b1f0de47
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:59:40 +0800

    3-22-2024 17:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,73 +1,26 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinaryTree {
-  constructor() {
-    this.root = null;
-    this.leafCount = 0;
-  }
-
-  addNode(value) {
-    const newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
+function shellSort(arr) {
+    const n = arr.length;
+    let gap = Math.floor(n/2);
+    
+    while (gap > 0) {
+        for (let i = gap; i < n; i++) {
+            let temp = arr[i];
+            let j = i;
+            
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            
+            arr[j] = temp;
+        }
+        
+        gap = Math.floor(gap/2);
     }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (!node.left) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (!node.right) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-
-  countLeafNodes() {
-    if (!this.root) {
-      return 0;
-    }
-
-    this.countLeafNodesRecursive(this.root);
-    return this.leafCount;
-  }
-
-  countLeafNodesRecursive(node) {
-    if (!node) {
-      return;
-    }
-
-    if (!node.left && !node.right) {
-      this.leafCount++;
-    }
-
-    this.countLeafNodesRecursive(node.left);
-    this.countLeafNodesRecursive(node.right);
-  }
+    
+    return arr;
 }
 
 // Example usage
-const binaryTree = new BinaryTree();
-binaryTree.addNode(10);
-binaryTree.addNode(5);
-binaryTree.addNode(15);
-binaryTree.addNode(3);
-binaryTree.addNode(7);
-binaryTree.addNode(12);
-binaryTree.addNode(18);
-
-console.log("Number of leaf nodes: ", binaryTree.countLeafNodes());
+const arr = [12, 34, 54, 2, 3];
+console.log(shellSort(arr)); // [2, 3, 12, 34, 54]
