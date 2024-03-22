886024d44424b2c79c26338671894ddd0992335a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-22 17:58:26 +0800

    3-22-2024 17:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-function calculateMean(numbers) {
-    let sum = 0;
-
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-    }
-
-    let mean = sum / numbers.length;
-    return mean;
-}
-
-// Example usage
-const numbers = [1, 2, 3, 4, 5];
-const mean = calculateMean(numbers);
-console.log(mean);
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log('An error occurred', error));
