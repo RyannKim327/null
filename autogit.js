59ab99e55cbb925d7c4af2964fb19bb2859e5f66
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:31:37 +0800

    3-23-2024 12:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,12 @@
-class Graph {
-  constructor() {
-    this.adjList = new Map();
-  }
+const axios = require('axios');
 
-  addVertex(vertex) {
-    this.adjList.set(vertex, []);
-  }
-
-  addEdge(v, w) {
-    this.adjList.get(v).push(w);
-  }
-
-  dfs(startingNode) {
-    let visited = {};
-    this._dfsHelper(startingNode, visited);
-  }
-
-  _dfsHelper(node, visited) {
-    if (!visited[node]) {
-      visited[node] = true;
-      console.log(node);
-
-      let neighbors = this.adjList.get(node);
-
-      for (let neighbor of neighbors) {
-        this._dfsHelper(neighbor, visited);
-      }
-    }
+async function fetchData() {
+  try {
+    const response = await axios.get('https://api.example.com/data');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
   }
 }
 
-// Example usage
-let graph = new Graph();
-
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-
-graph.addEdge('A', 'B');
-graph.addEdge('B', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('D', 'E');
-
-console.log('Depth First Search:');
-graph.dfs('A');
+fetchData();
