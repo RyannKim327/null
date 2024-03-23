b1950973cdac3a4a0a7f7fb024defbb550fe5055
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:36:58 +0800

    3-23-2024 12:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,39 @@
-let str = "   Hello,  World!   ";
-let newStr = str.replace(/\s/g, '');
-console.log(newStr); // Output: "Hello,World!"
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (!fast || !fast.next) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return true;
+}
+
+// Example usage
+// Creating a linked list with a cycle
+let head = new ListNode(1);
+let node2 = new ListNode(2);
+let node3 = new ListNode(3);
+let node4 = new ListNode(4);
+
+head.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node2; // creating a cycle
+
+console.log(hasCycle(head)); // Output: true
