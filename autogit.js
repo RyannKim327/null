4dd736911742954a6c32a0634b3e139167dc1593
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:43:29 +0800

    3-23-2024 12:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,23 @@
-function longestCommonPrefix(strs) {
-    if (strs.length === 0) return "";
-
-    const shortest = strs.reduce((shortest, str) => str.length < shortest.length ? str : shortest, strs[0]);
-    
-    for (let i = 0; i < shortest.length; i++) {
-        for (const str of strs) {
-            if (str[i] !== shortest[i]) {
-                return shortest.substring(0, i);
+function bubbleSort(arr) {
+    let len = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
             }
         }
-    }
-    
-    return shortest;
+    } while (swapped);
+
+    return arr;
 }
 
 // Example usage
-const strings = ["flower", "flow", "flight"];
-const prefix = longestCommonPrefix(strings);
-console.log(prefix); // Output: "fl"
+let arrayToSort = [5, 3, 8, 2, 1, 4];
+console.log("Original Array: ", arrayToSort);
+let sortedArray = bubbleSort(arrayToSort);
+console.log("Sorted Array: ", sortedArray);
