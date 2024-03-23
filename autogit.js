e63313de53bdc087f5fe70777b76d552ab9d7762
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:41:36 +0800

    3-23-2024 12:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,16 @@
-class TarjanSCC {
-    constructor(graph) {
-        this.graph = graph;
-        this.index = 0;
-        this.stack = [];
-        this.lowestLink = [];
-        this.visited = [];
-        this.onStack = [];
-        this.components = [];
+function isAnagram(str1, str2) {
+    const sanitizeString = function (str) {
+        return str.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
     }
 
-    findSCC() {
-        for (let i = 0; i < this.graph.length; i++) {
-            if (!this.visited[i]) {
-                this.tarjan(i);
-            }
-        }
-        return this.components;
-    }
-
-    tarjan(node) {
-        this.lowestLink[node] = this.index;
-        this.visited[node] = this.index;
-        this.index++;
-        this.stack.push(node);
-        this.onStack[node] = true;
-
-        for (let neighbor of this.graph[node]) {
-            if (!this.visited[neighbor]) {
-                this.tarjan(neighbor);
-                this.lowestLink[node] = Math.min(this.lowestLink[node], this.lowestLink[neighbor]);
-            } else if (this.onStack[neighbor]) {
-                this.lowestLink[node] = Math.min(this.lowestLink[node], this.visited[neighbor]);
-            }
-        }
-
-        if (this.lowestLink[node] === this.visited[node]) {
-            let component = [];
-            let nextNode = null;
-            do {
-                nextNode = this.stack.pop();
-                this.onStack[nextNode] = false;
-                component.push(nextNode);
-            } while (nextNode !== node);
-            this.components.push(component);
-        }
-    }
+    return sanitizeString(str1) === sanitizeString(str2);
 }
 
-// Example usage
-const graph = [
-    [1],      // Node 0 points to Node 1
-    [2],      // Node 1 points to Node 2
-    [0, 3],   // Node 2 points to Node 0 and Node 3
-    [4, 5],   // Node 3 points to Node 4 and Node 5
-    [5],      // Node 4 points to Node 5
-    [3]       // Node 5 points to Node 3
-];
-
-const tarjanSCC = new TarjanSCC(graph);
-const stronglyConnectedComponents = tarjanSCC.findSCC();
-console.log(stronglyConnectedComponents);
+// Test the function
+const string1 = "Listen";
+const string2 = "Silent";
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
