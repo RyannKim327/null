a67d2fe7425250df16fca29712634adc1ccca100
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:35:25 +0800

    3-23-2024 12:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,79 +1,15 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    const newNode = new Node(value);
-    if (this.size === 0) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.next = newNode;
-      this.tail = newNode;
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = Array.from(new Set(arr));
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = arr.filter((item, index) => arr.indexOf(item) === index);
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+let arr = [1, 2, 3, 3, 4, 5, 5];
+let uniqueArr = arr.reduce((acc, current) => {
+    if (!acc.includes(current)) {
+        acc.push(current);
     }
-    this.size++;
-  }
+    return acc;
+}, []);
 
-  dequeue() {
-    if (this.size === 0) {
-      return null;
-    }
-
-    const dequeuedValue = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-
-    if (this.size === 0) {
-      this.tail = null;
-    }
-
-    return dequeuedValue;
-  }
-
-  peek() {
-    if (this.size === 0) {
-      return null;
-    }
-
-    return this.head.value;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  printQueue() {
-    let current = this.head;
-    while (current !== null) {
-      console.log(current.value);
-      current = current.next;
-    }
-  }
-}
-
-// Example usage:
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log("Queue:");
-queue.printQueue();
-
-console.log("Dequeue:", queue.dequeue());
-
-console.log("Peek:", queue.peek());
-
-console.log("Queue after dequeue:");
-queue.printQueue();
+console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
