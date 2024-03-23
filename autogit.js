ea588da6f985f1d5372c0bca12e30b828b9a82f2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:36:50 +0800

    3-23-2024 12:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,3 @@
-function reverseWords(str) {
-    // Split the string into an array of words
-    let words = str.split(' ');
-
-    // Reverse the array
-    let reversedWords = words.reverse();
-
-    // Join the array back into a string
-    let reversedStr = reversedWords.join(' ');
-
-    return reversedStr;
-}
-
-// Example usage
-let originalString = "Hello World!";
-let reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: "World! Hello"
+let str = "   Hello,  World!   ";
+let newStr = str.replace(/\s/g, '');
+console.log(newStr); // Output: "Hello,World!"
