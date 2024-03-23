a29da19b1fb643591a5d62ab7878685607512baf
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:36:37 +0800

    3-23-2024 12:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,12 @@
-function removeVowels(str) {
-  return str.replace(/[aeiouAEIOU]/g, '');
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
 
-let inputString = "Hello, World!";
-let result = removeVowels(inputString);
-console.log(result); // Output: "Hll, Wrld!"
+// Calculate the factorial of 5
+const number = 5;
+const result = factorial(number);
+console.log(`Factorial of ${number} is: ${result}`);
