1e84574d591e0481d8529df9b0a59c3cafe7eabe
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:30:04 +0800

    3-23-2024 12:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,17 @@
-// Heapify a subtree rooted at index i in the array arr of size n
-function heapify(arr, n, i) {
-    let largest = i;
-    const left = 2 * i + 1;
-    const right = 2 * i + 2;
-
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
+function bubbleSort(arr) {
+    var len = arr.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                var temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-
-    if (largest !== i) {
-        [arr[i], arr[largest]] = [arr[largest], arr[i]];
-        heapify(arr, n, largest);
-    }
-}
-
-// Perform heap sort on the array arr
-function heapSort(arr) {
-    const n = arr.length;
-
-    // Build max heap
-    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-
-    // Extract elements from the heap one by one
-    for (let i = n - 1; i > 0; i--) {
-        [arr[0], arr[i]] = [arr[i], arr[0]];
-        heapify(arr, i, 0);
-    }
-
     return arr;
 }
 
-// Usage
-const arr = [12, 11, 13, 5, 6, 7];
-console.log("Original array: " + arr);
-const sortedArr = heapSort(arr);
-console.log("Sorted array: " + sortedArr);
+var array = [6, 4, 3, 1, 8, 7, 2, 5];
+console.log("Unsorted array:", array);
+console.log("Sorted array:", bubbleSort(array));
