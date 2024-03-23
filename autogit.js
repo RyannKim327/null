927a8ebe9f302b45ee3d24fc72b93325d11a6796
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:34:25 +0800

    3-23-2024 12:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,6 @@
-function tarjanStronglyConnectedComponents(graph) {
-    const stack = [];
-    const result = [];
-    let index = 0;
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-    const recursiveTarjan = (node) => {
-        node.index = index;
-        node.lowLink = index;
-        index++;
-        stack.push(node);
-        node.onStack = true;
+const commonElements = array1.filter(element => array2.includes(element));
 
-        node.neighbors.forEach((neighbor) => {
-            if (neighbor.index === undefined) {
-                recursiveTarjan(neighbor);
-                node.lowLink = Math.min(node.lowLink, neighbor.lowLink);
-            } else if (neighbor.onStack) {
-                node.lowLink = Math.min(node.lowLink, neighbor.index);
-            }
-        });
-
-        if (node.lowLink === node.index) {
-            const component = [];
-            let popNode;
-            do {
-                popNode = stack.pop();
-                popNode.onStack = false;
-                component.push(popNode);
-            } while (popNode !== node);
-            result.push(component);
-        }
-    };
-
-    graph.forEach((node) => {
-        if (node.index === undefined) {
-            recursiveTarjan(node);
-        }
-    });
-
-    return result;
-}
-const nodeA = { neighbors: [] };
-const nodeB = { neighbors: [] };
-const nodeC = { neighbors: [] };
-
-nodeA.neighbors.push(nodeB);
-nodeB.neighbors.push(nodeC);
-nodeC.neighbors.push(nodeA);
-
-const graph = [nodeA, nodeB, nodeC];
-
-const stronglyConnectedComponents = tarjanStronglyConnectedComponents(graph);
-console.log(stronglyConnectedComponents);
+console.log(commonElements); // Output: [3, 4, 5]
