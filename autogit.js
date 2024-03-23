224c2697e4909ac20901e58e2ec0de77e71f3aa7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:36:03 +0800

    3-23-2024 12:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,3 @@
-class Node {
-    constructor(value, left = null, right = null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
-}
-
-function sumBinaryTree(root) {
-    if (root === null) {
-        return 0;
-    }
-    
-    return root.value + sumBinaryTree(root.left) + sumBinaryTree(root.right);
-}
-
-// Example binary tree
-const tree = new Node(1,
-                      new Node(2,
-                               new Node(3),
-                               new Node(4)),
-                      new Node(5,
-                               new Node(6),
-                               new Node(7)));
-
-const sum = sumBinaryTree(tree);
-console.log("Sum of all nodes in the binary tree: " + sum);
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // Output: [5, 4, 3, 2, 1]
