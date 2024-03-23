5ebe88e8abf484e2f3733ccdd6ddf29609e16e28
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:31:24 +0800

    3-23-2024 12:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,22 @@
-function TarjanSCC(graph) {
-    let index = 0;
-    let stack = [];
-    let onStack = new Set();
-    let indexMap = new Map();
-    let lowlinkMap = new Map();
-    let result = [];
+function firstNonRepeatingChar(str) {
+    let charCount = {};
 
-    function strongConnect(node) {
-        indexMap.set(node, index);
-        lowlinkMap.set(node, index);
-        index++;
-        stack.push(node);
-        onStack.add(node);
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
 
-        graph[node].forEach(neighbor => {
-            if (!indexMap.has(neighbor)) {
-                strongConnect(neighbor);
-                lowlinkMap.set(node, Math.min(lowlinkMap.get(node), lowlinkMap.get(neighbor)));
-            } else if (onStack.has(neighbor)) {
-                lowlinkMap.set(node, Math.min(lowlinkMap.get(node), indexMap.get(neighbor)));
-            }
-        });
-
-        if (indexMap.get(node) === lowlinkMap.get(node)) {
-            let scc = [];
-            let popNode;
-            do {
-                popNode = stack.pop();
-                onStack.delete(popNode);
-                scc.push(popNode);
-            } while (popNode !== node);
-            result.push(scc);
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (charCount[char] === 1) {
+            return char;
         }
     }
 
-    for (let node in graph) {
-        if (!indexMap.has(node)) {
-            strongConnect(node);
-        }
-    }
-
-    return result;
+    return null; // If no non-repeating characters found
 }
 
-// Example Usage
-const graph = {
-    'A': ['B'],
-    'B': ['C'],
-    'C': ['A', 'D'],
-    'D': ['E'],
-    'E': ['F'],
-    'F': ['D', 'G'],
-    'G': ['H'],
-    'H': ['I'],
-    'I': []
-};
+let inputString = "hello";
+let firstNonRepeating = firstNonRepeatingChar(inputString);
 
-const scc = TarjanSCC(graph);
-console.log(scc);
+console.log("First non-repeating character:", firstNonRepeating);
