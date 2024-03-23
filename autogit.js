9a31bdf017b164e1770d5ff21bd26456c2097df4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:44:57 +0800

    3-23-2024 12:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,41 @@
-class HashTable {
-    constructor(size) {
-        this.size = size;
-        this.table = new Array(size);
+function dijkstra(graph, source) {
+  const distances = {};
+  const visited = {};
+  const queue = new PriorityQueue();
+
+  for (let node in graph) {
+    distances[node] = node === source ? 0 : Infinity;
+    queue.enqueue(node, distances[node]);
+  }
+
+  while (!queue.isEmpty()) {
+    const currentNode = queue.dequeue();
+
+    if (visited[currentNode]) {
+      continue;
     }
 
-    _hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 13) % this.size;
-        }
-        return hash;
-    }
+    visited[currentNode] = true;
 
-    set(key, value) {
-        const index = this._hash(key);
-        if (!this.table[index]) {
-            this.table[index] = [];
-        }
-        this.table[index].push([key, value]);
-    }
+    for (let neighbor in graph[currentNode]) {
+      const distance = distances[currentNode] + graph[currentNode][neighbor];
 
-    get(key) {
-        const index = this._hash(key);
-        if (this.table[index]) {
-            for (let i = 0; i < this.table[index].length; i++) {
-                if (this.table[index][i][0] === key) {
-                    return this.table[index][i][1];
-                }
-            }
-        }
-        return undefined;
+      if (distance < distances[neighbor]) {
+        distances[neighbor] = distance;
+        queue.enqueue(neighbor, distance);
+      }
     }
+  }
 
-    remove(key) {
-        const index = this._hash(key);
-        if (this.table[index]) {
-            this.table[index] = this.table[index].filter(item => item[0] !== key);
-        }
-    }
+  return distances;
 }
-const myHashTable = new HashTable(10);
+const graph = {
+  A: { B: 5, C: 3 },
+  B: { A: 5, C: 2, D: 1 },
+  C: { A: 3, B: 2, D: 6 },
+  D: { B: 1, C: 6 },
+};
 
-myHashTable.set("name", "Alice");
-myHashTable.set("age", 30);
-
-console.log(myHashTable.get("name")); // Output: Alice
-console.log(myHashTable.get("age")); // Output: 30
-
-myHashTable.remove("age");
-
-console.log(myHashTable.get("age")); // Output: undefined
+const source = 'A';
+const distances = dijkstra(graph, source);
+console.log(distances); // Output the shortest distances from the source node
