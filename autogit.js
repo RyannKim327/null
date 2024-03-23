4f698904a529a95b86186cccf9bf8aca9173a5bc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:37:14 +0800

    3-23-2024 12:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,14 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+function mean(numbers) {
+    let sum = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    
+    return sum / numbers.length;
+}
+
+// Example
+const numbers = [1, 2, 3, 4, 5];
+const average = mean(numbers);
+console.log("Mean:", average);
