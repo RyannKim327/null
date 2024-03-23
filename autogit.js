a92f91cd0bca70d770763b8c9e8abba0793004df
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:41:17 +0800

    3-23-2024 12:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,18 @@
-function biDirectionalSearch(startNode, goalNode) {
-    let startQueue = [startNode];
-    let goalQueue = [goalNode];
+function largestPrimeFactor(number) {
+    let factor = 2;
     
-    let startVisited = new Set();
-    let goalVisited = new Set();
-    
-    startVisited.add(startNode);
-    goalVisited.add(goalNode);
-    
-    while (startQueue.length > 0 && goalQueue.length > 0) {
-        let currentStartNode = startQueue.shift();
-        let currentGoalNode = goalQueue.shift();
-        
-        if (goalVisited.has(currentStartNode) || startVisited.has(currentGoalNode)) {
-            console.log("Path found!");
-            return;
-        }
-        
-        // Expand neighbors of currentStartNode
-        for (let neighbor of currentStartNode.neighbors) {
-            if (!startVisited.has(neighbor)) {
-                startVisited.add(neighbor);
-                startQueue.push(neighbor);
-            }
-        }
-        
-        // Expand neighbors of currentGoalNode
-        for (let neighbor of currentGoalNode.neighbors) {
-            if (!goalVisited.has(neighbor)) {
-                goalVisited.add(neighbor);
-                goalQueue.push(neighbor);
-            }
+    while (factor <= Math.sqrt(number)) {
+        if (number % factor === 0) {
+            number /= factor;
+        } else {
+            factor++;
         }
     }
     
-    console.log("Path not found");
+    return number;
 }
 
+const number = 600851475143; // Replace this with the number you want to find the largest prime factor of
+const largestPrimeFactorNumber = largestPrimeFactor(number);
+
+console.log(`The largest prime factor of ${number} is: ${largestPrimeFactorNumber}`);
