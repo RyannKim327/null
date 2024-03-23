b8c0bd034ec8cf6de2366746256e6a33bda100cf
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:36:14 +0800

    3-23-2024 12:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,10 @@
-function reverseWords(str) {
-    // Split the string into an array of words
-    let wordsArray = str.split(' ');
-
-    // Reverse the order of the array
-    let reversedWordsArray = wordsArray.reverse();
-
-    // Join the array back into a string
-    let reversedStr = reversedWordsArray.join(' ');
-
-    return reversedStr;
+function decimalToBinary(decimal) {
+    return decimal.toString(2);
 }
 
-// Test the function
-let str = "Hello World";
-let reversedStr = reverseWords(str);
-console.log(reversedStr); // Output: "World Hello"
+// Example usage
+const decimalNumber = 42;
+const binaryNumber = decimalToBinary(decimalNumber);
+
+console.log(`Decimal: ${decimalNumber}`);
+console.log(`Binary: ${binaryNumber}`);
