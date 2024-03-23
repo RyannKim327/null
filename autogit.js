611103a96e8b09f7495d3059b01d333cd1f077b3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:34:56 +0800

    3-23-2024 12:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,77 +1,25 @@
-// Node class to represent each element of the queue
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    
+    if (number <= 3) {
+        return true;
+    }
+    
+    if (number % 2 === 0 || number % 3 === 0) {
+        return false;
+    }
+    
+    for (let i = 5; i * i <= number; i += 6) {
+        if (number % i === 0 || number % (i + 2) === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-// Queue class with linked list implementation
-class Queue {
-  constructor() {
-    this.front = null;
-    this.rear = null;
-    this.size = 0;
-  }
-
-  isEmpty() {
-    return this.size === 0;
-  }
-
-  enqueue(data) {
-    const newNode = new Node(data);
-    if (this.isEmpty()) {
-      this.front = newNode;
-    } else {
-      this.rear.next = newNode;
-    }
-    this.rear = newNode;
-    this.size++;
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    const data = this.front.data;
-    this.front = this.front.next;
-    this.size--;
-    if (this.isEmpty()) {
-      this.rear = null;
-    }
-    return data;
-  }
-
-  peek() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    return this.front.data;
-  }
-
-  printQueue() {
-    if (this.isEmpty()) {
-      console.log("Queue is empty");
-    } else {
-      let current = this.front;
-      while (current !== null) {
-        console.log(current.data);
-        current = current.next;
-      }
-    }
-  }
-}
-
-// Example Usage
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-queue.printQueue(); // Output: 1, 2, 3
-
-console.log(queue.dequeue()); // Output: 1
-
-console.log(queue.peek()); // Output: 2
-
-queue.printQueue(); // Output: 2, 3
+// Test the function
+console.log(isPrime(17)); // true
+console.log(isPrime(15)); // false
