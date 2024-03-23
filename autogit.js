d0b82177f86958e05c26c52bdbdfee56330161b0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:40:28 +0800

    3-23-2024 12:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,8 @@
-function topologicalSort(graph) {
-    const visited = {};
-    const sorted = [];
-  
-    function visit(node) {
-        if (visited[node])
-            return;
-  
-        visited[node] = true;
-  
-        graph[node].forEach(neighbor => {
-            if (!visited[neighbor])
-                visit(neighbor);
-        });
-  
-        sorted.push(node);
-    }
-  
-    Object.keys(graph).forEach(visit);
-  
-    return sorted.reverse();
+let str = "Hello, World!";
+let substring = "World";
+
+if (str.includes(substring)) {
+    console.log("The string contains the substring.");
+} else {
+    console.log("The string does not contain the substring.");
 }
-  
-const graph = {
-    'A': ['B', 'C'],
-    'B': ['D'],
-    'C': [],
-    'D': ['E'],
-    'E': []
-};
-  
-const sortedNodes = topologicalSort(graph);
-console.log(sortedNodes);
