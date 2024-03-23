39106353865da183d784ee83f9fe5c75fbfadee2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:31:31 +0800

    3-23-2024 12:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,48 @@
-function firstNonRepeatingChar(str) {
-    let charCount = {};
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
 
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        charCount[char] = (charCount[char] || 0) + 1;
+  addVertex(vertex) {
+    this.adjList.set(vertex, []);
+  }
+
+  addEdge(v, w) {
+    this.adjList.get(v).push(w);
+  }
+
+  dfs(startingNode) {
+    let visited = {};
+    this._dfsHelper(startingNode, visited);
+  }
+
+  _dfsHelper(node, visited) {
+    if (!visited[node]) {
+      visited[node] = true;
+      console.log(node);
+
+      let neighbors = this.adjList.get(node);
+
+      for (let neighbor of neighbors) {
+        this._dfsHelper(neighbor, visited);
+      }
     }
-
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        if (charCount[char] === 1) {
-            return char;
-        }
-    }
-
-    return null; // If no non-repeating characters found
+  }
 }
 
-let inputString = "hello";
-let firstNonRepeating = firstNonRepeatingChar(inputString);
+// Example usage
+let graph = new Graph();
 
-console.log("First non-repeating character:", firstNonRepeating);
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('D', 'E');
+
+console.log('Depth First Search:');
+graph.dfs('A');
