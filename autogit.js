60893bf0c94f2587560c7ce93c9e5b4e11dc2d7e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:32:42 +0800

    3-23-2024 12:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,84 @@
-function countingSort(arr) {
-  let max = Math.max(...arr);
-  let min = Math.min(...arr);
-  let range = max - min + 1;
-  let count = Array(range).fill(0);
-  let output = [];
-  
-  arr.forEach(num => {
-    count[num - min]++;
-  });
-  
-  count.forEach((count, i) => {
-    while (count > 0) {
-      output.push(i + min);
-      count--;
+class BinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  extractMin() {
+    if (this.heap.length === 0) {
+      return null;
     }
-  });
-  
-  return output;
+
+    const minValue = this.heap[0];
+    const lastIndex = this.heap.length - 1;
+    this.heap[0] = this.heap[lastIndex];
+    this.heap.pop();
+    this.heapifyDown();
+
+    return minValue;
+  }
+
+  heapifyUp() {
+    let currentIndex = this.heap.length - 1;
+    while (currentIndex > 0) {
+      const parentIndex = Math.floor((currentIndex - 1) / 2);
+      if (this.heap[currentIndex] < this.heap[parentIndex]) {
+        [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+        currentIndex = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  heapifyDown() {
+    let currentIndex = 0;
+    while (true) {
+      let leftChildIndex = 2 * currentIndex + 1;
+      let rightChildIndex = 2 * currentIndex + 2;
+      let smallestChildIndex = currentIndex;
+
+      if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallestChildIndex]) {
+        smallestChildIndex = leftChildIndex;
+      }
+
+      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestChildIndex]) {
+        smallestChildIndex = rightChildIndex;
+      }
+
+      if (smallestChildIndex !== currentIndex) {
+        [this.heap[currentIndex], this.heap[smallestChildIndex]] = [this.heap[smallestChildIndex], this.heap[currentIndex]];
+        currentIndex = smallestChildIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  peek() {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    return this.heap[0];
+  }
+
+  getSize() {
+    return this.heap.length;
+  }
 }
 
 // Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-const sortedArr = countingSort(arr);
-console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
+const priorityQueue = new BinaryHeap();
+priorityQueue.insert(5);
+priorityQueue.insert(2);
+priorityQueue.insert(10);
+
+console.log(priorityQueue.peek()); // Output: 2
+console.log(priorityQueue.getSize()); // Output: 3
+console.log(priorityQueue.extractMin()); // Output: 2
+console.log(priorityQueue.getSize()); // Output: 2
