050c38e493a3575ff9ca1bb37015e7d1ff1f8e6c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:39:05 +0800

    3-23-2024 12:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,18 @@
-function buildPatternTable(pattern) {
-    const table = [0];
-    let prefix = 0;
-    
-    for (let i = 1; i < pattern.length; i++) {
-        if (pattern[i] === pattern[prefix]) {
-            prefix++;
-            table[i] = prefix;
-        } else {
-            prefix = 0;
-            table[i] = prefix;
-        }
-    }
-    
-    return table;
-}
+function countOccurrences(str, char) {
+    let count = 0;
 
-function KMP(text, pattern) {
-    const patternTable = buildPatternTable(pattern);
-    let textIndex = 0;
-    let patternIndex = 0;
-    
-    while (textIndex < text.length) {
-        if (text[textIndex] === pattern[patternIndex]) {
-            if (patternIndex === pattern.length - 1) {
-                return textIndex - pattern.length + 1;
-            }
-            patternIndex++;
-            textIndex++;
-        } else {
-            if (patternIndex !== 0) {
-                patternIndex = patternTable[patternIndex - 1];
-            } else {
-                textIndex++;
-            }
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
         }
     }
 
-    return -1;
+    return count;
 }
 
-// Example
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const index = KMP(text, pattern);
+// Example usage
+let myString = "hello world";
+let charToCount = "l";
+let occurrences = countOccurrences(myString, charToCount);
 
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+console.log(`The character "${charToCount}" occurs ${occurrences} times in the string "${myString}"`);
