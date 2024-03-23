b4743c0810fbc15239b36e4faa871e5b6c3e4e06
RyannKim327 <rksesgundo123@gmail.com>
2024-03-23 12:41:30 +0800

    3-23-2024 12:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,62 @@
-function largestPrimeFactor(number) {
-    let factor = 2;
-    
-    while (factor <= Math.sqrt(number)) {
-        if (number % factor === 0) {
-            number /= factor;
-        } else {
-            factor++;
+class TarjanSCC {
+    constructor(graph) {
+        this.graph = graph;
+        this.index = 0;
+        this.stack = [];
+        this.lowestLink = [];
+        this.visited = [];
+        this.onStack = [];
+        this.components = [];
+    }
+
+    findSCC() {
+        for (let i = 0; i < this.graph.length; i++) {
+            if (!this.visited[i]) {
+                this.tarjan(i);
+            }
+        }
+        return this.components;
+    }
+
+    tarjan(node) {
+        this.lowestLink[node] = this.index;
+        this.visited[node] = this.index;
+        this.index++;
+        this.stack.push(node);
+        this.onStack[node] = true;
+
+        for (let neighbor of this.graph[node]) {
+            if (!this.visited[neighbor]) {
+                this.tarjan(neighbor);
+                this.lowestLink[node] = Math.min(this.lowestLink[node], this.lowestLink[neighbor]);
+            } else if (this.onStack[neighbor]) {
+                this.lowestLink[node] = Math.min(this.lowestLink[node], this.visited[neighbor]);
+            }
+        }
+
+        if (this.lowestLink[node] === this.visited[node]) {
+            let component = [];
+            let nextNode = null;
+            do {
+                nextNode = this.stack.pop();
+                this.onStack[nextNode] = false;
+                component.push(nextNode);
+            } while (nextNode !== node);
+            this.components.push(component);
         }
     }
-    
-    return number;
 }
 
-const number = 600851475143; // Replace this with the number you want to find the largest prime factor of
-const largestPrimeFactorNumber = largestPrimeFactor(number);
+// Example usage
+const graph = [
+    [1],      // Node 0 points to Node 1
+    [2],      // Node 1 points to Node 2
+    [0, 3],   // Node 2 points to Node 0 and Node 3
+    [4, 5],   // Node 3 points to Node 4 and Node 5
+    [5],      // Node 4 points to Node 5
+    [3]       // Node 5 points to Node 3
+];
 
-console.log(`The largest prime factor of ${number} is: ${largestPrimeFactorNumber}`);
+const tarjanSCC = new TarjanSCC(graph);
+const stronglyConnectedComponents = tarjanSCC.findSCC();
+console.log(stronglyConnectedComponents);
