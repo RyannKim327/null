13d15e99ad3b2cd44baf397b1a1c782c699202c3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:53:41 +0800

    3-24-2024 9:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,21 @@
-let str = "   Hello,    World!   ";
-let stripped = str.replace(/\s/g, ''); // Removes all whitespace characters
+function bubbleSort(arr) {
+    let len = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let tmp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    
+    return arr;
+}
 
-console.log(stripped); // Output: "Hello,World!"
+// Example usage
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log(bubbleSort(arr)); // Output: [11, 12, 22, 25, 34, 64, 90]
