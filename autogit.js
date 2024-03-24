22196c51c2b6fb2a6eeb46ecf507d20c02ba8e07
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:30:57 +0800

    3-24-2024 10:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,3 @@
-function findSecondLargest(arr) {
-    arr.sort(function(a, b) {
-        return b - a;
-    });
+const userInput = prompt("Enter something:");
 
-    return arr[1];
-}
-
-const array = [5, 10, 3, 8, 1, 6];
-const secondLargest = findSecondLargest(array);
-
-console.log("Second largest element in the array is: " + secondLargest);
+console.log("You entered: " + userInput);
