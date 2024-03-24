0574841db51d9cd544692764ed1f93158906461f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:07:44 +0800

    3-24-2024 9:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,59 @@
-function kthSmallestElement(arr, k) {
-    arr.sort(function(a, b) {
-        return a - b;
-    });
+function tarjanSCC(graph) {
+    let index = 0;
+    let stack = [];
+    let indexes = {};
+    let lowLinks = {};
+    let onStack = {};
+    let scc = [];
 
-    return arr[k - 1];
+    function strongConnect(node) {
+        indexes[node] = index;
+        lowLinks[node] = index;
+        index++;
+        stack.push(node);
+        onStack[node] = true;
+
+        for (let neighbor of graph[node]) {
+            if (indexes[neighbor] === undefined) {
+                strongConnect(neighbor);
+                lowLinks[node] = Math.min(lowLinks[node], lowLinks[neighbor]);
+            } else if (onStack[neighbor]) {
+                lowLinks[node] = Math.min(lowLinks[node], indexes[neighbor]);
+            }
+        }
+
+        if (lowLinks[node] === indexes[node]) {
+            let component = [];
+            let nextNode;
+            do {
+                nextNode = stack.pop();
+                onStack[nextNode] = false;
+                component.push(nextNode);
+            } while (nextNode !== node);
+            scc.push(component);
+        }
+    }
+
+    for (let node in graph) {
+        if (indexes[node] === undefined) {
+            strongConnect(node);
+        }
+    }
+
+    return scc;
 }
 
-const array = [4, 2, 8, 1, 3, 9];
-const k = 3;
-const result = kthSmallestElement(array, k);
-console.log(result); // Output: 3
+// Example graph representation using adjacency list
+const graph = {
+    0: [1],
+    1: [2],
+    2: [0, 3],
+    3: [4],
+    4: [5, 6],
+    5: [0],
+    6: [4, 7],
+    7: [6]
+};
+
+const scc = tarjanSCC(graph);
+console.log(scc);
