498506e35b50a94ec688d6829a45fa6d1fdc4245
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:23:20 +0800

    3-24-2024 9:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,10 @@
-function tarjanSCC(graph) {
-    let index = 0;
-    let stack = [];
-    let indices = {};
-    let lowlinks = {};
-    let onStack = {};
-    let result = [];
-
-    function strongConnect(node) {
-        indices[node] = index;
-        lowlinks[node] = index;
-        index++;
-        stack.push(node);
-        onStack[node] = true;
-
-        for (let neighbor of graph[node]) {
-            if (!(neighbor in indices)) {
-                strongConnect(neighbor);
-                lowlinks[node] = Math.min(lowlinks[node], lowlinks[neighbor]);
-            } else if (onStack[neighbor]) {
-                lowlinks[node] = Math.min(lowlinks[node], indices[neighbor]);
-            }
-        }
-
-        if (lowlinks[node] === indices[node]) {
-            let component = [];
-            let nextNode = null;
-            do {
-                nextNode = stack.pop();
-                onStack[nextNode] = false;
-                component.push(nextNode);
-            } while (nextNode !== node);
-            result.push(component);
-        }
-    }
-
-    for (let node in graph) {
-        if (!(node in indices)) {
-            strongConnect(node);
-        }
-    }
-
-    return result;
+function factorial(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 }
 
-// Example usage:
-let graph = {
-    'A': ['B'],
-    'B': ['C', 'E'],
-    'C': ['A', 'D'],
-    'D': ['E'],
-    'E': ['F'],
-    'F': ['G'],
-    'G': ['D', 'H'],
-    'H': ['I'],
-    'I': ['J', 'G']
-};
-
-let scc = tarjanSCC(graph);
-console.log(scc);
+// Calculate factorial of 5
+console.log(factorial(5)); // Output: 120
