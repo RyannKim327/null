423d9c0eb769f39ca324aae37a4ff814343b0630
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:30:53 +0800

    3-24-2024 10:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,12 @@
-function boyerMooreSearch(text, pattern) {
-    const last = buildLastTable(pattern);
-    let n = text.length;
-    let m = pattern.length;
-    let i = m - 1;
-    let j = m - 1;
+function findSecondLargest(arr) {
+    arr.sort(function(a, b) {
+        return b - a;
+    });
 
-    while (i < n) {
-        if (text[i] === pattern[j]) {
-            if (j === 0) {
-                return i;
-            }
-            i--;
-            j--;
-        } else {
-            i += m - Math.min(j, 1 + last[text[i]]);
-            j = m - 1;
-        }
-    }
-
-    return -1; // Pattern not found
+    return arr[1];
 }
 
-function buildLastTable(pattern) {
-    const last = {};
-    for (let i = 0; i < 256; i++) {
-        last[String.fromCharCode(i)] = -1;
-    }
-    for (let i = 0; i < pattern.length; i++) {
-        last[pattern[i]] = i;
-    }
-    return last;
-}
+const array = [5, 10, 3, 8, 1, 6];
+const secondLargest = findSecondLargest(array);
 
-// Example Usage
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-const pattern = "consectetur";
-const index = boyerMooreSearch(text, pattern);
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found");
-}
+console.log("Second largest element in the array is: " + secondLargest);
