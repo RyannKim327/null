6f1e4dbea30e177a872bee3b26555d88bde8ddff
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:51:27 +0800

    3-24-2024 9:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,147 @@
-function bubbleSort(arr) {
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+class Node {
+    constructor(value, color, left, right, parent) {
+        this.value = value;
+        this.color = color;
+        this.left = left;
+        this.right = right;
+        this.parent = parent;
+    }
+}
+
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Insertion
+    insert(value) {
+        let newNode = new Node(value, 'red', null, null, null);
+        
+        this.insertNode(newNode);
+
+        this.fixViolations(newNode);
+    }
+
+    insertNode(newNode) {
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNodeHelper(this.root, newNode);
+        }
+    }
+
+    insertNodeHelper(root, newNode) {
+        if (newNode.value < root.value) {
+            if (root.left === null) {
+                root.left = newNode;
+                newNode.parent = root;
+            } else {
+                this.insertNodeHelper(root.left, newNode);
+            }
+        } else {
+            if (root.right === null) {
+                root.right = newNode;
+                newNode.parent = root;
+            } else {
+                this.insertNodeHelper(root.right, newNode);
             }
         }
     }
-    return arr;
-}
 
-// Example usage
-let unsortedArray = [5, 3, 8, 2, 1, 4];
-let sortedArray = bubbleSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 4, 5, 8]
+    fixViolations(node) {
+        while (node !== this.root && node.color !== 'black' && node.parent.color === 'red') {
+            let parent = node.parent;
+            let grandparent = parent.parent;
+
+            if (parent === grandparent.left) {
+                let uncle = grandparent.right;
+
+                if (uncle && uncle.color === 'red') {
+                    grandparent.color = 'red';
+                    parent.color = 'black';
+                    uncle.color = 'black';
+                    node = grandparent;
+                } else {
+                    if (node === parent.right) {
+                        this.rotateLeft(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+                    
+                    this.rotateRight(grandparent);
+                    let temp = parent.color;
+                    parent.color = grandparent.color;
+                    grandparent.color = temp;
+                    node = parent;
+                }
+            } else {
+                let uncle = grandparent.left;
+
+                if (uncle && uncle.color === 'red') {
+                    grandparent.color = 'red';
+                    parent.color = 'black';
+                    uncle.color = 'black';
+                    node = grandparent;
+                } else {
+                    if (node === parent.left) {
+                        this.rotateRight(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+
+                    this.rotateLeft(grandparent);
+                    let temp = parent.color;
+                    parent.color = grandparent.color;
+                    grandparent.color = temp;
+                    node = parent;
+                }
+            }
+        }
+
+        this.root.color = 'black';
+    }
+
+    rotateLeft(node) {
+        let rightChild = node.right;
+        node.right = rightChild.left;
+
+        if (rightChild.left !== null) {
+            rightChild.left.parent = node;
+        }
+
+        rightChild.parent = node.parent;
+
+        if (node.parent === null) {
+            this.root = rightChild;
+        } else if (node === node.parent.left) {
+            node.parent.left = rightChild;
+        } else {
+            node.parent.right = rightChild;
+        }
+
+        rightChild.left = node;
+        node.parent = rightChild;
+    }
+
+    rotateRight(node) {
+        let leftChild = node.left;
+        node.left = leftChild.right;
+
+        if (leftChild.right !== null) {
+            leftChild.right.parent = node;
+        }
+
+        leftChild.parent = node.parent;
+
+        if (node.parent === null) {
+            this.root = leftChild;
+        } else if (node === node.parent.left) {
+            node.parent.left = leftChild;
+        } else {
+            node.parent.right = leftChild;
+        }
+
+        leftChild.right = node;
+        node.parent = leftChild;
+    }
+}
