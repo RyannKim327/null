56c1b6b429fbb9b0f3a57a1b94cb5606cfec9fb7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:03:24 +0800

    3-24-2024 10:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,23 @@
-class HashTable {
-  constructor(size) {
-    this.size = size;
-    this.data = new Array(size);
-  }
-
-  _hash(key) {
-    let hash = 0;
-    for (let i = 0; i < key.length; i++) {
-      hash = (hash + key.charCodeAt(i) * i) % this.size;
-    }
-    return hash;
-  }
-
-  set(key, value) {
-    const address = this._hash(key);
-    if (!this.data[address]) {
-      this.data[address] = [];
-    }
-    this.data[address].push([key, value]);
-  }
-
-  get(key) {
-    const address = this._hash(key);
-    const bucket = this.data[address];
-    if (bucket) {
-      for (let i = 0; i < bucket.length; i++) {
-        if (bucket[i][0] === key) {
-          return bucket[i][1];
-        }
-      }
-    }
-    return undefined;
-  }
-
-  keys() {
-    const keysArray = [];
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.data[i]) {
-        for (let j = 0; j < this.data[i].length; j++) {
-          keysArray.push(this.data[i][j][0]);
-        }
-      }
-    }
-    return keysArray;
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
 }
 
-// Example usage:
-const hashTable = new HashTable(50);
-hashTable.set("apple", 10);
-hashTable.set("banana", 20);
-hashTable.set("orange", 30);
+function maxDepth(root) {
+  if (!root) {
+    return 0;
+  }
 
-console.log(hashTable.get("banana")); // Output: 20
+  const leftDepth = maxDepth(root.left);
+  const rightDepth = maxDepth(root.right);
+  
+  return 1 + Math.max(leftDepth, rightDepth);
+}
 
-console.log(hashTable.keys()); // Output: ["apple", "banana", "orange"]
+// Example of creating a binary tree
+const tree = new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3));
+
+console.log(maxDepth(tree)); // Output: 3
