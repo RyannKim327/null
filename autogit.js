c16c6fa84a39b83d039c4195d74709ebd6960cd3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:52:21 +0800

    3-24-2024 9:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,78 +1,94 @@
-class Edge {
-    constructor(source, target, weight) {
-        this.source = source;
-        this.target = target;
-        this.weight = weight;
+class Node {
+    constructor(x, y, parent = null) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+
+        this.g = 0;
+        this.h = 0;
+        this.f = 0;
+    }
+
+    isEqual(node) {
+        return this.x === node.x && this.y === node.y;
     }
 }
 
-class BellmanFord {
-    constructor(vertices, edges) {
-        this.vertices = vertices;
-        this.edges = edges;
-        this.distances = {};
-        this.previous = {};
-        this.INFINITY = Number.MAX_SAFE_INTEGER;
-    }
+function astar(start, end, grid) {
+    let openList = [];
+    let closedList = [];
 
-    initialize(source) {
-        for (let vertex of this.vertices) {
-            this.distances[vertex] = this.INFINITY;
-            this.previous[vertex] = null;
-        }
-        this.distances[source] = 0;
-    }
+    openList.push(start);
 
-    relax(edge) {
-        let source = edge.source;
-        let target = edge.target;
-        let weight = edge.weight;
+    while (openList.length > 0) {
+        let currentNode = openList[0];
+        let currentIndex = 0;
 
-        if (this.distances[source] + weight < this.distances[target]) {
-            this.distances[target] = this.distances[source] + weight;
-            this.previous[target] = source;
-        }
-    }
-
-    bellmanFord(source) {
-        this.initialize(source);
-
-        for (let i = 0; i < this.vertices.length - 1; i++) {
-            for (let edge of this.edges) {
-                this.relax(edge);
+        for (let i = 1; i < openList.length; i++) {
+            if (openList[i].f < currentNode.f || (openList[i].f === currentNode.f && openList[i].h < currentNode.h)) {
+                currentNode = openList[i];
+                currentIndex = i;
             }
         }
 
-        // Check for negative weight cycles
-        for (let edge of this.edges) {
-            let source = edge.source;
-            let target = edge.target;
-            let weight = edge.weight;
+        openList.splice(currentIndex, 1);
+        closedList.push(currentNode);
 
-            if (this.distances[source] + weight < this.distances[target]) {
-                console.log("Graph contains negative weight cycle");
-                return;
+        if (currentNode.isEqual(end)) {
+            let path = [];
+            let current = currentNode;
+            while (current !== null) {
+                path.push({ x: current.x, y: current.y });
+                current = current.parent;
             }
+            return path.reverse();
         }
 
-        console.log("Shortest distances from source", source, ":");
-        console.log(this.distances);
-        console.log("Previous vertices:");
-        console.log(this.previous);
+        let children = [];
+        let directions = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+
+        for (let i = 0; i < directions.length; i++) {
+            let newPosition = { x: currentNode.x + directions[i][0], y: currentNode.y + directions[i][1] };
+
+            if (newPosition.x < 0 || newPosition.x >= grid.length || newPosition.y < 0 || newPosition.y >= grid[0].length) {
+                continue;
+            }
+
+            if (grid[newPosition.x][newPosition.y] === 1) {
+                continue;
+            }
+
+            let newNode = new Node(newPosition.x, newPosition.y, currentNode);
+
+            if (closedList.some(node => node.isEqual(newNode))) {
+                continue;
+            }
+
+            newNode.g = currentNode.g + 1;
+            newNode.h = Math.abs(newNode.x - end.x) + Math.abs(newNode.y - end.y);
+            newNode.f = newNode.g + newNode.h;
+
+            if (openList.some(node => node.isEqual(newNode) && newNode.g >= node.g)) {
+                continue;
+            }
+
+            openList.push(newNode);
+        }
     }
+
+    return [];
 }
 
-// Example graph
-let vertices = ['A', 'B', 'C', 'D', 'E'];
-let edges = [
-    new Edge('A', 'B', 4),
-    new Edge('A', 'C', 2),
-    new Edge('B', 'C', 5),
-    new Edge('B', 'D', 10),
-    new Edge('C', 'D', 3),
-    new Edge('D', 'E', 7),
-    new Edge('E', 'A', 3)
+// Example usage
+let grid = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0]
 ];
 
-let bellmanFord = new BellmanFord(vertices, edges);
-bellmanFord.bellmanFord('A');
+let start = new Node(0, 0);
+let end = new Node(3, 3);
+
+let path = astar(start, end, grid);
+console.log(path);
