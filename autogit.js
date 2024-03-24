0a933bce00df09b7a08087ca376d45e7f0b9afec
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:12:10 +0800

    3-24-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,8 @@
-function tarjanStronglyConnectedComponents(graph) {
-    // Initialize variables
-    let index = 0;
-    let stack = [];
-    let indices = {};
-    let lowlinks = {};
-    let onStack = {};
-    let components = [];
-
-    function strongConnect(node) {
-        indices[node] = index;
-        lowlinks[node] = index;
-        index++;
-        stack.push(node);
-        onStack[node] = true;
-
-        graph[node].forEach(neighbor => {
-            if (typeof indices[neighbor] === 'undefined') {
-                strongConnect(neighbor);
-                lowlinks[node] = Math.min(lowlinks[node], lowlinks[neighbor]);
-            } else if (onStack[neighbor]) {
-                lowlinks[node] = Math.min(lowlinks[node], indices[neighbor]);
-            }
-        });
-
-        if (indices[node] === lowlinks[node]) {
-            let component = [];
-            let nextNode = null;
-            do {
-                nextNode = stack.pop();
-                onStack[nextNode] = false;
-                component.push(nextNode);
-            } while (nextNode !== node);
-            components.push(component);
-        }
-    }
-
-    // Start the algorithm
-    Object.keys(graph).forEach(node => {
-        if (typeof indices[node] === 'undefined') {
-            strongConnect(node);
-        }
-    });
-
-    return components;
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-// Example usage
-const graph = {
-    0: [1],
-    1: [2],
-    2: [0, 3],
-    3: [4],
-    4: [5, 6],
-    5: [0],
-    6: [3, 7],
-    7: [6]
-};
+const originalString = 'Hello, World!';
+const reversedString = reverseString(originalString);
 
-const components = tarjanStronglyConnectedComponents(graph);
-console.log(components);
+console.log(reversedString); // Output: '!dlroW ,olleH'
