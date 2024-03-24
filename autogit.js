bbdf65b4e3df526d58922038b4a0785ba16ad2a7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:02:02 +0800

    3-24-2024 10:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,4 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
+let str = "   Hello, World!   ";
+let trimmedStr = str.replace(/\s/g, "");
 
-function maxDepth(node) {
-    if (!node) {
-        return 0;
-    }
-    
-    let leftDepth = maxDepth(node.left);
-    let rightDepth = maxDepth(node.right);
-    
-    return 1 + Math.max(leftDepth, rightDepth);
-}
-
-// Example binary tree
-let root = new Node(1);
-root.left = new Node(2);
-root.right = new Node(3);
-root.left.left = new Node(4);
-root.left.right = new Node(5);
-
-console.log("Maximum depth of the binary tree is: " + maxDepth(root));
+console.log(trimmedStr); // Output: "Hello,World!"
