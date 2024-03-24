38b8888f6fc8c21e7da3f4a999411ab1810e2690
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:32:19 +0800

    3-24-2024 9:32 [User mode]

---
+++ autogit.js
@@ -0,0 +1,17 @@
+function findCommonElements(array1, array2) {
+  let commonElements = [];
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array2.includes(array1[i])) {
+      commonElements.push(array1[i]);
+    }
+  }
+  
+  return commonElements;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+
+const result = findCommonElements(array1, array2);
+console.log(result); // Output: [3, 4, 5]
