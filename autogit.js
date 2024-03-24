2031e401d16b6cb5606341e0482673ac76cec80c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:37:29 +0800

    3-24-2024 9:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,63 @@
-function mergeSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
+// Node class to represent each node in the binary tree
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
-
-    const n = arr.length;
-    const aux = new Array(n);
-    let step = 1;
-
-    while (step < n) {
-        let left = 0;
-        while (left + step < n) {
-            const mid = left + step;
-            const right = Math.min(left + 2 * step, n);
-            merge(arr, aux, left, mid, right);
-            left += 2 * step;
-        }
-        step *= 2;
-    }
-
-    return arr;
 }
 
-function merge(arr, aux, left, mid, right) {
-    let i = left;
-    let j = mid;
-    for (let k = left; k < right; k++) {
-        if (i < mid && (j >= right || arr[i] <= arr[j])) {
-            aux[k] = arr[i];
-            i++;
+// BinarySearchTree class to represent the binary tree
+class BinarySearchTree {
+    constructor() {
+        // root of the binary tree
+        this.root = null;
+    }
+    
+    // function to insert a new node with given data
+    insert(data) {
+        const newNode = new Node(data);
+        if (this.root === null) {
+            this.root = newNode;
         } else {
-            aux[k] = arr[j];
-            j++;
+            this.insertNode(this.root, newNode);
         }
     }
-    for (let k = left; k < right; k++) {
-        arr[k] = aux[k];
+    
+    // recursive function to insert a new node in the binary tree
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+    
+    // function to traverse the binary tree in inorder (left -> root -> right) fashion
+    inorder(node) {
+        if (node !== null) {
+            this.inorder(node.left);
+            console.log(node.data);
+            this.inorder(node.right);
+        }
     }
 }
 
 // Usage
-const arr = [8, 4, 1, 9, 6, 3, 7, 2, 5];
-console.log(mergeSort(arr));
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(20);
+bst.insert(3);
+bst.insert(8);
+
+// Print the binary tree in inorder traversal
+bst.inorder(bst.root);
