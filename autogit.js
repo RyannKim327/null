d8419358cb6c64b25f740dcfd9b94e3da540dab9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:13:38 +0800

    3-24-2024 9:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,15 @@
-const array = [1, 2, 3, 2, 4, 3];
-const uniqueArray = [...new Set(array)];
-console.log(uniqueArray); // [1, 2, 3, 4]
-const array = [1, 2, 3, 2, 4, 3];
-const uniqueArray = array.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArray); // [1, 2, 3, 4]
-const array = [1, 2, 3, 2, 4, 3];
-const uniqueArray = array.reduce((accumulator, currentValue) => {
-  if (!accumulator.includes(currentValue)) {
-    accumulator.push(currentValue);
-  }
-  return accumulator;
-}, []);
-console.log(uniqueArray); // [1, 2, 3, 4]
+let numbers = [1, 2, 3, 4, 5];
+
+// Remove element at index 2 (which is 3)
+numbers.splice(2, 1);
+
+console.log(numbers); // Output: [1, 2, 4, 5]
+let numbers = [1, 2, 3, 4, 5];
+let elementToRemove = 3;
+
+let index = numbers.indexOf(elementToRemove);
+if (index !== -1) {
+    numbers.splice(index, 1);
+}
+
+console.log(numbers); // Output: [1, 2, 4, 5]
