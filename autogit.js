b28985659108a74b7887afaf4690eeda81e75829
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:12:56 +0800

    3-24-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,3 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    let i = m, j = n;
-    let result = '';
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            result = str1[i - 1] + result;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result;
-}
-
-const str1 = "ABCBDAB";
-const str2 = "BDCAB";
-console.log(longestCommonSubsequence(str1, str2)); // Output: BCAB
+let numbers = [5, 3, 8, 1, 2, 9, 4, 7, 6];
+numbers.sort((a, b) => a - b);
+console.log(numbers); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
