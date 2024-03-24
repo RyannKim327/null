142dd26c3547847d8bae43d3899f52dbd86a390a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:26:18 +0800

    3-24-2024 10:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,55 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
+function tarjan(graph) {
+    let index = 0;
+    let stack = [];
+    let result = [];
+
+    function strongconnect(node) {
+        node.index = index;
+        node.lowlink = index;
+        index++;
+        stack.push(node);
+        node.onStack = true;
+
+        for (let neighbor of graph[node]) {
+            if (neighbor.index === undefined) {
+                strongconnect(neighbor);
+                node.lowlink = Math.min(node.lowlink, neighbor.lowlink);
+            } else if (neighbor.onStack) {
+                node.lowlink = Math.min(node.lowlink, neighbor.index);
+            }
+        }
+
+        if (node.lowlink === node.index) {
+            let component = [];
+            let currentNode;
+            do {
+                currentNode = stack.pop();
+                currentNode.onStack = false;
+                component.push(currentNode);
+            } while (currentNode !== node);
+            result.push(component);
+        }
+    }
+
+    for (let node of Object.keys(graph)) {
+        if (graph[node].index === undefined) {
+            strongconnect(node);
+        }
+    }
+
+    return result;
 }
 
-// Example
-const myString = "Hello, World!";
-const reversedString = reverseString(myString);
-console.log(reversedString); // Outputs: "!dlroW ,olleH"
+// Example usage
+let graph = {
+    0: [1],
+    1: [2],
+    2: [0, 3],
+    3: [4],
+    4: [5, 6],
+    5: [0],
+    6: [4]
+};
+
+let components = tarjan(graph);
+console.log(components);
