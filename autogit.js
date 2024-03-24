5acb73ccf76d6ca688a5df0f9c25a0c68bf607f1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:16:45 +0800

    3-24-2024 9:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,18 @@
-function computeLPSArray(pattern) {
-    let lps = new Array(pattern.length).fill(0);
-    let len = 0;
-    let i = 1;
-    
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len !== 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
-        }
-    }
-    
-    return lps;
+function fetchData() {
+  fetch('https://api.example.com/data')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      // Do something with the fetched data
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 }
 
-function kmpSearch(text, pattern) {
-    let lps = computeLPSArray(pattern);
-    let i = 0;
-    let j = 0;
-    let matches = [];
-    
-    while (i < text.length) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-        
-        if (j === pattern.length) {
-            matches.push(i - j);
-            j = lps[j - 1];
-        } else if (i < text.length && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-    
-    return matches;
-}
-
-// Example usage
-let text = "ABABDABACDABABCABAB";
-let pattern = "ABABCABAB";
-let result = kmpSearch(text, pattern);
-console.log(result);
+fetchData();
