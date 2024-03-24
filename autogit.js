f8696bc37a5462b7b1134ff856b3386907465a3c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:06:03 +0800

    3-24-2024 9:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,26 @@
-// Node class for creating tree nodes
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-// Function to count the number of leaf nodes in a binary tree
-function countLeafNodes(root) {
-    if (root === null) {
-        return 0;
+function hasCycle(head) {
+  if (!head) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow === fast) {
+      return true;
     }
-    if (root.left === null && root.right === null) {
-        return 1;
-    } else {
-        return countLeafNodes(root.left) + countLeafNodes(root.right);
-    }
+  }
+
+  return false;
 }
-
-// Create a sample binary tree
-let rootNode = new Node(1);
-rootNode.left = new Node(2);
-rootNode.right = new Node(3);
-rootNode.left.left = new Node(4);
-rootNode.left.right = new Node(5);
-rootNode.right.left = new Node(6);
-rootNode.right.right = new Node(7);
-
-// Count the number of leaf nodes
-const leafNodeCount = countLeafNodes(rootNode);
-console.log("Number of leaf nodes in the binary tree: " + leafNodeCount);
