9dfe35ba2de2cb6981454236148cc983840360ad
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:29:03 +0800

    3-24-2024 10:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,9 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+function findCommonElements(arr1, arr2) {
+    return arr1.filter(element => arr2.includes(element));
 }
 
-function findIntersection(list1, list2) {
-    let set = new Set();
-    
-    // Traverse list1 and store nodes in a Set
-    let current = list1;
-    while (current) {
-        set.add(current);
-        current = current.next;
-    }
-    
-    // Traverse list2 and check for nodes in the Set
-    current = list2;
-    while (current) {
-        if (set.has(current)) {
-            return current;
-        }
-        current = current.next;
-    }
-    
-    return null; // No intersection found
-}
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-// Example
-let commonNode = new Node(10);
-
-let list1 = new Node(2);
-list1.next = new Node(4);
-list1.next.next = new Node(6);
-list1.next.next.next = commonNode;
-
-let list2 = new Node(1);
-list2.next = new Node(3);
-list2.next.next = commonNode;
-
-let intersection = findIntersection(list1, list2);
-console.log("Intersection node value:", intersection.value);
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
