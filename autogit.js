5887058b8c3f6fe59ead7f3e2ff7bc3a8cd4b9d2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:42:59 +0800

    3-24-2024 9:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,119 +1,114 @@
 class Node {
     constructor(value) {
         this.value = value;
+        this.height = 1;
         this.left = null;
         this.right = null;
-        this.parent = null;
-        this.color = "red"; // New nodes are always red
     }
 }
 
-class RedBlackTree {
+class AVLTree {
     constructor() {
         this.root = null;
     }
 
-    // Helper method to perform left rotation
-    rotateLeft(node) {
-        let right = node.right;
-        node.right = right.left;
-        if (right.left) {
-            right.left.parent = node;
+    getHeight(node) {
+        if (node === null) {
+            return 0;
         }
-        right.parent = node.parent;
-        if (!node.parent) {
-            this.root = right;
-        } else if (node === node.parent.left) {
-            node.parent.left = right;
-        } else {
-            node.parent.right = right;
-        }
-        right.left = node;
-        node.parent = right;
+        return node.height;
     }
 
-    // Helper method to perform right rotation
-    rotateRight(node) {
-        let left = node.left;
-        node.left = left.right;
-        if (left.right) {
-            left.right.parent = node;
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
         }
-        left.parent = node.parent;
-        if (!node.parent) {
-            this.root = left;
-        } else if (node === node.parent.right) {
-            node.parent.right = left;
-        } else {
-            node.parent.left = left;
-        }
-        left.right = node;
-        node.parent = left;
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    rotateRight(y) {
+        let x = y.left;
+        let T = x.right;
+     
+        x.right = y;
+        y.left = T;
+     
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+     
+        return x;
+    }
+
+    rotateLeft(x) {
+        let y = x.right;
+        let T = y.left;
+     
+        y.left = x;
+        x.right = T;
+     
+        x.height = Math.max(this.getHeight(x.left), this.getHeight(x.right)) + 1;
+        y.height = Math.max(this.getHeight(y.left), this.getHeight(y.right)) + 1;
+     
+        return y;
     }
 
     insert(value) {
-        let newNode = new Node(value);
-        let currentNode = this.root;
-        let parentNode = null;
-
-        while (currentNode) {
-            parentNode = currentNode;
-            if (newNode.value < currentNode.value) {
-                currentNode = currentNode.left;
-            } else {
-                currentNode = currentNode.right;
-            }
-        }
-
-        newNode.parent = parentNode;
-        if (!parentNode) {
-            this.root = newNode;
-            this.root.color = "black";
-        } else if (newNode.value < parentNode.value) {
-            parentNode.left = newNode;
-        } else {
-            parentNode.right = newNode;
-        }
-
-        this.fixInsert(newNode);
+        this.root = this.insertNode(this.root, value);
     }
 
-    fixInsert(node) {
-        while (node !== this.root && node.parent.color === "red") {
-            if (node.parent === node.parent.parent.left) {
-                let uncle = node.parent.parent.right;
-                if (uncle && uncle.color === "red") {
-                    node.parent.color = "black";
-                    uncle.color = "black";
-                    node.parent.parent.color = "red";
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.right) {
-                        node = node.parent;
-                        this.rotateLeft(node);
-                    }
-                    node.parent.color = "black";
-                    node.parent.parent.color = "red";
-                    this.rotateRight(node.parent.parent);
-                }
-            } else {
-                let uncle = node.parent.parent.left;
-                if (uncle && uncle.color === "red") {
-                    node.parent.color = "black";
-                    uncle.color = "black";
-                    node.parent.parent.color = "red";
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.left) {
-                        node = node.parent;
-                        this.rotateRight(node);
-                    }
-                    node.parent.color = "black";
-                    node.parent.parent.color = "red";
-                    this.rotateLeft(node.parent.parent);
-                }
-            }
+    insertNode(node, value) {
+        if (node === null) {
+            return new Node(value);
         }
-        this.root.color = "black";
+
+        if (value < node.value) {
+            node.left = this.insertNode(node.left, value);
+        } else if (value > node.value) {
+            node.right = this.insertNode(node.right, value);
+        } else {
+            return node;
+        }
+
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+
+        let balance = this.getBalanceFactor(node);
+
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    inorder(node) {
+        if (node !== null) {
+            this.inorder(node.left);
+            console.log(node.value);
+            this.inorder(node.right);
+        }
     }
 }
+
+// Usage
+let avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(20);
+avlTree.insert(30);
+avlTree.insert(40);
+avlTree.insert(50);
+avlTree.insert(25);
+avlTree.inorder(avlTree.root);
