04e0e7c814d78bcfa29c2bb42d5e231c2e1335a9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:24:13 +0800

    3-24-2024 10:24 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,44 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
+function mergeSortIterative(arr) {
+    const n = arr.length;
+    const temp = new Array(n);
+    
+    for (let size = 1; size < n; size *= 2) {
+        for (let leftStart = 0; leftStart < n - 1; leftStart += 2 * size) {
+            let mid = Math.min(leftStart + size, n);
+            let rightEnd = Math.min(leftStart + 2 * size, n);
+            merge(arr, leftStart, mid, rightEnd, temp);
         }
     }
-
-    return quickSort(left).concat(pivot, quickSort(right));
+    
+    return arr;
 }
 
-const arr = [5, 3, 7, 2, 8, 4, 1, 6];
-const sortedArr = quickSort(arr);
-console.log(sortedArr);
+function merge(arr, leftStart, mid, rightEnd, temp) {
+    let left = leftStart;
+    let right = mid;
+    let index = leftStart;
+    
+    while (left < mid && right < rightEnd) {
+        if (arr[left] <= arr[right]) {
+            temp[index++] = arr[left++];
+        } else {
+            temp[index++] = arr[right++];
+        }
+    }
+    
+    while (left < mid) {
+        temp[index++] = arr[left++];
+    }
+    
+    while (right < rightEnd) {
+        temp[index++] = arr[right++];
+    }
+    
+    for (let i = leftStart; i < rightEnd; i++) {
+        arr[i] = temp[i];
+    }
+}
+
+// Example usage
+const arr = [38, 27, 43, 3, 9, 82, 10];
+console.log(mergeSortIterative(arr));
