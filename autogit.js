4baaa3c4f14c00aa3a5b09cfa0ea2747fe547a5d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:34:07 +0800

    3-24-2024 10:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,99 +1,50 @@
+// Node class to represent a node in the linked list
 class Node {
-    constructor(value, level) {
-        this.value = value;
-        this.next = new Array(level).fill(null);
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
     }
 }
 
-class SkipList {
-    constructor(maxLevel, probability) {
-        this.maxLevel = maxLevel;
-        this.probability = probability;
-        this.head = new Node(-Infinity, maxLevel);
-        this.level = 0;
+// LinkedList class to manage the linked list
+class LinkedList {
+    constructor() {
+        this.head = null;
     }
 
-    randomLevel() {
-        let level = 0;
-        while (Math.random() < this.probability && level < this.maxLevel) {
-            level++;
+    // Insert a new node at the beginning of the linked list
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    // Insert a new node at the end of the linked list
+    insertLast(data) {
+        let newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
         }
-        return level;
-    }
 
-    insert(value) {
-        const update = new Array(this.maxLevel);
         let current = this.head;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.next[i] && current.next[i].value < value) {
-                current = current.next[i];
-            }
-            update[i] = current;
+        while (current.next) {
+            current = current.next;
         }
-
-        current = current.next[0];
-
-        if (current === null || current.value !== value) {
-            const newNodeLevel = this.randomLevel();
-            const newNode = new Node(value, newNodeLevel);
-
-            if (newNodeLevel > this.level) {
-                for (let i = this.level + 1; i < newNodeLevel; i++) {
-                    update[i] = this.head;
-                }
-                this.level = newNodeLevel;
-            }
-
-            for (let i = 0; i < newNodeLevel; i++) {
-                newNode.next[i] = update[i].next[i];
-                update[i].next[i] = newNode;
-            }
-        }
+        current.next = newNode;
     }
 
-    search(value) {
+    // Print all the elements of the linked list
+    printList() {
         let current = this.head;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.next[i] && current.next[i].value < value) {
-                current = current.next[i];
-            }
-        }
-
-        current = current.next[0];
-
-        if (current !== null && current.value === value) {
-            return current;
-        } else {
-            return null;
-        }
-    }
-
-    remove(value) {
-        const update = new Array(this.maxLevel);
-        let current = this.head;
-
-        for (let i = this.level; i >= 0; i--) {
-            while (current.next[i] && current.next[i].value < value) {
-                current = current.next[i];
-            }
-            update[i] = current;
-        }
-
-        current = current.next[0];
-
-        if (current !== null && current.value === value) {
-            for (let i = 0; i <= this.level; i++) {
-                if (update[i].next[i] !== current) {
-                    break;
-                }
-                update[i].next[i] = current.next[i];
-            }
-
-            while (this.level > 0 && this.head.next[this.level] === null) {
-                this.level--;
-            }
+        while (current) {
+            console.log(current.data);
+            current = current.next;
         }
     }
 }
+
+// Usage example
+const linkedList = new LinkedList();
+linkedList.insertFirst(5);
+linkedList.insertFirst(10);
+linkedList.insertLast(15);
+linkedList.printList();
