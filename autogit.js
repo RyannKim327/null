34c5b46b3d2bbc815972540d34a3c5af97b74c64
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:25:27 +0800

    3-24-2024 9:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,18 @@
-const cron = require('node-cron');
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
 
-cron.schedule('* * * * *', () => {
-  console.log('Running a task every minute!');
-});
+// Example usage
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Unsorted array:", arr);
+console.log("Sorted array:", bubbleSort(arr));
