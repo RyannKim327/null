728e4eb47c9d0d81d0dc4f48cb5fefcfecae046f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:06:23 +0800

    3-24-2024 10:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,27 @@
-const axios = require('axios');
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+function maxDepth(root) {
+    if (root === null) {
+        return 0;
+    }
+    
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+    
+    return 1 + Math.max(leftDepth, rightDepth);
+}
+
+// Example binary tree
+const root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+
+console.log("Maximum depth of the binary tree:", maxDepth(root)); // Output: 3
