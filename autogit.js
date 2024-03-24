9cb3f4b5571a929101d110acfb175d5832a588ca
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:20:01 +0800

    3-24-2024 9:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,6 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  add(value) {
-    if (!this.head) {
-      this.head = new Node(value);
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = new Node(value);
-    }
-  }
-
-  isPalindrome() {
-    let values = [];
-    let current = this.head;
-
-    while (current) {
-      values.push(current.value);
-      current = current.next;
-    }
-
-    let left = 0;
-    let right = values.length - 1;
-
-    while (left < right) {
-      if (values[left] !== values[right]) {
-        return false;
-      }
-      left++;
-      right--;
-    }
-
-    return true;
-  }
-}
-
-// Example usage
-let linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(2);
-linkedList.add(1);
-
-console.log(linkedList.isPalindrome()); // Output: true
+let str = "123";
+let num = parseInt(str);
+console.log(num); // Output: 123
+let str = "456";
+let num = +str;
+console.log(num); // Output: 456
