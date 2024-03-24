e0401fee20cf0034c84339a8f74576b996de704f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:19:32 +0800

    3-24-2024 9:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,7 @@
-let numbers = [5, 2, 8, 4, 1];
+const cron = require('node-cron');
 
-numbers.sort(function(a, b) {
-    return a - b;
+// Schedule a task to run every minute
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute!');
 });
-
-console.log(numbers); // Output: [1, 2, 4, 5, 8]
-let numbers = [5, 2, 8, 4, 1];
-
-numbers.sort(function(a, b) {
-    return b - a;
-});
-
-console.log(numbers); // Output: [8, 5, 4, 2, 1]
+npm install node-cron
