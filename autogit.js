83c1941d67ac25b7ced59b7a270c0d34ae9a63a5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:18:45 +0800

    3-24-2024 10:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,11 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+let numbers = [4, 2, 8, 5, 1];
+numbers.sort(function(a, b) {
+  return a - b;
+});
 
-function reverseLinkedList(head) {
-  let prev = null;
-  let current = head;
-  let next = null;
+console.log(numbers);
+numbers.sort(function(a, b) {
+  return b - a;
+});
 
-  while (current !== null) {
-    next = current.next;
-    current.next = prev;
-    prev = current;
-    current = next;
-  }
-
-  // Update the head of the reversed list
-  head = prev;
-
-  return head;
-}
-
-// Sample linked list creation
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-
-console.log("Original linked list:");
-let temp = head;
-while (temp !== null) {
-  console.log(temp.data);
-  temp = temp.next;
-}
-
-head = reverseLinkedList(head);
-
-console.log("Reversed linked list:");
-temp = head;
-while (temp !== null) {
-  console.log(temp.data);
-  temp = temp.next;
-}
+console.log(numbers);
