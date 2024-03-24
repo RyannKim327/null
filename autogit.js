00417e3d403861303e61834b17e4b8fc60859d6a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:30:56 +0800

    3-24-2024 9:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,11 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+function findSecondLargest(arr) {
+    arr.sort(function(a, b) {
+        return b - a;
+    });
+    return arr[1];
 }
 
-let originalString = "Hello, World!";
-let newString = removeVowels(originalString);
-
-console.log(newString); // Output: "Hll, Wrld!"
+// Example usage
+let arr = [10, 5, 15, 20];
+let secondLargest = findSecondLargest(arr);
+console.log("The second largest element is: " + secondLargest);
