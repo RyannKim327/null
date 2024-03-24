ad948edf71681068d9c07e478f0ebaf2d345005e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:10:01 +0800

    3-24-2024 10:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,75 +1,40 @@
-class PriorityQueue {
-    constructor() {
-        this.heap = [];
-    }
-
-    size() {
-        return this.heap.length;
-    }
-
-    empty() {
-        return this.heap.length === 0;
-    }
-
-    insert(value, priority) {
-        const newNode = { value, priority };
-        this.heap.push(newNode);
-
-        let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor((currentIndex - 1) / 2);
-
-        while (
-            currentIndex > 0 && 
-            this.heap[currentIndex].priority < this.heap[parentIndex].priority
-        ) {
-            [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
-            currentIndex = parentIndex;
-            parentIndex = Math.floor((currentIndex - 1) / 2);
-        }
-    }
-
-    extractMin() {
-        if (this.heap.length === 0) {
-            return null;
-        }
-
-        const minNode = this.heap[0];
-        this.heap[0] = this.heap[this.heap.length - 1];
-        this.heap.pop();
-
-        let currentIndex = 0;
-
-        while (true) {
-            let leftChildIndex = 2 * currentIndex + 1;
-            let rightChildIndex = 2 * currentIndex + 2;
-            let smallest = currentIndex;
-
-            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].priority < this.heap[smallest].priority) {
-                smallest = leftChildIndex;
-            }
-
-            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[smallest].priority) {
-                smallest = rightChildIndex;
-            }
-
-            if (smallest !== currentIndex) {
-                [this.heap[currentIndex], this.heap[smallest]] = [this.heap[smallest], this.heap[currentIndex]];
-                currentIndex = smallest;
-            } else {
-                break;
-            }
-        }
-
-        return minNode;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
 }
 
-// Example usage
-const pq = new PriorityQueue();
-pq.insert('task1', 10);
-pq.insert('task2', 5);
-pq.insert('task3', 12);
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
 
-console.log(pq.extractMin()); // { value: 'task2', priority: 5 }
-console.log(pq.extractMin()); // { value: 'task1', priority: 10 }
-console.log(pq.extractMin()); // { value: 'task3', priority: 12 }
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Example usage:
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+// Uncomment the line below to create a cycle in the linked list
+// node4.next = node2;
+
+console.log(hasCycle(node1)); // Output: false
