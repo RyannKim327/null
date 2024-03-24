c9a25338c404b110f7a64f5607e195757e5f5242
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:25:57 +0800

    3-24-2024 9:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,12 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-function isPalindromeLinkedList(head) {
-  let values = [];
-  let current = head;
-  
-  while (current !== null) {
-    values.push(current.value);
-    current = current.next;
-  }
-  
-  let left = 0;
-  let right = values.length - 1;
-  
-  while (left < right) {
-    if (values[left] !== values[right]) {
-      return false;
+function customStringLength(str) {
+    let length = 0;
+    while (str[length] !== undefined) {
+        length++;
     }
-    left++;
-    right--;
-  }
-  
-  return true;
+    return length;
 }
 
 // Example usage
-let node1 = new Node(1);
-let node2 = new Node(2);
-let node3 = new Node(3);
-let node4 = new Node(3);
-let node5 = new Node(2);
-let node6 = new Node(1);
-
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-node5.next = node6;
-
-console.log(isPalindromeLinkedList(node1)); // Output: true
+const str = "Hello, World!";
+const length = customStringLength(str);
+console.log("Length of the string: ", length); // Output: 13
