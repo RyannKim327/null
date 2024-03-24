760906ecfbca57b755bed4de1adae6e3006a8f28
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:09:52 +0800

    3-24-2024 10:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,75 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  addNode(value) {
-    const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  findNthNodeFromEnd(n) {
-    let pointer1 = this.head;
-    let pointer2 = this.head;
-
-    // Move pointer2 to the nth node from the beginning
-    for (let i = 0; i < n; i++) {
-      if (pointer2 === null) {
-        return null; // Out of bounds
-      }
-      pointer2 = pointer2.next;
+class PriorityQueue {
+    constructor() {
+        this.heap = [];
     }
 
-    // Move both pointers until pointer2 reaches the end
-    while (pointer2 !== null) {
-      pointer1 = pointer1.next;
-      pointer2 = pointer2.next;
+    size() {
+        return this.heap.length;
     }
 
-    return (pointer1 !== null) ? pointer1.value : null;
-  }
+    empty() {
+        return this.heap.length === 0;
+    }
+
+    insert(value, priority) {
+        const newNode = { value, priority };
+        this.heap.push(newNode);
+
+        let currentIndex = this.heap.length - 1;
+        let parentIndex = Math.floor((currentIndex - 1) / 2);
+
+        while (
+            currentIndex > 0 && 
+            this.heap[currentIndex].priority < this.heap[parentIndex].priority
+        ) {
+            [this.heap[currentIndex], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[currentIndex]];
+            currentIndex = parentIndex;
+            parentIndex = Math.floor((currentIndex - 1) / 2);
+        }
+    }
+
+    extractMin() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+
+        const minNode = this.heap[0];
+        this.heap[0] = this.heap[this.heap.length - 1];
+        this.heap.pop();
+
+        let currentIndex = 0;
+
+        while (true) {
+            let leftChildIndex = 2 * currentIndex + 1;
+            let rightChildIndex = 2 * currentIndex + 2;
+            let smallest = currentIndex;
+
+            if (leftChildIndex < this.heap.length && this.heap[leftChildIndex].priority < this.heap[smallest].priority) {
+                smallest = leftChildIndex;
+            }
+
+            if (rightChildIndex < this.heap.length && this.heap[rightChildIndex].priority < this.heap[smallest].priority) {
+                smallest = rightChildIndex;
+            }
+
+            if (smallest !== currentIndex) {
+                [this.heap[currentIndex], this.heap[smallest]] = [this.heap[smallest], this.heap[currentIndex]];
+                currentIndex = smallest;
+            } else {
+                break;
+            }
+        }
+
+        return minNode;
+    }
 }
 
 // Example usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-linkedList.addNode(5);
+const pq = new PriorityQueue();
+pq.insert('task1', 10);
+pq.insert('task2', 5);
+pq.insert('task3', 12);
 
-console.log(linkedList.findNthNodeFromEnd(2)); // Output: 4
+console.log(pq.extractMin()); // { value: 'task2', priority: 5 }
+console.log(pq.extractMin()); // { value: 'task1', priority: 10 }
+console.log(pq.extractMin()); // { value: 'task3', priority: 12 }
