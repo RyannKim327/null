73497f599e160c56f93cd353652b9426dfc06864
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:08:39 +0800

    3-24-2024 10:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,17 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+function isAnagram(str1, str2) {
+    //Remove non-alphabetic characters and convert to lowercase
+    const cleanStr1 = str1.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+    const cleanStr2 = str2.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+
+    //Check if the sorted strings are equal
+    return cleanStr1 === cleanStr2;
 }
 
-let inputString = "Hello, World!";
-let outputString = removeVowels(inputString);
-console.log(outputString); // Output: "Hll, Wrld!"
+// Test the function
+const string1 = "Listen";
+const string2 = "Silent";
+if (isAnagram(string1, string2)) {
+    console.log("The strings are anagrams.");
+} else {
+    console.log("The strings are not anagrams.");
+}
