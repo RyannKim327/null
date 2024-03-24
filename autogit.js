23d25dbb3694ef77b8290850f66461ace9877df5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:40:55 +0800

    3-24-2024 9:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,9 @@
-function areAnagrams(str1, str2) {
-    // Remove non-alphabetic characters and convert to lowercase
-    const cleanStr1 = str1.replace(/[^a-z]/ig, '').toLowerCase();
-    const cleanStr2 = str2.replace(/[^a-z]/ig, '').toLowerCase();
+const axios = require('axios');
 
-    // Sort the characters in the strings
-    const sortedStr1 = cleanStr1.split('').sort().join('');
-    const sortedStr2 = cleanStr2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
-}
-
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
