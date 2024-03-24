974b39e63733f0ef4b5facdbfe55cbfaf5a6e1b2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:26:59 +0800

    3-24-2024 10:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,7 @@
-let str = 'Hello, world!';
-let substring = 'world';
-
-if (str.includes(substring)) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, "");  // using regex to match all vowels and replacing them with an empty string
 }
-let str = 'Hello, world!';
-let substring = 'WORLD';
 
-if (str.toLowerCase().includes(substring.toLowerCase())) {
-    console.log('The string contains the substring');
-} else {
-    console.log('The string does not contain the substring');
-}
+let originalString = "Hello, World!";
+let stringWithoutVowels = removeVowels(originalString);
+console.log(stringWithoutVowels);  // Output: "Hll, Wrld!"
