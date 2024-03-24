b19e81a931e91910e1070afb710af45b94bf0867
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:35:34 +0800

    3-24-2024 10:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,5 @@
-class TrieNode {
-    constructor() {
-        this.children = {};
-        this.isEndOfWord = false;
-    }
-}
+let string1 = "Hello";
+let string2 = "World";
 
-class Trie {
-    constructor() {
-        this.root = new TrieNode();
-    }
-
-    insert(word) {
-        let currentNode = this.root;
-        for (let i = 0; i < word.length; i++) {
-            const char = word[i];
-            if (!(char in currentNode.children)) {
-                currentNode.children[char] = new TrieNode();
-            }
-            currentNode = currentNode.children[char];
-        }
-        currentNode.isEndOfWord = true;
-    }
-
-    search(word) {
-        let currentNode = this.root;
-        for (let i = 0; i < word.length; i++) {
-            const char = word[i];
-            if (!(char in currentNode.children)) {
-                return false;
-            }
-            currentNode = currentNode.children[char];
-        }
-        return currentNode.isEndOfWord;
-    }
-
-    startsWith(prefix) {
-        let currentNode = this.root;
-        for (let i = 0; i < prefix.length; i++) {
-            const char = prefix[i];
-            if (!(char in currentNode.children)) {
-                return false;
-            }
-            currentNode = currentNode.children[char];
-        }
-        return true;
-    }
-}
-
-// Example usage:
-const trie = new Trie();
-trie.insert("apple");
-console.log(trie.search("apple")); // Output: true
-console.log(trie.search("app")); // Output: false
-console.log(trie.startsWith("app")); // Output: true
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString); // Output: "Hello World"
