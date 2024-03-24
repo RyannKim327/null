0eca69fd7971918c084137670bdbde96f1855d8f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:19:27 +0800

    3-24-2024 9:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,14 @@
-const axios = require('axios');
+let numbers = [5, 2, 8, 4, 1];
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+numbers.sort(function(a, b) {
+    return a - b;
+});
+
+console.log(numbers); // Output: [1, 2, 4, 5, 8]
+let numbers = [5, 2, 8, 4, 1];
+
+numbers.sort(function(a, b) {
+    return b - a;
+});
+
+console.log(numbers); // Output: [8, 5, 4, 2, 1]
