76f95db50db38327f2a0d384828cae001330c099
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:42:40 +0800

    3-24-2024 9:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,119 @@
-function isSortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.color = "red"; // New nodes are always red
     }
-    return true;
 }
 
-// Example usage
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [5, 4, 3, 2, 1];
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
 
-console.log(isSortedAscending(array1)); // Output: true
-console.log(isSortedAscending(array2)); // Output: false
+    // Helper method to perform left rotation
+    rotateLeft(node) {
+        let right = node.right;
+        node.right = right.left;
+        if (right.left) {
+            right.left.parent = node;
+        }
+        right.parent = node.parent;
+        if (!node.parent) {
+            this.root = right;
+        } else if (node === node.parent.left) {
+            node.parent.left = right;
+        } else {
+            node.parent.right = right;
+        }
+        right.left = node;
+        node.parent = right;
+    }
+
+    // Helper method to perform right rotation
+    rotateRight(node) {
+        let left = node.left;
+        node.left = left.right;
+        if (left.right) {
+            left.right.parent = node;
+        }
+        left.parent = node.parent;
+        if (!node.parent) {
+            this.root = left;
+        } else if (node === node.parent.right) {
+            node.parent.right = left;
+        } else {
+            node.parent.left = left;
+        }
+        left.right = node;
+        node.parent = left;
+    }
+
+    insert(value) {
+        let newNode = new Node(value);
+        let currentNode = this.root;
+        let parentNode = null;
+
+        while (currentNode) {
+            parentNode = currentNode;
+            if (newNode.value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+
+        newNode.parent = parentNode;
+        if (!parentNode) {
+            this.root = newNode;
+            this.root.color = "black";
+        } else if (newNode.value < parentNode.value) {
+            parentNode.left = newNode;
+        } else {
+            parentNode.right = newNode;
+        }
+
+        this.fixInsert(newNode);
+    }
+
+    fixInsert(node) {
+        while (node !== this.root && node.parent.color === "red") {
+            if (node.parent === node.parent.parent.left) {
+                let uncle = node.parent.parent.right;
+                if (uncle && uncle.color === "red") {
+                    node.parent.color = "black";
+                    uncle.color = "black";
+                    node.parent.parent.color = "red";
+                    node = node.parent.parent;
+                } else {
+                    if (node === node.parent.right) {
+                        node = node.parent;
+                        this.rotateLeft(node);
+                    }
+                    node.parent.color = "black";
+                    node.parent.parent.color = "red";
+                    this.rotateRight(node.parent.parent);
+                }
+            } else {
+                let uncle = node.parent.parent.left;
+                if (uncle && uncle.color === "red") {
+                    node.parent.color = "black";
+                    uncle.color = "black";
+                    node.parent.parent.color = "red";
+                    node = node.parent.parent;
+                } else {
+                    if (node === node.parent.left) {
+                        node = node.parent;
+                        this.rotateRight(node);
+                    }
+                    node.parent.color = "black";
+                    node.parent.parent.color = "red";
+                    this.rotateLeft(node.parent.parent);
+                }
+            }
+        }
+        this.root.color = "black";
+    }
+}
