d04ac3ceb6add24a864bbc0fd1c5d2c34b4d23a2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:19:56 +0800

    3-24-2024 9:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,57 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  add(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+  }
+
+  isPalindrome() {
+    let values = [];
+    let current = this.head;
+
+    while (current) {
+      values.push(current.value);
+      current = current.next;
     }
 
-    // Function to append a node at the end of the linked list
-    append(data) {
-        let newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
+    let left = 0;
+    let right = values.length - 1;
+
+    while (left < right) {
+      if (values[left] !== values[right]) {
+        return false;
+      }
+      left++;
+      right--;
     }
 
-    // Function to find the nth node from the end of the linked list
-    findNthFromEnd(n) {
-        let first = this.head;
-        let second = this.head;
-        for (let i = 0; i < n; i++) {
-            if (first === null) {
-                return null; // n is greater than the length of the linked list
-            }
-            first = first.next;
-        }
-        while (first !== null) {
-            first = first.next;
-            second = second.next;
-        }
-        return second;
-    }
+    return true;
+  }
 }
 
 // Example usage
 let linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
-linkedList.append(5);
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(2);
+linkedList.add(1);
 
-let n = 3;
-let nthNode = linkedList.findNthFromEnd(n);
-
-if (nthNode) {
-    console.log(`The ${n}th node from the end is: ${nthNode.data}`);
-} else {
-    console.log(`The linked list is shorter than ${n} nodes.`);
-}
+console.log(linkedList.isPalindrome()); // Output: true
