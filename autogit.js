b90597049e431827ae93d0c69472253c8f0d65fa
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:45:02 +0800

    3-24-2024 9:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,54 @@
-// Definition for singly-linked list.
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
+class HashTable {
+  constructor(size = 10) {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  hash(key) {
+    return key.toString().length % this.size;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (const entry of this.table[index]) {
+      if (entry[0] === key) {
+        return entry[1];
+      }
+    }
+    return undefined;
+  }
+
+  remove(key) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (let i = 0; i < this.table[index].length; i++) {
+      if (this.table[index][i][0] === key) {
+        this.table[index].splice(i, 1);
+        return;
+      }
+    }
+  }
 }
 
-// Function to find the intersection of two linked lists
-function getIntersectionNode(headA, headB) {
-    const listA = [];
-    const listB = [];
-  
-    // Traverse linked list A and store nodes in listA
-    while (headA) {
-        listA.push(headA);
-        headA = headA.next;
-    }
+// Example usage
+const ht = new HashTable();
 
-    // Traverse linked list B and store nodes in listB
-    while (headB) {
-        listB.push(headB);
-        headB = headB.next;
-    }
+ht.set('apple', 10);
+ht.set('banana', 20);
+console.log(ht.get('apple')); // Output: 10
 
-    let intersectionNode = null;
-
-    // Compare the two arrays to find the intersection
-    for (let nodeA of listA) {
-        for (let nodeB of listB) {
-            if (nodeA === nodeB) {
-                intersectionNode = nodeA;
-                break;
-            }
-        }
-        if (intersectionNode) {
-            break;
-        }
-    }
-
-    return intersectionNode;
-}
+ht.remove('apple');
+console.log(ht.get('apple')); // Output: undefined
