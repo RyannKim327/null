1feae52129f90f38642539f4133ca692440506ac
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:27:24 +0800

    3-24-2024 9:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,17 @@
-function findStringLength(str) {
-    let length = 0;
-    while (str[length] !== undefined) {
-        length++;
-    }
-    return length;
+function isAnagram(str1, str2) {
+  const sanitizeString = function (str) {
+    return str.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+  }
+
+  return sanitizeString(str1) === sanitizeString(str2);
 }
 
-let myString = "Hello, World!";
-let length = findStringLength(myString);
-console.log(length); // Output: 13
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+
+if (isAnagram(string1, string2)) {
+  console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+  console.log(`${string1} and ${string2} are not anagrams.`);
+}
