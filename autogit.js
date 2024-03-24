6855e3b67396d704eaa81974c32089b8b4f451bf
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:15:16 +0800

    3-24-2024 10:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,4 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        
-        arr[j + 1] = current;
-    }
-    
-    return arr;
-}
-
-// Example usage
-let arr = [5, 2, 4, 6, 1, 3];
-console.log("Before sorting:", arr);
-console.log("After sorting:", insertionSort(arr));
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
