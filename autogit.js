2535f5588f7dad6435b97fce37ca862486c3c329
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:11:51 +0800

    3-24-2024 10:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,82 +1,9 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function findCommonElements(arr1, arr2) {
+    return arr1.filter(item => arr2.includes(item));
 }
 
-class Queue {
-    constructor() {
-        this.front = null;
-        this.rear = null;
-        this.size = 0;
-    }
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    enqueue(data) {
-        const newNode = new Node(data);
-        
-        if (this.isEmpty()) {
-            this.front = newNode;
-        } else {
-            this.rear.next = newNode;
-        }
-        
-        this.rear = newNode;
-        this.size++;
-    }
-
-    dequeue() {
-        if (this.isEmpty()) {
-            return null;
-        }
-
-        const data = this.front.data;
-        this.front = this.front.next;
-        this.size--;
-
-        if (this.isEmpty()) {
-            this.rear = null;
-        }
-
-        return data;
-    }
-
-    peek() {
-        if (this.isEmpty()) {
-            return null;
-        }
-
-        return this.front.data;
-    }
-
-    print() {
-        let current = this.front;
-        let values = [];
-
-        while (current !== null) {
-            values.push(current.data);
-            current = current.next;
-        }
-
-        console.log(values.join(' -> '));
-    }
-}
-
-// Example usage:
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.peek()); // Output: 1
-
-queue.print(); // Output: 1 -> 2 -> 3
-
-queue.dequeue();
-
-queue.print(); // Output: 2 -> 3
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
