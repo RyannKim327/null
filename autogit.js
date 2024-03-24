9cd67fb590a5ac8f1368be5dc390831335967cbc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:43:48 +0800

    3-24-2024 9:43 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,8 @@
-function radixSort(arr) {
-  const getMax = (arr) => {
-    let max = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] > max) {
-        max = arr[i];
-      }
-    }
-    return max;
-  };
-
-  const countSort = (arr, exp) => {
-    const output = new Array(arr.length);
-    const count = new Array(10).fill(0);
-
-    for (let i = 0; i < arr.length; i++) {
-      count[Math.floor(arr[i] / exp) % 10]++;
-    }
-
-    for (let i = 1; i < 10; i++) {
-      count[i] += count[i - 1];
-    }
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-      const index = Math.floor(arr[i] / exp) % 10;
-      output[count[index] - 1] = arr[i];
-      count[index]--;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] = output[i];
-    }
-  };
-
-  const max = getMax(arr);
-
-  for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
-    countSort(arr, exp);
-  }
-
-  return arr;
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-// Usage
-const arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+const originalString = 'Hello, World!';
+const reversedString = reverseString(originalString);
+
+console.log(reversedString); // Output: '!dlroW ,olleH'
