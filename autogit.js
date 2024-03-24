104802baf38ddf3b198489e84a2520aedadac29d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:28:22 +0800

    3-24-2024 10:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,120 @@
+// Define a Node class to represent a node in the B-tree
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+    constructor(t) {
+        this.keys = [];
+        this.children = [];
+        this.isLeaf = true;
+        this.degree = t;
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
+// Define a BTree class to represent the B-tree itself
+class BTree {
+    constructor(t) {
+        this.root = new Node(t);
+        this.degree = t;
     }
 
-    // Add an element to the end of the list
-    append(data) {
-        const newNode = new Node(data);
+    // Insert a key into the B-tree
+    insert(key) {
+        let root = this.root;
 
-        if (!this.head) {
-            this.head = newNode;
+        // If the root is full, split the root and create a new root
+        if (root.keys.length === (2 * this.degree) - 1) {
+            let newRoot = new Node(this.degree);
+            newRoot.children.push(root);
+            this.splitChild(newRoot, 0);
+            this.root = newRoot;
+            this.insertNonFull(newRoot, key);
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
+            this.insertNonFull(root, key);
         }
     }
 
-    // Display all elements in the list
-    display() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
+    // Insert a key into a non-full node
+    insertNonFull(node, key) {
+        let i = node.keys.length - 1;
+
+        if (node.isLeaf) {
+            while (i >= 0 && key < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i];
+                i--;
+            }
+
+            node.keys[i + 1] = key;
+        } else {
+            while (i >= 0 && key < node.keys[i]) {
+                i--;
+            }
+
+            i++;
+
+            if (node.children[i].keys.length === (2 * this.degree) - 1) {
+                this.splitChild(node, i);
+
+                if (key > node.keys[i]) {
+                    i++;
+                }
+            }
+
+            this.insertNonFull(node.children[i], key);
+        }
+    }
+
+    // Split child node
+    splitChild(parent, index) {
+        let t = this.degree;
+        let child = parent.children[index];
+        let newChild = new Node(t);
+
+        newChild.isLeaf = child.isLeaf;
+
+        for (let i = 0; i < t - 1; i++) {
+            newChild.keys[i] = child.keys[i + t];
+        }
+
+        if (!child.isLeaf) {
+            for (let i = 0; i < t; i++) {
+                newChild.children[i] = child.children[i + t];
+            }
+        }
+
+        child.keys.length = t - 1;
+        child.children.length = t;
+
+        parent.keys.splice(index, 0, child.keys.pop());
+        parent.children.splice(index + 1, 0, newChild);
+    }
+
+    // Perform inorder traversal of the B-tree
+    inorderTraversal(node) {
+        if (node) {
+            let i;
+            for (i = 0; i < node.keys.length; i++) {
+                if (!node.isLeaf) {
+                    this.inorderTraversal(node.children[i]);
+                }
+                console.log(node.keys[i]);
+            }
+
+            if (!node.isLeaf) {
+                this.inorderTraversal(node.children[i]);
+            }
         }
     }
 }
 
 // Example usage
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
+const bTree = new BTree(3);
+bTree.insert(1);
+bTree.insert(3);
+bTree.insert(7);
+bTree.insert(10);
+bTree.insert(15);
+bTree.insert(20);
+bTree.insert(25);
+bTree.insert(16);
+bTree.insert(19);
+bTree.insert(27);
 
-linkedList.display();
+bTree.inorderTraversal(bTree.root);
