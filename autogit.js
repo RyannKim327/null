ca20e47c7dfc9e20ff06733a584b4080c0270372
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:14:56 +0800

    3-24-2024 10:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,83 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    
-    // Create a 2D array to store the lengths of the longest common subsequences
-    const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(0));
-    
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+class Node {
+    constructor(value, color) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.color = color; // 0 for black, 1 for red
     }
-    
-    // Backtrack to find the longest common subsequence
-    let i = m, j = n;
-    let lcs = '';
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    
-    return lcs;
 }
 
-// Test the function
-const str1 = "abcdef";
-const str2 = "acbcf";
-console.log(longestCommonSubsequence(str1, str2));  // Output: abcf
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        this.root = this._insertRecursive(this.root, value);
+        this.root.color = 0; // Ensure root is always black
+    }
+
+    _insertRecursive(node, value) {
+        if (node === null) {
+            return new Node(value, 1); // New nodes are always red
+        }
+
+        if (value < node.value) {
+            node.left = this._insertRecursive(node.left, value);
+        } else if (value > node.value) {
+            node.right = this._insertRecursive(node.right, value);
+        }
+
+        if (this._isRed(node.right) && !this._isRed(node.left)) {
+            node = this._rotateLeft(node);
+        }
+        if (this._isRed(node.left) && this._isRed(node.left.left)) {
+            node = this._rotateRight(node);
+        }
+        if (this._isRed(node.left) && this._isRed(node.right)) {
+            this._flipColors(node);
+        }
+
+        return node;
+    }
+
+    _rotateLeft(node) {
+        let temp = node.right;
+        node.right = temp.left;
+        temp.left = node;
+        temp.color = node.color;
+        node.color = 1;
+        return temp;
+    }
+
+    _rotateRight(node) {
+        let temp = node.left;
+        node.left = temp.right;
+        temp.right = node;
+        temp.color = node.color;
+        node.color = 1;
+        return temp;
+    }
+
+    _flipColors(node) {
+        node.color = 1;
+        node.left.color = 0;
+        node.right.color = 0;
+    }
+
+    _isRed(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.color === 1;
+    }
+}
+
+// Usage
+const rbTree = new RedBlackTree();
+rbTree.insert(7);
+rbTree.insert(3);
+rbTree.insert(18);
+rbTree.insert(10);
+// Continue inserting more elements...
+
