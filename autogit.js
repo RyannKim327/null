172a74871301355427811b5b1906aab4871440a9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:05:16 +0800

    3-24-2024 10:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,12 @@
-const userInput = prompt("Enter something:");
-console.log("You entered: " + userInput);
+function factorial(number) {
+    if (number === 0) {
+        return 1;
+    } else {
+        return number * factorial(number - 1);
+    }
+}
+
+// Example: Calculate the factorial of 5
+const number = 5;
+const result = factorial(number);
+console.log(`The factorial of ${number} is: ${result}`);
