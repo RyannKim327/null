871975c8fff81a568a48aff1bdcc9a233545fd31
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:27:10 +0800

    3-24-2024 10:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,40 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, "");  // using regex to match all vowels and replacing them with an empty string
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+    
+    // Initialize a 2D array to store the lengths of LCS
+    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+    
+    // Building the dp array
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+    
+    // Constructing the longest common subsequence
+    let lcs = "";
+    let i = m, j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+    
+    return lcs;
 }
 
-let originalString = "Hello, World!";
-let stringWithoutVowels = removeVowels(originalString);
-console.log(stringWithoutVowels);  // Output: "Hll, Wrld!"
+// Test
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+console.log(longestCommonSubsequence(str1, str2)); // Output: "ADH"
