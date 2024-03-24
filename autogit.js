7322e8fe6ae3b5db415c836d30031b1d787db317
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:10:41 +0800

    3-24-2024 10:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,79 @@
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num <= 3) return true;
-
-    if (num % 2 === 0 || num % 3 === 0) return false;
-
-    let i = 5;
-    while (i * i <= num) {
-        if (num % i === 0 || num % (i + 2) === 0) {
-            return false;
-        }
-        i += 6;
-    }
-
-    return true;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-function largestPrimeFactor(number) {
-    let largest = 0;
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+  }
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            if (isPrime(i)) {
-                largest = i;
-            }
-            if (isPrime(number / i)) {
-                largest = Math.max(largest, number / i);
-            }
-        }
+  enqueue(data) {
+    const newNode = new Node(data);
+
+    if (!this.rear) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
+    }
+  }
+
+  dequeue() {
+    if (!this.front) {
+      return null;
     }
 
-    return largest;
+    const dequeuedItem = this.front.data;
+    this.front = this.front.next;
+
+    if (!this.front) {
+      this.rear = null;
+    }
+
+    return dequeuedItem;
+  }
+
+  peek() {
+    if (!this.front) {
+      return null;
+    }
+
+    return this.front.data;
+  }
+
+  isEmpty() {
+    return this.front === null;
+  }
+
+  printQueue() {
+    let current = this.front;
+    const elements = [];
+
+    while (current) {
+      elements.push(current.data);
+      current = current.next;
+    }
+
+    console.log(elements.join(' '));
+  }
 }
 
-const number = 13195; // Example number
-const largestPrime = largestPrimeFactor(number);
-console.log("The largest prime factor of", number, "is", largestPrime);
+// Example Usage
+const queue = new Queue();
+
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+queue.printQueue(); // Output: 1 2 3
+
+console.log(queue.dequeue()); // Output: 1
+
+queue.printQueue(); // Output: 2 3
+
+console.log(queue.peek()); // Output: 2
