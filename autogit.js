0757c1f0ab50bf8eb10a067de7f3c8b1e45f3598
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:21:48 +0800

    3-24-2024 10:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+let str = "42";
+let num = parseInt(str);
 
-function findIntersection(list1, list2) {
-    let set = new Set();
+console.log(num); // Output: 42
+let str = "42";
+let num = Number(str);
 
-    // Traverse list 1 and store nodes in the set
-    let current = list1;
-    while (current != null) {
-        set.add(current);
-        current = current.next;
-    }
-
-    // Traverse list 2 and check for intersection
-    current = list2;
-    while (current != null) {
-        if (set.has(current)) {
-            return current;
-        }
-        current = current.next;
-    }
-
-    return null; // No intersection found
-}
-
-// Sample linked lists
-let intersectionNode = new Node(8);
-let list1 = new Node(4);
-list1.next = new Node(1);
-list1.next.next = intersectionNode;
-
-let list2 = new Node(5);
-list2.next = new Node(6);
-list2.next.next = new Node(1);
-list2.next.next.next = intersectionNode;
-
-let result = findIntersection(list1, list2);
-console.log(result); // Output should be the node with value 8
+console.log(num); // Output: 42
