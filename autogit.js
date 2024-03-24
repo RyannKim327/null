e11b4e1aef974e20c77311096058dbd48936d56e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:11:20 +0800

    3-24-2024 10:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,8 @@
-function buildKMPTable(pattern) {
-    let table = Array(pattern.length).fill(0);
-    let i = 1, j = 0;
-
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[j]) {
-            table[i] = j + 1;
-            i++;
-            j++;
-        } else {
-            if (j !== 0) {
-                j = table[j - 1];
-            } else {
-                table[i] = 0;
-                i++;
-            }
-        }
-    }
-
-    return table;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-function KMPSearch(text, pattern) {
-    let table = buildKMPTable(pattern);
-    let i = 0, j = 0;
-    const indices = [];
+const originalString = "Hello, World!";
+const stringWithoutVowels = removeVowels(originalString);
 
-    while (i < text.length) {
-        if (text[i] === pattern[j]) {
-            i++;
-            j++;
-            if (j === pattern.length) {
-                indices.push(i - j);
-                j = table[j - 1];
-            }
-        } else {
-            if (j !== 0) {
-                j = table[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-
-    return indices;
-}
-
-// Example of using the KMP algorithm
-let text = "ABABDABACDABABCABAB";
-let pattern = "ABABCABAB";
-let indices = KMPSearch(text, pattern);
-
-if (indices.length === 0) {
-    console.log("Pattern not found in the text.");
-} else {
-    console.log("Pattern found at index(es): " + indices.join(", "));
-}
+console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
