f4a47e3f71ccd22edf2f3a853f76605a7588937f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:22:07 +0800

    3-24-2024 9:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,9 @@
-class HashTable {
-  constructor() {
-    this.table = {};
-  }
-
-  // Hash function to calculate index for key
-  hash(key) {
-    let hash = 0;
-    for (let char of key) {
-      hash += char.charCodeAt(0);
-    }
-    return hash % 37; // Assuming table size of 37 for simplicity
-  }
-
-  // Insert key-value pair into hash table
-  insert(key, value) {
-    const index = this.hash(key);
-    if (!this.table[index]) {
-      this.table[index] = {};
-    }
-    this.table[index][key] = value;
-  }
-
-  // Retrieve value from hash table given key
-  get(key) {
-    const index = this.hash(key);
-    return this.table[index] ? this.table[index][key] : undefined;
-  }
-
-  // Remove key-value pair from hash table
-  remove(key) {
-    const index = this.hash(key);
-    if (this.table[index]) {
-      delete this.table[index][key];
-    }
-  }
+function findCommonElements(arr1, arr2) {
+    return arr1.filter(element => arr2.includes(element));
 }
 
-// Example usage
-const myHashTable = new HashTable();
-myHashTable.insert("name", "Alice");
-myHashTable.insert("age", 30);
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-console.log(myHashTable.get("name")); // Output: Alice
-console.log(myHashTable.get("age")); // Output: 30
-
-myHashTable.remove("age");
-console.log(myHashTable.get("age")); // Output: undefined
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
