e6685f73ebdfb6f56505605b41683f48e9801c4d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:40:02 +0800

    3-24-2024 9:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,72 +1,18 @@
-function burrowsWheelerTransform(text) {
-    // Generate all possible rotations of the text
-    let rotations = [];
-    for (let i = 0; i < text.length; i++) {
-        let rotatedText = text.slice(i) + text.slice(0, i);
-        rotations.push(rotatedText);
-    }
-
-    // Sort the rotations alphabetically
-    rotations.sort();
-
-    // Get the last characters of each rotation
-    let transformedText = rotations.map(text => text[text.length - 1]).join('');
-
-    // Find the index of the original text in the sorted rotations
-    let originalIndex = rotations.indexOf(text);
-
-    return { transformedText, originalIndex };
-}
-
-function inverseBurrowsWheelerTransform(transformedText, originalIndex) {
-    const sortedText = Array.from(transformedText).sort();
-    let table = {};
-    for (let char of sortedText) {
-        table[char] = table[char] || 0;
-        table[char]++;
-    }
-
-    let firstColumn = {};
-    let index = 0;
-    for (let char in table) {
-        firstColumn[char] = [];
-        for (let i = 0; i < table[char]; i++) {
-            firstColumn[char].push(index);
-            index += 1;
+function countOccurrences(str, char) {
+    let count = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
         }
     }
-
-    let currentIndex = originalIndex;
-    let originalText = '';
-    for (let i = 0; i < transformedText.length; i++) {
-        let char = transformedText[currentIndex];
-        originalText = char + originalText;
-        let rank = firstColumn[char].indexOf(currentIndex);
-        currentIndex = getCharIndex(transformedText, char, rank);
-    }
-
-    return originalText;
+    
+    return count;
 }
 
-function getCharIndex(text, char, rank) {
-    let index = -1;
-    for (let i = 0; i < text.length; i++) {
-        if (text[i] === char) {
-            rank--;
-            if (rank < 0) {
-                index = i;
-                break;
-            }
-        }
-    }
-    return index;
-}
+// Example usage
+const myString = "hello world";
+const character = "l";
+const occurrences = countOccurrences(myString, character);
 
-// Example
-let text = "hello world";
-let { transformedText, originalIndex } = burrowsWheelerTransform(text);
-console.log("Transformed Text:", transformedText);
-console.log("Original Index:", originalIndex);
-
-let originalText = inverseBurrowsWheelerTransform(transformedText, originalIndex);
-console.log("Original Text:", originalText);
+console.log(`The character "${character}" occurs ${occurrences} times in the string.`);
