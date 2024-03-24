caed13bb61fed5cbeec32ffdc157363d1fa6a93a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:34:21 +0800

    3-24-2024 9:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,47 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+function getLength(linkedList) {
+  let current = linkedList.head;
+  let length = 0;
+
+  while (current !== null) {
+    length++;
+    current = current.next;
+  }
+
+  return length;
+}
+
+// Usage
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+
+const length = getLength(linkedList);
+console.log(length); // Output: 4
