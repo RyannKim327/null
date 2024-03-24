293bc706c6529a6cbeefcb442e94a94d414038cd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:15:20 +0800

    3-24-2024 9:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,45 @@
+// Node class to represent each node in the linked list
 class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
+// LinkedList class to manage the linked list operations
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  getLength() {
-    let count = 0;
-    let current = this.head;
-
-    while (current) {
-      count++;
-      current = current.next;
+    constructor() {
+        this.head = null;
     }
 
-    return count;
-  }
+    // Method to add a new node to the end of the linked list
+    addNode(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Method to print the linked list
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
 
-// Create a linked list
+// Example usage
 const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-linkedList.append(4);
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
 
-console.log(linkedList.getLength()); // Outputs: 4
+linkedList.printList();
