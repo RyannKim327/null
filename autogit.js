7f340141f5a293a4b3110059ef465bb93cb2cf3d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:34:56 +0800

    3-24-2024 9:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,16 @@
-class Graph {
-    constructor() {
-        this.adjList = new Map();
-    }
+// Define the URL to connect to
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
 
-    addVertex(v) {
-        this.adjList.set(v, []);
-    }
-
-    addEdge(v, w) {
-        this.adjList.get(v).push(w);
-        this.adjList.get(w).push(v);
-    }
-
-    dfsUtil(v, visited) {
-        visited[v] = true;
-        console.log(v);
-
-        let neighbors = this.adjList.get(v);
-        for (let neighbor of neighbors) {
-            if (!visited[neighbor]) {
-                this.dfsUtil(neighbor, visited);
-            }
-        }
-    }
-
-    dfs(startingVertex) {
-        let visited = {};
-        for (let vertex of this.adjList.keys()) {
-            visited[vertex] = false;
-        }
-
-        this.dfsUtil(startingVertex, visited);
-    }
+// Define asynchronous task function
+async function connectAsyncTask() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('Response:', data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-let g = new Graph();
-let vertices = ['A', 'B', 'C', 'D', 'E', 'F'];
-for (let vertex of vertices) {
-    g.addVertex(vertex);
-}
-
-g.addEdge('A', 'B');
-g.addEdge('A', 'C');
-g.addEdge('B', 'D');
-g.addEdge('B', 'E');
-g.addEdge('C', 'F');
-
-g.dfs('A');
+// Call the asynchronous task function
+connectAsyncTask();
