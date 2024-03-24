f61bccdfe3770af555c7957a7b56fbbea6d9dd0b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:58:44 +0800

    3-24-2024 9:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,45 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
-        }
+function radixSort(arr) {
+  const getMax = (arr) => {
+    let max = 0;
+    for (let num of arr) {
+      if (num > max) {
+        max = num;
+      }
     }
-    return count;
+    return max;
+  };
+
+  const countingSort = (arr, exp) => {
+    const output = new Array(arr.length).fill(0);
+    const count = new Array(10).fill(0);
+
+    for (let i = 0; i < arr.length; i++) {
+      count[Math.floor(arr[i] / exp) % 10]++;
+    }
+
+    for (let i = 1; i < 10; i++) {
+      count[i] += count[i - 1];
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+      output[count[Math.floor(arr[i] / exp) % 10] - 1] = arr[i];
+      count[Math.floor(arr[i] / exp) % 10]--;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = output[i];
+    }
+  };
+
+  const max = getMax(arr);
+
+  for (let exp = 1; Math.floor(max / exp) > 0; exp *= 10) {
+    countingSort(arr, exp);
+  }
+
+  return arr;
 }
 
-let myString = "hello world";
-let myCharacter = "l";
-let occurrences = countOccurrences(myString, myCharacter);
-
-console.log(`The character '${myCharacter}' occurs ${occurrences} times in the string.`);
+// Example usage
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
