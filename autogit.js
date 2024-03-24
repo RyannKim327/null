05fb8691b9476c89f689d0f611862d2779df33c8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:40:08 +0800

    3-24-2024 9:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,11 @@
-function countOccurrences(str, char) {
-    let count = 0;
-    
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
-        }
+function calculateMean(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
     }
-    
-    return count;
+    return sum / numbers.length;
 }
 
-// Example usage
-const myString = "hello world";
-const character = "l";
-const occurrences = countOccurrences(myString, character);
-
-console.log(`The character "${character}" occurs ${occurrences} times in the string.`);
+let numbers = [1, 2, 3, 4, 5];
+let mean = calculateMean(numbers);
+console.log("Mean: ", mean);
