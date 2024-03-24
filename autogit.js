9715dd13964a236b7837c7ed53dc327af3367e24
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:31:26 +0800

    3-24-2024 9:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,30 @@
-function isPalindrome(str) {
-    let left = 0;
-    let right = str.length - 1;
-    
-    while (left < right) {
-        if (str[left] !== str[right]) {
-            return false;
-        }
-        left++;
-        right--;
+// Node definition for the binary tree
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-    
-    return true;
 }
 
-const testString = "racecar";
-if (isPalindrome(testString)) {
-    console.log(`${testString} is a palindrome.`);
-} else {
-    console.log(`${testString} is not a palindrome.`);
+// Function to find the sum of all nodes in a binary tree
+function sumBinaryTree(root) {
+    if (root === null) {
+        return 0;
+    }
+
+    return root.value + sumBinaryTree(root.left) + sumBinaryTree(root.right);
 }
+
+// Example binary tree
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+// Calculate the sum of all nodes in the binary tree
+const sum = sumBinaryTree(root);
+console.log("Sum of all nodes in the binary tree:", sum);
