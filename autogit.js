070ccb7e5bf7dbfba1fc83c384c1440a36792ba0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:07:20 +0800

    3-24-2024 10:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,24 @@
-const cron = require('node-cron');
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-cron.schedule('* * * * *', () => {
-  console.log('Running a task every minute.');
-});
-npm install node-cron
-node cron-job.js
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+// Example usage
+const array = [4, 2, 7, 1, 3, 9, 5];
+const sortedArray = quickSort(array);
+console.log(sortedArray);
