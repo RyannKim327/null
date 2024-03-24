f303a0cd3d2402ad946d47251d213658f1ea91d5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:36:19 +0800

    3-24-2024 9:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,98 +1,60 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.height = 1;
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
 }
 
-class AVLTree {
+class LinkedList {
     constructor() {
-        this.root = null;
+        this.head = null;
     }
 
-    getHeight(node) {
-        return node ? node.height : 0;
-    }
-
-    updateHeight(node) {
-        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
-    }
-
-    getBalanceFactor(node) {
-        return this.getHeight(node.right) - this.getHeight(node.left);
-    }
-
-    rotateRight(y) {
-        const x = y.left;
-        const T = x.right;
-
-        x.right = y;
-        y.left = T;
-
-        this.updateHeight(y);
-        this.updateHeight(x);
-
-        return x;
-    }
-
-    rotateLeft(x) {
-        const y = x.right;
-        const T = y.left;
-
-        y.left = x;
-        x.right = T;
-
-        this.updateHeight(x);
-        this.updateHeight(y);
-
-        return y;
-    }
-
-    insert(value) {
-        this.root = this.insertRecursive(this.root, value);
-    }
-
-    insertRecursive(node, value) {
-        if (node === null) {
-            return new Node(value);
-        }
-
-        if (value < node.value) {
-            node.left = this.insertRecursive(node.left, value);
+    addNode(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
         } else {
-            node.right = this.insertRecursive(node.right, value);
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
         }
+    }
 
-        this.updateHeight(node);
-
-        const balanceFactor = this.getBalanceFactor(node);
-
-        if (balanceFactor > 1 && value > node.right.value) {
-            return this.rotateLeft(node);
+    reverse() {
+        let prev = null;
+        let current = this.head;
+        while (current) {
+            const next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
         }
+        this.head = prev;
+    }
 
-        if (balanceFactor < -1 && value < node.left.value) {
-            return this.rotateRight(node);
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
         }
-
-        if (balanceFactor > 1 && value < node.right.value) {
-            node.right = this.rotateRight(node.right);
-            return this.rotateLeft(node);
-        }
-
-        if (balanceFactor < -1 && value > node.left.value) {
-            node.left = this.rotateLeft(node.left);
-            return this.rotateRight(node);
-        }
-
-        return node;
     }
 }
 
-// Usage
-const avlTree = new AVLTree();
-avlTree.insert(10);
-avlTree.insert(20);
-avlTree.insert(30);
+// Create a linked list
+const list = new LinkedList();
+list.addNode(1);
+list.addNode(2);
+list.addNode(3);
+list.addNode(4);
+
+console.log("Original list:");
+list.printList();
+
+list.reverse();
+
+console.log("\nReversed list:");
+list.printList();
