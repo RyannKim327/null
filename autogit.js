cf03ad71013863b9f0b2af5011f3a054cfe8ea48
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:31:01 +0800

    3-24-2024 9:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,6 @@
-function findSecondLargest(arr) {
-    arr.sort(function(a, b) {
-        return b - a;
-    });
-    return arr[1];
-}
+let array = [1, 2, 3, 4, 5];
+let indexToRemove = 2; // Index of the element to remove
 
-// Example usage
-let arr = [10, 5, 15, 20];
-let secondLargest = findSecondLargest(arr);
-console.log("The second largest element is: " + secondLargest);
+array.splice(indexToRemove, 1);
+
+console.log(array); // Output: [1, 2, 4, 5]
