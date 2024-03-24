6702c9f4e95031e4f7929820c50860b17250c94b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:45:09 +0800

    3-24-2024 9:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,16 @@
-class HashTable {
-  constructor(size = 10) {
-    this.size = size;
-    this.table = new Array(size);
-  }
-
-  hash(key) {
-    return key.toString().length % this.size;
-  }
-
-  set(key, value) {
-    const index = this.hash(key);
-    if (!this.table[index]) {
-      this.table[index] = [];
+function largestPrimeFactor(number) {
+    let divisor = 2;
+    while (number > 1) {
+        if (number % divisor === 0) {
+            number /= divisor;
+        } else {
+            divisor++;
+        }
     }
-    this.table[index].push([key, value]);
-  }
-
-  get(key) {
-    const index = this.hash(key);
-    if (!this.table[index]) {
-      return undefined;
-    }
-    for (const entry of this.table[index]) {
-      if (entry[0] === key) {
-        return entry[1];
-      }
-    }
-    return undefined;
-  }
-
-  remove(key) {
-    const index = this.hash(key);
-    if (!this.table[index]) {
-      return undefined;
-    }
-    for (let i = 0; i < this.table[index].length; i++) {
-      if (this.table[index][i][0] === key) {
-        this.table[index].splice(i, 1);
-        return;
-      }
-    }
-  }
+    return divisor;
 }
 
-// Example usage
-const ht = new HashTable();
+const number = 13195; // Change this to your desired number
+const largestFactor = largestPrimeFactor(number);
 
-ht.set('apple', 10);
-ht.set('banana', 20);
-console.log(ht.get('apple')); // Output: 10
-
-ht.remove('apple');
-console.log(ht.get('apple')); // Output: undefined
+console.log(`The largest prime factor of ${number} is ${largestFactor}`);
