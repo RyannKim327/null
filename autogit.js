637251fb7ac19eb5e24c0a0969c5373b90279535
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:28:30 +0800

    3-24-2024 10:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,120 +1,37 @@
-// Define a Node class to represent a node in the B-tree
-class Node {
-    constructor(t) {
-        this.keys = [];
-        this.children = [];
-        this.isLeaf = true;
-        this.degree = t;
+// Helper function to get the digit at a certain position in a number
+const getDigit = (num, i) => Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+
+// Helper function to find the number of digits in the largest number in a list
+const digitCount = num => (num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1);
+
+// Helper function to find the number of digits in the largest number in an array of numbers
+const mostDigits = nums => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(nums[i]));
+  }
+  return maxDigits;
+};
+
+// Radix Sort function
+const radixSort = nums => {
+  const maxDigitCount = mostDigits(nums);
+  
+  for (let k = 0; k < maxDigitCount; k++) {
+    const digitBuckets = Array.from({ length: 10 }, () => []);
+    
+    for (let i = 0; i < nums.length; i++) {
+      const digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
     }
-}
+    
+    nums = [].concat(...digitBuckets);
+  }
+  
+  return nums;
+};
 
-// Define a BTree class to represent the B-tree itself
-class BTree {
-    constructor(t) {
-        this.root = new Node(t);
-        this.degree = t;
-    }
-
-    // Insert a key into the B-tree
-    insert(key) {
-        let root = this.root;
-
-        // If the root is full, split the root and create a new root
-        if (root.keys.length === (2 * this.degree) - 1) {
-            let newRoot = new Node(this.degree);
-            newRoot.children.push(root);
-            this.splitChild(newRoot, 0);
-            this.root = newRoot;
-            this.insertNonFull(newRoot, key);
-        } else {
-            this.insertNonFull(root, key);
-        }
-    }
-
-    // Insert a key into a non-full node
-    insertNonFull(node, key) {
-        let i = node.keys.length - 1;
-
-        if (node.isLeaf) {
-            while (i >= 0 && key < node.keys[i]) {
-                node.keys[i + 1] = node.keys[i];
-                i--;
-            }
-
-            node.keys[i + 1] = key;
-        } else {
-            while (i >= 0 && key < node.keys[i]) {
-                i--;
-            }
-
-            i++;
-
-            if (node.children[i].keys.length === (2 * this.degree) - 1) {
-                this.splitChild(node, i);
-
-                if (key > node.keys[i]) {
-                    i++;
-                }
-            }
-
-            this.insertNonFull(node.children[i], key);
-        }
-    }
-
-    // Split child node
-    splitChild(parent, index) {
-        let t = this.degree;
-        let child = parent.children[index];
-        let newChild = new Node(t);
-
-        newChild.isLeaf = child.isLeaf;
-
-        for (let i = 0; i < t - 1; i++) {
-            newChild.keys[i] = child.keys[i + t];
-        }
-
-        if (!child.isLeaf) {
-            for (let i = 0; i < t; i++) {
-                newChild.children[i] = child.children[i + t];
-            }
-        }
-
-        child.keys.length = t - 1;
-        child.children.length = t;
-
-        parent.keys.splice(index, 0, child.keys.pop());
-        parent.children.splice(index + 1, 0, newChild);
-    }
-
-    // Perform inorder traversal of the B-tree
-    inorderTraversal(node) {
-        if (node) {
-            let i;
-            for (i = 0; i < node.keys.length; i++) {
-                if (!node.isLeaf) {
-                    this.inorderTraversal(node.children[i]);
-                }
-                console.log(node.keys[i]);
-            }
-
-            if (!node.isLeaf) {
-                this.inorderTraversal(node.children[i]);
-            }
-        }
-    }
-}
-
-// Example usage
-const bTree = new BTree(3);
-bTree.insert(1);
-bTree.insert(3);
-bTree.insert(7);
-bTree.insert(10);
-bTree.insert(15);
-bTree.insert(20);
-bTree.insert(25);
-bTree.insert(16);
-bTree.insert(19);
-bTree.insert(27);
-
-bTree.inorderTraversal(bTree.root);
+// Testing the radixSort function
+const unsortedNumbers = [170, 45, 75, 90, 802, 2, 24, 66];
+const sortedNumbers = radixSort(unsortedNumbers);
+console.log(sortedNumbers); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
