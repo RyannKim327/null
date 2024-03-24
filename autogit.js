5a3f40963551fa82d3e8d2dbb56eee78d5648d90
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:21:57 +0800

    3-24-2024 10:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,42 @@
-let str = "42";
-let num = parseInt(str);
+function fibonacciSearch(arr, x) {
+    let fibM2 = 0;  // (m-2)'th Fibonacci Number
+    let fibM1 = 1;  // (m-1)'th Fibonacci Number
+    let fibM = fibM2 + fibM1;  // m'th Fibonacci Number
 
-console.log(num); // Output: 42
-let str = "42";
-let num = Number(str);
+    while (fibM < arr.length) {
+        fibM2 = fibM1;
+        fibM1 = fibM;
+        fibM = fibM2 + fibM1;
+    }
 
-console.log(num); // Output: 42
+    let offset = -1;
+
+    while (fibM > 1) {
+        let i = Math.min(offset + fibM2, arr.length - 1);
+
+        if (arr[i] < x) {
+            fibM = fibM1;
+            fibM1 = fibM2;
+            fibM2 = fibM - fibM1;
+            offset = i;
+        } else if (arr[i] > x) {
+            fibM = fibM2;
+            fibM1 = fibM1 - fibM2;
+            fibM2 = fibM - fibM1;
+        } else {
+            return i;
+        }
+    }
+
+    if (fibM1 && arr[offset + 1] === x) {
+        return offset + 1;
+    }
+
+    return -1;
+}
+
+// Usage
+const arr = [2, 3, 5, 8, 13, 21, 34, 55, 89];
+const x = 21;
+const index = fibonacciSearch(arr, x);
+console.log(index);  // Output: 5
