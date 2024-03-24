a3fc6ffa296b1a197cf349777ecefb967329ac6a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:33:35 +0800

    3-24-2024 9:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,4 @@
-function reverseString(str) {
-    // Step 1: Convert the string to an array
-    var strArray = str.split('');
-    
-    // Step 2: Reverse the order of the elements in the array
-    var reversedArray = strArray.reverse();
-    
-    // Step 3: Join the elements back together to form a reversed string
-    var reversedStr = reversedArray.join('');
-    
-    return reversedStr;
-}
-
-// Test the function
-var originalStr = "Hello, World!";
-var reversedStr = reverseString(originalStr);
-
-console.log("Original string: " + originalStr);
-console.log("Reversed string: " + reversedStr);
-Original string: Hello, World!
-Reversed string: !dlroW ,olleH
+let str1 = "Hello";
+let str2 = "World";
+let concatenatedStr = str1 + " " + str2;
+console.log(concatenatedStr);
