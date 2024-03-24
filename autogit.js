386aa7bbdc92c248565ca3108b0b6d6904f742e3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:06:09 +0800

    3-24-2024 9:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,20 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-function hasCycle(head) {
-  if (!head) {
-    return false;
-  }
-
-  let slow = head;
-  let fast = head;
-
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-
-    if (slow === fast) {
-      return true;
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-  }
-
-  return false;
+    return arr;
 }
+
+// Example usage
+const array = [64, 25, 12, 22, 11];
+console.log("Original Array: " + array);
+const sortedArray = bubbleSort(array);
+console.log("Sorted Array: " + sortedArray);
