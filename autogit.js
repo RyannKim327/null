b19ab918ff7bb68d93bdf9d978d3d2501ee87f86
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:22:02 +0800

    3-24-2024 10:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-function fibonacciSearch(arr, x) {
-    let fibM2 = 0;  // (m-2)'th Fibonacci Number
-    let fibM1 = 1;  // (m-1)'th Fibonacci Number
-    let fibM = fibM2 + fibM1;  // m'th Fibonacci Number
-
-    while (fibM < arr.length) {
-        fibM2 = fibM1;
-        fibM1 = fibM;
-        fibM = fibM2 + fibM1;
-    }
-
-    let offset = -1;
-
-    while (fibM > 1) {
-        let i = Math.min(offset + fibM2, arr.length - 1);
-
-        if (arr[i] < x) {
-            fibM = fibM1;
-            fibM1 = fibM2;
-            fibM2 = fibM - fibM1;
-            offset = i;
-        } else if (arr[i] > x) {
-            fibM = fibM2;
-            fibM1 = fibM1 - fibM2;
-            fibM2 = fibM - fibM1;
-        } else {
-            return i;
-        }
-    }
-
-    if (fibM1 && arr[offset + 1] === x) {
-        return offset + 1;
-    }
-
-    return -1;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Usage
-const arr = [2, 3, 5, 8, 13, 21, 34, 55, 89];
-const x = 21;
-const index = fibonacciSearch(arr, x);
-console.log(index);  // Output: 5
+const inputString = 'Hello, World!';
+const result = removeVowels(inputString);
+
+console.log(result); // Output: "Hll, Wrld!"
