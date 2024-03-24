4c45a040ff4c33cba31fdcc4325d78c7064b5227
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:01:15 +0800

    3-24-2024 10:1 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,43 @@
-// Node class to represent each node in the linked list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function getMax(arr){
+    let max = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+function countSort(arr, exp){
+    let output = new Array(arr.length).fill(0);
+    let count = new Array(10).fill(0);
+
+    for(let i = 0; i < arr.length; i++){
+        count[Math.floor(arr[i] / exp) % 10]++;
+    }
+
+    for(let i = 1; i < 10; i++){
+        count[i] += count[i - 1];
+    }
+
+    for(let i = arr.length - 1; i >= 0; i--){
+        output[count[Math.floor(arr[i] / exp) % 10] - 1] = arr[i];
+        count[Math.floor(arr[i] / exp) % 10]--;
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = output[i];
     }
 }
 
-// LinkedList class to implement the linked list
-class LinkedList {
-    constructor() {
-        this.head = null;
+function radixSort(arr){
+    let max = getMax(arr);
+    for(let exp = 1; Math.floor(max / exp) > 0; exp *= 10){
+        countSort(arr, exp);
     }
-
-    // Method to add a new node to the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Method to display the linked list
-    display() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
+    return arr;
 }
 
-// Creating a new linked list
-const linkedList = new LinkedList();
-
-// Adding nodes to the linked list
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-
-// Displaying the linked list
-linkedList.display();
+// Example usage
+const arr = [170, 45, 75, 90, 802, 24, 2, 66];
+console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
