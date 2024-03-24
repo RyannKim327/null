6032ff267699847f4f60b658205a20af1d4f81bc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:23:58 +0800

    3-24-2024 10:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,31 @@
-function calculateTriangleArea(base, height) {
-    return 0.5 * base * height;
+const url = "https://api.example.com/data";
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.open("GET", url, true);
+    
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.responseText);
+      } else {
+        reject(xhr.statusText);
+      }
+    };
+    
+    xhr.onerror = function () {
+      reject(xhr.statusText);
+    };
+    
+    xhr.send();
+  });
 }
 
-// Example usage
-const base = 5;
-const height = 8;
-const area = calculateTriangleArea(base, height);
-console.log("The area of the triangle is: " + area);
+fetchData()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error fetching data: ", error);
+  });
