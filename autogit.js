5fc5c82f3c7243cef2986684c9e321c717870763
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:25:21 +0800

    3-24-2024 9:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,5 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+const cron = require('node-cron');
 
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return quickSort(left).concat(pivot, quickSort(right));
-}
-
-const array = [3, 6, 8, 10, 1, 2, 1];
-const sortedArray = quickSort(array);
-console.log(sortedArray);
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute!');
+});
