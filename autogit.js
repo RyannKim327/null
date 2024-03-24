8b4ba74b803e6f145d69b6790d5580d226a7a264
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:47:24 +0800

    3-24-2024 9:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,48 @@
-// Define the node structure for the linked list
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-// Define the LinkedList class
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  findMiddleElement() {
+    let slow = this.head;
+    let fast = this.head;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
     }
 
-    // Function to add a new node to the end of the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Function to get the length of the linked list
-    getLength() {
-        let length = 0;
-        let current = this.head;
-        while (current) {
-            length++;
-            current = current.next;
-        }
-        return length;
-    }
+    return slow.data;
+  }
 }
 
-// Create a linked list and add some nodes
+// Example usage
 const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+linkedList.append(5);
 
-// Get the length of the linked list
-const length = linkedList.getLength();
-console.log("Length of the linked list: " + length);
+const middleElement = linkedList.findMiddleElement();
+console.log("Middle Element:", middleElement);
