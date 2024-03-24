07d5fefcbcdbda73db5fa26a998e3cb6aeb8af01
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:23:54 +0800

    3-24-2024 10:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,9 @@
-function reverseWords(str) {
-    // Split the string into an array of words
-    var words = str.split(" ");
-    
-    // Reverse the array
-    var reversedWords = words.reverse();
-    
-    // Join the array back into a string
-    var reversedStr = reversedWords.join(" ");
-    
-    return reversedStr;
+function calculateTriangleArea(base, height) {
+    return 0.5 * base * height;
 }
 
-// Test the function
-var originalStr = "Hello world, how are you?";
-var reversedStr = reverseWords(originalStr);
-console.log(reversedStr); // Output: "you? are how world, Hello"
+// Example usage
+const base = 5;
+const height = 8;
+const area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
