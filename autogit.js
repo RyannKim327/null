6fa8b4bccd2c02fa530bda160a1eea0af00d0118
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:58:24 +0800

    3-24-2024 9:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,26 @@
-// Node class to represent a node in the linked list
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    } else {
+        var left = [];
+        var right = [];
+        var newArr = [];
+        var pivot = arr.pop();
+        var length = arr.length;
 
-// LinkedList class to represent the linked list
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    // Method to add a new node to the linked list
-    add(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
+        for (var i = 0; i < length; i++) {
+            if (arr[i] <= pivot) {
+                left.push(arr[i]);
+            } else {
+                right.push(arr[i]);
             }
-            current.next = newNode;
         }
-    }
 
-    // Method to find the length of the linked list
-    getLength() {
-        let current = this.head;
-        let length = 0;
-        while (current) {
-            length++;
-            current = current.next;
-        }
-        return length;
+        return newArr.concat(quickSort(left), pivot, quickSort(right));
     }
 }
 
-// Create a linked list
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
-
-// Get the length of the linked list
-const length = linkedList.getLength();
-console.log("Length of the linked list:", length);
+var array = [3, 0, 2, 5, -1, 4, 1];
+console.log("Original array: " + array);
+var sortedArray = quickSort(array);
+console.log("Sorted array: " + sortedArray);
