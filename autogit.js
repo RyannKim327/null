783f12391e90235d3b305255a2c2dfc3c664c5b9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:45:25 +0800

    3-24-2024 9:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,6 @@
-let str = "   Hello, World!   ";
-let trimmedStr = str.replace(/\s/g, '');
-console.log(trimmedStr); // Output: "Hello,World!"
+let arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+let uniqueArray = [...new Set(arrayWithDuplicates)];
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+let arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+let uniqueArray = arrayWithDuplicates.filter((value, index, self) => self.indexOf(value) === index);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
