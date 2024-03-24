5f84584cac83cb18787bbfefc76004629c2ea625
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:07:14 +0800

    3-24-2024 10:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,7 @@
-function selectionSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len - 1; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < len; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
+const cron = require('node-cron');
 
-        if (minIndex !== i) {
-            // Swap the elements
-            const temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
-
-    return arr;
-}
-
-// Test the selectionSort function
-const arr = [64, 25, 12, 22, 11];
-console.log("Original array: ", arr);
-const sortedArr = selectionSort(arr);
-console.log("Sorted array: ", sortedArr);
+cron.schedule('* * * * *', () => {
+  console.log('Running a task every minute.');
+});
+npm install node-cron
+node cron-job.js
