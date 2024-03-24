96703d0ed31254d10e4f3be5b8d0defe54aae675
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:14:32 +0800

    3-24-2024 9:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,17 @@
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n-1; i++) {
+        for (let j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Example usage:
+const arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Sorted array:", bubbleSort(arr));
