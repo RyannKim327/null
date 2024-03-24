a49467392e002f1ab598f1b976f5342e930b181b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:08:33 +0800

    3-24-2024 10:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,7 @@
-function countCharOccurrences(str, char) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === char) {
-            count++;
-        }
-    }
-    return count;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Test the function
-const text = "hello world";
-const charToCount = "o";
-const occurrences = countCharOccurrences(text, charToCount);
-console.log(`The character '${charToCount}' occurs ${occurrences} times in the text.`);
+let inputString = "Hello, World!";
+let outputString = removeVowels(inputString);
+console.log(outputString); // Output: "Hll, Wrld!"
