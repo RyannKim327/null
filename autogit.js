e9ee09c8a571abd4016b44f38e6b6397fab79b54
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:22:55 +0800

    3-24-2024 10:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,20 @@
-function longestCommonSubstring(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    let maxLength = 0;
-    let endIndex = 0;
-    const dp = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => 0));
-
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
+function findMajorityElement(arr) {
+    let counts = {};
+    
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        counts[num] = counts[num] ? counts[num] + 1 : 1; // Increment count of the number
+    }
+    
+    for (let key in counts) {
+        if (counts[key] > arr.length / 2) {
+            return key;
         }
     }
-
-    if (maxLength === 0) {
-        return "";
-    }
-
-    return str1.substr(endIndex - maxLength + 1, maxLength);
+    
+    return "No majority element found";
 }
 
-// Test the function
-const str1 = "hellojavascript";
-const str2 = "ilovejavascript";
-console.log(longestCommonSubstring(str1, str2)); // Output: "javascript"
+// Example usage
+const array = [2, 8, 7, 2, 2, 5, 2, 2, 9];
+console.log("Majority element: ", findMajorityElement(array));
