78f3ca6cc00b0f619bb751348b6acf9d4d3787e3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:10:53 +0800

    3-24-2024 9:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,4 @@
-function countingSort(arr) {
-    const max = Math.max(...arr);
-    const min = Math.min(...arr);
-    const range = max - min + 1;
-
-    const countArr = new Array(range).fill(0);
-    const outputArr = new Array(arr.length);
-
-    for (let i = 0; i < arr.length; i++) {
-        countArr[arr[i] - min]++;
-    }
-
-    for (let i = 1; i < range; i++) {
-        countArr[i] += countArr[i - 1];
-    }
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-        outputArr[countArr[arr[i] - min] - 1] = arr[i];
-        countArr[arr[i] - min]--;
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = outputArr[i];
-    }
-
-    return arr;
-}
-
-// Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + countingSort(arr));
+var str1 = "Hello";
+var str2 = "World";
+var result = str1 + " " + str2;
+console.log(result); // Outputs: Hello World
