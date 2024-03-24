2608eed17ac605aa5535c969c1b75f2cbfa09148
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:00:14 +0800

    3-24-2024 9:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,99 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
+class SkipNode {
+    constructor(value, level) {
+        this.value = value;
+        this.next = new Array(level + 1).fill(null);
+    }
+}
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
+class SkipList {
+    constructor(maxLevel, p) {
+        this.maxLevel = maxLevel;
+        this.p = p;
+        this.level = 0;
+        this.head = new SkipNode(null, maxLevel);
+    }
 
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+    randomLevel() {
+        let level = 0;
+        while (Math.random() < this.p && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] !== null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.next[0];
+
+        if (current === null || current.value !== value) {
+            let newLevel = this.randomLevel();
+            if (newLevel > this.level) {
+                for (let i = this.level + 1; i <= newLevel; i++) {
+                    update[i] = this.head;
+                }
+                this.level = newLevel;
+            }
+
+            let newNode = new SkipNode(value, newLevel);
+
+            for (let i = 0; i <= newLevel; i++) {
+                newNode.next[i] = update[i].next[i];
+                update[i].next[i] = newNode;
+            }
         }
     }
 
-    return -1; // return -1 if the target is not found in the array
-}
+    search(value) {
+        let current = this.head;
 
-// Example usage:
-const array = [1, 3, 5, 7, 9, 11, 13, 15, 17];
-const target = 7;
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] !== null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+        }
 
-const index = binarySearch(array, target);
-if (index !== -1) {
-    console.log(`Target ${target} found at index ${index}`);
-} else {
-    console.log(`Target ${target} not found in the array`);
+        current = current.next[0];
+
+        if (current !== null && current.value === value) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    delete(value) {
+        let update = new Array(this.maxLevel + 1).fill(null);
+        let current = this.head;
+
+        for (let i = this.level; i >= 0; i--) {
+            while (current.next[i] !== null && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.next[0];
+
+        if (current !== null && current.value === value) {
+            for (let i = 0; i <= this.level; i++) {
+                if (update[i].next[i] !== current) {
+                    break;
+                }
+                update[i].next[i] = current.next[i];
+            }
+
+            while (this.level > 0 && this.head.next[this.level] === null) {
+                this.level--;
+            }
+        }
+    }
 }
