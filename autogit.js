c1b2b00e487bf8fbe0f3089b7b6f161b33b0ad9f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:26:45 +0800

    3-24-2024 10:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,54 +1,14 @@
-// Define a Graph class to represent the graph
-class Graph {
-    constructor() {
-        this.adjList = new Map();
-    }
+// Define an array of integers
+let numbers = [5, 3, 8, 1, 2, 4];
 
-    addVertex(v) {
-        this.adjList.set(v, []);
-    }
+// Sort the array in ascending order
+numbers.sort(function(a, b) {
+  return a - b;
+});
 
-    addEdge(v, w) {
-        this.adjList.get(v).push(w);
-    }
-
-    // Depth-first search algorithm
-    dfs(start) {
-        let visited = {};
-        this._dfsHelper(start, visited);
-    }
-
-    _dfsHelper(v, visited) {
-        visited[v] = true;
-        console.log(v);
-
-        let neighbors = this.adjList.get(v);
-
-        for (let neighbor of neighbors) {
-            if (!visited[neighbor]) {
-                this._dfsHelper(neighbor, visited);
-            }
-        }
-    }
-}
-
-// Create a new graph
-let graph = new Graph();
-
-graph.addVertex(0);
-graph.addVertex(1);
-graph.addVertex(2);
-graph.addVertex(3);
-graph.addVertex(4);
-graph.addVertex(5);
-graph.addVertex(6);
-
-graph.addEdge(0, 1);
-graph.addEdge(0, 2);
-graph.addEdge(1, 3);
-graph.addEdge(1, 4);
-graph.addEdge(2, 5);
-graph.addEdge(2, 6);
-
-// Perform DFS from vertex 0
-graph.dfs(0);
+// Print the sorted array
+console.log(numbers);
+// Sort the array in descending order
+numbers.sort(function(a, b) {
+  return b - a;
+});
