2ec92096de48941754295f09da988addd4f0c6f4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:57:24 +0800

    3-24-2024 9:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,17 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+function reverseWords(str) {
+    // Split the string into an array of words
+    let wordsArray = str.split(" ");
+    
+    // Reverse the order of the array
+    wordsArray = wordsArray.reverse();
+    
+    // Join the array back into a string
+    let reversedStr = wordsArray.join(" ");
+    
+    return reversedStr;
 }
 
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    enqueue(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.size++;
-    }
-
-    dequeue() {
-        if (!this.head) {
-            return null;
-        }
-
-        const dequeuedValue = this.head.value;
-        this.head = this.head.next;
-        this.size--;
-
-        if (!this.head) {
-            this.tail = null;
-        }
-
-        return dequeuedValue;
-    }
-
-    peek() {
-        if (!this.head) {
-            return null;
-        }
-
-        return this.head.value;
-    }
-
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    getSize() {
-        return this.size;
-    }
-}
-
-// Example usage
-const queue = new Queue();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // 1
-console.log(queue.peek()); // 2
-console.log(queue.getSize()); // 2
+// Test the function
+let originalStr = "Hello world, how are you?";
+let reversedStr = reverseWords(originalStr);
+console.log(reversedStr); // Output: "you? are how world, Hello"
