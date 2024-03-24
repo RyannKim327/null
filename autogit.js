aa599f6474a2b1178a2e877c1d012bce24d4cc6b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:27:56 +0800

    3-24-2024 10:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,6 @@
-function isAnagram(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-    if (str1.length !== str2.length) {
-        return false;
-    }
-
-    const charMap = {};
-
-    for (let char of str1) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-
-    for (let char of str2) {
-        if (!charMap[char]) {
-            return false;
-        } else {
-            charMap[char]--;
-        }
-    }
-
-    return true;
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Example usage
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example: Generate a random number between 1 and 100
+console.log(getRandomNumber(1, 100));
