a965607d1681307555df966f14bc166732848dec
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:19:23 +0800

    3-24-2024 9:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,9 @@
-function longestIncreasingSubsequence(arr) {
-    let n = arr.length;
-    let lis = new Array(n).fill(1);
+const axios = require('axios');
 
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1) {
-                lis[i] = lis[j] + 1;
-            }
-        }
-    }
-
-    let maxLength = Math.max(...lis);
-    let result = [];
-    let lastIndex = lis.indexOf(maxLength);
-
-    for (let i = lastIndex; i >= 0; i--) {
-        if (lis[i] === maxLength) {
-            result.unshift(arr[i]);
-            maxLength--;
-        }
-    }
-
-    return result;
-}
-
-// Test the function
-let arr = [10, 22, 9, 33, 21, 50, 41, 60, 80];
-console.log(longestIncreasingSubsequence(arr)); // Output: [10, 22, 33, 50, 60, 80]
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
