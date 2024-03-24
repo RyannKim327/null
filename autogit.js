d314dff9b3443a4d6b5f5b6d6651d8b9415ea465
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:19:11 +0800

    3-24-2024 10:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,5 @@
-function isAnagram(str1, str2) {
-    // Remove any non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
-    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
+let str = "  Hello,   World!  ";
 
-    // Check if the lengths of the two strings are the same
-    if (str1.length !== str2.length) {
-        return false;
-    }
-
-    // Sort the characters of both strings and compare them
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
-
-    return sortedStr1 === sortedStr2;
-}
-
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
-
-if (isAnagram(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Remove whitespace using regular expression
+let trimmedStr = str.replace(/\s/g, '');
+console.log(trimmedStr);  // Output: "Hello,World!"
