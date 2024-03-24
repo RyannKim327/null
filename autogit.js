ec705acf7b9d84e81d8cd082b846efce219b59f7
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:53:32 +0800

    3-24-2024 9:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,11 @@
-function findFirstNonRepeatingChar(str) {
-    let charMap = {};
-  
-    // Create a map to store the count of each character in the string
-    for (let char of str) {
-        charMap[char] = (charMap[char] || 0) + 1;
-    }
-  
-    // Find the first character with count 1 in the map
-    for (let char of str) {
-        if (charMap[char] === 1) {
-            return char;
-        }
-    }
-  
-    return null; // Return null if there is no non-repeating character
-}
+const axios = require('axios');
 
-// Test the function
-let str = "hello world";
-let firstNonRepeatingChar = findFirstNonRepeatingChar(str);
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
 
-if (firstNonRepeatingChar) {
-    console.log(`The first non-repeating character in "${str}" is: ${firstNonRepeatingChar}`);
-} else {
-    console.log("There is no non-repeating character in the string.");
-}
+axios.get(url)
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
