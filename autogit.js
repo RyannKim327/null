5c40da5533671d4f380d2b3ad4d9e2f58f89c2e8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:19:01 +0800

    3-24-2024 10:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,29 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert to lowercase
-    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    
-    // Compare the string to its reverse
-    return str === str.split('').reverse().join('');
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+
+    if (number <= 3) {
+        return true;
+    }
+
+    if (number % 2 === 0 || number % 3 === 0) {
+        return false;
+    }
+
+    for (let i = 5; i * i <= number; i += 6) {
+        if (number % i === 0 || number % (i + 2) === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 // Test the function
-console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
-console.log(isPalindrome("Hello, World!")); // Output: false
+const num = 17;
+if (isPrime(num)) {
+    console.log(`${num} is a prime number.`);
+} else {
+    console.log(`${num} is not a prime number.`);
+}
