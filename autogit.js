75cf78d87f7a2592c0f596c29c0c07801cdbe8f9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:18:51 +0800

    3-24-2024 10:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,11 @@
-let numbers = [4, 2, 8, 5, 1];
-numbers.sort(function(a, b) {
-  return a - b;
-});
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    // Compare the string to its reverse
+    return str === str.split('').reverse().join('');
+}
 
-console.log(numbers);
-numbers.sort(function(a, b) {
-  return b - a;
-});
-
-console.log(numbers);
+// Test the function
+console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
+console.log(isPalindrome("Hello, World!")); // Output: false
