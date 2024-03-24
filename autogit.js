5e23f8a488f216789ead87a873e81aaf4836cad6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:45:20 +0800

    3-24-2024 9:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,3 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let currentVal = arr[i];
-        let j = i - 1;
-        while (j >= 0 && arr[j] > currentVal) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = currentVal;
-    }
-    return arr;
-}
-
-// Example usage:
-const arr = [5, 3, 8, 2, 1, 4];
-console.log("Original array:", arr);
-
-const sortedArr = insertionSort(arr);
-console.log("Sorted array:", sortedArr);
+let str = "   Hello, World!   ";
+let trimmedStr = str.replace(/\s/g, '');
+console.log(trimmedStr); // Output: "Hello,World!"
