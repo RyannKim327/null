edbf1c120cae82b019e14d5adbfbba2b0e37d95c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:49:39 +0800

    3-24-2024 9:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,49 @@
-function findStringLength(str) {
-    var count = 0;
-    for (var i = 0; str[i] !== undefined; i++) {
-        count++;
-    }
-    return count;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-var myString = "Hello, World!";
-var length = findStringLength(myString);
-console.log(length); // Outputs: 13
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  addNode(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  findMiddleElement() {
+    let slow = this.head;
+    let fast = this.head;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow.data;
+  }
+}
+
+// Create a linked list
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+
+// Find the middle element
+const middleElement = linkedList.findMiddleElement();
+console.log("Middle Element: ", middleElement);
