21531e3c071524228137df5051498def442e71c3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:48:24 +0800

    3-24-2024 9:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,5 @@
-function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap the elements
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    
-    return arr;
-}
+const array = [10, 5, 20, 15, 8];
 
-// Example usage
-const array = [64, 34, 25, 12, 22, 11, 90];
-console.log(bubbleSort(array));
+const maxValue = Math.max(...array);
+
+console.log("The maximum value in the array is: " + maxValue);
