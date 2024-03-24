9f9c67bb25ef0e9b96acf22dea694299621ea4ed
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 09:55:16 +0800

    3-24-2024 9:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,24 @@
-// Linked List Node
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+function findMajorityElement(arr) {
+    let count = 0;
+    let candidate = null;
 
-// Function to check if a linked list is a palindrome
-function isPalindrome(head) {
-    let values = [];
-    let currentNode = head;
-
-    // Traverse the linked list and store node values in an array
-    while (currentNode) {
-        values.push(currentNode.data);
-        currentNode = currentNode.next;
-    }
-
-    // Use two-pointer technique to check if the array is a palindrome
-    let left = 0;
-    let right = values.length - 1;
-
-    while (left < right) {
-        if (values[left] !== values[right]) {
-            return false;
+    for (let num of arr) {
+        if (count === 0) {
+            candidate = num;
         }
-        left++;
-        right--;
+
+        count += (num === candidate) ? 1 : -1;
     }
 
-    return true;
+    return candidate;
 }
 
-// Example linked list
-let node1 = new Node(1);
-let node2 = new Node(2);
-let node3 = new Node(3);
-let node4 = new Node(2);
-let node5 = new Node(1);
+// Example usage
+const arr = [2, 2, 1, 1, 1, 2, 2];
+const majorityElement = findMajorityElement(arr);
 
-node1.next = node2;
-node2.next = node3;
-node3.next = node4;
-node4.next = node5;
-
-console.log(isPalindrome(node1)); // Output: true
+if (majorityElement) {
+    console.log(`The majority element is: ${majorityElement}`);
+} else {
+    console.log("No majority element found");
+}
