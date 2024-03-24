bea3933210b68a512814b3a4a466a7c45b361155
RyannKim327 <rksesgundo123@gmail.com>
2024-03-24 10:04:40 +0800

    3-24-2024 10:4 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,6 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
-const commonElements = array1.filter(element => array2.includes(element));
-
-console.log(commonElements); // Output: [3, 4, 5]
+let str = "   Hello, World!   ";
+let trimmedStr = str.trim();
+console.log(trimmedStr); // Output: "Hello, World!"
+let str = "   Hello, World!   ";
+let trimmedStr = str.replace(/\s/g,'');
+console.log(trimmedStr); // Output: "Hello,World!"
