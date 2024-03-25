7d9633f31ace18dee21fbaa657560ac9efc648a3
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:21:43 +0800

    3-25-2024 22:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,42 @@
-function isAnagram(str1, str2) {
-  // Remove spaces and punctuation, convert to lowercase, and sort the characters
-  const cleanStr1 = str1.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-  const cleanStr2 = str2.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
-
-  // Check if the sorted strings are equal
-  return cleanStr1 === cleanStr2;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
-// Test the function
-const str1 = "listen";
-const str2 = "silent";
-if (isAnagram(str1, str2)) {
-  console.log(`${str1} and ${str2} are anagrams.`);
-} else {
-  console.log(`${str1} and ${str2} are not anagrams.`);
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
 }
+
+// Example usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
