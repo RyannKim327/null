14498e58f5e55247e90ec9f90d73a1303966ea7e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:21:24 +0800

    3-25-2024 22:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,71 +1,49 @@
-class PriorityQueue {
-  constructor() {
-    this.queue = [];
-  }
+function fibonacciSearch(arr, x) {
+    let fib1 = 0;
+    let fib2 = 1;
+    let fib3 = fib1 + fib2;
 
-  enqueue(item, priority) {
-    this.queue.push({ item, priority });
-    this.queue.sort((a, b) => a.priority - b.priority);
-  }
+    let n = arr.length;
 
-  dequeue() {
-    return this.queue.shift().item;
-  }
-
-  isEmpty() {
-    return this.queue.length === 0;
-  }
-}
-
-function beamSearch(initialNode, beamWidth, maxDepth, evaluationFunction) {
-  const queue = new PriorityQueue();
-  queue.enqueue([initialNode], 0);
-
-  while (!queue.isEmpty()) {
-    const path = queue.dequeue();
-    const node = path[path.length - 1];
-
-    if (path.length === maxDepth) {
-      return path;
+    while (fib3 < n) {
+        fib1 = fib2;
+        fib2 = fib3;
+        fib3 = fib1 + fib2;
     }
 
-    node.neighbors.forEach((neighbor) => {
-      const newPath = [...path, neighbor];
-      const score = evaluationFunction(newPath);
-      queue.enqueue(newPath, score);
-    });
+    let offset = -1;
 
-    while (queue.queue.length > beamWidth) {
-      queue.queue.pop();
+    while (fib3 > 1) {
+        let i = Math.min(offset + fib1, n - 1);
+
+        if (arr[i] < x) {
+            fib3 = fib2;
+            fib2 = fib1;
+            fib1 = fib3 - fib2;
+            offset = i;
+        } else if (arr[i] > x) {
+            fib3 = fib1;
+            fib2 = fib2 - fib1;
+            fib1 = fib3 - fib2;
+        } else {
+            return i;
+        }
     }
-  }
 
-  return null;
+    if (fib2 && arr[offset + 1] == x) {
+        return offset + 1;
+    }
+
+    return -1;
 }
 
-// Example usage
-class Node {
-  constructor(value, neighbors) {
-    this.value = value;
-    this.neighbors = neighbors;
-  }
+let arr = [1, 4, 8, 12, 20, 30, 40, 50];
+let x = 12;
+
+let result = fibonacciSearch(arr, x);
+
+if (result == -1) {
+    console.log("Element not found");
+} else {
+    console.log("Element found at index: " + result);
 }
-
-const node1 = new Node("A", []);
-const node2 = new Node("B", []);
-const node3 = new Node("C", []);
-const node4 = new Node("D", []);
-const node5 = new Node("E", []);
-const node6 = new Node("F", []);
-
-node1.neighbors = [node2, node3];
-node2.neighbors = [node4, node5];
-node3.neighbors = [node5, node6];
-
-const evaluationFunction = (path) => {
-  // Simple evaluation function: sum of values along the path
-  return path.reduce((acc, node) => acc + node.value.charCodeAt(0), 0);
-};
-
-const result = beamSearch(node1, 2, 3, evaluationFunction);
-console.log(result);
