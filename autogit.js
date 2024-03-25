98f3c447621b0dd8672c1736435913668021cce9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:33:26 +0800

    3-25-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,5 @@
-function selectionSort(arr) {
-    const n = arr.length;
-    
-    for(let i = 0; i < n; i++) {
-        let minIdx = i;
-        
-        for(let j = i + 1; j < n; j++) {
-            if(arr[j] < arr[minIdx]) {
-                minIdx = j;
-            }
-        }
-        
-        if(minIdx !== i) {
-            [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
-        }
-    }
-    
-    return arr;
-}
+// Prompt the user for input
+let userInput = prompt("Enter some text:");
 
-// Example
-const array = [64, 25, 12, 22, 11];
-const sortedArray = selectionSort(array);
-console.log(sortedArray);
+// Display the user input on the console
+console.log("You entered: " + userInput);
