27f87fe9f0e6f9e6dff745fbc094275890addd08
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:32:51 +0800

    3-25-2024 22:32 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,100 @@
-function reverseWords(str) {
-    return str.split(' ').reverse().join(' ');
+class PriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+
+  getParentIndex(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  getLeftChildIndex(index) {
+    return 2 * index + 1;
+  }
+
+  getRightChildIndex(index) {
+    return 2 * index + 2;
+  }
+
+  swap(index1, index2) {
+    const temp = this.heap[index1];
+    this.heap[index1] = this.heap[index2];
+    this.heap[index2] = temp;
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    const minValue = this.heap[0];
+    const lastValue = this.heap.pop();
+
+    if (this.heap.length > 0) {
+      this.heap[0] = lastValue;
+      this.heapifyDown();
+    }
+
+    return minValue;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+
+      if (this.heap[parentIndex] > this.heap[index]) {
+        this.swap(parentIndex, index);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      const leftChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+      let smallerChildIndex = leftChildIndex;
+
+      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[leftChildIndex]) {
+        smallerChildIndex = rightChildIndex;
+      }
+
+      if (this.heap[index] > this.heap[smallerChildIndex]) {
+        this.swap(index, smallerChildIndex);
+        index = smallerChildIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.heap[0];
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
 }
 
-// Example usage
-const originalString = 'Hello world from JavaScript';
-const reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: 'JavaScript from world Hello'
+// Example Usage
+const pq = new PriorityQueue();
+pq.push(10);
+pq.push(5);
+pq.push(15);
+
+console.log(pq.pop()); // Output: 5
+console.log(pq.pop()); // Output: 10
