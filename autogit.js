db9588647b74490266cf96f6f27c2959ffc27fb8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:38:22 +0800

    3-25-2024 22:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,5 @@
-function majorityElement(nums) {
-    let count = {};
-    
-    for (let num of nums) {
-        count[num] = (count[num] || 0) + 1;
-        if (count[num] > nums.length / 2) {
-            return num;
-        }
-    }
-    
-    return null;
-}
+const cron = require('node-cron');
 
-// Example
-const array = [2, 3, 2, 2, 4, 2, 2, 3, 2];
-console.log(majorityElement(array)); // Output: 2
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute!');
+});
