92cc6f662fa11a6fabe140f65bc546763d51bfcd
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:22:30 +0800

    3-25-2024 22:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,79 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+  constructor(order, leaf) {
+    this.order = order;
+    this.leaf = leaf;
+    this.keys = [];
+    this.children = [];
+  }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
+class BTree {
+  constructor(order) {
+    this.root = new Node(order, true);
+    this.order = order;
+  }
 
-    append(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
+  search(key, node = this.root) {
+    let i = 0;
+    while (i < node.keys.length && key > node.keys[i]) {
+      i++;
     }
+    if (node.keys[i] === key) {
+      return node;
+    }
+    if (node.leaf) {
+      return null;
+    } else {
+      return this.search(key, node.children[i]);
+    }
+  }
 
-    findNthFromEnd(n) {
-        let pointer1 = this.head;
-        let pointer2 = this.head;
-        for (let i = 0; i < n; i++) {
-            if (pointer2 === null) {
-                return null; // List is too short
-            }
-            pointer2 = pointer2.next;
-        }
-        while (pointer2.next) {
-            pointer1 = pointer1.next;
-            pointer2 = pointer2.next;
-        }
-        return pointer1.data;
+  insert(key) {
+    const root = this.root;
+    if (root.keys.length === 2 * this.order - 1) {
+      const newRoot = new Node(this.order, false);
+      newRoot.children.push(root);
+      this.splitChild(newRoot, 0);
+      this.insertNonFull(newRoot, key);
+      this.root = newRoot;
+    } else {
+      this.insertNonFull(root, key);
     }
+  }
+
+  splitChild(parent, i) {
+    const order = this.order;
+    const child = parent.children[i];
+    const newChild = new Node(order, child.leaf);
+    parent.keys.splice(i, 0, child.keys[order - 1]);
+    parent.children.splice(i + 1, 0, newChild);
+    newChild.keys = child.keys.splice(order, order - 1);
+    if (!child.leaf) {
+      newChild.children = child.children.splice(order, order);
+    }
+  }
+
+  insertNonFull(node, key) {
+    let i = node.keys.length - 1;
+    if (node.leaf) {
+      node.keys.push(null);
+      while (i >= 0 && key < node.keys[i]) {
+        node.keys[i + 1] = node.keys[i];
+        i--;
+      }
+      node.keys[i + 1] = key;
+    } else {
+      while (i >= 0 && key < node.keys[i]) {
+        i--;
+      }
+      i++;
+      if (node.children[i].keys.length === 2 * this.order - 1) {
+        this.splitChild(node, i);
+        if (key > node.keys[i]) {
+          i++;
+        }
+      }
+      this.insertNonFull(node.children[i], key);
+    }
+  }
 }
-
-// Test
-const ll = new LinkedList();
-ll.append(1);
-ll.append(2);
-ll.append(3);
-ll.append(4);
-ll.append(5);
-
-const n = 2; // Find the 2nd node from the end
-const nthFromEnd = ll.findNthFromEnd(n);
-console.log(`${n}th node from the end is: ${nthFromEnd}`);
