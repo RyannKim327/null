1f3b1c53a7f2a1da207b5e07bf818eb588f7a9c9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:30:20 +0800

    3-25-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,25 @@
-const axios = require('axios');
+function countingSort(arr, max) {
+    const count = new Array(max + 1).fill(0);
+    const output = new Array(arr.length);
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    for (let num of arr) {
+        count[num]++;
+    }
+
+    for (let i = 1; i < count.length; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (let num of arr) {
+        output[count[num] - 1] = num;
+        count[num]--;
+    }
+
+    return output;
+}
+
+// Example usage
+const arr = [4, 2, 2, 8, 3, 3, 1];
+const max = Math.max(...arr);
+const sortedArr = countingSort(arr, max);
+console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
