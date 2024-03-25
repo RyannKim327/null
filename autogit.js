c031e5761e2417937c2a0bf8cf93fc202fc2cbf9
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:31:50 +0800

    3-25-2024 22:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,39 @@
-let str = "42";
-let num = parseInt(str);
+// Definition for a singly-linked list node
+class ListNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
-console.log(num); // Output: 42
+// Function to check if a linked list is a palindrome
+function isPalindrome(head) {
+    // Convert the linked list into an array
+    let arr = [];
+    let current = head;
+    while (current !== null) {
+        arr.push(current.val);
+        current = current.next;
+    }
+
+    // Use two pointers technique to check palindrome
+    let left = 0;
+    let right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] !== arr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+
+// Example linked list
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(2);
+head.next.next.next.next = new ListNode(1);
+
+console.log(isPalindrome(head)); // Output should be true
