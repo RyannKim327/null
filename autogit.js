37d9969bbd19d61d170802effc0cfe1d5752103c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:36:30 +0800

    3-25-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,5 @@
-function findMedianSortedArrays(nums1, nums2) {
-    const combinedArray = [...nums1, ...nums2].sort((a, b) => a - b);
-    const n = combinedArray.length;
-    
-    if (n % 2 === 0) {
-        // If the combined array has an even number of elements, median is average of two middle elements
-        const midIndex = n / 2;
-        return (combinedArray[midIndex - 1] + combinedArray[midIndex]) / 2;
-    } else {
-        // If the combined array has an odd number of elements, median is the middle element
-        const midIndex = Math.floor(n / 2);
-        return combinedArray[midIndex];
-    }
-}
+// Take input from the user
+let userInput = prompt("Enter something:");
 
-const nums1 = [1, 3];
-const nums2 = [2, 4];
-
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.5
+// Display the input
+console.log("You entered: " + userInput);
