8ae065886b6891487d667968fbe6cd57f442c20a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:34:05 +0800

    3-25-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,28 @@
-const axios = require('axios');
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// Function to find the sum of all nodes in a binary tree
+function sumBinaryTree(root) {
+    if (root === null) {
+        return 0;
+    }
+    return root.value + sumBinaryTree(root.left) + sumBinaryTree(root.right);
+}
+
+// Example Binary Tree
+let root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+// Calculate the sum of all nodes
+let sum = sumBinaryTree(root);
+console.log("Sum of all nodes in the binary tree: " + sum);
