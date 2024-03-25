694f9a1ef080d989f544733165b1b7a46a977b46
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 21:09:44 +0800

    3-25-2024 21:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,36 @@
-function findFirstRepeatedChar(str) {
-    let charMap = {};
-    
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
+class ListNode {
+    constructor(value) {
+        this.val = value;
+        this.next = null;
     }
-    
-    return null;
 }
 
-let str = "javascript";
-let repeatedChar = findFirstRepeatedChar(str);
+function getIntersectionNode(headA, headB) {
+    if (!headA || !headB) {
+        return null;
+    }
 
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("There are no repeated characters in the string.");
+    let pointerA = headA;
+    let pointerB = headB;
+
+    while (pointerA !== pointerB) {
+        pointerA = pointerA ? pointerA.next : headB;
+        pointerB = pointerB ? pointerB.next : headA;
+    }
+
+    return pointerA;
 }
+
+// Example usage
+const listA = new ListNode(4);
+listA.next = new ListNode(1);
+listA.next.next = new ListNode(8);
+listA.next.next.next = new ListNode(4);
+listA.next.next.next.next = new ListNode(5);
+
+const listB = new ListNode(5);
+listB.next = new ListNode(0);
+listB.next.next = listA.next.next; // Intersection point
+
+const intersectionNode = getIntersectionNode(listA, listB);
+console.log(intersectionNode); // Output: ListNode { val: 8, next: ListNode { val: 4, next: ListNode { val: 5, next: null } } }
