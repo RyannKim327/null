a23247217059ae707d33a2cc2e3bb56b697c6d08
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:33:49 +0800

    3-25-2024 22:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,9 @@
-function calculateMean(numbers) {
-    var sum = numbers.reduce(function(a, b) { return a + b; }, 0);
-    return sum / numbers.length;
-}
+const axios = require('axios');
 
-// Example usage
-var numbers = [1, 2, 3, 4, 5];
-var mean = calculateMean(numbers);
-console.log(mean);
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
