5637db803c7cb304951258f8d7544decf46a40f8
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 21:07:28 +0800

    3-25-2024 21:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,70 @@
-class HashTable {
-    constructor() {
-        this.table = new Array(100); // Initialize an array with 100 slots
-    }
-
-    hash(key) {
-        let hashValue = 0;
-        for (let i = 0; i < key.length; i++) {
-            hashValue += key.charCodeAt(i) * (i + 1);
-        }
-        return hashValue % this.table.length; // Modulo to fit within the array size
-    }
-
-    set(key, value) {
-        const index = this.hash(key);
-        if (this.table[index]) {
-            this.table[index].push([key, value]);
-        } else {
-            this.table[index] = [[key, value]];
-        }
-    }
-
-    get(key) {
-        const index = this.hash(key);
-        if (!this.table[index]) {
-            return undefined;
-        }
-
-        for (let entry of this.table[index]) {
-            if (entry[0] === key) {
-                return entry[1];
-            }
-        }
-
-        return undefined;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
-// Example usage
-const hashTable = new HashTable();
-hashTable.set("name", "Alice");
-hashTable.set("age", 30);
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
 
-console.log(hashTable.get("name")); // Output: Alice
-console.log(hashTable.get("age")); // Output: 30
-console.log(hashTable.get("city")); // Output: undefined
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (node.value === value) {
+            return true;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else {
+            return this.searchNode(node.right, value);
+        }
+    }
+}
+
+// Example usage:
+const tree = new BinaryTree();
+tree.insert(5);
+tree.insert(3);
+tree.insert(7);
+tree.insert(2);
+tree.insert(4);
+
+console.log(tree.search(2)); // true
+console.log(tree.search(6)); // false
