03b00959c88b0b527192e31f5bb4deee2bf826ea
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:30:33 +0800

    3-25-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,101 @@
-function countingSort(arr, max) {
-    const count = new Array(max + 1).fill(0);
-    const output = new Array(arr.length);
-
-    for (let num of arr) {
-        count[num]++;
+class AVLNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
     }
-
-    for (let i = 1; i < count.length; i++) {
-        count[i] += count[i - 1];
-    }
-
-    for (let num of arr) {
-        output[count[num] - 1] = num;
-        count[num]--;
-    }
-
-    return output;
 }
 
-// Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-const max = Math.max(...arr);
-const sortedArr = countingSort(arr, max);
-console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
+class AVLTree {
+    constructor() {
+        this.root = null;
+    }
+
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+    }
+
+    rotateRight(y) {
+        const x = y.left;
+        const T = x.right;
+
+        x.right = y;
+        y.left = T;
+
+        this.updateHeight(y);
+        this.updateHeight(x);
+
+        return x;
+    }
+
+    rotateLeft(x) {
+        const y = x.right;
+        const T = y.left;
+
+        y.left = x;
+        x.right = T;
+
+        this.updateHeight(x);
+        this.updateHeight(y);
+
+        return y;
+    }
+
+    insertHelper(node, value) {
+        if (node === null) {
+            return new AVLNode(value);
+        }
+
+        if (value < node.value) {
+            node.left = this.insertHelper(node.left, value);
+        } else {
+            node.right = this.insertHelper(node.right, value);
+        }
+
+        this.updateHeight(node);
+
+        const balance = this.getBalanceFactor(node);
+
+        if (balance > 1 && value < node.left.value) {
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value > node.right.value) {
+            return this.rotateLeft(node);
+        }
+
+        if (balance > 1 && value > node.left.value) {
+            node.left = this.rotateLeft(node.left);
+            return this.rotateRight(node);
+        }
+
+        if (balance < -1 && value < node.right.value) {
+            node.right = this.rotateRight(node.right);
+            return this.rotateLeft(node);
+        }
+
+        return node;
+    }
+
+    insert(value) {
+        this.root = this.insertHelper(this.root, value);
+    }
+}
+
+// Usage
+const avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(20);
+avlTree.insert(30);
