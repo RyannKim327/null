8291e14c80ce471b8ccd888c1b0aa5d893e153d4
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 21:07:47 +0800

    3-25-2024 21:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,8 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+let str = "42";
+let num = parseInt(str);
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+console.log(num); // Output: 42
+let str = "42";
+let num = Number(str);
 
-  append(data) {
-    const newNode = new Node(data);
-
-    if (!this.head) {
-      this.head = newNode;
-      return;
-    }
-
-    let current = this.head;
-    while (current.next) {
-      current = current.next;
-    }
-
-    current.next = newNode;
-  }
-
-  print() {
-    let current = this.head;
-    while (current) {
-      console.log(current.data);
-      current = current.next;
-    }
-  }
-}
-
-// Example usage
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
-
-linkedList.print();
+console.log(num); // Output: 42
