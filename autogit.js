e6af74eaf6552b8bba035ed13fd6d4081fc21664
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:17:05 +0800

    3-25-2024 22:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,41 @@
-let str = "42";
-let num = parseInt(str);
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-console.log(num); // Output: 42
-let hexStr = "1A";
-let hexNum = parseInt(hexStr, 16);
+function hasCycle(head) {
+    if (!head || !head.next) {
+        return false;
+    }
 
-console.log(hexNum); // Output: 26
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Example usage
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+node5.next = node2; // creates a cycle
+
+console.log(hasCycle(node1)); // Output: true
