7d230cb91cdb37836558e16b9e03b71da3ad4e1a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:40:08 +0800

    3-25-2024 22:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,35 @@
-let arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
+function topologicalSort(graph) {
+    const visited = {};
+    const stack = [];
+    
+    function dfs(node) {
+        visited[node] = true;
+        
+        for (let neighbor of graph[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
+        
+        stack.unshift(node);
+    }
+    
+    for (let node in graph) {
+        if (!visited[node]) {
+            dfs(node);
+        }
+    }
+    
+    return stack;
+}
+
+// Example graph represented as an adjacency list
+const graph = {
+    1: [2, 3],
+    2: [4],
+    3: [4, 5],
+    4: [],
+    5: []
+};
+
+console.log(topologicalSort(graph)); // Output: [1, 3, 5, 2, 4]
