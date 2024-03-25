b7d6e98f5c6491d5772e61f3caf106e99358ac7d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:26:37 +0800

    3-25-2024 22:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,36 @@
-function findMedianSortedArrays(nums1, nums2) {
-    let combined = nums1.concat(nums2).sort((a, b) => a - b);
-    let medianIndex = Math.floor(combined.length / 2);
+class BeamSearch {
+    constructor(beamWidth, maxSteps) {
+        this.beamWidth = beamWidth;
+        this.maxSteps = maxSteps;
+    }
 
-    if (combined.length % 2 === 0) {
-        return (combined[medianIndex - 1] + combined[medianIndex]) / 2;
-    } else {
-        return combined[medianIndex];
+    search(startState, getNextStates, evaluate) {
+        let beams = [{ state: startState, score: 0, path: [startState]}];
+
+        for (let step = 0; step < this.maxSteps; step++) {
+            let nextBeams = [];
+            for (let beam of beams) {
+                let nextStates = getNextStates(beam.state);
+                for (let nextState of nextStates) {
+                    let score = beam.score + evaluate(nextState);
+                    nextBeams.push({ state: nextState, score: score, path: beam.path.concat([nextState]) });
+                }
+            }
+
+            nextBeams.sort((a, b) => b.score - a.score);
+            beams = nextBeams.slice(0, this.beamWidth);
+        }
+
+        return beams[0].path;
     }
 }
 
-// Example usage
-let nums1 = [1, 3];
-let nums2 = [2];
-console.log(findMedianSortedArrays(nums1, nums2)); // Output: 2.0
+// Usage example
+const beamSearch = new BeamSearch(3, 5);
+
+const startState = 1;
+const getNextStates = (state) => [state + 1, state + 2];
+const evaluate = (state) => -Math.abs(state - 10); // Example evaluation function that tries to get closer to 10
+
+const resultPath = beamSearch.search(startState, getNextStates, evaluate);
+console.log(resultPath);
