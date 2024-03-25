2edac7425ee030261b5281e9c0e03e9abd948ab1
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 21:09:05 +0800

    3-25-2024 21:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,68 @@
-let arr = [5, 2, 9, 12, 4, 7];
-let max = Math.max.apply(null, arr);
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-console.log("The maximum value in the array is: " + max);
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new TreeNode(value);
+
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (node.value === value) {
+            return true;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else {
+            return this.searchNode(node.right, value);
+        }
+    }
+}
+
+// Example usage:
+const binaryTree = new BinaryTree();
+binaryTree.insert(5);
+binaryTree.insert(3);
+binaryTree.insert(8);
+
+console.log(binaryTree.search(3)); // Output: true
+console.log(binaryTree.search(6)); // Output: false
