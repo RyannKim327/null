5f8c080d5112b47eb97766e66003c93bcb188f85
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:39:55 +0800

    3-25-2024 22:39 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,17 @@
-function decimalToBinary(decimal) {
-    return decimal.toString(2);
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
-// Example usage
-const decimalNumber = 10;
-const binaryNumber = decimalToBinary(decimalNumber);
-console.log(binaryNumber); // Output: 1010
+// Usage
+const array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+console.log(bubbleSort(array));
