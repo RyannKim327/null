d76f45f363413e08f50476bcd4aa38700d45982b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:23:35 +0800

    3-25-2024 22:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,16 @@
-// Node class to represent each node in the linked list
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+    constructor(t, leaf) {
+        this.keys = [];
+        this.children = [];
+        this.leaf = leaf;
+        this.degree = t;
     }
 }
-
-// LinkedList class to represent the linked list
-class LinkedList {
-    constructor() {
-        this.head = null;
+class BTree {
+    constructor(t) {
+        this.root = new Node(t, true);
+        this.t = t;
     }
-
-    // Method to add a new node to the end of the linked list
-    addNode(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Method to print all nodes in the linked list
-    printList() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
+    
+    // Implement methods for insertion, deletion, search, and other operations
 }
-
-// Usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-
-linkedList.printList();
