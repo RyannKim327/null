47e69267bba743ad0c1afb3c51a6914d98d22607
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:30:08 +0800

    3-25-2024 22:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,9 @@
-function BFS(graph, startNode) {
-    let visited = {};
-    let queue = [];
+const axios = require('axios');
 
-    visited[startNode] = true;
-    queue.push(startNode);
-
-    while (queue.length > 0) {
-        let currentNode = queue.shift();
-        console.log(currentNode);
-
-        let neighbors = graph[currentNode];
-        for (let i = 0; i < neighbors.length; i++) {
-            let neighbor = neighbors[i];
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                queue.push(neighbor);
-            }
-        }
-    }
-}
-
-// Example graph
-const graph = {
-    A: ['B', 'C'],
-    B: ['A', 'D', 'E'],
-    C: ['A', 'F'],
-    D: ['B'],
-    E: ['B', 'F'],
-    F: ['C', 'E']
-};
-
-BFS(graph, 'A');
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
