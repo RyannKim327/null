c62f74c5370c7aeff7d0379a813bc90eed9f9d7d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:15:42 +0800

    3-25-2024 22:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,32 @@
-function findKthSmallestElement(arr, k) {
-    arr.sort((a, b) => a - b);
-    return arr[k - 1];
+function longestCommonSubstring(str1, str2) {
+    let m = str1.length;
+    let n = str2.length;
+    let longestSubstringLength = 0;
+    let endIndex = 0;
+
+    // Create a 2D array to store the lengths of longest common suffixes
+    let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > longestSubstringLength) {
+                    longestSubstringLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            }
+        }
+    }
+
+    if (longestSubstringLength === 0) {
+        return "";
+    }
+
+    return str1.substr(endIndex - longestSubstringLength + 1, longestSubstringLength);
 }
 
-const array = [3, 1, 4, 2, 5];
-const k = 3;
-
-const kthSmallestElement = findKthSmallestElement(array, k);
-console.log(`The ${k}th smallest element in the array is: ${kthSmallestElement}`);
+// Test the function
+let str1 = "abcdefgh";
+let str2 = "xbcdzabcde";
+console.log(longestCommonSubstring(str1, str2)); // Output: "bcde"
