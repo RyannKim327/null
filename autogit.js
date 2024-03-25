4ccd09d3e407f46915840511c123e4ea1b22e215
RyannKim327 <rksesgundo123@gmail.com>
2024-03-25 22:17:44 +0800

    3-25-2024 22:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,76 @@
-function isAnagram(str1, str2) {
-    // Remove non-alphabetic characters and convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-    // Sort the characters in the strings
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
-
-    // Compare the sorted strings
-    return sortedStr1 === sortedStr2;
+class Node {
+    constructor(value, color) {
+        this.value = value;
+        this.color = color || 'red';
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+    }
 }
 
-// Test the function
-const str1 = "listen";
-const str2 = "silent";
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
 
-if (isAnagram(str1, str2)) {
-    console.log(`${str1} and ${str2} are anagrams.`);
-} else {
-    console.log(`${str1} and ${str2} are not anagrams.`);
+    insert(value) {
+        const newNode = new Node(value);
+        this.root = this.insertNode(this.root, newNode);
+        this.root.color = 'black';
+    }
+
+    insertNode(root, newNode) {
+        if (!root) {
+            return newNode;
+        }
+
+        if (newNode.value < root.value) {
+            root.left = this.insertNode(root.left, newNode);
+            root.left.parent = root;
+        } else if (newNode.value > root.value) {
+            root.right = this.insertNode(root.right, newNode);
+            root.right.parent = root;
+        }
+
+        if (this.isRed(root.right) && !this.isRed(root.left)) {
+            root = this.rotateLeft(root);
+        }
+        if (this.isRed(root.left) && this.isRed(root.left.left)) {
+            root = this.rotateRight(root);
+        }
+        if (this.isRed(root.left) && this.isRed(root.right)) {
+            this.flipColors(root);
+        }
+
+        return root;
+    }
+
+    isRed(node) {
+        if (!node) return false;
+        return node.color === 'red';
+    }
+
+    rotateLeft(node) {
+        let temp = node.right;
+        node.right = temp.left;
+        temp.left = node;
+        temp.color = node.color;
+        node.color = 'red';
+        return temp;
+    }
+
+    rotateRight(node) {
+        let temp = node.left;
+        node.left = temp.right;
+        temp.right = node;
+        temp.color = node.color;
+        node.color = 'red';
+        return temp;
+    }
+
+    flipColors(node) {
+        node.color = 'red';
+        node.left.color = 'black';
+        node.right.color = 'black';
+    }
 }
