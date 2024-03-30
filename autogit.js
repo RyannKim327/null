f53c07ab39c78e483639fc0c4c4ead436a5f8ec0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:19:24 +0800

    3-31-2024 5:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,8 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert the string to lowercase
-    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    
-    // Compare the clean string with its reverse
-    return cleanStr === cleanStr.split('').reverse().join('');
-}
-
-// Example usage
-console.log(isPalindrome("A man, a plan, a canal, Panama")); // Output: true
-console.log(isPalindrome("Hello, World!")); // Output: false
+let string1 = "Hello";
+let string2 = "World";
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString); // Output will be "Hello World"
+let string1 = "Hello";
+let string2 = "World";
+let concatenatedString = string1.concat(" ", string2);
+console.log(concatenatedString); // Output will be "Hello World"
