7b7f2a21134ef278e0b4b74590bc317740107f00
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:11:37 +0800

    3-31-2024 5:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,11 @@
-// Helper function to get the digit at a specific position of a number
-function getDigit(num, i) {
-  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
-}
+const axios = require('axios');
 
-// Helper function to get the number of digits in a number
-function digitCount(num) {
-  if (num === 0) return 1;
-  return Math.floor(Math.log10(Math.abs(num))) + 1;
-}
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
 
-// Helper function to get the number of digits in the largest number in an array
-function mostDigits(arr) {
-  let maxDigits = 0;
-  for (let i = 0; i < arr.length; i++) {
-    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
-  }
-  return maxDigits;
-}
-
-// Radix sort function
-function radixSort(arr) {
-  let maxDigits = mostDigits(arr);
-  for (let k = 0; k < maxDigits; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      let digit = getDigit(arr[i], k);
-      digitBuckets[digit].push(arr[i]);
-    }
-    arr = [].concat(...digitBuckets);
-  }
-  return arr;
-}
-
-// Usage
-let arr = [170, 45, 75, 90, 802, 24, 2, 66];
-console.log(radixSort(arr)); // Output: [2, 24, 45, 66, 75, 90, 170, 802]
+axios.get(url)
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
