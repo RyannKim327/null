029f1da89c19d627fac1919239bd74e173ae082d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:27:15 +0800

    3-31-2024 5:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,61 @@
-function mergeSortIterative(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-
-  const n = arr.length;
-  const result = [...arr];
-  const buffer = new Array(n);
-
-  for (let size = 1; size < n; size *= 2) {
-    for (let leftStart = 0; leftStart < n; leftStart += 2*size) {
-      const mid = Math.min(leftStart + size, n);
-      const rightStart = Math.min(leftStart + 2*size, n);
-
-      merge(result, buffer, leftStart, mid, rightStart);
-    }
-
-    // Swap the buffers
-    const temp = result;
-    result = buffer;
-    buffer = temp;
-  }
-
-  return result;
+// Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
 }
 
-function merge(arr, buffer, leftStart, mid, rightStart) {
-  let left = leftStart;
-  let right = mid;
-  let index = leftStart;
+// Function to find the intersection point of two linked lists
+function getIntersectionNode(headA, headB) {
+    let lenA = getLength(headA);
+    let lenB = getLength(headB);
 
-  while (left < mid && right < rightStart) {
-    if (arr[left] < arr[right]) {
-      buffer[index++] = arr[left++];
-    } else {
-      buffer[index++] = arr[right++];
+    let currentA = headA;
+    let currentB = headB;
+
+    // Traverse the longer list by the difference in lengths
+    if (lenA > lenB) {
+        for (let i = 0; i < lenA - lenB; i++) {
+            currentA = currentA.next;
+        }
+    } else if (lenB > lenA) {
+        for (let i = 0; i < lenB - lenA; i++) {
+            currentB = currentB.next;
+        }
     }
-  }
 
-  while (left < mid) {
-    buffer[index++] = arr[left++];
-  }
+    // Iterate over both lists in parallel to find the intersection point
+    while (currentA !== currentB) {
+        currentA = currentA.next;
+        currentB = currentB.next;
+    }
 
-  while (right < rightStart) {
-    buffer[index++] = arr[right++];
-  }
+    return currentA; // Returns the intersection node or null if there is no intersection
 }
 
-// Example
-const arr = [38, 27, 43, 3, 9, 82, 10];
-const sortedArr = mergeSortIterative(arr);
-console.log(sortedArr);
+// Function to get the length of a linked list
+function getLength(head) {
+    let current = head;
+    let length = 0;
+
+    while (current !== null) {
+        length++;
+        current = current.next;
+    }
+
+    return length;
+}
+
+// Example usage
+let commonNode = new ListNode(5);
+commonNode.next = new ListNode(10);
+
+let list1 = new ListNode(1);
+list1.next = new ListNode(2);
+list1.next.next = commonNode;
+
+let list2 = new ListNode(3);
+list2.next = new ListNode(4);
+list2.next.next = commonNode;
+
+let intersectionNode = getIntersectionNode(list1, list2);
+console.log(intersectionNode.val); // Output: 5
