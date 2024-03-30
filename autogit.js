460c4b9c31deb079ef7d5a33482acfd9337205d2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:28:49 +0800

    3-31-2024 5:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,42 @@
-function isPalindrome(s) {
-    let left = 0;
-    let right = s.length - 1;
-
-    while (left < right) {
-        if (s[left] !== s[right]) {
-            return false;
-        }
-        left++;
-        right--;
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-
-    return true;
 }
 
-// Test the function
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello")); // false
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example Usage
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+
+list.printList();
