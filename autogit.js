ad9e717a73b8cfe633090615f693fa2fe99b6cc5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:27:37 +0800

    3-31-2024 5:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,14 @@
-function largestPrimeFactor(number) {
-    let maxPrime = -1;
-
-    // Divide by 2 until the number becomes odd
-    while (number % 2 === 0) {
-        maxPrime = 2;
-        number = number / 2;
-    }
-
-    // Find the largest prime factor
-    for (let i = 3; i <= Math.sqrt(number); i = i + 2) {
-        while (number % i === 0) {
-            maxPrime = i;
-            number = number / i;
-        }
-    }
-
-    // If the number itself is prime
-    if (number > 2) {
-        maxPrime = number;
-    }
-
-    return maxPrime;
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert the string to lowercase
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    
+    // Reverse the cleaned string
+    const reversedStr = cleanStr.split('').reverse().join('');
+    
+    // Check if the cleaned string is equal to its reversed version
+    return cleanStr === reversedStr;
 }
 
-const number = 600851475143;
-const largestPrime = largestPrimeFactor(number);
-
-console.log("Largest Prime Factor of", number, "is", largestPrime);
+// Test the function
+const testString = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(testString)); // Output: true
