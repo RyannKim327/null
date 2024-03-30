64c597a67d955c69830707632804499f448e63b5
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:08:07 +0800

    3-31-2024 5:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,51 @@
-class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html"); // Load a local HTML file containing JavaScript code asynchronously
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.onPause();
     }
 }
+<uses-permission android:name="android.permission.INTERNET" />
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
 
-// Function to find the height of a binary tree
-function height(root) {
-    if (root === null) {
-        return 0;
-    }
+    <WebView
+        android:id="@+id/webview"
+        android:layout_width="match_parent"
+        android:layout_height="match_parent" />
 
-    const leftHeight = height(root.left);
-    const rightHeight = height(root.right);
-
-    return 1 + Math.max(leftHeight, rightHeight);
-}
-
-// Function to find the diameter of a binary tree
-function diameterOfBinaryTree(root) {
-    if (root === null) {
-        return 0;
-    }
-
-    const leftHeight = height(root.left);
-    const rightHeight = height(root.right);
-
-    const leftDiameter = diameterOfBinaryTree(root.left);
-    const rightDiameter = diameterOfBinaryTree(root.right);
-
-    // Diameter can be either in the left subtree, right subtree, or passing through the root
-    return Math.max(leftHeight + rightHeight + 1, Math.max(leftDiameter, rightDiameter));
-}
-
-// Testing the code
-const root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-
-console.log(diameterOfBinaryTree(root)); // Output the diameter of the binary tree
+</RelativeLayout>
