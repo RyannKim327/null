f08a36b0396b31348205f119f50d66c8783d21ed
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:10:08 +0800

    3-31-2024 5:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,12 @@
-let array = [1, 2, 3, 4, 5];
-let index = array.indexOf(3); // Find the index of the element you want to remove
-if (index > -1) {
-    array.splice(index, 1); // Remove 1 element starting from the index
+function calculateMean(numbers) {
+    var sum = 0;
+    for (var i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum / numbers.length;
 }
-console.log(array); // Result: [1, 2, 4, 5]
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-array = array.filter(item => item !== elementToRemove);
-console.log(array); // Result: [1, 2, 4, 5]
+
+// Example usage
+var numbers = [1, 2, 3, 4, 5];
+var mean = calculateMean(numbers);
+console.log("Mean: " + mean);
