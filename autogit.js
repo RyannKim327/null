9d16404932555518b00f6c5b87eace47f7eec924
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:31:20 +0800

    3-31-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,57 @@
-function checkAnagrams(str1, str2) {
-    // Remove spaces and punctuation, then convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+class HashTable {
+    constructor(size = 20) {
+        this.size = size;
+        this.buckets = new Array(size).fill(null);
+    }
 
-    // Sort the characters of the strings
-    str1 = str1.split('').sort().join('');
-    str2 = str2.split('').sort().join('');
+    hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash += key.charCodeAt(i);
+        }
+        return hash % this.size;
+    }
 
-    // Compare the sorted strings
-    return str1 === str2;
+    set(key, value) {
+        const index = this.hash(key);
+        if (!this.buckets[index]) {
+            this.buckets[index] = [];
+        }
+        this.buckets[index].push([key, value]);
+    }
+
+    get(key) {
+        const index = this.hash(key);
+        if (!this.buckets[index]) {
+            return null;
+        }
+        for (const pair of this.buckets[index]) {
+            if (pair[0] === key) {
+                return pair[1];
+            }
+        }
+        return null;
+    }
+
+    delete(key) {
+        const index = this.hash(key);
+        if (!this.buckets[index]) {
+            return null;
+        }
+        for (let i = 0; i < this.buckets[index].length; i++) {
+            if (this.buckets[index][i][0] === key) {
+                this.buckets[index].splice(i, 1);
+                return;
+            }
+        }
+    }
 }
 
-// Test the function
-const string1 = "Listen";
-const string2 = "Silent";
-if (checkAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+// Example usage
+const ht = new HashTable();
+ht.set('apple', 10);
+ht.set('banana', 20);
+console.log(ht.get('apple')); // Output: 10
+console.log(ht.get('banana')); // Output: 20
+ht.delete('apple');
+console.log(ht.get('apple')); // Output: null
