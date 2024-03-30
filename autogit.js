18b5832d7cc5f8adb9c3cf451059953ee07ad101
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:17:12 +0800

    3-31-2024 5:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,4 @@
-function largestPrimeFactor(number) {
-    let largestPrime = 1;
-
-    // Start checking from 2 (the smallest prime number)
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        while (number % i === 0) {
-            largestPrime = i;
-            number = number / i;
-        }
-    }
-
-    // The remaining number after factorization is also a prime factor
-    if (number > largestPrime) {
-        largestPrime = number;
-    }
-
-    return largestPrime;
-}
-
-// Example usage
-const number = 56;
-const result = largestPrimeFactor(number);
-console.log("The largest prime factor of", number, "is", result);
+let str1 = "Hello";
+let str2 = "world";
+let result = str1 + " " + str2;
+console.log(result); // Output: "Hello world"
