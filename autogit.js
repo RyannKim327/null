7983ce4a53a799afcffdda677bffb6d5cc836fdb
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:30:31 +0800

    3-31-2024 5:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,11 @@
-class Node {
-  constructor(order, leaf = true) {
-    this.order = order;
-    this.leaf = leaf;
-    this.keys = [];
-    this.children = [];
-  }
+let fruits = ['apple', 'banana', 'cherry', 'orange'];
 
-  insertNonFull(key) {
-    let i = this.keys.length - 1;
-  
-    if (this.leaf) {
-      while (i >= 0 && key < this.keys[i]) {
-        this.keys[i + 1] = this.keys[i];
-        i--;
-      }
-      this.keys[i + 1] = key;
-    } else {
-      while (i >= 0 && key < this.keys[i]) {
-        i--;
-      }
-      let child = this.children[i + 1];
-      if (child.keys.length === 2 * this.order - 1) {
-        this.splitChild(i + 1, child);
-        if (key > this.keys[i + 1]) {
-          i++;
-        }
-      }
-      this.children[i + 1].insertNonFull(key);
-    }
-  }
+// Find the index of the element you want to remove
+let index = fruits.indexOf('banana');
 
-  splitChild(i, child) {
-    let newChild = new Node(child.order, child.leaf);
-    newChild.keys = child.keys.splice(this.order);
-    if (!child.leaf) {
-      newChild.children = child.children.splice(this.order + 1);
-    }
-    this.keys.splice(i, 0, child.keys.pop());
-    this.children.splice(i + 1, 0, newChild);
-  }
+// Remove the element at that index
+if (index > -1) {
+    fruits.splice(index, 1);
 }
 
-class BTree {
-  constructor(order) {
-    this.root = new Node(order);
-  }
-
-  insert(key) {
-    let root = this.root;
-    
-    if (root.keys.length === 2 * root.order - 1) {
-      let newRoot = new Node(root.order, false);
-      newRoot.children.push(root);
-      newRoot.splitChild(0, root);
-      newRoot.insertNonFull(key);
-      this.root = newRoot;
-    } else {
-      root.insertNonFull(key);
-    }
-  }
-}
+console.log(fruits); // Output: ['apple', 'cherry', 'orange']
