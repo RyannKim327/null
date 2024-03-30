18a8a532279ac70c9ea85363a51ac0e4564ce109
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:26:33 +0800

    3-31-2024 5:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,8 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+const myString = "Hello, World!";
+const substring = "Hello";
 
-    const pivot = arr[Math.floor(arr.length / 2)];
-    const left = [];
-    const right = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        if (i === Math.floor(arr.length / 2)) {
-            continue;
-        }
-
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
+if (myString.includes(substring)) {
+    console.log("Substring found!");
+} else {
+    console.log("Substring not found!");
 }
-
-// Example usage:
-const unsortedArray = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-const sortedArray = quickSort(unsortedArray);
-console.log(sortedArray);
