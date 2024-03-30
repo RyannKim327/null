3d4d12e1d43fbd3590ffa4ef56ade6a77db296f0
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:30:19 +0800

    3-31-2024 5:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,18 @@
-function longestCommonSubstring(str1, str2) {
-    const dp = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
-    let maxLength = 0;
-    let endIndex = 0;
-
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
+    
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
         }
+        left++;
+        right--;
     }
-
-    if (maxLength === 0) {
-        return '';
-    }
-
-    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+    
+    return true;
 }
 
-// Example usage
-const str1 = "abcdef";
-const str2 = "zabcde";
-console.log(longestCommonSubstring(str1, str2)); // Output: "abcd"
+// Test the function
+console.log(isPalindrome("madam")); // Output: true
+console.log(isPalindrome("hello")); // Output: false
