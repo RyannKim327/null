f69fd6dc6ca939af0910d52ee892a09deec58a7c
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:19:39 +0800

    3-31-2024 5:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,8 @@
-function bubbleSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap elements
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    return arr;
+function removeVowels(str) {
+  return str.replace(/[aeiouAEIOU]/g, '');
 }
 
 // Example usage
-const arr = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original array: " + arr);
-console.log("Sorted array: " + bubbleSort(arr));
+const originalString = "Hello, World!";
+const stringWithoutVowels = removeVowels(originalString);
+console.log(stringWithoutVowels); // Output: "Hll, Wrld!"
