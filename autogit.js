43eafddd4c0b0e210ddb071ecd54a1cd0f630f80
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:08:13 +0800

    3-31-2024 5:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,7 @@
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.os.Bundle;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-public class MainActivity extends Activity {
-
-    private WebView webView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        webView = (WebView) findViewById(R.id.webview);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.setWebViewClient(new WebViewClient());
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-
-        webView.loadUrl("file:///android_asset/index.html"); // Load a local HTML file containing JavaScript code asynchronously
-    }
-
-    @SuppressLint("SetJavaScriptEnabled")
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        webView.onPause();
-    }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-<uses-permission android:name="android.permission.INTERNET" />
-<?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent">
 
-    <WebView
-        android:id="@+id/webview"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent" />
-
-</RelativeLayout>
+// Example: Generate a random number between 1 and 10
+var randomNum = getRandomInt(1, 10);
+console.log(randomNum);
