f4ec772dc6eafbeb96a9aebc7f947ab5ec2bf44a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:21:29 +0800

    3-31-2024 5:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,9 @@
-class Node {
-    constructor(value, color) {
-        this.value = value;
-        this.color = color;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-    }
+function calculateMean(numbers) {
+    const total = numbers.reduce((acc, val) => acc + val, 0);
+    return total / numbers.length;
 }
 
-class RedBlackTree {
-    constructor() {
-        this.root = null;
-    }
-
-    insert(value) {
-        let newNode = new Node(value, 'red');
-        this.root = this._insertNode(this.root, newNode);
-
-        // Fix any violations
-        this._fixViolations(newNode);
-    }
-
-    // Private methods
-    _insertNode(root, newNode) {
-        // Insert the node as in a BST
-        if (!root) return newNode;
-
-        if (newNode.value < root.value) {
-            root.left = this._insertNode(root.left, newNode);
-            root.left.parent = root;
-        } else if (newNode.value > root.value) {
-            root.right = this._insertNode(root.right, newNode);
-            root.right.parent = root;
-        } else {
-            // Duplicate values are not allowed in a red-black tree
-            return root;
-        }
-
-        return root;
-    }
-
-    _fixViolations(node) {
-        // Implement the rules for fixing violations in a red-black tree
-    }
-}
+// Example usage
+const numbers = [1, 2, 3, 4, 5];
+const mean = calculateMean(numbers);
+console.log(mean);  // Output: 3
