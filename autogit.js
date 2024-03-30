117c31a12d14561a7928843e7138545d4bd9131d
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:08:21 +0800

    3-31-2024 5:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,27 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-// Example: Generate a random number between 1 and 10
-var randomNum = getRandomInt(1, 10);
-console.log(randomNum);
+function findMiddleElement(head) {
+  let slowPtr = head;
+  let fastPtr = head;
+
+  while (fastPtr !== null && fastPtr.next !== null) {
+    slowPtr = slowPtr.next;
+    fastPtr = fastPtr.next.next;
+  }
+
+  return slowPtr.value;
+}
+
+// Example linked list
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+console.log(findMiddleElement(head)); // Output: 3
