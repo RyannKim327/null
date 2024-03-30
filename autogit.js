4bcd18d8860a1355af60bc133f3226f6620d5966
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:33:35 +0800

    3-31-2024 5:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,26 @@
-function findMajorityElement(arr) {
-    let freqMap = {};
-    let majorityElement = null;
-    let majorityCount = 0;
+const str = 'Hello, world!';
+const substring = 'world';
 
-    for (let i = 0; i < arr.length; i++) {
-        if (freqMap[arr[i]] === undefined) {
-            freqMap[arr[i]] = 1;
-        } else {
-            freqMap[arr[i]]++;
-        }
-
-        if (freqMap[arr[i]] > majorityCount) {
-            majorityElement = arr[i];
-            majorityCount = freqMap[arr[i]];
-        }
-    }
-
-    if (majorityCount > arr.length / 2) {
-        return majorityElement;
-    } else {
-        return null;
-    }
+if (str.includes(substring)) {
+  console.log('String contains the substring');
+} else {
+  console.log('String does not contain the substring');
 }
+const str = 'Hello, world!';
+const substring = 'world';
 
-// Test the function
-const arr1 = [2, 2, 3, 3, 2, 2, 2];
-console.log(findMajorityElement(arr1)); // Output: 2
+if (str.indexOf(substring) !== -1) {
+  console.log('String contains the substring');
+} else {
+  console.log('String does not contain the substring');
+}
+const str = 'Hello, world!';
+const substring = 'world';
 
-const arr2 = [1, 2, 3, 4, 5];
-console.log(findMajorityElement(arr2)); // Output: null
+const regex = new RegExp(substring);
+
+if (regex.test(str)) {
+  console.log('String contains the substring');
+} else {
+  console.log('String does not contain the substring');
+}
