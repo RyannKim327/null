71404ebd2338a3a7376d64c11a2deadd3bb156bc
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:23:36 +0800

    3-31-2024 5:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,72 @@
+// Node class to represent each element in the queue
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.children = [];
-    }
-
-    addChild(child) {
-        this.children.push(child);
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-function depthLimitedSearch(node, goal, depthLimit, currentDepth = 0) {
-    if (node.value === goal) {
-        return true;
-    }
+// Queue class to implement the queue using linked list
+class Queue {
+  constructor() {
+    this.front = null;
+    this.rear = null;
+    this.size = 0;
+  }
 
-    if (currentDepth === depthLimit) {
-        return false;
+  // Method to add an element to the queue
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (!this.front) {
+      this.front = newNode;
+      this.rear = newNode;
+    } else {
+      this.rear.next = newNode;
+      this.rear = newNode;
     }
+    this.size++;
+  }
 
-    for (let child of node.children) {
-        if (depthLimitedSearch(child, goal, depthLimit, currentDepth + 1)) {
-            return true;
-        }
+  // Method to remove an element from the queue
+  dequeue() {
+    if (!this.front) {
+      return null;
     }
+    let removedValue = this.front.value;
+    this.front = this.front.next;
+    if (!this.front) {
+      this.rear = null;
+    }
+    this.size--;
+    return removedValue;
+  }
 
-    return false;
+  // Method to check if the queue is empty
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  // Method to get the size of the queue
+  getSize() {
+    return this.size;
+  }
+
+  // Method to get the front element of the queue
+  getFront() {
+    if (!this.front) {
+      return null;
+    }
+    return this.front.value;
+  }
 }
 
-// Example usage
-const rootNode = new Node('A');
-const nodeB = new Node('B');
-const nodeC = new Node('C');
-const nodeD = new Node('D');
-const nodeE = new Node('E');
-const nodeF = new Node('F');
+// Example usage:
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
 
-rootNode.addChild(nodeB);
-rootNode.addChild(nodeC);
-nodeB.addChild(nodeD);
-nodeB.addChild(nodeE);
-nodeC.addChild(nodeF);
-
-const goalNode = 'F';
-const depthLimit = 2;
-
-console.log(depthLimitedSearch(rootNode, goalNode, depthLimit)); // Output: true
+console.log(queue.dequeue()); // Output: 1
+console.log(queue.getFront()); // Output: 2
+console.log(queue.getSize()); // Output: 2
+console.log(queue.isEmpty()); // Output: false
