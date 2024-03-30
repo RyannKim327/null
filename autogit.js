f8642a04d37d46bfd5a98bde23e285deb50fee9a
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:09:18 +0800

    3-31-2024 5:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,38 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+function depthLimitedSearch(node, goal, depth) {
+    if (depth === 0) {
+        return null;
     }
+
+    if (node === goal) {
+        return node;
+    }
+
+    for (let child of getChildren(node)) {
+        let result = depthLimitedSearch(child, goal, depth - 1);
+        if (result) {
+            return result;
+        }
+    }
+
+    return null;
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    reverse() {
-        let prev = null;
-        let current = this.head;
-        let next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        this.head = prev;
-    }
-
-    add(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    print() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
+function getChildren(node) {
+    // This function should return an array of child nodes of the given node.
+    // You can customize this function based on your specific problem.
+    // For demonstration purposes, we will assume getChildren returns an empty array.
+    return [];
 }
 
-// Example usage
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.add(4);
+// Usage
+let rootNode = /* set your root node */;
+let goalNode = /* set your goal node */;
+let depthLimit = 5;
 
-console.log("Original Linked List:");
-linkedList.print();
+let result = depthLimitedSearch(rootNode, goalNode, depthLimit);
 
-linkedList.reverse();
-
-console.log("Reversed Linked List:");
-linkedList.print();
+if (result) {
+    console.log('Goal node found!');
+} else {
+    console.log('Goal node not found within the depth limit.');
+}
