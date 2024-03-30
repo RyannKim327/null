87e3a104570a86c5fde5a972367ee78145281829
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:31:27 +0800

    3-31-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,21 @@
-class HashTable {
-    constructor(size = 20) {
-        this.size = size;
-        this.buckets = new Array(size).fill(null);
+function largestPrimeFactor(num) {
+    let factors = [];
+    
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+        while (num % i === 0) {
+            factors.push(i);
+            num /= i;
+        }
+    }
+    
+    if (num !== 1) {
+        factors.push(num);
     }
 
-    hash(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash += key.charCodeAt(i);
-        }
-        return hash % this.size;
-    }
-
-    set(key, value) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            this.buckets[index] = [];
-        }
-        this.buckets[index].push([key, value]);
-    }
-
-    get(key) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            return null;
-        }
-        for (const pair of this.buckets[index]) {
-            if (pair[0] === key) {
-                return pair[1];
-            }
-        }
-        return null;
-    }
-
-    delete(key) {
-        const index = this.hash(key);
-        if (!this.buckets[index]) {
-            return null;
-        }
-        for (let i = 0; i < this.buckets[index].length; i++) {
-            if (this.buckets[index][i][0] === key) {
-                this.buckets[index].splice(i, 1);
-                return;
-            }
-        }
-    }
+    return Math.max(...factors);
 }
 
 // Example usage
-const ht = new HashTable();
-ht.set('apple', 10);
-ht.set('banana', 20);
-console.log(ht.get('apple')); // Output: 10
-console.log(ht.get('banana')); // Output: 20
-ht.delete('apple');
-console.log(ht.get('apple')); // Output: null
+const number = 13195;
+const largestPrime = largestPrimeFactor(number);
+console.log(`The largest prime factor of ${number} is ${largestPrime}`);
