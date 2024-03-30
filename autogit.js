58d7edcdaf54221d676e640c87ddb6d1dc49499b
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:23:27 +0800

    3-31-2024 5:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,47 @@
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
+    }
 
-const commonElements = array1.filter(element => array2.includes(element));
+    addChild(child) {
+        this.children.push(child);
+    }
+}
 
-console.log(commonElements); // Output: [3, 4, 5]
+function depthLimitedSearch(node, goal, depthLimit, currentDepth = 0) {
+    if (node.value === goal) {
+        return true;
+    }
+
+    if (currentDepth === depthLimit) {
+        return false;
+    }
+
+    for (let child of node.children) {
+        if (depthLimitedSearch(child, goal, depthLimit, currentDepth + 1)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// Example usage
+const rootNode = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+const nodeF = new Node('F');
+
+rootNode.addChild(nodeB);
+rootNode.addChild(nodeC);
+nodeB.addChild(nodeD);
+nodeB.addChild(nodeE);
+nodeC.addChild(nodeF);
+
+const goalNode = 'F';
+const depthLimit = 2;
+
+console.log(depthLimitedSearch(rootNode, goalNode, depthLimit)); // Output: true
