4ddede47071ae2ceb164e0fdf01e5e63e1b597d2
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:30:13 +0800

    3-31-2024 5:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,65 +1,30 @@
-function findStronglyConnectedComponents(graph) {
-  let index = 0;
-  let stack = [];
-  let onStack = {};
-  let indices = {};
-  let lowLink = {};
-  let components = [];
+function longestCommonSubstring(str1, str2) {
+    const dp = Array(str1.length + 1).fill(null).map(() => Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
 
-  function strongConnect(node) {
-    indices[node] = index;
-    lowLink[node] = index;
-    index++;
-    stack.push(node);
-    onStack[node] = true;
-
-    for (let neighbor of graph[node]) {
-      if (indices[neighbor] === undefined) {
-        strongConnect(neighbor);
-        lowLink[node] = Math.min(lowLink[node], lowLink[neighbor]);
-      } else if (onStack[neighbor]) {
-        lowLink[node] = Math.min(lowLink[node], indices[neighbor]);
-      }
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
+        }
     }
 
-    if (lowLink[node] === indices[node]) {
-      let component = [];
-      let compNode;
-      do {
-        compNode = stack.pop();
-        onStack[compNode] = false;
-        component.push(compNode);
-      } while (compNode !== node);
-      components.push(component);
+    if (maxLength === 0) {
+        return '';
     }
-  }
 
-  for (let node in graph) {
-    if (indices[node] === undefined) {
-      strongConnect(node);
-    }
-  }
-
-  return components;
+    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
 }
 
-// Example graph representation (adjacency list)
-const graph = {
-  0: [1],
-  1: [2],
-  2: [0, 3],
-  3: [4],
-  4: [3, 5],
-  5: [6],
-  6: [5, 7],
-  7: [8],
-  8: [6, 9],
-  9: [7, 10],
-  10: [13],
-  11: [9, 12],
-  12: [10],
-  13: [11]
-};
-
-const stronglyConnectedComponents = findStronglyConnectedComponents(graph);
-console.log(stronglyConnectedComponents);
+// Example usage
+const str1 = "abcdef";
+const str2 = "zabcde";
+console.log(longestCommonSubstring(str1, str2)); // Output: "abcd"
