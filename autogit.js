b443f645fa6c67cbadefaadb5370f633769f99ec
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:13:55 +0800

    3-31-2024 5:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,8 @@
-function selectionSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len - 1; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < len; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-        if (minIndex !== i) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-        }
-    }
-    return arr;
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-// Example usage:
-const arr = [64, 25, 12, 22, 11];
-console.log("Original Array:", arr);
-console.log("Sorted Array:", selectionSort(arr.slice()));
+// Example
+let originalString = "Hello, World!";
+let reversedString = reverseString(originalString);
+console.log(reversedString); // Outputs: "!dlroW ,olleH"
