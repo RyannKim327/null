9c293c22b8ec54d497b5c3faba6734369cc811f6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:13:31 +0800

    3-31-2024 5:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,62 @@
-function isAnagram(str1, str2) {
-  // Removing non-alphanumeric characters and converting to lowercase
-  str1 = str1.replace(/[\W_]/g, "").toLowerCase();
-  str2 = str2.replace(/[\W_]/g, "").toLowerCase();
-
-  // Check if the strings are of the same length
-  if (str1.length !== str2.length) {
-    return false;
+class Graph {
+  constructor() {
+    this.nodes = [];
+    this.adjList = {};
   }
 
-  // Compare the sorted characters of the strings
-  const sortedStr1 = str1.split("").sort().join("");
-  const sortedStr2 = str2.split("").sort().join("");
+  addNode(node) {
+    this.nodes.push(node);
+    this.adjList[node] = [];
+  }
 
-  return sortedStr1 === sortedStr2;
+  addEdge(node1, node2) {
+    this.adjList[node1].push(node2);
+    this.adjList[node2].push(node1);
+  }
+
+  breadthFirstSearch(startNode, endNode) {
+    let visited = {};
+    let queue = [];
+    let path = [];
+  
+    queue.push(startNode);
+    visited[startNode] = true;
+  
+    while (queue.length > 0) {
+      let currentNode = queue.shift();
+      path.push(currentNode);
+  
+      if (currentNode === endNode) {
+        return path;
+      }
+  
+      this.adjList[currentNode].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+  
+    return "Path not found";
+  }
 }
 
-// Test the function
-console.log(isAnagram("listen", "silent")); // Output: true
-console.log(isAnagram("hello", "world")); // Output: false
+// Example usage
+const graph = new Graph();
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+graph.addNode("E");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+
+const startNode = "A";
+const endNode = "E";
+console.log(graph.breadthFirstSearch(startNode, endNode));
