3d83aa4f5adecab02311ad5c81bb2f4377163d7e
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:21:24 +0800

    3-31-2024 5:21 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,46 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-
-    const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
-
-    // Build the dp table
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+class Node {
+    constructor(value, color) {
+        this.value = value;
+        this.color = color;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
     }
-
-    // Reconstruct the longest common subsequence
-    let result = "";
-    let i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            result = str1[i - 1] + result;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result;
 }
 
-// Example usage
-const str1 = "ABCBDAB";
-const str2 = "BDCAB";
-const lcs = longestCommonSubsequence(str1, str2);
-console.log(lcs); // Output: "BCAB"
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        let newNode = new Node(value, 'red');
+        this.root = this._insertNode(this.root, newNode);
+
+        // Fix any violations
+        this._fixViolations(newNode);
+    }
+
+    // Private methods
+    _insertNode(root, newNode) {
+        // Insert the node as in a BST
+        if (!root) return newNode;
+
+        if (newNode.value < root.value) {
+            root.left = this._insertNode(root.left, newNode);
+            root.left.parent = root;
+        } else if (newNode.value > root.value) {
+            root.right = this._insertNode(root.right, newNode);
+            root.right.parent = root;
+        } else {
+            // Duplicate values are not allowed in a red-black tree
+            return root;
+        }
+
+        return root;
+    }
+
+    _fixViolations(node) {
+        // Implement the rules for fixing violations in a red-black tree
+    }
+}
