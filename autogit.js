11ab396c82eb34f5a9d2d5c46599e7e4a1a40393
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:07:09 +0800

    3-31-2024 5:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,129 +1,20 @@
-class Node {
-  constructor(data, color) {
-    this.data = data;
-    this.color = color;
-    this.left = null;
-    this.right = null;
-    this.parent = null;
-  }
+function isAnagram(str1, str2) {
+    // Remove any special characters, spaces, and convert to lowercase
+    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
+    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
+
+    // Check if the lengths of the two strings are equal
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Sort the characters in the strings and compare them
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    return sortedStr1 === sortedStr2;
 }
 
-class RedBlackTree {
-  constructor() {
-    this.root = null;
-  }
-
-  // Helper functions for rotation, coloring, and balancing
-
-  // Left rotation
-  rotateLeft(node) {
-    let temp = node.right;
-    node.right = temp.left;
-    if (temp.left !== null) {
-      temp.left.parent = node;
-    }
-    temp.parent = node.parent;
-    if (node.parent === null) {
-      this.root = temp;
-    } else if (node === node.parent.left) {
-      node.parent.left = temp;
-    } else {
-    node.parent.right = temp;
-    }
-    temp.left = node;
-    node.parent = temp;
-  }
-
-  // Right rotation
-  rotateRight(node) {
-    let temp = node.left;
-    node.left = temp.right;
-    if (temp.right !== null) {
-      temp.right.parent = node;
-    }
-    temp.parent = node.parent;
-    if (node.parent === null) {
-      this.root = temp;
-    } else if (node === node.parent.right) {
-      node.parent.right = temp;
-    } else {
-    node.parent.left = temp;
-    }
-    temp.right = node;
-    node.parent = temp;
-  }
-
-  // Helper function for fixing violations in the tree
-  fixViolation(node) {
-    let parent = null;
-    let grandparent = null;
-
-    while (node !== this.root && node.color !== 'black' && node.parent.color === 'red') {
-      parent = node.parent;
-      grandparent = node.parent.parent;
-
-      // Case A: Parent is left child of grandparent
-      if (parent === grandparent.left) {
-        let uncle = grandparent.right;
-        if (uncle !== null && uncle.color === 'red') {
-          grandparent.color = 'red';
-          parent.color = 'black';
-          uncle.color = 'black';
-          node = grandparent;
-        } else {
-          if (node === parent.right) {
-            this.rotateLeft(parent);
-            node = parent;
-            parent = node.parent;
-          }
-          this.rotateRight(grandparent);
-          let temp = parent.color;
-          parent.color = grandparent.color;
-          grandparent.color = temp;
-          node = parent;
-        }
-      } else {
-        // Case B: Parent is right child of grandparent
-        let uncle = grandparent.left;
-        if (uncle !== null && uncle.color === 'red') {
-          grandparent.color = 'red';
-          parent.color = 'black';
-          uncle.color = 'black';
-          node = grandparent;
-        } else {
-          if (node === parent.left) {
-            this.rotateRight(parent);
-            node = parent;
-            parent = node.parent;
-          }
-          this.rotateLeft(grandparent);
-          let temp = parent.color;
-          parent.color = grandparent.color;
-          grandparent.color = temp;
-          node = parent;
-        }
-      }
-    }
-    this.root.color = 'black';
-  }
-
-  insert(data) {
-    let newNode = new Node(data, 'red');
-    this.root = this.insertNode(this.root, newNode);
-    this.fixViolation(newNode);
-  }
-
-  insertNode(root, node) {
-    if (root === null) {
-      return node;
-    }
-    if (node.data < root.data) {
-      root.left = this.insertNode(root.left, node);
-      root.left.parent = root;
-    } else if (node.data > root.data) {
-      root.right = this.insertNode(root.right, node);
-      root.right.parent = root;
-    }
-    return root;
-  }
-}
+// Test the function
+console.log(isAnagram('listen', 'silent')); // Output: true
+console.log(isAnagram('hello', 'world')); // Output: false
