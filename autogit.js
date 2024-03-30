2cdb78256379bdbcc26b1e4c8b06c76f81bea9f6
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:25:47 +0800

    3-31-2024 5:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,21 @@
-function findStringLength(str) {
-    let count = 0;
-    for (let i = 0; str[i] !== undefined; i++) {
-        count++;
-    }
-    return count;
+function isAnagram(str1, str2) {
+    // Remove special characters and convert to lowercase
+    str1 = str1.replace(/[^\w]/g, "").toLowerCase();
+    str2 = str2.replace(/[^\w]/g, "").toLowerCase();
+
+    // Sort the characters of both strings
+    const sortedStr1 = str1.split("").sort().join("");
+    const sortedStr2 = str2.split("").sort().join("");
+
+    // Compare the sorted strings
+    return sortedStr1 === sortedStr2;
 }
 
-let myString = "Hello, World!";
-console.log("Length of the string:", findStringLength(myString));
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
