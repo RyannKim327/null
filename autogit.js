d10fa20406b4f0ff74e2ac1785ece2f278a93a6f
RyannKim327 <rksesgundo123@gmail.com>
2024-03-31 05:16:27 +0800

    3-31-2024 5:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,4 @@
-function calculateMean(numbers) {
-    var sum = 0;
-    var mean = 0;
-    
-    for(var i = 0; i < numbers.length; i++) {
-        sum += numbers[i];
-    }
-    
-    mean = sum / numbers.length;
-    
-    return mean;
-}
+let str = "Hello World!";
+let stringWithoutSpaces = str.replace(/\s/g, '');
 
-// Example usage
-var numbers = [2, 4, 6, 8, 10];
-var mean = calculateMean(numbers);
-console.log("Mean: " + mean);
+console.log(stringWithoutSpaces); // Output: "HelloWorld!"
