adc5266fa35f54c871b95623a8ec869f0ea95017
RyannKim327 <rksesgundo123@gmail.com>
2024-04-01 20:20:09 +0800

    4-1-2024 20:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,15 @@
-class TreeNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-function findDiameter(root) {
-    let diameter = 0;
-
-    function height(node) {
-        if (node === null) {
-            return 0;
+function countOccurrences(str, char) {
+    var count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            count++;
         }
-
-        let leftHeight = height(node.left);
-        let rightHeight = height(node.right);
-
-        diameter = Math.max(diameter, leftHeight + rightHeight);
-        return 1 + Math.max(leftHeight, rightHeight);
     }
-
-    height(root);
-
-    return diameter;
+    return count;
 }
 
-// Example binary tree
-let root = new TreeNode(1);
-root.left = new TreeNode(2);
-root.right = new TreeNode(3);
-root.left.left = new TreeNode(4);
-root.left.right = new TreeNode(5);
-root.right.left = new TreeNode(6);
-root.right.right = new TreeNode(7);
+var str = "hello world";
+var char = "o";
+var occurrences = countOccurrences(str, char);
 
-console.log(findDiameter(root)); // Output should be 4
+console.log("Number of occurrences of '" + char + "' in '" + str + "': " + occurrences);
