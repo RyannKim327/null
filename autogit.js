88b71f137b4892554d05b8f94f3026db32240454
RyannKim327 <rksesgundo123@gmail.com>
2024-04-01 20:22:54 +0800

    4-1-2024 20:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,44 @@
-let numbers = [10, 5, 20, 15, 30];
-let max = Math.max(...numbers);
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
-console.log(max); // Output: 30
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertAtEnd(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    getLength() {
+        let current = this.head;
+        let length = 0;
+        while (current) {
+            length++;
+            current = current.next;
+        }
+        return length;
+    }
+}
+
+// Example Usage
+const list = new LinkedList();
+list.insertAtEnd(1);
+list.insertAtEnd(2);
+list.insertAtEnd(3);
+
+const length = list.getLength();
+console.log("Length of the linked list: " + length); // Output: Length of the linked list: 3
