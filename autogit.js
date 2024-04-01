f8e81026e413cb23f84110314235422e82e54b5e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-01 20:23:37 +0800

    4-1-2024 20:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,60 +1,16 @@
-class Graph {
-  constructor() {
-    this.adjacencyList = {};
-  }
-
-  addVertex(vertex) {
-    if (!this.adjacencyList[vertex]) {
-      this.adjacencyList[vertex] = [];
-    }
-  }
-
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
-  }
-
-  depthFirstSearch(start) {
-    const visited = {};
-    const result = [];
-
-    const dfs = (vertex) => {
-      if (!vertex) {
-        return null;
-      }
-
-      visited[vertex] = true;
-      result.push(vertex);
-
-      this.adjacencyList[vertex].forEach(neighbor => {
-        if (!visited[neighbor]) {
-          return dfs(neighbor);
+function removeDuplicates(array) {
+    let uniqueArray = [];
+    
+    array.forEach((element) => {
+        if (!uniqueArray.includes(element)) {
+            uniqueArray.push(element);
         }
-      });
-    };
-
-    dfs(start);
-
-    return result;
-  }
+    });
+    
+    return uniqueArray;
 }
 
-// Example usage:
-const graph = new Graph();
+let originalArray = [1, 2, 2, 3, 4, 4, 5];
+let newArray = removeDuplicates(originalArray);
 
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addVertex('E');
-graph.addVertex('F');
-
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'E');
-graph.addEdge('D', 'E');
-graph.addEdge('D', 'F');
-graph.addEdge('E', 'F');
-
-console.log(graph.depthFirstSearch('A')); // Output: ['A', 'B', 'D', 'E', 'C', 'F']
+console.log(newArray); // Output: [1, 2, 3, 4, 5]
