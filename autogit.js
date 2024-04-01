0b69e7a3a86a0bc117c00bc8dbf9008770eafaf8
RyannKim327 <rksesgundo123@gmail.com>
2024-04-01 20:22:34 +0800

    4-1-2024 20:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,8 @@
-class Graph {
-    constructor() {
-        this.adjacencyList = {};
-    }
-
-    addVertex(vertex) {
-        if (!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex] = [];
-        }
-    }
-
-    addEdge(v1, v2) {
-        this.adjacencyList[v1].push(v2);
-        this.adjacencyList[v2].push(v1);
-    }
-
-    depthFirstSearch(startingNode) {
-        const result = [];
-        const visited = {};
-
-        const dfs = (vertex) => {
-            if (!vertex) return null;
-            
-            visited[vertex] = true;
-            result.push(vertex);
-
-            this.adjacencyList[vertex].forEach(neighbor => {
-                if (!visited[neighbor]) {
-                    return dfs(neighbor);
-                }
-            });
-        };
-
-        dfs(startingNode);
-
-        return result;
-    }
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
 // Example usage
-const graph = new Graph();
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'D');
-
-console.log(graph.depthFirstSearch('A')); // Output: ['A', 'B', 'D', 'C']
+const inputString = 'Hello, World!';
+const result = removeVowels(inputString);
+console.log(result); // Output: "Hll, Wrld!"
