1cf2888e1759d8448715c83a393ad66f168f9d0a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-01 20:23:13 +0800

    4-1-2024 20:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,118 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
+    constructor(value) {
+        this.value = value;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class LinkedList {
+class AVLTree {
     constructor() {
-        this.head = null;
+        this.root = null;
     }
 
-    insertAtEnd(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
+    getHeight(node) {
+        if (node === null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    getBalanceFactor(node) {
+        if (node === null) {
+            return 0;
+        }
+        return this.getHeight(node.left) - this.getHeight(node.right);
+    }
+
+    updateHeight(node) {
+        node.height = 1 + Math.max(this.getHeight(node.left), this.getHeight(node.right));
+    }
+
+    rightRotate(node) {
+        let newRoot = node.left;
+        node.left = newRoot.right;
+        newRoot.right = node;
+
+        this.updateHeight(node);
+        this.updateHeight(newRoot);
+
+        return newRoot;
+    }
+
+    leftRotate(node) {
+        let newRoot = node.right;
+        node.right = newRoot.left;
+        newRoot.left = node;
+
+        this.updateHeight(node);
+        this.updateHeight(newRoot);
+
+        return newRoot;
+    }
+
+    insert(value) {
+        this.root = this._insert(this.root, value);
+    }
+
+    _insert(node, value) {
+        if (node === null) {
+            return new Node(value);
+        }
+
+        if (value < node.value) {
+            node.left = this._insert(node.left, value);
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
+            node.right = this._insert(node.right, value);
         }
+
+        this.updateHeight(node);
+
+        let balanceFactor = this.getBalanceFactor(node);
+
+        if (balanceFactor > 1 && value < node.left.value) {
+            return this.rightRotate(node);
+        }
+
+        if (balanceFactor < -1 && value > node.right.value) {
+            return this.leftRotate(node);
+        }
+
+        if (balanceFactor > 1 && value > node.left.value) {
+            node.left = this.leftRotate(node.left);
+            return this.rightRotate(node);
+        }
+
+        if (balanceFactor < -1 && value < node.right.value) {
+            node.right = this.rightRotate(node.right);
+            return this.leftRotate(node);
+        }
+
+        return node;
     }
 
-    getLength() {
-        let current = this.head;
-        let length = 0;
-        while (current) {
-            length++;
-            current = current.next;
+    inorder() {
+        this._inorder(this.root);
+    }
+
+    _inorder(node) {
+        if (node === null) {
+            return;
         }
-        return length;
+
+        this._inorder(node.left);
+        console.log(node.value);
+        this._inorder(node.right);
     }
 }
 
-// Example Usage
-const list = new LinkedList();
-list.insertAtEnd(1);
-list.insertAtEnd(2);
-list.insertAtEnd(3);
+// Test AVL Tree
+let avlTree = new AVLTree();
+avlTree.insert(10);
+avlTree.insert(20);
+avlTree.insert(30);
+avlTree.insert(15);
+avlTree.insert(5);
 
-const length = list.getLength();
-console.log("Length of the linked list: " + length); // Output: Length of the linked list: 3
+avlTree.inorder();
