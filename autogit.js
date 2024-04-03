a9615add62d4522b6eee60e47db98c41c46f1272
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:59:19 +0800

    4-4-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,40 @@
-let str = "Hello, World! This is a test string with spaces.";
+// Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
 
-// Using regular expression to remove all spaces
-let strWithoutSpaces = str.replace(/\s/g, '');
+// Function to check if a linked list is a palindrome
+function isPalindrome(head) {
+    let currentNode = head;
+    let values = [];
 
-console.log(strWithoutSpaces);
+    // Traverse the linked list and push elements into the array
+    while (currentNode !== null) {
+        values.push(currentNode.val);
+        currentNode = currentNode.next;
+    }
+
+    // Use two pointers technique to compare elements
+    let left = 0;
+    let right = values.length - 1;
+
+    while (left < right) {
+        if (values[left] !== values[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+// Example usage
+let list = new ListNode(1);
+list.next = new ListNode(2);
+list.next.next = new ListNode(3);
+list.next.next.next = new ListNode(2);
+list.next.next.next.next = new ListNode(1);
+
+console.log(isPalindrome(list)); // Output: true
