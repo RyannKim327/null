e707cf9d5c0e31155b4cac5b62d60024dbf325b4
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:44:25 +0800

    4-4-2024 4:44 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,25 @@
-class Node {
-  constructor(value, cost, parent) {
-    this.value = value;
-    this.cost = cost;
-    this.parent = parent;
-  }
-}
+function findFirstRepeatedCharacter(str) {
+    let charMap = {};
 
-function beamSearch(graph, start, goal, beamWidth) {
-  let frontiers = [new Node(start, 0, null)];
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charAt(i);
 
-  while (frontiers.length > 0) {
-    let nextFrontiers = [];
-
-    frontiers.forEach((node) => {
-      if (node.value === goal) {
-        // Found goal, reconstruct path
-        let path = [node.value];
-        while (node.parent) {
-          path.unshift(node.parent.value);
-          node = node.parent;
+        if (char in charMap) {
+            return char;
+        } else {
+            charMap[char] = true;
         }
-        return path;
-      }
+    }
 
-      graph[node.value].forEach((neighbor) => {
-        let newCost = node.cost + neighbor.cost;
-        let newNode = new Node(neighbor.node, newCost, node);
-
-        nextFrontiers.push(newNode);
-      });
-    });
-
-    // Select top beamWidth nodes based on cost
-    nextFrontiers.sort((a, b) => a.cost - b.cost);
-    frontiers = nextFrontiers.slice(0, beamWidth);
-  }
-
-  return null; // No path found
+    return null; // If no repeated character is found
 }
 
-// Example graph
-const graph = {
-  A: [{ node: 'B', cost: 5 }, { node: 'C', cost: 3 }],
-  B: [{ node: 'D', cost: 2 }, { node: 'E', cost: 4 }],
-  C: [{ node: 'F', cost: 7 }, { node: 'G', cost: 6 }],
-  D: [{ node: 'H', cost: 1 }],
-  E: [{ node: 'H', cost: 3 }],
-  F: [{ node: 'H', cost: 8 }],
-  G: [{ node: 'H', cost: 5 }],
-  H: [],
-};
+// Example usage
+let inputString = "hello world";
+let result = findFirstRepeatedCharacter(inputString);
 
-const start = 'A';
-const goal = 'H';
-const beamWidth = 2;
-
-const path = beamSearch(graph, start, goal, beamWidth);
-
-if (path) {
-  console.log('Shortest path:', path.join(' -> '));
+if (result) {
+    console.log("The first repeated character is: " + result);
 } else {
-  console.log('No path found');
+    console.log("No repeated character found in the string.");
 }
