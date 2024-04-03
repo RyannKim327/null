af6f2ecdcead7100335a2cf6891f982a1950963a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:48:38 +0800

    4-4-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,13 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
+// Define the URL of the API
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
-    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    let i = m, j = n;
-    const result = [];
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            result.unshift(str1[i - 1]);
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return result.join('');
-}
-
-const str1 = 'ABCDGH';
-const str2 = 'AEDFHR';
-console.log(longestCommonSubsequence(str1, str2));
+// Fetch data from the API
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Output the data to the console
+    // You can now work with the data retrieved from the API
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
