6b18a368cdccd6ffd2aaf8d548c19ef1aeb877d8
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 05:00:09 +0800

    4-4-2024 5:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,20 @@
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
-}
+function findSecondLargest(arr) {
+    let max = arr[0];
+    let secondMax = -Infinity;
 
-function getIntersectionNode(headA, headB) {
-    if (!headA || !headB) {
-        return null;
-    }
-
-    let set = new Set();
-
-    let temp = headA;
-    while (temp) {
-        set.add(temp);
-        temp = temp.next;
-    }
-
-    temp = headB;
-    while (temp) {
-        if (set.has(temp)) {
-            return temp;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secondMax = max;
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] !== max) {
+            secondMax = arr[i];
         }
-        temp = temp.next;
     }
 
-    return null;
+    return secondMax;
 }
 
-// Example usage:
-const commonNode = new Node(5);
-const headA = new Node(1);
-headA.next = new Node(3);
-headA.next.next = commonNode;
-
-const headB = new Node(2);
-headB.next = commonNode;
-
-const result = getIntersectionNode(headA, headB);
-console.log(result); // Output: Node { value: 5, next: null }
+// Test the function
+const arr = [3, 5, 2, 8, 1, 9];
+const secondLargest = findSecondLargest(arr);
+console.log("Second largest element is: " + secondLargest);
