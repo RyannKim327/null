6ac4fe4768e6f7a828dfdab7a9177c4b50f030d9
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:48:26 +0800

    4-4-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,46 @@
-// Bi-directional Search Algorithm
-function biDirectionalSearch(start, goal) {
-    let startQueue = [start];
-    let goalQueue = [goal];
-    let startVisited = { [start]: true };
-    let goalVisited = { [goal]: true };
-
-    while (startQueue.length > 0 && goalQueue.length > 0) {
-        let startNode = startQueue.shift();
-        let goalNode = goalQueue.shift();
-
-        if (startVisited[goalNode]) {
-            return 'Path found';
-        }
-
-        if (goalVisited[startNode]) {
-            return 'Path found';
-        }
-
-        // Expand startNode
-        for (let neighbor of getNeighbors(startNode)) {
-            if (!startVisited[neighbor]) {
-                startQueue.push(neighbor);
-                startVisited[neighbor] = true;
-            }
-        }
-
-        // Expand goalNode
-        for (let neighbor of getNeighbors(goalNode)) {
-            if (!goalVisited[neighbor]) {
-                goalQueue.push(neighbor);
-                goalVisited[neighbor] = true;
-            }
-        }
+// Node class
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-
-    return 'Path not found';
 }
 
-// Function to get neighbors of a node
-function getNeighbors(node) {
-    // Define this function based on your problem
-    // It should return an array of neighbors of the given node
-    // E.g., return [node1, node2, ...];
+// Function to find the nth node from the end of a linked list
+function findNthFromEnd(head, n) {
+    let pointer1 = head;
+    let pointer2 = head;
+    
+    // Move pointer2 to nth node from the start
+    for (let i = 0; i < n; i++) {
+        if (pointer2 === null) {
+            return null; // n is greater than the length of the list
+        }
+        pointer2 = pointer2.next;
+    }
+    
+    // Move both pointers until pointer2 reaches the end of the list
+    while (pointer2 !== null) {
+        pointer1 = pointer1.next;
+        pointer2 = pointer2.next;
+    }
+    
+    return pointer1;
 }
 
-// Example usage
-let startNode = 'A';
-let goalNode = 'Z';
-console.log(biDirectionalSearch(startNode, goalNode));
+// Create a linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Find the 2nd node from the end
+let n = 2;
+let nthNode = findNthFromEnd(head, n);
+
+if (nthNode !== null) {
+    console.log(`The ${n}th node from the end is: ${nthNode.data}`);
+} else {
+    console.log(`There are less than ${n} nodes in the linked list.`);
+}
