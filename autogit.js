c6c565d13191bbecc5feab4d01fe0d8ebb3e2977
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:53:20 +0800

    4-4-2024 4:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,29 @@
-const str = 'Hello, world!';
-const subStr = 'world';
+function binarySearch(sortedArray, target) {
+    let left = 0;
+    let right = sortedArray.length - 1;
 
-if (str.includes(subStr)) {
-  console.log('Substring found!');
-} else {
-  console.log('Substring not found!');
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (sortedArray[mid] === target) {
+            return mid;
+        } else if (sortedArray[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1; // target not found
 }
-const str = 'Hello, world!';
-const subStr = 'world';
-const regex = new RegExp(subStr);
 
-if (str.match(regex)) {
-  console.log('Substring found!');
+// Test the binary search function
+const arr = [1, 3, 5, 7, 9, 11, 13, 15];
+const target = 7;
+const index = binarySearch(arr, target);
+
+if (index !== -1) {
+    console.log(`Found ${target} at index ${index}`);
 } else {
-  console.log('Substring not found!');
+    console.log(`${target} not found in the array`);
 }
