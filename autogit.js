3a1ff1d9fe89be6b0e2eb053a096ad7e8b8fbdac
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:49:23 +0800

    4-4-2024 4:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,11 @@
-function countingSort(arr, max) {
-    const countArr = new Array(max + 1).fill(0);
-    const sortedArr = [];
-
-    // Count occurrences of each element
-    for (let i = 0; i < arr.length; i++) {
-        countArr[arr[i]]++;
+function findStringLength(str) {
+    let length = 0;
+    for (let char of str) {
+        length++;
     }
-
-    // Build the sorted array using the count array
-    for (let i = 0; i < countArr.length; i++) {
-        while (countArr[i] > 0) {
-            sortedArr.push(i);
-            countArr[i]--;
-        }
-    }
-
-    return sortedArr;
+    return length;
 }
 
-// Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-const max = Math.max(...arr);
-const sortedArr = countingSort(arr, max);
-
-console.log(sortedArr);
+let myString = "Hello, World!";
+let length = findStringLength(myString);
+console.log(length); // Output: 13
