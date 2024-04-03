ab867653735ae17a587ae574cc42136f1aec062e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:42:17 +0800

    4-4-2024 4:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,51 +1,8 @@
-function biDirectionalSearch(graph, startNode, goalNode) {
-    let startQueue = [startNode];
-    let goalQueue = [goalNode];
-    let startVisited = new Set();
-    let goalVisited = new Set();
-
-    startVisited.add(startNode);
-    goalVisited.add(goalNode);
-
-    while (startQueue.length > 0 && goalQueue.length > 0) {
-        let startCurrentNode = startQueue.shift();
-        let goalCurrentNode = goalQueue.shift();
-
-        if (startVisited.has(goalCurrentNode) || goalVisited.has(startCurrentNode)) {
-            console.log("Path found!");
-            return true;
-        }
-
-        let startNeighbors = graph[startCurrentNode] || [];
-        let goalNeighbors = graph[goalCurrentNode] || [];
-
-        for (let neighbor of startNeighbors) {
-            if (!startVisited.has(neighbor)) {
-                startQueue.push(neighbor);
-                startVisited.add(neighbor);
-            }
-        }
-
-        for (let neighbor of goalNeighbors) {
-            if (!goalVisited.has(neighbor)) {
-                goalQueue.push(neighbor);
-                goalVisited.add(neighbor);
-            }
-        }
-    }
-
-    console.log("Path not found!");
-    return false;
-}
-
-// Example graph
-const graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-};
-
-biDirectionalSearch(graph, 'A', 'F');
+let str1 = "Hello";
+let str2 = "World";
+let concatenatedString = str1 + " " + str2;
+console.log(concatenatedString); // Output: Hello World
+let str1 = "Hello";
+let str2 = "World";
+let concatenatedString = str1.concat(" ", str2);
+console.log(concatenatedString); // Output: Hello World
