61cbee64f83bb4ecacef54757be040e7a44dfa64
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 05:00:17 +0800

    4-4-2024 5:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,29 @@
-function findSecondLargest(arr) {
-    let max = arr[0];
-    let secondMax = -Infinity;
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max) {
-            secondMax = max;
-            max = arr[i];
-        } else if (arr[i] > secondMax && arr[i] !== max) {
-            secondMax = arr[i];
-        }
-    }
-
-    return secondMax;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
 }
 
-// Test the function
-const arr = [3, 5, 2, 8, 1, 9];
-const secondLargest = findSecondLargest(arr);
-console.log("Second largest element is: " + secondLargest);
+function findMiddleElement(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.data;
+}
+
+// Creating a linked list
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Finding the middle element of the linked list
+const middleElement = findMiddleElement(head);
+console.log(middleElement);
