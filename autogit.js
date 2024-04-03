d75251157630d842fbe45884e1a86449c95dfdaf
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:42:41 +0800

    4-4-2024 4:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,5 @@
-function validateEmail(email) {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return emailRegex.test(email);
-}
+// Taking input from the user
+var input = prompt("Enter your name:");
 
-// Example usage
-const email = "example@example.com";
-if (validateEmail(email)) {
-  console.log("Valid email address");
-} else {
-  console.log("Invalid email address");
-}
+// Displaying the input
+console.log("Hello, " + input + "! Welcome to our website!");
