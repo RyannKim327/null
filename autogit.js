d6c607ed68bef7621bd395a7fa0b1bc8b1331873
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:51:34 +0800

    4-4-2024 4:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,31 @@
-let str = "123";
-let num = parseInt(str);
+function longestCommonSubstring(str1, str2) {
+    let dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
 
-console.log(num); // Output: 123
-let str = "10";
-let num = parseInt(str, 2); // Parsing a binary number
+    for (let i = 1; i <= str1.length; i++) {
+        for (let j = 1; j <= str2.length; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
 
-console.log(num); // Output: 2
+    if (maxLength === 0) {
+        return "";
+    }
+
+    return str1.substring(endIndex - maxLength + 1, endIndex + 1);
+}
+
+// Example
+const str1 = "abcdefg";
+const str2 = "bcdefgh";
+const commonSubstring = longestCommonSubstring(str1, str2);
+console.log(commonSubstring); // Output: "cdef"
