2589f9fdef21d334973d0719fe24250c2fb6f77e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:52:04 +0800

    4-4-2024 4:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,37 @@
-const str = 'Hello, World!';
-const subStr = 'Hello';
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-if (str.includes(subStr)) {
-    console.log('Substring found!');
-} else {
-    console.log('Substring not found!');
-}
-const str = 'Hello, World!';
-const subStr = 'Hello';
+    function merge(left, right) {
+        let sorted = [];
+        while (left.length && right.length) {
+            if (left[0] < right[0]) {
+                sorted.push(left.shift());
+            } else {
+                sorted.push(right.shift());
+            }
+        }
+        return sorted.concat(left.slice().concat(right.slice()));
+    }
 
-if (str.indexOf(subStr) !== -1) {
-    console.log('Substring found!');
-} else {
-    console.log('Substring not found!');
+    let sortedArray = arr.map((item) => [item]);
+
+    while (sortedArray.length > 1) {
+        let result = [];
+        for (let i = 0; i < sortedArray.length; i += 2) {
+            if (i + 1 < sortedArray.length) {
+                result.push(merge(sortedArray[i], sortedArray[i + 1]));
+            } else {
+                result.push(sortedArray[i]);
+            }
+        }
+        sortedArray = result;
+    }
+
+    return sortedArray[0];
 }
+
+// Test the mergeSort function
+let arr = [8, 3, 1, 7, 0, 10, 2];
+console.log(mergeSort(arr));
