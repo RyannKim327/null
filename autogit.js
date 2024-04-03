45ef816e30d28e8246c79d034cb653816b9c8670
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:59:24 +0800

    4-4-2024 4:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,9 @@
-// Definition for singly-linked list.
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
+const axios = require('axios');
 
-// Function to check if a linked list is a palindrome
-function isPalindrome(head) {
-    let currentNode = head;
-    let values = [];
-
-    // Traverse the linked list and push elements into the array
-    while (currentNode !== null) {
-        values.push(currentNode.val);
-        currentNode = currentNode.next;
-    }
-
-    // Use two pointers technique to compare elements
-    let left = 0;
-    let right = values.length - 1;
-
-    while (left < right) {
-        if (values[left] !== values[right]) {
-            return false;
-        }
-        left++;
-        right--;
-    }
-
-    return true;
-}
-
-// Example usage
-let list = new ListNode(1);
-list.next = new ListNode(2);
-list.next.next = new ListNode(3);
-list.next.next.next = new ListNode(2);
-list.next.next.next.next = new ListNode(1);
-
-console.log(isPalindrome(list)); // Output: true
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
