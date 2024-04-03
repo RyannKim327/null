72cff8ded0ca899842e4a6fdceffe5162efca5f4
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:48:16 +0800

    4-4-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,50 @@
-function largestPrimeFactor(number) {
-    let largestPrime = 2;
+// Bi-directional Search Algorithm
+function biDirectionalSearch(start, goal) {
+    let startQueue = [start];
+    let goalQueue = [goal];
+    let startVisited = { [start]: true };
+    let goalVisited = { [goal]: true };
 
-    while (number > largestPrime) {
-        if (number % largestPrime === 0) {
-            number /= largestPrime;
-        } else {
-            largestPrime++;
+    while (startQueue.length > 0 && goalQueue.length > 0) {
+        let startNode = startQueue.shift();
+        let goalNode = goalQueue.shift();
+
+        if (startVisited[goalNode]) {
+            return 'Path found';
+        }
+
+        if (goalVisited[startNode]) {
+            return 'Path found';
+        }
+
+        // Expand startNode
+        for (let neighbor of getNeighbors(startNode)) {
+            if (!startVisited[neighbor]) {
+                startQueue.push(neighbor);
+                startVisited[neighbor] = true;
+            }
+        }
+
+        // Expand goalNode
+        for (let neighbor of getNeighbors(goalNode)) {
+            if (!goalVisited[neighbor]) {
+                goalQueue.push(neighbor);
+                goalVisited[neighbor] = true;
+            }
         }
     }
 
-    return largestPrime;
+    return 'Path not found';
 }
 
-const number = 600851475143;
-const largestPrime = largestPrimeFactor(number);
+// Function to get neighbors of a node
+function getNeighbors(node) {
+    // Define this function based on your problem
+    // It should return an array of neighbors of the given node
+    // E.g., return [node1, node2, ...];
+}
 
-console.log("The largest prime factor of", number, "is", largestPrime);
+// Example usage
+let startNode = 'A';
+let goalNode = 'Z';
+console.log(biDirectionalSearch(startNode, goalNode));
