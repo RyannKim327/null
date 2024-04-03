cd73210120c81947c8a79294a72d4ea0d8d3dc5b
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:58:41 +0800

    4-4-2024 4:58 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,5 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
+// Ask the user for input
+let userInput = prompt("Please enter a value:");
 
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = current;
-    }
-
-    return arr;
-}
-
-// Example usage:
-const arr = [5, 2, 4, 6, 1, 3];
-console.log("Before sorting: ", arr);
-console.log("After sorting: ", insertionSort(arr));
+// Display the user input
+console.log("You entered: " + userInput);
