5dc12b54e0cda440a8ceb58808c7775fb1c7b5e4
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:57:45 +0800

    4-4-2024 4:57 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,34 @@
-function longestCommonSubsequence(str1, str2) {
-    let m = str1.length;
-    let n = str2.length;
-    
-    let dp = Array.from({ length: m + 1 }, () => Array.from({ length: n + 1 }, () => 0));
-    
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
     }
-    
-    let lcs = '';
-    let i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    
-    return lcs;
+
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
-let str1 = 'ABCDGH';
-let str2 = 'AEDFHR';
-console.log(longestCommonSubsequence(str1, str2));
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+// Example usage
+const arr = [5, 3, 8, 4, 2, 1, 9, 7, 6];
+const sortedArr = mergeSort(arr);
+console.log(sortedArr);
