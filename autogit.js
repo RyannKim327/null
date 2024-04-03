c385b615268cc466f0fddeeb721c370db2bb0b96
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:45:17 +0800

    4-4-2024 4:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,120 +1,20 @@
-// AVL Node class
-class AVLNode {
-    constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
-        this.height = 1;
-    }
-}
+function bubbleSort(arr) {
+    let n = arr.length;
 
-// AVL Tree class
-class AVLTree {
-    constructor() {
-        this.root = null;
-    }
-
-    // Get the height of a node
-    getHeight(node) {
-        if (node === null) {
-            return 0;
-        }
-        return node.height;
-    }
-
-    // Update the height of a node
-    updateHeight(node) {
-        node.height = Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-    }
-
-    // Get the balance factor of a node
-    getBalanceFactor(node) {
-        if (node === null) {
-            return 0;
-        }
-        return this.getHeight(node.left) - this.getHeight(node.right);
-    }
-
-    // Rotate right
-    rotateRight(node) {
-        let newRoot = node.left;
-        node.left = newRoot.right;
-        newRoot.right = node;
-        this.updateHeight(node);
-        this.updateHeight(newRoot);
-        return newRoot;
-    }
-
-    // Rotate left
-    rotateLeft(node) {
-        let newRoot = node.right;
-        node.right = newRoot.left;
-        newRoot.left = node;
-        this.updateHeight(node);
-        this.updateHeight(newRoot);
-        return newRoot;
-    }
-
-    // Rebalance the tree after insertion
-    rebalance(node) {
-        this.updateHeight(node);
-        let balanceFactor = this.getBalanceFactor(node);
-
-        // Left heavy
-        if (balanceFactor > 1) {
-            // Left right case
-            if (this.getBalanceFactor(node.left) < 0) {
-                node.left = this.rotateLeft(node.left);
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-            return this.rotateRight(node);
-        }
-
-        // Right heavy
-        if (balanceFactor < -1) {
-            // Right left case
-            if (this.getBalanceFactor(node.right) > 0) {
-                node.right = this.rotateRight(node.right);
-            }
-            return this.rotateLeft(node);
-        }
-
-        return node;
-    }
-
-    // Insert a value into the AVL tree
-    insert(value) {
-        this.root = this._insert(this.root, value);
-    }
-
-    _insert(node, value) {
-        if (node === null) {
-            return new AVLNode(value);
-        }
-
-        if (value < node.value) {
-            node.left = this._insert(node.left, value);
-        } else {
-            node.right = this._insert(node.right, value);
-        }
-
-        return this.rebalance(node);
-    }
-
-    // Inorder traversal
-    inorder(node) {
-        if (node !== null) {
-            this.inorder(node.left);
-            console.log(node.value);
-            this.inorder(node.right);
         }
     }
+
+    return arr;
 }
 
 // Example usage
-const avlTree = new AVLTree();
-avlTree.insert(10);
-avlTree.insert(20);
-avlTree.insert(30);
-avlTree.insert(15);
-
-avlTree.inorder(avlTree.root);
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray);
+console.log(sortedArray);
