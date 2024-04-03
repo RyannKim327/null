136ed3aec953a9b04018c3272fc0c0437e0323f7
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:46:13 +0800

    4-4-2024 4:46 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,66 +1,74 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+class BTreeNode {
+    constructor(t, leaf) {
+        this.t = t;
+        this.leaf = leaf;
+        this.keys = [];
+        this.children = [];
     }
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
+class BTree {
+    constructor(t) {
+        this.root = new BTreeNode(t, true);
+        this.t = t;
     }
 
-    add(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
+    insert(key) {
+        let root = this.root;
+        if (root.keys.length === (2 * this.t) - 1) {
+            let newRoot = new BTreeNode(this.t, false);
+            newRoot.children.push(root);
+            this.splitChild(newRoot, 0);
+            this.insertNonFull(newRoot, key);
+            this.root = newRoot;
         } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
+            this.insertNonFull(root, key);
         }
     }
 
-    remove(data) {
-        if (!this.head) {
-            return;
-        }
-        
-        if (this.head.data === data) {
-            this.head = this.head.next;
-            return;
-        }
-
-        let current = this.head;
-        let prev = current;
-        while (current) {
-            if (current.data === data) {
-                prev.next = current.next;
-                return;
+    insertNonFull(node, key) {
+        let i = node.keys.length - 1;
+        if (node.leaf) {
+            while (i >= 0 && key < node.keys[i]) {
+                node.keys[i + 1] = node.keys[i];
+                i--;
             }
-            prev = current;
-            current = current.next;
+            node.keys[i + 1] = key;
+        } else {
+            while (i >= 0 && key < node.keys[i]) {
+                i--;
+            }
+            i++;
+            if (node.children[i].keys.length === (2 * this.t) - 1) {
+                this.splitChild(node, i);
+                if (key > node.keys[i]) {
+                    i++;
+                }
+            }
+            this.insertNonFull(node.children[i], key);
         }
     }
 
-    print() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
+    splitChild(parent, index) {
+        let t = this.t;
+        let child = parent.children[index];
+        let newChild = new BTreeNode(t, child.leaf);
+        parent.keys.splice(index, 0, child.keys[t - 1]);
+        parent.children.splice(index + 1, 0, newChild);
+        newChild.keys = child.keys.splice(t, t - 1);
+        if (!child.leaf) {
+            newChild.children = child.children.splice(t, t);
         }
     }
 }
 
-// Example usage:
-const linkedList = new LinkedList();
-linkedList.add(1);
-linkedList.add(2);
-linkedList.add(3);
-linkedList.print();
+// Example Usage
+let bTree = new BTree(3);
 
-linkedList.remove(2);
-linkedList.print();
+bTree.insert(10);
+bTree.insert(20);
+bTree.insert(5);
+bTree.insert(6);
+bTree.insert(12);
+
+console.log(bTree.root);
