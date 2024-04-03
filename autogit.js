1eb72181ec752ff478c7ac4df8e3140ba058ce57
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:56:24 +0800

    4-4-2024 4:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,8 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+let str = "42";
+let num = parseInt(str);
 
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
+console.log(num); // Output: 42
+let str = "42";
+let num = Number(str);
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return quickSort(left).concat(pivot, quickSort(right));
-}
-
-// Example usage
-const array = [3, 5, 2, 8, 1, 4, 7, 6];
-const sortedArray = quickSort(array);
-console.log(sortedArray);  // Output: [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(num); // Output: 42
