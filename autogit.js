c5f505d7c0386fbe5b6c126bbeacfb4c6c59792a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:47:15 +0800

    4-4-2024 4:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,5 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+let numbers = [10, 5, 8, 20, 15];
 
-class BinaryTree {
-  constructor() {
-    this.root = null;
-  }
+let max = Math.max(...numbers);
 
-  insert(value) {
-    const newNode = new Node(value);
-    
-    if (this.root === null) {
-      this.root = newNode;
-    } else {
-      this.insertNode(this.root, newNode);
-    }
-  }
-
-  insertNode(node, newNode) {
-    if (newNode.value < node.value) {
-      if (node.left === null) {
-        node.left = newNode;
-      } else {
-        this.insertNode(node.left, newNode);
-      }
-    } else {
-      if (node.right === null) {
-        node.right = newNode;
-      } else {
-        this.insertNode(node.right, newNode);
-      }
-    }
-  }
-}
-
-// Usage
-const binaryTree = new BinaryTree();
-binaryTree.insert(5);
-binaryTree.insert(3);
-binaryTree.insert(7);
+console.log("The maximum value is: " + max);
