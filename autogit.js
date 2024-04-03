764cfbbe4be1eb3352d8d1f7a960dda6789969d6
RyannKim327 <rksesgundo123@gmail.com>
2024-04-04 04:48:02 +0800

    4-4-2024 4:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,25 @@
-let arr = [1, 2, 3, 3, 4, 5, 5];
-let uniqueArr = [...new Set(arr)];
-console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
-let arr = [1, 2, 3, 3, 4, 5, 5];
-let uniqueArr = arr.filter((value, index, self) => self.indexOf(value) === index);
-console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
-let arr = [1, 2, 3, 3, 4, 5, 5];
-let uniqueArr = arr.reduce((unique, item) => (unique.includes(item) ? unique : [...unique, item]), []);
-console.log(uniqueArr); // Output: [1, 2, 3, 4, 5]
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "!dlroW ,olleH"
+function reverseString(str) {
+    let reversedString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+    return reversedString;
+}
+
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "!dlroW ,olleH"
+function reverseString(str) {
+    return str.split("").reduce((reversed, char) => char + reversed, "");
+}
+
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "!dlroW ,olleH"
