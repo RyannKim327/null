d0d4b86b3824687c39af7c22e748a3bec406b8e6
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:18:11 +0800

    4-6-2024 10:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,8 @@
-class Node {
-    constructor() {
-        this.children = {};
-    }
-}
+let str = "42";
+let num = parseInt(str);
 
-class SuffixTree {
-    constructor(str) {
-        this.root = new Node();
-        for (let i = 0; i < str.length; i++) {
-            this.addSuffix(str.substring(i));
-        }
-    }
+console.log(num); // Output: 42
+let hexStr = "1A";
+let hexNum = parseInt(hexStr, 16);
 
-    addSuffix(suffix) {
-        let node = this.root;
-        for (let i = 0; i < suffix.length; i++) {
-            let char = suffix[i];
-            if (!node.children[char]) {
-                node.children[char] = new Node();
-            }
-            node = node.children[char];
-        }
-    }
-
-    search(pattern) {
-        let node = this.root;
-        for (let i = 0; i < pattern.length; i++) {
-            let char = pattern[i];
-            if (!node.children[char]) {
-                return false;
-            }
-            node = node.children[char];
-        }
-        return true;
-    }
-}
-
-// Example Usage
-let suffixTree = new SuffixTree("banana");
-console.log(suffixTree.search("ana")); // Output: true
-console.log(suffixTree.search("xyz")); // Output: false
+console.log(hexNum); // Output: 26
