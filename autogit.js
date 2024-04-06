381ae33cb843da799309d55ba203bd5f683715d9
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:12:40 +0800

    4-6-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,68 +1,24 @@
-function tarjanStronglyConnectedComponents(graph) {
-    let index = 0;
-    let stack = [];
-    let onStack = new Set();
-    let indexMap = new Map();
-    let lowLinkMap = new Map();
-    let result = [];
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-    function strongConnect(node) {
-        indexMap.set(node, index);
-        lowLinkMap.set(node, index);
-        index++;
-        stack.push(node);
-        onStack.add(node);
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
 
-        for (let neighbor of graph[node]) {
-            if (!indexMap.has(neighbor)) {
-                strongConnect(neighbor);
-                lowLinkMap.set(
-                    node,
-                    Math.min(lowLinkMap.get(node), lowLinkMap.get(neighbor))
-                );
-            } else if (onStack.has(neighbor)) {
-                lowLinkMap.set(
-                    node,
-                    Math.min(lowLinkMap.get(node), indexMap.get(neighbor))
-                );
-            }
-        }
-
-        if (indexMap.get(node) === lowLinkMap.get(node)) {
-            let component = [];
-            let popNode = stack.pop();
-            onStack.delete(popNode);
-            component.push(popNode);
-            
-            while (popNode !== node) {
-                popNode = stack.pop();
-                onStack.delete(popNode);
-                component.push(popNode);
-            }
-
-            result.push(component);
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
         }
     }
 
-    for (let node in graph) {
-        if (!indexMap.has(node)) {
-            strongConnect(node);
-        }
-    }
-
-    return result;
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
-// Example graph representation using adjacency list
-const graph = {
-    0: [1],
-    1: [2],
-    2: [0, 3],
-    3: [4],
-    4: [5, 6],
-    5: [0],
-    6: [4]
-};
-
-const scc = tarjanStronglyConnectedComponents(graph);
-console.log(scc);
+// Example usage:
+const array = [3, 7, 2, 1, 9, 4, 6, 8, 5];
+const sortedArray = quickSort(array);
+console.log(sortedArray);
