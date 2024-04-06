c3090dcf388cb876eb1dba8428590490e3506dc8
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:16:34 +0800

    4-6-2024 10:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,82 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
+// Suffix Tree Node definition
+class Node {
+    constructor() {
+        this.children = {};
+        this.startIndex = null;
+        this.endIndex = null;
     }
-
-    const pivot = arr[Math.floor(arr.length / 2)];
-    
-    const left = [];
-    const right = [];
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else if (arr[i] > pivot) {
-            right.push(arr[i]);
-        }
-    }
-    
-    return quickSort(left).concat(pivot, quickSort(right));
 }
 
-// Example usage:
-const array = [3, 0, 2, 5, -1, 4, 1];
-const sortedArray = quickSort(array);
-console.log(sortedArray);
+// Suffix Tree definition
+class SuffixTree {
+    constructor(text) {
+        this.root = new Node();
+        this.text = text;
+        this.buildSuffixTree();
+    }
+
+    buildSuffixTree() {
+        for (let i = 0; i < this.text.length; i++) {
+            this.addSuffix(this.text.substring(i));
+        }
+    }
+
+    addSuffix(suffix) {
+        let currentNode = this.root;
+
+        for (let i = 0; i < suffix.length; i++) {
+            const currentChar = suffix[i];
+            if (!(currentChar in currentNode.children)) {
+                currentNode.children[currentChar] = new Node();
+                currentNode.children[currentChar].startIndex = this.text.indexOf(suffix.substring(i));
+                currentNode.children[currentChar].endIndex = this.text.indexOf(suffix);
+                return;
+            } else {
+                let childNode = currentNode.children[currentChar];
+                let j = 0;
+
+                while (j < suffix.length && j < this.text.length) {
+                    if (this.text[childNode.startIndex + j] !== suffix[j]) {
+                        // Split the current node
+                        const newNode = new Node();
+                        newNode.startIndex = childNode.startIndex + j;
+                        newNode.endIndex = childNode.endIndex;
+
+                        // Update the child node
+                        childNode.endIndex = childNode.startIndex + j - 1;
+
+                        // Add the new node as a child
+                        childNode.children[this.text[newNode.startIndex]] = newNode;
+
+                        // Create a new child for the current character
+                        const newChild = new Node();
+                        newChild.startIndex = this.text.indexOf(suffix.substring(i + j));
+                        newChild.endIndex = this.text.indexOf(suffix);
+                        newNode.children[this.text[newChild.startIndex]] = newChild;
+
+                        return;
+                    }
+
+                    j++;
+                }
+
+                if (j === suffix.length && j < this.text.length) {
+                    // Continue traversal
+                    currentNode = childNode;
+                } else if (j < suffix.length && j === this.text.length) {
+                    // Add the remaining characters as children
+                    const newChild = new Node();
+                    newChild.startIndex = this.text.indexOf(suffix.substring(i + j));
+                    newChild.endIndex = this.text.indexOf(suffix);
+                    childNode.children[this.text[newChild.startIndex]] = newChild;
+                    return;
+                }
+            }
+        }
+    }
+}
+
+// Test the Suffix Tree
+const suffixTree = new SuffixTree("banana");
+
+console.log(suffixTree.root.children); // Output the root node's children
