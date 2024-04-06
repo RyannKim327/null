b38694e196a8ee510658a5761b5f3f87dd6bf3b4
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:15:42 +0800

    4-6-2024 10:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -12,6 +12,7 @@ class LinkedList {
 
     add(data) {
         const newNode = new Node(data);
+
         if (!this.head) {
             this.head = newNode;
         } else {
@@ -23,47 +24,18 @@ class LinkedList {
         }
     }
 
-    isPalindrome() {
-        if (!this.head) {
-            return true;
-        }
-
-        let slow = this.head;
-        let fast = this.head;
-        while (fast && fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        let prev = null;
-        let current = slow;
+    display() {
+        let current = this.head;
         while (current) {
-            let temp = current.next;
-            current.next = prev;
-            prev = current;
-            current = temp;
+            console.log(current.data);
+            current = current.next;
         }
-
-        let firstHalf = this.head;
-        let secondHalf = prev;
-        while (secondHalf) {
-            if (firstHalf.data !== secondHalf.data) {
-                return false;
-            }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
-        }
-
-        return true;
     }
 }
 
-// Example
-const ll = new LinkedList();
-ll.add(1);
-ll.add(2);
-ll.add(3);
-ll.add(2);
-ll.add(1);
-
-console.log(ll.isPalindrome()); // Output: true
+// Usage
+const linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.display();
