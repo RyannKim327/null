fbfc2d91b39f935d5cb48527a61abfe3663a6327
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:12:59 +0800

    4-6-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,8 @@
-function longestCommonSubstring(str1, str2) {
-    let maxLength = 0;
-    let endIndex = 0;
-
-    const dp = Array(str1.length + 1).fill(0).map(() => Array(str2.length + 1).fill(0));
-
-    for (let i = 1; i <= str1.length; i++) {
-        for (let j = 1; j <= str2.length; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
-        }
-    }
-
-    return str1.substr(endIndex - maxLength + 1, maxLength);
+function decimalToBinary(decimal) {
+    return (decimal >>> 0).toString(2);
 }
 
 // Example usage
-const str1 = 'abcdef';
-const str2 = 'bcdeft';
-const longestSubstring = longestCommonSubstring(str1, str2);
-console.log(longestSubstring); // Output: "cde"
+const decimalNumber = 15;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber); // Output: 1111
