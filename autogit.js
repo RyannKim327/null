aaff43a7f6c1838887e2b606e03d6b1211d5ab29
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:16:46 +0800

    4-6-2024 10:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,16 @@
-// Helper function to get the digit of a number at a specific place value
-function getDigit(num, place) {
-    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+function removeDuplicates(arr) {
+  return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
-// Helper function to get the number of digits in a number
-function digitCount(num) {
-    if (num === 0) return 1;
-    return Math.floor(Math.log10(Math.abs(num))) + 1;
+let arrayWithDuplicates = [1, 2, 3, 4, 2, 3, 5];
+let uniqueArray = removeDuplicates(arrayWithDuplicates);
+
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
 }
 
-// Helper function to get the maximum number of digits in an array of numbers
-function mostDigits(nums) {
-    let maxDigits = 0;
-    for (let i = 0; i < nums.length; i++) {
-        maxDigits = Math.max(maxDigits, digitCount(nums[i]));
-    }
-    return maxDigits;
-}
+let arrayWithDuplicates = [1, 2, 3, 4, 2, 3, 5];
+let uniqueArray = removeDuplicates(arrayWithDuplicates);
 
-function radixSort(arr) {
-    const maxDigitCount = mostDigits(arr);
-
-    for (let k = 0; k < maxDigitCount; k++) {
-        let digitBuckets = Array.from({ length: 10 }, () => []);
-
-        for (let i = 0; i < arr.length; i++) {
-            let digit = getDigit(arr[i], k);
-            digitBuckets[digit].push(arr[i]);
-        }
-
-        arr = [].concat(...digitBuckets);
-    }
-
-    return arr;
-}
-
-// Example usage
-const unsortedArray = [123, 456, 789, 321, 654, 987, 741];
-const sortedArray = radixSort(unsortedArray);
-console.log(sortedArray);
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
