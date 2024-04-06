398a5807e1fde948e08745a387d9a23ee10cce68
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:13:53 +0800

    4-6-2024 10:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,22 @@
-let str = "  Hello      World   ";
-let trimmedStr = str.replace(/\s/g, ''); // Replace all whitespace characters with an empty string
-console.log(trimmedStr); // Output: "HelloWorld"
+function firstNonRepeatingChar(str) {
+    let charMap = {};
+    
+    // Build a character map
+    for (let char of str) {
+        charMap[char] = (charMap[char] || 0) + 1;
+    }
+    
+    // Find the first non-repeating character
+    for (let char of str) {
+        if (charMap[char] === 1) {
+            return char;
+        }
+    }
+    
+    return null; // Return null if there is no non-repeating character
+}
+
+// Test the function
+let inputString = "hello world";
+let result = firstNonRepeatingChar(inputString);
+console.log("First non-repeating character:", result);
