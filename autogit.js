91f23d2db0d0b4a042c8100c177f9a5454a1d888
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:13:18 +0800

    4-6-2024 10:13 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,68 @@
 class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class LinkedList {
+class BinarySearchTree {
     constructor() {
-        this.head = null;
+        this.root = null;
     }
 
-    append(data) {
-        const newNode = new Node(data);
+    insert(value) {
+        const newNode = new Node(value);
 
-        if (!this.head) {
-            this.head = newNode;
-            return;
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
         }
-
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-
-        current.next = newNode;
     }
 
-    print() {
-        let current = this.head;
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (!node.left) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (!node.right) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
 
-        while (current) {
-            console.log(current.data);
-            current = current.next;
+    search(value) {
+        return this.searchNode(this.root, value);
+    }
+
+    searchNode(node, value) {
+        if (!node) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
         }
     }
 }
 
-// Example usage
-const linkedList = new LinkedList();
-linkedList.append(1);
-linkedList.append(2);
-linkedList.append(3);
+// Example Usage
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
 
-linkedList.print(); // Output: 1, 2, 3
+console.log(bst.search(5)); // Output: true
+console.log(bst.search(12)); // Output: false
