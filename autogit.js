9b0be6d43c5b6d3e6b16e671ff957446e32264b5
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:12:09 +0800

    4-6-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,60 @@
-function findSecondLargest(arr) {
-  arr.sort(function(a, b) {
-    return b - a;
-  });
-  
-  return arr[1];
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
-// Example array
-let array = [5, 2, 8, 10, 3];
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
 
-// Find the second largest element
-let secondLargest = findSecondLargest(array);
-console.log(secondLargest); // Output: 8
+    addNode(value) {
+        const node = new Node(value);
+        if (!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+
+    findNthNodeFromEnd(n) {
+        let firstPointer = this.head;
+        let secondPointer = this.head;
+
+        // Move the first pointer to the nth node from the beginning
+        for (let i = 0; i < n; i++) {
+            if (firstPointer === null) {
+                return null; // Return null if n is greater than the length of the linked list
+            }
+            firstPointer = firstPointer.next;
+        }
+
+        // Move both pointers simultaneously until the first pointer reaches the end
+        while (firstPointer !== null) {
+            firstPointer = firstPointer.next;
+            secondPointer = secondPointer.next;
+        }
+
+        return secondPointer;
+    }
+}
+const linkedList = new LinkedList();
+linkedList.addNode(1);
+linkedList.addNode(2);
+linkedList.addNode(3);
+linkedList.addNode(4);
+linkedList.addNode(5);
+const n = 2; // Specify the value of n
+const nthNodeFromEnd = linkedList.findNthNodeFromEnd(n);
+
+if (nthNodeFromEnd) {
+    console.log(`The ${n}th node from the end is: ${nthNodeFromEnd.value}`);
+} else {
+    console.log(`Could not find the ${n}th node from the end.`);
+}
