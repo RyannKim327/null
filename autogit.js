e67ee94d4abd7c17a940a3b0c746d5c67579cf31
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:12:50 +0800

    4-6-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,29 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output will be [5, 4, 3, 2, 1]
+function beamSearch(initialState, beamWidth, maxDepth, getNextStatesFn, heuristicFn) {
+    let open = [{ state: initialState, score: 0 }];
+    let closed = [];
+
+    for (let depth = 0; depth < maxDepth; depth++) {
+        let nextOpen = [];
+
+        for (let i = 0; i < open.length; i++) {
+            let current = open[i].state;
+            let currentScore = open[i].score;
+
+            let nextStates = getNextStatesFn(current);
+
+            for (let j = 0; j < nextStates.length; j++) {
+                let nextState = nextStates[j];
+                let nextStateScore = currentScore + heuristicFn(nextState);
+
+                nextOpen.push({ state: nextState, score: nextStateScore });
+            }
+        }
+
+        nextOpen.sort((a, b) => b.score - a.score);
+        open = nextOpen.slice(0, beamWidth);
+
+        closed = closed.concat(open);
+    }
+
+    return closed;
+}
