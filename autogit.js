6bb428eed634a291292ed90a772964ddd7566322
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:16:25 +0800

    4-6-2024 10:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,73 +1,25 @@
-// Node class to create nodes for the linked list
-class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
     }
+
+    const pivot = arr[Math.floor(arr.length / 2)];
+    
+    const left = [];
+    const right = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else if (arr[i] > pivot) {
+            right.push(arr[i]);
+        }
+    }
+    
+    return quickSort(left).concat(pivot, quickSort(right));
 }
 
-// Queue class to implement the queue using a linked list
-class Queue {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    // Method to add element to the end of the queue
-    enqueue(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.size++;
-    }
-
-    // Method to remove and return element from the front of the queue
-    dequeue() {
-        if (!this.head) {
-            return null;
-        }
-        const value = this.head.value;
-        this.head = this.head.next;
-        this.size--;
-        return value;
-    }
-
-    // Method to get the size of the queue
-    getSize() {
-        return this.size;
-    }
-
-    // Method to check if the queue is empty
-    isEmpty() {
-        return this.size === 0;
-    }
-
-    // Method to print the elements of the queue
-    printQueue() {
-        let current = this.head;
-        while (current) {
-            console.log(current.value);
-            current = current.next;
-        }
-    }
-}
-
-// Example usage
-const queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-queue.printQueue(); // Output: 1, 2, 3
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-
-queue.printQueue(); // Output: 3
+// Example usage:
+const array = [3, 0, 2, 5, -1, 4, 1];
+const sortedArray = quickSort(array);
+console.log(sortedArray);
