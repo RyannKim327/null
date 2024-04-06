f406e6a35bca2c3fa05cb4dc6e24b32383f38c29
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:14:33 +0800

    4-6-2024 10:14 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,9 @@
-function depthLimitedSearch(root, depthLimit) {
-    let stack = [{ node: root, depth: 0 }];
+const axios = require('axios');
 
-    while (stack.length > 0) {
-        let { node, depth } = stack.pop();
-
-        if (depth > depthLimit) {
-            continue;
-        }
-
-        // Perform your search logic on the current node here
-
-        if (node.isGoalState()) {
-            console.log("Goal node found!");
-            return node;
-        }
-
-        node.getSuccessorNodes().forEach(successor => {
-            stack.push({ node: successor, depth: depth + 1 });
-        });
-    }
-
-    console.log(`Goal node not found within depth limit of ${depthLimit}`);
-    return null;
-}
-
-// Usage example
-// depthLimitedSearch(rootNode, 5); // Perform depth-limited search with depth limit of 5
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
