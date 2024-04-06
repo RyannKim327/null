9bd44cf7b7b1d3a89a4ef0417a5dd4d72c67566f
RyannKim327 <rksesgundo123@gmail.com>
2024-04-06 10:12:43 +0800

    4-6-2024 10:12 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,3 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
-
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
-}
-
-// Example usage:
-const array = [3, 7, 2, 1, 9, 4, 6, 8, 5];
-const sortedArray = quickSort(array);
-console.log(sortedArray);
+let array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output will be [5, 4, 3, 2, 1]
