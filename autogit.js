6dffbf90dd1929f77c5218b1c764ebb011a228ed
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:15:24 +0800

    4-17-2024 22:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,41 @@
-function longestCommonSubstring(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    
-    const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-    let maxLength = 0;
-    let endIndex = 0;
-    
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            }
-        }
-    }
-    
-    if (maxLength === 0) {
-        return "";
-    } else {
-        return str1.substring(endIndex - maxLength + 1, endIndex + 1);
-    }
+// Depth-Limited Search Algorithm
+function depthLimitedSearch(node, goal, depthLimit) {
+    return depthLimitedSearchRecursive(node, goal, depthLimit, 0);
 }
 
-const str1 = "abcdef";
-const str2 = "xbcde";
-console.log(longestCommonSubstring(str1, str2)); // Output: "bcde"
+function depthLimitedSearchRecursive(node, goal, depthLimit, currentDepth) {
+    if (currentDepth > depthLimit) {
+        return null;
+    }
+
+    if (node === goal) {
+        return node;
+    }
+
+    if (!node.children) {
+        return null;
+    }
+
+    for (let child of node.children) {
+        let result = depthLimitedSearchRecursive(child, goal, depthLimit, currentDepth + 1);
+        if (result) {
+            return result;
+        }
+    }
+
+    return null;
+}
+
+// Example Usage
+let nodeA = { value: 'A', children: [] };
+let nodeB = { value: 'B', children: [] };
+let nodeC = { value: 'C', children: [] };
+let nodeD = { value: 'D', children: [] };
+let nodeE = { value: 'E', children: [nodeB, nodeC] };
+let nodeF = { value: 'F', children: [nodeD, nodeE] };
+
+nodeA.children = [nodeF];
+
+let goalNode = depthLimitedSearch(nodeA, nodeD, 3);
+
+console.log(goalNode); // Output: { value: 'D', children: [] }
