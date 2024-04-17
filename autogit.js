7debda9cb11564232000003ae4e35c6d07afd68e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:25:46 +0800

    4-17-2024 22:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,18 @@
-let str = "42";
-let intValue = parseInt(str);
-console.log(intValue);  // Output: 42
-let str = "42";
-let intValue = Number(str);
-console.log(intValue);  // Output: 42
+function areAnagrams(str1, str2) {
+    // Remove any non-alphabetic characters and convert the strings to lowercase
+    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
+
+    // Sort the characters of the strings and compare them
+    return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+// Test the function
+const string1 = 'listen';
+const string2 = 'silent';
+
+if (areAnagrams(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
