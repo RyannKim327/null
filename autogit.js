02aecff109f2287ab26c4ba61126a66b6b865c5a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:55:37 +0800

    4-17-2024 21:55 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,15 @@
-function interpolationSearch(arr, x) {
-    let low = 0;
-    let high = arr.length - 1;
-    
-    while (low <= high && x >= arr[low] && x <= arr[high]) {
-        if (low == high) {
-            if (arr[low] === x) return low;
-            return -1;
-        }
-        
-        let pos = low + Math.floor(((high - low) / (arr[high] - arr[low])) * (x - arr[low]));
-        
-        if (arr[pos] === x) return pos;
-        
-        if (arr[pos] < x) {
-            low = pos + 1;
-        } else {
-            high = pos - 1;
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
         }
     }
-    
-    return -1;
+    return count;
 }
 
-// Example usage
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const searchValue = 5;
-const index = interpolationSearch(arr, searchValue);
+let str = "hello world";
+let char = "o";
+let occurrences = countOccurrences(str, char);
 
-if (index !== -1) {
-    console.log(`${searchValue} found at index: ${index}`);
-} else {
-    console.log(`${searchValue} not found`);
-}
+console.log(`The character "${char}" occurs ${occurrences} times in the string "${str}".`);
