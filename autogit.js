553b4137aebd9b9c4736f37a8986a8554358aa0e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:42:07 +0800

    4-17-2024 21:41 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,58 @@
-var str1 = "Hello";
-var str2 = "World";
-var result = str1 + " " + str2;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
-console.log(result);  // Output: Hello World
+// Function to check if a linked list is a palindrome
+function isPalindrome(head) {
+    let values = [];
+    let slow = head;
+    let fast = head;
+
+    // Extract values of the linked list into an array
+    while (fast && fast.next) {
+        values.push(slow.value);
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Handle odd number of nodes
+    if (fast) {
+        slow = slow.next;
+    }
+
+    // Reverse the second half of the linked list
+    let secondHalf = null;
+    while (slow) {
+        let temp = slow.next;
+        slow.next = secondHalf;
+        secondHalf = slow;
+        slow = temp;
+    }
+
+    // Compare the first half and the reversed second half
+    while (secondHalf) {
+        if (values.pop() !== secondHalf.value) {
+            return false;
+        }
+        secondHalf = secondHalf.next;
+    }
+
+    return true;
+}
+
+// Example usage
+let a = new Node('r');
+let b = new Node('a');
+let c = new Node('d');
+let d = new Node('a');
+let e = new Node('r');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(isPalindrome(a)); // Output: true
