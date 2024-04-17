5b3ae6d0c5b54dae482241166e7f0945312a8014
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:07:56 +0800

    4-17-2024 22:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,30 @@
-function isPalindrome(str) {
-    let left = 0;
-    let right = str.length - 1;
-
-    while (left < right) {
-        if (str[left] !== str[right]) {
-            return false;
-        }
-        left++;
-        right--;
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
     }
-
-    return true;
 }
 
-// Test the function
-const word = "racecar";
-if (isPalindrome(word)) {
-    console.log("'" + word + "' is a palindrome.");
-} else {
-    console.log("'" + word + "' is not a palindrome.");
+function countLeafNodes(root) {
+    if (!root) {
+        return 0;
+    }
+    
+    if (!root.left && !root.right) {
+        return 1;
+    }
+    
+    return countLeafNodes(root.left) + countLeafNodes(root.right);
 }
+
+// Example binary tree
+let root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+
+console.log("Number of leaf nodes:", countLeafNodes(root)); // Output should be 4
