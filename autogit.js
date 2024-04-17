1555db93c5c6496c205bb6a002a388f78af18574
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:47:58 +0800

    4-17-2024 21:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,8 @@
-function isSortedAscending(arr) {
-    for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [1, 3, 2, 4, 5];
-
-console.log(isSortedAscending(arr1)); // Output: true
-console.log(isSortedAscending(arr2)); // Output: false
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
