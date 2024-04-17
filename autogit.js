058224e3e59f311364fc52c52ca705cadefdac59
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:27:17 +0800

    4-17-2024 21:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,12 @@
-function longestCommonPrefix(strings) {
-    if (strings.length === 0) return "";
-    
-    let prefix = "";
-    let firstStr = strings[0];
-    
-    for (let i = 0; i < firstStr.length; i++) {
-        let char = firstStr[i];
-        for (let j = 1; j < strings.length; j++) {
-            if (strings[j][i] !== char) {
-                return prefix;
-            }
-        }
-        prefix += char;
-    }
-    
-    return prefix;
-}
+let numbers = [5, 2, 9, 3, 7];
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-// Example usage
-let strings = ["flower", "flour", "flight"];
-console.log(longestCommonPrefix(strings)); // Output: "fl"
+console.log(numbers);
+let numbers = [5, 2, 9, 3, 7];
+numbers.sort(function(a, b) {
+    return b - a;
+});
+
+console.log(numbers);
