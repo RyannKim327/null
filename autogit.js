55b1891b8a20328e73db5ad4f4d1f22a204ceef6
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:11:44 +0800

    4-17-2024 22:11 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,32 @@
-function findFirstRepeatedCharacter(str) {
-    let charMap = {};
+function isAnagram(str1, str2) {
+  // Remove any non-word characters and convert the strings to lowercase
+  str1 = str1.replace(/[\W_]/g, '').toLowerCase();
+  str2 = str2.replace(/[\W_]/g, '').toLowerCase();
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = true;
-        }
+  // Check if the lengths of the two strings are different
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  // Use an object to store the character frequencies of str1
+  const charFreq = {};
+  for (let char of str1) {
+    charFreq[char] = (charFreq[char] || 0) + 1;
+  }
+
+  // Compare the character frequencies of str2 with str1
+  for (let char of str2) {
+    if (!charFreq[char]) {
+      return false;
+    } else {
+      charFreq[char]--;
     }
+  }
 
-    return null;
+  return true;
 }
 
-let str = "hello world";
-let repeatedChar = findFirstRepeatedCharacter(str);
-
-if (repeatedChar) {
-    console.log("The first repeated character is:", repeatedChar);
-} else {
-    console.log("No repeated characters found in the string.");
-}
+// Test the function
+const string1 = "listen";
+const string2 = "silent";
+console.log(isAnagram(string1, string2)); // Output: true
