e83f3ea2487f0669f390e3799792e3d2402ac059
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:35:56 +0800

    4-17-2024 22:35 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,7 @@
-function findMajorityElement(arr) {
-    let majorityElement = arr[0];
-    let count = 1;
+let numbers = [5, 2, 8, 1, 3];
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] === majorityElement) {
-            count++;
-        } else {
-            count--;
-        }
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-        if (count === 0) {
-            majorityElement = arr[i];
-            count = 1;
-        }
-    }
-
-    count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === majorityElement) {
-            count++;
-        }
-    }
-
-    if (count > arr.length / 2) {
-        return majorityElement;
-    } else {
-        return -1; // no majority element found
-    }
-}
-
-// Test
-const arr = [2, 2, 3, 4, 2, 2, 2];
-const majorityElement = findMajorityElement(arr);
-if (majorityElement !== -1) {
-    console.log("Majority element is: " + majorityElement);
-} else {
-    console.log("No majority element found");
-}
+console.log(numbers); // Output: [1, 2, 3, 5, 8]
