e68791958f5d472831c9887cf7475dd036d666eb
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:17:00 +0800

    4-17-2024 22:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,58 @@
-function longestCommonSubstring(s1, s2) {
-    const m = s1.length;
-    const n = s2.length;
-
-    const dp = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
-    let maxLength = 0;
-    let endIndex = 0;
-
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (s1[i - 1] === s2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endIndex = i - 1;
-                }
-            } else {
-                dp[i][j] = 0;
-            }
-        }
+// Red-Black Tree Node class
+class Node {
+    constructor(data, color) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+        this.parent = null;
+        this.color = color; // RED or BLACK
     }
-
-    if (maxLength === 0) {
-        return "";
-    }
-
-    return s1.slice(endIndex - maxLength + 1, endIndex + 1);
 }
 
-const s1 = "abcdef";
-const s2 = "abcfde";
-console.log(longestCommonSubstring(s1, s2)); // Output: "bcf"
+// Red-Black Tree class
+class RedBlackTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Insert a value into the tree
+    insert(data) {
+        let newNode = new Node(data, 'RED');
+        this.root = this.insertNode(this.root, newNode);
+
+        // Fix any violations of the Red-Black tree properties
+        this.fixViolations(newNode);
+    }
+
+    // Recursive function to insert a node into the tree
+    insertNode(root, node) {
+        if (root === null) return node;
+
+        if (node.data < root.data) {
+            root.left = this.insertNode(root.left, node);
+            root.left.parent = root;
+        } else if (node.data > root.data) {
+            root.right = this.insertNode(root.right, node);
+            root.right.parent = root;
+        }
+
+        return root;
+    }
+
+    // Fix violations of Red-Black tree properties after insertion
+    fixViolations(node) {
+        while (node !== this.root && node.color === 'RED' && node.parent.color === 'RED') {
+            // Implement the fix here
+            // This involves rotations and recoloring of nodes
+        }
+
+        this.root.color = 'BLACK'; // Ensure root is always black
+    }
+}
+
+// Usage
+let rbTree = new RedBlackTree();
+rbTree.insert(10);
+rbTree.insert(20);
+rbTree.insert(30);
+rbTree.insert(15);
