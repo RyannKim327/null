c3134357587d8ee8438bddbd2c71715aedc7c7dc
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:25:11 +0800

    4-17-2024 21:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,44 @@
-function findFirstNonRepeatingCharacter(str) {
-    for (let i = 0; i < str.length; i++) {
-        let char = str.charAt(i);
-        if (str.indexOf(char) === i && str.indexOf(char, i + 1) === -1) {
-            return char;
-        }
+class Node {
+  constructor(value, children = []) {
+    this.value = value;
+    this.children = children;
+  }
+}
+
+function depthLimitedSearch(node, target, depthLimit, currentDepth = 0) {
+  // Base case: If the current node contains the target value, return true
+  if (node.value === target) {
+    return true;
+  }
+
+  // Base case: If the depth limit is reached, return false
+  if (currentDepth >= depthLimit) {
+    return false;
+  }
+
+  // Recursively search the children of the current node
+  for (let child of node.children) {
+    if (depthLimitedSearch(child, target, depthLimit, currentDepth + 1)) {
+      return true;
     }
-    return null;
+  }
+
+  return false;
 }
 
 // Example usage
-let inputString = "hello";
-let firstNonRepeatingChar = findFirstNonRepeatingCharacter(inputString);
-console.log("The first non-repeating character in the string is: " + firstNonRepeatingChar);
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+
+nodeA.children = [nodeB, nodeC];
+nodeB.children = [nodeD, nodeE];
+
+const targetValue = 'E';
+const depthLimit = 2;
+
+const result = depthLimitedSearch(nodeA, targetValue, depthLimit);
+
+console.log(`Target value found: ${result}`);
