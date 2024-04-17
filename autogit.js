2dffe15adca2e29febbe5fc11d2d892c3b98c904
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:16:49 +0800

    4-17-2024 22:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,32 @@
-// Sample array of integers
-let numbers = [5, 3, 8, 1, 2];
+function longestCommonSubstring(s1, s2) {
+    const m = s1.length;
+    const n = s2.length;
 
-// Use the sort method to sort the array
-numbers.sort((a, b) => a - b);
+    const dp = Array(m + 1).fill(0).map(() => Array(n + 1).fill(0));
+    let maxLength = 0;
+    let endIndex = 0;
 
-// Print the sorted array
-console.log(numbers); // Output: [1, 2, 3, 5, 8]
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
+                    endIndex = i - 1;
+                }
+            } else {
+                dp[i][j] = 0;
+            }
+        }
+    }
+
+    if (maxLength === 0) {
+        return "";
+    }
+
+    return s1.slice(endIndex - maxLength + 1, endIndex + 1);
+}
+
+const s1 = "abcdef";
+const s2 = "abcfde";
+console.log(longestCommonSubstring(s1, s2)); // Output: "bcf"
