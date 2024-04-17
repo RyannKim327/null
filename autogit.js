ce89025c3c58c0207d64b82686586045381bb270
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:31:36 +0800

    4-17-2024 21:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,135 +1,28 @@
-// Define a Node class to represent a node in the red-black tree
 class Node {
-    constructor(value, color) {
+    constructor(value) {
         this.value = value;
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-        this.color = color; // 0 for red, 1 for black
+        this.next = null;
     }
 }
 
-// Define a RedBlackTree class
-class RedBlackTree {
-    constructor() {
-        this.root = null;
+function findMiddleElement(head) {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
     }
 
-    // Method to perform left rotation
-    leftRotate(node) {
-        const temp = node.right;
-        node.right = temp.left;
-        
-        if (temp.left !== null) {
-            temp.left.parent = node;
-        }
-
-        temp.parent = node.parent;
-
-        if (node.parent === null) {
-            this.root = temp;
-        } else if (node === node.parent.left) {
-            node.parent.left = temp;
-        } else {
-            node.parent.right = temp;
-        }
-
-        temp.left = node;
-        node.parent = temp;
-    }
-
-    // Method to perform right rotation
-    rightRotate(node) {
-        const temp = node.left;
-        node.left = temp.right;
-
-        if (temp.right !== null) {
-            temp.right.parent = node;
-        }
-
-        temp.parent = node.parent;
-
-        if (node.parent === null) {
-            this.root = temp;
-        } else if (node === node.parent.right) {
-            node.parent.right = temp;
-        } else {
-            node.parent.left = temp;
-        }
-
-        temp.right = node;
-        node.parent = temp;
-    }
-
-    // Method to fix violations after insertion
-    fixInsertion(node) {
-        while (node !== this.root && node.parent.color === 0) {
-            if (node.parent === node.parent.parent.left) {
-                let uncle = node.parent.parent.right;
-                
-                if (uncle.color === 0) {
-                    node.parent.color = 1;
-                    uncle.color = 1;
-                    node.parent.parent.color = 0;
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.right) {
-                        node = node.parent;
-                        this.leftRotate(node);
-                    }
-
-                    node.parent.color = 1;
-                    node.parent.parent.color = 0;
-                    this.rightRotate(node.parent.parent);
-                }
-            } else {
-                let uncle = node.parent.parent.left;
-                
-                if (uncle.color === 0) {
-                    node.parent.color = 1;
-                    uncle.color = 1;
-                    node.parent.parent.color = 0;
-                    node = node.parent.parent;
-                } else {
-                    if (node === node.parent.left) {
-                        node = node.parent;
-                        this.rightRotate(node);
-                    }
-
-                    node.parent.color = 1;
-                    node.parent.parent.color = 0;
-                    this.leftRotate(node.parent.parent);
-                }
-            }
-        }
-        this.root.color = 1;
-    }
-
-    // Method to insert a node into the red-black tree
-    insert(value) {
-        const newNode = new Node(value, 0);
-        let y = null;
-        let x = this.root;
-
-        while (x !== null) {
-            y = x;
-            if (newNode.value < x.value) {
-                x = x.left;
-            } else {
-                x = x.right;
-            }
-        }
-
-        newNode.parent = y;
-
-        if (y === null) {
-            this.root = newNode;
-        } else if (newNode.value < y.value) {
-            y.left = newNode;
-        } else {
-            y.right = newNode;
-        }
-
-        this.fixInsertion(newNode);
-    }
+    return slow.value;
 }
+
+// Create a sample linked list
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+// Find and print the middle element of the linked list
+console.log(findMiddleElement(head)); // Output: 3
