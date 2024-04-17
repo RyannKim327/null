8657e9aec239a40acc9aac8e239578842f28185c
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:19:01 +0800

    4-17-2024 21:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,8 @@
-function largestPrimeFactor(number) {
-    if (number <= 1) {
-        return null;
-    }
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = [...new Set(arrayWithDuplicates)];
 
-    let largestFactor = 1;
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = arrayWithDuplicates.filter((value, index, self) => self.indexOf(value) === index);
 
-    // Find and remove all divisors of 2
-    while (number % 2 === 0) {
-        largestFactor = 2;
-        number /= 2;
-    }
-
-    // Find and remove all other prime factors
-    for (let i = 3; i <= Math.sqrt(number); i += 2) {
-        while (number % i === 0) {
-            largestFactor = i;
-            number /= i;
-        }
-    }
-
-    // If the remaining number is a prime greater than 2
-    if (number > 2) {
-        largestFactor = number;
-    }
-
-    return largestFactor;
-}
-
-// Test the function with a number
-const number = 13195;
-console.log(largestPrimeFactor(number)); // Output: 29
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
