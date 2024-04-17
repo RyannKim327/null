842a813887c5039bb30cbb9b719c56c4fc26d2d0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:46:05 +0800

    4-17-2024 21:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,23 @@
-const string1 = "Hello";
-const string2 = "World";
-const concatenatedString = string1 + " " + string2;
-console.log(concatenatedString); // Output: Hello World
+function longestCommonPrefix(strs) {
+    if (strs.length === 0) return '';
+
+    let prefix = '';
+
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return prefix;
+            }
+        }
+
+        prefix += char;
+    }
+
+    return prefix;
+}
+
+// Example usage
+const strings = ['flower', 'flow', 'flight'];
+console.log(longestCommonPrefix(strings)); // Output: 'fl'
