f600220f30d9b34afca941df4e49b1d27ab65402
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:17:53 +0800

    4-17-2024 22:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,23 @@
-function removeVowels(str) {
-    return str.replace(/[aeiouAEIOU]/g, '');
+function isPrime(number) {
+    if (number <= 1) {
+        return false;
+    }
+    
+    // Check for divisibility from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+    
+    // If no number divided the input number, then it's a prime number
+    return true;
 }
 
-const inputString = "Hello, World!";
-const outputString = removeVowels(inputString);
-
-console.log(outputString); // Output: "Hll, Wrld!"
+// Test the function
+const number = 11;
+if (isPrime(number)) {
+    console.log(`${number} is a prime number.`);
+} else {
+    console.log(`${number} is not a prime number.`);
+}
