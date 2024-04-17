20062491a482111f5c93959ccbebbaf6593b1c88
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:33:56 +0800

    4-17-2024 21:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,14 @@
-function computeLPSArray(pattern, m, lps) {
-    let len = 0;
-    lps[0] = 0;
-    let i = 1;
-    while (i < m) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        } else {
-            if (len !== 0) {
-                len = lps[len - 1];
-            } else {
-                lps[i] = 0;
-                i++;
-            }
+function firstNonRepeatingChar(str) {
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charAt(i);
+        if (str.indexOf(char) == i && str.indexOf(char, i + 1) == -1) {
+            return char;
         }
     }
+    return null;
 }
 
-function KMPSearch(text, pattern) {
-    const n = text.length;
-    const m = pattern.length;
-
-    const lps = new Array(m).fill(0);
-    computeLPSArray(pattern, m, lps);
-
-    let i = 0;
-    let j = 0;
-    const indices = [];
-
-    while (i < n) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-
-        if (j === m) {
-            indices.push(i - j);
-            j = lps[j - 1];
-        } else if (i < n && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
-        }
-    }
-
-    return indices;
-}
-
-// Example usage
-const text = "ABABDABACDABABCABAB";
-const pattern = "ABABCABAB";
-const indices = KMPSearch(text, pattern);
-console.log(indices);
+// Test the function
+let str = "hello world";
+let firstNonRepeatChar = firstNonRepeatingChar(str);
+console.log("The first non-repeating character in '" + str + "' is: " + firstNonRepeatChar);
