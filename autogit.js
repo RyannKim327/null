0464b92d9012a2e57864a9ea511946621dc013c5
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:10:42 +0800

    4-17-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,14 @@
-class Edge {
-    constructor(src, dest, weight) {
-        this.src = src;
-        this.dest = dest;
-        this.weight = weight;
-    }
+function isPalindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const alphanumericStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+    
+    // Reverse the string
+    const reversedStr = alphanumericStr.split('').reverse().join('');
+    
+    // Check if the original string is equal to the reversed string
+    return alphanumericStr === reversedStr;
 }
 
-class Graph {
-    constructor(numVertices, edges) {
-        this.numVertices = numVertices;
-        this.edges = edges;
-    }
-
-    bellmanFord(source) {
-        let distance = new Array(this.numVertices).fill(Infinity);
-        distance[source] = 0;
-
-        for (let i = 0; i < this.numVertices - 1; i++) {
-            for (let j = 0; j < this.edges.length; j++) {
-                let edge = this.edges[j];
-                if (distance[edge.src] + edge.weight < distance[edge.dest]) {
-                    distance[edge.dest] = distance[edge.src] + edge.weight;
-                }
-            }
-        }
-
-        for (let j = 0; j < this.edges.length; j++) {
-            let edge = this.edges[j];
-            if (distance[edge.src] + edge.weight < distance[edge.dest]) {
-                console.log("Negative weight cycle detected, Bellman-Ford algorithm failed.");
-                return;
-            }
-        }
-
-        return distance;
-    }
-}
-
-// Example usage
-const numVertices = 5;
-const edges = [
-    new Edge(0, 1, -1),
-    new Edge(0, 2, 4),
-    new Edge(1, 2, 3),
-    new Edge(1, 3, 2),
-    new Edge(1, 4, 2),
-    new Edge(3, 2, 5),
-    new Edge(3, 1, 1),
-    new Edge(4, 3, -3)
-];
-
-const graph = new Graph(numVertices, edges);
-const sourceNode = 0;
-const shortestPaths = graph.bellmanFord(sourceNode);
-
-console.log("Shortest paths from node", sourceNode + ":");
-
-for (let i = 0; i < shortestPaths.length; i++) {
-    console.log("Node", i, ":", shortestPaths[i]);
-}
+// Test the function
+const testString = "A man, a plan, a canal, Panama!";
+console.log(isPalindrome(testString)); // Output: true
