c084d95b9a5bf646f81afd216439b07fada84415
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:17:44 +0800

    4-17-2024 22:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,8 @@
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        
-        arr[j + 1] = current;
-    }
-
-    return arr;
+function removeVowels(str) {
+    return str.replace(/[aeiouAEIOU]/g, '');
 }
 
-// Example usage
-let arr = [5, 3, 8, 1, 2];
-console.log("Before sorting:", arr);
-arr = insertionSort(arr);
-console.log("After sorting:", arr);
+const inputString = "Hello, World!";
+const outputString = removeVowels(inputString);
+
+console.log(outputString); // Output: "Hll, Wrld!"
