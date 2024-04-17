24fc265f501599d8abbeeb7959c1eae9360b76e0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:25:57 +0800

    4-17-2024 22:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,3 @@
-function binarySearch(arr, target) {
-    let left = 0;
-    let right = arr.length - 1;
-
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-
-        if (arr[mid] === target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-
-    return -1; // Not found
-}
-
-// Example usage
-const arr = [1, 3, 5, 7, 9, 11, 13, 15];
-const target = 7;
-const index = binarySearch(arr, target);
-console.log(index); // Output: 3
+let str = "   Hello,  World!   ";
+let trimmedStr = str.replace(/\s/g, '');
+console.log(trimmedStr); // Output: "Hello,World!"
