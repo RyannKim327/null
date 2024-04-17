8e002e23ee0c5081ebbbeae2d1111c25f6a36306
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:34:33 +0800

    4-17-2024 22:34 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,56 +1,62 @@
-function computeLPSArray(pattern) {
-    let lps = new Array(pattern.length);
-    let len = 0;
-    lps[0] = 0;
-    let i = 1;
+// Define a node for the binary tree
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
 
-    while (i < pattern.length) {
-        if (pattern[i] === pattern[len]) {
-            len++;
-            lps[i] = len;
-            i++;
+// Define the binary tree class
+class BinaryTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // Insert a new value into the binary tree
+    insert(value) {
+        const newNode = new Node(value);
+
+        if (!this.root) {
+            this.root = newNode;
         } else {
-            if (len !== 0) {
-                len = lps[len - 1];
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
             } else {
-                lps[i] = 0;
-                i++;
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
             }
         }
     }
 
-    return lps;
-}
-
-function KMP(pattern, text) {
-    let lps = computeLPSArray(pattern);
-    let i = 0;
-    let j = 0;
-    let matches = [];
-
-    while (i < text.length) {
-        if (pattern[j] === text[i]) {
-            i++;
-            j++;
-        }
-
-        if (j === pattern.length) {
-            matches.push(i - j);
-            j = lps[j - 1];
-        } else if (i < text.length && pattern[j] !== text[i]) {
-            if (j !== 0) {
-                j = lps[j - 1];
-            } else {
-                i++;
-            }
+    // Perform an in-order traversal of the binary tree
+    inOrderTraversal(node = this.root) {
+        if (node !== null) {
+            this.inOrderTraversal(node.left);
+            console.log(node.value);
+            this.inOrderTraversal(node.right);
         }
     }
-
-    return matches;
 }
 
-// Example usage
-let text = "ABCABCDABABCDABCDABDE";
-let pattern = "ABCDABD";
-let matches = KMP(pattern, text);
-console.log("Pattern found at indices:", matches);
+// Create a new binary tree and insert some values
+const binaryTree = new BinaryTree();
+binaryTree.insert(5);
+binaryTree.insert(3);
+binaryTree.insert(8);
+binaryTree.insert(2);
+binaryTree.insert(4);
+
+// Perform an in-order traversal to display the values
+binaryTree.inOrderTraversal();
