dbf87cdc0f4693e5da3880064016a4b5356cbe86
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:06:06 +0800

    4-17-2024 22:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,2 @@
-// Define the URL of the API
-const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
-
-// Make a GET request to the API
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+let userInput = prompt("Enter something:");
+console.log("You entered: " + userInput);
