997283e336ec944b23689090ad9c1ccd5df3730e
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:00:03 +0800

    4-17-2024 21:59 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,8 @@
-function isSortedAscending(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      return false;
-    }
-  }
-  return true;
+function reverseString(str) {
+    return str.split('').reverse().join('');
 }
 
-// Test the function
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [2, 4, 6, 1, 3];
+let originalString = 'Hello, World!';
+let reversedString = reverseString(originalString);
 
-console.log(isSortedAscending(array1)); // Output: true
-console.log(isSortedAscending(array2)); // Output: false
+console.log(reversedString); // Outputs: '!dlroW ,olleH'
