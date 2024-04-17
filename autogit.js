8206d467c7028d914b1d139e2a5fd926bab2dc57
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:40:51 +0800

    4-17-2024 21:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,22 @@
-function buildPatternTable(pattern) {
-    let patternTable = [0];
-    let prefixIndex = 0;
-    let suffixIndex = 1;
+function bubbleSort(arr) {
+  let len = arr.length;
+  let swapped;
 
-    while (suffixIndex < pattern.length) {
-        if (pattern[suffixIndex] === pattern[prefixIndex]) {
-            patternTable[suffixIndex] = prefixIndex + 1;
-            suffixIndex++;
-            prefixIndex++;
-        } else if (prefixIndex === 0) {
-            patternTable[suffixIndex] = 0;
-            suffixIndex++;
-        } else {
-            prefixIndex = patternTable[prefixIndex - 1];
-        }
+  do {
+    swapped = false;
+    for (let i = 0; i < len - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
     }
+  } while (swapped);
 
-    return patternTable;
+  return arr;
 }
 
-function kmpSearch(text, pattern) {
-    if (pattern.length === 0) {
-        return 0;
-    }
-
-    let textIndex = 0;
-    let patternIndex = 0;
-    let patternTable = buildPatternTable(pattern);
-
-    while (textIndex < text.length) {
-        if (text[textIndex] === pattern[patternIndex]) {
-            if (patternIndex === pattern.length - 1) {
-                return textIndex - pattern.length + 1;
-            }
-            textIndex++;
-            patternIndex++;
-        } else if (patternIndex > 0) {
-            patternIndex = patternTable[patternIndex - 1];
-        } else {
-            textIndex++;
-        }
-    }
-
-    return -1;
-}
-
-// Example usage
-let text = "ababcababcabc";
-let pattern = "abc";
-let index = kmpSearch(text, pattern);
-console.log(index); // Output: 2
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Before sorting: " + arr);
+console.log("After sorting: " + bubbleSort(arr));
