c17b27b5f0654001f8703126c840a2490a07e5f1
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:16:01 +0800

    4-17-2024 22:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,62 +1,12 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
+function findSecondLargest(arr) {
+    arr = arr.sort(function(a, b) {
+        return b - a;
+    });
+
+    return arr[1];
 }
 
-class LinkedList {
-    constructor() {
-        this.head = null;
-    }
-
-    addNode(data) {
-        const newNode = new Node(data);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    display() {
-        let current = this.head;
-        while (current) {
-            console.log(current.data);
-            current = current.next;
-        }
-    }
-
-    reverse() {
-        let prev = null;
-        let current = this.head;
-        let next = null;
-
-        while (current !== null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-        this.head = prev;
-    }
-}
-
-// Example Usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-linkedList.addNode(4);
-
-console.log("Original Linked List:");
-linkedList.display();
-
-linkedList.reverse();
-
-console.log("Reversed Linked List:");
-linkedList.display();
+// Test the function
+const array = [3, 7, 1, 9, 5];
+const secondLargest = findSecondLargest(array);
+console.log("Second largest element:", secondLargest);
