caa1ddf2bbbc3548e2a9ee715cd71b8a66566754
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:09:14 +0800

    4-17-2024 22:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,50 @@
-function reverseWords(str) {
-    return str.split(' ').reverse().join(' ');
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    // Push element to the top of the stack
+    push(element) {
+        this.items.push(element);
+    }
+
+    // Pop element from the top of the stack
+    pop() {
+        if (this.isEmpty()) {
+            return "Underflow";
+        }
+        return this.items.pop();
+    }
+
+    // Peek the top element of the stack
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    // Check if the stack is empty
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    // Print the stack elements
+    printStack() {
+        let str = "";
+        for (let i = 0; i < this.items.length; i++) {
+            str += this.items[i] + " ";
+        }
+        return str;
+    }
 }
 
-// Example usage
-const originalString = "Hello world";
-const reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: "world Hello"
+// Example usage:
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+console.log(stack.printStack()); // Output: 1 2 3
+
+console.log(stack.pop()); // Output: 3
+console.log(stack.printStack()); // Output: 1 2
+
+console.log(stack.peek()); // Output: 2
