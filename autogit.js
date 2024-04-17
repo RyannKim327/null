f2c813547bd51b1ee32569a1fe088df922389602
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:00:20 +0800

    4-17-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,18 @@
-function reverseString(str) {
-    return str.split('').reverse().join('');
+function countOccurrences(text, word) {
+    let count = 0;
+    const words = text.split(" ");
+    
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].toLowerCase() === word.toLowerCase()) {
+            count++;
+        }
+    }
+    
+    return count;
 }
 
-let originalString = 'Hello, World!';
-let reversedString = reverseString(originalString);
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget consectetur dui. Nam pharetra velit at justo suscipit, eu malesuada sapien ullamcorper. Sed ultricies cursus eros, vel gravida lorem feugiat vel. Nulla facilisi pharetra.";
+const word = "consectetur";
 
-console.log(reversedString); // Outputs: '!dlroW ,olleH'
+const occurrences = countOccurrences(text, word);
+console.log(`The word "${word}" occurs ${occurrences} times in the text.`);
