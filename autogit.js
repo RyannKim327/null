47f07cb6dac60710167769719e4086a7089cde22
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:09:24 +0800

    4-17-2024 22:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,15 @@
-// Taking user input using prompt
-let userInput = prompt("Please enter something:");
+function isArraySortedAscending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
-// Displaying the user input
-console.log("You entered: " + userInput);
+// Example usage
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [5, 4, 3, 2, 1];
+
+console.log(isArraySortedAscending(arr1)); // Output: true
+console.log(isArraySortedAscending(arr2)); // Output: false
