2fc01ea1858874bf878a9670785e1f28b39b905c
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:25:37 +0800

    4-17-2024 22:25 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,6 @@
-function rabinKarpSearch(text, pattern) {
-    const prime = 101; // Prime number to use as base for hashing
-    const patternLength = pattern.length;
-    const textLength = text.length;
-    const patternHash = hash(pattern, patternLength);
-    let textHash = hash(text, patternLength);
-
-    for (let i = 0; i <= textLength - patternLength; i++) {
-        if (textHash === patternHash && text.substring(i, i + patternLength) === pattern) {
-            return i; // Pattern found at index i
-        }
-        if (i < textLength - patternLength) {
-            textHash = recalculateHash(text, i, patternLength, textHash, prime);
-        }
-    }
-
-    return -1; // Pattern not found
-}
-
-function hash(str, length) {
-    let hash = 0;
-    for (let i = 0; i < length; i++) {
-        hash += str.charCodeAt(i) * Math.pow(prime, i);
-    }
-    return hash;
-}
-
-function recalculateHash(str, oldIndex, patternLength, oldHash, prime) {
-    let newHash = oldHash - str.charCodeAt(oldIndex);
-    newHash = newHash / prime;
-    newHash += str.charCodeAt(oldIndex + patternLength) * Math.pow(prime, patternLength - 1);
-    return newHash;
-}
-
-// Test
-const text = "abedabc";
-const pattern = "abc";
-const index = rabinKarpSearch(text, pattern);
-console.log(index); // Output: 3 (Index of pattern in text)
+let str = "42";
+let intValue = parseInt(str);
+console.log(intValue);  // Output: 42
+let str = "42";
+let intValue = Number(str);
+console.log(intValue);  // Output: 42
