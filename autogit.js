10d1c594130b52acdf3b94b09ba7d3b5cc7294bc
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:00:59 +0800

    4-17-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,10 @@
-function binarySearchRecursive(array, target, start, end) {
-    if (start > end) {
-        return -1;
-    }
+const axios = require('axios');
 
-    const middle = Math.floor((start + end) / 2);
-
-    if (array[middle] === target) {
-        return middle;
-    } else if (array[middle] < target) {
-        return binarySearchRecursive(array, target, middle + 1, end);
-    } else {
-        return binarySearchRecursive(array, target, start, middle - 1);
-    }
-}
-
-// Example usage:
-const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15];
-const target = 7;
-const index = binarySearchRecursive(sortedArray, target, 0, sortedArray.length - 1);
-
-if (index !== -1) {
-    console.log(`Element found at index: ${index}`);
-} else {
-    console.log("Element not found");
-}
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then((response) => {
+    console.log('Title:', response.data.title);
+    console.log('Body:', response.data.body);
+  })
+  .catch((error) => {
+    console.error('Error:', error.message);
+  });
