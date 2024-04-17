d6f3ec9a837ce0c18bd88b71f123235af878d3b0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:45:41 +0800

    4-17-2024 21:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,15 @@
 function countOccurrences(str, char) {
     let count = 0;
-
     for (let i = 0; i < str.length; i++) {
         if (str[i] === char) {
             count++;
         }
     }
-
     return count;
 }
 
 // Example usage
-const myString = "hello world";
-const charToCount = "o";
-const occurrences = countOccurrences(myString, charToCount);
-
-console.log(`The character "${charToCount}" occurs ${occurrences} times in the string "${myString}".`);
+let myString = "hello";
+let charToCount = "l";
+let occurrences = countOccurrences(myString, charToCount);
+console.log(`The character '${charToCount}' occurs ${occurrences} times in the string '${myString}'.`);
