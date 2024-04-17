0a6b72f70e31609d0edc7d170b07fee6d6b0f638
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:48:37 +0800

    4-17-2024 21:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,16 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+let str = "Hello, world!";
+let substring = "world";
+
+if (str.includes(substring)) {
+    console.log("The string contains the substring.");
+} else {
+    console.log("The string does not contain the substring.");
+}
+let str = "Hello, world!";
+let substring = "world";
+
+if (str.indexOf(substring) !== -1) {
+    console.log("The string contains the substring.");
+} else {
+    console.log("The string does not contain the substring.");
+}
