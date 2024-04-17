7bb1fdf873e3e150c27b13a7d0f254e917c4f93b
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:11:07 +0800

    4-17-2024 22:10 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,19 @@
-const removeDuplicates = (arr) => {
-  return arr.filter((item, index) => arr.indexOf(item) === index);
-};
+function findSecondLargest(arr) {
+    let max = arr[0];
+    let secondMax = -Infinity;
 
-const arrWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const arrWithoutDuplicates = removeDuplicates(arrWithDuplicates);
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            secondMax = max;
+            max = arr[i];
+        } else if (arr[i] > secondMax && arr[i] < max) {
+            secondMax = arr[i];
+        }
+    }
 
-console.log(arrWithoutDuplicates); // Output: [1, 2, 3, 4, 5]
+    return secondMax !== -Infinity ? secondMax : 'No second largest element found';
+}
+
+const arr = [10, 5, 20, 8, 15];
+const secondLargest = findSecondLargest(arr);
+console.log(secondLargest); // Output: 15
