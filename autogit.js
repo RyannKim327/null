4f790480a907db397dd3ffe0954f08b6f5eaf28f
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:33:18 +0800

    4-17-2024 21:33 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,47 @@
-const url = 'https://jsonplaceholder.typicode.com/posts/1';
+// Define a Node class to represent each node in the linked list
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// Define a LinkedList class to represent the linked list
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to add a node to the end of the linked list
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to print the linked list
+    print() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Usage
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.print();
