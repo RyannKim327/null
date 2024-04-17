c6e6f7ce36c76899a6f71f1ddedb8a3842461b1a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:38:12 +0800

    4-17-2024 21:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,24 @@
-class Edge {
-    constructor(source, destination, weight) {
-        this.source = source;
-        this.destination = destination;
-        this.weight = weight;
-    }
-}
-
-class Graph {
-    constructor(vertices, edges) {
-        this.vertices = vertices;
-        this.edges = edges;
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
     }
 
-    initializeDistance(source) {
-        let distance = {};
-        for (let i = 0; i < this.vertices; i++) {
-            distance[i] = Infinity;
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
         }
-        distance[source] = 0;
-        return distance;
     }
 
-    bellmanFord(source) {
-        let distance = this.initializeDistance(source);
-
-        for (let i = 0; i < this.vertices - 1; i++) {
-            for (let j = 0; j < this.edges.length; j++) {
-                let edge = this.edges[j];
-                if (distance[edge.source] + edge.weight < distance[edge.destination]) {
-                    distance[edge.destination] = distance[edge.source] + edge.weight;
-                }
-            }
-        }
-
-        for (let i = 0; i < this.edges.length; i++) {
-            let edge = this.edges[i];
-            if (distance[edge.source] + edge.weight < distance[edge.destination]) {
-                console.log("Graph contains negative weight cycle");
-                return;
-            }
-        }
-
-        console.log("Shortest distances from source:");
-        console.log(distance);
-    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 // Example usage
-let vertices = 5;
-let edges = [
-    new Edge(0, 1, -1),
-    new Edge(0, 2, 4),
-    new Edge(1, 2, 3),
-    new Edge(1, 3, 2),
-    new Edge(1, 4, 2),
-    new Edge(3, 2, 5),
-    new Edge(3, 1, 1),
-    new Edge(4, 3, -3)
-];
-
-let graph = new Graph(vertices, edges);
-graph.bellmanFord(0);
+const unsortedArray = [6, 3, 8, 2, 9, 1];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 3, 6, 8, 9]
