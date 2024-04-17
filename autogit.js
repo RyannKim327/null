6dff0f06ad4de050d97d2c2fbdd8ad4be1a089a0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:15:13 +0800

    4-17-2024 22:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,30 @@
 function longestCommonSubstring(str1, str2) {
     const m = str1.length;
     const n = str2.length;
-
+    
     const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
-    let maxLen = 0;
-    let endIndex = -1;
-
+    let maxLength = 0;
+    let endIndex = 0;
+    
     for (let i = 1; i <= m; i++) {
         for (let j = 1; j <= n; j++) {
             if (str1[i - 1] === str2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLen) {
-                    maxLen = dp[i][j];
+                if (dp[i][j] > maxLength) {
+                    maxLength = dp[i][j];
                     endIndex = i - 1;
                 }
-            } else {
-                dp[i][j] = 0;
             }
         }
     }
-
-    if (maxLen === 0) {
-        return '';
+    
+    if (maxLength === 0) {
+        return "";
+    } else {
+        return str1.substring(endIndex - maxLength + 1, endIndex + 1);
     }
-
-    return str1.substring(endIndex - maxLen + 1, endIndex + 1);
 }
 
-// Example usage
-const str1 = 'abcdef';
-const str2 = 'bcde';
-console.log(longestCommonSubstring(str1, str2)); // Output: 'bcde'
+const str1 = "abcdef";
+const str2 = "xbcde";
+console.log(longestCommonSubstring(str1, str2)); // Output: "bcde"
