eb1736c904b1d89785fa98e7fb30dcd6f288b6ee
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:38:34 +0800

    4-17-2024 21:38 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,9 @@
-function quickSort(arr) {
-    if (arr.length <= 1) {
-        return arr;
-    }
+const axios = require('axios');
 
-    const pivot = arr[0];
-    const left = [];
-    const right = [];
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < pivot) {
-            left.push(arr[i]);
-        } else {
-            right.push(arr[i]);
-        }
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
-}
-
-// Example usage
-const unsortedArray = [6, 3, 8, 2, 9, 1];
-const sortedArray = quickSort(unsortedArray);
-console.log(sortedArray); // Output: [1, 2, 3, 6, 8, 9]
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching data: ', error);
+  });
