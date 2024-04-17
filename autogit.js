1ffed7a7d89a86deb766db71f3f5bcc3a1ac4b92
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:29:49 +0800

    4-17-2024 21:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,32 @@
-function findFirstRepeatedCharacter(str) {
-    let charMap = {};
+function findMajorityElement(arr) {
+    let map = new Map();
     
-    for (let i = 0; i < str.length; i++) {
-        let char = str[i];
-        
-        if (charMap[char]) {
-            return char;
+    for (let num of arr) {
+        if (map.has(num)) {
+            map.set(num, map.get(num) + 1);
         } else {
-            charMap[char] = true;
+            map.set(num, 1);
         }
     }
     
-    return null;
+    let majorityElement = null;
+    let majorityCount = 0;
+    
+    for (let [key, value] of map.entries()) {
+        if (value > arr.length / 2) {
+            majorityElement = key;
+            majorityCount = value;
+            break;
+        }
+    }
+    
+    if (majorityElement !== null) {
+        return majorityElement;
+    } else {
+        return "No majority element found";
+    }
 }
 
-let str = "Hello World";
-let repeatedChar = findFirstRepeatedCharacter(str);
-
-if (repeatedChar) {
-    console.log("The first repeated character is: " + repeatedChar);
-} else {
-    console.log("No repeated characters found in the string.");
-}
+let arr = [2, 2, 3, 4, 2, 2, 2];
+let majorityElement = findMajorityElement(arr);
+console.log("Majority Element: " + majorityElement);
