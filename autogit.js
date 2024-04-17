38885d7080e20307ddca56402e6a5cf25905028b
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:51:51 +0800

    4-17-2024 21:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,6 @@
-function countingSort(arr) {
-    const max = Math.max(...arr);
-    const countArray = new Array(max + 1).fill(0);
+let string1 = "Hello";
+let string2 = "World";
 
-    // Count occurrences of each element
-    for (let i = 0; i < arr.length; i++) {
-        countArray[arr[i]]++;
-    }
-
-    // Modify countArray to store actual position of elements
-    for (let i = 1; i < countArray.length; i++) {
-        countArray[i] += countArray[i - 1];
-    }
-
-    const sortedArray = new Array(arr.length);
-
-    // Build the sorted array
-    for (let i = arr.length - 1; i >= 0; i--) {
-        sortedArray[countArray[arr[i]] - 1] = arr[i];
-        countArray[arr[i]]--;
-    }
-
-    return sortedArray;
-}
-
-// Example usage
-const arr = [4, 2, 2, 8, 3, 3, 1];
-const sortedArr = countingSort(arr);
-console.log(sortedArr); // Output: [1, 2, 2, 3, 3, 4, 8]
+let concatenatedString = string1 + " " + string2;
+console.log(concatenatedString);
+Hello World
