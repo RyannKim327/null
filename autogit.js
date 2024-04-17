16456e5c0e2d0b147f9478198714b54fa0633d14
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:36:30 +0800

    4-17-2024 22:36 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,59 @@
-function burrowsWheelerTransform(input) {
-    // Create an array of all possible rotations of the input string
-    let rotations = [];
-    for (let i = 0; i < input.length; i++) {
-        rotations.push(input.slice(i) + input.slice(0, i));
+class TarjanSCC {
+    constructor(graph) {
+        this.graph = graph;
+        this.n = graph.length;
+        this.index = 0;
+        this.lowLink = new Array(this.n).fill(-1);
+        this.ids = new Array(this.n).fill(-1);
+        this.onStack = new Array(this.n).fill(false);
+        this.stack = [];
+        this.sccCount = 0;
+        this.sccs = [];
     }
 
-    // Sort the rotations lexicographically
-    rotations.sort();
-
-    // Extract the last characters of each rotation to form the transformed string
-    let transformedString = rotations.map(rotation => rotation.slice(-1)).join('');
-
-    // Find the index of the original input in the sorted rotations
-    let originalIndex = rotations.indexOf(input);
-
-    return { transformedString, originalIndex };
-}
-
-function inverseBurrowsWheelerTransform(transformedString, originalIndex) {
-    let table = new Array(transformedString.length);
-    for (let i = 0; i < table.length; i++) {
-        table[i] = { char: transformedString[i], index: i };
-    }
-
-    table.sort((a, b) => {
-        if (a.char === b.char) {
-            return a.index - b.index;
+    findSCCs() {
+        for (let i = 0; i < this.n; i++) {
+            if (this.ids[i] === -1) {
+                this.dfs(i);
+            }
         }
-        return a.char.localeCompare(b.char);
-    });
-
-    let output = '';
-    let current = table[originalIndex];
-    for (let i = 0; i < table.length; i++) {
-        output += current.char;
-        current = table[current.index];
+        return this.sccs;
     }
 
-    return output;
+    dfs(at) {
+        this.lowLink[at] = this.index;
+        this.ids[at] = this.index;
+        this.index++;
+        this.stack.push(at);
+        this.onStack[at] = true;
+
+        // Visit all neighbors
+        for (let to of this.graph[at]) {
+            if (this.ids[to] === -1) {
+                this.dfs(to);
+            }
+            if (this.onStack[to]) {
+                this.lowLink[at] = Math.min(this.lowLink[at], this.lowLink[to]);
+            }
+        }
+
+        if (this.lowLink[at] === this.ids[at]) {
+            // Start a new strongly connected component
+            let component = [];
+            let node = -1;
+            while (node !== at) {
+                node = this.stack.pop();
+                this.onStack[node] = false;
+                component.push(node);
+            }
+            this.sccs.push(component);
+            this.sccCount++;
+        }
+    }
 }
 
-// Usage example
-let inputString = 'banana';
-let { transformedString, originalIndex } = burrowsWheelerTransform(inputString);
-console.log('Transformed String:', transformedString);
-
-let originalString = inverseBurrowsWheelerTransform(transformedString, originalIndex);
-console.log('Original String:', originalString);
+// Example Usage
+const graph = [[1], [2], [0], [4], [3]];
+const tarjan = new TarjanSCC(graph);
+const sccs = tarjan.findSCCs();
+console.log(sccs);
