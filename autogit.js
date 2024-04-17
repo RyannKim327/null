e333565c39d07805c874303b015c0133461a6029
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:22:10 +0800

    4-17-2024 22:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,26 @@
-function reverseString(str) {
-    // Convert the string to an array of characters
-    var arr = str.split("");
-    
-    // Reverse the array
-    var reversedArr = arr.reverse();
-    
-    // Join the characters back together to create a new string
-    var reversedStr = reversedArr.join("");
-    
-    return reversedStr;
+function isAnagram(str1, str2) {
+    // Remove non-alphabetic characters and convert to lowercase
+    str1 = str1.replace(/[^a-zA-Z]/g, "").toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+    // Sort the characters of both strings
+    str1 = str1.split('').sort().join('');
+    str2 = str2.split('').sort().join('');
+
+    // Compare the two sorted strings
+    if (str1 === str2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-// Example usage
-var originalStr = "Hello, World!";
-var reversedStr = reverseString(originalStr);
-console.log(reversedStr); // Outputs: "!dlroW ,olleH"
+// Test the function
+const string1 = "Listen";
+const string2 = "Silent";
+if (isAnagram(string1, string2)) {
+    console.log(`${string1} and ${string2} are anagrams.`);
+} else {
+    console.log(`${string1} and ${string2} are not anagrams.`);
+}
+Listen and Silent are anagrams.
