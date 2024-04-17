6c31a95474288f0fa368249f7ac6354ef1dabf80
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:19:55 +0800

    4-17-2024 21:19 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,52 @@
-function largestPrimeFactor(number) {
-    let i = 2;
-    while (i <= number) {
-        if (number % i === 0) {
-            number /= i;
-        } else {
-            i++;
-        }
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
     }
-    return i;
 }
 
-const number = 600851475143; // The number you want to find the largest prime factor of
-const largestFactor = largestPrimeFactor(number);
-console.log(`The largest prime factor of ${number} is ${largestFactor}`);
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                node.children[char] = new TrieNode();
+            }
+            node = node.children[char];
+        }
+        node.isEndOfWord = true;
+    }
+
+    search(word) {
+        let node = this.root;
+        for (let char of word) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return node.isEndOfWord;
+    }
+
+    startsWith(prefix) {
+        let node = this.root;
+        for (let char of prefix) {
+            if (!node.children[char]) {
+                return false;
+            }
+            node = node.children[char];
+        }
+        return true;
+    }
+}
+
+// Example usage
+let trie = new Trie();
+trie.insert("apple");
+console.log(trie.search("apple"));   // Output: true
+console.log(trie.search("app"));     // Output: false
+console.log(trie.startsWith("app"));  // Output: true
