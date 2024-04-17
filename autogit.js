50a9cec3a7f9dd3d1b5c0e2bb29545b3acec3ebf
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:07:44 +0800

    4-17-2024 22:7 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,22 @@
-function findLongestIncreasingSubsequence(arr) {
-    const n = arr.length;
-    const dp = new Array(n).fill(1);
+function isPalindrome(str) {
+    let left = 0;
+    let right = str.length - 1;
 
-    for (let i = 1; i < n; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
-                dp[i] = dp[j] + 1;
-            }
+    while (left < right) {
+        if (str[left] !== str[right]) {
+            return false;
         }
+        left++;
+        right--;
     }
 
-    let maxLength = 0;
-    for (let len of dp) {
-        if (len > maxLength) {
-            maxLength = len;
-        }
-    }
-
-    const result = [];
-    let endIndex = dp.indexOf(maxLength);
-
-    while (endIndex >= 0) {
-        result.unshift(arr[endIndex]);
-        maxLength--;
-        
-        for (let i = 0; i < endIndex; i++) {
-            if (dp[i] === maxLength && arr[i] < arr[endIndex]) {
-                endIndex = i;
-                break;
-            }
-        }
-    }
-
-    return result;
+    return true;
 }
 
-const arr = [3, 10, 2, 1, 20];
-console.log(findLongestIncreasingSubsequence(arr)); // Output: [3, 10, 20]
+// Test the function
+const word = "racecar";
+if (isPalindrome(word)) {
+    console.log("'" + word + "' is a palindrome.");
+} else {
+    console.log("'" + word + "' is not a palindrome.");
+}
