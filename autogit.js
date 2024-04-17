5468bdc8fcc667d9b657cbf624757b0f4347bea4
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:27:04 +0800

    4-17-2024 21:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,11 @@
-function isPrime(num) {
-    if (num < 2) return false;
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    return true;
-}
+let numbers = [5, 2, 9, 1, 5, 6];
+numbers.sort(function(a, b) {
+    return a - b;
+});
 
-function largestPrimeFactor(number) {
-    let largestPrime = 2;
+console.log(numbers);
+numbers.sort(function(a, b) {
+    return b - a;
+});
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {
-            let factor = number / i;
-            
-            if (isPrime(factor)) {
-                largestPrime = factor;
-                break;
-            }
-            
-            if (isPrime(i)) {
-                largestPrime = i;
-            }
-        }
-    }
-
-    return largestPrime;
-}
-
-// Example usage
-const number = 13195;
-console.log(`Largest prime factor of ${number} is: ${largestPrimeFactor(number)}`);
+console.log(numbers);
