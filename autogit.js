319a0e369d5d8eead30216fa395b35abe31fced3
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:18:26 +0800

    4-17-2024 22:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,18 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
-function findNthNodeFromEnd(head, n) {
-  let leftPointer = head;
-  let rightPointer = head;
-
-  // Move rightPointer forward by n steps
-  for (let i = 0; i < n; i++) {
-    if (rightPointer === null) {
-      return null; // Indicates that n is greater than the number of nodes in the list
+function bubbleSort(arr) {
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    rightPointer = rightPointer.next;
-  }
-
-  // Move both pointers simultaneously
-  while (rightPointer !== null) {
-    leftPointer = leftPointer.next;
-    rightPointer = rightPointer.next;
-  }
-
-  // leftPointer now points to the nth node from the end of the list
-  return leftPointer.value;
+    return arr;
 }
 
-// Example linked list: 1 -> 2 -> 3 -> 4 -> 5
-let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-head.next.next.next = new Node(4);
-head.next.next.next.next = new Node(5);
-
-// Find the 2nd node from the end
-console.log(findNthNodeFromEnd(head, 2)); // Output: 4
+let arr = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original Array: " + arr);
+console.log("Sorted Array: " + bubbleSort(arr));
