9d9c44869deb33801c765a92324ed5650c462729
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:18:44 +0800

    4-17-2024 21:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,3 @@
-function isPrime(num) {
-    if (num <= 1) {
-        return false;
-    }
-    
-    if (num <= 3) {
-        return true;
-    }
-    
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
-            return false;
-        }
-    }
-    
-    return true;
-}
-
-// Test the function
-const number = 17;
-if (isPrime(number)) {
-    console.log(`${number} is a prime number.`);
-} else {
-    console.log(`${number} is not a prime number.`);
-}
+let array = [1, 2, 3, 4, 5];
+array.reverse();
+console.log(array); // Output: [5, 4, 3, 2, 1]
