5c1a4efc003354df3df9cff9b82da3434ce3a511
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:27:11 +0800

    4-17-2024 21:27 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,22 @@
-let numbers = [5, 2, 9, 1, 5, 6];
-numbers.sort(function(a, b) {
-    return a - b;
-});
+function longestCommonPrefix(strings) {
+    if (strings.length === 0) return "";
+    
+    let prefix = "";
+    let firstStr = strings[0];
+    
+    for (let i = 0; i < firstStr.length; i++) {
+        let char = firstStr[i];
+        for (let j = 1; j < strings.length; j++) {
+            if (strings[j][i] !== char) {
+                return prefix;
+            }
+        }
+        prefix += char;
+    }
+    
+    return prefix;
+}
 
-console.log(numbers);
-numbers.sort(function(a, b) {
-    return b - a;
-});
-
-console.log(numbers);
+// Example usage
+let strings = ["flower", "flour", "flight"];
+console.log(longestCommonPrefix(strings)); // Output: "fl"
