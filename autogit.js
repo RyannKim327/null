4af6cb36ada9292ec433da9ffa419eb06830e7e0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:30:57 +0800

    4-17-2024 21:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,68 @@
-function isPalindrome(str) {
-    // Remove non-alphanumeric characters and convert to lowercase
-    str = str.toLowerCase().replace(/[\W_]/g, '');
-    
-    // Compare the string with its reverse
-    return str === str.split('').reverse().join('');
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-// Example usage
-const str1 = 'A man, a plan, a canal, Panama';
-console.log(isPalindrome(str1)); // Output: true
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
 
-const str2 = 'hello';
-console.log(isPalindrome(str2)); // Output: false
+  insert(data) {
+    const newNode = new Node(data);
+
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  search(data) {
+    return this.searchNode(this.root, data);
+  }
+
+  searchNode(node, data) {
+    if (node === null) {
+      return false;
+    }
+
+    if (data < node.data) {
+      return this.searchNode(node.left, data);
+    } else if (data > node.data) {
+      return this.searchNode(node.right, data);
+    } else {
+      return true;
+    }
+  }
+}
+
+// Example Usage
+const bst = new BinarySearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log(bst.search(7));  // Output: true
+console.log(bst.search(20)); // Output: false
