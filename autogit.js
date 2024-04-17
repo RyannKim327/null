a99ef48873dbfbe96a3ee7e76393ac6474614940
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:56:13 +0800

    4-17-2024 21:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,15 @@
-function isSorted(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < arr[i - 1]) {
-            return false;
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
         }
     }
-    return true;
+    return count;
 }
 
-function shuffle(arr) {
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-}
-
-function bogosort(arr) {
-    while (!isSorted(arr)) {
-        shuffle(arr);
-    }
-    return arr;
-}
-
-let array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-console.log("Before sorting:", array);
-array = bogosort(array);
-console.log("After sorting:", array);
+// Example usage
+const myString = "hello, world!";
+const myChar = "l";
+const occurrences = countOccurrences(myString, myChar);
+console.log(`The character '${myChar}' occurs ${occurrences} times in the string.`);
