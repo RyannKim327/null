7f373095294f7d72b8c846aa384bcc05bc939b40
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:37:18 +0800

    4-17-2024 22:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,76 @@
-class ListNode {
+class Node {
     constructor(value) {
-        this.val = value;
-        this.next = null;
+        this.value = value;
+        this.next = [];
     }
 }
 
-function findMiddleElement(head) {
-    let slowPtr = head;
-    let fastPtr = head;
-
-    while (fastPtr !== null && fastPtr.next !== null) {
-        slowPtr = slowPtr.next;
-        fastPtr = fastPtr.next.next;
+class SkipList {
+    constructor(maxLevel, probability) {
+        this.maxLevel = maxLevel;
+        this.head = new Node(-Infinity);
+        this.probability = probability;
     }
 
-    return slowPtr.val;
+    getRandomLevel() {
+        let level = 1;
+        while (Math.random() < this.probability && level < this.maxLevel) {
+            level++;
+        }
+        return level;
+    }
+
+    insert(value) {
+        let update = [];
+        let current = this.head;
+
+        for (let i = this.maxLevel - 1; i >= 0; i--) {
+            while (current.next[i] && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            update[i] = current;
+        }
+
+        let level = this.getRandomLevel();
+        let newNode = new Node(value);
+        
+        for (let i = 0; i < level; i++) {
+            newNode.next[i] = update[i].next[i];
+            update[i].next[i] = newNode;
+        }
+    }
+
+    search(value) {
+        let current = this.head;
+        for (let i = this.maxLevel - 1; i >= 0; i--) {
+            while (current.next[i] && current.next[i].value < value) {
+                current = current.next[i];
+            }
+        }
+
+        current = current.next[0];
+        return current && current.value === value ? current : null;
+    }
+
+    delete(value) {
+        let update = [];
+        let current = this.head;
+
+        for (let i = this.maxLevel - 1; i >= 0; i--) {
+            while (current.next[i] && current.next[i].value < value) {
+                current = current.next[i];
+            }
+            update[i] = current;
+        }
+
+        current = current.next[0];
+
+        if (current && current.value === value) {
+            for (let i = 0; i < this.maxLevel; i++) {
+                if (update[i].next[i] === current) {
+                    update[i].next[i] = current.next[i];
+                }
+            }
+        }
+    }
 }
-
-// Create a sample linked list
-let head = new ListNode(1);
-head.next = new ListNode(2);
-head.next.next = new ListNode(3);
-head.next.next.next = new ListNode(4);
-head.next.next.next.next = new ListNode(5);
-
-console.log(findMiddleElement(head)); // Output: 3
