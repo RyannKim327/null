d2cc96f1d45ab89e9060a02f5f17450555dd6b51
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:26:35 +0800

    4-17-2024 21:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,10 @@
-function longestCommonSubsequence(str1, str2) {
-    const m = str1.length;
-    const n = str2.length;
-    
-    // Create a matrix to store the lengths of longest common subsequences of substrings
-    const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
-    
-    // Fill the matrix based on the characters in the strings
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (str1[i - 1] === str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-    
-    // Reconstruct the longest common subsequence
-    let lcs = "";
-    let i = m, j = n;
-    while (i > 0 && j > 0) {
-        if (str1[i - 1] === str2[j - 1]) {
-            lcs = str1[i - 1] + lcs;
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-    
-    return lcs;
+function countOccurrences(str, word) {
+    const regex = new RegExp(word, 'g');
+    const matches = str.match(regex);
+    return matches ? matches.length : 0;
 }
 
-// Example
-const str1 = "ABCDGH";
-const str2 = "AEDFHR";
-const lcs = longestCommonSubsequence(str1, str2);
-console.log("Longest Common Subsequence: ", lcs);
+const str = 'Hello world! Hello everyone!';
+const word = 'Hello';
+const count = countOccurrences(str, word);
+console.log(`The word "${word}" appears ${count} times in the string.`);
