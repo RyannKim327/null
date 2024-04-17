b038a3d295435022946a8c07f7aac29d5f228f3f
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:19:00 +0800

    4-17-2024 22:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,6 @@
-function preprocessPattern(pattern) {
-    const table = new Array(256).fill(pattern.length);
-    
-    for (let i = 0; i < pattern.length - 1; i++) {
-        table[pattern.charCodeAt(i)] = pattern.length - 1 - i;
-    }
-    
-    return table;
-}
-function boyerMooreHorspool(text, pattern) {
-    const table = preprocessPattern(pattern);
-    const patternLength = pattern.length;
-    const textLength = text.length;
-    
-    let i = 0;
-    
-    while (i <= textLength - patternLength) {
-        let j = patternLength - 1;
+let str = "  Hello,   World!  ";
 
-        while (j >= 0 && pattern[j] === text[i + j]) {
-            j--;
-        }
-        
-        if (j < 0) {
-            return i;
-        } else {
-            i += table[text.charCodeAt(i + patternLength - 1)];
-        }
-    }
-    
-    return -1; // pattern not found
-}
-const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
-const pattern = "consectetur";
+// Using regular expression to remove whitespace
+let newStr = str.replace(/\s/g, '');
 
-const index = boyerMooreHorspool(text, pattern);
-
-if (index !== -1) {
-    console.log(`Pattern found at index ${index}`);
-} else {
-    console.log("Pattern not found in text");
-}
+console.log(newStr); // Output: "Hello,World!"
