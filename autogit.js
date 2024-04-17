7a124b78160291d3d7e7bb9c48cc4a714cfbd1b3
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:29:24 +0800

    4-17-2024 21:29 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,5 @@
-function findFirstRepeatedCharacter(str) {
-    let charMap = {};
+const cron = require('node-cron');
 
-    for (let char of str) {
-        if (charMap[char]) {
-            return char;
-        } else {
-            charMap[char] = 1;
-        }
-    }
-
-    return null;
-}
-
-// Example usage
-const inputString = "hello world";
-const repeatedChar = findFirstRepeatedCharacter(inputString);
-
-if (repeatedChar) {
-    console.log(`The first repeated character in "${inputString}" is "${repeatedChar}".`);
-} else {
-    console.log(`No repeated characters found in "${inputString}".`);
-}
+cron.schedule('* * * * *', () => {
+    console.log('Running a task every minute!');
+});
