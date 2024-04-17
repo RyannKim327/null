cc66fde7e52c56311c151769ca62766a48b790b0
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:56:49 +0800

    4-17-2024 21:56 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,11 @@
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
+function validateEmail(email) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-  }
-
-  addNode(data) {
-    const newNode = new Node(data);
-    if (!this.head) {
-      this.head = newNode;
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newNode;
-    }
-  }
-
-  getLength() {
-    let current = this.head;
-    let count = 0;
-
-    while (current) {
-      count++;
-      current = current.next;
-    }
-
-    return count;
-  }
+const email = 'example@example.com';
+if (validateEmail(email)) {
+  console.log('Email is valid');
+} else {
+  console.log('Email is invalid');
 }
-
-// Usage
-const linkedList = new LinkedList();
-linkedList.addNode(1);
-linkedList.addNode(2);
-linkedList.addNode(3);
-
-console.log(linkedList.getLength()); // Output: 3
