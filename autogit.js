a659e47ce4d2f4b41133ba0ea9a601ef2ba518e5
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:08:46 +0800

    4-17-2024 22:8 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,21 @@
-class HashTable {
-    constructor() {
-        this.table = {};
-    }
-    
-    // Function to generate a hash key
-    hashCode(key) {
-        let hash = 0;
-        for (let i = 0; i < key.length; i++) {
-            hash = (hash + key.charCodeAt(i) * 31) % 1013; // prime number to reduce collisions
-        }
-        return hash;
-    }
-    
-    // Function to insert a key-value pair into the hash table
-    put(key, value) {
-        const hashedKey = this.hashCode(key);
-        this.table[hashedKey] = value;
-    }
-    
-    // Function to get the value associated with a key in the hash table
-    get(key) {
-        const hashedKey = this.hashCode(key);
-        return this.table[hashedKey];
-    }
-    
-    // Function to check if a key exists in the hash table
-    contains(key) {
-        const hashedKey = this.hashCode(key);
-        return this.table.hasOwnProperty(hashedKey);
-    }
-    
-    // Function to remove a key-value pair from the hash table
-    remove(key) {
-        const hashedKey = this.hashCode(key);
-        delete this.table[hashedKey];
-    }
+function areAnagrams(str1, str2) {
+    // Remove non-alphabetic characters and convert to lowercase
+    str1 = str1.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    str2 = str2.replace(/[^a-zA-Z]/g, '').toLowerCase();
+
+    // Sort the characters of both strings
+    const sortedStr1 = str1.split('').sort().join('');
+    const sortedStr2 = str2.split('').sort().join('');
+
+    // Compare the sorted strings
+    return sortedStr1 === sortedStr2;
 }
-const myHashTable = new HashTable();
 
-myHashTable.put('name', 'Alice');
-myHashTable.put('age', 30);
-
-console.log(myHashTable.get('name')); // Output: Alice
-
-console.log(myHashTable.contains('age')); // Output: true
-
-myHashTable.remove('age');
-
-console.log(myHashTable.contains('age')); // Output: false
+// Test the function
+const str1 = "listen";
+const str2 = "silent";
+if (areAnagrams(str1, str2)) {
+    console.log(`${str1} and ${str2} are anagrams.`);
+} else {
+    console.log(`${str1} and ${str2} are not anagrams.`);
+}
