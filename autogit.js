bb77d6aa7d6a2cae99d028dbf5404bbaf8d704f5
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:20:20 +0800

    4-17-2024 22:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,63 @@
-let str = "Hello World";
-let lowerCaseStr = str.toLowerCase();
-console.log(lowerCaseStr); // Output: "hello world"
+class Graph {
+  constructor() {
+    this.vertices = {};
+  }
+
+  addVertex(vertex) {
+    this.vertices[vertex] = [];
+  }
+  
+  addEdge(vertex1, vertex2) {
+    this.vertices[vertex1].push(vertex2);
+    this.vertices[vertex2].push(vertex1);
+  }
+}
+
+function biDirectionalSearch(graph, start, goal) {
+  let visitedFromStart = new Set();
+  let visitedFromGoal = new Set();
+  let queueStart = [start];
+  let queueGoal = [goal];
+  
+  while (queueStart.length > 0 && queueGoal.length > 0) {
+    let currVertexStart = queueStart.shift();
+    let currVertexGoal = queueGoal.shift();
+    
+    visitedFromStart.add(currVertexStart);
+    visitedFromGoal.add(currVertexGoal);
+    
+    if (visitedFromGoal.has(currVertexStart) || visitedFromStart.has(currVertexGoal)) {
+      // Path found
+      return true;
+    }
+    
+    for (let neighbor of graph.vertices[currVertexStart]) {
+      if (!visitedFromStart.has(neighbor)) {
+        queueStart.push(neighbor);
+      }
+    }
+    
+    for (let neighbor of graph.vertices[currVertexGoal]) {
+      if (!visitedFromGoal.has(neighbor)) {
+        queueGoal.push(neighbor);
+      }
+    }
+  }
+  
+  return false;
+}
+
+// Example usage
+let graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addEdge('A', 'B');
+graph.addEdge('B', 'C');
+graph.addEdge('C', 'D');
+
+let start = 'A';
+let goal = 'D';
+let pathExists = biDirectionalSearch(graph, start, goal);
+console.log(pathExists); // Output should be true
