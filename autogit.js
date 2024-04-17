5da5df60c61c8ed8b83f0bcd08a323b607f6569a
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:18:53 +0800

    4-17-2024 21:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,32 @@
-let array = [1, 2, 3, 4, 5];
-array.reverse();
-console.log(array); // Output: [5, 4, 3, 2, 1]
+function largestPrimeFactor(number) {
+    if (number <= 1) {
+        return null;
+    }
+
+    let largestFactor = 1;
+
+    // Find and remove all divisors of 2
+    while (number % 2 === 0) {
+        largestFactor = 2;
+        number /= 2;
+    }
+
+    // Find and remove all other prime factors
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
+        while (number % i === 0) {
+            largestFactor = i;
+            number /= i;
+        }
+    }
+
+    // If the remaining number is a prime greater than 2
+    if (number > 2) {
+        largestFactor = number;
+    }
+
+    return largestFactor;
+}
+
+// Test the function with a number
+const number = 13195;
+console.log(largestPrimeFactor(number)); // Output: 29
