910a058c02e778e94f58bb5d8f20e5fc33da3d04
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:26:51 +0800

    4-17-2024 21:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,46 @@
-function countOccurrences(str, char) {
-    let count = 0;
-
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === char) {
-            count++;
-        }
+// Node class to create individual nodes in the linked list
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-
-    return count;
 }
 
-// Example usage
-let myString = "hello world";
-let charToCount = "l";
-let occurrences = countOccurrences(myString, charToCount);
-console.log(`The character "${charToCount}" occurs ${occurrences} times in the string "${myString}"`);
+// Linked List class to create the linked list
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to add a node to the end of the linked list
+    append(data) {
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to print all the nodes in the linked list
+    printList() {
+        let current = this.head;
+        while (current) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Example of how to use the Linked List class
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+
+linkedList.printList();
