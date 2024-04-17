a06882fe7d035d6b8fe31b5689dd997918cbf64d
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:22:37 +0800

    4-17-2024 21:22 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,43 +1,19 @@
-function findKthSmallest(arr, k) {
-    if (k < 1 || k > arr.length) {
-        return null;
+function findMedianSortedArrays(nums1, nums2) {
+    const combinedArray = nums1.concat(nums2).sort((a, b) => a - b);
+    const length = combinedArray.length;
+
+    if (length % 2 === 0) {
+        // If the combined array length is even
+        const mid = length / 2;
+        return (combinedArray[mid - 1] + combinedArray[mid]) / 2;
+    } else {
+        // If the combined array length is odd
+        const mid = Math.floor(length / 2);
+        return combinedArray[mid];
     }
-
-    const partition = (arr, left, right) => {
-        const pivot = arr[right];
-        let i = left;
-
-        for (let j = left; j < right; j++) {
-            if (arr[j] < pivot) {
-                [arr[i], arr[j]] = [arr[j], arr[i]];
-                i++;
-            }
-        }
-
-        [arr[i], arr[right]] = [arr[right], arr[i]];
-        return i;
-    };
-
-    const quickSelect = (arr, left, right, k) => {
-        if (left === right) {
-            return arr[left];
-        }
-
-        const pivotIndex = partition(arr, left, right);
-
-        if (k === pivotIndex) {
-            return arr[k];
-        } else if (k < pivotIndex) {
-            return quickSelect(arr, left, pivotIndex - 1, k);
-        } else {
-            return quickSelect(arr, pivotIndex + 1, right, k);
-        }
-    };
-
-    return quickSelect(arr, 0, arr.length - 1, k - 1);
 }
 
-const arr = [3, 1, 4, 2, 5];
-const k = 2;
-const kthSmallest = findKthSmallest(arr, k);
-console.log(`The ${k}th smallest element in the array is: ${kthSmallest}`);
+const nums1 = [1, 3];
+const nums2 = [2];
+const median = findMedianSortedArrays(nums1, nums2);
+console.log(median);
