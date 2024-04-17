025c4b43d444c2bb15e4ec8159565b2c96f493b2
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:52:01 +0800

    4-17-2024 21:51 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,44 @@
-let string1 = "Hello";
-let string2 = "World";
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-let concatenatedString = string1 + " " + string2;
-console.log(concatenatedString);
-Hello World
+function hasCycle(head) {
+  if (!head || !head.next) {
+    return false;
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (fast && fast.next) {
+    if (slow === fast) {
+      return true;
+    }
+    
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
+}
+// Create a linked list with a cycle
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+node1.next = node2;
+node2.next = node3;
+node3.next = node1; // Cycle
+
+console.log(hasCycle(node1)); // Output: true
+
+// Create a linked list without a cycle
+let newNode1 = new Node(1);
+let newNode2 = new Node(2);
+let newNode3 = new Node(3);
+newNode1.next = newNode2;
+newNode2.next = newNode3;
+
+console.log(hasCycle(newNode1)); // Output: false
