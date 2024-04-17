fc914965b6f9e90cf49ad038e3801f828ed66b35
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:09:53 +0800

    4-17-2024 22:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,37 @@
-function burrowsWheelerTransform(input) {
-    let matrix = [];
-    
-    for (let i = 0; i < input.length; i++) {
-        matrix.push(input.slice(i) + input.slice(0, i));
-    }
-    
-    matrix.sort();
-    
-    let transformedString = "";
-    let index = 0;
-    
-    for (let i = 0; i < matrix.length; i++) {
-        transformedString += matrix[i][matrix[i].length - 1];
-        if (matrix[i] === input) {
-            index = i;
+function longestCommonSubsequence(str1, str2) {
+    const m = str1.length;
+    const n = str2.length;
+
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
         }
     }
-    
-    return {transformedString, index};
-}
 
-function burrowsWheelerInverseTransform(transformedString, index) {
-    let matrix = [];
-    
-    for (let i = 0; i < transformedString.length; i++) {
-        let temp = [];
-        for (let j = 0; j < transformedString.length; j++) {
-            temp[j] = transformedString[j];
+    let i = m, j = n;
+    let lcs = '';
+
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            lcs = str1[i - 1] + lcs;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
         }
-        matrix.push(temp);
-        transformedString = temp[0] + transformedString.slice(0, -1);
     }
-    
-    matrix.sort();
-    
-    let originalString = "";
-    
-    for (let i = 0; i < matrix.length; i++) {
-        originalString += matrix[i][index];
-    }
-    
-    return originalString;
+
+    return lcs;
 }
 
-// Example
-let inputString = "hello world";
-let {transformedString, index} = burrowsWheelerTransform(inputString);
-let originalString = burrowsWheelerInverseTransform(transformedString, index);
-
-console.log("Input String:", inputString);
-console.log("Transformed String:", transformedString);
-console.log("Original String:", originalString);
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+console.log(longestCommonSubsequence(str1, str2)); // Output: "ADH"
