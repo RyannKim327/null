ec8877638566af0adcb1ac9a6ef2417335a8c568
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:00:36 +0800

    4-17-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,8 @@
-function areAnagrams(str1, str2) {
-    const sanitizeString = (str) => str.toLowerCase().replace(/[^a-z]/g, '');
-  
-    const sortedStr1 = sanitizeString(str1).split('').sort().join('');
-    const sortedStr2 = sanitizeString(str2).split('').sort().join('');
-  
-    return sortedStr1 === sortedStr2;
-}
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = Array.from(new Set(arrayWithDuplicates));
 
-// Test the function
-const string1 = "listen";
-const string2 = "silent";
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = arrayWithDuplicates.filter((item, index) => arrayWithDuplicates.indexOf(item) === index);
 
-if (areAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
