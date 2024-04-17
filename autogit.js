12ecde4c34e665443b379fbad21d3427b6ced443
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:06:33 +0800

    4-17-2024 22:6 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,15 @@
-function checkAnagrams(str1, str2) {
-    // Remove special characters and spaces, convert to lowercase
-    str1 = str1.replace(/[^\w]/g, '').toLowerCase();
-    str2 = str2.replace(/[^\w]/g, '').toLowerCase();
-
-    // Check if the lengths of the strings are the same after removing spaces and special characters
-    if (str1.length !== str2.length) {
-        return false;
+function countOccurrences(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++;
+        }
     }
-    
-    // Convert strings to arrays, sort them, and compare
-    const sortedStr1 = str1.split('').sort().join('');
-    const sortedStr2 = str2.split('').sort().join('');
-
-    return sortedStr1 === sortedStr2;
+    return count;
 }
 
-// Example usage
-const string1 = 'listen';
-const string2 = 'silent';
-if (checkAnagrams(string1, string2)) {
-    console.log(`${string1} and ${string2} are anagrams.`);
-} else {
-    console.log(`${string1} and ${string2} are not anagrams.`);
-}
+let inputString = "hello world";
+let characterToCount = "l";
+let occurrences = countOccurrences(inputString, characterToCount);
+
+console.log(`The character '${characterToCount}' appears ${occurrences} times in the string.`);
