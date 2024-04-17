a7e09735732439f1e65506212179940aaca7c4d8
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 22:00:53 +0800

    4-17-2024 22:0 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,26 @@
-const axios = require('axios');
+function binarySearchRecursive(array, target, start, end) {
+    if (start > end) {
+        return -1;
+    }
 
-// Make a GET request to a URL
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    const middle = Math.floor((start + end) / 2);
+
+    if (array[middle] === target) {
+        return middle;
+    } else if (array[middle] < target) {
+        return binarySearchRecursive(array, target, middle + 1, end);
+    } else {
+        return binarySearchRecursive(array, target, start, middle - 1);
+    }
+}
+
+// Example usage:
+const sortedArray = [1, 3, 5, 7, 9, 11, 13, 15];
+const target = 7;
+const index = binarySearchRecursive(sortedArray, target, 0, sortedArray.length - 1);
+
+if (index !== -1) {
+    console.log(`Element found at index: ${index}`);
+} else {
+    console.log("Element not found");
+}
