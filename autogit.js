da6a8f3c8fcfcf482c5e41702e6f1e99764b0315
RyannKim327 <rksesgundo123@gmail.com>
2024-04-17 21:54:09 +0800

    4-17-2024 21:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,38 @@
-const axios = require('axios');
+function generateFibonacci(n) {
+    let fib = [0, 1];
+    while (fib[fib.length - 1] < n) {
+        fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+    }
+    return fib;
+}
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+function fibonacciSearch(arr, key) {
+    const n = arr.length;
+    let fib = generateFibonacci(n);
+    let offset = 0;
+    
+    for (let i = fib.length - 1; i > 1; i--) {
+        const index = Math.min(offset + fib[i - 2], n - 1);
+        
+        if (arr[index] < key) {
+            offset = index;
+        } else if (arr[index] > key) {
+            // Do nothing, continue with the loop
+        } else {
+            return index;
+        }
+    }
+    
+    return -1; // Element not found
+}
+
+// Example usage
+const arr = [1, 3, 5, 8, 13, 21, 34, 55, 89];
+const key = 13;
+const index = fibonacciSearch(arr, key);
+
+if (index !== -1) {
+    console.log(`Element found at index ${index}`);
+} else {
+    console.log(`Element not found in the array`);
+}
