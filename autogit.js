aa0371f5560534a13b684a8e3546b4e589e2d058
RyannKim327 <rksesgundo123@gmail.com>
2024-08-24 20:31:44 +0800

    8-24-2024 20:31 [User mode]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-let arr = [1, 2, 3, 4, 5];
-
-for (let i = 0; i < Math.floor(arr.length / 2); i++) {
-    let temp = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
-}
-
-console.log(arr); // Output: [5, 4, 3, 2, 1]
