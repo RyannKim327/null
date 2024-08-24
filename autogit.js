05da92c71b880fc17fe15db1d2a96f98188fe149
RyannKim327 <rksesgundo123@gmail.com>
2024-08-24 20:16:11 +0800

    8-24-2024 20:16 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,11 @@
-import tweepy
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
 
-# Twitter API credentials
-consumer_key = 'YOUR_CONSUMER_KEY'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
-
-# Authenticate with Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
-# Retrieve and display latest tweets
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+number = 56
+print(f"The largest prime factor of {number} is: {largest_prime_factor(number)}")
