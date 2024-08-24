f8983a6cd4816653f5329979e3084691cb3b9149
RyannKim327 <rksesgundo123@gmail.com>
2024-08-24 20:15:34 +0800

    8-24-2024 20:15 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,17 @@
-import schedule
-import time
+import tweepy
 
-def job():
-    print("Task is running...")
+# Twitter API credentials
+consumer_key = 'YOUR_CONSUMER_KEY'
+consumer_secret = 'YOUR_CONSUMER_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
 
-schedule.every().day.at("08:00").do(job)
-schedule.every().wednesday.at("13:00").do(job)
+# Authenticate with Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Retrieve and display latest tweets
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
