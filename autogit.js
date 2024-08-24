89bacabd901e0b98d3fcce8d361139375e50384c
RyannKim327 <rksesgundo123@gmail.com>
2024-08-24 20:17:26 +0800

    8-24-2024 20:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,28 @@
-def calculate_mean(numbers):
-    sum_numbers = sum(numbers)
-    mean = sum_numbers / len(numbers)
-    return mean
+import heapq
 
-# Example list of numbers
-numbers = [1, 2, 3, 4, 5]
-mean_value = calculate_mean(numbers)
-print("Mean:", mean_value)
+def beam_search(start_state, get_next_states, evaluate_state, beam_width):
+    beam = [(start_state, 0)]
+    
+    while True:
+        next_beam = []
+        
+        for state, score in beam:
+            for next_state in get_next_states(state):
+                next_score = evaluate_state(next_state)
+                heapq.heappush(next_beam, (next_state, score + next_score))
+                
+        beam = heapq.nlargest(beam_width, next_beam, key=lambda x: x[1])
+        
+        if all([evaluate_state(state) == 0 for state, _ in beam]) or len(beam) == 0:
+            break
+            
+    return beam
+
+# Example usage
+start_state = ...
+get_next_states = ...
+evaluate_state = ...
+beam_width = 3
+
+result = beam_search(start_state, get_next_states, evaluate_state, beam_width)
+print(result)
