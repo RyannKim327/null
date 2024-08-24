70e4fc948368f2feb759063b6ebab7c293cb2976
RyannKim327 <rksesgundo123@gmail.com>
2024-08-24 20:23:30 +0800

    8-24-2024 20:23 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,40 +1,12 @@
-def compute_lps_array(pattern):
-    j = 0
-    lps = [0] * len(pattern)
-    i = 1
-    while i < len(pattern):
-        if pattern[i] == pattern[j]:
-            j += 1
-            lps[i] = j
-            i += 1
-        else:
-            if j != 0:
-                j = lps[j-1]
-            else:
-                lps[i] = 0
-                i += 1
-    return lps
-
-def kmp_search(text, pattern):
-    lps = compute_lps_array(pattern)
-    i = 0  # index for text
-    j = 0  # index for pattern
-    while i < len(text):
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
-
-        if j == len(pattern):
-            print("Pattern found at index " + str(i - j))
-            j = lps[j - 1]
-
-        elif i < len(text) and text[i] != pattern[j]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
+def is_sorted_ascending(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i + 1]:
+            return False
+    return True
 
 # Example usage
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-kmp_search(text, pattern)
+arr = [1, 2, 3, 4, 5]
+if is_sorted_ascending(arr):
+    print("The array is sorted in ascending order.")
+else:
+    print("The array is not sorted in ascending order.")
