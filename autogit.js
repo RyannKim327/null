2f1407f19e0f4062564784a61625d3ca1163c336
RyannKim327 <rksesgundo123@gmail.com>
2024-09-06 21:03:40 +0800

    9-6-2024 21:3 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,24 @@
-// Define an array
-let array = [1, 2, 3, 4, 5];
+def first_non_repeating_char(s):
+    char_count = {}
+    
+    # Count the frequency of each character
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+            
+    # Find the first non-repeating character
+    for char in s:
+        if char_count[char] == 1:
+            return char
+    
+    return None
 
-// Find the index of the element you want to remove
-let index = array.indexOf(3);
-
-// Remove the element at that index
-if (index > -1) {
-    array.splice(index, 1);
-}
-
-console.log(array); // Output: [1, 2, 4, 5]
+# Test the function
+string = "hello world"
+result = first_non_repeating_char(string)
+if result:
+    print("The first non-repeating character is:", result)
+else:
+    print("No non-repeating characters found in the string.")
