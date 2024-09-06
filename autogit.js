1554a0f84f37a60e84175a343e6325c0a062cd2e
RyannKim327 <rksesgundo123@gmail.com>
2024-09-07 07:20:34 +0800

    9-7-2024 7:20 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,53 @@
-def find_string_length(input_string):
-    count = 0
-    for char in input_string:
-        count += 1
-    return count
+#include <iostream>
 
-input_string = "Hello, World!"
-length = find_string_length(input_string)
-print("Length of the string:", length)
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+    
+public:
+    LinkedList() : head(nullptr) {}
+    
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    
+    void display() {
+        Node* current = head;
+        while (current != nullptr) {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+    
+    list.display();
+
+    return 0;
+}
