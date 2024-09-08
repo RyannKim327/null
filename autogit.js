37355ea81a37f72e2da80cc9c586d1738719dc71
RyannKim327 <rksesgundo123@gmail.com>
2024-09-08 23:19:00 +0800

    9-8-2024 23:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,30 @@
-def reverse_words_order(sentence):
-    words = sentence.split()
-    reversed_sentence = ' '.join(reversed(words))
-    return reversed_sentence
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-# Example
-input_sentence = "Hello world how are you"
-reversed_sentence = reverse_words_order(input_sentence)
-print(reversed_sentence)
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a max heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is:", arr)
