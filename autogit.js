b0056f75f71489fa05ae7c08e0da0fcbb4ac5d33
RyannKim327 <rksesgundo123@gmail.com>
2024-09-09 05:47:58 +0800

    9-9-2024 5:47 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,13 @@
-def heapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-    if left < n and arr[left] > arr[largest]:
-        largest = left
-
-    if right < n and arr[right] > arr[largest]:
-        largest = right
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    # Build a max heap
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    # Extract elements one by one
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-arr = [12, 11, 13, 5, 6, 7]
-heap_sort(arr)
+# Example usage
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
 print("Sorted array is:", arr)
