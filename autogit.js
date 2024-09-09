912aeeb338548dd8836d2bcb2d6eb2d2d23d66cf
RyannKim327 <rksesgundo123@gmail.com>
2024-09-09 20:40:45 +0800

    9-9-2024 20:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,14 @@
-from collections import defaultdict
+import re
 
-def topological_sort(graph):
-    in_degree = {node: 0 for node in graph}
-    for node in graph:
-        for neighbor in graph[node]:
-            in_degree[neighbor] += 1
-
-    zero_in_degree_nodes = [node for node in graph if in_degree[node] == 0]
-
-    top_order = []
-    while zero_in_degree_nodes:
-        node = zero_in_degree_nodes.pop()
-        top_order.append(node)
-        for neighbor in graph[node]:
-            in_degree[neighbor] -= 1
-            if in_degree[neighbor] == 0:
-                zero_in_degree_nodes.append(neighbor)
-
-    if len(top_order) == len(graph):
-        return top_order
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(pattern, email):
+        return True
     else:
-        return []
+        return False
 
-# Example of using the topological sort algorithm
-graph = {
-    'A': ['C'],
-    'B': ['C', 'D'],
-    'C': ['E'],
-    'D': ['F'],
-    'E': ['F'],
-    'F': []
-}
-
-print(topological_sort(graph))
+email = "example@email.com"
+if validate_email(email):
+    print("Email is valid.")
+else:
+    print("Email is invalid.")
