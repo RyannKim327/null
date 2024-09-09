4463d2cf887dce07b5b016e05e8699bb41f664a6
RyannKim327 <rksesgundo123@gmail.com>
2024-09-09 20:40:26 +0800

    9-9-2024 20:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -5,30 +5,31 @@ def topological_sort(graph):
     for node in graph:
         for neighbor in graph[node]:
             in_degree[neighbor] += 1
-    
-    queue = [node for node in in_degree if in_degree[node] == 0]
-    result = []
-    
-    while queue:
-        node = queue.pop(0)
-        result.append(node)
+
+    zero_in_degree_nodes = [node for node in graph if in_degree[node] == 0]
+
+    top_order = []
+    while zero_in_degree_nodes:
+        node = zero_in_degree_nodes.pop()
+        top_order.append(node)
         for neighbor in graph[node]:
             in_degree[neighbor] -= 1
             if in_degree[neighbor] == 0:
-                queue.append(neighbor)
-    
-    if len(result) == len(graph):
-        return result
-    else:
-        print("Graph has a cycle")
+                zero_in_degree_nodes.append(neighbor)
 
-# Example usage:
+    if len(top_order) == len(graph):
+        return top_order
+    else:
+        return []
+
+# Example of using the topological sort algorithm
 graph = {
-    'A': ['C', 'B'],
+    'A': ['C'],
     'B': ['C', 'D'],
     'C': ['E'],
-    'D': ['E'],
-    'E': []
+    'D': ['F'],
+    'E': ['F'],
+    'F': []
 }
 
 print(topological_sort(graph))
