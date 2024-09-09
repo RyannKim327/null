d23e2f2a546ccbf7769346317b544f28d8b77d69
RyannKim327 <rksesgundo123@gmail.com>
2024-09-09 20:40:17 +0800

    9-9-2024 20:40 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,34 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+from collections import defaultdict
 
-# Test the quicksort function
-arr = [3, 6, 8, 10, 1, 2, 1]
-print(quicksort(arr))
+def topological_sort(graph):
+    in_degree = {node: 0 for node in graph}
+    for node in graph:
+        for neighbor in graph[node]:
+            in_degree[neighbor] += 1
+    
+    queue = [node for node in in_degree if in_degree[node] == 0]
+    result = []
+    
+    while queue:
+        node = queue.pop(0)
+        result.append(node)
+        for neighbor in graph[node]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+    
+    if len(result) == len(graph):
+        return result
+    else:
+        print("Graph has a cycle")
+
+# Example usage:
+graph = {
+    'A': ['C', 'B'],
+    'B': ['C', 'D'],
+    'C': ['E'],
+    'D': ['E'],
+    'E': []
+}
+
+print(topological_sort(graph))
