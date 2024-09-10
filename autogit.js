76c8a9a2c6e716f43c90cd948fea8fa446bb0a2d
RyannKim327 <rksesgundo123@gmail.com>
2024-09-11 04:48:15 +0800

    9-11-2024 4:48 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,10 @@
-def heapify(arr, n, i):
-    largest = i
-    l = 2 * i + 1
-    r = 2 * i + 2
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
     
-    if l < n and arr[i] < arr[l]:
-        largest = l
-        
-    if r < n and arr[largest] < arr[r]:
-        largest = r
-        
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
-def heapSort(arr):
-    n = len(arr)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
     
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-        
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-arr = [12, 11, 13, 5, 6, 7]
-heapSort(arr)
-print("Sorted array is:", arr)
+    return visited
