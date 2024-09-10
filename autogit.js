4bde6824da6eb7bd30590e04c22e305ceef6f58c
RyannKim327 <rksesgundo123@gmail.com>
2024-09-10 22:28:20 +0800

    9-10-2024 22:28 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,14 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+def findMedianSortedArrays(nums1, nums2):
+    m = len(nums1)
+    n = len(nums2)
+    nums = sorted(nums1 + nums2)
 
-// Node definition for the linked list
-struct Node {
-    int data;
-    Node* next;
-    Node(int val) : data(val), next(nullptr) {}
-};
+    if (m + n) % 2 == 0:
+        return (nums[(m + n) // 2 - 1] + nums[(m + n) // 2]) / 2
+    else:
+        return nums[(m + n) // 2]
 
-// Function to check if a linked list is a palindrome
-bool isPalindrome(Node* head) {
-    vector<int> values;
-    Node* current = head;
-    
-    // Store the elements of the linked list in an array
-    while (current != nullptr) {
-        values.push_back(current->data);
-        current = current->next;
-    }
-    
-    int i = 0, j = values.size() - 1;
-    
-    // Compare elements from the beginning and end of the array
-    while (i < j) {
-        if (values[i] != values[j]) {
-            return false;
-        }
-        i++;
-        j--;
-    }
-    
-    return true;
-}
-
-// Main function to test the palindrome check
-int main() {
-    Node *head = new Node(1);
-    head->next = new Node(2);
-    head->next->next = new Node(3);
-    head->next->next->next = new Node(2);
-    head->next->next->next->next = new Node(1);
-    
-    if (isPalindrome(head)) {
-        cout << "Linked list is a palindrome." << endl;
-    } else {
-        cout << "Linked list is not a palindrome." << endl;
-    }
-    
-    return 0;
-}
+# Test the function
+nums1 = [1, 3]
+nums2 = [2]
+print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
