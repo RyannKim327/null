a72da6b2a980629b9e7ba06516aa72e6200b1b28
RyannKim327 <rksesgundo123@gmail.com>
2024-09-10 22:27:06 +0800

    9-10-2024 22:26 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,29 @@
-my_string = "Hello, World!"
-lower_string = my_string.lower()
-print(lower_string)
-let myString = "Hello, World!";
-let lowerString = myString.toLowerCase();
-console.log(lowerString);
-String myString = "Hello, World!";
-String lowerString = myString.toLowerCase();
-System.out.println(lowerString);
-#include <iostream>
-#include <algorithm>
-#include <cctype>
-#include <string>
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-int main()
-{
-    std::string myString = "Hello, World!";
-    std::transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
-    std::cout << myString << std::endl;
-    return 0;
-}
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# Usage
+arr = [12, 11, 13, 5, 6, 7]
+heapSort(arr)
+print("Sorted array is", arr)
