36c70c7bc4a00f5fc92f356c42acd9c7bc9509d0
RyannKim327 <rksesgundo123@gmail.com>
2024-09-12 21:42:30 +0800

    9-12-2024 21:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,4 @@
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
-
-# Test the selection sort function
-arr = [64, 25, 12, 22, 11]
-sorted_arr = selection_sort(arr)
-print("Sorted array is:", sorted_arr)
+string str1 = "Hello";
+string str2 = "World";
+string result = str1 + str2;
+print(result); 
