d5344394cb0a1a5483a2071d60d2d74766b5d912
RyannKim327 <rksesgundo123@gmail.com>
2024-09-12 21:43:00 +0800

    9-12-2024 21:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,8 @@
-def is_palindrome(s):
-    start = 0
-    end = len(s) - 1
+function countLeafNodes(node):
+    if node is NULL:
+        return 0
+    if node.left is NULL and node.right is NULL:
+        return 1
+    return countLeafNodes(node.left) + countLeafNodes(node.right)
 
-    while start < end:
-        if s[start] != s[end]:
-            return False
-        start += 1
-        end -= 1
-
-    return True
-
-s = "madam"
-if is_palindrome(s):
-    print("Palindrome")
-else:
-    print("Not a palindrome")
+leafNodeCount = countLeafNodes(root)
