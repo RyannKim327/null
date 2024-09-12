568cb2bc4ea6e2167bf5cf3e28dadebc63edbe44
RyannKim327 <rksesgundo123@gmail.com>
2024-09-13 05:37:14 +0800

    9-13-2024 5:37 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,38 @@
-def breadth_limited_search(graph, start, goal, limit):
-    visited = []
-    queue = [[start]]
-    
-    if start == goal:
-        return start
-    
-    while queue:
-        path = queue.pop(0)
-        node = path[-1]
-        
-        if node not in visited:
-            neighbors = graph[node]
-            
-            for neighbor in neighbors:
-                new_path = list(path)
-                new_path.append(neighbor)
-                queue.append(new_path)
-                
-                if neighbor == goal:
-                    if len(new_path) <= limit:
-                        return new_path
-            
-            visited.append(node)
-    
-    return "No path found within the limit"
+class HashTable:
+    def __init__(self):
+        self.size = 10
+        self.table = [None] * self.size
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    def insert(self, key, value):
+        index = self.hash_function(key)
+        if self.table[index] is None:
+            self.table[index] = [(key, value)]
+        else:
+            self.table[index].append((key, value))
+
+    def get(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for k, v in self.table[index]:
+                if k == key:
+                    return v
+        return None
+
+    def delete(self, key):
+        index = self.hash_function(key)
+        if self.table[index] is not None:
+            for i, (k, v) in enumerate(self.table[index]):
+                if k == key:
+                    del self.table[index][i]
+
+# Example usage
+hash_table = HashTable()
+hash_table.insert('key1', 'value1')
+hash_table.insert('key2', 'value2')
+
+print(hash_table.get('key1'))  # Output: value1
+hash_table.delete('key1')
+print(hash_table.get('key1'))  # Output: None
