ad5782e0b10a7cc2e3db2237a97e6992a81b62cd
RyannKim327 <rksesgundo123@gmail.com>
2024-09-12 21:42:24 +0800

    9-12-2024 21:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,14 @@
-def bubble_sort(arr):
+def selection_sort(arr):
     n = len(arr)
-    
     for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
-# Example usage
-arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = bubble_sort(arr)
+# Test the selection sort function
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
 print("Sorted array is:", sorted_arr)
