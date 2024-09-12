a873f569124246557ce53a9faf51e15f5d1241a6
RyannKim327 <rksesgundo123@gmail.com>
2024-09-12 21:42:53 +0800

    9-12-2024 21:42 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,17 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+def is_palindrome(s):
+    start = 0
+    end = len(s) - 1
 
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear = None
-    
-    def enqueue(self, data):
-        new_node = Node(data)
-        if self.rear is None:
-            self.front = new_node
-            self.rear = new_node
-        else:
-            self.rear.next = new_node
-            self.rear = new_node
-    
-    def dequeue(self):
-        if self.front is None:
-            print("Queue is empty")
-            return None
-        else:
-            data = self.front.data
-            self.front = self.front.next
-            if self.front is None:
-                self.rear = None
-            return data
+    while start < end:
+        if s[start] != s[end]:
+            return False
+        start += 1
+        end -= 1
 
-    def display(self):
-        current = self.front
-        if current is None:
-            print("Queue is empty")
-            return
-        while current is not None:
-            print(current.data, end=" ")
-            current = current.next
-        print()
+    return True
 
-# Test the Queue implementation
-q = Queue()
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-q.display()
-
-q.dequeue()
-q.display()
-
-q.dequeue()
-q.display()
+s = "madam"
+if is_palindrome(s):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
