3dee237ef6d187ba2a56a90d2b71df2709528535
RyannKim327 <rksesgundo123@gmail.com>
2024-09-30 05:45:55 +0800

    9-30-2024 5:45 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,17 @@
-def reverse_words_order(s):
-    words = s.split()
-    reversed_words = words[::-1]
-    reversed_string = ' '.join(reversed_words)
-    return reversed_string
+def findMedianSortedArrays(nums1, nums2):
+    merged = sorted(nums1 + nums2)
+    n = len(merged)
 
-# Test the function
-input_string = "Hello, World! How are you?"
-reversed_string = reverse_words_order(input_string)
-print(reversed_string)
+    if n % 2 == 0:
+        return (merged[n // 2 - 1] + merged[n // 2]) / 2
+    else:
+        return merged[n // 2]
+
+# Input arrays
+nums1 = [1, 3]
+nums2 = [2]
+
+# Finding the median
+median = findMedianSortedArrays(nums1, nums2)
+
+print("Median of the two sorted arrays is:", median)
