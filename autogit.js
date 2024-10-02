6dbf4de44e20bc89ac3ae1176b1491f8a5c551f3
RyannKim327 <rksesgundo123@gmail.com>
2024-10-02 23:09:49 +0800

    10-2-2024 23:9 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,30 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-    
-    left = merge_sort(left)
-    right = merge_sort(right)
-    
-    return merge(left, right)
+class Stack:
+    def __init__(self):
+        self.stack = []
 
-def merge(left, right):
-    result = []
-    left_idx, right_idx = 0, 0
-    
-    while left_idx < len(left) and right_idx < len(right):
-        if left[left_idx] < right[right_idx]:
-            result.append(left[left_idx])
-            left_idx += 1
-        else:
-            result.append(right[right_idx])
-            right_idx += 1
-    
-    result.extend(left[left_idx:])
-    result.extend(right[right_idx:])
-    
-    return result
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.stack.pop()
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.stack[-1]
+
+    def size(self):
+        return len(self.stack)
 
 # Example usage
-arr = [38, 27, 43, 3, 9, 82, 10]
-sorted_arr = merge_sort(arr)
-print(sorted_arr)
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.pop())  # Output: 3
