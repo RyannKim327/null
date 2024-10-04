1e343b0282ef3562daaec17df96a5cdf128af0ea
RyannKim327 <rksesgundo123@gmail.com>
2024-10-05 07:49:42 +0800

    10-5-2024 7:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,8 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+def reverse_words_order(input_string):
+    words = input_string.split()
+    reversed_string = ' '.join(reversed(words))
+    return reversed_string
 
-arr = [3, 6, 8, 10, 1, 2, 1]
-print(quicksort(arr))
+input_string = "Hello World, this is a test"
+reversed_string = reverse_words_order(input_string)
+print(reversed_string)
