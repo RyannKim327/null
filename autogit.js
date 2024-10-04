cd54072c2c4b71e75d1de2f511f4246af98214d1
RyannKim327 <rksesgundo123@gmail.com>
2024-10-05 07:49:48 +0800

    10-5-2024 7:49 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,34 @@
-def reverse_words_order(input_string):
-    words = input_string.split()
-    reversed_string = ' '.join(reversed(words))
-    return reversed_string
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-input_string = "Hello World, this is a test"
-reversed_string = reverse_words_order(input_string)
-print(reversed_string)
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+        print()
+
+# Example usage:
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+
+llist.display()
