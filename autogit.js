d5fa5f1afafb356ac137c7aa12d99071406d23b8
RyannKim327 <rksesgundo123@gmail.com>
2024-10-05 07:50:17 +0800

    10-5-2024 7:50 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,84 @@
 class Node:
     def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
-        self.val = key
+        self.height = 1
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if root.val < key:
-            root.right = insert(root.right, key)
+class AVLTree:
+    def getHeight(self, node):
+        if node is None:
+            return 0
+        return node.height
+
+    def getBalance(self, node):
+        if node is None:
+            return 0
+        return self.getHeight(node.left) - self.getHeight(node.right)
+
+    def rotateRight(self, y):
+        x = y.left
+        T2 = x.right
+
+        x.right = y
+        y.left = T2
+
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+
+        return x
+
+    def rotateLeft(self, x):
+        y = x.right
+        T2 = y.left
+
+        y.left = x
+        x.right = T2
+
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        return y
+
+    def insert(self, root, key):
+        if root is None:
+            return Node(key)
+        elif key < root.key:
+            root.left = self.insert(root.left, key)
         else:
-            root.left = insert(root.left, key)
-    return root
+            root.right = self.insert(root.right, key)
 
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(root.val)
-        inorder_traversal(root.right)
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
 
-# Example usage
+        balance = self.getBalance(root)
+
+        if balance > 1 and key < root.left.key:
+            return self.rotateRight(root)
+
+        if balance < -1 and key > root.right.key:
+            return self.rotateLeft(root)
+
+        if balance > 1 and key > root.left.key:
+            root.left = self.rotateLeft(root.left)
+            return self.rotateRight(root)
+
+        if balance < -1 and key < root.right.key:
+            root.right = self.rotateRight(root.right)
+            return self.rotateLeft(root)
+
+        return root
+
+    def preorder(self, root):
+        if root is not None:
+            print(root.key, end=" ")
+            self.preorder(root.left)
+            self.preorder(root.right)
+
+avl_tree = AVLTree()
 root = None
-root = insert(root, 5)
-insert(root, 3)
-insert(root, 7)
-insert(root, 1)
-insert(root, 4)
+keys = [10, 20, 30, 40, 50, 25]
 
-print("Inorder traversal:")
-inorder_traversal(root)
+for key in keys:
+    root = avl_tree.insert(root, key)
+
+avl_tree.preorder(root)
