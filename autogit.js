a9c4d7707ead40ee00f5dae4ed8c51a9cd64f8bd
RyannKim327 <rksesgundo123@gmail.com>
2024-10-08 05:54:26 +0800

    10-8-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,9 @@
-boolean isSorted(int[] arr) {
-    for (int i = 0; i < arr.length - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return false; // Array is not sorted in ascending order
-        }
-    }
-    return true; // Array is sorted in ascending order
-}
+FUNCTION findMaxSumSubarray(arr):
+    max_so_far = arr[0]
+    max_ending_here = arr[0]
+    
+    FOR i = 1 TO arr.length - 1:
+        max_ending_here = MAX(arr[i], max_ending_here + arr[i])
+        max_so_far = MAX(max_so_far, max_ending_here)
+    
+    RETURN max_so_far
