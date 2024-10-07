84bae20047997c70f4d6f994db56ada2548de038
RyannKim327 <rksesgundo123@gmail.com>
2024-10-08 05:54:33 +0800

    10-8-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,22 @@
-FUNCTION findMaxSumSubarray(arr):
-    max_so_far = arr[0]
-    max_ending_here = arr[0]
-    
-    FOR i = 1 TO arr.length - 1:
-        max_ending_here = MAX(arr[i], max_ending_here + arr[i])
-        max_so_far = MAX(max_so_far, max_ending_here)
-    
-    RETURN max_so_far
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+
+# Example graph in the form of an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Start DFS from node 'A'
+dfs(graph, 'A')
