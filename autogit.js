d341a995baa6694dda835be71c4a619920c5737e
RyannKim327 <rksesgundo123@gmail.com>
2024-10-08 05:53:51 +0800

    10-8-2024 5:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,37 @@
-let array = [1, 2, 3, 4, 5];
-let indexToRemove = 2;
+from collections import deque
 
-array.splice(indexToRemove, 1);
+class Graph:
+    def __init__(self):
+        self.graph = {}
 
-console.log(array); // Output: [1, 2, 4, 5]
-let array = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-let indexToRemove = array.indexOf(elementToRemove);
+    def add_edge(self, node, edge):
+        if node in self.graph:
+            self.graph[node].append(edge)
+        else:
+            self.graph[node] = [edge]
 
-if (indexToRemove > -1) {
-    array.splice(indexToRemove, 1);
-}
+    def BFS(self, start_node):
+        visited = set()
+        queue = deque([start_node])
+        visited.add(start_node)
 
-console.log(array); // Output: [1, 2, 4, 5]
+        while queue:
+            current_node = queue.popleft()
+            print(current_node, end=" ")
+
+            if current_node in self.graph:
+                for neighbor in self.graph[current_node]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+
+# Example Usage
+graph = Graph()
+graph.add_edge(0, 1)
+graph.add_edge(0, 2)
+graph.add_edge(1, 3)
+graph.add_edge(2, 4)
+graph.add_edge(2, 5)
+
+print("Breadth First Search Starting from Node 0:")
+graph.BFS(0)
