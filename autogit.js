29db0aeb2ec6ea7d15cc590796e999da27f7d96a
RyannKim327 <rksesgundo123@gmail.com>
2024-10-08 05:53:31 +0800

    10-8-2024 5:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,84 +1,17 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.height = 1
+function findCommonElements(arr1, arr2) {
+    const commonElements = [];
 
-class AVLTree:
-    def getHeight(self, node):
-        if node is None:
-            return 0
-        return node.height
+    arr1.forEach((element) => {
+        if (arr2.includes(element)) {
+            commonElements.push(element);
+        }
+    });
 
-    def getBalance(self, node):
-        if node is None:
-            return 0
-        return self.getHeight(node.left) - self.getHeight(node.right)
+    return commonElements;
+}
 
-    def rotateRight(self, y):
-        x = y.left
-        T2 = x.right
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
 
-        x.right = y
-        y.left = T2
-
-        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
-        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
-
-        return x
-
-    def rotateLeft(self, x):
-        y = x.right
-        T2 = y.left
-
-        y.left = x
-        x.right = T2
-
-        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
-        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
-
-        return y
-
-    def insert(self, root, key):
-        if root is None:
-            return Node(key)
-        elif key < root.key:
-            root.left = self.insert(root.left, key)
-        else:
-            root.right = self.insert(root.right, key)
-
-        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
-
-        balance = self.getBalance(root)
-
-        if balance > 1 and key < root.left.key:
-            return self.rotateRight(root)
-
-        if balance < -1 and key > root.right.key:
-            return self.rotateLeft(root)
-
-        if balance > 1 and key > root.left.key:
-            root.left = self.rotateLeft(root.left)
-            return self.rotateRight(root)
-
-        if balance < -1 and key < root.right.key:
-            root.right = self.rotateRight(root.right)
-            return self.rotateLeft(root)
-
-        return root
-
-    def preorder(self, root):
-        if root is not None:
-            print(root.key, end=" ")
-            self.preorder(root.left)
-            self.preorder(root.right)
-
-avl_tree = AVLTree()
-root = None
-keys = [10, 20, 30, 40, 50, 25]
-
-for key in keys:
-    root = avl_tree.insert(root, key)
-
-avl_tree.preorder(root)
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [3, 4, 5]
