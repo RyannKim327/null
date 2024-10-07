fc63d073fdcb2ff7b7e0038c3dc8b554d3081136
RyannKim327 <rksesgundo123@gmail.com>
2024-10-08 05:54:47 +0800

    10-8-2024 5:54 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,21 @@
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(size)]
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    
+# Example adjacency list representation of a graph
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
 
-    def _hash_function(self, key):
-        return hash(key) % self.size
-
-    def insert(self, key, value):
-        index = self._hash_function(key)
-        for pair in self.table[index]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-        self.table[index].append([key, value])
-
-    def search(self, key):
-        index = self._hash_function(key)
-        for pair in self.table[index]:
-            if pair[0] == key:
-                return pair[1]
-        return None
-
-    def delete(self, key):
-        index = self._hash_function(key)
-        for i, pair in enumerate(self.table[index]):
-            if pair[0] == key:
-                del self.table[index][i]
-                return
-        raise KeyError(f"Key '{key}' not found in hash table")
+dfs(graph, 'A')
