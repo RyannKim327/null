074a8403ccecbefba4978e83a8d3a8bb7df71b2a
RyannKim327 <rksesgundo123@gmail.com>
2024-10-14 08:30:56 +0800

    10-14-2024 8:30 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,12 @@
-from queue import PriorityQueue
+def calculate_mean(numbers):
+    if len(numbers) == 0:
+        return 0
+    else:
+        return sum(numbers) / len(numbers)
 
-def beam_search(initial_state, branching_factor, beam_width, scoring_function):
-    beam = PriorityQueue()
-    beam.put((scoring_function(initial_state), initial_state))
+# Example list of numbers
+numbers = [1, 2, 3, 4, 5]
 
-    while not beam.empty():
-        candidates = []
-        for _ in range(branching_factor):
-            if beam.empty():
-                break
-            _, state = beam.get()
-            if is_goal(state):
-                return state
-            next_states = generate_next_states(state)
-            for next_state in next_states:
-                candidates.append((scoring_function(next_state), next_state))
-        candidates.sort(key=lambda x: x[0], reverse=True)
-        for i in range(min(beam_width, len(candidates))):
-            beam.put(candidates[i])
-
-    return None
+# Calculate the mean
+mean = calculate_mean(numbers)
+print("Mean:", mean)
