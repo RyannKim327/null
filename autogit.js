d8a70be8e38b10fafa5b9339c228400b1b23a864
RyannKim327 <rksesgundo123@gmail.com>
2024-10-15 06:05:32 +0800

    10-15-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,21 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+Node findNthFromEnd(Node head, int n) {
+    Node slowPtr = head;
+    Node fastPtr = head;
 
-def height(node):
-    if node is None:
-        return 0
-    return 1 + max(height(node.left), height(node.right))
+    // Move fastPtr by n nodes
+    for (int i = 0; i < n; i++) {
+        if (fastPtr == null) {
+            return null; // List is shorter than n
+        }
+        fastPtr = fastPtr.next;
+    }
 
-def diameter(node):
-    if node is None:
-        return 0
-    left_height = height(node.left)
-    right_height = height(node.right)
+    // Move both pointers simultaneously
+    while (fastPtr != null) {
+        slowPtr = slowPtr.next;
+        fastPtr = fastPtr.next;
+    }
 
-    left_diameter = diameter(node.left)
-    right_diameter = diameter(node.right)
-
-    return max(left_height + right_height + 1, max(left_diameter, right_diameter))
-
-# Sample binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Diameter of the binary tree is:", diameter(root))
+    // At this point, slowPtr is pointing to the nth node from the end
+    return slowPtr;
+}
