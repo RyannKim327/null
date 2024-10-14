edea7521899cc6952e7cb207949210bf1d2c06cc
RyannKim327 <rksesgundo123@gmail.com>
2024-10-15 06:05:49 +0800

    10-15-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,11 @@
-Node findNthFromEnd(Node head, int n) {
-    Node slowPtr = head;
-    Node fastPtr = head;
+def is_palindrome(s):
+    start = 0
+    end = len(s) - 1
 
-    // Move fastPtr by n nodes
-    for (int i = 0; i < n; i++) {
-        if (fastPtr == null) {
-            return null; // List is shorter than n
-        }
-        fastPtr = fastPtr.next;
-    }
+    while start < end:
+        if s[start] != s[end]:
+            return False
+        start += 1
+        end -= 1
 
-    // Move both pointers simultaneously
-    while (fastPtr != null) {
-        slowPtr = slowPtr.next;
-        fastPtr = fastPtr.next;
-    }
-
-    // At this point, slowPtr is pointing to the nth node from the end
-    return slowPtr;
-}
+    return True
