34f5df250769754ade9f113696e5ee06215cbfc7
RyannKim327 <rksesgundo123@gmail.com>
2024-10-15 06:06:00 +0800

    10-15-2024 6:5 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,19 @@
-def is_palindrome(s):
-    start = 0
-    end = len(s) - 1
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-    while start < end:
-        if s[start] != s[end]:
+def has_cycle(head):
+    if head is None or head.next is None:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if fast is None or fast.next is None:
             return False
-        start += 1
-        end -= 1
+        slow = slow.next
+        fast = fast.next.next
 
     return True
