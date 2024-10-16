309cf39371c5dc312a9ffec1f2546c727b6eba0f
RyannKim327 <rksesgundo123@gmail.com>
2024-10-16 19:52:43 +0800

    10-16-2024 19:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,9 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+def reverse_words_order(sentence):
+    words = sentence.split()
+    reverse_sentence = ' '.join(reversed(words))
+    return reverse_sentence
 
-def has_cycle(head):
-    if head is None or head.next is None:
-        return False
-
-    slow = head
-    fast = head.next
-
-    while slow != fast:
-        if fast is None or fast.next is None:
-            return False
-        slow = slow.next
-        fast = fast.next.next
-
-    return True
+# Example usage
+sentence = "Hello, how are you?"
+reversed_sentence = reverse_words_order(sentence)
+print(reversed_sentence)
