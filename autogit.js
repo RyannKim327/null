d48229af58f3e4085200d29827d7dba70fea8a11
RyannKim327 <rksesgundo123@gmail.com>
2024-10-16 19:53:05 +0800

    10-16-2024 19:52 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,26 @@
-def reverse_words_in_string(input_string):
-    words = input_string.split()  # Split the string into words
-    reversed_words = ' '.join(reversed(words))  # Reverse the list of words and join them back together
-    return reversed_words
+def longest_common_substring(s1, s2):
+    m = len(s1)
+    n = len(s2)
+    
+    # Create a matrix to store the lengths of common substrings
+    dp = [[0]*(n+1) for _ in range(m+1)]
+    
+    max_len = 0
+    end_idx = 0
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    end_idx = i - 1
+            else:
+                dp[i][j] = 0
+    
+    return s1[end_idx-max_len+1:end_idx+1]
 
-# Example
-input_string = "Hello world, how are you?"
-reversed_string = reverse_words_in_string(input_string)
-print(reversed_string)  # Output: "you? are how world, Hello"
+# Example usage
+s1 = "abcdefg"
+s2 = "xyzbcde"
+print(longest_common_substring(s1, s2))
