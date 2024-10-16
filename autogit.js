d5f5b168996b7dd3fe3f1588cfe5129e992784bd
RyannKim327 <rksesgundo123@gmail.com>
2024-10-16 19:53:17 +0800

    10-16-2024 19:53 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,25 @@
-def longest_common_substring(s1, s2):
-    m = len(s1)
-    n = len(s2)
-    
-    # Create a matrix to store the lengths of common substrings
-    dp = [[0]*(n+1) for _ in range(m+1)]
-    
-    max_len = 0
-    end_idx = 0
-    
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-                if dp[i][j] > max_len:
-                    max_len = dp[i][j]
-                    end_idx = i - 1
-            else:
-                dp[i][j] = 0
-    
-    return s1[end_idx-max_len+1:end_idx+1]
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = left + (right - left) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
 
 # Example usage
-s1 = "abcdefg"
-s2 = "xyzbcde"
-print(longest_common_substring(s1, s2))
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 7
+result = binary_search(arr, target)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
