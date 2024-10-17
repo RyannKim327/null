5277171e1558973792b99e75f5f714809ba67016
RyannKim327 <rksesgundo123@gmail.com>
2024-10-17 23:18:26 +0800

    10-17-2024 23:18 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,73 +1,19 @@
-class BTreeNode:
-    def __init__(self, leaf=True):
-        self.leaf = leaf
-        self.keys = []
-        self.children = []
+def are_anagrams(str1, str2):
+    # Remove spaces and punctuation, convert to lowercase
+    str1 = ''.join(e for e in str1 if e.isalnum()).lower()
+    str2 = ''.join(e for e in str2 if e.isalnum()).lower()
 
-class BTree:
-    def __init__(self, t):
-        self.root = BTreeNode(leaf=True)
-        self.t = t
+    # Sort the strings
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
 
-    def insert(self, key):
-        if len(self.root.keys) == (2 * self.t) - 1:
-            new_root = BTreeNode(leaf=False)
-            new_root.children.append(self.root)
-            self._split_child(new_root, 0)
-            self.root = new_root
-        self._insert_non_full(self.root, key)
+    # Compare the sorted strings
+    return sorted_str1 == sorted_str2
 
-    def _insert_non_full(self, node, key):
-        i = len(node.keys) - 1
-        if node.leaf:
-            node.keys.append(None)
-            while i >= 0 and key < node.keys[i]:
-                node.keys[i + 1] = node.keys[i]
-                i -= 1
-            node.keys[i + 1] = key
-        else:
-            while i >= 0 and key < node.keys[i]:
-                i -= 1
-            i += 1
-            if len(node.children[i].keys) == (2 * self.t) - 1:
-                self._split_child(node, i)
-                if key > node.keys[i]:
-                    i += 1
-            self._insert_non_full(node.children[i], key)
-
-    def _split_child(self, parent, index):
-        t = self.t
-        y = parent.children[index]
-        z = BTreeNode(leaf=y.leaf)
-
-        parent.children.insert(index + 1, z)
-        parent.keys.insert(index, y.keys[t - 1])
-
-        z.keys = y.keys[t: (2 * t) - 1]
-        y.keys = y.keys[0: t - 1]
-
-        if not y.leaf:
-            z.children = y.children[t: 2 * t]
-            y.children = y.children[0: t - 1]
-
-    def search(self, key, node=None):
-        if node is None:
-            node = self.root
-        i = 0
-        while i < len(node.keys) and key > node.keys[i]:
-            i += 1
-        if i < len(node.keys) and key == node.keys[i]:
-            return True
-        if node.leaf:
-            return False
-        else:
-            return self.search(key, node.children[i])
-
-# Usage
-bt = BTree(3)
-bt.insert(3)
-bt.insert(7)
-bt.insert(1)
-
-print(bt.search(7))  # Output: True
-print(bt.search(5))  # Output: False
+# Test the function
+str1 = "Listen"
+str2 = "Silent"
+if are_anagrams(str1, str2):
+    print(f"{str1} and {str2} are anagrams.")
+else:
+    print(f"{str1} and {str2} are not anagrams.")
