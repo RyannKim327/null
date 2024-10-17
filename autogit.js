02c20be3139d1c039174fe80f844d8949e0d4bae
RyannKim327 <rksesgundo123@gmail.com>
2024-10-17 23:18:13 +0800

    10-17-2024 23:17 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,73 @@
-from collections import defaultdict
+class BTreeNode:
+    def __init__(self, leaf=True):
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
 
-def topological_sort(graph):
-    in_degree = {node: 0 for node in graph}
-    for node in graph:
-        for neighbor in graph[node]:
-            in_degree[neighbor] += 1
+class BTree:
+    def __init__(self, t):
+        self.root = BTreeNode(leaf=True)
+        self.t = t
 
-    queue = [node for node in in_degree if in_degree[node] == 0]
-    result = []
+    def insert(self, key):
+        if len(self.root.keys) == (2 * self.t) - 1:
+            new_root = BTreeNode(leaf=False)
+            new_root.children.append(self.root)
+            self._split_child(new_root, 0)
+            self.root = new_root
+        self._insert_non_full(self.root, key)
 
-    while queue:
-        node = queue.pop(0)
-        result.append(node)
+    def _insert_non_full(self, node, key):
+        i = len(node.keys) - 1
+        if node.leaf:
+            node.keys.append(None)
+            while i >= 0 and key < node.keys[i]:
+                node.keys[i + 1] = node.keys[i]
+                i -= 1
+            node.keys[i + 1] = key
+        else:
+            while i >= 0 and key < node.keys[i]:
+                i -= 1
+            i += 1
+            if len(node.children[i].keys) == (2 * self.t) - 1:
+                self._split_child(node, i)
+                if key > node.keys[i]:
+                    i += 1
+            self._insert_non_full(node.children[i], key)
 
-        for neighbor in graph[node]:
-            in_degree[neighbor] -= 1
-            if in_degree[neighbor] == 0:
-                queue.append(neighbor)
+    def _split_child(self, parent, index):
+        t = self.t
+        y = parent.children[index]
+        z = BTreeNode(leaf=y.leaf)
 
-    if len(result) == len(graph):
-        return result
-    else:
-        return []
+        parent.children.insert(index + 1, z)
+        parent.keys.insert(index, y.keys[t - 1])
 
-# Example graph
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D'],
-    'C': ['D'],
-    'D': []
-}
+        z.keys = y.keys[t: (2 * t) - 1]
+        y.keys = y.keys[0: t - 1]
 
-print(topological_sort(graph))
+        if not y.leaf:
+            z.children = y.children[t: 2 * t]
+            y.children = y.children[0: t - 1]
+
+    def search(self, key, node=None):
+        if node is None:
+            node = self.root
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+        if i < len(node.keys) and key == node.keys[i]:
+            return True
+        if node.leaf:
+            return False
+        else:
+            return self.search(key, node.children[i])
+
+# Usage
+bt = BTree(3)
+bt.insert(3)
+bt.insert(7)
+bt.insert(1)
+
+print(bt.search(7))  # Output: True
+print(bt.search(5))  # Output: False
