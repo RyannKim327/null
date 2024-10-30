42e1c6ab22d827619e16fbec0566458724ec6b93
RyannKim327 <rksesgundo123@gmail.com>
2024-10-31 05:31:45 +0800

    10-31-2024 5:31 [User mode]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,12 @@
-user_input = input("Enter a number: ")
-print("You entered:", user_input)
+def burrows_wheeler_transform(input_str):
+    input_str += '$'  # Add special end-of-string marker
+    rotations = [input_str[i:] + input_str[:i] for i in range(len(input_str))]  # Generate all rotations of the input string
+    rotations.sort()  # Sort the rotations lexicographically
+    bwt_str = ''.join([rotation[-1] for rotation in rotations])  # Construct the Burrows-Wheeler Transform string
+    return bwt_str
+input_str = "banana"
+bwt_str = burrows_wheeler_transform(input_str)
+print("Input String:", input_str)
+print("Burrows-Wheeler Transform:", bwt_str)
+Input String: banana
+Burrows-Wheeler Transform: annb$aa
