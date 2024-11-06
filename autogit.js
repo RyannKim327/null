0144d1dc46dab19d3f24a5b3f89726a8d03acb70
RyannKim327 <rksesgundo123@gmail.com>
2024-11-07 05:45:04 +0800

    11-7-2024 5:44 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,4 @@
-# Python program to take user input
-user_input = input("Enter something: ")
-print("You entered:", user_input)
+decimal_number = 42
+binary_number = bin(decimal_number)[2:]  # Indexing [2:] removes the '0b' prefix from the binary representation
+
+print(f"The binary representation of {decimal_number} is: {binary_number}")
