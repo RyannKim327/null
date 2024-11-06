b80f65b0e89742ed52cc37f0f80b31834a1269c1
RyannKim327 <rksesgundo123@gmail.com>
2024-11-07 05:44:55 +0800

    11-7-2024 5:44 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,3 @@
-def largest_prime_factor(n):
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-    return n
-    
-number = 123456789
-largest_prime = largest_prime_factor(number)
-print("Largest prime factor of", number, "is:", largest_prime)
+# Python program to take user input
+user_input = input("Enter something: ")
+print("You entered:", user_input)
