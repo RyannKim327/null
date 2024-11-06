55b8aa60cc7e22e930733421fd28b7c10b5329c6
RyannKim327 <rksesgundo123@gmail.com>
2024-11-07 05:45:11 +0800

    11-7-2024 5:45 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,37 @@
-decimal_number = 42
-binary_number = bin(decimal_number)[2:]  # Indexing [2:] removes the '0b' prefix from the binary representation
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-print(f"The binary representation of {decimal_number} is: {binary_number}")
+def insert(node, key):
+    if node is None:
+        return TreeNode(key)
+    else:
+        if key < node.val:
+            node.left = insert(node.left, key)
+        else:
+            node.right = insert(node.right, key)
+    return node
+
+def search(node, key):
+    if node is None or node.val == key:
+        return node
+    if node.val < key:
+        return search(node.right, key)
+    return search(node.left, key)
+
+root = None
+keys = [8, 3, 10, 1, 6, 14, 4, 7, 13]
+
+for key in keys:
+    root = insert(root, key)
+
+# Search for a key in the BST
+key_to_search = 6
+result = search(root, key_to_search)
+
+if result:
+    print(f"Key {key_to_search} found in the tree.")
+else:
+    print(f"Key {key_to_search} not found in the tree.")
