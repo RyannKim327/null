b69b506abc0cf4122a9ecf57d3d28707d31e2a89
RyannKim327 <rksesgundo123@gmail.com>
2024-11-07 05:44:38 +0800

    11-7-2024 5:44 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,84 +1,20 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.height = 1
+// Definition for a binary tree node
+class TreeNode {
+public:
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-class AVLTree:
-    def insert(self, root, key):
-        if not root:
-            return Node(key)
-        elif key < root.key:
-            root.left = self.insert(root.left, key)
-        else:
-            root.right = self.insert(root.right, key)
+int maxDepth(TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
 
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
 
-        balance = self.get_balance(root)
-
-        if balance > 1 and key < root.left.key:
-            return self.right_rotate(root)
-        
-        if balance < -1 and key > root.right.key:
-            return self.left_rotate(root)
-        
-        if balance > 1 and key > root.left.key:
-            root.left = self.left_rotate(root.left)
-            return self.right_rotate(root)
-        
-        if balance < -1 and key < root.right.key:
-            root.right = self.right_rotate(root.right)
-            return self.left_rotate(root)
-
-        return root
-
-    def get_height(self, root):
-        if not root:
-            return 0
-        return root.height
-
-    def get_balance(self, root):
-        if not root:
-            return 0
-        return self.get_height(root.left) - self.get_height(root.right)
-
-    def left_rotate(self, z):
-        y = z.right
-        T2 = y.left
-
-        y.left = z
-        z.right = T2
-
-        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
-        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
-
-        return y
-
-    def right_rotate(self, z):
-        y = z.left
-        T3 = y.right
-
-        y.right = z
-        z.left = T3
-
-        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
-        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
-
-        return y
-
-    def pre_order(self, root):
-        if not root:
-            return
-        print("{0} ".format(root.key), end="")
-        self.pre_order(root.left)
-        self.pre_order(root.right)
-
-my_tree = AVLTree()
-root = None
-keys = [9, 5, 10, 0, 6, 11, -1, 1, 2]
-for key in keys:
-    root = my_tree.insert(root, key)
-
-my_tree.pre_order(root)
+    // Return the maximum depth of the left and right subtrees plus 1
+    return 1 + max(leftDepth, rightDepth);
+}
