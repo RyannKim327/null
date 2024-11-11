9e10567c07f057cf27ecdfd43cb9e900d56f7b1b
RyannKim327 <rksesgundo123@gmail.com>
2024-11-12 06:11:49 +0800

    11-12-2024 6:11 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,14 @@
-def are_anagrams(str1, str2):
-    # Remove spaces and convert both strings to lowercase
-    str1 = str1.replace(' ', '').lower()
-    str2 = str2.replace(' ', '').lower()
-    
-    # Check if the sorted strings are equal
-    return sorted(str1) == sorted(str2)
-    
-# Test the function
-str1 = "Listen"
-str2 = "Silent"
-if are_anagrams(str1, str2):
-    print("The strings are anagrams!")
+my_string = "Hello, World!"
+substring = "Hello"
+
+if substring in my_string:
+    print("Substring found in the string")
 else:
-    print("The strings are not anagrams.")
+    print("Substring not found in the string")
+my_string = "Hello, World!"
+substring = "Hello"
+
+if my_string.find(substring) != -1:
+    print("Substring found in the string")
+else:
+    print("Substring not found in the string")
