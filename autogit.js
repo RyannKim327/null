87c98aa7f7386ab6dd294d55c780c00b0c5d7cff
RyannKim327 <rksesgundo123@gmail.com>
2024-11-12 06:11:57 +0800

    11-12-2024 6:11 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,29 @@
-my_string = "Hello, World!"
-substring = "Hello"
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-if substring in my_string:
-    print("Substring found in the string")
-else:
-    print("Substring not found in the string")
-my_string = "Hello, World!"
-substring = "Hello"
+    if left < n and arr[left] > arr[largest]:
+        largest = left
 
-if my_string.find(substring) != -1:
-    print("Substring found in the string")
-else:
-    print("Substring not found in the string")
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+# Example usage:
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is", arr)
