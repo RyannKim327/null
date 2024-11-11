6fa3d7d3e2fe23b9c07df5cb8f53428332ffa58f
RyannKim327 <rksesgundo123@gmail.com>
2024-11-12 06:12:56 +0800

    11-12-2024 6:12 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,36 @@
 from collections import defaultdict
 
-def topologicalSortUtil(node, visited, stack, graph):
-    visited[node] = True
-    for neighbor in graph[node]:
-        if not visited[neighbor]:
-            topologicalSortUtil(neighbor, visited, stack, graph)
-    stack.append(node)
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
 
-def topologicalSort(graph):
-    visited = {node: False for node in graph}
-    stack = []
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
 
-    for node in graph:
-        if not visited[node]:
-            topologicalSortUtil(node, visited, stack, graph)
+    def topological_sort_util(self, v, visited, stack):
+        visited[v] = True
 
-    stack.reverse()
-    return stack
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.topological_sort_util(i, visited, stack)
 
-# Example graph for testing the algorithm
-graph = {
-    0: [1, 2],
-    1: [3],
-    2: [3],
-    3: [4],
-    4: []
-}
+        stack.append(v)
 
-topologically_sorted = topologicalSort(graph)
-print("Topological sort result:", topologically_sorted)
+    def topological_sort(self):
+        visited = [False] * len(self.graph)
+        stack = []
+
+        for i in range(len(self.graph)):
+            if not visited[i]:
+                self.topological_sort_util(i, visited, stack)
+
+        return stack[::-1]
+
+# Test the implementation
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+
+print(g.topological_sort())
