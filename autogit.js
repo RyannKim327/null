a78ad605d3a6567505e20228a3288cfcd4644770
RyannKim327 <rksesgundo123@gmail.com>
2024-11-19 14:33:35 +0800

    11-19-2024 14:33 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,42 @@
-string_with_whitespace = "  Hello,  World!  "
-string_without_whitespace = string_with_whitespace.replace(" ", "")
-print(string_without_whitespace)
-import re
+def fibonacci_search(arr, x):
+    fib_m_minus_2 = 0
+    fib_m_minus_1 = 1
+    fib_m = fib_m_minus_1 + fib_m_minus_2
 
-string_with_whitespace = "  Hello,  World!  "
-string_without_whitespace = re.sub(r'\s+', '', string_with_whitespace)
-print(string_without_whitespace)
+    while fib_m < len(arr):
+        fib_m_minus_2 = fib_m_minus_1
+        fib_m_minus_1 = fib_m
+        fib_m = fib_m_minus_1 + fib_m_minus_2
+
+    offset = -1
+
+    while fib_m > 1:
+        i = min(offset + fib_m_minus_2, len(arr) - 1)
+
+        if arr[i] < x:
+            fib_m = fib_m_minus_1
+            fib_m_minus_1 = fib_m_minus_2
+            fib_m_minus_2 = fib_m - fib_m_minus_1
+            offset = i
+
+        elif arr[i] > x:
+            fib_m = fib_m_minus_2
+            fib_m_minus_1 = fib_m_minus_1 - fib_m_minus_2
+            fib_m_minus_2 = fib_m - fib_m_minus_1
+
+        else:
+            return i
+
+    if fib_m_minus_1 and arr[offset + 1] == x:
+        return offset + 1
+
+    return -1
+
+arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100]
+x = 85
+
+result = fibonacci_search(arr, x)
+if result != -1:
+    print("Element found at index", result)
+else:
+    print("Element not found")
