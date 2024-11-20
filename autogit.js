5fd9c953b32185ae853408e5c3a309c931f71408
RyannKim327 <rksesgundo123@gmail.com>
2024-11-20 11:17:54 +0800

    11-20-2024 11:17 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,22 @@
-function reverseArray(arr) {
-    let left = 0;
-    let right = arr.length - 1;
+import heapq
 
-    while (left < right) {
-        // Swap elements at left and right indices
-        let temp = arr[left];
-        arr[left] = arr[right];
-        arr[right] = temp;
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
 
-        // Move the left index to the right and the right index to the left
-        left++;
-        right--;
-    }
+    def push(self, item, priority):
+        heapq.heappush(self.heap, (priority, item))
 
-    return arr;
-}
+    def pop(self):
+        priority, item = heapq.heappop(self.heap)
+        return item
 
-let array = [1, 2, 3, 4, 5];
-console.log("Original Array: " + array);
+# Example Usage
+pq = PriorityQueue()
+pq.push('task1', 5)
+pq.push('task2', 1)
+pq.push('task3', 3)
 
-let reversedArray = reverseArray(array);
-console.log("Reversed Array: " + reversedArray);
+print(pq.pop())  # Output: task2
+print(pq.pop())  # Output: task3
+print(pq.pop())  # Output: task1
