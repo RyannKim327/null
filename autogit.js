24f1ac779073633386bbde54584ee1c2a39605c2
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:32:15 +0800

    11-23-2024 8:32 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,36 @@
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(size)]
-    
-    def _hash_function(self, key):
-        return hash(key) % self.size
-    
-    def insert(self, key, value):
-        index = self._hash_function(key)
-        for pair in self.table[index]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-        self.table[index].append([key, value])
-    
-    def get(self, key):
-        index = self._hash_function(key)
-        for pair in self.table[index]:
-            if pair[0] == key:
-                return pair[1]
-        raise KeyError(key)
-    
-    def delete(self, key):
-        index = self._hash_function(key)
-        for i, pair in enumerate(self.table[index]):
-            if pair[0] == key:
-                del self.table[index][i]
-                return
-        raise KeyError(key)
+def bidirectional_search(start, goal):
+    forward_queue = [start]
+    backward_queue = [goal]
+    forward_visited = {start}
+    backward_visited = {goal}
+
+    while forward_queue and backward_queue:
+        current_node = forward_queue.pop(0)
+        if current_node in backward_visited:
+            return "Path found"
+
+        for neighbor in get_neighbors(current_node):
+            if neighbor not in forward_visited:
+                forward_visited.add(neighbor)
+                forward_queue.append(neighbor)
+
+        current_node = backward_queue.pop(0)
+        if current_node in forward_visited:
+            return "Path found"
+
+        for neighbor in get_neighbors(current_node):
+            if neighbor not in backward_visited:
+                backward_visited.add(neighbor)
+                backward_queue.append(neighbor)
+
+    return "No path found"
+
+def get_neighbors(node):
+    # Implement this function to get the neighbors of a given node
+    pass
+
+# Example usage
+start = "A"
+goal = "Z"
+result = bidirectional_search(start, goal)
+print(result)
