11c3f339375ca6bd1c7b0948f49fa601f484c9e1
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:24:54 +0800

    11-23-2024 8:24 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,8 @@
-def selection_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-    return arr
-
-# Example Usage
-arr = [64, 25, 12, 22, 11]
-sorted_arr = selection_sort(arr)
-print("Sorted array is:", sorted_arr)
+function countLeafNodes(node):
+    if node is null:
+        return 0
+    
+    if node.left is null and node.right is null:
+        return 1
+    
+    return countLeafNodes(node.left) + countLeafNodes(node.right)
