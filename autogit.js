e6a3508c279972bd89789d0ad05f4565f5266616
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:23:10 +0800

    11-23-2024 8:23 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,103 +1,15 @@
-class Node:
-    def __init__(self, key, color='Red'):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.parent = None
-        self.color = color
+def burrows_wheeler_transform(input_string):
+    input_string += '$'  # Add a sentinel character to the end of the input string
+    cyclic_permutations = [input_string[i:] + input_string[:i] for i in range(len(input_string))]
+    sorted_permutations = sorted(cyclic_permutations)
+    
+    transformed_string = ''.join(perm[-1] for perm in sorted_permutations)
+    original_position = sorted_permutations.index(input_string)
+    
+    return transformed_string, original_position
 
-class RedBlackTree:
-    def __init__(self):
-        self.NULL_LEAF = Node(None, color='Black')
-        self.NULL_LEAF.left = None
-        self.NULL_LEAF.right = None
-        self.root = self.NULL_LEAF
-
-    def insert(self, key):
-        new_node = Node(key)
-        new_node.left = self.NULL_LEAF
-        new_node.right = self.NULL_LEAF
-
-        parent = None
-        current = self.root
-        while current != self.NULL_LEAF:
-            parent = current
-            if new_node.key < current.key:
-                current = current.left
-            else:
-                current = current.right
-
-        new_node.parent = parent
-
-        if parent is None:
-            self.root = new_node
-        elif new_node.key < parent.key:
-            parent.left = new_node
-        else:
-            parent.right = new_node
-
-        new_node.color = 'Red'
-        self.fix_violation(new_node)
-
-    def fix_violation(self, node):
-        while node != self.root and node.parent.color == 'Red':
-            if node.parent == node.parent.parent.left:
-                uncle = node.parent.parent.right
-                if uncle.color == 'Red':
-                    node.parent.color = 'Black'
-                    uncle.color = 'Black'
-                    node.parent.parent.color = 'Red'
-                    node = node.parent.parent
-                else:
-                    if node == node.parent.right:
-                        node = node.parent
-                        self.left_rotate(node)
-                    node.parent.color = 'Black'
-                    node.parent.parent.color = 'Red'
-                    self.right_rotate(node.parent.parent)
-            else:
-                uncle = node.parent.parent.left
-                if uncle.color == 'Red':
-                    node.parent.color = 'Black'
-                    uncle.color = 'Black'
-                    node.parent.parent.color = 'Red'
-                    node = node.parent.parent
-                else:
-                    if node == node.parent.left:
-                        node = node.parent
-                        self.right_rotate(node)
-                    node.parent.color = 'Black'
-                    node.parent.parent.color = 'Red'
-                    self.left_rotate(node.parent.parent)
-        self.root.color = 'Black'
-
-    def left_rotate(self, x):
-        y = x.right
-        x.right = y.left
-        if y.left != self.NULL_LEAF:
-            y.left.parent = x
-        y.parent = x.parent
-        if x.parent is None:
-            self.root = y
-        elif x == x.parent.left:
-            x.parent.left = y
-        else:
-            x.parent.right = y
-        y.left = x
-        x.parent = y
-
-    def right_rotate(self, x):
-        y = x.left
-        x.left = y.right
-        if y.right != self.NULL_LEAF:
-            y.right.parent = x
-        y.parent = x.parent
-        if x.parent is None:
-            self.root = y
-        elif x == x.parent.right:
-            x.parent.right = y
-        else:
-            x.parent.left = y
-        y.right = x
-        x.parent = y
-
+# Example usage
+input_string = "banana"
+transformed_string, original_position = burrows_wheeler_transform(input_string)
+print("Burrows-Wheeler transformed string:", transformed_string)
+print("Original position of input string:", original_position)
