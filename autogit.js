0203fc180cac15a59e86619bc2eb0619b1f63c0e
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:31:34 +0800

    11-23-2024 8:31 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,13 @@
-def preprocess_pattern(pattern):
-    lps = [0] * len(pattern)
-    j = 0
-    for i in range(1, len(pattern)):
-        while j > 0 and pattern[i] != pattern[j]:
-            j = lps[j-1]
-        if pattern[i] == pattern[j]:
-            j += 1
-        lps[i] = j
-    return lps
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-def kmp(text, pattern):
-    lps = preprocess_pattern(pattern)
-    i, j = 0, 0
-    while i < len(text):
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
-            if j == len(pattern):
-                print("Pattern found at index:", i - j)
-                j = lps[j-1]
-        else:
-            if j != 0:
-                j = lps[j-1]
-            else:
-                i += 1
-
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-kmp(text, pattern)
+# Example usage
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+print("Sorted array is:", arr)
