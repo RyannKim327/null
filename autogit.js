88683a1aef9c6b032da2f7c4d4bd72dcd1bd4e6c
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:26:10 +0800

    11-23-2024 8:26 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,95 @@
-def fibonacci_search(arr, x):
-    # Initialize Fibonacci numbers
-    fib1, fib2 = 0, 1
-    fib3 = fib1 + fib2
-    
-    # Find the smallest Fibonacci number greater than or equal to the length of the array
-    while fib3 < len(arr):
-        fib1 = fib2
-        fib2 = fib3
-        fib3 = fib1 + fib2
-    
-    offset = -1
-    
-    while fib3 > 1:
-        i = min(offset + fib1, len(arr) - 1)
-        
-        if arr[i] < x:
-            fib3 = fib2
-            fib2 = fib1
-            fib1 = fib3 - fib2
-            offset = i
-        elif arr[i] > x:
-            fib3 = fib1
-            fib2 = fib2 - fib1
-            fib1 = fib3 - fib2
+class Node:
+    def __init__(self, key, color, parent, left, right):
+        self.key = key
+        self.color = color
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+class RedBlackTree:
+    def __init__(self):
+        self.NIL = Node(key=None, color='BLACK', parent=None, left=None, right=None)
+        self.root = self.NIL
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.NIL:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent == self.NIL:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
         else:
-            return i
-    
-    if fib2 and arr[offset+1] == x:
-        return offset+1
-    
-    return -1
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.NIL:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent == self.NIL:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def insert_fixup(self, z):
+        while z.parent.color == 'RED':
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
+                if y.color == 'RED':
+                    z.parent.color = 'BLACK'
+                    y.color = 'BLACK'
+                    z.parent.parent.color = 'RED'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.right:
+                        z = z.parent
+                        self.left_rotate(z)
+                    z.parent.color = 'BLACK'
+                    z.parent.parent.color = 'RED'
+                    self.right_rotate(z.parent.parent)
+            else:
+                y = z.parent.parent.left
+                if y.color == 'RED':
+                    z.parent.color = 'BLACK'
+                    y.color = 'BLACK'
+                    z.parent.parent.color = 'RED'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.left:
+                        z = z.parent
+                        self.right_rotate(z)
+                    z.parent.color = 'BLACK'
+                    z.parent.parent.color = 'RED'
+                    self.left_rotate(z.parent.parent)
+        self.root.color = 'BLACK'
+
+    def insert(self, key):
+        z = Node(key=key, color='RED', parent=self.NIL, left=self.NIL, right=self.NIL)
+        y = self.NIL
+        x = self.root
+        while x != self.NIL:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.parent = y
+        if y == self.NIL:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+        z.left = self.NIL
+        z.right = self.NIL
+        self.insert_fixup(z)
