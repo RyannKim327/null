385c1fc73d92b30974f9c2fd7701b90248ad327e
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:28:14 +0800

    11-23-2024 8:28 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,70 @@
-# Convert a string to an integer
-str_num = "42"
-int_num = int(str_num)
+#include <iostream>
 
-print(int_num)  # Output: 42
+using namespace std;
+
+// Linked list node structure
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int d) : data(d), next(nullptr) {}
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    // Function to add element to the queue
+    void enqueue(int data) {
+        Node* newNode = new Node(data);
+        
+        if (rear == nullptr) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+        
+        cout << data << " enqueued to queue" << endl;
+    }
+
+    // Function to remove element from the queue
+    int dequeue() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+
+        int data = front->data;
+        Node* temp = front;
+        front = front->next;
+
+        if (front == nullptr) {
+            rear = nullptr;
+        }
+
+        delete temp;
+
+        cout << data << " dequeued from queue" << endl;
+
+        return data;
+    }
+};
+
+int main() {
+    Queue q;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    q.dequeue();
+    q.dequeue();
+
+    return 0;
+}
