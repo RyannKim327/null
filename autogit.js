fbee71874197bf4047068d43fafdbeeeda85dea4
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:31:02 +0800

    11-23-2024 8:30 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,26 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
+def precompute_bad_character_table(pattern):
+    table = {}
+    for i in range(len(pattern) - 1):
+        table[pattern[i]] = len(pattern) - 1 - i
+    return table
 
-def count_leaf_nodes(root):
-    if root is None:
-        return 0
-    if root.left is None and root.right is None:
-        return 1
-    return count_leaf_nodes(root.left) + count_leaf_nodes(root.right)
+def search(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    table = precompute_bad_character_table(pattern)
+
+    i = 0
+    while i <= n - m:
+        j = m - 1
+        while j >= 0 and pattern[j] == text[i + j]:
+            j -= 1
+        if j < 0:
+            print("Pattern occurs at index", i)
+            i += table.get(text[i + m - 1], m)
+        else:
+            i += table.get(text[i + m - 1], 1)
+
+text = "abcbabcbabcabcbabcabc"
+pattern = "abc"
+
+search(text, pattern)
