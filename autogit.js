1389c22e4f6097ca3f4aa71f73f08290dff912ce
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:20:21 +0800

    11-23-2024 8:20 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,2 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
-
-# Example usage
-arr = [1, 3, 5, 7, 9, 11, 13, 15]
-target = 9
-result = binary_search(arr, target)
-
-if result != -1:
-    print(f"Target found at index {result}")
-else:
-    print("Target not found in the array")
+string_with_spaces = "This is a string with spaces"
+string_without_spaces = string_with_spaces.replace(" ", "")
