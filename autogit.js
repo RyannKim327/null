508181a9bea2499923c71bb6166ff098e4097399
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:27:04 +0800

    11-23-2024 8:26 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,11 @@
-def longest_common_substring(str1, str2):
-    m = len(str1)
-    n = len(str2)
+# Define a list of numbers
+numbers = [2, 4, 6, 8, 10]
 
-    # Create a 2D table to store the lengths of longest common suffixes of substrings
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
+# Calculate the sum of all numbers in the list
+sum_of_numbers = sum(numbers)
 
-    max_length = 0
-    end_index = 0
+# Calculate the mean by dividing the sum by the total number of elements
+mean = sum_of_numbers / len(numbers)
 
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-                if dp[i][j] > max_length:
-                    max_length = dp[i][j]
-                    end_index = i
-            else:
-                dp[i][j] = 0
-
-    start_index = end_index - max_length
-    return str1[start_index:end_index]
-
-# Example
-str1 = "abcdef"
-str2 = "bcdf"
-print(longest_common_substring(str1, str2))
+# Print the mean
+print("Mean of the list is:", mean)
