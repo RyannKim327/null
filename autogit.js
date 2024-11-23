e380c55c36b5b74bb902b2e14ea0918a431f2d56
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:27:19 +0800

    11-23-2024 8:27 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,14 @@
-def boyer_moore(text, pattern):
-    def bad_character_table(pattern):
-        table = {}
-        for i in range(len(pattern) - 1):
-            table[pattern[i]] = len(pattern) - 1 - i
-        return table
+import requests
 
-    def good_suffix_table(pattern):
-        table = [0] * (len(pattern) + 1)
-        i = len(pattern)
-        j = len(pattern) + 1
-        table[i] = j
+# API endpoint URL
+url = 'https://jsonplaceholder.typicode.com/posts/1'
 
-        while i > 0:
-            while j <= len(pattern) and pattern[i - 1] != pattern[j - 1]:
-                if table[j] == 0:
-                    table[j] = j - i
-                j = table[j]
+# Making a GET request to the API
+response = requests.get(url)
 
-            i -= 1
-            j -= 1
-            table[i] = j
-
-        return table
-
-    bad_char = bad_character_table(pattern)
-    good_suffix = good_suffix_table(pattern)
-    i = 0
-
-    while i <= len(text) - len(pattern):
-        j = len(pattern) - 1
-
-        while j >= 0 and pattern[j] == text[i + j]:
-            j -= 1
-
-        if j < 0:
-            print("Pattern found at index", i)
-            i += good_suffix[0]
-        else:
-            if text[i + j] in bad_char:
-                i += max(good_suffix[j+1], j - bad_char[text[i + j]])
-            else:
-                i += good_suffix[j]
-
-    return -1
-
-# Usage
-text = "ABAAABCD"
-pattern = "ABC"
-boyer_moore(text, pattern)
+# Checking if the request was successful
+if response.status_code == 200:
+    # Printing the response content
+    print(response.json())
+else:
+    print('An error occurred with status code:', response.status_code)
