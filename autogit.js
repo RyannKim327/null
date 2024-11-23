8bf41ff3b57e09cf2d47a3dd7928052e491a9b36
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:32:57 +0800

    11-23-2024 8:32 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,12 @@
-# Sorting an array of integers in Python
-arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-arr.sort()
-print(arr)
-// Sorting an array of integers in JavaScript
-let arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-arr.sort((a, b) => a - b);
-console.log(arr);
-// Sorting an array of integers in Java
-import java.util.Arrays;
+def my_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
 
-int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-Arrays.sort(arr);
-System.out.println(Arrays.toString(arr));
-// Sorting an array of integers in C++
-#include <algorithm>
-#include <iostream>
-
-int main() {
-    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    std::sort(arr, arr + n);
-    
-    for (int i = 0; i < n; i++) {
-        std::cout << arr[i] << " ";
-    }
-    
-    return 0;
-}
-// Sorting an array of integers in C#
-using System;
-
-int[] arr = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-Array.Sort(arr);
-Console.WriteLine(string.Join(" ", arr));
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = my_sort(arr)
+print("Sorted array:", sorted_arr)
