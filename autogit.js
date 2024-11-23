cd26b30b1c958c7335aba1b5aa9245228d6bb941
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:30:03 +0800

    11-23-2024 8:30 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,7 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def length(self, node):
-        if node is None:
-            return 0
-        return 1 + self.length(node.next)
-
-# Create a sample linked list
-llist = LinkedList()
-llist.head = Node(1)
-second = Node(2)
-third = Node(3)
-
-llist.head.next = second
-second.next = third
-
-print("Length of the linked list is:", llist.length(llist.head))
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
