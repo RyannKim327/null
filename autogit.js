a6b61fc3d0ae0e55b5ef9d53fd4f681891869313
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:30:00 +0800

    11-23-2024 8:29 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
 
@@ -7,41 +7,18 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
+    def length(self, node):
+        if node is None:
+            return 0
+        return 1 + self.length(node.next)
 
-    def find_nth_from_end(self, n):
-        length = 0
-        current = self.head
-        while current:
-            length += 1
-            current = current.next
-
-        if n > length or n < 1:
-            print("Invalid value of n")
-            return
-
-        target_index = length - n
-        current = self.head
-        for i in range(target_index):
-            current = current.next
-
-        print(f"The {n}th node from the end is: {current.data}")
-
-# Create a linked list
+# Create a sample linked list
 llist = LinkedList()
-llist.append(1)
-llist.append(2)
-llist.append(3)
-llist.append(4)
-llist.append(5)
+llist.head = Node(1)
+second = Node(2)
+third = Node(3)
 
-# Find the nth node from the end
-llist.find_nth_from_end(2)
+llist.head.next = second
+second.next = third
+
+print("Length of the linked list is:", llist.length(llist.head))
