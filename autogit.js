f688a65028ac2f0eed37443fd270aa1a51fd5f19
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:23:53 +0800

    11-23-2024 8:23 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,12 @@
-def kmp_search(text, pattern):
-    L = len(pattern)
-    table = [0] * L
-    j = 0
+def count_character_occurrences(input_string, target_character):
+    count = 0
+    for char in input_string:
+        if char == target_character:
+            count += 1
+    return count
 
-    compute_prefix_table(pattern, L, table)
+input_string = "Hello, World!"
+target_character = "o"
 
-    i = 0
-    while i < len(text):
-        if pattern[j] == text[i]:
-            i += 1
-            j += 1
-
-            if j == L:
-                print("Pattern found at index " + str(i - j))
-                j = table[j - 1]
-        else:
-            if j != 0:
-                j = table[j - 1]
-            else:
-                i += 1
-
-def compute_prefix_table(pattern, L, table):
-    length = 0
-    table[0] = 0
-    i = 1
-
-    while i < L:
-        if pattern[length] == pattern[i]:
-            length += 1
-            table[i] = length
-            i += 1
-        else:
-            if length != 0:
-                length = table[length - 1]
-            else:
-                table[i] = 0
-                i += 1
-
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-kmp_search(text, pattern)
+occurrences = count_character_occurrences(input_string, target_character)
+print(f"The character '{target_character}' occurs {occurrences} times in the string.")
