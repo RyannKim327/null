9dd55fda32775b3f9f793b0a771efac9295a71dd
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:26:16 +0800

    11-23-2024 8:26 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,95 +1,32 @@
-class Node:
-    def __init__(self, key, color, parent, left, right):
-        self.key = key
-        self.color = color
-        self.parent = parent
-        self.left = left
-        self.right = right
+def beam_search(start_state, beam_width, max_steps):
+    beam = [(start_state, 0)]  # initialize beam with the start state and its initial score
+    for _ in range(max_steps):
+        new_beam = []
+        for state, score in beam:
+            # Generate possible next states from the current state
+            next_states = generate_next_states(state)
+            for next_state in next_states:
+                new_score = score + score_fn(next_state)
+                new_beam.append((next_state, new_score))
+        
+        # Keep only the top beam_width states
+        new_beam = sorted(new_beam, key=lambda x: x[1], reverse=True)[:beam_width]
+        beam = new_beam
+    
+    # Return the best state found
+    return max(beam, key=lambda x: x[1])[0]
 
-class RedBlackTree:
-    def __init__(self):
-        self.NIL = Node(key=None, color='BLACK', parent=None, left=None, right=None)
-        self.root = self.NIL
+# Example functions
+def generate_next_states(state):
+    # Generate possible next states from the current state
+    pass
 
-    def left_rotate(self, x):
-        y = x.right
-        x.right = y.left
-        if y.left != self.NIL:
-            y.left.parent = x
-        y.parent = x.parent
-        if x.parent == self.NIL:
-            self.root = y
-        elif x == x.parent.left:
-            x.parent.left = y
-        else:
-            x.parent.right = y
-        y.left = x
-        x.parent = y
+def score_fn(state):
+    # Calculate the score for a given state
+    pass
 
-    def right_rotate(self, x):
-        y = x.left
-        x.left = y.right
-        if y.right != self.NIL:
-            y.right.parent = x
-        y.parent = x.parent
-        if x.parent == self.NIL:
-            self.root = y
-        elif x == x.parent.right:
-            x.parent.right = y
-        else:
-            x.parent.left = y
-        y.right = x
-        x.parent = y
-
-    def insert_fixup(self, z):
-        while z.parent.color == 'RED':
-            if z.parent == z.parent.parent.left:
-                y = z.parent.parent.right
-                if y.color == 'RED':
-                    z.parent.color = 'BLACK'
-                    y.color = 'BLACK'
-                    z.parent.parent.color = 'RED'
-                    z = z.parent.parent
-                else:
-                    if z == z.parent.right:
-                        z = z.parent
-                        self.left_rotate(z)
-                    z.parent.color = 'BLACK'
-                    z.parent.parent.color = 'RED'
-                    self.right_rotate(z.parent.parent)
-            else:
-                y = z.parent.parent.left
-                if y.color == 'RED':
-                    z.parent.color = 'BLACK'
-                    y.color = 'BLACK'
-                    z.parent.parent.color = 'RED'
-                    z = z.parent.parent
-                else:
-                    if z == z.parent.left:
-                        z = z.parent
-                        self.right_rotate(z)
-                    z.parent.color = 'BLACK'
-                    z.parent.parent.color = 'RED'
-                    self.left_rotate(z.parent.parent)
-        self.root.color = 'BLACK'
-
-    def insert(self, key):
-        z = Node(key=key, color='RED', parent=self.NIL, left=self.NIL, right=self.NIL)
-        y = self.NIL
-        x = self.root
-        while x != self.NIL:
-            y = x
-            if z.key < x.key:
-                x = x.left
-            else:
-                x = x.right
-        z.parent = y
-        if y == self.NIL:
-            self.root = z
-        elif z.key < y.key:
-            y.left = z
-        else:
-            y.right = z
-        z.left = self.NIL
-        z.right = self.NIL
-        self.insert_fixup(z)
+# Usage
+start_state = initial_state
+beam_width = 5
+max_steps = 10
+best_state = beam_search(start_state, beam_width, max_steps)
