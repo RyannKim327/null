7eb02d2fb94cf18770d3bfe8e015b4c913d31083
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:26:25 +0800

    11-23-2024 8:26 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,39 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+from collections import deque
+
+def breadth_limited_search(graph, start, goal, depth_limit):
+    queue = deque([(start, 0)])
+    
+    while queue:
+        node, depth = queue.popleft()
+        
+        if node == goal:
+            return True
+        
+        if depth < depth_limit:
+            neighbors = graph.get(node, [])
+            for neighbor in neighbors:
+                queue.append((neighbor, depth + 1))
+    
+    return False
 
 # Example usage
-arr = [3, 6, 8, 10, 1, 2, 1]
-print(quicksort(arr))
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['G'],
+    'F': ['H'],
+    'G': [],
+    'H': ['I'],
+    'I': []
+}
+
+start_node = 'A'
+goal_node = 'I'
+depth_limit = 3
+
+if breadth_limited_search(graph, start_node, goal_node, depth_limit):
+    print("Goal found within depth limit.")
+else:
+    print("Goal not found within depth limit.")
