908f8cc90769604c58cbbd69c0a8ab7ca03bbb09
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:34:18 +0800

    11-23-2024 8:34 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,8 @@
-my_string = "Hello, World!"
-lowercase_string = my_string.lower()
-print(lowercase_string)
+def reverse_words(input_string):
+    words = input_string.split()
+    reversed_words = " ".join(reversed(words))
+    return reversed_words
+
+input_string = "Hello World! This is a test."
+reversed_string = reverse_words(input_string)
+print(reversed_string)
