eb94ad3eb995bb76a6bf694e656b803e248d656d
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:32:09 +0800

    11-23-2024 8:32 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,30 @@
-def is_palindrome(s):
-    s = ''.join(e for e in s if e.isalnum()).lower()
-    return s == s[::-1]
-
-# Test the function
-input_string = "A man, a plan, a canal, Panama"
-result = is_palindrome(input_string)
-if result:
-    print("The input string is a palindrome.")
-else:
-    print("The input string is not a palindrome.")
+class HashTable:
+    def __init__(self, size):
+        self.size = size
+        self.table = [[] for _ in range(size)]
+    
+    def _hash_function(self, key):
+        return hash(key) % self.size
+    
+    def insert(self, key, value):
+        index = self._hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                pair[1] = value
+                return
+        self.table[index].append([key, value])
+    
+    def get(self, key):
+        index = self._hash_function(key)
+        for pair in self.table[index]:
+            if pair[0] == key:
+                return pair[1]
+        raise KeyError(key)
+    
+    def delete(self, key):
+        index = self._hash_function(key)
+        for i, pair in enumerate(self.table[index]):
+            if pair[0] == key:
+                del self.table[index][i]
+                return
+        raise KeyError(key)
