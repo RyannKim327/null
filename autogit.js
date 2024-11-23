6934445d3be990fbddc2b231a74ca437abccade8
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:14:40 +0800

    11-23-2024 8:14 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,23 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-num = 17  # Change this to the number you want to check
-if is_prime(num):
-    print(f"{num} is a prime number.")
-else:
-    print(f"{num} is not a prime number.")
+def count_leaf_nodes(node):
+    if node is None:
+        return 0
+    if node.left is None and node.right is None:
+        return 1
+    return count_leaf_nodes(node.left) + count_leaf_nodes(node.right)
+
+# Example binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+print("Number of leaf nodes in the binary tree:", count_leaf_nodes(root))
