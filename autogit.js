8f3afbff568a99f28d318ed21fdccb1ec719c9a8
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:25:07 +0800

    11-23-2024 8:25 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,4 @@
-function isPalindrome(s):
-    n = length of s
-    left = 0
-    right = n - 1
-    
-    while left < right:
-        if s[left] != s[right]:
-            return false
-        left = left + 1
-        right = right - 1
-        
-    return true
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log('Error:', error));
