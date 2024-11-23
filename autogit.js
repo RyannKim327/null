0fd9de7f9a491d31b8554478ba70152ce7575a14
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:17:36 +0800

    11-23-2024 8:17 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++
@@ -1,8 +0,0 @@
-from suffix_tree import Tree
-
-# Create an instance of the Suffix Tree
-tree = Tree("banana")
-
-# Search for a substring in the Suffix Tree
-indexes = tree.search('na')
-print("Substring 'na' found at indexes:", indexes)
