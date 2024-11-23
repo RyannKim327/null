c3d3d0277829e269a12f5eda1e338db3cb937741
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:35:26 +0800

    11-23-2024 8:35 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,8 @@
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
-
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
-
-    def bellman_ford(self, src):
-        dist = [float('inf')] * self.V
-        dist[src] = 0
-
-        for _ in range(self.V - 1):
-            for u, v, w in self.graph:
-                if dist[u] != float('inf') and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-
-        for u, v, w in self.graph:
-            if dist[u] != float('inf') and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
-
-        for i in range(self.V):
-            print(f"Distance from {src} to {i} is {dist[i]}")
-
-g = Graph(5)
-g.add_edge(0, 1, -1)
-g.add_edge(0, 2, 4)
-g.add_edge(1, 2, 3)
-g.add_edge(1, 3, 2)
-g.add_edge(1, 4, 2)
-g.add_edge(3, 2, 5)
-g.add_edge(3, 1, 1)
-g.add_edge(4, 3, -3)
-
-g.bellman_ford(0)
+bool isSorted(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
