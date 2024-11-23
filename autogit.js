ae639e913e301f677337d004a7c28d6521cd86b7
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:30:52 +0800

    11-23-2024 8:30 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,12 @@
-from collections import deque
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
-    
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            print(node)
-            queue.extend([neighbor for neighbor in graph[node] if neighbor not in visited])
-            
-# Example graph represented as an adjacency list
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-bfs(graph, 'A')
+def count_leaf_nodes(root):
+    if root is None:
+        return 0
+    if root.left is None and root.right is None:
+        return 1
+    return count_leaf_nodes(root.left) + count_leaf_nodes(root.right)
