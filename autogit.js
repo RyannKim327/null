b77241bfccdd622023f56c416d05381d20f0af8e
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:23:58 +0800

    11-23-2024 8:23 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,10 @@
-def count_character_occurrences(input_string, target_character):
-    count = 0
-    for char in input_string:
-        if char == target_character:
-            count += 1
-    return count
+def reverse_words_in_string(input_string):
+    words = input_string.split()
+    reversed_words = words[::-1]
+    reversed_string = ' '.join(reversed_words)
+    return reversed_string
 
-input_string = "Hello, World!"
-target_character = "o"
-
-occurrences = count_character_occurrences(input_string, target_character)
-print(f"The character '{target_character}' occurs {occurrences} times in the string.")
+# Example usage
+input_string = "Hello, world! This is a test."
+reversed_string = reverse_words_in_string(input_string)
+print(reversed_string)
