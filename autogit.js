b4c76b353b406de007c885f500d817572085402a
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:20:00 +0800

    11-23-2024 8:19 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,9 @@
-my_array = [1, 2, 3, 4, 5]
-reversed_array = my_array[::-1]
-print(reversed_array)
+def is_sorted_ascending(arr):
+    return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
+
+# Example usage
+array = [1, 2, 3, 4, 5]
+if is_sorted_ascending(array):
+    print("The array is sorted in ascending order")
+else:
+    print("The array is not sorted in ascending order")
