1923755557b2be30b38d48d86372de18c96e35b6
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:28:31 +0800

    11-23-2024 8:28 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,69 +1,14 @@
-#include <iostream>
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-struct Node {
-    int key;
-    Node* left;
-    Node* right;
-    
-    Node(int k) : key(k), left(nullptr), right(nullptr) {}
-};
+# Input a number
+num = int(input("Enter a number: "))
 
-class BinaryTree {
-private:
-    Node* root;
-
-    Node* insertRec(Node* root, int key) {
-        if (root == nullptr) {
-            return new Node(key);
-        }
-        
-        if (key < root->key) {
-            root->left = insertRec(root->left, key);
-        } else if (key > root->key) {
-            root->right = insertRec(root->right, key);
-        }
-        
-        return root;
-    }
-
-    Node* searchRec(Node* root, int key) {
-        if (root == nullptr || root->key == key) {
-            return root;
-        }
-        
-        if (key < root->key) {
-            return searchRec(root->left, key);
-        }
-        
-        return searchRec(root->right, key);
-    }
-
-public:
-    BinaryTree() : root(nullptr) {}
-
-    void insert(int key) {
-        root = insertRec(root, key);
-    }
-
-    Node* search(int key) {
-        return searchRec(root, key);
-    }
-};
-
-int main() {
-    BinaryTree tree;
-
-    tree.insert(5);
-    tree.insert(3);
-    tree.insert(7);
-    
-    // Search for a key
-    Node* result = tree.search(3);
-    if (result != nullptr) {
-        std::cout << "Key found in the tree" << std::endl;
-    } else {
-        std::cout << "Key not found in the tree" << std::endl;
-    }
-
-    return 0;
-}
+# Check if the number is negative
+if num < 0:
+    print("Factorial is not defined for negative numbers")
+else:
+    print("Factorial of", num, "is", factorial(num))
