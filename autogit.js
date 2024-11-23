4b18b320c141fc58df4b482db019fefff01049dd
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:22:03 +0800

    11-23-2024 8:21 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,5 @@
-def decimal_to_binary(decimal):
-    if decimal == 0:
-        return '0'
-    binary = ''
-    while decimal > 0:
-        binary = str(decimal % 2) + binary
-        decimal = decimal // 2
-    return binary
-
-decimal_number = 10
-binary_number = decimal_to_binary(decimal_number)
-print(f"The binary representation of {decimal_number} is: {binary_number}")
+// Make a GET request using Fetch API
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
