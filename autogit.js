a141ac9d269b83b6e91641617d0ad55bf8c809b3
RyannKim327 <rksesgundo123@gmail.com>
2024-11-23 08:30:08 +0800

    11-23-2024 8:30 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,20 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def getIntersectionNode(headA, headB):
+    node_set = set()
+    
+    curr = headA
+    while curr:
+        node_set.add(curr)
+        curr = curr.next
+    
+    curr = headB
+    while curr:
+        if curr in node_set:
+            return curr
+        curr = curr.next
+    
+    return None
