e21b030ceaa2000fdc18a9f3ca37cd88b955af93
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:21:08 +0800

    11-24-2024 21:21 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,55 @@
-def build_lps(pattern):
-    m = len(pattern)
-    lps = [0] * m
-    j = 0
+class TarjansSCC:
+    def __init__(self, graph):
+        self.graph = graph
+        self.num_vertices = len(graph)
+        self.index = 0
+        self.lowlink = [float('inf')] * self.num_vertices
+        self.visited = [False] * self.num_vertices
+        self.stack = []
+        self.scc_components = []
 
-    for i in range(1, m):
-        while j > 0 and pattern[i] != pattern[j]:
-            j = lps[j - 1]
-        if pattern[i] == pattern[j]:
-            j += 1
-        lps[i] = j
+    def tarjan_scc(self):
+        for v in range(self.num_vertices):
+            if not self.visited[v]:
+                self._tarjan_scc(v)
 
-    return lps
+    def _tarjan_scc(self, v):
+        self.lowlink[v] = self.index
+        self.index += 1
+        self.visited[v] = True
+        self.stack.append(v)
+        for w in self.graph[v]:
+            if not self.visited[w]:
+                self._tarjan_scc(w)
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+            elif w in self.stack:
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+        if self.lowlink[v] == v:
+            component = []
+            while True:
+                w = self.stack.pop()
+                component.append(w)
+                if w == v:
+                    break
+            self.scc_components.append(component)
 
-def kmp_search(text, pattern):
-    n = len(text)
-    m = len(pattern)
-    lps = build_lps(pattern)
-    j = 0
+# Example graph
+graph = {
+    0: [1],
+    1: [2],
+    2: [0, 3],
+    3: [4],
+    4: [5],
+    5: [3, 6],
+    6: [7],
+    7: [8, 10],
+    8: [6],
+    9: [7],
+    10: [11],
+    11: [9],
+}
 
-    for i in range(n):
-        while j > 0 and text[i] != pattern[j]:
-            j = lps[j - 1]
-        if text[i] == pattern[j]:
-            j += 1
-        if j == m:
-            return i - m + 1
-    return -1
+tarjan = TarjansSCC(graph)
+tarjan.tarjan_scc()
 
-# Example usage
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-index = kmp_search(text, pattern)
-
-if index != -1:
-    print("Pattern found at index:", index)
-else:
-    print("Pattern not found")
+print(tarjan.scc_components)
