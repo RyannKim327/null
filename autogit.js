6be532fa4828fc4d2234cc0e14907d857bd6860b
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:27:27 +0800

    11-24-2024 21:27 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,35 @@
-original_string = "  Hello,   World!  "
-modified_string = original_string.replace(" ", "")
-print(modified_string)
-Hello,World!
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    left = merge_sort(left)
+    right = merge_sort(right)
+    
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_idx = 0
+    right_idx = 0
+    
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            result.append(left[left_idx])
+            left_idx += 1
+        else:
+            result.append(right[right_idx])
+            right_idx += 1
+            
+    result += left[left_idx:]
+    result += right[right_idx:]
+    
+    return result
+
+# Example usage
+arr = [12, 11, 13, 5, 6, 7]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
