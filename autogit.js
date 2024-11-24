18707903d7c72478e3ad44d82ae1c13f4199b1ae
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:24:48 +0800

    11-24-2024 21:24 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,24 @@
-def counting_sort(arr):
-    max_val = max(arr)
-    count = [0] * (max_val + 1)
-    output = [0] * len(arr)
-    
-    for num in arr:
-        count[num] += 1
-    
-    for i in range(1, len(count)):
-        count[i] += count[i - 1]
-    
-    for num in reversed(arr):
-        output[count[num] - 1] = num
-        count[num] -= 1
-    
-    return output
+from collections import defaultdict, deque
 
-# Example usage
-arr = [4, 2, 2, 8, 3, 3, 1]
-sorted_arr = counting_sort(arr)
-print(sorted_arr)
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node)
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+# Example graph representation using a dictionary
+graph = defaultdict(list)
+graph[0] = [1, 2]
+graph[1] = [2]
+graph[2] = [0, 3]
+graph[3] = [3]
+
+bfs(graph, 2)
