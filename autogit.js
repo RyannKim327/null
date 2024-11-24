50bfeac92757c23ecd0c9452fd64b9967b053029
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:27:19 +0800

    11-24-2024 21:27 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,10 @@
-def heapify(arr, n, i):
-    largest = i
-    left = 2*i + 1
-    right = 2*i + 2
-    
-    if left < n and arr[left] > arr[largest]:
-        largest = left
-        
-    if right < n and arr[right] > arr[largest]:
-        largest = right
-        
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-        
-def heap_sort(arr):
-    n = len(arr)
-    
-    for i in range(n//2 - 1, -1, -1):
-        heapify(arr, n, i)
-        
-    for i in range(n-1, 0, -1):
-        arr[0], arr[i] = arr[i], arr[0]
-        heapify(arr, i, 0)
-        
-    return arr
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-# Example usage
-arr = [12, 11, 13, 5, 6, 7]
-sorted_arr = heap_sort(arr)
-print("Sorted array is", sorted_arr)
+# Test the function
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
