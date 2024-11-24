00a78b4f177590c7e1da7cbb468a8b82ff42fd3c
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:27:50 +0800

    11-24-2024 21:27 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,60 @@
-def reverse_words(sentence):
-    words = sentence.split()
-    reversed_sentence = ' '.join(reversed(words))
-    return reversed_sentence
+#include <iostream>
+#include <stack>
 
-# Example usage
-sentence = "Hello world! This is a string."
-reversed_sentence = reverse_words(sentence)
-print(reversed_sentence)
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+bool isPalindrome(Node* head) {
+    if (head == nullptr || head->next == nullptr) {
+        // An empty list or a list with one node is considered a palindrome
+        return true;
+    }
+
+    Node* slow = head;
+    Node* fast = head;
+    stack<int> firstHalf;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        firstHalf.push(slow->data);
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Handle odd number of nodes
+    if (fast != nullptr) {
+        slow = slow->next;
+    }
+
+    while (slow != nullptr) {
+        if (slow->data != firstHalf.top()) {
+            return false;
+        }
+        firstHalf.pop();
+        slow = slow->next;
+    }
+
+    return true;
+}
+
+int main() {
+    // Create a linked list: 1->2->3->2->1
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(2);
+    head->next->next->next->next = new Node(1);
+
+    if (isPalindrome(head)) {
+        cout << "Linked list is a palindrome." << endl;
+    } else {
+        cout << "Linked list is not a palindrome." << endl;
+    }
+
+    return 0;
+}
