c59f9c1e32136331b18944d5864a473cbd8fc38f
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:27:44 +0800

    11-24-2024 21:27 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,9 @@
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
-
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1
+def reverse_words(sentence):
+    words = sentence.split()
+    reversed_sentence = ' '.join(reversed(words))
+    return reversed_sentence
 
 # Example usage
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-target = 5
-result = binary_search(arr, target)
-
-if result != -1:
-    print(f"Element found at index {result}")
-else:
-    print("Element not found")
+sentence = "Hello world! This is a string."
+reversed_sentence = reverse_words(sentence)
+print(reversed_sentence)
