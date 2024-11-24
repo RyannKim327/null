321305e509d8acefcec69d95ac95fb415368c078
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:24:28 +0800

    11-24-2024 21:24 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,27 @@
-def heapify(arr, n, i):
-    largest = i 
-    l = 2 * i + 1
-    r = 2 * i + 2
+def interpolation_search(arr, x):
+    lo = 0
+    hi = len(arr) - 1
+    
+    while lo <= hi and x >= arr[lo] and x <= arr[hi]:
+        pos = lo + ((hi - lo) // (arr[hi] - arr[lo])) * (x - arr[lo])
+        
+        if arr[pos] == x:
+            return pos
+        
+        if arr[pos] < x:
+            lo = pos + 1
+        else:
+            hi = pos - 1
+    
+    return -1
 
-    if l < n and arr[l] > arr[largest]:
-        largest = l
+# Test the interpolation search algorithm
+arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+x = 10
 
-    if r < n and arr[r] > arr[largest]:
-        largest = r
+result = interpolation_search(arr, x)
 
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-# Example usage
-arr = [12, 11, 13, 5, 6, 7]
-heap_sort(arr)
-print("Sorted array is", arr)
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
