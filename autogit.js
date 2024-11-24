4063fbe3e41cf5a349fbc730e35288064be43577
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:25:51 +0800

    11-24-2024 21:25 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,4 @@
-def calculate_mean(numbers):
-    sum = 0
-    for num in numbers:
-        sum += num
-    mean = sum / len(numbers)
-    return mean
+decimal_number = 10
+binary_number = bin(decimal_number)[2:]
 
-# Example usage
-numbers = [1, 2, 3, 4, 5]
-mean = calculate_mean(numbers)
-print("Mean:", mean)
+print(f"The binary representation of {decimal_number} is: {binary_number}")
