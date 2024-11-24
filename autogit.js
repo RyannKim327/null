a2767ff11031f125b1530d61809844085de4d980
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:27:16 +0800

    11-24-2024 21:27 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,31 @@
-class BinaryHeap:
-    def __init__(self):
-        self.heap = []
+def heapify(arr, n, i):
+    largest = i
+    left = 2*i + 1
+    right = 2*i + 2
     
-    def insert(self, value):
-        self.heap.append(value)
-        self.heapify_up(len(self.heap) - 1)
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+        
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+        
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+        
+def heap_sort(arr):
+    n = len(arr)
     
-    def remove_max(self):
-        if not self.heap:
-            return None
+    for i in range(n//2 - 1, -1, -1):
+        heapify(arr, n, i)
         
-        max_val = self.heap[0]
-        last_val = self.heap.pop()
-        if self.heap:
-            self.heap[0] = last_val
-            self.heapify_down(0)
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
         
-        return max_val
-    
-    def heapify_up(self, index):
-        parent_index = (index - 1) // 2
-        if index > 0 and self.heap[index] > self.heap[parent_index]:
-            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-            self.heapify_up(parent_index)
-    
-    def heapify_down(self, index):
-        left_index = 2 * index + 1
-        right_index = 2 * index + 2
-        largest = index
-        
-        if left_index < len(self.heap) and self.heap[left_index] > self.heap[index]:
-            largest = left_index
-        if right_index < len(self.heap) and self.heap[right_index] > self.heap[largest]:
-            largest = right_index
-        
-        if largest != index:
-            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
-            self.heapify_down(largest)
+    return arr
 
-# Example Usage
-heap = BinaryHeap()
-heap.insert(3)
-heap.insert(5)
-heap.insert(1)
-heap.insert(7)
-print(heap.remove_max())  # Output: 7
-print(heap.remove_max())  # Output: 5
+# Example usage
+arr = [12, 11, 13, 5, 6, 7]
+sorted_arr = heap_sort(arr)
+print("Sorted array is", sorted_arr)
