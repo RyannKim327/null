fb291ad6e45fce86083777d1f3fede5c3db2ab78
RyannKim327 <rksesgundo123@gmail.com>
2024-11-24 21:25:29 +0800

    11-24-2024 21:25 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,27 @@
-from queue import PriorityQueue
+def findMedianSortedArrays(nums1, nums2):
+    merged_array = []
+    i, j = 0, 0
 
-def astar_search(graph, start, goal):
-    open_set = PriorityQueue()
-    open_set.put((0, start))
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not open_set.empty():
-        _, current = open_set.get()
-        
-        if current == goal:
-            path = []
-            while current is not None:
-                path.insert(0, current)
-                current = came_from[current]
-            return path
-        
-        for next_node in graph[current]:
-            new_cost = cost_so_far[current] + graph[current][next_node]
-            if next_node not in cost_so_far or new_cost < cost_so_far[next_node]:
-                cost_so_far[next_node] = new_cost
-                priority = new_cost + heuristic(next_node, goal)
-                open_set.put((priority, next_node))
-                came_from[next_node] = current
-                
-    return None
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] < nums2[j]:
+            merged_array.append(nums1[i])
+            i += 1
+        else:
+            merged_array.append(nums2[j])
+            j += 1
 
-def heuristic(node, goal):
-    # A simple heuristic function (e.g., Euclidean distance)
-    return abs(node[0] - goal[0]) + abs(node[1] - goal[1])
+    merged_array += nums1[i:]
+    merged_array += nums2[j:]
 
-# Example graph representation
-graph = {
-    (0, 0): {(0, 1): 1, (1, 0): 1},
-    (0, 1): {(0, 0): 1, (1, 1): 1},
-    (1, 0): {(0, 0): 1, (1, 1): 1},
-    (1, 1): {(0, 1): 1, (1, 0): 1}
-}
+    total_length = len(merged_array)
+    if total_length % 2 == 0:
+        mid = total_length // 2
+        return (merged_array[mid - 1] + merged_array[mid]) / 2
+    else:
+        return merged_array[total_length // 2]
 
-start = (0, 0)
-goal = (1, 1)
-
-path = astar_search(graph, start, goal)
-print(path)
+# Example usage
+nums1 = [1, 3]
+nums2 = [2]
+result = findMedianSortedArrays(nums1, nums2)
+print(result)
