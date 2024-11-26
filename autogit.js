579698fd7b2948c350ee0e50d0716222a0551020
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 07:21:58 +0800

    11-27-2024 7:21 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,16 @@
-text = "hello world hello"
-word = "hello"
-count = text.count(word)
-print(count)
-text = "hello world hello"
-word = "hello"
-count = 0
-for w in text.split():
-    if w == word:
-        count += 1
-print(count)
+def burrows_wheeler_transform(text):
+    # Create a list of all rotations of the text
+    rotations = [text[i:] + text[:i] for i in range(len(text))]
+    
+    # Sort the rotations lexicographically
+    rotations.sort()
+    
+    # Extract the last characters of each rotation to form the Burrows-Wheeler transformed text
+    bwt_transform = ''.join(rotation[-1] for rotation in rotations)
+    
+    return bwt_transform
+
+# Test the implementation
+text = 'banana'
+bwt_result = burrows_wheeler_transform(text)
+print("Burrows-Wheeler Transform of '{}' is '{}'".format(text, bwt_result))
