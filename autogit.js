e3f0e9b1adee755c834ddf0b1f707bc2b1d8b963
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 07:21:47 +0800

    11-27-2024 7:21 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,23 @@
-def reverse_words_order(input_string):
-    words = input_string.split()
-    reversed_words = words[::-1]
-    reversed_string = ' '.join(reversed_words)
-    return reversed_string
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-input_string = "Hello World How are you"
-reversed_string = reverse_words_order(input_string)
-print(reversed_string)
+def maxDepth(node):
+    if node is None:
+        return 0
+    else:
+        left_depth = maxDepth(node.left)
+        right_depth = maxDepth(node.right)
+
+        return max(left_depth, right_depth) + 1
+
+# Example binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Maximum depth of the binary tree is:", maxDepth(root))
