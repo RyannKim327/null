1d568d8dbb2be92980bb43f33acf607a9dfe1dc8
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 07:22:04 +0800

    11-27-2024 7:22 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,3 @@
-my_list = [1, 2, 3, 1, 2, 4, 5, 6, 5]
-
-# Removing duplicates
-unique_list = list(set(my_list))
-
-print(unique_list)
-[1, 2, 3, 4, 5, 6]
+arr = [5, 10, 3, 8, 15]
+max_value = max(arr)
+print("The maximum value in the array is:", max_value)
