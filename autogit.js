26eca3fd12f19d75a127bbffe15b01143eaf4c63
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 07:19:39 +0800

    11-27-2024 7:19 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,7 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+def remove_vowels(input_string):
+    vowels = "AEIOUaeiou"
+    return "".join([letter for letter in input_string if letter not in vowels])
 
-def max_depth(node):
-    if node is None:
-        return 0
-    else:
-        left_depth = max_depth(node.left)
-        right_depth = max_depth(node.right)
-
-        return max(left_depth, right_depth) + 1
-
-# Example binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Maximum depth of the binary tree is:", max_depth(root))
+input_string = "Hello, World!"
+output_string = remove_vowels(input_string)
+print(output_string)
