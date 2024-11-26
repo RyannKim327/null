e95a1805589f149347a84d28e83ea045d7f7ec4e
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 07:02:57 +0800

    11-27-2024 7:2 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,12 @@
-def counting_sort(arr):
-    max_val = max(arr)
-    count_arr = [0] * (max_val + 1)
-    sorted_arr = [0] * len(arr)
-    
-    # Count the occurrences of each element in the array
-    for num in arr:
-        count_arr[num] += 1
-    
-    # Calculate the cumulative count of elements
-    for i in range(1, len(count_arr)):
-        count_arr[i] += count_arr[i - 1]
-    
-    # Build the sorted array
-    for num in arr:
-        sorted_arr[count_arr[num] - 1] = num
-        count_arr[num] -= 1
-    
-    return sorted_arr
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
 
-# Test the counting sort algorithm
-arr = [4, 2, 2, 8, 3, 3, 1]
-sorted_arr = counting_sort(arr)
-print("Sorted array:", sorted_arr)
+number = 1234567890  # Enter the number for which you want to find the largest prime factor
+result = largest_prime_factor(number)
+print(result)
