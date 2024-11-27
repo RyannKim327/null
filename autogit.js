53591f98d0aec0378449c5804ccccbe9e80caf72
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:14:47 +0800

    11-27-2024 21:14 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,15 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
+import android.os.AsyncTask
+import java.net.URL
 
-class BinarySearchTree:
-    def __init__(self):
-        self.root = None
+class ConnectAsyncTask : AsyncTask<String, Void, String>() {
 
-    def insert(self, key):
-        if self.root is None:
-            self.root = Node(key)
-        else:
-            self._insert_recursive(self.root, key)
+    override fun doInBackground(vararg urls: String): String {
+        val url = URL(urls[0])
+        val connection = url.openConnection() as HttpURLConnection
+        return connection.inputStream.bufferedReader().use { it.readText() }
+    }
 
-    def _insert_recursive(self, node, key):
-        if key < node.key:
-            if node.left is None:
-                node.left = Node(key)
-            else:
-                self._insert_recursive(node.left, key)
-        elif key > node.key:
-            if node.right is None:
-                node.right = Node(key)
-            else:
-                self._insert_recursive(node.right, key)
-
-    def search(self, key):
-        return self._search_recursive(self.root, key)
-
-    def _search_recursive(self, node, key):
-        if node is None or node.key == key:
-            return node
-        if key < node.key:
-            return self._search_recursive(node.left, key)
-        return self._search_recursive(node.right, key)
+    override fun onPostExecute(result: String) {
+        // Process the result here
+    }
+}
