d3404ec17ba7aa1cf6ca59f93b56bf6a7c831382
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:23:19 +0800

    11-27-2024 21:23 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,29 @@
-# Original array with duplicate elements
-arr = [1, 2, 3, 3, 4, 5, 2]
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-# Removing duplicates
-arr = list(set(arr))
+    if left < n and arr[i] < arr[left]:
+        largest = left
 
-print(arr)
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# Example usage:
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is", arr)
