05bef7263caf766c3e4560ef35d2ba8f0db79424
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:18:13 +0800

    11-27-2024 21:18 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,22 @@
-import statistics
+def max_subarray_sum(arr):
+    max_ending_here = max_so_far = arr[0]
+    start = end = 0
+    temp_start = 0
+    
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i] + max_ending_here:
+            temp_start = i
+            max_ending_here = arr[i]
+        else:
+            max_ending_here += arr[i]
+        
+        if max_ending_here > max_so_far:
+            start = temp_start
+            end = i
+            max_so_far = max_ending_here
+            
+    return arr[start:end+1]
 
-# List of numbers
-num_list = [1, 2, 3, 4, 5]
-
-# Calculate the mean
-mean = statistics.mean(num_list)
-
-# Print the mean
-print("Mean:", mean)
+# Test the function
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print("Maximum sum subarray:", max_subarray_sum(arr))
