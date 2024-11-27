696256a9cd1c2664ecb06bbd587136edb5052a09
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:17:00 +0800

    11-27-2024 21:16 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,21 @@
-const axios = require('axios');
+def depth_first_search(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            depth_first_search(graph, neighbor, visited)
 
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+# Example graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+start_node = 'A'
+depth_first_search(graph, start_node)
