b0b71d60a3e4f5297dd22c5db37a38c896262294
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:29:43 +0800

    11-27-2024 21:29 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,8 @@
-def burrows_wheeler_transform(text):
-    text = text + "$"  # Append a special character at the end of the text
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-    # Generate all possible rotations of the text
-    rotations = [text[i:] + text[:i] for i in range(len(text))]
-
-    # Sort the rotations lexicographically
-    rotations.sort()
-
-    # Extract the last characters of each rotation to get the Burrows-Wheeler Transform
-    bwt = "".join(rotation[-1] for rotation in rotations)
-
-    # Find the index of the original text in the sorted list of rotations
-    original_index = rotations.index(text)
-
-    return bwt, original_index
-
-# Example usage
-text = "banana"
-bwt, original_index = burrows_wheeler_transform(text)
-print("Burrows-Wheeler Transform:", bwt)
-print("Original text index:", original_index)
+num = 5
+print("Factorial of", num, "is", factorial(num))
