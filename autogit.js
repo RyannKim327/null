1ca835c47ce911b8bb0be4e7b8cb08f0c995e0c1
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 20:30:43 +0800

    11-27-2024 20:30 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,23 @@
-from croniter import croniter
-from datetime import datetime
+def max_subarray_sum(arr):
+    max_ending_here = max_so_far = arr[0]
+    start = end = 0
+    temp_start = 0
 
-# Define the cron schedule
-schedule = '*/1 * * * *'  # Run every minute
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i] + max_ending_here:
+            temp_start = i
+            max_ending_here = arr[i]
+        else:
+            max_ending_here += arr[i]
 
-# Create a cron iterator object
-cron = croniter(schedule, datetime.now())
+        if max_ending_here > max_so_far:
+            start = temp_start
+            end = i
+            max_so_far = max_ending_here
 
-# Get the next 5 execution times
-next_runtimes = [cron.get_next(datetime) for _ in range(5)]
+    return arr[start:end+1], max_so_far
 
-print("Next 5 runtimes:")
-for runtime in next_runtimes:
-    print(runtime)
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+subarray, max_sum = max_subarray_sum(arr)
+print("Maximum sum subarray is:", subarray)
+print("Maximum sum is:", max_sum)
