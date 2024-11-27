327cf16d034ad08ebdc7e1f319dd7c19f46c45cc
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:13:47 +0800

    11-27-2024 21:13 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,14 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-    
-    return merge(left, right)
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
 
-def merge(left, right):
-    result = []
-    i = j = 0
-    
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    
-    result.extend(left[i:])
-    result.extend(right[j:])
-    
-    return result
+    min_len = min(map(len, strings))
+    for i in range(min_len):
+        if any(strings[j][i] != strings[0][i] for j in range(1, len(strings))):
+            return strings[0][:i]
+
+    return strings[0][:min_len]
 
 # Example usage
-arr = [38, 27, 43, 3, 9, 82, 10]
-sorted_arr = merge_sort(arr)
-print(sorted_arr)
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings))
