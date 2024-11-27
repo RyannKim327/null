68a17f2133592553375f951bb241cfdfaae8f9b1
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:27:49 +0800

    11-27-2024 21:27 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,34 @@
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
+def longest_common_subsequence(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    # Initialize a 2D table to store the lengths of longest common subsequences
+    dp = [[0]*(n+1) for _ in range(m+1)]
+    
+    # Fill the DP table
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+    # Reconstruct the longest common subsequence
+    lcs = ""
+    i, j = m, n
+    while i > 0 and j > 0:
+        if str1[i-1] == str2[j-1]:
+            lcs = str1[i-1] + lcs
+            i -= 1
+            j -= 1
+        elif dp[i-1][j] > dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    
+    return lcs
 
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
-
-    def print_solution(self, dist):
-        print("Vertex Distance from Source")
-        for i in range(self.V):
-            print("{0}\t\t{1}".format(i, dist[i]))
-
-    def bellman_ford(self, src):
-        dist = [float("Inf")] * self.V
-        dist[src] = 0
-
-        for _ in range(self.V - 1):
-            for u, v, w in self.graph:
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-
-        for u, v, w in self.graph:
-            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
-
-        self.print_solution(dist)
-g = Graph(5)
-g.add_edge(0, 1, -1)
-g.add_edge(0, 2, 4)
-g.add_edge(1, 2, 3)
-g.add_edge(1, 3, 2)
-g.add_edge(1, 4, 2)
-g.add_edge(3, 2, 5)
-g.add_edge(3, 1, 1)
-g.add_edge(4, 3, -3)
-
-g.bellman_ford(0)
+# Example usage
+str1 = "abcde"
+str2 = "ace"
+print(longest_common_subsequence(str1, str2))  # Output: "ace"
