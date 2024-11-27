480009b97393c5d6e9b03ec4657d631e88b1baaa
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:27:32 +0800

    11-27-2024 21:27 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,38 @@
-def calculate_mean(numbers):
-    return sum(numbers) / len(numbers)
+from collections import deque
 
-# Example list of numbers
-numbers = [1, 2, 3, 4, 5]
+def bfs(graph, start, goal):
+    queue = deque()
+    queue.append(start)
+    visited = set()
+    
+    while queue:
+        node = queue.popleft()
+        if node == goal:
+            return True
+        
+        if node not in visited:
+            visited.add(node)
+            neighbors = graph[node]
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    
+    return False
 
-# Calculate the mean
-mean = calculate_mean(numbers)
-print("Mean:", mean)
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+start_node = 'A'
+goal_node = 'F'
+
+if bfs(graph, start_node, goal_node):
+    print("Path found from {} to {}: {}".format(start_node, goal_node, "Yes"))
+else:
+    print("No path found from {} to {}".format(start_node, goal_node))
