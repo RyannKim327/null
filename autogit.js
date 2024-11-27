cabc8601fd8bad6b516f7845746ab9113b5c9a97
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:24:30 +0800

    11-27-2024 21:24 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,36 @@
-def is_sorted_ascending(arr):
-    for i in range(len(arr) - 1):
-        if arr[i] > arr[i+1]:
-            return False
-    return True
+def merge_sort_iterative(arr):
+    n = len(arr)
+    width = 1
+    
+    while width < n:
+        left = 0
+        while left < n:
+            mid = left + width
+            right = min(left + 2*width, n)
+            merged = merge(arr[left:mid], arr[mid:right])
+            arr[left:right] = merged
+            left += 2*width
+        
+        width *= 2
 
-# Test the function
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [5, 4, 3, 2, 1]
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    
+    result.extend(left[i:])
+    result.extend(right[j:])
+    
+    return result
 
-print(is_sorted_ascending(arr1))  # True
-print(is_sorted_ascending(arr2))  # False
+# Example usage
+arr = [38, 27, 43, 3, 9, 82, 10]
+merge_sort_iterative(arr)
+print(arr)
