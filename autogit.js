ff2de9a905d2927fe331a78a5435d50cce3fff4f
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:22:26 +0800

    11-27-2024 21:22 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,14 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_index = i
+        min_idx = i
         for j in range(i+1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
-# Example Usage
-arr = [64, 25, 12, 22, 11]
+# Test the selection sort algorithm
+arr = [64, 34, 25, 12, 22, 11, 90]
 sorted_arr = selection_sort(arr)
 print("Sorted array is:", sorted_arr)
