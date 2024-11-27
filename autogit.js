2e9e49bec1ae77bb5b050a4a40e7428e879d1929
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:22:33 +0800

    11-27-2024 21:22 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,7 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+# Original array with duplicate elements
+arr = [1, 2, 3, 3, 4, 5, 2]
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if root.val < key:
-            root.right = insert(root.right, key)
-        else:
-            root.left = insert(root.left, key)
-    return root
+# Removing duplicates
+arr = list(set(arr))
 
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(root.val)
-        inorder_traversal(root.right)
-
-# Example usage
-root = None
-root = insert(root, 50)
-insert(root, 30)
-insert(root, 20)
-insert(root, 40)
-insert(root, 70)
-insert(root, 60)
-insert(root, 80)
-
-inorder_traversal(root)
+print(arr)
