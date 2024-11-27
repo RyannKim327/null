bb3247b59d3cd86da32c8503eff45b336199dc5f
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:24:20 +0800

    11-27-2024 21:24 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,12 @@
-from collections import deque
+def largest_prime_factor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
 
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
-    
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            print(node)
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    queue.append(neighbor)
-
-# Example usage
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-bfs(graph, 'A')
+num = __your_number__
+result = largest_prime_factor(num)
+print("The largest prime factor of", num, "is:", result)
