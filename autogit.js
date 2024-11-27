99ada9a2b97df3a77c00381b8f761a21e7feec96
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:25:41 +0800

    11-27-2024 21:25 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,19 @@
-from queue import PriorityQueue
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-def beam_search(start_state, beam_width, max_steps, goal_state):
-    agenda = PriorityQueue()
-    agenda.put(start_state)
-    
-    for _ in range(max_steps):
-        new_agenda = PriorityQueue()
-        
-        while not agenda.empty():
-            state = agenda.get()
-            
-            if state == goal_state:
-                return state
-            
-            successors = state.generate_successors()
-            for successor in successors:
-                new_agenda.put(successor)
-            
-        agenda = new_agenda
-        
-        # Keep only the top beam_width states
-        top_states = []
-        for _ in range(beam_width):
-            if not agenda.empty():
-                top_states.append(agenda.get())
-            else:
-                break
-        for state in top_states:
-            agenda.put(state)
-    
-    return None
+def hasCycle(head):
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while slow != fast:
+        if not fast or not fast.next:
+            return False
+        slow = slow.next
+        fast = fast.next.next
+
+    return True
