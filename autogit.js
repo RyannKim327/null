b6eeae12964f7873e63353749755406e431889b9
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:13:51 +0800

    11-27-2024 21:13 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,13 @@
-def longest_common_prefix(strings):
-    if not strings:
-        return ""
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-    min_len = min(map(len, strings))
-    for i in range(min_len):
-        if any(strings[j][i] != strings[0][i] for j in range(1, len(strings))):
-            return strings[0][:i]
-
-    return strings[0][:min_len]
-
-# Example usage
-strings = ["flower", "flow", "flight"]
-print(longest_common_prefix(strings))
+# Example usage:
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+print("Sorted array is:", arr)
