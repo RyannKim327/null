79598c02e29d672872cd5eaaf12a484b56dc061f
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:15:06 +0800

    11-27-2024 21:15 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,8 @@
-function astar_search(start):
-    add start to open list
-    while open list is not empty:
-        current = node from open list with the lowest f
-        
-        if current is goal node:
-            return reconstruct_path(current)
-        
-        remove current from open list
-        add current to closed list
-        
-        for each neighbor of current:
-            if neighbor is in closed list:
-                continue
-            
-            new_cost = current.g + distance(current, neighbor)
-            if neighbor is not in open list or new_cost < neighbor.g:
-                neighbor.g = new_cost
-                neighbor.h = heuristic(neighbor)
-                neighbor.f = neighbor.g + neighbor.h
-                neighbor.parent = current
-                
-                if neighbor is not in open list:
-                    add neighbor to open list
-    
-    return failure
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
 
-function reconstruct_path(node):
-    path = []
-    while node is not null:
-        add node to path
-        node = node.parent
-    reverse path
-    return path
+var inputString = "hello";
+var reversedString = reverseString(inputString);
+
+console.log(reversedString); // Output: "olleh"
