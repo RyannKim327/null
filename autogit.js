e6caaddc7704d62d9a3ae9f2af52e4660915dc94
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:19:13 +0800

    11-27-2024 21:19 [Bakit na mimiss ko pa rin sya]

---
+++ autogit.js
@@ -0,0 +1,26 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+def bfs(graph, start):
+    visited = []
+    queue = [start]
+
+    while queue:
+        node = queue.pop(0)
+        if node not in visited:
+            visited.append(node)
+            neighbors = graph[node]
+
+            for neighbor in neighbors:
+                queue.append(neighbor)
+
+    return visited
+
+# Example usage
+result = bfs(graph, 'A')
+print(result)
