a9530f046f2090cfb16fdb39e57577e78cadf32a
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:16:19 +0800

    11-27-2024 21:16 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,9 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+const axios = require('axios');
 
-def reverse_linked_list(head):
-    prev = None
-    current = head
-
-    while current is not None:
-        next_node = current.next
-        current.next = prev
-        prev = current
-        current = next_node
-
-    return prev
-
-# Example usage
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-
-node1.next = node2
-node2.next = node3
-
-new_head = reverse_linked_list(node1)
-
-current = new_head
-while current is not None:
-    print(current.value)
-    current = current.next
+axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
