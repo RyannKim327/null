9542338670be838149e730bcd3a5ce48d213f21c
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:27:36 +0800

    11-27-2024 21:27 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,4 @@
-from collections import deque
-
-def bfs(graph, start, goal):
-    queue = deque()
-    queue.append(start)
-    visited = set()
-    
-    while queue:
-        node = queue.popleft()
-        if node == goal:
-            return True
-        
-        if node not in visited:
-            visited.add(node)
-            neighbors = graph[node]
-            for neighbor in neighbors:
-                if neighbor not in visited:
-                    queue.append(neighbor)
-                    
-    return False
-
-# Example usage
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-start_node = 'A'
-goal_node = 'F'
-
-if bfs(graph, start_node, goal_node):
-    print("Path found from {} to {}: {}".format(start_node, goal_node, "Yes"))
-else:
-    print("No path found from {} to {}".format(start_node, goal_node))
+arr = [3, 5, 2, 8, 1, 10]
+max_value = max(arr)
+print("The maximum value in the array is:", max_value)
+The maximum value in the array is: 10
