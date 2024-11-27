6a8ef33a56dab5daf2c203a8b11b878215545838
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:18:23 +0800

    11-27-2024 21:18 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,32 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+def counting_sort(arr, exp):
+    output = [0] * len(arr)
+    count = [0] * 10
 
-def find_middle_element(head):
-    slow_ptr = head
-    fast_ptr = head
+    for i in range(len(arr)):
+        index = arr[i] // exp
+        count[index % 10] += 1
 
-    while fast_ptr is not None and fast_ptr.next is not None:
-        slow_ptr = slow_ptr.next
-        fast_ptr = fast_ptr.next.next
+    for i in range(1, 10):
+        count[i] += count[i - 1]
 
-    return slow_ptr.data
+    i = len(arr) - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
 
-# Create a sample linked list
-head = Node(1)
-head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = Node(4)
-head.next.next.next.next = Node(5)
+    for i in range(len(arr)):
+        arr[i] = output[i]
 
-# Find the middle element of the linked list
-middle_element = find_middle_element(head)
-print("Middle element of the linked list is:", middle_element)
+def radix_sort(arr):
+    max_num = max(arr)
+
+    exp = 1
+    while max_num // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr)
+print(arr)
