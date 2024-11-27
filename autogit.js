1c75d3e550f6b3bf0ed28a6d744794047c44faac
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:29:52 +0800

    11-27-2024 21:29 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,46 @@
-from collections import Counter
+#include <iostream>
 
-def first_non_repeating_char(s):
-    char_count = Counter(s)
-    for char in s:
-        if char_count[char] == 1:
-            return char
-    return None
+struct Node {
+    int data;
+    Node* next;
+};
 
-# Example
-input_string = "hello world"
-result = first_non_repeating_char(input_string)
-print("First non-repeating character:", result)
+Node* findNthNodeFromEnd(Node* head, int n) {
+    if (head == nullptr) {
+        return nullptr;
+    }
+
+    int length = 0;
+    Node* current = head;
+
+    // Calculate the length of the linked list
+    while (current != nullptr) {
+        length++;
+        current = current->next;
+    }
+
+    // Calculate the position of the nth node from the beginning
+    int positionFromStart = length - n;
+
+    // Move the current pointer to the node at positionFromStart
+    current = head;
+    for (int i = 0; i < positionFromStart && current != nullptr; i++) {
+        current = current->next;
+    }
+
+    return current;
+}
+
+int main() {
+    Node* head = new Node{1, new Node{2, new Node{3, new Node{4, new Node{5, nullptr}}}}};
+    int n = 2;
+    Node* result = findNthNodeFromEnd(head, n);
+
+    if (result) {
+        std::cout << "The data in the " << n << "th node from the end is: " << result->data << std::endl;
+    } else {
+        std::cout << "The node does not exist." << std::endl;
+    }
+
+    return 0;
+}
