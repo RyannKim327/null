a56122039f73ab4c9b365fe68507f648f9eddfaa
RyannKim327 <rksesgundo123@gmail.com>
2024-11-27 21:12:55 +0800

    11-27-2024 21:12 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,9 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-    
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-            
-    return -1
+def find_string_length(input_str):
+    count = 0
+    for char in input_str:
+        count += 1
+    return count
 
-# Test the binary search function
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = 6
-result = binary_search(arr, target)
-
-if result != -1:
-    print("Element found at index:", result)
-else:
-    print("Element not found in the array.")
+input_str = "Hello, World!"
+length = find_string_length(input_str)
+print("Length of the string is:", length)
