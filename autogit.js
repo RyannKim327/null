ebd874a495adfb77fd4f4307507f4289e6a311ca
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:41:11 +0800

    12-3-2024 21:41 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,12 @@
-function longestCommonSubsequence(str1, str2):
-    m = length(str1)
-    n = length(str2)
-    array = create a (m+1) x (n+1) array filled with zeros
-
-    for i from 1 to m:
-        for j from 1 to n:
-            if str1[i-1] == str2[j-1]:
-                array[i][j] = array[i-1][j-1] + 1
-            else:
-                array[i][j] = max(array[i-1][j], array[i][j-1])
-
-    // Backtrack to find the longest common subsequence
-    lcs = ""
-    i = m
-    j = n
-    while i > 0 and j > 0:
-        if str1[i-1] == str2[j-1]:
-            lcs = str1[i-1] + lcs
-            i = i - 1
-            j = j - 1
+def largest_prime_factor(number):
+    i = 2
+    while i * i <= number:
+        if number % i:
+            i += 1
         else:
-            if array[i-1][j] > array[i][j-1]:
-                i = i - 1
-            else:
-                j = j - 1
+            number //= i
+    return number
 
-    return lcs
+number = 56
+largest_prime_factor = largest_prime_factor(number)
+print(f"The largest prime factor of {number} is {largest_prime_factor}")
