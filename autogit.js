9408c77d35841e1bf03d894fab78ac6a77272a40
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:36:59 +0800

    12-3-2024 21:36 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,88 @@
-# Node definition for the linked list
+import random
+
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, value=None, level=0):
+        self.value = value
+        self.forward = [None]*(level+1)
 
-# Function to find the length of a linked list
-def length_of_linked_list(head):
-    current = head
-    length = 0
-
-    while current is not None:
-        length += 1
-        current = current.next
-
-    return length
-
-# Example usage
-# Create a linked list
-head = Node(1)
-head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = Node(4)
-
-# Find the length of the linked list
-print(length_of_linked_list(head))  # Output: 4
+class SkipList:
+    def __init__(self, max_level, p):
+        self.max_level = max_level
+        self.p = p
+        self.header = Node()
+        self.level = 0
+        
+    def random_level(self):
+        level = 0
+        while random.random() < self.p and level < self.max_level:
+            level += 1
+        return level
+    
+    def insert(self, value):
+        update = [None]*(self.max_level+1)
+        current = self.header
+        
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+            update[i] = current
+        
+        level = self.random_level()
+        
+        if level > self.level:
+            for i in range(self.level+1, level+1):
+                update[i] = self.header
+            self.level = level
+        
+        new_node = Node(value, level)
+        
+        for i in range(level+1):
+            new_node.forward[i] = update[i].forward[i]
+            update[i].forward[i] = new_node
+    
+    def search(self, value):
+        current = self.header
+        
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+        
+        current = current.forward[0]
+        
+        if current and current.value == value:
+            print("Found")
+        else:
+            print("Not Found")
+        
+    def delete(self, value):
+        update = [None]*(self.max_level+1)
+        current = self.header
+        
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].value < value:
+                current = current.forward[i]
+            update[i] = current
+        
+        current = current.forward[0]
+        
+        if current and current.value == value:
+            for i in range(self.level+1):
+                if update[i].forward[i] != current:
+                    break
+                update[i].forward[i] = current.forward[i]
+            
+            while self.level > 0 and self.header.forward[self.level] is None:
+                self.level -= 1
+            print("Deleted")
+        else:
+            print("Not Found")
+        
+    def display(self):
+        print("Skip list:")
+        for i in range(self.level+1):
+            node = self.header.forward[i]
+            print("Level {}: ".format(i), end="")
+            while node:
+                print(node.value, end=" ")
+                node = node.forward[i]
+            print("")
