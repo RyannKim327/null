b22081a2ca8c58d7c914eaa8bfa8f88ffa333f9c
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:28:32 +0800

    12-3-2024 21:28 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,42 @@
-def largest_prime_factor(n):
-    i = 2
-    while i * i <= n:
-        if n % i:
-            i += 1
-        else:
-            n //= i
-    return n
+def fibonacci_search(arr, x):
+    fib_n2 = 0
+    fib_n1 = 1
+    fib = fib_n1 + fib_n2
 
-number = 1234567890
-result = largest_prime_factor(number)
-print(result)
+    while fib < len(arr):
+        fib_n2 = fib_n1
+        fib_n1 = fib
+        fib = fib_n1 + fib_n2
+
+    offset = -1
+
+    while fib > 1:
+        i = min(offset + fib_n2, len(arr) - 1)
+
+        if arr[i] < x:
+            fib = fib_n1
+            fib_n1 = fib_n2
+            fib_n2 = fib - fib_n1
+            offset = i
+        elif arr[i] > x:
+            fib = fib_n2
+            fib_n1 = fib_n1 - fib_n2
+            fib_n2 = fib - fib_n1
+        else:
+            return i
+
+    if fib_n1 and arr[offset] == x:
+        return offset
+
+    return -1
+
+# Example usage:
+arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100]
+x = 35
+
+result = fibonacci_search(arr, x)
+
+if result != -1:
+    print(f"Element found at index {result}")
+else:
+    print("Element not found")
