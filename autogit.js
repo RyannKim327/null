bff48e153781594313d95ceaa02e317e7d2e4a2f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:25:07 +0800

    12-3-2024 21:25 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,13 @@
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString = string1 + " " + string2;
-console.log(concatenatedString); // Output: "Hello World"
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [x for x in arr[1:] if x <= pivot]
+        greater = [x for x in arr[1:] if x > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+# Example usage:
+arr = [3, 6, 8, 10, 1, 2, 1]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
