bf1aebf704fed64397b9f0468b0b71a3d9a35956
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:31:41 +0800

    12-3-2024 21:31 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,10 @@
-def longest_common_prefix(strs):
-    if not strs:
-        return ""
-    
-    # Sort the strings to make it easier to find the common prefix
-    strs.sort()
-    
-    # Compare the first and last strings in the sorted set
-    first_str = strs[0]
-    last_str = strs[-1]
-    
-    # Find the common prefix between the first and last strings
-    i = 0
-    while i < len(first_str) and i < len(last_str) and first_str[i] == last_str[i]:
-        i += 1
-    
-    return first_str[:i]
+import requests
 
-# Example usage
-strings = ["flower", "flow", "flight"]
-print(longest_common_prefix(strings))  # Output: "fl"
+url = "https://api.example.com/data"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("Error: Unable to fetch data from API")
