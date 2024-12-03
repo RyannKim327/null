5003ad53b3f77109621787cccacbaac20614e746
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:27:34 +0800

    12-3-2024 21:27 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,33 @@
-def astar_search(start, goal, heuristic):
-    open_set = PriorityQueue()
-    open_set.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not open_set.empty():
-        current = open_set.get()
-        
-        if current == goal:
-            path = reconstruct_path(came_from, start, goal)
-            return path
-        
-        for next in get_neighbors(current):
-            new_cost = cost_so_far[current] + get_cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(next, goal)
-                open_set.put(next, priority)
-                came_from[next] = current
-    
-    return None
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-def reconstruct_path(came_from, start, goal):
-    current = goal
-    path = []
-    while current != start:
-        path.append(current)
-        current = came_from[current]
-    path.append(start)
-    path.reverse()
-    return path
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def length(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+# Example of how to use the code
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+print("Length of the linked list is:", llist.length())
