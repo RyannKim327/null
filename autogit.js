4d0a39f039ca4e1ca93a7b49b35a0e9d2eb6db3a
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:37:05 +0800

    12-3-2024 21:36 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,88 +1,28 @@
-import random
-
 class Node:
-    def __init__(self, value=None, level=0):
-        self.value = value
-        self.forward = [None]*(level+1)
+    def __init__(self):
+        self.outgoing_edges = {}  # Outgoing edges from this node
+        self.suffix_link = None
 
-class SkipList:
-    def __init__(self, max_level, p):
-        self.max_level = max_level
-        self.p = p
-        self.header = Node()
-        self.level = 0
-        
-    def random_level(self):
-        level = 0
-        while random.random() < self.p and level < self.max_level:
-            level += 1
-        return level
+class SuffixTree:
+    def __init__(self, s):
+        self.root = Node()
+        self.active_node = self.root
+        self.active_edge = 0
+        self.active_length = 0
+        self.remaining_suffixes = 0
+        self.end = [-1]
+        self.s = s
+        self.build_suffix_tree()
     
-    def insert(self, value):
-        update = [None]*(self.max_level+1)
-        current = self.header
-        
-        for i in range(self.level, -1, -1):
-            while current.forward[i] and current.forward[i].value < value:
-                current = current.forward[i]
-            update[i] = current
-        
-        level = self.random_level()
-        
-        if level > self.level:
-            for i in range(self.level+1, level+1):
-                update[i] = self.header
-            self.level = level
-        
-        new_node = Node(value, level)
-        
-        for i in range(level+1):
-            new_node.forward[i] = update[i].forward[i]
-            update[i].forward[i] = new_node
-    
-    def search(self, value):
-        current = self.header
-        
-        for i in range(self.level, -1, -1):
-            while current.forward[i] and current.forward[i].value < value:
-                current = current.forward[i]
-        
-        current = current.forward[0]
-        
-        if current and current.value == value:
-            print("Found")
-        else:
-            print("Not Found")
-        
-    def delete(self, value):
-        update = [None]*(self.max_level+1)
-        current = self.header
-        
-        for i in range(self.level, -1, -1):
-            while current.forward[i] and current.forward[i].value < value:
-                current = current.forward[i]
-            update[i] = current
-        
-        current = current.forward[0]
-        
-        if current and current.value == value:
-            for i in range(self.level+1):
-                if update[i].forward[i] != current:
-                    break
-                update[i].forward[i] = current.forward[i]
-            
-            while self.level > 0 and self.header.forward[self.level] is None:
-                self.level -= 1
-            print("Deleted")
-        else:
-            print("Not Found")
-        
-    def display(self):
-        print("Skip list:")
-        for i in range(self.level+1):
-            node = self.header.forward[i]
-            print("Level {}: ".format(i), end="")
-            while node:
-                print(node.value, end=" ")
-                node = node.forward[i]
-            print("")
+    def build_suffix_tree(self):
+        # Implement Ukkonen's algorithm to build the tree
+        pass
+
+    def search_substring(self, substr):
+        # Implement a method to search for a substring in the suffix tree
+        pass
+
+# Example usage
+s = "banana"
+tree = SuffixTree(s)
+tree.search_substring("ana")
