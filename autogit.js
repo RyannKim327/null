7edca035db1f030230c89e300257a8a2a649edc1
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:25:29 +0800

    12-3-2024 21:25 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,12 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+def largest_prime_factor(number):
+    i = 2
+    while i * i <= number:
+        if number % i:
+            i += 1
+        else:
+            number //= i
+    return number
 
-def diameter(node):
-    if node is None:
-        return 0
-    left_height = height(node.left)
-    right_height = height(node.right)
-    
-    left_diameter = diameter(node.left)
-    right_diameter = diameter(node.right)
-    
-    return max(left_height + right_height + 1, max(left_diameter, right_diameter))
 
-def height(node):
-    if node is None:
-        return 0
-    return 1 + max(height(node.left), height(node.right))
-
-# Example usage
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Diameter of the tree is", diameter(root))
+number = 56
+print(largest_prime_factor(number))
