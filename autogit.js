24fc88034cdcd0db942367a436ec583d91ca5e03
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:38:58 +0800

    12-3-2024 21:38 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,33 @@
-def calculate_string_length(input_string):
-    count = 0
-    for char in input_string:
-        count += 1
-    return count
+def count_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
 
-# Test the function
-input_string = "Hello, World!"
-length = calculate_string_length(input_string)
-print("Length of the string:", length)
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(n):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    max_num = max(arr)
+    exp = 1
+
+    while max_num // exp > 0:
+        count_sort(arr, exp)
+        exp *= 10
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr)
+print("Sorted array:", arr)
