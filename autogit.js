fb36d5597d0318b2a115ca65113c4323eed2a228
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:31:37 +0800

    12-3-2024 21:31 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,21 @@
-def heapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-
-    if left < n and arr[i] < arr[left]:
-        largest = left
-
-    if right < n and arr[largest] < arr[right]:
-        largest = right
-
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n - 1, 0, -1):
-        arr[0], arr[i] = arr[i], arr[0]
-        heapify(arr, i, 0)
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    
+    # Sort the strings to make it easier to find the common prefix
+    strs.sort()
+    
+    # Compare the first and last strings in the sorted set
+    first_str = strs[0]
+    last_str = strs[-1]
+    
+    # Find the common prefix between the first and last strings
+    i = 0
+    while i < len(first_str) and i < len(last_str) and first_str[i] == last_str[i]:
+        i += 1
+    
+    return first_str[:i]
 
 # Example usage
-arr = [12, 11, 13, 5, 6, 7]
-heap_sort(arr)
-print("Sorted array is:", arr)
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings))  # Output: "fl"
