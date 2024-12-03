7dd6bb6e99cedf1c3c207690907a93fcc1b5bab7
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:34:45 +0800

    12-3-2024 21:34 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,8 @@
-def is_palindrome(s):
-    s = s.lower()  # Convert the string to lowercase
-    s = ''.join(e for e in s if e.isalnum())  # Remove non-alphanumeric characters
-    return s == s[::-1]
+def remove_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    return ''.join([char for char in input_string if char not in vowels])
 
-# Test the function
-s = "A man, a plan, a canal, Panama"
-if is_palindrome(s):
-    print("String is a palindrome")
-else:
-    print("String is not a palindrome")
+input_string = "Hello, World!"
+output_string = remove_vowels(input_string)
+
+print(output_string)
