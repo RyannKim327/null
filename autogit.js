cca50e1354311866f7b7b71069df4e32c2cdc677
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:31:32 +0800

    12-3-2024 21:31 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,29 @@
-from crontab import CronTab
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-cron = CronTab(user=True)  # Access the cron table of the current user
+    if left < n and arr[i] < arr[left]:
+        largest = left
 
-job = cron.new(command='echo "Hello, World!"')
-job.minute.every(1)  # Run the job every minute
+    if right < n and arr[largest] < arr[right]:
+        largest = right
 
-cron.write()  # Write the changes to the user's crontab
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+# Example usage
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is:", arr)
