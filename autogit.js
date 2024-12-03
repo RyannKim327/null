888170a330ef3d9554f3c013f39eaabac18649fb
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:30:48 +0800

    12-3-2024 21:30 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,18 @@
-def fibonacci_search(arr, x):
-    fib_minus_2 = 0
-    fib_minus_1 = 1
-    fib = fib_minus_1 + fib_minus_2
-    
-    while fib < len(arr):
-        fib_minus_2 = fib_minus_1
-        fib_minus_1 = fib
-        fib = fib_minus_1 + fib_minus_2
-    
-    offset = -1
-    
-    while fib > 1:
-        i = min(offset + fib_minus_2, len(arr) - 1)
-        
-        if arr[i] < x:
-            fib = fib_minus_1
-            fib_minus_1 = fib_minus_2
-            fib_minus_2 = fib - fib_minus_1
-            offset = i
-        elif arr[i] > x:
-            fib = fib_minus_2
-            fib_minus_1 = fib_minus_1 - fib_minus_2
-            fib_minus_2 = fib - fib_minus_1
-        else:
-            return i
-    
-    if fib_minus_1 and arr[offset + 1] == x:
-        return offset + 1
-    
-    return -1
+def burrows_wheeler_transform(text):
+    table = [text[i:] + text[:i] for i in range(len(text))]
+    table.sort()
+    transformed_text = ''.join([row[-1] for row in table])
+    return transformed_text
 
-# Example usage
-arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100]
-x = 85
+def inverse_burrows_wheeler_transform(text):
+    table = [''] * len(text)
+    for i in range(len(text)):
+        table = sorted([text[j] + table[j] for j in range(len(text))])
+    transformed_text = [row for row in table if row.endswith('$')][0]
+    return transformed_text.rstrip('$')
 
-result = fibonacci_search(arr, x)
-if result != -1:
-    print("Element found at index", result)
-else:
-    print("Element not found")
+# Example
+text = "banana"
+transformed_text = burrows_wheeler_transform(text + '$')
+original_text = inverse_burrows_wheeler_transform(transformed_text)
+print("Original Text:", original_text)
