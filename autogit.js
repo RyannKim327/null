3a86956ebd28d106e2cc976d4c9955563f419448
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:31:22 +0800

    12-3-2024 21:31 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,17 @@
-def burrows_wheeler_transform(text):
-    table = [text[i:] + text[:i] for i in range(len(text))]
-    table.sort()
-    transformed_text = ''.join([row[-1] for row in table])
-    return transformed_text
+def is_palindrome(s):
+    left, right = 0, len(s) - 1
+    
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    
+    return True
 
-def inverse_burrows_wheeler_transform(text):
-    table = [''] * len(text)
-    for i in range(len(text)):
-        table = sorted([text[j] + table[j] for j in range(len(text))])
-    transformed_text = [row for row in table if row.endswith('$')][0]
-    return transformed_text.rstrip('$')
-
-# Example
-text = "banana"
-transformed_text = burrows_wheeler_transform(text + '$')
-original_text = inverse_burrows_wheeler_transform(transformed_text)
-print("Original Text:", original_text)
+# Test the function
+s = "madam"
+if is_palindrome(s):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
