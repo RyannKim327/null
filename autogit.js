efbd39619a62fac4476fa4f50a139d3aacc79d45
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:32:44 +0800

    12-3-2024 21:32 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,26 @@
-def tarjan_scc(graph):
-    def dfs(node):
-        nonlocal dfs_num, dfs_low, stack, scc_list
-        dfs_num[node] = dfs_low[node] = visit_counter
-        visit_counter += 1
-        stack.append(node)
-        on_stack[node] = True
+def is_anagram(str1, str2):
+    # Convert both strings to lowercase
+    str1 = str1.lower()
+    str2 = str2.lower()
 
-        for neighbor in graph[node]:
-            if dfs_num[neighbor] == -1:
-                dfs(neighbor)
-            if on_stack[neighbor]:
-                dfs_low[node] = min(dfs_low[node], dfs_low[neighbor])
+    # Remove spaces and special characters
+    str1 = ''.join(e for e in str1 if e.isalnum())
+    str2 = ''.join(e for e in str2 if e.isalnum())
 
-        if dfs_low[node] == dfs_num[node]:
-            scc = []
-            while True:
-                current = stack.pop()
-                on_stack[current] = False
-                scc.append(current)
-                if current == node:
-                    break
-            scc_list.append(scc)
+    # Sort both strings
+    str1_sorted = sorted(str1)
+    str2_sorted = sorted(str2)
 
-    n = len(graph)
-    dfs_num = [-1] * n
-    dfs_low = [-1] * n
-    stack = []
-    on_stack = [False] * n
-    visit_counter = 0
-    scc_list = []
+    # Compare the sorted strings
+    if str1_sorted == str2_sorted:
+        return True
+    else:
+        return False
 
-    for i in range(n):
-        if dfs_num[i] == -1:
-            dfs(i)
-
-    return scc_list
-
-# Example usage
-graph = {0: [1], 1: [2], 2: [0, 3, 6], 3: [4], 4: [5, 7], 5: [3], 6: [5, 7], 7: [6]}
-strongly_connected_components = tarjan_scc(graph)
-print(strongly_connected_components)
+# Test the function
+string1 = "Listen"
+string2 = "Silent"
+if is_anagram(string1, string2):
+    print(f"{string1} and {string2} are anagrams.")
+else:
+    print(f"{string1} and {string2} are not anagrams.")
