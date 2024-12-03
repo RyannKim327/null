b5434b341f9547ec1a6fbc75eeb5e986ec3cfe75
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:32:30 +0800

    12-3-2024 21:32 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,10 @@
-def counting_sort(arr, exp):
-    n = len(arr)
-    output = [0] * n
-    count = [0] * 10
-    
-    for i in range(n):
-        index = arr[i] // exp
-        count[index % 10] += 1
-    
-    for i in range(1, 10):
-        count[i] += count[i - 1]
-    
-    i = n - 1
-    while i >= 0:
-        index = arr[i] // exp
-        output[count[index % 10] - 1] = arr[i]
-        count[index % 10] -= 1
-        i -= 1
-    
-    for i in range(n):
-        arr[i] = output[i]
+def calculate_string_length(string):
+    count = 0
+    for char in string:
+        count += 1
+    return count
 
-def radix_sort(arr):
-    max_num = max(arr)
-    exp = 1
-    while max_num // exp > 0:
-        counting_sort(arr, exp)
-        exp *= 10
-
-arr = [170, 45, 75, 90, 802, 24, 2, 66]
-radix_sort(arr)
-print("Sorted array is:", arr)
+# Example usage
+my_string = "Hello, World!"
+length = calculate_string_length(my_string)
+print(length)  # Output will be 13
