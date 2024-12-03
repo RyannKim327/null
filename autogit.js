820ec48d134e73e81cdc9501bbf603aa869c6e3d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:26:18 +0800

    12-3-2024 21:26 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,10 @@
-class SuffixTree:
-    def __init__(self, string):
-        self.root = {}
-        self.end_char = '$'
-        self.string = string + self.end_char
-        self.build_suffix_tree()
+import random
 
-    def build_suffix_tree(self):
-        for i in range(len(self.string)):
-            current = self.root
-            for char in self.string[i:]:
-                if char not in current:
-                    current[char] = {}
-                current = current[char]
-    
-    def search(self, pattern):
-        current = self.root
-        for char in pattern:
-            if char not in current:
-                return False
-            current = current[char]
-        return True
-    
-    def print_tree(self):
-        def print_node(node, level):
-            for key, value in node.items():
-                print('  ' * level + key)
-                print_node(value, level + 1)
-        
-        print_node(self.root, 0)
-        
-# Example Usage
-suffix_tree = SuffixTree("banana")
-suffix_tree.print_tree()
-print(suffix_tree.search("nan"))
+# Define the range [min_val, max_val]
+min_val = 10
+max_val = 20
+
+# Generate a random number within the specified range
+random_number = random.randint(min_val, max_val)
+
+print(random_number)
