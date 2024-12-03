1873ffbddd6a839fbe3597d0773044ae9b1f6847
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:39:48 +0800

    12-3-2024 21:39 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,29 @@
-def find_second_largest(arr):
-    # Sort the array in descending order
-    arr.sort(reverse=True)
-    
-    # Return the element at index 1
-    return arr[1]
+def merge_sort_iterative(arr):
+    # Split the array into individual elements as subarrays
+    arr = [[x] for x in arr]
 
-# Test the function
-arr = [1, 5, 3, 9, 7]
-print("Second largest element:", find_second_largest(arr))
+    # Merge neighboring arrays in a bottom-up manner
+    while len(arr) > 1:
+        tmp = []
+        for i in range(0, len(arr) - 1, 2):
+            tmp.append(merge(arr[i], arr[i + 1]))
+        if len(arr) % 2 == 1:
+            tmp.append(arr[-1])
+        arr = tmp
+
+    return arr[0]
+
+def merge(arr1, arr2):
+    result = []
+    while arr1 and arr2:
+        if arr1[0] < arr2[0]:
+            result.append(arr1.pop(0))
+        else:
+            result.append(arr2.pop(0))
+    result += arr1 + arr2
+    return result
+
+# Test the merge sort algorithm
+arr = [38, 27, 43, 3, 9, 82, 10]
+result = merge_sort_iterative(arr)
+print(result)
