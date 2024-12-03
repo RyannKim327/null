4f221b7ee3efa7f27eddf580231905c134b28f64
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:24:05 +0800

    12-3-2024 21:23 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,12 @@
-class PriorityQueue:
-    def __init__(self):
-        self.heap = []
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
 
-    def insert(self, item):
-        self.heap.append(item)
-        self._heapify_up(len(self.heap) - 1)
-
-    def extract_min(self):
-        if len(self.heap) == 0:
-            return None
-        
-        min_item = self.heap[0]
-        last_item = self.heap.pop()
-        if len(self.heap) > 0:
-            self.heap[0] = last_item
-            self._heapify_down(0)
-        
-        return min_item
-
-    def _heapify_up(self, index):
-        while index > 0:
-            parent_index = (index - 1) // 2
-            if self.heap[index] < self.heap[parent_index]:
-                self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
-                index = parent_index
-            else:
-                break
-
-    def _heapify_down(self, index):
-        while True:
-            left_child_index = 2 * index + 1
-            right_child_index = 2 * index + 2
-            min_index = index
-
-            if left_child_index < len(self.heap) and self.heap[left_child_index] < self.heap[min_index]:
-                min_index = left_child_index
-            if right_child_index < len(self.heap) and self.heap[right_child_index] < self.heap[min_index]:
-                min_index = right_child_index
-
-            if min_index != index:
-                self.heap[index], self.heap[min_index] = self.heap[min_index], self.heap[index]
-                index = min_index
-            else:
-                break
+# Example usage
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(quicksort(arr))
