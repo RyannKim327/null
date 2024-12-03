6dda245de3efc214e555fe926f51c4d3f3093d8f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:34:41 +0800

    12-3-2024 21:34 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,11 @@
-def longest_common_substring(s1, s2):
-    m = len(s1)
-    n = len(s2)
-    longest_length = 0
-    ending_index = 0
-
-    # Create a 2D array to store the lengths of the longest common suffixes
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if s1[i - 1] == s2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-                if dp[i][j] > longest_length:
-                    longest_length = dp[i][j]
-                    ending_index = i
-            else:
-                dp[i][j] = 0
-
-    if longest_length == 0:
-        return ""
-
-    start_index = ending_index - longest_length
-    return s1[start_index:ending_index]
+def is_palindrome(s):
+    s = s.lower()  # Convert the string to lowercase
+    s = ''.join(e for e in s if e.isalnum())  # Remove non-alphanumeric characters
+    return s == s[::-1]
 
 # Test the function
-s1 = "abcdefg"
-s2 = "xyzabcde"
-print(longest_common_substring(s1, s2))  # Output: "abcde"
+s = "A man, a plan, a canal, Panama"
+if is_palindrome(s):
+    print("String is a palindrome")
+else:
+    print("String is not a palindrome")
