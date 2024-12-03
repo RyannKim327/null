8bb42f5add7967a86984e8a130b4cea68774015e
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:24:10 +0800

    12-3-2024 21:24 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,30 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quicksort(left) + middle + quicksort(right)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-# Example usage
-arr = [3, 6, 8, 10, 1, 2, 1]
-print(quicksort(arr))
+def max_depth(node):
+    if node is None:
+        return 0
+    else:
+        left_depth = max_depth(node.left)
+        right_depth = max_depth(node.right)
+
+        return max(left_depth, right_depth) + 1
+
+# Example binary tree
+#      1
+#     / \
+#    2   3
+#   /
+#  4
+# \
+#  5
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.left.right = Node(5)
+
+print("Maximum depth of the binary tree:", max_depth(root))
