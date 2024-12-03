a61846d30a78841460042804272077682b8b76c7
RyannKim327 <rksesgundo123@gmail.com>
2024-12-03 21:26:14 +0800

    12-3-2024 21:26 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,35 @@
-def largest_prime_factor(number):
-    i = 2
-    while i * i <= number:
-        if number % i:
-            i += 1
-        else:
-            number //= i
-    return number
+class SuffixTree:
+    def __init__(self, string):
+        self.root = {}
+        self.end_char = '$'
+        self.string = string + self.end_char
+        self.build_suffix_tree()
 
-
-number = 56
-print(largest_prime_factor(number))
+    def build_suffix_tree(self):
+        for i in range(len(self.string)):
+            current = self.root
+            for char in self.string[i:]:
+                if char not in current:
+                    current[char] = {}
+                current = current[char]
+    
+    def search(self, pattern):
+        current = self.root
+        for char in pattern:
+            if char not in current:
+                return False
+            current = current[char]
+        return True
+    
+    def print_tree(self):
+        def print_node(node, level):
+            for key, value in node.items():
+                print('  ' * level + key)
+                print_node(value, level + 1)
+        
+        print_node(self.root, 0)
+        
+# Example Usage
+suffix_tree = SuffixTree("banana")
+suffix_tree.print_tree()
+print(suffix_tree.search("nan"))
