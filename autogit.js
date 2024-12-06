326adb278205eb041767405377199f3ddccf6b12
RyannKim327 <rksesgundo123@gmail.com>
2024-12-06 08:32:49 +0800

    12-6-2024 8:32 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,46 @@
-my_string = "Hello, World"
-new_string = my_string.replace(" ", "")
-print(new_string)
-import re
+def compute_lps_array(pattern):
+    length = len(pattern)
+    lps = [0] * length
+    j = 0
 
-my_string = "Hello, World"
-new_string = re.sub(r'\s+', '', my_string)
-print(new_string)
+    for i in range(1, length):
+        while j > 0 and pattern[i] != pattern[j]:
+            j = lps[j - 1]
+
+        if pattern[i] == pattern[j]:
+            j += 1
+
+        lps[i] = j
+
+    return lps
+
+def kmp_search(text, pattern):
+    lps = compute_lps_array(pattern)
+    m = len(text)
+    n = len(pattern)
+    i = 0
+    j = 0
+
+    while i < m:
+        if text[i] == pattern[j]:
+            i += 1
+            j += 1
+
+            if j == n:
+                return i - j
+        
+        else:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return -1
+
+text = "ABABDABACDABABCABAB"
+pattern = "ABABCABAB"
+result = kmp_search(text, pattern)
+if result != -1:
+    print("Pattern found at index:", result)
+else:
+    print("Pattern not found in the text.")
