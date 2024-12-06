64df7afb47cf786a3e51af051f8ded145197ff11
RyannKim327 <rksesgundo123@gmail.com>
2024-12-06 08:32:55 +0800

    12-6-2024 8:32 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,46 +1,24 @@
-def compute_lps_array(pattern):
-    length = len(pattern)
-    lps = [0] * length
-    j = 0
+arr = [1, 2, 3, 4, 5]
+reversed_arr = arr[::-1]
+print(reversed_arr)
+let arr = [1, 2, 3, 4, 5];
+let reversedArr = arr.reverse();
+console.log(reversedArr);
+import java.util.Arrays;
+import java.util.Collections;
 
-    for i in range(1, length):
-        while j > 0 and pattern[i] != pattern[j]:
-            j = lps[j - 1]
+Integer[] arr = {1, 2, 3, 4, 5};
+Collections.reverse(Arrays.asList(arr));
+System.out.println(Arrays.toString(arr));
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
-        if pattern[i] == pattern[j]:
-            j += 1
-
-        lps[i] = j
-
-    return lps
-
-def kmp_search(text, pattern):
-    lps = compute_lps_array(pattern)
-    m = len(text)
-    n = len(pattern)
-    i = 0
-    j = 0
-
-    while i < m:
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
-
-            if j == n:
-                return i - j
-        
-        else:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-
-    return -1
-
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-result = kmp_search(text, pattern)
-if result != -1:
-    print("Pattern found at index:", result)
-else:
-    print("Pattern not found in the text.")
+int main() {
+    std::vector<int> arr = {1, 2, 3, 4, 5};
+    std::reverse(arr.begin(), arr.end());
+    for(int i : arr) {
+        std::cout << i << " ";
+    }
+    return 0;
+}
