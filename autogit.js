b8bbc0fa2fc28a5167bc537327b1ab9b19e3e517
RyannKim327 <rksesgundo123@gmail.com>
2024-12-06 08:33:00 +0800

    12-6-2024 8:32 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,10 @@
-arr = [1, 2, 3, 4, 5]
-reversed_arr = arr[::-1]
-print(reversed_arr)
-let arr = [1, 2, 3, 4, 5];
-let reversedArr = arr.reverse();
-console.log(reversedArr);
-import java.util.Arrays;
-import java.util.Collections;
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-Integer[] arr = {1, 2, 3, 4, 5};
-Collections.reverse(Arrays.asList(arr));
-System.out.println(Arrays.toString(arr));
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-int main() {
-    std::vector<int> arr = {1, 2, 3, 4, 5};
-    std::reverse(arr.begin(), arr.end());
-    for(int i : arr) {
-        std::cout << i << " ";
-    }
-    return 0;
-}
+# Testing the factorial function
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
