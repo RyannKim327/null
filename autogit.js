366ec3e29e82f2355fb54b3f99e857f271e629e7
RyannKim327 <rksesgundo123@gmail.com>
2024-12-08 22:37:48 +0800

    12-8-2024 22:37 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,8 @@
-def longest_increasing_subsequence(nums):
-    n = len(nums)
-    if n == 0:
-        return 0
+# Original string with whitespace
+string_with_whitespace = "  Hello,   world!  "
 
-    lis = [1] * n
+# Remove whitespace
+string_without_whitespace = string_with_whitespace.replace(" ", "")
 
-    for i in range(1, n):
-        for j in range(i):
-            if nums[i] > nums[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
-
-    max_length = max(lis)
-    subsequence = []
-    idx = lis.index(max_length)
-    subsequence.append(nums[idx])
-
-    for i in range(idx - 1, -1, -1):
-        if nums[i] < nums[idx] and lis[i] == lis[idx] - 1:
-            subsequence.insert(0, nums[i])
-            idx = i
-
-    return subsequence
-
-# Example usage
-nums = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-print(longest_increasing_subsequence(nums))
+print(string_without_whitespace)
+Hello,world!
