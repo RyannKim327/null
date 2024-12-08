3f03c592b8a8f3ac32115941dc72c5d368cea131
RyannKim327 <rksesgundo123@gmail.com>
2024-12-08 22:39:09 +0800

    12-8-2024 22:39 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,36 @@
-import android.os.AsyncTask;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import queue
 
-public class MyAsyncTask extends AsyncTask<String, Void, String> {
+def beam_search(graph, start, beam_width):
+    q = queue.Queue()
+    q.put([start])
+    
+    while not q.empty():
+        path = q.get()
+        node = path[-1]
 
-    @Override
-    protected String doInBackground(String... params) {
-        StringBuilder result = new StringBuilder();
+        if len(path) == beam_width:
+            return path
         
-        try {
-            URL url = new URL(params[0]);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
+        for neighbor in graph[node]:
+            new_path = list(path)
+            new_path.append(neighbor)
+            q.put(new_path)
+            
+    return None
 
-            InputStream inputStream = conn.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-
-            reader.close();
-            inputStream.close();
-            conn.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return result.toString();
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        // Handle the result here, such as updating UI with the response data
-    }
+# Example of using the beam search algorithm
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': ['H', 'I'],
+    'E': ['J', 'K'],
+    'F': ['L', 'M'],
+    'G': ['N', 'O']
 }
+
+start_node = 'A'
+beam_width = 3
+
+result = beam_search(graph, start_node, beam_width)
+print(result)
