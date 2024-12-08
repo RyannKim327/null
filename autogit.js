b785a08446afc78f95fcc351703ccbd388b067f4
RyannKim327 <rksesgundo123@gmail.com>
2024-12-08 22:40:05 +0800

    12-8-2024 22:40 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,23 @@
-def find_string_length(input_string):
-    count = 0
-    for char in input_string:
-        count += 1
-    return count
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-input_string = "Hello, World!"
-print("Length of the string:", find_string_length(input_string))
+def max_depth(node):
+    if node is None:
+        return 0
+    else:
+        left_depth = max_depth(node.left)
+        right_depth = max_depth(node.right)
+        
+        return max(left_depth, right_depth) + 1
+
+# Example binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Maximum depth of the binary tree is:", max_depth(root))
