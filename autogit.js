0a3a9639d9f1334b715c354aedcc63955714d71f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:26:00 +0800

    12-9-2024 10:25 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,20 @@
-import requests
+# Define a list of integers
+arr = [5, 2, 8, 1, 3]
 
-url = "https://jsonplaceholder.typicode.com/posts"
-response = requests.get(url)
+# Sort the list in ascending order
+sorted_arr = sorted(arr)
+print(sorted_arr)
 
-if response.status_code == 200:
-    data = response.json()
-    for post in data:
-        print(post['title'])
-else:
-    print("Failed to fetch data from API")
+# Sort the list in descending order
+sorted_arr_desc = sorted(arr, reverse=True)
+print(sorted_arr_desc)
+# Define a list of integers
+arr = [5, 2, 8, 1, 3]
 
+# Sort the list in ascending order
+arr.sort()
+print(arr)
+
+# Sort the list in descending order
+arr.sort(reverse=True)
+print(arr)
