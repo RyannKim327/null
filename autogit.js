ba857d01df007ddbb0175959565749cbaf20f612
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:07:30 +0800

    12-9-2024 10:7 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,36 @@
-def depth_first_search(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            depth_first_search(graph, neighbor, visited)
-    
-    return visited
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
 
-# Example graph representation as an adjacency list
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
+        merge_sort(L)
+        merge_sort(R)
 
-start_node = 'A'
-visited_nodes = depth_first_search(graph, start_node)
-print("Nodes visited in DFS order:", visited_nodes)
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
+
+# Example usage
+arr = [38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
