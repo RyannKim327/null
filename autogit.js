a0c1f4e55a35a6750fb7ba3f8bb694f3296fa16d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:07:50 +0800

    12-9-2024 10:7 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,20 @@
-def remove_vowels(input_str):
-    vowels = "aeiouAEIOU"
-    return ''.join([char for char in input_str if char not in vowels])
+my_string = "123"
+my_integer = int(my_string)
+print(my_integer)
+String myString = "123";
+int myInteger = Integer.parseInt(myString);
+System.out.println(myInteger);
+#include <iostream>
+#include <string>
+#include <sstream>
 
-input_str = "Hello, World!"
-result = remove_vowels(input_str)
-print(result)
+int main() {
+    std::string myString = "123";
+    int myInteger;
+    std::istringstream(myString) >> myInteger;
+    std::cout << myInteger << std::endl;
+    return 0;
+}
+let myString = "123";
+let myInteger = parseInt(myString);
+console.log(myInteger);
