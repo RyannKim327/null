e1e568c2fd136f3debab7f05d312c0a8224b7bf3
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:25:01 +0800

    12-9-2024 10:24 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,16 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+    prefix = strings[0]
+    for string in strings[1:]:
+        i = 0
+        while i < len(prefix) and i < len(string) and prefix[i] == string[i]:
+            i += 1
+        prefix = prefix[:i]
 
-    def reverse(self):
-        prev = None
-        current = self.head
-        while current is not None:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        self.head = prev
+    return prefix
 
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def print_list(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.next
-
-# Example usage:
-llist = LinkedList()
-llist.push(20)
-llist.push(4)
-llist.push(15)
-llist.push(85)
-
-print("Given linked list:")
-llist.print_list()
-
-llist.reverse()
-
-print("\nReversed linked list:")
-llist.print_list()
+# Example usage
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings)) # Output: "fl"
