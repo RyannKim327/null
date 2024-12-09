282462714e94543eb43a4b4079049a6fa8182114
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:27:02 +0800

    12-9-2024 10:26 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,23 @@
-def longest_common_substring(str1, str2):
-    m = len(str1)
-    n = len(str2)
-    
-    longest_length = 0
-    end_index = 0
-    
-    # Create a 2D table to store the lengths of longest common suffixes of substrings
-    table = [[0] * (n+1) for i in range(m+1)]
-    
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if str1[i-1] == str2[j-1]:
-                table[i][j] = table[i-1][j-1] + 1
-                if table[i][j] > longest_length:
-                    longest_length = table[i][j]
-                    end_index = i - 1
-            else:
-                table[i][j] = 0
-    
-    return str1[end_index-longest_length+1:end_index+1]
+import heapq
 
-# Test the function
-str1 = "abcdef"
-str2 = "xbcde"
-print(longest_common_substring(str1, str2))  # Output: "bcde"
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+# Example Usage:
+pq = PriorityQueue()
+pq.push('Task 1', 5)
+pq.push('Task 2', 3)
+pq.push('Task 3', 7)
+
+print(pq.pop())  # Task 3
+print(pq.pop())  # Task 1
+print(pq.pop())  # Task 2
