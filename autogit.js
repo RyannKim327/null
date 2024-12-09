ac4b4e99aad4b6e0f18d48f777931a5888d18851
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:27:57 +0800

    12-9-2024 10:27 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,6 @@
-def generate_bad_character_table(pattern):
-    table = {}
-    for i in range(len(pattern) - 1):
-        table[pattern[i]] = len(pattern) - 1 - i
-    return table
+arr = [1, 2, 3, 3, 4, 5, 5, 6]
 
-def boyer_moore_horspool(text, pattern):
-    m = len(pattern)
-    n = len(text)
-    if m > n:
-        return -1
+# Using set to remove duplicate elements
+arr = list(set(arr))
 
-    skip_table = generate_bad_character_table(pattern)
-
-    i = 0
-    while i <= n - m:
-        j = m - 1
-        while j >= 0 and pattern[j] == text[i + j]:
-            j -= 1
-        if j < 0:
-            return i
-        else:
-            i += skip_table.get(text[i + m - 1], m)
-
-    return -1
-
-text = "ABAAABCD"
-pattern = "ABC"
-result = boyer_moore_horspool(text, pattern)
-if result != -1:
-    print(f"Pattern found at index {result}")
-else:
-    print("Pattern not found")
+print(arr)
