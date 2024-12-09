88bfb75e30516a71388ec54d64b9ce1bee2985b8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:24:46 +0800

    12-9-2024 10:24 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,15 +1,4 @@
-function binarySearch(arr, target):
-    left = 0
-    right = length(arr) - 1
-
-    while left <= right:
-        mid = left + (right - left) / 2
-
-        if arr[mid] == target:
-            return mid
-        else if arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return -1
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
