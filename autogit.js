dcd88e2f14a85b8b5cfbfeb71bf236cd57cc84fb
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:26:04 +0800

    12-9-2024 10:26 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,20 @@
-# Define a list of integers
-arr = [5, 2, 8, 1, 3]
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-# Sort the list in ascending order
-sorted_arr = sorted(arr)
-print(sorted_arr)
-
-# Sort the list in descending order
-sorted_arr_desc = sorted(arr, reverse=True)
-print(sorted_arr_desc)
-# Define a list of integers
-arr = [5, 2, 8, 1, 3]
-
-# Sort the list in ascending order
-arr.sort()
-print(arr)
-
-# Sort the list in descending order
-arr.sort(reverse=True)
-print(arr)
+# Test the function
+num = 17
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
