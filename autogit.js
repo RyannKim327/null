d611603469c230348f8e3e6aa712c5e4f613e913
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:31:09 +0800

    12-9-2024 10:31 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,8 @@
-def find_nth_from_end(head, n):
-    p1 = head
-    p2 = head
+# Define an array
+array = [3, 7, 2, 9, 4, 1]
 
-    # Move p2 n steps ahead
-    for _ in range(n):
-        if p2 is None:
-            return None
-        p2 = p2.next
+# Find the maximum value in the array
+max_value = max(array)
 
-    # Move p1 and p2 simultaneously until p2 reaches the end
-    while p2 is not None:
-        p1 = p1.next
-        p2 = p2.next
-
-    return p1
+# Print the maximum value
+print("Maximum value in the array:", max_value)
