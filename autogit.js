41d328355df17e4deea075f2d717994c07b86f06
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:27:08 +0800

    12-9-2024 10:27 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,17 @@
-import heapq
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
+    
+    prefix = ""
+    for i in range(len(strings[0])):
+        char = strings[0][i]
+        for string in strings[1:]:
+            if i >= len(string) or string[i] != char:
+                return prefix
+        prefix += char
+    
+    return prefix
 
-class PriorityQueue:
-    def __init__(self):
-        self._queue = []
-        self._index = 0
-
-    def push(self, item, priority):
-        heapq.heappush(self._queue, (-priority, self._index, item))
-        self._index += 1
-
-    def pop(self):
-        return heapq.heappop(self._queue)[-1]
-
-# Example Usage:
-pq = PriorityQueue()
-pq.push('Task 1', 5)
-pq.push('Task 2', 3)
-pq.push('Task 3', 7)
-
-print(pq.pop())  # Task 3
-print(pq.pop())  # Task 1
-print(pq.pop())  # Task 2
+# Test the function
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings))  # Output: "fl"
