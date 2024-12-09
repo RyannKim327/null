90c169f1580bef634cff60fdf00a4d22797a52a9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:19:39 +0800

    12-9-2024 10:19 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,50 @@
-let originalString = "Hello World!";
-let lowercaseString = originalString.toLowerCase();
-console.log(lowercaseString); // Output: "hello world!"
-original_string = "Hello World!"
-lowercase_string = original_string.lower()
-print(lowercase_string) # Output: "hello world!"
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+        print()
+
+# Example usage
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+
+print("Original linked list:")
+llist.print_list()
+
+llist.reverse()
+
+print("Reversed linked list:")
+llist.print_list()
