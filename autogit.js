f6493ae0e5038925d1f2d6b50c5e11c7ffe14bc2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:23:59 +0800

    12-9-2024 10:23 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,95 @@
-#include <iostream>
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
 
-// Node class to represent each element in the linked list
-class Node {
-public:
-    int data;
-    Node* next;
+class AVLTree:
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, root, key):
+        if not root:
+            return Node(key)
+        elif key < root.key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+        
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        
+        balance = self.get_balance(root)
+        
+        # Left Left Case
+        if balance > 1 and key < root.left.key:
+            return self.right_rotate(root)
+        
+        # Right Right Case
+        if balance < -1 and key > root.right.key:
+            return self.left_rotate(root)
+        
+        # Left Right Case
+        if balance > 1 and key > root.left.key:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        
+        # Right Left Case
+        if balance < -1 and key < root.right.key:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+        
+        return root
+    
+    def get_height(self, root):
+        if not root:
+            return 0
+        return root.height
+    
+    def get_balance(self, root):
+        if not root:
+            return 0
+        return self.get_height(root.left) - self.get_height(root.right)
+    
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+        
+        y.left = z
+        z.right = T2
+        
+        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        
+        return y
+    
+    def right_rotate(self, y):
+        x = y.left
+        T2 = x.right
+        
+        x.right = y
+        y.left = T2
+        
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+        
+        return x
+    
+    def insert_key(self, key):
+        self.root = self.insert(self.root, key)
+        
+    def pre_order_traversal(self, root):
+        if root:
+            print("{0} ".format(root.key), end="")
+            self.pre_order_traversal(root.left)
+            self.pre_order_traversal(root.right)
 
-    Node(int data) {
-        this->data = data;
-        this->next = nullptr;
-    }
-};
+avl_tree = AVLTree()
+avl_tree.insert_key(10)
+avl_tree.insert_key(20)
+avl_tree.insert_key(30)
+avl_tree.insert_key(40)
+avl_tree.insert_key(50)
+avl_tree.insert_key(25)
 
-// Linked list class to manage the nodes
-class LinkedList {
-public:
-    Node* head;
-
-    LinkedList() {
-        head = nullptr;
-    }
-
-    void insert(int data) {
-        Node* newNode = new Node(data);
-
-        if (head == nullptr) {
-            head = newNode;
-        } else {
-            Node* temp = head;
-            while (temp->next != nullptr) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
-
-    void display() {
-        Node* temp = head;
-        while (temp != nullptr) {
-            std::cout << temp->data << " ";
-            temp = temp->next;
-        }
-        std::cout << std::endl;
-    }
-};
-
-int main() {
-    LinkedList list;
-    list.insert(1);
-    list.insert(2);
-    list.insert(3);
-
-    list.display();
-
-    return 0;
-}
+avl_tree.pre_order_traversal(avl_tree.root)
