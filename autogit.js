96a9769bbb47371a8a879470c53359f2d56cec62
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:30:27 +0800

    12-9-2024 10:30 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,14 @@
-from collections import defaultdict
+import re
 
-class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
+def validate_email(email):
+    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(regex, email):
+        return True
+    else:
+        return False
 
-    def add_edge(self, u, v):
-        self.graph[u].append(v)
-
-    def topological_sort_util(self, v, visited, stack):
-        visited[v] = True
-        for i in self.graph[v]:
-            if not visited[i]:
-                self.topological_sort_util(i, visited, stack)
-        stack.append(v)
-
-    def topological_sort(self):
-        visited = [False] * len(self.graph)
-        stack = []
-        for i in range(len(self.graph)):
-            if not visited[i]:
-                self.topological_sort_util(i, visited, stack)
-        return stack[::-1]
-
-# Test the implementation
-g = Graph()
-g.add_edge(5, 2)
-g.add_edge(5, 0)
-g.add_edge(4, 0)
-g.add_edge(4, 1)
-g.add_edge(2, 3)
-g.add_edge(3, 1)
-
-result = g.topological_sort()
-print(result)
+email = "example@email.com"
+if validate_email(email):
+    print("Email is valid")
+else:
+    print("Email is not valid")
