a746e7fa4864137c970f904d2fd28f91825a6c0f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:20:36 +0800

    12-9-2024 10:20 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,11 @@
-class Graph:
-    def __init__(self, vertices):
-        self.V = vertices
-        self.graph = []
+# Define an array of integers
+arr = [4, 2, 7, 1, 9, 5]
 
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
+# Sort the array in ascending order
+sorted_arr = sorted(arr)
 
-    def bellman_ford(self, src):
-        dist = [float("Inf")] * self.V
-        dist[src] = 0
+print(sorted_arr)
+# Sort the array in descending order
+sorted_arr_desc = sorted(arr, reverse=True)
 
-        for _ in range(self.V - 1):
-            for u, v, w in self.graph:
-                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-
-        for u, v, w in self.graph:
-            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
-                print("Graph contains negative weight cycle")
-                return
-
-        print("Vertex Distance from Source")
-        for i in range(self.V):
-            print(f"{i}\t\t{dist[i]}")
-
-# Example Usage
-g = Graph(5)
-g.add_edge(0, 1, -1)
-g.add_edge(0, 2, 4)
-g.add_edge(1, 2, 3)
-g.add_edge(1, 3, 2)
-g.add_edge(1, 4, 2)
-g.add_edge(3, 2, 5)
-g.add_edge(3, 1, 1)
-g.add_edge(4, 3, -3)
-
-g.bellman_ford(0)
+print(sorted_arr_desc)
