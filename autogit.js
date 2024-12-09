2340ee6424d6eccc8313073ce1ad372dddd9b9bb
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:22:01 +0800

    12-9-2024 10:21 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,16 @@
-def longest_common_subsequence(s1, s2):
-    m, n = len(s1), len(s2)
-    dp = [[0] * (n+1) for _ in range(m+1)]
-
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-            else:
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-    result = ""
-    i, j = m, n
-    while i > 0 and j > 0:
-        if s1[i-1] == s2[j-1]:
-            result = s1[i-1] + result
-            i -= 1
+def is_palindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        while i < j and not s[i].isalnum():
+            i += 1
+        while i < j and not s[j].isalnum():
             j -= 1
-        elif dp[i-1][j] > dp[i][j-1]:
-            i -= 1
-        else:
-            j -= 1
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True
 
-    return result
-
-s1 = "ABCDGH"
-s2 = "AEDFHR"
-print(longest_common_subsequence(s1, s2))  # Output: "ADH"
+# Test the function
+s = "A man, a plan, a canal, Panama"
+print(is_palindrome(s))  # Output: True
