267b4d8a8034f1f6b96a47205f5feb110b860053
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:31:06 +0800

    12-9-2024 10:31 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,16 @@
-import re
+def find_nth_from_end(head, n):
+    p1 = head
+    p2 = head
 
-def validate_email(email):
-    regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    if re.match(regex, email):
-        return True
-    else:
-        return False
+    # Move p2 n steps ahead
+    for _ in range(n):
+        if p2 is None:
+            return None
+        p2 = p2.next
 
-email = "example@email.com"
-if validate_email(email):
-    print("Email is valid")
-else:
-    print("Email is not valid")
+    # Move p1 and p2 simultaneously until p2 reaches the end
+    while p2 is not None:
+        p1 = p1.next
+        p2 = p2.next
+
+    return p1
