8bde4b64bcef432c54f94eac2b887400a89bb18d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:26:52 +0800

    12-9-2024 10:26 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,26 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+def longest_common_substring(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    longest_length = 0
+    end_index = 0
+    
+    # Create a 2D table to store the lengths of longest common suffixes of substrings
+    table = [[0] * (n+1) for i in range(m+1)]
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if str1[i-1] == str2[j-1]:
+                table[i][j] = table[i-1][j-1] + 1
+                if table[i][j] > longest_length:
+                    longest_length = table[i][j]
+                    end_index = i - 1
+            else:
+                table[i][j] = 0
+    
+    return str1[end_index-longest_length+1:end_index+1]
 
 # Test the function
-num = 17
-if is_prime(num):
-    print(f"{num} is a prime number")
-else:
-    print(f"{num} is not a prime number")
+str1 = "abcdef"
+str2 = "xbcde"
+print(longest_common_substring(str1, str2))  # Output: "bcde"
