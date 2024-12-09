59644ff4038393c3b9a29563249916c9e115b3a3
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:21:45 +0800

    12-9-2024 10:21 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,7 @@
-class TreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+# Define an array of integers
+arr = [5, 2, 8, 3, 1]
 
-def count_leaf_nodes(node):
-    if node is None:
-        return 0
-    if node.left is None and node.right is None:
-        return 1
-    return count_leaf_nodes(node.left) + count_leaf_nodes(node.right)
+# Sort the array
+arr.sort()
 
-# Example of creating a binary tree
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right.left = TreeNode(6)
-root.right.right = TreeNode(7)
-
-# Calculate the number of leaf nodes
-num_leaf_nodes = count_leaf_nodes(root)
-print("Number of leaf nodes:", num_leaf_nodes)
+print(arr)
