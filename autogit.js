97f01d72404c02fd82750e13d1231d9437feed16
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:24:03 +0800

    12-9-2024 10:23 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,95 +1,15 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-        self.height = 1
+function binarySearch(arr, target):
+    left = 0
+    right = length(arr) - 1
 
-class AVLTree:
-    def __init__(self):
-        self.root = None
-        
-    def insert(self, root, key):
-        if not root:
-            return Node(key)
-        elif key < root.key:
-            root.left = self.insert(root.left, key)
+    while left <= right:
+        mid = left + (right - left) / 2
+
+        if arr[mid] == target:
+            return mid
+        else if arr[mid] < target:
+            left = mid + 1
         else:
-            root.right = self.insert(root.right, key)
-        
-        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
-        
-        balance = self.get_balance(root)
-        
-        # Left Left Case
-        if balance > 1 and key < root.left.key:
-            return self.right_rotate(root)
-        
-        # Right Right Case
-        if balance < -1 and key > root.right.key:
-            return self.left_rotate(root)
-        
-        # Left Right Case
-        if balance > 1 and key > root.left.key:
-            root.left = self.left_rotate(root.left)
-            return self.right_rotate(root)
-        
-        # Right Left Case
-        if balance < -1 and key < root.right.key:
-            root.right = self.right_rotate(root.right)
-            return self.left_rotate(root)
-        
-        return root
-    
-    def get_height(self, root):
-        if not root:
-            return 0
-        return root.height
-    
-    def get_balance(self, root):
-        if not root:
-            return 0
-        return self.get_height(root.left) - self.get_height(root.right)
-    
-    def left_rotate(self, z):
-        y = z.right
-        T2 = y.left
-        
-        y.left = z
-        z.right = T2
-        
-        z.height = 1 + max(self.get_height(z.left), self.get_height(z.right))
-        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
-        
-        return y
-    
-    def right_rotate(self, y):
-        x = y.left
-        T2 = x.right
-        
-        x.right = y
-        y.left = T2
-        
-        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
-        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
-        
-        return x
-    
-    def insert_key(self, key):
-        self.root = self.insert(self.root, key)
-        
-    def pre_order_traversal(self, root):
-        if root:
-            print("{0} ".format(root.key), end="")
-            self.pre_order_traversal(root.left)
-            self.pre_order_traversal(root.right)
+            right = mid - 1
 
-avl_tree = AVLTree()
-avl_tree.insert_key(10)
-avl_tree.insert_key(20)
-avl_tree.insert_key(30)
-avl_tree.insert_key(40)
-avl_tree.insert_key(50)
-avl_tree.insert_key(25)
-
-avl_tree.pre_order_traversal(avl_tree.root)
+    return -1
