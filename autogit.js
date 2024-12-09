dbf2a179b42df7e2ecb408da22cdd86c15e91f12
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:08:35 +0800

    12-9-2024 10:8 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,35 @@
-my_string = "123"
-my_integer = int(my_string)
-print(my_integer)
-String myString = "123";
-int myInteger = Integer.parseInt(myString);
-System.out.println(myInteger);
-#include <iostream>
-#include <string>
-#include <sstream>
+from collections import defaultdict
 
-int main() {
-    std::string myString = "123";
-    int myInteger;
-    std::istringstream(myString) >> myInteger;
-    std::cout << myInteger << std::endl;
-    return 0;
+def topological_sort(graph):
+    indegree = {node: 0 for node in graph}
+    for node in graph:
+        for neighbor in graph[node]:
+            indegree[neighbor] += 1
+    
+    queue = [node for node in graph if indegree[node] == 0]
+    result = []
+    
+    while queue:
+        node = queue.pop(0)
+        result.append(node)
+        for neighbor in graph[node]:
+            indegree[neighbor] -= 1
+            if indegree[neighbor] == 0:
+                queue.append(neighbor)
+    
+    if len(result) == len(graph):
+        return result
+    else:
+        return None
+
+# Example usage
+graph = {
+    'A': ['C', 'D'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': [],
+    'F': []
 }
-let myString = "123";
-let myInteger = parseInt(myString);
-console.log(myInteger);
+
+print(topological_sort(graph))
