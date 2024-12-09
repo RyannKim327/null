3456e303d33a750f14b35ca9b0f305202b1f7f9b
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:23:52 +0800

    12-9-2024 10:23 [Parang malabo na pala, di na ko aasa]

---
+++ autogit.js
@@ -0,0 +1,57 @@
+#include <iostream>
+
+// Node class to represent each element in the linked list
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+// Linked list class to manage the nodes
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insert(int data) {
+        Node* newNode = new Node(data);
+
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insert(1);
+    list.insert(2);
+    list.insert(3);
+
+    list.display();
+
+    return 0;
+}
