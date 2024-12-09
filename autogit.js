dac563fb8f49235ee4006fd9c8db98bd3c370867
RyannKim327 <rksesgundo123@gmail.com>
2024-12-09 10:20:52 +0800

    12-9-2024 10:20 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,10 @@
-def burrows_wheeler_transform(text):
-    text += '$'  # Add a marker character
-    rotations = [text[i:] + text[:i] for i in range(len(text))]  # Generate all cyclic rotations of the text
-    rotations.sort()  # Sort the rotations lexicographically
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-    transformed_text = ''.join(rotation[-1] for rotation in rotations)  # Get the last character of each rotation
-    index = rotations.index(text)  # Find the index of the original text
-    return transformed_text, index
-
-def inverse_burrows_wheeler_transform(transformed_text, index):
-    table = [''] * len(transformed_text)
-    for i in range(len(transformed_text)):
-        table = sorted(transformed_text[i] + table[i] for i in range(len(transformed_text)))
-
-    original_text = table[index]
-    return original_text.rstrip('$')
-
-# Test the implementation
-text = 'banana'
-transformed_text, index = burrows_wheeler_transform(text)
-print('Transformed text:', transformed_text)
-print('Original text:', inverse_burrows_wheeler_transform(transformed_text, index))
+# Test the factorial function
+num = 5
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
