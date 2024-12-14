e03f1023d72e95c5fe629ba70878e1e2827da8f1
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:55:34 +0800

    12-14-2024 23:55 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,11 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    
-    mid = len(arr) // 2
-    left = arr[:mid]
-    right = arr[mid:]
-    
-    left = merge_sort(left)
-    right = merge_sort(right)
-    
-    return merge(left, right)
+import schedule
+import time
 
-def merge(left, right):
-    result = []
-    i = j = 0
-    
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-    
-    result += left[i:]
-    result += right[j:]
-    
-    return result
+def job():
+    print("Task executed at " + str(time.time()))
 
-# Example usage
-arr = [38, 27, 43, 3, 9, 82, 10]
-sorted_arr = merge_sort(arr)
-print(sorted_arr)
+schedule.every(10).seconds.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
