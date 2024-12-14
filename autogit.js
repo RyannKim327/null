6439e7e69eb9b56502b068fef34823926cdce9bf
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:52:29 +0800

    12-14-2024 23:52 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,41 @@
-decimal_num = 10
-binary_num = bin(decimal_num)
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
 
-print(f"The binary representation of {decimal_num} is: {binary_num}")
-The binary representation of 10 is: 0b1010
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def bellman_ford(self, src):
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+
+        for _ in range(self.V - 1):
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                print("Graph contains negative weight cycle")
+                return
+
+        self.print_solution(dist)
+
+    def print_solution(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(len(dist)):
+            print(f"{i}\t\t{dist[i]}")
+
+
+g = Graph(5)
+g.add_edge(0, 1, -1)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 3)
+g.add_edge(1, 3, 2)
+g.add_edge(1, 4, 2)
+g.add_edge(3, 2, 5)
+g.add_edge(3, 1, 1)
+g.add_edge(4, 3, -3)
+
+g.bellman_ford(0)
