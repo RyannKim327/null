674ab979643319d9db80e25d263b2d80964f0841
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 00:00:38 +0800

    12-15-2024 0:0 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,31 @@
-def getIntersectionNode(headA, headB):
-    if not headA or not headB:
-        return None
+import sys
 
-    lenA = getLength(headA)
-    lenB = getLength(headB)
+def dijkstra(graph, source):
+    nodes = set(graph.keys())
+    dist = {node: float('inf') for node in nodes}
+    dist[source] = 0
+    
+    while nodes:
+        min_node = min(nodes, key=lambda x: dist[x])
+        nodes.remove(min_node)
+        
+        for neighbor in graph[min_node]:
+            new_dist = dist[min_node] + graph[min_node][neighbor]
+            if new_dist < dist[neighbor]:
+                dist[neighbor] = new_dist
+    
+    return dist
 
-    currA = headA
-    currB = headB
+# Example graph
+graph = {
+    'A': {'B': 5, 'C': 3},
+    'B': {'A': 5, 'C': 2, 'D': 1},
+    'C': {'A': 3, 'B': 2, 'D': 4},
+    'D': {'B': 1, 'C': 4}
+}
 
-    while lenA > lenB:
-        currA = currA.next
-        lenA -= 1
+source = 'A'
+shortest_paths = dijkstra(graph, source)
 
-    while lenB > lenA:
-        currB = currB.next
-        lenB -= 1
-
-    while currA != currB:
-        currA = currA.next
-        currB = currB.next
-
-    return currA
-
-def getLength(node):
-    length = 0
-
-    while node:
-        length += 1
-        node = node.next
-
-    return length
+for node in shortest_paths:
+    print(f'Shortest path from {source} to {node}: {shortest_paths[node]}')
