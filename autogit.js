80d547297c457ea8cbe212a16523cf9fca6cc387
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 00:05:08 +0800

    12-15-2024 0:5 [Kahit anong gawin prii, sya pa rin]

---
+++ autogit.js
@@ -0,0 +1,14 @@
+import re
+
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if re.match(pattern, email):
+        return True
+    else:
+        return False
+
+email = "test@example.com"
+if validate_email(email):
+    print("Valid email address")
+else:
+    print("Invalid email address")
