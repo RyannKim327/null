73b03011cc2c4ab599dfe631546b17daa00e73be
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:58:30 +0800

    12-14-2024 23:58 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,7 @@
-def are_anagrams(str1, str2):
-    str1 = str1.lower().replace(" ", "").replace(",", "").replace(".", "")
-    str2 = str2.lower().replace(" ", "").replace(",", "").replace(".", "")
-
-    return sorted(str1) == sorted(str2)
+def second_largest(arr):
+    arr.sort(reverse=True)
+    return arr[1]
 
 # Test the function
-str1 = "Listen"
-str2 = "Silent"
-if are_anagrams(str1, str2):
-    print(f"'{str1}' and '{str2}' are anagrams.")
-else:
-    print(f"'{str1}' and '{str2}' are not anagrams.")
+arr = [10, 5, 20, 8]
+print(second_largest(arr))  # Output: 10
