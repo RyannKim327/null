d5187e4adcd46349247935741f7365e9ccd5e234
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:54:19 +0800

    12-14-2024 23:54 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,53 +1,27 @@
-class Graph:
-    def __init__(self, num_vertices):
-        self.num_vertices = num_vertices
-        self.adj_list = [[] for _ in range(num_vertices)]
-        self.index = 0
-        self.stack = []
-        self.lowlink = [-1] * num_vertices
-        self.on_stack = [False] * num_vertices
-        self.scc_list = []
-
-    def add_edge(self, u, v):
-        self.adj_list[u].append(v)
-
-    def tarjan_scc(self, v):
-        self.lowlink[v] = self.index
-        self.index += 1
-        self.stack.append(v)
-        self.on_stack[v] = True
-
-        for w in self.adj_list[v]:
-            if self.lowlink[w] == -1:
-                self.tarjan_scc(w)
-                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
-            elif self.on_stack[w]:
-                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
-
-        if self.lowlink[v] == v:
-            scc = []
-            while True:
-                w = self.stack.pop()
-                self.on_stack[w] = False
-                scc.append(w)
-                if w == v:
-                    break
-            self.scc_list.append(scc)
-
-    def find_scc(self):
-        for v in range(self.num_vertices):
-            if self.lowlink[v] == -1:
-                self.tarjan_scc(v)
-
-        return self.scc_list
-
-# Example usage
-g = Graph(5)
-g.add_edge(0, 1)
-g.add_edge(1, 2)
-g.add_edge(2, 0)
-g.add_edge(1, 3)
-g.add_edge(3, 4)
-
-scc_list = g.find_scc()
-print(scc_list)
+def compute_prefix(pattern):
+    m = len(pattern)
+    prefix = [0] * m
+    prefix[0] = 0
+    k = 0
+    for q in range(1, m):
+        while k > 0 and pattern[k] != pattern[q]:
+            k = prefix[k - 1]
+        if pattern[k] == pattern[q]:
+            k += 1
+        prefix[q] = k
+    return prefix
+def kmp_search(text, pattern):
+    n = len(text)
+    m = len(pattern)
+    prefix = compute_prefix(pattern)
+    q = 0
+    matches = []
+    for i in range(n):
+        while q > 0 and pattern[q] != text[i]:
+            q = prefix[q - 1]
+        if pattern[q] == text[i]:
+            q += 1
+        if q == m:
+            matches.append(i - m + 1)
+            q = prefix[q - 1]
+    return matches
