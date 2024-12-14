b0301c7be78756ce49b5409702f35ff715d03bfd
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:57:29 +0800

    12-14-2024 23:57 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,44 @@
-def longest_increasing_subsequence(array):
-    if not array:
-        return 0
+def fibonacci_search(arr, x):
+    # Create Fibonacci series
+    fib_M_minus_2 = 0
+    fib_M_minus_1 = 1
+    fib_M = fib_M_minus_1 + fib_M_minus_2
 
-    n = len(array)
-    lis = [1] * n
+    while fib_M < len(arr):
+        fib_M_minus_2 = fib_M_minus_1
+        fib_M_minus_1 = fib_M
+        fib_M = fib_M_minus_1 + fib_M_minus_2
 
-    for i in range(1, n):
-        for j in range(i):
-            if array[i] > array[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
+    offset = -1
 
-    return max(lis)
+    while fib_M > 1:
+        i = min(offset + fib_M_minus_2, len(arr) - 1)
 
-# Example
-array = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-result = longest_increasing_subsequence(array)
-print("Length of Longest Increasing Subsequence is:", result)
+        if arr[i] < x:
+            fib_M = fib_M_minus_1
+            fib_M_minus_1 = fib_M_minus_2
+            fib_M_minus_2 = fib_M - fib_M_minus_1
+            offset = i
+
+        elif arr[i] > x:
+            fib_M = fib_M_minus_2
+            fib_M_minus_1 = fib_M_minus_1 - fib_M_minus_2
+            fib_M_minus_2 = fib_M - fib_M_minus_1
+
+        else:
+            return i
+
+    if fib_M_minus_1 and arr[offset + 1] == x:
+        return offset + 1
+
+    return -1
+
+# Test the Fibonacci search algorithm
+arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100]
+x = 85
+
+index = fibonacci_search(arr, x)
+if index != -1:
+    print("Element found at index:", index)
+else:
+    print("Element not found")
