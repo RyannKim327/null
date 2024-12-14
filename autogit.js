25852a3ed79a2d4c6b725de7c111f47003e26154
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:56:24 +0800

    12-14-2024 23:56 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,16 @@
-def bubble_sort(arr):
+def selection_sort(arr):
     n = len(arr)
-
+    
     for i in range(n):
-        # Flag to check if any swapping is done in this pass
-        swapped = False
-
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                # Swap elements
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-
-        # If no two elements were swapped in this pass, array is sorted
-        if not swapped:
-            break
-
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
     return arr
 
-# Test the bubble sort algorithm
-arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = bubble_sort(arr)
-print("Sorted array:", sorted_arr)
+# Test the selection sort algorithm
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array is:", sorted_arr)
