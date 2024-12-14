1d85b7fbbf5eb83551d01907a03fa21977c2e2e8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:53:36 +0800

    12-14-2024 23:53 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,53 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Graph:
+    def __init__(self, num_vertices):
+        self.num_vertices = num_vertices
+        self.adj_list = [[] for _ in range(num_vertices)]
+        self.index = 0
+        self.stack = []
+        self.lowlink = [-1] * num_vertices
+        self.on_stack = [False] * num_vertices
+        self.scc_list = []
 
-def nth_node_from_end(head, n):
-    main_ptr = head
-    ref_ptr = head
+    def add_edge(self, u, v):
+        self.adj_list[u].append(v)
 
-    # Move ref_ptr n steps ahead
-    for i in range(n):
-        if ref_ptr is None:
-            return None  # n is greater than the length of the linked list
-        ref_ptr = ref_ptr.next
+    def tarjan_scc(self, v):
+        self.lowlink[v] = self.index
+        self.index += 1
+        self.stack.append(v)
+        self.on_stack[v] = True
 
-    # Move both pointers until ref_ptr reaches the end
-    while ref_ptr is not None:
-        main_ptr = main_ptr.next
-        ref_ptr = ref_ptr.next
+        for w in self.adj_list[v]:
+            if self.lowlink[w] == -1:
+                self.tarjan_scc(w)
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+            elif self.on_stack[w]:
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
 
-    return main_ptr
+        if self.lowlink[v] == v:
+            scc = []
+            while True:
+                w = self.stack.pop()
+                self.on_stack[w] = False
+                scc.append(w)
+                if w == v:
+                    break
+            self.scc_list.append(scc)
 
-# Helper function to print the linked list
-def print_list(node):
-    while node:
-        print(node.data, end=' ')
-        node = node.next
-    print()
+    def find_scc(self):
+        for v in range(self.num_vertices):
+            if self.lowlink[v] == -1:
+                self.tarjan_scc(v)
+
+        return self.scc_list
 
 # Example usage
-head = Node(1)
-head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = Node(4)
-head.next.next.next.next = Node(5)
+g = Graph(5)
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(1, 3)
+g.add_edge(3, 4)
 
-n = 2
-result = nth_node_from_end(head, n)
-if result:
-    print(f"The {n}th node from the end of the linked list is: {result.data}")
-else:
-    print("The value of 'n' is greater than the length of the linked list.")
+scc_list = g.find_scc()
+print(scc_list)
