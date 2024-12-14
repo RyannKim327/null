e15fe47a75b2f4065de092b243873fbfc421e6ae
RyannKim327 <rksesgundo123@gmail.com>
2024-12-14 23:59:52 +0800

    12-14-2024 23:59 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,122 @@
+RED = 'RED'
+BLACK = 'BLACK'
+
 class Node:
-    def __init__(self, key):
+    def __init__(self, key, color=RED):
+        self.key = key
         self.left = None
         self.right = None
-        self.val = key
+        self.parent = None
+        self.color = color
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if root.val < key:
-            root.right = insert(root.right, key)
+class RedBlackTree:
+    def __init__(self):
+        self.NIL = Node(None, BLACK)
+        self.root = self.NIL
+
+    def insert(self, key):
+        new_node = Node(key)
+        new_node.left = self.NIL
+        new_node.right = self.NIL
+
+        parent = None
+        current = self.root
+
+        while current != self.NIL:
+            parent = current
+            if new_node.key < current.key:
+                current = current.left
+            else:
+                current = current.right
+
+        new_node.parent = parent
+
+        if parent is None:
+            self.root = new_node
+        elif new_node.key < parent.key:
+            parent.left = new_node
         else:
-            root.left = insert(root.left, key)
-    return root
+            parent.right = new_node
 
-def inorder_traversal(node):
-    if node:
-        inorder_traversal(node.left)
-        print(node.val)
-        inorder_traversal(node.right)
+        new_node.color = RED
+        self._fix_insert(new_node)
 
-# Usage:
-root = None
-root = insert(root, 5)
-root = insert(root, 3)
-root = insert(root, 7)
-root = insert(root, 1)
-root = insert(root, 4)
+    def _fix_insert(self, node):
+        while node.parent.color == RED:
+            if node.parent == node.parent.parent.left:
+                uncle = node.parent.parent.right
+                if uncle.color == RED:
+                    node.parent.color = BLACK
+                    uncle.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self._left_rotate(node)
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._right_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.left
+                if uncle.color == RED:
+                    node.parent.color = BLACK
+                    uncle.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self._right_rotate(node)
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._left_rotate(node.parent.parent)
 
-inorder_traversal(root)
+        self.root.color = BLACK
+
+    def _left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.NIL:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def _right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.NIL:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def inorder_traversal(self):
+        self._inorder_traversal_helper(self.root)
+
+    def _inorder_traversal_helper(self, node):
+        if node != self.NIL:
+            self._inorder_traversal_helper(node.left)
+            print(node.key, "-", node.color)
+            self._inorder_traversal_helper(node.right)
+
+# Example Usage
+tree = RedBlackTree()
+tree.insert(10)
+tree.insert(20)
+tree.insert(30)
+tree.insert(15)
+tree.inorder_traversal()
