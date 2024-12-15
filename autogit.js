0c3b94ab948eb6761db6b76f7779e3b07a745e18
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:03:17 +0800

    12-15-2024 8:3 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,64 +1,13 @@
-def boyer_moore(text, pattern):
-    def pre_process(pattern):
-        m = len(pattern)
-        bad_char = [-1] * 256  # Initialize bad character array
-        for i in range(m):
-            bad_char[ord(pattern[i])] = i
-        return bad_char
+def max_sum_subarray(arr):
+    max_sum = arr[0]
+    current_sum = arr[0]
 
-    def good_suffix_table(pattern):
-        m = len(pattern)
-        table = [0] * m
-        table[m - 1] = m
-        length = suffix_length(pattern)
+    for num in arr[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
 
-        for i in range(m - 2, -1, -1):
-            if length[i] == i + 1:
-                table[m - 1] = i + 1
+    return max_sum
 
-        for i in range(m - 1):
-            table[m - length[i] - 1] = m - 1 - i
-
-        return table
-
-    def suffix_length(pattern):
-        m = len(pattern)
-        length = [0] * m
-        length[m - 1] = m
-        g = m - 1
-        f = 0
-
-        for i in range(m - 2, -1, -1):
-            if i > g and length[i + m - 1 - f] < i - g:
-                length[i] = length[i + m - 1 - f]
-            else:
-                if i < g:
-                    g = i
-                f = i
-                while g >= 0 and pattern[g] == pattern[g + m - 1 - f]:
-                    g -= 1
-                length[i] = f - g
-
-        return length
-
-    n = len(text)
-    m = len(pattern)
-    bc_table = pre_process(pattern)
-    gs_table = good_suffix_table(pattern)
-
-    i = 0
-    while i <= n - m:
-        j = m - 1
-        while j >= 0 and pattern[j] == text[i + j]:
-            j -= 1
-        if j < 0:
-            print("Pattern found at index", i)
-            i += gs_table[0]
-        else:
-            bc_shift = j - bc_table[ord(text[i + j])]
-            gs_shift = gs_table[j]
-            i += max(bc_shift, gs_shift)
-
-text = "ABAAABCD"
-pattern = "ABC"
-boyer_moore(text, pattern)
+# Test the function
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_sum_subarray(arr))  # Output should be 6 (The subarray [4, -1, 2, 1] has the maximum sum)
