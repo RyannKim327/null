8cff7e46dee0e72ee13fcdea9c09d43ac67d87c0
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:02:19 +0800

    12-15-2024 8:2 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,49 @@
 class Node:
     def __init__(self, value):
         self.value = value
-        self.left = None
-        self.right = None
+        self.next = None
 
-def max_depth(node):
-    if node is None:
-        return 0
-    else:
-        left_depth = max_depth(node.left)
-        right_depth = max_depth(node.right)
-        return max(left_depth, right_depth) + 1
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-# Example of creating a binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
 
-print("Maximum depth of the binary tree is:", max_depth(root))
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_temp = current.next
+            current.next = prev
+            prev = current
+            current = next_temp
+        self.head = prev
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.value, end=' ')
+            current = current.next
+
+# Test the implementation
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+
+print("Original Linked List:")
+llist.print_list()
+
+llist.reverse()
+
+print("\nReversed Linked List:")
+llist.print_list()
