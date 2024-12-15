c40143ae524e841423d09c7a0be5da1b2a94a4f0
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:04:12 +0800

    12-15-2024 8:4 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,66 +1,21 @@
-class SkipNode:
-    def __init__(self, value, level):
-        self.value = value
-        self.next = [None] * level
-import random
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
 
-class SkipList:
-    def __init__(self, max_levels, probability):
-        self.max_levels = max_levels
-        self.probability = probability
-        self.head = SkipNode(float('-inf'), max_levels)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
 
-    def random_level(self):
-        level = 1
-        while random.random() < self.probability and level < self.max_levels:
-            level += 1
-        return level
+# Example graph as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
 
-    def insert(self, value):
-        level = self.random_level()
-        new_node = SkipNode(value, level)
-        current = self.head
-
-        for i in range(level - 1, -1, -1):
-            while current.next[i] and current.next[i].value < value:
-                current = current.next[i]
-            new_node.next[i] = current.next[i]
-            current.next[i] = new_node
-
-    def search(self, value):
-        current = self.head
-        for i in range(self.max_levels - 1, -1, -1):
-            while current.next[i] and current.next[i].value < value:
-                current = current.next[i]
-        if current.next[0] and current.next[0].value == value:
-            return True
-        return False
-
-    def delete(self, value):
-        current = self.head
-        for i in range(self.max_levels - 1, -1, -1):
-            while current.next[i] and current.next[i].value < value:
-                current = current.next[i]
-            if current.next[i] and current.next[i].value == value:
-                current.next[i] = current.next[i].next[i]
-
-    def print_list(self):
-        current = self.head
-        while current.next[0]:
-            print(current.next[0].value, end=' ')
-            current = current.next[0]
-        print()
-skip_list = SkipList(4, 0.5)
-skip_list.insert(3)
-skip_list.insert(6)
-skip_list.insert(2)
-skip_list.insert(4)
-skip_list.insert(5)
-
-print(skip_list.search(3))  # Output: True
-print(skip_list.search(7))  # Output: False
-
-skip_list.delete(3)
-print(skip_list.search(3))  # Output: False
-
-skip_list.print_list()  # Output: 2 4 5 6
+dfs(graph, 'A')
