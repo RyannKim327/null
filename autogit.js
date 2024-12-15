56569414d287300754c7c341309c3cf485cb0e2e
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:04:29 +0800

    12-15-2024 8:4 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,20 @@
-def depth_limited_search_iterative(problem, depth_limit):
-    for depth in range(1, depth_limit + 1):
-        result = depth_limited_search(problem, depth)
-        if result is not None:
-            return result
-    return None
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-def depth_limited_search(problem, depth):
-    return recursive_dls(problem, problem.get_start_state(), depth)
-
-def recursive_dls(problem, node, depth):
-    if problem.is_goal(node):
-        return node
-    elif depth == 0:
-        return None
-    else:
-        for action in problem.get_actions(node):
-            child_node = problem.get_child_node(node, action)
-            result = recursive_dls(problem, child_node, depth - 1)
-            if result is not None:
-                return result
-    return None
+# Test the function
+num = 17
+if is_prime(num):
+    print(num, "is a prime number")
+else:
+    print(num, "is not a prime number")
