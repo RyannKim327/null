c66a9082fb02e5bc41d2d913262cfbe285193ad6
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:03:25 +0800

    12-15-2024 8:3 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,3 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-def height(node):
-    if node is None:
-        return 0
-    else:
-        return 1 + max(height(node.left), height(node.right))
-
-def diameter(root):
-    if root is None:
-        return 0
-    
-    lheight = height(root.left)
-    rheight = height(root.right)
-    
-    ldiameter = diameter(root.left)
-    rdiameter = diameter(root.right)
-    
-    return max(lheight + rheight + 1, max(ldiameter, rdiameter))
-
-# Example usage:
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Diameter of the binary tree is", diameter(root))
+array = [10, 5, 8, 15, 3]
+max_value = max(array)
+print("Maximum value in the array is:", max_value)
