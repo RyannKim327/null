68545190bb277ecae748d9fbba29aa058ca14928
RyannKim327 <rksesgundo123@gmail.com>
2024-12-15 08:02:23 +0800

    12-15-2024 8:2 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,49 +1,17 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+def selection_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    
+    return arr
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
-
-    def reverse(self):
-        prev = None
-        current = self.head
-        while current:
-            next_temp = current.next
-            current.next = prev
-            prev = current
-            current = next_temp
-        self.head = prev
-
-    def print_list(self):
-        current = self.head
-        while current:
-            print(current.value, end=' ')
-            current = current.next
-
-# Test the implementation
-llist = LinkedList()
-llist.append(1)
-llist.append(2)
-llist.append(3)
-llist.append(4)
-
-print("Original Linked List:")
-llist.print_list()
-
-llist.reverse()
-
-print("\nReversed Linked List:")
-llist.print_list()
+# Example usage
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array:", sorted_arr)
