d964b897fb27eb2b9f2ed22776943d9a8b72c552
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:22:52 +0800

    12-16-2024 21:22 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,29 @@
-import requests
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
 
-api_key = 'your_api_key_here'
-city = 'New York'
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    if left < n and arr[i] < arr[left]:
+        largest = left
 
-response = requests.get(url)
-data = response.json()
+    if right < n and arr[largest] < arr[right]:
+        largest = right
 
-if data['cod'] == 200:
-    temperature = data['main']['temp']
-    description = data['weather'][0]['description']
-    print(f'Temperature in {city}: {temperature}K')
-    print(f'Weather description: {description}')
-else:
-    print('Error fetching data')
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# Example usage
+arr = [12, 11, 13, 5, 6, 7]
+heap_sort(arr)
+print("Sorted array is", arr)
