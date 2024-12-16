0c18aa487a1e4cec928e88b41a88a0e8efbd3169
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:16:31 +0800

    12-16-2024 21:16 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,39 +1,20 @@
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(size)]
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for next_node in graph[start] - visited:
+        dfs(graph, next_node, visited)
+    return visited
 
-    def _hash_function(self, key):
-        return hash(key) % self.size
+# Example graph represented as an adjacency list
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
 
-    def add(self, key, value):
-        hash_key = self._hash_function(key)
-        for pair in self.table[hash_key]:
-            if pair[0] == key:
-                pair[1] = value
-                return
-        self.table[hash_key].append([key, value])
-
-    def get(self, key):
-        hash_key = self._hash_function(key)
-        for pair in self.table[hash_key]:
-            if pair[0] == key:
-                return pair[1]
-        return None
-
-    def remove(self, key):
-        hash_key = self._hash_function(key)
-        for i, pair in enumerate(self.table[hash_key]):
-            if pair[0] == key:
-                del self.table[hash_key][i]
-                return
-
-# Example usage
-hash_table = HashTable(10)
-hash_table.add('key1', 'value1')
-hash_table.add('key2', 'value2')
-
-print(hash_table.get('key1'))  # Output: value1
-
-hash_table.remove('key1')
-print(hash_table.get('key1'))  # Output: None
+dfs(graph, 'A')
