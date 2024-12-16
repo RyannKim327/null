16d69855f8ec64e2a71c52997e39c45fcced36d1
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:12:32 +0800

    12-16-2024 21:12 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,14 @@
-def counting_sort(arr):
-    max_num = max(arr)
-    min_num = min(arr)
-    count_arr = [0] * (max_num - min_num + 1)
-    output = [0] * len(arr)
+string = "Hello, World!"
+substring = "World"
 
-    for num in arr:
-        count_arr[num - min_num] += 1
+if substring in string:
+    print("Substring found in the string.")
+else:
+    print("Substring not found in the string.")
+string = "Hello, World!"
+substring = "World"
 
-    for i in range(1, len(count_arr)):
-        count_arr[i] += count_arr[i - 1]
-
-    for num in reversed(arr):
-        output[count_arr[num - min_num] - 1] = num
-        count_arr[num - min_num] -= 1
-
-    return output
-
-# Example usage
-arr = [4, 2, 2, 8, 3, 3, 1]
-sorted_arr = counting_sort(arr)
-print("Sorted array is:", sorted_arr)
+if string.find(substring) != -1:
+    print("Substring found in the string.")
+else:
+    print("Substring not found in the string.")
