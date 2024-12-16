11538d41f9fdd3c87f7a6b74297c89cb19de2fac
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:10:26 +0800

    12-16-2024 21:10 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,37 @@
-def is_sorted_ascending(arr):
-    for i in range(len(arr) - 1):
-        if arr[i] > arr[i + 1]:
-            return False
-    return True
+class Node:
+    def __init__(self, start_index, end_index):
+        self.start_index = start_index
+        self.end_index = end_index
+        self.children = {}
+class SuffixTree:
+    def __init__(self, text):
+        self.root = Node(None, None)
+        self.text = text
+        self.build_suffix_tree()
 
-# Example usage
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [5, 4, 3, 2, 1]
+    def build_suffix_tree(self):
+        n = len(self.text)
+        for i in range(n):
+            self.add_suffix(self.text[i:], i)
 
-print(is_sorted_ascending(arr1))  # Output: True
-print(is_sorted_ascending(arr2))  # Output: False
+    def add_suffix(self, suffix, index):
+        current_node = self.root
+        for i in range(len(suffix)):
+            c = suffix[i]
+            if c not in current_node.children:
+                current_node.children[c] = Node(index + i, len(self.text) - 1)
+                return
+            else:
+                child = current_node.children[c]
+                j = 0
+                while j < child.end_index - child.start_index + 1 and suffix[i + j] == self.text[child.start_index + j]:
+                    j += 1
+                if j == child.end_index - child.start_index + 1:
+                    current_node = child
+                    i += j - 1
+                else:
+                    child_split = Node(child.start_index, child.start_index + j - 1)
+                    child.start_index += j
+                    current_node.children[c] = Node(index + i, len(self.text) - 1)
+                    current_node.children[c].children[self.text[child.start_index]] = child_split
+                    return
