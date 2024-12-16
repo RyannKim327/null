32c3e176c2623d7266422eecc720fbe8cd9a1a30
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:19:52 +0800

    12-16-2024 21:19 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,9 @@
-function DFS(graph, start_node):
-    create a stack data structure
-    push start_node to the stack
+def is_palindrome(s):
+    return s == s[::-1]
 
-    while the stack is not empty:
-        current_node = pop from the stack
-        if current_node is not visited:
-            mark current_node as visited
-            process current_node
-
-            for each neighbor of current_node in graph:
-                push neighbor to the stack
-def dfs(graph, start_node):
-    visited = set()
-    stack = [start_node]
-
-    while stack:
-        current_node = stack.pop()
-        if current_node not in visited:
-            visited.add(current_node)
-            print(current_node)
-
-            for neighbor in graph[current_node]:
-                if neighbor not in visited:
-                    stack.append(neighbor)
-
-# Example usage
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-dfs(graph, 'A')
+# Test the function
+input_string = "madam"
+if is_palindrome(input_string):
+    print(f"{input_string} is a palindrome")
+else:
+    print(f"{input_string} is not a palindrome")
