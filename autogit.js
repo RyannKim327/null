3277b9922a696296c6c9748bcf51f1125fd2b034
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:16:35 +0800

    12-16-2024 21:16 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,23 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    print(start)
-    for next_node in graph[start] - visited:
-        dfs(graph, next_node, visited)
-    return visited
+def longest_increasing_subsequence(arr):
+    n = len(arr)
+    lis = [1] * n
 
-# Example graph represented as an adjacency list
-graph = {
-    'A': {'B', 'C'},
-    'B': {'A', 'D', 'E'},
-    'C': {'A', 'F'},
-    'D': {'B'},
-    'E': {'B', 'F'},
-    'F': {'C', 'E'}
-}
+    for i in range(1, n):
+        for j in range(0, i):
+            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
+                lis[i] = lis[j] + 1
 
-dfs(graph, 'A')
+    max_length = max(lis)
+    index = lis.index(max_length)
+    sequence = [arr[index]]
+
+    for i in range(index - 1, -1, -1):
+        if lis[i] == max_length - 1:
+            sequence = [arr[i]] + sequence
+            max_length -= 1
+
+    return sequence
+
+# Test the function
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print(longest_increasing_subsequence(arr))
