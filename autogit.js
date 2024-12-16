4b11ad84c725b5ecd43c3c686b6d18b46516a0a9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:12:28 +0800

    12-16-2024 21:12 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,22 @@
-def first_non_repeating_char(input_string):
-    char_freq = {}
-    
-    # Count the frequency of each character
-    for char in input_string:
-        if char in char_freq:
-            char_freq[char] += 1
-        else:
-            char_freq[char] = 1
-    
-    # Find the first non-repeating character
-    for char in input_string:
-        if char_freq[char] == 1:
-            return char
-    
-    return None
+def counting_sort(arr):
+    max_num = max(arr)
+    min_num = min(arr)
+    count_arr = [0] * (max_num - min_num + 1)
+    output = [0] * len(arr)
 
-# Test the function
-input_string = "hello world"
-result = first_non_repeating_char(input_string)
-if result:
-    print("The first non-repeating character is:", result)
-else:
-    print("No non-repeating character found in the input string.")
+    for num in arr:
+        count_arr[num - min_num] += 1
+
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i - 1]
+
+    for num in reversed(arr):
+        output[count_arr[num - min_num] - 1] = num
+        count_arr[num - min_num] -= 1
+
+    return output
+
+# Example usage
+arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(arr)
+print("Sorted array is:", sorted_arr)
