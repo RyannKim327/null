fd0c96ab3c5c2acf4711de52887018994f09f4b2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:09:25 +0800

    12-16-2024 21:9 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,32 @@
-str = "Hello, World!"
-reversed_str = str[::-1]
-print(reversed_str)
-str = "Hello, World!"
-reversed_str = ''.join(reversed(str))
-print(reversed_str)
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+def diameter(node):
+    if node is None:
+        return 0
+
+    left_height = height(node.left)
+    right_height = height(node.right)
+
+    left_diameter = diameter(node.left)
+    right_diameter = diameter(node.right)
+
+    return max(left_height + right_height + 1, max(left_diameter, right_diameter))
+
+def height(node):
+    if node is None:
+        return 0
+
+    return 1 + max(height(node.left), height(node.right))
+
+# Constructing a sample binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Diameter of the binary tree is:", diameter(root))
