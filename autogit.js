e15a62c01e49de7eae8bae415bb8ff5d49e743c8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:08:15 +0800

    12-16-2024 21:8 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,44 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
 
-def height(node):
-    if node is None:
-        return 0
-    return 1 + max(height(node.left), height(node.right))
+class BinaryTree:
+    def __init__(self):
+        self.root = None
 
-def diameter(node):
-    if node is None:
-        return 0
-    
-    left_height = height(node.left)
-    right_height = height(node.right)
-    
-    left_diameter = diameter(node.left)
-    right_diameter = diameter(node.right)
-    
-    return max(left_height + right_height + 1, max(left_diameter, right_diameter))
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_recursive(self.root, key)
 
-# Example usage:
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+    def _insert_recursive(self, current_node, key):
+        if key < current_node.key:
+            if current_node.left is None:
+                current_node.left = Node(key)
+            else:
+                self._insert_recursive(current_node.left, key)
+        else:
+            if current_node.right is None:
+                current_node.right = Node(key)
+            else:
+                self._insert_recursive(current_node.right, key)
 
-print("Diameter of the binary tree is:", diameter(root))
+    def inorder_traversal(self, node):
+        if node is not None:
+            self.inorder_traversal(node.left)
+            print(node.key, end=' ')
+            self.inorder_traversal(node.right)
+
+# Example usage
+bt = BinaryTree()
+bt.insert(5)
+bt.insert(3)
+bt.insert(7)
+bt.insert(2)
+bt.insert(4)
+
+print("Inorder traversal of binary tree:")
+bt.inorder_traversal(bt.root)
