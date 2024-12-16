8c3d6fa8650f249d1820efe48d15b869dc47902e
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:14:50 +0800

    12-16-2024 21:14 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,25 @@
-function largestPrimeFactor(number):
-    maxPrime = -1
-    
-    while number % 2 == 0:
-        maxPrime = 2
-        number = number / 2
-    
-    for i from 3 to sqrt(number) step 2:
-        while number % i == 0:
-            maxPrime = i
-            number = number / i
-    
-    if number > 2:
-        maxPrime = number
+import math
 
-    return maxPrime
+def largest_prime_factor(n):
+    largest_prime = 0
 
+    # Remove all factors of 2
+    while n % 2 == 0:
+        largest_prime = 2
+        n = n // 2
+
+    # Check for odd prime factors
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            largest_prime = i
+            n = n // i
+
+    # If the remaining number is a prime number greater than 2
+    if n > 2:
+        largest_prime = n
+
+    return largest_prime
+
+# Example usage
 number = 13195
-print("The largest prime factor of", number, "is", largestPrimeFactor(number))
+print(largest_prime_factor(number))
