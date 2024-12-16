4fc2fefac4d32ed60281f9cc6543273a7136a71f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:15:29 +0800

    12-16-2024 21:15 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,14 @@
-import math
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
-def largest_prime_factor(n):
-    largest_prime = 0
-
-    # Remove all factors of 2
-    while n % 2 == 0:
-        largest_prime = 2
-        n = n // 2
-
-    # Check for odd prime factors
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            largest_prime = i
-            n = n // i
-
-    # If the remaining number is a prime number greater than 2
-    if n > 2:
-        largest_prime = n
-
-    return largest_prime
-
-# Example usage
-number = 13195
-print(largest_prime_factor(number))
+# Test the function
+number = 11
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
