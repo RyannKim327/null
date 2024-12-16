59e41256482319af4a0508ea53f5897c30f74ad2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:21:56 +0800

    12-16-2024 21:21 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,57 +1,16 @@
-// Define the ListNode class
-class ListNode {
-    int val;
-    ListNode next;
-    
-    ListNode(int x) { 
-        val = x; 
-    }
-}
+import requests
 
-// Function to check if a linked list is a palindrome
-public boolean isPalindrome(ListNode head) {
-    if(head == null || head.next == null) {
-        return true;
-    }
-    
-    // Find the middle of the linked list
-    ListNode slow = head;
-    ListNode fast = head;
-    
-    while(fast.next != null && fast.next.next != null) {
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-    
-    // Reverse the second half of the linked list
-    ListNode secondHalf = reverseList(slow.next);
-    
-    // Compare the first and second halves
-    ListNode p1 = head;
-    ListNode p2 = secondHalf;
-    
-    while(p2 != null) {
-        if(p1.val != p2.val) {
-            return false;
-        }
-        p1 = p1.next;
-        p2 = p2.next;
-    }
-    
-    return true;
-}
+api_key = 'your_api_key_here'
+city = 'New York'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
-// Function to reverse a linked list
-public ListNode reverseList(ListNode head) {
-    ListNode prev = null;
-    ListNode curr = head;
-    
-    while(curr != null) {
-        ListNode next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
-    }
-    
-    return prev;
-}
+response = requests.get(url)
+data = response.json()
+
+if data['cod'] == 200:
+    temperature = data['main']['temp']
+    description = data['weather'][0]['description']
+    print(f'Temperature in {city}: {temperature}K')
+    print(f'Weather description: {description}')
+else:
+    print('Error fetching data')
