a8575d18d287c73f40465e4bdd251b2fe7808a19
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:08:18 +0800

    12-16-2024 21:8 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,6 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.left = None
-        self.right = None
-
-class BinaryTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, key):
-        if self.root is None:
-            self.root = Node(key)
-        else:
-            self._insert_recursive(self.root, key)
-
-    def _insert_recursive(self, current_node, key):
-        if key < current_node.key:
-            if current_node.left is None:
-                current_node.left = Node(key)
-            else:
-                self._insert_recursive(current_node.left, key)
-        else:
-            if current_node.right is None:
-                current_node.right = Node(key)
-            else:
-                self._insert_recursive(current_node.right, key)
-
-    def inorder_traversal(self, node):
-        if node is not None:
-            self.inorder_traversal(node.left)
-            print(node.key, end=' ')
-            self.inorder_traversal(node.right)
-
-# Example usage
-bt = BinaryTree()
-bt.insert(5)
-bt.insert(3)
-bt.insert(7)
-bt.insert(2)
-bt.insert(4)
-
-print("Inorder traversal of binary tree:")
-bt.inorder_traversal(bt.root)
+str = "Hello, World!"
+reversed_str = str[::-1]
+print(reversed_str)
+str = "Hello, World!"
+reversed_str = ''.join(reversed(str))
+print(reversed_str)
