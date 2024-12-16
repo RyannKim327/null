edf31204dc4ea54942938a0eccd2ddd988647730
RyannKim327 <rksesgundo123@gmail.com>
2024-12-16 21:18:43 +0800

    12-16-2024 21:18 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,92 @@
-def find_string_length(input_string):
-    length = 0
-    for char in input_string:
-        length += 1
-    return length
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
 
-# Example usage
-input_str = "Hello, World!"
-print("Length of the string:", find_string_length(input_str))
+class AVLTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, root, key):
+        if not root:
+            return Node(key)
+        elif key < root.key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        balance = self.getBalance(root)
+
+        if balance > 1 and key < root.left.key:
+            return self.rightRotate(root)
+        
+        if balance < -1 and key > root.right.key:
+            return self.leftRotate(root)
+        
+        if balance > 1 and key > root.left.key:
+            root.left = self.leftRotate(root.left)
+            return self.rightRotate(root)
+        
+        if balance < -1 and key < root.right.key:
+            root.right = self.rightRotate(root.right)
+            return self.leftRotate(root)
+
+        return root
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+        return root.height
+
+    def getBalance(self, root):
+        if not root:
+            return 0
+        return self.getHeight(root.left) - self.getHeight(root.right)
+
+    def leftRotate(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        return y
+
+    def rightRotate(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        return y
+
+    def insertKey(self, key):
+        self.root = self.insert(self.root, key)
+
+    def preOrder(self, root):
+        if not root:
+            return
+        print(root.key)
+        self.preOrder(root.left)
+        self.preOrder(root.right)
+
+avl_tree = AVLTree()
+avl_tree.insertKey(10)
+avl_tree.insertKey(20)
+avl_tree.insertKey(30)
+avl_tree.insertKey(40)
+avl_tree.insertKey(50)
+avl_tree.insertKey(25)
+
+avl_tree.preOrder(avl_tree.root)
