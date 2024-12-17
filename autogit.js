e5b50c8007191bb053ad87853c70cd0ea4a82429
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 00:15:06 +0800

    12-18-2024 0:15 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,6 @@
-def find_first_repeated_char(input_string):
-    seen = {}
-    for char in input_string:
-        if char in seen:
-            return char
-        seen[char] = True
-    return None
-
-input_string = "abcdefgha"
-result = find_first_repeated_char(input_string)
-if result:
-    print("First repeated character is:", result)
-else:
-    print("No repeated characters found in the string.")
+numbers = [5, 2, 10, 7, 3]
+numbers.sort()
+print(numbers)
+numbers = [5, 2, 10, 7, 3]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
