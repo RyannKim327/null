18d8ce21aae454b5495d9437dfc250cbd7534b41
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 00:04:28 +0800

    12-18-2024 0:4 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,42 @@
-import schedule
-import time
+def fibonacci_search(arr, x):
+    fib_M = 0
+    fib_N = 1
+    fib_K = fib_M + fib_N
+    
+    while fib_K < len(arr):
+        fib_M = fib_N
+        fib_N = fib_K
+        fib_K = fib_M + fib_N
+    
+    offset = -1
+    
+    while fib_K > 1:
+        i = min(offset+fib_M, len(arr)-1)
+        
+        if arr[i] < x:
+            fib_K = fib_N
+            fib_N = fib_M
+            fib_M = fib_K - fib_N
+            offset = i
+        
+        elif arr[i] > x:
+            fib_K = fib_M
+            fib_N = fib_N - fib_M
+            fib_M = fib_K - fib_N
+        
+        else:
+            return i
+    
+    if fib_N and arr[offset+1] == x:
+        return offset+1
+    
+    return -1
 
-def job():
-    print("Task executed")
-
-# Schedules a job that runs every hour
-schedule.every().hour.do(job)
-
-# Keeps the script running so that scheduled tasks can be executed
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Test the Fibonacci search algorithm
+arr = [2, 3, 4, 10, 40]
+x = 10
+index = fibonacci_search(arr, x)
+if index != -1:
+    print("Element found at index", index)
+else:
+    print("Element not found")
