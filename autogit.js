36f756bacb5e7957d8345b0e3def5c189fabb7ab
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 00:05:11 +0800

    12-18-2024 0:5 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,42 +1,13 @@
-def fibonacci_search(arr, x):
-    fib_M = 0
-    fib_N = 1
-    fib_K = fib_M + fib_N
-    
-    while fib_K < len(arr):
-        fib_M = fib_N
-        fib_N = fib_K
-        fib_K = fib_M + fib_N
-    
-    offset = -1
-    
-    while fib_K > 1:
-        i = min(offset+fib_M, len(arr)-1)
-        
-        if arr[i] < x:
-            fib_K = fib_N
-            fib_N = fib_M
-            fib_M = fib_K - fib_N
-            offset = i
-        
-        elif arr[i] > x:
-            fib_K = fib_M
-            fib_N = fib_N - fib_M
-            fib_M = fib_K - fib_N
-        
-        else:
-            return i
-    
-    if fib_N and arr[offset+1] == x:
-        return offset+1
-    
-    return -1
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
-# Test the Fibonacci search algorithm
-arr = [2, 3, 4, 10, 40]
-x = 10
-index = fibonacci_search(arr, x)
-if index != -1:
-    print("Element found at index", index)
-else:
-    print("Element not found")
+# Example usage:
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+print("Sorted array:", arr)
