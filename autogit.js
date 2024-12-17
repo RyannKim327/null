44de78e4509496eaece44e8ee01d19a8f1d8f26a
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 00:08:50 +0800

    12-18-2024 0:8 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,56 @@
-def kth_smallest_element(arr, k):
-    arr.sort()
-    return arr[k-1]
+from collections import defaultdict
 
-# Example usage
-arr = [7, 10, 4, 3, 20, 15]
-k = 3
-result = kth_smallest_element(arr, k)
-print(f"The {k}th smallest element is: {result}")
+class TarjanSCC:
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes
+        self.graph = defaultdict(list)
+        self.index = 0
+        self.lowlink = [None] * num_nodes
+        self.visited = [False] * num_nodes
+        self.stack = []
+        self.sccs = []
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def strong_connect(self, v):
+        self.lowlink[v] = self.index
+        self.visited[v] = True
+        self.index += 1
+        self.stack.append(v)
+
+        for w in self.graph[v]:
+            if self.lowlink[w] is None:
+                self.strong_connect(w)
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+            elif self.visited[w]:
+                self.lowlink[v] = min(self.lowlink[v], self.lowlink[w])
+
+        if self.lowlink[v] == v:
+            scc = []
+            while True:
+                w = self.stack.pop()
+                scc.append(w)
+                self.visited[w] = False
+                if w == v:
+                    break
+            self.sccs.append(scc)
+
+    def find_sccs(self):
+        for v in range(self.num_nodes):
+            if self.lowlink[v] is None:
+                self.strong_connect(v)
+
+        return self.sccs
+
+# Example usage:
+g = TarjanSCC(5)
+g.add_edge(0, 1)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(1, 3)
+g.add_edge(3, 4)
+g.add_edge(4, 3)
+
+sccs = g.find_sccs()
+print(sccs)
