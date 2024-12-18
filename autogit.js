6afdd6609b047862cd62e77ecfa5fd029ad60224
RyannKim327 <rksesgundo123@gmail.com>
2024-12-19 07:52:57 +0800

    12-19-2024 7:52 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,5 @@
-BFS(graph, start_node):
-    visited = {}  // A set to keep track of visited nodes
-    queue = Queue()  // Initialize a queue data structure
-    queue.enqueue(start_node)  // Enqueue the start node
-    
-    while queue is not empty:
-        current_node = queue.dequeue()
-        
-        if current_node is not in visited:
-            visited.add(current_node)
-            
-            // Process the node (e.g., print it)
-            print(current_node)
-            
-            for each neighbor in graph.neighbors(current_node):
-                if neighbor is not in visited:
-                    queue.enqueue(neighbor)
+# Taking input from the user
+user_input = input("Enter something: ")
+
+# Printing the user input
+print("You entered:", user_input)
