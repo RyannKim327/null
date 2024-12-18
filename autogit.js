9f5e360b55dea3109d31846467af55368c18c72c
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 19:44:09 +0800

    12-18-2024 19:44 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,32 @@
-def max_subarray_sum(arr):
-    max_ending_here = max_so_far = arr[0]
+def bellman_ford(graph, source):
+    distance = {node: float('inf') for node in graph}
+    predecessor = {node: None for node in graph}
+    distance[source] = 0
+    
+    for _ in range(len(graph) - 1):
+        for node in graph:
+            for neighbor in graph[node]:
+                if distance[node] + graph[node][neighbor] < distance[neighbor]:
+                    distance[neighbor] = distance[node] + graph[node][neighbor]
+                    predecessor[neighbor] = node
+                    
+    for node in graph:
+        for neighbor in graph[node]:
+            if distance[node] + graph[node][neighbor] < distance[neighbor]:
+                print("Graph contains negative cycle")
+                return
+    
+    return distance, predecessor
 
-    for x in arr[1:]:
-        max_ending_here = max(x, max_ending_here + x)
-        max_so_far = max(max_so_far, max_ending_here)
+graph = {
+    'A': {'B': -1, 'C': 4},
+    'B': {'C': 3, 'D': 2, 'E': 2},
+    'C': {},
+    'D': {'B': 1, 'C': 5},
+    'E': {'D': -3}
+}
 
-    return max_so_far
-
-# Example usage
-arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-print(max_subarray_sum(arr))
+source_node = 'A'
+shortest_distances, predecessors = bellman_ford(graph, source_node)
+print("Shortest distances:", shortest_distances)
+print("Predecessors:", predecessors)
