94c50f9a0f7b42ea59cb2f656a175e19f845a888
RyannKim327 <rksesgundo123@gmail.com>
2024-12-19 07:51:03 +0800

    12-19-2024 7:50 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,1 +1,19 @@
-^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$
+def find_majority_element(arr):
+    count = 0
+    candidate = None
+    
+    for num in arr:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif num == candidate:
+            count += 1
+        else:
+            count -= 1
+    
+    return candidate
+
+# Example usage
+arr = [2, 2, 3, 2, 4, 2, 2]
+majority_element = find_majority_element(arr)
+print("Majority element is:", majority_element)
