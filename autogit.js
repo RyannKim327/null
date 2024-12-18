23a66736138a7ce86923de78ebc1187ad9696e99
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 19:45:05 +0800

    12-18-2024 19:45 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,5 @@
-def longest_increasing_subsequence(arr):
-    n = len(arr)
-    lis = [1] * n
-
-    for i in range(1, n):
-        for j in range(i):
-            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
-                lis[i] = lis[j] + 1
-
-    max_length = 0
-    for length in lis:
-        max_length = max(max_length, length)
-
-    return max_length
-
-# Example usage
-arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-print("Length of longest increasing subsequence:", longest_increasing_subsequence(arr))
+string1 = "Hello, "
+string2 = "world!"
+result = string1 + string2
+print(result)
+Hello, world!
