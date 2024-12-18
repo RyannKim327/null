ce8607da9aa5e9387d9cb8e65c2be612fe57925c
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 19:44:13 +0800

    12-18-2024 19:44 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,10 @@
-def bellman_ford(graph, source):
-    distance = {node: float('inf') for node in graph}
-    predecessor = {node: None for node in graph}
-    distance[source] = 0
-    
-    for _ in range(len(graph) - 1):
-        for node in graph:
-            for neighbor in graph[node]:
-                if distance[node] + graph[node][neighbor] < distance[neighbor]:
-                    distance[neighbor] = distance[node] + graph[node][neighbor]
-                    predecessor[neighbor] = node
-                    
-    for node in graph:
-        for neighbor in graph[node]:
-            if distance[node] + graph[node][neighbor] < distance[neighbor]:
-                print("Graph contains negative cycle")
-                return
-    
-    return distance, predecessor
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-graph = {
-    'A': {'B': -1, 'C': 4},
-    'B': {'C': 3, 'D': 2, 'E': 2},
-    'C': {},
-    'D': {'B': 1, 'C': 5},
-    'E': {'D': -3}
-}
-
-source_node = 'A'
-shortest_distances, predecessors = bellman_ford(graph, source_node)
-print("Shortest distances:", shortest_distances)
-print("Predecessors:", predecessors)
+# Test the function with a number
+number = 5
+result = factorial(number)
+print("Factorial of", number, "is", result)
