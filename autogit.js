74a565f97766f3d6dde6fad14652220c9738a59e
RyannKim327 <rksesgundo123@gmail.com>
2024-12-18 19:45:17 +0800

    12-18-2024 19:45 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,21 @@
-const axios = require('axios');
+def counting_sort(arr):
+    max_val = max(arr)
+    count_arr = [0] * (max_val + 1)
+    sorted_arr = [0] * len(arr)
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    for num in arr:
+        count_arr[num] += 1
+
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i-1]
+
+    for num in arr:
+        sorted_arr[count_arr[num] - 1] = num
+        count_arr[num] -= 1
+
+    return sorted_arr
+
+# Example usage
+arr = [4, 2, 2, 8, 3, 3, 1]
+sorted_arr = counting_sort(arr)
+print(sorted_arr)
