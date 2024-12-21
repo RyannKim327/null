5f852aa3ef2af5c542af28687cc730b3b7859792
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:42:53 +0800

    12-21-2024 18:42 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,48 +1,19 @@
-def tarjan(graph):
-    index = {}
-    lowlink = {}
-    onStack = {}
-    stack = []
-    result = []
-    index_counter = [0]
+Algorithm:
+1. Initialize two pointers, slow_ptr and fast_ptr, to the start of the linked list.
+2. Move slow_ptr by one step and fast_ptr by two steps in each iteration.
+3. If there is a cycle in the linked list, the two pointers will meet at some point.
+4. If they meet, it means that the linked list contains a cycle; otherwise, if fast_ptr reaches the end of the list, there is no cycle.
 
-    def strongconnect(v):
-        index[v] = index_counter[0]
-        lowlink[v] = index_counter[0]
-        index_counter[0] += 1
-        stack.append(v)
-        onStack[v] = True
+Pseudocode:
+function has_cycle(head):
+    slow_ptr = head
+    fast_ptr = head
 
-        for w in graph.get(v, []):
-            if w not in index:
-                strongconnect(w)
-                lowlink[v] = min(lowlink[v], lowlink[w])
-            elif w in onStack:
-                lowlink[v] = min(lowlink[v], index[w])
+    while fast_ptr is not null and fast_ptr.next is not null:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next.next
 
-        if lowlink[v] == index[v]:
-            scc = []
-            while True:
-                w = stack.pop()
-                onStack.pop(w)
-                scc.append(w)
-                if w == v:
-                    break
-            result.append(scc)
+        if slow_ptr is equal to fast_ptr:
+            return true
 
-    for v in graph.keys():
-        if v not in index:
-            strongconnect(v)
-
-    return result
-
-# Example usage:
-graph = {
-    0: [1],
-    1: [2],
-    2: [0, 3],
-    3: [4],
-    4: [3]
-}
-
-print(tarjan(graph))
+    return false
