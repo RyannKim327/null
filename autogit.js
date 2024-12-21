0a034379eae0bd66db58ceef1d68d01de4dd0628
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:39:11 +0800

    12-21-2024 18:39 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,22 @@
-# Python code with user input
-name = input("Enter your name: ")
-print("Hello, " + name)
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def count_leaf_nodes(node):
+    if node is None:
+        return 0
+    if node.left is None and node.right is None:
+        return 1
+    else:
+        return count_leaf_nodes(node.left) + count_leaf_nodes(node.right)
+
+# Driver code
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Number of leaf nodes in the binary tree:", count_leaf_nodes(root))
