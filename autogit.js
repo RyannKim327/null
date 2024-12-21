3443cf530b2009e2a529e66163320a6334004c32
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:44:53 +0800

    12-21-2024 18:44 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,5 @@
-def is_palindrome(s):
-    s = s.lower().replace(" ", "")  # Convert the string to lowercase and remove spaces
-    return s == s[::-1]
-
-# Test the function
-string = "A man a plan a canal Panama"
-if is_palindrome(string):
-    print("Palindrome")
-else:
-    print("Not a palindrome")
+# Remove all spaces from a string in Python
+original_string = "Hello, World! This is a test."
+no_spaces_string = original_string.replace(" ", "")
+print(no_spaces_string)
+Hello,World!Thisisatest.
