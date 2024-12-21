0f70c2ef828a3aa3d5d92d50e72fa2fb4685ce14
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:43:54 +0800

    12-21-2024 18:43 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,37 @@
-var string = "Hello, world!";
-var substring = "world";
-if (string.indexOf(substring) !== -1) {
-    console.log("String contains the substring");
-} else {
-    console.log("String does not contain the substring");
+def depth_limited_search(graph, start, goal, depth_limit):
+    stack = [(start, [start])]
+    
+    while stack:
+        (node, path) = stack.pop()
+        
+        if node == goal:
+            return path
+        
+        if len(path) < depth_limit:
+            for neighbor in graph[node]:
+                stack.append((neighbor, path + [neighbor]))
+    
+    return None
+
+# Example usage
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['G', 'H'],
+    'F': ['I'],
+    'G': [],
+    'H': [],
+    'I': []
 }
+
+start_node = 'A'
+goal_node = 'I'
+depth_limit = 3
+
+result = depth_limited_search(graph, start_node, goal_node, depth_limit)
+if result:
+    print("Path found:", result)
+else:
+    print("Path not found within depth limit.")
