506ed1638200d371a2b94368b6602e19f662dbf3
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:43:57 +0800

    12-21-2024 18:43 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,14 @@
-def depth_limited_search(graph, start, goal, depth_limit):
-    stack = [(start, [start])]
-    
-    while stack:
-        (node, path) = stack.pop()
-        
-        if node == goal:
-            return path
-        
-        if len(path) < depth_limit:
-            for neighbor in graph[node]:
-                stack.append((neighbor, path + [neighbor]))
-    
-    return None
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
 
 # Example usage
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['G', 'H'],
-    'F': ['I'],
-    'G': [],
-    'H': [],
-    'I': []
-}
-
-start_node = 'A'
-goal_node = 'I'
-depth_limit = 3
-
-result = depth_limited_search(graph, start_node, goal_node, depth_limit)
-if result:
-    print("Path found:", result)
-else:
-    print("Path not found within depth limit.")
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array is:", sorted_arr)
