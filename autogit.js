481d5aac814a22d0d0d5677e7844c4f8be253804
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:48:13 +0800

    12-21-2024 18:48 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,9 @@
-# Function to count occurrences of a word in a string
-def count_word_occurrences(input_string, target_word):
-    words = input_string.split()
+def find_string_length(string):
     count = 0
-    for word in words:
-        if word == target_word:
-            count += 1
+    for char in string:
+        count += 1
     return count
 
 # Test the function
-input_string = "apple banana apple orange apple"
-target_word = "apple"
-occurrences = count_word_occurrences(input_string, target_word)
-print("Number of occurrences of '" + target_word + "':", occurrences)
+string = "Hello, World!"
+print(find_string_length(string))  # Output: 13
