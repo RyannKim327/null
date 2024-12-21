406b8a599d98267821b811d5cc35f1f478775926
RyannKim327 <rksesgundo123@gmail.com>
2024-12-21 18:43:49 +0800

    12-21-2024 18:43 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,7 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-num = 13  # change this to the number you want to check
-if is_prime(num):
-    print(f"{num} is a prime number")
-else:
-    print(f"{num} is not a prime number")
+var string = "Hello, world!";
+var substring = "world";
+if (string.indexOf(substring) !== -1) {
+    console.log("String contains the substring");
+} else {
+    console.log("String does not contain the substring");
+}
