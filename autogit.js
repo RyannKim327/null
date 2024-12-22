0ce5728eea2b8cbb5ad8e2d26e65b722b010cf77
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:12:58 +0800

    12-22-2024 16:12 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,14 @@
-def countingSort(arr):
-    max_val = max(arr)
-    min_val = min(arr)
-    range_val = max_val - min_val + 1
+import math
 
-    count = [0] * range_val
-    output = [0] * len(arr)
+n = 5
+factorial = math.factorial(n)
+print(f'The factorial of {n} is {factorial}')
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-    for num in arr:
-        count[num - min_val] += 1
-
-    for i in range(1, len(count)):
-        count[i] += count[i - 1]
-
-    for i in range(len(arr) - 1, -1, -1):
-        output[count[arr[i] - min_val] - 1] = arr[i]
-        count[arr[i] - min_val] -= 1
-
-    for i in range(len(arr)):
-        arr[i] = output[i]
-
-arr = [4, 2, 2, 8, 3, 3, 1]
-countingSort(arr)
-print("Sorted array is:", arr)
+n = 5
+result = factorial(n)
+print(f'The factorial of {n} is {result}')
