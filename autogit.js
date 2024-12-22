52067481a1148fe404e933f16724b4ba9d70fcf2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:14:18 +0800

    12-22-2024 16:14 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,13 @@
-arr = [3, 7, 2, 8, 10]
-max_value = max(arr)
-print("The maximum value in the array is:", max_value)
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+# Example usage:
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+print("Sorted array is:", arr)
