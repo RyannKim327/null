c9edf5a837f9d50f2caa9fc8e57fdfe36d8c9da2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:13:18 +0800

    12-22-2024 16:13 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,23 @@
-class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+def find_majority_element(nums):
+    candidate = None
+    count = 0
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def push(self, new_data):
-        new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
-
-    def get_length(self):
-        current = self.head
-        count = 0
-
-        while current:
+    for num in nums:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif num == candidate:
             count += 1
-            current = current.next
+        else:
+            count -= 1
 
-        return count
+    count_candidate = nums.count(candidate)
 
-# Example usage
-linked_list = LinkedList()
-linked_list.push(3)
-linked_list.push(7)
-linked_list.push(12)
-linked_list.push(16)
+    if count_candidate > len(nums) // 2:
+        return candidate
+    else:
+        return None
 
-print("Length of the linked list is:", linked_list.get_length())
+# Test the function
+nums = [3, 3, 4, 2, 4, 4, 2, 4, 4]
+print(find_majority_element(nums))  # Output: 4
