c7919ef07e1c680769684a2d2d3699605b44ec9f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:14:11 +0800

    12-22-2024 16:14 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,17 @@
-function buildPrefixArray(pattern):
-    n = length(pattern)
-    prefixArray = createArray of size n
-    
-    len = 0
-    i = 1
-    prefixArray[0] = 0
-    
-    while i < n:
-        if pattern[i] == pattern[len]:
-            len = len + 1
-            prefixArray[i] = len
-            i = i + 1
-        else:
-            if len != 0:
-                len = prefixArray[len - 1]
-            else:
-                prefixArray[i] = 0
-                i = i + 1
-    
-    return prefixArray
+import android.os.AsyncTask;
 
-function KMPSearch(text, pattern):
-    n = length(text)
-    m = length(pattern)
-    prefixArray = buildPrefixArray(pattern)
-    
-    i = 0
-    j = 0
+public class MyAsyncTask extends AsyncTask<Void, Void, String> {
 
-    while i < n:
-        if pattern[j] == text[i]:
-            i = i + 1
-            j = j + 1
+    @Override
+    protected String doInBackground(Void... voids) {
+        // Perform background task here
+        return "Task completed!";
+    }
 
-        if j == m:
-            print("Pattern found at index " + (i - j))
-            j = prefixArray[j - 1]
-
-        else if i < n and pattern[j] != text[i]:
-            if j != 0:
-                j = prefixArray[j - 1]
-            else:
-                i = i + 1
+    @Override
+    protected void onPostExecute(String result) {
+        // Update UI with the result
+    }
+}
+MyAsyncTask myTask = new MyAsyncTask();
+myTask.execute();
