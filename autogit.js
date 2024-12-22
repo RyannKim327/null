4956ebb3f0934384eb9eb000436a84d94e729439
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:13:13 +0800

    12-22-2024 16:13 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,37 +1,32 @@
-def boyer_moore_horspool(text, pattern):
-    text_length = len(text)
-    pattern_length = len(pattern)
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
-    if pattern_length == 0:
-        return 0
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-    bad_character = {}
-    for i in range(pattern_length - 1):
-        bad_character[ord(pattern[i])] = pattern_length - i - 1
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
 
-    index = 0
-    while index <= text_length - pattern_length:
-        skip = 0
-        for j in range(pattern_length - 1, -1, -1):
-            if text[index + j] != pattern[j]:
-                if ord(text[index + j]) in bad_character:
-                    skip = bad_character[ord(text[index + j])]
-                else:
-                    skip = pattern_length
-                break
+    def get_length(self):
+        current = self.head
+        count = 0
 
-        if skip == 0:
-            return index
+        while current:
+            count += 1
+            current = current.next
 
-        index += skip
+        return count
 
-    return -1
+# Example usage
+linked_list = LinkedList()
+linked_list.push(3)
+linked_list.push(7)
+linked_list.push(12)
+linked_list.push(16)
 
-text = "example text for testing"
-pattern = "text"
-result = boyer_moore_horspool(text, pattern)
-
-if result != -1:
-    print("Pattern found at index:", result)
-else:
-    print("Pattern not found")
+print("Length of the linked list is:", linked_list.get_length())
