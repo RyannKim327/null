ec6d70f47fa09491655defa079d92857367265d5
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:13:03 +0800

    12-22-2024 16:12 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,14 +1,22 @@
-import math
+str1 = "Hello"
+str2 = "World"
+concatenated_string = str1 + " " + str2
+print(concatenated_string)
+String str1 = "Hello";
+String str2 = "World";
+String concatenatedString = str1 + " " + str2;
+System.out.println(concatenatedString);
+let str1 = "Hello";
+let str2 = "World";
+let concatenatedString = str1 + " " + str2;
+console.log(concatenatedString);
+#include <iostream>
+#include <string>
 
-n = 5
-factorial = math.factorial(n)
-print(f'The factorial of {n} is {factorial}')
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-n = 5
-result = factorial(n)
-print(f'The factorial of {n} is {result}')
+int main() {
+    std::string str1 = "Hello";
+    std::string str2 = "World";
+    std::string concatenatedString = str1 + " " + str2;
+    std::cout << concatenatedString << std::endl;
+    return 0;
+}
