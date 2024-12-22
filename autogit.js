1498af1f50d91d33c608747210f518313550c1e6
RyannKim327 <rksesgundo123@gmail.com>
2024-12-22 16:14:07 +0800

    12-22-2024 16:14 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,44 @@
-import subprocess
+function buildPrefixArray(pattern):
+    n = length(pattern)
+    prefixArray = createArray of size n
+    
+    len = 0
+    i = 1
+    prefixArray[0] = 0
+    
+    while i < n:
+        if pattern[i] == pattern[len]:
+            len = len + 1
+            prefixArray[i] = len
+            i = i + 1
+        else:
+            if len != 0:
+                len = prefixArray[len - 1]
+            else:
+                prefixArray[i] = 0
+                i = i + 1
+    
+    return prefixArray
 
-# Execute a shell command using cron
-command = 'echo "Hello, world!"'  # Example command
-cron_expression = '* * * * *'  # Run every minute
+function KMPSearch(text, pattern):
+    n = length(text)
+    m = length(pattern)
+    prefixArray = buildPrefixArray(pattern)
+    
+    i = 0
+    j = 0
 
-# Create a cron job to run the command
-cron_job = f'echo "{cron_expression} {command}" | crontab -'
-subprocess.call(cron_job, shell=True)
+    while i < n:
+        if pattern[j] == text[i]:
+            i = i + 1
+            j = j + 1
+
+        if j == m:
+            print("Pattern found at index " + (i - j))
+            j = prefixArray[j - 1]
+
+        else if i < n and pattern[j] != text[i]:
+            if j != 0:
+                j = prefixArray[j - 1]
+            else:
+                i = i + 1
