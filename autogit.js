d4ee0ed7804c49ecdbd5d629449adca3ff4639e7
RyannKim327 <rksesgundo123@gmail.com>
2024-12-25 22:03:34 +0800

    12-25-2024 22:3 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,15 @@
-import random
-
-start = 1
-end = 100
-
-random_number = random.randint(start, end)
-print("Random number between", start, "and", end, "is:", random_number)
+def beam_search(initial_state, beam_width, max_depth):
+    beam = [initial_state]
+    
+    for _ in range(max_depth):
+        new_beam = []
+        for state in beam:
+            actions = generate_actions(state)
+            for action in actions:
+                new_state = apply_action(state, action)
+                new_beam.append(new_state)
+        
+        new_beam = sorted(new_beam, key=lambda x: heuristic_function(x))[:beam_width]
+        beam = new_beam
+        
+    return beam
