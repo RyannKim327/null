6c61afb89060d94fa5b9c804c66be2667853468b
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:41:25 +0800

    12-26-2024 15:41 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,7 +1,3 @@
-my_string = "   Hello,   World!   "
-new_string = my_string.replace(" ", "")
-print(new_string)  # Output: "Hello,World!"
-import re
-my_string = "   Hello,   World!   "
-new_string = re.sub(r'\s+', '', my_string)
-print(new_string)  # Output: "Hello,World!"
+# Python code with input
+user_input = input("Enter something: ")
+print("You entered: " + user_input)
