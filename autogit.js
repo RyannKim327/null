b5490975208874ec611d422240ea69c9c3c79680
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:44:09 +0800

    12-26-2024 15:44 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,22 @@
-str1 = "Hello, "
-str2 = "world!"
-concatenated_string = str1 + str2
-print(concatenated_string)
-Hello, world!
+import math
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    
+    max_divisor = math.isqrt(number) + 1
+    for i in range(3, max_divisor, 2):
+        if number % i == 0:
+            return False
+    return True
+
+# Test the function
+number = 17
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
