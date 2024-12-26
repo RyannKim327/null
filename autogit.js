caa9a24788d922bc57099cf205f2d8f6b9e43e09
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:50:48 +0800

    12-26-2024 15:50 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,10 @@
-import random
-
-# Generate a random number between 1 and 100
-random_number = random.randint(1, 100)
-
-print(random_number)
+# Removing whitespace from the beginning and end of a string
+my_string = "  Hello, World!  "
+trimmed_string = my_string.strip()
+print(trimmed_string)
+Hello, World!
+# Removing all whitespace from a string
+my_string = "  Hello,     World!  "
+clean_string = my_string.replace(" ", "")
+print(clean_string)
+Hello,World!
