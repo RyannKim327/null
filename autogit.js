d4343f84d79ad4e4c6b084bb0e48a47ae42cfe66
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:50:40 +0800

    12-26-2024 15:50 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,21 +1,6 @@
-def longest_increasing_subsequence(arr):
-    n = len(arr)
-    lis = [1]*n
-    
-    for i in range(1, n):
-        for j in range(0, i):
-            if arr[i] > arr[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
-    
-    max_length = max(lis)
-    result = []
-    for i in range(n - 1, -1, -1):
-        if lis[i] == max_length:
-            result.insert(0, arr[i])
-            max_length -= 1
-
-    return result
-
-# Example usage
-arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-print(longest_increasing_subsequence(arr))
+arr = [1, 2, 3, 4, 5]
+arr.reverse()
+print(arr)
+arr = [1, 2, 3, 4, 5]
+reversed_arr = arr[::-1]
+print(reversed_arr)
