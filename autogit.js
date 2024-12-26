cb08b855db36b70983f9591aa9bb256d22ee1212
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:41:08 +0800

    12-26-2024 15:40 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,50 @@
-def find_intersection(list1, list2):
-    set1 = set()
-    set2 = set()
-    
-    # Store elements of list1 in set1
-    current = list1.head
-    while current:
-        set1.add(current.data)
-        current = current.next
-    
-    # Store elements of list2 in set2
-    current = list2.head
-    while current:
-        set2.add(current.data)
-        current = current.next
-    
-    intersection_list = LinkedList()
-    
-    # Traverse list1 and list2 to find intersection elements
-    current = list1.head
-    while current:
-        if current.data in set2:
-            intersection_list.append(current.data)
-        current = current.next
-    
-    return intersection_list
+import heapq
+
+def astar_search(start, goal, graph, heuristic):
+    open_list = []
+    closed_list = set()
+    heapq.heappush(open_list, (0, start))
+
+    while open_list:
+        _, current = heapq.heappop(open_list)
+
+        if current == goal:
+            path = []
+            while current is not None:
+                path.append(current)
+                current = graph[current][1]
+            return path[::-1]
+
+        closed_list.add(current)
+
+        for neighbor in graph[current][0]:
+            if neighbor in closed_list:
+                continue
+
+            tentative_g = graph[current][2] + graph[current][0][neighbor]
+            if (tentative_g, neighbor) not in open_list:
+                heapq.heappush(open_list, (tentative_g + heuristic(neighbor, goal), neighbor))
+                graph[neighbor][2] = tentative_g
+                graph[neighbor][1] = current
+
+    return None
+
+# Example usage
+graph = {
+    'A': ({'B': 1, 'C': 4}, None, float('inf')),
+    'B': ({'A': 1, 'D': 2}, None, float('inf')),
+    'C': ({'A': 4, 'D': 5}, None, float('inf')),
+    'D': ({'B': 2, 'C': 5}, None, float('inf'))
+}
+
+heuristic = {
+    'A': 7,
+    'B': 6,
+    'C': 2,
+    'D': 1
+}
+
+start = 'A'
+goal = 'D'
+path = astar_search(start, goal, graph, lambda node, goal: heuristic[node])
+print(path)
