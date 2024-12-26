3d9674ea410600a8fd397bd6db631341de947a99
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:49:25 +0800

    12-26-2024 15:49 [Ang cute nya, lalo na pag inis sya saken]

---
+++ autogit.js
@@ -0,0 +1,14 @@
+my_string = "Hello, World!"
+substring = "Hello"
+
+if substring in my_string:
+    print("Substring found in the string.")
+else:
+    print("Substring not found in the string.")
+my_string = "Hello, World!"
+substring = "Hello"
+
+if my_string.find(substring) != -1:
+    print("Substring found in the string.")
+else:
+    print("Substring not found in the string.")
