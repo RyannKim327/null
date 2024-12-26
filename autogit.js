42770316a7c66aa87ad8355227d60b98db2a5380
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:49:50 +0800

    12-26-2024 15:49 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,4 @@
-def depth_limited_search(state, depth_limit):
-    if is_goal_state(state):
-        return state
-    
-    if depth_limit == 0:
-        return None
-    
-    for next_state in generate_next_states(state):
-        result = depth_limited_search(next_state, depth_limit - 1)
-        if result is not None:
-            return result
-    
-    return None
+my_string = "Hello, World!"
+lowercase_string = my_string.lower()
+print(lowercase_string)
+hello, world!
