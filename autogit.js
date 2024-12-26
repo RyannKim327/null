d12d4f03fb5d456fe839babe376f9415d9dd9dac
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:41:45 +0800

    12-26-2024 15:41 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,6 @@
-# Python code with input
-user_input = input("Enter something: ")
-print("You entered: " + user_input)
+array = [5, 2, 9, 1, 5]
+array.sort()
+print(array)
+array = [5, 2, 9, 1, 5]
+sorted_array = sorted(array)
+print(sorted_array)
