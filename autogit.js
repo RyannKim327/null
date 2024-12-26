b9a8c1fdbaee06f45448ff5c1e2547afc97c9ad8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-26 15:43:08 +0800

    12-26-2024 15:43 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,18 @@
-def longest_increasing_subsequence(arr):
-    n = len(arr)
-    lis = [1]*n
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    max_divisor = int(num ** 0.5) + 1
+    for i in range(3, max_divisor, 2):
+        if num % i == 0:
+            return False
+    return True
 
-    for i in range(1, n):
-        for j in range(0, i):
-            if arr[i] > arr[j]:
-                lis[i] = max(lis[i], lis[j] + 1)
-
-    max_length = max(lis)
-    subsequence = []
-    idx = lis.index(max_length)
-    subsequence.append(arr[idx])
-    max_length -= 1
-
-    for i in range(idx-1, -1, -1):
-        if lis[i] == max_length and arr[i] < subsequence[-1]:
-            subsequence.append(arr[i])
-            max_length -= 1
-
-    subsequence.reverse()
-    return subsequence
-
-# Example usage
-arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-result = longest_increasing_subsequence(arr)
-print("Longest increasing subsequence:", result)
+num = 17
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
