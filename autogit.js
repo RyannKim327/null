478881daa24135f8d3c379de109fd1b8c1798252
RyannKim327 <rksesgundo123@gmail.com>
2024-12-28 16:11:11 +0800

    12-28-2024 16:10 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,25 +1,14 @@
-def partition(arr, low, high):
-    i = (low - 1)         
-    pivot = arr[high]    
-  
-    for j in range(low, high):
-        if arr[j] <= pivot:
-            i = i + 1
-            arr[i], arr[j] = arr[j], arr[i]
-  
-    arr[i + 1], arr[high] = arr[high], arr[i + 1]
-    return i + 1
-  
-def quick_sort(arr, low, high):
-    if low < high:
-        pi = partition(arr, low, high)
-  
-        quick_sort(arr, low, pi - 1)
-        quick_sort(arr, pi + 1, high)
-  
-arr = [10, 7, 8, 9, 1, 5]
-n = len(arr)
-quick_sort(arr, 0, n - 1)
-print("Sorted array is:")
-for i in range(n):
-    print("%d" % arr[i])
+def remove_vowels(input_string):
+    vowels = "aeiouAEIOU"
+    result = ""
+    
+    for char in input_string:
+        if char not in vowels:
+            result += char
+    
+    return result
+
+# Test the function
+input_string = "Hello, World!"
+result = remove_vowels(input_string)
+print(result)
