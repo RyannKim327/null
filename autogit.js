9ca344352b31a5a2be321763d4ba27ad40e068ac
RyannKim327 <rksesgundo123@gmail.com>
2024-12-28 16:13:21 +0800

    12-28-2024 16:13 [Sana ako pa rin]

---
+++ autogit.js
@@ -0,0 +1,14 @@
+// Make a GET request using fetch
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
