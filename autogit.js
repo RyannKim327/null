1145e5434d3cb3aab91c3e116c220986e6a16165
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 19:14:00 +0000

    12-30-2024 3:13 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,20 +1,41 @@
-def find_second_largest(arr):
-    if len(arr) < 2:
-        return None
+def fibonacci_search(arr, x):
+    fib_n_2 = 0
+    fib_n_1 = 1
+    fib_n = fib_n_1 + fib_n_2
+    
+    while fib_n < len(arr):
+        fib_n_2 = fib_n_1
+        fib_n_1 = fib_n
+        fib_n = fib_n_1 + fib_n_2
+    
+    offset = -1
+    
+    while fib_n > 1:
+        i = min(offset + fib_n_2, len(arr) - 1)
+        
+        if arr[i] < x:
+            fib_n = fib_n_1
+            fib_n_1 = fib_n_2
+            fib_n_2 = fib_n - fib_n_1
+            offset = i
+        elif arr[i] > x:
+            fib_n = fib_n_2
+            fib_n_1 = fib_n_1 - fib_n_2
+            fib_n_2 = fib_n - fib_n_1
+        else:
+            return i
+    
+    if fib_n_1 and arr[offset + 1] == x:
+        return offset + 1
+    
+    return -1
 
-    max_num = max(arr[0], arr[1])
-    second_max = min(arr[0], arr[1])
+# Test the Fibonacci search algorithm
+arr = [2, 3, 5, 8, 13, 21, 34, 55, 89]
+x = 55
+index = fibonacci_search(arr, x)
 
-    for i in range(2, len(arr)):
-        if arr[i] > max_num:
-            second_max = max_num
-            max_num = arr[i]
-        elif arr[i] > second_max and arr[i] != max_num:
-            second_max = arr[i]
-
-    return second_max
-
-# Example usage
-arr = [5, 2, 8, 9, 1]
-result = find_second_largest(arr)
-print(result)
+if index != -1:
+    print(f"Element found at index: {index}")
+else:
+    print("Element not found")
