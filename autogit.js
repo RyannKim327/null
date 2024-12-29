1a5da96fe70281496d4e9988639f238cb33bf028
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:51:31 +0000

    12-30-2024 1:51 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,17 +1,30 @@
-def selection_sort(arr):
-    n = len(arr)
-    
-    for i in range(n):
-        min_index = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    
-    return arr
+def findMedianSortedArrays(nums1, nums2):
+    m, n = len(nums1), len(nums2)
+    if m > n:
+        nums1, nums2, m, n = nums2, nums1, n, m
+    imin, imax, half_len = 0, m, (m + n + 1) // 2
+    while imin <= imax:
+        i = (imin + imax) // 2
+        j = half_len - i
+        if i < m and nums2[j - 1] > nums1[i]:
+            imin = i + 1
+        elif i > 0 and nums1[i - 1] > nums2[j]:
+            imax = i - 1
+        else:
+            if i == 0: max_of_left = nums2[j - 1]
+            elif j == 0: max_of_left = nums1[i - 1]
+            else: max_of_left = max(nums1[i - 1], nums2[j - 1])
 
-# Example usage
-arr = [64, 25, 12, 22, 11]
-sorted_arr = selection_sort(arr)
-print("Sorted array:", sorted_arr)
+            if (m + n) % 2 == 1:
+                return max_of_left
+
+            if i == m: min_of_right = nums2[j]
+            elif j == n: min_of_right = nums1[i]
+            else: min_of_right = min(nums1[i], nums2[j])
+
+            return (max_of_left + min_of_right) / 2.0
+
+# Test case
+nums1 = [1, 3]
+nums2 = [2]
+print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
