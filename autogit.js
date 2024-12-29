5d7d4c2ca2aa138bf9cefe8b2ead749a2801eb28
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 18:26:06 +0000

    12-30-2024 2:26 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,20 @@
-def calculate_mean(numbers):
-    sum_numbers = sum(numbers)
-    count_numbers = len(numbers)
-    
-    mean = sum_numbers / count_numbers
-    
-    return mean
+def find_second_largest(arr):
+    if len(arr) < 2:
+        return None
 
-# Example list of numbers
-numbers = [1, 2, 3, 4, 5]
-mean = calculate_mean(numbers)
-print("Mean of the list is:", mean)
+    max_num = max(arr[0], arr[1])
+    second_max = min(arr[0], arr[1])
+
+    for i in range(2, len(arr)):
+        if arr[i] > max_num:
+            second_max = max_num
+            max_num = arr[i]
+        elif arr[i] > second_max and arr[i] != max_num:
+            second_max = arr[i]
+
+    return second_max
+
+# Example usage
+arr = [5, 2, 8, 9, 1]
+result = find_second_largest(arr)
+print(result)
