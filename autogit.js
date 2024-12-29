8b12080a693056e25fce1a03c6983cd7f5259e93
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 23:19:04 +0000

    12-30-2024 7:18 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,33 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-number = 29  # Change this to the number you want to check
-if is_prime(number):
-    print(f"{number} is a prime number")
-else:
-    print(f"{number} is not a prime number")
+def has_cycle(head):
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head.next
+
+    while fast and fast.next:
+        if slow == fast:
+            return True
+
+        slow = slow.next
+        fast = fast.next.next
+
+    return False
+
+# Example usage
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2  # Creating a cycle
+
+print(has_cycle(node1))  # Output: True
