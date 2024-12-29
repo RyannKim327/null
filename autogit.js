946b945e664d5592a002ca68b8a7ccd8775a6e4b
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:50:44 +0000

    12-30-2024 1:50 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,2 +1,17 @@
-user_input = input("Enter something: ")
-print("You entered:", user_input)
+def selection_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    
+    return arr
+
+# Example usage
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array:", sorted_arr)
