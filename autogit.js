23df57442069ea7258935110fea96ef992c0c1be
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 19:14:04 +0000

    12-30-2024 3:14 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,18 @@
-def fibonacci_search(arr, x):
-    fib_n_2 = 0
-    fib_n_1 = 1
-    fib_n = fib_n_1 + fib_n_2
-    
-    while fib_n < len(arr):
-        fib_n_2 = fib_n_1
-        fib_n_1 = fib_n
-        fib_n = fib_n_1 + fib_n_2
-    
-    offset = -1
-    
-    while fib_n > 1:
-        i = min(offset + fib_n_2, len(arr) - 1)
-        
-        if arr[i] < x:
-            fib_n = fib_n_1
-            fib_n_1 = fib_n_2
-            fib_n_2 = fib_n - fib_n_1
-            offset = i
-        elif arr[i] > x:
-            fib_n = fib_n_2
-            fib_n_1 = fib_n_1 - fib_n_2
-            fib_n_2 = fib_n - fib_n_1
-        else:
-            return i
-    
-    if fib_n_1 and arr[offset + 1] == x:
-        return offset + 1
-    
-    return -1
+function bubbleSort(arr) {
+    var n = arr.length;
+    for (var i = 0; i < n-1; i++) {
+        for (var j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                var temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
 
-# Test the Fibonacci search algorithm
-arr = [2, 3, 5, 8, 13, 21, 34, 55, 89]
-x = 55
-index = fibonacci_search(arr, x)
-
-if index != -1:
-    print(f"Element found at index: {index}")
-else:
-    print("Element not found")
+var array = [64, 34, 25, 12, 22, 11, 90];
+console.log("Original Array: " + array);
+var sortedArray = bubbleSort(array);
+console.log("Sorted Array: " + sortedArray);
