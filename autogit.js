99675301427645c200964f93ce4bb4d799c47625
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 22:17:25 +0000

    12-30-2024 6:17 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,5 @@
-def is_sorted_ascending(arr):
-    for i in range(1, len(arr)):
-        if arr[i] < arr[i - 1]:
-            return False
-    return True
-
-# Test the function
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [5, 4, 3, 2, 1]
-
-print(is_sorted_ascending(arr1))  # Output: True
-print(is_sorted_ascending(arr2))  # Output: False
+string1 = "Hello, "
+string2 = "world!"
+result = string1 + string2
+print(result)
+Hello, world!
