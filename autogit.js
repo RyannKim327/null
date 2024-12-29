44b015ce50e3b05059f30522c349df6247517236
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:23:51 +0000

    12-30-2024 1:23 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,10 @@
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    else:
-        pivot = arr[0]
-        less_than_pivot = [x for x in arr[1:] if x <= pivot]
-        greater_than_pivot = [x for x in arr[1:] if x > pivot]
-        return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+# Remove spaces using the replace() method
+string_with_spaces = "Hello, world!"
+string_without_spaces = string_with_spaces.replace(" ", "")
+print(string_without_spaces)
+import re
 
-# Example usage
-arr = [6, 8, 3, 5, 2]
-sorted_arr = quicksort(arr)
-print(sorted_arr)
+# Remove spaces using regular expression
+string_with_spaces = "Hello, world!"
+string_without_spaces = re.sub(r'\s+', '', string_with_spaces)
+print(string_without_spaces)
