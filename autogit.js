4842193c4ecdab11d6593d2c30474c2eb2eb26db
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:25:43 +0000

    12-30-2024 1:25 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,2 @@
-# Remove spaces using the replace() method
-string_with_spaces = "Hello, world!"
-string_without_spaces = string_with_spaces.replace(" ", "")
-print(string_without_spaces)
-import re
-
-# Remove spaces using regular expression
-string_with_spaces = "Hello, world!"
-string_without_spaces = re.sub(r'\s+', '', string_with_spaces)
-print(string_without_spaces)
+user_input = input("Enter a number: ")
+print("You entered:", user_input)
