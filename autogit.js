5c996a1ddee7a253f6b123b6cf9cc8049bad8dd3
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 23:17:47 +0000

    12-30-2024 7:17 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,14 @@
-str1 = "Hello"
-str2 = "World"
-concatenated_string = str1 + str2
-print(concatenated_string)
-String str1 = "Hello";
-String str2 = "World";
-String concatenatedString = str1 + str2;
-System.out.println(concatenatedString);
-let str1 = "Hello";
-let str2 = "World";
-let concatenatedString = str1 + str2;
-console.log(concatenatedString);
-#include <iostream>
-#include <string>
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
-int main() {
-    std::string str1 = "Hello";
-    std::string str2 = "World";
-    std::string concatenatedString = str1 + str2;
-    std::cout << concatenatedString << std::endl;
-    return 0;
-}
-str1 = "Hello"
-str2 = "World"
-concatenated_string = str1 + str2
-puts concatenated_string
+number = 5
+factorial_of_number = factorial(number)
+print(f"The factorial of {number} is {factorial_of_number}")
+import math
+
+number = 5
+factorial_of_number = math.factorial(number)
+print(f"The factorial of {number} is {factorial_of_number}")
