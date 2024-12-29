3fc83fa2a798e41c0f63ef4594dba264914d6775
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 20:20:33 +0000

    12-30-2024 4:20 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,16 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-# Test the factorial function
-number = 5
-result = factorial(number)
-print(f'The factorial of {number} is: {result}')
+function hasCycle(head):
+    if head is NULL:
+        return false
+    
+    // Initialize two pointers
+    slow_ptr = head
+    fast_ptr = head
+    
+    while fast_ptr is not NULL and fast_ptr.next is not NULL:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next.next
+        
+        if slow_ptr is equal to fast_ptr:
+            return true
+    
+    return false
