2839ae93440b8165cb42bf8f6a60e176ae13f61d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:35:46 +0000

    12-30-2024 1:35 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,28 @@
-#include <iostream>
-#include <string>
+def longest_common_substring(string1, string2):
+    m = len(string1)
+    n = len(string2)
 
-bool isPalindrome(const std::string& s) {
-    int i = 0;
-    int j = s.length() - 1;
+    # Create a 2D table to store the lengths of common substrings
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-    while (i < j) {
-        if (s[i] != s[j]) {
-            return false;
-        }
-        i++;
-        j--;
-    }
+    max_length = 0
+    end_index = 0
 
-    return true;
-}
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if string1[i - 1] == string2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_index = i
+            else:
+                dp[i][j] = 0
 
-int main() {
-    std::string str = "racecar";
-    
-    if (isPalindrome(str)) {
-        std::cout << "The string is a palindrome.";
-    } else {
-        std::cout << "The string is not a palindrome.";
-    }
+    start_index = end_index - max_length
+    longest_common_substring = string1[start_index:end_index]
 
-    return 0;
-}
+    return longest_common_substring
+string1 = "programming"
+string2 = "programmer"
+result = longest_common_substring(string1, string2)
+print(result)  # Output: "program"
