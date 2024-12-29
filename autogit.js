b250b2628ceaf98d5f6729393a77ae085f0b04a1
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 17:25:47 +0000

    12-30-2024 1:25 []

--- autogit.js
+++ autogit.js
@@ -1,2 +1,22 @@
-user_input = input("Enter a number: ")
-print("You entered:", user_input)
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def find_intersection(head1, head2):
+    node_set = set()
+    
+    # Traverse the first linked list and store all nodes in a set
+    current = head1
+    while current:
+        node_set.add(current)
+        current = current.next
+    
+    # Traverse the second linked list and check for intersection with the nodes in the set
+    current = head2
+    while current:
+        if current in node_set:
+            return current
+        current = current.next
+    
+    return None
