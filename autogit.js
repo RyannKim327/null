aa4613f90d694892d51b6253ae7932cdd645e491
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 19:14:07 +0000

    12-30-2024 3:14 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,10 @@
-function bubbleSort(arr) {
-    var n = arr.length;
-    for (var i = 0; i < n-1; i++) {
-        for (var j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                var temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-    return arr;
-}
+arr = [1, 2, 3, 4, 5]
+left = 0
+right = len(arr) - 1
 
-var array = [64, 34, 25, 12, 22, 11, 90];
-console.log("Original Array: " + array);
-var sortedArray = bubbleSort(array);
-console.log("Sorted Array: " + sortedArray);
+while left < right:
+    arr[left], arr[right] = arr[right], arr[left]
+    left += 1
+    right -= 1
+
+print(arr)  # Output: [5, 4, 3, 2, 1]
