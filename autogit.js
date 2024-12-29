b224fe68f8ba4dad05bd9320b64867e35cd544e5
RyannKim327 <rksesgundo123@gmail.com>
2024-12-29 23:18:01 +0000

    12-30-2024 7:17 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,32 @@
-def findMedianSortedArrays(nums1, nums2):
-    merged_array = sorted(nums1 + nums2)
-    n = len(merged_array)
+def counting_sort(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
     
-    if n % 2 == 0:
-        return (merged_array[n // 2 - 1] + merged_array[n // 2]) / 2
-    else:
-        return merged_array[n // 2]
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+    
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+    
+    for i in range(n):
+        arr[i] = output[i]
 
-# Example
-nums1 = [1, 3]
-nums2 = [2]
-print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
+def radix_sort(arr):
+    max_num = max(arr)
+    exp = 1
+    while max_num // exp > 0:
+        counting_sort(arr, exp)
+        exp *= 10
+
+arr = [170, 45, 75, 90, 802, 24, 2, 66]
+radix_sort(arr)
+print("Sorted array is:", arr)
