e92d1a28b1b74721a2c250ce0113ff693d914799
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 06:29:52 +0000

    12-30-2024 14:29 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,34 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+def findMedianSortedArrays(nums1, nums2):
+    m, n = len(nums1), len(nums2)
+    total_length = m + n
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
+    if total_length % 2 == 1:
+        return findKthElement(nums1, nums2, total_length // 2)
     else:
-        if root.val < key:
-            root.right = insert(root.right, key)
+        return (findKthElement(nums1, nums2, total_length // 2 - 1) + findKthElement(nums1, nums2, total_length // 2)) / 2.0
+
+def findKthElement(nums1, nums2, k):
+    if not nums1:
+        return nums2[k]
+    if not nums2:
+        return nums1[k]
+        
+    if k == 0:
+        return min(nums1[0], nums2[0])
+
+    mid1, mid2 = len(nums1) // 2, len(nums2) // 2
+    if mid1 + mid2 < k:
+        if nums1[mid1] > nums2[mid2]:
+            return findKthElement(nums1, nums2[mid2 + 1:], k - mid2 - 1)
         else:
-            root.left = insert(root.left, key)
-    return root
+            return findKthElement(nums1[mid1 + 1:], nums2, k - mid1 - 1)
+    else:
+        if nums1[mid1] > nums2[mid2]:
+            return findKthElement(nums1[:mid1], nums2, k)
+        else:
+            return findKthElement(nums1, nums2[:mid2], k)
 
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(root.val)
-        inorder_traversal(root.right)
-
-# Usage
-root = Node(10)
-root = insert(root, 5)
-root = insert(root, 15)
-root = insert(root, 3)
-root = insert(root, 7)
-
-inorder_traversal(root)
+# Example arrays
+nums1 = [1, 3]
+nums2 = [2]
+print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
