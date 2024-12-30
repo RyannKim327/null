0c03cd6e57e59602743ba0907f04df689ec0e9a6
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 19:15:54 +0000

    12-31-2024 3:15 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,39 @@
-// Node definition
-struct Node {
-    int data;
-    struct Node* next;
-};
+import heapq
 
-// Function to find the length of a linked list
-int findLinkedListLength(struct Node* head) {
-    int length = 0;
-    struct Node* current = head;
+def astar_search(graph, start, goal):
+    open_list = []
+    closed_set = set()
 
-    while (current != NULL) {
-        length++;
-        current = current->next;
-    }
+    # Priority queue for open list
+    heapq.heappush(open_list, (0, start))
+    
+    while open_list:
+        cost, current_node = heapq.heappop(open_list)
+        
+        if current_node == goal:
+            return path
+        
+        closed_set.add(current_node)
+        
+        for neighbor, weight in graph[current_node].items():
+            if neighbor not in closed_set:
+                heapq.heappush(open_list, (cost + weight, neighbor))
 
-    return length;
+    return None
+
+# Example usage
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'D': 3, 'E': 2},
+    'C': {'F': 5},
+    'D': {'G': 1},
+    'E': {'G': 3},
+    'F': {'G': 2},
+    'G': {}
 }
+
+start = 'A'
+goal = 'G'
+
+result = astar_search(graph, start, goal)
+print(result)
