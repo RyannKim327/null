7031c9f29de95550ad83920353e158bd0ed4b1c9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 06:29:06 +0000

    12-30-2024 14:29 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,26 +1,20 @@
-def longest_common_substring(str1, str2):
-    m = len(str1)
-    n = len(str2)
-    
-    # Create a 2D table to store the length of the longest common suffix
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    
-    # Variables to store the length of the longest common substring and its ending position
-    max_length = 0
-    end_position = 0
-    
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-                if dp[i][j] > max_length:
-                    max_length = dp[i][j]
-                    end_position = i
-    
-    start_position = end_position - max_length
-    return str1[start_position:end_position]
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
 
-# Example usage
-str1 = "abcdefg"
-str2 = "xyzabcd"
-print(longest_common_substring(str1, str2))  # Output: "abcd"
+    min_len = min(len(s) for s in strings)
+    prefix = ""
+    
+    for i in range(min_len):  
+        char = strings[0][i]
+        
+        if all(s[i] == char for s in strings):
+            prefix += char
+        else:
+            break
+            
+    return prefix
+
+# Example usage:
+strings = ["flower", "flow", "flight"]
+print(longest_common_prefix(strings))  # Output: "fl"
