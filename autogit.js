bdc231d6003024ca630f7cda2de1762a96598f94
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 05:19:29 +0000

    12-30-2024 13:19 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,5 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-def maxDepth(node):
-    if node is None:
-        return 0
-    else:
-        left_depth = maxDepth(node.left)
-        right_depth = maxDepth(node.right)
-
-        return max(left_depth, right_depth) + 1
-
-# Example usage
-# Create a simple binary tree
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
-
-print("Maximum depth of the binary tree is:", maxDepth(root))
+string1 = "Hello, "
+string2 = "World!"
+result = string1 + string2
+print(result)
+Hello, World!
