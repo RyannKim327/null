bc12fe8aa59223ff66c5fbdd2e27bf5bc599dccf
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 03:26:31 +0000

    12-30-2024 11:26 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,29 @@
-# Replace spaces with an empty string
-new_string = your_string.replace(" ", "")
-import re
+from collections import deque
 
-# Use regular expression to remove all whitespace characters
-new_string = re.sub(r"\s", "", your_string)
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            print(node)
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Starting node for BFS
+start_node = 'A'
+bfs(graph, start_node)
