db0f1a1a99d8fb2d39dd37c30499e5d7eb3429f4
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 05:19:18 +0000

    12-30-2024 13:19 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,36 @@
-# Taking user input
-user_input = input("Enter some text: ")
+from collections import defaultdict
 
-# Printing the user input
-print("You entered:", user_input)
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def breadth_first_search(self, start_node):
+        visited = set()
+        queue = []
+
+        visited.add(start_node)
+        queue.append(start_node)
+
+        while queue:
+            current_node = queue.pop(0)
+            print(current_node)
+
+            for neighbor in self.graph[current_node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+# Example usage
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("Breadth First Traversal starting from vertex 2:")
+g.breadth_first_search(2)
