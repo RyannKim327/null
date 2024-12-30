a1b356d6648e559529f291ae787a889583ad3995
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 08:27:16 +0000

    12-30-2024 16:27 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,14 @@
-# String input
-string_number = "42"
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-# Convert string to integer
-integer_number = int(string_number)
-
-# Printing the converted integer
-print(integer_number)
+# Test the function
+num = 17
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
