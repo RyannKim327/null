0120d22b1870dbf66f4ccd9e2f6299a85cf6e3df
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 01:39:00 +0000

    12-30-2024 9:38 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,22 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+def find_second_largest(arr):
+    if len(arr) < 2:
+        return "Array should have at least two elements"
+    
+    max_num = arr[0]
+    second_max = float('-inf')
+    
+    for i in range(1, len(arr)):
+        if arr[i] > max_num:
+            second_max = max_num
+            max_num = arr[i]
+        elif arr[i] > second_max and arr[i] != max_num:
+            second_max = arr[i]
+    
+    if second_max == float('-inf'):
+        return "There is no second largest element"
+    else:
+        return second_max
 
-def has_cycle(head):
-    if not head or not head.next:
-        return False
-
-    slow = head
-    fast = head.next
-
-    while fast and fast.next:
-        if slow == fast:
-            return True
-
-        slow = slow.next
-        fast = fast.next.next
-
-    return False
-
-# Example usage
-node1 = Node(1)
-node2 = Node(2)
-node3 = Node(3)
-node4 = Node(4)
-
-node1.next = node2
-node2.next = node3
-node3.next = node4
-node4.next = node2  # Creating a cycle
-
-print(has_cycle(node1))  # Output: True
+# Example
+arr = [5, 2, 8, 10, 3]
+print(find_second_largest(arr))
