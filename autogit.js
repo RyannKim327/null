cf44e98d3392dfce1f280e7167fae8ee9f28a9a2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 04:24:04 +0000

    12-30-2024 12:24 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,12 @@
-# Method 1: Using replace() function
-text = "  Hello,   World!  "
-cleaned_text = text.replace(" ", "")
-print(cleaned_text)
-import re
+def is_sorted_ascending(arr):
+    for i in range(len(arr) - 1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
 
-# Method 2: Using Regular Expressions
-text = "  Hello,   World!  "
-cleaned_text = re.sub(r'\s+', '', text)
-print(cleaned_text)
+# Example usage
+arr = [1, 2, 3, 4, 5]
+if is_sorted_ascending(arr):
+    print("Array is sorted in ascending order.")
+else:
+    print("Array is not sorted in ascending order.")
