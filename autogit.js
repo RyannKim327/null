25bc1cc43a6556fd15d78e509a168f04511b63d2
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 01:39:07 +0000

    12-30-2024 9:39 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,41 @@
-decimal_number = 42
-binary_number = bin(decimal_number)
+def preprocess_bad_character_table(pattern):
+    table = {}
 
-print(f"The binary representation of {decimal_number} is {binary_number}")
-The binary representation of 42 is 0b101010
+    for i in range(len(pattern) - 1):
+        table[pattern[i]] = len(pattern) - 1 - i
+
+    return table
+
+def boyer_moore_horspool(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    
+    if m > n:
+        return -1
+
+    bc_table = preprocess_bad_character_table(pattern)
+
+    i = m - 1
+    j = m - 1
+
+    while i < n:
+        if text[i] == pattern[j]:
+            if j == 0:
+                return i
+            else:
+                i -= 1
+                j -= 1
+        else:
+            i += bc_table.get(text[i], m)
+            j = m - 1
+
+    return -1
+
+text = "exampletext"
+pattern = "text"
+result = boyer_moore_horspool(text, pattern)
+
+if result != -1:
+    print("Pattern found at index:", result)
+else:
+    print("Pattern not found")
