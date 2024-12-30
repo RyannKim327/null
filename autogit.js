886b4b8c863ead9710ec0ba44565629a7f57fd40
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 08:26:08 +0000

    12-30-2024 16:26 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,8 @@
-string1 = "Hello"
-string2 = "World"
-concatenated_string = string1 + string2
-print(concatenated_string)
-let string1 = "Hello";
-let string2 = "World";
-let concatenatedString  = string1 + string2;
-console.log(concatenatedString);
-String string1 = "Hello";
-String string2 = "World";
-String concatenatedString = string1 + string2;
-System.out.println(concatenatedString);
-string string1 = "Hello";
-string string2 = "World";
-string concatenatedString = string1 + string2;
-Console.WriteLine(concatenatedString);
+s = "   Hello, World!   "
+stripped_string = s.strip()
+print(stripped_string)
+Hello, World!
+s = "   Hello, World!   "
+no_whitespace_string = s.replace(" ", "")
+print(no_whitespace_string)
+Hello,World!
