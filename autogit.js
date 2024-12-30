a3433e62cb71f8a9868b1bef0e51c7db079a2d31
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 20:21:46 +0000

    12-31-2024 4:21 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,48 @@
-def is_palindrome(s):
-    # Convert the string to lowercase
-    s = s.lower()
-    
-    # Remove spaces and special characters from the string
-    s = ''.join(e for e in s if e.isalnum())
-    
-    # Check if the string is a palindrome
-    for i in range(len(s) // 2):
-        if s[i] != s[len(s) - i - 1]:
-            return False
-    return True
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Test the function
-string = "A man, a plan, a canal, Panama"
-if is_palindrome(string):
-    print("The string is a palindrome.")
-else:
-    print("The string is not a palindrome.")
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def find_nth_from_end(self, n):
+        length = 0
+        current = self.head
+        while current:
+            length += 1
+            current = current.next
+
+        if n > length:
+            return None
+
+        target = length - n
+        current = self.head
+        for i in range(target):
+            current = current.next
+
+        return current.data
+
+# Create a linked list
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+llist.append(5)
+
+# Find the 2nd node from the end
+n = 2
+result = llist.find_nth_from_end(n)
+print(f'The {n}th node from the end is {result}')
