136d0eaaa492ca21d36c1afb364f6f8ecefe4489
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 08:26:12 +0000

    12-30-2024 16:26 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,12 @@
-s = "   Hello, World!   "
-stripped_string = s.strip()
-print(stripped_string)
-Hello, World!
-s = "   Hello, World!   "
-no_whitespace_string = s.replace(" ", "")
-print(no_whitespace_string)
-Hello,World!
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+# Example usage
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(quicksort(arr))
