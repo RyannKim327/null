d1e2a1a316fb1897c400424c4c24762ee9bac352
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 04:24:47 +0000

    12-30-2024 12:24 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,5 @@
-def is_sorted_ascending(arr):
-    for i in range(len(arr) - 1):
-        if arr[i] > arr[i+1]:
-            return False
-    return True
+# Taking user input
+user_input = input("Enter some text: ")
 
-# Example usage
-arr = [1, 2, 3, 4, 5]
-if is_sorted_ascending(arr):
-    print("Array is sorted in ascending order.")
-else:
-    print("Array is not sorted in ascending order.")
+# Printing the user input
+print("You entered:", user_input)
