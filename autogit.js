b20e8559ffe6ea4c4c4baed89692b5a7ecf8866c
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 14:18:13 +0000

    12-30-2024 22:18 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,7 @@
-#include <iostream>
-#include <stack>
-using namespace std;
+def reverse_string(input_string):
+    return input_string[::-1]
 
-struct Node {
-    int data;
-    Node* next;
-};
-
-bool isPalindrome(Node* head) {
-    stack<int> s;
-    Node* curr = head;
-
-    // Traverse the linked list and store values in a stack
-    while (curr != NULL) {
-        s.push(curr->data);
-        curr = curr->next;
-    }
-
-    // Traverse the linked list again and compare elements with stack
-    curr = head;
-    while (curr != NULL) {
-        int top = s.top();
-        s.pop();
-
-        if (curr->data != top) {
-            return false;
-        }
-
-        curr = curr->next;
-    }
-
-    return true;
-}
-
-int main() {
-    Node* head = new Node{1, new Node{2, new Node{3, new Node{2, new Node{1, NULL}}}}};
-    if (isPalindrome(head)) {
-        cout << "The linked list is a palindrome." << endl;
-    } else {
-        cout << "The linked list is not a palindrome." << endl;
-    }
-
-    return 0;
-}
+# Test the function
+original_string = "Hello, World!"
+reversed_string = reverse_string(original_string)
+print(reversed_string)
