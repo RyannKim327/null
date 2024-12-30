e9035a4ea2e342f079ad2607a8987bbf5b93be61
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 06:28:43 +0000

    12-30-2024 14:28 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,7 @@
-def reverse_array(arr):
-    left = 0
-    right = len(arr) - 1
-    
-    while left < right:
-        # Swap elements
-        arr[left], arr[right] = arr[right], arr[left]
-        
-        # Move pointers
-        left += 1
-        right -= 1
-    
-    return arr
+def calculate_mean(numbers):
+    total = sum(numbers)
+    mean = total / len(numbers)
+    return mean
 
-# Test the function
-my_array = [1, 2, 3, 4, 5]
-reversed_array = reverse_array(my_array)
-print(reversed_array)
+numbers = [1, 2, 3, 4, 5]
+print(calculate_mean(numbers))
