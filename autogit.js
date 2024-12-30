4f48d3ccede752ffefa4f301fe1625e2b755f260
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 01:40:05 +0000

    12-30-2024 9:40 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,12 @@
-from cron import Cron
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
 
-def my_task():
-    print("Executing my task")
-
-# Define a cron schedule to run the task every minute
-cron = Cron()
-cron.schedule("*/1 * * * *", my_task)
-
-# Run the cron job
-cron.run_forever()
+# Example usage:
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(quicksort(arr))
