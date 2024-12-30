bd86835a58df297a6addf4f51011c426ba90cb32
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 05:19:21 +0000

    12-30-2024 13:19 [Kaya pala, may iba na siguro sya]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,7 @@
-from collections import defaultdict
-
-class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
-
-    def add_edge(self, u, v):
-        self.graph[u].append(v)
-
-    def breadth_first_search(self, start_node):
-        visited = set()
-        queue = []
-
-        visited.add(start_node)
-        queue.append(start_node)
-
-        while queue:
-            current_node = queue.pop(0)
-            print(current_node)
-
-            for neighbor in self.graph[current_node]:
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
-
-# Example usage
-g = Graph()
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 0)
-g.add_edge(2, 3)
-g.add_edge(3, 3)
-
-print("Breadth First Traversal starting from vertex 2:")
-g.breadth_first_search(2)
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
