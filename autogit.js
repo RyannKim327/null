abc47736ccf4a19b4bf7a82d00e35be02a72eafb
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 03:26:34 +0000

    12-30-2024 11:26 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,14 @@
-from collections import deque
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
 
-def bfs(graph, start):
-    visited = set()
-    queue = deque([start])
-
-    while queue:
-        node = queue.popleft()
-        if node not in visited:
-            visited.add(node)
-            print(node)
-
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    queue.append(neighbor)
-
-# Example graph represented as an adjacency list
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-# Starting node for BFS
-start_node = 'A'
-bfs(graph, start_node)
+# Example usage
+arr = [64, 25, 12, 22, 11]
+sorted_arr = selection_sort(arr)
+print("Sorted array is:", sorted_arr)
