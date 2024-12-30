83f0e988cf7b010ad5138599c17ab03742a2bf81
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 01:39:03 +0000

    12-30-2024 9:39 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,5 @@
-def find_second_largest(arr):
-    if len(arr) < 2:
-        return "Array should have at least two elements"
-    
-    max_num = arr[0]
-    second_max = float('-inf')
-    
-    for i in range(1, len(arr)):
-        if arr[i] > max_num:
-            second_max = max_num
-            max_num = arr[i]
-        elif arr[i] > second_max and arr[i] != max_num:
-            second_max = arr[i]
-    
-    if second_max == float('-inf'):
-        return "There is no second largest element"
-    else:
-        return second_max
+decimal_number = 42
+binary_number = bin(decimal_number)
 
-# Example
-arr = [5, 2, 8, 10, 3]
-print(find_second_largest(arr))
+print(f"The binary representation of {decimal_number} is {binary_number}")
+The binary representation of 42 is 0b101010
