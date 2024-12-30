02742cd05f9d9317b66280b1420c4dc197002149
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 13:26:20 +0000

    12-30-2024 21:26 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,33 @@
-def is_anagram(str1, str2):
-    return sorted(str1) == sorted(str2)
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-str1 = "listen"
-str2 = "silent"
+def insert_node(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert_node(root.right, key)
+        else:
+            root.left = insert_node(root.left, key)
+    return root
 
-if is_anagram(str1, str2):
-    print("The strings are anagrams.")
-else:
-    print("The strings are not anagrams.")
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+# Example usage:
+root = None
+root = insert_node(root, 50)
+insert_node(root, 30)
+insert_node(root, 20)
+insert_node(root, 40)
+insert_node(root, 70)
+insert_node(root, 60)
+insert_node(root, 80)
+
+inorder_traversal(root)
