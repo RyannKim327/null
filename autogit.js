21c3e8476761b10b0782e803366b394eb73832e9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 16:24:36 +0000

    12-31-2024 0:24 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,10 @@
-# Define an array
-array = [1, 2, 3, 4, 5]
+def find_string_length(input_string):
+    count = 0
+    for char in input_string:
+        count += 1
+    return count
 
-# Reverse the order of elements in the array
-reversed_array = array[::-1]
-
-# Print the reversed array
-print(reversed_array)
+# Test the function
+input_string = "Hello, World!"
+length = find_string_length(input_string)
+print("Length of the string:", length)
