12b600f920c92a3bcd3871cb021117e62f4cf48d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 13:25:19 +0000

    12-30-2024 21:25 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,36 +1,113 @@
 class Node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
+    def __init__(self, value, color='red'):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.color = color
 
-class LinkedList:
+class RedBlackTree:
     def __init__(self):
-        self.head = None
+        self.root = None
 
-    def append(self, data):
-        new_node = Node(data)
-        if self.head is None:
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
+    def insert(self, value):
+        new_node = Node(value)
 
-    def length(self):
-        current = self.head
-        count = 0
-        while current:
-            count += 1
-            current = current.next
-        return count
+        if self.root is None:
+            self.root = new_node
+            self.root.color = 'black'
+        else:
+            current = self.root
+            parent = None
 
-# Creating a linked list
-llist = LinkedList()
-llist.append(1)
-llist.append(2)
-llist.append(3)
-llist.append(4)
-llist.append(5)
+            while current is not None:
+                parent = current
 
-print("Length of the linked list is:", llist.length())
+                if value < current.value:
+                    current = current.left
+                else:
+                    current = current.right
+
+            new_node.parent = parent
+
+            if value < parent.value:
+                parent.left = new_node
+            else:
+                parent.right = new_node
+
+            self.fix_violation(new_node)
+
+    def fix_violation(self, node):
+        while node != self.root and node.parent.color == 'red':
+            if node.parent == node.parent.parent.left:
+                uncle = node.parent.parent.right
+
+                if uncle is not None and uncle.color == 'red':
+                    node.parent.color = 'black'
+                    uncle.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self.left_rotate(node)
+
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.right_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.left
+
+                if uncle is not None and uncle.color == 'red':
+                    node.parent.color = 'black'
+                    uncle.color = 'black'
+                    node.parent.parent.color = 'red'
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.right_rotate(node)
+
+                    node.parent.color = 'black'
+                    node.parent.parent.color = 'red'
+                    self.left_rotate(node.parent.parent)
+
+        self.root.color = 'black'
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+
+        if y.left is not None:
+            y.left.parent = x
+
+        y.parent = x.parent
+
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+
+        if x.right is not None:
+            x.right.parent = y
+
+        x.parent = y.parent
+
+        if y.parent is None:
+            self.root = x
+        elif y == y.parent.right:
+            y.parent.right = x
+        else:
+            y.parent.left = x
+
+        x.right = y
+        y.parent = x
