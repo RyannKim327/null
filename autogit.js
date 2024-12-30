04f12264c96237b932f5967525c9740fe05f0aaf
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 02:53:02 +0000

    12-30-2024 10:52 [Hindi naman ako naghahabol, hindi rin naman sya nagpapahabol]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,19 @@
-def counting_sort(arr):
-    max_num = max(arr)
-    min_num = min(arr)
-    range_of_elements = max_num - min_num + 1
-
-    count = [0] * range_of_elements
-    output = [0] * len(arr)
+def are_anagrams(str1, str2):
+    # Remove non-alphabetic characters and convert to lowercase
+    str1_clean = ''.join(filter(str.isalpha, str1)).lower()
+    str2_clean = ''.join(filter(str.isalpha, str2)).lower()
     
-    for num in arr:
-        count[num - min_num] += 1
-
-    for i in range(1, len(count)):
-        count[i] += count[i - 1]
+    # Sort the characters of both strings alphabetically
+    str1_sorted = ''.join(sorted(str1_clean))
+    str2_sorted = ''.join(sorted(str2_clean))
     
-    for num in reversed(arr):
-        output[count[num - min_num] - 1] = num
-        count[num - min_num] -= 1
+    # Compare the sorted strings
+    return str1_sorted == str2_sorted
 
-    return output
-
-# Testing the counting sort algorithm
-arr = [4, 2, 2, 8, 3, 3, 1]
-sorted_arr = counting_sort(arr)
-print("Sorted array is:", sorted_arr)
+# Test the function
+str1 = "listen"
+str2 = "silent"
+if are_anagrams(str1, str2):
+    print("The strings are anagrams.")
+else:
+    print("The strings are not anagrams.")
