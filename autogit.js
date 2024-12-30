483f22ceeeaf59a91004ced41c92d34e1ae99609
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 13:25:24 +0000

    12-30-2024 21:25 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,113 +1,22 @@
-class Node:
-    def __init__(self, value, color='red'):
-        self.value = value
-        self.left = None
-        self.right = None
-        self.parent = None
-        self.color = color
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        # Flag to check if any swapping is done in this pass
+        swapped = False
+        
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+                
+        # If no two elements were swapped in the inner loop, then the array is already sorted
+        if not swapped:
+            break
+            
+    return arr
 
-class RedBlackTree:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        new_node = Node(value)
-
-        if self.root is None:
-            self.root = new_node
-            self.root.color = 'black'
-        else:
-            current = self.root
-            parent = None
-
-            while current is not None:
-                parent = current
-
-                if value < current.value:
-                    current = current.left
-                else:
-                    current = current.right
-
-            new_node.parent = parent
-
-            if value < parent.value:
-                parent.left = new_node
-            else:
-                parent.right = new_node
-
-            self.fix_violation(new_node)
-
-    def fix_violation(self, node):
-        while node != self.root and node.parent.color == 'red':
-            if node.parent == node.parent.parent.left:
-                uncle = node.parent.parent.right
-
-                if uncle is not None and uncle.color == 'red':
-                    node.parent.color = 'black'
-                    uncle.color = 'black'
-                    node.parent.parent.color = 'red'
-                    node = node.parent.parent
-                else:
-                    if node == node.parent.right:
-                        node = node.parent
-                        self.left_rotate(node)
-
-                    node.parent.color = 'black'
-                    node.parent.parent.color = 'red'
-                    self.right_rotate(node.parent.parent)
-            else:
-                uncle = node.parent.parent.left
-
-                if uncle is not None and uncle.color == 'red':
-                    node.parent.color = 'black'
-                    uncle.color = 'black'
-                    node.parent.parent.color = 'red'
-                    node = node.parent.parent
-                else:
-                    if node == node.parent.left:
-                        node = node.parent
-                        self.right_rotate(node)
-
-                    node.parent.color = 'black'
-                    node.parent.parent.color = 'red'
-                    self.left_rotate(node.parent.parent)
-
-        self.root.color = 'black'
-
-    def left_rotate(self, x):
-        y = x.right
-        x.right = y.left
-
-        if y.left is not None:
-            y.left.parent = x
-
-        y.parent = x.parent
-
-        if x.parent is None:
-            self.root = y
-        elif x == x.parent.left:
-            x.parent.left = y
-        else:
-            x.parent.right = y
-
-        y.left = x
-        x.parent = y
-
-    def right_rotate(self, y):
-        x = y.left
-        y.left = x.right
-
-        if x.right is not None:
-            x.right.parent = y
-
-        x.parent = y.parent
-
-        if y.parent is None:
-            self.root = x
-        elif y == y.parent.right:
-            y.parent.right = x
-        else:
-            y.parent.left = x
-
-        x.right = y
-        y.parent = x
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print("Sorted array:", sorted_arr)
