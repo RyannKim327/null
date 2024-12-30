d77769881e36c59dc5f4b4b33a2c3bb46eee2e73
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 06:28:56 +0000

    12-30-2024 14:28 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,63 +1,27 @@
-import random
+def boyer_moore_horspool(text, pattern):
+    skip = {}
+    for i in range(len(pattern) - 1):
+        skip[pattern[i]] = len(pattern) - i - 1
 
-class Node:
-    def __init__(self, key, value, level):
-        self.key = key
-        self.value = value
-        self.forward = [None] * level
+    i = 0
+    while i <= len(text) - len(pattern):
+        j = len(pattern) - 1
+        while j >= 0 and pattern[j] == text[i + j]:
+            j -= 1
+        if j == -1:
+            return i
+        else:
+            if text[i + len(pattern) - 1] in skip:
+                i += skip[text[i + len(pattern) - 1]]
+            else:
+                i += len(pattern)
+    return -1
 
-class SkipList:
-    def __init__(self, max_level, p):
-        self.max_level = max_level
-        self.p = p
-        self.header = self.create_node(None, None, max_level)
+text = "exampletextwithpattern"
+pattern = "pattern"
+result = boyer_moore_horspool(text, pattern)
 
-    def create_node(self, key, value, level):
-        return Node(key, value, level)
-
-    def random_level(self):
-        level = 1
-        while random.random() < self.p and level < self.max_level:
-            level += 1
-        return level
-
-    def insert(self, key, value):
-        update = [None] * self.max_level
-        current = self.header
-
-        for i in range(self.max_level - 1, -1, -1):
-            while current.forward[i] and current.forward[i].key < key:
-                current = current.forward[i]
-            update[i] = current
-
-        level = self.random_level()
-        new_node = self.create_node(key, value, level)
-
-        for i in range(level):
-            new_node.forward[i] = update[i].forward[i]
-            update[i].forward[i] = new_node
-
-    def search(self, key):
-        current = self.header
-        for i in range(self.max_level - 1, -1, -1):
-            while current.forward[i] and current.forward[i].key < key:
-                current = current.forward[i]
-        current = current.forward[0]
-        if current and current.key == key:
-            return current.value
-        return None
-
-    def delete(self, key):
-        update = [None] * self.max_level
-        current = self.header
-
-        for i in range(self.max_level - 1, -1, -1):
-            while current.forward[i] and current.forward[i].key < key:
-                current = current.forward[i]
-            update[i] = current
-
-        current = current.forward[0]
-
-        if current and current.key == key:
-            for i in range(len(current.forward)):
-                update[i].forward[i] = current.forward[i]
+if result != -1:
+    print("Pattern found at index:", result)
+else:
+    print("Pattern not found in text")
