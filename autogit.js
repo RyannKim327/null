dea2d0a6c62a5c931102597123b5ee28565f44e4
RyannKim327 <rksesgundo123@gmail.com>
2024-12-30 09:20:53 +0000

    12-30-2024 17:20 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,19 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap elements
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
 
-def find_middle_element(head):
-    if head is None:
-        return None
-
-    slow_ptr = head
-    fast_ptr = head
-
-    while fast_ptr is not None and fast_ptr.next is not None:
-        slow_ptr = slow_ptr.next
-        fast_ptr = fast_ptr.next.next
-
-    return slow_ptr.data
-
-# Example of using the find_middle_element function
-# Construct a sample linked list
-head = Node(1)
-head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = Node(4)
-head.next.next.next.next = Node(5)
-
-print("Middle element of the linked list is:", find_middle_element(head))
+// Example usage
+let array = [5, 3, 8, 2, 1, 4];
+console.log("Original Array: ", array);
+console.log("Sorted Array: ", bubbleSort(array));
