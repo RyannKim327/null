c2a5e0f88027c13a4addd6a6430247d30bdfd2df
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 04:44:32 +0000

    12-31-2024 12:44 [Kaya pala, may iba na siguro sya]

--- autogit.js
+++ autogit.js
@@ -1,27 +1,17 @@
-def longest_common_substring(s1, s2):
-    m = len(s1)
-    n = len(s2)
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num == 2:
+        return True
+    if num % 2 == 0:
+        return False
+    for i in range(3, int(num**0.5) + 1, 2):
+        if num % i == 0:
+            return False
+    return True
 
-    # Create a table to store the lengths of the common substrings
-    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
-
-    max_length = 0
-    end_index = 0
-
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if s1[i-1] == s2[j-1]:
-                dp[i][j] = dp[i-1][j-1] + 1
-                if dp[i][j] > max_length:
-                    max_length = dp[i][j]
-                    end_index = i
-            else:
-                dp[i][j] = 0
-
-    start_index = end_index - max_length
-    return s1[start_index:end_index]
-
-# Test the function
-s1 = "abcdefg"
-s2 = "xyabcz"
-print(longest_common_substring(s1, s2))  # Output: "abc"
+number = 17
+if is_prime(number):
+    print(f"{number} is a prime number")
+else:
+    print(f"{number} is not a prime number")
