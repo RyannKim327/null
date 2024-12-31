6cf94144bbb7170b524feaf26b8deec8dd56bf45
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 05:50:38 +0000

    12-31-2024 13:50 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,22 +1,34 @@
-class SkipNode {
-    int value;
-    SkipNode[] next;
+str = "Hello, World!"
+lowercase_str = str.lower()
+print(lowercase_str)
+var str = "Hello, World!";
+var lowercase_str = str.toLowerCase();
+console.log(lowercase_str);
+String str = "Hello, World!";
+String lowercaseStr = str.toLowerCase();
+System.out.println(lowercaseStr);
+#include <iostream>
+#include <algorithm>
+#include <string>
 
-    public SkipNode(int value, int levels) {
-        this.value = value;
-        this.next = new SkipNode[levels];
+int main() {
+    std::string str = "Hello, World!";
+    std::transform(str.begin(), str.end(), str.begin(),
+                   [](unsigned char c){ return std::tolower(c); });
+    std::cout << str << std::endl;
+    return 0;
+}
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Hello, World!";
+        string lowercaseStr = str.ToLower();
+        Console.WriteLine(lowercaseStr);
     }
 }
-class SkipList {
-    SkipNode head;
-    int maxLevels;
-    int size;
-
-    public SkipList(int maxLevels) {
-        this.head = new SkipNode(Integer.MIN_VALUE, maxLevels);
-        this.maxLevels = maxLevels;
-        this.size = 0;
-    }
-
-    // Implement insertion, deletion, and search methods here
-}
+$str = "Hello, World!";
+$lowercase_str = strtolower($str);
+echo $lowercase_str;
