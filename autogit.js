c2ad73af58cbbf5adeed9180051cd64a09161f20
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:35:57 +0000

    12-31-2024 16:35 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++
@@ -1,9 +0,0 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-
-# Function call to calculate the factorial of 5
-result = factorial(5)
-print(result)  # Output: 120
