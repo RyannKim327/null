21ef1641fe1c6582df12845d63c372f696c3e750
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 03:54:12 +0000

    12-31-2024 11:54 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,24 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+
+result = binary_search(arr, target)
+
+if result != -1:
+    print("Element found at index:", result)
+else:
+    print("Element not found")
