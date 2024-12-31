815442b7ab85dc39cf4a337457bd8a476be623ea
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 03:54:03 +0000

    12-31-2024 11:54 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,4 @@
-from queue import PriorityQueue
-
-def astar_search(start, goal, graph):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    
-    came_from = {}
-    cost_so_far = {}
-    
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not frontier.empty():
-        current = frontier.get()
-        
-        if current == goal:
-            break
-        
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + graph.cost(current, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
-    
-    path = reconstruct_path(came_from, start, goal)
-    return path
-
-def reconstruct_path(came_from, start, goal):
-    current = goal
-    path = []
-    while current != start:
-        path.append(current)
-        current = came_from[current]
-    path.append(start)
-    path.reverse()
-    return path
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
