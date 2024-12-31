53d815b2df7f10fbcb615b3c4f65ff5ade7107e9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:13:03 +0000

    12-31-2024 15:12 [Kaya pala, may iba na siguro sya]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,12 @@
-def longest_common_substring(s1, s2):
-    m = len(s1)
-    n = len(s2)
+def is_palindrome(s):
+    # Remove non-alphanumeric characters and convert to lowercase
+    cleaned_string = ''.join(c.lower() for c in s if c.isalnum())
+    # Compare cleaned string with its reverse
+    return cleaned_string == cleaned_string[::-1]
 
-    # Create a table to store the length of the longest common suffix
-    # of s1[:i] and s2[:j]
-    table = [[0] * (n + 1) for _ in range(m + 1)]
-
-    # Variables to keep track of the longest common substring and its length
-    longest_length = 0
-    end_index = 0
-
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if s1[i - 1] == s2[j - 1]:
-                table[i][j] = table[i - 1][j - 1] + 1
-                if table[i][j] > longest_length:
-                    longest_length = table[i][j]
-                    end_index = i
-            else:
-                table[i][j] = 0
-
-    start_index = end_index - longest_length
-    return s1[start_index:end_index]
-
-s1 = "abcdef"
-s2 = "bcde"
-result = longest_common_substring(s1, s2)
-print(result)
+# Test the function
+input_string = "A man, a plan, a canal, Panama!"
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
