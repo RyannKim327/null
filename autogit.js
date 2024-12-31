657e49dcd7d57afbf58d6d8803de78e245639d98
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:26:36 +0000

    12-31-2024 15:26 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,5 +1,18 @@
-function sumOfNodes(root):
-    if root is null:
-        return 0
-        
-    return root.value + sumOfNodes(root.left) + sumOfNodes(root.right)
+def dfs(graph, node, visited):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+if __name__ == "__main__":
+    graph = {
+        'A': ['B', 'C'],
+        'B': ['D', 'E'],
+        'C': ['F'],
+        'D': [],
+        'E': ['F'],
+        'F': []
+    }
+
+    visited = set()
+    dfs(graph, 'A', visited)
