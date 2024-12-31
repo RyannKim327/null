30c47c007f2a8c453fc8c8b43c48efd3fece76c5
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 06:55:16 +0000

    12-31-2024 14:55 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,6 @@
-def find_common_elements(arr1, arr2):
-    common_elements = []
-    for element in arr1:
-        if element in arr2 and element not in common_elements:
-            common_elements.append(element)
-    return common_elements
+import math
 
-# Example usage
-array1 = [1, 2, 3, 4, 5]
-array2 = [3, 4, 5, 6, 7]
-result = find_common_elements(array1, array2)
-print(result)
+number = 5
+factorial = math.factorial(number)
+
+print(f"The factorial of {number} is {factorial}")
