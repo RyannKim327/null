944235650409210c24bc379da1881e9cff50a97a
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 01:26:47 +0000

    12-31-2024 9:26 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,3 +1,22 @@
-decimal_num = 10
-binary_num = bin(decimal_num)
-print(binary_num)
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        # Flag to check if any swapping is done in this iteration
+        swapped = False
+
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap the elements
+                swapped = True
+
+        # If no swapping is done in this iteration, the array is already sorted
+        if not swapped:
+            break
+
+    return arr
+
+# Test the bubble sort function
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = bubble_sort(arr)
+print("Sorted array is:", sorted_arr)
