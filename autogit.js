b8a438bb0f74dcd9405943a609d4ef0549b27fc9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:12:54 +0000

    12-31-2024 15:12 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,32 @@
-def find_majority_element(arr):
-    count = 0
-    candidate = None
+class Stack:
+    def __init__(self):
+        self.stack = []
 
-    for num in arr:
-        if count == 0:
-            candidate = num
-            count = 1
-        elif candidate == num:
-            count += 1
+    def isEmpty(self):
+        return len(self.stack) == 0
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if not self.isEmpty():
+            return self.stack.pop()
         else:
-            count -= 1
+            return None
 
-    return candidate
+    def peek(self):
+        if not self.isEmpty():
+            return self.stack[-1]
+        else:
+            return None
 
-# Example usage
-arr = [1, 2, 2, 2, 3, 2, 4, 2, 2]
-majority_element = find_majority_element(arr)
-print(f"The majority element is: {majority_element}")
+    def size(self):
+        return len(self.stack)
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+print(stack.peek())  # Output: 3
+print(stack.pop())   # Output: 3
+print(stack.size())  # Output: 2
