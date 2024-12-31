50391168c343edfa0166f4370fc2821402c4f701
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:56:07 +0000

    12-31-2024 16:56 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

---
+++ autogit.js
@@ -0,0 +1,10 @@
+import random
+
+# Specify the range
+start_range = 1
+end_range = 100
+
+# Generate a random number within the specified range
+random_number = random.randint(start_range, end_range)
+
+print("Random number within the range [{}, {}]: {}".format(start_range, end_range, random_number))
