60bd785e2a20da11e2eb4fe48a29945a980585ea
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 04:44:28 +0000

    12-31-2024 12:44 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,27 @@
-class Node:
-    def __init__(self, state, depth):
-        self.state = state
-        self.depth = depth
+def longest_common_substring(s1, s2):
+    m = len(s1)
+    n = len(s2)
 
-def depth_limited_search(root, depth_limit):
-    stack = [root]
+    # Create a table to store the lengths of the common substrings
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
 
-    while stack:
-        current_node = stack.pop()
-        
-        if current_node.depth < depth_limit:
-            # Generate child nodes for expansion
-            # For example, child_nodes = generate_child_nodes(current_node)
-            
-            # Add child nodes to the stack
-            # stack.extend(child_nodes)
-            
-            # Add a condition to check if the goal state is reached
-            # For example, if current_node.state == goal_state:
-            #     return True
-            
-    return False
+    max_length = 0
+    end_index = 0
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1
+                if dp[i][j] > max_length:
+                    max_length = dp[i][j]
+                    end_index = i
+            else:
+                dp[i][j] = 0
+
+    start_index = end_index - max_length
+    return s1[start_index:end_index]
+
+# Test the function
+s1 = "abcdefg"
+s2 = "xyabcz"
+print(longest_common_substring(s1, s2))  # Output: "abc"
