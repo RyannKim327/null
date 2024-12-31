d4118933f179670e304b314f3684f3ed602f96e8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:16:27 +0000

    12-31-2024 16:16 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,34 +1,3 @@
-def preprocess(pattern):
-    skip_table = {}
-    pattern_len = len(pattern)
-    
-    for i in range(pattern_len - 1):
-        skip_table[pattern[i]] = pattern_len - i - 1
-    
-    return skip_table
-
-def boyer_moore_horspool(text, pattern):
-    text_len = len(text)
-    pattern_len = len(pattern)
-    skip_table = preprocess(pattern)
-    
-    i = pattern_len - 1
-    while i < text_len:
-        j = pattern_len - 1
-        while text[i] == pattern[j]:
-            if j == 0:
-                return i
-            i -= 1
-            j -= 1
-        i += skip_table.get(text[i], pattern_len)
-    
-    return -1
-
-# Test the algorithm
-text = "hello world this is a test"
-pattern = "world"
-result = boyer_moore_horspool(text, pattern)
-if result != -1:
-    print("Pattern found at index:", result)
-else:
-    print("Pattern not found")
+decimal_number = 42
+binary_number = bin(decimal_number)
+print(binary_number)
