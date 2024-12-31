97028d4dd2396ca60a3dd8df1759982f894116a4
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 12:39:57 +0000

    12-31-2024 20:39 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,12 @@
-function findLongestIncreasingSubsequence(arr):
-    n = length of arr
-    dp = array of size n, initialized with 1's
-    
-    for i from 1 to n-1:
-        for j from 0 to i-1:
-            if arr[i] > arr[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
-    
-    max_length = maximum value in dp
-    
-    return max_length
+import schedule
+import time
+
+def job():
+    print("Hello, this is a scheduled task!")
+
+# Schedule the job to run every minute
+schedule.every().minute.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
