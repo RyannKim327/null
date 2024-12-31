24ac85d84fb54b57ef1ac1a1fe61058dce81ce8d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:26:31 +0000

    12-31-2024 15:26 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,50 +1,5 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Queue:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def enqueue(self, data):
-        new_node = Node(data)
-        if self.tail is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-
-    def dequeue(self):
-        if self.head is None:
-            return None
-        data = self.head.data
-        self.head = self.head.next
-        if self.head is None:
-            self.tail = None
-        return data
-
-    def is_empty(self):
-        return self.head is None
-
-    def print_queue(self):
-        current = self.head
-        while current:
-            print(current.data, end=' ')
-            current = current.next
-        print()
-
-# Example usage
-queue = Queue()
-queue.enqueue(1)
-queue.enqueue(2)
-queue.enqueue(3)
-
-queue.print_queue()  # Output: 1 2 3
-
-print(queue.dequeue())  # Output: 1
-print(queue.dequeue())  # Output: 2
-
-queue.print_queue()  # Output: 3
+function sumOfNodes(root):
+    if root is null:
+        return 0
+        
+    return root.value + sumOfNodes(root.left) + sumOfNodes(root.right)
