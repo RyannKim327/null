afe19643e9cae1b1ff4468ad5c5a3afef2b0c1c4
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 01:26:54 +0000

    12-31-2024 9:26 [Parang malabo na pala, di na ko aasa]

--- autogit.js
+++ autogit.js
@@ -1,23 +1,13 @@
-// Definition for singly-linked list
-class ListNode {
-public:
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
+def is_palindrome(s):
+    # Remove spaces and convert string to lowercase
+    s = s.replace(" ", "").lower()
+    
+    # Compare the string with its reverse
+    return s == s[::-1]
 
-ListNode* findMiddle(ListNode* head) {
-    if (head == NULL) {
-        return NULL;
-    }
-
-    ListNode *slow = head;
-    ListNode *fast = head;
-
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    return slow;
-}
+# Test the function
+input_string = "A man a plan a canal Panama"
+if is_palindrome(input_string):
+    print("The input string is a palindrome.")
+else:
+    print("The input string is not a palindrome.")
