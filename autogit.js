75a9f3fc75a62ba3159a33c4abb673fb7c94d9ed
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:26:38 +0000

    12-31-2024 15:26 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,18 +1,4 @@
-def dfs(graph, node, visited):
-    if node not in visited:
-        print(node)
-        visited.add(node)
-        for neighbor in graph[node]:
-            dfs(graph, neighbor, visited)
-if __name__ == "__main__":
-    graph = {
-        'A': ['B', 'C'],
-        'B': ['D', 'E'],
-        'C': ['F'],
-        'D': [],
-        'E': ['F'],
-        'F': []
-    }
-
-    visited = set()
-    dfs(graph, 'A', visited)
+my_string = "Hello, World!"
+lowercase_string = my_string.lower()
+print(lowercase_string)
+hello, world!
