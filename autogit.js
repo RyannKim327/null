4672808f29838ea13e591510971dfa1269f25332
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:56:20 +0000

    12-31-2024 16:56 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,55 +1,7 @@
-def build_lps(pattern):
-    lps = [0] * len(pattern)
-    j = 0
-    i = 1
+def remove_vowels(input_string):
+    vowels = "AEIOUaeiou"
+    return "".join([char for char in input_string if char not in vowels])
 
-    while i < len(pattern):
-        if pattern[i] == pattern[j]:
-            j += 1
-            lps[i] = j
-            i += 1
-        else:
-            if j != 0:
-                j = lps[j-1]
-            else:
-                lps[i] = 0
-                i += 1
-
-    return lps
-
-def kmp_search(text, pattern):
-    n = len(text)
-    m = len(pattern)
-    
-    if m == 0:
-        return 0
-
-    lps = build_lps(pattern)
-
-    i = 0  # index for text[]
-    j = 0  # index for pattern[]
-    
-    while i < n:
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
-
-            if j == m:
-                return i - j
-        else:
-            if j != 0:
-                j = lps[j-1]
-            else:
-                i += 1
-
-    return -1
-
-# Test the KMP algorithm
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-
-index = kmp_search(text, pattern)
-if index != -1:
-    print("Pattern found at index:", index)
-else:
-    print("Pattern not found in the text.")
+input_string = "Hello, World!"
+result = remove_vowels(input_string)
+print(result)
