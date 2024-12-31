1726e670331c2a5240d81416ddf279a9381bc428
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 03:54:16 +0000

    12-31-2024 11:54 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.js
+++ autogit.js
@@ -1,24 +1,19 @@
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
+bool hasCycle(Node *head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
 
-    while left <= right:
-        mid = (left + right) // 2
+    Node *slow = head;
+    Node *fast = head;
 
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
 
-    return -1
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-target = 5
+        if (slow == fast) {
+            return true;
+        }
+    }
 
-result = binary_search(arr, target)
-
-if result != -1:
-    print("Element found at index:", result)
-else:
-    print("Element not found")
+    return false;
+}
