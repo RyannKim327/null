b13b3377adda0be420cf5629234274d1452ac75f
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 05:59:21 +0000

    12-31-2024 13:59 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,30 +1,37 @@
-struct Node {
-    int data;
-    Node** forward; // Pointers to the next nodes at different levels
-};
-class SkipList {
-private:
-    int maxLevels; // Maximum number of levels in the skip list
-    int level;
-    Node* header; // Header node of the skip list
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
 
-public:
-    SkipList(int maxLevels);
-    Node* createNode(int data, int level);
-    int randomLevel();
-    void insertElement(int data);
-    void deleteElement(int data);
-    void searchElement(int data);
-};
-SkipList::SkipList(int maxLevels) {
-    this->maxLevels = maxLevels;
-    this->level = 0;
-    
-    // Create the header node
-    header = createNode(-1, maxLevels);
-    
-    // Initialize the forward pointers of the header node to NULL
-    for (int i = 0; i < maxLevels; i++) {
-        header->forward[i] = NULL;
-    }
-}
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def bellman_ford(self, src):
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+
+        for _ in range(self.V - 1):
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                print("Graph contains negative weight cycle")
+                return
+
+        print("Vertex Distance from Source")
+        for i in range(self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+g = Graph(5)
+g.add_edge(0, 1, -1)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 3)
+g.add_edge(1, 3, 2)
+g.add_edge(1, 4, 2)
+g.add_edge(3, 2, 5)
+g.add_edge(3, 1, 1)
+g.add_edge(4, 3, -3)
+
+g.bellman_ford(0)
