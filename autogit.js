83a4da01724d364bf4a8ccd14892effdc7610b02
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 02:54:12 +0000

    12-31-2024 10:54 [Kahit anong gawin prii, sya pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,33 @@
-arr = [5, 3, 8, 1, 2]
-sorted_arr = sorted(arr)
-print(sorted_arr)
-arr = [5, 3, 8, 1, 2]
-arr.sort()
-print(arr)
-arr = [5, 3, 8, 1, 2]
-sorted_arr_desc = sorted(arr, reverse=True)
-print(sorted_arr_desc)
-arr = [5, 3, 8, 1, 2]
-arr.sort(reverse=True)
-print(arr)
+original_array = [1, 2, 3, 1, 2, 4, 5]
+unique_array = list(set(original_array))
+print(unique_array)
+let originalArray = [1, 2, 3, 1, 2, 4, 5];
+let uniqueArray = [...new Set(originalArray)];
+console.log(uniqueArray);
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer[] originalArray = {1, 2, 3, 1, 2, 4, 5};
+        List<Integer> uniqueList = new ArrayList<>(new HashSet<>(Arrays.asList(originalArray)));
+        System.out.println(uniqueList);
+    }
+}
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+int main() {
+    std::vector<int> originalArray = {1, 2, 3, 1, 2, 4, 5};
+    std::unordered_set<int> uniqueSet(originalArray.begin(), originalArray.end());
+    originalArray.assign(uniqueSet.begin(), uniqueSet.end());
+    
+    for (int num : originalArray) {
+        std::cout << num << " ";
+    }
+    
+    return 0;
+}
