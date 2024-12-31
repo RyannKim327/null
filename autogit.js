dbbf456fe6ea1fa967de07cf36752b94a8810ae8
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 02:08:55 +0000

    12-31-2024 10:8 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,61 +1,30 @@
-import random
-import math
-
 class Node:
-    def __init__(self, value, level):
-        self.value = value
-        self.forward = [None] * (level + 1)
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+        
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+            
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+        
+# Example usage:
+root = Node(5)
+insert(root, 3)
+insert(root, 8)
+insert(root, 1)
+insert(root, 4)
 
-class SkipList:
-    def __init__(self, max_level, p):
-        self.max_level = max_level
-        self.p = p
-        self.header = self.create_node(-1, max_level)
-        self.level = 0
-
-    def create_node(self, value, level):
-        return Node(value, level)
-
-    def random_level(self):
-        level = 0
-        while random.random() < self.p and level < self.max_level:
-            level += 1
-        return level
-
-    def insert(self, value):
-        update = [None] * (self.max_level + 1)
-        current = self.header
-
-        for i in range(self.level, -1, -1):
-            while current.forward[i] and current.forward[i].value < value:
-                current = current.forward[i]
-            update[i] = current
-
-        current = current.forward[0]
-
-        if current is None or current.value != value:
-            new_level = self.random_level()
-
-            if new_level > self.level:
-                for i in range(self.level + 1, new_level + 1):
-                    update[i] = self.header
-                self.level = new_level
-
-            new_node = self.create_node(value, new_level)
-
-            for i in range(new_level + 1):
-                new_node.forward[i] = update[i].forward[i]
-                update[i].forward[i] = new_node
-
-    def search(self, value):
-        current = self.header
-
-        for i in range(self.level, -1, -1):
-            while current.forward[i] and current.forward[i].value < value:
-                current = current.forward[i]
-
-        current = current.forward[0]
-
-        if current and current.value == value:
-            return True
-        return False
+inorder_traversal(root)
