b5b4d5b14cf8de92dc20c5abb03eeb524a74d8d9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:36:11 +0000

    12-31-2024 15:36 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,4 +1,16 @@
-my_string = "Hello, World!"
-lowercase_string = my_string.lower()
-print(lowercase_string)
-hello, world!
+import math
+
+def largest_prime_factor(n):
+    largest_prime = 2
+    
+    while n > largest_prime:
+        if n % largest_prime == 0:
+            n //= largest_prime
+        else:
+            largest_prime += 1
+    
+    return largest_prime
+
+number = 56
+result = largest_prime_factor(number)
+print(f"The largest prime factor of {number} is: {result}")
