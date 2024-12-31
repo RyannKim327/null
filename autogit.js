018b923fe8528e25b47580e29bae1ce9d35a78fc
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:56:11 +0000

    12-31-2024 16:56 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,34 @@
-import random
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+    
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+    
+    return merge(left_half, right_half)
 
-# Specify the range
-start_range = 1
-end_range = 100
+def merge(left, right):
+    result = []
+    i = j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+            
+    result.extend(left[i:])
+    result.extend(right[j:])
+    
+    return result
 
-# Generate a random number within the specified range
-random_number = random.randint(start_range, end_range)
-
-print("Random number within the range [{}, {}]: {}".format(start_range, end_range, random_number))
+# Example usage
+arr = [38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
