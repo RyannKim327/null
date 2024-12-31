c79e5eba6ea41201be1e076d1904e1c9e356dc7b
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 02:54:05 +0000

    12-31-2024 10:54 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,19 +1,12 @@
-graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
-
-visited = set()
-
-def dfs(node):
-    if node not in visited:
-        print(node, end=' ')
-        visited.add(node)
-        for neighbor in graph[node]:
-            dfs(neighbor)
-
-dfs('A')
+arr = [5, 3, 8, 1, 2]
+sorted_arr = sorted(arr)
+print(sorted_arr)
+arr = [5, 3, 8, 1, 2]
+arr.sort()
+print(arr)
+arr = [5, 3, 8, 1, 2]
+sorted_arr_desc = sorted(arr, reverse=True)
+print(sorted_arr_desc)
+arr = [5, 3, 8, 1, 2]
+arr.sort(reverse=True)
+print(arr)
