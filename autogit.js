50f1fc12986e78d9faadce283641c1af49656925
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 03:35:35 +0000

    12-31-2024 11:35 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,41 +1,14 @@
-import heapq
+import math
 
-class BeamSearch:
-    def __init__(self, k):
-        self.k = k
+number = 5
+factorial = math.factorial(number)
+print(f'The factorial of {number} is: {factorial}')
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-    def beam_search(self, initial_state):
-        beam = [initial_state]
-
-        while True:
-            next_beam = []
-            for state in beam:
-                next_states = expand_states(state)
-                next_beam.extend(next_states)
-
-            beam = heapq.nlargest(self.k, next_beam, key=lambda x: x.score)
-
-            if stopping_criteria(beam):
-                break
-
-        best_state = max(beam, key=lambda x: x.score)
-        return best_state
-
-def expand_states(state):
-    # Generate next states based on the current state
-    pass
-
-def stopping_criteria(beam):
-    # Define the stopping criterion
-    pass
-
-# Define your search state class
-class SearchState:
-    def __init__(self, sequence, score):
-        self.sequence = sequence
-        self.score = score
-
-# Example usage
-initial_state = SearchState(sequence=[start_token], score=0)
-beam_search = BeamSearch(k=3)
-best_state = beam_search.beam_search(initial_state)
+number = 5
+result = factorial(number)
+print(f'The factorial of {number} is: {result}')
