4e2b0f5051a9f06ccfc2245e7bbe3bad347c4925
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 06:55:57 +0000

    12-31-2024 14:55 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,23 @@
-import math
+import heapq
 
-number = 5
-factorial = math.factorial(number)
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+        self.index = 0
 
-print(f"The factorial of {number} is {factorial}")
+    def push(self, priority, item):
+        heapq.heappush(self.heap, (priority, self.index, item))
+        self.index += 1
+
+    def pop(self):
+        return heapq.heappop(self.heap)[-1]
+
+# Example usage
+pq = PriorityQueue()
+pq.push(3, 'task3')
+pq.push(1, 'task1')
+pq.push(2, 'task2')
+
+print(pq.pop())  # Output: task1
+print(pq.pop())  # Output: task2
+print(pq.pop())  # Output: task3
