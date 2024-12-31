8cfa0d243de5d1baf1e7d5af63da8e919b7f4b75
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 02:09:42 +0000

    12-31-2024 10:9 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,5 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
+import random
 
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def insert(self, word):
-        current = self.root
-        for char in word:
-            if char not in current.children:
-                current.children[char] = TrieNode()
-            current = current.children[char]
-        current.is_end_of_word = True
-
-    def search(self, word):
-        current = self.root
-        for char in word:
-            if char not in current.children:
-                return False
-            current = current.children[char]
-        return current.is_end_of_word
-
-# Example usage
-trie = Trie()
-trie.insert("apple")
-trie.insert("app")
-print(trie.search("apple"))  # Output: True
-print(trie.search("apples")) # Output: False
+# Generate a random number between 1 and 100
+random_number = random.randint(1, 100)
+print(random_number)
