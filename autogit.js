b7f7470dbeb2e58ab5c3018c9af613b33726d8a7
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:54:24 +0000

    12-31-2024 15:54 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,33 @@
-def reverse_words(string):
-    words = string.split()
-    reversed_words = " ".join(reversed(words))
-    return reversed_words
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-# Example usage
-input_string = "Hello World, how are you?"
-reversed_string = reverse_words(input_string)
-print(reversed_string)  # Output: "you? are how World, Hello"
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        print(root.val)
+        inorder_traversal(root.right)
+
+# Usage
+root = None
+root = insert(root, 50)
+insert(root, 30)
+insert(root, 20)
+insert(root, 40)
+insert(root, 70)
+insert(root, 60)
+insert(root, 80)
+
+inorder_traversal(root)
