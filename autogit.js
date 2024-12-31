c9b13f4220d43d2da5a23611ee6ca2c76db7a5e9
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 06:55:00 +0000

    12-31-2024 14:54 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,126 @@
-arr = [1, 2, 2, 3, 4, 4, 5]
+RED = True
+BLACK = False
 
-# Convert the array to a set to remove duplicates
-arr_without_duplicates = list(set(arr))
+class Node:
+    def __init__(self, key, color=RED):
+        self.key = key
+        self.color = color
+        self.left = None
+        self.right = None
+        self.parent = None
 
-print(arr_without_duplicates)
+class RedBlackTree:
+    def __init__(self):
+        self.nil = Node(None, BLACK)
+        self.root = self.nil
+
+    def insert(self, key):
+        new_node = Node(key)
+        new_node.left = self.nil
+        new_node.right = self.nil
+
+        parent = None
+        current = self.root
+        while current != self.nil:
+            parent = current
+            if key < current.key:
+                current = current.left
+            else:
+                current = current.right
+
+        new_node.parent = parent
+        if parent is None:
+            self.root = new_node
+        elif key < parent.key:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+        new_node.color = RED
+        self._fix_insert(new_node)
+
+    def _fix_insert(self, node):
+        while node.parent.color == RED:
+            if node.parent == node.parent.parent.left:
+                uncle = node.parent.parent.right
+                if uncle.color == RED:
+                    node.parent.color = BLACK
+                    uncle.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.right:
+                        node = node.parent
+                        self._left_rotate(node)
+
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._right_rotate(node.parent.parent)
+            else:
+                uncle = node.parent.parent.left
+                if uncle.color == RED:
+                    node.parent.color = BLACK
+                    uncle.color = BLACK
+                    node.parent.parent.color = RED
+                    node = node.parent.parent
+                else:
+                    if node == node.parent.left:
+                        node = node.parent
+                        self._right_rotate(node)
+
+                    node.parent.color = BLACK
+                    node.parent.parent.color = RED
+                    self._left_rotate(node.parent.parent)
+
+        self.root.color = BLACK
+
+    def _left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+
+        if y.left != self.nil:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+
+        y.left = x
+        x.parent = y
+
+    def _right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+
+        if y.right != self.nil:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent is None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+
+        y.right = x
+        x.parent = y
+
+    def inorder_traversal(self, node):
+        if node != self.nil:
+            self.inorder_traversal(node.left)
+            print(node.key)
+            self.inorder_traversal(node.right)
+
+rbt = RedBlackTree()
+rbt.insert(10)
+rbt.insert(20)
+rbt.insert(30)
+rbt.insert(15)
+rbt.insert(5)
+
+rbt.inorder_traversal(rbt.root)
