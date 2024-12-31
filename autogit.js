2e96008cfc34a810479e5edfa66ac813de92e82d
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 03:15:59 +0000

    12-31-2024 11:15 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,44 +1,19 @@
-def fibonacci_search(arr, x):
-    n = len(arr)
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
     
-    fib_m_minus_2 = 0
-    fib_m_minus_1 = 1
-    fib_m = fib_m_minus_1 + fib_m_minus_2
+    min_len = min(len(s) for s in strs)
+    prefix = ""
     
-    while fib_m < n:
-        fib_m_minus_2 = fib_m_minus_1
-        fib_m_minus_1 = fib_m
-        fib_m = fib_m_minus_1 + fib_m_minus_2
-    
-    offset = -1
-    
-    while fib_m > 1:
-        i = min(offset + fib_m_minus_2, n - 1)
-        
-        if arr[i] < x:
-            fib_m = fib_m_minus_1
-            fib_m_minus_1 = fib_m_minus_2
-            fib_m_minus_2 = fib_m - fib_m_minus_1
-            offset = i
-        elif arr[i] > x:
-            fib_m = fib_m_minus_2
-            fib_m_minus_1 = fib_m_minus_1 - fib_m_minus_2
-            fib_m_minus_2 = fib_m - fib_m_minus_1
+    for i in range(min_len):
+        char = strs[0][i]
+        if all(s[i] == char for s in strs):
+            prefix += char
         else:
-            return i
+            break
     
-    if fib_m_minus_1 and arr[offset+1] == x:
-        return offset+1
-    
-    return -1
+    return prefix
 
-# Test the function
-arr = [10, 22, 35, 40, 45, 50, 80, 82, 85, 90, 100]
-x = 85
-
-result = fibonacci_search(arr, x)
-
-if result != -1:
-    print("Element found at index", result)
-else:
-    print("Element not found")
+# Example usage
+strs = ["flower", "flow", "flight"]
+print(longestCommonPrefix(strs))  # Output: "fl"
