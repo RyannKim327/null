8124bb7a36144c45db9b7f99b8f4d94b6adfc545
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 01:27:52 +0000

    12-31-2024 9:27 [Bakit na mimiss ko pa rin sya]

--- autogit.js
+++ autogit.js
@@ -1,6 +1,17 @@
-const url = 'https://jsonplaceholder.typicode.com/posts';
+def longest_increasing_subsequence(arr):
+    if not arr:
+        return 0
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
+    n = len(arr)
+    lis = [1] * n
+
+    for i in range(1, n):
+        for j in range(0, i):
+            if arr[i] > arr[j]:
+                lis[i] = max(lis[i], lis[j] + 1)
+
+    return max(lis)
+
+# Example usage
+arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print("Length of the Longest Increasing Subsequence is", longest_increasing_subsequence(arr))
