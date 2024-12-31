521e231de02fdfd4663cf3457b7bba2ac2b1e749
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 15:20:30 +0000

    12-31-2024 23:20 [Sana ako pa rin]

--- autogit.js
+++ autogit.js
@@ -1,12 +1,22 @@
-import schedule
-import time
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
 
-def job():
-    print("Hello, this is a scheduled task!")
+    visited.add(start)
+    print(start)
 
-# Schedule the job to run every minute
-schedule.every().minute.do(job)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Example graph
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+dfs(graph, 'A')
