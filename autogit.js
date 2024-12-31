e7c029f27f1be16aea1c437fcaa74eac7ebd4975
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 08:35:55 +0000

    12-31-2024 16:35 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,9 @@
-def reverse_words_in_string(input_str):
-    words = input_str.split()  # Split the string into words
-    reversed_words = words[::-1]  # Reverse the order of words
-    reversed_str = ' '.join(reversed_words)  # Join the reversed words
-    return reversed_str
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
-# Test the function
-input_str = "Hello World! This is a test."
-reversed_str = reverse_words_in_string(input_str)
-print(reversed_str)  # Output: "test. a is This World! Hello"
+# Function call to calculate the factorial of 5
+result = factorial(5)
+print(result)  # Output: 120
