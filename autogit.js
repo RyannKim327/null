ece255dffebef215bd50d0f9d202bafde940fb8c
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 09:40:20 +0000

    12-31-2024 17:40 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,38 +1,4 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+my_array = [1, 2, 3, 4, 5]
+reversed_array = my_array[::-1]
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def reverse(self):
-        prev = None
-        current = self.head
-        while current is not None:
-            next = current.next
-            current.next = prev
-            prev = current
-            current = next
-        self.head = prev
-
-    def printList(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" ")
-            temp = temp.next
-
-# Example usage
-llist = LinkedList()
-llist.head = Node(1)
-llist.head.next = Node(2)
-llist.head.next.next = Node(3)
-
-print("Original list:")
-llist.printList()
-
-llist.reverse()
-
-print("\nReversed list:")
-llist.printList()
+print(reversed_array)
