8a67b4f197a117fca1b80bbd593af9ab2a5e8365
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 04:53:55 +0000

    12-31-2024 12:53 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.js
+++ autogit.js
@@ -1,35 +1,28 @@
-def tarjans_algorithm(graph):
-    index = 0
-    stack = []
-    indices = {}
-    low_links = {}
-    result = []
+def longest_common_substring(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    # Initialize a 2D table to store the lengths of common substrings
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    # Variable to store the length of the longest common substring
+    max_len = 0
+    
+    # Variable to store the ending index of the longest common substring
+    end_index = 0
+    
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if str1[i - 1] == str2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    end_index = i - 1
+    
+    start_index = end_index - max_len + 1
+    return str1[start_index:end_index+1]
 
-    def dfs(node):
-        nonlocal index
-        indices[node] = index
-        low_links[node] = index
-        index += 1
-        stack.append(node)
-
-        for neighbor in graph[node]:
-            if neighbor not in indices:
-                dfs(neighbor)
-                low_links[node] = min(low_links[node], low_links[neighbor])
-            elif neighbor in stack:
-                low_links[node] = min(low_links[node], indices[neighbor])
-
-        if indices[node] == low_links[node]:
-            component = []
-            while True:
-                top = stack.pop()
-                component.append(top)
-                if top == node:
-                    break
-            result.append(component)
-
-    for node in graph:
-        if node not in indices:
-            dfs(node)
-
-    return result
+# Test the function
+str1 = "abcdefg"
+str2 = "xyzabcde"
+print(longest_common_substring(str1, str2))
