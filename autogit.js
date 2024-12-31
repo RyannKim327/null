7f20c7482207d34b6bbb618ca37559aee0ed0654
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 06:46:03 +0000

    12-31-2024 14:46 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,45 +1,6 @@
-def build_lps(pattern):
-    m = len(pattern)
-    lps = [0] * m
-    length = 0
+arr = [1, 2, 2, 3, 4, 4, 5]
 
-    i = 1
-    while i < m:
-        if pattern[i] == pattern[length]:
-            length += 1
-            lps[i] = length
-            i += 1
-        else:
-            if length != 0:
-                length = lps[length - 1]
-            else:
-                lps[i] = 0
-                i += 1
+# Convert the array to a set to remove duplicates
+arr_without_duplicates = list(set(arr))
 
-    return lps
-
-def kmp_search(text, pattern):
-    n = len(text)
-    m = len(pattern)
-    lps = build_lps(pattern)
-
-    i = 0  # index for text[]
-    j = 0  # index for pattern[]
-
-    while i < n:
-        if pattern[j] == text[i]:
-            i += 1
-            j += 1
-
-        if j == m:
-            print("Pattern found at index", i - j)
-            j = lps[j - 1]
-        elif i < n and pattern[j] != text[i]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-kmp_search(text, pattern)
+print(arr_without_duplicates)
