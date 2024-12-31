f497f1c3b01c57782d3b6e8ecce601d058cff900
RyannKim327 <rksesgundo123@gmail.com>
2024-12-31 07:36:24 +0000

    12-31-2024 15:36 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,8 +1,37 @@
-# Given array with duplicate elements
-arr = [1, 2, 3, 4, 2, 3, 1, 5]
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
 
-# Convert array to a set to remove duplicates
-arr = list(set(arr))
+    queue = [[item] for item in arr]
 
-print(arr)
-[1, 2, 3, 4, 5]
+    while len(queue) > 1:
+        first = queue.pop(0)
+        second = queue.pop(0)
+
+        merged = []
+        i = j = 0
+
+        while i < len(first) and j < len(second):
+            if first[i] < second[j]:
+                merged.append(first[i])
+                i += 1
+            else:
+                merged.append(second[j])
+                j += 1
+
+        while i < len(first):
+            merged.append(first[i])
+            i += 1
+
+        while j < len(second):
+            merged.append(second[j])
+            j += 1
+
+        queue.append(merged)
+
+    return queue[0]
+
+# Test the merge sort algorithm
+arr = [38, 27, 43, 3, 9, 82, 10]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)
