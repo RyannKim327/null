177e44efe874017cb1d17efd9fef455a57b3358d
RyannKim327 <rksesgundo123@gmail.com>
2025-01-01 18:27:35 +0000

    1-2-2025 2:27 [Paano kaya kapag di na sya?]

--- autogit.js
+++ autogit.js
@@ -1,11 +1,9 @@
-int findLength(Node* head) {
-    int length = 0;
-    Node* current = head;
-    
-    while (current != nullptr) {
-        length++;
-        current = current->next;
-    }
-    
-    return length;
-}
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = 5
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
