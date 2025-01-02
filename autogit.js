842a70b9b961c41fa5b22691ed8b8797b3c1500f
RyannKim327 <rksesgundo123@gmail.com>
2025-01-02 15:21:09 +0000

    1-2-2025 23:21 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,33 +1,18 @@
-// Define a structure for a node in the binary tree
+/* Node structure for the linked list */
 struct Node {
     int data;
-    Node* left;
-    Node* right;
+    struct Node* next;
 };
 
-// Function to find the height of the tree
-int height(Node* root) {
-    if (root == nullptr) {
-        return 0;
+/* Function to find the length of a linked list */
+int findLinkedListLength(struct Node* head) {
+    int length = 0;
+    struct Node* current = head;
+
+    while (current != NULL) {
+        length++;
+        current = current->next;
     }
 
-    int left_height = height(root->left);
-    int right_height = height(root->right);
-
-    return 1 + max(left_height, right_height);
-}
-
-// Function to find the diameter of the tree
-int diameter(Node* root) {
-    if (root == nullptr) {
-        return 0;
-    }
-
-    int left_height = height(root->left);
-    int right_height = height(root->right);
-
-    int left_diameter = diameter(root->left);
-    int right_diameter = diameter(root->right);
-
-    return max(left_height + right_height + 1, max(left_diameter, right_diameter));
+    return length;
 }
