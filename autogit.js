43247c3b1e1c72eb9d33318cdf6afc41ffe9a612
RyannKim327 <rksesgundo123@gmail.com>
2025-01-02 06:29:27 +0000

    1-2-2025 14:29 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,32 +1,9 @@
-def depth_limited_search(node, goal, depth_limit):
-    if node == goal:
-        return [node]
-    if depth_limit == 0:
-        return None
-    if node is None:
-        return None
+original_array = [1, 2, 2, 3, 4, 4, 5]
+unique_array = []
 
-    for child in expand(node):
-        result = depth_limited_search(child, goal, depth_limit - 1)
-        if result is not None:
-            return [node] + result
+for element in original_array:
+    if element not in unique_array:
+        unique_array.append(element)
 
-    return None
-
-def expand(node):
-    # Function to generate child nodes of a given node
-    # Implement this function according to your specific problem
-
-    return []
-
-# Example usage
-start_node = "A"
-goal_node = "D"
-depth_limit = 3
-
-result_path = depth_limited_search(start_node, goal_node, depth_limit)
-
-if result_path is not None:
-    print("Path found:", result_path)
-else:
-    print("Path not found within depth limit")
+print(unique_array)
+[1, 2, 3, 4, 5]
