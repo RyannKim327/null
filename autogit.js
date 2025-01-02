901cf4bb283962c49fe0bc123a0a7b5a93d9f3af
RyannKim327 <rksesgundo123@gmail.com>
2025-01-02 12:40:36 +0000

    1-2-2025 20:40 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,52 +1,7 @@
-#include <iostream>
+def remove_vowels(input_string):
+    vowels = 'aeiouAEIOU'
+    return ''.join([char for char in input_string if char not in vowels])
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
-ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    if (!headA || !headB) {
-        return nullptr;
-    }
-    
-    ListNode *pA = headA;
-    ListNode *pB = headB;
-    
-    while (pA != pB) {
-        pA = pA ? pA->next : headB;
-        pB = pB ? pB->next : headA;
-    }
-    
-    return pA;
-}
-
-// Helper function to create linked list for testing
-ListNode* createList(std::initializer_list<int> values) {
-    ListNode dummy(0);
-    ListNode* tail = &dummy;
-    
-    for (int val : values) {
-        tail->next = new ListNode(val);
-        tail = tail->next;
-    }
-    
-    return dummy.next;
-}
-
-int main() {
-    ListNode* listA = createList({4, 1, 8, 4, 5});
-    ListNode* listB = createList({5, 6, 1, 8, 4, 5});
-
-    listA->next->next->next->next->next = listB->next->next->next;
-
-    ListNode* intersectionNode = getIntersectionNode(listA, listB);
-    if (intersectionNode) {
-        std::cout << "Intersection at node with value: " << intersectionNode->val << "\n";
-    } else {
-        std::cout << "No intersection found\n";
-    }
-
-    return 0;
-}
+input_string = "Hello, World!"
+output_string = remove_vowels(input_string)
+print(output_string)  # Output: "Hll, Wrld!"
