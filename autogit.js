77092e52dc9a8a40d67636ee16f07216c432f259
RyannKim327 <rksesgundo123@gmail.com>
2025-01-02 09:21:39 +0000

    1-2-2025 17:21 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,9 +1,52 @@
-original_array = [1, 2, 2, 3, 4, 4, 5]
-unique_array = []
+#include <iostream>
 
-for element in original_array:
-    if element not in unique_array:
-        unique_array.append(element)
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
-print(unique_array)
-[1, 2, 3, 4, 5]
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    if (!headA || !headB) {
+        return nullptr;
+    }
+    
+    ListNode *pA = headA;
+    ListNode *pB = headB;
+    
+    while (pA != pB) {
+        pA = pA ? pA->next : headB;
+        pB = pB ? pB->next : headA;
+    }
+    
+    return pA;
+}
+
+// Helper function to create linked list for testing
+ListNode* createList(std::initializer_list<int> values) {
+    ListNode dummy(0);
+    ListNode* tail = &dummy;
+    
+    for (int val : values) {
+        tail->next = new ListNode(val);
+        tail = tail->next;
+    }
+    
+    return dummy.next;
+}
+
+int main() {
+    ListNode* listA = createList({4, 1, 8, 4, 5});
+    ListNode* listB = createList({5, 6, 1, 8, 4, 5});
+
+    listA->next->next->next->next->next = listB->next->next->next;
+
+    ListNode* intersectionNode = getIntersectionNode(listA, listB);
+    if (intersectionNode) {
+        std::cout << "Intersection at node with value: " << intersectionNode->val << "\n";
+    } else {
+        std::cout << "No intersection found\n";
+    }
+
+    return 0;
+}
