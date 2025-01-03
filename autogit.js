1134f45c3d006ffa74d89373c454c256d9aea30c
RyannKim327 <rksesgundo123@gmail.com>
2025-01-03 18:28:14 +0000

    1-4-2025 2:28 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.js
+++ autogit.js
@@ -1,29 +1,13 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-def maxDepth(root):
-    if root is None:
-        return 0
+def findMedianSortedArrays(nums1, nums2):
+    nums = sorted(nums1 + nums2)
+    n = len(nums)
     
-    left_depth = maxDepth(root.left)
-    right_depth = maxDepth(root.right)
-    
-    return 1 + max(left_depth, right_depth)
+    if n % 2 == 0:
+        return (nums[n // 2 - 1] + nums[n // 2]) / 2
+    else:
+        return nums[n // 2]
 
-# Example of creating a binary tree
-#        1
-#       / \
-#      2   3
-#     / \     
-#    4   5
-
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.right = TreeNode(3)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-
-print(maxDepth(root))  # Output: 3
+# Test the function
+nums1 = [1, 3]
+nums2 = [2]
+print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
