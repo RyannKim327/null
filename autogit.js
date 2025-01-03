600c3e9b8270a6cd6e7c1e354d5c62a26e92a2b2
RyannKim327 <rksesgundo123@gmail.com>
2025-01-03 18:28:18 +0000

    1-4-2025 2:28 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,13 +1,34 @@
-def findMedianSortedArrays(nums1, nums2):
-    nums = sorted(nums1 + nums2)
-    n = len(nums)
-    
-    if n % 2 == 0:
-        return (nums[n // 2 - 1] + nums[n // 2]) / 2
-    else:
-        return nums[n // 2]
+import android.os.AsyncTask
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
-# Test the function
-nums1 = [1, 3]
-nums2 = [2]
-print(findMedianSortedArrays(nums1, nums2))  # Output: 2.0
+class ConnectAsyncTask : AsyncTask<String, Void, String>() {
+
+    override fun doInBackground(vararg urls: String): String {
+        val url = URL(urls[0])
+        val urlConnection = url.openConnection() as HttpURLConnection
+
+        try {
+            val inputStream = BufferedInputStream(urlConnection.inputStream)
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val response = StringBuilder()
+            var line: String?
+
+            while (bufferedReader.readLine().also { line = it } != null) {
+                response.append(line)
+            }
+
+            return response.toString()
+        } finally {
+            urlConnection.disconnect()
+        }
+    }
+
+    override fun onPostExecute(result: String) {
+        // Handle the result here
+        // This method runs on the UI thread
+    }
+}
