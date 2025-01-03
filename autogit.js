f25f1ad238881b1102f50c721bb7c23ffb287dde
RyannKim327 <rksesgundo123@gmail.com>
2025-01-03 15:21:28 +0000

    1-3-2025 23:21 [Hindi naman ako naghahabol, hindi rin naman sya nagpapahabol]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,33 @@
-def isPalindrome(s):
-    start, end = 0, len(s) - 1
-    while start < end:
-        while start < end and not s[start].isalnum():
-            start += 1
-        while start < end and not s[end].isalnum():
-            end -= 1
-        if s[start].lower() != s[end].lower():
-            return False
-        start += 1
-        end -= 1
-    return True
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Test the function
-s = "A man, a plan, a canal: Panama"
-print(isPalindrome(s)) # Output: True
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" ")
+            current = current.next
+
+# Create a linked list
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+
+# Display the linked list
+llist.display()
