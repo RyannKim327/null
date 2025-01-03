cc4295831880fb4a5ad5308652ce8e3c3f95df5e
RyannKim327 <rksesgundo123@gmail.com>
2025-01-03 09:21:21 +0000

    1-3-2025 17:21 []

--- autogit.js
+++ autogit.js
@@ -1,34 +1,6 @@
-import heapq
+import math
 
-def dijkstra(graph, start):
-    distances = {node: float('infinity') for node in graph}
-    distances[start] = 0
-    priority_queue = [(0, start)]
+number = 5
+factorial = math.factorial(number)
 
-    while priority_queue:
-        current_distance, current_node = heapq.heappop(priority_queue)
-
-        if current_distance > distances[current_node]:
-            continue
-
-        for neighbor, weight in graph[current_node].items():
-            distance = current_distance + weight
-
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor))
-
-    return distances
-
-# Example graph
-graph = {
-    'A': {'B': 1, 'C': 4},
-    'B': {'A': 1, 'C': 2, 'D': 5},
-    'C': {'A': 4, 'B': 2, 'D': 1},
-    'D': {'B': 5, 'C': 1}
-}
-
-start_node = 'A'
-shortest_distances = dijkstra(graph, start_node)
-
-print(shortest_distances)
+print(f'The factorial of {number} is: {factorial}')
