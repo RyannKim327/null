077c50e7a1094ef526820a9ead0f23aa0ca301d4
RyannKim327 <rksesgundo123@gmail.com>
2025-01-03 21:18:34 +0000

    1-4-2025 5:18 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,31 +1,10 @@
-def precompute_bad_char_table(pattern):
-    bad_char_table = {}
-    for i in range(len(pattern) - 1):
-        bad_char_table[pattern[i]] = len(pattern) - 1 - i
-    return bad_char_table
+def check_anagram(str1, str2):
+    return sorted(str1) == sorted(str2)
 
-def boyer_moore_horspool(text, pattern):
-    m = len(pattern)
-    n = len(text)
-    bad_char_table = precompute_bad_char_table(pattern)
-    
-    i = m - 1
-    while i < n:
-        k = 0
-        while k < m and pattern[m - 1 - k] == text[i - k]:
-            k += 1
-        if k == m:
-            return i - m + 1
-        if text[i] in bad_char_table:
-            i += bad_char_table[text[i]]
-        else:
-            i += m
-    return -1
+str1 = "listen"
+str2 = "silent"
 
-text = "ABAAABCD"
-pattern = "ABC"
-index = boyer_moore_horspool(text, pattern)
-if index != -1:
-    print("Pattern found at index:", index)
+if check_anagram(str1, str2):
+    print("The strings are anagrams")
 else:
-    print("Pattern not found")
+    print("The strings are not anagrams")
