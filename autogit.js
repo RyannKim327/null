e69abf135c5d7af483149b0834aedba5782d61c8
RyannKim327 <rksesgundo123@gmail.com>
2025-01-04 18:26:54 +0000

    1-5-2025 2:26 [Wala, talaga lang attractive sya saken]

--- autogit.js
+++ autogit.js
@@ -1,16 +1,11 @@
-def reverse_words_in_string(input_string):
-    # split the string into words
-    words = input_string.split()
+def count_occurrences(string, char):
+    count = 0
+    for c in string:
+        if c == char:
+            count += 1
+    return count
 
-    # reverse the order of words
-    reversed_words = words[::-1]
-
-    # join the words back together
-    reversed_string = ' '.join(reversed_words)
-
-    return reversed_string
-
-# Example usage
-input_string = "Hello World Welcome"
-reversed_string = reverse_words_in_string(input_string)
-print(reversed_string)
+string = "Hello, World!"
+char = "o"
+result = count_occurrences(string, char)
+print(f"The character '{char}' occurs {result} times in the string.")
