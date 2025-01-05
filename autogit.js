cbe5d8ae5d27fad3f1478417efd0b31c27f9903d
RyannKim327 <rksesgundo123@gmail.com>
2025-01-05 09:19:36 +0000

    1-5-2025 17:19 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,47 +1,7 @@
-def compute_lps_array(pattern):
-    lps = [0] * len(pattern)
-    j = 0
-    i = 1
-    while i < len(pattern):
-        if pattern[i] == pattern[j]:
-            j += 1
-            lps[i] = j
-            i += 1
-        else:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                lps[i] = 0
-                i += 1
-    return lps
+string1 = "Hello"
+string2 = "World"
 
-def kmp_search(text, pattern):
-    lps = compute_lps_array(pattern)
-    i = 0
-    j = 0
-    matches = []
+result = string1 + string2
 
-    while i < len(text):
-        if text[i] == pattern[j]:
-            i += 1
-            j += 1
-        if j == len(pattern):
-            matches.append(i - j)
-            j = lps[j - 1]
-        elif i < len(text) and text[i] != pattern[j]:
-            if j != 0:
-                j = lps[j - 1]
-            else:
-                i += 1
-
-    return matches
-
-# Test the KMP search algorithm
-text = "ABABDABACDABABCABAB"
-pattern = "ABABCABAB"
-matches = kmp_search(text, pattern)
-
-if matches:
-    print("Pattern found at index(es):", matches)
-else:
-    print("Pattern not found in the text.")
+print(result)
+HelloWorld
