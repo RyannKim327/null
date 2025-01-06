ef4ca3f54241b0b113620d98d21fc1401ea9cb9d
RyannKim327 <rksesgundo123@gmail.com>
2025-01-06 12:42:42 +0000

    1-6-2025 20:42 [Nugagawen kapag miss ko na sya?]

--- autogit.js
+++ autogit.js
@@ -1,28 +1,22 @@
-def heapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+# Graph representation using adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
 
-    if left < n and arr[i] < arr[left]:
-        largest = left
+visited = set()
 
-    if right < n and arr[largest] < arr[right]:
-        largest = right
+def dfs(node):
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(neighbor)
 
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        heapify(arr, n, largest)
-
-def heap_sort(arr):
-    n = len(arr)
-
-    for i in range(n // 2 - 1, -1, -1):
-        heapify(arr, n, i)
-
-    for i in range(n - 1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]
-        heapify(arr, i, 0)
-
-arr = [12, 11, 13, 5, 6, 7]
-heap_sort(arr)
-print("Sorted array is", arr)
+# Starting node for depth-first search
+start_node = 'A'
+dfs(start_node)
