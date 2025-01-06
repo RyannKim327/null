b4cc501453253c1f1b94d0c6a95f9874e0069694
RyannKim327 <rksesgundo123@gmail.com>
2025-01-06 06:30:49 +0000

    1-6-2025 14:30 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,10 +1,37 @@
-def is_palindrome(s):
-    s = s.lower().replace(' ', '')  # Convert to lowercase and remove spaces
-    return s == s[::-1]  # Check if the string is equal to its reverse
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-# Test the function
-input_str = "A man a plan a canal Panama"
-if is_palindrome(input_str):
-    print("Palindrome")
-else:
-    print("Not a palindrome")
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def find_middle_element(self):
+        slow_pointer = self.head
+        fast_pointer = self.head
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+        return slow_pointer.data
+
+# Test the code
+llist = LinkedList()
+llist.append(1)
+llist.append(2)
+llist.append(3)
+llist.append(4)
+llist.append(5)
+
+middle_element = llist.find_middle_element()
+print("Middle Element:", middle_element)
