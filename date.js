3ffda18cd1951950001355bfd0ae1b1067dd8ff8
Master Piece in a Piece of Paper <weryses19@gmail.com>
2025-01-09 11:30:06 +0800

    Update date.js

--- date.js
+++ date.js
@@ -1,7 +1,10 @@
 module.exports = (timezone) => {
-	const a = new Date()
-	const b = new Date(a.toLocaleString('en-US', {
-		timeZone: timezone
-	}))
-	return new Date(a.getTime() - (a.getTime() - b.getTime()))
+	if(timezone){
+		const a = new Date()
+		const b = new Date(a.toLocaleString('en-US', {
+			timeZone: timezone
+		}))
+		return new Date(a.getTime() - (a.getTime() - b.getTime()))
+	}
+	return new Date()
 }
