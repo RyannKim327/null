17252533283cf96b8cf342727e1552128b0d9d8c
RyannKim327 <rksesgundo123@gmail.com>
2025-01-11 02:09:12 +0000

    1-11-2025 2:9 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.js
+++ autogit.js
@@ -1,189 +1,199 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct Node {
     int data;
-    struct Node* next;
+    struct Node* left;
+    struct Node* right;
 } Node;
 
-typedef struct Queue {
-    Node* front;
-    Node* rear;
-} Queue;
-
-void initQueue(Queue* q) {
-    q->front = q->rear = NULL;
-}
-
-void enqueue(Queue* q, int data) {
+Node* createNode(int data) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->data = data;
-    node->next = NULL;
-    if (q->rear == NULL) {
-        q->front = q->rear = node;
-    } else {
-        q->rear->next = node;
-        q->rear = node;
-    }
+    node->left = node->right = NULL;
+    return node;
 }
 
-int dequeue(Queue* q) {
-    if (q->front == NULL) {
-        printf("Queue is empty\n");
-        return -1;
+Node* insertNode(Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
     }
-    int data = q->front->data;
-    Node* node = q->front;
-    q->front = q->front->next;
-    if (q->front == NULL) {
-        q->rear = NULL;
+
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
     }
-    free(node);
-    return data;
+
+    return root;
+}
+
+void inorderTraversal(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+
+    inorderTraversal(root->left);
+    printf("%d ", root->data);
+    inorderTraversal(root->right);
+}
+
+int main() {
+    Node* root = NULL;
+    root = insertNode(root, 5);
+    root = insertNode(root, 3);
+    root = insertNode(root, 7);
+    root = insertNode(root, 2);
+    root = insertNode(root, 4);
+    root = insertNode(root, 6);
+    root = insertNode(root, 8);
+
+    printf("Inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
 }
 #include <iostream>
 
 using namespace std;
 
-struct Node {
+class Node {
+public:
     int data;
-    Node* next;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
 };
 
-struct Queue {
-    Node* front;
-    Node* rear;
+class BinarySearchTree {
+private:
+    Node* root;
+
+public:
+    BinarySearchTree() {
+        root = nullptr;
+    }
+
+    void insertNode(int data) {
+        root = insertNodeRecursive(root, data);
+    }
+
+    Node* insertNodeRecursive(Node* node, int data) {
+        if (node == nullptr) {
+            return new Node(data);
+        }
+
+        if (data < node->data) {
+            node->left = insertNodeRecursive(node->left, data);
+        } else if (data > node->data) {
+            node->right = insertNodeRecursive(node->right, data);
+        }
+
+        return node;
+    }
+
+    void inorderTraversal() {
+        inorderTraversalRecursive(root);
+    }
+
+    void inorderTraversalRecursive(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        inorderTraversalRecursive(node->left);
+        cout << node->data << " ";
+        inorderTraversalRecursive(node->right);
+    }
 };
 
-void initQueue(Queue& q) {
-    q.front = q.rear = nullptr;
+int main() {
+    BinarySearchTree bst;
+
+    bst.insertNode(5);
+    bst.insertNode(3);
+    bst.insertNode(7);
+    bst.insertNode(2);
+    bst.insertNode(4);
+    bst.insertNode(6);
+    bst.insertNode(8);
+
+    cout << "Inorder traversal: ";
+    bst.inorderTraversal();
+    cout << endl;
+
+    return 0;
 }
+public class BinarySearchTree {
+    private Node root;
 
-void enqueue(Queue& q, int data) {
-    Node* node = new Node();
-    node->data = data;
-    node->next = nullptr;
-    if (q.rear == nullptr) {
-        q.front = q.rear = node;
-    } else {
-        q.rear->next = node;
-        q.rear = node;
-    }
-}
-
-int dequeue(Queue& q) {
-    if (q.front == nullptr) {
-        cout << "Queue is empty" << endl;
-        return -1;
-    }
-    int data = q.front->data;
-    Node* node = q.front;
-    q.front = q.front->next;
-    if (q.front == nullptr) {
-        q.rear = nullptr;
-    }
-    delete node;
-    return data;
-}
-public class Queue {
-    private Node front;
-    private Node rear;
-
-    public Queue() {
-        front = rear = null;
+    public BinarySearchTree() {
+        root = null;
     }
 
-    public void enqueue(int data) {
-        Node node = new Node(data);
-        if (rear == null) {
-            front = rear = node;
-        } else {
-            rear.next = node;
-            rear = node;
+    public void insertNode(int data) {
+        root = insertNodeRecursive(root, data);
+    }
+
+    private Node insertNodeRecursive(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
         }
+
+        if (data < node.data) {
+            node.left = insertNodeRecursive(node.left, data);
+        } else if (data > node.data) {
+            node.right = insertNodeRecursive(node.right, data);
+        }
+
+        return node;
     }
 
-    public int dequeue() {
-        if (front == null) {
-            System.out.println("Queue is empty");
-            return -1;
+    public void inorderTraversal() {
+        inorderTraversalRecursive(root);
+    }
+
+    private void inorderTraversalRecursive(Node node) {
+        if (node == null) {
+            return;
         }
-        int data = front.data;
-        Node node = front;
-        front = front.next;
-        if (front == null) {
-            rear = null;
-        }
-        return data;
+
+        inorderTraversalRecursive(node.left);
+        System.out.print(node.data + " ");
+        inorderTraversalRecursive(node.right);
     }
 
     private static class Node {
         int data;
-        Node next;
+        Node left;
+        Node right;
 
         Node(int data) {
             this.data = data;
-            this.next = null;
+            this.left = null;
+            this.right = null;
         }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+
+        bst.insertNode(5);
+        bst.insertNode(3);
+        bst.insertNode(7);
+        bst.insertNode(2);
+        bst.insertNode(4);
+        bst.insertNode(6);
+        bst.insertNode(8);
+
+        System.out.print("Inorder traversal: ");
+        bst.inorderTraversal();
+        System.out.println();
     }
 }
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Queue:
-    def __init__(self):
-        self.front = self.rear = None
-
-    def enqueue(self, data):
-        node = Node(data)
-        if self.rear is None:
-            self.front = self.rear = node
-        else:
-            self.rear.next = node
-            self.rear = node
-
-    def dequeue(self):
-        if self.front is None:
-            print("Queue is empty")
-            return -1
-        data = self.front.data
-        node = self.front
-        self.front = self.front.next
-        if self.front is None:
-            self.rear = None
-        return data
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
-
-class Queue {
-    constructor() {
-        this.front = this.rear = null;
-    }
-
-    enqueue(data) {
-        const node = new Node(data);
-        if (this.rear === null) {
-            this.front = this.rear = node;
-        } else {
-            this.rear.next = node;
-            this.rear = node;
-        }
-    }
-
-    dequeue() {
-        if (this.front === null) {
-            console.log("Queue is empty");
-            return -1;
-        }
-        const data = this.front.data;
-        const node = this.front;
-        this.front = this.front.next;
-        if (this.front === null) {
-            this.rear = null;
-        }
-        return
