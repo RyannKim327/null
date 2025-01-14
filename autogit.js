61186018fe3f01f5036697e592f04b09e5603994
RyannKim327 <rksesgundo123@gmail.com>
2025-01-14 00:33:09 +0000

    1-14-2025 0:33 [Hindi naman ako naghahabol, hindi rin naman sya nagpapahabol]

---
+++ autogit.js
@@ -0,0 +1,11 @@
+const reversedString = str.split("").reverse().join("");
+reversed_string = my_string[::-1]
+String reversedString = new StringBuilder(myString).reverse().toString();
+string reversedString = new string(myString.Reverse().ToArray());
+std::string reversedString = myString;
+std::reverse(reversedString.begin(), reversedString.end());
+reversed_string = my_string.reverse
+let reversedString = String(myString.reversed())
+$reversedString = strrev($myString);
+reversedString := reverse(myString)
+reversed_string <- paste(rev(strsplit(my_string, "")[[1]]), collapse = "")
