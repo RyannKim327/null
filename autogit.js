bcf589cd33c44ca0ae5a93e872e1a192db871bce
RyannKim327 <rksesgundo123@gmail.com>
2025-01-14 11:01:23 +0000

    1-14-2025 11:0 [Pwede pa sa late game mga gar]

--- autogit.js
+++ autogit.js
@@ -1,1 +1,16 @@
-May error sa API
+import axios from 'axios';
+
+const apiKey = 'YOUR_API_KEY_HERE';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+async function getWeather(city) {
+  try {
+    const response = await axios.get(`${apiUrl}?q=${city}&appid=${apiKey}`);
+    const data = response.data;
+    console.log(`The weather in ${city} is ${data.weather[0].main} with a temperature of ${data.main.temp}°C`);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWeather('New York');
