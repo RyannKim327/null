20f67e45e51dc05e0e18844eeab1988d8d6ef193
RyannKim327 <rksesgundo123@gmail.com>
2025-01-14 05:01:41 +0000

    1-14-2025 5:1 []

--- autogit.js
+++ autogit.js
@@ -1,1 +1,64 @@
-May error sa API
+void bubbleSort(int arr[], int n) {
+  int i, j, temp;
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+void bubbleSort(int arr[], int n) {
+  int i, j, temp;
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+public static void bubbleSort(int[] arr) {
+  int n = arr.length;
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+def bubble_sort(arr):
+  n = len(arr)
+  for i in range(n - 1):
+    for j in range(n - i - 1):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+function bubbleSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+def bubble_sort(arr)
+  n = arr.length
+  (n - 1).times do |i|
+    (n - i - 1).times do |j|
+      if arr[j] > arr[j + 1]
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+      end
+    end
+  end
+end
