2cb6a772674be629213349900f338fa7300f7cc0
RyannKim327 <rksesgundo123@gmail.com>
2025-01-16 08:09:32 +0000

    1-16-2025 8:9 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,6 +1,1 @@
-const originalString = "Hello World";
-const noSpacesString = originalString.replace(/\s+/g, "");
-console.log(noSpacesString); // Output: "HelloWorld"
-const originalString = "Hello World";
-const noSpacesString = originalString.split(" ").join("");
-console.log(noSpacesString); // Output: "HelloWorld"
+// May error sa API
