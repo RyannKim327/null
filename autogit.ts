08657f7ae7350812fd67abcfcc9de59e1de837c4
RyannKim327 <rksesgundo123@gmail.com>
2025-01-16 20:01:28 +0000

    1-16-2025 20:0 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,16 @@
-// May error sa API
+let str = "Hello World";
+let substr = "World";
+
+if (str.includes(substr)) {
+  console.log("The string contains the substring!");
+} else {
+  console.log("The string does not contain the substring.");
+}
+let str = "Hello World";
+let substr = "World";
+
+if (str.indexOf(substr) !== -1) {
+  console.log("The string contains the substring!");
+} else {
+  console.log("The string does not contain the substring.");
+}
