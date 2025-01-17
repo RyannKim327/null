4f916b2558d758c7e94c9b4eccaffde73e6f167b
RyannKim327 <rksesgundo123@gmail.com>
2025-01-17 09:01:29 +0000

    1-17-2025 9:0 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+let str = "hello world";
+let reversedStr = str.split(" ").reverse().join(" ");
+console.log(reversedStr); // Output: "world hello"
+let str = "hello,world";
+let reversedStr = str.split(/\s*,/).reverse().join(" ");
+console.log(reversedStr); // Output: "world hello"
+let str = "   hello world   ";
+let reversedStr = str.trim().split(" ").reverse().join(" ");
+console.log(reversedStr); // Output: "world hello"
