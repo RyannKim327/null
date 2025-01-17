ef9be5b94dcef75c6d6d8e356fea67ae3d2989a6
RyannKim327 <rksesgundo123@gmail.com>
2025-01-17 23:01:53 +0000

    1-17-2025 23:1 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,8 +1,19 @@
-let str1 = "Hello";
-let str2 = "World";
-
-let result = str1 + " " + str2; // "Hello World"
-let str1 = "Hello";
-let str2 = "World";
-
-let result = `${str1} ${str2}`; // "Hello World"
+function largestPrimeFactor(n: number): number {
+  let maxFactor = 1;
+  let i = 2;
+  while (i * i <= n) {
+    if (n % i === 0) {
+      maxFactor = i;
+      while (n % i === 0) {
+        n /= i;
+      }
+    }
+    i++;
+  }
+  if (n > 1) {
+    maxFactor = n;
+  }
+  return maxFactor;
+}
+console.log(largestPrimeFactor(13195)); // Output: 29
+console.log(largestPrimeFactor(600851475143)); // Output: 6857
