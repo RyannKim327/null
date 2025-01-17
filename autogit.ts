dd03bcbc33ec150e0306ef18ac3e7832118d7ef5
RyannKim327 <rksesgundo123@gmail.com>
2025-01-17 23:01:10 +0000

    1-17-2025 23:0 [Parang malabo na pala, di na ko aasa]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,8 @@
-// May error sa API
+let str1 = "Hello";
+let str2 = "World";
+
+let result = str1 + " " + str2; // "Hello World"
+let str1 = "Hello";
+let str2 = "World";
+
+let result = `${str1} ${str2}`; // "Hello World"
