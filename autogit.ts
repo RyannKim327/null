126638c22623b5e14c7872d1c0a0873f4c54bd2a
RyannKim327 <rksesgundo123@gmail.com>
2025-01-18 10:00:53 +0000

    1-18-2025 10:0 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,40 @@
-// May error sa API
+function fibonacciSearch(arr: number[], target: number): number {
+  let fibMMm2 = 0; // (m-2)'th Fibonacci No.
+  let fibMMm1 = 1; // (m-1)'th Fibonacci No.
+  let fibM = fibMMm2 + fibMMm1; // m'th Fibonacci
+
+  while (fibM < arr.length) {
+    fibMMm2 = fibMMm1;
+    fibMMm1 = fibM;
+    fibM = fibMMm2 + fibMMm1;
+  }
+
+  let offset = -1;
+
+  while (fibM > 1) {
+    let i = Math.min(offset + fibMMm2, arr.length - 1);
+
+    if (arr[i] < target) {
+      fibM = fibMMm1;
+      fibMMm1 = fibMMm2;
+      fibMMm2 = fibM - fibMMm1;
+      offset = i;
+    } else if (arr[i] > target) {
+      fibM = fibMMm2;
+      fibMMm1 = fibMMm1 - fibMMm2;
+      fibMMm2 = fibM - fibMMm1;
+    } else {
+      return i;
+    }
+  }
+
+  if (fibMMm1 && arr[offset + 1] === target) {
+    return offset + 1;
+  }
+
+  return -1;
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const target = 5;
+const result = fibonacciSearch(arr, target);
+console.log(result); // Output: 4
