cc3679fb373b836e229da1e9d3250c8bce6d776d
RyannKim327 <rksesgundo123@gmail.com>
2025-01-18 08:00:43 +0000

    1-18-2025 8:0 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,1 @@
-const str = "hello world";
-const char = "o";
-const count = str.split(char).length - 1;
-
-console.log(`The character "${char}" occurs ${count} times in the string.`);
-const str = "hello world";
-const char = "o";
-const count = (str.match(new RegExp(char, "g")) || []).length;
-
-console.log(`The character "${char}" occurs ${count} times in the string.`);
+// May error sa API
