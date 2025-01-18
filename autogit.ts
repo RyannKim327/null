8ca4e415588d9772fcfb5b605ae90cddfe7f3222
RyannKim327 <rksesgundo123@gmail.com>
2025-01-18 04:01:08 +0000

    1-18-2025 4:0 [Nugagawen kapag miss ko na sya?]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,31 @@
-// May error sa API
+function isPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+function isPalindrome(s: string): boolean {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
