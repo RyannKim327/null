543ccb5e34879fec7eec7e68aabf7a9f08fd4d2f
RyannKim327 <rksesgundo123@gmail.com>
2025-01-19 00:36:52 +0000

    1-19-2025 0:36 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+function areAnagrams(str1: string, str2: string): boolean {
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+function areAnagrams(str1: string, str2: string): boolean {
+  return str1.localeCompare(str2, 'en', { sensitivity: 'base' }) === 0;
+}
+function areAnagrams(str1: string, str2: string): boolean {
+  return str1.split('').every(c => str2.includes(c)) && str2.split('').every(c => str1.includes(c));
+}
