b2eaee297eedb72c9a3fbd0bad8c263bde5049a1
RyannKim327 <rksesgundo123@gmail.com>
2025-01-19 15:00:52 +0000

    1-19-2025 15:0 [Ang cute nya, lalo na pag inis sya saken]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,37 @@
-// May error sa API
+function longestCommonSubsequence(str1: string, str2: string): string {
+  const m = str1.length;
+  const n = str2.length;
+  const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(0));
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  let lcs = '';
+  let i = m;
+  let j = n;
+
+  while (i > 0 && j > 0) {
+    if (str1[i - 1] === str2[j - 1]) {
+      lcs = str1[i - 1] + lcs;
+      i--;
+      j--;
+    } else if (dp[i - 1][j] > dp[i][j - 1]) {
+      i--;
+    } else {
+      j--;
+    }
+  }
+
+  return lcs;
+}
+const str1 = 'ABCBDAB';
+const str2 = 'BDCABA';
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: 'BCBA'
