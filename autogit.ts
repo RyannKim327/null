484942704ada6690a670f84f0934917111476802
RyannKim327 <rksesgundo123@gmail.com>
2025-01-19 03:12:13 +0000

    1-19-2025 3:12 [Bakit na mimiss ko pa rin sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,57 @@
-// May error sa API
+import * as cron from 'node-cron';
+
+interface Job {
+  id: string;
+  cronExpression: string;
+  task: () => void;
+}
+
+class CronJobManager {
+  private jobs: Job[] = [];
+
+  addJob(job: Job) {
+    this.jobs.push(job);
+    cron.schedule(job.cronExpression, job.task);
+    console.log(`Job ${job.id} added with cron expression ${job.cronExpression}`);
+  }
+
+  start() {
+    console.log('Cron job manager started');
+  }
+
+  stop() {
+    this.jobs.forEach((job) => {
+      cron.cancel(job.id);
+      console.log(`Job ${job.id} stopped`);
+    });
+    console.log('Cron job manager stopped');
+  }
+}
+
+const jobManager = new CronJobManager();
+
+// Add some sample jobs
+jobManager.addJob({
+  id: 'job1',
+  cronExpression: '0 0 12 * * *', // Run every day at 12:00 PM
+  task: () => {
+    console.log('Running job 1');
+    // Do some task here
+  },
+});
+
+jobManager.addJob({
+  id: 'job2',
+  cronExpression: '0 30 14 * * *', // Run every day at 2:30 PM
+  task: () => {
+    console.log('Running job 2');
+    // Do some task here
+  },
+});
+
+jobManager.start();
+
+// Stop the job manager after 10 minutes
+setTimeout(() => {
+  jobManager.stop();
+}, 600000);
