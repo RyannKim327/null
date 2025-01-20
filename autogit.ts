a452082f03705e6ad78b7713a408099d282c646c
RyannKim327 <rksesgundo123@gmail.com>
2025-01-20 09:01:04 +0000

    1-20-2025 9:1 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,22 @@
-// May error sa API
+function getStringLength(str: string): number {
+  let length = 0;
+  for (let i = 0; str[i] !== undefined; i++) {
+    length++;
+  }
+  return length;
+}
+function getStringLength(str: string): number {
+  let length = 0;
+  let i = 0;
+  while (str[i] !== undefined) {
+    length++;
+    i++;
+  }
+  return length;
+}
+function getStringLength(str: string, index: number = 0): number {
+  if (str[index] === undefined) {
+    return index;
+  }
+  return getStringLength(str, index + 1);
+}
