89fee56729e4f1f4853590ebb5ae790738cd3505
RyannKim327 <rksesgundo123@gmail.com>
2025-01-20 17:00:39 +0000

    1-20-2025 17:0 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,21 @@
-// May error sa API
+function_strlen(str: string): number {
+  let len = 0;
+  for (let i = 0; str[i] !== undefined; i++) {
+    len++;
+  }
+  return len;
+}
+function_strlen(str: string): number {
+  let len = 0;
+  let i = 0;
+  while (str[i] !== undefined) {
+    len++;
+    i++;
+  }
+  return len;
+}
+function_strlen(str: string): number {
+  let len = 0;
+  Array.prototype.forEach.call(str, () => len++);
+  return len;
+}
