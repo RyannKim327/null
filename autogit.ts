d6c8070d64baede11283a5f9dadef78e35eb2919
RyannKim327 <rksesgundo123@gmail.com>
2025-01-20 08:00:48 +0000

    1-20-2025 8:0 [Paano kaya kapag di na sya?]

--- autogit.ts
+++ autogit.ts
@@ -1,11 +1,1 @@
-const originalString = "Hello World";
-const reversedString = originalString.split(" ").reverse().join(" ");
-console.log(reversedString); // Output: "World Hello"
-const originalString = "Hello,World";
-const reversedString = originalString.split(/\s*,\s*/).reverse().join(" ");
-console.log(reversedString); // Output: "World Hello"
-const originalString = "Hello World";
-const words = originalString.split(" ");
-const reversedWords = [...words].reverse();
-const reversedString = reversedWords.join(" ");
-console.log(reversedString); // Output: "World Hello"
+// May error sa API
