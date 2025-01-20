6f9f0486081d43dc5995e4cbfc4b3f7313a1dbd7
RyannKim327 <rksesgundo123@gmail.com>
2025-01-20 20:01:01 +0000

    1-20-2025 20:0 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,64 @@
-// May error sa API
+interface Node<T> {
+  value: T;
+  children: Node<T>[];
+}
+
+function dfs<T>(root: Node<T>, callback: (node: Node<T>) => void): void {
+  const stack: Node<T>[] = [root];
+
+  while (stack.length > 0) {
+    const node = stack.pop();
+    callback(node);
+
+    for (const child of node.children) {
+      stack.push(child);
+    }
+  }
+}
+const nodeA = {
+  value: 'A',
+  children: [
+    {
+      value: 'B',
+      children: [
+        {
+          value: 'D',
+          children: [],
+        },
+        {
+          value: 'E',
+          children: [],
+        },
+      ],
+    },
+    {
+      value: 'C',
+      children: [
+        {
+          value: 'F',
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
+dfs(nodeA, (node) => {
+  console.log(node.value);
+});
+A
+B
+D
+E
+C
+F
+function dfsRecursive<T>(node: Node<T>, callback: (node: Node<T>) => void): void {
+  callback(node);
+
+  for (const child of node.children) {
+    dfsRecursive(child, callback);
+  }
+}
+dfsRecursive(nodeA, (node) => {
+  console.log(node.value);
+});
