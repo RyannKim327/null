1d12f1d7bc7baa1ff4b142e37103215f261aa3e4
RyannKim327 <rksesgundo123@gmail.com>
2025-01-21 08:00:48 +0000

    1-21-2025 8:0 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,15 @@
-// May error sa API
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+const mean = numbers.reduce((acc, current) => acc + current, 0) / numbers.length;
+
+console.log(mean); // Output: 3
+const numbers: number[] = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (const num of numbers) {
+  sum += num;
+}
+
+const mean = sum / numbers.length;
+
+console.log(mean); // Output: 3
