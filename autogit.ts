72c06728d3b7b614f073835e30a7bd854c4f36ee
RyannKim327 <rksesgundo123@gmail.com>
2025-01-21 06:10:19 +0000

    1-21-2025 6:10 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,6 @@
-// May error sa API
+let arr = [1, 2, 3, 4, 5];
+arr.reverse();
+console.log(arr); // [5, 4, 3, 2, 1]
+let arr = [1, 2, 3, 4, 5];
+let reversedArr = [...arr].slice().reverse();
+console.log(reversedArr); // [5, 4, 3, 2, 1]
