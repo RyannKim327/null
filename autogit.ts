d8e01f9293cb759a47a6da4961fcbd0ff379f2c8
RyannKim327 <rksesgundo123@gmail.com>
2025-01-21 23:00:57 +0000

    1-21-2025 23:0 [Paano kaya kapag di na sya?]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+const decimalNumber = 12;
+const binaryNumber = decimalNumber.toString(2);
+console.log(binaryNumber); // Output: "1100"
+const decimalNumber = 12;
+const binaryNumber = (decimalNumber as number).toString(2);
+console.log(binaryNumber); // Output: "1100"
+const decimalNumber = 12;
+const binaryNumber = decimalNumber.toString(2).padStart(8, '0');
+console.log(binaryNumber); // Output: "00001100"
