1c110830ce86d58be42459047b1fa2fdb7b26614
RyannKim327 <rksesgundo123@gmail.com>
2025-01-21 03:09:44 +0000

    1-21-2025 3:9 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,14 +1,1 @@
-function calculateTriangleArea(base: number, height: number): number {
-  return (base * height) / 2;
-}
-const base = 5;
-const height = 6;
-const area = calculateTriangleArea(base, height);
-console.log(`The area of the triangle is ${area}`);
-The area of the triangle is 15
-function calculateTriangleArea(base: number, height: number): number | undefined {
-  if (base <= 0 || height <= 0) {
-    return undefined; // or throw an error, depending on your needs
-  }
-  return (base * height) / 2;
-}
+// May error sa API
