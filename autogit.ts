c0f09a0843190bec12c3a4d295f3c8f9cdd61f2c
RyannKim327 <rksesgundo123@gmail.com>
2025-01-21 17:01:38 +0000

    1-21-2025 17:1 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,16 +1,1 @@
-function isPalindrome(s: string): boolean {
-  let left = 0;
-  let right = s.length - 1;
-
-  while (left < right) {
-    if (s[left] !== s[right]) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-
-  return true;
-}
-console.log(isPalindrome("madam")); // true
-console.log(isPalindrome("hello")); // false
+// May error sa API
