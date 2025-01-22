566bf4b61083dcbb04aa5e2a029266444f679b61
RyannKim327 <rksesgundo123@gmail.com>
2025-01-22 10:00:58 +0000

    1-22-2025 10:0 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,6 +1,1 @@
-const originalString = "Hello World";
-const noSpacesString = originalString.replace(/\s+/g, "");
-console.log(noSpacesString); // Output: "HelloWorld"
-const originalString = "   Hello World   ";
-const trimmedString = originalString.trim();
-console.log(trimmedString); // Output: "Hello World"
+// May error sa API
