27b8f85b5777fb374cfe87ff16cd4ce83b0d5fa5
RyannKim327 <rksesgundo123@gmail.com>
2025-01-23 17:01:17 +0000

    1-23-2025 17:1 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,19 @@
-// May error sa API
+const arr = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArr = Array.from(new Set(arr));
+console.log(uniqueArr); // [1, 2, 3, 4, 5]
+const arr = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArr = arr.filter((value, index, self) => self.indexOf(value) === index);
+console.log(uniqueArr); // [1, 2, 3, 4, 5]
+const arr = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArr = arr.reduce((acc, current) => {
+  if (!acc.includes(current)) {
+    acc.push(current);
+  }
+  return acc;
+}, []);
+console.log(uniqueArr); // [1, 2, 3, 4, 5]
+import _ from 'lodash';
+
+const arr = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArr = _.uniq(arr);
+console.log(uniqueArr); // [1, 2, 3, 4, 5]
