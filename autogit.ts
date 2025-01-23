8888fdb3a4d4c80fafa66c916418bf7d4c62d4ca
RyannKim327 <rksesgundo123@gmail.com>
2025-01-23 00:34:09 +0000

    1-23-2025 0:34 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,15 +1,1 @@
-function bubbleSort(arr: number[]): number[] {
-  let swapped: boolean;
-  do {
-    swapped = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-        swapped = true;
-      }
-    }
-  } while (swapped);
-  return arr;
-}
-const arr = [5, 2, 8, 3, 1, 6, 4];
-console.log(bubbleSort(arr)); // [1, 2, 3, 4, 5, 6, 8]
+// May error sa API
