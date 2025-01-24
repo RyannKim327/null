b145a1212f042a64a72d147a02c81f3043d1c686
RyannKim327 <rksesgundo123@gmail.com>
2025-01-24 19:00:48 +0000

    1-24-2025 19:0 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,26 @@
-// May error sa API
+import axios, { AxiosResponse } from 'axios';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const apiEndpoint = 'https://jsonplaceholder.typicode.com/users';
+
+async function getUsers(): Promise<User[]> {
+  try {
+    const response: AxiosResponse<User[]> = await axios.get(apiEndpoint);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+async function main() {
+  const users: User[] = await getUsers();
+  console.log(users);
+}
+
+main();
