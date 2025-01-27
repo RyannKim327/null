c7ad61b205d7949b6e88bb5c5680cb14350ac96d
RyannKim327 <rksesgundo123@gmail.com>
2025-01-27 08:11:01 +0000

    1-27-2025 8:10 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,6 @@
-// May error sa API
+const reversedString = str.split("").reverse().join("");
+const originalString = "hello";
+const reversedString = originalString.split("").reverse().join("");
+console.log(reversedString); // Output: "olleh"
+const reversedString = [...str].reverse().join("");
+const reversedString = str.split("").reduce((acc, char) => char + acc, "");
