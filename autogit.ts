058f855d313fd68c832f8d305fc1b7d075a9b8d3
RyannKim327 <rksesgundo123@gmail.com>
2025-01-27 10:01:15 +0000

    1-27-2025 10:1 [Wala, talaga lang attractive sya saken]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,8 @@
-// May error sa API
+let str = "   hello world   ";
+str = str.trim(); // outputs: "hello world"
+let str = "   hello world   ";
+str = str.replace(/\s+/g, ''); // outputs: "helloworld"
+let str = "   hello world   ";
+str = str.replace(/^\s+|\s+$/g, ''); // outputs: "hello world"
+let str = "   hello world   ";
+str = str.normalize('NFKC'); // outputs: "hello world"
