921926774ab324819dc53cfcd23bd7c4df800474
RyannKim327 <rksesgundo123@gmail.com>
2025-01-28 02:05:50 +0000

    1-28-2025 2:5 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,16 @@
-// May error sa API
+function isPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
