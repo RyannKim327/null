f21e1c8e7be68b64760e1e9f2806dec3e2a3d6c4
RyannKim327 <rksesgundo123@gmail.com>
2025-01-28 23:00:54 +0000

    1-28-2025 23:0 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,25 @@
-// May error sa API
+function isPalindrome(str: string): boolean {
+  return str === str.split('').reverse().join('');
+}
+function isPalindrome(str: string): boolean {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+
+  return true;
+}
+function isPalindrome(str: string): boolean {
+  return /^(\w+)\1$/.test(str);
+}
+const str = 'madam';
+console.log(isPalindrome(str)); // true
+
+const str2 = 'hello';
+console.log(isPalindrome(str2)); // false
