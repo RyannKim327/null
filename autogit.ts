be08e40facb799a9ddbf6f89812a83eae166f7fe
RyannKim327 <rksesgundo123@gmail.com>
2025-01-28 05:01:11 +0000

    1-28-2025 5:0 [Nugagawen kapag miss ko na sya?]

--- autogit.ts
+++ autogit.ts
@@ -1,6 +1,1 @@
-const originalString = "Hello World";
-const noVowelsString = originalString.replace(/[aeiou]/gi, "");
-console.log(noVowelsString); // Output: "Hll Wrld"
-const originalString = "Hello World";
-const noVowelsString = originalString.replace(/[aeiou]/gi, () => "");
-console.log(noVowelsString); // Output: "Hll Wrld"
+// May error sa API
