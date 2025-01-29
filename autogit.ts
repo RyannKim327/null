236d17d28061c3d5ae8dcb81e70b17dbfb5eeba2
RyannKim327 <rksesgundo123@gmail.com>
2025-01-29 08:09:51 +0000

    1-29-2025 8:9 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,6 @@
-// May error sa API
+function isPalindrome(str: string): boolean {
+  return str === str.split('').reverse().join('');
+}
+console.log(isPalindrome('madam')); // true
+console.log(isPalindrome('hello')); // false
+const isPalindrome = (str: string) => str === [...str].reverse().join('');
