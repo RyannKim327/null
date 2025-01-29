599f64f0ccbd11acecb4b3186d4482b05cad596a
RyannKim327 <rksesgundo123@gmail.com>
2025-01-29 21:00:48 +0000

    1-29-2025 21:0 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,18 @@
-// May error sa API
+let arr = [1, 2, 3, 4, 5];
+let index = arr.indexOf(3);
+if (index !== -1) {
+  arr.splice(index, 1);
+}
+console.log(arr); // [1, 2, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+arr = arr.filter(x => x !== 3);
+console.log(arr); // [1, 2, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+let index = arr.findIndex(x => x === 3);
+if (index !== -1) {
+  arr.splice(index, 1);
+}
+console.log(arr); // [1, 2, 4, 5]
+let arr = [1, 2, 3, 4, 5];
+arr = [...arr.filter(x => x !== 3)];
+console.log(arr); // [1, 2, 4, 5]
