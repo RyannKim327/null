603eae0980bff38977d7ae5435ea8fdbac23f25b
RyannKim327 <rksesgundo123@gmail.com>
2025-01-29 22:01:02 +0000

    1-29-2025 22:0 [Hindi naman ako naghahabol, hindi rin naman sya nagpapahabol]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,14 @@
-// May error sa API
+function isPrime(n: number): boolean {
+    if (n <= 1) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime(25));  // False
+console.log(isPrime(23));  // True
