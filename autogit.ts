d2727e26931b16f7c2a88fd5cfe4dc6025c85c4a
RyannKim327 <rksesgundo123@gmail.com>
2025-01-29 08:10:06 +0000

    1-29-2025 8:9 []

--- autogit.ts
+++ autogit.ts
@@ -1,6 +1,1 @@
-function isPalindrome(str: string): boolean {
-  return str === str.split('').reverse().join('');
-}
-console.log(isPalindrome('madam')); // true
-console.log(isPalindrome('hello')); // false
-const isPalindrome = (str: string) => str === [...str].reverse().join('');
+// May error sa API
