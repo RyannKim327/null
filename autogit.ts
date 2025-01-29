dd2d5a50ac2dfadcfca7d2226a1edaeb3401f457
RyannKim327 <rksesgundo123@gmail.com>
2025-01-29 20:00:43 +0000

    1-29-2025 20:0 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,28 @@
-// May error sa API
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+
+const uniqueNumbers = Array.from(new Set(numbers));
+console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+
+const uniqueNumbers = numbers.filter((item, index, array) => {
+  return array.indexOf(item) === index;
+});
+console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+
+const uniqueNumbers = numbers.reduce((acc, current) => {
+  if (!acc.includes(current)) {
+    acc.push(current);
+  }
+  return acc;
+}, []);
+console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
+const numbers = [1, 2, 2, 3, 4, 4, 5];
+
+const uniqueNumbers = [];
+numbers.forEach((number) => {
+  if (uniqueNumbers.lastIndexOf(number) === -1) {
+    uniqueNumbers.push(number);
+  }
+});
+console.log(uniqueNumbers); // [1, 2, 3, 4, 5]
