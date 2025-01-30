36dcf8d91fc850899e96e5fbccd879c1f718a764
RyannKim327 <rksesgundo123@gmail.com>
2025-01-30 00:33:37 +0000

    1-30-2025 0:33 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,11 @@
-// May error sa API
+function countWordOccurrences(str: string, word: string): number {
+  return str.split(word).length - 1;
+}
+const str = "hello hello world hello";
+const word = "hello";
+const count = countWordOccurrences(str, word);
+console.log(count); // Output: 3
+function countWordOccurrences(str: string, word: string): number {
+  const regex = new RegExp(`\\b${word}\\b`, 'gi');
+  return (str.match(regex) || []).length;
+}
