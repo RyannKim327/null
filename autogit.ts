bf1f6bd78397cdcac4831589cfe41db990760d3a
RyannKim327 <rksesgundo123@gmail.com>
2025-01-30 23:00:57 +0000

    1-30-2025 23:0 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,23 @@
-function selectionSort(arr
+class Node {
+    data: number;
+    next: Node | null;
+
+    constructor(data: number) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function reverseList(head: Node): Node {
+    let previous: Node | null = null;
+    let current: Node = head;
+
+    while (current !== null) {
+        const next: Node = current.next!;
+        current.next = previous;
+        previous = current;
+        current = next;
+    }
+
+    return previous!;
+}
