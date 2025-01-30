eff33bebe281b2a4348e7279f83c9c921753cd43
RyannKim327 <rksesgundo123@gmail.com>
2025-01-30 21:01:02 +0000

    1-30-2025 21:1 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.ts
+++ autogit.ts
@@ -1,8 +1,1 @@
-function randomNumber(min: number, max: number): number {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const randomValue = randomNumber(1, 10); // generates a random number between 1 and 10
-console.log(randomValue);
-function randomNumber(min: number, max: number): number {
-  return Math.random() * (max - min) + min;
-}
+// May error sa API
