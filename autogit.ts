93c4c9190b523d431982836a95ae6dd8015c5322
RyannKim327 <rksesgundo123@gmail.com>
2025-01-31 00:34:07 +0000

    1-31-2025 0:34 [Wala, talaga lang attractive sya saken]

--- autogit.ts
+++
@@ -1,8 +0,0 @@
-function calculateFactorial(number: number): number {
-    let result = 1;
-    for (let i = 1; i <= number; i++) {
-        result *= i;
-    }
-    return result;
-}
-console.log(calculateFactorial(5)); // Outputs: 120
