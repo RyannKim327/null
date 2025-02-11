1d94255c5949d4b75a9992a5867268c72c9ddb05
RyannKim327 <rksesgundo123@gmail.com>
2025-02-11 11:01:12 +0000

    2-11-2025 11:0 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,78 +1,127 @@
-class Node<T> {
-    value: T;
-    next: Node<T> | null;
+class BTreeNode {
+    keys: number[];
+    children: BTreeNode[];
+    isLeaf: boolean;
+    t: number; // Minimum degree (defines the range for number of keys)
 
-    constructor(value: T) {
-        this.value = value;
-        this.next = null;
+    constructor(t: number, isLeaf: boolean) {
+        this.t = t;
+        this.isLeaf = isLeaf;
+        this.keys = [];
+        this.children = [];
     }
-}
-class LinkedList<T> {
-    private head: Node<T> | null = null;
-    private tail: Node<T> | null = null;
-    private size: number = 0;
 
-    // Add a new node at the end of the list
-    add(value: T): void {
-        const newNode = new Node(value);
-        if (this.head === null) {
-            this.head = newNode;
-            this.tail = newNode;
+    // Function to traverse all nodes in a subtree rooted with this node
+    traverse() {
+        let i: number;
+        for (i = 0; i < this.keys.length; i++) {
+            // If this is not a leaf, traverse the child before printing the key
+            if (!this.isLeaf) {
+                this.children[i].traverse();
+            }
+            console.log(this.keys[i]);
+        }
+
+        // Print the subtree rooted with the last child
+        if (!this.isLeaf) {
+            this.children[i].traverse();
+        }
+    }
+
+    // Function to search a key in the subtree rooted with this node
+    search(key: number): BTreeNode | null {
+        let i = 0;
+        while (i < this.keys.length && key > this.keys[i]) {
+            i++;
+        }
+
+        // If the found key is equal to the key, return this node
+        if (i < this.keys.length && this.keys[i] === key) {
+            return this;
+        }
+
+        // If this node is a leaf node, then the key is not present
+        if (this.isLeaf) {
+            return null;
+        }
+
+        // Go to the appropriate child
+        return this.children[i].search(key);
+    }
+
+    // Function to insert a new key in the subtree rooted with this node
+    insertNonFull(key: number) {
+        let i = this.keys.length - 1;
+
+        // If this is a leaf node
+        if (this.isLeaf) {
+            // Find the location of new key to be inserted
+            while (i >= 0 && key < this.keys[i]) {
+                i--;
+            }
+            // Insert the new key at found location
+            this.keys.splice(i + 1, 0, key);
         } else {
-            if (this.tail) {
-                this.tail.next = newNode;
-                this.tail = newNode;
+            // Find the child which is going to have the new key
+            while (i >= 0 && key < this.keys[i]) {
+                i--;
+            }
+            // Check if the found child is full
+            if (this.children[i + 1].keys.length === 2 * this.t - 1) {
+                // If the child is full, then split it
+                this.splitChild(i + 1);
+                // After split, the middle key of child goes up and
+                // the child is split into two. See which of the two
+                // is going to have the new key
+                if (key > this.keys[i + 1]) {
+                    i++;
+                }
+            }
+            this.children[i + 1].insertNonFull(key);
+        }
+    }
+
+    // Function to split the child of this node. `i` is index of the child
+    // to be split. The child will be split into two and a new key will be
+    // promoted to this node.
+    splitChild(i: number) {
+        const t = this.t;
+        const y = this.children[i];
+        const z = new BTreeNode(t, y.isLeaf);
+
+        // Copy the last t-1 keys of y to z
+        for (let j = 0; j < t - 1; j++) {
+            z.keys.push(y.keys[j + t]);
+        }
+
+        // Copy the last t children of y to z
+        if (!y.isLeaf) {
+            for (let j = 0; j < t; j++) {
+                z.children.push(y.children[j + t]);
             }
         }
-        this.size++;
-    }
 
-    // Remove a node from the list by value
-    remove(value: T): boolean {
-        if (this.head === null) return false;
+        // Reduce the number of keys in y
+        y.keys.length = t - 1;
 
-        if (this.head.value === value) {
-            this.head = this.head.next;
-            this.size--;
-            return true;
-        }
+        // Since this node is going to have a new child,
+        // create space for the new child
+        this.children.splice(i + 1, 0, z);
 
-        let current = this.head;
-        while (current.next !== null) {
-            if (current.next.value === value) {
-                current.next = current.next.next;
-                this.size--;
-                return true;
-            }
-            current = current.next;
-        }
-        return false;
-    }
-
-    // Get the size of the linked list
-    getSize(): number {
-        return this.size;
-    }
-
-    // Print the linked list
-    print(): void {
-        let current = this.head;
-        let result = '';
-        while (current !== null) {
-            result += current.value + (current.next ? ' -> ' : '');
-            current = current.next;
-        }
-        console.log(result);
+        // A key of y will move to this node. Find location of
+        // new key and move all greater keys one space ahead
+        this.keys.splice(i, 0, y.keys.pop()!);
     }
 }
-const myList = new LinkedList<number>();
-myList.add(10);
-myList.add(20);
-myList.add(30);
-myList.print(); // Output: 10 -> 20 -> 30
 
-console.log("Size of linked list:", myList.getSize()); // Output: Size of linked list: 3
+class BTree {
+    root: BTreeNode | null;
+    t: number; // Minimum degree
 
-myList.remove(20);
-myList.print(); // Output: 10 -> 30
-console.log("Size of linked list:", myList.getSize()); // Output: Size of linked list: 2
+    constructor(t: number) {
+        this.root = null;
+        this.t = t;
+    }
+
+    // Function to traverse the tree
+    traverse
