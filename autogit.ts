bbba4e96962b64e7e2a9bbf644972bbbf02d8d0a
RyannKim327 <rksesgundo123@gmail.com>
2025-02-12 19:01:52 +0000

    2-12-2025 19:1 [Nugagawen kapag miss ko na sya?]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,8 @@
-function reverseWords(input: string): string {
-    return input.split(' ')
-                .reverse()
-                .join(' ');
+function decimalToBinary(decimal: number): string {
+    return decimal.toString(2);
 }
 
 // Example usage:
-const originalString = "Hello world from TypeScript";
-const reversedString = reverseWords(originalString);
-console.log(reversedString); // Output: "TypeScript from world Hello"
+const decimalNumber = 42;
+const binaryString = decimalToBinary(decimalNumber);
+console.log(`The binary representation of ${decimalNumber} is ${binaryString}.`);
