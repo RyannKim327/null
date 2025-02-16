5845d3e46b86f2a32aba1cb31e7fe022f97c2343
RyannKim327 <rksesgundo123@gmail.com>
2025-02-16 22:00:58 +0000

    2-16-2025 22:0 [Wala, talaga lang attractive sya saken]

--- autogit.ts
+++ autogit.ts
@@ -1,48 +1,10 @@
-class Node {
-    value: any;
-    next: Node | null;
+let string1: string = "Hello, ";
+let string2: string = "world!";
+let concatenated: string = string1 + string2;
 
-    constructor(value: any) {
-        this.value = value;
-        this.next = null;
-    }
-}
+console.log(concatenated); // Output: Hello, world!
+let string1: string = "Hello, ";
+let string2: string = "world!";
+let concatenated: string = `${string1}${string2}`;
 
-class LinkedList {
-    head: Node | null;
-
-    constructor() {
-        this.head = null;
-    }
-
-    append(value: any) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-
-    length(): number {
-        let count = 0;
-        let current = this.head;
-        while (current) {
-            count++;
-            current = current.next;
-        }
-        return count;
-    }
-}
-
-// Example usage:
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-
-console.log(list.length()); // Output: 3
+console.log(concatenated); // Output: Hello, world!
