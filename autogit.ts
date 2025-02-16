393bb19f34702c95ad54a1963f374ee5377e341c
RyannKim327 <rksesgundo123@gmail.com>
2025-02-16 10:00:44 +0000

    2-16-2025 10:0 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,52 +1,11 @@
-class Node {
-    value: number;
-    next: Node | null;
-
-    constructor(value: number) {
-        this.value = value;
-        this.next = null;
-    }
+let str: string = "123";
+let num: number = parseInt(str, 10); // The second argument is the radix (base)
+let str: string = "123";
+let num: number = +str;
+let str: string = "123";
+let num: number = Number(str);
+let str: string = "123.45";
+let num: number = Math.floor(Number(str));
+if (!isNaN(num)) {
+    // num is a valid number
 }
-
-class LinkedList {
-    head: Node | null;
-
-    constructor() {
-        this.head = null;
-    }
-
-    // Method to add a new node to the list
-    add(value: number) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Method to find the length of the linked list
-    length(): number {
-        let count = 0;
-        let current = this.head;
-
-        while (current) {
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
-}
-
-// Example usage:
-const list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(3);
-
-console.log(list.length()); // Output: 3
