46a3b1b01538d712c68cc8be965dd2b0376e153e
RyannKim327 <rksesgundo123@gmail.com>
2025-02-17 05:00:54 +0000

    2-17-2025 5:0 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.ts
+++ autogit.ts
@@ -1,136 +1,22 @@
-enum Color {
-    RED = 'RED',
-    BLACK = 'BLACK',
+function findCommonElements(arr1: number[], arr2: number[]): number[] {
+    return arr1.filter(value => arr2.includes(value));
 }
 
-class Node<T> {
-    public  T;
-    public color: Color;
-    public left: Node<T> | null = null;
-    public right: Node<T> | null = null;
-    public parent: Node<T> | null = null;
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
 
-    constructor( T) {
-        this.data = data;
-        this.color = Color.RED; // New nodes are red
-    }
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
+function findCommonElements(arr1: number[], arr2: number[]): number[] {
+    const set1 = new Set(arr1);
+    const set2 = new Set(arr2);
+    
+    const commonElements = [...set1].filter(value => set2.has(value));
+    return commonElements;
 }
 
-class RedBlackTree<T> {
-    private root: Node<T> | null = null;
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [4, 5, 6, 7, 8];
 
-    private rotateLeft(x: Node<T>): void {
-        const y = x.right;
-        if (!y) return;
-        
-        x.right = y.left;
-        if (y.left) y.left.parent = x;
-
-        y.parent = x.parent;
-        if (!x.parent) {
-            this.root = y;
-        } else if (x === x.parent.left) {
-            x.parent.left = y;
-        } else {
-            x.parent.right = y;
-        }
-
-        y.left = x;
-        x.parent = y;
-    }
-
-    private rotateRight(y: Node<T>): void {
-        const x = y.left;
-        if (!x) return;
-
-        y.left = x.right;
-        if (x.right) x.right.parent = y;
-
-        x.parent = y.parent;
-        if (!y.parent) {
-            this.root = x;
-        } else if (y === y.parent.left) {
-            y.parent.left = x;
-        } else {
-            y.parent.right = x;
-        }
-
-        x.right = y;
-        y.parent = x;
-    }
-
-    private fixInsert(z: Node<T>): void {
-        while (z.parent && z.parent.color === Color.RED) {
-            if (z.parent === z.parent.parent?.left) {
-                const y = z.parent.parent?.right;
-                if (y && y.color === Color.RED) {
-                    z.parent.color = Color.BLACK;
-                    y.color = Color.BLACK;
-                    z.parent.parent.color = Color.RED;
-                    z = z.parent.parent;
-                } else {
-                    if (z === z.parent.right) {
-                        z = z.parent;
-                        this.rotateLeft(z);
-                    }
-                    z.parent.color = Color.BLACK;
-                    z.parent.parent.color = Color.RED;
-                    this.rotateRight(z.parent.parent);
-                }
-            } else {
-                const y = z.parent.parent?.left;
-                if (y && y.color === Color.RED) {
-                    z.parent.color = Color.BLACK;
-                    y.color = Color.BLACK;
-                    z.parent.parent.color = Color.RED;
-                    z = z.parent.parent;
-                } else {
-                    if (z === z.parent.left) {
-                        z = z.parent;
-                        this.rotateRight(z);
-                    }
-                    z.parent.color = Color.BLACK;
-                    z.parent.parent.color = Color.RED;
-                    this.rotateLeft(z.parent.parent);
-                }
-            }
-        }
-        this.root.color = Color.BLACK;
-    }
-
-    public insert( T): void {
-        const z = new Node<T>(data);
-        let y: Node<T> | null = null;
-        let x: Node<T> | null = this.root;
-
-        while (x) {
-            y = x;
-            if (z.data < x.data) {
-                x = x.left;
-            } else {
-                x = x.right;
-            }
-        }
-
-        z.parent = y;
-        if (!y) {
-            this.root = z;
-        } else if (z.data < y.data) {
-            y.left = z;
-        } else {
-            y.right = z;
-        }
-
-        this.fixInsert(z);
-    }
-
-    // Additional methods for deletion, traversal, etc. can be implemented similarly
-}
-
-// Example usage
-const rbTree = new RedBlackTree<number>();
-rbTree.insert(10);
-rbTree.insert(20);
-rbTree.insert(30);
-
-console.log(rbTree);
+const commonElements = findCommonElements(array1, array2);
+console.log(commonElements); // Output: [4, 5]
