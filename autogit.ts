6ff9ac74113cbdcd62de5a484ecabb2c55cd931f
RyannKim327 <rksesgundo123@gmail.com>
2025-02-17 12:14:01 +0000

    2-17-2025 12:13 [Mahirap lang isipin, pero tanggap ko.]

---
+++ autogit.ts
@@ -0,0 +1,10 @@
+let string1: string = "Hello, ";
+let string2: string = "World!";
+let concatenated: string = string1 + string2;
+
+console.log(concatenated); // Output: Hello, World!
+let string1: string = "Hello, ";
+let string2: string = "World!";
+let concatenated: string = `${string1}${string2}`;
+
+console.log(concatenated); // Output: Hello, World!
