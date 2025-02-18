c0705c628901b5860318efb7c4a01aaf5d64bd33
RyannKim327 <rksesgundo123@gmail.com>
2025-02-18 13:16:28 +0000

    2-18-2025 13:16 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,22 +1,6 @@
-function findCommonElements(arr1: number[], arr2: number[]): number[] {
-    return arr1.filter(value => arr2.includes(value));
-}
+const mainString: string = "Hello, world!";
+const substring: string = "world";
 
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
+const containsSubstring: boolean = mainString.includes(substring);
 
-const commonElements = findCommonElements(array1, array2);
-console.log(commonElements); // Output: [4, 5]
-function findCommonElements(arr1: number[], arr2: number[]): number[] {
-    const set1 = new Set(arr1);
-    const set2 = new Set(arr2);
-    
-    const commonElements = [...set1].filter(value => set2.has(value));
-    return commonElements;
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [4, 5, 6, 7, 8];
-
-const commonElements = findCommonElements(array1, array2);
-console.log(commonElements); // Output: [4, 5]
+console.log(containsSubstring); // Output: true
