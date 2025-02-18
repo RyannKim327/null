51226e6d1c3776110b2fe9a0310e4cc8917e8c76
RyannKim327 <rksesgundo123@gmail.com>
2025-02-18 08:10:29 +0000

    2-18-2025 8:10 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,20 +1,8 @@
-function insertionSort(arr: number[]): number[] {
-    for (let i = 1; i < arr.length; i++) {
-        let key = arr[i];
-        let j = i - 1;
-
-        // Move elements of arr[0..i-1], that are
-        // greater than key, to one position ahead
-        // of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-    return arr;
+function removeVowels(input: string): string {
+    return input.replace(/[aeiouAEIOU]/g, '');
 }
 
 // Example usage:
-const array = [12, 11, 13, 5, 6];
-console.log(insertionSort(array)); // Output: [5, 6, 11, 12, 13]
+const originalString = "Hello, World!";
+const stringWithoutVowels = removeVowels(originalString);
+console.log(stringWithoutVowels); // Hll, Wrld!
