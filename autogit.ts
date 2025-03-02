c80a531707a488bdd96d0605dfd22433028e17b6
RyannKim327 <rksesgundo123@gmail.com>
2025-03-02 21:00:59 +0000

    3-2-2025 21:0 [Wala, talaga lang attractive sya saken]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,49 @@
-let string1: string = "Hello";
-let string2: string = "World";
-let concatenated: string = string1 + " " + string2;
+class ListNode {
+    value: number;
+    next: ListNode | null;
 
-console.log(concatenated); // Output: "Hello World"
-let string1: string = "Hello";
-let string2: string = "World";
-let concatenated: string = `${string1} ${string2}`;
+    constructor(value: number) {
+        this.value = value;
+        this.next = null;
+    }
+}
 
-console.log(concatenated); // Output: "Hello World"
+class LinkedList {
+    head: ListNode | null;
+
+    constructor() {
+        this.head = null;
+    }
+
+    // Method to add a new node at the end of the list
+    append(value: number) {
+        const newNode = new ListNode(value);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to find the length of the linked list
+    length(): number {
+        let count = 0;
+        let current = this.head;
+        while (current) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
+
+// Example usage:
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+console.log("Length of linked list:", list.length()); // Output: Length of linked list: 3
