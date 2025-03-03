ac964e1bd8743cbf9987c241b47a274bc5f12134
RyannKim327 <rksesgundo123@gmail.com>
2025-03-03 02:15:32 +0000

    3-3-2025 2:15 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.ts
+++ autogit.ts
@@ -1,163 +1,19 @@
-enum Color {
-    RED,
-    BLACK
-}
+function countWordOccurrences(text: string, word: string): number {
+    // Normalize the text and the word to lower case to make the search case-insensitive
+    const normalizedText = text.toLowerCase();
+    const normalizedWord = word.toLowerCase();
 
-class Node {
-    public color: Color;
-    public left: Node | null;
-    public right: Node | null;
-    public parent: Node | null;
-    public value: number;
+    // Split the text into an array of words
+    const wordsArray = normalizedText.split(/\s+/); // Split by whitespace
 
-    constructor(value: number) {
-        this.value = value;
-        this.color = Color.RED; // New nodes are red by default
-        this.left = null;
-        this.right = null;
-        this.parent = null;
-    }
-}
+    // Filter the array to count occurrences of the word
+    const count = wordsArray.filter(w => w === normalizedWord).length;
 
-class RedBlackTree {
-    private root: Node | null;
-
-    constructor() {
-        this.root = null;
-    }
-
-    private rotateLeft(x: Node) {
-        const y = x.right!;
-        x.right = y.left;
-
-        if (y.left !== null) {
-            y.left.parent = x;
-        }
-
-        y.parent = x.parent;
-
-        if (x.parent === null) {
-            this.root = y;
-        } else if (x === x.parent.left) {
-            x.parent.left = y;
-        } else {
-            x.parent.right = y;
-        }
-
-        y.left = x;
-        x.parent = y;
-    }
-
-    private rotateRight(y: Node) {
-        const x = y.left!;
-        y.left = x.right;
-
-        if (x.right !== null) {
-            x.right.parent = y;
-        }
-
-        x.parent = y.parent;
-
-        if (y.parent === null) {
-            this.root = x;
-        } else if (y === y.parent.right) {
-            y.parent.right = x;
-        } else {
-            y.parent.left = x;
-        }
-
-        x.right = y;
-        y.parent = x;
-    }
-
-    private fixViolation(z: Node) {
-        while (z.parent && z.parent.color === Color.RED) {
-            if (z.parent === z.parent.parent?.left) {
-                const y = z.parent.parent?.right;
-
-                if (y && y.color === Color.RED) {
-                    z.parent.color = Color.BLACK;
-                    y.color = Color.BLACK;
-                    z.parent.parent!.color = Color.RED;
-                    z = z.parent.parent!;
-                } else {
-                    if (z === z.parent.right) {
-                        z = z.parent;
-                        this.rotateLeft(z);
-                    }
-                    z.parent.color = Color.BLACK;
-                    z.parent.parent!.color = Color.RED;
-                    this.rotateRight(z.parent.parent!);
-                }
-            } else {
-                const y = z.parent.parent?.left;
-
-                if (y && y.color === Color.RED) {
-                    z.parent.color = Color.BLACK;
-                    y.color = Color.BLACK;
-                    z.parent.parent!.color = Color.RED;
-                    z = z.parent.parent!;
-                } else {
-                    if (z === z.parent.left) {
-                        z = z.parent;
-                        this.rotateRight(z);
-                    }
-                    z.parent.color = Color.BLACK;
-                    z.parent.parent!.color = Color.RED;
-                    this.rotateLeft(z.parent.parent!);
-                }
-            }
-        }
-        this.root!.color = Color.BLACK;
-    }
-
-    public insert(value: number) {
-        const newNode = new Node(value);
-        let y: Node | null = null;
-        let x: Node | null = this.root;
-
-        while (x !== null) {
-            y = x;
-            if (newNode.value < x.value) {
-                x = x.left;
-            } else {
-                x = x.right;
-            }
-        }
-
-        newNode.parent = y;
-
-        if (y === null) {
-            this.root = newNode;
-        } else if (newNode.value < y.value) {
-            y.left = newNode;
-        } else {
-            y.right = newNode;
-        }
-
-        this.fixViolation(newNode);
-    }
-
-    public inorderTraversal(node: Node | null = this.root): number[] {
-        const result: number[] = [];
-        if (node !== null) {
-            result.push(...this.inorderTraversal(node.left));
-            result.push(node.value);
-            result.push(...this.inorderTraversal(node.right));
-        }
-        return result;
-    }
-
-    public getRoot(): Node | null {
-        return this.root;
-    }
+    return count;
 }
 
 // Example usage
-const rbTree = new RedBlackTree();
-rbTree.insert(10);
-rbTree.insert(20);
-rbTree.insert(30);
-rbTree.insert(15);
-
-console.log(rbTree.inorderTraversal()); // Output: [10, 15, 20, 30]
+const text = "Hello world! This is a test. Hello again, world!";
+const wordToCount = "hello";
+const occurrences = countWordOccurrences(text, wordToCount);
+console.log(`The word "${wordToCount}" occurs ${occurrences} times.`);
