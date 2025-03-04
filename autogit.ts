59a93c728e323f3a5d8c28b65c8d0826069eae08
RyannKim327 <rksesgundo123@gmail.com>
2025-03-04 08:10:16 +0000

    3-4-2025 8:10 []

--- autogit.ts
+++
@@ -1,8 +0,0 @@
-function removeSpaces(input: string): string {
-    return input.replace(/\s+/g, '');
-}
-
-// Example usage:
-const originalString = "Hello,  World!  How are you?";
-const stringWithoutSpaces = removeSpaces(originalString);
-console.log(stringWithoutSpaces); // Output: "Hello,World!Howareyou?"
