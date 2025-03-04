09328ddfe14a18d01f2bef341df66ed4e186c868
RyannKim327 <rksesgundo123@gmail.com>
2025-03-04 03:20:33 +0000

    3-4-2025 3:19 [Pwede pa sa late game mga gar]

--- autogit.ts
+++
@@ -1,7 +0,0 @@
-function getRandomNumber(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Example usage:
-const randomNum = getRandomNumber(1, 10);
-console.log(randomNum); // This will log a random number between 1 and 10 (inclusive)
