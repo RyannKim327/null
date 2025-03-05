294543532bf5bfde891a2bc3356f284a6db22533
RyannKim327 <rksesgundo123@gmail.com>
2025-03-05 05:00:52 +0000

    3-5-2025 5:0 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,12 +1,42 @@
-function factorial(n: number): number {
-    // Base case: factorial of 0 or 1 is 1
-    if (n === 0 || n === 1) {
-        return 1;
-    }
-    // Recursive case: n! = n * (n - 1)!
-    return n * factorial(n - 1);
-}
+ns create MyAsyncApp --template @nativescript/template-hello-world-ng
+cd MyAsyncApp
+npm install axios
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
-// Example usage:
-const number = 5;
-console.log(`Factorial of ${number} is ${factorial(number)}`); // Output: Factorial of 5 is 120
+@Component({
+  selector: 'ns-app',
+  templateUrl: './app.component.html',
+})
+export class AppComponent implements OnInit {
+  public  any[] = [];
+  public loading: boolean = true;
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  private async fetchData() {
+    try {
+      this.loading = true;
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      this.data = response.data;
+    } catch (error) {
+      console.error('Error fetching ', error);
+    } finally {
+      this.loading = false;
+    }
+  }
+}
+<StackLayout *ngIf="loading">
+  <Label text="Loading..." class="loading-message"></Label>
+</StackLayout>
+
+<StackLayout *ngIf="!loading">
+  <ListView [items]="data">
+    <ng-template let-item="item">
+      <Label [text]="item.title" class="item-title"></Label>
+    </ng-template>
+  </ListView>
+</StackLayout>
+ns run android
