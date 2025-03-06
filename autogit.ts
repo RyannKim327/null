9a87887f5e346fc763044246c52d835134a26b89
RyannKim327 <rksesgundo123@gmail.com>
2025-03-06 20:00:52 +0000

    3-6-2025 20:0 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,9 +1,32 @@
-const numbers: number[] = [5, 2, 9, 1, 7];
+function reverseString(str: string): string {
+  return [...str].reverse().join('');
+}
+function reverseString(str: string): string {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
+function reverseString(str: string): string {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+function reverseString(str: string): string {
+  if (!str) return str;
+  return reverseString(str.substr(1)) + str[0];
+}
+const originalString = 'Hello, World!';
+const reversedString1 = reverseString(originalString); // '!dlroW ,olleH'
+const reversedString2 = reverseString(originalString); // '!dlroW ,olleH'
+const reversedString3 = reverseString(originalString); // '!dlroW ,olleH'
+const reversedString4 = reverseString(originalString); // '!dlroW ,olleH'
+const reversedString5 = reverseString(originalString); // '!dlroW ,olleH'
 
-// Sort in ascending order
-numbers.sort((a, b) => a - b);
-console.log(numbers); // Output: [1, 2, 5, 7, 9]
-
-// Sort in descending order
-numbers.sort((a, b) => b - a);
-console.log(numbers); // Output: [9, 7, 5, 2, 1]
+console.log(reversedString1);
+console.log(reversedString2);
+console.log(reversedString3);
+console.log(reversedString4);
+console.log(reversedString5);
