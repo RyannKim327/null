8069cdc949330cfa55cfac8c5274d18db643aba6
RyannKim327 <rksesgundo123@gmail.com>
2025-03-06 19:02:31 +0000

    3-6-2025 19:2 []

--- autogit.ts
+++ autogit.ts
@@ -1,41 +1,28 @@
-function findLCS(str1: string, str2: string): string {
-  const m = str1.length;
-  const n = str2.length;
+class ListNode {
+  val: number;
+  next: ListNode | null;
 
-  // Create a 2D array to store the lengths of LCS of substrings
-  const dp: number[][] = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-
-  // Build the dp table
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (str1[i - 1] === str2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      } else {
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      }
-    }
+  constructor(val: number) {
+    this.val = val;
+    this.next = null;
   }
-
-  // Reconstruct the LCS string
-  let lcs = '';
-  let i = m, j = n;
-  while (i > 0 && j > 0) {
-    if (str1[i - 1] === str2[j - 1]) {
-      lcs = str1[i - 1] + lcs;
-      i--;
-      j--;
-    } else if (dp[i - 1][j] > dp[i][j - 1]) {
-      i--;
-    } else {
-      j--;
-    }
-  }
-
-  return lcs;
 }
 
-// Example usage
-const str1 = 'ABCBDAB';
-const str2 = 'BDCABA';
-const lcs = findLCS(str1, str2);
-console.log(`LCS: ${lcs}`); // Output: LCS: BCBA
+function hasCycle(head: ListNode | null): boolean {
+  if (!head || !head.next) {
+    return false; // Empty list or single node
+  }
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow !== fast) {
+    if (!fast || !fast.next) {
+      return false; // Reached the end of the list
+    }
+    slow = slow.next!;
+    fast = fast.next.next!;
+  }
+
+  return true; // Cycle detected
+}
