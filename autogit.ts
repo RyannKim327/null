ce06eb91ef1f34638da4a73aedb2d57ecaf33894
RyannKim327 <rksesgundo123@gmail.com>
2025-03-06 21:00:32 +0000

    3-6-2025 21:0 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,8 +1,30 @@
-function removeVowels(input: string): string {
-    return input.replace(/[aeiouAEIOU]/g, '');
-}
+function findMajorityElement(nums: number[]): number | null {
+  let majority = null;
+  let count = 0;
 
-// Example usage:
-const originalString = "Hello, World!";
-const stringWithoutVowels = removeVowels(originalString);
-console.log(stringWithoutVowels); // Hll, Wrld!
+  for (const num of nums) {
+    if (count === 0) {
+      majority = num;
+    }
+
+    if (num === majority) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  count = 0;
+  for (const num of nums) {
+    if (num === majority) {
+      count++;
+    }
+  }
+
+  return count > nums.length / 2 ? majority : null;
+}
+const nums1 = [3, 2, 3];
+const nums2 = [2, 2, 1, 1, 1, 2, 2];
+
+console.log(findMajorityElement(nums1)); // Output: 3
+console.log(findMajorityElement(nums2)); // Output: 2
