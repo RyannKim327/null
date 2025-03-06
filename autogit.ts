5b85cce8fb0b9200ce720631429fed1681097509
RyannKim327 <rksesgundo123@gmail.com>
2025-03-06 20:00:55 +0000

    3-6-2025 20:0 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,32 +1,8 @@
-function reverseString(str: string): string {
-  return [...str].reverse().join('');
+function removeVowels(input: string): string {
+    return input.replace(/[aeiouAEIOU]/g, '');
 }
-function reverseString(str: string): string {
-  let reversedStr = '';
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversedStr += str[i];
-  }
-  return reversedStr;
-}
-function reverseString(str: string): string {
-  return str.split('').reverse().join('');
-}
-function reverseString(str: string): string {
-  return str.split('').reduce((rev, char) => char + rev, '');
-}
-function reverseString(str: string): string {
-  if (!str) return str;
-  return reverseString(str.substr(1)) + str[0];
-}
-const originalString = 'Hello, World!';
-const reversedString1 = reverseString(originalString); // '!dlroW ,olleH'
-const reversedString2 = reverseString(originalString); // '!dlroW ,olleH'
-const reversedString3 = reverseString(originalString); // '!dlroW ,olleH'
-const reversedString4 = reverseString(originalString); // '!dlroW ,olleH'
-const reversedString5 = reverseString(originalString); // '!dlroW ,olleH'
 
-console.log(reversedString1);
-console.log(reversedString2);
-console.log(reversedString3);
-console.log(reversedString4);
-console.log(reversedString5);
+// Example usage:
+const originalString = "Hello, World!";
+const stringWithoutVowels = removeVowels(originalString);
+console.log(stringWithoutVowels); // Hll, Wrld!
