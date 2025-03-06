af268586280eaf13e50ac3fc1ff7466e127ed610
RyannKim327 <rksesgundo123@gmail.com>
2025-03-06 10:01:19 +0000

    3-6-2025 10:1 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,130 +1,17 @@
-class BTreeNode {
-    keys: number[];
-    children: BTreeNode[];
-    isLeaf: boolean;
-    t: number; // Minimum degree (defines the range for number of keys)
+function reverseWords(input: string): string {
+    // Step 1: Split the string into an array of words using space as a delimiter
+    const wordsArray = input.split(' ');
 
-    constructor(t: number, isLeaf: boolean) {
-        this.t = t;
-        this.isLeaf = isLeaf;
-        this.keys = [];
-        this.children = [];
-    }
+    // Step 2: Reverse the array of words
+    const reversedArray = wordsArray.reverse();
 
-    // Function to traverse all nodes in a subtree rooted with this node
-    traverse() {
-        let i: number;
-        for (i = 0; i < this.keys.length; i++) {
-            // If this is not a leaf, then before the next key, traverse the child
-            if (!this.isLeaf) {
-                this.children[i].traverse();
-            }
-            console.log(this.keys[i]);
-        }
+    // Step 3: Join the reversed array back into a string
+    const reversedString = reversedArray.join(' ');
 
-        // Finally, traverse the last child
-        if (!this.isLeaf) {
-            this.children[i].traverse();
-        }
-    }
-
-    // Function to insert a new key in this node
-    insertNonFull(key: number) {
-        let i = this.keys.length - 1;
-
-        // If this is a leaf node
-        if (this.isLeaf) {
-            // Find the location of new key to be inserted
-            while (i >= 0 && key < this.keys[i]) {
-                i--;
-            }
-            // Insert the new key at found location
-            this.keys.splice(i + 1, 0, key);
-        } else {
-            // Find the child which is going to have the new key
-            while (i >= 0 && key < this.keys[i]) {
-                i--;
-            }
-            // Check if the found child is full
-            if (this.children[i + 1].keys.length === 2 * this.t - 1) {
-                // If the child is full, then split it
-                this.splitChild(i + 1);
-                // After split, the middle key of child goes up and
-                // this key is going to be inserted in the appropriate child
-                if (key > this.keys[i + 1]) {
-                    i++;
-                }
-            }
-            this.children[i + 1].insertNonFull(key);
-        }
-    }
-
-    // Function to split the child of this node. `i` is index of the child
-    // in the children array
-    splitChild(i: number) {
-        const t = this.t;
-        const y = this.children[i];
-        const z = new BTreeNode(t, y.isLeaf);
-
-        // Give z the last t-1 keys of y
-        for (let j = 0; j < t - 1; j++) {
-            z.keys.push(y.keys[j + t]);
-        }
-
-        // If y is not a leaf, then give z the last t children of y
-        if (!y.isLeaf) {
-            for (let j = 0; j < t; j++) {
-                z.children.push(y.children[j + t]);
-            }
-        }
-
-        // Reduce the number of keys in y
-        y.keys.length = t - 1;
-
-        // Since this node is going to have a new child, create space for it
-        this.children.splice(i + 1, 0, z);
-
-        // A key of y will move to this node. Find location of new key
-        // and move a key to this node
-        this.keys.splice(i, 0, y.keys.pop()!);
-    }
+    return reversedString;
 }
 
-class BTree {
-    root: BTreeNode;
-    t: number; // Minimum degree
-
-    constructor(t: number) {
-        this.root = new BTreeNode(t, true);
-        this.t = t;
-    }
-
-    // Function to traverse the tree
-    traverse() {
-        this.root.traverse();
-    }
-
-    // Function to insert a new key
-    insert(key: number) {
-        const root = this.root;
-
-        // If root is full, then tree grows in height
-        if (root.keys.length === 2 * this.t - 1) {
-            const newRoot = new BTreeNode(this.t, false);
-            newRoot.children.push(root);
-            newRoot.splitChild(0);
-            newRoot.insertNonFull(key);
-            this.root = newRoot;
-        } else {
-            root.insertNonFull(key);
-        }
-    }
-}
-
-// Example usage
-const bTree = new BTree(3); // A B-tree with minimum degree 3
-bTree.insert(10);
-bTree.insert(20);
-bTree.insert(5);
-bTree.insert(6);
-bTree.insert(
+// Example usage:
+const inputString = "Hello world this is TypeScript";
+const reversedString = reverseWords(inputString);
+console.log(reversedString); // Output: "TypeScript is this world Hello"
