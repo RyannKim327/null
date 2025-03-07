d153d2c44e5e3546e902faf47b67374fce2e761d
RyannKim327 <rksesgundo123@gmail.com>
2025-03-07 15:02:37 +0000

    3-7-2025 15:2 [Sana ako pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,10 @@
-// May error sa API
+let string1: string = "Hello, ";
+let string2: string = "world!";
+let concatenated: string = string1 + string2;
+
+console.log(concatenated); // Output: Hello, world!
+let string1: string = "Hello, ";
+let string2: string = "world!";
+let concatenated: string = `${string1}${string2}`;
+
+console.log(concatenated); // Output: Hello, world!
