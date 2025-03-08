bbc6c33b906783d813f32e25ca005396522c93a5
RyannKim327 <rksesgundo123@gmail.com>
2025-03-08 21:01:21 +0000

    3-8-2025 21:1 []

--- autogit.ts
+++ autogit.ts
@@ -1,13 +1,7 @@
-const originalArray: number[] = [1, 2, 3, 4, 5];
-console.log("Original Array:", originalArray);
+function reverseString(str: string): string {
+    return str.split('').reverse().join('');
+}
 
-// Reverse the array
-const reversedArray = originalArray.reverse();
-console.log("Reversed Array:", reversedArray);
-const originalArray: number[] = [1, 2, 3, 4, 5];
-console.log("Original Array:", originalArray);
-
-// Create a new reversed array
-const reversedArray = originalArray.slice().reverse();
-console.log("Reversed Array:", reversedArray);
-console.log("Original Array after reversing:", originalArray);
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: !dlroW ,olleH
