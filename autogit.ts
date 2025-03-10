5dce4c24996a6bcb1b9ad0dfa4e1e54113437ca9
RyannKim327 <rksesgundo123@gmail.com>
2025-03-10 20:04:16 +0000

    3-10-2025 20:0 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++
@@ -1,9 +0,0 @@
-function decimalToBinary(decimal: number): string {
-    return decimal.toString(2);
-}
-
-// Example usage:
-const decimalNumber = 42;
-const binaryString = decimalToBinary(decimalNumber);
-console.log(`The binary representation of ${decimalNumber} is ${binaryString}`);
-The binary representation of 42 is 101010
