2b6ec23874b3019d89015f6d7f8095fd8f779554
RyannKim327 <rksesgundo123@gmail.com>
2025-03-12 05:01:04 +0000

    3-12-2025 5:0 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,4 @@
-let string1: string = "Hello, ";
-let string2: string = "world!";
-let concatenated: string = string1 + string2;
+let originalString: string = "Hello, World!";
+let lowerCaseString: string = originalString.toLowerCase();
 
-console.log(concatenated); // Output: Hello, world!
-let string1: string = "Hello, ";
-let string2: string = "world!";
-let concatenated: string = `${string1}${string2}`;
-
-console.log(concatenated); // Output: Hello, world!
+console.log(lowerCaseString); // Output: "hello, world!"
