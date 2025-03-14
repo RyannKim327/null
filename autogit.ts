dbff27554c11b9513e84550c53d500954afd1f3d
RyannKim327 <rksesgundo123@gmail.com>
2025-03-14 11:02:25 +0000

    3-14-2025 11:2 [Sana ako pa rin]

--- autogit.ts
+++
@@ -1,8 +0,0 @@
-function removeVowels(input: string): string {
-    return input.replace(/[aeiouAEIOU]/g, '');
-}
-
-// Example usage:
-const originalString = "Hello, World!";
-const stringWithoutVowels = removeVowels(originalString);
-console.log(stringWithoutVowels); // Hll, Wrld!
