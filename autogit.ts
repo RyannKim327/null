7a18e9180930a66d275f00ee23d12f365678b310
RyannKim327 <rksesgundo123@gmail.com>
2025-03-16 18:01:23 +0000

    3-16-2025 18:1 [Wala, talaga lang attractive sya saken]

--- autogit.ts
+++ autogit.ts
@@ -1,138 +1,57 @@
-class MinHeap {
-    private heap: number[];
+class ListNode {
+    value: number;
+    next: ListNode | null;
 
-    constructor() {
-        this.heap = [];
-    }
-
-    private getParentIndex(index: number): number {
-        return Math.floor((index - 1) / 2);
-    }
-
-    private getLeftChildIndex(index: number): number {
-        return index * 2 + 1;
-    }
-
-    private getRightChildIndex(index: number): number {
-        return index * 2 + 2;
-    }
-
-    private hasParent(index: number): boolean {
-        return this.getParentIndex(index) >= 0;
-    }
-
-    private hasLeftChild(index: number): boolean {
-        return this.getLeftChildIndex(index) < this.heap.length;
-    }
-
-    private hasRightChild(index: number): boolean {
-        return this.getRightChildIndex(index) < this.heap.length;
-    }
-
-    private parent(index: number): number {
-        return this.heap[this.getParentIndex(index)];
-    }
-
-    private leftChild(index: number): number {
-        return this.heap[this.getLeftChildIndex(index)];
-    }
-
-    private rightChild(index: number): number {
-        return this.heap[this.getRightChildIndex(index)];
-    }
-
-    private swap(indexOne: number, indexTwo: number): void {
-        const temp = this.heap[indexOne];
-        this.heap[indexOne] = this.heap[indexTwo];
-        this.heap[indexTwo] = temp;
-    }
-
-    public insert(value: number): void {
-        this.heap.push(value);
-        this.heapifyUp();
-    }
-
-    private heapifyUp(): void {
-        let index = this.heap.length - 1;
-        while (this.hasParent(index) && this.parent(index) > this.heap[index]) {
-            this.swap(this.getParentIndex(index), index);
-            index = this.getParentIndex(index);
-        }
-    }
-
-    public remove(): number | null {
-        if (this.heap.length === 0) {
-            return null;
-        }
-        const item = this.heap[0];
-        this.heap[0] = this.heap[this.heap.length - 1];
-        this.heap.pop();
-        this.heapifyDown();
-        return item;
-    }
-
-    private heapifyDown(): void {
-        let index = 0;
-        while (this.hasLeftChild(index)) {
-            let smallerChildIndex = this.getLeftChildIndex(index);
-            if (this.hasRightChild(index) && this.rightChild(index) < this.leftChild(index)) {
-                smallerChildIndex = this.getRightChildIndex(index);
-            }
-            if (this.heap[index] < this.heap[smallerChildIndex]) {
-                break;
-            } else {
-                this.swap(index, smallerChildIndex);
-            }
-            index = smallerChildIndex;
-        }
-    }
-
-    public peek(): number | null {
-        return this.heap.length > 0 ? this.heap[0] : null;
-    }
-
-    public isEmpty(): boolean {
-        return this.heap.length === 0;
-    }
-
-    public size(): number {
-        return this.heap.length;
+    constructor(value: number) {
+        this.value = value;
+        this.next = null;
     }
 }
-class PriorityQueue {
-    private heap: MinHeap;
+
+class LinkedList {
+    head: ListNode | null;
 
     constructor() {
-        this.heap = new MinHeap();
+        this.head = null;
     }
 
-    public enqueue(value: number): void {
-        this.heap.insert(value);
+    // Method to add a new node at the end of the list
+    append(value: number) {
+        const newNode = new ListNode(value);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
     }
 
-    public dequeue(): number | null {
-        return this.heap.remove();
-    }
+    // Method to find the middle element
+    findMiddle(): ListNode | null {
+        let slowPointer: ListNode | null = this.head;
+        let fastPointer: ListNode | null = this.head;
 
-    public peek(): number | null {
-        return this.heap.peek();
-    }
+        while (fastPointer && fastPointer.next) {
+            slowPointer = slowPointer?.next || null; // Move slow pointer by 1
+            fastPointer = fastPointer.next.next; // Move fast pointer by 2
+        }
 
-    public isEmpty(): boolean {
-        return this.heap.isEmpty();
-    }
-
-    public size(): number {
-        return this.heap.size();
+        return slowPointer; // Slow pointer will be at the middle
     }
 }
-const pq = new PriorityQueue();
-pq.enqueue(5);
-pq.enqueue(3);
-pq.enqueue(8);
-pq.enqueue(1);
 
-console.log(pq.peek()); // Output: 1
-console.log(pq.dequeue()); // Output: 1
-console.log(pq.peek()); // Output: 3
-console.log(pq.size()); // Output: 3
+// Example usage:
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+
+const middleNode = list.findMiddle();
+if (middleNode) {
+    console.log(`The middle element is: ${middleNode.value}`); // Output: The middle element is: 3
+}
