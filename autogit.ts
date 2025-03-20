9d22950135570285ab783dbac0fb608e1c327c25
RyannKim327 <rksesgundo123@gmail.com>
2025-03-20 00:36:50 +0000

    3-20-2025 0:36 [Hindi naman ako naghahabol, hindi rin naman sya nagpapahabol]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,28 @@
-// May error sa API
+function merge(left: number[], right: number[]): number[] {
+    let result: number[] = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+function mergeSort(array: number[]): number[] {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const middle = Math.floor(array.length / 2);
+    const left = array.slice(0, middle);
+    const right = array.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
