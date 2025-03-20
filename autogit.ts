cd8519aed0264e069a7f6d7c5fa5e792d98e32ee
RyannKim327 <rksesgundo123@gmail.com>
2025-03-20 02:15:28 +0000

    3-20-2025 2:15 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,4 @@
-// May error sa API
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
