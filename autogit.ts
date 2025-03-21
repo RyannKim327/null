d379d0868efddaebdfba5d2c2f2f225526da155e
RyannKim327 <rksesgundo123@gmail.com>
2025-03-21 08:10:52 +0000

    3-21-2025 8:10 [From valentines, hanggang pasko, na sana pati bagong taon]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,15 @@
-// May error sa API
+function areAnagrams(str1: string, str2: string): boolean {
+    const cleanStr1 = str1.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+    const cleanStr2 = str2.toLowerCase().replace(/[^a-z]/g, '').split('').sort().join('');
+
+    return cleanStr1 === cleanStr2;
+}
+
+const string1 = "listen";
+const string2 = "silent";
+
+if (areAnagrams(string1, string2)) {
+    console.log("The strings are anagrams.");
+} else {
+    console.log("The strings are not anagrams.");
+}
