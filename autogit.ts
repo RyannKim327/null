bd57c3a885ae20c60f42fba6045d4a3776e0040f
RyannKim327 <rksesgundo123@gmail.com>
2025-03-26 12:14:11 +0000

    3-26-2025 12:13 [Sana di dumating ung araw na un, kasi ako na ang aayaw]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,40 @@
-// May error sa API
+function longestCommonSubsequence(str1: string, str2: string): string {
+    const m = str1.length;
+    const n = str2.length;
+    const dp: number[][] = [];
+
+    for (let i = 0; i <= m; i++) {
+        dp[i] = [];
+        for (let j = 0; j <= n; j++) {
+            if (i === 0 || j === 0) {
+                dp[i][j] = 0;
+            } else if (str1[i - 1] === str2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    let result = '';
+    let i = m;
+    let j = n;
+    while (i > 0 && j > 0) {
+        if (str1[i - 1] === str2[j - 1]) {
+            result = str1[i - 1] + result;
+            i--;
+            j--;
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return result;
+}
+
+const str1 = "ABCDGH";
+const str2 = "AEDFHR";
+const lcs = longestCommonSubsequence(str1, str2);
+console.log(lcs); // Output: "ADH"
