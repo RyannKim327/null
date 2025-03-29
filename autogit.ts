0b7972f0ec0827d58af2327995491e80fd606b2a
RyannKim327 <rksesgundo123@gmail.com>
2025-03-29 04:01:41 +0000

    3-29-2025 4:1 [Mahirap lang isipin, pero tanggap ko.]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,50 @@
-// May error sa API
+class Graph {
+  private adjacencyList: Map<number, number[]>;
+
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addVertex(vertex: number) {
+    if (!this.adjacencyList.has(vertex)) {
+      this.adjacencyList.set(vertex, []);
+    }
+  }
+
+  addEdge(vertex1: number, vertex2: number) {
+    this.adjacencyList.get(vertex1)?.push(vertex2);
+    this.adjacencyList.get(vertex2)?.push(vertex1);
+  }
+
+  depthFirstSearch(startingVertex: number) {
+    const visited: Set<number> = new Set();
+    this.dfsRecursive(startingVertex, visited);
+  }
+
+  private dfsRecursive(vertex: number, visited: Set<number>) {
+    visited.add(vertex);
+    console.log(vertex);
+
+    const neighbors = this.adjacencyList.get(vertex);
+    if (neighbors) {
+      for (const neighbor of neighbors) {
+        if (!visited.has(neighbor)) {
+          this.dfsRecursive(neighbor, visited);
+        }
+      }
+    }
+  }
+}
+
+const graph = new Graph();
+graph.addVertex(0);
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addEdge(0, 1);
+graph.addEdge(0, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+
+graph.depthFirstSearch(0);
