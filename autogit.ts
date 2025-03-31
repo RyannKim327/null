ba95d3bf3e1e11365230ca9837f13da99beeb6c3
RyannKim327 <rksesgundo123@gmail.com>
2025-03-31 12:15:21 +0000

    3-31-2025 12:15 [Parang malabo na pala, di na ko aasa]

--- autogit.ts
+++ autogit.ts
@@ -1,15 +1,1 @@
-function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
-    const mergedArray = [...nums1, ...nums2].sort((a, b) => a - b);
-    const length = mergedArray.length;
-
-    if (length % 2 === 0) {
-        return (mergedArray[length / 2 - 1] + mergedArray[length / 2]) / 2;
-    } else {
-        return mergedArray[Math.floor(length / 2)];
-    }
-}
-
-const nums1 = [1, 3];
-const nums2 = [2];
-const median = findMedianSortedArrays(nums1, nums2);
-console.log(median);
+// May error sa API
