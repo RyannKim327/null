c2cb8638e6379866dddb844b797698b2d540d48b
RyannKim327 <rksesgundo123@gmail.com>
2025-03-31 23:01:46 +0000

    3-31-2025 23:1 []

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,9 @@
-// May error sa API
+function getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const minRange = 1;
+const maxRange = 100;
+const randomNumber = getRandomNumber(minRange, maxRange);
+
+console.log(randomNumber);
