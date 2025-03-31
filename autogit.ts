ebc6c47f1df42c7b92756d712b5a2db736266170
RyannKim327 <rksesgundo123@gmail.com>
2025-03-31 03:29:33 +0000

    3-31-2025 3:29 [Ano kaya ang susunod na ilalagay ko dito]

--- autogit.ts
+++ autogit.ts
@@ -1,7 +1,1 @@
-function decimalToBinary(decimal: number): string {
-    return (decimal >>> 0).toString(2);
-}
-
-const decimalNumber = 10;
-const binaryNumber = decimalToBinary(decimalNumber);
-console.log(binaryNumber); // Output: 1010
+// May error sa API
