32fb5cac2af67039570cf6331dbd9b319c430e36
RyannKim327 <rksesgundo123@gmail.com>
2025-03-31 02:24:55 +0000

    3-31-2025 2:22 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,7 @@
-// May error sa API
+function decimalToBinary(decimal: number): string {
+    return (decimal >>> 0).toString(2);
+}
+
+const decimalNumber = 10;
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber); // Output: 1010
