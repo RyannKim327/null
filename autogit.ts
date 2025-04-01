99a97e8b548af0a9c7c21fe254d11946a8637e04
RyannKim327 <rksesgundo123@gmail.com>
2025-04-01 00:43:46 +0000

    4-1-2025 0:43 [Pwede pa sa late game mga gar]

--- autogit.ts
+++ autogit.ts
@@ -1,18 +1,1 @@
-function longestCommonPrefix(strings: string[]): string {
-    if (strings.length === 0) return '';
-
-    let prefix = '';
-    for (let i = 0; i < strings[0].length; i++) {
-        const char = strings[0][i];
-        if (strings.some(str => str[i] !== char)) {
-            break;
-        }
-        prefix += char;
-    }
-
-    return prefix;
-}
-
-const strings = ['flower', 'flow', 'flight'];
-const commonPrefix = longestCommonPrefix(strings);
-console.log(commonPrefix); // Output: "fl"
+// May error sa API
