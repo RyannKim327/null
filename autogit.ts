4c99f397123add603f8ac76894b80f19999b731e
RyannKim327 <rksesgundo123@gmail.com>
2025-04-02 14:03:40 +0000

    4-2-2025 14:2 [Kaya pala, may iba na siguro sya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,31 @@
-// May error sa API
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+function findNthNodeFromEnd(head: ListNode | null, n: number): ListNode | null {
+    let length = 0;
+    let current = head;
+
+    while (current) {
+        length++;
+        current = current.next;
+    }
+
+    if (n > length) {
+        return null;
+    }
+
+    let position = length - n;
+    current = head;
+
+    for (let i = 0; i < position; i++) {
+        current = current.next;
+    }
+
+    return current;
+}
