814fb426be6f8fc841e55c930bc7515c8f926674
RyannKim327 <rksesgundo123@gmail.com>
2025-04-02 10:12:27 +0000

    4-2-2025 10:11 [Kahit anong gawin prii, sya pa rin]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,20 @@
-// May error sa API
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+}
+
+function findMiddle(head: ListNode | null): ListNode | null {
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
