a81a285a7fc4ba63e96ffffc14afde4bcfcbaf26
RyannKim327 <rksesgundo123@gmail.com>
2025-04-02 16:01:50 +0000

    4-2-2025 16:1 []

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,8 @@
-// May error sa API
+function removeVowels(str: string): string {
+    return str.replace(/[aeiouAEIOU]/g, '');
+}
+
+const inputString = "Hello, World!";
+const result = removeVowels(inputString);
+
+console.log(result); // Output: "Hll, Wrld!"
