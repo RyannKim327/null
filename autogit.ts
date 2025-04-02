984874320e95f9ab7f50259af16518251e25756d
RyannKim327 <rksesgundo123@gmail.com>
2025-04-02 19:02:08 +0000

    4-2-2025 19:2 [Parang malabo na pala, di na ko aasa]

--- autogit.ts
+++ autogit.ts
@@ -1,12 +1,1 @@
-import axios, { AxiosResponse } from 'axios';
-
-async function fetchData(): Promise<void> {
-  try {
-    const response: AxiosResponse = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-}
-
-fetchData();
+// May error sa API
