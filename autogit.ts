7da87c8fa6bac431fe78abe893e4731d16fce826
RyannKim327 <rksesgundo123@gmail.com>
2025-04-02 15:03:37 +0000

    4-2-2025 15:2 [Parang malabo na pala, di na ko aasa]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,12 @@
-// May error sa API
+let str1: string = "Hello";
+let str2: string = "World";
+
+let concatenatedString: string = str1 + " " + str2;
+
+console.log(concatenatedString); // Output: Hello World
+let str1: string = "Hello";
+let str2: string = "World";
+
+let concatenatedString: string = `${str1} ${str2}`;
+
+console.log(concatenatedString); // Output: Hello World
