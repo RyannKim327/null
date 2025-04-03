a32221a71e2767f8708704f42149f6d16d0ed161
RyannKim327 <rksesgundo123@gmail.com>
2025-04-03 04:11:35 +0000

    4-3-2025 4:9 [Saan aabot ang bente pesos mo?]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,35 @@
-// May error sa API
+function heapify(arr: number[], n: number, i: number) {
+    let largest = i;
+    const left = 2 * i + 1;
+    const right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
+    }
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
+    }
+
+    if (largest !== i) {
+        [arr[i], arr[largest]] = [arr[largest], arr[i]];
+        heapify(arr, n, largest);
+    }
+}
+function heapSort(arr: number[]) {
+    const n = arr.length;
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(arr, n, i);
+    }
+
+    for (let i = n - 1; i > 0; i--) {
+        [arr[0], arr[i]] = [arr[i], arr[0]];
+        heapify(arr, i, 0);
+    }
+
+    return arr;
+}
+const array = [12, 11, 13, 5, 6, 7];
+const sortedArray = heapSort(array);
+console.log(sortedArray);
