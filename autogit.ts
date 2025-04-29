2333e45027365a325c876f8c669c96b6a8c6ea8d
RyannKim327 <rksesgundo123@gmail.com>
2025-04-29 12:17:12 +0000

    4-29-2025 12:15 [Pinasaasa nya lang ako.]

--- autogit.ts
+++ autogit.ts
@@ -1,25 +1,8 @@
-class ListNode<T> {
-  value: T;
-  next: ListNode<T> | null;
-
-  constructor(value: T, next: ListNode<T> | null = null) {
-    this.value = value;
-    this.next = next;
-  }
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function getLength<T>(head: ListNode<T> | null): number {
-  let length = 0;
-  let current = head;
-
-  while (current !== null) {
-    length++;
-    current = current.next;
-  }
-
-  return length;
+function getRandomFloat(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
 }
-const node3 = new ListNode(3);
-const node2 = new ListNode(2, node3);
-const node1 = new ListNode(1, node2);
-
-console.log(getLength(node1));  // Output: 3
