8d2636041df1564a5a6a2246a51e0cba46232a74
RyannKim327 <rksesgundo123@gmail.com>
2025-04-29 03:33:29 +0000

    4-29-2025 3:33 [Buti na lang umiwas na, kasi meron na pala]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,7 @@
-function calculateMean(numbers: number[]): number {
-  if (numbers.length === 0) return 0; // or handle as appropriate
-  const sum = numbers.reduce((acc, num) => acc + num, 0);
-  return sum / numbers.length;
+function getStringLength(str: string): number {
+    let length = 0;
+    while (str[length] !== undefined) {
+        length++;
+    }
+    return length;
 }
-
-// Usage:
-const nums = [10, 20, 30, 40, 50];
-const mean = calculateMean(nums);
-console.log("Mean:", mean); // Output: Mean: 30
