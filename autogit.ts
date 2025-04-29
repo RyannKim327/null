5133b0fe554d49e7ea972b27be16dca1849d50d9
RyannKim327 <rksesgundo123@gmail.com>
2025-04-29 15:03:20 +0000

    4-29-2025 15:3 [Sanaol Santol]

--- autogit.ts
+++ autogit.ts
@@ -1,35 +1,13 @@
-function longestCommonSubsequence(s1: string, s2: string): string {
-    const m = s1.length;
-    const n = s2.length;
+function calculateMean(numbers: number[]): number {
+  if (numbers.length === 0) {
+    throw new Error("Cannot calculate mean of an empty array.");
+  }
 
-    // dp[i][j] will hold the length of LCS of s1[0..i-1] and s2[0..j-1]
-    const dp: number[][] = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-
-    // Fill dp table
-    for (let i = 1; i <= m; i++) {
-        for (let j = 1; j <= n; j++) {
-            if (s1[i - 1] === s2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-            } else {
-                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-
-    // Build the LCS string by backtracking through dp
-    let i = m, j = n;
-    const lcsChars = [];
-    while (i > 0 && j > 0) {
-        if (s1[i - 1] === s2[j - 1]) {
-            lcsChars.push(s1[i - 1]);
-            i--;
-            j--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
-            i--;
-        } else {
-            j--;
-        }
-    }
-
-    return lcsChars.reverse().join('');
+  const sum = numbers.reduce((accumulator, current) => accumulator + current, 0);
+  return sum / numbers.length;
 }
+
+// Example use:
+const myNumbers = [10, 20, 30, 40, 50];
+const mean = calculateMean(myNumbers);
+console.log(mean); // Outputs: 30
