52103fd34873c175bf76c6678c2a2d3ccb670a39
RyannKim327 <rksesgundo123@gmail.com>
2025-04-30 06:17:33 +0000

    4-30-2025 6:16 [Hindi daw, sheesh]

--- autogit.ts
+++ autogit.ts
@@ -1,7 +1,84 @@
-const arr = [1, 2, 3, 4, 5];
-arr.reverse();
-console.log(arr); // Output: [5, 4, 3, 2, 1]
-const arr = [1, 2, 3, 4, 5];
-const reversed = [...arr].reverse();
-console.log(reversed); // [5, 4, 3, 2, 1]
-console.log(arr);      // [1, 2, 3, 4, 5] - original untouched
+class PriorityQueue<T> {
+  private heap: T[] = [];
+  private compare: (a: T, b: T) => number;
+
+  constructor(compareFn: (a: T, b: T) => number) {
+    this.compare = compareFn;
+  }
+
+  private swap(i: number, j: number): void {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  private bubbleUp(index: number): void {
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.compare(this.heap[index], this.heap[parentIndex]) >= 0) {
+        break;
+      }
+      this.swap(index, parentIndex);
+      index = parentIndex;
+    }
+  }
+
+  private bubbleDown(index: number): void {
+    const length = this.heap.length;
+    while (true) {
+      const left = 2 * index + 1;
+      const right = 2 * index + 2;
+      let smallest = index;
+
+      if (left < length && this.compare(this.heap[left], this.heap[smallest]) < 0) {
+        smallest = left;
+      }
+
+      if (right < length && this.compare(this.heap[right], this.heap[smallest]) < 0) {
+        smallest = right;
+      }
+
+      if (smallest === index) {
+        break;
+      }
+
+      this.swap(index, smallest);
+      index = smallest;
+    }
+  }
+
+  enqueue(value: T): void {
+    this.heap.push(value);
+    this.bubbleUp(this.heap.length - 1);
+  }
+
+  dequeue(): T | undefined {
+    if (this.heap.length === 0) {
+      return undefined;
+    }
+    const min = this.heap[0];
+    const end = this.heap.pop()!;
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.bubbleDown(0);
+    }
+    return min;
+  }
+
+  peek(): T | undefined {
+    return this.heap[0];
+  }
+
+  size(): number {
+    return this.heap.length;
+  }
+}
+// Min-heap: smallest number has highest priority
+const pq = new PriorityQueue<number>((a, b) => a - b);
+
+pq.enqueue(5);
+pq.enqueue(1);
+pq.enqueue(10);
+pq.enqueue(3);
+
+console.log(pq.dequeue()); // 1
+console.log(pq.peek());    // 3
+console.log(pq.size());    // 3
