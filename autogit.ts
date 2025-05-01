66e80120b5c85eb9d5315371d91589da85d01516
RyannKim327 <rksesgundo123@gmail.com>
2025-05-01 19:01:49 +0000

    5-1-2025 19:0 [Paano yan, lugi nanaman tayo]

--- autogit.ts
+++ autogit.ts
@@ -1,45 +1,18 @@
-mkdir ts-api-example
-cd ts-api-example
-npm init -y
-npm install typescript ts-node @types/node --save-dev
-npx tsc --init
-// index.ts
+const str = "Hello, TypeScript!";
+const substr = "Type";
 
-// Define a User interface to type the data we expect from the API
-interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
+if (str.includes(substr)) {
+  console.log("Substring found!");
+} else {
+  console.log("Substring not found.");
 }
-
-// Function to fetch users from the JSONPlaceholder API
-async function fetchUsers(): Promise<User[]> {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    const users: User[] = await response.json();
-    return users;
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-    return [];
-  }
+if (str.indexOf(substr) !== -1) {
+  console.log("Substring found!");
+} else {
+  console.log("Substring not found.");
 }
-
-// Function to display the users in the console
-async function displayUsers(): Promise<void> {
-  const users = await fetchUsers();
-  if (users.length > 0) {
-    users.forEach(user => {
-      console.log(`ID: ${user.id}, Name: ${user.name}, Username: ${user.username}, Email: ${user.email}`);
-    });
-  } else {
-    console.log('No users found');
-  }
+if (str.search(/Type/) !== -1) {
+  console.log("Substring found!");
+} else {
+  console.log("Substring not found.");
 }
-
-// Execute the displayUsers function
-displayUsers();
-npx ts-node index.ts
