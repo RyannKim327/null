33d1ac578b2ad09ba1097b263efde2e5d1534685
RyannKim327 <rksesgundo123@gmail.com>
2025-05-01 22:02:02 +0000

    5-1-2025 22:1 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- autogit.ts
+++ autogit.ts
@@ -1,25 +1,8 @@
-function quicksort(arr: number[]): number[] {
-  if (arr.length <= 1) return arr;
-
-  const pivot = arr[Math.floor(arr.length / 2)];
-  const less: number[] = [];
-  const equal: number[] = [];
-  const greater: number[] = [];
-
-  for (const num of arr) {
-    if (num < pivot) {
-      less.push(num);
-    } else if (num > pivot) {
-      greater.push(num);
-    } else {
-      equal.push(num);
-    }
-  }
-
-  return [...quicksort(less), ...equal, ...quicksort(greater)];
+function removeVowels(str: string): string {
+  return str.replace(/[aeiouAEIOU]/g, '');
 }
 
 // Example usage:
-const unsorted = [3, 6, 8, 10, 1, 2, 1];
-console.log(quicksort(unsorted));
-// Output: [1, 1, 2, 3, 6, 8, 10]
+const original = "Hello, TypeScript!";
+const noVowels = removeVowels(original);
+console.log(noVowels); // Hll, TypScrpt!
