3a03be9534b8ad34517585d2cc8c48e5f1342ea4
RyannKim327 <rksesgundo123@gmail.com>
2025-05-01 18:13:14 +0000

    5-1-2025 18:13 [Buti na lang umiwas na, kasi meron na pala]

--- autogit.ts
+++ autogit.ts
@@ -1,50 +1,6 @@
-class Node<T> {
-    value: T;
-    next: Node<T> | null;
-
-    constructor(value: T) {
-        this.value = value;
-        this.next = null;
-    }
-}
-
-class LinkedList<T> {
-    head: Node<T> | null;
-
-    constructor() {
-        this.head = null;
-    }
-
-    // Method to add a new node at the end of the list
-    add(value: T): void {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode;
-        }
-    }
-
-    // Method to find the length of the linked list
-    length(): number {
-        let count = 0;
-        let current = this.head;
-
-        while (current) {
-            count++;
-            current = current.next;
-        }
-
-        return count;
-    }
-}
-const list = new LinkedList<number>();
-list.add(1);
-list.add(2);
-list.add(3);
-
-console.log("Length of linked list:", list.length()); // Output: Length of linked list: 3
+const array = [1, 2, 2, 3, 4, 4, 5];
+const uniqueArray = Array.from(new Set(array));
+console.log(uniqueArray); // [1, 2, 3, 4, 5]
+const uniqueArray = [...new Set(array)];
+const uniqueArray = array.filter((item, index) => array.indexOf(item) === index);
+console.log(uniqueArray);
