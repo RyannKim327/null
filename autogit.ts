b4bd5f0e92d7e5bf92111db019417cf88073125a
RyannKim327 <rksesgundo123@gmail.com>
2025-05-01 02:40:05 +0000

    5-1-2025 2:40 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- autogit.ts
+++ autogit.ts
@@ -1,29 +1,5 @@
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = Array.from(new Set(arrayWithDuplicates));
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
+}
 
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = arrayWithDuplicates.filter((item, index) => {
-    return arrayWithDuplicates.indexOf(item) === index;
-});
-
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray = arrayWithDuplicates.reduce((acc: number[], item) => {
-    if (!acc.includes(item)) {
-        acc.push(item);
-    }
-    return acc;
-}, []);
-
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
-const arrayWithDuplicates = [1, 2, 2, 3, 4, 4, 5];
-const uniqueArray: number[] = [];
-
-arrayWithDuplicates.forEach(item => {
-    if (!uniqueArray.includes(item)) {
-        uniqueArray.push(item);
-    }
-});
-
-console.log(uniqueArray); // Output: [1, 2, 3, 4, 5]
+console.log(reverseString("hello")); // outputs "olleh"
