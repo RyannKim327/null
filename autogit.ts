11796241dafd088abb5bb4be09757ea88d8b09d6
RyannKim327 <rksesgundo123@gmail.com>
2025-05-01 05:02:00 +0000

    5-1-2025 5:1 [Pinasaasa nya lang ako.]

--- autogit.ts
+++
@@ -1,4 +0,0 @@
-const originalString = "This is a string with spaces";
-const stringWithoutSpaces = originalString.replace(/\s+/g, '');
-console.log(stringWithoutSpaces); // Output: "Thisisastringwithspaces"
-const stringWithoutSpaces = originalString.replace(/ /g, '');
