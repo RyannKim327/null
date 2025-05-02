ee40b9a282550e9a80413d85211c3dc0ed3ea476
RyannKim327 <rksesgundo123@gmail.com>
2025-05-02 21:02:33 +0000

    5-2-2025 21:2 [Sanaol Santol]

--- autogit.ts
+++ autogit.ts
@@ -1,54 +1,27 @@
-class ListNode {
-    value: number;
-    next: ListNode | null;
+class ListNode<T> {
+    value: T;
+    next: ListNode<T> | null;
 
-    constructor(value: number) {
+    constructor(value: T, next: ListNode<T> | null = null) {
         this.value = value;
-        this.next = null;
+        this.next = next;
     }
 }
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-    if (!headA || !headB) {
-        return null; // If either list is empty, return null
-    }
 
-    const visitedNodes = new Set<ListNode>();
+function hasCycle<T>(head: ListNode<T> | null): boolean {
+    if (!head) return false;
 
-    // Traverse the first linked list and store all nodes in the set
-    let currentA: ListNode | null = headA;
-    while (currentA) {
-        visitedNodes.add(currentA);
-        currentA = currentA.next;
-    }
+    let slow: ListNode<T> | null = head;
+    let fast: ListNode<T> | null = head;
 
-    // Traverse the second linked list and check for intersections
-    let currentB: ListNode | null = headB;
-    while (currentB) {
-        if (visitedNodes.has(currentB)) {
-            return currentB; // Intersection found
+    while (fast && fast.next) {
+        slow = slow!.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            return true; // Cycle detected
         }
-        currentB = currentB.next;
     }
 
-    return null; // No intersection found
-}
-// Example to create intersected linked lists
-const nodeA1 = new ListNode(1);
-const nodeA2 = new ListNode(2);
-const intersectingNode = new ListNode(3);
-const nodeB1 = new ListNode(4);
-const nodeB2 = new ListNode(5);
-
-nodeA1.next = nodeA2;
-nodeA2.next = intersectingNode;
-nodeB1.next = nodeB2;
-nodeB2.next = intersectingNode;
-
-// Now nodeA1 & nodeB1 intersect at `intersectingNode`
-const intersectionNode = getIntersectionNode(nodeA1, nodeB1);
-
-if (intersectionNode) {
-    console.log('Intersection Node Value:', intersectionNode.value); // Output: 3
-} else {
-    console.log('No intersection');
+    return false; // No cycle
 }
