c6ffe527fb0a8850f33e3fea0f29c0155e17b757
RyannKim327 <rksesgundo123@gmail.com>
2025-05-03 15:02:02 +0000

    5-3-2025 15:0 [Saan aabot ang bente pesos mo?]

--- autogit.ts
+++ autogit.ts
@@ -9,28 +9,6 @@ function isPalindrome(s: string): boolean {
     left++;
     right--;
   }
+
   return true;
-}
-function isValidPalindrome(s: string): boolean {
-  let left = 0;
-  let right = s.length - 1;
-
-  while (left < right) {
-    // Move left pointer to next alphanumeric
-    while (left < right && !isAlphaNumeric(s[left])) left++;
-
-    // Move right pointer to previous alphanumeric
-    while (left < right && !isAlphaNumeric(s[right])) right--;
-
-    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-      return false;
-    }
-    left++;
-    right--;
-  }
-  return true;
-}
-
-function isAlphaNumeric(c: string): boolean {
-  return /^[a-z0-9]$/i.test(c);
 }
