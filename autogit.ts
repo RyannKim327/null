6842f6d9a2d17e6e04b03958de15e6f00b1bdeb6
RyannKim327 <rksesgundo123@gmail.com>
2025-05-03 21:02:27 +0000

    5-3-2025 21:2 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- autogit.ts
+++ autogit.ts
@@ -1,10 +1,4 @@
-function getRandomInt(min: number, max: number): number {
-  const lower = Math.ceil(min);
-  const upper = Math.floor(max);
-  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
-}
-const randomNum = getRandomInt(5, 15);
-console.log(randomNum); // could be any integer 5 through 15, inclusive
-function getRandomFloat(min: number, max: number): number {
-  return Math.random() * (max - min) + min;
-}
+let originalString: string = "Hello, WORLD!";
+let lowerCaseString: string = originalString.toLowerCase();
+
+console.log(lowerCaseString); // Output: "hello, world!"
