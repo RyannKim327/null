a759353da7575eec7041587e1e5840c490905a90
RyannKim327 <rksesgundo123@gmail.com>
2025-05-05 15:08:22 +0000

    5-5-2025 15:8 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- autogit.ts
+++ autogit.ts
@@ -1,11 +1,5 @@
-const numbers: number[] = [5, 3, 8, 1, 2];
-
-// Sort in ascending order
-numbers.sort((a, b) => a - b);
-
-console.log(numbers); // Output: [1, 2, 3, 5, 8]
-numbers.sort((a, b) => b - a);
-console.log(numbers); // Output: [8, 5, 3, 2, 1]
-function sortNumbers(arr: number[], ascending = true): number[] {
-  return arr.sort((a, b) => ascending ? a - b : b - a);
+function reverseString(str: string): string {
+  return str.split('').reverse().join('');
 }
+
+console.log(reverseString("hello"));  // Outputs: "olleh"
