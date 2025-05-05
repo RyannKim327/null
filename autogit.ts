b0c7fbe9ce35dae085c7ae62ef7183162342deb7
RyannKim327 <rksesgundo123@gmail.com>
2025-05-05 09:11:48 +0000

    5-5-2025 9:11 [Pinasaasa nya lang ako.]

--- autogit.ts
+++ autogit.ts
@@ -1,44 +1,14 @@
-class ListNode {
-    value: number;
-    next: ListNode | null;
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) return "";
 
-    constructor(value: number, next: ListNode | null = null) {
-        this.value = value;
-        this.next = next;
+  let prefix = strs[0];
+  
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
     }
+  }
+  
+  return prefix;
 }
-
-function isPalindrome(head: ListNode | null): boolean {
-    if (!head) return true; // An empty list is a palindrome
-
-    let slow = head;
-    let fast = head;
-    const stack: number[] = [];
-
-    // Push the first half of the elements onto the stack
-    while (fast && fast.next) {
-        stack.push(slow.value);
-        slow = slow.next;
-        fast = fast.next.next;
-    }
-
-    // If the list has an odd number of elements, skip the middle element
-    if (fast) {
-        slow = slow.next;
-    }
-
-    // Compare the second half of the list with the stack
-    while (slow) {
-        const top = stack.pop();
-        if (top !== slow.value) {
-            return false; // Not a palindrome if there is a mismatch
-        }
-        slow = slow.next;
-    }
-
-    return true; // The linked list is a palindrome
-}
-
-// Example usage
-const head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
-console.log(isPalindrome(head)); // Output: true
