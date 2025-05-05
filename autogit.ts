ea4ea74d0ff8e24147ee0c585d5cb6401b684609
RyannKim327 <rksesgundo123@gmail.com>
2025-05-05 18:02:06 +0000

    5-5-2025 18:0 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- autogit.ts
+++ autogit.ts
@@ -1,72 +1,24 @@
-class Node {
-    value: number;
-    next: Node | null;
+function selectionSort(arr: number[]): number[] {
+    // Make a copy if you want to avoid mutating the original array
+    const array = [...arr];
+    const n = array.length;
 
-    constructor(value: number) {
-        this.value = value;
-        this.next = null;
+    for (let i = 0; i < n - 1; i++) {
+        // Assume the min is the first element in unsorted part
+        let minIndex = i;
+
+        // Check through the rest of the array to find the true min
+        for (let j = i + 1; j < n; j++) {
+            if (array[j] < array[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap if minIndex isn't the current i
+        if (minIndex !== i) {
+            [array[i], array[minIndex]] = [array[minIndex], array[i]];
+        }
     }
+
+    return array;
 }
-
-class LinkedList {
-    head: Node | null;
-
-    constructor() {
-        this.head = null;
-    }
-
-    // Function to add a new node at the end of the list
-    append(value: number) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            return;
-        }
-        let current = this.head;
-        while (current.next) {
-            current = current.next;
-        }
-        current.next = newNode;
-    }
-
-    // Function to print the linked list
-    print() {
-        let current = this.head;
-        const values: number[] = [];
-        while (current) {
-            values.push(current.value);
-            current = current.next;
-        }
-        console.log(values.join(" -> "));
-    }
-
-    // Function to reverse the linked list
-    reverse() {
-        let previous: Node | null = null;
-        let current: Node | null = this.head;
-        let next: Node | null = null;
-
-        while (current) {
-            next = current.next;  // Store next node
-            current.next = previous;  // Reverse the current node's pointer
-            previous = current;  // Move pointers one position forward
-            current = next;
-        }
-
-        this.head = previous;  // Update head to the new front of the list
-    }
-}
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-list.append(5);
-
-console.log("Original Linked List:");
-list.print();
-
-list.reverse();
-
-console.log("Reversed Linked List:");
-list.print();
