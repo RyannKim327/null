26996cd5e447cae1a69bab05773c7278061b28c7
RyannKim327 <rksesgundo123@gmail.com>
2025-05-06 16:12:43 +0000

    5-6-2025 16:12 [Buti na lang umiwas na, kasi meron na pala]

--- autogit.ts
+++ autogit.ts
@@ -1,9 +1,28 @@
-const str = "123";
-const num = parseInt(str, 10); // 10 is the radix (base)
-console.log(num); // 123
-const str = "123";
-const num = +str;
-console.log(num); // 123
-const str = "123";
-const num = Number(str);
-console.log(num); // 123
+import * as readline from 'readline';
+
+// Create an interface for reading input from the command line
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Function to prompt the user for their name and age
+function askUserDetails() {
+  rl.question("What is your name? ", (name) => {
+    rl.question("How old are you? ", (age) => {
+      const ageNumber = parseInt(age);
+
+      if (isNaN(ageNumber)) {
+        console.log("Please enter a valid age.");
+      } else {
+        console.log(`Hello, ${name}! You are ${ageNumber} years old.`);
+      }
+
+      // Close the readline interface
+      rl.close();
+    });
+  });
+}
+
+// Invoke the function to start the prompt
+askUserDetails();
