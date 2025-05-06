cc2304b9251ae0d7456354a58b9310ef8cce96c9
RyannKim327 <rksesgundo123@gmail.com>
2025-05-06 09:10:51 +0000

    5-6-2025 9:10 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- autogit.ts
+++ autogit.ts
@@ -1,24 +1,9 @@
-function maxSubArray(nums: number[]): { maxSum: number; start: number; end: number } {
-  let maxSum = nums[0];
-  let currentSum = 0;
-  let start = 0;
-  let tempStart = 0;
-  let end = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    currentSum += nums[i];
-
-    if (currentSum > maxSum) {
-      maxSum = currentSum;
-      start = tempStart;
-      end = i;
-    }
-
-    if (currentSum < 0) {
-      currentSum = 0;
-      tempStart = i + 1;
-    }
-  }
-
-  return { maxSum, start, end };
+function removeVowels(input: string): string {
+    return input.replace(/[aeiouAEIOU]/g, '');
 }
+
+// Example usage:
+const originalString = "Hello, World!";
+const stringWithoutVowels = removeVowels(originalString);
+
+console.log(stringWithoutVowels); // "Hll, Wrld!"
