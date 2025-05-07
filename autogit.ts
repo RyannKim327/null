f1a5c14ce53a902e5950190dbec4ead0722a2df9
RyannKim327 <rksesgundo123@gmail.com>
2025-05-07 00:39:43 +0000

    5-7-2025 0:39 [Paano yan, lugi nanaman tayo]

--- autogit.ts
+++ autogit.ts
@@ -1,30 +1,3 @@
-function largestPrimeFactor(n: number): number {
-    let number = n;
-    let maxPrime = -1;
-
-    // Check for the number of 2s that divide n
-    while (number % 2 === 0) {
-        maxPrime = 2;
-        number /= 2;
-    }
-
-    // n must be odd at this point
-    // Check for odd factors from 3 to the square root of n
-    for (let i = 3; i * i <= number; i += 2) {
-        while (number % i === 0) {
-            maxPrime = i;
-            number /= i;
-        }
-    }
-
-    // This condition is to check if n is a prime number greater than 2
-    if (number > 2) {
-        maxPrime = number;
-    }
-
-    return maxPrime;
+function removeAllWhitespace(input: string): string {
+  return input.replace(/\s+/g, '');
 }
-
-// Example usage:
-const number = 13195;
-console.log(`The largest prime factor of ${number} is: ${largestPrimeFactor(number)}`);
