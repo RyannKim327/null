490d5247a09836c0224eed4a872fddbfca243f00
RyannKim327 <rksesgundo123@gmail.com>
2025-05-07 06:12:08 +0000

    5-7-2025 6:12 [Nagcommit na din naman ako sa kanya, pero ayaw lang talaga nya]

--- autogit.ts
+++ autogit.ts
@@ -1,98 +1,39 @@
-class PriorityQueue<T> {
-  private heap: T[] = [];
-  private compare: (a: T, b: T) => boolean;
+function topologicalSort(graph: Map<number, number[]>): number[] {
+    const visited = new Set<number>();
+    const stack: number[] = [];
+    const tempMark = new Set<number>(); // To detect cycles
 
-  constructor(compareFn: (a: T, b: T) => boolean) {
-    this.compare = compareFn;
-  }
-
-  private swap(i: number, j: number) {
-    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-  }
-
-  private parentIndex(index: number): number {
-    return Math.floor((index - 1) / 2);
-  }
-
-  private leftChildIndex(index: number): number {
-    return index * 2 + 1;
-  }
-
-  private rightChildIndex(index: number): number {
-    return index * 2 + 2;
-  }
-
-  private siftUp(index: number): void {
-    let parent = this.parentIndex(index);
-    while (
-      index > 0 &&
-      this.compare(this.heap[index], this.heap[parent])
-    ) {
-      this.swap(index, parent);
-      index = parent;
-      parent = this.parentIndex(index);
-    }
-  }
-
-  private siftDown(index: number): void {
-    let left = this.leftChildIndex(index);
-    let right = this.rightChildIndex(index);
-    let next = index;
-
-    if (
-      left < this.heap.length &&
-      this.compare(this.heap[left], this.heap[next])
-    ) {
-      next = left;
+    function visit(node: number) {
+        if (tempMark.has(node)) {
+            throw new Error("Graph has a cycle, topological sort not possible");
+        }
+        if (!visited.has(node)) {
+            tempMark.add(node);
+            const neighbors = graph.get(node) || [];
+            for (const neighbor of neighbors) {
+                visit(neighbor);
+            }
+            tempMark.delete(node);
+            visited.add(node);
+            stack.push(node);
+        }
     }
 
-    if (
-      right < this.heap.length &&
-      this.compare(this.heap[right], this.heap[next])
-    ) {
-      next = right;
+    for (const node of graph.keys()) {
+        if (!visited.has(node)) {
+            visit(node);
+        }
     }
 
-    if (next !== index) {
-      this.swap(index, next);
-      this.siftDown(next);
-    }
-  }
-
-  public enqueue(item: T): void {
-    this.heap.push(item);
-    this.siftUp(this.heap.length - 1);
-  }
-
-  public dequeue(): T | undefined {
-    if (this.heap.length === 0) return undefined;
-
-    const root = this.heap[0];
-    const last = this.heap.pop()!;
-    if (this.heap.length > 0) {
-      this.heap[0] = last;
-      this.siftDown(0);
-    }
-    return root;
-  }
-
-  public peek(): T | undefined {
-    return this.heap[0];
-  }
-
-  public size(): number {
-    return this.heap.length;
-  }
-
-  public isEmpty(): boolean {
-    return this.heap.length === 0;
-  }
+    return stack.reverse();
 }
+const graph = new Map<number, number[]>();
+graph.set(5, [2, 0]);
+graph.set(4, [0, 1]);
+graph.set(2, [3]);
+graph.set(3, [1]);
+graph.set(0, []);
+graph.set(1, []);
 
-// Example usage for a min-heap of numbers:
-const minPQ = new PriorityQueue<number>((a, b) => a < b);
-minPQ.enqueue(5);
-minPQ.enqueue(3);
-minPQ.enqueue(6);
-console.log(minPQ.dequeue()); // 3
-console.log(minPQ.peek());    // 5
+const sorted = topologicalSort(graph);
+console.log(sorted); // Example output: [5, 4, 2, 3, 1, 0]
