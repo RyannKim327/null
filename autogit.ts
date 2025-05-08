90761013c840b6b4f08213944aa17a2ee3fb641b
RyannKim327 <rksesgundo123@gmail.com>
2025-05-08 21:02:53 +0000

    5-8-2025 21:2 [Saan aabot ang bente pesos mo?]

--- autogit.ts
+++ autogit.ts
@@ -1,14 +1,27 @@
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+function maxSubArray(nums: number[]): { maxSum: number, start: number, end: number } {
+  let maxSum = nums[0];
+  let currentSum = nums[0];
+  let start = 0;
+  let maxStart = 0;
+  let maxEnd = 0;
 
-const commonElements = arr1.filter(element => arr2.includes(element));
+  for (let i = 1; i < nums.length; i++) {
+    if (currentSum + nums[i] < nums[i]) {
+      currentSum = nums[i];
+      start = i;
+    } else {
+      currentSum += nums[i];
+    }
 
-console.log(commonElements); // Output: [3, 4]
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+    if (currentSum > maxSum) {
+      maxSum = currentSum;
+      maxStart = start;
+      maxEnd = i;
+    }
+  }
 
-const set2 = new Set(arr2);
-
-const commonElements = arr1.filter(element => set2.has(element));
-
-console.log(commonElements); // Output: [3, 4]
+  return { maxSum, start: maxStart, end: maxEnd };
+}
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const result = maxSubArray(arr);
+console.log(result); // { maxSum: 6, start: 3, end: 6 }
