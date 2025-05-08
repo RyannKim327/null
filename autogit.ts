74de60921e62df0d6f2b167ce70d215b6ca849a5
RyannKim327 <rksesgundo123@gmail.com>
2025-05-08 07:00:41 +0000

    5-8-2025 7:0 [Buti na lang umiwas na, kasi meron na pala]

--- autogit.ts
+++ autogit.ts
@@ -1,21 +1,6 @@
-function quicksort(arr: number[]): number[] {
-  if (arr.length <= 1) {
-    return arr; // Base case: arrays with 0 or 1 element are already sorted
-  }
-
-  const pivot = arr[arr.length - 1]; // Choose the last element as pivot
-  const left: number[] = [];
-  const right: number[] = [];
-
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
-    }
-  }
-
-  return [...quicksort(left), pivot, ...quicksort(right)];
+function removeVowels(input: string): string {
+  return input.replace(/[aeiouAEIOU]/g, '');
 }
-const sorted = quicksort([3, 6, 1, 8, 4, 5]);
-console.log(sorted); // [1, 3, 4, 5, 6, 8]
+
+const example = "Hello, TypeScript!";
+console.log(removeVowels(example)); // "Hll, TypScrpt!"
