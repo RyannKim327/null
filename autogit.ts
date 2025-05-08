09c2b5ce5a538b0df91a351718de726555e3691e
RyannKim327 <rksesgundo123@gmail.com>
2025-05-08 17:03:49 +0000

    5-8-2025 17:3 [Buti na lang umiwas na, kasi meron na pala]

--- autogit.ts
+++ autogit.ts
@@ -1,36 +1,27 @@
-function isPalindrome(s: string): boolean {
-  // Normalize the string: remove non-alphanumeric and case-insensitive
-  // This can be done in place with pointers (more on that below)
-  
-  let left = 0;
-  let right = s.length - 1;
+import axios from 'axios';
 
-  while (left < right) {
-    // Move left pointer to the next alphanumeric character
-    while (left < right && !isAlphaNumeric(s[left])) {
-      left++;
-    }
-    // Move right pointer to the previous alphanumeric character
-    while (left < right && !isAlphaNumeric(s[right])) {
-      right--;
-    }
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
 
-    // Compare characters case-insensitively
-    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-      return false;
+async function fetchUsers(): Promise<void> {
+  try {
+    const response = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users');
+    const users = response.data;
+
+    users.forEach(user => {
+      console.log(`${user.name} (${user.username}) - ${user.email}`);
+    });
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(`Axios error: ${error.message}`);
+    } else {
+      console.error(`Unexpected error: ${error}`);
     }
-    left++;
-    right--;
   }
-
-  return true;
 }
 
-function isAlphaNumeric(char: string): boolean {
-  const code = char.charCodeAt(0);
-  return (
-    (code >= 48 && code <= 57) || // '0'-'9'
-    (code >= 65 && code <= 90) || // 'A'-'Z'
-    (code >= 97 && code <= 122)   // 'a'-'z'
-  );
-}
+fetchUsers();
