900de04f77a8f6f8d4a4eab30a21b85ef337a901
RyannKim327 <rksesgundo123@gmail.com>
2025-05-08 21:00:59 +0000

    5-8-2025 21:0 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- autogit.ts
+++ autogit.ts
@@ -1,17 +1,6 @@
-function isPrime(num: number): boolean {
-  if (num <= 1) return false; // 1 and below are not prime
-  if (num <= 3) return true;  // 2 and 3 are prime
-
-  if (num % 2 === 0 || num % 3 === 0) return false;
-
-  // Check for factors from 5 to sqrt(num)
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-
-  return true;
-}
-console.log(isPrime(11));  // true
-console.log(isPrime(15));  // false
-console.log(isPrime(2));   // true
-console.log(isPrime(1));   // false
+let str1 = "Hello, ";
+let str2 = "world!";
+let result = str1 + str2;  // "Hello, world!"
+let str1 = "Hello, ";
+let str2 = "world!";
+let result = `${str1}${str2}`;  // "Hello, world!"
