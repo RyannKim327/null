57ec15524a15217047fcede6fb4b71ed0fda8443
RyannKim327 <rksesgundo123@gmail.com>
2025-05-09 05:06:28 +0000

    5-9-2025 5:5 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- autogit.ts
+++ autogit.ts
@@ -1,48 +1,8 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  
-  constructor(val: number) {
-    this.val = val;
-    this.next = null;
-  }
+function reverseString(input: string): string {
+    return input.split('').reverse().join('');
 }
 
-function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
-  if (!headA || !headB) return null;
-
-  // Helper to get length of list
-  function getLength(head: ListNode | null): number {
-    let length = 0;
-    let current = head;
-    while (current) {
-      length++;
-      current = current.next;
-    }
-    return length;
-  }
-
-  let lenA = getLength(headA);
-  let lenB = getLength(headB);
-
-  // Align both heads
-  while (lenA > lenB) {
-    headA = headA!.next;
-    lenA--;
-  }
-  while (lenB > lenA) {
-    headB = headB!.next;
-    lenB--;
-  }
-
-  // Traverse both together
-  while (headA && headB) {
-    if (headA === headB) {
-      return headA; // Intersection found
-    }
-    headA = headA.next;
-    headB = headB.next;
-  }
-
-  return null; // No intersection
-}
+// Example usage
+const originalString = "Hello, World!";
+const reversedString = reverseString(originalString);
+console.log(reversedString); // Output: "!dlroW ,olleH"
