2795da3d804b8ba5188320cb5f47c875ec8ef141
RyannKim327 <rksesgundo123@gmail.com>
2025-05-09 16:11:16 +0000

    5-9-2025 16:9 [Sanaol Santol]

--- autogit.ts
+++ autogit.ts
@@ -1,75 +1,90 @@
-// Node class represents each element in the linked list
-class Node<T> {
-  value: T;
-  next: Node<T> | null = null;
+class PriorityQueue<T> {
+  private heap: T[] = [];
+  private compare: (a: T, b: T) => boolean;
 
-  constructor(value: T) {
-    this.value = value;
+  // compare defines the priority order; default is min-heap
+  constructor(compareFn?: (a: T, b: T) => boolean) {
+    this.compare = compareFn || ((a, b) => a < b);
+  }
+
+  private swap(i: number, j: number) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  private parent(index: number) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  private leftChild(index: number) {
+    return 2 * index + 1;
+  }
+
+  private rightChild(index: number) {
+    return 2 * index + 2;
+  }
+
+  private siftUp(index: number) {
+    let parent = this.parent(index);
+    while (index > 0 && this.compare(this.heap[index], this.heap[parent])) {
+      this.swap(index, parent);
+      index = parent;
+      parent = this.parent(index);
+    }
+  }
+
+  private siftDown(index: number) {
+    let left = this.leftChild(index);
+    let right = this.rightChild(index);
+    let smallest = index;
+
+    if (left < this.heap.length && this.compare(this.heap[left], this.heap[smallest])) {
+      smallest = left;
+    }
+    if (right < this.heap.length && this.compare(this.heap[right], this.heap[smallest])) {
+      smallest = right;
+    }
+    if (smallest !== index) {
+      this.swap(index, smallest);
+      this.siftDown(smallest);
+    }
+  }
+
+  public enqueue(element: T) {
+    this.heap.push(element);
+    this.siftUp(this.heap.length - 1);
+  }
+
+  public dequeue(): T | undefined {
+    if (this.isEmpty()) return undefined;
+
+    const root = this.heap[0];
+    const last = this.heap.pop()!;
+    if (this.heap.length > 0) {
+      this.heap[0] = last;
+      this.siftDown(0);
+    }
+    return root;
+  }
+
+  public peek(): T | undefined {
+    return this.heap[0];
+  }
+
+  public size(): number {
+    return this.heap.length;
+  }
+
+  public isEmpty(): boolean {
+    return this.heap.length === 0;
   }
 }
+const pq = new PriorityQueue<number>();
 
-class Queue<T> {
-  private head: Node<T> | null = null;
-  private tail: Node<T> | null = null;
-  private size: number = 0;
+pq.enqueue(10);
+pq.enqueue(5);
+pq.enqueue(20);
+pq.enqueue(1);
 
-  // Add to the end (tail) of the queue
-  enqueue(value: T): void {
-    const newNode = new Node(value);
-
-    if (!this.tail) {
-      // If queue is empty, head and tail are the same
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      // Link the new node to the end and move the tail pointer
-      this.tail.next = newNode;
-      this.tail = newNode;
-    }
-
-    this.size++;
-  }
-
-  // Remove from the front (head) of the queue
-  dequeue(): T | null {
-    if (!this.head) {
-      return null; // Queue is empty
-    }
-
-    const value = this.head.value;
-    this.head = this.head.next;
-    this.size--;
-
-    if (!this.head) {
-      // If queue becomes empty, reset tail to null as well
-      this.tail = null;
-    }
-
-    return value;
-  }
-
-  // Check the element at the front of the queue without removing it
-  peek(): T | null {
-    return this.head ? this.head.value : null;
-  }
-
-  // Check if the queue is empty
-  isEmpty(): boolean {
-    return this.size === 0;
-  }
-
-  // Get the size of the queue
-  getSize(): number {
-    return this.size;
-  }
+while (!pq.isEmpty()) {
+  console.log(pq.dequeue());  // Output: 1, 5, 10, 20
 }
-const queue = new Queue<number>();
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
-
-console.log(queue.peek()); // 10
-console.log(queue.dequeue()); // 10
-console.log(queue.dequeue()); // 20
-console.log(queue.getSize()); // 1
-console.log(queue.isEmpty()); // false
