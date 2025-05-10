5cf9123fa9357d98ce1ca9a2c7fb42a8fa893bb9
RyannKim327 <rksesgundo123@gmail.com>
2025-05-10 20:02:00 +0000

    5-10-2025 20:1 [Saan aabot ang bente pesos mo?]

--- autogit.ts
+++ autogit.ts
@@ -1,27 +1,9 @@
-function longestCommonSubstring(s1: string, s2: string): string {
-  const m = s1.length;
-  const n = s2.length;
-
-  // Create a 2D array filled with zeros
-  const dp: number[][] = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
-  let maxLength = 0;     // Max length of common substring found
-  let maxEndIndex = 0;   // Ending index of the longest common substring in s1
-
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (s1[i - 1] === s2[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-
-        if (dp[i][j] > maxLength) {
-          maxLength = dp[i][j];
-          maxEndIndex = i;  // Store the end index in s1
-        }
-      }
-    }
-  }
-
-  // Extract the longest common substring from s1 using maxLength and maxEndIndex
-  return s1.substring(maxEndIndex - maxLength, maxEndIndex);
-}
-console.log(longestCommonSubstring("abcde", "abfce"));  // Output: "bc"
-console.log(longestCommonSubstring("abcdef", "zabcf")); // Output: "abc"
+const str = "123";
+const num = parseInt(str, 10);  // the second argument is the radix (base)
+console.log(num);  // 123
+const str = "123";
+const num = +str;
+console.log(num);  // 123
+const str = "123";
+const num = Number(str);
+console.log(num);  // 123
