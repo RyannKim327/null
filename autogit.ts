72e288704ea6ab95e90b33ea105614bb91be25af
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 18:03:45 +0000

    5-11-2025 18:2 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- autogit.ts
+++ autogit.ts
@@ -1,76 +1,21 @@
-class Graph {
-  adjacencyList: Map<number, number[]>;
+function findSecondLargest(arr: number[]): number | undefined {
+    if (arr.length < 2) return undefined; // Not enough elements
+    
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
 
-  constructor() {
-    this.adjacencyList = new Map();
-  }
-
-  addEdge(from: number, to: number) {
-    if (!this.adjacencyList.has(from)) {
-      this.adjacencyList.set(from, []);
-    }
-    this.adjacencyList.get(from)!.push(to);
-  }
-
-  tarjanSCC(): number[][] {
-    const adjacencyList = this.adjacencyList;
-    const stack: number[] = [];
-    const onStack: Set<number> = new Set();
-    const ids: Map<number, number> = new Map();
-    const lowLinks: Map<number, number> = new Map();
-    const sccs: number[][] = [];
-    let id = 0;
-
-    function dfs(at: number) {
-      ids.set(at, id);
-      lowLinks.set(at, id);
-      id++;
-      stack.push(at);
-      onStack.add(at);
-
-      const neighbors = adjacencyList.get(at) || [];
-      neighbors.forEach(to => {
-        if (!ids.has(to)) {
-          dfs(to);
-          lowLinks.set(at, Math.min(lowLinks.get(at)!, lowLinks.get(to)!));
-        } else if (onStack.has(to)) {
-          lowLinks.set(at, Math.min(lowLinks.get(at)!, ids.get(to)!));
+    for (const num of arr) {
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num > secondLargest && num < largest) {
+            secondLargest = num;
         }
-      });
-
-      // On recursive callback, if we're at root node (start of SCC)
-      if (ids.get(at) === lowLinks.get(at)) {
-        const scc: number[] = [];
-        let node: number;
-
-        do {
-          node = stack.pop()!;
-          onStack.delete(node);
-          scc.push(node);
-        } while (node !== at);
-
-        sccs.push(scc);
-      }
     }
 
-    // Run DFS from every node that hasn't been visited
-    for (const node of adjacencyList.keys()) {
-      if (!ids.has(node)) {
-        dfs(node);
-      }
-    }
-
-    return sccs;
-  }
+    return secondLargest === -Infinity ? undefined : secondLargest;
 }
 
-// Example usage
-const graph = new Graph();
-graph.addEdge(1, 2);
-graph.addEdge(2, 3);
-graph.addEdge(3, 1);
-graph.addEdge(3, 4);
-
-const sccs = graph.tarjanSCC();
-console.log(sccs); 
-// Output: [[4], [1, 3, 2]] -- the strongly connected components in the graph
+// Example usage:
+const arr = [3, 5, 1, 2, 4];
+console.log(findSecondLargest(arr)); // Output: 4
