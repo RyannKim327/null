f594fc31dcbf6d23bbdccaf5b8eb4ffdfdb4d302
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 08:12:00 +0000

    5-11-2025 8:10 [Nagcommit na din naman ako sa kanya, pero ayaw lang talaga nya]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,22 @@
-// May error sa API
+class ListNode<T> {
+  value: T;
+  next: ListNode<T> | null;
+
+  constructor(value: T, next: ListNode<T> | null = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+function findMiddleNode<T>(head: ListNode<T> | null): T | null {
+  if (!head) return null;
+
+  let slow: ListNode<T> | null = head;
+  let fast: ListNode<T> | null = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow!.next;
+    fast = fast.next.next;
+  }
+
+  return slow ? slow.value : null;
+}
