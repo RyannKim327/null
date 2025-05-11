2fa21c4cf3f874815c98da107bb75e536b5282b5
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 00:47:19 +0000

    5-11-2025 0:47 [Pinasaasa nya lang ako.]

--- autogit.ts
+++ autogit.ts
@@ -1,148 +1,53 @@
-enum Color {
-  RED,
-  BLACK,
-}
+class HashTable<K, V> {
+  private buckets: Array<Array<[K, V]>>;
+  private capacity: number;
 
-class Node<T> {
-  value: T;
-  color: Color;
-  left: Node<T> | null = null;
-  right: Node<T> | null = null;
-  parent: Node<T> | null = null;
-
-  constructor(value: T, color: Color = Color.RED) {
-    this.value = value;
-    this.color = color;
+  constructor(capacity: number = 42) {
+    this.capacity = capacity;
+    this.buckets = Array.from({ length: capacity }, () => []);
   }
 
-  get isRed(): boolean {
-    return this.color === Color.RED;
+  // Simple hash function for string keys
+  private hash(key: K): number {
+    if (typeof key !== 'string') {
+      throw new Error('Only string keys are supported');
+    }
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash << 5) - hash + key.charCodeAt(i);
+      hash |= 0; // Convert to 32bit integer
+    }
+    return Math.abs(hash) % this.capacity;
   }
 
-  get isBlack(): boolean {
-    return this.color === Color.BLACK;
-  }
-}
-class RedBlackTree<T> {
-  root: Node<T> | null = null;
+  set(key: K, value: V): void {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const pairIndex = bucket.findIndex(([k]) => k === key);
 
-  private leftRotate(x: Node<T>) {
-    const y = x.right!;
-    x.right = y.left;
-    if (y.left) y.left.parent = x;
-    y.parent = x.parent;
-
-    if (!x.parent) {
-      this.root = y;
-    } else if (x === x.parent.left) {
-      x.parent.left = y;
+    if (pairIndex >= 0) {
+      bucket[pairIndex][1] = value; // Update existing value
     } else {
-      x.parent.right = y;
+      bucket.push([key, value]); // Insert new pair
     }
-
-    y.left = x;
-    x.parent = y;
   }
 
-  private rightRotate(y: Node<T>) {
-    const x = y.left!;
-    y.left = x.right;
-    if (x.right) x.right.parent = y;
-    x.parent = y.parent;
-
-    if (!y.parent) {
-      this.root = x;
-    } else if (y === y.parent.left) {
-      y.parent.left = x;
-    } else {
-      y.parent.right = x;
-    }
-
-    x.right = y;
-    y.parent = x;
-  }
-  insert(value: T) {
-    const newNode = new Node(value);
-    if (!this.root) {
-      this.root = newNode;
-      this.root.color = Color.BLACK;
-      return;
-    }
-
-    let current: Node<T> | null = this.root;
-    let parent: Node<T> | null = null;
-
-    while (current) {
-      parent = current;
-      if (value < current.value) {
-        current = current.left;
-      } else {
-        current = current.right;
-      }
-    }
-
-    newNode.parent = parent;
-    if (value < parent!.value) {
-      parent!.left = newNode;
-    } else {
-      parent!.right = newNode;
-    }
-
-    this.insertFixup(newNode);
+  get(key: K): V | undefined {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const pair = bucket.find(([k]) => k === key);
+    return pair ? pair[1] : undefined;
   }
 
-  private insertFixup(node: Node<T>) {
-    while (node.parent && node.parent.color === Color.RED) {
-      const parent = node.parent;
-      const grandparent = parent.parent;
+  remove(key: K): boolean {
+    const index = this.hash(key);
+    const bucket = this.buckets[index];
+    const pairIndex = bucket.findIndex(([k]) => k === key);
 
-      if (!grandparent) break;
-
-      if (parent === grandparent.left) {
-        const uncle = grandparent.right;
-
-        if (uncle && uncle.color === Color.RED) {
-          // Case 1: Uncle is red - recolor
-          parent.color = Color.BLACK;
-          uncle.color = Color.BLACK;
-          grandparent.color = Color.RED;
-          node = grandparent;
-        } else {
-          // Case 2 & 3: Uncle black
-          if (node === parent.right) {
-            node = parent;
-            this.leftRotate(node);
-          }
-          parent.color = Color.BLACK;
-          grandparent.color = Color.RED;
-          this.rightRotate(grandparent);
-        }
-      } else {
-        // parent is the right child of grandparent
-        const uncle = grandparent.left;
-
-        if (uncle && uncle.color === Color.RED) {
-          // Case 1 mirror
-          parent.color = Color.BLACK;
-          uncle.color = Color.BLACK;
-          grandparent.color = Color.RED;
-          node = grandparent;
-        } else {
-          if (node === parent.left) {
-            node = parent;
-            this.rightRotate(node);
-          }
-          parent.color = Color.BLACK;
-          grandparent.color = Color.RED;
-          this.leftRotate(grandparent);
-        }
-      }
+    if (pairIndex >= 0) {
+      bucket.splice(pairIndex, 1);
+      return true;
     }
-    this.root!.color = Color.BLACK;
+    return false;
   }
 }
-const tree = new RedBlackTree<number>();
-tree.insert(10);
-tree.insert(20);
-tree.insert(30);
-tree.insert(15);
