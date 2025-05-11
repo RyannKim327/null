8e9b4eb1c666e2e0e5454efb4ba6669e08d6728a
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 13:18:18 +0000

    5-11-2025 13:17 [Sorry kung isang araw, sino ka na lang sa akin]

--- autogit.ts
+++ autogit.ts
@@ -1,15 +1,1 @@
-const numbers = [3, 7, 2, 9, 5];
-const max = Math.max(...numbers);
-console.log(max); // 9
-const numbers = [3, 7, 2, 9, 5];
-let max = numbers[0];
-
-for (let i = 1; i < numbers.length; i++) {
-  if (numbers[i] > max) {
-    max = numbers[i];
-  }
-}
-console.log(max); // 9
-const numbers = [3, 7, 2, 9, 5];
-const max = numbers.reduce((a, b) => (a > b ? a : b));
-console.log(max); // 9
+// May error sa API
