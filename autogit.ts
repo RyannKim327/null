0c0f252fc8945c3e76a6f08ecf434e767f0107cc
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 03:36:57 +0000

    5-11-2025 3:36 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- autogit.ts
+++ autogit.ts
@@ -1,13 +1,9 @@
-function isPrime(num: number): boolean {
-  if (num <= 1) return false; // 0 and 1 aren't prime
-  if (num <= 3) return true;  // 2 and 3 are prime
-
-  if (num % 2 === 0 || num % 3 === 0) return false; // eliminate multiples of 2 and 3
-
-  // Check divisors up to the square root of num
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-
-  return true;
+const decimalNumber = 42;
+const binaryString = decimalNumber.toString(2);
+console.log(binaryString); // Output: "101010"
+function decimalToBinary(num: number): string {
+  return num.toString(2);
 }
+
+// Usage
+console.log(decimalToBinary(255)); // "11111111"
