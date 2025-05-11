8fd878073c6d07f1b77de3561779f4bcf5009351
RyannKim327 <rksesgundo123@gmail.com>
2025-05-11 07:03:35 +0000

    5-11-2025 7:2 [Wag nang umulit, at mukhang hindi na ako uulit pa]

--- autogit.ts
+++ autogit.ts
@@ -1,17 +1,5 @@
-const originalArray = [1, 2, 3, 4, 3];
-const valueToRemove = 3;
-const newArray = originalArray.filter(element => element !== valueToRemove);
-console.log(newArray); // [1, 2, 4]
-const originalArray = [1, 2, 3, 4, 3];
-const valueToRemove = 3;
-
-const index = originalArray.indexOf(valueToRemove);
-if (index > -1) {
-  originalArray.splice(index, 1); // modifies original array
-}
-console.log(originalArray); // [1, 2, 4, 3]
-const originalArray = ['a', 'b', 'c', 'd'];
-const indexToRemove = 2;
-
-originalArray.splice(indexToRemove, 1);
-console.log(originalArray); // ['a', 'b', 'd']
+const original = "  Hello  world \n\t ";
+const noWhitespace = original.replace(/\s+/g, '');
+console.log(noWhitespace); // "Helloworld"
+const trimmed = original.trim();
+console.log(trimmed); // "Hello  world"
