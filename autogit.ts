a6690d77f324f2007f55506c7d57a3a32768f7b0
RyannKim327 <rksesgundo123@gmail.com>
2025-05-12 08:03:13 +0000

    5-12-2025 8:2 [Sanaol Santol]

--- autogit.ts
+++ autogit.ts
@@ -1,17 +1,4 @@
-function isPrime(num: number): boolean {
-  if (num <= 1) return false;       // 0 and 1 are not prime
-  if (num <= 3) return true;        // 2 and 3 are prime
-
-  if (num % 2 === 0 || num % 3 === 0) return false;
-
-  // Check divisors up to square root of num
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-
-  return true;
-}
-console.log(isPrime(7));   // true
-console.log(isPrime(15));  // false
-console.log(isPrime(97));  // true
-console.log(isPrime(1));   // false
+const originalString = "This is a string with spaces";
+const stringWithoutSpaces = originalString.replace(/\s+/g, '');
+console.log(stringWithoutSpaces); // "Thisisastringwithspaces"
+const stringWithoutSpaces = originalString.replace(/ /g, '');
