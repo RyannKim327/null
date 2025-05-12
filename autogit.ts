6452a71d132b2e0e6185fe0356c2aa43c97a5213
RyannKim327 <rksesgundo123@gmail.com>
2025-05-12 09:10:58 +0000

    5-12-2025 9:10 [Paano yan, lugi nanaman tayo]

--- autogit.ts
+++ autogit.ts
@@ -1,15 +1,21 @@
-let arr = [1, 2, 3, 2, 4];
-let valueToRemove = 2;
-arr = arr.filter(item => item !== valueToRemove);
-console.log(arr); // [1, 3, 4]
-let arr = [1, 2, 3, 2, 4];
-let valueToRemove = 2;
-const index = arr.indexOf(valueToRemove);
-if (index !== -1) {
-  arr.splice(index, 1);
+class ListNode<T> {
+  value: T;
+  next: ListNode<T> | null;
+
+  constructor(value: T, next: ListNode<T> | null = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
-console.log(arr); // [1, 3, 2, 4]
-let arr = ['a', 'b', 'c', 'd'];
-const indexToRemove = 2; // removes 'c'
-arr.splice(indexToRemove, 1);
-console.log(arr); // ['a', 'b', 'd']
+
+function getLength<T>(head: ListNode<T> | null): number {
+  let count = 0;
+  let current = head;
+
+  while (current !== null) {
+    count++;
+    current = current.next;
+  }
+
+  return count;
+}
