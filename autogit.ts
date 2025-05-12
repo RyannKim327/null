c0961a572de039dc850be6d683c2adf07dadf581
RyannKim327 <rksesgundo123@gmail.com>
2025-05-12 21:02:08 +0000

    5-12-2025 21:1 [Mukhang kailangan ko nang i-practice na si sya ma miss]

--- autogit.ts
+++ autogit.ts
@@ -1,6 +1,22 @@
-const decimalNumber = 42;
-const binaryString = decimalNumber.toString(2);
-console.log(binaryString); // Output: "101010"
-const decimalNumber = 42;
-const binaryString = decimalNumber.toString(2).padStart(8, '0');
-console.log(binaryString); // Output: "00101010"
+const arr = [1, 2, 3, 2, 4];
+const valueToRemove = 2;
+
+const filteredArr = arr.filter(element => element !== valueToRemove);
+console.log(filteredArr); // [1, 3, 4]
+const arr = [1, 2, 3, 4];
+const indexToRemove = 2;
+
+if (indexToRemove > -1 && indexToRemove < arr.length) {
+  arr.splice(indexToRemove, 1);
+}
+
+console.log(arr); // [1, 2, 4]
+const arr = [1, 2, 3, 2, 4];
+const valueToRemove = 2;
+
+const index = arr.indexOf(valueToRemove);
+if (index !== -1) {
+  arr.splice(index, 1);
+}
+
+console.log(arr); // [1, 3, 2, 4]
