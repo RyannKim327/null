92828e1f39745f205b44e19682201ca3f41532ad
RyannKim327 <rksesgundo123@gmail.com>
2025-05-12 07:03:22 +0000

    5-12-2025 7:3 [Sanaol Santol]

--- autogit.ts
+++ autogit.ts
@@ -1,16 +1,8 @@
-function longestCommonPrefix(strs: string[]): string {
-  if (strs.length === 0) return "";
-
-  for (let i = 0; i < strs[0].length; i++) {
-    const char = strs[0][i];
-    for (let j = 1; j < strs.length; j++) {
-      if (i >= strs[j].length || strs[j][i] !== char) {
-        return strs[0].slice(0, i);
-      }
-    }
-  }
-
-  return strs[0];
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-const strings = ["flower", "flow", "flight"];
-console.log(longestCommonPrefix(strings)); // Outputs: "fl"
+function getRandomFloat(min: number, max: number): number {
+  return Math.random() * (max - min) + min;
+}
