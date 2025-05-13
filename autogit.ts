b334324d452e77ca5d56bc56b12e7a72851f7e0b
RyannKim327 <rksesgundo123@gmail.com>
2025-05-13 14:00:44 +0000

    5-13-2025 14:0 [Hindi naman daw ako pangit, pero ayaw nya kaya wag na]

--- autogit.ts
+++ autogit.ts
@@ -1,13 +1,7 @@
-function isPrime(num: number): boolean {
-  if (num <= 1) return false; // 0 and 1 are not prime
-  if (num <= 3) return true;  // 2 and 3 are prime
-
-  if (num % 2 === 0 || num % 3 === 0) return false;
-
-  // only check factors up to the square root of num
-  for (let i = 5; i * i <= num; i += 6) {
-    if (num % i === 0 || num % (i + 2) === 0) return false;
-  }
-
-  return true;
+const str = "Hello world, how are you?";
+const noSpaces = str.replace(/\s+/g, '');
+console.log(noSpaces); // "Helloworld,howareyou?"
+const noSpaces = str.replace(/ /g, '');
+function removeSpaces(input: string): string {
+  return input.replace(/\s+/g, '');
 }
