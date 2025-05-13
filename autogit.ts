b04725281fcb9d3e01cbae207ff1a1b34b664a7c
RyannKim327 <rksesgundo123@gmail.com>
2025-05-13 00:41:21 +0000

    5-13-2025 0:41 [May pinagsamahan man tayo, pero tingin ko wala na un]

--- autogit.ts
+++ autogit.ts
@@ -1,1 +1,15 @@
-// May error sa API
+const decimalNumber: number = 42;
+const binaryString: string = decimalNumber.toString(2);
+
+console.log(binaryString); // Output: "101010"
+function decimalToBinary(decimal: number): string {
+    return decimal.toString(2);
+}
+
+console.log(decimalToBinary(42)); // "101010"
+console.log(decimalToBinary(15)); // "1111"
+function decimalToBinaryArray(decimal: number): number[] {
+    return decimal.toString(2).split('').map(bit => parseInt(bit, 10));
+}
+
+console.log(decimalToBinaryArray(42)); // [1, 0, 1, 0, 1, 0]
