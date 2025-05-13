1c4f914175fd0a9ef09fd6b669a9b0ebb52e4f8e
RyannKim327 <rksesgundo123@gmail.com>
2025-05-13 00:41:17 +0000

    5-13-2025 0:40 [Wag na ipaglaban kung gusto, basta masaya sya doon, don na sya]

--- autogit.ts
+++ autogit.ts
@@ -1,12 +1,1 @@
-let arr = [1, 5, 2, 5];
-const valueToRemove = 5;
-arr = arr.filter(item => item !== valueToRemove);
-console.log(arr); // [1, 2]
-const index = arr.indexOf(valueToRemove);
-if (index !== -1) {
-  arr.splice(index, 1);
-}
-console.log(arr); // removes just the first 5
-const indexToRemove = 2; // example
-arr.splice(indexToRemove, 1);
-console.log(arr);
+// May error sa API
