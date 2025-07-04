556e1eea9193afeb362208f15772fb087e42d8c8
RyannKim327 <rksesgundo123@gmail.com>
2025-07-04 22:02:46 +0000

    7-4-2025 22:2 [Buti na lang umiwas na, kasi meron na pala]

--- src/autogit.ts
+++ src/autogit.ts
@@ -1,29 +1,7 @@
-function bubbleSort(arr: number[]): number[] {
-    const n = arr.length;
-    let swapped: boolean;
-
-    for (let i = 0; i < n - 1; i++) {
-        swapped = false;
-
-        // Last i elements are already sorted
-        for (let j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
-                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-                swapped = true;
-            }
-        }
-
-        // If no two elements were swapped by inner loop, then break
-        if (!swapped) {
-            break;
-        }
-    }
-
-    return arr;
+function getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Example usage:
-const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
-console.log("Unsorted array:", unsortedArray);
-console.log("Sorted array:", bubbleSort(unsortedArray));
+const randomNum = getRandomNumber(1, 10);
+console.log(randomNum); // This will log a random number between 1 and 10 (inclusive).
